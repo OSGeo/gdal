fa82c0c44428fa813f7dfd89639c490d2a7f2ae5
Frank Warmerdam <warmerdam@pobox.com>
1999-02-04 22:15:44 +0000

    New
    git-svn-id: https://svn.osgeo.org/gdal/trunk@152 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ frmts/aigrid/aigdataset.cpp
@@ -0,0 +1,253 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  Arc/Info Binary Grid Driver
+ * Purpose:  Implements GDAL interface to underlying library.
+ * Author:   Frank Warmerdam, warmerda@home.com
+ *
+ ******************************************************************************
+ * Copyright (c) 1999, Frank Warmerdam
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *****************************************************************************
+ *
+ * $Log$
+ * Revision 1.1  1999/02/04 22:15:44  warmerda
+ * New
+ *
+ */
+
+#include "gdal_priv.h"
+#include "aigrid.h"
+
+CPL_C_START
+void	GDALRegister_AIGrid(void);
+CPL_C_END
+
+
+/************************************************************************/
+/* ==================================================================== */
+/*				AIGDataset				*/
+/* ==================================================================== */
+/************************************************************************/
+
+class AIGRasterBand;
+
+class CPL_DLL AIGDataset : public GDALDataset
+{
+    friend	AIGRasterBand;
+    
+    AIGInfo_t	*psInfo;
+
+  public:
+                AIGDataset();
+                ~AIGDataset();
+
+    static GDALDataset *Open( GDALOpenInfo * );
+
+    virtual CPLErr GetGeoTransform( double * );
+};
+
+/************************************************************************/
+/* ==================================================================== */
+/*                            AIGRasterBand                             */
+/* ==================================================================== */
+/************************************************************************/
+
+class AIGRasterBand : public GDALRasterBand
+{
+    friend	AIGDataset;
+
+  public:
+
+                   AIGRasterBand( AIGDataset *, int );
+
+    virtual CPLErr IReadBlock( int, int, void * );
+};
+
+static GDALDriver	*poAIGDriver = NULL;
+
+/************************************************************************/
+/*                           AIGRasterBand()                            */
+/************************************************************************/
+
+AIGRasterBand::AIGRasterBand( AIGDataset *poDS, int nBand )
+
+{
+    this->poDS = poDS;
+    this->nBand = nBand;
+
+    nBlockXSize = poDS->psInfo->nBlockXSize;
+    nBlockYSize = poDS->psInfo->nBlockYSize;
+    eDataType = GDT_Byte;
+}
+
+/************************************************************************/
+/*                             IReadBlock()                             */
+/************************************************************************/
+
+CPLErr AIGRasterBand::IReadBlock( int nBlockXOff, int nBlockYOff,
+                                  void * pImage )
+
+{
+    AIGDataset	*poODS = (AIGDataset *) poDS;
+    GUInt32	*panGridRaster;
+    int		i;
+
+    panGridRaster = (GUInt32 *) CPLMalloc(4*nBlockXSize*nBlockYSize);
+    if( AIGReadTile( poODS->psInfo, nBlockXOff, nBlockYOff, panGridRaster )
+        != CE_None )
+    {
+        CPLFree( panGridRaster );
+        return CE_Failure;
+    }
+
+    for( i = 0; i < nBlockXSize * nBlockYSize; i++ )
+    {
+        ((GByte *) pImage)[i] = panGridRaster[i];
+    }
+
+    CPLFree( panGridRaster );
+
+    return( CE_None );
+}
+
+
+/************************************************************************/
+/* ==================================================================== */
+/*                            AIGDataset                               */
+/* ==================================================================== */
+/************************************************************************/
+
+
+/************************************************************************/
+/*                            AIGDataset()                            */
+/************************************************************************/
+
+AIGDataset::AIGDataset()
+
+{
+    psInfo = NULL;
+}
+
+/************************************************************************/
+/*                           ~AIGDataset()                            */
+/************************************************************************/
+
+AIGDataset::~AIGDataset()
+
+{
+    if( psInfo != NULL )
+        AIGClose( psInfo );
+}
+
+
+/************************************************************************/
+/*                                Open()                                */
+/************************************************************************/
+
+GDALDataset *AIGDataset::Open( GDALOpenInfo * poOpenInfo )
+
+{
+    AIGInfo_t	*psInfo;
+    
+/* -------------------------------------------------------------------- */
+/*      Verify that this is a AIG file.                                 */
+/* -------------------------------------------------------------------- */
+    if( !poOpenInfo->bStatOK )
+        return NULL;
+
+    if( !VSI_ISDIR( poOpenInfo->sStat.st_mode ) )
+        return NULL;
+
+/* -------------------------------------------------------------------- */
+/*      Open the file.                                                  */
+/* -------------------------------------------------------------------- */
+    psInfo = AIGOpen( poOpenInfo->pszFilename, "r" );
+    if( psInfo == NULL )
+        return NULL;
+    
+/* -------------------------------------------------------------------- */
+/*      Create a corresponding GDALDataset.                             */
+/* -------------------------------------------------------------------- */
+    AIGDataset 	*poDS;
+
+    poDS = new AIGDataset();
+
+    poDS->psInfo = psInfo;
+    poDS->poDriver = poAIGDriver;
+
+/* -------------------------------------------------------------------- */
+/*      Establish raster info.                                          */
+/* -------------------------------------------------------------------- */
+    poDS->nRasterXSize = psInfo->nPixels;
+    poDS->nRasterYSize = psInfo->nLines;
+    poDS->nBands = 1;
+
+/* -------------------------------------------------------------------- */
+/*      Create band information objects.                                */
+/* -------------------------------------------------------------------- */
+    poDS->papoBands = (GDALRasterBand **)VSICalloc(sizeof(GDALRasterBand *),
+                                                   poDS->nBands);
+    poDS->papoBands[0] = new AIGRasterBand( poDS, 1 );
+
+    return( poDS );
+}
+
+
+/************************************************************************/
+/*                          GetGeoTransform()                           */
+/************************************************************************/
+
+CPLErr AIGDataset::GetGeoTransform( double * padfTransform )
+
+{
+    padfTransform[0] = psInfo->dfLLX;
+    padfTransform[1] = psInfo->dfCellSizeX;
+    padfTransform[2] = 0;
+
+    padfTransform[3] = psInfo->dfURY;
+    padfTransform[4] = 0;
+    padfTransform[5] = psInfo->dfCellSizeY;
+    
+    return( CE_Failure );
+}
+
+/************************************************************************/
+/*                          GDALRegister_AIG()                        */
+/************************************************************************/
+
+void GDALRegister_AIGrid()
+
+{
+    GDALDriver	*poDriver;
+
+    if( poAIGDriver == NULL )
+    {
+        poAIGDriver = poDriver = new GDALDriver();
+        
+        poDriver->pszShortName = "AIGrid";
+        poDriver->pszLongName = "Arc/Info Binary Grid";
+        
+        poDriver->pfnOpen = AIGDataset::Open;
+
+        GetGDALDriverManager()->RegisterDriver( poDriver );
+    }
+}
+
