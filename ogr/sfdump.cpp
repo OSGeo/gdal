94051fd5a3feeaa5c8ee661b55e718b6fa6eaff8
Frank Warmerdam <warmerdam@pobox.com>
1999-03-31 15:15:36 +0000

    Added command processing ... just use simple strings
    git-svn-id: https://svn.osgeo.org/gdal/trunk@249 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/sfdump.cpp
+++ ogr/sfdump.cpp
@@ -2,65 +2,109 @@
 #define DBINITCONSTANTS
 
 #include "oledb_sup.h"
+#include "ogr_geometry.h"
 
+// Get various classid.
 #include "msdaguid.h"
 #include "MSjetoledb.h"
+#include "sfclsid.h"
 
-static HRESULT SFDumpGeomColumn( IOpenRowset*, LPWSTR, const char * );
+static HRESULT SFDumpGeomColumn( IOpenRowset*, const char *, const char * );
+
+/************************************************************************/
+/*                               Usage()                                */
+/************************************************************************/
+
+static void Usage()
+
+{
+    printf( "Usage: sfdump [-provider provider_clsid_alias] [-ds datasource]\n"
+            "              [-table tablename] [-column geom_column_name]\n" );
+    exit( 1 );
+}
 
 /************************************************************************/
 /*                                main()                                */
 /************************************************************************/
 
-void main( int argc, char ** argv )
+void main( int nArgc, char ** papszArgv )
 {
-   REFCLSID    hProviderCLSID = CLSID_JETOLEDB_3_51;
-   LPWSTR      pwszDataSource = L"f:\\opengis\\SFCOMTestData\\World.mdb";
-   LPWSTR      pwszTable = L"worldmif_geometry";
-   const char *pszGeomColumn = "WKB_GEOMETRY";
-   HRESULT     hr;
-   IOpenRowset *pIOpenRowset = NULL;
+    CLSID       &hProviderCLSID = (CLSID) CLSID_JETOLEDB_3_51;
+    const char *pszDataSource = "f:\\opengis\\SFData\\World.mdb";
+    const char *pszTable = "worldmif_geometry";
+    const char *pszGeomColumn = "WKB_GEOMETRY";
+    HRESULT     hr;
+    IOpenRowset *pIOpenRowset = NULL;
    
 /* -------------------------------------------------------------------- */
 /*      Initialize OLE                                                  */
 /* -------------------------------------------------------------------- */
-   if( !OleSupInitialize() )
-   {
-      exit( 1 );
-   }
+    if( !OleSupInitialize() )
+    {
+        exit( 1 );
+    }
 
 /* -------------------------------------------------------------------- */
-/*      Process commandline switches ... nothing for now.  How do we    */
-/*      convert regular strings into LPWSTR's?                          */
+/*      Process commandline switches                                    */
 /* -------------------------------------------------------------------- */
-   /* .... */
+    for( int iArg = 1; iArg < nArgc; iArg++ )
+    {
+        if( iArg < nArgc - 1 && stricmp( papszArgv[iArg], "-provider") == 0 )
+        {
+            iArg++;
+            if( stricmp(papszArgv[iArg],"Cadcorp") == 0 )
+                hProviderCLSID = CLSID_CadcorpSFProvider;
+            else
+                /* need generic translator */;
+        }
+
+        else if( iArg < nArgc-1 && stricmp( papszArgv[iArg],"-ds") == 0 )
+        {
+            pszDataSource = papszArgv[++iArg];
+        }
+
+        else if( iArg < nArgc-1 && stricmp( papszArgv[iArg],"-table") == 0 )
+        {
+            pszTable = papszArgv[++iArg];
+        }
+
+        else if( iArg < nArgc-1 && stricmp( papszArgv[iArg],"-column") == 0 )
+        {
+            pszGeomColumn = papszArgv[++iArg];
+        }
+        else
+        {
+            printf( "Unrecognised option: %s\n\n", papszArgv[iArg] );
+            Usage();
+        }
+    }
 
 /* -------------------------------------------------------------------- */
 /*      Open the data provider source (for instance select JET, and     */
 /*      access an MDB file.                                             */
 /* -------------------------------------------------------------------- */
-   hr = OledbSupGetDataSource( hProviderCLSID, pwszDataSource, 
-                               &pIOpenRowset );
+    hr = OledbSupGetDataSource( hProviderCLSID, pszDataSource, 
+                                &pIOpenRowset );
    
-   if( FAILED( hr ) )
-      goto error;
+    if( FAILED( hr ) )
+        goto error;
 
-   fprintf( stdout, "Acquired data source %S.\n", pwszDataSource );
+    fprintf( stdout, "Acquired data source %S.\n", pszDataSource );
 
 /* -------------------------------------------------------------------- */
 /*      Ask for a dump of a particular column.                          */
 /* -------------------------------------------------------------------- */
-   SFDumpGeomColumn( pIOpenRowset, pwszTable, pszGeomColumn );
+    SFDumpGeomColumn( pIOpenRowset, pszTable, pszGeomColumn );
 
 /* -------------------------------------------------------------------- */
 /*      Cleanup                                                         */
 /* -------------------------------------------------------------------- */
- error:    
+  error:    
 
-   if( pIOpenRowset != NULL )
-      pIOpenRowset->Release();
+    if( pIOpenRowset != NULL )
+        pIOpenRowset->Release();
 
-   OleSupUninitialize();
+    OleSupUninitialize();
 }    
 
 /************************************************************************/
@@ -71,44 +115,64 @@ void main( int argc, char ** argv )
 /************************************************************************/
 
 static HRESULT SFDumpGeomColumn( IOpenRowset* pIOpenRowset, 
-                                 LPWSTR pwszTableName, 
+                                 const char *pszTableName, 
                                  const char *pszColumnName )
 
 {
-   HRESULT            hr;
-   OledbSupRowset     oTable;
-   int                iColOrdinal;
+    HRESULT            hr;
+    OledbSupRowset     oTable;
+    int                iColOrdinal;
 
 /* -------------------------------------------------------------------- */
 /*      Open the table.                                                 */
 /* -------------------------------------------------------------------- */
-   hr = oTable.OpenTable( pIOpenRowset, pwszTableName );
-   if( FAILED( hr ) )
-      return hr;
+    hr = oTable.OpenTable( pIOpenRowset, pszTableName );
+    if( FAILED( hr ) )
+        return hr;
 
 /* -------------------------------------------------------------------- */
 /*      Find the ordinal of the requested column.                       */
 /* -------------------------------------------------------------------- */
-   iColOrdinal = oTable.GetColumnOrdinal( pszColumnName );
-   if( iColOrdinal == -1 )
-   {
-      fprintf( stderr, "Unable to find column `%s'.\n", pszColumnName );
-      return ResultFromScode( S_FALSE );
-   }
-
+    iColOrdinal = oTable.GetColumnOrdinal( pszColumnName );
+    if( iColOrdinal == -1 )
+    {
+        fprintf( stderr, "Unable to find column `%s'.\n", pszColumnName );
+        return ResultFromScode( S_FALSE );
+    }
+   
 /* -------------------------------------------------------------------- */
 /*      For now we just read through, counting records to verify        */
 /*      things are working.                                             */
 /* -------------------------------------------------------------------- */
-   int      nRecordCount = 0;
+    int      nRecordCount = 0;
 
-   while( oTable.GetNextRecord( &hr ) )
-   {
-      nRecordCount++;
-   }
+    while( oTable.GetNextRecord( &hr ) )
+    {
+        BYTE      *pabyData;
+        int       nSize, nDBType;
+        OGRGeometry * poGeom;
 
-   printf( "Read %d records.\n", nRecordCount );
 
-   return ResultFromScode( S_OK );
+        pabyData = (BYTE *) 
+            oTable.GetFieldData( iColOrdinal, &nDBType, NULL, &nSize );
+
+        assert( nDBType == DBTYPE_BYTES );
+
+        if( OGRGeometryFactory::createFromWkb( pabyData, &poGeom, nSize )
+            == OGRERR_NONE )
+        {
+            poGeom->dumpReadable( stdout );
+        }
+        else
+        {
+            fprintf( stderr, "Unable to decode record %d\n", nRecordCount );
+        }
+
+        nRecordCount++;
+    }
+
+    printf( "Read %d records.\n", nRecordCount );
+
+    return ResultFromScode( S_OK );
 }
 
