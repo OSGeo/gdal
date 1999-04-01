89ce7e47693a8d66d8ef45fcdace130c3323b3cd
Frank Warmerdam <warmerdam@pobox.com>
1999-04-01 17:55:43 +0000

    Added unsubtle IStream field support.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@254 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/sfdump.cpp
+++ ogr/sfdump.cpp
@@ -9,7 +9,10 @@
 #include "MSjetoledb.h"
 #include "sfclsid.h"
 
+const IID IID_IGeometry = {0x6A124031,0xFE38,0x11d0,{0xBE,0xCE,0x00,0x80,0x5F,0x7C,0x42,0x68}};
+
 static HRESULT SFDumpGeomColumn( IOpenRowset*, const char *, const char * );
+static HRESULT SFDumpSchema( IOpenRowset*, const char * );
 
 /************************************************************************/
 /*                               Usage()                                */
@@ -19,7 +22,8 @@ static void Usage()
 
 {
     printf( "Usage: sfdump [-provider provider_clsid_alias] [-ds datasource]\n"
-            "              [-table tablename] [-column geom_column_name]\n" );
+            "              [-table tablename] [-column geom_column_name]\n"
+            "              [-action {dumpgeom,dumpschema}]\n" );
     exit( 1 );
 }
 
@@ -35,6 +39,7 @@ void main( int nArgc, char ** papszArgv )
     const char *pszGeomColumn = "WKB_GEOMETRY";
     HRESULT     hr;
     IOpenRowset *pIOpenRowset = NULL;
+    const char  *pszAction = "dumpgeom";
    
 /* -------------------------------------------------------------------- */
 /*      Initialize OLE                                                  */
@@ -72,6 +77,11 @@ void main( int nArgc, char ** papszArgv )
         {
             pszGeomColumn = papszArgv[++iArg];
         }
+
+        else if( iArg < nArgc-1 && stricmp( papszArgv[iArg],"-action") == 0 )
+        {
+            pszAction = papszArgv[++iArg];
+        }
         else
         {
             printf( "Unrecognised option: %s\n\n", papszArgv[iArg] );
@@ -94,7 +104,17 @@ void main( int nArgc, char ** papszArgv )
 /* -------------------------------------------------------------------- */
 /*      Ask for a dump of a particular column.                          */
 /* -------------------------------------------------------------------- */
-    SFDumpGeomColumn( pIOpenRowset, pszTable, pszGeomColumn );
+    if( stricmp(pszAction,"dumpgeom") == 0 )
+        SFDumpGeomColumn( pIOpenRowset, pszTable, pszGeomColumn );
+
+    else if( stricmp(pszAction,"dumpschema") == 0 )
+        SFDumpSchema( pIOpenRowset, pszTable );
+
+    else
+    {
+        printf( "Action not recognised: %s\n\n", pszAction );
+        Usage();
+    }
 
 /* -------------------------------------------------------------------- */
 /*      Cleanup                                                         */
@@ -139,6 +159,8 @@ static HRESULT SFDumpGeomColumn( IOpenRowset* pIOpenRowset,
         fprintf( stderr, "Unable to find column `%s'.\n", pszColumnName );
         return ResultFromScode( S_FALSE );
     }
+    else
+        fprintf( stdout, "Geometry Column Ordinal = %d\n", iColOrdinal );
    
 /* -------------------------------------------------------------------- */
 /*      For now we just read through, counting records to verify        */
@@ -149,29 +171,139 @@ static HRESULT SFDumpGeomColumn( IOpenRowset* pIOpenRowset,
     while( oTable.GetNextRecord( &hr ) )
     {
         BYTE      *pabyData;
-        int       nSize, nDBType;
+        int       nSize, nDBType, nStatus;
         OGRGeometry * poGeom;
 
-
         pabyData = (BYTE *) 
-            oTable.GetFieldData( iColOrdinal, &nDBType, NULL, &nSize );
+            oTable.GetFieldData( iColOrdinal, &nDBType, &nStatus, &nSize );
 
-        assert( nDBType == DBTYPE_BYTES );
-
-        if( OGRGeometryFactory::createFromWkb( pabyData, &poGeom, nSize )
-            == OGRERR_NONE )
+/* -------------------------------------------------------------------- */
+/*      Handle raw binary data using our own client side classes.       */
+/* -------------------------------------------------------------------- */
+        if( nDBType == DBTYPE_BYTES )
         {
-            poGeom->dumpReadable( stdout );
+            if( OGRGeometryFactory::createFromWkb( pabyData, &poGeom, nSize )
+                == OGRERR_NONE )
+            {
+                poGeom->dumpReadable( stdout );
+                delete poGeom;
+            }
+            else 
+            {
+                fprintf( stderr, "Unable to decode record %d\n", 
+                         nRecordCount );
+            }
         }
+
+/* -------------------------------------------------------------------- */
+/*      Try to instantiate persistent objects.                          */
+/* -------------------------------------------------------------------- */
+        else if( nDBType == DBTYPE_IUNKNOWN )
+        {
+            IUnknown * pIUnknown = *((IUnknown **) pabyData);
+            IStream *  pIStream = NULL;
+            
+            hr = pIUnknown->QueryInterface( IID_IStream,
+                                            (void**)&pIStream );
+            if( FAILED(hr) )
+            {
+                printf( "After QueryInterface hr=%d, pInterface=%p\n", 
+                        hr, pIStream );
+                if( FAILED(hr) )
+                    DumpErrorHResult( hr, "QueryInterfacE" );
+            }
+            else
+            {
+                STATSTG      oStreamStat;
+                BYTE         *pabyData = NULL;
+                ULONG        nSize;
+
+                hr = pIStream->Stat( &oStreamStat, STATFLAG_NONAME );
+                if( FAILED(hr) )
+                {
+                    DumpErrorHResult( hr, "IStream::Stat()" );
+                    break;
+                }
+                
+                nSize = oStreamStat.cbSize.LowPart;
+                printf( "Storage length = %d\n", nSize );
+
+                pabyData = (BYTE *) CoTaskMemAlloc( nSize );
+                hr = pIStream->Read( pabyData, nSize, NULL );
+                if( FAILED(hr) )
+                {
+                    DumpErrorHResult( hr, "IStream::Read()" );
+                }
+                else
+                {
+                    OGRGeometry      *poGeom;
+
+                    if( OGRGeometryFactory::createFromWkb( pabyData, &poGeom, 
+                                                           nSize )
+                        == OGRERR_NONE )
+                    {
+                        poGeom->dumpReadable( stdout );
+                        delete poGeom;
+                    }
+                    else 
+                    {
+                        fprintf( stderr, "Unable to decode record %d\n", 
+                                 nRecordCount );
+                    }
+                }
+                CoTaskMemFree( pabyData );
+                pIStream->Release();
+            }
+        }
+
         else
         {
-            fprintf( stderr, "Unable to decode record %d\n", nRecordCount );
+            fprintf( stdout, 
+                     "Geometry column not of appropriate type: %d\n",
+                     nDBType );
+            break;
         }
 
         nRecordCount++;
     }
 
     printf( "Read %d records.\n", nRecordCount );
+
+    return ResultFromScode( S_OK );
+}
+
+/************************************************************************/
+/*                            SFDumpSchema()                            */
+/************************************************************************/
+
+HRESULT SFDumpSchema( IOpenRowset * pIOpenRowset, const char * pszTable )
+
+{
+    HRESULT            hr;
+    OledbSupRowset     oTable;
+    
+/* -------------------------------------------------------------------- */
+/*      Open the table.                                                 */
+/* -------------------------------------------------------------------- */
+    hr = oTable.OpenTable( pIOpenRowset, pszTable );
+    if( FAILED( hr ) )
+        return hr;
+
+/* -------------------------------------------------------------------- */
+/*      Dump each column                                                */
+/*                                                                      */
+/*      Note that iterating between 0 and numcolumns-1 is't really      */
+/*      the same as iterating over the ordinals.  If this table is a    */
+/*      subset view, we will miss some columns, and get lots of         */
+/*      NULLs.                                                          */
+/* -------------------------------------------------------------------- */
+    for( int iCol = 0; iCol < oTable.GetNumColumns(); iCol++ )
+    {
+        DBCOLUMNINFO      *poColumnInfo = oTable.GetColumnInfo( iCol );
+
+        if( poColumnInfo != NULL )
+            OledbSupWriteColumnInfo( stdout, poColumnInfo );
+    }
 
     return ResultFromScode( S_OK );
 }
