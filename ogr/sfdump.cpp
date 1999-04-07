261d86f1cdf7ac7a3fee95b4f0daf9df276350c2
Frank Warmerdam <warmerdam@pobox.com>
1999-04-07 11:56:33 +0000

    Added table dumping, and try to use COM Geometry classes (though this isn't really working properly).
    git-svn-id: https://svn.osgeo.org/gdal/trunk@266 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/sfdump.cpp
+++ ogr/sfdump.cpp
@@ -6,15 +6,20 @@
 
 #include "ogr_geometry.h"
 
+#include "geometryidl.h"
+
 // Get various classid.
 #include "msdaguid.h"
 #include "MSjetoledb.h"
 #include "sfclsid.h"
 
-const IID IID_IGeometry = {0x6A124031,0xFE38,0x11d0,{0xBE,0xCE,0x00,0x80,0x5F,0x7C,0x42,0x68}};
+//const IID IID_IGeometry = {0x6A124031,0xFE38,0x11d0,{0xBE,0xCE,0x00,0x80,0x5F,0x7C,0x42,0x68}};
+
+const IID IID_IGeometryFactory = {0x6A124033,0xFE38,0x11d0,{0xBE,0xCE,0x00,0x80,0x5F,0x7C,0x42,0x68}};
 
 static HRESULT SFDumpGeomColumn( IOpenRowset*, const char *, const char * );
 static HRESULT SFDumpSchema( IOpenRowset*, const char * );
+static HRESULT SFDumpRowset( OledbSupRowset * );
 
 /************************************************************************/
 /*                               Usage()                                */
@@ -112,6 +117,14 @@ void main( int nArgc, char ** papszArgv )
     else if( stricmp(pszAction,"dumpschema") == 0 )
         SFDumpSchema( pIOpenRowset, pszTable );
 
+    else if( stricmp(pszAction,"dump") == 0 )
+    {
+        OledbSupRowset      oTable;
+
+        hr = oTable.OpenTable( pIOpenRowset, pszTable );
+        if( !FAILED(hr) )
+            SFDumpRowset( &oTable );
+    }
     else
     {
         printf( "Action not recognised: %s\n\n", pszAction );
@@ -143,7 +156,7 @@ static HRESULT SFDumpGeomColumn( IOpenRowset* pIOpenRowset,
 {
     HRESULT           hr;
     OledbSFTable      oTable;
-
+    
 /* -------------------------------------------------------------------- */
 /*      Open the table.                                                 */
 /* -------------------------------------------------------------------- */
@@ -156,6 +169,20 @@ static HRESULT SFDumpGeomColumn( IOpenRowset* pIOpenRowset,
 /* -------------------------------------------------------------------- */
     if( pszColumnName != NULL )
         oTable.SelectGeometryColumn( pszColumnName );
+
+/* -------------------------------------------------------------------- */
+/*      Try and instantiate a Cadcorp geometry factory.                 */
+/* -------------------------------------------------------------------- */ 
+    IGeometryFactory *pIGeometryFactory = NULL;
+
+    hr = CoCreateInstance( CLSID_CadcorpSFGeometryFactory, NULL, 
+                           CLSCTX_INPROC_SERVER, 
+                           IID_IGeometryFactory, (void **)&pIGeometryFactory); 
+    if( FAILED(hr) ) 
+    {
+        DumpErrorHResult( hr, 
+                          "CoCreateInstance of CLSID_CadcorpSFGeomtryFactory" );
+    }
 
 /* -------------------------------------------------------------------- */
 /*      For now we just read through, counting records to verify        */
@@ -178,24 +205,60 @@ static HRESULT SFDumpGeomColumn( IOpenRowset* pIOpenRowset,
             continue;
 
 /* -------------------------------------------------------------------- */
-/*      Create and report geometry.                                     */
+/*      Create and report geometry using built in class.                */
 /* -------------------------------------------------------------------- */
-        if( OGRGeometryFactory::createFromWkb( pabyData, &poGeom, nSize )
-            == OGRERR_NONE )
+        if( pIGeometryFactory == NULL )
         {
-            poGeom->dumpReadable( stdout );
-            delete poGeom;
+            if( OGRGeometryFactory::createFromWkb( pabyData, &poGeom, nSize )
+                == OGRERR_NONE )
+            {
+                poGeom->dumpReadable( stdout );
+                delete poGeom;
+            }
+            else 
+            {
+                fprintf( stderr, "Unable to decode record %d\n", 
+                         nRecordCount );
+            }
         }
-        else 
+
+/* -------------------------------------------------------------------- */
+/*      Create geometry objects via COM.                                */
+/* -------------------------------------------------------------------- */
+        else
         {
-            fprintf( stderr, "Unable to decode record %d\n", 
-                     nRecordCount );
+            IGeometry      *pIGeometry = NULL;
+            VARIANT        oVarData;
+            SAFEARRAYBOUND aoBounds[1];
+            SAFEARRAY      *pArray;
+
+            aoBounds[0].lLbound = 0;
+            aoBounds[0].cElements = nSize;
+            
+            VariantInit( &oVarData );
+            oVarData.vt = VT_UI1 | VT_ARRAY;
+            pArray = SafeArrayCreate(VT_UI1,1,aoBounds);
+            pArray->pvData = pabyData;
+            oVarData.byref = pArray;
+
+            hr = pIGeometryFactory->CreateFromWKB( oVarData, NULL,
+                                                   &pIGeometry );
+
+            printf( "pIGeometry = %p\n", pIGeometry );
+
+            if( FAILED(hr) )
+                DumpErrorHResult( hr, "CreateFromWKB()" );
+            else if( pIGeometry != NULL )
+                pIGeometry->Release();
         }
 
         nRecordCount++;
     }
 
     printf( "Read %d records.\n", nRecordCount );
+
+    if( pIGeometryFactory == NULL )
+        pIGeometryFactory->Release();
 
     return ResultFromScode( S_OK );
 }
@@ -236,3 +299,23 @@ HRESULT SFDumpSchema( IOpenRowset * pIOpenRowset, const char * pszTable )
     return ResultFromScode( S_OK );
 }
 
+/************************************************************************/
+/*                            SFDumpRowset()                            */
+/************************************************************************/
+
+static HRESULT SFDumpRowset( OledbSupRowset * poTable )
+
+{
+    HRESULT           hr;
+    int      nRecordCount = 0;
+
+    while( poTable->GetNextRecord( &hr ) )
+    {
+        poTable->DumpRow( stdout );
+        nRecordCount++;
+    }
+
+    printf( "Read %d records.\n", nRecordCount );
+
+    return hr;
+}
