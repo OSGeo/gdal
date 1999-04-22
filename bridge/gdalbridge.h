5c538765d07abe8f91e3a187ecf6fe1e06c9dd1b
Frank Warmerdam <warmerdam@pobox.com>
1999-04-22 13:36:43 +0000

    Added copyright header.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@288 f0d54148-0727-0410-94bb-9a71ac55c965

--- bridge/gdalbridge.h
+++ bridge/gdalbridge.h
@@ -1,5 +1,38 @@
 /******************************************************************************
- * $Id: gdalbridg
+ * $Id$
+ *
+ * Project:  GDAL Bridge 
+ * Purpose:  Declarations for GDAL Bridge support.
+ * Author:   Frank Warmerdam, warmerda@home.com
+ *
+ * This file needs to be kept up to date with the contents of gdal.h by hand.
+ *
+ ******************************************************************************
+ * Copyright (c) 1999, Frank Warmerdam
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ******************************************************************************
+ *
+ * $Log$
+ * Revision 1.3  1999/04/22 13:36:43  warmerda
+ * Added copyright header.
+ *
  */
 
 #ifndef GDALBRIDGE_H_INCLUDED
