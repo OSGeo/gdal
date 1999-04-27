0e3b15956ca29d8b6c9ed4adbb7af66657aa682d
Frank Warmerdam <warmerdam@pobox.com>
1999-04-27 22:09:30 +0000

    New
    git-svn-id: https://svn.osgeo.org/gdal/trunk@297 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ frmts/iso8211/8211view.cpp
@@ -0,0 +1,179 @@
+/* ****************************************************************************
+ * $Id$
+ *
+ * Project:  SDTS Translator
+ * Purpose:  Example program dumping data in 8211 data to stdout.
+ * Author:   Frank Warmerdam, warmerda@home.com
+ *
+ ******************************************************************************
+ * Copyright (c) 1999, Frank Warmerdam
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ******************************************************************************
+ *
+ * $Log$
+ * Revision 1.1  1999/04/27 22:09:30  warmerda
+ * New
+ *
+ */
+
+#include <stdio.h>
+#include "iso8211.h"
+
+static void ViewRecordField( DDFField * poField );
+static int ViewSubfield( DDFSubfieldDefn *poSFDefn,
+                         const char * pachFieldData,
+                         int nBytesRemaining );
+
+/* **********************************************************************/
+/*                                main()                                */
+/* **********************************************************************/
+
+int main( int nArgc, char ** papszArgv )
+
+{
+    DDFModule	oModule;
+    const char	*pszFilename = "SC01CATD.DDF";
+
+    if( nArgc > 1 )
+        pszFilename = papszArgv[1];
+
+/* -------------------------------------------------------------------- */
+/*      Open the file.  Note that by default errors are reported to     */
+/*      stderr, so we don't bother doing it ourselves.                  */
+/* -------------------------------------------------------------------- */
+    if( !oModule.Open( pszFilename ) )
+    {
+        exit( 1 );
+    }
+
+/* -------------------------------------------------------------------- */
+/*      Loop reading records till there are none left.                  */
+/* -------------------------------------------------------------------- */
+    DDFRecord	*poRecord;
+    int		iRecord = 0;
+    
+    while( (poRecord = oModule.ReadRecord()) != NULL )
+    {
+        printf( "Record %d (%d bytes)\n",
+                ++iRecord, poRecord->GetDataSize() );
+                
+	/* ------------------------------------------------------------ */
+	/*      Loop over each field in this particular record.         */
+	/* ------------------------------------------------------------ */
+        for( int iField = 0; iField < poRecord->GetFieldCount(); iField++ )
+        {
+            DDFField	*poField = poRecord->GetField( iField );
+
+            ViewRecordField( poField );
+        }
+    }
+}
+
+/* **********************************************************************/
+/*                          ViewRecordField()                           */
+/*                                                                      */
+/*      Dump the contents of a field instance in a record.              */
+/* **********************************************************************/
+
+static void ViewRecordField( DDFField * poField )
+
+{
+    int		nBytesRemaining;
+    const char  *pachFieldData;
+    DDFFieldDefn *poFieldDefn = poField->GetFieldDefn();
+    
+    // Report general information about the field.
+    printf( "    Field %s: %s\n",
+            poFieldDefn->GetName(), poFieldDefn->GetDescription() );
+
+    // Get pointer to this fields raw data.  We will move through
+    // it consuming data as we report subfield values.
+            
+    pachFieldData = poField->GetData();
+    nBytesRemaining = poField->GetDataSize();
+                
+    /* -------------------------------------------------------- */
+    /*	    Loop over the repeat count for this fields 		*/
+    /*	    subfields.  The repeat count will almost		*/
+    /*	    always be one.					*/
+    /* -------------------------------------------------------- */
+    int		iRepeat;
+            
+    for( iRepeat = 0; iRepeat < poField->GetRepeatCount(); iRepeat++ )
+    {
+
+        /* -------------------------------------------------------- */
+        /*   Loop over all the subfields of this field, advancing   */
+        /*   the data pointer as we consume data.		    */
+        /* -------------------------------------------------------- */
+        int	iSF;
+        
+        for( iSF = 0; iSF < poFieldDefn->GetSubfieldCount(); iSF++ )
+        {
+            DDFSubfieldDefn *poSFDefn = poFieldDefn->GetSubfield( iSF );
+            int		nBytesConsumed;
+
+            nBytesConsumed = ViewSubfield( poSFDefn, pachFieldData,
+                                           nBytesRemaining );
+
+            nBytesRemaining -= nBytesConsumed;
+            pachFieldData += nBytesConsumed;
+        }
+    }
+}
+
+/* **********************************************************************/
+/*                            ViewSubfield()                            */
+/* **********************************************************************/
+
+static int ViewSubfield( DDFSubfieldDefn *poSFDefn,
+                         const char * pachFieldData,
+                         int nBytesRemaining )
+
+{
+    int		nBytesConsumed = 0;
+    
+    switch( poSFDefn->GetType() )
+    {
+      case DDFInt:
+        printf( "        %s = %d\n",
+                poSFDefn->GetName(),
+                poSFDefn->ExtractIntData( pachFieldData, nBytesRemaining,
+                                          &nBytesConsumed ) );
+        break;
+
+      case DDFFloat:
+        printf( "        %s = %f\n",
+                poSFDefn->GetName(),
+                poSFDefn->ExtractFloatData( pachFieldData, nBytesRemaining,
+                                            &nBytesConsumed ) );
+        break;
+
+      case DDFString:
+        printf( "        %s = `%s'\n",
+                poSFDefn->GetName(),
+                poSFDefn->ExtractStringData( pachFieldData, nBytesRemaining,
+                                             &nBytesConsumed ) );
+        break;
+        
+    }
+
+    return nBytesConsumed;
+}
