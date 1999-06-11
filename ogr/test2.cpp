79713515c78a4d8e24610606d6341ed471071398
Frank Warmerdam <warmerdam@pobox.com>
1999-06-11 19:21:52 +0000

    now uses OGRFeatures
    git-svn-id: https://svn.osgeo.org/gdal/trunk@542 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/test2.cpp
+++ ogr/test2.cpp
@@ -1,34 +1,49 @@
-#include "ogr_geometry.h"
+#include "ogr_feature.h"
 #include "../frmts/shapelib/shapefil.h"
 #include <assert.h>
 
+#include "dbmalloc.h"
+
 OGRGeometry *SHPReadOGRObject( SHPHandle hSHP, int iShape );
+OGRFeatureDefn *SHPReadOGRFeatureDefn( SHPHandle hSHP, DBFHandle hDBF );
+OGRFeature *SHPReadOGRFeature( SHPHandle hSHP, DBFHandle hDBF,
+                               OGRFeatureDefn * poDefn, int iShape );
 
 int main( int argc, char ** argv )
 
 {
     SHPHandle	hSHP;
+    DBFHandle   hDBF;
     int		nShapeCount, i;
+    OGRFeatureDefn *poDefn;
     
     assert( argc == 2 );
 
     hSHP = SHPOpen( argv[1], "rb" );
+    hDBF = DBFOpen( argv[1], "rb" );
 
-    assert( hSHP != NULL );
+    assert( hSHP != NULL && hDBF != NULL );
 
     SHPGetInfo( hSHP, &nShapeCount, NULL, NULL, NULL );
 
+    poDefn = SHPReadOGRFeatureDefn( hSHP, hDBF );
+
     for( i = 0; i < nShapeCount; i++ )
     {
-        OGRGeometry	*poShape;
+        OGRFeature	*poFeature;
 
-        poShape = SHPReadOGRObject( hSHP, i );
+        poFeature = SHPReadOGRFeature( hSHP, hDBF, poDefn, i );
 
-        if( poShape != NULL )
-            poShape->dumpReadable( stdout );
+        if( poFeature != NULL )
+            poFeature->DumpReadable( stdout );
 
-        delete poShape;
+        delete poFeature;
     }
 
+    delete poDefn;
+
+    DBFClose( hDBF );
     SHPClose( hSHP );
+
+    malloc_dump(2);
 }
