fc580ecc7709603c296b2eb010bf40bfd620cdfc
Frank Warmerdam <warmerdam@pobox.com>
1999-06-11 19:21:27 +0000

    Fleshed out operational definitions
    git-svn-id: https://svn.osgeo.org/gdal/trunk@541 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/ogr_feature.h
+++ ogr/ogr_feature.h
@@ -6,7 +6,7 @@
  * Author:   Frank Warmerdam, warmerda@home.com
  *
  ******************************************************************************
- * Copyright (c) 1999, Frank Warmerdam
+ * Copyright (c) 1999,  Les Technologies SoftMap Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -28,6 +28,9 @@
  ******************************************************************************
  *
  * $Log$
+ * Revision 1.2  1999/06/11 19:21:27  warmerda
+ * Fleshed out operational definitions
+ *
  * Revision 1.1  1999/05/31 17:14:53  warmerda
  * New
  *
@@ -59,6 +62,37 @@ enum OGRJustification
 };
 
 /************************************************************************/
+/*                               OGRField                               */
+/************************************************************************/
+
+typedef union {
+    int		Integer;
+    double	Real;
+    char       *String;
+    // wchar	*WideString;
+    
+    union {
+        int	nCount;
+        int	*paList;
+    } IntegerList;
+    
+    union {
+        int	nCount;
+        double	*paList;
+    } RealList;
+    
+    union {
+        int	nCount;
+        char	*paList;
+    } StringList;
+
+//    union {
+//        int	nCount;
+//        wchar	*paList;
+//    } WideStringList;
+} OGRField;
+
+/************************************************************************/
 /*                             OGRFieldDefn                             */
 /************************************************************************/
 
@@ -69,13 +103,40 @@ enum OGRJustification
 class OGRFieldDefn
 {
   private:
+    char		*pszName;
     OGRFieldType	eType;			
     OGRJustification	eJustify;		
     int			nWidth;			/* zero is variable */
     int			nPrecision;
+    OGRField		uDefault;
+
+    void		Initialize( const char *, OGRFieldType );
     
   public:
-    
+        		OGRFieldDefn( const char *, OGRFieldType );
+                        OGRFieldDefn( OGRFieldDefn * );
+    			~OGRFieldDefn();
+
+    void		SetName( const char * );
+    const char	       *GetNameRef() { return pszName; }
+
+    OGRFieldType	GetType() { return eType; }
+    void                SetType( OGRFieldType eTypeIn ) { eType = eTypeIn;}
+    static const char  *GetFieldTypeName( OGRFieldType );
+
+    OGRJustification    GetJustify() { return eJustify; }
+    void                SetJustify( OGRJustification eJustifyIn )
+					        { eJustify = eJustifyIn; }
+
+    int			GetWidth() { return nWidth; }
+    void                SetWidth( int nWidthIn ) { nWidth = nWidthIn; }
+
+    int			GetPrecision() { return nPrecision; }
+    void                SetPrecision( int nPrecisionIn )
+        				        { nPrecision = nPrecisionIn; }
+
+    void		SetDefault( const OGRField * );
+    const OGRField     *GetDefaultRef() { return &uDefault; }
 };
 
 /************************************************************************/
@@ -89,11 +150,33 @@ class OGRFieldDefn
 class OGRFeatureDefn
 {
   private:
-    int		nFields;
-    OGRFieldDefn *pasFieldDefn;
+    int		nRefCount;
+    
+    int		nFieldCount;
+    OGRFieldDefn **papoFieldDefn;
+
+    OGRwkbGeometryType eGeomType;
+
+    char	*pszFeatureClassName;
     
   public:
-    
+    		OGRFeatureDefn( const char * pszName = NULL );
+    virtual    ~OGRFeatureDefn();
+
+    const char	*GetName() { return pszFeatureClassName; }
+
+    int		GetFieldCount() { return nFieldCount; }
+    OGRFieldDefn *GetFieldDefn( int i );
+    int		GetFieldIndex( const char * );
+
+    void	AddFieldDefn( OGRFieldDefn * );
+
+    OGRwkbGeometryType GetGeomType() { return eGeomType; }
+    void        SetGeomType( OGRwkbGeometryType );
+
+    int		Reference() { return ++nRefCount; }
+    int		Dereference() { return --nRefCount; }
+    int		GetReferenceCount() { return nRefCount; }
 };
 
 /************************************************************************/
@@ -108,10 +191,40 @@ class OGRFeature
 {
   private:
     
-    OGRSchema	*poSchema;
+    OGRFeatureDefn 	*poDefn;
+    OGRGeometry		*poGeometry;
+    OGRField		*pauFields;
     
   public:
+    			OGRFeature( OGRFeatureDefn * );
+    virtual            ~OGRFeature();                        
+
+    OGRFeatureDefn     *GetDefnRef() { return poDefn; }
+    
+    OGRErr		SetGeometryDirectly( OGRGeometry * );
+    OGRErr		SetGeometry( OGRGeometry * );
+    OGRGeometry        *GetGeometryRef( OGRGeometry * ) { return poGeometry; }
+
+    OGRFeature	       *Clone();
+
+    int			GetFieldCount() { return poDefn->GetFieldCount(); }
+    OGRFieldDefn       *GetFieldDefnRef( int i )
+        			      { return poDefn->GetFieldDefn(i); }
+    int			GetFieldIndex( const char * pszName)
+                                      { return poDefn->GetFieldIndex(pszName);}
+    
+    OGRField	       *GetRawFieldRef( int i ) { return pauFields + i; }
+
+    int			GetFieldAsInteger( int i );
+    double		GetFieldAsDouble( int i );
+    const char	       *GetFieldAsString( int i );
+
+    void		SetField( int i, int nValue );
+    void		SetField( int i, double dfValue );
+    void		SetField( int i, const char * pszValue );
+    void		SetField( int i, OGRField * puValue );
+
+    void		DumpReadable( FILE * );
 };
 
 #endif /* ndef _OGR_FEATURE_H_INCLUDED */
-
