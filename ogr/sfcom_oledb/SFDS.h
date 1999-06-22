7fb73fcc2ce0b9c0b4a8276a2f6da34be3dc1fa0
Ken Shih <kshih@home.com>
1999-06-22 15:52:56 +0000

    Added Initialize error return for invalid data set.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@559 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/sfcom_oledb/SFDS.h
+++ ogr/sfcom_oledb/SFDS.h
@@ -28,6 +28,9 @@
  ******************************************************************************
  *
  * $Log$
+ * Revision 1.4  1999/06/22 15:52:56  kshih
+ * Added Initialize error return for invalid data set.
+ *
  * Revision 1.3  1999/06/12 17:15:42  kshih
  * Make use of datasource property
  * Add schema rowsets
@@ -41,18 +44,159 @@
 #define __CSFSource_H_
 #include "resource.h"       // main symbols
 #include "SFRS.h"
+#include "sfutil.h"
+
+// IDBInitializeImpl
+template <class T>
+class ATL_NO_VTABLE MyIDBInitializeImpl : public IDBInitialize
+{
+public:
+	MyIDBInitializeImpl()
+	{
+		m_dwStatus = 0;
+		m_pCUtlPropInfo = NULL;
+		m_cSessionsOpen = 0;
+	}
+	~MyIDBInitializeImpl()
+	{
+		delete m_pCUtlPropInfo;
+	}
+
+	STDMETHOD(Uninitialize)(void)
+	{
+		ATLTRACE2(atlTraceDBProvider, 0, "IDBInitializeImpl::Uninitialize\n");
+		T* pT = (T*)this;
+		pT->Lock();
+		if (pT->m_cSessionsOpen != 0)
+		{  
+			ATLTRACE2(atlTraceDBProvider, 0, "Uninitialized called with Open Sessions\n");
+			return DB_E_OBJECTOPEN;
+		}
+		delete m_pCUtlPropInfo;
+		m_pCUtlPropInfo = NULL;
+		pT->m_dwStatus |= DSF_PERSIST_DIRTY;
+		pT->m_dwStatus &= DSF_MASK_INIT;    // Clear all non-init flags.
+		pT->Unlock();
+		return S_OK;
+
+	}
+
+	LONG m_cSessionsOpen;
+	DWORD m_dwStatus;
+	CUtlPropInfo<T>* m_pCUtlPropInfo;
+
+	STDMETHOD(Initialize)(void)
+	{
+
+		ATLTRACE2(atlTraceDBProvider, 0, "IDBInitializeImpl::Initialize\n");
+		T *pT = (T*)(this);
+		T::ObjectLock lock(pT);
+		HRESULT hr;
+		if (pT->m_dwStatus & DSF_INITIALIZED)
+		{
+			ATLTRACE2(atlTraceDBProvider, 0, "IDBInitializeImpl::Initialize Error : Already Initialized\n");
+			return DB_E_ALREADYINITIALIZED;
+		}
+		delete m_pCUtlPropInfo;
+		m_pCUtlPropInfo = NULL;
+		ATLTRY(m_pCUtlPropInfo = new CUtlPropInfo<T>())
+		if (m_pCUtlPropInfo == NULL)
+		{
+			ATLTRACE2(atlTraceDBProvider, 0, "IDBInitializeImpl::Initialize Error : OOM\n");
+			return E_OUTOFMEMORY;
+		}
+		hr = m_pCUtlPropInfo->FInit();
+		if (hr == S_OK)
+		{
+			pT->m_dwStatus |= DSF_INITIALIZED;
+			
+// Inserted code!!!!!
+			IDBProperties *pIDBProp;
+			hr = QueryInterface(IID_IDBProperties, (void **) &pIDBProp);		
+			
+			if (SUCCEEDED(hr))
+			{
+	
+				char		*pszDataSource;
+				DBPROPIDSET sPropIdSets[1];
+				DBPROPID	rgPropIds[1];
+				
+				ULONG		nPropSets;
+				DBPROPSET	*rgPropSets;
+				
+				rgPropIds[0] = DBPROP_INIT_DATASOURCE;
+				
+				sPropIdSets[0].cPropertyIDs = 1;
+				sPropIdSets[0].guidPropertySet = DBPROPSET_DBINIT;
+				sPropIdSets[0].rgPropertyIDs = rgPropIds;
+				
+				pIDBProp->GetProperties(1,sPropIdSets,&nPropSets,&rgPropSets);
+				pIDBProp->Release();			
+				if (rgPropSets)
+				{
+					USES_CONVERSION;
+					char *pszSource = (char *)  OLE2A(rgPropSets[0].rgProperties[0].vValue.bstrVal);
+					pszDataSource = (char *) malloc(1+strlen(pszSource));
+					strcpy(pszDataSource,pszSource);
+				}
+				
+				if (rgPropSets)
+				{
+					int i;
+					for (i=0; i < nPropSets; i++)
+					{
+						CoTaskMemFree(rgPropSets[i].rgProperties);
+					}
+					CoTaskMemFree(rgPropSets);
+				}
+				
+				
+				SHPHandle hSHP;
+				DBFHandle hDBF;
+				
+				hr = E_FAIL;
+				
+				if (NULL != (hSHP = SFGetSHPHandle(pszDataSource)))
+				{
+					SHPClose(hSHP);
+					
+					if (NULL != (hDBF = SFGetDBFHandle(pszDataSource)))
+					{
+						DBFClose(hDBF);
+						
+						hr = S_OK;
+					}
+				}
+				
+				free(pszDataSource);
+			}
+		}
+		else
+		{
+			delete m_pCUtlPropInfo;
+			m_pCUtlPropInfo = NULL;
+		}
+		return hr;
+	}
+
+};
+
+
 /////////////////////////////////////////////////////////////////////////////
 // CDataSource
 class ATL_NO_VTABLE CSFSource : 
 	public CComObjectRootEx<CComSingleThreadModel>,
 	public CComCoClass<CSFSource, &CLSID_SF>,
 	public IDBCreateSessionImpl<CSFSource, CSFSession>,
-	public IDBInitializeImpl<CSFSource>,
+	public MyIDBInitializeImpl<CSFSource>,
 	public IDBPropertiesImpl<CSFSource>,
 	public IPersistImpl<CSFSource>,
 	public IInternalConnectionImpl<CSFSource>
 {
 public:
+
+	
+
 	HRESULT FinalConstruct()
 	{
 		// Do all my initialization here.
