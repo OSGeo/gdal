cdffb7e94ce8eeeb4b29830721b77b55b38173f0
Frank Warmerdam <warmerdam@pobox.com>
1999-07-23 19:36:09 +0000

    added support for data type translation and a swapping function
    git-svn-id: https://svn.osgeo.org/gdal/trunk@656 f0d54148-0727-0410-94bb-9a71ac55c965

--- gcore/rasterio.cpp
+++ gcore/rasterio.cpp
@@ -1,4 +1,12 @@
 /******************************************************************************
+ * $Id$
+ *
+ * Project:  GDAL Core
+ * Purpose:  Contains default implementation of GDALRasterBand::IRasterIO()
+ *           and supporting functions of broader utility.
+ * Author:   Frank Warmerdam, warmerda@home.com
+ *
+ ******************************************************************************
  * Copyright (c) 1998, Frank Warmerdam
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
@@ -20,10 +28,10 @@
  * DEALINGS IN THE SOFTWARE.
  ******************************************************************************
  *
- * rasterio.cpp: This files exists to contain the default implementation
- *               of GDALRasterBand::IRasterIO() which is rather complex.
- *
  * $Log$
+ * Revision 1.5  1999/07/23 19:36:09  warmerda
+ * added support for data type translation and a swapping function
+ *
  * Revision 1.4  1999/01/11 15:38:38  warmerda
  * Added optimized case for simple 1:1 copies.
  *
@@ -35,7 +43,6 @@
  *
  * Revision 1.1  1998/12/06 22:15:42  warmerda
  * New
- *
  */
 
 #include "gdal_priv.h"
@@ -183,8 +190,17 @@ CPLErr GDALRasterBand::IRasterIO( GDALRWFlag eRWFlag,
             }
             else
             {
-                /* type to type translation */
-                CPLAssert( FALSE );
+                /* type to type conversion ... ouch, this is expensive way
+                   of handling single words */
+                
+                if( eRWFlag == GF_Read )
+                    GDALCopyWords( pabySrcBlock + iSrcOffset, eDataType, 0,
+                                   ((GByte *) pData) + iBufOffset, eBufType, 0,
+                                   1 );
+                else
+                    GDALCopyWords( ((GByte *) pData) + iBufOffset, eBufType, 0,
+                                   pabySrcBlock + iSrcOffset, eDataType, 0,
+                                   1 );
             }
 
             iBufOffset += nPixelSpace;
@@ -194,3 +210,288 @@ CPLErr GDALRasterBand::IRasterIO( GDALRWFlag eRWFlag,
     return( CE_None );
 }
 
+/************************************************************************/
+/*                           GDALSwapWords()                            */
+/************************************************************************/
+
+void GDALSwapWords( void *pData, int nWordSize, int nWordCount,
+                    int nWordSkip )
+
+{
+    int		i;
+    GByte	*pabyData = (GByte *) pData;
+
+    switch( nWordSize )
+    {
+      case 1:
+        break;
+
+      case 2:
+        CPLAssert( nWordSize >= 2 );
+        for( i = 0; i < nWordCount; i++ )
+        {
+            GByte	byTemp;
+
+            byTemp = pabyData[0];
+            pabyData[0] = pabyData[1];
+            pabyData[1] = byTemp;
+
+            pabyData += nWordSize;
+        }
+        break;
+        
+      case 4:
+        CPLAssert( nWordSize >= 4 );
+        for( i = 0; i < nWordCount; i++ )
+        {
+            GByte	byTemp;
+
+            byTemp = pabyData[0];
+            pabyData[0] = pabyData[3];
+            pabyData[3] = byTemp;
+
+            byTemp = pabyData[1];
+            pabyData[1] = pabyData[2];
+            pabyData[2] = byTemp;
+
+            pabyData += nWordSize;
+        }
+        break;
+
+      case 8:
+        CPLAssert( nWordSize >= 8 );
+        for( i = 0; i < nWordCount; i++ )
+        {
+            GByte	byTemp;
+
+            byTemp = pabyData[0];
+            pabyData[0] = pabyData[7];
+            pabyData[7] = byTemp;
+
+            byTemp = pabyData[1];
+            pabyData[1] = pabyData[6];
+            pabyData[6] = byTemp;
+
+            byTemp = pabyData[2];
+            pabyData[2] = pabyData[5];
+            pabyData[5] = byTemp;
+
+            byTemp = pabyData[3];
+            pabyData[3] = pabyData[4];
+            pabyData[4] = byTemp;
+
+            pabyData += nWordSize;
+        }
+        break;
+
+      default:
+        CPLAssert( FALSE );
+    }
+}
+
+/************************************************************************/
+/*                           GDALCopyWords()                            */
+/************************************************************************/
+
+void 
+    GDALCopyWords( void * pSrcData, GDALDataType eSrcType, int nSrcPixelOffset,
+                   void * pDstData, GDALDataType eDstType, int nDstPixelOffset,
+                   int nWordCount )
+
+{
+/* -------------------------------------------------------------------- */
+/*      Special case when no data type translation is required.         */
+/* -------------------------------------------------------------------- */
+    if( eSrcType == eDstType )
+    {
+        int	nWordSize = GDALGetDataTypeSize(eSrcType)/8;
+        int	i;
+
+        // contiguous blocks.
+        if( nWordSize == nSrcPixelOffset && nWordSize == nDstPixelOffset )
+        {
+            memcpy( pDstData, pSrcData, nSrcPixelOffset * nWordCount );
+            return;
+        }
+
+        // source or destination is not contiguous
+        for( i = 0; i < nWordCount; i++ )
+        {
+            memcpy( ((GByte *)pSrcData) + i * nSrcPixelOffset,
+                    ((GByte *)pDstData) + i * nDstPixelOffset,
+                    nWordSize );
+        }
+
+        return;
+    }
+
+/* ==================================================================== */
+/*      General translation case                                        */
+/* ==================================================================== */
+    for( int iWord = 0; iWord < nWordCount; iWord++ )
+    {
+        GByte 	*pabySrcWord, *pabyDstWord;
+        double	dfPixelValue;
+
+        pabySrcWord = ((GByte *) pSrcData) + iWord * nSrcPixelOffset;
+
+/* -------------------------------------------------------------------- */
+/*      Fetch source value based on data type.                          */
+/* -------------------------------------------------------------------- */
+        switch( eSrcType )
+        {
+          case GDT_Byte:
+            dfPixelValue = *pabySrcWord;
+            break;
+
+          case GDT_UInt16:
+          {
+              GUInt16	nVal;
+
+              memcpy( &nVal, pabySrcWord, 2 );
+              dfPixelValue = nVal;
+          }
+          break;
+          
+          case GDT_Int16:
+          {
+              GInt16	nVal;
+
+              memcpy( &nVal, pabySrcWord, 2 );
+              dfPixelValue = nVal;
+          }
+          break;
+          
+          case GDT_Int32:
+          {
+              GInt32	nVal;
+
+              memcpy( &nVal, pabySrcWord, 4 );
+              dfPixelValue = nVal;
+          }
+          break;
+          
+          case GDT_UInt32:
+          {
+              GUInt32	nVal;
+
+              memcpy( &nVal, pabySrcWord, 4 );
+              dfPixelValue = nVal;
+          }
+          break;
+          
+          case GDT_Float32:
+          {
+              float	fVal;
+
+              memcpy( &fVal, pabySrcWord, 4 );
+              dfPixelValue = fVal;
+          }
+          break;
+          
+          case GDT_Float64:
+          {
+              memcpy( &dfPixelValue, pabySrcWord, 4 );
+          }
+          break;
+
+          default:
+            CPLAssert( FALSE );
+        }
+        
+/* -------------------------------------------------------------------- */
+/*      Set the destination pixel, doing range clipping as needed.      */
+/* -------------------------------------------------------------------- */
+        pabyDstWord = ((GByte *) pDstData) + iWord * nDstPixelOffset;
+        switch( eSrcType )
+        {
+          case GDT_Byte:
+          {
+              if( dfPixelValue < 0.0 )
+                  *pabyDstWord = 0;
+              else if( dfPixelValue > 255.0 )
+                  *pabyDstWord = 255;
+              else
+                  *pabyDstWord = (GByte) dfPixelValue;
+          }
+          break;
+
+          case GDT_UInt16:
+          {
+              GUInt16	nVal;
+              
+              if( dfPixelValue < 0.0 )
+                  nVal = 0;
+              else if( dfPixelValue > 65535.0 )
+                  nVal = 65535;
+              else
+                  nVal = (GUInt16) dfPixelValue;
+
+              memcpy( pabyDstWord, &nVal, 2 );
+          }
+          break;
+
+          case GDT_Int16:
+          {
+              GInt16	nVal;
+              
+              if( dfPixelValue < -32768 )
+                  nVal = -32768;
+              else if( dfPixelValue > 32767 )
+                  nVal = 32767;
+              else
+                  nVal = (GInt16) dfPixelValue;
+
+              memcpy( pabyDstWord, &nVal, 2 );
+          }
+          break;
+          
+          case GDT_UInt32:
+          {
+              GUInt32	nVal;
+              
+              if( dfPixelValue < 0 )
+                  nVal = 0;
+              else if( dfPixelValue > 4294967295U )
+                  nVal = 4294967295U;
+              else
+                  nVal = (GInt32) dfPixelValue;
+
+              memcpy( pabyDstWord, &nVal, 4 );
+          }
+          break;
+          
+          case GDT_Int32:
+          {
+              GInt32	nVal;
+              
+              if( dfPixelValue < -2147483647.0 )
+                  nVal = -2147483647;
+              else if( dfPixelValue > 2147483647 )
+                  nVal = 2147483647;
+              else
+                  nVal = (GInt32) dfPixelValue;
+
+              memcpy( pabyDstWord, &nVal, 4 );
+          }
+          break;
+
+          case GDT_Float32:
+          {
+              float 	fVal;
+
+              fVal = dfPixelValue;
+
+              memcpy( pabyDstWord, &fVal, 4 );
+          }
+          break;
+
+          case GDT_Float64:
+              memcpy( pabyDstWord, &dfPixelValue, 8 );
+              break;
+              
+          default:
+            CPLAssert( FALSE );
+        }
+    } /* next iWord */
+}
