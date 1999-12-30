e6b39464d4ac4769778d866613a960e53fa832b4
Frank Warmerdam <warmerdam@pobox.com>
1999-12-30 02:39:43 +0000

    Added projections support.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@1093 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/raw/doq1dataset.cpp
+++ frmts/raw/doq1dataset.cpp
@@ -28,9 +28,11 @@
  ******************************************************************************
  *
  * $Log$
+ * Revision 1.2  1999/12/30 02:39:43  warmerda
+ * Added projections support.
+ *
  * Revision 1.1  1999/12/29 20:43:05  warmerda
  * New
- *
  */
 
 #include "rawdataset.h"
@@ -38,11 +40,26 @@
 
 static GDALDriver	*poDOQ1Driver = NULL;
 
-static int DOQGetField( unsigned char *, int );
+static double DOQGetField( unsigned char *, int );
 
 CPL_C_START
 void	GDALRegister_DOQ1(void);
 CPL_C_END
+
+#define UTM_FORMAT \
+"PROJCS[\"%s / UTM zone 1N\",GEOGCS[%s,PRIMEM[\"Greenwich\",0],UNIT[\"degree\",0.0174532925199433]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"latitude_of_origin\",0],PARAMETER[\"central_meridian\",%d],PARAMETER[\"scale_factor\",0.9996],PARAMETER[\"false_easting\",500000],PARAMETER[\"false_northing\",0],%s]"
+
+#define WGS84_DATUM \
+"\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563]]"
+
+#define WGS72_DATUM \
+"\"WGS 72\",DATUM[\"WGS_1972\",SPHEROID[\"NWL 10D\",6378135,298.26]]"
+
+#define NAD27_DATUM \
+"\"NAD27\",DATUM[\"North_American_Datum_1927\",SPHEROID[\"Clarke 1866\",6378206.4,294.978698213901]]"
+
+#define NAD83_DATUM \
+"\"NAD83\",DATUM[\"North_American_Datum_1983\",SPHEROID[\"GRS 1980\",6378137,298.257222101]]"
 
 /************************************************************************/
 /* ==================================================================== */
@@ -54,13 +71,17 @@ class DOQ1Dataset : public RawDataset
 {
     FILE	*fpImage;	// image data file.
     
+    double	dfULX, dfULY;
+    double	dfXPixelSize, dfYPixelSize;
+
+    char	*pszProjection;
     
   public:
     		DOQ1Dataset();
     	        ~DOQ1Dataset();
-    
+
     CPLErr 	GetGeoTransform( double * padfTransform );
-    const char  *GetProjectionString( void );
+    const char  *GetProjectionRef( void );
     
     static GDALDataset *Open( GDALOpenInfo * );
 };
@@ -71,6 +92,7 @@ class DOQ1Dataset : public RawDataset
 
 DOQ1Dataset::DOQ1Dataset()
 {
+    pszProjection = NULL;
     fpImage = NULL;
 }
 
@@ -81,6 +103,7 @@ DOQ1Dataset::DOQ1Dataset()
 DOQ1Dataset::~DOQ1Dataset()
 
 {
+    CPLFree( pszProjection );
     if( fpImage != NULL )
         VSIFClose( fpImage );
 }
@@ -92,6 +115,13 @@ DOQ1Dataset::~DOQ1Dataset()
 CPLErr DOQ1Dataset::GetGeoTransform( double * padfTransform )
 
 {
+    padfTransform[0] = dfULX;
+    padfTransform[1] = dfXPixelSize;
+    padfTransform[2] = 0.0;
+    padfTransform[3] = dfULY;
+    padfTransform[4] = 0.0;
+    padfTransform[5] = -1 * dfYPixelSize;
+    
     return( CE_None );
 }
 
@@ -99,10 +129,10 @@ CPLErr DOQ1Dataset::GetGeoTransform( double * padfTransform )
 /*                        GetProjectionString()                         */
 /************************************************************************/
 
-const char *DOQ1Dataset::GetProjectionString()
+const char *DOQ1Dataset::GetProjectionRef()
 
 {
-    return "";
+    return pszProjection;
 }
 
 /************************************************************************/
@@ -117,16 +147,16 @@ GDALDataset *DOQ1Dataset::Open( GDALOpenInfo * poOpenInfo )
 /* -------------------------------------------------------------------- */
 /*	We assume the user is pointing to the binary (ie. .bil) file.	*/
 /* -------------------------------------------------------------------- */
-    if( poOpenInfo->nHeaderBytes < 180 && poOpenInfo->fp != NULL )
+    if( poOpenInfo->nHeaderBytes < 212 && poOpenInfo->fp != NULL )
         return NULL;
 
 /* -------------------------------------------------------------------- */
 /*	Attempt to extract a few key values from the header.		*/
 /* -------------------------------------------------------------------- */
-    nWidth = DOQGetField(poOpenInfo->pabyHeader + 150, 6);
-    nHeight = DOQGetField(poOpenInfo->pabyHeader + 144, 6);
-    nBandStorage = DOQGetField(poOpenInfo->pabyHeader + 162, 3);
-    nBandTypes = DOQGetField(poOpenInfo->pabyHeader + 156, 3);
+    nWidth = (int) DOQGetField(poOpenInfo->pabyHeader + 150, 6);
+    nHeight = (int) DOQGetField(poOpenInfo->pabyHeader + 144, 6);
+    nBandStorage = (int) DOQGetField(poOpenInfo->pabyHeader + 162, 3);
+    nBandTypes = (int) DOQGetField(poOpenInfo->pabyHeader + 156, 3);
 
 /* -------------------------------------------------------------------- */
 /*      Do these values look coherent for a DOQ file?  It would be      */
@@ -182,7 +212,7 @@ GDALDataset *DOQ1Dataset::Open( GDALOpenInfo * poOpenInfo )
         nBytesPerPixel = 3;
 
     nBytesPerLine = nBytesPerPixel * nWidth;
-    nSkipBytes = 3 * nBytesPerLine;
+    nSkipBytes = 4 * nBytesPerLine;
     
 /* -------------------------------------------------------------------- */
 /*      Create band information objects.                                */
@@ -198,6 +228,89 @@ GDALDataset *DOQ1Dataset::Open( GDALOpenInfo * poOpenInfo )
                                nSkipBytes + i, nBytesPerPixel, nBytesPerLine,
                                GDT_Byte, TRUE );
     }
+
+/* -------------------------------------------------------------------- */
+/*      Establish the projection string.                                */
+/* -------------------------------------------------------------------- */
+    if( ((int) DOQGetField(poOpenInfo->pabyHeader + 195, 3)) != 1 )
+        poDS->pszProjection = VSIStrdup("");
+    else
+    {
+        const char *pszDatumLong, *pszDatumShort;
+        const char *pszUnits;
+        int	   nZone;
+
+        nZone = (int) DOQGetField(poOpenInfo->pabyHeader + 198, 6);
+
+        if( ((int) DOQGetField(poOpenInfo->pabyHeader + 204, 3)) == 1 )
+            pszUnits = "UNIT[\"US survey foot\",0.304800609601219]";
+        else
+            pszUnits = "UNIT[\"metre\",1]";
+
+        switch( (int) DOQGetField(poOpenInfo->pabyHeader + 167, 2) )
+        {
+          case 1:
+            pszDatumLong = NAD27_DATUM;
+            pszDatumShort = "NAD 27";
+            break;
+            
+          case 2:
+            pszDatumLong = WGS72_DATUM;
+            pszDatumShort = "WGS 72";
+            break;
+            
+          case 3:
+            pszDatumLong = WGS84_DATUM;
+            pszDatumShort = "WGS 84";
+            break;
+            
+          case 4:
+            pszDatumLong = NAD83_DATUM;
+            pszDatumShort = "NAD 83";
+            break;
+
+          default:
+            pszDatumLong = "DATUM[\"unknown\"]";
+            pszDatumShort = "unknown";
+            break;
+        }
+        
+        poDS->pszProjection = 
+            CPLStrdup(CPLSPrintf( UTM_FORMAT, pszDatumShort, pszDatumLong,
+                                  nZone * 6 - 183, pszUnits ));
+    }
+    
+/* -------------------------------------------------------------------- */
+/*      Read the georeferencing information.                            */
+/* -------------------------------------------------------------------- */
+    unsigned char	abyRecordData[500];
+    
+    if( VSIFSeek( poDS->fpImage, nBytesPerLine * 2, SEEK_SET ) != 0
+        || VSIFRead(abyRecordData,sizeof(abyRecordData),1,poDS->fpImage) != 1 )
+    {
+        CPLError( CE_Failure, CPLE_FileIO,
+                  "Header read error on %s.\n",
+                  poOpenInfo->pszFilename );
+        return NULL;
+    }
+
+    poDS->dfULX = DOQGetField( abyRecordData + 288, 24 );
+    poDS->dfULY = DOQGetField( abyRecordData + 312, 24 );
+
+    if( VSIFSeek( poDS->fpImage, nBytesPerLine * 3, SEEK_SET ) != 0
+        || VSIFRead(abyRecordData,sizeof(abyRecordData),1,poDS->fpImage) != 1 )
+    {
+        CPLError( CE_Failure, CPLE_FileIO,
+                  "Header read error on %s.\n",
+                  poOpenInfo->pszFilename );
+        return NULL;
+    }
+
+    poDS->dfXPixelSize = DOQGetField( abyRecordData + 59, 12 );
+    poDS->dfYPixelSize = DOQGetField( abyRecordData + 71, 12 );
+
+    poDS->dfULX -= poDS->dfXPixelSize / 2;
+    poDS->dfULY += poDS->dfYPixelSize / 2;
 
     return( poDS );
 }
@@ -228,13 +341,20 @@ void GDALRegister_DOQ1()
 /*                            DOQGetField()                             */
 /************************************************************************/
 
-static int DOQGetField( unsigned char *pabyData, int nBytes )
+static double DOQGetField( unsigned char *pabyData, int nBytes )
 
 {
     char	szWork[128];
+    int		i;
 
     strncpy( szWork, (const char *) pabyData, nBytes );
     szWork[nBytes] = '\0';
 
-    return atoi(szWork);
+    for( i = 0; i < nBytes; i++ )
+    {
+        if( szWork[i] == 'D' || szWork[i] == 'd' )
+            szWork[i] = 'E';
+    }
+
+    return atof(szWork);
 }
