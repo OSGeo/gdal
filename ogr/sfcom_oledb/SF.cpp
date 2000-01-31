5c598abb9af32d44fb296113425a5e78e1ab23cd
Frank Warmerdam <warmerdam@pobox.com>
2000-01-31 16:26:21 +0000

    added header
    git-svn-id: https://svn.osgeo.org/gdal/trunk@1159 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/sfcom_oledb/SF.cpp
+++ ogr/sfcom_oledb/SF.cpp
@@ -1,9 +1,37 @@
-// SF.cpp : Implementation of DLL Exports.
-
-
-// Note: Proxy/Stub Information
-//      To build a separate proxy/stub DLL, 
-//      run nmake -f SFps.mk in the project directory.
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  OpenGIS Simple Features Reference Implementation
+ * Purpose:  Implementation of DLL Exports.
+ * Author:   Ken Shih, kshih@home.com
+ *
+ ******************************************************************************
+ * Copyright (c) 1999, Les Technologies SoftMap Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ******************************************************************************
+ *
+ * $Log$
+ * Revision 1.3  2000/01/31 16:26:21  warmerda
+ * added header
+ *
+ */
 
 #include "stdafx.h"
 #include "resource.h"
@@ -13,8 +41,6 @@
 #include "SF_i.c"
 #include "SFSess.h"
 #include "SFDS.h"
-
-
 
 CComModule _Module;
 
@@ -35,6 +61,7 @@ BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID /*lpReserved*/)
     }
     else if (dwReason == DLL_PROCESS_DETACH)
         _Module.Term();
+
     return TRUE;    // ok
 }
 
