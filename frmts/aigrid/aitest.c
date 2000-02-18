fea4e82de4b7409acce2f7f4db3305811a9d7555
Frank Warmerdam <warmerdam@pobox.com>
2000-02-18 14:56:48 +0000

    Added range of floating point magic numbers.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@1186 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/aigrid/aitest.c
+++ frmts/aigrid/aitest.c
@@ -22,13 +22,17 @@ static void DumpMagic( AIGInfo_t * psInfo, int bVerbose )
         VSIFSeek( psInfo->fpGrid, psInfo->panBlockOffset[i]+2, SEEK_SET );
         VSIFRead( &byMagic, 1, 1, psInfo->fpGrid );
 
-        if ( byMagic != 0 && byMagic != 0x43 && byMagic != 0x04
-             && byMagic != 0x08 && byMagic != 0x10 && byMagic != 0xd7 
-             && byMagic != 0xdf && byMagic != 0xe0 && byMagic != 0xfc
-             && byMagic != 0xf8 && byMagic != 0xff && byMagic != 0x41
-             && byMagic != 0x40 && byMagic != 0x42 && byMagic != 0xf0
-             && byMagic != 0xcf )
+        if( byMagic != 0 && byMagic != 0x43 && byMagic != 0x04
+            && byMagic != 0x08 && byMagic != 0x10 && byMagic != 0xd7 
+            && byMagic != 0xdf && byMagic != 0xe0 && byMagic != 0xfc
+            && byMagic != 0xf8 && byMagic != 0xff && byMagic != 0x41
+            && byMagic != 0x40 && byMagic != 0x42 && byMagic != 0xf0
+            && byMagic != 0xcf
+            && (psInfo->nCellType == AIG_CELLTYPE_INT
+                || (byMagic < 0x43 && byMagic > 0x3f)) )
             bReport = TRUE;
+
+        
 
         if( byMagic == 0 && psInfo->panBlockSize[i] > 8 
             && psInfo->nCellType == AIG_CELLTYPE_INT )
