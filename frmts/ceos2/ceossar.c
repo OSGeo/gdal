f91a76a33bb562564bdc9c69122848228270f64e
Frank Warmerdam <warmerdam@pobox.com>
2000-03-31 13:34:43 +0000

    ported to GDAL, replace headers
    git-svn-id: https://svn.osgeo.org/gdal/trunk@1301 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/ceos2/ceossar.c
+++ frmts/ceos2/ceossar.c
@@ -1,22 +1,37 @@
-/* Copyright (c) 1997
- * Atlantis Scientific Inc, 20 Colonnade, Suite 110
- * Nepean, Ontario, K2E 7M6, Canada
+/******************************************************************************
+ * $Id$
  *
- * All rights reserved.  Not to be used, reproduced
- * or disclosed without permission.
+ * Project:  ASI CEOS Translator
+ * Purpose:  Functions related to CeosSARVolume_t.
+ * Author:   Paul Lahaie, pjlahaie@atlsci.com
+ *
+ ******************************************************************************
+ * Copyright (c) 2000, Atlantis Scientific Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ******************************************************************************
+ *
+ * $Log$
+ * Revision 1.2  2000/03/31 13:34:43  warmerda
+ * ported to GDAL, replace headers
+ *
  */
-
-/* +---------------------------------------------------------------------+
- * |@@@@@@@@@@    @@@| EASI/PACE V6.0, Copyright (c) 1997.               |
- * |@@@@@@ ***      @|                                                   |
- * |@@@  *******    @| PCI Inc., 50 West Wilmot Street,                  |
- * |@@  *********  @@| Richmond Hill, Ontario, L4B 1M5, Canada.          |
- * |@    *******  @@@|                                                   |
- * |@      *** @@@@@@| All rights reserved. Not to be used, reproduced   |
- * |@@@    @@@@@@@@@@| or disclosed without permission.                  |
- * +---------------------------------------------------------------------+
- */
-
 
 #include "ceos.h"
 
@@ -119,3 +134,4 @@ void DeleteCeosSARVolume(CeosSARVolume_t *volume)
 	HFree( volume );
     }
 }
+
