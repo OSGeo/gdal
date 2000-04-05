605f7d40dacf2b9d3c258b773736e9df7c8e8e85
Frank Warmerdam <warmerdam@pobox.com>
2000-04-05 20:50:32 +0000

    'gdalized' file.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@1340 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/gtiff/libtiff/port.h
+++ frmts/gtiff/libtiff/port.h
@@ -7,25 +7,28 @@
  */
 #ifndef _PORT_
 #define _PORT_ 1
+#include "cpl_port.h"
+    
 #ifdef __cplusplus
 extern "C" {
 #endif
-#include <sys/types.h>
+
+#ifdef CPL_LSB
+#define HOST_BIGENDIAN 0
+#else
+#define HOST_BIGENDIAN 1
+#endif
+    
 #define HOST_FILLORDER FILLORDER_LSB2MSB
 #define HOST_BIGENDIAN	0
-#define HAVE_MMAP 1
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-#include <fcntl.h>
+#define HAVE_MMAP 0
+    
 typedef double dblparam_t;
-#ifdef __STRICT_ANSI__
-#define	INLINE	__inline__
-#else
-#define	INLINE	inline
-#endif
+    
+#define	INLINE
+    
 #define GLOBALDATA(TYPE,NAME)	extern TYPE NAME
+
 #ifdef __cplusplus
 }
 #endif
