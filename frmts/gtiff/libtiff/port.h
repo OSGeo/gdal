4fd40182272ad8137434f1f477261df6b5387c19
Frank Warmerdam <warmerdam@pobox.com>
2000-04-05 20:54:09 +0000

    Patched up a bit to work better on IRIX.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@1341 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/gtiff/libtiff/port.h
+++ frmts/gtiff/libtiff/port.h
@@ -1,12 +1,6 @@
-/*
- * Warning, this file was automatically created by the TIFF configure script
- * VERSION:	 v3.5.5
- * DATE:	 Tue Mar 28 23:28:13 EST 2000
- * TARGET:	 i586-unknown-linux
- * CCOMPILER:	 /usr/bin/gcc-2.95.2 20000220 (Debian GNU/Linux)
- */
 #ifndef _PORT_
 #define _PORT_ 1
+
 #include "cpl_port.h"
     
 #ifdef __cplusplus
@@ -20,7 +14,6 @@ extern "C" {
 #endif
     
 #define HOST_FILLORDER FILLORDER_LSB2MSB
-#define HOST_BIGENDIAN	0
 #define HAVE_MMAP 0
     
 typedef double dblparam_t;
@@ -28,6 +21,9 @@ typedef double dblparam_t;
 #define	INLINE
     
 #define GLOBALDATA(TYPE,NAME)	extern TYPE NAME
+
+#include <sys/types.h>
+#include <fcntl.h>
 
 #ifdef __cplusplus
 }
