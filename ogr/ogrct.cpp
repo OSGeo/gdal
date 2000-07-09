c488e3c8e6527f898c09a11fe994baae723001e1
Frank Warmerdam <warmerdam@pobox.com>
2000-07-09 20:48:28 +0000

    rewrote to use PROJ.4 datum shifting
    git-svn-id: https://svn.osgeo.org/gdal/trunk@1521 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/ogrct.cpp
+++ ogr/ogrct.cpp
@@ -28,6 +28,9 @@
  ******************************************************************************
  *
  * $Log$
+ * Revision 1.5  2000/07/09 20:48:28  warmerda
+ * rewrote to use PROJ.4 datum shifting
+ *
  * Revision 1.4  2000/03/24 14:49:31  warmerda
  * fetch TOWGS84 coefficients
  *
@@ -58,6 +61,8 @@ static PJ	*(*pfn_pj_init)(int, char**) = NULL;
 static UV	(*pfn_pj_fwd)(UV, PJ *) = NULL;
 static UV	(*pfn_pj_inv)(UV, PJ *) = NULL;
 static void	(*pfn_pj_free)(PJ *) = NULL;
+static int      (*pfn_pj_transform)(PJ *, PJ*, long, int, 
+                                    double *, double *, double * );
 
 #define RAD_TO_DEG	57.29577951308232
 #define DEG_TO_RAD	.0174532925199432958
@@ -75,16 +80,12 @@ static void	(*pfn_pj_free)(PJ *) = NULL;
 class OGRProj4CT : public OGRCoordinateTransformation
 {
     OGRSpatialReference *poSRSSource;
-    void	*psPJSource; 		/* NULL if lat/long */
-    double      *padfSourceBursaWolf;	/* NULL if WGS84 or unknown */
-    double	dfSourceSemiMajor;
-    double      dfSourceSemiMinor;
+    void	*psPJSource;
+    int         bSourceLatLong;
 
     OGRSpatialReference *poSRSTarget;
-    void        *psPJTarget; 		/* NULL if lat/long */
-    double      *padfTargetBursaWolf;	/* NULL if WGS84 or unkonwn */
-    double	dfTargetSemiMajor;
-    double      dfTargetSemiMinor;
+    void        *psPJTarget;
+    int         bTargetLatLong;
 
 public:
 		OGRProj4CT();
@@ -125,6 +126,18 @@ static int LoadProjLibrary()
     pfn_pj_fwd = (UV (*)(UV,PJ*)) CPLGetSymbol( LIBNAME, "pj_fwd" );
     pfn_pj_inv = (UV (*)(UV,PJ*)) CPLGetSymbol( LIBNAME, "pj_inv" );
     pfn_pj_free = (void (*)(PJ*)) CPLGetSymbol( LIBNAME, "pj_free" );
+    pfn_pj_transform = (int (*)(PJ*,PJ*,long,int,double*,double*,double*))
+                        CPLGetSymbol( LIBNAME, "pj_transform" );
+
+    if( pfn_pj_transform == NULL )
+    {
+        CPLError( CE_Failure, CPLE_AppDefined, 
+                  "Attempt to load %s, but couldn't find pj_transform.\n"
+                  "Please upgrade to PROJ 4.1.2 or later.", 
+                  LIBNAME );
+
+        return FALSE;
+    }
 
     return( TRUE );
 }
@@ -216,12 +229,7 @@ OGRProj4CT::OGRProj4CT()
 
 {
     poSRSSource = NULL;
-    psPJSource = NULL;
-    padfSourceBursaWolf = NULL;
-
     poSRSTarget = NULL;
-    psPJTarget = NULL;
-    padfTargetBursaWolf = NULL;
 }
 
 /************************************************************************/
@@ -242,12 +250,6 @@ OGRProj4CT::~OGRProj4CT()
 
     if( psPJTarget != NULL )
         pfn_pj_free( psPJTarget );
-
-    if( padfSourceBursaWolf != NULL )
-        CPLFree( padfSourceBursaWolf );
-
-    if( padfTargetBursaWolf != NULL )
-        CPLFree( padfTargetBursaWolf );
 }
 
 /************************************************************************/
@@ -261,122 +263,54 @@ int OGRProj4CT::Initialize( OGRSpatialReference * poSourceIn,
     poSRSSource = poSourceIn->Clone();
     poSRSTarget = poTargetIn->Clone();
 
-/* -------------------------------------------------------------------- */
-/*      Collect ellipsoid axes for source and target.                   */
-/* -------------------------------------------------------------------- */
-    const char *pszValue;
-
-    pszValue = poSRSSource->GetAttrValue( "SPHEROID", 1 );
-    if( pszValue == NULL || atof(pszValue) == 0.0 )
-        return FALSE;
-    else
-        dfSourceSemiMajor = atof(pszValue);
-
-    pszValue = poSRSSource->GetAttrValue( "SPHEROID", 2 );
-    if( pszValue == NULL || atof(pszValue) == 0.0 )
-        return FALSE;
-    else
-        dfSourceSemiMinor = dfSourceSemiMajor
-            * (1 - 1.0 / atof(pszValue));
-
-    pszValue = poSRSTarget->GetAttrValue( "SPHEROID", 1 );
-    if( pszValue == NULL || atof(pszValue) == 0.0 )
-        return FALSE;
-    else
-        dfTargetSemiMajor = atof(pszValue);
-
-    pszValue = poSRSTarget->GetAttrValue( "SPHEROID", 2 );
-    if( pszValue == NULL || atof(pszValue) == 0.0 )
-        return FALSE;
-    else
-        dfTargetSemiMinor = dfTargetSemiMajor
-            * (1 - 1.0 / atof(pszValue));
+    bSourceLatLong = poSRSSource->IsGeographic();
+    bTargetLatLong = poSRSTarget->IsGeographic();
 
 /* -------------------------------------------------------------------- */
 /*      Establish PROJ.4 handle for source if projection.               */
 /* -------------------------------------------------------------------- */
-    if( poSRSSource->IsProjected() )
-    {
-        char	*pszProj4Defn, **papszArgs;
+    char	*pszProj4Defn, **papszArgs;
 
-        if( poSRSSource->exportToProj4( &pszProj4Defn ) != OGRERR_NONE )
-            return FALSE;
+    if( poSRSSource->exportToProj4( &pszProj4Defn ) != OGRERR_NONE )
+        return FALSE;
 
-        papszArgs = CSLTokenizeStringComplex( pszProj4Defn, " +",TRUE,FALSE );
-
-        psPJSource = pfn_pj_init( CSLCount(papszArgs), papszArgs );
-
-        if( psPJSource == NULL )
-            CPLError( CE_Failure, CPLE_NotSupported, 
-                      "Failed to initialize PROJ.4 with `%s'.", 
-                      pszProj4Defn );
-
-        CSLDestroy( papszArgs );
-        CPLFree( pszProj4Defn );
-
-        if( psPJSource == NULL )
-            return FALSE;
-    }
+    printf( "source = %s\n", pszProj4Defn );
+    papszArgs = CSLTokenizeStringComplex( pszProj4Defn, " +",TRUE,FALSE );
+    
+    psPJSource = pfn_pj_init( CSLCount(papszArgs), papszArgs );
+    
+    if( psPJSource == NULL )
+        CPLError( CE_Failure, CPLE_NotSupported, 
+                  "Failed to initialize PROJ.4 with `%s'.", 
+                  pszProj4Defn );
+    
+    CSLDestroy( papszArgs );
+    CPLFree( pszProj4Defn );
+    
+    if( psPJSource == NULL )
+        return FALSE;
 
 /* -------------------------------------------------------------------- */
 /*      Establish PROJ.4 handle for target if projection.               */
 /* -------------------------------------------------------------------- */
-    if( poSRSTarget->IsProjected() )
-    {
-        char	*pszProj4Defn, **papszArgs;
+    if( poSRSTarget->exportToProj4( &pszProj4Defn ) != OGRERR_NONE )
+        return FALSE;
 
-        if( poSRSTarget->exportToProj4( &pszProj4Defn ) != OGRERR_NONE )
-            return FALSE;
-
-        papszArgs = CSLTokenizeStringComplex( pszProj4Defn, " +",TRUE,FALSE );
-
-        psPJTarget = pfn_pj_init( CSLCount(papszArgs), papszArgs );
-
-        if( psPJTarget == NULL )
-            CPLError( CE_Failure, CPLE_NotSupported, 
-                      "Failed to initialize PROJ.4 with `%s'.", 
-                      pszProj4Defn );
-
-        CSLDestroy( papszArgs );
-        CPLFree( pszProj4Defn );
-
-        if( psPJTarget == NULL )
-            return FALSE;
-    }
-
-/* -------------------------------------------------------------------- */
-/*      Do we need Bursa-Wolf coefficients?                             */
-/* -------------------------------------------------------------------- */
-    if( !poSRSSource->IsSameGeogCS( poSRSTarget ) )
-    {
-        double adfCoeffs[7];
-
-        if( poSRSSource->GetTOWGS84( adfCoeffs ) == OGRERR_NONE 
-            && (adfCoeffs[0] != 0.0 
-                || adfCoeffs[1] != 0.0 
-                || adfCoeffs[2] != 0.0 
-                || adfCoeffs[3] != 0.0 
-                || adfCoeffs[4] != 0.0 
-                || adfCoeffs[5] != 0.0 
-                || adfCoeffs[6] != 0.0) )
-        {
-            padfSourceBursaWolf = (double *) CPLMalloc(sizeof(double)*7);
-            memcpy( padfSourceBursaWolf, adfCoeffs, sizeof(double)*7 );
-        }
-
-        if( poSRSTarget->GetTOWGS84( adfCoeffs ) == OGRERR_NONE 
-            && (adfCoeffs[0] != 0.0 
-                || adfCoeffs[1] != 0.0 
-                || adfCoeffs[2] != 0.0 
-                || adfCoeffs[3] != 0.0 
-                || adfCoeffs[4] != 0.0 
-                || adfCoeffs[5] != 0.0 
-                || adfCoeffs[6] != 0.0) )
-        {
-            padfTargetBursaWolf = (double *) CPLMalloc(sizeof(double)*7);
-            memcpy( padfTargetBursaWolf, adfCoeffs, sizeof(double)*7 );
-        }
-    }
+    printf( "target = %s\n", pszProj4Defn );
+    papszArgs = CSLTokenizeStringComplex( pszProj4Defn, " +",TRUE,FALSE );
+    
+    psPJTarget = pfn_pj_init( CSLCount(papszArgs), papszArgs );
+    
+    if( psPJTarget == NULL )
+        CPLError( CE_Failure, CPLE_NotSupported, 
+                  "Failed to initialize PROJ.4 with `%s'.", 
+                  pszProj4Defn );
+    
+    CSLDestroy( papszArgs );
+    CPLFree( pszProj4Defn );
+    
+    if( psPJTarget == NULL )
+        return FALSE;
 
     return TRUE;
 }
@@ -408,119 +342,31 @@ OGRSpatialReference *OGRProj4CT::GetTargetCS()
 int OGRProj4CT::Transform( int nCount, double *x, double *y, double *z )
 
 {
-/* -------------------------------------------------------------------- */
-/*      Transform from projected source coordinates into source GCS     */
-/*      lat/long coordinates.  Note that PROJ.4 is also applying        */
-/*      linear units translation into meters if needed.                 */
-/* -------------------------------------------------------------------- */
-    if( psPJSource != NULL )
-    {
-        UV	uv;
+    int   err, i;
 
-        for( int i = 0; i < nCount; i++ )
+    if( bSourceLatLong )
+    {
+        for( i = 0; i < nCount; i++ )
         {
-            uv.u = x[i];
-            uv.v = y[i];
-            
-            uv = pfn_pj_inv( uv, psPJSource );
-            
-            x[i] = uv.u * RAD_TO_DEG;
-            y[i] = uv.v * RAD_TO_DEG;
+            x[i] *= DEG_TO_RAD;
+            y[i] *= DEG_TO_RAD;
         }
     }
 
-/* -------------------------------------------------------------------- */
-/*      Do we need to translate to WGS84?                               */
-/* -------------------------------------------------------------------- */
-    if( padfSourceBursaWolf != NULL || padfTargetBursaWolf != NULL )
+    err = pfn_pj_transform( psPJSource, psPJTarget, nCount, 1, x, y, z );
+
+    if( err != 0 )
     {
-        double	dfXGC, dfYGC, dfZGC;
-        double  dfSrcESquared, dfNU, dfTrgESquared;
-
-        /* FIXME:we should really adjust for non-greenwich prime meridians! */
-
-        /* Compute the eccentricity squared */
-        dfSrcESquared = ((dfSourceSemiMajor*dfSourceSemiMajor) 
-                         - (dfSourceSemiMinor*dfSourceSemiMinor))
-            / (dfSourceSemiMajor*dfSourceSemiMajor);
-
-        dfTrgESquared = ((dfTargetSemiMajor*dfTargetSemiMajor) 
-                         - (dfTargetSemiMinor*dfTargetSemiMinor))
-            / (dfTargetSemiMajor*dfTargetSemiMajor);
-
-        for( int i = 0; i < nCount; i++ )
-        {
-            double	dfLatRad, dfLonRad, dfHeight;
-
-            if( z != NULL )
-                dfHeight = z[i];
-            else
-                dfHeight = 0.0;
-
-            dfLonRad = x[i] * DEG_TO_RAD;
-            dfLatRad = y[i] * DEG_TO_RAD;
-
-            /* Compute prime vertical radius at lat/long point */
-            dfNU = dfSourceSemiMajor /
-                sqrt(1 - dfSrcESquared * sin(dfLatRad)*sin(dfLatRad));
-
-            /* change to geocentric coordinates */
-            dfXGC = (dfNU + dfHeight) * cos(dfLatRad) * cos(dfLonRad);
-            dfYGC = (dfNU + dfHeight) * cos(dfLatRad) * sin(dfLonRad);
-            dfZGC = ((1 - dfSrcESquared) * dfNU + dfHeight) * sin(dfLatRad);
-
-            /* apply the source bursa-wolf tranform 
-               (FIXME: for now just the raw shift) */
-            if( padfSourceBursaWolf != NULL )
-            {
-                dfXGC += padfSourceBursaWolf[0];
-                dfYGC += padfSourceBursaWolf[1];
-                dfZGC += padfSourceBursaWolf[2];
-            }
-
-            /* apply the target bursa-wolf tranform 
-               (FIXME: for now just the raw shift) */
-            if( padfTargetBursaWolf != NULL )
-            {
-                dfXGC -= padfTargetBursaWolf[0];
-                dfYGC -= padfTargetBursaWolf[1];
-                dfZGC -= padfTargetBursaWolf[2];
-            }
-
-            /* change back to lat/long on target ellipsoid */
-            dfLatRad = atan((dfZGC + dfTrgESquared * dfNU * sin(dfLatRad))
-                / sqrt(dfXGC*dfXGC + dfYGC*dfYGC));
-            dfLonRad = atan(dfYGC / dfXGC);
-            
-            if( z != NULL )
-            {
-                dfHeight = dfXGC / (cos(dfLatRad)*cos(dfLonRad)) - dfNU;
-                z[i] = dfHeight;
-            }
-
-            x[i] = dfLonRad * RAD_TO_DEG;
-            y[i] = dfLatRad * RAD_TO_DEG;
-        }
+        printf( "pfn_pj_transform failed:%d.\n", err );
+        return FALSE;
     }
 
-/* -------------------------------------------------------------------- */
-/*      Transform to projected target coordinates from target GCS       */
-/*      lat/long coordinates.  Note that PROJ.4 is also applying        */
-/*      linear units translation from meters if needed.                 */
-/* -------------------------------------------------------------------- */
-    if( psPJTarget != NULL )
+    if( bTargetLatLong )
     {
-        UV	uv;
-
-        for( int i = 0; i < nCount; i++ )
+        for( i = 0; i < nCount; i++ )
         {
-            uv.u = x[i] * DEG_TO_RAD;
-            uv.v = y[i] * DEG_TO_RAD;
-            
-            uv = pfn_pj_fwd( uv, psPJTarget );
-            
-            x[i] = uv.u;
-            y[i] = uv.v;
+            x[i] *= RAD_TO_DEG;
+            y[i] *= RAD_TO_DEG;
         }
     }
 
