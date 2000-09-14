ec98ea49e067f29bab8ec73dfb098a28440c3777
Frank Warmerdam <warmerdam@pobox.com>
2000-09-14 21:05:17 +0000

    don't fail if BEENWRITING not set
    git-svn-id: https://svn.osgeo.org/gdal/trunk@1685 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/gtiff/libtiff/tif_flush.c
+++ frmts/gtiff/libtiff/tif_flush.c
@@ -45,12 +45,18 @@ TIFFFlush(TIFF* tif)
 
 /*
  * Flush buffered data to the file.
+ *
+ * Frank Warmerdam'2000: I modified this to return 1 if TIFF_BEENWRITING
+ * is not set, so that TIFFFlush() will proceed to write out the directory.
+ * The documentation says returning 1 is an error indicator, but not having
+ * been writing isn't exactly a an error.  Hopefully this doesn't cause
+ * problems for other people. 
  */
 int
 TIFFFlushData(TIFF* tif)
 {
 	if ((tif->tif_flags & TIFF_BEENWRITING) == 0)
-		return (0);
+		return (1);
 	if (tif->tif_flags & TIFF_POSTENCODE) {
 		tif->tif_flags &= ~TIFF_POSTENCODE;
 		if (!(*tif->tif_postencode)(tif))
@@ -58,3 +64,9 @@ TIFFFlushData(TIFF* tif)
 	}
 	return (TIFFFlushData1(tif));
 }
+
+
+
+
+
+
