071aa6ae335b009cfe817f8b38f902e923590648
Frank Warmerdam <warmerdam@pobox.com>
2000-11-09 06:22:15 +0000

    New
    git-svn-id: https://svn.osgeo.org/gdal/trunk@1805 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ ogr/ogr_srs_esri.cpp
@@ -0,0 +1,263 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  OpenGIS Simple Features Reference Implementation
+ * Purpose:  OGRSpatialReference translation to/from ESRI .prj definitions.
+ * Author:   Frank Warmerdam, warmerda@home.com
+ *
+ ******************************************************************************
+ * Copyright (c) 2000, Frank Warmerdam
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ******************************************************************************
+ *
+ * $Log$
+ * Revision 1.1  2000/11/09 06:22:15  warmerda
+ * New
+ *
+ */
+
+#include "ogr_spatialref.h"
+#include "ogr_p.h"
+
+/************************************************************************/
+/*                         OSRImportFromESRI()                          */
+/************************************************************************/
+
+OGRErr OSRImportFromESRI( OGRSpatialReferenceH hSRS, char **papszPrj )
+
+{
+    return ((OGRSpatialReference *) hSRS)->importFromESRI( papszPrj );
+}
+
+/************************************************************************/
+/*                              OSR_GDV()                               */
+/*                                                                      */
+/*      Fetch a particular parameter out of the parameter list, or      */
+/*      the indicated default if it isn't available.  This is a         */
+/*      helper function for importFromESRI().                           */
+/************************************************************************/
+
+static double OSR_GDV( char **papszNV, const char * pszField, 
+                       double dfDefaultValue )
+
+{
+    int		iLine;
+
+    if( papszNV == NULL || papszNV[0] == NULL )
+        return dfDefaultValue;
+
+    if( EQUALN(pszField,"PARAM_",6) )
+    {
+        int	nOffset;
+
+        for( iLine = 0; 
+             papszNV[iLine] != NULL && !EQUALN(papszNV[iLine],"Paramet",7);
+             iLine++ ) {}
+
+        for( nOffset=atoi(pszField+6); 
+             papszNV[iLine] != NULL && nOffset > 0; 
+             nOffset--, iLine++ ) {}
+
+        if( papszNV[iLine] != NULL )
+        {
+            char	**papszTokens, *pszLine = papszNV[iLine];
+            double	dfValue;
+            
+            int         i;
+            
+            // Trim comments.
+            for( i=0; pszLine[i] != '\0'; i++ )
+            {
+                if( pszLine[i] == '/' && pszLine[i+1] == '*' )
+                    pszLine[i] = '\0';
+            }
+
+            papszTokens = CSLTokenizeString(papszNV[iLine]);
+            if( CSLCount(papszTokens) == 3 )
+            {
+                dfValue = ABS(atof(papszTokens[0]))
+                    + atof(papszTokens[1]) / 60.0
+                    + atof(papszTokens[2]) / 3600.0;
+
+                if( atof(papszTokens[0]) < 0.0 )
+                    dfValue *= -1;
+            }
+            else
+                dfValue = atof(papszTokens[0]);
+
+            CSLDestroy( papszTokens );
+
+            return dfValue;
+        }
+        else
+            return dfDefaultValue;
+    }
+    else
+    {
+        for( iLine = 0; 
+             papszNV[iLine] != NULL && 
+                 !EQUALN(papszNV[iLine],pszField,strlen(pszField));
+             iLine++ ) {}
+
+        if( papszNV[iLine] == NULL )
+            return dfDefaultValue;
+        else
+            return atof( papszNV[iLine] + strlen(pszField) );
+    }
+}
+
+/************************************************************************/
+/*                              OSR_GDS()                               */
+/************************************************************************/
+
+static const char*OSR_GDS( char **papszNV, const char * pszField, 
+                           const char *pszDefaultValue )
+
+{
+    int		iLine;
+
+    if( papszNV == NULL || papszNV[0] == NULL )
+        return pszDefaultValue;
+
+    for( iLine = 0; 
+         papszNV[iLine] != NULL && 
+             !EQUALN(papszNV[iLine],pszField,strlen(pszField));
+         iLine++ ) {}
+
+    if( papszNV[iLine] == NULL )
+        return pszDefaultValue;
+    else
+    {
+        static char	szResult[80];
+        char	**papszTokens;
+        
+        papszTokens = CSLTokenizeString(papszNV[iLine]);
+
+        if( CSLCount(papszTokens) > 1 )
+            strncpy( szResult, papszTokens[1], sizeof(szResult));
+        else
+            strncpy( szResult, pszDefaultValue, sizeof(szResult));
+        
+        CSLDestroy( papszTokens );
+        return szResult;
+    }
+}
+
+/************************************************************************/
+/*                          importFromESRI()                            */
+/************************************************************************/
+
+OGRErr OGRSpatialReference::importFromESRI( char **papszPrj )
+
+{
+/* -------------------------------------------------------------------- */
+/*      Operate on the basis of the projection name.                    */
+/* -------------------------------------------------------------------- */
+    const char *pszProj = OSR_GDS( papszPrj, "Projection", NULL );
+
+    if( pszProj == NULL )
+    {
+        CPLDebug( "OGR_ESRI", "Can't find Projection\n" );
+        return OGRERR_CORRUPT_DATA;
+    }
+
+    else if( EQUAL(pszProj,"GEOGRAPHIC") )
+    {
+    }
+    
+    else if( EQUAL(pszProj,"utm") )
+    {
+        // I am not sure how southern hemisphere is marked.
+        SetUTM( (int) OSR_GDV( papszPrj, "zone", 0.0 ) );
+    }
+
+    else if( EQUAL(pszProj,"ALBERS") )
+    {
+        SetACEA( OSR_GDV( papszPrj, "PARAM_1", 0.0 ), 
+                 OSR_GDV( papszPrj, "PARAM_2", 0.0 ), 
+                 OSR_GDV( papszPrj, "PARAM_4", 0.0 ), 
+                 OSR_GDV( papszPrj, "PARAM_3", 0.0 ), 
+                 OSR_GDV( papszPrj, "PARAM_5", 0.0 ), 
+                 OSR_GDV( papszPrj, "PARAM_6", 0.0 ) );
+    }
+
+    else if( EQUAL(pszProj,"EQUIDISTANT_CONIC") )
+    {
+        int	nStdPCount = (int) OSR_GDV( papszPrj, "PARAM_1", 0.0 );
+
+        if( nStdPCount == 1 )
+        {
+            SetEC( OSR_GDV( papszPrj, "PARAM_2", 0.0 ), 
+                   OSR_GDV( papszPrj, "PARAM_2", 0.0 ), 
+                   OSR_GDV( papszPrj, "PARAM_4", 0.0 ), 
+                   OSR_GDV( papszPrj, "PARAM_3", 0.0 ), 
+                   OSR_GDV( papszPrj, "PARAM_5", 0.0 ), 
+                   OSR_GDV( papszPrj, "PARAM_6", 0.0 ) );
+        }
+        else
+        {
+            SetEC( OSR_GDV( papszPrj, "PARAM_2", 0.0 ), 
+                   OSR_GDV( papszPrj, "PARAM_3", 0.0 ), 
+                   OSR_GDV( papszPrj, "PARAM_5", 0.0 ), 
+                   OSR_GDV( papszPrj, "PARAM_4", 0.0 ), 
+                   OSR_GDV( papszPrj, "PARAM_5", 0.0 ), 
+                   OSR_GDV( papszPrj, "PARAM_7", 0.0 ) );
+        }
+    }
+
+    else
+    {
+        CPLDebug( "OGR_ESRI", "Unsupported projection: %s", pszProj );
+        SetLocalCS( pszProj );
+    }
+
+/* -------------------------------------------------------------------- */
+/*      Try to translate the datum.                                     */
+/* -------------------------------------------------------------------- */
+    const char *pszValue;
+    int	       bFullDefined = FALSE;
+
+    if( !IsLocal() )
+    {
+        pszValue = OSR_GDS( papszPrj, "Datum", "WGS84");
+        if( EQUAL(pszValue,"NAD27") || EQUAL(pszValue,"NAD83")
+            || EQUAL(pszValue,"WGS84") || EQUAL(pszValue,"WGS72") )
+        {
+            SetWellKnownGeogCS( pszValue );
+            bFullDefined = TRUE;
+        }
+    }
+
+/* -------------------------------------------------------------------- */
+/*      Linear units translation                                        */
+/* -------------------------------------------------------------------- */
+    if( IsLocal() || IsProjected() )
+    {
+        pszValue = OSR_GDS( papszPrj, "Units", NULL );
+        if( pszValue == NULL )
+            SetLinearUnits( SRS_UL_METER, 1.0 );
+        else if( EQUAL(pszValue,"FEET") )
+            SetLinearUnits( SRS_UL_FOOT, atof(SRS_UL_FOOT_CONV) );
+        else
+            SetLinearUnits( pszValue, 1.0 );
+    }
+    
+    return OGRERR_NONE;
+}
