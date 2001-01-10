202f847bbbdb12af4202e8dd621dc65a7f8446aa
Frank Warmerdam <warmerdam@pobox.com>
2001-01-10 16:11:33 +0000

    added lots of documentation, and extents api
    git-svn-id: https://svn.osgeo.org/gdal/trunk@1939 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/ogrsf_frmts/dgn/dgnlib.h
+++ ogr/ogrsf_frmts/dgn/dgnlib.h
@@ -28,6 +28,9 @@
  ******************************************************************************
  *
  * $Log$
+ * Revision 1.4  2001/01/10 16:11:33  warmerda
+ * added lots of documentation, and extents api
+ *
  * Revision 1.3  2000/12/28 21:28:43  warmerda
  * added element index support
  *
@@ -54,25 +57,44 @@ CPL_C_START
  * Definitions of public structures and API of DGN Library.
  */
 
+/**
+ * DGN Point structure.
+ *
+ * Note that the DGNReadElement() function transforms points into "master"
+ * coordinate system space when they are in the file in UOR (units of
+ * resolution) coordinates. 
+ */
+
 typedef struct {
-    double x;
-    double y;
-    double z;
+    double x;	/*!< X (normally eastwards) coordinate. */
+    double y;   /*!< y (normally northwards) coordinate. */
+    double z;   /*!< z, up coordinate.  Zero for 2D objects. */
 } DGNPoint;
 
+/**
+ * Element summary information.
+ *
+ * Minimal information kept about each element if an element summary
+ * index is built for a file by DGNGetElementIndex().
+ */
 typedef struct {
-    unsigned char	level;
-    unsigned char       type;
-    unsigned char       stype;
-    unsigned char       flags;
-    long                offset;
+    unsigned char	level;   /*!< Element Level: 0-63 */
+    unsigned char       type;    /*!< Element type (DGNT_*) */
+    unsigned char       stype;   /*!< Structure type (DGNST_*) */
+    unsigned char       flags;   /*!< Other flags */
+    long                offset;  /*!< Offset within file (private) */
 } DGNElementInfo;  
 
-/* -------------------------------------------------------------------- */
-/*      DGNElemCore                                                     */
-/* -------------------------------------------------------------------- */
-
-/** Core element structure. */
+/**
+ * Core element structure. 
+ *
+ * Core information kept about each element that can be read from a DGN
+ * file.  This structure is the first component of each specific element 
+ * structure (like DGNElemMultiPoint).  Normally the DGNElemCore.stype
+ * field would be used to decide what specific structure type to case the
+ * DGNElemCore pointer to. 
+ *
+ */
 
 typedef struct {
 #ifdef DGN_DEBUG
@@ -80,17 +102,20 @@ typedef struct {
     GUInt32     size;
 #endif
 
-    int         element_id;     /** Element number (zero based) */
-    int         stype;          /** Structure type: (DGNST_*) */
-    int		level;		/** Element Level: 0-63 */
-    int		type;		/** Element type (DGNT_) */
-    int		complex;	/** Is element complex? */
+    int         element_id;     /*!< Element number (zero based) */
+    int         stype;          /*!< Structure type: (DGNST_*) */
+    int		level;		/*!< Element Level: 0-63 */
+    int		type;		/*!< Element type (DGNT_) */
+    int		complex;	/*!< Is element complex? */
 
-    int		graphic_group;  /** Graphic group number */
-    int		properties;     /** Properties: ORing of DGNP_ flags */
-    int         color;          /** Color index (0-255) */
-    int         weight;         /** Line Weight (0-31) */
-    int         style;          /** Line Style: One of DGNS_* values */
+    int		graphic_group;  /*!< Graphic group number */
+    int		properties;     /*!< Properties: ORing of DGNP_ flags */
+    int         color;          /*!< Color index (0-255) */
+    int         weight;         /*!< Line Weight (0-31) */
+    int         style;          /*!< Line Style: One of DGNS_* values */
+
+    int		attr_bytes;	/*!< Bytes of attribute data, usually zero. */
+    unsigned char *attr_data;   /*!< Raw attribute data */
 } DGNElemCore;
 
 /** 
@@ -105,8 +130,8 @@ typedef struct {
 typedef struct {
   DGNElemCore 	core;
 
-  int		num_vertices;  /** Number of vertices in "vertices" */
-  DGNPoint      vertices[2];   /** Array of two or more vertices */
+  int		num_vertices;  /*!< Number of vertices in "vertices" */
+  DGNPoint      vertices[2];   /*!< Array of two or more vertices */
 
 } DGNElemMultiPoint;    
 
@@ -121,12 +146,12 @@ typedef struct {
 typedef struct {
   DGNElemCore 	core;
 
-  DGNPoint	origin;		/** Origin of ellipse */
+  DGNPoint	origin;		/*!< Origin of ellipse */
 
-  double	primary_axis;	/** Primary axis length */
-  double        secondary_axis; /** Secondary axis length */
+  double	primary_axis;	/*!< Primary axis length */
+  double        secondary_axis; /*!< Secondary axis length */
 
-  double	rotation;       /** Counterclockwise rotation in degrees */
+  double	rotation;       /*!< Counterclockwise rotation in degrees */
   long          quat[4];
 
 } DGNElemEllipse;
@@ -166,7 +191,7 @@ typedef struct {
   DGNElemCore 	core;
 
   int           screen_flag;
-  GByte         color_info[256][3];
+  GByte         color_info[256][3]; /*!< Color table, 256 colors by red (0), green(1) and blue(2) component. */
 } DGNElemColorTable;
 
 /** 
@@ -175,33 +200,51 @@ typedef struct {
  * The core.stype code is DGNST_TCB.
  *
  * Returned for DGNT_TCB(9).
+ *
+ * The first TCB in the file is used to determine the dimension (2D vs. 3D),
+ * and transformation from UOR (units of resolution) to subunits, and subunits
+ * to master units.  This is handled transparently within DGNReadElement(), so
+ * it is not normally necessary to handle this element type at the application
+ * level, though it can be useful to get the sub_units, and master_units names.
  */
 
 typedef struct {
     DGNElemCore core;
 
-    int		dimension;
+    int		dimension;         /*!< Dimension (2 or 3) */
 
-    double	origin_x;
-    double      origin_y;
-    double      origin_z;
+    double	origin_x;       /*!< X origin of UOR space in master units(?)*/
+    double      origin_y;       /*!< Y origin of UOR space in master units(?)*/
+    double      origin_z;       /*!< Z origin of UOR space in master units(?)*/
     
-    long	uor_per_subunit;
-    char	sub_units[3];
-    long        subunits_per_master;
-    char        master_units[3];
+    long	uor_per_subunit;   /*!< UOR per subunit. */
+    char	sub_units[3];      /*!< User name for subunits (2 chars)*/
+    long        subunits_per_master; /*!< Subunits per master unit. */
+    char        master_units[3];   /*!< User name for master units (2 chars)*/
 
 } DGNElemTCB;
 
 /* -------------------------------------------------------------------- */
 /*      Structure types                                                 */
 /* -------------------------------------------------------------------- */
-#define DGNST_CORE		   1
-#define DGNST_MULTIPOINT	   2
-#define DGNST_COLORTABLE           3
-#define DGNST_TCB                  4
-#define DGNST_ELLIPSE              5
-#define DGNST_TEXT                 6
+
+/** DGNElemCore style: Element uses DGNElemCore structure */
+#define DGNST_CORE		   1 
+
+/** DGNElemCore style: Element uses DGNElemMultiPoint structure */
+#define DGNST_MULTIPOINT	   2 
+
+/** DGNElemCore style: Element uses DGNElemColorTable structure */
+#define DGNST_COLORTABLE           3 
+
+/** DGNElemCore style: Element uses DGNElemTCB structure */
+#define DGNST_TCB                  4 
+
+/** DGNElemCore style: Element uses DGNElemEllipse structure */
+#define DGNST_ELLIPSE              5 
+
+/** DGNElemCore style: Element uses DGNElemText structure */
+#define DGNST_TEXT                 6 
 
 /* -------------------------------------------------------------------- */
 /*      Element types                                                   */
@@ -248,12 +291,26 @@ typedef struct {
 #define DGN_GDL_REF_FILE        9
 
 /* -------------------------------------------------------------------- */
+/*      Word 17 property flags.                                         */
+/* -------------------------------------------------------------------- */
+#define DGNPF_HOLE	   0x8000
+#define DGNPF_SNAPPABLE    0x4000
+#define DGNPF_PLANAR       0x2000
+#define DGNPF_ORIENTATION  0x1000
+#define DGNPF_ATTRIBUTES   0x0800
+#define DGNPF_MODIFIED     0x0400
+#define DGNPF_NEW          0x0200
+#define DGNPF_LOCKED       0x0100
+
+/* -------------------------------------------------------------------- */
 /*      API                                                             */
 /* -------------------------------------------------------------------- */
+/** Opaque handle representing DGN file, used with DGN API. */
 typedef void *DGNHandle;
 
 DGNHandle CPL_DLL    DGNOpen( const char * );
 const DGNElementInfo CPL_DLL *DGNGetElementIndex( DGNHandle, int * );
+int CPL_DLL          DGNGetExtents( DGNHandle, double * );
 DGNElemCore CPL_DLL *DGNReadElement( DGNHandle );
 void CPL_DLL         DGNFreeElement( DGNHandle, DGNElemCore * );
 void CPL_DLL         DGNRewind( DGNHandle );
