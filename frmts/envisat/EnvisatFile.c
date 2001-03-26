98d2870219f0ce021561f7af265f4c7f10d2c79d
Frank Warmerdam <warmerdam@pobox.com>
2001-03-26 18:44:03 +0000

    removed debugging statements
    git-svn-id: https://svn.osgeo.org/gdal/trunk@2129 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/envisat/EnvisatFile.c
+++ frmts/envisat/EnvisatFile.c
@@ -28,6 +28,9 @@
  ******************************************************************************
  *
  * $Log$
+ * Revision 1.4  2001/03/26 18:44:03  warmerda
+ * removed debugging statements
+ *
  * Revision 1.3  2001/03/26 18:43:05  warmerda
  * fixed bug in EnvisatFile_Close
  *
@@ -707,7 +710,6 @@ void EnvisatFile_Close( EnvisatFile *self )
     S_NameValueList_Destroy( &(self->mph_count), &(self->mph_entries) );
     S_NameValueList_Destroy( &(self->sph_count), &(self->sph_entries) );
 
-printf( "-- 5 --\n" );
     for( i = 0; i < self->ds_count; i++ )
     {
         if( self->ds_info != NULL && self->ds_info[i] != NULL )
@@ -718,16 +720,12 @@ printf( "-- 5 --\n" );
             free( self->ds_info[i] );
         }
     }
-printf( "-- 6 --\n" );
     if( self->ds_info != NULL )
         free( self->ds_info );
-printf( "-- 6.5 --\n" );
     if( self->filename != NULL )
         free( self->filename );
-printf( "-- 7 --\n" );
 
     free( self );
-printf( "-- 8 --\n" );
 }
 
 /*-----------------------------------------------------------------------------
