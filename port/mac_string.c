3747360a2b2a6d843b5b7dd6bfa9677c93ef7db8
Frank Warmerdam <warmerdam@pobox.com>
2001-04-30 18:15:39 +0000

    New
    git-svn-id: https://svn.osgeo.org/gdal/trunk@2166 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ port/mac_string.c
@@ -0,0 +1,88 @@
+/**********************************************************************
+ * $Id$
+ *
+ * Project:  CPL - Common Portability Library
+ * Purpose:  String functions for MacOS (pre MacOS X).
+ * Author:   Dennis Christopher, dennis@avenza.com
+ *
+ **********************************************************************
+ * Copyright (c) 2001, Avenza Systems, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ **********************************************************************
+ *
+ * $Log$
+ * Revision 1.1  2001/04/30 18:15:39  warmerda
+ * New
+ *
+ *
+ **********************************************************************/
+
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+ 
+#include "cpl_port.h"
+ 
+int strcasecmp(char * str1, char * str2)
+{
+    int i;
+    char * temp1, *temp2;
+ 	
+    for(i=0;str1[i]!='\0';i++)
+        temp1[i]=tolower(str1[i]);
+    temp1[i]='\0';
+	
+    for(i=0;str2[i]!='\0';i++)
+        temp2[i]=tolower(str2[i]);
+    temp2[i]='\0';
+	
+    return (strcmp( temp1, temp2) );
+
+ 
+}
+
+int strncasecmp(char * str1, char * str2, int len)
+{
+    int i;
+    char * temp1, *temp2;
+ 	
+    for(i=0;str1[i]!='\0';i++)
+        temp1[i]=tolower(str1[i]);
+    temp1[i]='\0';
+	
+    for(i=0;str2[i]!='\0';i++)
+        temp2[i]=tolower(str2[i]);
+    temp2[i]='\0';
+	
+    return (strncmp( temp1, temp2, len) );
+ 
+}
+ 
+char * strdup (char *instr)
+{
+    char * temp = calloc(strlen(instr)+1, 1);
+ 	 
+    if (temp)
+    {
+        strcpy(temp, instr);
+    }
+ 	 
+    return temp;
+}
