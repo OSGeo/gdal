e6410bf2ea700bf346b68c65ffd001b380cda635
Frank Warmerdam <warmerdam@pobox.com>
2001-12-08 04:44:23 +0000

    added BSB
    git-svn-id: https://svn.osgeo.org/gdal/trunk@2734 f0d54148-0727-0410-94bb-9a71ac55c965

--- gcore/gdal_frmts.h
+++ gcore/gdal_frmts.h
@@ -28,6 +28,9 @@
  ******************************************************************************
  *
  * $Log$
+ * Revision 1.10  2001/12/08 04:44:23  warmerda
+ * added BSB
+ *
  * Revision 1.9  2001/11/27 14:39:02  warmerda
  * added usgsdem
  *
@@ -97,6 +100,8 @@ void CPL_DLL GDALRegister_FujiBAS(void);
 void CPL_DLL GDALRegister_FIT(void);
 void CPL_DLL GDALRegister_VRT(void);
 void CPL_DLL GDALRegister_USGSDEM(void);
+void CPL_DLL GDALRegister_LDF(void);
+void CPL_DLL GDALRegister_BSB(void);
 CPL_C_END
 
 #endif /* ndef GDAL_FRMTS_H_INCLUDED */
