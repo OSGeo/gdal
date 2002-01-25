62573fa7ff29f43c345f5745f2e68567433b400d
Frank Warmerdam <warmerdam@pobox.com>
2002-01-25 20:37:02 +0000

    New
    git-svn-id: https://svn.osgeo.org/gdal/trunk@2875 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ ogr/ogrsf_frmts/gml/ogrgmldatasource.cpp
@@ -0,0 +1,311 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  OGR
+ * Purpose:  Implements OGRGMLDataSource class.
+ * Author:   Frank Warmerdam, warmerdam@pobox.com
+ *
+ ******************************************************************************
+ * Copyright (c) 2002, Frank Warmerdam <warmerdam@pobox.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ******************************************************************************
+ *
+ * $Log$
+ * Revision 1.1  2002/01/25 20:37:02  warmerda
+ * New
+ *
+ */
+
+#include "ogr_gml.h"
+#include "cpl_conv.h"
+#include "cpl_string.h"
+
+CPL_CVSID("$Id$");
+
+/************************************************************************/
+/*                         OGRGMLDataSource()                         */
+/************************************************************************/
+
+OGRGMLDataSource::OGRGMLDataSource()
+
+{
+    pszName = NULL;
+    papoLayers = NULL;
+    nLayers = 0;
+
+    poReader = NULL;
+    fpOutput = NULL;
+}
+
+/************************************************************************/
+/*                        ~OGRGMLDataSource()                         */
+/************************************************************************/
+
+OGRGMLDataSource::~OGRGMLDataSource()
+
+{
+
+    if( fpOutput != NULL )
+    {
+        VSIFPrintf( fpOutput, "%s", 
+                    "</gml:featureCollection>\n" );
+
+        VSIFClose( fpOutput );
+    }
+
+    CPLFree( pszName );
+
+    for( int i = 0; i < nLayers; i++ )
+        delete papoLayers[i];
+    
+    CPLFree( papoLayers );
+}
+
+/************************************************************************/
+/*                                Open()                                */
+/************************************************************************/
+
+int OGRGMLDataSource::Open( const char * pszNewName, int bTestOpen )
+
+{
+    FILE	*fp;
+    char	szHeader[1000];
+
+/* -------------------------------------------------------------------- */
+/*      Open the source file.                                           */
+/* -------------------------------------------------------------------- */
+    fp = VSIFOpen( pszNewName, "r" );
+    if( fp == NULL )
+    {
+        if( !bTestOpen )
+            CPLError( CE_Failure, CPLE_OpenFailed, 
+                      "Failed to open GML file `%s'.", 
+                      pszNewName );
+
+        return FALSE;
+    }
+
+/* -------------------------------------------------------------------- */
+/*      If we aren't sure it is GML, load a header chunk and check      */
+/*      for signs it is GML                                             */
+/* -------------------------------------------------------------------- */
+    if( bTestOpen )
+    {
+        VSIFRead( szHeader, 1, sizeof(szHeader), fp );
+        szHeader[sizeof(szHeader)-1] = '\0';
+
+        if( szHeader[0] != '<' 
+            && strstr(szHeader,"opengis.net/gml") == NULL )
+        {
+            return FALSE;
+            VSIFClose( fp );
+        }
+    }
+    
+/* -------------------------------------------------------------------- */
+/*      We assume now that it is GML.  Close and instantiate a          */
+/*      GMLReader on it.                                                */
+/* -------------------------------------------------------------------- */
+    VSIFClose( fp );
+    
+    pszName = CPLStrdup( pszNewName );
+
+    poReader = CreateGMLReader();
+    poReader->SetSourceFile( pszNewName );
+    
+/* -------------------------------------------------------------------- */
+/*      Force a first pass to establish the schema.  Eventually we      */
+/*      will have mechanisms for remembering the schema and related     */
+/*      information.                                                    */
+/* -------------------------------------------------------------------- */
+    if( !poReader->PrescanForSchema() )
+    {
+        // we assume an errors have been reported.
+        return FALSE;
+    }
+
+/* -------------------------------------------------------------------- */
+/*      Translate the GMLFeatureClasses into layers.                    */
+/* -------------------------------------------------------------------- */
+    papoLayers = (OGRGMLLayer **)
+        CPLCalloc( sizeof(OGRGMLLayer *), poReader->GetClassCount());
+    nLayers = 0;
+
+    while( nLayers < poReader->GetClassCount() )
+    {
+        papoLayers[nLayers] = TranslateGMLSchema(poReader->GetClass(nLayers));
+        nLayers++;
+    }
+    
+
+    
+    return TRUE;
+}
+
+/************************************************************************/
+/*                         TranslateGMLSchema()                         */
+/************************************************************************/
+
+OGRGMLLayer *OGRGMLDataSource::TranslateGMLSchema( GMLFeatureClass *poClass )
+
+{
+    OGRGMLLayer *poLayer;
+
+/* -------------------------------------------------------------------- */
+/*      Create an empty layer.                                          */
+/* -------------------------------------------------------------------- */
+    poLayer = new OGRGMLLayer( poClass->GetName(), NULL, FALSE, 
+                               wkbUnknown, this );
+
+/* -------------------------------------------------------------------- */
+/*      Added attributes (properties).                                  */
+/* -------------------------------------------------------------------- */
+    for( int iField = 0; iField < poClass->GetPropertyCount(); iField++ )
+    {
+        GMLPropertyDefn *poProperty = poClass->GetProperty( iField );
+        OGRFieldType eFType;
+
+        if( poProperty->GetType() == GMLPT_Untyped )
+            eFType = OFTString;
+        else if( poProperty->GetType() == GMLPT_String )
+            eFType = OFTString;
+        else if( poProperty->GetType() == GMLPT_Integer )
+            eFType = OFTInteger;
+        else if( poProperty->GetType() == GMLPT_Real )
+            eFType = OFTReal;
+        else
+            eFType = OFTString;
+        
+        OGRFieldDefn oField( poProperty->GetName(), eFType );
+        poLayer->GetLayerDefn()->AddFieldDefn( &oField );
+    }
+
+    return poLayer;
+}
+
+/************************************************************************/
+/*                               Create()                               */
+/************************************************************************/
+
+int OGRGMLDataSource::Create( const char *pszFilename, 
+                              char ** /* papszOptions */ )
+
+{
+    if( fpOutput != NULL || poReader != NULL )
+    {
+        CPLAssert( FALSE );
+        return FALSE;
+    }
+
+/* -------------------------------------------------------------------- */
+/*      Create the output file.                                         */
+/* -------------------------------------------------------------------- */
+    pszName = CPLStrdup( pszFilename );
+
+    fpOutput = VSIFOpen( pszFilename, "wt" );
+    if( fpOutput == NULL )
+    {
+        CPLError( CE_Failure, CPLE_OpenFailed, 
+                  "Failed to create GML file %s.", 
+                  pszFilename );
+        return FALSE;
+    }
+
+/* -------------------------------------------------------------------- */
+/*      Write out "standard" header.                                    */
+/* -------------------------------------------------------------------- */
+    VSIFPrintf( fpOutput, "%s", 
+                "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n" );
+
+    VSIFPrintf( fpOutput, "%s", 
+                "<gml:featureCollection\n"
+                "     xmlns:gml=\"http://www.opengis.net/gml\">\n" );
+
+
+    return TRUE;
+}
+
+/************************************************************************/
+/*                            CreateLayer()                             */
+/************************************************************************/
+
+OGRLayer *
+OGRGMLDataSource::CreateLayer( const char * pszLayerName,
+                               OGRSpatialReference *poSRS,
+                               OGRwkbGeometryType eType,
+                               char ** papszOptions )
+
+{
+/* -------------------------------------------------------------------- */
+/*      Verify we are in update mode.                                   */
+/* -------------------------------------------------------------------- */
+    if( fpOutput == NULL )
+    {
+        CPLError( CE_Failure, CPLE_NoWriteAccess,
+                  "Data source %s opened for read access.\n"
+                  "New layer %s cannot be created.\n",
+                  pszName, pszLayerName );
+
+        return NULL;
+    }
+
+/* -------------------------------------------------------------------- */
+/*      Create the layer object.                                        */
+/* -------------------------------------------------------------------- */
+    OGRGMLLayer	*poLayer;
+
+    poLayer = new OGRGMLLayer( pszLayerName, poSRS, TRUE, eType, this );
+
+/* -------------------------------------------------------------------- */
+/*      Add layer to data source layer list.                            */
+/* -------------------------------------------------------------------- */
+    papoLayers = (OGRGMLLayer **)
+        CPLRealloc( papoLayers,  sizeof(OGRGMLLayer *) * (nLayers+1) );
+    
+    papoLayers[nLayers++] = poLayer;
+
+    return poLayer;
+}
+
+/************************************************************************/
+/*                           TestCapability()                           */
+/************************************************************************/
+
+int OGRGMLDataSource::TestCapability( const char * pszCap )
+
+{
+    if( EQUAL(pszCap,ODsCCreateLayer) )
+        return TRUE;
+    else
+        return FALSE;
+}
+
+/************************************************************************/
+/*                              GetLayer()                              */
+/************************************************************************/
+
+OGRLayer *OGRGMLDataSource::GetLayer( int iLayer )
+
+{
+    if( iLayer < 0 || iLayer >= nLayers )
+        return NULL;
+    else
+        return papoLayers[iLayer];
+}
