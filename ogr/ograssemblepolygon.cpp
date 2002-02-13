d118b72cd8c407f10279f732e70cf635f8c45452
Frank Warmerdam <warmerdam@pobox.com>
2002-02-13 19:58:29 +0000

    New
    git-svn-id: https://svn.osgeo.org/gdal/trunk@2940 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ ogr/ograssemblepolygon.cpp
@@ -0,0 +1,255 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  S-57 Reader
+ * Purpose:  Implements polygon assembly from a bunch of arcs.
+ * Author:   Frank Warmerdam, warmerdam@pobox.com
+ *
+ ******************************************************************************
+ * Copyright (c) 1999, Frank Warmerdam
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ******************************************************************************
+ *
+ * $Log$
+ * Revision 1.1  2002/02/13 19:58:29  warmerda
+ * New
+ *
+ * Revision 1.7  2001/12/19 22:07:17  warmerda
+ * avoid warnings by initializing variables
+ *
+ * Revision 1.6  2001/07/18 04:55:16  warmerda
+ * added CPL_CSVID
+ *
+ * Revision 1.5  2000/06/16 18:01:26  warmerda
+ * added debug output in case of assembly failure
+ *
+ * Revision 1.4  1999/11/18 19:01:25  warmerda
+ * expanded tabs
+ *
+ * Revision 1.3  1999/11/18 18:57:28  warmerda
+ * Added error reporting
+ *
+ * Revision 1.2  1999/11/10 14:17:26  warmerda
+ * Fixed defaulting of peErr parameter.
+ *
+ * Revision 1.1  1999/11/04 21:18:46  warmerda
+ * New
+ *
+ */
+
+#include "ogr_geometry.h"
+#include "cpl_conv.h"
+
+CPL_CVSID("$Id$");
+
+/************************************************************************/
+/*                           AddEdgeToRing()                            */
+/************************************************************************/
+
+static void AddEdgeToRing( OGRLinearRing * poRing, OGRLineString * poLine,
+                           int bReverse, int bDropVertex )
+
+{
+/* -------------------------------------------------------------------- */
+/*      Establish order and range of traverse.                          */
+/* -------------------------------------------------------------------- */
+    int         iStart=0, iEnd=0, iStep=0;
+    int         nVertToAdd = poLine->getNumPoints();
+
+    if( bDropVertex && bReverse )
+    {
+        iStart = nVertToAdd - 2;
+        iEnd = 0;
+        iStep = -1;
+    }
+    else if( bDropVertex && !bReverse )
+    {
+        iStart = 1;
+        iEnd = nVertToAdd - 1;
+        iStep = 1;
+    }
+    else if( !bDropVertex && !bReverse )
+    {
+        iStart = 0;
+        iEnd = nVertToAdd - 1;
+        iStep = 1;
+    }
+    else if( !bDropVertex && bReverse )
+    {
+        iStart = nVertToAdd - 1;
+        iEnd = 0;
+        iStep = -1;
+    }
+
+/* -------------------------------------------------------------------- */
+/*      Append points to ring.                                          */
+/* -------------------------------------------------------------------- */
+    int         iOutVertex = poRing->getNumPoints();
+
+    poRing->setNumPoints( iOutVertex + ABS(iEnd-iStart) + 1 );
+    
+    for( int i = iStart; i != (iEnd+iStep); i += iStep )
+    {
+        poRing->setPoint( iOutVertex++,
+                          poLine->getX(i), poLine->getY(i), poLine->getZ(i) );
+    }
+}
+
+/************************************************************************/
+/*                             PointEqual()                             */
+/*                                                                      */
+/*      Compare points on two linear strings.                           */
+/************************************************************************/
+
+static int PointsEqual( OGRLineString *poLine1, int iPoint1,
+                        OGRLineString *poLine2, int iPoint2 )
+
+{
+    if( poLine1->getX(iPoint1) == poLine2->getX(iPoint2)
+        && poLine1->getY(iPoint1) == poLine2->getY(iPoint2) )
+        return TRUE;
+    else
+        return FALSE;
+}
+
+/************************************************************************/
+/*                      OGRBuildPolygonFromEdges()                      */
+/************************************************************************/
+
+OGRPolygon *OGRBuildPolygonFromEdges( OGRGeometryCollection * poLines,
+                                      int bBestEffort, OGRErr * peErr )
+
+{
+    int         bSuccess = TRUE;
+    OGRPolygon  *poPolygon = new OGRPolygon();
+
+/* -------------------------------------------------------------------- */
+/*      Setup array of line markers indicating if they have been        */
+/*      added to a ring yet.                                            */
+/* -------------------------------------------------------------------- */
+    int         nEdges = poLines->getNumGeometries();
+    int         *panEdgeConsumed, nRemainingEdges = nEdges;
+
+    panEdgeConsumed = (int *) CPLCalloc(sizeof(int),nEdges);
+
+/* ==================================================================== */
+/*      Loop generating rings.                                          */
+/* ==================================================================== */
+    while( nRemainingEdges > 0 )
+    {
+        int             iEdge;
+        OGRLineString   *poLine;
+        
+/* -------------------------------------------------------------------- */
+/*      Find the first unconsumed edge.                                 */
+/* -------------------------------------------------------------------- */
+        for( iEdge = 0; panEdgeConsumed[iEdge]; iEdge++ ) {}
+
+        poLine = (OGRLineString *) poLines->getGeometryRef(iEdge);
+
+/* -------------------------------------------------------------------- */
+/*      Start a new ring, copying in the current line directly          */
+/* -------------------------------------------------------------------- */
+        OGRLinearRing   *poRing = new OGRLinearRing();
+        
+        AddEdgeToRing( poRing, poLine,
+                       FALSE, FALSE );
+
+        panEdgeConsumed[iEdge] = TRUE;
+        nRemainingEdges--;
+        
+/* ==================================================================== */
+/*      Loop adding edges to this ring until we make a whole pass       */
+/*      within finding anything to add.                                 */
+/* ==================================================================== */
+        int             bWorkDone = TRUE;
+
+        while( !PointsEqual(poRing,0,poRing,poRing->getNumPoints()-1)
+               && nRemainingEdges > 0
+               && bWorkDone )
+        {
+            bWorkDone = FALSE;
+            
+            for( iEdge = 0; iEdge < nEdges; iEdge++ )
+            {
+                if( panEdgeConsumed[iEdge] )
+                    continue;
+
+                poLine = (OGRLineString *) poLines->getGeometryRef(iEdge);
+                
+                if( PointsEqual(poLine,0,poRing,poRing->getNumPoints()-1) )
+                {
+                    AddEdgeToRing( poRing, poLine, FALSE, TRUE );
+                }
+                else if( PointsEqual(poLine,poLine->getNumPoints()-1,
+                                     poRing,poRing->getNumPoints()-1) )
+                {
+                    AddEdgeToRing( poRing, poLine, TRUE, TRUE );
+                }
+                else
+                {
+                    continue;
+                }
+                    
+                panEdgeConsumed[iEdge] = TRUE;
+                nRemainingEdges--;
+                bWorkDone = TRUE;
+            }
+        }
+
+/* -------------------------------------------------------------------- */
+/*      Did we fail to complete the ring?                               */
+/* -------------------------------------------------------------------- */
+        if( !PointsEqual(poRing,0,poRing,poRing->getNumPoints()-1) )
+        {
+            CPLDebug( "S57", 
+                     "Failed to close ring %d.\n"
+                     "End Points are: (%.8f,%.7f) and (%.7f,%.7f)\n",
+                     poPolygon->getNumInteriorRings()+1,
+                     poRing->getX(0), poRing->getY(0), 
+                     poRing->getX(poRing->getNumPoints()-1), 
+                     poRing->getY(poRing->getNumPoints()-1) );
+
+            bSuccess = FALSE;
+        }
+
+        poPolygon->addRingDirectly( poRing );
+    } /* next ring */
+
+/* -------------------------------------------------------------------- */
+/*      Cleanup.                                                        */
+/* -------------------------------------------------------------------- */
+    CPLFree( panEdgeConsumed );
+
+// Eventually we should at least identify the external ring properly,
+// perhaps even ordering the direction of rings, though this isn't
+// required by the OGC geometry model.
+
+    if( peErr != NULL )
+    {
+        if( bSuccess )
+            *peErr = OGRERR_NONE;
+        else
+            *peErr = OGRERR_FAILURE;
+    }
+    
+    return poPolygon;
+}
+
