2d03a5009fe531d7b66b9307da56604a36c7e36f
Frank Warmerdam <warmerdam@pobox.com>
2002-11-11 20:35:42 +0000

    added support for converting to vax floating point
    git-svn-id: https://svn.osgeo.org/gdal/trunk@3733 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/ogrsf_frmts/dgn/dgnfloat.cpp
+++ ogr/ogrsf_frmts/dgn/dgnfloat.cpp
@@ -28,6 +28,9 @@
  ******************************************************************************
  *
  * $Log$
+ * Revision 1.4  2002/11/11 20:35:42  warmerda
+ * added support for converting to vax floating point
+ *
  * Revision 1.3  2001/07/18 04:55:16  warmerda
  * added CPL_CSVID
  *
@@ -139,5 +142,125 @@ void	DGN2IEEEDouble(void * dbl)
     dest[7] = src[3];
 #else
     memcpy( dbl, &dt, 8 );
+#endif
+}
+
+/************************************************************************/
+/*                           IEEE2DGNDouble()                           */
+/************************************************************************/
+
+void	IEEE2DGNDouble(void * dbl)
+
+{
+    double64_t 	dt;
+    GInt32	exponent;
+    GInt32	sign;
+    GByte	*src,*dest;
+	
+#ifdef CPL_LSB
+    src  = (GByte *) dbl;
+    dest = (GByte *) &dt;
+
+    dest[0] = src[4];
+    dest[1] = src[5];
+    dest[2] = src[6];
+    dest[3] = src[7];
+    dest[4] = src[0];
+    dest[5] = src[1];
+    dest[6] = src[2];
+    dest[7] = src[3];
+#else
+    memcpy( &dt, dbl, 8 );
+#endif
+
+    sign 	 = dt.hi & 0x80000000;
+    exponent = dt.hi >> 20;
+    exponent = exponent & 0x000007ff;
+
+/* -------------------------------------------------------------------- */
+/*	An exponent of zero means a zero value.				*/
+/* -------------------------------------------------------------------- */
+    if (exponent)
+        exponent = exponent -1023+129;
+
+/* -------------------------------------------------------------------- */
+/*	In the case of overflow, return the largest number we can	*/
+/* -------------------------------------------------------------------- */
+    if (exponent > 255)
+    {
+        dest = (GByte *) dbl;
+
+        if (sign) 
+            dest[1] = 0xff;
+        else
+            dest[1] = 0x7f;
+
+        dest[0] = 0xff;
+        dest[2] = 0xff;
+        dest[3] = 0xff;
+        dest[4] = 0xff;
+        dest[5] = 0xff;
+        dest[6] = 0xff;
+        dest[7] = 0xff;
+
+        return;
+    }	
+
+/* -------------------------------------------------------------------- */
+/*	In the case of of underflow return zero				*/
+/* -------------------------------------------------------------------- */
+    else if ((exponent < 0 ) ||
+             (exponent == 0 && sign == 0))
+    {
+        dest = (GByte *) dbl;
+
+        dest[0] = 0x00;
+        dest[1] = 0x00;
+        dest[2] = 0x00;
+        dest[3] = 0x00;
+        dest[4] = 0x00;
+        dest[5] = 0x00;
+        dest[6] = 0x00;
+        dest[7] = 0x00;
+
+        return;
+    }
+    else
+    {
+/* -------------------------------------------------------------------- */
+/*	    Shift the fraction 3 bits left and set the exponent and sign*/
+/* -------------------------------------------------------------------- */
+        dt.hi = dt.hi << 3;
+        dt.hi = dt.hi | (dt.lo >> 29);
+        dt.hi = dt.hi & 0x007fffff;
+        dt.hi = dt.hi | (exponent << 23) | sign;
+	    
+        dt.lo =	dt.lo << 3;
+    }
+
+/* -------------------------------------------------------------------- */
+/*	Convert the double back to VAX format				*/
+/* -------------------------------------------------------------------- */
+    src = (GByte *) &dt;
+    dest = (GByte *) dbl;
+
+#ifdef CPL_LSB
+    dest[2] = src[0];
+    dest[3] = src[1];
+    dest[0] = src[2];
+    dest[1] = src[3];
+    dest[6] = src[4];
+    dest[7] = src[5];
+    dest[4] = src[6];
+    dest[5] = src[7];
+#else
+    dest[1] = src[0];
+    dest[0] = src[1];
+    dest[3] = src[2];
+    dest[2] = src[3];
+    dest[5] = src[4];
+    dest[4] = src[5];
+    dest[7] = src[6];
+    dest[6] = src[7];
 #endif
 }
