49792cde05765f34e2b1d163c10367f7ff62f713
Frank Warmerdam <warmerdam@pobox.com>
2002-12-05 21:44:19 +0000

    initial implementation
    git-svn-id: https://svn.osgeo.org/gdal/trunk@3892 f0d54148-0727-0410-94bb-9a71ac55c965

--- alg/gdalsimplewarp.cpp
+++ alg/gdalsimplewarp.cpp
@@ -29,6 +29,9 @@
  ******************************************************************************
  *
  * $Log$
+ * Revision 1.2  2002/12/05 21:44:19  warmerda
+ * initial implementation
+ *
  * Revision 1.1  2002/12/05 05:43:23  warmerda
  * New
  *
@@ -38,4 +41,186 @@
 #include "gdal_alg.h"
 
 CPL_CVSID("$Id$");
+
+/************************************************************************/
+/*                        GDALSimpleImageWarp()                         */
+/************************************************************************/
+
+int GDALSimpleImageWarp( GDALDatasetH hSrcDS, GDALDatasetH hDstDS, 
+                         int nBandCount, int *panBandList,
+                         GByte *pabyRedLUT, 
+                         GByte *pabyGreenLUT,
+                         GByte *pabyBlueLUT,
+                         GDALTransformerFunc pfnTransform, void *pTransformArg,
+                         GDALProgressFunc pfnProgress, void *pProgressArg, 
+                         const char **papszWarpOptions )
+
+{
+    int		iBand, bCancelled = FALSE;
+
+/* -------------------------------------------------------------------- */
+/*      If no bands provided assume we should process all bands.        */
+/* -------------------------------------------------------------------- */
+    if( nBandCount == 0 )
+    {
+        int nResult;
+
+        nBandCount = GDALGetRasterCount( hSrcDS ); 
+        panBandList = (int *) CPLCalloc(sizeof(int),nBandCount);
+
+        for( iBand = 0; iBand < nBandCount; iBand++ )
+            panBandList[iBand] = iBand+1;
+
+        nResult = GDALSimpleImageWarp( hSrcDS, hDstDS, nBandCount, panBandList,
+                                       pabyRedLUT, pabyGreenLUT, pabyBlueLUT,
+                                       pfnTransform, pTransformArg, 
+                                       pfnProgress, pProgressArg, 
+                                       papszWarpOptions );
+        CPLFree( panBandList );
+        return nResult;
+    }
+
+/* -------------------------------------------------------------------- */
+/*      Post initial progress.                                          */
+/* -------------------------------------------------------------------- */
+    if( pfnProgress )
+    {
+        if( !pfnProgress( 0.0, "Loading source image", pProgressArg ) )
+            return FALSE;
+    }
+
+/* -------------------------------------------------------------------- */
+/*      Load the source image band(s).                                  */
+/* -------------------------------------------------------------------- */
+    int   nSrcXSize = GDALGetRasterXSize(hSrcDS);
+    int   nSrcYSize = GDALGetRasterYSize(hSrcDS);
+    GByte **papabySrcData;
+
+    papabySrcData = (GByte **) CPLCalloc(nBandCount,sizeof(int));
+    for( iBand = 0; iBand < nBandCount; iBand++ )
+    {
+        papabySrcData[iBand] = (GByte *) VSIMalloc(nSrcXSize*nSrcYSize);
+        
+        GDALRasterIO( GDALGetRasterBand(hSrcDS,panBandList[iBand]), GF_Read,
+                      0, 0, nSrcXSize, nSrcYSize, 
+                      papabySrcData[iBand], nSrcXSize, nSrcYSize, GDT_Byte, 
+                      0, 0 );
+    }
+
+/* -------------------------------------------------------------------- */
+/*      Allocate scanline buffers for output image.                     */
+/* -------------------------------------------------------------------- */
+    int nDstXSize = GDALGetRasterXSize( hDstDS );
+    int nDstYSize = GDALGetRasterYSize( hDstDS );
+    GByte **papabyDstLine;
+
+    papabyDstLine = (GByte **) CPLCalloc(nBandCount,sizeof(int));
+    
+    for( iBand = 0; iBand < nBandCount; iBand++ )
+        papabyDstLine[iBand] = (GByte *) CPLMalloc( nDstXSize );
+
+/* -------------------------------------------------------------------- */
+/*      Allocate x,y,z coordinate arrays for transformation ... one     */
+/*      scanlines worth of positions.                                   */
+/* -------------------------------------------------------------------- */
+    double *padfX, *padfY, *padfZ;
+    int    *pabSuccess;
+
+    padfX = (double *) CPLMalloc(sizeof(double) * nDstXSize);
+    padfY = (double *) CPLMalloc(sizeof(double) * nDstXSize);
+    padfZ = (double *) CPLMalloc(sizeof(double) * nDstXSize);
+    pabSuccess = (int *) CPLMalloc(sizeof(int) * nDstXSize);
+
+/* -------------------------------------------------------------------- */
+/*      Loop over all the scanlines in the output image.                */
+/* -------------------------------------------------------------------- */
+    int iDstY;
+
+    for( iDstY = 0; iDstY < nDstYSize; iDstY++ )
+    {
+        int iDstX;
+
+        // Clear output buffer to "transparent" value.  Shouldn't we
+        // really be reading from the destination file to support overlay?
+        for( iBand = 0; iBand < nBandCount; iBand++ )
+            memset( papabyDstLine[iBand], 0, nDstXSize );
+
+        // Set point to transform.
+        for( iDstX = 0; iDstX < nDstXSize; iDstX++ )
+        {
+            padfX[iDstX] = iDstX + 0.5;
+            padfY[iDstX] = iDstY + 0.5;
+            padfZ[iDstX] = 0.0;
+        }
+
+        // Transform the points from destination pixel/line coordinates
+        // to source pixel/line coordinates.
+        pfnTransform( pTransformArg, TRUE, nDstXSize, 
+                      padfX, padfY, padfZ, pabSuccess );
+
+        // Loop over the output scanline.
+        for( iDstX = 0; iDstX < nDstXSize; iDstX++ )
+        {
+            if( !pabSuccess[iDstX] )
+                continue;
+
+            // We test against the value before casting to avoid the
+            // problem of asymmetric truncation effects around zero.  That is
+            // -0.5 will be 0 when cast to an int. 
+            if( padfX[iDstX] < 0.0 || padfY[iDstX] < 0.0 )
+                continue;
+
+            int iSrcX, iSrcY, iSrcOffset;
+
+            iSrcX = (int) padfX[iDstX];
+            iSrcY = (int) padfY[iDstX];
+
+            if( iSrcX >= nSrcXSize || iSrcY >= nSrcYSize )
+                continue;
+
+            iSrcOffset = iSrcX + iSrcY * nSrcXSize;
+            
+            for( iBand = 0; iBand < nBandCount; iBand++ )
+                papabyDstLine[iBand][iDstX] = papabySrcData[iBand][iSrcOffset];
+        }
+
+        // Write scanline to disk. 
+        for( iBand = 0; iBand < nBandCount; iBand++ )
+        {
+            GDALRasterIO( GDALGetRasterBand(hDstDS,iBand+1), GF_Write,
+                          0, iDstY, nDstXSize, 1, 
+                          papabyDstLine[iBand], nDstXSize, 1, GDT_Byte, 0, 0 );
+        }
+
+        if( pfnProgress != NULL )
+        {
+            if( !pfnProgress( (iDstY+1) / (double) nDstYSize, 
+                              "", pProgressArg ) )
+            {
+                CPLError( CE_Failure, CPLE_UserInterrupt, "User terminated" );
+                bCancelled = TRUE;
+                break;
+            }
+        }
+    }
+
+/* -------------------------------------------------------------------- */
+/*      Cleanup working buffers.                                        */
+/* -------------------------------------------------------------------- */
+    for( iBand = 0; iBand < nBandCount; iBand++ )
+    {
+        CPLFree( papabyDstLine[iBand] );
+        CPLFree( papabySrcData[iBand] );
+    }
+
+    CPLFree( papabyDstLine );
+    CPLFree( papabySrcData );
+    CPLFree( padfX );
+    CPLFree( padfY );
+    CPLFree( padfZ );
+    CPLFree( pabSuccess );
+    
+    return !bCancelled;
+}
+
 
