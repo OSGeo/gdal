c511fd57a038bda295ba41e5ad31e0a2f0d76f97
Frank Warmerdam <warmerdam@pobox.com>
2002-12-06 17:58:00 +0000

    fix a few bugs
    git-svn-id: https://svn.osgeo.org/gdal/trunk@3897 f0d54148-0727-0410-94bb-9a71ac55c965

--- alg/gdal_crs.c
+++ alg/gdal_crs.c
@@ -51,6 +51,9 @@
  ***************************************************************************
  *
  * $Log$
+ * Revision 1.3  2002/12/06 17:58:00  warmerda
+ * fix a few bugs
+ *
  * Revision 1.2  2002/12/05 05:46:08  warmerda
  * fixed linkage problem
  *
@@ -98,7 +101,34 @@ typedef struct
 /*                      GDALCreateGCPTransformer()                      */
 /************************************************************************/
 
-void *GDALCreateGCPTransformer( int nGCPCount, GDAL_GCP *pasGCPList, 
+/**
+ * Create GCP based polynomial transformer.
+ *
+ * Computes least squares fit polynomials from a provided set of GCPs,
+ * and stores the coefficients for later transformation of points between
+ * pixel/line and georeferenced coordinates. 
+ *
+ * The return value should be used as a TransformArg in combination with
+ * the transformation function GDALGCPTransform which fits the 
+ * GDALTransformerFunc signature.  The returned transform argument should
+ * be deallocated with GDALDestroyGCPTransformer when no longer needed.
+ *
+ * This function may fail (returning NULL) if the provided set of GCPs
+ * are inadequate for the requested order, the determinate is zero or they
+ * are otherwise "ill conditioned".  
+ *
+ * Note that 2nd order requires at least 6 GCPs, and 3rd order requires at
+ * least 10 gcps.  If nReqOrder is 0 the highest order possible with the
+ * provided gcp count will be used.
+ *
+ * @param nGCPCount the number of GCPs in pasGCPList.
+ * @param pasGCPList an array of GCPs to be used as input.
+ * @param nReqOrder the requested polynomial order.  It should be 1, 2 or 3.
+ * 
+ * @return the transform argument or NULL if creation fails. 
+ */
+
+void *GDALCreateGCPTransformer( int nGCPCount, const GDAL_GCP *pasGCPList, 
                                 int nReqOrder, int bReversed )
 
 {
@@ -106,6 +136,16 @@ void *GDALCreateGCPTransformer( int nGCPCount, GDAL_GCP *pasGCPList,
     double *padfGeoX, *padfGeoY, *padfRasterX, *padfRasterY;
     int    *panStatus, iGCP;
     struct Control_Points sPoints;
+
+    if( nReqOrder == 0 )
+    {
+        if( nGCPCount >= 10 )
+            nReqOrder = 2;
+        else if( nGCPCount >= 6 )
+            nReqOrder = 2;
+        else
+            nReqOrder = 1;
+    }
     
     psInfo = (GCPTransformInfo *) CPLCalloc(sizeof(GCPTransformInfo),1);
     psInfo->bReversed = bReversed;
@@ -124,16 +164,17 @@ void *GDALCreateGCPTransformer( int nGCPCount, GDAL_GCP *pasGCPList,
     {
         panStatus[iGCP] = 1;
         padfGeoX[iGCP] = pasGCPList[iGCP].dfGCPX;
-        padfGeoY[iGCP] = pasGCPList[iGCP].dfGCPX;
+        padfGeoY[iGCP] = pasGCPList[iGCP].dfGCPY;
         padfRasterX[iGCP] = pasGCPList[iGCP].dfGCPPixel;
         padfRasterY[iGCP] = pasGCPList[iGCP].dfGCPLine;
     }
 
     sPoints.count = nGCPCount;
-    sPoints.e1 = padfGeoX;
-    sPoints.n1 = padfGeoY;
-    sPoints.e2 = padfRasterX;
-    sPoints.n2 = padfRasterY;
+    sPoints.e1 = padfRasterX;
+    sPoints.n1 = padfRasterY;
+    sPoints.e2 = padfGeoX;
+    sPoints.n2 = padfGeoY;
+    sPoints.status = panStatus;
     
 /* -------------------------------------------------------------------- */
 /*      Compute the forward and reverse polynomials.                    */
@@ -141,10 +182,10 @@ void *GDALCreateGCPTransformer( int nGCPCount, GDAL_GCP *pasGCPList,
     if( CRS_compute_georef_equations( &sPoints,
                                       psInfo->adfToGeoX, psInfo->adfToGeoY,
                                       psInfo->adfFromGeoX, psInfo->adfFromGeoY,
-                                      nReqOrder ) )
+                                      nReqOrder ) != 1 )
     {
         CPLError( CE_Failure, CPLE_AppDefined,
-                  "Failed to compute polynimail equations of desired order\n"
+                  "Failed to compute polynomial equations of desired order\n"
                   "for provided control points." );
         goto CleanupAfterError;
     }
@@ -166,6 +207,16 @@ void *GDALCreateGCPTransformer( int nGCPCount, GDAL_GCP *pasGCPList,
 /*                     GDALDestroyGCPTransformer()                      */
 /************************************************************************/
 
+/**
+ * Destroy GCP transformer.
+ *
+ * This function is used to destroy information about a GCP based
+ * polynomial transformation created with GDALCreateGCPTransformer(). 
+ *
+ * @param pTransformArg the transform arg previously returned by 
+ * GDALCreateGCPTransformer(). 
+ */
+
 void GDALDestroyGCPTransformer( void *pTransformArg )
 
 {
@@ -175,6 +226,27 @@ void GDALDestroyGCPTransformer( void *pTransformArg )
 /************************************************************************/
 /*                          GDALGCPTransform()                          */
 /************************************************************************/
+
+/**
+ * Transforms point based on GCP derived polynomial model.
+ *
+ * This function matches the GDALTransformerFunc signature, and can be
+ * used to transform one or more points from pixel/line coordinates to
+ * georeferenced coordinates (SrcToDst) or vice versa (DstToSrc). 
+ *
+ * @param pTransformArg return value from GDALCreateGCPTransformer(). 
+ * @param bDstToSrc TRUE if transformation is from the destination 
+ * (georeferenced) coordinates to pixel/line or FALSE when transforming
+ * from pixel/line to georeferenced coordinates.
+ * @param nPointCount the number of values in the x, y and z arrays.
+ * @param x array containing the X values to be transformed.
+ * @param y array containing the Y values to be transformed.
+ * @param z array containing the Z values to be transformed.
+ * @param panSuccess array in which a flag indicating success (TRUE) or
+ * failure (FALSE) of the transformation are placed.
+ *
+ * @return TRUE.
+ */
 
 int GDALGCPTransform( void *pTransformArg, int bDstToSrc, 
                       int nPointCount, 
