d4dcc1fb4bf3e5bd52ffe9c1e41590ed998f93a3
Frank Warmerdam <warmerdam@pobox.com>
2002-12-07 16:13:38 +0000

    New
    git-svn-id: https://svn.osgeo.org/gdal/trunk@3908 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ apps/gdalwarp.c
@@ -0,0 +1,315 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  Mapinfo Image Warper
+ * Purpose:  Commandline program for doing a variety of image warps, including
+ *           image reprojection.
+ * Author:   Frank Warmerdam <warmerdam@pobox.com>
+ *
+ ******************************************************************************
+ * Copyright (c) 2002, i3 - information integration and imaging 
+ *                          Fort Collin, CO
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ******************************************************************************
+ *
+ * $Log$
+ * Revision 1.1  2002/12/07 16:13:38  warmerda
+ * New
+ *
+ */
+
+#include "gdal_alg.h"
+#include "cpl_string.h"
+#include "ogr_srs_api.h"
+
+CPL_CVSID("$Id$");
+
+static GDALDatasetH 
+GDALWarpCreateOutput( GDALDatasetH hSrcDS, const char *pszFilename, 
+                      const char *pszFormat, const char *pszSourceSRS, 
+                      const char *pszTargetSRS );
+
+/************************************************************************/
+/*                               Usage()                                */
+/************************************************************************/
+
+static void Usage()
+
+{
+    printf( 
+        "Usage: gdalwarp [--version] [--formats]\n"
+        "    [-s_srs srs_def] [-t_srs srs_def]\n"
+        "    [-te xmin ymin xmax ymax] [-tr xres yres] [-ts width height]\n"
+        "    [-of format] [-co \"NAME=VALUE\"]* srcfile dstfile\n" );
+    exit( 1 );
+}
+
+/************************************************************************/
+/*                             SanitizeSRS                              */
+/************************************************************************/
+
+char *SanitizeSRS( const char *pszUserInput )
+
+{
+    OGRSpatialReferenceH hSRS;
+    char *pszResult = NULL;
+
+    hSRS = OSRNewSpatialReference( NULL );
+    if( OSRSetFromUserInput( hSRS, pszUserInput ) == OGRERR_NONE )
+        OSRExportToWkt( hSRS, &pszResult );
+
+    OSRDestroySpatialReference( hSRS );
+
+    return pszResult;
+}
+
+/************************************************************************/
+/*                                main()                                */
+/************************************************************************/
+
+int main( int argc, char ** argv )
+
+{
+    GDALDatasetH	hSrcDS, hDstDS;
+    const char         *pszFormat = "GTiff";
+    char               *pszTargetSRS = NULL;
+    char               *pszSourceSRS = NULL;
+    const char         *pszSrcFilename = NULL, *pszDstFilename = NULL;
+    int                 bCreateOutput = FALSE, i;
+    void               *hTransformArg;
+
+    GDALAllRegister();
+
+/* -------------------------------------------------------------------- */
+/*      Parse arguments.                                                */
+/* -------------------------------------------------------------------- */
+    for( i = 1; i < argc; i++ )
+    {
+        if( EQUAL(argv[i],"--version") )
+        {
+            printf( "%s\n", GDALVersionInfo( "--version" ) );
+            exit( 0 );
+        }
+        else if( EQUAL(argv[i],"--formats") )
+        {
+            int iDr;
+
+            printf( "Supported Formats:\n" );
+            for( iDr = 0; iDr < GDALGetDriverCount(); iDr++ )
+            {
+                GDALDriverH hDriver = GDALGetDriver(iDr);
+                
+                printf( "  %s: %s\n",
+                        GDALGetDriverShortName( hDriver ),
+                        GDALGetDriverLongName( hDriver ) );
+            }
+
+            exit( 0 );
+        }
+        else if( EQUAL(argv[i],"-of") && i < argc-1 )
+        {
+            pszFormat = argv[++i];
+            bCreateOutput = TRUE;
+        }
+        else if( EQUAL(argv[i],"-t_srs") && i < argc-1 )
+        {
+            pszTargetSRS = SanitizeSRS(argv[++i]);
+        }
+        else if( EQUAL(argv[i],"-s_srs") && i < argc-1 )
+        {
+            pszSourceSRS = SanitizeSRS(argv[++i]);
+        }
+        else if( argv[i][0] == '-' )
+            Usage();
+        else if( pszSrcFilename == NULL )
+            pszSrcFilename = argv[i];
+        else if( pszDstFilename == NULL )
+            pszDstFilename = argv[i];
+        else
+            Usage();
+    }
+
+    if( pszDstFilename == NULL )
+        Usage();
+
+/* -------------------------------------------------------------------- */
+/*      Open source dataset.                                            */
+/* -------------------------------------------------------------------- */
+    hSrcDS = GDALOpen( pszSrcFilename, GA_ReadOnly );
+    
+    if( hSrcDS == NULL )
+        exit( 2 );
+
+    if( pszSourceSRS == NULL )
+        pszSourceSRS = CPLStrdup(GDALGetProjectionRef( hSrcDS ));
+
+    if( pszTargetSRS == NULL )
+        pszTargetSRS = CPLStrdup(pszSourceSRS);
+
+/* -------------------------------------------------------------------- */
+/*      Does the output dataset already exist?                          */
+/* -------------------------------------------------------------------- */
+    CPLPushErrorHandler( CPLQuietErrorHandler );
+    hDstDS = GDALOpen( pszDstFilename, GA_Update );
+    CPLPopErrorHandler();
+
+    if( hDstDS != NULL && bCreateOutput )
+    {
+        fprintf( stderr, 
+                 "Output dataset %s exists,\n"
+                 "but some commandline options were provided indicating a new dataset\n"
+                 "should be created.  Please delete existing dataset and run again.",
+                 pszDstFilename );
+        exit( 1 );
+    }
+
+/* -------------------------------------------------------------------- */
+/*      If not, we need to create it.                                   */
+/* -------------------------------------------------------------------- */
+    if( hDstDS == NULL )
+        hDstDS = GDALWarpCreateOutput( hSrcDS, pszDstFilename, pszFormat, 
+                                       pszSourceSRS, pszTargetSRS );
+
+    if( hDstDS == NULL )
+        exit( 1 );
+
+/* -------------------------------------------------------------------- */
+/*      Create a transformation object from the source to               */
+/*      destination coordinate system.                                  */
+/* -------------------------------------------------------------------- */
+    hTransformArg = 
+        GDALCreateGenImgProjTransformer( hSrcDS, pszSourceSRS, 
+                                         hDstDS, pszTargetSRS, 
+                                         TRUE, 1000.0 );
+
+    if( hTransformArg == NULL )
+        exit( 1 );
+
+/* -------------------------------------------------------------------- */
+/*      Now actually invoke the warper to do the work.                  */
+/* -------------------------------------------------------------------- */
+    GDALSimpleImageWarp( hSrcDS, hDstDS, 0, NULL, 
+                         GDALGenImgProjTransform, hTransformArg,
+                         GDALTermProgress, NULL, NULL );
+
+/* -------------------------------------------------------------------- */
+/*      Cleanup.                                                        */
+/* -------------------------------------------------------------------- */
+    GDALClose( hDstDS );
+    GDALClose( hSrcDS );
+
+    GDALDumpOpenDatasets( stderr );
+
+    GDALDestroyDriverManager();
+    
+    exit( 0 );
+}
+
+/************************************************************************/
+/*                        GDALWarpCreateOutput()                        */
+/*                                                                      */
+/*      Create the output file based on various commandline options,    */
+/*      and the input file.                                             */
+/************************************************************************/
+
+static GDALDatasetH 
+GDALWarpCreateOutput( GDALDatasetH hSrcDS, const char *pszFilename, 
+                      const char *pszFormat, const char *pszSourceSRS, 
+                      const char *pszTargetSRS )
+
+{
+    GDALDriverH hDriver;
+    GDALDatasetH hDstDS;
+    void *hTransformArg;
+    double adfDstGeoTransform[6];
+    int nPixels=0, nLines=0;
+
+/* -------------------------------------------------------------------- */
+/*      Find the output driver.                                         */
+/* -------------------------------------------------------------------- */
+    hDriver = GDALGetDriverByName( pszFormat );
+    if( hDriver == NULL 
+        || GDALGetMetadataItem( hDriver, GDAL_DCAP_CREATE, NULL ) == NULL )
+    {
+        int	iDr;
+        
+        printf( "Output driver `%s' not recognised or does not support\n", 
+                pszFormat );
+        printf( "direct output file creation.  The following format drivers are configured\n"
+                "and support direct output:\n" );
+
+        for( iDr = 0; iDr < GDALGetDriverCount(); iDr++ )
+        {
+            GDALDriverH hDriver = GDALGetDriver(iDr);
+
+            if( GDALGetMetadataItem( hDriver, GDAL_DCAP_CREATE, NULL) != NULL )
+            {
+                printf( "  %s: %s\n",
+                        GDALGetDriverShortName( hDriver  ),
+                        GDALGetDriverLongName( hDriver ) );
+            }
+        }
+        printf( "\n" );
+        exit( 1 );
+    }
+
+/* -------------------------------------------------------------------- */
+/*      Create a transformation object from the source to               */
+/*      destination coordinate system.                                  */
+/* -------------------------------------------------------------------- */
+    hTransformArg = 
+        GDALCreateGenImgProjTransformer( hSrcDS, pszSourceSRS, 
+                                         NULL, pszTargetSRS, 
+                                         TRUE, 1000.0 );
+
+    if( hTransformArg == NULL )
+        return NULL;
+
+/* -------------------------------------------------------------------- */
+/*      Get approximate output definition.                              */
+/* -------------------------------------------------------------------- */
+    if( GDALSuggestedWarpOutput( hSrcDS, 
+                                 GDALGenImgProjTransform, hTransformArg, 
+                                 adfDstGeoTransform, &nPixels, &nLines )
+        != CE_None )
+        return NULL;
+
+    GDALDestroyGenImgProjTransformer( hTransformArg );
+
+/* -------------------------------------------------------------------- */
+/*      Create the output file.                                         */
+/* -------------------------------------------------------------------- */
+    hDstDS = GDALCreate( hDriver, pszFilename, nPixels, nLines, 
+                         GDALGetRasterCount(hSrcDS),
+                         GDALGetRasterDataType(GDALGetRasterBand(hSrcDS,1)),
+                         NULL );
+
+    if( hDstDS == NULL )
+        return NULL;
+
+/* -------------------------------------------------------------------- */
+/*      Write out the projection definition.                            */
+/* -------------------------------------------------------------------- */
+    GDALSetProjection( hDstDS, pszTargetSRS );
+    GDALSetGeoTransform( hDstDS, adfDstGeoTransform );
+
+    return hDstDS;
+}
+    
