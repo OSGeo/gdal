39a7f0ada49f5bfc3fb521913849c2c54e891a05
Andrey Kiselev <dron@ak4719.spb.edu>
2003-02-18 15:07:49 +0000

    IRS-1C/1D support added.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@4298 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/raw/fastdataset.cpp
+++ frmts/raw/fastdataset.cpp
@@ -2,7 +2,7 @@
  * $Id$
  *
  * Project:  EOSAT FAST Format reader
- * Purpose:  Reads Landsat FAST-L7A
+ * Purpose:  Reads Landsat FAST-L7A, IRS 1C/1D
  * Author:   Andrey Kiselev, dron@at1895.spb.edu
  *
  ******************************************************************************
@@ -28,6 +28,9 @@
  ******************************************************************************
  * 
  * $Log$
+ * Revision 1.4  2003/02/18 15:07:49  dron
+ * IRS-1C/1D support added.
+ *
  * Revision 1.3  2003/02/14 21:05:40  warmerda
  * Don't use path for rawdataset.h.
  *
@@ -68,7 +71,15 @@ CPL_C_END
 
 #define FAST_FILENAME_SIZE	29
 #define FAST_VALUE_SIZE		24
+#define FAST_DATE_SIZE		8
+#define FAST_SATNAME_SIZE	10
+#define FAST_SENSORNAME_SIZE	10
 #define ADM_HEADER_SIZE		4608
+
+typedef enum {	// Satellites:
+    LANDSAT,	// Landsat 7
+    IRS		// IRS 1C/1D
+} FASTSatellite;
 
 /************************************************************************/
 /* ==================================================================== */
@@ -85,7 +96,10 @@ class FASTDataset : public GDALDataset
 
     FILE	*fpHeader;
     FILE	*fpChannels[6];
+    const char	*pszFilename;
+    char	*pszDirname;
     GDALDataType eDataType;
+    FASTSatellite iSatellite;
 
     void	ComputeGeoref();
     
@@ -96,13 +110,13 @@ class FASTDataset : public GDALDataset
     static GDALDataset *Open( GDALOpenInfo * );
 
     CPLErr 	GetGeoTransform( double * padfTransform );
-    const char *GetProjectionRef();
-
+    const char	*GetProjectionRef();
+    FILE	*FOpenChannel( char *pszFilename, int iBand );
 };
 
 /************************************************************************/
 /* ==================================================================== */
-/*                            FASTRasterBand                             */
+/*                            FASTRasterBand                            */
 /* ==================================================================== */
 /************************************************************************/
 
@@ -118,7 +132,7 @@ class FASTRasterBand : public RawRasterBand
 
 
 /************************************************************************/
-/*                           FASTRasterBand()                            */
+/*                           FASTRasterBand()                           */
 /************************************************************************/
 
 FASTRasterBand::FASTRasterBand( FASTDataset *poDS, int nBand, FILE * fpRaw,
@@ -138,29 +152,33 @@ FASTRasterBand::FASTRasterBand( FASTDataset *poDS, int nBand, FILE * fpRaw,
 /************************************************************************/
 
 /************************************************************************/
-/*                           FASTDataset()                           */
+/*                           FASTDataset()                              */
 /************************************************************************/
 
 FASTDataset::FASTDataset()
 
 {
     fpHeader = NULL;
-    pszProjection = NULL;
+    pszDirname = NULL;
+    pszProjection = CPLStrdup( "" );
     nBands = 0;
 }
 
 /************************************************************************/
-/*                            ~FASTDataset()                         */
+/*                            ~FASTDataset()                            */
 /************************************************************************/
 
 FASTDataset::~FASTDataset()
 
 {
     int i;
+    if ( pszDirname )
+	CPLFree( pszDirname );
     if ( pszProjection )
 	CPLFree( pszProjection );
     for ( i = 0; i < nBands; i++ )
-	VSIFClose( fpChannels[i] );
+	if ( fpChannels[i] )
+	    VSIFClose( fpChannels[i] );
     if( fpHeader != NULL )
         VSIFClose( fpHeader );
 }
@@ -185,6 +203,101 @@ const char *FASTDataset::GetProjectionRef()
 {
         return pszProjection;
 }
+/************************************************************************/
+/*                             FOpenChannel()                           */
+/************************************************************************/
+
+FILE *FASTDataset::FOpenChannel( char *pszFilename, int iBand )
+{
+    const char	*pszChannelFilename;
+    char	*pszPrefix = CPLStrdup( CPLGetBasename( this->pszFilename ) );
+    char	*pszSuffix = CPLStrdup( CPLGetExtension( this->pszFilename ) );
+
+    switch ( iSatellite )
+    {
+	case LANDSAT:
+	if ( pszFilename[0] != ' ' )
+	{
+	    pszChannelFilename = CPLFormFilename( pszDirname, pszFilename, NULL );
+	    fpChannels[iBand] = VSIFOpen( pszChannelFilename, "rb" );
+	}
+	else
+	    fpChannels[iBand] = NULL;
+	break;
+	case IRS:
+	default:
+	pszChannelFilename = CPLFormFilename( pszDirname,
+	    CPLSPrintf( "%s.%d", pszPrefix, iBand + 1 ), pszSuffix );
+	fpChannels[iBand] = VSIFOpen( pszChannelFilename, "rb" );
+	if ( fpChannels[iBand] )
+	    break;
+	pszChannelFilename = CPLFormFilename( pszDirname,
+	    CPLSPrintf( "IMAGERY%d", iBand + 1 ), pszSuffix );
+	fpChannels[iBand] = VSIFOpen( pszChannelFilename, "rb" );
+	if ( fpChannels[iBand] )
+	    break;
+	pszChannelFilename = CPLFormFilename( pszDirname,
+	    CPLSPrintf( "imagery%d", iBand + 1 ), pszSuffix );
+	fpChannels[iBand] = VSIFOpen( pszChannelFilename, "rb" );
+	if ( fpChannels[iBand] )
+	    break;
+	pszChannelFilename = CPLFormFilename( pszDirname,
+	    CPLSPrintf( "IMAGERY%d.DAT", iBand + 1 ), NULL );
+	fpChannels[iBand] = VSIFOpen( pszChannelFilename, "rb" );
+	if ( fpChannels[iBand] )
+	    break;
+	pszChannelFilename = CPLFormFilename( pszDirname,
+	    CPLSPrintf( "imagery%d.dat", iBand + 1 ), NULL );
+	fpChannels[iBand] = VSIFOpen( pszChannelFilename, "rb" );
+	if ( fpChannels[iBand] )
+	    break;
+	pszChannelFilename = CPLFormFilename( pszDirname,
+	    CPLSPrintf( "IMAGERY%d.dat", iBand + 1 ), NULL );
+	fpChannels[iBand] = VSIFOpen( pszChannelFilename, "rb" );
+	if ( fpChannels[iBand] )
+	    break;
+	pszChannelFilename = CPLFormFilename( pszDirname,
+	    CPLSPrintf( "imagery%d.DAT", iBand + 1 ), NULL );
+	fpChannels[iBand] = VSIFOpen( pszChannelFilename, "rb" );
+	if ( fpChannels[iBand] )
+	    break;
+	pszChannelFilename = CPLFormFilename( pszDirname,
+	    CPLSPrintf( "BAND%d", iBand + 1 ), pszSuffix );
+	fpChannels[iBand] = VSIFOpen( pszChannelFilename, "rb" );
+	if ( fpChannels[iBand] )
+	    break;
+	pszChannelFilename = CPLFormFilename( pszDirname,
+	    CPLSPrintf( "band%d", iBand + 1 ), pszSuffix );
+	fpChannels[iBand] = VSIFOpen( pszChannelFilename, "rb" );
+	if ( fpChannels[iBand] )
+	    break;
+	pszChannelFilename = CPLFormFilename( pszDirname,
+	    CPLSPrintf( "BAND%d.DAT", iBand + 1 ), NULL );
+	fpChannels[iBand] = VSIFOpen( pszChannelFilename, "rb" );
+	if ( fpChannels[iBand] )
+	    break;
+	pszChannelFilename = CPLFormFilename( pszDirname,
+	    CPLSPrintf( "band%d.dat", iBand + 1 ), NULL );
+	fpChannels[iBand] = VSIFOpen( pszChannelFilename, "rb" );
+	if ( fpChannels[iBand] )
+	    break;
+	pszChannelFilename = CPLFormFilename( pszDirname,
+	    CPLSPrintf( "BAND%d.dat", iBand + 1 ), NULL );
+	fpChannels[iBand] = VSIFOpen( pszChannelFilename, "rb" );
+	if ( fpChannels[iBand] )
+	    break;
+	pszChannelFilename = CPLFormFilename( pszDirname,
+	    CPLSPrintf( "band%d.DAT", iBand + 1 ), NULL );
+	fpChannels[iBand] = VSIFOpen( pszChannelFilename, "rb" );
+	break;
+    }
+    
+    CPLDebug( "FAST", "Band %d filename: %s", iBand + 1, pszChannelFilename);
+
+    CPLFree( pszPrefix );
+    CPLFree( pszSuffix );
+    return fpChannels[iBand];
+}
 
 /************************************************************************/
 /*                                Open()                                */
@@ -195,16 +308,13 @@ GDALDataset *FASTDataset::Open( GDALOpenInfo * poOpenInfo )
 {
     int		i;
 	
-    if( poOpenInfo->fp == NULL ||
-            strlen(poOpenInfo->pszFilename) < FAST_FILENAME_SIZE )
+    if( poOpenInfo->fp == NULL )
         return NULL;
 
-    if ( !EQUALN(poOpenInfo->pszFilename + strlen(poOpenInfo->pszFilename) - 4,
-                ".FST", 4) )
-	return NULL;
-
-    if( !EQUALN((const char *) poOpenInfo->pabyHeader, "REQ ID =", 8) &&
-	!EQUALN((const char *) poOpenInfo->pabyHeader + 80, "SATELLITE =", 11) && 
+    if( !EQUALN((const char *) poOpenInfo->pabyHeader + 52,
+		"ACQUISITION DATE =", 18) &&
+	!EQUALN((const char *) poOpenInfo->pabyHeader + 80,
+		"SATELLITE =", 11) && 
 	!EQUALN((const char *) poOpenInfo->pabyHeader + 101, " SENSOR =", 9) && 
 	!EQUALN((const char *) poOpenInfo->pabyHeader + 183, " LOCATION =", 11) )
         return NULL;
@@ -213,116 +323,122 @@ GDALDataset *FASTDataset::Open( GDALOpenInfo * poOpenInfo )
 /*      Create a corresponding GDALDataset.                             */
 /* -------------------------------------------------------------------- */
     FASTDataset	*poDS;
-    char	*pszDirname;
 
     poDS = new FASTDataset();
 
     poDS->fpHeader = poOpenInfo->fp;
     poOpenInfo->fp = NULL;
-    pszDirname = CPLStrdup( CPLGetDirname( poOpenInfo->pszFilename ) );
+    poDS->pszFilename = poOpenInfo->pszFilename;
+    poDS->pszDirname = CPLStrdup( CPLGetDirname( poOpenInfo->pszFilename ) );
     
 /* -------------------------------------------------------------------- */
-/*    Read the administrative header and calibration coefficients.      */
+/*    Read the administrative header and calibration coefficients from  */
+/*    radiometric record.                                               */
 /* -------------------------------------------------------------------- */
-    char	*pszHeader;
-    const char	*pszChannelFilename;
+    char	pszHeader[ADM_HEADER_SIZE];
+    char	pszDate[FAST_DATE_SIZE + 1];
+    char	pszSatName[FAST_SATNAME_SIZE + 1];
+    char	pszSensorName[FAST_SENSORNAME_SIZE + 1];
     char	pszFilename[FAST_FILENAME_SIZE + 1];
     char	pszValue[FAST_VALUE_SIZE + 1];
-    
-    pszHeader = (char *)CPLMalloc( ADM_HEADER_SIZE );
+ 
     VSIFSeek( poDS->fpHeader, 0, SEEK_SET );
-    VSIFRead( pszHeader, 1, ADM_HEADER_SIZE, poDS->fpHeader );
+    if ( VSIFRead( pszHeader, 1, ADM_HEADER_SIZE, poDS->fpHeader ) <
+	 ADM_HEADER_SIZE )
+    {
+	CPLError( CE_Failure, CPLE_AppDefined,
+		  "Failed to read FAST header file: %s\n."
+		  "Expected %d bytes in header.\n",
+		  poOpenInfo->pszFilename, ADM_HEADER_SIZE );
+	delete poDS;
+	return NULL;
+    }
+
+    // Read acquisition date
+    pszDate[FAST_DATE_SIZE] = '\0';
+    memcpy( pszDate, pszHeader + 70, FAST_DATE_SIZE );
+    poDS->SetMetadataItem( "ACQUISITION_DATE", pszDate );
+
+    // Read satellite name
+    pszSatName[FAST_SATNAME_SIZE] = '\0';
+    memcpy( pszSatName, pszHeader + 91, FAST_SATNAME_SIZE );
+    poDS->SetMetadataItem( "SATELLITE", pszSatName );
+    if ( EQUALN(pszSatName, "LANDSAT", 7) )
+	poDS->iSatellite = LANDSAT;
+    else if ( EQUALN(pszSatName, "IRS", 3) )
+	poDS->iSatellite = IRS;
+    else
+	poDS->iSatellite = IRS;
+
+    // Read sensor name
+    pszSatName[FAST_SENSORNAME_SIZE] = '\0';
+    memcpy( pszSensorName, pszHeader + 110, FAST_SENSORNAME_SIZE );
+    poDS->SetMetadataItem( "SENSOR", pszSensorName );
 
     pszFilename[FAST_FILENAME_SIZE] = '\0';
-    pszFilename[FAST_VALUE_SIZE] = '\0';
+    pszValue[FAST_VALUE_SIZE] = '\0';
     poDS->nBands = 0;
-    if ( pszHeader[1130] != ' ' )
+    memcpy( pszFilename, pszHeader + 1130, FAST_FILENAME_SIZE );
+    if ( poDS->FOpenChannel( pszFilename, poDS->nBands ) )
     {
-	memcpy( pszFilename, pszHeader + 1130, FAST_FILENAME_SIZE );
-	pszChannelFilename = CPLFormFilename( pszDirname, pszFilename, NULL );
-        poDS->fpChannels[poDS->nBands] = VSIFOpen( pszChannelFilename, "rb" );
-	if ( poDS->fpChannels[poDS->nBands] )
-	{
-	    poDS->nBands++;
-	    memcpy( pszValue, pszHeader + 1616, FAST_VALUE_SIZE );
-	    poDS->SetMetadataItem( CPLSPrintf("BIAS%d",  poDS->nBands), pszValue );
-	    memcpy( pszValue, pszHeader + 1641, FAST_VALUE_SIZE );
-	    poDS->SetMetadataItem( CPLSPrintf("GAIN%d",  poDS->nBands), pszValue );
-	}
+	poDS->nBands++;
+	memcpy( pszValue, pszHeader + 1616, FAST_VALUE_SIZE );
+	poDS->SetMetadataItem( CPLSPrintf("BIAS%d",  poDS->nBands), pszValue );
+	memcpy( pszValue, pszHeader + 1641, FAST_VALUE_SIZE );
+	poDS->SetMetadataItem( CPLSPrintf("GAIN%d",  poDS->nBands), pszValue );
     }
-    if ( pszHeader[1169] != ' ' )
+    memcpy( pszFilename, pszHeader + 1169, FAST_FILENAME_SIZE );
+    if ( poDS->FOpenChannel( pszFilename, poDS->nBands ) )
     {
-	memcpy( pszFilename, pszHeader + 1169, FAST_FILENAME_SIZE );
-	pszChannelFilename = CPLFormFilename( pszDirname, pszFilename, NULL );
-	poDS->fpChannels[poDS->nBands] = VSIFOpen( pszChannelFilename, "rb" );
-	if ( poDS->fpChannels[poDS->nBands] )
-	{
-	    poDS->nBands++;
-	    memcpy( pszValue, pszHeader + 1696, FAST_VALUE_SIZE );
-	    poDS->SetMetadataItem( CPLSPrintf("BIAS%d",  poDS->nBands), pszValue );
-	    memcpy( pszValue, pszHeader + 1721, FAST_VALUE_SIZE );
-	    poDS->SetMetadataItem( CPLSPrintf("GAIN%d",  poDS->nBands), pszValue );
-	}
+	poDS->nBands++;
+	memcpy( pszValue, pszHeader + 1696, FAST_VALUE_SIZE );
+	poDS->SetMetadataItem( CPLSPrintf("BIAS%d",  poDS->nBands), pszValue );
+	memcpy( pszValue, pszHeader + 1721, FAST_VALUE_SIZE );
+	poDS->SetMetadataItem( CPLSPrintf("GAIN%d",  poDS->nBands), pszValue );
     }
-    if ( pszHeader[1210] != ' ' )
+    memcpy( pszFilename, pszHeader + 1210, FAST_FILENAME_SIZE );
+    if ( poDS->FOpenChannel( pszFilename, poDS->nBands ) )
     {
-	memcpy( pszFilename, pszHeader + 1210, FAST_FILENAME_SIZE );
-	pszChannelFilename = CPLFormFilename( pszDirname, pszFilename, NULL );
-        poDS->fpChannels[poDS->nBands] = VSIFOpen( pszChannelFilename, "rb" );
-	if ( poDS->fpChannels[poDS->nBands] )
-	{
-	    poDS->nBands++;
-	    memcpy( pszValue, pszHeader + 1776, FAST_VALUE_SIZE );
-	    poDS->SetMetadataItem( CPLSPrintf("BIAS%d",  poDS->nBands), pszValue );
-	    memcpy( pszValue, pszHeader + 1801, FAST_VALUE_SIZE );
-	    poDS->SetMetadataItem( CPLSPrintf("GAIN%d",  poDS->nBands), pszValue );
-	}
+	poDS->nBands++;
+	memcpy( pszValue, pszHeader + 1776, FAST_VALUE_SIZE );
+	poDS->SetMetadataItem( CPLSPrintf("BIAS%d",  poDS->nBands), pszValue );
+	memcpy( pszValue, pszHeader + 1801, FAST_VALUE_SIZE );
+	poDS->SetMetadataItem( CPLSPrintf("GAIN%d",  poDS->nBands), pszValue );
     }
-    if ( pszHeader[1249] != ' ' )
+    memcpy( pszFilename, pszHeader + 1249, FAST_FILENAME_SIZE );
+    if ( poDS->FOpenChannel( pszFilename, poDS->nBands ) )
     {
-	memcpy( pszFilename, pszHeader + 1249, FAST_FILENAME_SIZE );
-	pszChannelFilename = CPLFormFilename( pszDirname, pszFilename, NULL );
-        poDS->fpChannels[poDS->nBands] = VSIFOpen( pszChannelFilename, "rb" );
-	if ( poDS->fpChannels[poDS->nBands] )
-	{
-	    poDS->nBands++;
-	    memcpy( pszValue, pszHeader + 1856, FAST_VALUE_SIZE );
-	    poDS->SetMetadataItem( CPLSPrintf("BIAS%d",  poDS->nBands), pszValue );
-	    memcpy( pszValue, pszHeader + 1881, FAST_VALUE_SIZE );
-	    poDS->SetMetadataItem( CPLSPrintf("GAIN%d",  poDS->nBands), pszValue );
-	}
+	poDS->nBands++;
+	memcpy( pszValue, pszHeader + 1856, FAST_VALUE_SIZE );
+	poDS->SetMetadataItem( CPLSPrintf("BIAS%d",  poDS->nBands), pszValue );
+	memcpy( pszValue, pszHeader + 1881, FAST_VALUE_SIZE );
+	poDS->SetMetadataItem( CPLSPrintf("GAIN%d",  poDS->nBands), pszValue );
     }
-    if ( pszHeader[1290] != ' ' )
+    memcpy( pszFilename, pszHeader + 1290, FAST_FILENAME_SIZE );
+    if ( poDS->FOpenChannel( pszFilename, poDS->nBands ) )
     {
-	memcpy( pszFilename, pszHeader + 1290, FAST_FILENAME_SIZE );
-	pszChannelFilename = CPLFormFilename( pszDirname, pszFilename, NULL );
-        poDS->fpChannels[poDS->nBands] = VSIFOpen( pszChannelFilename, "rb" );
-	if ( poDS->fpChannels[poDS->nBands] )
-	{
-	    poDS->nBands++;
-	    memcpy( pszValue, pszHeader + 1936, FAST_VALUE_SIZE );
-	    poDS->SetMetadataItem( CPLSPrintf("BIAS%d",  poDS->nBands), pszValue );
-	    memcpy( pszValue, pszHeader + 1961, FAST_VALUE_SIZE );
-	    poDS->SetMetadataItem( CPLSPrintf("GAIN%d",  poDS->nBands), pszValue );
-	}
+	poDS->nBands++;
+	memcpy( pszValue, pszHeader + 1936, FAST_VALUE_SIZE );
+	poDS->SetMetadataItem( CPLSPrintf("BIAS%d",  poDS->nBands), pszValue );
+	memcpy( pszValue, pszHeader + 1961, FAST_VALUE_SIZE );
+	poDS->SetMetadataItem( CPLSPrintf("GAIN%d",  poDS->nBands), pszValue );
     }
-    if ( pszHeader[1329] != ' ' )
+    memcpy( pszFilename, pszHeader + 1329, FAST_FILENAME_SIZE );
+    if ( poDS->FOpenChannel( pszFilename, poDS->nBands ) )
     {
-	memcpy( pszFilename, pszHeader + 1329, FAST_FILENAME_SIZE );
-	pszChannelFilename = CPLFormFilename( pszDirname, pszFilename, NULL );
-        poDS->fpChannels[poDS->nBands] = VSIFOpen( pszChannelFilename, "rb" );
-	if ( poDS->fpChannels[poDS->nBands] )
-	{
-	    poDS->nBands++;
-	    memcpy( pszValue, pszHeader + 2016, FAST_VALUE_SIZE );
-	    poDS->SetMetadataItem( CPLSPrintf("BIAS%d",  poDS->nBands), pszValue );
-	    memcpy( pszValue, pszHeader + 2041, FAST_VALUE_SIZE );
-	    poDS->SetMetadataItem( CPLSPrintf("GAIN%d",  poDS->nBands), pszValue );
-	}
+	poDS->nBands++;
+	memcpy( pszValue, pszHeader + 2016, FAST_VALUE_SIZE );
+	poDS->SetMetadataItem( CPLSPrintf("BIAS%d",  poDS->nBands), pszValue );
+	memcpy( pszValue, pszHeader + 2041, FAST_VALUE_SIZE );
+	poDS->SetMetadataItem( CPLSPrintf("GAIN%d",  poDS->nBands), pszValue );
     }
 
     if ( !poDS->nBands )
+    {
+	CPLDebug( "FAST", "Failed to find and open band data files." );
+	delete poDS;
 	return NULL;
+    }
     
     poDS->nRasterXSize = atoi( pszHeader + 842 );
     poDS->nRasterYSize = atoi( pszHeader + 870 );
@@ -351,7 +467,10 @@ GDALDataset *FASTDataset::Open( GDALOpenInfo * poOpenInfo )
     
     if ( EQUALN(pszHeader + 3103, "UTM", 3) )
     {
-        iUTMZone = atoi( pszHeader + 3592 );
+        if ( poDS->iSatellite == LANDSAT )
+	    iUTMZone = atoi( pszHeader + 3592 );
+	else
+	    iUTMZone = (int)atof( pszHeader + 3232 );
         if( *(pszHeader + 3662) == 'N' )	// North hemisphere
 	    oSRS.SetUTM( iUTMZone, TRUE );
         else					// South hemisphere
@@ -387,9 +506,6 @@ GDALDataset *FASTDataset::Open( GDALOpenInfo * poOpenInfo )
     for( i = 1; i <= poDS->nBands; i++ )
         poDS->SetBand( i, new FASTRasterBand( poDS, i, poDS->fpChannels[i - 1],
 	    0, 1, poDS->nRasterXSize, poDS->eDataType, TRUE));
-
-    CPLFree( pszDirname );
-    CPLFree( pszHeader );
 
     return( poDS );
 }
