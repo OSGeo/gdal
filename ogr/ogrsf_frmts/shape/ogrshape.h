e97a68811ba922603061c90442dab87c12085428
Frank Warmerdam <warmerdam@pobox.com>
2003-03-03 05:06:46 +0000

    implemented DeleteDataSource
    git-svn-id: https://svn.osgeo.org/gdal/trunk@4385 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/ogrsf_frmts/shape/ogrshape.h
+++ ogr/ogrsf_frmts/shape/ogrshape.h
@@ -29,6 +29,9 @@
  ******************************************************************************
  *
  * $Log$
+ * Revision 1.11  2003/03/03 05:06:46  warmerda
+ * implemented DeleteDataSource
+ *
  * Revision 1.10  2002/06/15 00:07:23  aubin
  * mods to enable 64bit file i/o
  *
@@ -181,6 +184,7 @@ class OGRShapeDriver : public OGRSFDriver
 
     virtual OGRDataSource *CreateDataSource( const char *pszName,
                                              char ** = NULL );
+    OGRErr              DeleteDataSource( const char *pszDataSource );
     
     int                 TestCapability( const char * );
 };
