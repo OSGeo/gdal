9b78f02deaa305ef06cf3e9c50e9be17892b758a
Frank Warmerdam <warmerdam@pobox.com>
2003-03-18 06:01:03 +0000

    Added GDALFlushCache()
    git-svn-id: https://svn.osgeo.org/gdal/trunk@4494 f0d54148-0727-0410-94bb-9a71ac55c965

--- gcore/gdal.h
+++ gcore/gdal.h
@@ -29,6 +29,9 @@
  ******************************************************************************
  *
  * $Log$
+ * Revision 1.62  2003/03/18 06:01:03  warmerda
+ * Added GDALFlushCache()
+ *
  * Revision 1.61  2003/02/20 18:34:12  warmerda
  * added GDALGetRasterAccess()
  *
@@ -472,6 +475,7 @@ CPLErr CPL_DLL  GDALBuildOverviews( GDALDatasetH, const char *, int, int *,
                                     int, int *, GDALProgressFunc, void * );
 void CPL_DLL    GDALGetOpenDatasets( GDALDatasetH ***hDS, int *pnCount );
 int CPL_DLL     GDALGetAccess( GDALDatasetH hDS );
+void CPL_DLL    GDALFlushCache( GDALDatasetH hDS );
 
 /* ==================================================================== */
 /*      GDALRasterBand ... one band/channel in a dataset.               */
