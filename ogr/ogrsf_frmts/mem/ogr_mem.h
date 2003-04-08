717f165d7a97b0c41e82bff9fbabe9f167183b38
Frank Warmerdam <warmerdam@pobox.com>
2003-04-08 19:32:47 +0000

    New
    git-svn-id: https://svn.osgeo.org/gdal/trunk@4651 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ ogr/ogrsf_frmts/mem/ogr_mem.h
@@ -0,0 +1,135 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  OpenGIS Simple Features Reference Implementation
+ * Purpose:  Private definitions within the OGR Memory driver.
+ * Author:   Frank Warmerdam, warmerdam@pobox.com
+ *
+ ******************************************************************************
+ * Copyright (c) 2003, Frank Warmerdam <warmerdam@pobox.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ******************************************************************************
+ *
+ * $Log$
+ * Revision 1.1  2003/04/08 19:32:47  warmerda
+ * New
+ *
+ */
+
+#ifndef _OGRMEM_H_INCLUDED
+#define _OGRMEM_H_INCLUDED
+
+#include "ogrsf_frmts.h"
+
+/************************************************************************/
+/*                             OGRMemLayer                              */
+/************************************************************************/
+
+class OGRMemLayer : public OGRLayer
+{
+    OGRSpatialReference *poSRS;
+    OGRFeatureDefn     *poFeatureDefn;
+    OGRGeometry		*poFilterGeom;
+    
+    int                 nFeatureCount;
+    int                 nMaxFeatureCount;
+    OGRFeature        **papoFeatures;
+
+    int                 iNextReadFID;
+    int                 iNextCreateFID;
+
+    OGRwkbGeometryType  eWkbType;
+
+  public:
+    			OGRMemLayer( const char * pszName,
+                                     OGRSpatialReference *poSRS,
+                                     OGRwkbGeometryType eGeomType );
+    			~OGRMemLayer();
+
+    OGRGeometry *	GetSpatialFilter() { return poFilterGeom; }
+    void		SetSpatialFilter( OGRGeometry * );
+
+    void		ResetReading();
+    OGRFeature *	GetNextFeature();
+
+    OGRFeature         *GetFeature( long nFeatureId );
+    OGRErr              SetFeature( OGRFeature *poFeature );
+    OGRErr              CreateFeature( OGRFeature *poFeature );
+    
+    OGRFeatureDefn *	GetLayerDefn() { return poFeatureDefn; }
+
+    int                 GetFeatureCount( int );
+    OGRErr              GetExtent(OGREnvelope *psExtent, int bForce);
+
+    virtual OGRErr      CreateField( OGRFieldDefn *poField,
+                                     int bApproxOK = TRUE );
+
+    virtual OGRSpatialReference *GetSpatialRef();
+    
+    int                 TestCapability( const char * );
+};
+
+/************************************************************************/
+/*                           OGRMemDataSource                           */
+/************************************************************************/
+
+class OGRMemDataSource : public OGRDataSource
+{
+    OGRMemLayer     **papoLayers;
+    int			nLayers;
+    
+    char		*pszName;
+
+  public:
+    			OGRMemDataSource( const char *, char ** );
+    			~OGRMemDataSource();
+
+    const char	        *GetName() { return pszName; }
+    int			GetLayerCount() { return nLayers; }
+    OGRLayer		*GetLayer( int );
+
+    virtual OGRLayer    *CreateLayer( const char *, 
+                                      OGRSpatialReference * = NULL,
+                                      OGRwkbGeometryType = wkbUnknown,
+                                      char ** = NULL );
+
+    int                 TestCapability( const char * );
+};
+
+/************************************************************************/
+/*                             OGRMemDriver                             */
+/************************************************************************/
+
+class OGRMemDriver : public OGRSFDriver
+{
+  public:
+    		~OGRMemDriver();
+                
+    const char *GetName();
+    OGRDataSource *Open( const char *, int );
+
+    virtual OGRDataSource *CreateDataSource( const char *pszName,
+                                             char ** = NULL );
+    
+    int                 TestCapability( const char * );
+};
+
+
+#endif /* ndef _OGRMEM_H_INCLUDED */
---
+++ ogr/ogrsf_frmts/mem/ogrmemlayer.cpp
@@ -0,0 +1,302 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  OpenGIS Simple Features Reference Implementation
+ * Purpose:  Implements OGRMemLayer class.
+ * Author:   Frank Warmerdam, warmerdam@pobox.com
+ *
+ ******************************************************************************
+ * Copyright (c) 2003, Frank Warmerdam <warmerdam@pobox.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ******************************************************************************
+ *
+ * $Log$
+ * Revision 1.1  2003/04/08 19:32:47  warmerda
+ * New
+ *
+ */
+
+#include "ogr_mem.h"
+#include "cpl_conv.h"
+
+CPL_CVSID("$Id$");
+
+/************************************************************************/
+/*                            OGRMemLayer()                             */
+/************************************************************************/
+
+OGRMemLayer::OGRMemLayer( const char * pszName, OGRSpatialReference *poSRSIn, 
+                          OGRwkbGeometryType eReqType )
+
+{
+    poFilterGeom = NULL;
+    if( poSRSIn == NULL )
+        poSRS = NULL;
+    else
+        poSRS = poSRSIn->Clone();
+    
+    iNextReadFID = 0;
+    iNextCreateFID = 0;
+
+    nFeatureCount = 0;
+    nMaxFeatureCount = 0;
+    papoFeatures = NULL;
+
+    poFeatureDefn = new OGRFeatureDefn( pszName );
+    poFeatureDefn->SetGeomType( eReqType );
+}
+
+/************************************************************************/
+/*                           ~OGRMemLayer()                           */
+/************************************************************************/
+
+OGRMemLayer::~OGRMemLayer()
+
+{
+    for( int i = 0; i < nMaxFeatureCount; i++ )
+    {
+        if( papoFeatures[i] != NULL )
+            delete papoFeatures[i];
+    }
+    CPLFree( papoFeatures );
+    
+    delete poFeatureDefn;
+
+    if( poSRS != NULL && poSRS->Dereference() == 0 )
+        delete poSRS;
+
+    if( poFilterGeom != NULL )
+        delete poFilterGeom;
+}
+
+/************************************************************************/
+/*                          SetSpatialFilter()                          */
+/************************************************************************/
+
+void OGRMemLayer::SetSpatialFilter( OGRGeometry * poGeomIn )
+
+{
+    if( poFilterGeom != NULL )
+    {
+        delete poFilterGeom;
+        poFilterGeom = NULL;
+    }
+
+    if( poGeomIn != NULL )
+        poFilterGeom = poGeomIn->clone();
+}
+
+/************************************************************************/
+/*                            ResetReading()                            */
+/************************************************************************/
+
+void OGRMemLayer::ResetReading()
+
+{
+    iNextReadFID = 0;
+}
+
+/************************************************************************/
+/*                           GetNextFeature()                           */
+/************************************************************************/
+
+OGRFeature *OGRMemLayer::GetNextFeature()
+
+{
+    while( iNextReadFID < nMaxFeatureCount )
+    {
+        OGRFeature *poFeature = papoFeatures[iNextReadFID++];
+
+        if( poFeature == NULL )
+            continue;
+
+        if( (poFilterGeom == NULL
+            || poFilterGeom->Intersect( poFeature->GetGeometryRef() ) )
+            && (m_poAttrQuery == NULL
+                || m_poAttrQuery->Evaluate( poFeature )) )
+            return poFeature->Clone();
+    }
+
+    return NULL;
+}
+
+/************************************************************************/
+/*                             GetFeature()                             */
+/************************************************************************/
+
+OGRFeature *OGRMemLayer::GetFeature( long nFeatureId )
+
+{
+    if( nFeatureId < 0 || nFeatureId >= nMaxFeatureCount )
+        return NULL;
+    else if( papoFeatures[nFeatureId] == NULL )
+        return NULL;
+    else
+        return papoFeatures[nFeatureId]->Clone();
+}
+
+/************************************************************************/
+/*                             SetFeature()                             */
+/************************************************************************/
+
+OGRErr OGRMemLayer::SetFeature( OGRFeature *poFeature )
+
+{
+    if( poFeature == NULL )
+        return OGRERR_FAILURE;
+
+    if( poFeature->GetFID() == OGRNullFID )
+    {
+        while( iNextCreateFID < nMaxFeatureCount 
+               && papoFeatures[iNextCreateFID] != NULL )
+            iNextCreateFID++;
+        poFeature->SetFID( iNextCreateFID++ );
+    }
+
+    if( poFeature->GetFID() >= nMaxFeatureCount )
+    {
+        int nNewCount = MAX(2*nMaxFeatureCount+10, poFeature->GetFID() + 1 );
+
+        papoFeatures = (OGRFeature **) 
+            CPLRealloc( papoFeatures, sizeof(OGRFeature *) * nNewCount);
+        memset( papoFeatures + nMaxFeatureCount, 0, 
+                sizeof(OGRFeature *) * (nNewCount - nMaxFeatureCount) );
+        nMaxFeatureCount = nNewCount;
+    }
+
+    if( papoFeatures[poFeature->GetFID()] != NULL )
+    {
+        delete papoFeatures[poFeature->GetFID()];
+        papoFeatures[poFeature->GetFID()] = NULL;
+        nFeatureCount--;
+    }
+
+    papoFeatures[poFeature->GetFID()] = poFeature->Clone();
+    nFeatureCount++;
+
+    return OGRERR_NONE;
+}
+
+/************************************************************************/
+/*                           CreateFeature()                            */
+/************************************************************************/
+
+OGRErr OGRMemLayer::CreateFeature( OGRFeature *poFeature )
+
+{
+    if( poFeature->GetFID() != OGRNullFID 
+        && poFeature->GetFID() >= 0
+        && poFeature->GetFID() < nMaxFeatureCount )
+    {
+        if( papoFeatures[poFeature->GetFID()] != NULL )
+            poFeature->SetFID( OGRNullFID );
+    }
+
+    if( poFeature->GetFID() > 10000000 )
+        poFeature->SetFID( OGRNullFID );
+
+    return SetFeature( poFeature );
+}
+
+/************************************************************************/
+/*                          GetFeatureCount()                           */
+/*                                                                      */
+/*      If a spatial filter is in effect, we turn control over to       */
+/*      the generic counter.  Otherwise we return the total count.      */
+/*      Eventually we should consider implementing a more efficient     */
+/*      way of counting features matching a spatial query.              */
+/************************************************************************/
+
+int OGRMemLayer::GetFeatureCount( int bForce )
+
+{
+    if( poFilterGeom != NULL || m_poAttrQuery != NULL )
+        return OGRLayer::GetFeatureCount( bForce );
+    else
+        return nFeatureCount;
+}
+
+/************************************************************************/
+/*                             GetExtent()                              */
+/*                                                                      */
+/*      Fetch extent of the data currently stored in the dataset.       */
+/*      The bForce flag has no effect on SHO files since that value     */
+/*      is always in the header.                                        */
+/*                                                                      */
+/*      Returns OGRERR_NONE/OGRRERR_FAILURE.                            */
+/************************************************************************/
+
+OGRErr OGRMemLayer::GetExtent (OGREnvelope *psExtent, int bForce)
+
+{
+    return OGRLayer::GetExtent( psExtent, bForce );
+}
+
+/************************************************************************/
+/*                           TestCapability()                           */
+/************************************************************************/
+
+int OGRMemLayer::TestCapability( const char * pszCap )
+
+{
+    if( EQUAL(pszCap,OLCRandomRead) )
+        return TRUE;
+
+    else if( EQUAL(pszCap,OLCSequentialWrite) 
+             || EQUAL(pszCap,OLCRandomWrite) )
+        return TRUE;
+
+    else if( EQUAL(pszCap,OLCFastFeatureCount) )
+        return poFilterGeom == NULL;
+
+    else if( EQUAL(pszCap,OLCFastSpatialFilter) )
+        return FALSE;
+
+    else if( EQUAL(pszCap,OLCFastGetExtent) )
+        return TRUE;
+
+    else if( EQUAL(pszCap,OLCCreateField) )
+        return TRUE;
+
+    else 
+        return FALSE;
+}
+
+/************************************************************************/
+/*                            CreateField()                             */
+/************************************************************************/
+
+OGRErr OGRMemLayer::CreateField( OGRFieldDefn *poField, int bApproxOK )
+
+{
+    poFeatureDefn->AddFieldDefn( poField );
+
+    return OGRERR_NONE;
+}
+
+/************************************************************************/
+/*                           GetSpatialRef()                            */
+/************************************************************************/
+
+OGRSpatialReference *OGRMemLayer::GetSpatialRef()
+
+{
+    return poSRS;
+}
