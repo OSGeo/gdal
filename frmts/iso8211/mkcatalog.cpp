9c50021d0eb67ed3b3297970cdd12f2fe6fca3ae
Frank Warmerdam <warmerdam@pobox.com>
2003-09-15 20:50:04 +0000

    updated
    git-svn-id: https://svn.osgeo.org/gdal/trunk@5302 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/iso8211/mkcatalog.cpp
+++ frmts/iso8211/mkcatalog.cpp
@@ -28,6 +28,9 @@
  ******************************************************************************
  *
  * $Log$
+ * Revision 1.3  2003/09/15 20:50:04  warmerda
+ * updated
+ *
  * Revision 1.2  2003/09/03 20:36:26  warmerda
  * added subfield writing support
  *
@@ -41,11 +44,289 @@
 CPL_CVSID("$Id$");
 
 
-/* **********************************************************************/
-/*                                main()                                */
-/* **********************************************************************/
+/************************************************************************/
+/*                               mk_s57()                               */
+/************************************************************************/
 
-int main( int nArgc, char ** papszArgv )
+void mk_s57()
+
+{
+    DDFModule  oModule;
+    DDFFieldDefn *poFDefn;
+
+    oModule.Initialize();
+
+/* -------------------------------------------------------------------- */
+/*      Create the '0000' definition.                                   */
+/* -------------------------------------------------------------------- */
+    poFDefn = new DDFFieldDefn();
+
+    poFDefn->Create( "0000", "", "0001DSIDDSIDDSSI0001DSPM0001VRIDVRIDATTVVRIDVRPCVRIDVRPTVRIDSGCCVRIDSG2DVRIDSG3D0001FRIDFRIDFOIDFRIDATTFFRIDNATFFRIDFFPCFRIDFFPTFRIDFSPCFRIDFSPT",
+                     DDFFieldDefn::elementary, 
+                     DDFFieldDefn::char_string );
+
+    oModule.AddField( poFDefn );
+
+/* -------------------------------------------------------------------- */
+/*      Create the '0001' definition.                                   */
+/* -------------------------------------------------------------------- */
+    poFDefn = new DDFFieldDefn();
+
+    poFDefn->Create( "0001", "ISO 8211 Record Identifier", "", 
+                     DDFFieldDefn::elementary, DDFFieldDefn::bit_string,
+                     "(b12)" );
+
+    oModule.AddField( poFDefn );
+
+/* -------------------------------------------------------------------- */
+/*      Create the DSID field.                                          */
+/* -------------------------------------------------------------------- */
+    poFDefn = new DDFFieldDefn();
+
+    poFDefn->Create( "DSID", "Data set identification field", "",
+                     DDFFieldDefn::vector, DDFFieldDefn::mixed_data_type );
+
+    poFDefn->AddSubfield( "RCNM", "b11" );
+    poFDefn->AddSubfield( "RCID", "b14" );
+    poFDefn->AddSubfield( "EXPP", "b11" );
+    poFDefn->AddSubfield( "INTU", "b11" );
+    poFDefn->AddSubfield( "DSNM", "A" );
+    poFDefn->AddSubfield( "EDTN", "A" );
+    poFDefn->AddSubfield( "UPDN", "A" );
+    poFDefn->AddSubfield( "UADT", "A(8)" );
+    poFDefn->AddSubfield( "ISDT", "A(8)" );
+    poFDefn->AddSubfield( "STED", "R(4)" );
+    poFDefn->AddSubfield( "PRSP", "b11" );
+    poFDefn->AddSubfield( "PSDN", "A" );
+    poFDefn->AddSubfield( "PRED", "A" );
+    poFDefn->AddSubfield( "PROF", "b11" );
+    poFDefn->AddSubfield( "AGEN", "b12" );
+    poFDefn->AddSubfield( "COMT", "A" );
+
+    oModule.AddField( poFDefn );
+
+/* -------------------------------------------------------------------- */
+/*      Create the DSSI field.                                          */
+/* -------------------------------------------------------------------- */
+    poFDefn = new DDFFieldDefn();
+
+    poFDefn->Create( "DSSI", "Data set structure information field", "",
+                     DDFFieldDefn::vector, DDFFieldDefn::mixed_data_type );
+
+    poFDefn->AddSubfield( "DSTR", "b11" );
+    poFDefn->AddSubfield( "AALL", "b11" );
+    poFDefn->AddSubfield( "NALL", "b11" );
+    poFDefn->AddSubfield( "NOMR", "b14" );
+    poFDefn->AddSubfield( "NOCR", "b14" );
+    poFDefn->AddSubfield( "NOGR", "b14" );
+    poFDefn->AddSubfield( "NOLR", "b14" );
+    poFDefn->AddSubfield( "NOIN", "b14" );
+    poFDefn->AddSubfield( "NOCN", "b14" );
+    poFDefn->AddSubfield( "NOED", "b14" );
+    poFDefn->AddSubfield( "NOFA", "b14" );
+
+    oModule.AddField( poFDefn );
+
+/* -------------------------------------------------------------------- */
+/*      Create the DSPM field.                                          */
+/* -------------------------------------------------------------------- */
+    poFDefn = new DDFFieldDefn();
+
+    poFDefn->Create( "DSPM", "Data set parameter field", "",
+                     DDFFieldDefn::vector, DDFFieldDefn::mixed_data_type );
+
+    poFDefn->AddSubfield( "RCNM", "b11" );
+    poFDefn->AddSubfield( "RCID", "b14" );
+    poFDefn->AddSubfield( "HDAT", "b11" );
+    poFDefn->AddSubfield( "VDAT", "b11" );
+    poFDefn->AddSubfield( "SDAT", "b11" );
+    poFDefn->AddSubfield( "CSCL", "b14" );
+    poFDefn->AddSubfield( "DUNI", "b11" );
+    poFDefn->AddSubfield( "HUNI", "b11" );
+    poFDefn->AddSubfield( "PUNI", "b11" );
+    poFDefn->AddSubfield( "COUN", "b11" );
+    poFDefn->AddSubfield( "COMF", "b14" );
+    poFDefn->AddSubfield( "SOMF", "b14" );
+    poFDefn->AddSubfield( "COMT", "A" );
+
+    oModule.AddField( poFDefn );
+
+/* -------------------------------------------------------------------- */
+/*      Create the VRID field.                                          */
+/* -------------------------------------------------------------------- */
+    poFDefn = new DDFFieldDefn();
+
+    poFDefn->Create( "VRID", "Vector record identifier field", "",
+                     DDFFieldDefn::vector, DDFFieldDefn::mixed_data_type );
+
+    poFDefn->AddSubfield( "RCNM", "b11" );
+    poFDefn->AddSubfield( "RCID", "b14" );
+    poFDefn->AddSubfield( "RVER", "b12" );
+    poFDefn->AddSubfield( "RUIN", "b11" );
+
+    oModule.AddField( poFDefn );
+
+/* -------------------------------------------------------------------- */
+/*      Create the ATTV field.                                          */
+/* -------------------------------------------------------------------- */
+    poFDefn = new DDFFieldDefn();
+
+    poFDefn->Create( "ATTV", "Vector record attribute field", "",
+                     DDFFieldDefn::vector, DDFFieldDefn::mixed_data_type );
+
+    /* how do I mark this as repeating? */
+    poFDefn->AddSubfield( "ATTL", "b12" );
+    poFDefn->AddSubfield( "ATVL", "A" );
+
+    oModule.AddField( poFDefn );
+
+/* -------------------------------------------------------------------- */
+/*      Create the SG2D field.                                          */
+/* -------------------------------------------------------------------- */
+    poFDefn = new DDFFieldDefn();
+
+    poFDefn->Create( "SG2D", "2-D coordinate field", "*",
+                     DDFFieldDefn::vector, DDFFieldDefn::mixed_data_type );
+
+    /* how do I mark this as repeating? */
+    poFDefn->AddSubfield( "YCOO", "b24" );
+    poFDefn->AddSubfield( "XCOO", "b24" );
+
+    oModule.AddField( poFDefn );
+
+/* -------------------------------------------------------------------- */
+/*      Create the SG3D field.                                          */
+/* -------------------------------------------------------------------- */
+    poFDefn = new DDFFieldDefn();
+
+    poFDefn->Create( "SG3D", "3-D coordinate (sounding array) field", "*",
+                     DDFFieldDefn::vector, DDFFieldDefn::mixed_data_type );
+
+    /* how do I mark this as repeating? */
+    poFDefn->AddSubfield( "YCOO", "b24" );
+    poFDefn->AddSubfield( "XCOO", "b24" );
+    poFDefn->AddSubfield( "VE3D", "b24" );
+
+    oModule.AddField( poFDefn );
+
+/* -------------------------------------------------------------------- */
+
+// need to add: VRPC, VRPT, SGCC, FRID, FOID, ATTF, NATF, FFPC, 
+// FFPT, FSPC, and FSPT
+
+/* -------------------------------------------------------------------- */
+/*      Create file.                                                    */
+/* -------------------------------------------------------------------- */
+    oModule.Dump( stdout );
+
+    oModule.Create( "out.000" );
+
+/* -------------------------------------------------------------------- */
+/*      Create a record.                                                */
+/* -------------------------------------------------------------------- */
+    DDFRecord *poRec = new DDFRecord( &oModule );
+    DDFField *poField;
+
+    poField = poRec->AddField( oModule.FindFieldDefn( "0001" ) );
+    poRec->SetFieldRaw( poField, 0, "\1\0\036", 3 );
+
+    poField = poRec->AddField( oModule.FindFieldDefn( "DSID" ) );
+
+    poRec->SetIntSubfield   ( "DSID", 0, "RCNM", 0, 10 );
+    poRec->SetIntSubfield   ( "DSID", 0, "RCID", 0, 1 );
+    poRec->SetIntSubfield   ( "DSID", 0, "EXPP", 0, 1 );
+    poRec->SetIntSubfield   ( "DSID", 0, "INTU", 0, 4 );
+    poRec->SetStringSubfield( "DSID", 0, "DSNM", 0, "GB4X0000.000" );
+    poRec->SetStringSubfield( "DSID", 0, "EDTN", 0, "2" );
+    poRec->SetStringSubfield( "DSID", 0, "UPDN", 0, "0" );
+    poRec->SetStringSubfield( "DSID", 0, "UADT", 0, "20010409" );
+    poRec->SetStringSubfield( "DSID", 0, "ISDT", 0, "20010409" );
+    poRec->SetFloatSubfield ( "DSID", 0, "STED", 0, 3.1 );
+    poRec->SetIntSubfield   ( "DSID", 0, "PRSP", 0, 1 );
+    poRec->SetStringSubfield( "DSID", 0, "PSDN", 0, "" );
+    poRec->SetStringSubfield( "DSID", 0, "PRED", 0, "2.0" );
+    poRec->SetIntSubfield   ( "DSID", 0, "PROF", 0, 1 );
+    poRec->SetIntSubfield   ( "DSID", 0, "AGEN", 0, 540 );
+    poRec->SetStringSubfield( "DSID", 0, "COMT", 0, "" );
+
+    poField = poRec->AddField( oModule.FindFieldDefn( "DSSI" ) );
+
+    poRec->SetIntSubfield   ( "DSSI", 0, "DSTR", 0, 2 );
+    poRec->SetIntSubfield   ( "DSSI", 0, "AALL", 0, 1 );
+    poRec->SetIntSubfield   ( "DSSI", 0, "NALL", 0, 1 );
+    poRec->SetIntSubfield   ( "DSSI", 0, "NOMR", 0, 22 );
+    poRec->SetIntSubfield   ( "DSSI", 0, "NOCR", 0, 0 );
+    poRec->SetIntSubfield   ( "DSSI", 0, "NOGR", 0, 2141 );
+    poRec->SetIntSubfield   ( "DSSI", 0, "NOLR", 0, 15 );
+    poRec->SetIntSubfield   ( "DSSI", 0, "NOIN", 0, 512 );
+    poRec->SetIntSubfield   ( "DSSI", 0, "NOCN", 0, 2181 );
+    poRec->SetIntSubfield   ( "DSSI", 0, "NOED", 0, 3192 );
+    poRec->SetIntSubfield   ( "DSSI", 0, "NOFA", 0, 0 );
+
+    poRec->Write();
+    delete poRec;
+
+/* -------------------------------------------------------------------- */
+/*      Create a record.                                                */
+/* -------------------------------------------------------------------- */
+    poRec = new DDFRecord( &oModule );
+
+    poField = poRec->AddField( oModule.FindFieldDefn( "0001" ) );
+    poRec->SetFieldRaw( poField, 0, "\2\0\036", 3 );
+
+    poField = poRec->AddField( oModule.FindFieldDefn( "DSPM" ) );
+
+    poRec->SetIntSubfield   ( "DSPM", 0, "RCNM", 0, 20 );
+    poRec->SetIntSubfield   ( "DSPM", 0, "RCID", 0, 1 );
+    poRec->SetIntSubfield   ( "DSPM", 0, "HDAT", 0, 2 );
+    poRec->SetIntSubfield   ( "DSPM", 0, "VDAT", 0, 17 );
+    poRec->SetIntSubfield   ( "DSPM", 0, "SDAT", 0, 23 );
+    poRec->SetIntSubfield   ( "DSPM", 0, "CSCL", 0, 52000 );
+    poRec->SetIntSubfield   ( "DSPM", 0, "DUNI", 0, 1 );
+    poRec->SetIntSubfield   ( "DSPM", 0, "HUNI", 0, 1 );
+    poRec->SetIntSubfield   ( "DSPM", 0, "PUNI", 0, 1 );
+    poRec->SetIntSubfield   ( "DSPM", 0, "COUN", 0, 1 );
+    poRec->SetIntSubfield   ( "DSPM", 0, "COMF", 0, 1000000 );
+    poRec->SetIntSubfield   ( "DSPM", 0, "SOMF", 0, 10 );
+
+    poRec->Write();
+    delete poRec;
+
+/* -------------------------------------------------------------------- */
+/*      Create a record.                                                */
+/* -------------------------------------------------------------------- */
+    poRec = new DDFRecord( &oModule );
+
+    poField = poRec->AddField( oModule.FindFieldDefn( "0001" ) );
+    poRec->SetFieldRaw( poField, 0, "\3\0\036", 3 );
+
+    poField = poRec->AddField( oModule.FindFieldDefn( "VRID" ) );
+
+    poRec->SetIntSubfield   ( "VRID", 0, "RCNM", 0, 110 );
+    poRec->SetIntSubfield   ( "VRID", 0, "RCID", 0, 518 );
+    poRec->SetIntSubfield   ( "VRID", 0, "RVER", 0, 1 );
+    poRec->SetIntSubfield   ( "VRID", 0, "RUIN", 0, 1 );
+
+    poField = poRec->AddField( oModule.FindFieldDefn( "SG3D" ) );
+
+    poRec->SetIntSubfield   ( "SG3D", 0, "YCOO", 0, -325998702 );
+    poRec->SetIntSubfield   ( "SG3D", 0, "XCOO", 0, 612175350 );
+    poRec->SetIntSubfield   ( "SG3D", 0, "VE3D", 0, 174 );
+
+    poRec->SetIntSubfield   ( "SG3D", 0, "YCOO", 1, -325995189 );
+    poRec->SetIntSubfield   ( "SG3D", 0, "XCOO", 1, 612228812 );
+    poRec->SetIntSubfield   ( "SG3D", 0, "VE3D", 1, 400 );
+
+    poRec->Write();
+
+    delete poRec;
+}
+
+/************************************************************************/
+/*                             mk_catalog()                             */
+/************************************************************************/
+
+void mk_catalog()
 
 {
     DDFModule  oModule;
@@ -143,5 +424,15 @@ int main( int nArgc, char ** papszArgv )
     poRec->SetStringSubfield( "CATD", 0, "CRCS", 0, "555C3AD4" );
     poRec->Write();
     delete poRec;
-
 }
+
+/* **********************************************************************/
+/*                                main()                                */
+/* **********************************************************************/
+
+int main( int nArgc, char ** papszArgv )
+
+{
+    mk_s57();
+}
+
