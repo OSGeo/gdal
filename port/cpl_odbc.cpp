a0d145720ed59f7d293705b2a1fc65ee06ebb52a
Frank Warmerdam <warmerdam@pobox.com>
2003-09-24 15:39:14 +0000

    New
    git-svn-id: https://svn.osgeo.org/gdal/trunk@5348 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ port/cpl_odbc.cpp
@@ -0,0 +1,537 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  SignalFusion.com
+ * Purpose:  Declarations for ODBC Access Cover API.
+ * Author:   Frank Warmerdam, warmerdam@pobox.com
+ *
+ ******************************************************************************
+ * Copyright (c) 2003, Frank Warmerdam
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ******************************************************************************
+ *
+ * $Log$
+ * Revision 1.1  2003/09/24 15:38:27  warmerda
+ * New
+ *
+ */
+
+#include "cpl_odbc.h"
+
+CPL_CVSID("$Id$");
+
+
+/************************************************************************/
+/*                           CPLODBCSession()                           */
+/************************************************************************/
+
+CPLODBCSession::CPLODBCSession()
+
+{
+    m_szLastError[0] = '\0';
+    m_hEnv = NULL;
+    m_hDBC = NULL;
+}
+
+/************************************************************************/
+/*                          ~CPLODBCSession()                           */
+/************************************************************************/
+
+CPLODBCSession::~CPLODBCSession()
+
+{
+    CloseSession();
+}
+
+/************************************************************************/
+/*                            CloseSession()                            */
+/************************************************************************/
+
+int CPLODBCSession::CloseSession()
+
+{
+    if( m_hDBC!=NULL ) 
+    {
+        SQLDisconnect( m_hDBC );
+        SQLFreeConnect( m_hDBC );
+        m_hDBC = NULL;
+    }
+    
+    if( m_hEnv!=NULL )
+    {
+        SQLFreeEnv( m_hEnv );
+        m_hEnv = NULL;
+    }
+
+    return TRUE;
+}
+
+/************************************************************************/
+/*                               Failed()                               */
+/*                                                                      */
+/*      Test if a return code indicates failure, return TRUE if that    */
+/*      is the case. Also update error text.                            */
+/************************************************************************/
+
+int CPLODBCSession::Failed( int nRetCode, HSTMT hStmt )
+
+{
+    SQLCHAR achSQLState[SQL_MAX_MESSAGE_LENGTH];
+    SQLINTEGER nNativeError;
+    SQLSMALLINT nTextLength;
+
+
+    if( nRetCode == SQL_SUCCESS || nRetCode == SQL_SUCCESS_WITH_INFO )
+        return FALSE;
+
+    SQLError( m_hEnv, m_hDBC, hStmt, achSQLState, &nNativeError,
+              (SQLCHAR *) m_szLastError, sizeof(m_szLastError)-1, 
+              &nTextLength );
+    m_szLastError[nTextLength] = '\0';
+
+    return TRUE;
+}
+
+/************************************************************************/
+/*                          EstablishSession()                          */
+/************************************************************************/
+
+int CPLODBCSession::EstablishSession( const char *pszDSN, 
+                                      const char *pszUserid, 
+                                      const char *pszPassword )
+
+{
+    CloseSession();
+    
+    if( Failed( SQLAllocEnv( &m_hEnv ) ) )
+        return FALSE;
+
+    if( Failed( SQLAllocConnect( m_hEnv, &m_hDBC ) ) )
+    {
+        CloseSession();
+        return FALSE;
+    }
+
+    SQLSetConnectOption( m_hDBC,SQL_LOGIN_TIMEOUT,5 );
+
+    if( pszUserid == NULL )
+        pszUserid = "";
+    if( pszPassword == NULL )
+        pszPassword = "";
+
+    if( Failed( SQLConnect( m_hDBC, (SQLCHAR *) pszDSN, SQL_NTS, 
+                            (SQLCHAR *) pszUserid, SQL_NTS, 
+                            (SQLCHAR *) pszPassword, SQL_NTS ) ) )
+    {
+        CloseSession();
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+
+/************************************************************************/
+/*                          CPLODBCStatement()                          */
+/************************************************************************/
+
+CPLODBCStatement::CPLODBCStatement( CPLODBCSession *poSession )
+
+{
+    m_poSession = poSession;
+
+    if( Failed( 
+            SQLAllocStmt( poSession->GetConnection(), &m_hStmt ) ) )
+    {
+        m_hStmt = NULL;
+        return;
+    }
+
+    m_nColCount = 0;
+    m_papszColNames = NULL;
+    m_panColType = NULL;
+    m_panColSize = NULL;
+    m_panColPrecision = NULL;
+    m_panColNullable = NULL;
+
+    m_papszColValues = NULL;
+
+    m_pszStatement = NULL;
+    m_nStatementMax = 0;
+    m_nStatementLen = 0;
+}
+
+/************************************************************************/
+/*                         ~CPLODBCStatement()                          */
+/************************************************************************/
+
+CPLODBCStatement::~CPLODBCStatement()
+
+{
+    ClearColumnData();
+    Clear();
+
+    if( m_hStmt != NULL )
+        SQLFreeStmt( m_hStmt, SQL_DROP );
+}
+
+/************************************************************************/
+/*                             ExecuteSQL()                             */
+/************************************************************************/
+
+int CPLODBCStatement::ExecuteSQL( const char *pszStatement )
+
+{
+    if( m_poSession == NULL || m_hStmt == NULL )
+    {
+        // we should post an error.
+        return FALSE;
+    }
+
+    if( pszStatement != NULL )
+    {
+        Clear();
+        Append( pszStatement );
+    }
+
+    if( Failed( 
+            SQLExecDirect( m_hStmt, (SQLCHAR *) m_pszStatement, SQL_NTS ) ) )
+        return FALSE;
+
+    return CollectResultsInfo();
+}
+
+/************************************************************************/
+/*                         CollectResultsInfo()                         */
+/************************************************************************/
+
+int CPLODBCStatement::CollectResultsInfo()
+
+{
+    if( m_poSession == NULL || m_hStmt == NULL )
+    {
+        // we should post an error.
+        return FALSE;
+    }
+
+    if( Failed( SQLNumResultCols(m_hStmt,&m_nColCount) ) )
+        return FALSE;
+
+/* -------------------------------------------------------------------- */
+/*      Allocate per column information.                                */
+/* -------------------------------------------------------------------- */
+    m_papszColNames = (char **) calloc(sizeof(char *),(m_nColCount+1));
+    m_papszColValues = (char **) calloc(sizeof(char *),(m_nColCount+1));
+
+    m_panColType = (short *) calloc(sizeof(short),m_nColCount);
+    m_panColSize = (short *) calloc(sizeof(short),m_nColCount);
+    m_panColPrecision = (short *) calloc(sizeof(short),m_nColCount);
+    m_panColNullable = (short *) calloc(sizeof(short),m_nColCount);
+
+/* -------------------------------------------------------------------- */
+/*      Fetch column descriptions.                                      */
+/* -------------------------------------------------------------------- */
+    for( int iCol = 0; iCol < m_nColCount; iCol++ )
+    {
+        char szColName[256];
+        SQLSMALLINT nNameLength;
+
+        if( Failed( 
+                SQLDescribeCol( m_hStmt, iCol+1, 
+                                (SQLCHAR *) szColName, sizeof(szColName),
+                                &nNameLength,
+                                m_panColType + iCol,
+                                (SQLUINTEGER*) m_panColSize + iCol,
+                                m_panColPrecision + iCol,
+                                m_panColNullable + iCol )) )
+            return FALSE;
+
+        szColName[nNameLength] = '\0';
+        m_papszColNames[iCol] = strdup(szColName);
+    }
+
+    return TRUE;
+}
+
+/************************************************************************/
+/*                            GetColCount()                             */
+/************************************************************************/
+
+int CPLODBCStatement::GetColCount()
+
+{
+    return m_nColCount;
+}
+
+/************************************************************************/
+/*                             GetColName()                             */
+/************************************************************************/
+
+const char *CPLODBCStatement::GetColName( int iCol )
+
+{
+    if( iCol < 0 || iCol >= m_nColCount )
+        return NULL;
+    else
+        return m_papszColNames[iCol];
+}
+
+/************************************************************************/
+/*                             GetColType()                             */
+/************************************************************************/
+
+short CPLODBCStatement::GetColType( int iCol )
+
+{
+    if( iCol < 0 || iCol >= m_nColCount )
+        return -1;
+    else
+        return m_panColType[iCol];
+}
+
+/************************************************************************/
+/*                             GetColSize()                             */
+/************************************************************************/
+
+short CPLODBCStatement::GetColSize( int iCol )
+
+{
+    if( iCol < 0 || iCol >= m_nColCount )
+        return -1;
+    else
+        return m_panColSize[iCol];
+}
+
+/************************************************************************/
+/*                          GetColPrecision()                           */
+/************************************************************************/
+
+short CPLODBCStatement::GetColPrecision( int iCol )
+
+{
+    if( iCol < 0 || iCol >= m_nColCount )
+        return -1;
+    else
+        return m_panColPrecision[iCol];
+}
+
+/************************************************************************/
+/*                           GetColNullable()                           */
+/************************************************************************/
+
+short CPLODBCStatement::GetColNullable( int iCol )
+
+{
+    if( iCol < 0 || iCol >= m_nColCount )
+        return -1;
+    else
+        return m_panColNullable[iCol];
+}
+
+/************************************************************************/
+/*                               Fetch()                                */
+/*                                                                      */
+/*      Fetch one row from a results set.                               */
+/************************************************************************/
+
+int CPLODBCStatement::Fetch()
+
+{
+    ClearColumnData();
+
+    if( m_hStmt == NULL || m_nColCount < 1 )
+        return FALSE;
+
+/* -------------------------------------------------------------------- */
+/*      Fetch a new row.                                                */
+/* -------------------------------------------------------------------- */
+    if( Failed( SQLFetch( m_hStmt ) ) )
+        return FALSE;
+
+/* -------------------------------------------------------------------- */
+/*      Pull out all the column values.                                 */
+/* -------------------------------------------------------------------- */
+    int iCol;
+    
+    for( iCol = 0; iCol < m_nColCount; iCol++ )
+    {
+        char szWrkData[8193];
+        SQLINTEGER cbDataLen;
+
+        szWrkData[0] = '\0';
+        if( Failed( SQLGetData( m_hStmt, iCol+1, SQL_C_CHAR,
+                                szWrkData, sizeof(szWrkData)-1, 
+                                &cbDataLen ) ) )
+            return FALSE;
+
+        if( cbDataLen == SQL_NULL_DATA )
+            m_papszColValues[iCol] = NULL;
+        else
+        {
+            szWrkData[cbDataLen] = '\0';
+            while( cbDataLen > 0 && szWrkData[cbDataLen-1] == ' ' )
+                szWrkData[--cbDataLen] = '\0';
+            
+            m_papszColValues[iCol] = strdup( szWrkData );
+        }
+    }
+
+    return TRUE;
+}
+
+/************************************************************************/
+/*                             GetColData()                             */
+/************************************************************************/
+
+const char *CPLODBCStatement::GetColData( int iCol )
+
+{
+    if( iCol < 0 || iCol >= m_nColCount )
+        return NULL;
+    else 
+        return m_papszColValues[iCol];
+}
+
+/************************************************************************/
+/*                          ClearColumnData()                           */
+/************************************************************************/
+
+void CPLODBCStatement::ClearColumnData()
+
+{
+    if( m_nColCount > 0 )
+    {
+        for( int iCol = 0; iCol < m_nColCount; iCol++ )
+        {
+            if( m_papszColValues[iCol] != NULL )
+            {
+                free( m_papszColValues[iCol] );
+                m_papszColValues[iCol] = NULL;
+            }
+        }
+    }
+}
+
+/************************************************************************/
+/*                               Failed()                               */
+/************************************************************************/
+
+int CPLODBCStatement::Failed( int nResultCode )
+
+{
+    if( m_poSession != NULL )
+        return m_poSession->Failed( nResultCode, m_hStmt );
+
+    return TRUE;
+}
+
+/************************************************************************/
+/*                         Append(const char *)                         */
+/************************************************************************/
+
+void CPLODBCStatement::Append( const char *pszText )
+
+{
+    int  nTextLen = strlen(pszText);
+
+    if( m_nStatementMax < m_nStatementLen + nTextLen + 1 )
+    {
+        m_nStatementMax = (m_nStatementLen + nTextLen) * 2 + 100;
+        if( m_pszStatement == NULL )
+        {
+            m_pszStatement = (char *) malloc(m_nStatementMax);
+            m_pszStatement[0] = '\0';
+        }
+        else
+        {
+            m_pszStatement = (char *) realloc(m_pszStatement, m_nStatementMax);
+        }
+    }
+
+    strcpy( m_pszStatement + m_nStatementLen, pszText );
+    m_nStatementLen += nTextLen;
+}
+
+/************************************************************************/
+/*                             Append(int)                              */
+/************************************************************************/
+
+void CPLODBCStatement::Append( int nValue )
+
+{
+    char szFormattedValue[100];
+
+    sprintf( szFormattedValue, "%d", nValue );
+    Append( szFormattedValue );
+}
+
+/************************************************************************/
+/*                            Append(double)                            */
+/************************************************************************/
+
+void CPLODBCStatement::Append( double dfValue )
+
+{
+    char szFormattedValue[100];
+
+    sprintf( szFormattedValue, "%24g", dfValue );
+    Append( szFormattedValue );
+}
+
+/************************************************************************/
+/*                              Appendf()                               */
+/************************************************************************/
+
+int CPLODBCStatement::Appendf( const char *pszFormat, ... )
+
+{
+    va_list args;
+    char    szFormattedText[8000];
+    int     bSuccess;
+
+    va_start( args, pszFormat );
+#if defined(HAVE_VSNPRINTF)
+    bSuccess = vsnprintf( szFormattedText, sizeof(szFormattedText)-1, 
+                          pszFormat, args ) < (int) sizeof(szFormattedText)-1;
+#else
+    vsnprintf( szFormattedText, pszFormat, args );
+    bSuccess = TRUE;
+#endif
+    va_end( args );
+
+    if( bSuccess )
+        Append( szFormattedText );
+
+    return bSuccess;
+}
+                                
+/************************************************************************/
+/*                               Clear()                                */
+/************************************************************************/
+
+void CPLODBCStatement::Clear()
+
+{
+    if( m_pszStatement != NULL )
+        free( m_pszStatement );
+
+    m_nStatementLen = 0;
+    m_nStatementMax = 0;
+}
