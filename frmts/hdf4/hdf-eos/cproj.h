3883d8a49f66ec89b03550f7924e97bff0031814
Andrey Kiselev <dron@ak4719.spb.edu>
2004-06-19 21:35:08 +0000

    New.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@6245 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ frmts/hdf4/hdf-eos/cproj.h
@@ -0,0 +1,35 @@
+#include <math.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PI 	3.141592653589793238
+#define HALF_PI (PI*0.5)
+#define TWO_PI 	(PI*2.0)
+#define EPSLN	1.0e-10
+#define R2D     57.2957795131
+/*
+#define D2R     0.0174532925199
+*/
+#define D2R     1.745329251994328e-2
+#define S2R	4.848136811095359e-6
+
+#define OK	0
+#define ERROR  -1
+#define IN_BREAK -2
+
+/* Misc macros
+  -----------*/
+#define SQUARE(x)       x * x   /* x**2 */
+#define CUBE(x)     x * x * x   /* x**3 */
+#define QUAD(x) x * x * x * x   /* x**4 */
+
+#define GMAX(A, B)      ((A) > (B) ? (A) : (B)) /* assign maximum of a and b */
+#define GMIN(A, B)      ((A) < (B) ? (A) : (B)) /* assign minimum of a and b */
+
+#define IMOD(A, B)      (A) - (((A) / (B)) * (B)) /* Integer mod function */
+
+#ifdef __cplusplus
+}
+#endif
