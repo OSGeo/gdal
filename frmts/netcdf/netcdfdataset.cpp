5877e70a5ae22c01052fb752539eea11ee2d8433
Frank Warmerdam <warmerdam@pobox.com>
2004-10-14 13:59:13 +0000

    Added error for non-GMT netCDF files.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@6666 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/netcdf/netcdfdataset.cpp
+++ frmts/netcdf/netcdfdataset.cpp
@@ -28,6 +28,9 @@
  ******************************************************************************
  * 
  * $Log$
+ * Revision 1.3  2004/10/14 13:59:13  fwarmerdam
+ * Added error for non-GMT netCDF files.
+ *
  * Revision 1.2  2004/01/07 21:02:19  warmerda
  * fix up driver metadata
  *
@@ -234,6 +237,9 @@ GDALDataset *netCDFDataset::Open( GDALOpenInfo * poOpenInfo )
     if( nc_inq_varid( cdfid, "dimension", &nm_id ) != NC_NOERR 
         || nc_inq_varid( cdfid, "z", &z_id ) != NC_NOERR )
     {
+        CPLError( CE_Warning, CE_AppDefined, 
+                  "%s is a netCDF file, but not in GMT configuration.",
+                  poOpenInfo->pszFilename );
         nc_close( cdfid );
         return NULL;
     }
