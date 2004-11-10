b69c19fe9c484490224b844aa4c3a3c583cec0cf
k.dejong <k.dejong@geo.uu.nl>
2004-11-10 10:09:19 +0000

    Initial versions. Read only driver.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@6760 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ frmts/pcraster/pcrasterrasterband.h
@@ -0,0 +1,95 @@
+#ifndef INCLUDED_PCRASTERRASTERBAND
+#define INCLUDED_PCRASTERRASTERBAND
+
+
+
+// Library headers.
+
+// PCRaster library headers.
+
+// Module headers.
+#ifndef INCLUDED_GDAL_PRIV
+#include "gdal_priv.h"
+#define INCLUDED_GDAL_PRIV
+#endif
+
+
+
+// namespace {
+  // PCRasterRasterBand declarations.
+// }
+class PCRasterDataset;
+
+
+
+// namespace {
+
+
+
+//! This class specialises the GDALRasterBand class for PCRaster rasters.
+/*!
+*/
+class PCRasterRasterBand: public GDALRasterBand
+{
+
+private:
+
+  PCRasterDataset const* d_dataset;
+
+  //! Assignment operator. NOT IMPLEMENTED.
+  PCRasterRasterBand& operator=        (const PCRasterRasterBand&);
+
+  //! Copy constructor. NOT IMPLEMENTED.
+                   PCRasterRasterBand  (const PCRasterRasterBand&);
+
+protected:
+
+  double           GetNoDataValue      (int* success);
+
+public:
+
+  //----------------------------------------------------------------------------
+  // CREATORS
+  //----------------------------------------------------------------------------
+
+                   PCRasterRasterBand  (PCRasterDataset* dataset);
+
+  /* virtual */    ~PCRasterRasterBand ();
+
+  //----------------------------------------------------------------------------
+  // MANIPULATORS
+  //----------------------------------------------------------------------------
+
+  //----------------------------------------------------------------------------
+  // ACCESSORS
+  //----------------------------------------------------------------------------
+
+  CPLErr           IReadBlock          (int nBlockXoff,
+                                        int nBlockYoff,
+                                        void* buffer);
+
+};
+
+
+
+//------------------------------------------------------------------------------
+// INLINE FUNCTIONS
+//------------------------------------------------------------------------------
+
+
+
+//------------------------------------------------------------------------------
+// FREE OPERATORS
+//------------------------------------------------------------------------------
+
+
+
+//------------------------------------------------------------------------------
+// FREE FUNCTIONS
+//------------------------------------------------------------------------------
+
+
+
+// } // namespace
+
+#endif
