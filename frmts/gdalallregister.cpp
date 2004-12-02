05163a7f601f10d5eb6c02dec29faa8abcfcf154
Frank Warmerdam <warmerdam@pobox.com>
2004-12-02 15:58:58 +0000

    temporarily disable ilwis driver
    git-svn-id: https://svn.osgeo.org/gdal/trunk@6841 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/gdalallregister.cpp
+++ frmts/gdalallregister.cpp
@@ -25,6 +25,9 @@
  * Main format registration function.
  * 
  * $Log$
+ * Revision 1.71  2004/12/02 15:58:58  fwarmerdam
+ * temporarily disable ilwis driver
+ *
  * Revision 1.70  2004/11/30 17:02:37  lichun
  * Added ILWIS driver support
  *
@@ -393,7 +396,7 @@ void GDALAllRegister()
 #endif
 
 #ifdef FRMT_ilwis
-    GDALRegister_ILWIS();
+//    GDALRegister_ILWIS();
 #endif
 
 #ifdef FRMT_netcdf
