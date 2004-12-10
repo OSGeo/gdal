0d124566cccfde46559e56fcbff53058aeafb074
Frank Warmerdam <warmerdam@pobox.com>
2004-12-10 21:25:57 +0000

    added ICHIPB reader
    git-svn-id: https://svn.osgeo.org/gdal/trunk@6863 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/nitf/nitfimage.c
+++ frmts/nitf/nitfimage.c
@@ -29,6 +29,9 @@
  ******************************************************************************
  *
  * $Log$
+ * Revision 1.28  2004/12/10 21:25:57  fwarmerdam
+ * added ICHIPB reader
+ *
  * Revision 1.27  2004/05/06 14:58:06  warmerda
  * added USE00A and STDIDC parsing and reporting as metadata
  *
@@ -1492,12 +1495,23 @@ int NITFReadRPC00B( NITFImage *psImage, NITFRPC00BInfo *psRPC )
                            "RPC00B", NULL );
 
     if( pachTRE == NULL )
+	{
+		pachTRE = NITFFindTRE( psImage->pachTRE, psImage->nTREBytes,
+						"RPC00A", NULL );
+	}
+
+	if( pachTRE == NULL )
+	{
         return FALSE;
+	}
 
 /* -------------------------------------------------------------------- */
 /*      Parse out field values.                                         */
 /* -------------------------------------------------------------------- */
     psRPC->SUCCESS = atoi(NITFGetField(szTemp, pachTRE, 0, 1 ));
+	
+	if ( !psRPC->SUCCESS )
+	fprintf( stdout, "RPC Extension not Populated!\n");
 
     psRPC->ERR_BIAS = atof(NITFGetField(szTemp, pachTRE, 1, 7 ));
     psRPC->ERR_RAND = atof(NITFGetField(szTemp, pachTRE, 8, 7 ));
@@ -1530,6 +1544,82 @@ int NITFReadRPC00B( NITFImage *psImage, NITFRPC00BInfo *psRPC )
     }
 
     return TRUE;
+}
+
+/************************************************************************/
+/*                           NITFReadICHIPB()                           */
+/*                                                                      */
+/*      Read an ICHIPB structure if the TRE is available.               */
+/************************************************************************/
+
+int NITFReadICHIPB( NITFImage *psImage, NITFICHIPBInfo *psICHIP )
+
+{
+    const char *pachTRE;
+    char szTemp[32];
+    int  i;
+
+/* -------------------------------------------------------------------- */
+/*      Do we have the TRE?                                             */
+/* -------------------------------------------------------------------- */
+    pachTRE = NITFFindTRE( psImage->pachTRE, psImage->nTREBytes, 
+                           "ICHIPB", NULL );
+
+    if( pachTRE == NULL )
+	{
+		pachTRE = NITFFindTRE( psImage->pachTRE, psImage->nTREBytes,
+						"ICHIPA", NULL );
+	}
+
+	if( pachTRE == NULL )
+	{
+        return FALSE;
+	}
+
+/* -------------------------------------------------------------------- */
+/*      Parse out field values.                                         */
+/* -------------------------------------------------------------------- */
+	psICHIP->XFRM_FLAG = atoi(NITFGetField(szTemp, pachTRE, 0, 2 ));
+
+	if ( psICHIP->XFRM_FLAG == 0 )
+	{
+		psICHIP->SCALE_FACTOR = atof(NITFGetField(szTemp, pachTRE, 2, 10 ));
+		psICHIP->ANAMORPH_CORR = atoi(NITFGetField(szTemp, pachTRE, 12, 2 ));
+		psICHIP->SCANBLK_NUM = atoi(NITFGetField(szTemp, pachTRE, 14, 2 ));
+
+		psICHIP->OP_ROW_11 = atof(NITFGetField(szTemp, pachTRE, 16, 12 ));
+		psICHIP->OP_COL_11 = atof(NITFGetField(szTemp, pachTRE, 28, 12 ));
+
+		psICHIP->OP_ROW_12 = atof(NITFGetField(szTemp, pachTRE, 40, 12 ));
+		psICHIP->OP_COL_12 = atof(NITFGetField(szTemp, pachTRE, 52, 12 ));
+
+		psICHIP->OP_ROW_21 = atof(NITFGetField(szTemp, pachTRE, 64, 12 ));
+		psICHIP->OP_COL_21 = atof(NITFGetField(szTemp, pachTRE, 76, 12 ));
+
+		psICHIP->OP_ROW_22 = atof(NITFGetField(szTemp, pachTRE, 88, 12 ));
+		psICHIP->OP_COL_22 = atof(NITFGetField(szTemp, pachTRE, 100, 12 ));
+
+		psICHIP->FI_ROW_11 = atof(NITFGetField(szTemp, pachTRE, 112, 12 ));
+		psICHIP->FI_COL_11 = atof(NITFGetField(szTemp, pachTRE, 124, 12 ));
+
+		psICHIP->FI_ROW_12 = atof(NITFGetField(szTemp, pachTRE, 136, 12 ));
+		psICHIP->FI_COL_12 = atof(NITFGetField(szTemp, pachTRE, 148, 12 ));
+
+		psICHIP->FI_ROW_21 = atof(NITFGetField(szTemp, pachTRE, 160, 12 ));
+		psICHIP->FI_COL_21 = atof(NITFGetField(szTemp, pachTRE, 172, 12 ));
+
+		psICHIP->FI_ROW_22 = atof(NITFGetField(szTemp, pachTRE, 184, 12 ));
+		psICHIP->FI_COL_22 = atof(NITFGetField(szTemp, pachTRE, 196, 12 ));
+
+		psICHIP->FI_ROW = atoi(NITFGetField(szTemp, pachTRE, 208, 8 ));
+		psICHIP->FI_COL = atoi(NITFGetField(szTemp, pachTRE, 216, 8 ));
+	}
+	else
+	{
+		fprintf( stdout, "Chip is already de-warpped?\n" );
+	}
+
+	return TRUE;
 }
 
 /************************************************************************/
