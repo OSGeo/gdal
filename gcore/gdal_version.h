1350dfa0161ca3b2e635f073a3b7b6f89292a764
Frank Warmerdam <warmerdam@pobox.com>
2005-02-23 14:53:18 +0000

    New
    git-svn-id: https://svn.osgeo.org/gdal/trunk@7213 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ gcore/gdal_version.h
@@ -0,0 +1,23 @@
+
+/* -------------------------------------------------------------------- */
+/*      GDAL Version Information.                                       */
+/* -------------------------------------------------------------------- */
+
+#ifndef GDAL_VERSION_MAJOR
+#  define GDAL_VERSION_MAJOR    1
+#  define GDAL_VERSION_MINOR    2
+#  define GDAL_VERSION_REV      5
+#  define GDAL_VERSION_BUILD    1
+#endif
+
+#ifndef GDAL_VERSION_NUM
+#  define GDAL_VERSION_NUM      (GDAL_VERSION_MAJOR*1000+GDAL_VERSION_MINOR*100+GDAL_VERSION_REV*10+GDAL_VERSION_BUILD)
+#endif
+
+#ifndef GDAL_RELEASE_DATE
+#  define GDAL_RELEASE_DATE     20041122
+#endif
+#ifndef GDAL_RELEASE_NAME
+#  define GDAL_RELEASE_NAME     "1.2.5.1"
+#endif
+
