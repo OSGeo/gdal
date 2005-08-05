9f3d27b5d5962abdeafdac8ef59c0a4a92ba130b
Frank Warmerdam <warmerdam@pobox.com>
2005-08-05 20:35:25 +0000

    Update version info to 1.3.0.0b1"
    git-svn-id: https://svn.osgeo.org/gdal/trunk@8021 f0d54148-0727-0410-94bb-9a71ac55c965

--- gcore/gdal_version.h
+++ gcore/gdal_version.h
@@ -5,8 +5,8 @@
 
 #ifndef GDAL_VERSION_MAJOR
 #  define GDAL_VERSION_MAJOR    1
-#  define GDAL_VERSION_MINOR    2
-#  define GDAL_VERSION_REV      6
+#  define GDAL_VERSION_MINOR    3
+#  define GDAL_VERSION_REV      0
 #  define GDAL_VERSION_BUILD    0
 #endif
 
@@ -15,9 +15,9 @@
 #endif
 
 #ifndef GDAL_RELEASE_DATE
-#  define GDAL_RELEASE_DATE     20050313
+#  define GDAL_RELEASE_DATE     20050805
 #endif
 #ifndef GDAL_RELEASE_NAME
-#  define GDAL_RELEASE_NAME     "1.2.6.0"
+#  define GDAL_RELEASE_NAME     "1.3.0.0b1"
 #endif
 
