92a9f9e695f820fef2458ee41e0278554f46c4c0
Kevin Ruland <kruland@ku.edu>
2005-09-27 14:35:11 +0000

    Updated pod and test.pl from Ari.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@8459 f0d54148-0727-0410-94bb-9a71ac55c965

--- swig/perl/test.pl
+++ swig/perl/test.pl
@@ -1,7 +1,7 @@
 BEGIN { $| = 1; }
 END {print "not ok 1\n" unless $loaded;}
-use gdalconst;
 use gdal;
+use gdalconst;
 use osr;
 use ogr;
 $loaded = 1;
@@ -15,22 +15,32 @@ $verbose = 0;
 #   Get/SetGeoTransform
 #   Get/SetNoDataValue
 #   Colortable operations
+#   WriteRaster
 #   Open dataset
+#   ReadRaster
 #
 # for all OGR drivers:
 #   Create datasource
 #   Create layer
-#   Schema test
-#   Get/SetGeometry test
+#   Create field
+#   Create geometry
+#   Open layer
+#   Open field
+#   Open geom
 #
 # if verbose = 1, all operations (skip,fail,ok) are printed out
 
-gdal::UseExceptions();
-
-system "rm -rf tmp_ds_*";
+#system "rm -rf tmp_ds_*";
 
 @types = ('GDT_Byte','GDT_UInt16','GDT_Int16','GDT_UInt32','GDT_Int32',
 	  'GDT_Float32','GDT_Float64','GDT_CInt16','GDT_CInt32','GDT_CFloat32','GDT_CFloat64');
+
+%pack_types = ('GDT_Byte'=>'c',
+	       'GDT_Int16'=>'s',
+	       'GDT_Int32'=>'i',
+	       'GDT_Float32'=>'f',
+	       'GDT_Float64'=>'d',
+	       );
 
 %types;
 
@@ -52,6 +62,8 @@ sub dumphash {
 		   'MFF','MFF2','FujiBAS','GSC','FAST','BT','LAN','CPG','IDA',
 		   'NDF','L1B','FIT','RMF','USGSDEM','GXF');
 
+#@tested_drivers = ('GTiff');
+
 $n = gdal::GetDriverCount();
 
 for $i (@tested_drivers) {
@@ -62,41 +74,40 @@ for $i (@tested_drivers) {
 #    print "$driver->{ShortName}\n";
 #    next;
 
-    print "---->",$driver->{LongName}," ($driver->{ShortName})\n" if $verbose;
+    $name = $driver->{ShortName};
     
-    $metadata = $driver->GetMetadata();
-
+    my $metadata = $driver->GetMetadata();
+    
     unless ($metadata->{DCAP_CREATE} eq 'YES') {
-	print "Create dataset skipped because no create capability announced.\n" if $verbose;
+	mytest('skipped: no capability',undef,$name,'dataset create');
 	next;
     }
-
+    
     @create = split /\s+/,$metadata->{DMD_CREATIONDATATYPES};
     
     @create = ('Byte','Float32','UInt16',
 	       'Int16','CInt16','CInt32','CFloat32') if $driver->{ShortName} eq 'MFF2';
-
+    
     $ext = '.'.$metadata->{DMD_EXTENSION};
 
     unless (@create) {
-	print "Create dataset skipped because no creation datatypes announced.\n" if $verbose;
+	mytest('skipped: no creation datatypes',undef,$name,'dataset create');
 	next;
     }
 
     if ($driver->{ShortName} eq 'PAux') {
-	print "Create dataset skipped because of ?.\n" if $verbose;
+	mytest('skipped: PAux',undef,$name,'dataset create');
 	next;
     }
 
     for $type (@create) {
 
 	if (($type eq 'CInt32') and ('MFF2' eq $driver->{ShortName})) {
-	    print "Create dataset skipped because causes a fatal error." if $verbose;
+	    mytest('skipped: will cause a fatal error',undef,$name,$type,'dataset create');
 	    next;
 	}
 
 	$typenr = $types{'GDT_'.$type};
-	print "$type $typenr\n" if $verbose;
 
 	$ext = '' if $driver->{ShortName} eq 'ILWIS';
 
@@ -109,22 +120,11 @@ for $i (@tested_drivers) {
 	eval {
 	    $dataset = $driver->Create($filename, $width, $height, $bands , $typenr, []);
 	};
-	$err = "without an error message.";
-	if ($@) {
-	    $@ =~ s/\n/ /g;
-	    $@ =~ s/\s+$//;
-	    $@ =~ s/\s+/ /g;
-	    $@ =~ s/^\s+$//;
-	    $err = $@ ? "with error:\n'$@'" : "without an error message.";
-	}
+	mytest($dataset,'no error message',$name,$type,'dataset create');
+	next unless $dataset;
 
-	unless ($dataset) {
-	    push @fails,"Creation of $type $driver->{LongName} raster dataset failed, $err\n";
-	    print "Create dataset failed, error: $@\n" if $verbose;
-	    next;
-	} else {
-	    print "Create dataset ok.\n" if $verbose;
-	}
+	mytest($dataset->{RasterXSize} == $width,'RasterXSize',$name,$type,'RasterXSize');
+	mytest($dataset->{RasterYSize} == $height,'RasterYSize',$name,$type,'RasterYSize');
 
 	my $band = $dataset->GetRasterBand(1);
 	
@@ -136,20 +136,15 @@ for $i (@tested_drivers) {
 	    $driver->{ShortName} ne 'BMP' and
 	    $driver->{ShortName} ne 'EHdr') 
 	{
-	    $transform = $dataset->GetGeoTransform();
+	    my $transform = $dataset->GetGeoTransform();
 	    $transform->[5] = 12;
 	    $dataset->SetGeoTransform($transform);
-	    $transform2 = $dataset->GetGeoTransform();
-	    if ($transform->[5] != $transform2->[5]) {
-		$msg = "Get/SetGeoTransform for $type $driver->{LongName} failed.\n";
-		push @fails,$msg;
-	    } else {
-		$msg = "Get/SetGeoTransform ok\n";
-	    }
+	    my $transform2 = $dataset->GetGeoTransform();
+	    mytest($transform->[5] == $transform2->[5],
+		   "$transform->[5] != $transform2->[5]",$name,$type,'Get/SetGeoTransform');
 	} else {
-	    $msg = "Get/SetGeoTransform skipped\n";
+	    mytest('skipped',undef,$name,$type,'Get/SetGeoTransform');
 	}
-	print $msg if $verbose;
 	
 	if ($driver->{ShortName} ne 'NITF' and 
 	    $driver->{ShortName} ne 'HFA' and
@@ -161,17 +156,11 @@ for $i (@tested_drivers) {
 	    $driver->{ShortName} ne 'RMF') 
 	{
 	    $band->SetNoDataValue(5.5);
-	    $value = $band->GetNoDataValue;
-	    if ($value != 5.5) {
-		$msg = "Get/SetNoDataValue for $type $driver->{LongName} failed.\n";
-		push @fails,$msg;
-	    } else {
-		$msg = "Get/SetNoDataValue ok\n";
-	    }
+	    my $value = $band->GetNoDataValue;
+	    mytest($value == 5.5,"$value != 5.5",$name,$type,'Get/SetNoDataValue');
 	} else {
-	    $msg = "Get/SetNoDataValue skipped.\n";
+	    mytest('skipped',undef,$name,$type,'Get/SetNoDataValue');
 	}
-	print $msg if $verbose;
 	
 	if (($driver->{ShortName} eq 'GTiff' and ($type ne 'Byte' or $type ne 'UInt16')) or
 	    ($driver->{ShortName} eq 'ELAS') or
@@ -181,7 +170,7 @@ for $i (@tested_drivers) {
 	    ($driver->{ShortName} eq 'RMF') or
 	    ($driver->{ShortName} eq 'NITF'))
 	{
-	    $msg = "Colortable operations skipped.\n";
+	    mytest('skipped',undef,$name,$type,'Colortable');
 	} else {
 	    my $colortable = new gdal::ColorTable();
 	    @rgba = (255,0,0,255);
@@ -189,18 +178,29 @@ for $i (@tested_drivers) {
 	    $band->SetRasterColorTable($colortable);
 	    $colortable = $band->GetRasterColorTable;
 	    @rgba2 = $colortable->GetColorEntry(0);
-	    
-	    if ($rgb[0] != $rgb2[0] or 
-		$rgb[1] != $rgb2[1] or 
-		$rgb[2] != $rgb2[2] or 
-		$rgb[3] != $rgb2[3]) {
-		$msg = "Colortable operation for $type $driver->{LongName} failed.\n";
-		push @fails,$msg;
-	    } else {
-		$msg = "Colortable operations ok.\n";
-	    }		
+
+	    mytest($rgb[0] == $rgb2[0] and
+		    $rgb[1] == $rgb2[1] and
+		    $rgb[2] == $rgb2[2] and
+		    $rgb[3] == $rgb2[3],"colors do not match",$name,$type,'Colortable');
 	}
-	print $msg if $verbose;
+
+	my $pc = $pack_types{"GDT_$type"};
+	if ($pc and $driver->{ShortName} ne 'VRT') {
+
+	    $pc = "${pc}[$width]";
+	    $scanline = pack($pc,(1..$width));
+
+	    for my $yoff (0..$height-1) {
+		$band->WriteRaster( 0, $yoff, $width, 1, $scanline );
+	    }
+
+	} else {
+	    mytest('skipped',undef,$name,$type,'WriteRaster');
+	}
+
+	undef $band;
+	undef $dataset;
 	
 	if ($driver->{ShortName} eq 'VRT' or 
 	    $driver->{ShortName} eq 'MEM' or 
@@ -208,8 +208,7 @@ for $i (@tested_drivers) {
 	    ($driver->{ShortName} eq 'MFF2' and 
 	     ($type eq 'Int32' or $type eq 'Float64' or $type eq 'CFloat64')))
 	{
-	    $msg = "Open dataset skipped.\n";
-	    
+	    mytest('skipped',undef,$name,$type,'open');
 	} else {
 	    
 	    $ext = '.'.$metadata->{DMD_EXTENSION};
@@ -218,25 +217,35 @@ for $i (@tested_drivers) {
 	    eval {
 		$dataset = gdal::Open($filename);
 	    };
-	    if ($@) {
-		$@ =~ s/\n/ /g;
-		$@ =~ s/\s+$//;
-		$@ =~ s/\s+/ /g;
+	    mytest($dataset,'no message',$name,$type,'open');
+
+	    if ($dataset) {
+		mytest($dataset->{RasterXSize} == $width,'RasterXSize',$name,$type,'RasterXSize');
+		mytest($dataset->{RasterYSize} == $height,'RasterYSize',$name,$type,'RasterYSize');
+
+		my $band = $dataset->GetRasterBand(1);
+
+		if ($pc) {
+
+		    $scanline = $band->ReadRaster( 0, 0, $width, 1);
+		    my @data = unpack($pc, $scanline);
+
+		    mytest($data[49] == 50,'',$name,$type,'ReadRaster');
+
+		}
+		
 	    }
-	    
-	    $msg = $dataset ? "open dataset ok\n" : "open dataset failed, error: $@\n";
 	    
 	    undef $dataset;
 	}
-	print $msg if $verbose;
-	
-	undef $dataset;
 
     }
 }
 
 @types = ('wkbUnknown','wkbPoint','wkbLineString','wkbPolygon',
 	  'wkbMultiPoint','wkbMultiLineString','wkbMultiPolygon','wkbGeometryCollection');
+
+#@types = ();
 
 %types = ();
 
@@ -245,121 +254,199 @@ for (@types) {$types{$_} = eval "\$ogr::$_"};
 for $i (0..ogr::GetDriverCount()-1) {
     my $driver = ogr::GetDriver($i);
     $name = $driver->{name};
-    print "---->","$name\n" if $verbose;
 
     if (!$driver->TestCapability($ogr::ODrCCreateDataSource)) {
-	print "Create datasource skipped because no capability.\n" if $verbose;
+	mytest('skipped: no capability',undef,$name,'datasource create');
 	next;
     }
 
-    if ($driver->{name} eq 'S57' or $driver->{name} eq 'CSV' or $driver->{name} eq 'GML') {
-	print "Create datasource skipped because really no capability?\n" if $verbose;
+    if ($name eq 'S57' or $name eq 'CSV' or $name eq 'GML' or $name eq 'PostgreSQL') {
+	mytest('skipped: apparently no capability',undef,$name,'datasource create');
 	next;
     }
 
-    if ($driver->{name} eq 'TIGER' or $driver->{name} eq 'DGN') {
-	print "Create datasource skipped because layer creations will fail.\n" if $verbose;
+    if ($name eq 'TIGER' or $name eq 'DGN') {
+	mytest("skipped: can't create layers afterwards.",undef,$name,'datasource create');
 	next;
     }
 
-    $dir = "tmp_ds_$i";
-    system "mkdir $dir";
+    @field_types = ('OFTInteger','OFTIntegerList','OFTReal','OFTRealList','OFTString',
+		    'OFTStringList','OFTWideString','OFTWideStringList','OFTBinary');
+    
+    if ($name eq 'ESRI Shapefile') {
+	@field_types = ('OFTInteger','OFTReal','OFTString','OFTInteger');
+    } elsif ($name eq 'MapInfo File') {
+	@field_types = ('OFTInteger','OFTReal','OFTString');
+    }
+    
+    %field_types;
+    for (@field_types) {$field_types{$_} = eval "\$ogr::$_"};
 
+    $dir0 = $name;
+    $dir0 =~ s/ //g;
+    $dir = "tmp_ds_$dir0";
+    system "mkdir $dir" unless $name eq 'Memory';
+    
     eval {
 	$datasource = $driver->CreateDataSource($dir);
     };
-    $err = "without an error message.";
-    if ($@) {
-	$@ =~ s/\n/ /g;
-	$@ =~ s/\s+$//;
-	$@ =~ s/\s+/ /g;
-	$@ =~ s/^\s+$//;
-	$err = $@ ? "with error:\n'$@'" : "without an error message.";
-    }
-
-    unless ($datasource) {
-	push @fails,"Creation of datasource for $driver->{name} failed $err\n";
-	print "Create datasource failed, $err\n" if $verbose;
-	next;
-    } else {
-	print "Create datasource ok.\n" if $verbose;
-    }
+    $msg = mytest($datasource,'no message',$name,'datasource create');
     
-    for $type (keys %types) {
-	print "$type\n" if $verbose;
+    next unless $datasource;
+    
+    for $type (@types) {
+	
+	if ($name eq 'ESRI Shapefile' and $type eq 'wkbGeometryCollection') {
+	    mytest("skipped, will fail",undef,$name,$type,'layer create');
+	    next;
+	}
 
-	if ($driver->{name} eq 'ESRI Shapefile' and $type eq 'wkbGeometryCollection') {
-	    print "Create layer skipped because it will fail.\n" if $verbose;
+	if ($type eq 'wkbMultiPolygon') {
+	    mytest("skipped, no test yet",undef,$name,$type,'layer create');
 	    next;
 	}
 
 	eval {
 	    $layer = $datasource->CreateLayer($type, undef, $types{$type});
 	};
-	$err = "without an error message.";
-	if ($@) {
-	    $@ =~ s/\n/ /g;
-	    $@ =~ s/\s+$//;
-	    $@ =~ s/\s+/ /g;
-	    $@ =~ s/^\s+$//;
-	    $err = $@ ? "with error:\n'$@'" : "without an error message.";   
-	}
-	unless ($layer) {
-	    push @fails,"Creation of $type layer for $driver->{name} failed $err\n";
-	    print "Create layer failed, $err\n" if $verbose;
-	    next;
-	} else {
-	    print "Create layer ok.\n" if $verbose;
+	$msg = mytest($layer,'no message',$name,$type,'layer create');
+	
+	next unless $layer;
+
+	# create one field of each type
+
+	for $ft (@field_types) {
+
+	    my $column = new ogr::FieldDefn($ft, $field_types{$ft});
+
+	    $layer->CreateField($column);
+
 	}
 
-	my $column = new ogr::FieldDefn('testing', $ogr::OFTInteger);
+	{
+	    my $schema = $layer->GetLayerDefn();
 
-	$layer->CreateField($column);
+	    $i = 0;
+	    for $ft (@field_types) {
 
-	my $schema = $layer->GetLayerDefn();
+		my $column = $schema->GetFieldDefn($i++);
+		my $n = $column->GetName;
+		mytest($n eq $ft,"$n ne $ft",$name,$type,$ft,'field create');
 
-	my $column2 = $schema->GetFieldDefn(0);
-
-	if ($column2->GetName ne 'testing') {
-	    $msg = "Schema test $type $driver->{name} failed.\n";
-	    push @fails,$msg;
-	} else {
-	    $msg = "Schema test ok.\n";
-	}
-	print $msg if $verbose;
-
-	$t = $type eq 'wkbUnknown' ? $ogr::wkbPolygon : $types{$type};
-
-	my $geom = new ogr::Geometry($t);
-
-#	$geom->AddPoint(1,1);
-
-	my $feature = new ogr::Feature($schema);
-	$feature->SetGeometry($geom);
-
-	my $geom2 = $feature->GetGeometryRef();
-
-	if ($type eq 'wkbPoint') {
-	    $msg = "Get/SetGeometry test skipped for $type.\n";
-	} else {
-	    $t2 = $geom2->GetGeometryType;
-	    if ($t2 != $t) {
-		$msg = "Get/SetGeometry test $type $driver->{name} failed ($t vs. $t2).\n";
-		push @fails,$msg;
-	    } else {
-		$msg = "Get/SetGeometry test ok.\n";
 	    }
+
+	    my $feature = new ogr::Feature($schema);
+	    
+	    $t = $type eq 'wkbUnknown' ? $ogr::wkbPolygon : $types{$type};
+	    
+	    my $geom = new ogr::Geometry($t);
+	    my $r;
+
+	    if ($type eq 'wkbPoint' or $type eq 'wkbLineString') {
+		$geom->AddPoint(1,1);
+		$geom->AddPoint(2,2);
+	    } elsif ($type eq 'wkbUnknown' or $type eq 'wkbPolygon') {
+		$r = new ogr::Geometry($ogr::wkbLinearRing);
+		$r->AddPoint(0,0);
+		$r->AddPoint(0,1);
+		$r->AddPoint(1,1);
+		$r->AddPoint(1,0);
+		$geom->AddGeometry($r);
+		$geom->CloseRings;
+	    } else {
+#		print "what to add to $type\n";
+		mytest('skipped',undef,$name,$type,'geom create');
+	    }
+	    
+	    $feature->SetGeometry($geom);
+
+	    $i = 0;
+	    for $ft (@field_types) {
+		$feature->SetField($i++,2);
+	    }
+
+	    $layer->CreateFeature($feature);
+	    $layer->SyncToDisk;
+
 	}
-	print $msg if $verbose;
+
+	undef $layer;
+
+	# now open
+
+	if ($name eq 'Memory')
+	{
+	    mytest('skipped',undef,$name,$type,'layer open');
+	    
+	} else {
+
+	    undef $datasource;
+
+	    eval {
+		if ($name eq 'MapInfo File') {
+		    $datasource = ogr::Open("$dir/$type.tab");
+		    $layer = $datasource->GetLayerByIndex;
+		} else {
+		    $datasource = $driver->CreateDataSource($dir);
+		    $layer = $datasource->GetLayerByName($type);
+		}
+	    };
+
+	    $msg = mytest($layer,'no message',$name,$type,"layer $type open");
+	    next unless $layer;
+
+	    # check to see if the fields exist and the types are the same
+
+	    my $schema = $layer->GetLayerDefn();
+
+	    $i = 0;
+	    for $ft (@field_types) {
+		my $column = $schema->GetFieldDefn($i++);
+		my $n = $column->GetName;
+		mytest($n eq $ft,"$n ne $ft",$name,$type,$ft,'field open (name)');
+		my $t = $column->GetType;
+		my $t2 = $field_types{$ft};
+		mytest($t == $t2,"$t != $t2",$name,$type,$ft,'field open (type)');
+	    }
+
+	    if ($type eq 'wkbPoint' or $type eq 'wkbLineString' or $type eq 'wkbPolygon') {
+
+		$layer->ResetReading;
+#		my $feature = $layer->GetFeature(0);
+		my $feature = $layer->GetNextFeature;
+		
+		mytest($feature,'getFeature failed',$name,$type,'feature open');
+		
+		if ($feature) {
+		    
+		    my $geom = $feature->GetGeometryRef();
+		    
+		    if ($type eq 'wkbPointlll') {
+			mytest('skipped',undef,$name,$type,'geom open');
+		    } else {
+			$t = $type eq 'wkbUnknown' ? $ogr::wkbPolygon : $types{$type};
+			$t2 = $geom->GetGeometryType;
+			mytest($t == $t2,"$t != $t2",$name,$type,'geom open');
+		    }
+		}
+	    } else {
+		mytest('skipped',undef,$name,$type,'feature open');
+	    }
+	    
+	    undef $layer;
+	}
 
     }
-
-    undef $layer;
+    
     undef $datasource;
+
+#    $driver->DeleteDataSource($dir); #  <- Delete the data source 
+
 }
 
 if (@fails) {
     print "unexpected failures:\n",@fails;
+    print "all other tests ok.\n";
 } else {
     print "all tests ok.\n";
 }
@@ -367,3 +454,26 @@ if (@fails) {
 system "rm -rf tmp_ds_*";
 
 
+sub mytest {
+    my $test = shift;
+    my $msg = shift;
+    my $context = join(': ',@_);
+    unless ($test) {
+	my $err = $msg;
+	if ($@) {
+	    $@ =~ s/\n/ /g;
+	    $@ =~ s/\s+$//;
+	    $@ =~ s/\s+/ /g;
+	    $@ =~ s/^\s+$//;
+	    $err = $@ ? "'$@'" : $msg;
+	}
+	$msg = "$context: $err: not ok\n";
+	push @fails,$msg;
+    } elsif ($test =~ /^skip/) {
+	$msg = "$context: $test.\n";
+    } else {
+	$msg = "$context: ok.\n";
+    }
+    print $msg if $verbose;
+    return $msg;
+}
