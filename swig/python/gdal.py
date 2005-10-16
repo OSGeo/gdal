d35e72bb6bcb8213cd149c18dbbb1e1abf2b324c
Howard Butler <hobu.inc@gmail.com>
2005-10-16 21:45:55 +0000

    A *very* cheesy and poor implementation of DataSource.GetDriver()
    git-svn-id: https://svn.osgeo.org/gdal/trunk@8647 f0d54148-0727-0410-94bb-9a71ac55c965

--- swig/python/gdal.py
+++ swig/python/gdal.py
@@ -1,4 +1,4 @@
-# This file was created automatically by SWIG 1.3.26.
+# This file was created automatically by SWIG 1.3.27.
 # Don't modify this file, modify the SWIG interface instead.
 
 import _gdal
