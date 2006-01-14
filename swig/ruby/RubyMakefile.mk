04b9b877ed0f645ae20dc5793fb4e4d09e1785e9
Charles Savageruby <cfis@savagexi.com>
2006-01-14 01:48:46 +0000

    Significant updates to make files so they more closely integrate with the gdal build system.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@9001 f0d54148-0727-0410-94bb-9a71ac55c965

--- swig/ruby/RubyMakefile.mk
+++ swig/ruby/RubyMakefile.mk
@@ -18,23 +18,15 @@ RUBY = ruby
 include $(GDAL_ROOT)/GDALmake.opt
 
 RUBY_MODULES = gdal.so ogr.so gdalconst.so osr.so
-RUBY_MAJOR_VERSION := $(shell ruby -rrbconfig -e "puts Config::CONFIG['MAJOR']")
-RUBY_MINOR_VERSION := $(shell ruby -rrbconfig -e "puts Config::CONFIG['MINOR']")
-RUBY_VERSION := $(RUBY_MAJOR_VERSION).$(RUBY_MINOR_VERSION)
+RUBY_INCLUDE_DIR := $(shell ruby -rrbconfig -e "puts Config::CONFIG['archdir']")
+RUBY_LIB_DIR := $(shell ruby -rrbconfig -e "puts Config::CONFIG['libdir']")
 RUBY_SO_NAME := $(shell ruby -rrbconfig -e "puts Config::CONFIG['RUBY_SO_NAME']")
+RUBY_EXTENSIONS_DIR := $(shell ruby -rrbconfig -e "puts Config::CONFIG['sitearchdir']")
+INSTALL_DIR := $(RUBY_EXTENSIONS_DIR)/gdal
 
-RUBY_DIR := $(shell ruby -rrbconfig -e "puts Config::TOPDIR")
-RUBY_ARCH := $(shell ruby -rrbconfig -e "puts Config::CONFIG['arch']")
-RUBY_ARCH_DIR := $(RUBY_DIR)/lib/ruby/$(RUBY_VERSION)/$(RUBY_ARCH)
-
-SITE_LIB_DIR := $(shell ruby -rrbconfig -e "puts Config::CONFIG['sitelibdir']")
-INSTALL_DIR := $(SITE_LIB_DIR)/$(RUBY_ARCH)/gdal
-
-GDAL_INCLUDE := -I../../port -I../../gcore -I../../alg -I../../ogr
-RUBY_INCLUDE := -I$(RUBY_ARCH_DIR)
-
-GDAL_LIB := -L$(GDAL_ROOT) -lgdal 
-RUBY_LIB := -shared -L$(RUBY_DIR)/lib -l$(RUBY_SO_NAME)
+RUBY_INCLUDE = -I$(RUBY_INCLUDE_DIR)
+LDFLAGS += -shared -L$(RUBY_LIB_DIR)
+RUBY_LIB := -l$(RUBY_SO_NAME)
 
 build: $(RUBY_MODULES)
 
@@ -50,10 +42,10 @@ $(INSTALL_DIR):
 	mkdir -v $(INSTALL_DIR)
 
 install: $(INSTALL_DIR)
-	cp *.so $(INSTALL_DIR)
+	$(INSTALL) $(RUBY_MODULES) $(INSTALL_DIR) 
 
 $(RUBY_MODULES): %.so: %_wrap.o
-	$(LD) $(LDFLAGS) $(LIBS) $(GDAL_LIB) $(RUBY_LIB) $< -o $@
+	$(LD) $(LDFLAGS) $(LIBS) $(GDAL_SLIB_LINK) $(RUBY_LIB) $< -o $@
 
 %.o: %.cpp
 	$(CXX) $(CFLAGS) $(GDAL_INCLUDE) $(RUBY_INCLUDE)  -c $<
