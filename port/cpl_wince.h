c68e700e58b7a77bb983d6e54e84fddcc9c850c7
Mateusz Loskot <mateusz@loskot.net>
2006-02-19 21:52:09 +0000

    [WINCE] Header for Windows CE specific stuff
    git-svn-id: https://svn.osgeo.org/gdal/trunk@9209 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ port/cpl_wince.h
@@ -0,0 +1,24 @@
+/**********************************************************************
+ * $Id$
+ *
+ * Name:     cpl_wince.h
+ * Project:  CPL - Common Portability Library
+ * Purpose:  Windows CE specifics
+ * Author:   Mateusz £oskot, mloskot@taxussi.com.pl
+ *
+ **********************************************************************/
+#ifndef _CPL_WINCE_H_INCLUDED
+#define _CPL_WINCE_H_INCLUDED
+
+#include "cpl_config.h"
+
+#if defined(WIN32CE)
+
+#include "cpl_conv.h"
+#include "windows.h"
+
+
+
+#endif /* #if defined(WIN32CE) */
+
+#endif /* #ifndef _CPL_WINCE_H_INCLUDED */
