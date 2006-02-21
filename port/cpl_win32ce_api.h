6c9e8074aea69b0eade49a1588d7fec5f670f6ec
Mateusz Loskot <mateusz@loskot.net>
2006-02-21 19:48:43 +0000

    [WCE] Added copyright note
    git-svn-id: https://svn.osgeo.org/gdal/trunk@9233 f0d54148-0727-0410-94bb-9a71ac55c965

--- port/cpl_win32ce_api.h
+++ port/cpl_win32ce_api.h
@@ -1,12 +1,41 @@
- /**********************************************************************
+/******************************************************************************
+ * Copyright (c) 2006, Mateusz £oskot
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ******************************************************************************
+ * $Id$
+ *
+ * Name:     cpl_win32ce_api.h
+ * Project:  CPL - Common Portability Library
+ * Purpose:  ASCII wrappers around only Unicode Windows CE API.
+ * Author:   Mateusz £oskot, mloskot@taxussi.com.pl
+ *
+ ******************************************************************************
  *
  * $Log$
+ * Revision 1.2  2006/02/21 19:48:43  mloskot
+ * [WCE] Added copyright note
+ *
  * Revision 1.1  2006/02/19 21:50:56  mloskot
  * [WCE] non-Unicode wrappers around Unicode-only Windows CE API
  *
- *
- *
- **********************************************************************/
+ ******************************************************************************/
 #ifndef _CPL_WINCEAPI_H_INCLUDED
 #define _CPL_WINCEAPI_H_INCLUDED    1
 
