9e74580a0abf743a68f262790d2f6557cd2fe5f1
Frank Warmerdam <warmerdam@pobox.com>
2006-03-29 03:04:15 +0000

    obsolete
    git-svn-id: https://svn.osgeo.org/gdal/trunk@9423 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/sfcom_serv/ocg_public.h
+++
@@ -1,7 +0,0 @@
-
-
-// This is the class factory for the OGRCOMGeometry server.
-// {b8f308f0-08a9-11d3-b3f9-0080c8e62564}
-//DEFINE_GUID(CLSID_OGRCOMClassFactory,
-//0xb8f308f0, 0x08a9, 0x11d3, 0xb3, 0xf9, 0x00, 0x80, 0xc8, 0xe6, 0x25, 0x64);
-
