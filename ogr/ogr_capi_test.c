1e391c7ab139d198d64d203370cc12393de0ca8d
Frank Warmerdam <warmerdam@pobox.com>
2006-03-29 00:59:39 +0000

    Added copyright message.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@9418 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/ogr_capi_test.c
+++ ogr/ogr_capi_test.c
@@ -1,6 +1,27 @@
 /**********************************************************************
  * $Id$
  **********************************************************************
+ * Copyright (c) 2003, Daniel Morissette
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ******************************************************************************
+ *
  *
  * Test the OGR C API (ogr_api.h)
  *
@@ -11,6 +32,9 @@
  *
  **********************************************************************
  * $Log$
+ * Revision 1.3  2006/03/29 00:59:39  fwarmerdam
+ * Added copyright message.
+ *
  * Revision 1.2  2003/02/19 03:43:11  danmo
  * Added cvs log, etc.
  *
