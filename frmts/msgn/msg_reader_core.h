b574f8106dbfd53ec81123a6113a8d4dca32878f
Frank Warmerdam <warmerdam@pobox.com>
2006-04-04 12:59:28 +0000

    added headers with explicit permission from Frans
    git-svn-id: https://svn.osgeo.org/gdal/trunk@9502 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/msgn/msg_reader_core.h
+++ frmts/msgn/msg_reader_core.h
@@ -1,6 +1,37 @@
-/* $Id$
-   Purpose: Base class for reading in the headers of MSG native images
-*/
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  MSG Native Reader
+ * Purpose:  Base class for reading in the headers of MSG native images
+ * Author:   Frans van den Bergh, fvdbergh@csir.co.za
+ *
+ ******************************************************************************
+ * Copyright (c) 2005, Frans van den Bergh <fvdbergh@csir.co.za>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ******************************************************************************
+ *
+ * $Log$
+ * Revision 1.2  2006/04/04 12:59:28  fwarmerdam
+ * added headers with explicit permission from Frans
+ *
+ */
 
 #ifndef MSG_READER_CORE_H
 #define MSG_READER_CORE_H
