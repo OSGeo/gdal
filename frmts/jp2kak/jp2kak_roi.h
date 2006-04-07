a9267a09da20b3407985a3ad95b47ca8c0fa2490
Frank Warmerdam <warmerdam@pobox.com>
2006-04-07 05:37:11 +0000

    remove redundant comments
    git-svn-id: https://svn.osgeo.org/gdal/trunk@9539 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/jp2kak/jp2kak_roi.h
+++ frmts/jp2kak/jp2kak_roi.h
@@ -29,6 +29,9 @@
  ******************************************************************************
  * 
  * $Log$
+ * Revision 1.2  2006/04/07 05:37:11  fwarmerdam
+ * remove redundant comments
+ *
  * Revision 1.1  2003/04/17 16:47:27  warmerda
  * New
  *
@@ -45,11 +48,6 @@ class kd_roi_rect_node : public kdu_roi_node {
       { tile_dims = tile_region;
         roi_dims = roi_region & tile_region; }
     void release()
-      /* In this simplest incarnation of an ROI source node, the granting
-         agent (`kdu_roi_rect') does not keep track of the nodes it grants
-         via its `acquire_node' interface; therefore, the node's own `release'
-         function must destroy the resource.  More sophisticated ROI sources
-         will not usually do this. */
       { delete this; }
     void pull(kdu_byte buf[], int width);
   private: // Data
@@ -62,40 +60,12 @@ class kd_roi_rect_node : public kdu_roi_node {
 /*****************************************************************************/
 
 class kdu_roi_rect : public kdu_roi_image {
-  /* [SYNOPSIS]
-     Serves ROI mask information through the standardized interfaces offered
-     by the base class, `kdu_roi_image', for the simple case in which there
-     is only one foreground region, having a rectangular geometry.  Although
-     somewhat useful in its own right, the main purpose of this object is
-     to provide the simplest possible example of implementing the services
-     advertised by the abstract base class, `kdu_roi_image'.
-     [//]
-     For a more sophisticated example, refer to `kdu_roi_graphics'.
-  */
   public: // Member functions
     kdu_roi_rect(kdu_codestream codestream, kdu_dims region);
-      /* [SYNOPSIS]
-           Accepts a single rectangular region, specified relative to the
-           high resolution code-stream canvas coordinate system.  This will
-           be the foreground region associated with all ROI mask generation
-           activities.  It is transformed into a region on each of the
-           individual image components by applying the usual coordinate
-           transformation rules.
-         [ARG: codestream]
-           Master interface to the internal code-stream management
-           machinery associated with the image being compressed.
-         [ARG: region]
-           The supplied region should incorporate the effects of any
-           prevailing geometric transformations, which may have been set
-           up by calls to `codestream.change_appearance'.  As a result,
-           if `region' were set to that returned by `codestream.get_dims'
-           (called with a negative `comp_idx' argument), the foreground
-           would correspond exactly to the full image region on the canvas.
-      */
     ~kdu_roi_rect()
       { if (comp_regions != NULL) delete[] comp_regions; }
     kdu_roi_node *acquire_node(int comp_idx, kdu_dims tile_region)
-      { /* [SYNOPSIS] See the description of `kdu_roi_image::acquire_node'. */
+      {
         assert((comp_idx >= 0) && (comp_idx < num_components));
         return new kd_roi_rect_node(tile_region,comp_regions[comp_idx]);
       }
