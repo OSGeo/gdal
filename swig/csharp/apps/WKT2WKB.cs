ea2e9d0e3c83fb4d60f6be50c5ed236a3cd82317
Tamas Szekeres <szekerest@gmail.com>
2006-11-23 22:52:46 +0000

    Added a sample for ExportToWkb
    git-svn-id: https://svn.osgeo.org/gdal/trunk@10371 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ swig/csharp/apps/WKT2WKB.cs
@@ -0,0 +1,48 @@
+using System;
+
+using OGR;
+
+
+/**
+
+ * <p>Title: GDAL C# wkt2wkb example.</p>
+ * <p>Description: A sample app for demonstrating the usage of ExportToWkb.</p>
+ * @author Tamas Szekeres (szekerest@gmail.com)
+ * @version 1.0
+ */
+
+
+
+/// <summary>
+/// A C# based sample for demonstrating the usage of ExportToWkb.
+/// </summary> 
+
+class WKT2WKB {
+	
+	public static void usage() 
+
+	{ 
+		Console.WriteLine("usage example: wkt2wkb \"POINT(47.0 19.2)\"");
+		System.Environment.Exit(-1);
+	}
+ 
+	public static void Main(string[] args) {
+
+		if (args.Length != 1) usage();
+
+		/* -------------------------------------------------------------------- */
+		/*      Register format(s).                                             */
+		/* -------------------------------------------------------------------- */
+        ogr.RegisterAll();
+
+        Geometry geom = new Geometry(ogr.wkbUnknown, args[0], 0, null, null);
+        
+        int wkbSize = geom.WkbSize();
+        if (wkbSize > 0) 
+        {
+            byte[] wkb = new byte[wkbSize];
+            geom.ExportToWkb( wkb );
+            Console.WriteLine( BitConverter.ToString(wkb) );
+        }
+	}
+}
