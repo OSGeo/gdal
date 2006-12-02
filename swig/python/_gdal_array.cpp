1d9f1a7ec1da9c6bc45cf11f3f0fcfbba50d4d7a
Howard Butler <hobu.inc@gmail.com>
2006-12-02 05:18:17 +0000

    move the numpy GDAL driver into _gdal_array instead of its own file
    git-svn-id: https://svn.osgeo.org/gdal/trunk@10433 f0d54148-0727-0410-94bb-9a71ac55c965

--- swig/python/_gdal_array.cpp
+++ swig/python/_gdal_array.cpp
@@ -31,15 +31,17 @@
 
 #include "Python.h"
 
+#include "gdal_priv.h"
 #include "gdal_array.h"
 
-/************************************************************************/
-/*                          GDALRegister_NUMPY()                        */
-/************************************************************************/
+
 
 static PyObject *GDALArrayError;
 
 
+/************************************************************************/
+/*                          GetArrayFilename()                          */
+/************************************************************************/
 
 PyObject* GetArrayFilename(PyObject *self, PyObject *args){
     GDALRegister_NUMPY();
@@ -59,7 +61,12 @@ PyObject* GetArrayFilename(PyObject *self, PyObject *args){
     return Py_BuildValue( "s", szString );
 
 }
-    
+ 
+
+/************************************************************************/
+/*                          GDALRegister_NUMPY()                        */
+/************************************************************************/
+   
 PyObject* GDALRegister_NUMPY(void)
 
 {
@@ -111,3 +118,309 @@ init_gdal_array(void)
     
 
 }
+
+/************************************************************************/
+/*                            NUMPYDataset()                            */
+/************************************************************************/
+
+NUMPYDataset::NUMPYDataset()
+
+{
+    pszProjection = CPLStrdup("");
+    adfGeoTransform[0] = 0.0;
+    adfGeoTransform[1] = 1.0;
+    adfGeoTransform[2] = 0.0;
+    adfGeoTransform[3] = 0.0;
+    adfGeoTransform[4] = 0.0;
+    adfGeoTransform[5] = 1.0;
+
+    nGCPCount = 0;
+    pasGCPList = NULL;
+    pszGCPProjection = CPLStrdup("");
+}
+
+/************************************************************************/
+/*                            ~NUMPYDataset()                            */
+/************************************************************************/
+
+NUMPYDataset::~NUMPYDataset()
+
+{
+    CPLFree( pszProjection );
+
+    CPLFree( pszGCPProjection );
+    if( nGCPCount > 0 )
+    {
+        GDALDeinitGCPs( nGCPCount, pasGCPList );
+        CPLFree( pasGCPList );
+    }
+
+    FlushCache();
+    Py_DECREF( psArray );
+}
+
+/************************************************************************/
+/*                          GetProjectionRef()                          */
+/************************************************************************/
+
+const char *NUMPYDataset::GetProjectionRef()
+
+{
+    return( pszProjection );
+}
+
+/************************************************************************/
+/*                           SetProjection()                            */
+/************************************************************************/
+
+CPLErr NUMPYDataset::SetProjection( const char * pszNewProjection )
+
+{
+    CPLFree( pszProjection );
+    pszProjection = CPLStrdup( pszNewProjection );
+
+    return CE_None;
+}
+
+/************************************************************************/
+/*                          GetGeoTransform()                           */
+/************************************************************************/
+
+CPLErr NUMPYDataset::GetGeoTransform( double * padfTransform )
+
+{
+    memcpy( padfTransform, adfGeoTransform, sizeof(double)*6 );
+    return CE_None;
+}
+
+/************************************************************************/
+/*                          SetGeoTransform()                           */
+/************************************************************************/
+
+CPLErr NUMPYDataset::SetGeoTransform( double * padfTransform )
+
+{
+    memcpy( adfGeoTransform, padfTransform, sizeof(double)*6 );
+    return( CE_None );
+}
+
+/************************************************************************/
+/*                            GetGCPCount()                             */
+/************************************************************************/
+
+int NUMPYDataset::GetGCPCount()
+
+{
+    return nGCPCount;
+}
+
+/************************************************************************/
+/*                          GetGCPProjection()                          */
+/************************************************************************/
+
+const char *NUMPYDataset::GetGCPProjection()
+
+{
+    return pszGCPProjection;
+}
+
+/************************************************************************/
+/*                               GetGCPs()                              */
+/************************************************************************/
+
+const GDAL_GCP *NUMPYDataset::GetGCPs()
+
+{
+    return pasGCPList;
+}
+
+/************************************************************************/
+/*                              SetGCPs()                               */
+/************************************************************************/
+
+CPLErr NUMPYDataset::SetGCPs( int nGCPCount, const GDAL_GCP *pasGCPList,
+                              const char *pszGCPProjection )
+
+{
+    CPLFree( this->pszGCPProjection );
+    if( this->nGCPCount > 0 )
+    {
+        GDALDeinitGCPs( this->nGCPCount, this->pasGCPList );
+        CPLFree( this->pasGCPList );
+    }
+
+    this->pszGCPProjection = CPLStrdup(pszGCPProjection);
+
+    this->nGCPCount = nGCPCount;
+
+    this->pasGCPList = GDALDuplicateGCPs( nGCPCount, pasGCPList );
+
+    return CE_None;
+}
+
+/************************************************************************/
+/*                                Open()                                */
+/************************************************************************/
+
+GDALDataset *NUMPYDataset::Open( GDALOpenInfo * poOpenInfo )
+
+{
+    PyArrayObject *psArray;
+    GDALDataType  eType;
+    int     nBands;
+
+/* -------------------------------------------------------------------- */
+/*      Is this a numpy dataset name?                                   */
+/* -------------------------------------------------------------------- */
+    if( !EQUALN(poOpenInfo->pszFilename,"NUMPY:::",8) 
+        || poOpenInfo->fp != NULL )
+        return NULL;
+
+    psArray = NULL;
+    sscanf( poOpenInfo->pszFilename+8, "%p", &(psArray) );
+    if( psArray == NULL )
+    {
+        CPLError( CE_Failure, CPLE_AppDefined, 
+                  "Failed to parse meaningful pointer value from NUMPY name\n"
+                  "string: %s\n", 
+                  poOpenInfo->pszFilename );
+        return NULL;
+    }
+
+/* -------------------------------------------------------------------- */
+/*      If we likely have corrupt definitions of the NUMPY stuff,       */
+/*      then warn now.                                                  */
+/* -------------------------------------------------------------------- */
+#ifdef NUMPY_DEFS_WRONG
+    CPLError( CE_Warning, CPLE_AppDefined, 
+              "It would appear you have built GDAL without having it use\n"
+              "the Numeric python include files.  Old definitions have\n"
+              "been used instead at build time, and it is quite possible that\n"
+              "the things will shortly fail or crash if they are wrong.\n"
+              "Consider installing Numeric, and rebuilding with HAVE_NUMPY\n"
+              "enabled in gdal\nmake.opt." );
+#endif
+
+/* -------------------------------------------------------------------- */
+/*      Is this a directly mappable Python array?  Verify rank, and     */
+/*      data type.                                                      */
+/* -------------------------------------------------------------------- */
+
+    if( psArray->nd < 2 || psArray->nd > 3 )
+    {
+        CPLError( CE_Failure, CPLE_AppDefined, 
+                  "Illegal numpy array rank %d.\n", 
+                  psArray->nd );
+        return NULL;
+    }
+
+    switch( psArray->descr->type_num )
+    {
+      case NPY_CDOUBLE:
+        eType = GDT_CFloat64;
+        break;
+
+      case NPY_CFLOAT:
+        eType = GDT_CFloat32;
+        break;
+
+      case NPY_DOUBLE:
+        eType = GDT_Float64;
+        break;
+
+      case NPY_FLOAT:
+        eType = GDT_Float32;
+        break;
+
+      case NPY_INT:
+      case NPY_LONG:
+        eType = GDT_Int32;
+        break;
+
+      case NPY_UINT:
+      case NPY_ULONG:
+        eType = GDT_UInt32;
+        break;
+
+      case NPY_SHORT:
+        eType = GDT_Int16;
+        break;
+
+      case NPY_USHORT:
+        eType = GDT_UInt16;
+        break;
+
+      case NPY_BYTE:
+      case NPY_UBYTE:
+        eType = GDT_Byte;
+        break;
+
+      default:
+        CPLError( CE_Failure, CPLE_AppDefined, 
+                  "Unable to access numpy arrays of typecode `%c'.\n", 
+                  psArray->descr->type );
+        return NULL;
+    }
+
+/* -------------------------------------------------------------------- */
+/*      Create the new NUMPYDataset object.                             */
+/* -------------------------------------------------------------------- */
+    NUMPYDataset *poDS;
+
+    poDS = new NUMPYDataset();
+
+    poDS->psArray = psArray;
+
+    poDS->eAccess = GA_ReadOnly;
+
+/* -------------------------------------------------------------------- */
+/*      Add a reference to the array.                                   */
+/* -------------------------------------------------------------------- */
+    Py_INCREF( psArray );
+
+/* -------------------------------------------------------------------- */
+/*      Workout the data layout.                                        */
+/* -------------------------------------------------------------------- */
+    int    nBandOffset;
+    int    nPixelOffset;
+    int    nLineOffset;
+
+    if( psArray->nd == 3 )
+    {
+        nBands = psArray->dimensions[0];
+        nBandOffset = psArray->strides[0];
+        poDS->nRasterXSize = psArray->dimensions[2];
+        nPixelOffset = psArray->strides[2];
+        poDS->nRasterYSize = psArray->dimensions[1];
+        nLineOffset = psArray->strides[1];
+    }
+    else
+    {
+        nBands = 1;
+        nBandOffset = 0;
+        poDS->nRasterXSize = psArray->dimensions[1];
+        nPixelOffset = psArray->strides[1];
+        poDS->nRasterYSize = psArray->dimensions[0];
+        nLineOffset = psArray->strides[0];
+    }
+
+/* -------------------------------------------------------------------- */
+/*      Create band information objects.                                */
+/* -------------------------------------------------------------------- */
+    for( int iBand = 0; iBand < nBands; iBand++ )
+    {
+        poDS->SetBand( iBand+1, 
+                       (GDALRasterBand *) 
+                       MEMCreateRasterBand( poDS, iBand+1, 
+                                (GByte *) psArray->data + nBandOffset*iBand,
+                                          eType, nPixelOffset, nLineOffset,
+                                          FALSE ) );
+    }
+
+/* -------------------------------------------------------------------- */
+/*      Try to return a regular handle on the file.                     */
+/* -------------------------------------------------------------------- */
+    return poDS;
+}
+
+
