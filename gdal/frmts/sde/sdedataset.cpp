72294fffa25416ba0f2b8e42e96a2f82844323d2
Howard Butler <hobu.inc@gmail.com>
2007-01-18 04:59:24 +0000

    SDE scaffolding
    git-svn-id: https://svn.osgeo.org/gdal/trunk@10648 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ gdal/frmts/sde/sdedataset.cpp
@@ -0,0 +1,213 @@
+/******************************************************************************
+ * $Id: jdemdataset.cpp 10645 2007-01-18 02:22:39Z warmerdam $
+ *
+ * Project:  ESRI ArcSDE Raster reader
+ * Purpose:  Dataset implementaion for ESRI ArcSDE Rasters
+ * Author:   Frank Warmerdam, warmerdam@pobox.com
+ *
+ ******************************************************************************
+ * Copyright (c) 2007, Howard Butler <hobu@hobu.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+
+#include "gdal_pam.h"
+
+CPL_CVSID("$Id: jdemdataset.cpp 10645 2007-01-18 02:22:39Z warmerdam $");
+
+CPL_C_START
+void	GDALRegister_SDE(void);
+CPL_C_END
+
+
+
+/************************************************************************/
+/* ==================================================================== */
+/*				SDEDataset				*/
+/* ==================================================================== */
+/************************************************************************/
+
+class SDERasterBand;
+
+class SDEDataset : public GDALPamDataset
+{
+    friend class SDERasterBand;
+
+    FILE	*fp;
+    GByte	abyHeader[1012];
+
+  public:
+		~SDEDataset();
+    
+    static GDALDataset *Open( GDALOpenInfo * );
+
+    CPLErr 	GetGeoTransform( double * padfTransform );
+    const char *GetProjectionRef();
+};
+
+/************************************************************************/
+/* ==================================================================== */
+/*                            SDERasterBand                             */
+/* ==================================================================== */
+/************************************************************************/
+
+class SDERasterBand : public GDALPamRasterBand
+{
+    friend class SDEDataset;
+    
+  public:
+
+    		SDERasterBand( SDEDataset *, int );
+    
+    virtual CPLErr IReadBlock( int, int, void * );
+};
+
+
+/************************************************************************/
+/*                           SDERasterBand()                            */
+/************************************************************************/
+
+SDERasterBand::SDERasterBand( SDEDataset *poDS, int nBand )
+
+{
+    this->poDS = poDS;
+    this->nBand = nBand;
+    
+    eDataType = GDT_Float32;
+
+    nBlockXSize = poDS->GetRasterXSize();
+    nBlockYSize = 1;
+}
+
+/************************************************************************/
+/*                             IReadBlock()                             */
+/************************************************************************/
+
+CPLErr SDERasterBand::IReadBlock( int nBlockXOff, int nBlockYOff,
+                                  void * pImage )
+
+{
+    SDEDataset *poGDS = (SDEDataset *) poDS;
+    char	*pszRecord;
+    int		nRecordSize = nBlockXSize*5 + 9 + 2;
+    int		i;
+
+
+
+    return CE_None;
+}
+
+/************************************************************************/
+/* ==================================================================== */
+/*				JDEMDataset				*/
+/* ==================================================================== */
+/************************************************************************/
+
+/************************************************************************/
+/*                            ~SDEDataset()                             */
+/************************************************************************/
+
+SDEDataset::~SDEDataset()
+
+{
+
+}
+
+/************************************************************************/
+/*                          GetGeoTransform()                           */
+/************************************************************************/
+
+CPLErr SDEDataset::GetGeoTransform( double * padfTransform )
+
+{
+//    double	dfLLLat, dfLLLong, dfURLat, dfURLong;
+//
+//    dfLLLat = JDEMGetAngle( (char *) abyHeader + 29 );
+//    dfLLLong = JDEMGetAngle( (char *) abyHeader + 36 );
+//    dfURLat = JDEMGetAngle( (char *) abyHeader + 43 );
+//    dfURLong = JDEMGetAngle( (char *) abyHeader + 50 );
+//    
+//    padfTransform[0] = dfLLLong;
+//    padfTransform[3] = dfURLat;
+//    padfTransform[1] = (dfURLong - dfLLLong) / GetRasterXSize();
+//    padfTransform[2] = 0.0;
+//        
+//    padfTransform[4] = 0.0;
+//    padfTransform[5] = -1 * (dfURLat - dfLLLat) / GetRasterYSize();
+
+
+    return CE_None;
+}
+
+/************************************************************************/
+/*                          GetProjectionRef()                          */
+/************************************************************************/
+
+const char *SDEDataset::GetProjectionRef()
+
+{
+    return( "GEOGCS[\"Tokyo\",DATUM[\"Tokyo\",SPHEROID[\"Bessel 1841\",6377397.155,299.1528128,AUTHORITY[\"EPSG\",7004]],TOWGS84[-148,507,685,0,0,0,0],AUTHORITY[\"EPSG\",6301]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",8901]],UNIT[\"DMSH\",0.0174532925199433,AUTHORITY[\"EPSG\",9108]],AXIS[\"Lat\",NORTH],AXIS[\"Long\",EAST],AUTHORITY[\"EPSG\",4301]]" );
+}
+
+/************************************************************************/
+/*                                Open()                                */
+/************************************************************************/
+
+GDALDataset *SDEDataset::Open( GDALOpenInfo * poOpenInfo )
+
+{
+    
+/* -------------------------------------------------------------------- */
+/*      Create a corresponding GDALDataset.                             */
+/* -------------------------------------------------------------------- */
+    SDEDataset 	*poDS;
+
+    poDS = new SDEDataset();
+
+    poDS->fp = poOpenInfo->fp;
+    poOpenInfo->fp = NULL;
+
+    return( poDS );
+}
+
+/************************************************************************/
+/*                          GDALRegister_SDE()                          */
+/************************************************************************/
+
+void GDALRegister_SDE()
+
+{
+    GDALDriver	*poDriver;
+
+    if( GDALGetDriverByName( "SDE" ) == NULL )
+    {
+        poDriver = new GDALDriver();
+        
+        poDriver->SetDescription( "SDE" );
+        poDriver->SetMetadataItem( GDAL_DMD_LONGNAME, 
+                                   "ESRI ArcSDE (.thishasnoextension)" );
+        poDriver->SetMetadataItem( GDAL_DMD_HELPTOPIC, 
+                                   "frmt_various.html#SDE" );
+       // poDriver->SetMetadataItem( GDAL_DMD_EXTENSION, "mem" );
+
+        poDriver->pfnOpen = SDEDataset::Open;
+
+        GetGDALDriverManager()->RegisterDriver( poDriver );
+    }
+}
