76aeab302e31b3de0a989cc187e8116f97a26b00
Howard Butler <hobu.inc@gmail.com>
2007-01-20 15:54:10 +0000

    basic connect and list layers if one not specified
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@10669 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/sde/sdedataset.cpp
+++ frmts/sde/sdedataset.cpp
@@ -3,7 +3,7 @@
  *
  * Project:  ESRI ArcSDE Raster reader
  * Purpose:  Dataset implementaion for ESRI ArcSDE Rasters
- * Author:   Frank Warmerdam, warmerdam@pobox.com
+ * Author:   Howard Butler, hobu@hobu.net
  *
  ******************************************************************************
  * Copyright (c) 2007, Howard Butler <hobu@hobu.net>
@@ -27,56 +27,69 @@
  * DEALINGS IN THE SOFTWARE.
  ****************************************************************************/
 
-#include "gdal_pam.h"
+#include "sdedataset.h"
 
-CPL_CVSID("$Id$");
 
-CPL_C_START
-void	GDALRegister_SDE(void);
-CPL_C_END
+
 
 
 
 /************************************************************************/
-/* ==================================================================== */
-/*				SDEDataset				*/
-/* ==================================================================== */
+/*                           IssueSDEError()                            */
+/* from ogrsdedatasource.cpp											*/
 /************************************************************************/
 
-class SDERasterBand;
+void IssueSDEError( int nErrorCode, 
+                    const char *pszFunction )
 
-class SDEDataset : public GDALPamDataset
 {
-    friend class SDERasterBand;
+    char szErrorMsg[SE_MAX_MESSAGE_LENGTH+1];
 
-    FILE	*fp;
-    GByte	abyHeader[1012];
+    if( pszFunction == NULL )
+        pszFunction = "SDERASTER";
 
-  public:
-		~SDEDataset();
-    
-    static GDALDataset *Open( GDALOpenInfo * );
+    SE_error_get_string( nErrorCode, szErrorMsg );
 
-    CPLErr 	GetGeoTransform( double * padfTransform );
-    const char *GetProjectionRef();
-};
+    CPLError( CE_Failure, CPLE_AppDefined, 
+              "%s: %d/%s", 
+              pszFunction, nErrorCode, szErrorMsg );
+}
 
 /************************************************************************/
-/* ==================================================================== */
-/*                            SDERasterBand                             */
-/* ==================================================================== */
+/*                           IssueSDEExtendedError()                    */
 /************************************************************************/
+void IssueSDEExtendedError ( int nErrorCode,
+                           const char *pszFunction,
+                           SE_CONNECTION* connection,
+                           SE_STREAM* stream) {
+ 
+    SE_ERROR err;
+    char szErrorMsg[SE_MAX_MESSAGE_LENGTH+1];
 
-class SDERasterBand : public GDALPamRasterBand
-{
-    friend class SDEDataset;
-    
-  public:
+    if( pszFunction == NULL )
+        pszFunction = "SDERASTER";
 
-    		SDERasterBand( SDEDataset *, int );
+    SE_error_get_string( nErrorCode, szErrorMsg );
+
+        
+    if (connection)
+        SE_connection_get_ext_error( *connection, &err );
+    if (stream)
+        SE_stream_get_ext_error( *stream, &err );
     
-    virtual CPLErr IReadBlock( int, int, void * );
-};
+    if (connection || stream) {
+        CPLError ( CE_Failure, CPLE_AppDefined,
+                   "%s: %d/%s ---- %s ---- %s ---- %s ---- %s",
+                   pszFunction, nErrorCode, szErrorMsg, 
+                   err.sde_error, err.ext_error,
+                   err.err_msg1, err.err_msg2 );           
+
+    } else {
+        CPLError ( CE_Failure, CPLE_AppDefined,
+           "%s: %d/%s",
+           pszFunction, nErrorCode, szErrorMsg );
+    } 
+}
 
 
 /************************************************************************/
@@ -104,29 +117,13 @@ CPLErr SDERasterBand::IReadBlock( int nBlockXOff, int nBlockYOff,
 
 {
     SDEDataset *poGDS = (SDEDataset *) poDS;
-    char	*pszRecord;
-    int		nRecordSize = nBlockXSize*5 + 9 + 2;
-    int		i;
+    char  *pszRecord;
+    int   nRecordSize = nBlockXSize*5 + 9 + 2;
+    int   i;
 
 
 
     return CE_None;
-}
-
-/************************************************************************/
-/* ==================================================================== */
-/*				JDEMDataset				*/
-/* ==================================================================== */
-/************************************************************************/
-
-/************************************************************************/
-/*                            ~SDEDataset()                             */
-/************************************************************************/
-
-SDEDataset::~SDEDataset()
-
-{
-
 }
 
 /************************************************************************/
@@ -136,7 +133,7 @@ SDEDataset::~SDEDataset()
 CPLErr SDEDataset::GetGeoTransform( double * padfTransform )
 
 {
-//    double	dfLLLat, dfLLLong, dfURLat, dfURLong;
+//    double  dfLLLat, dfLLLong, dfURLat, dfURLong;
 //
 //    dfLLLat = JDEMGetAngle( (char *) abyHeader + 29 );
 //    dfLLLong = JDEMGetAngle( (char *) abyHeader + 36 );
@@ -166,6 +163,34 @@ const char *SDEDataset::GetProjectionRef()
 }
 
 /************************************************************************/
+/*                                SDEDataset()                          */
+/************************************************************************/
+
+SDEDataset::SDEDataset( SE_CONNECTION* connection )
+
+{
+    hConnection         = connection;
+    nSubDataCount       = 0;
+    pszLayerName        = NULL;
+    pszColumnName       = NULL;
+    poSDERasterColumns  = NULL;
+
+}
+
+
+
+/************************************************************************/
+/*                            ~SDEDataset()                             */
+/************************************************************************/
+
+SDEDataset::~SDEDataset()
+
+{
+
+}
+
+
+/************************************************************************/
 /*                                Open()                                */
 /************************************************************************/
 
@@ -176,13 +201,125 @@ GDALDataset *SDEDataset::Open( GDALOpenInfo * poOpenInfo )
 /* -------------------------------------------------------------------- */
 /*      Create a corresponding GDALDataset.                             */
 /* -------------------------------------------------------------------- */
-    SDEDataset 	*poDS;
 
-    poDS = new SDEDataset();
 
-    poDS->fp = poOpenInfo->fp;
-    poOpenInfo->fp = NULL;
+/* -------------------------------------------------------------------- */
+/*      If we aren't prefixed with SDE: then ignore this datasource.    */
+/* -------------------------------------------------------------------- */
+    if( !EQUALN(poOpenInfo->pszFilename,"SDE:",4) )
+        return FALSE;
 
+/* -------------------------------------------------------------------- */
+/*      Parse arguments on comma.  We expect (layer is optional):       */
+/*        SDE:server,instance,database,username,password,layer          */
+/* -------------------------------------------------------------------- */
+    char **papszTokens = CSLTokenizeStringComplex( poOpenInfo->pszFilename+4, ",",
+                                                   TRUE, TRUE );
+    CPLDebug( "SDERASTER", "Open(\"%s\") revealed %d tokens.", poOpenInfo->pszFilename,
+              CSLCount( papszTokens ) );
+
+
+    if( CSLCount( papszTokens ) < 5 || CSLCount( papszTokens ) > 6 )
+    {
+        CPLError( CE_Failure, CPLE_OpenFailed, 
+                  "SDE connect string had wrong number of arguments.\n"
+                  "Expected 'SDE:server,instance,database,username,password,layer'\n"
+		          "The layer name value is optional.\n"
+                  "Got '%s'", 
+                  poOpenInfo->pszFilename );
+        return FALSE;
+    }
+
+/* -------------------------------------------------------------------- */
+/*      Try to establish connection.                                    */
+/* -------------------------------------------------------------------- */
+    int 	    nSDEErr;
+    SE_ERROR    sSDEErrorInfo;
+
+	SE_CONNECTION connection = NULL;
+		
+    nSDEErr = SE_connection_create( papszTokens[0], 
+                                    papszTokens[1], 
+                                    papszTokens[2], 
+                                    papszTokens[3],
+                                    papszTokens[4],
+                                    &sSDEErrorInfo, &connection );
+
+    if( nSDEErr != SE_SUCCESS )
+    {
+        IssueSDEError( nSDEErr, "SE_connection_create" );
+        return FALSE;
+    }
+
+
+/* -------------------------------------------------------------------- */
+/*      Set unprotected concurrency policy, suitable for single         */
+/*      threaded access.                                                */
+/* -------------------------------------------------------------------- */
+    nSDEErr = SE_connection_set_concurrency( connection,
+                                             SE_UNPROTECTED_POLICY);
+
+    if( nSDEErr != SE_SUCCESS) {
+        IssueSDEError( nSDEErr, NULL );
+        return FALSE;
+    }
+
+    SDEDataset *poDS;
+
+    poDS = new SDEDataset(&connection);
+
+/* -------------------------------------------------------------------- */
+/*      If we were given a layer name, use that directly, otherwise     */
+/*      query for subdatasets.                                          */
+/* -------------------------------------------------------------------- */
+
+
+    if (CSLCount( papszTokens ) == 6 ) {
+
+        poDS->pszLayerName = CPLStrdup( papszTokens[5] );
+        
+        // FIXME this needs to be a configuration option or allow it to
+        // come in via the arguments
+        poDS->pszColumnName = CPLStrdup( "RASTER" );
+        CPLDebug( "SDERASTER", "'%s' raster layer specified... using it directly with '%s' as the raster column name.", 
+                  poDS->pszLayerName,
+                  poDS->pszColumnName);
+    } else {
+
+    	SE_RASCOLINFO* columns;
+      	nSDEErr = SE_rastercolumn_get_info_list(connection, 
+      	                                        &columns, 
+      	                                        &(poDS->nSubDataCount));
+      	
+        CPLDebug( "SDERASTER", "No layername specified, %d subdatasets available.", 
+                  poDS->nSubDataCount);
+        if( nSDEErr != SE_SUCCESS )
+        {
+            IssueSDEError( nSDEErr, "SE_rascolinfo_get_info_list" );
+            return FALSE;
+        }
+    
+        /* Allocate an array of column information */
+    	poDS->poSDERasterColumns =  (SDERasterColumns*) \
+    	                            CPLMalloc( (poDS->nSubDataCount) * 
+    	                                       sizeof( SDERasterColumns ));
+    	                                       
+        for (int i = 0; i < poDS->nSubDataCount; i++) {
+            nSDEErr = SE_rascolinfo_get_raster_column (columns[i], 
+                                                       poDS->poSDERasterColumns[i].szTableName, 
+                                                       poDS->poSDERasterColumns[i].szColumnName); 
+            if( nSDEErr != SE_SUCCESS )
+            {
+                IssueSDEError( nSDEErr, "SE_rascolinfo_get_raster_column" );
+                return FALSE;
+            }
+        }
+        
+        SE_rastercolumn_free_info_list(poDS->nSubDataCount,
+                                       columns);
+
+    }
+    
     return( poDS );
 }
 
@@ -193,7 +330,7 @@ GDALDataset *SDEDataset::Open( GDALOpenInfo * poOpenInfo )
 void GDALRegister_SDE()
 
 {
-    GDALDriver	*poDriver;
+    GDALDriver  *poDriver;
 
     if( GDALGetDriverByName( "SDE" ) == NULL )
     {
@@ -201,7 +338,7 @@ void GDALRegister_SDE()
         
         poDriver->SetDescription( "SDE" );
         poDriver->SetMetadataItem( GDAL_DMD_LONGNAME, 
-                                   "ESRI ArcSDE (.thishasnoextension)" );
+                                   "ESRI ArcSDE" );
         poDriver->SetMetadataItem( GDAL_DMD_HELPTOPIC, 
                                    "frmt_various.html#SDE" );
        // poDriver->SetMetadataItem( GDAL_DMD_EXTENSION, "mem" );
