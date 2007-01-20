f99d21fa0f0a660fd4c4fd58ec8a9b82e51478a9
Howard Butler <hobu.inc@gmail.com>
2007-01-20 15:52:03 +0000

    add a header file
    git-svn-id: https://svn.osgeo.org/gdal/trunk@10668 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ gdal/frmts/sde/sdedataset.h
@@ -0,0 +1,80 @@
+#include "gdal_pam.h"
+
+
+CPL_CVSID("$Id: sdedataset.cpp 10650 2007-01-18 05:02:33Z hobu $");
+
+CPL_C_START
+void    GDALRegister_SDE(void);
+
+
+CPL_C_END
+
+#include <sdetype.h>
+#include <sdeerno.h>
+#include <sderaster.h>
+
+
+
+
+
+/************************************************************************/
+/* ==================================================================== */
+/*              SDEDataset              */
+/* ==================================================================== */
+/************************************************************************/
+
+typedef struct SDERasterColumns {
+  char         szTableName[SE_QUALIFIED_TABLE_NAME+1];
+  char         szColumnName[SE_MAX_COLUMN_LEN+1];
+} SDERasterColumns;
+
+
+class SDERasterBand;
+
+class SDEDataset : public GDALPamDataset
+{
+    friend class SDERasterBand;
+
+    private:
+        
+        // SDE-specific stuff
+        SE_CONNECTION*      hConnection;
+        char                *pszLayerName;
+        char                *pszColumnName;
+        long                nSubDataCount;
+        SDERasterColumns    *poSDERasterColumns;
+        
+        GDALColorTable *poCT;
+
+    public:
+        SDEDataset(SE_CONNECTION* connection);
+        ~SDEDataset();
+        
+        static GDALDataset *Open( GDALOpenInfo * );
+        
+        CPLErr  GetGeoTransform( double * padfTransform );
+        const char *GetProjectionRef();
+};
+
+/************************************************************************/
+/* ==================================================================== */
+/*                            SDERasterBand                             */
+/* ==================================================================== */
+/************************************************************************/
+
+class SDERasterBand : public GDALPamRasterBand
+{
+    friend class SDEDataset;
+    
+    public:
+
+        SDERasterBand( SDEDataset *, int );
+    
+    virtual CPLErr IReadBlock( int, int, void * );
+//    virtual double GetMinimum( int *pbSuccess );
+//    virtual double GetMaximum( int *pbSuccess );
+//    virtual double GetNoDataValue( int *pbSuccess );
+//
+//    virtual GDALColorInterp GetColorInterpretation();
+//    virtual GDALColorTable *GetColorTable();
+};
