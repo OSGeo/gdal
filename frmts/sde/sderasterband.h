253a74ae96a8ce0e910eee5b22a1b5235a4b2941
Howard Butler <hobu.inc@gmail.com>
2007-01-26 18:15:21 +0000

    some light restructuring to use explicit header files with include guards
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@10697 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ frmts/sde/sderasterband.h
@@ -0,0 +1,28 @@
+#include "gdal_sde.h"
+
+/************************************************************************/
+/* ==================================================================== */
+/*                            SDERasterBand                             */
+/* ==================================================================== */
+/************************************************************************/
+
+class SDERasterBand : public GDALPamRasterBand
+{
+    friend class SDEDataset;
+    
+    public:
+
+        SDERasterBand( SDEDataset * poDS, int nBand , const SE_RASBANDINFO& band);
+    
+    GDALColorTable*         ComputeColorTable(const SE_RASBANDINFO& band);
+    virtual CPLErr IReadBlock( int, int, void * );
+    virtual CPLErr GetStatistics( int bApproxOK, int bForce,
+                                  double *pdfMin, double *pdfMax, 
+                                  double *pdfMean, double *pdfStdDev );
+//    virtual double GetMinimum( int *pbSuccess );
+//    virtual double GetMaximum( int *pbSuccess );
+//    virtual double GetNoDataValue( int *pbSuccess );
+//
+//    virtual GDALColorInterp GetColorInterpretation();
+//    virtual GDALColorTable *GetColorTable();
+};
