b89851e48b6af84b0bcaa89881b99fe2500cb510
Howard Butler <hobu.inc@gmail.com>
2007-01-26 19:50:04 +0000

    GetStatistics and reorganize RasterBand stuff
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@10700 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/sde/sderasterband.h
+++ frmts/sde/sderasterband.h
@@ -13,15 +13,21 @@ class SDERasterBand : public GDALPamRasterBand
 {
     friend class SDEDataset;
     
+    private:
+        const SE_RASBANDINFO* poBand;
+        
+        GDALDataType            MorphESRIRasterType(int gtype);
+    
     public:
 
-        SDERasterBand( SDEDataset * poDS, int nBand , const SE_RASBANDINFO& band);
+        SDERasterBand( SDEDataset * poDS, int nBand , const SE_RASBANDINFO* band);
     
     GDALColorTable*         ComputeColorTable(const SE_RASBANDINFO& band);
     virtual CPLErr IReadBlock( int, int, void * );
     virtual CPLErr GetStatistics( int bApproxOK, int bForce,
                                   double *pdfMin, double *pdfMax, 
                                   double *pdfMean, double *pdfStdDev );
+    virtual GDALDataType SDERasterBand::GetRasterDataType(void);
 //    virtual double GetMinimum( int *pbSuccess );
 //    virtual double GetMaximum( int *pbSuccess );
 //    virtual double GetNoDataValue( int *pbSuccess );
