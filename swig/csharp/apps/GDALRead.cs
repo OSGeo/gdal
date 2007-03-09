7dfba0d8b58a9b38e1b8613aff0af9c0b6f8e33d
Tamas Szekeres <szekerest@gmail.com>
2007-03-09 14:26:57 +0000

    Support for reading indexed and grayscale images.
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@10907 f0d54148-0727-0410-94bb-9a71ac55c965

--- swig/csharp/apps/GDALRead.cs
+++ swig/csharp/apps/GDALRead.cs
@@ -86,12 +86,6 @@ class GDALRead {
             /* -------------------------------------------------------------------- */
             /*      Processing the raster                                           */
             /* -------------------------------------------------------------------- */
-            if (ds.RasterCount < 3) 
-            {
-                Console.WriteLine("The number of the raster bands is not enough to run this sample");
-                System.Environment.Exit(-1);
-            }
-
             SaveBitmapBuffered(ds, args[1]);
             
         }
@@ -105,8 +99,46 @@ class GDALRead {
     {
         // Get the GDAL Band objects from the Dataset
         Band redBand = ds.GetRasterBand(1);
+
+		if (redBand.GetRasterColorInterpretation() == ColorInterp.GCI_PaletteIndex)
+		{
+			SaveBitmapPaletteBuffered(ds, filename);
+			return;
+		}
+
+		if (redBand.GetRasterColorInterpretation() == ColorInterp.GCI_GrayIndex)
+		{
+			SaveBitmapGrayBuffered(ds, filename);
+			return;
+		}
+
+		if (redBand.GetRasterColorInterpretation() != ColorInterp.GCI_RedBand)
+		{
+            Console.WriteLine("Non RGB images are not supported by this sample!");
+			return;
+		}
+
+		if (ds.RasterCount < 3) 
+		{
+			Console.WriteLine("The number of the raster bands is not enough to run this sample");
+			System.Environment.Exit(-1);
+		}
+
         Band greenBand = ds.GetRasterBand(2);
+
+		if (redBand.GetRasterColorInterpretation() != ColorInterp.GCI_GreenBand)
+		{
+			Console.WriteLine("Non RGB images are not supported by this sample!");
+			return;
+		}
+
         Band blueBand = ds.GetRasterBand(3);
+
+		if (redBand.GetRasterColorInterpretation() != ColorInterp.GCI_BlueBand)
+		{
+			Console.WriteLine("Non RGB images are not supported by this sample!");
+			return;
+		}
 
         // Get the width and height of the Dataset
         int width = ds.RasterXSize;
@@ -139,4 +171,84 @@ class GDALRead {
 
         bitmap.Save(filename);
     }
+
+	private static void SaveBitmapPaletteBuffered(Dataset ds, string filename) 
+	{
+		// Get the GDAL Band objects from the Dataset
+		Band band = ds.GetRasterBand(1);
+
+		ColorTable ct = band.GetRasterColorTable();
+		if (ct == null)
+		{
+			Console.WriteLine("   Band has no color table!");
+			return;
+		}
+
+		if (ct.GetPaletteInterpretation() != PaletteInterp.GPI_RGB)
+		{
+			Console.WriteLine("   Only RGB palette interp is supported by this sample!");
+			return;
+		}
+
+		// Get the width and height of the Dataset
+		int width = ds.RasterXSize;
+		int height = ds.RasterYSize;
+
+		// Create a Bitmap to store the GDAL image in
+		Bitmap bitmap = new Bitmap(width, height, PixelFormat.Format32bppRgb);
+
+		DateTime start = DateTime.Now;
+        
+		byte[] r = new byte[width * height];
+		
+		band.ReadRaster(0, 0, width, height, r, width, height, 0, 0);
+		TimeSpan renderTime = DateTime.Now - start;
+		Console.WriteLine("SaveBitmapBuffered fetch time: " + renderTime.TotalMilliseconds + " ms");
+
+		int i, j;
+		for (i = 0; i< width; i++) 
+		{
+			for (j=0; j<height; j++)
+			{
+				ColorEntry entry = ct.GetColorEntry(r[i+j*width]);
+				Color newColor = Color.FromArgb(Convert.ToInt32(entry.c1),Convert.ToInt32(entry.c2), Convert.ToInt32(entry.c3));
+				bitmap.SetPixel(i, j, newColor);
+			}
+		}
+
+		bitmap.Save(filename);
+	}
+
+	private static void SaveBitmapGrayBuffered(Dataset ds, string filename) 
+	{
+		// Get the GDAL Band objects from the Dataset
+		Band band = ds.GetRasterBand(1);
+
+		// Get the width and height of the Dataset
+		int width = ds.RasterXSize;
+		int height = ds.RasterYSize;
+
+		// Create a Bitmap to store the GDAL image in
+		Bitmap bitmap = new Bitmap(width, height, PixelFormat.Format32bppRgb);
+
+		DateTime start = DateTime.Now;
+        
+		byte[] r = new byte[width * height];
+		
+		band.ReadRaster(0, 0, width, height, r, width, height, 0, 0);
+		TimeSpan renderTime = DateTime.Now - start;
+		Console.WriteLine("SaveBitmapBuffered fetch time: " + renderTime.TotalMilliseconds + " ms");
+
+		int i, j;
+		for (i = 0; i< width; i++) 
+		{
+			for (j=0; j<height; j++)
+			{
+				Color newColor = Color.FromArgb(Convert.ToInt32(r[i+j*width]),Convert.ToInt32(r[i+j*width]), Convert.ToInt32(r[i+j*width]));
+				bitmap.SetPixel(i, j, newColor);
+			}
+		}
+
+		bitmap.Save(filename);
+	}
 }
