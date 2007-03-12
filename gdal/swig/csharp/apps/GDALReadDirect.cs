aa90adbe30b925d22f2af1d0179b639afa69304f
Tamas Szekeres <szekerest@gmail.com>
2007-03-12 23:26:48 +0000

    Support for direct reading of 8bppIndexed images
    git-svn-id: https://svn.osgeo.org/gdal/trunk@10941 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/swig/csharp/apps/GDALReadDirect.cs
+++ gdal/swig/csharp/apps/GDALReadDirect.cs
@@ -36,7 +36,7 @@ class GDALReadDirect {
         // Using early initialization of System.Console
         Console.WriteLine("");
 
-        try 
+        //try 
         {
             /* -------------------------------------------------------------------- */
             /*      Register driver(s).                                             */
@@ -86,26 +86,32 @@ class GDALReadDirect {
             /* -------------------------------------------------------------------- */
             /*      Processing the raster                                           */
             /* -------------------------------------------------------------------- */
-
-            if (ds.RasterCount < 3) 
-            {
-                Console.WriteLine("The number of the raster bands is not enough to run this sample");
-                System.Environment.Exit(-1);
-            }
-
             SaveBitmapDirect(ds, args[1]);
             
         }
-        catch (Exception e)
+        /*catch (Exception e)
         {
             Console.WriteLine("Application error: " + e.Message);
-        }
+        }/**/
     }
 
     private static void SaveBitmapDirect(Dataset ds, string filename) 
     {
         // Get the GDAL Band objects from the Dataset
         Band redBand = ds.GetRasterBand(1);
+
+        if (redBand.GetRasterColorInterpretation() == ColorInterp.GCI_PaletteIndex)
+        {
+            SaveBitmapPaletteDirect(ds, filename);
+            return;
+        }
+
+        if (ds.RasterCount < 3) 
+        {
+            Console.WriteLine("The number of the raster bands is not enough to run this sample");
+            System.Environment.Exit(-1);
+        }
+
         Band greenBand = ds.GetRasterBand(2);
         Band blueBand = ds.GetRasterBand(3);
 
@@ -129,6 +135,65 @@ class GDALReadDirect {
             blueBand.ReadRaster(0, 0, width, height, buf, width, height, DataType.GDT_Byte, 4, stride);
             greenBand.ReadRaster(0, 0, width, height, new IntPtr(buf.ToInt32()+1), width, height, DataType.GDT_Byte, 4, stride);
             redBand.ReadRaster(0, 0, width, height, new IntPtr(buf.ToInt32()+2), width, height, DataType.GDT_Byte, 4, stride);
+            TimeSpan renderTime = DateTime.Now - start;
+            Console.WriteLine("SaveBitmapDirect fetch time: " + renderTime.TotalMilliseconds + " ms");
+        }
+        finally 
+        {
+            bitmap.UnlockBits(bitmapData);
+        }
+
+        bitmap.Save(filename);
+    }
+
+    private static void SaveBitmapPaletteDirect(Dataset ds, string filename) 
+    {
+        // Get the GDAL Band objects from the Dataset
+        Band band = ds.GetRasterBand(1);
+
+        ColorTable ct = band.GetRasterColorTable();
+        if (ct == null)
+        {
+            Console.WriteLine("   Band has no color table!");
+            return;
+        }
+
+        if (ct.GetPaletteInterpretation() != PaletteInterp.GPI_RGB)
+        {
+            Console.WriteLine("   Only RGB palette interp is supported by this sample!");
+            return;
+        }
+
+        // Get the width and height of the Dataset
+        int width = ds.RasterXSize;
+        int height = ds.RasterYSize;
+
+        // Create a Bitmap to store the GDAL image in
+        Bitmap bitmap = new Bitmap(width, height, PixelFormat.Format8bppIndexed);
+
+        DateTime start = DateTime.Now;
+        
+        byte[] r = new byte[width * height];
+		
+        band.ReadRaster(0, 0, width, height, r, width, height, 0, 0);
+        // Use GDAL raster reading methods to read the image data directly into the Bitmap
+        BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, PixelFormat.Format8bppIndexed);
+
+        try 
+        {
+            int iCol = ct.GetCount();
+            ColorPalette pal = bitmap.Palette;
+            for (int i = 0; i < iCol; i++)
+            {
+                ColorEntry ce = ct.GetColorEntry(i);
+                pal.Entries[i] = Color.FromArgb(ce.c4, ce.c1, ce.c2, ce.c3);
+            }
+            bitmap.Palette = pal;
+            
+            int stride = bitmapData.Stride;
+            IntPtr buf = bitmapData.Scan0;
+
+            band.ReadRaster(0, 0, width, height, buf, width, height, DataType.GDT_Byte, 1, stride);
             TimeSpan renderTime = DateTime.Now - start;
             Console.WriteLine("SaveBitmapDirect fetch time: " + renderTime.TotalMilliseconds + " ms");
         }
