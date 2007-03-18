2e61d3e0bea1f8de0eaa128502fa0f87c9d904c8
Frank Warmerdam <warmerdam@pobox.com>
2007-03-18 17:28:22 +0000

    added minimal http driver test
    git-svn-id: https://svn.osgeo.org/gdal/trunk@11010 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ autotest/gdrivers/gdalhttp.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+###############################################################################
+# $Id: wcs.py,v 1.4 2007/01/02 14:44:31 fwarmerdam Exp $
+#
+# Project:  GDAL/OGR Test Suite
+# Purpose:  Test HTTP Driver.
+# Author:   Frank Warmerdam <warmerdam@pobox.com>
+# 
+###############################################################################
+# Copyright (c) 2007, Frank Warmerdam <warmerdam@pobox.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+###############################################################################
+
+import os
+import sys
+import string
+import array
+import gdal
+
+sys.path.append( '../pymod' )
+
+import gdaltest
+
+###############################################################################
+# Verify we have the driver.
+
+def http_1():
+
+    try:
+        drv = gdal.GetDriverByName( 'HTTP' )
+    except:
+	return 'skip'
+
+    tst = gdaltest.GDALTest( 'HTTP','http://home.gdal.org/~warmerda/frank.gif',
+                             1, 35415, filename_absolute = 1 )
+    return tst.testOpen()
+
+
+gdaltest_list = [ http_1 ]
+
+if __name__ == '__main__':
+
+    gdaltest.setup_run( 'http' )
+
+    gdaltest.run_tests( gdaltest_list )
+
+    gdaltest.summarize()
+
