539f13bb2fa523da15aebd28b7f46042a7c67b7c
Frank Warmerdam <warmerdam@pobox.com>
2007-03-18 15:00:00 +0000

    restore VSIFPrintfL
    git-svn-id: https://svn.osgeo.org/gdal/trunk@11006 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/ers/ershdrnode.cpp
+++ gdal/frmts/ers/ershdrnode.cpp
@@ -245,7 +245,7 @@ int ERSHdrNode::WriteSelf( FILE * fp, int nIndent )
     {
         if( papszItemValue[i] != NULL )
         {
-            if( VSIFPrintf( fp, "%s%s\t= %s\n", 
+            if( VSIFPrintfL( fp, "%s%s\t= %s\n", 
                              oIndent.c_str(), 
                              papszItemName[i], 
                              papszItemValue[i] ) < 1 )
@@ -253,11 +253,11 @@ int ERSHdrNode::WriteSelf( FILE * fp, int nIndent )
         }
         else
         {
-            VSIFPrintf( fp, "%s%s Begin\n", 
+            VSIFPrintfL( fp, "%s%s Begin\n", 
                          oIndent.c_str(), papszItemName[i] );
             if( !papoItemChild[i]->WriteSelf( fp, nIndent+1 ) )
                 return FALSE;
-            if( VSIFPrintf( fp, "%s%s End\n", 
+            if( VSIFPrintfL( fp, "%s%s End\n", 
                              oIndent.c_str(), papszItemName[i] ) < 1 )
                 return FALSE;
         }
