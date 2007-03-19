960115735ea6d4a52f069ab68ac9cd2176185c1a
Andrey Kiselev <dron@ak4719.spb.edu>
2007-03-19 14:16:40 +0000

    Include in documentation generation.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@11015 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogr_core.h
+++ gdal/ogr/ogr_core.h
@@ -33,6 +33,12 @@
 #include "cpl_port.h"
 
 /**
+ * \file
+ *
+ * Core portability services for cross-platform OGR code.
+ */
+
+/**
  * Simple container for a bounding region.
  */
 
@@ -109,23 +115,27 @@ typedef int     OGRBoolean;
 
 typedef enum 
 {
-    wkbUnknown = 0,             /* non-standard */
-    wkbPoint = 1,               /* rest are standard WKB type codes */
-    wkbLineString = 2,
-    wkbPolygon = 3,
-    wkbMultiPoint = 4,
-    wkbMultiLineString = 5,
-    wkbMultiPolygon = 6,
-    wkbGeometryCollection = 7,
-    wkbNone = 100,              /* non-standard, for pure attribute records */
-    wkbLinearRing = 101,        /* non-standard, just for createGeometry() */
-    wkbPoint25D = 0x80000001,   /* 2.5D extensions as per 99-402 */
-    wkbLineString25D = 0x80000002,
-    wkbPolygon25D = 0x80000003,
-    wkbMultiPoint25D = 0x80000004,
-    wkbMultiLineString25D = 0x80000005,
-    wkbMultiPolygon25D = 0x80000006,
-    wkbGeometryCollection25D = 0x80000007
+    wkbUnknown = 0,         /**< unknown type, non-standard */
+    wkbPoint = 1,           /**< 0-dimensional geometric object, standard WKB */
+    wkbLineString = 2,      /**< 1-dimensional geometric object with linear
+                             *   interpolation between Points, standard WKB */
+    wkbPolygon = 3,         /**< planar 2-dimensional geometric object defined
+                             *   by 1 exterior boundary and 0 or more interior
+                             *   boundaries, standard WKB */
+    wkbMultiPoint = 4,      /**< GeometryCollection of Points, standard WKB */
+    wkbMultiLineString = 5, /**< GeometryCollection of LineStrings, standard WKB */
+    wkbMultiPolygon = 6,    /**< GeometryCollection of Polygons, standard WKB */
+    wkbGeometryCollection = 7, /**< geometric object that is a collection of 1
+                                    or more geometric objects, standard WKB */
+    wkbNone = 100,          /**< non-standard, for pure attribute records */
+    wkbLinearRing = 101,    /**< non-standard, just for createGeometry() */
+    wkbPoint25D = 0x80000001, /**< 2.5D extension as per 99-402 */
+    wkbLineString25D = 0x80000002, /**< 2.5D extension as per 99-402 */
+    wkbPolygon25D = 0x80000003, /**< 2.5D extension as per 99-402 */
+    wkbMultiPoint25D = 0x80000004, /**< 2.5D extension as per 99-402 */
+    wkbMultiLineString25D = 0x80000005, /**< 2.5D extension as per 99-402 */
+    wkbMultiPolygon25D = 0x80000006, /**< 2.5D extension as per 99-402 */
+    wkbGeometryCollection25D = 0x80000007 /**< 2.5D extension as per 99-402 */
 } OGRwkbGeometryType;
 
 #define wkb25DBit 0x80000000
