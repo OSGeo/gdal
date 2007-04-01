a52a23c97591c9bbb0737565f1864dd118a3b674
Frank Warmerdam <warmerdam@pobox.com>
2007-04-01 23:58:15 +0000

    minimal netcdf driver tests
    git-svn-id: https://svn.osgeo.org/gdal/trunk@11154 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ autotest/gdrivers/netcdf.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+###############################################################################
+# $Id: pcraster.py 11065 2007-03-24 09:35:32Z mloskot $
+#
+# Project:  GDAL/OGR Test Suite
+# Purpose:  Test NetCDF driver support.
+# Author:   Frank Warmerdam <warmerdam@pobox.com>
+# 
+###############################################################################
+# Copyright (c) 2007, Frank Warmerdam <warmerdam@pobox.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+###############################################################################
+
+import os
+import sys
+import gdal
+
+sys.path.append( '../pymod' )
+
+import gdaltest
+
+###############################################################################
+# Perform simple read test.
+
+def netcdf_1():
+
+    gdaltest.netcdf_drv = gdal.GetDriverByName( 'NETCDF' )
+
+    if gdaltest.netcdf_drv is None:
+        return 'skip'
+
+    tst = gdaltest.GDALTest( 'NetCDF', 'NETCDF:"data/bug636.nc":tas', 1, 31621,
+                             filename_absolute = 1 )
+
+    # We don't want to gum up the test stream output with the
+    # 'Warning 1: No UNIDATA NC_GLOBAL:Conventions attribute' message.
+    gdal.PushErrorHandler( 'CPLQuietErrorHandler' )
+    result = tst.testOpen()
+    gdal.PopErrorHandler()
+
+    return result
+
+###############################################################################
+# Verify a simple createcopy operation.  We can't do the trivial gdaltest
+# operation because the new file will only be accessable via subdatasets!
+
+def netcdf_2():
+
+    if gdaltest.netcdf_drv is None:
+        return 'skip'
+
+    src_ds = gdal.Open( 'data/byte.tif' )
+    
+    base_ds = gdaltest.netcdf_drv.CreateCopy( 'tmp/netcdf2.nc', src_ds)
+
+    sds_md = base_ds.GetMetadata('SUBDATASETS')
+    if sds_md['SUBDATASET_1_NAME'] != 'NETCDF:"tmp/netcdf2.nc":Band1':
+        gdaltest.post_reason( 'wrong subdataset name.' )
+        return 'false'
+
+    tst = gdaltest.GDALTest( 'NetCDF', 'NETCDF:"tmp/netcdf2.nc":Band1',
+                             1, 4672,
+                             filename_absolute = 1 )
+
+    wkt = """PROJCS["NAD27 / UTM zone 11N",
+    GEOGCS["NAD27",
+        DATUM["North_American_Datum_1927",
+            SPHEROID["Clarke 1866",6378206.4,294.9786982139006,
+                AUTHORITY["EPSG","7008"]],
+            AUTHORITY["EPSG","6267"]],
+        PRIMEM["Greenwich",0],
+        UNIT["degree",0.0174532925199433],
+        AUTHORITY["EPSG","4267"]],
+    PROJECTION["Transverse_Mercator"],
+    PARAMETER["latitude_of_origin",0],
+    PARAMETER["central_meridian",-117],
+    PARAMETER["scale_factor",0.9996],
+    PARAMETER["false_easting",500000],
+    PARAMETER["false_northing",0],
+    UNIT["metre",1,
+        AUTHORITY["EPSG","9001"]],
+    AUTHORITY["EPSG","26711"]]"""
+
+    result = tst.testOpen( check_prj = wkt )
+
+    if result != 'success':
+        return result
+
+    base_ds = None
+    gdaltest.clean_tmp()
+
+    return 'success'
+    
+gdaltest_list = [
+    netcdf_1,
+    netcdf_2 ]
+
+
+if __name__ == '__main__':
+
+    gdaltest.setup_run( 'netcdf' )
+
+    gdaltest.run_tests( gdaltest_list )
+
+    gdaltest.summarize()
+
