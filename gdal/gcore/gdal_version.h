a76ef364ad1514df9ad763b0e7196fda4d0d3199
Frank Warmerdam <warmerdam@pobox.com>
2007-04-02 20:08:17 +0000

    Distinguish development verison from 1.4.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@11173 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/gcore/gdal_version.h
+++ gdal/gcore/gdal_version.h
@@ -5,7 +5,7 @@
 
 #ifndef GDAL_VERSION_MAJOR
 #  define GDAL_VERSION_MAJOR    1
-#  define GDAL_VERSION_MINOR    4
+#  define GDAL_VERSION_MINOR    5
 #  define GDAL_VERSION_REV      0
 #  define GDAL_VERSION_BUILD    0
 #endif
@@ -15,9 +15,9 @@
 #endif
 
 #ifndef GDAL_RELEASE_DATE
-#  define GDAL_RELEASE_DATE     20070102
+#  define GDAL_RELEASE_DATE     20070401
 #endif
 #ifndef GDAL_RELEASE_NAME
-#  define GDAL_RELEASE_NAME     "1.4.0.0"
+#  define GDAL_RELEASE_NAME     "1.5dev"
 #endif
 
