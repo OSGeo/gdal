b5fac9d26a6bd65bfa9ef7ccb02a7a0944f21fdb
Ivan Lucena <ivan.lucena@pmldnet.com>
2007-04-19 06:20:16 +0000

    removing gcc's warnings
    git-svn-id: https://svn.osgeo.org/gdal/trunk@11286 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/ingr/IngrTypes.h
+++ gdal/frmts/ingr/IngrTypes.h
@@ -58,8 +58,8 @@ typedef   signed char       int8;
 typedef unsigned char       uint8;
 typedef   signed short      int16;
 typedef unsigned short      uint16;
-typedef   signed int        int32;
-typedef unsigned int        uint32;
+//??typedef   signed int        int32; code on probation
+//??typedef unsigned int        uint32;
 typedef   signed long long  int64;
 typedef unsigned long long  uint64;
 typedef double              real64;
