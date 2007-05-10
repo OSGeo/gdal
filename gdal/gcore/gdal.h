f910e08d4d25f442ff3147bcb3c04b69250a4d1b
Tamas Szekeres <szekerest@gmail.com>
2007-05-10 20:24:14 +0000

    Added missing declarations for GDALIdentifyDriver
    git-svn-id: https://svn.osgeo.org/gdal/trunk@11457 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/gcore/gdal.h
+++ gdal/gcore/gdal.h
@@ -186,7 +186,7 @@ GDALDatasetH CPL_DLL CPL_STDCALL
 GDALCreateCopy( GDALDriverH, const char *, GDALDatasetH,
                 int, char **, GDALProgressFunc, void * );
 
-GDALDriverH CPL_STDCALL GDALIdentifyDriver( const char * pszFilename,
+GDALDriverH CPL_DLL CPL_STDCALL GDALIdentifyDriver( const char * pszFilename,
                                             char ** papszFileList );
 GDALDatasetH CPL_DLL CPL_STDCALL
 GDALOpen( const char *pszFilename, GDALAccess eAccess );
