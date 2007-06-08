f5a8337c426247848587ad2b5136b1a839a386b3
Frank Warmerdam <warmerdam@pobox.com>
2007-06-08 16:50:04 +0000

    do not run autoheader
    git-svn-id: https://svn.osgeo.org/gdal/branches/1.4@11623 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/autogen.sh
+++ gdal/autogen.sh
@@ -2,5 +2,7 @@
 set -x
 #libtoolize --force --copy
 aclocal -I ./m4
-autoheader
+# We deliberately do not use autoheader, since it introduces PACKAGE junk
+# that conflicts with other packages in cpl_config.h.in (FrankW)
+#autoheader
 autoconf
