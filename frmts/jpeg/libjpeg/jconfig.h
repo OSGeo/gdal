2dd529970250e9a1663786c6a976d39d7c52597e
Frank Warmerdam <warmerdam@pobox.com>
2007-06-11 19:55:11 +0000

    Make sure definition of HAVE stuff matches cpl_config.h.
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@11648 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/jpeg/libjpeg/jconfig.h
+++ frmts/jpeg/libjpeg/jconfig.h
@@ -8,8 +8,8 @@
 #undef void
 #undef const
 #undef CHAR_IS_UNSIGNED
-#define HAVE_STDDEF_H 
-#define HAVE_STDLIB_H 
+#define HAVE_STDDEF_H 1
+#define HAVE_STDLIB_H 1
 #undef NEED_BSD_STRINGS
 #undef NEED_SYS_TYPES_H
 #undef NEED_FAR_POINTERS
