94706096cee1c423707986c59ff3eed8cdfbeb01
Frank Warmerdam <warmerdam@pobox.com>
2007-06-27 18:44:04 +0000

    updated to 1.4.2
    git-svn-id: https://svn.osgeo.org/gdal/branches/1.4@11708 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/gcore/gdal_version.h
+++ gdal/gcore/gdal_version.h
@@ -6,7 +6,7 @@
 #ifndef GDAL_VERSION_MAJOR
 #  define GDAL_VERSION_MAJOR    1
 #  define GDAL_VERSION_MINOR    4
-#  define GDAL_VERSION_REV      1
+#  define GDAL_VERSION_REV      2
 #  define GDAL_VERSION_BUILD    0
 #endif
 
@@ -15,9 +15,9 @@
 #endif
 
 #ifndef GDAL_RELEASE_DATE
-#  define GDAL_RELEASE_DATE     20070409
+#  define GDAL_RELEASE_DATE     20070627
 #endif
 #ifndef GDAL_RELEASE_NAME
-#  define GDAL_RELEASE_NAME     "1.4.1.0"
+#  define GDAL_RELEASE_NAME     "1.4.2.0"
 #endif
 
