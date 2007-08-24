bb7b2b55e3c0da781dfb5b5a992522148be74035
Frank Warmerdam <warmerdam@pobox.com>
2007-08-24 18:02:14 +0000

    From Adam: md5 implementation switched for public domain one.  Added tileservice minidriver.  Match GDAL style more closely.
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@11955 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/wms/cache.cpp
+++ frmts/wms/cache.cpp
@@ -1,62 +1,91 @@
+/******************************************************************************
+*
+* Project:  WMS Client Driver
+* Purpose:  Implementation of Dataset and RasterBand classes for WMS
+*           and other similar services.
+* Author:   Adam Nowacki, nowak@xpam.de
+*
+******************************************************************************
+* Copyright (c) 2007, Adam Nowacki
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+****************************************************************************/
+
 #include "stdinc.h"
 
 GDALWMSCache::GDALWMSCache() {
-	m_cache_path = "./gdalwmscache";
-	m_postfix = "";
-	m_cache_depth = 2;
+    m_cache_path = "./gdalwmscache";
+    m_postfix = "";
+    m_cache_depth = 2;
 }
 
 GDALWMSCache::~GDALWMSCache() {
 }
 
 CPLErr GDALWMSCache::Initialize(CPLXMLNode *config) {
-	const char *cache_path = CPLGetXMLValue(config, "Path", "./gdalwmscache");
-	m_cache_path = cache_path;
+    const char *cache_path = CPLGetXMLValue(config, "Path", "./gdalwmscache");
+    m_cache_path = cache_path;
 
-	const char *cache_depth = CPLGetXMLValue(config, "Depth", "2");
-	m_cache_depth = atoi(cache_depth);
+    const char *cache_depth = CPLGetXMLValue(config, "Depth", "2");
+    m_cache_depth = atoi(cache_depth);
 
-	const char *cache_extension = CPLGetXMLValue(config, "Extension", "");
-	m_postfix = cache_extension;
+    const char *cache_extension = CPLGetXMLValue(config, "Extension", "");
+    m_postfix = cache_extension;
 
-	return CE_None;
+    return CE_None;
 }
 
 CPLErr GDALWMSCache::Write(const char *key, const CPLString &file_name) {
-	CPLString cache_file(KeyToCacheFile(key));
-	//	printf("GDALWMSCache::Write(%s, %s) -> %s\n", key, file_name.c_str());
-	if (CPLCopyFile(cache_file.c_str(), file_name.c_str()) != CE_None) {
-		MakeDirs(cache_file.c_str());
-		CPLCopyFile(cache_file.c_str(), file_name.c_str());
-	}
+    CPLString cache_file(KeyToCacheFile(key));
+    //	printf("GDALWMSCache::Write(%s, %s) -> %s\n", key, file_name.c_str());
+    if (CPLCopyFile(cache_file.c_str(), file_name.c_str()) != CE_None) {
+        MakeDirs(cache_file.c_str());
+        CPLCopyFile(cache_file.c_str(), file_name.c_str());
+    }
 
-	return CE_None;
+    return CE_None;
 }
 
 CPLErr GDALWMSCache::Read(const char *key, CPLString *file_name) {
-	CPLErr ret = CE_Failure;
-	CPLString cache_file(KeyToCacheFile(key));
-	FILE *f = VSIFOpen(cache_file.c_str(), "rb");
-	if (f != NULL) {
-		VSIFClose(f);
-		*file_name = cache_file;
-		ret = CE_None;
-	}
-	//    printf("GDALWMSCache::Read(...) -> %s\n", cache_file.c_str());
+    CPLErr ret = CE_Failure;
+    CPLString cache_file(KeyToCacheFile(key));
+    FILE *f = VSIFOpen(cache_file.c_str(), "rb");
+    if (f != NULL) {
+        VSIFClose(f);
+        *file_name = cache_file;
+        ret = CE_None;
+    }
+    //    printf("GDALWMSCache::Read(...) -> %s\n", cache_file.c_str());
 
-	return ret;
+    return ret;
 }
 
 CPLString GDALWMSCache::KeyToCacheFile(const char *key) {
-	CPLString hash(MD5String(key));
-	CPLString cache_file(m_cache_path);
+    CPLString hash(MD5String(key));
+    CPLString cache_file(m_cache_path);
 
-	if (cache_file.size() && (cache_file[cache_file.size() - 1] != '/')) cache_file.append(1, '/');
-	for (int i = 0; i < m_cache_depth; ++i) {
-		cache_file.append(1, hash[i]);
-		cache_file.append(1, '/');
-	}
-	cache_file.append(hash);
-	cache_file.append(m_postfix);
-	return cache_file;
+    if (cache_file.size() && (cache_file[cache_file.size() - 1] != '/')) cache_file.append(1, '/');
+    for (int i = 0; i < m_cache_depth; ++i) {
+        cache_file.append(1, hash[i]);
+        cache_file.append(1, '/');
+    }
+    cache_file.append(hash);
+    cache_file.append(m_postfix);
+    return cache_file;
 }
--- frmts/wms/gdalhttp.cpp
+++ frmts/wms/gdalhttp.cpp
@@ -1,166 +1,198 @@
+/******************************************************************************
+*
+* Project:  WMS Client Driver
+* Purpose:  Implementation of Dataset and RasterBand classes for WMS
+*           and other similar services.
+* Author:   Adam Nowacki, nowak@xpam.de
+*
+******************************************************************************
+* Copyright (c) 2007, Adam Nowacki
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+****************************************************************************/
+
 #include "stdinc.h"
 
 //#ifdef HAVE_CURL
 static size_t CPLHTTPWriteFunc(void *buffer, size_t count, size_t nmemb, void *req) {
-	CPLHTTPRequest *psRequest = reinterpret_cast<CPLHTTPRequest *>(req);
-	size_t size = count * nmemb;
+    CPLHTTPRequest *psRequest = reinterpret_cast<CPLHTTPRequest *>(req);
+    size_t size = count * nmemb;
 
-	if (size == 0) return 0;
+    if (size == 0) return 0;
 
-	const size_t required_size = psRequest->nDataLen + size + 1;
-	if (required_size > psRequest->nDataAlloc) {
-		size_t new_size = required_size * 2;
-		if (new_size < 512) new_size = 512;
-		psRequest->nDataAlloc = new_size;
-		psRequest->pabyData = reinterpret_cast<GByte *>(VSIRealloc(psRequest->pabyData, new_size));
-		if (psRequest->pabyData == NULL) {
-			psRequest->pszError = CPLStrdup(CPLString().Printf("Out of memory allocating %u bytes for HTTP data buffer.", static_cast<int>(new_size)));
-			psRequest->nDataAlloc = 0;
-			psRequest->nDataLen = 0;
-			return 0;
-		}
-	}
-	memcpy(psRequest->pabyData + psRequest->nDataLen, buffer, size);
-	psRequest->nDataLen += size;
-	psRequest->pabyData[psRequest->nDataLen] = 0;
-	return nmemb;
+    const size_t required_size = psRequest->nDataLen + size + 1;
+    if (required_size > psRequest->nDataAlloc) {
+        size_t new_size = required_size * 2;
+        if (new_size < 512) new_size = 512;
+        psRequest->nDataAlloc = new_size;
+        psRequest->pabyData = reinterpret_cast<GByte *>(VSIRealloc(psRequest->pabyData, new_size));
+        if (psRequest->pabyData == NULL) {
+            psRequest->pszError = CPLStrdup(CPLString().Printf("Out of memory allocating %u bytes for HTTP data buffer.", static_cast<int>(new_size)));
+            psRequest->nDataAlloc = 0;
+            psRequest->nDataLen = 0;
+            return 0;
+        }
+    }
+    memcpy(psRequest->pabyData + psRequest->nDataLen, buffer, size);
+    psRequest->nDataLen += size;
+    psRequest->pabyData[psRequest->nDataLen] = 0;
+    return nmemb;
 }
 //#endif /* def HAVE_CURL */
 
 void CPLHTTPInitializeRequest(CPLHTTPRequest *psRequest, const char *pszURL, const char **papszOptions) {
-	psRequest->pszURL = CPLStrdup(pszURL);
-	psRequest->papszOptions = CSLDuplicate(const_cast<char **>(papszOptions));
-	psRequest->nStatus = 0;
-	psRequest->pszContentType = 0;
-	psRequest->pszError = 0;
-	psRequest->pabyData = 0;
-	psRequest->nDataLen = 0;
-	psRequest->nDataAlloc = 0;
-	psRequest->m_curl_handle = 0;
-	psRequest->m_headers = 0;
-	psRequest->m_curl_error = 0;
+    psRequest->pszURL = CPLStrdup(pszURL);
+    psRequest->papszOptions = CSLDuplicate(const_cast<char **>(papszOptions));
+    psRequest->nStatus = 0;
+    psRequest->pszContentType = 0;
+    psRequest->pszError = 0;
+    psRequest->pabyData = 0;
+    psRequest->nDataLen = 0;
+    psRequest->nDataAlloc = 0;
+    psRequest->m_curl_handle = 0;
+    psRequest->m_headers = 0;
+    psRequest->m_curl_error = 0;
 
-	psRequest->m_curl_handle = curl_easy_init();
-	if (psRequest->m_curl_handle == NULL) {
-		CPLError(CE_Fatal, CPLE_AppDefined, "CPLHTTPInitializeRequest(): Unable to create CURL handle.\n");
-	}
+    psRequest->m_curl_handle = curl_easy_init();
+    if (psRequest->m_curl_handle == NULL) {
+        CPLError(CE_Fatal, CPLE_AppDefined, "CPLHTTPInitializeRequest(): Unable to create CURL handle.\n");
+    }
 
-	curl_easy_setopt(psRequest->m_curl_handle, CURLOPT_URL, psRequest->pszURL);
+    curl_easy_setopt(psRequest->m_curl_handle, CURLOPT_URL, psRequest->pszURL);
 
-	const char *timeout = CSLFetchNameValue(const_cast<char **>(psRequest->papszOptions), "TIMEOUT");
-	if (timeout != NULL) {
-		curl_easy_setopt(psRequest->m_curl_handle, CURLOPT_TIMEOUT, atoi(timeout));
-	}
+    const char *timeout = CSLFetchNameValue(const_cast<char **>(psRequest->papszOptions), "TIMEOUT");
+    if (timeout != NULL) {
+        curl_easy_setopt(psRequest->m_curl_handle, CURLOPT_TIMEOUT, atoi(timeout));
+    }
 
-	const char *headers = CSLFetchNameValue(const_cast<char **>(psRequest->papszOptions), "HEADERS");
-	if (headers != NULL) {
-		psRequest->m_headers = curl_slist_append(psRequest->m_headers, headers);
-		curl_easy_setopt(psRequest->m_curl_handle, CURLOPT_HTTPHEADER, psRequest->m_headers);
-	}
+    const char *headers = CSLFetchNameValue(const_cast<char **>(psRequest->papszOptions), "HEADERS");
+    if (headers != NULL) {
+        psRequest->m_headers = curl_slist_append(psRequest->m_headers, headers);
+        curl_easy_setopt(psRequest->m_curl_handle, CURLOPT_HTTPHEADER, psRequest->m_headers);
+    }
 
-	/* Enable following redirections.  Requires libcurl 7.10.1 at least */
-	curl_easy_setopt(psRequest->m_curl_handle, CURLOPT_FOLLOWLOCATION, 1);
-	curl_easy_setopt(psRequest->m_curl_handle, CURLOPT_MAXREDIRS, 10);
+    /* Enable following redirections.  Requires libcurl 7.10.1 at least */
+    curl_easy_setopt(psRequest->m_curl_handle, CURLOPT_FOLLOWLOCATION, 1);
+    curl_easy_setopt(psRequest->m_curl_handle, CURLOPT_MAXREDIRS, 10);
 
-	/* NOSIGNAL should be set to true for timeout to work in multithread
-	environments on Unix, requires libcurl 7.10 or more recent.
-	(this force avoiding the use of sgnal handlers) */
+    /* NOSIGNAL should be set to true for timeout to work in multithread
+    environments on Unix, requires libcurl 7.10 or more recent.
+    (this force avoiding the use of sgnal handlers) */
 #ifdef CURLOPT_NOSIGNAL
-	curl_easy_setopt(psRequest->m_curl_handle, CURLOPT_NOSIGNAL, 1);
+    curl_easy_setopt(psRequest->m_curl_handle, CURLOPT_NOSIGNAL, 1);
 #endif
 
-	curl_easy_setopt(psRequest->m_curl_handle, CURLOPT_WRITEDATA, psRequest);
-	curl_easy_setopt(psRequest->m_curl_handle, CURLOPT_WRITEFUNCTION, CPLHTTPWriteFunc);
+    curl_easy_setopt(psRequest->m_curl_handle, CURLOPT_WRITEDATA, psRequest);
+    curl_easy_setopt(psRequest->m_curl_handle, CURLOPT_WRITEFUNCTION, CPLHTTPWriteFunc);
 
-	psRequest->m_curl_error = reinterpret_cast<char *>(CPLMalloc(CURL_ERROR_SIZE + 1));
-	curl_easy_setopt(psRequest->m_curl_handle, CURLOPT_ERRORBUFFER, psRequest->m_curl_error);
+    psRequest->m_curl_error = reinterpret_cast<char *>(CPLMalloc(CURL_ERROR_SIZE + 1));
+    curl_easy_setopt(psRequest->m_curl_handle, CURLOPT_ERRORBUFFER, psRequest->m_curl_error);
 }
 
 void CPLHTTPCleanupRequest(CPLHTTPRequest *psRequest) {
-	if (psRequest->m_curl_handle) {
-		curl_easy_cleanup(psRequest->m_curl_handle);
-		psRequest->m_curl_handle = 0;
-	}
-	if (psRequest->m_headers) {
-		curl_slist_free_all(psRequest->m_headers);
-		psRequest->m_headers = 0;
-	}
-	if (psRequest->m_curl_error) {
-		CPLFree(psRequest->m_curl_error);
-		psRequest->m_curl_error = 0;
-	}
+    if (psRequest->m_curl_handle) {
+        curl_easy_cleanup(psRequest->m_curl_handle);
+        psRequest->m_curl_handle = 0;
+    }
+    if (psRequest->m_headers) {
+        curl_slist_free_all(psRequest->m_headers);
+        psRequest->m_headers = 0;
+    }
+    if (psRequest->m_curl_error) {
+        CPLFree(psRequest->m_curl_error);
+        psRequest->m_curl_error = 0;
+    }
 
-	if (psRequest->pszContentType) {
-		CPLFree(psRequest->pszContentType);
-		psRequest->pszContentType = 0;
-	}
-	if (psRequest->pszError) {
-		CPLFree(psRequest->pszError);
-		psRequest->pszError = 0;
-	}
-	if (psRequest->pabyData) {
-		CPLFree(psRequest->pabyData);
-		psRequest->pabyData = 0;
-		psRequest->nDataLen = 0;
-		psRequest->nDataAlloc = 0;
-	}
-	if (psRequest->papszOptions) {
-		CSLDestroy(psRequest->papszOptions);
-		psRequest->papszOptions = 0;
-	}
-	if (psRequest->pszURL) {
-		CPLFree(psRequest->pszURL);
-		psRequest->pszURL = 0;
-	}
+    if (psRequest->pszContentType) {
+        CPLFree(psRequest->pszContentType);
+        psRequest->pszContentType = 0;
+    }
+    if (psRequest->pszError) {
+        CPLFree(psRequest->pszError);
+        psRequest->pszError = 0;
+    }
+    if (psRequest->pabyData) {
+        CPLFree(psRequest->pabyData);
+        psRequest->pabyData = 0;
+        psRequest->nDataLen = 0;
+        psRequest->nDataAlloc = 0;
+    }
+    if (psRequest->papszOptions) {
+        CSLDestroy(psRequest->papszOptions);
+        psRequest->papszOptions = 0;
+    }
+    if (psRequest->pszURL) {
+        CPLFree(psRequest->pszURL);
+        psRequest->pszURL = 0;
+    }
 }
 
-void CPLHTTPFetchMulti(CPLHTTPRequest *pasRequest, int nRequestCount) {
-	CURLM *curl_multi = 0;
-	int still_running;
+CPLErr CPLHTTPFetchMulti(CPLHTTPRequest *pasRequest, int nRequestCount) {
+    CPLErr ret = CE_None;
+    CURLM *curl_multi = 0;
+    int still_running;
 
-	curl_multi = curl_multi_init();
-	if (curl_multi == NULL) {
-		CPLError(CE_Fatal, CPLE_AppDefined, "CPLHTTPFetchMulti(): Unable to create CURL multi-handle.\n");
-	}
+    curl_multi = curl_multi_init();
+    if (curl_multi == NULL) {
+        CPLError(CE_Fatal, CPLE_AppDefined, "CPLHTTPFetchMulti(): Unable to create CURL multi-handle.\n");
+    }
 
-	for (int i = 0; i < nRequestCount; ++i) {
-		CPLHTTPRequest *const psRequest = &pasRequest[i];
+    for (int i = 0; i < nRequestCount; ++i) {
+        CPLHTTPRequest *const psRequest = &pasRequest[i];
 
-		curl_multi_add_handle(curl_multi, psRequest->m_curl_handle);
-	}
+        curl_multi_add_handle(curl_multi, psRequest->m_curl_handle);
+    }
 
-	while (curl_multi_perform(curl_multi, &still_running) == CURLM_CALL_MULTI_PERFORM);
-	while (still_running) {
-		struct timeval timeout;
-		fd_set fdread, fdwrite, fdexcep;
-		int maxfd;
+    while (curl_multi_perform(curl_multi, &still_running) == CURLM_CALL_MULTI_PERFORM);
+    while (still_running) {
+        struct timeval timeout;
+        fd_set fdread, fdwrite, fdexcep;
+        int maxfd;
 
-		FD_ZERO(&fdread);
-		FD_ZERO(&fdwrite);
-		FD_ZERO(&fdexcep);
-		curl_multi_fdset(curl_multi, &fdread, &fdwrite, &fdexcep, &maxfd);
-		timeout.tv_sec = 0;
-		timeout.tv_usec = 100000;
-		select(maxfd + 1, &fdread, &fdwrite, &fdexcep, &timeout);
-		while (curl_multi_perform(curl_multi, &still_running) == CURLM_CALL_MULTI_PERFORM);
-	}
+        FD_ZERO(&fdread);
+        FD_ZERO(&fdwrite);
+        FD_ZERO(&fdexcep);
+        curl_multi_fdset(curl_multi, &fdread, &fdwrite, &fdexcep, &maxfd);
+        timeout.tv_sec = 0;
+        timeout.tv_usec = 100000;
+        select(maxfd + 1, &fdread, &fdwrite, &fdexcep, &timeout);
+        while (curl_multi_perform(curl_multi, &still_running) == CURLM_CALL_MULTI_PERFORM);
+    }
 
-	for (int i = 0; i < nRequestCount; ++i) {
-		CPLHTTPRequest *const psRequest = &pasRequest[i];
+    for (int i = 0; i < nRequestCount; ++i) {
+        CPLHTTPRequest *const psRequest = &pasRequest[i];
 
-		long response_code = 0;
-		curl_easy_getinfo(psRequest->m_curl_handle, CURLINFO_RESPONSE_CODE, &response_code);
-		psRequest->nStatus = response_code;
+        long response_code = 0;
+        curl_easy_getinfo(psRequest->m_curl_handle, CURLINFO_RESPONSE_CODE, &response_code);
+        psRequest->nStatus = response_code;
 
-		char *content_type = 0;
-		curl_easy_getinfo(psRequest->m_curl_handle, CURLINFO_CONTENT_TYPE, &content_type);
-		if (content_type) psRequest->pszContentType = CPLStrdup(content_type);
+        char *content_type = 0;
+        curl_easy_getinfo(psRequest->m_curl_handle, CURLINFO_CONTENT_TYPE, &content_type);
+        if (content_type) psRequest->pszContentType = CPLStrdup(content_type);
 
-		if ((psRequest->pszError == NULL) && (psRequest->m_curl_error != NULL) && (psRequest->m_curl_error[0] != '\0')) {
-			psRequest->pszError = CPLStrdup(psRequest->m_curl_error);
-		}
+        if ((psRequest->pszError == NULL) && (psRequest->m_curl_error != NULL) && (psRequest->m_curl_error[0] != '\0')) {
+            psRequest->pszError = CPLStrdup(psRequest->m_curl_error);
+        }
 
-		curl_multi_remove_handle(curl_multi, pasRequest[i].m_curl_handle);
-	}
-	curl_multi_cleanup(curl_multi);
+        curl_multi_remove_handle(curl_multi, pasRequest[i].m_curl_handle);
+    }
+    curl_multi_cleanup(curl_multi);
+
+    return ret;
 }
