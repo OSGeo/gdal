36a4a09fa66923acce59717d8d8f8009039de71b
Pirmin Kalberer <pka@sourcepole.ch>
2007-08-29 14:07:30 +0000

    Fix for area polygonisation with crossing lines (curves) as described in https://trac.osgeo.org/gdal/ticket/1613
    git-svn-id: https://svn.osgeo.org/gdal/trunk@11992 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/ili/ili1readerp.h
+++ gdal/ogr/ogrsf_frmts/ili/ili1readerp.h
@@ -14,16 +14,16 @@
  * the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included
  * in all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
  * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  ****************************************************************************/
 
@@ -43,7 +43,7 @@ class OGRILI1Layer;
 /************************************************************************/
 
 class ILI1Reader : public IILI1Reader
-{    
+{
 private:
     FILE         *fpItf;
     int          nLayers;
@@ -73,7 +73,7 @@ public:
     void         AddAreaLayer( OGRILI1Layer * poAreaLayer,  OGRILI1Layer * poLineLayer );
     void         AddSurfaceLayer( OGRILI1Layer * poDataLayer,  OGRILI1Layer * poPolyLayer );
     int          AddIliGeom(OGRFeature *feature, int iField, long fpos);
-    OGRMultiPolygon* Polygonize( OGRGeometryCollection* poLines );
+    OGRMultiPolygon* Polygonize( OGRGeometryCollection* poLines, bool fix_crossing_lines = false );
     void         PolygonizeAreaLayers();
     void         JoinSurfaceLayers();
     OGRILI1Layer *GetLayer( int );
