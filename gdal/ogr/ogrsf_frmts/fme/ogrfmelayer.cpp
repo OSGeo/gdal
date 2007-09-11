a5479b969c012089d9dd4ecd8a5d8d5834f8b4f5
Frank Warmerdam <warmerdam@pobox.com>
2007-09-11 23:57:40 +0000

    License altered to MIT/X with explicit permission from Dale Lutz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@12123 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/fme/ogrfmelayer.cpp
+++ gdal/ogr/ogrsf_frmts/fme/ogrfmelayer.cpp
@@ -9,19 +9,24 @@
  *
  ******************************************************************************
  * Copyright (c) 1999, 2001 Safe Software Inc.
- * All Rights Reserved
  *
- * This software may not be copied or reproduced, in all or in part, 
- * without the prior written consent of Safe Software Inc.
- *
- * The entire risk as to the results and performance of the software,
- * supporting text and other information contained in this file
- * (collectively called the "Software") is with the user.  Although
- * Safe Software Incorporated has used considerable efforts in preparing 
- * the Software, Safe Software Incorporated does not warrant the
- * accuracy or completeness of the Software. In no event will Safe Software 
- * Incorporated be liable for damages, including loss of profits or 
- * consequential damages, arising out of the use of the Software.
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
  ****************************************************************************/
 
 #include "fme2ogr.h"
