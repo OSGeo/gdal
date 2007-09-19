a18390d9e53be23b0cef59b6ba79651b2698a376
Frank Warmerdam <warmerdam@pobox.com>
2007-09-19 14:48:25 +0000

    Migrate sfcom_oledb out of trunk per PSC motion (https://trac.osgeo.org/gdal/ticket/1817)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@12204 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/sfcom_oledb/sftraceback.h
+++
@@ -1,6 +0,0 @@
-#include "stdafx.h"
-
-IDBProperties *SFGetDataSourceProperties(ICommand *pICommand);
-IDBProperties *SFGetDataSourceProperties(IRowsetInfo* pIRInfo);
-IDBProperties *SFGetDataSourceProperties(IGetDataSource *pIGetDataSource);
-IDBProperties *SFGetDataSourceProperties(IUnknown *pIUnknown);
