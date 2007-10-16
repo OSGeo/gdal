f52a46b2e54e174fc5dbe6c71f0bdb9ad574a82b
Frank Warmerdam <warmerdam@pobox.com>
2007-10-16 20:30:40 +0000

    simple test of virtual warp using geolocation (#1919)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@12445 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ autotest/gcore/geoloc.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+###############################################################################
+# $Id: vrtwarp.py 12181 2007-09-17 20:42:03Z warmerdam $
+#
+# Project:  GDAL/OGR Test Suite
+# Purpose:  Test Geolocation warper.
+# Author:   Frank Warmerdam <warmerdam@pobox.com>
+# 
+###############################################################################
+# Copyright (c) 2007, Frank Warmerdam <warmerdam@pobox.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+###############################################################################
+
+import os
+import sys
+import gdal
+
+sys.path.append( '../pymod' )
+
+import gdaltest
+
+###############################################################################
+# Verify warped result.
+
+def geoloc_1():
+
+    tst = gdaltest.GDALTest( 'VRT', 'warpsst.vrt', 1, 62558 )
+    return tst.testOpen()
+
+
+gdaltest_list = [
+    geoloc_1 ]
+
+if __name__ == '__main__':
+
+    gdaltest.setup_run( 'geoloc' )
+
+    gdaltest.run_tests( gdaltest_list )
+
+    gdaltest.summarize()
+
