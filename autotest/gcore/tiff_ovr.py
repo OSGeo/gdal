0d2e507f124de5875738c4883176d72e38530497
Frank Warmerdam <warmerdam@pobox.com>
2007-11-04 00:41:03 +0000

    added testing of big2grayscale overviews (https://trac.osgeo.org/gdal/ticket/1908)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@12614 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/gcore/tiff_ovr.py
+++ autotest/gcore/tiff_ovr.py
@@ -31,6 +31,7 @@
 import os
 import sys
 import gdal
+import shutil
 
 sys.path.append( '../pymod' )
 
@@ -141,13 +142,70 @@ def tiff_ovr_3():
 
     return 'success'
 
+###############################################################################
+# Test generation 
 
+def tiff_ovr_4():
+
+    shutil.copyfile( 'data/oddsize_1bit2b.tif', 'tmp/ovr4.tif' )
+
+    wrk_ds = gdal.Open('tmp/ovr4.tif',gdal.GA_Update)
+    wrk_ds.BuildOverviews( 'AVERAGE_BIT2GRAYSCALE', overviewlist = [2,4] )
+    wrk_ds = None
+
+    wrk_ds = gdal.Open('tmp/ovr4.tif')
+
+    ovband = wrk_ds.GetRasterBand(1).GetOverview(1)
+    md = ovband.GetMetadata()
+    if not md.has_key('RESAMPLING') \
+       or md['RESAMPLING'] != 'AVERAGE_BIT2GRAYSCALE':
+        gdaltest.post_reason( 'Did not get expected RESAMPLING metadata.' )
+        return 'fail'
+
+    # compute average value of overview band image data.
+    ovimage = ovband.ReadRaster(0,0,ovband.XSize,ovband.YSize)
+
+    pix_count = ovband.XSize * ovband.YSize
+    sum = 0.0
+    for i in range(pix_count):
+        sum = sum + ord(ovimage[i])
+    average = sum / pix_count
+    exp_average = 154.8144
+    if abs(average - exp_average) > 0.1:
+        print average
+        gdaltest.post_reason( 'got wrong average for overview image' )
+        return 'fail'
+
+    # read base band as overview resolution and verify we aren't getting
+    # the grayscale image.
+
+    frband = wrk_ds.GetRasterBand(1)
+    ovimage = frband.ReadRaster(0,0,frband.XSize,frband.YSize,
+                                ovband.XSize,ovband.YSize)
+
+    pix_count = ovband.XSize * ovband.YSize
+    sum = 0.0
+    for i in range(pix_count):
+        sum = sum + ord(ovimage[i])
+    average = sum / pix_count
+    exp_average = 0.6096
+    
+    if abs(average - exp_average) > 0.01:
+        print average
+        gdaltest.post_reason( 'got wrong average for downsampled image' )
+        return 'fail'
+
+    wrk_ds = None
+
+    return 'success'
+    
 
 ###############################################################################
 # Cleanup
 
 def tiff_ovr_cleanup():
     gdaltest.tiff_drv.Delete( 'tmp/mfloat32.tif' )
+    gdaltest.tiff_drv.Delete( 'tmp/ovr4.tif' )
     gdaltest.tiff_drv = None
 
     return 'success'
@@ -156,6 +214,7 @@ gdaltest_list = [
     tiff_ovr_1,
     tiff_ovr_2,
     tiff_ovr_3,
+    tiff_ovr_4,
     tiff_ovr_cleanup ]
 
 if __name__ == '__main__':
