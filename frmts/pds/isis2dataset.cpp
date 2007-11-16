b2ef805c9ac397b15fe4be9e5195bcf087f6dc15
Frank Warmerdam <warmerdam@pobox.com>
2007-11-16 16:36:34 +0000

    Restore PAM support lost in Trent's patch (#1996).
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@12802 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/pds/isis2dataset.cpp
+++ frmts/pds/isis2dataset.cpp
@@ -675,6 +675,12 @@ GDALDataset *ISIS2Dataset::Open( GDALOpenInfo * poOpenInfo )
 /* -------------------------------------------------------------------- */
     poDS->oOvManager.Initialize( poDS, poOpenInfo->pszFilename );
 
+/* -------------------------------------------------------------------- */ 
+/*      Initialize any PAM information.                                 */ 
+/* -------------------------------------------------------------------- */ 
+    poDS->SetDescription( poOpenInfo->pszFilename ); 
+    poDS->TryLoadXML(); 
+
     return( poDS );
 }
 
