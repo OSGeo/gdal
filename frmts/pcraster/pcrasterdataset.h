d6ba40dbaafe018c551cb0c2310d54a16f58ad16
Frank Warmerdam <warmerdam@pobox.com>
2007-11-29 15:08:00 +0000

    clarified licensing with permission of Kor (#1822)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@13149 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/pcraster/pcrasterdataset.h
+++ frmts/pcraster/pcrasterdataset.h
@@ -1,7 +1,34 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  PCRaster Integration
+ * Purpose:  PCRaster CSF 2.0 raster file driver declarations.
+ * Author:   Kor de Jong, k.dejong at geog.uu.nl
+ *
+ ******************************************************************************
+ * Copyright (c) 2004, Kor de Jong
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+
 #ifndef INCLUDED_PCRASTERDATASET
 #define INCLUDED_PCRASTERDATASET
-
-
 
 // Library headers.
 
