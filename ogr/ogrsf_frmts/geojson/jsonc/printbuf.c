7ad901c8549cd05a3bd1e4498280cfa05e02d253
Frank Warmerdam <warmerdam@pobox.com>
2007-12-02 18:32:25 +0000

    use CPLVASPrintf() to avoid porting problems (#2044)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@13198 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/ogrsf_frmts/geojson/jsonc/printbuf.c
+++ ogr/ogrsf_frmts/geojson/jsonc/printbuf.c
@@ -26,6 +26,8 @@
 #include "debug.h"
 #include "printbuf.h"
 
+#include "cpl_string.h"
+
 struct printbuf* printbuf_new()
 {
   struct printbuf *p;
@@ -61,73 +63,19 @@ int printbuf_memappend(struct printbuf *p, char *buf, int size)
   return size;
 }
 
-#if !HAVE_VSNPRINTF && defined(WIN32)
-# define vsnprintf _vsnprintf
-#elif !HAVE_VSNPRINTF /* !HAVE_VSNPRINTF */
-# error Need vsnprintf!
-#endif /* !HAVE_VSNPRINTF && defined(WIN32) */
-
-#ifndef HAVE_VASPRINTF
-/* CAW: compliant version of vasprintf */
-static int vasprintf(char **buf, const char *fmt, va_list ap)
-{
-#ifndef WIN32
-	static char _T_emptybuffer = '\0';
-#endif /* !defined(WIN32) */
-	int chars;
-	char *b;
-
-	if(!buf) { return -1; }
-
-#ifdef WIN32
-	chars = _vscprintf(fmt, ap)+1;
-#else /* !defined(WIN32) */
-	/* CAW: RAWR! We have to hope to god here that vsnprintf doesn't overwrite
-	   our buffer like on some 64bit sun systems.... but hey, its time to move on */
-	chars = vsnprintf(&_T_emptybuffer, 0, fmt, ap)+1;
-	if(chars < 0) { chars *= -1; } /* CAW: old glibc versions have this problem */
-#endif /* defined(WIN32) */
-
-	b = (char*)malloc(sizeof(char)*chars);
-	if(!b) { return -1; }
-
-	if((chars = vsprintf(b, fmt, ap)) < 0)
-	{
-		free(b);
-	} else {
-		*buf = b;
-	}
-
-	return chars;
-}
-#endif /* !HAVE_VASPRINTF */
-
 int sprintbuf(struct printbuf *p, const char *msg, ...)
 {
   va_list ap;
   char *t;
-  int size;
-  char buf[128];
+  int size, ret;
 
-  /* user stack buffer first */
   va_start(ap, msg);
-  size = vsnprintf(buf, 128, msg, ap);
+  if((size = CPLVASPrintf(&t, msg, ap)) == -1) return -1;
   va_end(ap);
-  /* if string is greater than stack buffer, then use dynamic string
-     with vasprintf.  Note: some implementation of vsnprintf return -1
-     if output is truncated whereas some return the number of bytes that
-     would have been writen - this code handles both cases. */
-  if(size == -1 || size > 127) {
-    int ret;
-    va_start(ap, msg);
-    if((size = vasprintf(&t, msg, ap)) == -1) return -1;
-    va_end(ap);
-    ret = printbuf_memappend(p, t, size);
-    free(t);
-    return ret;
-  } else {
-    return printbuf_memappend(p, buf, size);
-  }
+  
+  ret = printbuf_memappend(p, t, size);
+  free(t);
+  return ret;
 }
 
 void printbuf_reset(struct printbuf *p)
