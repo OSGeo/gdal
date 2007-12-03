af84c1b88acb8c677e945c0de5bb1c483c9940ea
Ari Jolma <ari.jolma@tkk.fi>
2007-12-03 20:57:42 +0000

    better make(.bat) for mingw dos prompt
    git-svn-id: https://svn.osgeo.org/gdal/trunk@13215 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/swig/perl/clean.bat
+++
@@ -1,9 +0,0 @@
-del tmp_ds_*
-del *.gmt
-rd tmp_ds_ESRIShapefile /S /Q
-rd tmp_ds_MapInfoFile /S /Q
-rd tmp_ds_KML /S /Q
-rd tmp_ds_BNA /S /Q
-rd tmp_ds_GMT /S /Q
-rd tmp_ds_GPX /S /Q
-rd tmp_ds_GeoJSON /S /Q
