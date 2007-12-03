abb95f688801815cb7e4e135f9d75dec625a7cbc
Ari Jolma <ari.jolma@tkk.fi>
2007-12-03 20:57:42 +0000

    better make(.bat) for mingw dos prompt
    git-svn-id: https://svn.osgeo.org/gdal/trunk@13215 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/swig/perl/make.bat
+++ gdal/swig/perl/make.bat
@@ -1,4 +1,32 @@
 dmake -f Makefile_Geo__GDAL %1
-dmake -f Makefile_Geo__GDAL__Const
-dmake -f Makefile_Geo__OGR
-dmake -f Makefile_Geo__OSR
+
+if "%1" == "test" goto clean
+
+dmake -f Makefile_Geo__GDAL__Const %1
+dmake -f Makefile_Geo__OGR %1
+dmake -f Makefile_Geo__OSR %1
+
+if "%1" == "clean" goto clean
+
+goto end
+
+:clean
+echo off
+del tmp_ds_*
+del *.gmt
+rd tmp_ds_ESRIShapefile /S /Q
+rd tmp_ds_MapInfoFile /S /Q
+rd tmp_ds_KML /S /Q
+rd tmp_ds_BNA /S /Q
+rd tmp_ds_GMT /S /Q
+rd tmp_ds_GPX /S /Q
+rd tmp_ds_GeoJSON /S /Q
+
+if "%1" == "test" goto end
+
+del *.c
+del *.cpp
+del *.old
+rd lib /S /Q
+
+:end
