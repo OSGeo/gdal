0c0d8aec9e392b2dd27225d9f26c38a3d665a7c7
Pirmin Kalberer <pka@sourcepole.ch>
2007-12-17 14:52:10 +0000

    Patch https://trac.osgeo.org/gdal/ticket/2042 applied - Fixes XML parser memory leaks Warning in GEOS 2 compilation
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@13354 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/ogrsf_frmts/ili/ili2reader.cpp
+++ ogr/ogrsf_frmts/ili/ili2reader.cpp
@@ -62,11 +62,11 @@ static const char *ILI2_AREA = "AREA";
 static const char *ILI2_SURFACE = "SURFACE";
 
 
-// 
+//
 // helper functions
-// 
+//
 int cmpStr(string s1, string s2) {
-  
+
   string::const_iterator p1 = s1.begin();
   string::const_iterator p2 = s2.begin();
 
@@ -101,7 +101,7 @@ string trim(string tmpstr) {
 
 int getGeometryTypeOfElem(DOMElement* elem) {
   int type = ILI2_STRING_TYPE;
-  
+
   if (elem && elem->getNodeType() == DOMNode::ELEMENT_NODE) {
     if (cmpStr(ILI2_COORD, XMLString::transcode(elem->getTagName())) == 0) {
       type = ILI2_COORD_TYPE;
@@ -122,21 +122,21 @@ int getGeometryTypeOfElem(DOMElement* elem) {
 
 char *getObjValue(DOMElement *elem) {
   DOMElement *textElem = (DOMElement *)elem->getFirstChild();
-  
+
   if ((textElem != NULL) && (textElem->getNodeType() == DOMNode::TEXT_NODE))
     return CPLStrdup(XMLString::transcode(textElem->getNodeValue()));
-  
+
   return NULL;
 }
 
-char *getREFValue(DOMElement *elem) {  
+char *getREFValue(DOMElement *elem) {
   return CPLStrdup(XMLString::transcode(elem->getAttribute(XMLString::transcode(ILI2_REF))));
 }
 
 OGRPoint *getPoint(DOMElement *elem) {
   // elem -> COORD (or ARC)
   OGRPoint *pt = new OGRPoint();
-  
+
   DOMElement *coordElem = (DOMElement *)elem->getFirstChild();
   while (coordElem != NULL) {
     if (cmpStr("C1", XMLString::transcode(coordElem->getTagName())) == 0)
@@ -158,10 +158,10 @@ OGRLineString *ILI2Reader::getArc(DOMElement *elem) {
   OGRPoint *ptStart = getPoint((DOMElement *)elem->getPreviousSibling()); // COORD or ARC
   // end point
   OGRPoint *ptEnd = new OGRPoint();
-  // point on the arc 
+  // point on the arc
   OGRPoint *ptOnArc = new OGRPoint();
   double radius = 0; // radius
-  
+
   DOMElement *arcElem = (DOMElement *)elem->getFirstChild();
   while (arcElem != NULL) {
     if (cmpStr("C1", XMLString::transcode(arcElem->getTagName())) == 0)
@@ -190,7 +190,7 @@ OGRLineString *ILI2Reader::getArc(DOMElement *elem) {
 OGRLineString *getLineString(DOMElement *elem) {
   // elem -> POLYLINE
   OGRLineString *ls = new OGRLineString();
-  
+
   DOMElement *lineElem = (DOMElement *)elem->getFirstChild();
   while (lineElem != NULL) {
     if (cmpStr(ILI2_COORD, XMLString::transcode(lineElem->getTagName())) == 0)
@@ -198,13 +198,13 @@ OGRLineString *getLineString(DOMElement *elem) {
     else if (cmpStr(ILI2_ARC, XMLString::transcode(lineElem->getTagName())) == 0) {
       // end point
       OGRPoint *ptEnd = new OGRPoint();
-      // point on the arc 
+      // point on the arc
       OGRPoint *ptOnArc = new OGRPoint();
       // radius
       double radius = 0;
-      
+
       DOMElement *arcElem = (DOMElement *)lineElem->getFirstChild();
-      while (arcElem != NULL) {        
+      while (arcElem != NULL) {
         if (cmpStr("C1", XMLString::transcode(arcElem->getTagName())) == 0)
           ptEnd->setX(atof(getObjValue(arcElem)));
         else if (cmpStr("C2", XMLString::transcode(arcElem->getTagName())) == 0)
@@ -219,44 +219,44 @@ OGRLineString *getLineString(DOMElement *elem) {
           ptOnArc->setZ(atof(getObjValue(arcElem)));
         else if (cmpStr("R", XMLString::transcode(arcElem->getTagName())) == 0)
           radius = atof(getObjValue(arcElem));
-        
+
         arcElem = (DOMElement *)arcElem->getNextSibling();
       }
-      
+
       ptEnd->flattenTo2D();
       ptOnArc->flattenTo2D();
       OGRPoint *ptStart = getPoint((DOMElement *)lineElem->getPreviousSibling()); // COORD or ARC
       interpolateArc(ls, ptStart, ptOnArc, ptEnd, PI/180);
     } /* else { // FIXME StructureValue in Polyline not yet supported
     } */
-        
+
     lineElem = (DOMElement *)lineElem->getNextSibling();
   }
-  
+
   return ls;
 }
 
 OGRLineString *getBoundary(DOMElement *elem) {
-  
+
   DOMElement *lineElem = (DOMElement *)elem->getFirstChild();
   if (lineElem != NULL)
     if (cmpStr(ILI2_POLYLINE, XMLString::transcode(lineElem->getTagName())) == 0)
       return getLineString(lineElem);
-  
+
   return new OGRLineString;
 }
 
 OGRPolygon *getPolygon(DOMElement *elem) {
   OGRPolygon *pg = new OGRPolygon();
-  
+
   DOMElement *boundaryElem = (DOMElement *)elem->getFirstChild(); // outer boundary
   while (boundaryElem != NULL) {
     if (cmpStr(ILI2_BOUNDARY, XMLString::transcode(boundaryElem->getTagName())) == 0)
       pg->addRing((OGRLinearRing *)getBoundary(boundaryElem));
-        
+
     boundaryElem = (DOMElement *)boundaryElem->getNextSibling(); // inner boundaries
   }
-  
+
   return pg;
 }
 
@@ -266,7 +266,7 @@ OGRGeometry *ILI2Reader::getGeometry(DOMElement *elem, int type) {
   DOMElement *childElem = elem;
   while (childElem != NULL) {
     switch (type) {
-      case ILI2_COORD_TYPE : 
+      case ILI2_COORD_TYPE :
         if (cmpStr(ILI2_COORD, XMLString::transcode(childElem->getTagName())) == 0)
           return getPoint(childElem);
         break;
@@ -288,18 +288,18 @@ OGRGeometry *ILI2Reader::getGeometry(DOMElement *elem, int type) {
           (cmpStr(ILI2_SURFACE, XMLString::transcode(childElem->getTagName())) == 0))
           return getPolygon(childElem);
         break;
-      default : 
+      default :
         if (type >= ILI2_GEOMCOLL_TYPE) {
           int subType = getGeometryTypeOfElem(childElem); //????
           gm->addGeometry(getGeometry(childElem, subType));
         }
         break;
     }
-    
+
     // GEOMCOLL
     childElem = (DOMElement *)childElem->getNextSibling();
   }
-  
+
   return gm;
 }
 
@@ -337,7 +337,7 @@ int ILI2Reader::ReadModel(char **modelFilenames) {
 
   iom_init();
 
-  // set error listener to a iom provided one, that just 
+  // set error listener to a iom provided one, that just
   // dumps all errors to stderr
   iom_seterrlistener(iom_stderrlistener);
 
@@ -422,7 +422,7 @@ char* fieldName(DOMElement* elem) {
 // We cannot do this sort of dynamic stack alloc on MSVC6.
 //  DOMNode* elements[depth];
   DOMNode* elements[1000];
-  CPLAssert( depth < sizeof(elements) / sizeof(DOMNode*) );
+  CPLAssert( depth < (int)(sizeof(elements) / sizeof(DOMNode*)) );
 
   int d=0;
   for (node = elem; d<depth; node = node->getParentNode()) elements[d++] = node;
@@ -498,7 +498,7 @@ ILI2Reader::ILI2Reader() {
     m_bReadStarted = FALSE;
 
     m_pszFilename = NULL;
-    
+
     SetupParser();
 }
 
@@ -527,11 +527,11 @@ int ILI2Reader::SetupParser() {
         {
             XMLPlatformUtils::Initialize();
         }
-        
+
         catch (const XMLException& toCatch)
         {
             CPLError( CE_Failure, CPLE_AppDefined,
-              "Unable to initalize Xerces C++ based ILI2 reader. Error message:\n%s\n", 
+              "Unable to initalize Xerces C++ based ILI2 reader. Error message:\n%s\n",
               toCatch.getMessage() );
             return FALSE;
         }
@@ -544,7 +544,7 @@ int ILI2Reader::SetupParser() {
 
     // Create and initialize parser.
     m_poSAXReader = XMLReaderFactory::createXMLReader();
-    
+
     m_poILI2Handler = new ILI2Handler( this );
 
     m_poSAXReader->setContentHandler( m_poILI2Handler );
@@ -567,10 +567,12 @@ int ILI2Reader::SetupParser() {
 //    m_poSAXReader->setValidationSchemaFullChecking(true);
 #else
 */
-    m_poSAXReader->setFeature(
-        XMLString::transcode("http://xml.org/sax/features/validation"), false);
-    m_poSAXReader->setFeature(
-        XMLString::transcode("http://xml.org/sax/features/namespaces"), false);
+    XMLCh *tmpCh = XMLString::transcode("http://xml.org/sax/features/validation");
+    m_poSAXReader->setFeature(tmpCh, false);
+    XMLString::release(&tmpCh);
+    tmpCh = XMLString::transcode("http://xml.org/sax/features/namespaces");
+    m_poSAXReader->setFeature(tmpCh, false);
+    XMLString::release(&tmpCh);
 //#endif
 
     m_bReadStarted = FALSE;
@@ -593,22 +595,22 @@ void ILI2Reader::CleanupParser() {
 
 int ILI2Reader::SaveClasses( const char *pszFile = NULL ) {
 
-    // Add logic later to determine reasonable default schema file. 
+    // Add logic later to determine reasonable default schema file.
     if( pszFile == NULL )
         return FALSE;
-        
+
     // parse and create layers and features
     m_poSAXReader->parse(pszFile);
 
   if (m_missAttrs.size() != 0) {
     m_missAttrs.sort();
     m_missAttrs.unique();
-    string attrs = "";  
+    string attrs = "";
     list<string>::const_iterator it = m_missAttrs.begin();
     for (it = m_missAttrs.begin(); it != m_missAttrs.end(); ++it)
       attrs += *it + ", ";
-   
-    CPLError( CE_Warning, CPLE_NotSupported, 
+
+    CPLError( CE_Warning, CPLE_NotSupported,
               "Failed to add new definition to existing layers, attributes not saved: %s", attrs.c_str() );
   }
 
@@ -640,9 +642,9 @@ int ILI2Reader::AddFeature(DOMElement *elem) {
       break;
     }
   }
-  
+
   // add a layer
-  if (newLayer) { // FIXME in Layer: SRS Writer Type datasource    
+  if (newLayer) { // FIXME in Layer: SRS Writer Type datasource
     CPLDebug( "OGR_ILI", "Adding layer: %s", pszName );
     // new layer data
     OGRSpatialReference *poSRSIn = NULL; // FIXME fix values for initial layer
@@ -652,7 +654,7 @@ int ILI2Reader::AddFeature(DOMElement *elem) {
     curLayer = new OGRILI2Layer(CPLStrdup(pszName), poSRSIn, bWriterIn, eReqType, poDSIn);
     m_listLayer.push_back(curLayer);
   }
-  
+
   // the feature and field definition
   OGRFeatureDefn *featureDef = curLayer->GetLayerDefn();
   if (newLayer) {
@@ -670,7 +672,7 @@ int ILI2Reader::AddFeature(DOMElement *elem) {
   int fIndex = feature->GetFieldIndex(ILI2_TID);
   char *fChVal = XMLString::transcode(elem->getAttribute(XMLString::transcode(ILI2_TID)));
   feature->SetField(fIndex, CPLStrdup(fChVal));
-  
+
   SetFieldValues(feature, elem);
   curLayer->SetFeature(feature);
 
