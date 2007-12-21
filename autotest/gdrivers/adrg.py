446608dab27a417a91e4daa2669c28148737d917
Even Rouault <even.rouault@mines-paris.org>
2007-12-21 10:06:27 +0000

    Remove unused procedures from ADRG autotest
    git-svn-id: https://svn.osgeo.org/gdal/trunk@13417 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/gdrivers/adrg.py
+++ autotest/gdrivers/adrg.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 ###############################################################################
-# $Id: png.py 12180 2007-09-17 20:40:27Z warmerdam $
+# $Id: adrg.py $
 #
 # Project:  GDAL/OGR Test Suite
 # Purpose:  Test read/write functionality for ADRG driver.
@@ -49,10 +49,6 @@ def adrg_1():
 
 def adrg_2():
 
-#    tst = gdaltest.GDALTest( 'ADRG', 'SMALL_ADRG/ABCDEF01.GEN', 1, 62833 )
-
-#    return tst.testCreateCopy()
-
     drv = gdal.GetDriverByName( 'ADRG' )
     srcds = gdal.Open( 'data/SMALL_ADRG/ABCDEF01.GEN' )
     
@@ -64,94 +60,6 @@ def adrg_2():
 
     return 'success'
     
-###############################################################################
-# Verify the geotransform, colormap, and nodata setting for test file. 
-
-def png_3():
-
-    ds = gdal.Open( 'data/test.png' )
-    cm = ds.GetRasterBand(1).GetRasterColorTable()
-    if cm.GetCount() != 16 \
-       or cm.GetColorEntry(0) != (255,255,255,0) \
-       or cm.GetColorEntry(1) != (255,255,208,255):
-        gdaltest.post_reason( 'Wrong colormap entries' )
-        return 'fail'
-
-    cm = None
-
-    if int(ds.GetRasterBand(1).GetNoDataValue()) != 0:
-        gdaltest.post_reason( 'Wrong nodata value.' )
-        return 'fail'
-
-    # This geotransform test is also verifying the fix for bug 1414, as
-    # the world file is in a mixture of numeric representations for the
-    # numbers.  (mixture of "," and "." in file)
-
-    gt_expected = (700000.305, 0.38, 0.01, 4287500.695, -0.01, -0.38)
-
-    gt = ds.GetGeoTransform()
-    for i in range(6):
-        if abs(gt[i] - gt_expected[i]) > 0.0001:
-            print 'expected:', gt_expected
-            print 'got:', gt
-            
-            gdaltest.post_reason( 'Mixed locale world file read improperly.' )
-            return 'fail'
-
-    return 'success'
-    
-###############################################################################
-# Test RGB mode creation and reading.
-
-def png_4():
-
-    tst = gdaltest.GDALTest( 'PNG', 'rgb.ntf', 3, 21349 )
-
-    return tst.testCreateCopy()
-
-###############################################################################
-# Test RGBA 16bit read support.
-
-def png_5():
-
-    tst = gdaltest.GDALTest( 'PNG', 'rgba16.png', 3, 1815 )
-    return tst.testOpen()
-
-###############################################################################
-# Test RGBA 16bit mode creation and reading.
-
-def png_6():
-
-    tst = gdaltest.GDALTest( 'PNG', 'rgba16.png', 4, 4873 )
-
-    return tst.testCreateCopy()
-
-###############################################################################
-# Test RGB NODATA_VALUES metadata write (and read) support.
-# This is handled via the tRNS block in PNG.
-
-def png_7():
-
-    drv = gdal.GetDriverByName( 'PNG' )
-    srcds = gdal.Open( 'data/tbbn2c16.png' )
-    
-    dstds = drv.CreateCopy( 'tmp/png7.png', srcds )
-    srcds = None
-
-    dstds = gdal.Open( 'tmp/png7.png' )
-    md = dstds.GetMetadata()
-    dstds = None
-
-    if md['NODATA_VALUES'] != '32639 32639 32639':
-        gdaltest.post_reason( 'NODATA_VALUES wrong' )
-        return 'fail'
-
-    dstds = None
-
-    drv.Delete( 'tmp/png7.png' )
-
-    return 'success'
-
 
 ###############################################################################
 # Cleanup procedure
