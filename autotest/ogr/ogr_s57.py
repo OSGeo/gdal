81022278c1523d7e78825940704699ba6a78a223
Frank Warmerdam <warmerdam@pobox.com>
2007-12-30 22:50:18 +0000

    SOUNDG is now MultiPoint25D
    git-svn-id: https://svn.osgeo.org/gdal/trunk@13465 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/ogr/ogr_s57.py
+++ autotest/ogr/ogr_s57.py
@@ -1,3 +1,31 @@
+#!/usr/bin/env python
+###############################################################################
+# $Id$
+#
+# Project:  GDAL/OGR Test Suite
+# Purpose:  Test OGR S-57 driver functionality.
+# Author:   Frank Warmerdam <warmerdam@pobox.com>
+# 
+###############################################################################
+# Copyright (c) 2007, Frank Warmerdam <warmerdam@pobox.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
 ###############################################################################
 
 import os
@@ -47,7 +75,7 @@ def ogr_s57_2():
                    ('SBDARE', ogr.wkbUnknown, 2),
                    ('SLCONS', ogr.wkbUnknown, 1),
                    ('SLOTOP', ogr.wkbLineString, 1),
-                   ('SOUNDG', ogr.wkbMultiPoint, 2),
+                   ('SOUNDG', ogr.wkbMultiPoint25D, 2),
                    ('M_COVR', ogr.wkbPolygon, 1),
                    ('M_NSYS', ogr.wkbPolygon, 1),
                    ('M_QUAL', ogr.wkbPolygon, 1) ]
