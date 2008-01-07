69c3c2e05ffa10027416fb9060e3571786d00b94
Frank Warmerdam <warmerdam@pobox.com>
2008-01-07 16:04:03 +0000

    add geoconcept format driver (https://trac.osgeo.org/gdal/ticket/2134)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@13483 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ ogr/ogrsf_frmts/geoconcept/geoconcept_syscoord.h
@@ -0,0 +1,75 @@
+/**********************************************************************
+ * $Id: geoconcept_syscoord.h
+ *
+ * Name:     geoconcept_syscoord.h
+ * Project:  OpenGIS Simple Features Reference Implementation
+ * Purpose:  Implements translation between Geoconcept SysCoord
+ *           and OGRSpatialRef format
+ * Language: C
+ *
+ **********************************************************************
+ * Copyright (c) 2007,  Geoconcept and IGN
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ **********************************************************************/
+#ifndef _GEOCONCEPT_SYSCOORD_H_INCLUDED
+#define _GEOCONCEPT_SYSCOORD_H_INCLUDED
+
+#include "ogr_srs_api.h"
+
+#ifdef GCSRS_DLLEXPORT
+#  define GCSRSAPI_CALL     __declspec(dllexport)
+#  define GCSRSAPI_CALL1(x) __declspec(dllexport) x
+#endif
+
+#ifndef GCSRSAPI_CALL
+#  define GCSRSAPI_CALL
+#endif
+
+#ifndef GCSRSAPI_CALL1
+#  define GCSRSAPI_CALL1(x) x GCSRSAPI_CALL
+#endif
+
+/* -------------------------------------------------------------------- */
+/*      Macros for controlling CVSID and ensuring they don't appear     */
+/*      as unreferenced variables resulting in lots of warnings.        */
+/* -------------------------------------------------------------------- */
+#ifndef DISABLE_CVSID
+#  define GCSRS_CVSID(string)     static char gcsrs_cvsid[] = string; \
+static char *cvsid_aw() { return( cvsid_aw() ? ((char *) NULL) : gcsrs_cvsid ); }
+#else
+#  define GCSRS_CVSID(string)
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* -------------------------------------------------------------------- */
+/*      GCSRS API Prototypes                                            */
+/* -------------------------------------------------------------------- */
+
+int GCSRSAPI_CALL OGRSpatialReference2SysCoord_GCSRS ( OGRSpatialReferenceH poSR );
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* ndef _GEOCONCEPT_SYSCOORD_H_INCLUDED */
