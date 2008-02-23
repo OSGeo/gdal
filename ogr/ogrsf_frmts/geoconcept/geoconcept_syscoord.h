c939c55efef9b6a495ddede1ad7f05ef5a449f7b
Even Rouault <even.rouault@mines-paris.org>
2008-02-23 22:36:51 +0000

    Apply geoconcept-20080223.patch (except for the makefile)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@13869 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/ogrsf_frmts/geoconcept/geoconcept_syscoord.h
+++ ogr/ogrsf_frmts/geoconcept/geoconcept_syscoord.h
@@ -64,26 +64,131 @@ extern "C" {
 /* -------------------------------------------------------------------- */
 /*      GCSRS API Types                                                 */
 /* -------------------------------------------------------------------- */
+typedef struct _tSpheroidInfo_GCSRS GCSpheroidInfo;
+typedef struct _tDatumInfo_GCSRS GCDatumInfo;
+typedef struct _tProjectionInfo_GCSRS GCProjectionInfo;
 typedef struct _tSysCoord_GCSRS GCSysCoord;
 
+struct _tSpheroidInfo_GCSRS {
+  const char *pszSpheroidName;
+  double      dfA; /* semi major axis in meters */
+  double      dfE; /* excentricity */
+  int         nEllipsoidID;
+};
+
+struct _tDatumInfo_GCSRS {
+  const char *pszDatumName;
+  double      dfShiftX;
+  double      dfShiftY;
+  double      dfShiftZ;
+  double      dfRotX;
+  double      dfRotY;
+  double      dfRotZ;
+  double      dfScaleFactor;
+  double      dfDiffA; /*
+                        * semi-major difference to-datum minus from-datum :
+                        * http://home.hiwaay.net/~taylorc/bookshelf/math-science/geodesy/datum/transform/molodensky/
+                        */
+  double      dfDiffFlattening; /*
+                                 * Change in flattening : "to" minus "from"
+                                 */
+  int         nEllipsoidID;
+  int         nDatumID;
+};
+
+struct _tProjectionInfo_GCSRS {
+  const char *pszProjName;
+  int         nSphere;/*
+                       * 1 = sphere de courbure
+                       * 2 = sphere equatoriale
+                       * 3 = sphere bitangeante
+                       * 4 = sphere polaire nord
+                       * 5 = sphere polaire sud
+                       * 6 = Hotine
+                       */
+  int         nProjID;
+};
+
 struct _tSysCoord_GCSRS {
-  int coordSystemID;
-  int timeZoneValue;
+  char   *pszSysCoordName;
+  char   *pszUnit;
+
+  double  dfPM;
+  /* inherited : */
+  double  dfLambda0;
+  double  dfPhi0;
+  double  dfk0;
+  double  dfX0;
+  double  dfY0;
+  double  dfPhi1;
+  double  dfPhi2;
+
+  int     nDatumID;
+  int     nProjID;
+  int     coordSystemID;
+  int     timeZoneValue;/* when 0, replace by zone */
 };
 
 /* -------------------------------------------------------------------- */
 /*      GCSRS API Prototypes                                            */
 /* -------------------------------------------------------------------- */
 
+#define GetInfoSpheroidID_GCSRS(theSpheroid)              (theSpheroid)->nEllipsoidID
+#define GetInfoSpheroidName_GCSRS(theSpheroid)            (theSpheroid)->pszSpheroidName
+#define GetInfoSpheroidSemiMajor_GCSRS(theSpheroid)       (theSpheroid)->dfA
+#define GetInfoSpheroidExcentricity_GCSRS(theSpheroid)    (theSpheroid)->dfE
+
+#define GetInfoDatumID_GCSRS(theDatum)                    (theDatum)->nDatumID
+#define GetInfoDatumName_GCSRS(theDatum)                  (theDatum)->pszDatumName
+#define GetInfoDatumShiftX_GCSRS(theDatum)                (theDatum)->dfShiftX
+#define GetInfoDatumShiftY_GCSRS(theDatum)                (theDatum)->dfShiftY
+#define GetInfoDatumShiftZ_GCSRS(theDatum)                (theDatum)->dfShiftZ
+#define GetInfoDatumDiffA_GCSRS(theDatum)                 (theDatum)->dfDiffA
+#define GetInfoDatumRotationX_GCSRS(theDatum)             (theDatum)->dfRotX
+#define GetInfoDatumRotationY_GCSRS(theDatum)             (theDatum)->dfRotY
+#define GetInfoDatumRotationZ_GCSRS(theDatum)             (theDatum)->dfRotZ
+#define GetInfoDatumScaleFactor_GCSRS(theDatum)           (theDatum)->dfScaleFactor
+#define GetInfoDatumDiffFlattening_GCSRS(theDatum)        (theDatum)->dfDiffFlattening
+#define GetInfoDatumSpheroidID_GCSRS(theDatum)            (theDatum)->nEllipsoidID
+
+#define GetInfoProjID_GCSRS(theProj)                      (theProj)->nProjID
+#define GetInfoProjName_GCSRS(theProj)                    (theProj)->pszProjName
+#define GetInfoProjSphereType_GCSRS(theProj)              (theProj)->nSphere
+#define GetInfoProjSpheroidID_GCSRS(theProj)              (theProj)->nEllipsoidID
+
 GCSysCoord GCSRSAPI_CALL1(*) CreateSysCoord_GCSRS ( int srsid, int timezone );
 void GCSRSAPI_CALL DestroySysCoord_GCSRS ( GCSysCoord** theSysCoord );
+#define GetSysCoordSystemID_GCSRS(theSysCoord)            (theSysCoord)->coordSystemID
+#define SetSysCoordSystemID_GCSRS(theSysCoord,v)          (theSysCoord)->coordSystemID= (v)
+#define GetSysCoordTimeZone_GCSRS(theSysCoord)            (theSysCoord)->timeZoneValue
+#define SetSysCoordTimeZone_GCSRS(theSysCoord,v)          (theSysCoord)->timeZoneValue= (v)
+#define GetSysCoordName_GCSRS(theSysCoord)                (theSysCoord)->pszSysCoordName
+#define SetSysCoordName_GCSRS(theSysCoord,v)              (theSysCoord)->pszSysCoordName= (v)
+#define GetSysCoordUnit_GCSRS(theSysCoord)                (theSysCoord)->pszUnit
+#define SetSysCoordUnit_GCSRS(theSysCoord,v)              (theSysCoord)->pszUnit= (v)
+#define GetSysCoordPrimeMeridian_GCSRS(theSysCoord)       (theSysCoord)->dfPM
+#define SetSysCoordPrimeMeridian_GCSRS(theSysCoord,v)     (theSysCoord)->dfPM= (v)
+#define GetSysCoordCentralMeridian_GCSRS(theSysCoord)     (theSysCoord)->dfLambda0
+#define SetSysCoordCentralMeridian_GCSRS(theSysCoord,v)   (theSysCoord)->dfLambda0= (v)
+#define GetSysCoordLatitudeOfOrigin_GCSRS(theSysCoord)    (theSysCoord)->dfPhi0
+#define SetSysCoordLatitudeOfOrigin_GCSRS(theSysCoord,v)  (theSysCoord)->dfPhi0= (v)
+#define GetSysCoordStandardParallel1_GCSRS(theSysCoord)   (theSysCoord)->dfPhi1
+#define SetSysCoordStandardParallel1_GCSRS(theSysCoord,v) (theSysCoord)->dfPhi1= (v)
+#define GetSysCoordStandardParallel2_GCSRS(theSysCoord)   (theSysCoord)->dfPhi2
+#define SetSysCoordStandardParallel2_GCSRS(theSysCoord,v) (theSysCoord)->dfPhi2= (v)
+#define GetSysCoordScaleFactor_GCSRS(theSysCoord)         (theSysCoord)->dfk0
+#define SetSysCoordScaleFactor_GCSRS(theSysCoord,v)       (theSysCoord)->dfk0= (v)
+#define GetSysCoordFalseEasting_GCSRS(theSysCoord)        (theSysCoord)->dfX0
+#define SetSysCoordFalseEasting_GCSRS(theSysCoord,v)      (theSysCoord)->dfX0= (v)
+#define GetSysCoordFalseNorthing_GCSRS(theSysCoord)       (theSysCoord)->dfY0
+#define SetSysCoordFalseNorthing_GCSRS(theSysCoord,v)     (theSysCoord)->dfY0= (v)
+#define GetSysCoordDatumID_GCSRS(theSysCoord)             (theSysCoord)->nDatumID
+#define SetSysCoordDatumID_GCSRS(theSysCoord,v)           (theSysCoord)->nDatumID= (v)
+#define GetSysCoordProjID_GCSRS(theSysCoord)              (theSysCoord)->nProjID
+#define SetSysCoordProjID_GCSRS(theSysCoord,v)            (theSysCoord)->nProjID= (v)
+
 GCSysCoord GCSRSAPI_CALL1(*) OGRSpatialReference2SysCoord_GCSRS ( OGRSpatialReferenceH poSR );
 OGRSpatialReferenceH GCSRSAPI_CALL SysCoord2OGRSpatialReference_GCSRS ( GCSysCoord* syscoord );
-
-#define GetSysCoordSystemID_GCSRS(theSysCoord)   (theSysCoord)->coordSystemID
-#define SetSysCoordSystemID_GCSRS(theSysCoord,v) (theSysCoord)->coordSystemID= (v)
-#define GetSysCoordTimeZone_GCSRS(theSysCoord)   (theSysCoord)->timeZoneValue
-#define SetSysCoordTimeZone_GCSRS(theSysCoord,v) (theSysCoord)->timeZoneValue= (v)
 
 #ifdef __cplusplus
 }
