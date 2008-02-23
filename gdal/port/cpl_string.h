38629b00f06f0a1c3b68c302d8f0bf360bcd65a1
Even Rouault <even.rouault@mines-paris.org>
2008-02-23 17:18:44 +0000

    Add infrastructure to detect bad arguments to printf-like functions
    git-svn-id: https://svn.osgeo.org/gdal/trunk@13858 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/port/cpl_string.h
+++ gdal/port/cpl_string.h
@@ -211,7 +211,8 @@ public:
 
     void Clear() { resize(0); }
 
-    CPLString &Printf( const char *pszFormat, ... );
+    /* There seems to be a bug in the way the compiler count indices... Should be CPL_PRINT_FUNC_FORMAT (1, 2) */
+    CPLString &Printf( const char *pszFormat, ... ) CPL_PRINT_FUNC_FORMAT (2, 3);
     CPLString &vPrintf( const char *pszFormat, va_list args );
     CPLString &FormatC( double dfValue, const char *pszFormat = NULL );
     CPLString &Trim();
