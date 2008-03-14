a26c764c1e1574f80c22851048fee19df23db399
Frank Warmerdam <warmerdam@pobox.com>
2008-03-14 06:52:16 +0000

    prepare for 1.5.1 release
    git-svn-id: https://svn.osgeo.org/gdal/branches/1.5@14004 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/gcore/gdal_version.h
+++ gdal/gcore/gdal_version.h
@@ -6,7 +6,7 @@
 #ifndef GDAL_VERSION_MAJOR
 #  define GDAL_VERSION_MAJOR    1
 #  define GDAL_VERSION_MINOR    5
-#  define GDAL_VERSION_REV      0
+#  define GDAL_VERSION_REV      1
 #  define GDAL_VERSION_BUILD    0
 #endif
 
@@ -15,9 +15,9 @@
 #endif
 
 #ifndef GDAL_RELEASE_DATE
-#  define GDAL_RELEASE_DATE     20071218
+#  define GDAL_RELEASE_DATE     20080314
 #endif
 #ifndef GDAL_RELEASE_NAME
-#  define GDAL_RELEASE_NAME     "1.5.0"
+#  define GDAL_RELEASE_NAME     "1.5.1"
 #endif
 
