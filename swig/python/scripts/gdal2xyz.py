61116f6dc722fe691583d1336e5ad2fed7c66b93
Howard Butler <hobu.inc@gmail.com>
2008-03-19 04:38:44 +0000

    apply Brent's patch for #2280
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@14041 f0d54148-0727-0410-94bb-9a71ac55c965

--- swig/python/scripts/gdal2xyz.py
+++ swig/python/scripts/gdal2xyz.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     skip = 1    
     srcfile = None
     dstfile = None
-    band_num = 1
+    band_nums = []
 
     gdal.AllRegister()
     argv = gdal.GeneralCmdLineProcessor( sys.argv )
@@ -81,7 +81,7 @@ if __name__ == '__main__':
             i = i + 1
 
         elif arg == '-band':
-            band_num = int(argv[i+1])
+            band_nums.append( int(argv[i+1]) )
             i = i + 1
 
         elif arg[0] == '-':
@@ -101,16 +101,20 @@ if __name__ == '__main__':
     if srcfile is None:
         Usage()
 
+    if band_nums == []: band_nums = [1]
     # Open source file. 
     srcds = gdal.Open( srcfile )
     if srcds is None:
         print 'Could not open %s.' % srcfile
         sys.exit( 1 )
 
-    band = srcds.GetRasterBand(band_num)
-    if band is None:
-        print 'Could not get band %d' % band_num
-        sys.exit( 1 )
+    bands = []
+    for band_num in band_nums: 
+        band = srcds.GetRasterBand(band_num)
+        if band is None:
+            print 'Could not get band %d' % band_num
+            sys.exit( 1 )
+        bands.append(band)
 
     gt = srcds.GetGeoTransform()
   
@@ -124,20 +128,26 @@ if __name__ == '__main__':
     else:
         dst_fh = sys.stdout
 
+    band_format = ("%g " * len(bands)).rstrip() + '\n'
+
     # Setup an appropriate print format.
     if abs(gt[0]) < 180 and abs(gt[3]) < 180 \
        and abs(srcds.RasterXSize * gt[1]) < 180 \
        and abs(srcds.RasterYSize * gt[5]) < 180:
-        format = '%.10g %.10g %g\n'
+        format = '%.10g %.10g %s'
     else:
-        format = '%.3f %.3f %g\n'
+        format = '%.3f %.3f %s'
 
     # Loop emitting data.
 
     for y in range(srcwin[1],srcwin[1]+srcwin[3],skip):
 
-        data = band.ReadAsArray( srcwin[0], y, srcwin[2], 1 )    
-        data = Numeric.reshape( data, (srcwin[2],) )
+        data = []
+        for band in bands:
+
+            band_data = band.ReadAsArray( srcwin[0], y, srcwin[2], 1 )    
+            band_data = Numeric.reshape( band_data, (srcwin[2],) )
+            data.append(band_data)
 
         for x_i in range(0,srcwin[2],skip):
 
@@ -146,7 +156,13 @@ if __name__ == '__main__':
             geo_x = gt[0] + (x+0.5) * gt[1] + (y+0.5) * gt[2]
             geo_y = gt[3] + (x+0.5) * gt[4] + (y+0.5) * gt[5]
 
-            line = format % (float(geo_x),float(geo_y),data[x_i])
+            x_i_data = []
+            for i in range(len(bands)):
+                x_i_data.append(data[i][x_i])
+            
+            band_str = band_format % tuple(x_i_data)
+
+            line = format % (float(geo_x),float(geo_y), band_str)
 
             dst_fh.write( line )
 
