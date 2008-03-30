e33ba5b398f27cb8ee335346e328a88d71d9cb84
Even Rouault <even.rouault@mines-paris.org>
2008-03-30 10:33:39 +0000

    thinplatespline : memory allocated with 'calloc' freed with 'delete' (https://trac.osgeo.org/gdal/ticket/2301)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@14122 f0d54148-0727-0410-94bb-9a71ac55c965

--- alg/thinplatespline.cpp
+++ alg/thinplatespline.cpp
@@ -276,14 +276,14 @@ int VizGeorefSpline2D::solve(void)
     type = VIZ_GEOREF_SPLINE_FULL;
     // Make the necessary memory allocations
     if ( _AA )
-        delete _AA;
+        CPLFree(_AA);
     if ( _Ainv )
-        delete _Ainv;
+        CPLFree(_Ainv);
 	
     _nof_eqs = _nof_points + 3;
 	
-    _AA = ( double * )calloc( _nof_eqs * _nof_eqs, sizeof( double ) );
-    _Ainv = ( double * )calloc( _nof_eqs * _nof_eqs, sizeof( double ) );
+    _AA = ( double * )CPLCalloc( _nof_eqs * _nof_eqs, sizeof( double ) );
+    _Ainv = ( double * )CPLCalloc( _nof_eqs * _nof_eqs, sizeof( double ) );
 	
     // Calc the values of the matrix A
     for ( r = 0; r < 3; r++ )
