b6de597a1e2d089c60e04e6e48349dcf710fff77
Even Rouault <even.rouault@mines-paris.org>
2008-06-07 12:27:10 +0000

    Test support for reading embedded masks of a TIFF file (TIFFTAG_SUBFILETYPE=FILETYPE_MASK)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@14648 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/gcore/mask.py
+++ autotest/gcore/mask.py
@@ -234,6 +234,235 @@ def mask_5():
     return 'success'
 
 ###############################################################################
+# Test a TIFF file with 1 band and an embedded mask of 1 bit
+
+def mask_6():
+
+    if gdaltest.have_ng == 0:
+        return 'skip'
+
+    ds = gdal.Open('data/test_with_mask_1bit.tif')
+    band = ds.GetRasterBand(1)
+
+    if band.GetMaskFlags() != gdal.GMF_PER_DATASET:
+        gdaltest.post_reason( 'Did not get expected mask.' )
+        return 'fail'
+
+    cs = band.GetMaskBand().Checksum()
+    if cs != 100:
+        gdaltest.post_reason( 'Got wrong mask checksum' )
+        print cs
+        return 'fail'
+
+    return 'success' 
+
+
+###############################################################################
+# Test a TIFF file with 3 bands and an embedded mask of 1 band of 1 bit
+
+def mask_7():
+
+    if gdaltest.have_ng == 0:
+        return 'skip'
+
+    ds = gdal.Open('data/test3_with_1mask_1bit.tif')
+    for i in (1,2,3):
+        band = ds.GetRasterBand(i)
+
+        if band.GetMaskFlags() != gdal.GMF_PER_DATASET:
+            gdaltest.post_reason( 'Did not get expected mask.' )
+            return 'fail'
+
+        cs = band.GetMaskBand().Checksum()
+        if cs != 100:
+            gdaltest.post_reason( 'Got wrong mask checksum' )
+            print cs
+            return 'fail'
+
+    return 'success' 
+
+###############################################################################
+# Test a TIFF file with 1 band and an embedded mask of 8 bit.
+# Note : The TIFF6 specification, page 37, only allows 1 BitsPerSample && 1 SamplesPerPixel,
+
+def mask_8():
+
+    if gdaltest.have_ng == 0:
+        return 'skip'
+
+    ds = gdal.Open('data/test_with_mask_8bit.tif')
+    band = ds.GetRasterBand(1)
+
+    if band.GetMaskFlags() != gdal.GMF_PER_DATASET + gdal.GMF_ALPHA:
+        gdaltest.post_reason( 'Did not get expected mask.' )
+        return 'fail'
+
+    cs = band.GetMaskBand().Checksum()
+    if cs != 1222:
+        gdaltest.post_reason( 'Got wrong mask checksum' )
+        print cs
+        return 'fail'
+
+    return 'success' 
+
+###############################################################################
+# Test a TIFF file with 3 bands with an embedded mask of 1 bit with 3 bands.
+# Note : The TIFF6 specification, page 37, only allows 1 BitsPerSample && 1 SamplesPerPixel,
+
+def mask_9():
+
+    if gdaltest.have_ng == 0:
+        return 'skip'
+
+    ds = gdal.Open('data/test3_with_mask_1bit.tif')
+    for i in (1,2,3):
+        band = ds.GetRasterBand(i)
+
+        if band.GetMaskFlags() != 0:
+            gdaltest.post_reason( 'Did not get expected mask.' )
+            return 'fail'
+
+        cs = band.GetMaskBand().Checksum()
+        if cs != 100:
+            gdaltest.post_reason( 'Got wrong mask checksum' )
+            print cs
+            return 'fail'
+
+    return 'success' 
+
+###############################################################################
+# Test a TIFF file with 3 bands with an embedded mask of 8 bit with 3 bands.
+# Note : The TIFF6 specification, page 37, only allows 1 BitsPerSample && 1 SamplesPerPixel,
+
+def mask_10():
+
+    if gdaltest.have_ng == 0:
+        return 'skip'
+
+    ds = gdal.Open('data/test3_with_mask_8bit.tif')
+    for i in (1,2,3):
+        band = ds.GetRasterBand(i)
+
+        if band.GetMaskFlags() != gdal.GMF_ALPHA:
+            gdaltest.post_reason( 'Did not get expected mask.' )
+            return 'fail'
+
+        cs = band.GetMaskBand().Checksum()
+        if cs != 1222:
+            gdaltest.post_reason( 'Got wrong mask checksum' )
+            print cs
+            return 'fail'
+
+    return 'success' 
+
+###############################################################################
+# Test a TIFF file with an overview, an embedded mask of 1 bit, and an embedded
+# mask for the overview
+
+def mask_11():
+
+    if gdaltest.have_ng == 0:
+        return 'skip'
+
+    ds = gdal.Open('data/test_with_mask_1bit_and_ovr.tif')
+    band = ds.GetRasterBand(1)
+
+    # Let's fetch the mask
+    if band.GetMaskFlags() != gdal.GMF_PER_DATASET:
+        gdaltest.post_reason( 'Did not get expected mask.' )
+        return 'fail'
+
+    cs = band.GetMaskBand().Checksum()
+    if cs != 100:
+        gdaltest.post_reason( 'Got wrong mask checksum' )
+        print cs
+        return 'fail'
+
+    # Let's fetch the overview
+    band = ds.GetRasterBand(1).GetOverview(0)
+    cs = band.Checksum()
+    if cs != 1126:
+        gdaltest.post_reason( 'Got wrong overview checksum' )
+        print cs
+        return 'fail'
+
+    # Let's fetch the mask of the overview
+    if band.GetMaskFlags() != gdal.GMF_PER_DATASET:
+        gdaltest.post_reason( 'Did not get expected mask.' )
+        return 'fail'
+
+    cs = band.GetMaskBand().Checksum()
+    if cs != 25:
+        gdaltest.post_reason( 'Got wrong checksum for the mask of the overview' )
+        print cs
+        return 'fail'
+
+    # Let's fetch the overview of the mask == the mask of the overview
+    band = ds.GetRasterBand(1).GetMaskBand().GetOverview(0)
+    cs = band.Checksum()
+    if cs != 25:
+        gdaltest.post_reason( 'Got wrong checksum for the overview of the mask' )
+        print cs
+        return 'fail'
+
+    return 'success' 
+
+
+###############################################################################
+# Test a TIFF file with 3 bands, an overview, an embedded mask of 1 bit, and an embedded
+# mask for the overview
+
+def mask_12():
+
+    if gdaltest.have_ng == 0:
+        return 'skip'
+
+    ds = gdal.Open('data/test3_with_mask_1bit_and_ovr.tif')
+
+    for i in (1,2,3):
+        band = ds.GetRasterBand(i)
+
+        # Let's fetch the mask
+        if band.GetMaskFlags() != gdal.GMF_PER_DATASET:
+            gdaltest.post_reason( 'Did not get expected mask.' )
+            return 'fail'
+
+        cs = band.GetMaskBand().Checksum()
+        if cs != 100:
+            gdaltest.post_reason( 'Got wrong mask checksum' )
+            print cs
+            return 'fail'
+
+        # Let's fetch the overview
+        band = ds.GetRasterBand(i).GetOverview(0)
+        cs = band.Checksum()
+        if cs != 1126:
+            gdaltest.post_reason( 'Got wrong overview checksum' )
+            print cs
+            return 'fail'
+
+        # Let's fetch the mask of the overview
+        if band.GetMaskFlags() != gdal.GMF_PER_DATASET:
+            gdaltest.post_reason( 'Did not get expected mask.' )
+            return 'fail'
+
+        cs = band.GetMaskBand().Checksum()
+        if cs != 25:
+            gdaltest.post_reason( 'Got wrong checksum for the mask of the overview' )
+            print cs
+            return 'fail'
+
+        # Let's fetch the overview of the mask == the mask of the overview
+        band = ds.GetRasterBand(i).GetMaskBand().GetOverview(0)
+        cs = band.Checksum()
+        if cs != 25:
+            gdaltest.post_reason( 'Got wrong checksum for the overview of the mask' )
+            print cs
+            return 'fail'
+
+    return 'success' 
+
+###############################################################################
 # Cleanup.
 
 
@@ -242,7 +471,14 @@ gdaltest_list = [
     mask_2,
     mask_3,
     mask_4,
-    mask_5 ]
+    mask_5,
+    mask_6,
+    mask_7,
+    mask_8,
+    mask_9,
+    mask_10,
+    mask_11,
+    mask_12 ]
 
 if __name__ == '__main__':
 
