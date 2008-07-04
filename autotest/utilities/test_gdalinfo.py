ee862e8bb86b11936d5d337418bd77b25f60666f
Even Rouault <even.rouault@mines-paris.org>
2008-07-04 20:09:32 +0000

    Add temptative test for commandline utilities
    git-svn-id: https://svn.osgeo.org/gdal/trunk@14807 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ autotest/utilities/test_gdalinfo.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+###############################################################################
+# $Id: test_gdalinfo.py $
+#
+# Project:  GDAL/OGR Test Suite
+# Purpose:  gdalinfo testing
+# Author:   Even Rouault <even dot rouault @ mines-paris dot org>
+# 
+###############################################################################
+# Copyright (c) 2008, Even Rouault <even dot rouault @ mines-paris dot org>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+###############################################################################
+
+import os
+import sys
+
+sys.path.append( '../pymod' )
+
+import gdaltest
+
+###############################################################################
+# 
+def test_gdalinfo_init():
+    try:
+        sys.winver
+        gdaltest.devnull = 'NUL'
+    except:
+        gdaltest.devnull = '/dev/null'
+
+    gdaltest.shelltestskip = True;
+    try:
+        ret = os.system('../../gdal/apps/gdalinfo --version')
+        if ret == 0:
+            gdaltest.shelltestskip = False;
+            return 'success'
+    except:
+        pass
+
+    return 'skip'
+
+###############################################################################
+# 
+
+def test_gdalinfo_1():
+    if gdaltest.shelltestskip:
+        return 'skip'
+
+    ret = os.system('../../gdal/apps/gdalinfo ../gcore/byte.tif >' + gdaltest.devnull)
+
+    if ret == 0:
+        return 'success'
+    else:
+        return 'fail'
+
+
+gdaltest_list = [
+    test_gdalinfo_init,
+    test_gdalinfo_1,
+    ]
+
+
+if __name__ == '__main__':
+
+    gdaltest.setup_run( 'test_gdalinfo' )
+
+    gdaltest.run_tests( gdaltest_list )
+
+    gdaltest.summarize()
+
+
+
+
+
