ca3aaa5b273053d3377f193d76982d87966e8800
Even Rouault <even.rouault@mines-paris.org>
2008-07-06 08:16:26 +0000

    Simplify get_cli_utility_path_internal
    git-svn-id: https://svn.osgeo.org/gdal/trunk@14838 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/pymod/test_cli_utilities.py
+++ autotest/pymod/test_cli_utilities.py
@@ -36,51 +36,34 @@ cli_exe_path = { }
 ###############################################################################
 # 
 def get_cli_utility_path_internal(cli_utility_name):
-
-    try:
-        sys.winver
-        devnull = 'NUL'
-    except:
-        devnull = '/dev/null'
-
     # First try : in the apps directory of the GDAL source tree
     # This is the case for the buildbot directory tree
     try:
         cli_utility_path = os.path.join(os.getcwd(), '..', '..', 'gdal', 'apps', cli_utility_name)
-        ret = os.system(cli_utility_path + ' --utility_version > ' + devnull)
-        if ret == 0:
-            ret = os.popen(cli_utility_path + ' --utility_version').read()
+        (child_stdin, child_stdout, child_stderr) = os.popen3(cli_utility_path + ' --utility_version')
+        ret = child_stdout.read()
+        child_stdin.close()
+        child_stdout.close()
+        child_stderr.close()
+
+        if ret.find('GDAL') != -1:
             print ret
-
-            if ret.find('GDAL') != -1:
-                return cli_utility_path
-    except:
-        pass
-
-    # Second try for szekerest-vc71-full
-    try:
-        cli_utility_path = os.getcwd() + '/../../gdal/apps/' + cli_utility_name
-        # Remove the ' > NUL'
-        ret = os.system(cli_utility_path + ' --utility_version')
-        if ret == 0:
-            ret = os.popen(cli_utility_path + ' --utility_version').read()
-            print ret
-
-            if ret.find('GDAL') != -1:
-                return cli_utility_path
+            return cli_utility_path
     except:
         pass
 
     # Otherwise look up in the system path
     try:
         cli_utility_path = cli_utility_name
-        ret = os.system(cli_utility_path + ' --utility_version > ' + devnull)
-        if ret == 0:
-            ret = os.popen(cli_utility_path + ' --utility_version').read()
-            print ret
+        (child_stdin, child_stdout, child_stderr) = os.popen3(cli_utility_path + ' --utility_version')
+        ret = child_stdout.read()
+        child_stdin.close()
+        child_stdout.close()
+        child_stderr.close()
 
-            if ret.find('GDAL') != -1:
-                return cli_utility_path
+        if ret.find('GDAL') != -1:
+            print ret
+            return cli_utility_path
     except:
         pass
 
