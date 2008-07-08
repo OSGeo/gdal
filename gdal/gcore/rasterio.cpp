f6200b3f9ec57f7478b4a73284944bdd7ac0e54e
Even Rouault <even.rouault@mines-paris.org>
2008-07-08 20:40:41 +0000

    Add special case in GDALCopyWholeRaster to be more friendly when writing a tiled compressed GeoTIFF : with that, one of the 21600x21600 blue-marble tiles can be compressed into 69 MB instead of 133 MB
    git-svn-id: https://svn.osgeo.org/gdal/trunk@14862 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/gcore/rasterio.cpp
+++ gdal/gcore/rasterio.cpp
@@ -1809,18 +1809,43 @@ CPLErr CPL_STDCALL GDALDatasetCopyWholeRaster(
         && (EQUAL(pszInterleave,"PIXEL") || EQUAL(pszInterleave,"LINE")) )
         bInterleave = TRUE;
 
+    /* If the destination is compressed, we must try to write blocks just once, to save */
+    /* disk space (GTiff case for example), and to avoid data loss (JPEG compression for example) */
+    int bDstIsCompressed = FALSE;
+    const char* pszDstCompressed= CSLFetchNameValue( papszOptions, "COMPRESSED" );
+    if (pszDstCompressed != NULL && CSLTestBoolean(pszDstCompressed))
+        bDstIsCompressed = TRUE;
+
 /* -------------------------------------------------------------------- */
 /*      What will our swath size be?                                    */
 /* -------------------------------------------------------------------- */
-    int nTargetSwathSize = 10000000; // ~ 10MB
-    void *pSwathBuf;
-    int nMemoryPerLine = nXSize * (GDALGetDataTypeSize(eDT) / 8);
+    /* When writing interleaved data in a compressed format, we want to be sure */
+    /* that each block will only be written once, so the swath size must not be */
+    /* greater than the block cache. */
+    /* So as the default cache size is 40 MB, 10 MB is a safe value */
+    int nTargetSwathSize = atoi(CPLGetConfigOption("GDAL_SWATH_SIZE", "10000000"));
+    if (nTargetSwathSize < 1000000)
+        nTargetSwathSize = 1000000;
 
+    /* But let's check that  */
+    if (bDstIsCompressed && bInterleave && nTargetSwathSize > GDALGetCacheMax())
+    {
+        CPLError(CE_Warning, CPLE_AppDefined,
+                 "When translating into a compressed interleave format, the block cache size (%d) "
+                 "should be at least the size of the swath (%d)", GDALGetCacheMax(), nTargetSwathSize);
+    }
+
+    void *pSwathBuf;
+
+    int nPixelSize = (GDALGetDataTypeSize(eDT) / 8);
     if( bInterleave)
-        nMemoryPerLine *= nBandCount;
+        nPixelSize *= nBandCount;
 
     // aim for one row of blocks.  Do not settle for less.
+    int nSwathCols  = nXSize;
     int nSwathLines = nBlockYSize;
+
+    int nMemoryPerLine = nXSize * nPixelSize;
 
     /* Do the computation on a big int since for example when translating */
     /* the JPL WMS layer, we overflow 32 bits*/
@@ -1841,28 +1866,55 @@ CPLErr CPL_STDCALL GDALDatasetCopyWholeRaster(
     else if( nSwathLines == 1 
         || nMemoryPerLine * nSwathLines < nTargetSwathSize / 10 )
         nSwathLines = MIN(nYSize,MAX(1,nTargetSwathSize/nMemoryPerLine));
-    
-    pSwathBuf = VSIMalloc(nMemoryPerLine * nSwathLines);
+
+
+    if (bDstIsCompressed)
+    {
+        if (nSwathLines < nBlockYSize)
+        {
+            nSwathLines = nBlockYSize;
+
+            /* Number of pixels that can be read/write simultaneously */
+            nSwathCols = nTargetSwathSize / (nSwathLines * nPixelSize);
+            nSwathCols = (nSwathCols / nBlockXSize) * nBlockXSize;
+            if (nSwathCols == 0)
+                nSwathCols = nBlockXSize;
+            CPLAssert(nSwathCols < nXSize);
+
+            CPLDebug( "GDAL", 
+              "GDALDatasetCopyWholeRaster(): because of compression and too high block,\n"
+              "use partial width at one time");
+        }
+        else
+        {
+            /* Round on a multiple of nBlockYSize */
+            nSwathLines = (nSwathLines / nBlockYSize) * nBlockYSize;
+            CPLDebug( "GDAL", 
+              "GDALDatasetCopyWholeRaster(): because of compression, \n"
+              "round nSwathLines to block height : %d", nSwathLines);
+        }
+    }
+
+    pSwathBuf = VSIMalloc3(nSwathCols, nSwathLines, nPixelSize );
     if( pSwathBuf == NULL )
     {
         CPLError( CE_Failure, CPLE_OutOfMemory,
-                  "Failed to allocate %d byte swath buffer in\n"
-                  "GDALDatasetCopyWholeRaster()",
-                  nMemoryPerLine * nSwathLines );
+                "Failed to allocate %d*%d*%d byte swath buffer in\n"
+                "GDALDatasetCopyWholeRaster()",
+                nSwathCols, nSwathLines, nPixelSize );
         return CE_Failure;
     }
 
     CPLDebug( "GDAL", 
-              "GDALDatasetCopyWholeRaster(): %d line swath, bInterleave=%d", 
-              nSwathLines, bInterleave );
-              
+            "GDALDatasetCopyWholeRaster(): %d*%d swaths, bInterleave=%d", 
+            nSwathCols, nSwathLines, bInterleave );
 
 /* ==================================================================== */
 /*      Band oriented (uninterleaved) case.                             */
 /* ==================================================================== */
     if( !bInterleave )
     {
-        int iBand, iY;
+        int iBand, iX, iY;
 
         for( iBand = 0; iBand < nBandCount && eErr == CE_None; iBand++ )
         {
@@ -1875,28 +1927,36 @@ CPLErr CPL_STDCALL GDALDatasetCopyWholeRaster(
                 if( iY + nThisLines > nYSize )
                     nThisLines = nYSize - iY;
 
-                eErr = poSrcDS->RasterIO( GF_Read, 
-                                          0, iY, nXSize, nThisLines,
-                                          pSwathBuf, nXSize, nThisLines, 
-                                          eDT, 1, &nBand, 
-                                          0, 0, 0 );
-                
-                if( eErr == CE_None )
-                    eErr = poDstDS->RasterIO( GF_Write, 
-                                              0, iY, nXSize, nThisLines,
-                                              pSwathBuf, nXSize, nThisLines, 
-                                              eDT, 1, &nBand,
-                                              0, 0, 0 );
-
-                if( eErr == CE_None 
-                    && !pfnProgress( 
-                        iBand / (float)nBandCount
-                        + (iY+nThisLines) / (float) (nYSize*nBandCount),
-                        NULL, pProgressData ) )
+                for( iX = 0; iX < nXSize && eErr == CE_None; iX += nSwathCols )
                 {
-                    eErr = CE_Failure;
-                    CPLError( CE_Failure, CPLE_UserInterrupt, 
-                              "User terminated CreateCopy()" );
+                    int nThisCols = nSwathCols;
+
+                    if( iX + nThisCols > nXSize )
+                        nThisCols = nXSize - iX;
+
+                    eErr = poSrcDS->RasterIO( GF_Read, 
+                                            iX, iY, nThisCols, nThisLines,
+                                            pSwathBuf, nThisCols, nThisLines, 
+                                            eDT, 1, &nBand, 
+                                            0, 0, 0 );
+
+                    if( eErr == CE_None )
+                        eErr = poDstDS->RasterIO( GF_Write, 
+                                                iX, iY, nThisCols, nThisLines,
+                                                pSwathBuf, nThisCols, nThisLines, 
+                                                eDT, 1, &nBand,
+                                                0, 0, 0 );
+
+                    if( eErr == CE_None 
+                        && !pfnProgress( 
+                            iBand / (float)nBandCount
+                            + (iY+nThisLines) / (float) (nYSize*nBandCount),
+                            NULL, pProgressData ) )
+                    {
+                        eErr = CE_Failure;
+                        CPLError( CE_Failure, CPLE_UserInterrupt, 
+                                "User terminated CreateCopy()" );
+                    }
                 }
             }
         }
@@ -1905,37 +1965,45 @@ CPLErr CPL_STDCALL GDALDatasetCopyWholeRaster(
 /* ==================================================================== */
 /*      Pixel interleaved case.                                         */
 /* ==================================================================== */
-    else if( bInterleave )
+    else /* if( bInterleave ) */
     {
-        int iY;
-        
+        int iY, iX;
+
         for( iY = 0; iY < nYSize && eErr == CE_None; iY += nSwathLines )
         {
             int nThisLines = nSwathLines;
-            
+
             if( iY + nThisLines > nYSize )
                 nThisLines = nYSize - iY;
-            
-            eErr = poSrcDS->RasterIO( GF_Read, 
-                                      0, iY, nXSize, nThisLines,
-                                      pSwathBuf, nXSize, nThisLines, 
-                                      eDT, nBandCount, NULL, 
-                                      0, 0, 0 );
-                
-            if( eErr == CE_None )
-                eErr = poDstDS->RasterIO( GF_Write, 
-                                          0, iY, nXSize, nThisLines,
-                                          pSwathBuf, nXSize, nThisLines, 
-                                          eDT, nBandCount, NULL, 
-                                          0, 0, 0 );
 
-            if( eErr == CE_None 
-                && !pfnProgress( (iY+nThisLines) / (float) nYSize,
-                                 NULL, pProgressData ) )
+            for( iX = 0; iX < nXSize && eErr == CE_None; iX += nSwathCols )
             {
-                eErr = CE_Failure;
-                CPLError( CE_Failure, CPLE_UserInterrupt, 
-                          "User terminated CreateCopy()" );
+                int nThisCols = nSwathCols;
+
+                if( iX + nThisCols > nXSize )
+                    nThisCols = nXSize - iX;
+
+                eErr = poSrcDS->RasterIO( GF_Read, 
+                                        iX, iY, nThisCols, nThisLines,
+                                        pSwathBuf, nThisCols, nThisLines, 
+                                        eDT, nBandCount, NULL, 
+                                        0, 0, 0 );
+
+                if( eErr == CE_None )
+                    eErr = poDstDS->RasterIO( GF_Write, 
+                                            iX, iY, nThisCols, nThisLines,
+                                            pSwathBuf, nThisCols, nThisLines, 
+                                            eDT, nBandCount, NULL, 
+                                            0, 0, 0 );
+
+                if( eErr == CE_None 
+                    && !pfnProgress( (iY+nThisLines) / (float) nYSize,
+                                    NULL, pProgressData ) )
+                {
+                    eErr = CE_Failure;
+                    CPLError( CE_Failure, CPLE_UserInterrupt, 
+                            "User terminated CreateCopy()" );
+                }
             }
         }
     }
