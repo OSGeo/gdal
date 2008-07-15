7f4751b8ce8f014e4195a03179e69bd771abf440
Tamas Szekeres <szekerest@gmail.com>
2008-07-15 09:59:42 +0000

    Added examples for Dataset.GetGCPs, Dataset.SetGCPs and GCPsToGeoTransform.
    git-svn-id: https://svn.osgeo.org/gdal/branches/1.5@14918 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/swig/csharp/apps/GDALWrite.cs
+++ gdal/swig/csharp/apps/GDALWrite.cs
@@ -74,6 +74,17 @@ class GDALWrite {
                 System.Environment.Exit(-1);
             }
 
+            /* -------------------------------------------------------------------- */
+            /*      Setting corner GCPs.                                            */
+            /* -------------------------------------------------------------------- */
+            GCP[] GCPs = new GCP[] { 
+                new GCP(44.5, 27.5, 0, 0, 0, "info0", "id0"),
+                new GCP(45.5, 27.5, 0, 100, 0, "info1", "id1"),
+                new GCP(44.5, 26.5, 0, 0, 100, "info2", "id2"),
+                new GCP(45.5, 26.5, 0, 100, 100, "info3", "id3")
+            };
+            ds.SetGCPs(GCPs, "");
+
             Band ba = ds.GetRasterBand(1);
 
             byte [] buffer = new byte [w * h];
