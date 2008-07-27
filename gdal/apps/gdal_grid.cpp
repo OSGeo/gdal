OSGeo/gdal

commit 700165384e38feb1a10b3a041975a72bee5bd303

Fix tiling in gdal_grid and output of geotransform when -txe and -tye not specified (#2508)
git-svn-id: https://svn.osgeo.org/gdal/branches/1.5@15051 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/apps/gdal_grid.cpp
@@ -258,9 +258,9 @@ static CPLErr ParseAlgorithmAndOptions( const char *pszAlgoritm,
 
 static void ProcessLayer( OGRLayerH hSrcLayer, GDALDatasetH hDstDS,
                           GUInt32 nXSize, GUInt32 nYSize, int nBand,
-                          int bIsXExtentSet, int bIsYExtentSet,
-                          double dfXMin, double dfXMax,
-                          double dfYMin, double dfYMax,
+                          int& bIsXExtentSet, int& bIsYExtentSet,
+                          double& dfXMin, double& dfXMax,
+                          double& dfYMin, double& dfYMax,
                           GDALDataType eType,
                           GDALGridAlgorithm eAlgorithm, void *pOptions,
                           int bQuiet, GDALProgressFunc pfnProgress )
@@ -310,12 +310,14 @@ static void ProcessLayer( OGRLayerH hSrcLayer, GDALDatasetH hDstDS,
     {
         dfXMin = *std::min_element(adfX.begin(), adfX.end());
         dfXMax = *std::max_element(adfX.begin(), adfX.end());
+        bIsXExtentSet = TRUE;
     }
 
     if ( !bIsYExtentSet )
     {
         dfYMin = *std::min_element(adfY.begin(), adfY.end());
         dfYMax = *std::max_element(adfY.begin(), adfY.end());
+        bIsYExtentSet = TRUE;
     }
 
 /* -------------------------------------------------------------------- */
@@ -360,18 +362,26 @@ static void ProcessLayer( OGRLayerH hSrcLayer, GDALDatasetH hDstDS,
                                           (double)++nBlock / nBlockCount,
                                           pfnProgress, NULL );
 
+            int nXRequest = nBlockXSize;
+            if (nXOffset + nXRequest > nXSize)
+                nXRequest = nXSize - nXOffset;
+
+            int nYRequest = nBlockYSize;
+            if (nYOffset + nYRequest > nYSize)
+                nYRequest = nYSize - nYOffset;
+
             GDALGridCreate( eAlgorithm, pOptions,
                             adfX.size(), &(adfX[0]), &(adfY[0]), &(adfZ[0]),
                             dfXMin + dfDeltaX * nXOffset,
-                            dfXMin + dfDeltaX * (nXOffset + nBlockXSize),
+                            dfXMin + dfDeltaX * (nXOffset + nXRequest),
                             dfYMin + dfDeltaY * nYOffset,
-                            dfYMin + dfDeltaY * (nYOffset + nBlockYSize),
-                            nBlockXSize, nBlockYSize, eType, pData,
+                            dfYMin + dfDeltaY * (nYOffset + nYRequest),
+                            nXRequest, nBlockYSize, eType, pData,
                             GDALScaledProgress, pScaledProgress );
 
             GDALRasterIO( hBand, GF_Write, nXOffset, nYOffset,
-                          nBlockXSize, nBlockYSize, pData,
-                          nBlockXSize, nBlockYSize, eType, 0, 0 );
+                          nXRequest, nYRequest, pData,
+                          nXRequest, nYRequest, eType, 0, 0 );
 
             GDALDestroyScaledProgress( pScaledProgress );
         }
@@ -395,7 +405,7 @@ int main( int argc, char ** argv )
     GUInt32         nXSize = 0, nYSize = 0;
     double          dfXMin = 0.0, dfXMax = 0.0, dfYMin = 0.0, dfYMax = 0.0;
     int             bIsXExtentSet = FALSE, bIsYExtentSet = FALSE;
-    GDALGridAlgorithm eAlgorithm;
+    GDALGridAlgorithm eAlgorithm = GGA_InverseDistanceToAPower;
     void            *pOptions = NULL;
     char            *pszOutputSRS = NULL;
     int             bQuiet = FALSE;

commit e3c419624537a69f00befc7fa1c0a4480ab6b668

Fix typo introduced in r15051 (https://trac.osgeo.org/gdal/ticket/2508)
git-svn-id: https://svn.osgeo.org/gdal/branches/1.5@15054 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/apps/gdal_grid.cpp
@@ -376,7 +376,7 @@ static void ProcessLayer( OGRLayerH hSrcLayer, GDALDatasetH hDstDS,
                             dfXMin + dfDeltaX * (nXOffset + nXRequest),
                             dfYMin + dfDeltaY * nYOffset,
                             dfYMin + dfDeltaY * (nYOffset + nYRequest),
-                            nXRequest, nBlockYSize, eType, pData,
+                            nXRequest, nYRequest, eType, pData,
                             GDALScaledProgress, pScaledProgress );
 
             GDALRasterIO( hBand, GF_Write, nXOffset, nYOffset,