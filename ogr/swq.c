4817ecdbe9949e359c46bba4b8c8e053130029f9
Mateusz Loskot <mateusz@loskot.net>
2008-08-14 18:04:34 +0000

    Fixed OGR SQL to properly escape single quotes in string literals (Ticket #2220).
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@15146 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/swq.c
+++ ogr/swq.c
@@ -173,6 +173,8 @@ static char *swq_token( const char *expression, char **next, int *is_literal )
                 expression++;
             else if( *expression == '\\' && expression[1] == '\'' )
                 expression++;
+            else if( *expression == '\'' && expression[1] == '\'' )
+                expression++;
             else if( *expression == '"' )
             {
                 expression++;
