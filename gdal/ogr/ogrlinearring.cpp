35cffac0703da91c60dab73b3876bae0bf11ca7c
Even Rouault <even.rouault@mines-paris.org>
2008-10-05 21:47:58 +0000

    Correctly handle the case where the outer and the inner ring touches at one point (https://trac.osgeo.org/gdal/ticket/2589)
    git-svn-id: https://svn.osgeo.org/gdal/branches/1.5@15462 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrlinearring.cpp
+++ gdal/ogr/ogrlinearring.cpp
@@ -446,9 +446,6 @@ OGRBoolean OGRLinearRing::isPointInRing(const OGRPoint* poPoint) const
         return 0;
     }
 
-    CPLDebug( "OGR", "OGRLinearRing::isPointInRing(): passed point: (%.8f,%.8f)",
-              poPoint->getX(), poPoint->getY() );
-
     const int iNumPoints = getNumPoints();
 
     // Simple validation
@@ -464,7 +461,6 @@ OGRBoolean OGRLinearRing::isPointInRing(const OGRPoint* poPoint) const
     if ( !( dfTestX >= extent.MinX && dfTestX <= extent.MaxX
          && dfTestY >= extent.MinY && dfTestY <= extent.MaxY ) )
     {
-        CPLDebug( "OGR", "OGRLinearRing::isPointInRing(): passed point is out of extent of ring" );
         return 0;
     }
 
@@ -481,6 +477,73 @@ OGRBoolean OGRLinearRing::isPointInRing(const OGRPoint* poPoint) const
 
         const double x2 = getX(iPointPrev) - dfTestX;
         const double y2 = getY(iPointPrev) - dfTestY;
+
+        if( ( ( y1 > 0 ) && ( y2 <= 0 ) ) || ( ( y2 > 0 ) && ( y1 <= 0 ) ) ) 
+        {
+            // Check if ray intersects with segment of the ring
+            const double dfIntersection = ( x1 * y2 - x2 * y1 ) / (y2 - y1);
+            if ( 0.0 < dfIntersection )
+            {
+                // Count intersections
+                iNumCrossings++;
+            }
+        }
+    }
+
+    // If iNumCrossings number is even, given point is outside the ring,
+    // when the crossings number is odd, the point is inside the ring.
+    return ( ( iNumCrossings % 2 ) == 1 ? 1 : 0 );
+}
+
+/************************************************************************/
+/*                       isPointInRingOrOnRing()                        */
+/************************************************************************/
+
+OGRBoolean OGRLinearRing::isPointInRingOrOnRing(const OGRPoint* poPoint) const
+{
+    if ( NULL == poPoint )
+    {
+        CPLDebug( "OGR", "OGRLinearRing::isPointInRingOrOnRing(const  OGRPoint* poPoint) - passed point is NULL!" );
+        return 0;
+    }
+
+    const int iNumPoints = getNumPoints();
+
+    // Simple validation
+    if ( iNumPoints < 4 )
+        return 0;
+
+    const double dfTestX = poPoint->getX();
+    const double dfTestY = poPoint->getY();
+
+    // Fast test if point is inside extent of the ring
+    OGREnvelope extent;
+    getEnvelope(&extent);
+    if ( !( dfTestX >= extent.MinX && dfTestX <= extent.MaxX
+         && dfTestY >= extent.MinY && dfTestY <= extent.MaxY ) )
+    {
+        return 0;
+    }
+
+	// For every point p in ring,
+    // test if ray starting from given point crosses segment (p - 1, p)
+    int iNumCrossings = 0;
+
+    for ( int iPoint = 1; iPoint < iNumPoints; iPoint++ ) 
+    {
+        const int iPointPrev = iPoint - 1;
+
+        const double x1 = getX(iPoint) - dfTestX;
+        const double y1 = getY(iPoint) - dfTestY;
+
+        const double x2 = getX(iPointPrev) - dfTestX;
+        const double y2 = getY(iPointPrev) - dfTestY;
+
+        /* If the point is on the segment, return immediatly */
+        if ( x1 * y2 - x2 * y1 == 0 )
+        {
+            return 1;
+        }
 
         if( ( ( y1 > 0 ) && ( y2 <= 0 ) ) || ( ( y2 > 0 ) && ( y1 <= 0 ) ) ) 
         {
