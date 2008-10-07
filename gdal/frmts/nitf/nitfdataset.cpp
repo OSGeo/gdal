d6e5350fb29a10b37e6a4a983dbfd4396cd82677
Frank Warmerdam <warmerdam@pobox.com>
2008-10-07 02:53:20 +0000

    fallback to pam info for nodata (#2596)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@15464 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/nitf/nitfdataset.cpp
+++ gdal/frmts/nitf/nitfdataset.cpp
@@ -458,7 +458,7 @@ double NITFRasterBand::GetNoDataValue( int *pbSuccess )
     if( psImage->bNoDataSet )
         return psImage->nNoDataValue;
     else
-        return -1e10;
+        return GDALPamRasterBand::GetNoDataValue( pbSuccess );
 }
 
 /************************************************************************/
