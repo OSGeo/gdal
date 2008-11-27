bccf4601fa44121b1fe4106dd9205dc65e462a5d
Frank Warmerdam <warmerdam@pobox.com>
2008-11-27 19:55:24 +0000

    ensure stdcall attribute set on error handler (#2688)
    git-svn-id: https://svn.osgeo.org/gdal/branches/1.5@15823 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/swig/python/extensions/osr_wrap.cpp
+++ gdal/swig/python/extensions/osr_wrap.cpp
@@ -2622,7 +2622,9 @@ typedef void OSRCoordinateTransformationShadow;
 
 int bUseExceptions=0;
 
-void VeryQuiteErrorHandler(CPLErr eclass, int code, const char *msg ) {
+void CPL_STDCALL 
+VeryQuietErrorHandler(CPLErr eclass, int code, const char *msg ) 
+{
   /* If the error class is CE_Fatal, we want to have a message issued
      because the CPL support code does an abort() before any exception
      can be generated */
@@ -2634,7 +2636,7 @@ void VeryQuiteErrorHandler(CPLErr eclass, int code, const char *msg ) {
 
 void UseExceptions() {
   bUseExceptions = 1;
-  CPLSetErrorHandler( (CPLErrorHandler) VeryQuiteErrorHandler );
+  CPLSetErrorHandler( (CPLErrorHandler) VeryQuietErrorHandler );
 }
 
 void DontUseExceptions() {
