97ce09974f387a6a1c577672e1c2ac4266bcb359
Frank Warmerdam <warmerdam@pobox.com>
2008-12-31 04:10:29 +0000

    avoid incorrect GEOGCS AXIS settings (#2713)
    git-svn-id: https://svn.osgeo.org/gdal/branches/1.6@16019 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/raw/landataset.cpp
+++ gdal/frmts/raw/landataset.cpp
@@ -29,6 +29,7 @@
 
 #include "rawdataset.h"
 #include "cpl_string.h"
+#include "ogr_srs_api.h"
 
 CPL_CVSID("$Id$");
 
@@ -507,7 +508,7 @@ GDALDataset *LANDataset::Open( GDALOpenInfo * poOpenInfo )
 
     if( nCoordSys == 0 )
     {
-        poDS->pszProjection = CPLStrdup("GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9108\"]],AXIS[\"Lat\",NORTH],AXIS[\"Long\",EAST],AUTHORITY[\"EPSG\",\"4326\"]]");
+        poDS->pszProjection = CPLStrdup(SRS_WKT_WGS84);
             
     }
     else if( nCoordSys == 1 )
