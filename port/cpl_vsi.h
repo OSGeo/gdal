ff9ec41c09e8f886aac8b86f0b052fc0ac56a744
Frank Warmerdam <warmerdam@pobox.com>
2009-01-14 06:26:56 +0000

    add /vsisubfile virtual file driver
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@16079 f0d54148-0727-0410-94bb-9a71ac55c965

--- port/cpl_vsi.h
+++ port/cpl_vsi.h
@@ -190,6 +190,7 @@ char CPL_DLL *VSIStrerror( int );
 /* ==================================================================== */
 void CPL_DLL VSIInstallMemFileHandler(void);
 void CPL_DLL VSIInstallLargeFileHandler(void);
+void CPL_DLL VSIInstallSubFileHandler(void);
 void VSIInstallGZipFileHandler(void); /* No reason to export that */
 void VSIInstallZipFileHandler(void); /* No reason to export that */
 void CPL_DLL VSICleanupFileManager(void);
