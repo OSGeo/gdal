5b8de632017cac797ae1600a59b27074596bd09c
Even Rouault <even.rouault@mines-paris.org>
2009-01-18 09:43:05 +0000

    Remove buggy closing parenthesis at the end of the WKT name for projection SRS_PT_LAMBERT_CONFORMAL_CONIC_2SP_BELGIUM (#2763)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@16107 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/ogr_srs_api.h
+++ ogr/ogr_srs_api.h
@@ -124,7 +124,7 @@ typedef enum {
 #define SRS_PT_LAMBERT_CONFORMAL_CONIC_2SP                              \
                                 "Lambert_Conformal_Conic_2SP"
 #define SRS_PT_LAMBERT_CONFORMAL_CONIC_2SP_BELGIUM                      \
-                                "Lambert_Conformal_Conic_2SP_Belgium)"
+                                "Lambert_Conformal_Conic_2SP_Belgium"
 #define SRS_PT_LAMBERT_AZIMUTHAL_EQUAL_AREA                     \
                                 "Lambert_Azimuthal_Equal_Area"
 #define SRS_PT_MERCATOR_1SP     "Mercator_1SP"
