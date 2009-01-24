4bb7587c3d4123f8cb6bd5a0e5f0d6bf49e16001
Even Rouault <even.rouault@mines-paris.org>
2009-01-24 20:18:09 +0000

    Test all drivers for Create() and CreateCopy() with nBands == 0 (#2799)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@16172 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/gcore/misc.py
+++ autotest/gcore/misc.py
@@ -30,6 +30,8 @@
 
 import gdal
 import sys
+import os
+import shutil
 
 sys.path.append( '../pymod' )
 
@@ -94,10 +96,152 @@ def misc_3():
 
     return 'success'
 
+###############################################################################
+# Test Create() with invalid arguments
+
+def misc_4():
+
+    gdal.PushErrorHandler('CPLQuietErrorHandler')
+
+    # Test a few invalid argument
+    drv = gdal.GetDriverByName('GTiff')
+    drv.Create('tmp/foo', 0, 100, 1)
+    drv.Create('tmp/foo', 100, 1, 1)
+    drv.Create('tmp/foo', 100, 100, -1)
+    drv.Delete('tmp/foo')
+
+    gdal.PopErrorHandler()
+
+    return 'success'
+
+###############################################################################
+# Test Create() with various band numbers (including 0) and datatype
+
+def misc_5_internal(drv, datatype, nBands):
+    #print 'drv = %s, nBands = %d, datatype = %s' % (drv.ShortName, nBands, gdal.GetDataTypeName(datatype))
+    os.mkdir('tmp/tmp')
+    ds = drv.Create('tmp/tmp/foo', 100, 100, nBands, datatype)
+    ds = None
+    shutil.rmtree('tmp/tmp')
+
+    return
+
+def misc_5():
+
+    gdal.PushErrorHandler('CPLQuietErrorHandler')
+
+    try:
+        shutil.rmtree('tmp/tmp')
+    except:
+        pass
+
+    # Test Create() with various band numbers, including 0
+    for i in range(gdal.GetDriverCount()):
+        drv = gdal.GetDriver(i)
+        md = drv.GetMetadata()
+        if md.has_key('DCAP_CREATE'):
+            datatype = gdal.GDT_Byte
+            for nBands in range(6):
+                misc_5_internal(drv, datatype, nBands)
+
+            nBands = 1
+            for datatype in (gdal.GDT_UInt16,
+                             gdal.GDT_Int16,
+                             gdal.GDT_UInt32,
+                             gdal.GDT_Int32,
+                             gdal.GDT_Float32,
+                             gdal.GDT_Float64,
+                             gdal.GDT_CInt16,
+                             gdal.GDT_CInt32,
+                             gdal.GDT_CFloat32,
+                             gdal.GDT_CFloat64):
+                misc_5_internal(drv, datatype, nBands)
+
+    gdal.PopErrorHandler()
+
+    return 'success'
+
+###############################################################################
+# Test CreateCopy() with a source dataset with various band numbers (including 0) and datatype
+
+def misc_6_internal(datatype, nBands):
+    if nBands == 0:
+        ds = gdal.GetDriverByName('ILWIS').Create('tmp/tmp.mpl', 100, 100, nBands, datatype)
+    else:
+        ds = gdal.GetDriverByName('GTiff').Create('tmp/tmp.tif', 10, 10, nBands, datatype)
+        ds.GetRasterBand(1).Fill(255)
+        ds.SetGeoTransform([2,1.0/10,0,49,0,-1.0/10])
+        ds.SetProjection('GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["degree",0.01745329251994328]]')
+        ds = None
+        ds = gdal.Open('tmp/tmp.tif')
+    if ds is not None:
+        for i in range(gdal.GetDriverCount()):
+            drv = gdal.GetDriver(i)
+            md = drv.GetMetadata()
+            if md.has_key('DCAP_CREATECOPY') or md.has_key('DCAP_CREATE'):
+                #print 'drv = %s, nBands = %d, datatype = %s' % (drv.ShortName, nBands, gdal.GetDataTypeName(datatype))
+
+                skip = False
+                # FIXME: A few cases that crashes and should be investigated
+                if drv.ShortName == 'JPEG2000':
+                    if (nBands == 2 or nBands >= 5) or \
+                        not (datatype == gdal.GDT_Byte or datatype == gdal.GDT_Int16 or datatype == gdal.GDT_UInt16):
+                            skip = True
+                if drv.ShortName == 'JP2ECW' and datatype == gdal.GDT_Float64:
+                    skip = True
+                if drv.ShortName == 'NITF' and gdal.DataTypeIsComplex(datatype) == 1:
+                    skip = True
+
+                if skip is False:
+                    os.mkdir('tmp/tmp')
+                    dst_ds = drv.CreateCopy('tmp/tmp/foo', ds)
+                    dst_ds = None
+                    shutil.rmtree('tmp/tmp')
+        ds = None
+        if nBands == 0:
+            gdal.GetDriverByName('ILWIS').Delete('tmp/tmp.mpl')
+        else:
+            gdal.GetDriverByName('GTiff').Delete('tmp/tmp.tif')
+    return True
+
+def misc_6():
+
+    gdal.PushErrorHandler('CPLQuietErrorHandler')
+
+    try:
+        shutil.rmtree('tmp/tmp')
+    except:
+        pass
+
+    datatype = gdal.GDT_Byte
+    for nBands in range(6):
+        if misc_6_internal(datatype, nBands) is not True:
+            return 'fail'
+
+    nBands = 1
+    for datatype in (gdal.GDT_UInt16,
+                     gdal.GDT_Int16,
+                     gdal.GDT_UInt32,
+                     gdal.GDT_Int32,
+                     gdal.GDT_Float32,
+                     gdal.GDT_Float64,
+                     gdal.GDT_CInt16,
+                     gdal.GDT_CInt32,
+                     gdal.GDT_CFloat32,
+                     gdal.GDT_CFloat64):
+        if misc_6_internal(datatype, nBands) is not True:
+            return 'fail'
+
+    gdal.PopErrorHandler()
+
+    return 'success'
 
 gdaltest_list = [ misc_1,
                   misc_2,
-                  misc_3 ]
+                  misc_3,
+                  misc_4,
+                  misc_5,
+                  misc_6 ]
 
 if __name__ == '__main__':
 
