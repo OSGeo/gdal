692abf7210334621acd737dbdfada66585a29a9e
Even Rouault <even.rouault@mines-paris.org>
2009-01-25 14:38:44 +0000

    Additionnal tests for BSB reading issues (#2782)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@16179 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/gdrivers/bsb.py
+++ autotest/gdrivers/bsb.py
@@ -37,14 +37,23 @@ sys.path.append( '../pymod' )
 import gdaltest
 
 ###############################################################################
-# Test Read
+# Test driver availability
 
-def bsb_1():
+def bsb_0():
     try:
         gdaltest.bsb_dr = gdal.GetDriverByName( 'BSB' )
     except:
+        gdaltest.bsb_dr = None
+
+    if gdaltest.bsb_dr is None:
         return 'skip'
 
+    return 'success'
+
+###############################################################################
+# Test Read
+
+def bsb_1():
     if gdaltest.bsb_dr is None:
         return 'skip'
 
@@ -56,7 +65,6 @@ def bsb_1():
 # Test CreateCopy
 
 def bsb_2():
-
     if gdaltest.bsb_dr is None:
         return 'skip'
 
@@ -68,10 +76,71 @@ def bsb_2():
 
     return tst.testCreateCopy()
 
+###############################################################################
+# Read a BSB with an index table at the end (#2782)
+# The rgbsmall_index.kap has been generated from rgbsmall.kap by moving the
+# data of first line from offset 2382 to offset 2384, and generating the index table
+# --> This is probably not a valid BSB file, but it proves that we can read the index table
+
+def bsb_3():
+    if gdaltest.bsb_dr is None:
+        return 'skip'
+
+    tst = gdaltest.GDALTest( 'BSB', 'rgbsmall_index.kap', 1, 30321 )
+
+    return tst.testOpen()
+
+###############################################################################
+# Read a BSB without an index table but with 0 in the middle of line data
+# The rgbsmall_with_line_break.kap has been generated from rgbsmall.kap by
+# adding a 0 character in the middle of line data
+
+def bsb_4():
+    if gdaltest.bsb_dr is None:
+        return 'skip'
+
+    tst = gdaltest.GDALTest( 'BSB', 'rgbsmall_with_line_break.kap', 1, 30321 )
+
+    return tst.testOpen()
+
+###############################################################################
+# Read a truncated BSB (at the level of the written scanline number starting a new row)
+
+def bsb_5():
+    if gdaltest.bsb_dr is None:
+        return 'skip'
+
+    tst = gdaltest.GDALTest( 'BSB', 'rgbsmall_truncated.kap', 1, 29696 )
+
+    gdal.PushErrorHandler('CPLQuietErrorHandler')
+    ret = tst.testOpen()
+    gdal.PopErrorHandler()
+
+    return ret
+
+###############################################################################
+# Read another truncated BSB (in the middle of row data)
+
+def bsb_6():
+    if gdaltest.bsb_dr is None:
+        return 'skip'
+
+    tst = gdaltest.GDALTest( 'BSB', 'rgbsmall_truncated2.kap', 1, 29696 )
+
+    gdal.PushErrorHandler('CPLQuietErrorHandler')
+    ret = tst.testOpen()
+    gdal.PopErrorHandler()
+
+    return ret
 
 gdaltest_list = [
+    bsb_0,
     bsb_1,
     bsb_2,
+    bsb_3,
+    bsb_4,
+    bsb_5,
+    bsb_6
     ]
 
 
