570df154b9862afa47956f190d0c169910ecb2d6
Frank Warmerdam <warmerdam@pobox.com>
2009-01-30 05:08:40 +0000

    Add logic to chip GCPs around to avoid split over dateline problems (https://trac.osgeo.org/gdal/ticket/2809) Add logic to reproject GCPs into Mercator for mercator projected images.
    git-svn-id: https://svn.osgeo.org/gdal/branches/1.6@16203 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/bsb/bsbdataset.cpp
+++ gdal/frmts/bsb/bsbdataset.cpp
@@ -53,7 +53,7 @@ class BSBDataset : public GDALPamDataset
 {
     int         nGCPCount;
     GDAL_GCP    *pasGCPList;
-    const char  *pszGCPProjection;
+    CPLString   osGCPProjection;
 
     double      adfGeoTransform[6];
     int         bGeoTransformSet;
@@ -187,7 +187,7 @@ BSBDataset::BSBDataset()
 
     nGCPCount = 0;
     pasGCPList = NULL;
-    pszGCPProjection = "GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",7030]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY[\"EPSG\",6326]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",8901]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",9108]],AUTHORITY[\"EPSG\",4326]]";
+    osGCPProjection = "GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",7030]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY[\"EPSG\",6326]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",8901]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",9108]],AUTHORITY[\"EPSG\",4326]]";
 
     adfGeoTransform[0] = 0.0;     /* X Origin (top left corner) */
     adfGeoTransform[1] = 1.0;     /* X Pixel size */
@@ -238,20 +238,131 @@ const char *BSBDataset::GetProjectionRef()
 
 {
     if( bGeoTransformSet )
-        return pszGCPProjection;
+        return osGCPProjection;
     else
         return "";
 }
 
 /************************************************************************/
-/*                            ScanForGCPs( isNos, *pszFilename )        */
-/* Modified by Chaitanya kumar CH, chaitanyach@gmail.com to make the    */
-/* pasGCPList length dynamic.                                           */
+/*                     GDALHeuristicDatelineWrap()                      */
+/************************************************************************/
+
+static void 
+GDALHeuristicDatelineWrap( int nPointCount, double *padfX )
+
+{
+    int i;
+    double dfX_PM_Min, dfX_PM_Max, dfX_Dateline_Min, dfX_Dateline_Max;
+    int    bUsePMWrap;
+
+    if( nPointCount < 2 )
+        return;
+
+/* -------------------------------------------------------------------- */
+/*      Work out what the longitude range will be centering on the      */
+/*      prime meridian (-180 to 180) and centering on the dateline      */
+/*      (0 to 360).                                                     */
+/* -------------------------------------------------------------------- */
+    for( i = 0; i < nPointCount; i++ )
+    {
+        double dfX_PM, dfX_Dateline;
+
+        dfX_PM = padfX[i];
+        if( dfX_PM > 180 )
+            dfX_PM -= 360.0;
+
+        dfX_Dateline = padfX[i];
+        if( dfX_Dateline < 0 )
+            dfX_Dateline += 360.0;
+
+        if( i == 0 )
+        {
+            dfX_PM_Min = dfX_PM_Max = dfX_PM;
+            dfX_Dateline_Min = dfX_Dateline_Max = dfX_Dateline;
+        }
+        else
+        {
+            dfX_PM_Min = MIN(dfX_PM_Min,dfX_PM);
+            dfX_PM_Max = MAX(dfX_PM_Max,dfX_PM);
+            dfX_Dateline_Min = MIN(dfX_Dateline_Min,dfX_Dateline);
+            dfX_Dateline_Max = MAX(dfX_Dateline_Max,dfX_Dateline);
+        }
+    }
+
+/* -------------------------------------------------------------------- */
+/*      Do nothing if the range is always fairly small - no apparent    */
+/*      wrapping issues.                                                */
+/* -------------------------------------------------------------------- */
+    if( (dfX_PM_Max - dfX_PM_Min) < 270.0
+        && (dfX_Dateline_Max - dfX_Dateline_Min) < 270.0 )
+        return;
+
+/* -------------------------------------------------------------------- */
+/*      Do nothing if both appproach have a wide range - best not to    */
+/*      fiddle if we aren't sure we are improving things.               */
+/* -------------------------------------------------------------------- */
+    if( (dfX_PM_Max - dfX_PM_Min) > 270.0
+        && (dfX_Dateline_Max - dfX_Dateline_Min) > 270.0 )
+        return;
+
+/* -------------------------------------------------------------------- */
+/*      Pick which way to transform things.                             */
+/* -------------------------------------------------------------------- */
+    if( (dfX_PM_Max - dfX_PM_Min) > 270.0
+        && (dfX_Dateline_Max - dfX_Dateline_Min) < 270.0 )
+        bUsePMWrap = FALSE;
+    else
+        bUsePMWrap = TRUE;
+
+
+/* -------------------------------------------------------------------- */
+/*      Apply rewrapping.                                               */
+/* -------------------------------------------------------------------- */
+    for( i = 0; i < nPointCount; i++ )
+    {
+        if( bUsePMWrap )
+        {
+            if( padfX[i] > 180 )
+                padfX[i] -= 360.0;
+        }
+        else 
+        {
+            if( padfX[i] < 0 )
+                padfX[i] += 360.0;
+        }
+    }
+}
+
+/************************************************************************/
+/*                   GDALHeuristicDatelineWrapGCPs()                    */
+/************************************************************************/
+
+static void
+GDALHeuristicDatelineWrapGCPs( int nPointCount, GDAL_GCP *pasGCPList )
+{
+    std::vector<double> oadfX;
+    int i;
+
+    oadfX.resize( nPointCount );
+    for( i = 0; i < nPointCount; i++ )
+        oadfX[i] = pasGCPList[i].dfGCPX;
+
+    GDALHeuristicDatelineWrap( nPointCount, &(oadfX[0]) );
+
+    for( i = 0; i < nPointCount; i++ )
+        pasGCPList[i].dfGCPX = oadfX[i];
+}
+
+/************************************************************************/
+/*                            ScanForGCPs()                             */
 /************************************************************************/
 
 void BSBDataset::ScanForGCPs( bool isNos, const char *pszFilename )
 
 {
+/* -------------------------------------------------------------------- */
+/*      Collect GCPs as appropriate to source.                          */
+/* -------------------------------------------------------------------- */
     nGCPCount = 0;
 
     if ( isNos )
@@ -259,6 +370,72 @@ void BSBDataset::ScanForGCPs( bool isNos, const char *pszFilename )
         ScanForGCPsNos(pszFilename);
     } else {
         ScanForGCPsBSB();
+    }
+
+/* -------------------------------------------------------------------- */
+/*      Apply heuristics to re-wrap GCPs to maintain continguity        */
+/*      over the international dateline.                                */
+/* -------------------------------------------------------------------- */
+    if( nGCPCount > 1 )
+        GDALHeuristicDatelineWrapGCPs( nGCPCount, pasGCPList );
+
+/* -------------------------------------------------------------------- */
+/*      Can we derive a reasonable coordinate system definition for     */
+/*      this file?  For now we keep it simple, just handling            */
+/*      mercator. In the future we should consider others.              */
+/* -------------------------------------------------------------------- */
+    CPLString osUnderlyingSRS;
+    int i;
+
+    for( i = 0; psInfo->papszHeader[i] != NULL; i++ )
+    {
+        if( EQUALN(psInfo->papszHeader[i],"KNP/",4) )
+        {
+            const char *pszPR = strstr(psInfo->papszHeader[i],"PR=");
+
+            if( pszPR == NULL )
+            {
+                /* no match */
+            }
+            else if( EQUALN(pszPR,"PR=MERCATOR", 11) )
+            {
+                // We somewhat arbitrarily select our first GCPX as our 
+                // central meridian.  This is mostly helpful to ensure 
+                // that regions crossing the dateline will be contiguous 
+                // in mercator.
+                osUnderlyingSRS.Printf( "PROJCS[\"Global Mercator\",GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563]],PRIMEM[\"Greenwich\",0],UNIT[\"degree\",0.01745329251994328]],PROJECTION[\"Mercator_2SP\"],PARAMETER[\"standard_parallel_1\",0],PARAMETER[\"latitude_of_origin\",0],PARAMETER[\"central_meridian\",%d],PARAMETER[\"false_easting\",0],PARAMETER[\"false_northing\",0],UNIT[\"Meter\",1]]", (int) pasGCPList[0].dfGCPX );
+            }
+            
+            break;
+        }
+    }
+
+/* -------------------------------------------------------------------- */
+/*      If we got an alternate underlying coordinate system, try        */
+/*      converting the GCPs to that coordinate system.                  */
+/* -------------------------------------------------------------------- */
+    if( osUnderlyingSRS.length() > 0 )
+    {
+        OGRSpatialReference oWGS84_SRS, oProjected_SRS;
+        OGRCoordinateTransformation *poCT;
+        
+        oWGS84_SRS.SetWellKnownGeogCS( "WGS84" );
+        oProjected_SRS.SetFromUserInput( osUnderlyingSRS );
+        
+        poCT = OGRCreateCoordinateTransformation( &oWGS84_SRS, 
+                                                  &oProjected_SRS );
+        if( poCT != NULL )
+        {
+            for( i = 0; i < nGCPCount; i++ )
+            {
+                poCT->Transform( 1, 
+                                 &(pasGCPList[i].dfGCPX), 
+                                 &(pasGCPList[i].dfGCPY), 
+                                 &(pasGCPList[i].dfGCPZ) );
+            }
+
+            osGCPProjection = osUnderlyingSRS;
+        }
     }
 
 /* -------------------------------------------------------------------- */
@@ -272,13 +449,14 @@ void BSBDataset::ScanForGCPs( bool isNos, const char *pszFilename )
 }
 
 /************************************************************************/
-/*                            ScanForGCPsNos( pszFilename )             */
-/* Nos files have an accompanying .geo file, that contains some of the  */
-/* information normally contained in the header section with BSB files. */
-/* we try and open a file with the same name, but a .geo extension, and */
-/* look for lines like...                                               */
-/*   PointX=long lat line pixel    (using the same naming system as BSB)*/
-/*   Point1=-22.0000 64.250000 197 744                                  */
+/*                           ScanForGCPsNos()                           */
+/*                                                                      */
+/*      Nos files have an accompanying .geo file, that contains some    */
+/*      of the information normally contained in the header section     */
+/*      with BSB files. we try and open a file with the same name,      */
+/*      but a .geo extension, and look for lines like...                */
+/*      PointX=long lat line pixel    (using the same naming system     */
+/*      as BSB) Point1=-22.0000 64.250000 197 744                       */
 /************************************************************************/
 
 void BSBDataset::ScanForGCPsNos( const char *pszFilename )
@@ -505,10 +683,7 @@ int BSBDataset::GetGCPCount()
 const char *BSBDataset::GetGCPProjection()
 
 {
-    if( nGCPCount > 0 && pszGCPProjection != NULL )
-        return pszGCPProjection;
-    else
-        return "";
+    return osGCPProjection;
 }
 
 /************************************************************************/
