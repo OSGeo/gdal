ef01ba0d2fd69d352638be657bebf500ff1b46c6
Even Rouault <even.rouault@mines-paris.org>
2009-02-08 19:21:20 +0000

    Add test for RIK driver
    git-svn-id: https://svn.osgeo.org/gdal/trunk@16279 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ autotest/gdrivers/rik.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+###############################################################################
+# $Id$
+#
+# Project:  GDAL/OGR Test Suite
+# Purpose:  RIK Testing.
+# Author:   Even Rouault <even dot rouault at mines dash paris dot org>
+#
+###############################################################################
+# Copyright (c) 2009, Even Rouault <even dot rouault at mines dash paris dot org>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+###############################################################################
+
+import os
+import sys
+import gdal
+
+sys.path.append( '../pymod' )
+
+import gdaltest
+
+###############################################################################
+# Test a RIK map
+# Data downloaded from : http://www.lantmateriet.se/upload/filer/kartor/programvaror/sverige500_swe99.zip
+
+def rik_online_1():
+
+    if not gdaltest.download_file('http://www.lantmateriet.se/upload/filer/kartor/programvaror/sverige500_swe99.zip', 'sverige500_swe99.zip'):
+        return 'skip'
+
+    try:
+        os.stat('tmp/cache/sverige500_swe99.rik')
+        file_to_test = 'tmp/cache/sverige500_swe99.rik'
+    except:
+        try:
+            print 'Uncompressing ZIP file...'
+            import zipfile
+            zfobj = zipfile.ZipFile('tmp/cache/sverige500_swe99.zip')
+            outfile = open('tmp/cache/sverige500_swe99.rik', 'wb')
+            outfile.write(zfobj.read('sverige500_swe99.rik'))
+            outfile.close()
+            file_to_test = 'tmp/cache/sverige500_swe99.rik'
+        except:
+            return 'skip'
+
+    tst = gdaltest.GDALTest('RIK', file_to_test, 1, 17162, filename_absolute = 1 )
+    return tst.testOpen()
+
+
+gdaltest_list = [
+    rik_online_1
+    ]
+
+
+if __name__ == '__main__':
+
+    gdaltest.setup_run( 'RIK' )
+
+    gdaltest.run_tests( gdaltest_list )
+
+    gdaltest.summarize()
+
