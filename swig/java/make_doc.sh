ff2ce310f4b5a323064ba7265c375d71f00aa5f0
Even Rouault <even.rouault@mines-paris.org>
2009-03-06 23:03:21 +0000

    Java bindings : commit our custom utility to hook Javadoc into the SWIG-generated java files, after they have been created; First version of javadoc.java
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@16485 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ swig/java/make_doc.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Patch the generated SIWG Java files to add the Javadoc into them
+# thanks to the small utility add_javadoc
+rm -rf org_patched
+mkdir org_patched
+cp -r org org_patched
+echo "Patching .java files with Javadoc from javadoc.java"
+gcc -g -Wall add_javadoc.c -o add_javadoc
+./add_javadoc javadoc.java org_patched `find org -name "*.java"`
+
+# Generate the HTML Javadoc
+rm -rf java
+javadoc -public -d ./java -sourcepath org_patched -subpackages org.gdal -link http://java.sun.com/javase/6/docs/api -windowtitle "GDAL 1.7.0 Java bindings API"
+
+# Create a zip with the Javadoc
+rm -f javadoc.zip
+zip -r javadoc.zip java
