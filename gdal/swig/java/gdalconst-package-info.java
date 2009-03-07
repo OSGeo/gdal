b92c884b5fb7c5db4712c3e7fb75050ca114fffc
Even Rouault <even.rouault@mines-paris.org>
2009-03-07 22:28:16 +0000

    Javadoc : add gdalconst doc
    git-svn-id: https://svn.osgeo.org/gdal/trunk@16514 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ gdal/swig/java/gdalconst-package-info.java
@@ -0,0 +1,7 @@
+/**
+  * Various constants used by the org.gdal.gdal package.
+  *
+  * These constants correspond to different enumerations : error codes, XML content type, metadata, palette interpretation,
+  * color interpretation, data type, raster color table attribute type, band mask flags, resampling methods...
+  */
+package org.gdal.gdalconst;
