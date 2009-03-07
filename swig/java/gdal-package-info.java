f316997ac6c818b8068cedbd16064751582d3e6f
Even Rouault <even.rouault@mines-paris.org>
2009-03-07 12:01:13 +0000

    Javadoc : add doc for Dataset object
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@16492 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ swig/java/gdal-package-info.java
@@ -0,0 +1,6 @@
+/**
+  * Provides the classes necessary to handle GDAL rasters.
+  *
+  * This package provides the classes for the Dataset, Band, Driver,... objects that are used for handling GDAL rasters.
+  */
+package org.gdal.gdal;
