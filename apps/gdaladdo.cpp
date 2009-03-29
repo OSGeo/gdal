a601aa45705ac2e11d592e270360ddf934ed19c6
Even Rouault <even.rouault@mines-paris.org>
2009-03-29 10:43:26 +0000

    Dummy commit
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@16677 f0d54148-0727-0410-94bb-9a71ac55c965

--- apps/gdaladdo.cpp
+++ apps/gdaladdo.cpp
@@ -37,6 +37,7 @@ CPL_CVSID("$Id$");
 /************************************************************************/
 
 static void Usage()
+
 {
     printf( "Usage: gdaladdo [-r {nearest,average,gauss,average_mp,average_magphase,mode}]\n"
             "                [-ro] [-clean] [--help-general] filename levels\n"
