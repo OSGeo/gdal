cfb2d7e6f7866812d3bb05bfdb4a7cc8e4d6c6d8
Frank Warmerdam <warmerdam@pobox.com>
2009-03-29 02:29:28 +0000

    added mechanism to clear overviews if zero overviews requested (#2915)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@16670 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/apps/gdaladdo.cpp
+++ gdal/apps/gdaladdo.cpp
@@ -40,23 +40,25 @@ static void Usage()
 
 {
     printf( "Usage: gdaladdo [-r {nearest,average,gauss,average_mp,average_magphase,mode}]\n"
-            "                [-ro] [--help-general] filename levels\n"
+            "                [-ro] [-clean] [--help-general] filename levels\n"
             "\n"
             "  -r : choice of resampling method\n"
             "  -ro : open the dataset in read-only mode, in order to generate\n"
             "        external overview (for GeoTIFF datasets especially)\n"
+            "  -clean : remove all overviews\n"
             "\n"
             "Usefull configuration variables :\n"
             "  --config USE_RRD YES : Use Erdas Imagine format (.aux) as overview format.\n"
             "Below, only for external overviews in GeoTIFF format:\n"
-            "  --config COMPRESS_OVERVIEW {JPEG,LZW,PACKBITS,DEFLATE} : TIFF compression method\n"
-            "  --config PHOTOMETRIC_OVERVIEW {RGB,YCBCR,...} : TIFF photometric interpretation\n"
+            "  --config COMPRESS_OVERVIEW {JPEG,LZW,PACKBITS,DEFLATE} : TIFF compression\n"
+            "  --config PHOTOMETRIC_OVERVIEW {RGB,YCBCR,...} : TIFF photometric interp.\n"
             "  --config INTERLEAVE_OVERVIEW {PIXEL|BAND} : TIFF interleaving method\n"
-            "  --config BIGTIFF_OVERVIEW {IF_NEEDED|IF_SAFER|YES|NO} : whether BigTIFF must be used\n"
+            "  --config BIGTIFF_OVERVIEW {IF_NEEDED|IF_SAFER|YES|NO} : is BigTIFF used\n"
             "\n"
             "Examples:\n"
             " %% gdaladdo -r average abc.tif 2 4 8 16\n"
-            " %% gdaladdo --config COMPRESS_OVERVIEW JPEG --config PHOTOMETRIC_OVERVIEW YCBCR\n"
+            " %% gdaladdo --config COMPRESS_OVERVIEW JPEG\n"
+            "             --config PHOTOMETRIC_OVERVIEW YCBCR\n"
             "             --config INTERLEAVE_OVERVIEW PIXEL -ro abc.tif 2 4 8 16\n");
     exit( 1 );
 }
@@ -75,6 +77,7 @@ int main( int nArgc, char ** papszArgv )
     int          nLevelCount = 0;
     int          nResultStatus = 0;
     int          bReadOnly = FALSE;
+    int          bClean = FALSE;
 
     /* Check that we are running against at least GDAL 1.4 */
     /* Note to developers : if we use newer API, please change the requirement */
@@ -106,6 +109,8 @@ int main( int nArgc, char ** papszArgv )
             pszResampling = papszArgv[++iArg];
         else if( EQUAL(papszArgv[iArg],"-ro"))
             bReadOnly = TRUE;
+        else if( EQUAL(papszArgv[iArg],"-clean"))
+            bClean = TRUE;
         else if( pszFilename == NULL )
             pszFilename = papszArgv[iArg];
         else if( atoi(papszArgv[iArg]) > 0 )
@@ -114,7 +119,7 @@ int main( int nArgc, char ** papszArgv )
             Usage();
     }
 
-    if( pszFilename == NULL || nLevelCount == 0 )
+    if( pszFilename == NULL || (nLevelCount == 0 && !bClean) )
         Usage();
 
 /* -------------------------------------------------------------------- */
@@ -123,7 +128,11 @@ int main( int nArgc, char ** papszArgv )
     if (bReadOnly)
         hDataset = NULL;
     else
+    {
+        CPLPushErrorHandler( CPLQuietErrorHandler );
         hDataset = GDALOpen( pszFilename, GA_Update );
+        CPLPopErrorHandler();
+    }
 
     if( hDataset == NULL )
         hDataset = GDALOpen( pszFilename, GA_ReadOnly );
@@ -132,9 +141,21 @@ int main( int nArgc, char ** papszArgv )
         exit( 2 );
 
 /* -------------------------------------------------------------------- */
+/*      Clean overviews.                                                *
+/* -------------------------------------------------------------------- */
+    if ( bClean &&
+        GDALBuildOverviews( hDataset,pszResampling, 0, 0, 
+                             0, NULL, GDALTermProgress, NULL ) != CE_None )
+    {
+        printf( "Cleaning overviews failed.\n" );
+        nResultStatus = 200;
+    }
+
+/* -------------------------------------------------------------------- */
 /*      Generate overviews.                                             */
 /* -------------------------------------------------------------------- */
-    if (GDALBuildOverviews( hDataset,pszResampling, nLevelCount, anLevels,
+    if (nLevelCount > 0 && nResultStatus == 0 &&
+        GDALBuildOverviews( hDataset,pszResampling, nLevelCount, anLevels,
                              0, NULL, GDALTermProgress, NULL ) != CE_None )
     {
         printf( "Overview building failed.\n" );
