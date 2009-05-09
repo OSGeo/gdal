4e825b8c724b3bbea7d7a6fbc831150283c41ee8
Howard Butler <hobu.inc@gmail.com>
2009-05-09 01:39:46 +0000

    increment version information in preparation for release
    git-svn-id: https://svn.osgeo.org/gdal/branches/1.6@16974 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/gcore/gdal_version.h
+++ gdal/gcore/gdal_version.h
@@ -6,8 +6,8 @@
 #ifndef GDAL_VERSION_MAJOR
 #  define GDAL_VERSION_MAJOR    1
 #  define GDAL_VERSION_MINOR    6
-#  define GDAL_VERSION_REV      0
-#  define GDAL_VERSION_BUILD    4
+#  define GDAL_VERSION_REV      1
+#  define GDAL_VERSION_BUILD    0
 #endif
 
 #ifndef GDAL_VERSION_NUM
@@ -15,9 +15,9 @@
 #endif
 
 #ifndef GDAL_RELEASE_DATE
-#  define GDAL_RELEASE_DATE     20081204
+#  define GDAL_RELEASE_DATE     20090509
 #endif
 #ifndef GDAL_RELEASE_NAME
-#  define GDAL_RELEASE_NAME     "1.6.0"
+#  define GDAL_RELEASE_NAME     "1.6.1"
 #endif
 
