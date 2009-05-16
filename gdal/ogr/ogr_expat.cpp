c449f30a03792b96f2f23607a03b4df77458677d
Even Rouault <even.rouault@mines-paris.org>
2009-05-16 13:24:10 +0000

    Add convenience function to create a safe XML Expat parser, to be used by KML, GPX and GeoRSS drivers
    git-svn-id: https://svn.osgeo.org/gdal/trunk@17027 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ gdal/ogr/ogr_expat.cpp
@@ -0,0 +1,71 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  OGR
+ * Purpose:  Convenience function for parsing with Expat library
+ * Author:   Even Rouault, even dot rouault at mines dash paris dot org
+ *
+ ******************************************************************************
+ * Copyright (c) 2009, Even Rouault
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+
+#ifdef HAVE_EXPAT
+
+#include "ogr_expat.h"
+#include "cpl_error.h"
+
+#define OGR_EXPAT_MAX_ALLOWED_ALLOC 100000
+
+static void* OGRExpatMalloc(size_t size)
+{
+    if (size < OGR_EXPAT_MAX_ALLOWED_ALLOC)
+        return malloc(size);
+    else
+    {
+        CPLError(CE_Failure, CPLE_OutOfMemory,
+                 "Expat tried to malloc %d bytes. File probably corrupted", (int)size);
+        return NULL;
+    }
+}
+
+static void* OGRExpatRealloc(void *ptr, size_t size)
+{
+    if (size < OGR_EXPAT_MAX_ALLOWED_ALLOC)
+        return realloc(ptr, size);
+    else
+    {
+        CPLError(CE_Failure, CPLE_OutOfMemory,
+                 "Expat tried to realloc %d bytes. File probably corrupted", (int)size);
+        free(ptr);
+        return NULL;
+    }
+}
+
+XML_Parser OGRCreateExpatXMLParser()
+{
+    XML_Memory_Handling_Suite memsuite;
+    memsuite.malloc_fcn = OGRExpatMalloc;
+    memsuite.realloc_fcn = OGRExpatRealloc;
+    memsuite.free_fcn = free;
+    return XML_ParserCreate_MM(NULL, &memsuite, NULL);
+}
+
+#endif
