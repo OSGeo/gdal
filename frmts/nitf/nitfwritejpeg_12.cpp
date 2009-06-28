857636d27ab215a1dab1923822c8944005fa5697
Even Rouault <even.rouault@mines-paris.org>
2009-06-28 09:59:38 +0000

    Enable the NITF driver to read and create 12bit JPEG compressed NITF files  when JPEG12_ENABLED=yes
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@17305 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ frmts/nitf/nitfwritejpeg_12.cpp
@@ -0,0 +1,5 @@
+#if defined(JPEG_DUAL_MODE_8_12)
+#  define LIBJPEG_12_PATH "../../jpeg/libjpeg12/jpeglib.h" 
+#  define NITFWriteJPEGBlock NITFWriteJPEGBlock_12
+#  include "nitfwritejpeg.cpp"
+#endif /* defined(JPEG_DUAL_MODE_8_12) */
