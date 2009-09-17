ba70105e1e1654811e9f28f6600948afe37caa71
Frank Warmerdam <warmerdam@pobox.com>
2009-09-17 14:24:30 +0000

    fix problem with joins and special fields (#3143)
    git-svn-id: https://svn.osgeo.org/gdal/branches/1.5@17647 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/generic/ogr_gensql.cpp
+++ gdal/ogr/ogrsf_frmts/generic/ogr_gensql.cpp
@@ -534,6 +534,9 @@ OGRFeature *OGRGenSQLResultsLayer::TranslateFeature( OGRFeature *poSrcFeat )
     {
         swq_col_def *psColDef = psSelectInfo->column_defs + iField;
 
+        if( psColDef->table_index != 0 )
+            continue;
+
         if ( psColDef->field_index >= iFIDFieldIndex &&
             psColDef->field_index < iFIDFieldIndex + SPECIAL_FIELD_COUNT )
         {
@@ -546,7 +549,7 @@ OGRFeature *OGRGenSQLResultsLayer::TranslateFeature( OGRFeature *poSrcFeat )
                 poDstFeat->SetField( iField, poSrcFeat->GetFieldAsString(psColDef->field_index) );
             }
         }
-        else if( psColDef->table_index == 0 )
+        else
             poDstFeat->SetField( iField,
                          poSrcFeat->GetRawFieldRef( psColDef->field_index ) );
     }
