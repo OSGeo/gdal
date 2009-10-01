537f9731524ca764a09536e1adc89ffbfe8bd8b6
Even Rouault <even.rouault@mines-paris.org>
2009-10-01 21:58:05 +0000

    Epsilon driver : add CreateCopy() support. Rasterlite : update for Epsilon write mode
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@17727 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/rasterlite/rasterlitedataset.cpp
+++ frmts/rasterlite/rasterlitedataset.cpp
@@ -1289,6 +1289,9 @@ void GDALRegister_Rasterlite()
 
 {
     GDALDriver  *poDriver;
+    
+    if (! GDAL_CHECK_VERSION("Rasterlite driver"))
+        return;
 
     if( GDALGetDriverByName( "Rasterlite" ) == NULL )
     {
@@ -1309,10 +1312,10 @@ void GDALRegister_Rasterlite()
 "   <Option name='TILED' type='boolean' default='YES' description='Use tiling'/>"
 "   <Option name='BLOCKXSIZE' type='int' default='256' description='Tile Width'/>"
 "   <Option name='BLOCKYSIZE' type='int' default='256' description='Tile Height'/>"
-"   <Option name='DRIVER' type='string' default='GTiff' description='GDAL driver to use for storing tiles'/>"
-"   <Option name='COMPRESS' type='string' default='Compression method of the GTiff driver'/>"
-"   <Option name='QUALITY' type='int' description='JPEG quality 1-100 (GTiff / JPEG drivers)' default='75'/>"
-"   <Option name='PHOTOMETRIC' type='string-select' description='Photometric interpretation of the GTiff driver'>"
+"   <Option name='DRIVER' type='string' default='GTiff' description='GDAL driver to use for storing tiles' default='GTiff'/>"
+"   <Option name='COMPRESS' type='string' default='(GTiff driver) Compression method' default='NONE'/>"
+"   <Option name='QUALITY' type='int' description='(GTiff / JPEG drivers) JPEG quality 1-100' default='75'/>"
+"   <Option name='PHOTOMETRIC' type='string-select' description='(GTiff driver) Photometric interpretation'>"
 "       <Value>MINISBLACK</Value>"
 "       <Value>MINISWHITE</Value>"
 "       <Value>PALETTE</Value>"
@@ -1323,6 +1326,8 @@ void GDALRegister_Rasterlite()
 "       <Value>ICCLAB</Value>"
 "       <Value>ITULAB</Value>"
 "   </Option>"
+"   <Option name='TARGET' type='int' description='(EPSILON driver) target size reduction as a percentage of the original (0-100)' default='96'/>"
+"   <Option name='FILTER' type='string' description='(EPSILON driver) Filter ID' default='daub97lift'/>"
 "</CreationOptionList>" );
 
         poDriver->pfnOpen = RasterliteDataset::Open;
