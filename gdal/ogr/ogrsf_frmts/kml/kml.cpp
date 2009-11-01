OSGeo/gdal

commit 134386380a03501454c201cb8f68d0fc531a42ed

KML driver: fix bad performance with huge number of layers; fix how we deal with multi-line content (I'm still wondering why we don't preserve original content)
git-svn-id: https://svn.osgeo.org/gdal/trunk@17491 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/ogr/ogrsf_frmts/kml/kml.cpp
@@ -45,13 +45,14 @@ KML::KML()
 	poTrunk_ = NULL;
 	poCurrent_ = NULL;
 	nNumLayers_ = -1;
-        nCurrentLayer_ = -1;
+        papoLayers_ = NULL;
 }
 
 KML::~KML()
 {
     if( NULL != pKMLFile_ )
         VSIFCloseL(pKMLFile_);
+    CPLFree(papoLayers_);
 
     delete poTrunk_;
 }
@@ -328,18 +329,19 @@ void XMLCALL KML::endElement(void* pUserData, const char* pszName)
         poKML->nDepth_--;
         poTmp = poKML->poCurrent_;
         // Split the coordinates
-        if(poKML->poCurrent_->getName().compare("coordinates") == 0)
+        if(poKML->poCurrent_->getName().compare("coordinates") == 0 &&
+           poKML->poCurrent_->numContent() == 1)
         {
-            std::string sData = poKML->poCurrent_->getContent(0);
+            std::string& sData = poKML->poCurrent_->getContent(0);
             std::size_t nPos = 0;
             std::size_t nLength = sData.length();
+            const char* pszData = sData.c_str();
             while(TRUE)
             {
                 // Cut off whitespaces
                 while(nPos < nLength &&
-                      (sData[nPos] == ' ' || sData[nPos] == '\n'
-                    || sData[nPos] == '\r' || 
-                        sData[nPos] == '\t' ))
+                      (pszData[nPos] == ' ' || pszData[nPos] == '\n'
+                       || pszData[nPos] == '\r' || pszData[nPos] == '\t' ))
                     nPos ++;
 
                 if (nPos == nLength)
@@ -349,15 +351,68 @@ void XMLCALL KML::endElement(void* pUserData, const char* pszName)
 
                 // Get content
                 while(nPos < nLength &&
-                      sData[nPos] != ' ' && sData[nPos] != '\n' && sData[nPos] != '\r' && 
-                      sData[nPos] != '\t')
+                      pszData[nPos] != ' ' && pszData[nPos] != '\n' && pszData[nPos] != '\r' && 
+                      pszData[nPos] != '\t')
                     nPos++;
 
                 if(nPos - nPosBegin > 0)
-                    poKML->poCurrent_->addContent(sData.substr(nPosBegin, nPos - nPosBegin));
+                {
+                    std::string sTmp(pszData + nPosBegin, nPos - nPosBegin);
+                    poKML->poCurrent_->addContent(sTmp);
+                }
             }
             if(poKML->poCurrent_->numContent() > 1)
                 poKML->poCurrent_->deleteContent(0);
+        }
+        else if (poKML->poCurrent_->numContent() == 1)
+        {
+            std::string& sData = poKML->poCurrent_->getContent(0);
+            std::string sDataWithoutNL;
+            std::size_t nPos = 0;
+            std::size_t nLength = sData.length();
+            const char* pszData = sData.c_str();
+            std::size_t nLineStartPos = 0;
+            int bLineStart = TRUE;
+
+            /* Re-assemble multi-line content by removing leading spaces for each line */
+            /* I'm not sure why we do that. Shouldn't we preserve content as such ? */
+            while(nPos < nLength)
+            {
+                char ch = pszData[nPos];
+                if (bLineStart && (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r'))
+                    nLineStartPos ++;
+                else if (ch == '\n' || ch == '\r')
+                {
+                    if (!bLineStart)
+                    {
+                        std::string sTmp(pszData + nLineStartPos, nPos - nLineStartPos);
+                        if (sDataWithoutNL.size() > 0)
+                            sDataWithoutNL += " ";
+                        sDataWithoutNL += sTmp;
+                        bLineStart = TRUE;
+                    }
+                    nLineStartPos = nPos + 1;
+                }
+                else
+                {
+                    bLineStart = FALSE;
+                }
+                nPos ++;
+            }
+
+            if (nLineStartPos > 0)
+            {
+                if (nLineStartPos < nPos)
+                {
+                    std::string sTmp(pszData + nLineStartPos, nPos - nLineStartPos);
+                    if (sDataWithoutNL.size() > 0)
+                        sDataWithoutNL += " ";
+                    sDataWithoutNL += sTmp;
+                }
+
+                poKML->poCurrent_->deleteContent(0);
+                poKML->poCurrent_->addContent(sDataWithoutNL);
+            }
         }
 
         if(poKML->poCurrent_->getParent() != NULL)
@@ -402,24 +457,11 @@ void XMLCALL KML::dataHandler(void* pUserData, const char* pszData, int nLen)
     }
 
     std::string sData(pszData, nLen);
-    std::string sTmp;
 
-    if(poKML->poCurrent_->getName().compare("coordinates") == 0)
-    {
-        if(poKML->poCurrent_->numContent() == 0)
-            poKML->poCurrent_->addContent(sData);
-        else
-            poKML->poCurrent_->appendContent(sData);
-    }
+    if(poKML->poCurrent_->numContent() == 0)
+        poKML->poCurrent_->addContent(sData);
     else
-    {
-        while(sData[0] == ' ' || sData[0] == '\n' || sData[0] == '\r' || sData[0] == '\t')
-        {
-            sData = sData.substr(1, sData.length()-1);
-        }
-        if(sData.length() > 0)
-            poKML->poCurrent_->addContent(sData);
-    }
+        poKML->poCurrent_->appendContent(sData);
 }
 
 bool KML::isValid()
@@ -500,19 +542,10 @@ int KML::getNumLayers() const
 }
 
 bool KML::selectLayer(int nNum) {
-    if (nNum == nCurrentLayer_)
-        return TRUE;
-
     if(this->nNumLayers_ < 1 || nNum >= this->nNumLayers_)
         return FALSE;
-    poCurrent_ = poTrunk_->getLayer(nNum);
-    if(poCurrent_ == NULL)
-        return FALSE;
-    else
-    {
-        nCurrentLayer_ = nNum;
-        return TRUE;
-    }
+    poCurrent_ = papoLayers_[nNum];
+    return TRUE;
 }
 
 std::string KML::getCurrentName() const

gdal/ogr/ogrsf_frmts/kml/kml.h
@@ -89,6 +89,7 @@ protected:
 	KMLNode* poTrunk_;
 	// number of layers;
 	int nNumLayers_;
+        KMLNode** papoLayers_;
 
 private:
 	// depth of the DOM
@@ -103,7 +104,6 @@ private:
 	std::string sError_;
 	// current KMLNode
 	KMLNode *poCurrent_;
-        int nCurrentLayer_;
         
         XML_Parser oCurrentParser;
         int nDataHandlerCounter;

gdal/ogr/ogrsf_frmts/kml/kmlnode.cpp
@@ -317,7 +317,7 @@ void KMLNode::setName(std::string const& sIn)
     sName_ = sIn;
 }
 
-std::string KMLNode::getName() const
+const std::string& KMLNode::getName() const
 {
     return sName_;
 }
@@ -373,14 +373,9 @@ void KMLNode::appendContent(std::string const& text)
     tmp += text;
 }
 
-std::string KMLNode::getContent(std::size_t index) const
+std::string& KMLNode::getContent(std::size_t index) const
 {
-    std::string tmp;
-    if( index < pvsContent_->size() )
-    {
-        tmp = (*pvsContent_)[index];
-    }
-    return tmp;
+    return (*pvsContent_)[index];
 }
 
 void KMLNode::deleteContent(std::size_t index)
@@ -406,25 +401,8 @@ int KMLNode::getLayerNumber() const
     return nLayerNumber_;
 }
 
-KMLNode* KMLNode::getLayer(int nNum)
-{
-    KMLNode* poTmp = NULL;
-    if(nLayerNumber_ == nNum)
-        return this;
-
-    kml_nodes_t::size_type size = pvpoChildren_->size();
-    for( kml_nodes_t::size_type i = 0; i < size; ++i )
-    {
-        if((poTmp = (*pvpoChildren_)[i]->getLayer(nNum)) != NULL)
-            return poTmp;
-    }
-
-    return NULL;
-}
-
 std::string KMLNode::getNameElement() const
 {
-    std::string sElem;
     kml_nodes_t::size_type subsize = 0;
     kml_nodes_t::size_type size = pvpoChildren_->size();
 
@@ -435,12 +413,7 @@ std::string KMLNode::getNameElement() const
             subsize = (*pvpoChildren_)[i]->pvsContent_->size();
             if( subsize > 0 )
             {
-                sElem = (*(*pvpoChildren_)[i]->pvsContent_)[0];
-                for( kml_nodes_t::size_type j = 1; j < subsize; ++j )
-                {
-                    sElem += " " + (*(*pvpoChildren_)[i]->pvsContent_)[j];
-                }
-                return sElem;
+                return (*(*pvpoChildren_)[i]->pvsContent_)[0];
             }
             break;
         }
@@ -450,7 +423,6 @@ std::string KMLNode::getNameElement() const
 
 std::string KMLNode::getDescriptionElement() const
 {
-    std::string sElem;
     kml_nodes_t::size_type subsize = 0;
     kml_nodes_t::size_type size = pvpoChildren_->size();
     for( kml_nodes_t::size_type i = 0; i < size; ++i )
@@ -460,12 +432,7 @@ std::string KMLNode::getDescriptionElement() const
             subsize = (*pvpoChildren_)[i]->pvsContent_->size();
             if ( subsize > 0 )
             {
-                sElem = (*(*pvpoChildren_)[i]->pvsContent_)[0];
-                for( kml_nodes_t::size_type j = 1; j < subsize; ++j )
-                {
-                    sElem += " " + (*(*pvpoChildren_)[i]->pvsContent_)[j];
-                }
-                return sElem;
+                return (*(*pvpoChildren_)[i]->pvsContent_)[0];
             }
             break;
         }

gdal/ogr/ogrsf_frmts/kml/kmlnode.h
@@ -54,7 +54,7 @@ public:
     Nodetype getType() const;
     
     void setName(std::string const& name);
-    std::string getName() const;
+    const std::string& getName() const;
     
     void setLevel(std::size_t level);
     std::size_t getLevel() const;
@@ -71,14 +71,12 @@ public:
 
     void addContent(std::string const& text);
     void appendContent(std::string const& text);
-    std::string getContent(std::size_t index) const;
+    std::string& getContent(std::size_t index) const;
     void deleteContent(std::size_t index);
     std::size_t numContent();
 
     void setLayerNumber(int nNum);
     int getLayerNumber() const;
-
-    KMLNode* getLayer(int);
 
     std::string getNameElement() const;
     std::string getDescriptionElement() const;

gdal/ogr/ogrsf_frmts/kml/kmlvector.cpp
@@ -137,6 +137,8 @@ void KMLVector::findLayers(KMLNode* poNode)
             nodeType == MultiLineString || nodeType == MultiPolygon)
         {
             poNode->setLayerNumber(nNumLayers_++);
+            papoLayers_ = (KMLNode**)CPLRealloc(papoLayers_, nNumLayers_ * sizeof(KMLNode*));
+            papoLayers_[nNumLayers_ - 1] = poNode;
         }
         else
         {

commit 0564a5242d4004a8431af423eaca505c88de8bb0

Fix regression in KML driver introduced in r17491 (trunk only) that caused XML content within <description> tag to be lost
git-svn-id: https://svn.osgeo.org/gdal/trunk@17945 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/ogr/ogrsf_frmts/kml/kml.cpp
@@ -245,7 +245,10 @@ void XMLCALL KML::startElement(void* pUserData, const char* pszName, const char*
             sNewContent += ppszAttr[i + 1];
         }
         sNewContent += ">";
-        poKML->poCurrent_->addContent(sNewContent);
+        if(poKML->poCurrent_->numContent() == 0)
+            poKML->poCurrent_->addContent(sNewContent);
+        else
+            poKML->poCurrent_->appendContent(sNewContent);
     }
 }
 
@@ -436,7 +439,10 @@ void XMLCALL KML::endElement(void* pUserData, const char* pszName)
         std::string sNewContent = "</";
         sNewContent += pszName;
         sNewContent += ">";
-        poKML->poCurrent_->addContent(sNewContent);
+        if(poKML->poCurrent_->numContent() == 0)
+            poKML->poCurrent_->addContent(sNewContent);
+        else
+            poKML->poCurrent_->appendContent(sNewContent);
     }
 }
 