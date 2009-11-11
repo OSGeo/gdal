09b1e6703eb759b53575c16d61fed144589e6383
Frank Warmerdam <warmerdam@pobox.com>
2009-11-11 17:09:44 +0000

    handle projection methods 1024 and 9841 as google mercator special case (https://trac.osgeo.org/gdal/ticket/3217)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@17991 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/gtiff/gt_wkt_srs.cpp
+++ frmts/gtiff/gt_wkt_srs.cpp
@@ -497,6 +497,12 @@ char *GTIFGetOGISDefn( GTIF *hGTIF, GTIFDefn * psDefn )
             oSRS.SetMercator( adfParm[0], adfParm[1],
                               adfParm[4],
                               adfParm[5], adfParm[6] );
+                              
+            if (psDefn->Projection == 1024 || psDefn->Projection == 9841) // override hack for google mercator. 
+            {
+                oSRS.SetExtension( "PROJCS", "PROJ4",  
+                                    "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs" ); 
+            }
             break;
 
           case CT_ObliqueStereographic:
