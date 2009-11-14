63f8c6b1524973b7c66e55955c10ab37c96d90fb
Even Rouault <even.rouault@mines-paris.org>
2009-11-14 14:27:14 +0000

    Cleanup: remove m_bands_count as it is just an alias for nBands
    git-svn-id: https://svn.osgeo.org/gdal/trunk@18018 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/wms/rasterband.cpp
+++ gdal/frmts/wms/rasterband.cpp
@@ -343,7 +343,7 @@ CPLErr GDALWMSRasterBand::ReadBlockFromFile(int x, int y, const char *file_name,
         }
         if (ret == CE_None) {
             int nDSRasterCount = ds->GetRasterCount();
-            if (nDSRasterCount != m_parent_dataset->m_bands_count) {
+            if (nDSRasterCount != m_parent_dataset->nBands) {
                 /* Maybe its an image with color table */
                 bool accepted_as_ct = false;
                 if ((eDataType == GDT_Byte) && (ds->GetRasterCount() == 1)) {
@@ -374,13 +374,13 @@ CPLErr GDALWMSRasterBand::ReadBlockFromFile(int x, int y, const char *file_name,
                     }
                 }
 
-                if (nDSRasterCount == 4 && m_parent_dataset->m_bands_count == 3)
+                if (nDSRasterCount == 4 && m_parent_dataset->nBands == 3)
                 {
                     /* metacarta TMS service sometimes return a 4 band PNG instead of the expected 3 band... */
                 }
                 else if (!accepted_as_ct) {
                     CPLError(CE_Failure, CPLE_AppDefined, "GDALWMS: Incorrect bands count %d in downloaded block, expected %d.",
-                        nDSRasterCount, m_parent_dataset->m_bands_count);
+                        nDSRasterCount, m_parent_dataset->nBands);
                     ret = CE_Failure;
                 }
             }
