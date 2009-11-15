474ea680088fb6b877a65d32910c12c13b0de454
Jorge Arevalo <jorgearevalo@gis4free.org>
2009-11-15 02:06:06 +0000

    Added script for creating RASTER_OVERVIEWS table
    git-svn-id: https://svn.osgeo.org/gdal/trunk@18032 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ autotest/gdrivers/data/create_raster_overviews_table.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Create table raster overviews in "public" schema
+
+DATABASE_NAME=gisdb
+HOST_NAME=localhost
+USER_NAME=postgres	# Need a user with permission to create tables here!
+
+CREATE_OV_TABLE="CREATE TABLE public.raster_overviews (o_table_catalog character varying(256) NOT NULL, o_table_schema character varying(256) NOT NULL, o_table_name character varying(256) NOT NULL, o_column character varying(256) NOT NULL, r_table_catalog character varying(256) NOT NULL, r_table_schema character varying(256) NOT NULL, r_table_name character varying(256) NOT NULL, r_column character varying(256) NOT NULL, out_db boolean NOT NULL, overview_factor integer NOT NULL, CONSTRAINT raster_overviews_pk PRIMARY KEY (o_table_catalog, o_table_schema, o_table_name, o_column));"
+
+psql -h $HOST_NAME -U $USER_NAME -d $DATABASE_NAME -c "$CREATE_OV_TABLE"
+psql -h $HOST_NAME -U $USER_NAME -d $DATABASE_NAME -c "ALTER TABLE public.raster_overviews OWNER TO gis;"
