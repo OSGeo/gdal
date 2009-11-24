212a676d8445ff28c0ad66a057e75b33a0d5d5eb
Even Rouault <even.rouault@mines-paris.org>
2009-11-24 20:48:51 +0000

    Check for NULL returns of GDALGetDriverByName() in the unlikely case HFA or GTiff drivers would be disabled
    git-svn-id: https://svn.osgeo.org/gdal/trunk@18092 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/hfa/hfa_overviews.cpp
+++ gdal/frmts/hfa/hfa_overviews.cpp
@@ -80,6 +80,13 @@ CPLErr HFAAuxBuildOverviews( const char *pszOvrFilename,
 /*      base band.                                                      */
 /* -------------------------------------------------------------------- */
         GDALDriver *poHFADriver = (GDALDriver *) GDALGetDriverByName("HFA");
+        if (poHFADriver == NULL)
+        {
+            CPLError( CE_Failure, CPLE_AppDefined, 
+                      "HFA driver is unavailable." );
+            return CE_Failure;
+        }
+        
         const char *apszOptions[3] = { "COMPRESSED=YES", NULL, NULL };
         
         CPLString osDepFileOpt = "DEPENDENT_FILE=";
