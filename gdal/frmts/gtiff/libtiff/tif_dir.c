b7a0a7a78cef8fa62efaed492048856e958b07df
Frank Warmerdam <warmerdam@pobox.com>
2009-12-04 01:45:42 +0000

    updated to libtiff cvs head, mostly to readscanline fix (#3259)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@18169 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/gtiff/libtiff/tif_dir.c
+++ gdal/frmts/gtiff/libtiff/tif_dir.c
@@ -1,4 +1,4 @@
-/* $Id: tif_dir.c,v 1.100 2009-01-23 05:25:19 fwarmerdam Exp $ */
+/* $Id: tif_dir.c,v 1.101 2009-11-30 18:19:16 fwarmerdam Exp $ */
 
 /*
  * Copyright (c) 1988-1997 Sam Leffler
@@ -377,6 +377,10 @@ _TIFFVSetField(TIFF* tif, uint32 tag, va_list ap)
 		for (i = 0; i < v; i++)
 			_TIFFsetShortArray(&td->td_transferfunction[i],
 			    va_arg(ap, uint16*), 1L<<td->td_bitspersample);
+		break;
+	case TIFFTAG_REFERENCEBLACKWHITE:
+		/* XXX should check for null range */
+		_TIFFsetFloatArray(&td->td_refblackwhite, va_arg(ap, float*), 6);
 		break;
 	case TIFFTAG_INKNAMES:
 		v = (uint16) va_arg(ap, uint16_vap);
@@ -887,6 +891,9 @@ _TIFFVGetField(TIFF* tif, uint32 tag, va_list ap)
 				*va_arg(ap, uint16**) = td->td_transferfunction[2];
 			}
 			break;
+		case TIFFTAG_REFERENCEBLACKWHITE:
+			*va_arg(ap, float**) = td->td_refblackwhite;
+			break;
 		case TIFFTAG_INKNAMES:
 			*va_arg(ap, char**) = td->td_inknames;
 			break;
@@ -1075,6 +1082,7 @@ TIFFFreeDirectory(TIFF* tif)
 	CleanupField(td_sampleinfo);
 	CleanupField(td_subifd);
 	CleanupField(td_inknames);
+	CleanupField(td_refblackwhite);
 	CleanupField(td_transferfunction[0]);
 	CleanupField(td_transferfunction[1]);
 	CleanupField(td_transferfunction[2]);
