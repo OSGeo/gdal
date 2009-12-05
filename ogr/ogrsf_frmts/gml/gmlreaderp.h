6abbe20290aaf0c716da79350693f1432438a208
Chaitanya Kumar <chaitanya.ch@gmail.com>
2009-12-05 20:45:43 +0000

    Fix minor memory leak. (https://trac.osgeo.org/gdal/ticket/1540)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@18186 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/ogrsf_frmts/gml/gmlreaderp.h
+++ ogr/ogrsf_frmts/gml/gmlreaderp.h
@@ -200,6 +200,8 @@ public:
 class GMLReader : public IGMLReader 
 {
 private:
+    static int    m_bXercesInitialized;
+    static int    m_nInstanceCount;
     int           m_bClassListLocked;
 
     int         m_nClassCount;
