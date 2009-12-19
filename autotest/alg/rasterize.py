a61c2d97e12a541710833f57d8dcb88f7440f634
Chaitanya Kumar <chaitanya.ch@gmail.com>
2009-12-19 04:41:45 +0000

    Fix minor bugs from r18227. Add rasterization using Z value for polygons(rudimentary). Added test in autotest. https://trac.osgeo.org/gdal/ticket/3202
    git-svn-id: https://svn.osgeo.org/gdal/trunk@18340 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/alg/rasterize.py
+++ autotest/alg/rasterize.py
@@ -167,9 +167,72 @@ def rasterize_2():
     
     return 'success'
 
+###############################################################################
+# Rasterization with BURN_VALUE_FROM.
+
+def rasterize_3():
+
+    try:
+        x = gdal.RasterizeLayer
+        gdaltest.have_ng = 1
+    except:
+        gdaltest.have_ng = 0
+        return 'skip'
+
+    # Setup working spatial reference
+    sr_wkt = 'LOCAL_CS["arbitrary"]'
+    sr = osr.SpatialReference( sr_wkt )
+    
+    # Create a memory raster to rasterize into.
+
+    target_ds = gdal.GetDriverByName('MEM').Create( '', 100, 100, 3,
+                                                    gdal.GDT_Byte )
+    target_ds.SetGeoTransform( (1000,1,0,1100,0,-1) )
+    target_ds.SetProjection( sr_wkt )
+    
+    # Create a memory layer to rasterize from.
+
+    rast_ogr_ds = \
+              ogr.GetDriverByName('Memory').CreateDataSource( 'wrk' )
+    rast_mem_lyr = rast_ogr_ds.CreateLayer( 'poly', srs=sr )
+
+    # Add polygons and linestrings.
+    wkt_geom = ['POLYGON((1020 1030 40,1020 1045 30,1050 1045 20,1050 1030 35,1020 1030 40))',
+                'POLYGON((1010 1046 85,1015 1055 35,1055 1060 26,1054 1048 35,1010 1046 85))',
+                'POLYGON((1020 1076 190,1025 1085 35,1065 1090 26,1064 1078 35,1020 1076 190),(1023 1079 5,1061 1081 35,1062 1087 26,1028 1082 35,1023 1079 85))',
+                'LINESTRING(1005 1000 10, 1100 1050 120)',
+                'LINESTRING(1000 1000 150, 1095 1050 -5, 1080 1080 200)']
+    for g in wkt_geom:
+        feat = ogr.Feature( rast_mem_lyr.GetLayerDefn() )
+        feat.SetGeometryDirectly( ogr.Geometry(wkt = g) )
+        rast_mem_lyr.CreateFeature( feat )
+
+    # Run the algorithm.
+
+    err = gdal.RasterizeLayer( target_ds, [3,2,1], rast_mem_lyr,
+                               burn_values = [10,10,55], options = ["BURN_VALUE_FROM=Z"] )
+
+    if err != 0:
+        print err
+        gdaltest.post_reason( 'got non-zero result code from RasterizeLayer' )
+        return 'fail'
+
+    # Check results.
+
+    expected = 15006
+    checksum = target_ds.GetRasterBand(2).Checksum()
+    if checksum != expected:
+        print checksum
+        gdaltest.post_reason( 'Did not get expected image checksum' )
+        gdal.GetDriverByName('GTiff').CreateCopy('tmp/rasterize_3.tif',target_ds)
+        return 'fail'
+    
+    return 'success'
+
 gdaltest_list = [
     rasterize_1,
-    rasterize_2
+    rasterize_2,
+    rasterize_3,
     ]
 
 if __name__ == '__main__':
