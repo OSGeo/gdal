23bf4d4270c7f7fd88df77ee76e59473ad46c52f
Even Rouault <even.rouault@mines-paris.org>
2009-12-23 18:22:14 +0000

    Add tests for OGR GTM write support (https://trac.osgeo.org/gdal/ticket/3113)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@18376 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/ogr/ogr_gtm.py
+++ autotest/ogr/ogr_gtm.py
@@ -65,7 +65,7 @@ def ogr_gtm_init():
 ###############################################################################
 # Test waypoints gtm layer.
 
-def ogr_gtm_1():
+def ogr_gtm_read_1():
     if not gdaltest.have_gtm:
         return 'skip'
     
@@ -89,7 +89,6 @@ def ogr_gtm_1():
         gdaltest.post_reason( 'Wrong comment field value' )
         return 'fail'
 
-
     if feat.GetField('icon') != 92:
         gdaltest.post_reason( 'Wrong icon field value' )
         return 'fail'
@@ -100,9 +99,9 @@ def ogr_gtm_1():
 
     wkt = 'POINT (-47.789974212646484 -21.201919555664062)'
     if ogrtest.check_feature_geometry( feat, wkt):
+        gdaltest.post_reason( 'Unexpected geometry' )
         return 'fail'
     
-
     # Test 2nd feature
     feat = lyr.GetNextFeature()
 
@@ -116,17 +115,17 @@ def ogr_gtm_1():
 
 
     if feat.GetField('icon') != 4:
-        gdaltest.post_reason( 'Wrong time field value' )
+        gdaltest.post_reason( 'Wrong icon field value' )
         return 'fail'
 
     if feat.GetField('time') != '2009/12/18 17:34:46':
-        gdaltest.post_reason( 'Wrong icon field value' )
+        gdaltest.post_reason( 'Wrong time field value' )
         return 'fail'
 
     wkt = 'POINT (-47.909481048583984 -21.294229507446289)'
     if ogrtest.check_feature_geometry( feat, wkt):
+        gdaltest.post_reason( 'Unexpected geometry' )
         return 'fail'
-
 
     # Test 3rd feature
     feat = lyr.GetNextFeature()
@@ -139,7 +138,6 @@ def ogr_gtm_1():
         gdaltest.post_reason( 'Wrong comment field value' )
         return 'fail'
 
-
     if feat.GetField('icon') != 61:
         gdaltest.post_reason( 'Wrong icon field value' )
         return 'fail'
@@ -150,14 +148,14 @@ def ogr_gtm_1():
 
     wkt = 'POINT (-47.806097491943362 -21.176849600708007)'
     if ogrtest.check_feature_geometry( feat, wkt):
+        gdaltest.post_reason( 'Unexpected geometry' )
         return 'fail'
-    
 
     return 'success'
 
 ###############################################################################
-# Test traks gtm layer.
-def ogr_gtm_2():
+# Test tracks gtm layer.
+def ogr_gtm_read_2():
     if not gdaltest.have_gtm:
         return 'skip'
     
@@ -194,8 +192,8 @@ def ogr_gtm_2():
           '-47.809136624130645 -21.176562836150087,' + \
           '-47.809931418108405 -21.175971104366582)'
     if ogrtest.check_feature_geometry( feat, wkt):
+        gdaltest.post_reason( 'Unexpected geometry' )
         return 'fail'
-    
 
     # Test 2nd feature
     feat = lyr.GetNextFeature()
@@ -223,8 +221,6 @@ def ogr_gtm_2():
     if ogrtest.check_feature_geometry( feat, wkt):
         return 'fail'
     
-
-
     # Test 3rd feature
     feat = lyr.GetNextFeature()
 
@@ -253,11 +249,235 @@ def ogr_gtm_2():
                       '-47.7894287109375 -21.194473266601562)'
 
     if ogrtest.check_feature_geometry( feat, wkt):
+        gdaltest.post_reason( 'Unexpected geometry' )
         return 'fail'
     
+    return 'success'
+
+
+###############################################################################
+# Write test
+
+###############################################################################
+# Waypoint write
+def ogr_gtm_write_1():
+
+    ds = ogr.GetDriverByName('GPSTrackMaker').CreateDataSource('tmp/gtm.gtm')
+    lyr = ds.CreateLayer('gtm_waypoints', geom_type = ogr.wkbPoint)
+
+    dst_feat = ogr.Feature( lyr.GetLayerDefn() )
+    dst_feat.SetField('name', 'WAY0000000')
+    dst_feat.SetField('comment', 'Waypoint 0')
+    dst_feat.SetField('icon', 10)
+    dst_feat.SetField('time', '2009/12/23 14:25:46')
+    dst_feat.SetGeometry(ogr.CreateGeometryFromWkt('POINT (-21 -47)'))
+    if lyr.CreateFeature( dst_feat ) != 0:
+        gdaltest.post_reason('CreateFeature failed.')
+        return 'fail'
+    dst_feat.Destroy()
+
+    dst_feat = ogr.Feature( lyr.GetLayerDefn() )
+    dst_feat.SetField('name', 'WAY0000001')
+    dst_feat.SetField('comment', 'Waypoint 1')
+    dst_feat.SetField('icon', 31)
+    dst_feat.SetGeometry(ogr.CreateGeometryFromWkt('POINT (-21.123 -47.231 800)'))
+    if lyr.CreateFeature( dst_feat ) != 0:
+        gdaltest.post_reason('CreateFeature failed.')
+        return 'fail'
+    dst_feat.Destroy()
+
+    lyr = ds.CreateLayer('gtm_tracks', geom_type = ogr.wkbLineString)
+
+    dst_feat = ogr.Feature( lyr.GetLayerDefn() )
+    dst_feat.SetField('name', '1st Street')
+    dst_feat.SetField('type', 2)
+    dst_feat.SetField('color', 0x0000FF)
+    dst_feat.SetGeometry(ogr.CreateGeometryFromWkt('LINESTRING (-21.1 -47.1, -21.2 -47.2, -21.3 -47.3, -21.4 -47.4)'))
+    if lyr.CreateFeature( dst_feat ) != 0:
+        gdaltest.post_reason('CreateFeature failed.')
+        return 'fail'
+    dst_feat.Destroy()
+
+    dst_feat = ogr.Feature( lyr.GetLayerDefn() )
+    dst_feat.SetField('name', '2nd Street')
+    dst_feat.SetField('type', 1)
+    dst_feat.SetField('color', 0x000000)
+    dst_feat.SetGeometry(ogr.CreateGeometryFromWkt('LINESTRING (-21.11 -47.1, -21.21 -47.2, -21.31 -47.3, -21.41 -47.4)'))
+    if lyr.CreateFeature( dst_feat ) != 0:
+        gdaltest.post_reason('CreateFeature failed.')
+        return 'fail'
+    dst_feat.Destroy()
+
+    dst_feat = ogr.Feature( lyr.GetLayerDefn() )
+    dst_feat.SetField('name', '3rd Street')
+    dst_feat.SetField('type', 2)
+    dst_feat.SetField('color', 0x000000)
+    dst_feat.SetGeometry(ogr.CreateGeometryFromWkt('MULTILINESTRING ((-21.12 -47.1, -21.22 -47.2, -21.32 -47.3, -21.42 -47.4),' + \
+                                                   '(-21.12 -47.1, -21.02 -47.0, -20.92 -46.9))'))
+    if lyr.CreateFeature( dst_feat ) != 0:
+        gdaltest.post_reason('CreateFeature failed.')
+        return 'fail'
+    dst_feat.Destroy()
+
+    ds.Destroy()
 
     return 'success'
 
+###############################################################################
+# Check previous test
+def ogr_gtm_check_write_1():
+    if not gdaltest.have_gtm:
+        return 'skip'
+    
+    ds = ogr.Open('tmp/gtm.gtm')
+    lyr = ds.GetLayerByName('gtm_waypoints')
+    if lyr.GetFeatureCount() != 2:
+        gdaltest.post_reason('Bad feature count.')
+        return 'fail'
+
+    # Test 1st waypoint
+    feat = lyr.GetNextFeature()
+
+    if feat.GetField('name') != 'WAY0000000':
+        gdaltest.post_reason( 'Wrong name field value' )
+        return 'fail'
+
+    if feat.GetField('comment') != 'Waypoint 0':
+        gdaltest.post_reason( 'Wrong comment field value' )
+        return 'fail'
+
+    if feat.GetField('icon') != 10:
+        gdaltest.post_reason( 'Wrong icon field value' )
+        return 'fail'
+
+    if feat.GetField('time') != '2009/12/23 14:25:46':
+        gdaltest.post_reason( 'Wrong time field value' )
+        return 'fail'
+
+    wkt = 'POINT (-21 -47)'
+    if ogrtest.check_feature_geometry( feat, wkt):
+        gdaltest.post_reason( 'Unexpected geometry' )
+        return 'fail'
+    feat.Destroy()
+    
+    # Test 2nd waypoint
+    feat = lyr.GetNextFeature()
+
+    if feat.GetField('name') != 'WAY0000001':
+        gdaltest.post_reason( 'Wrong name field value' )
+        return 'fail'
+
+    if feat.GetField('comment') != 'Waypoint 1':
+        gdaltest.post_reason( 'Wrong comment field value' )
+        return 'fail'
+
+    if feat.GetField('icon') != 31:
+        gdaltest.post_reason( 'Wrong icon field value' )
+        return 'fail'
+
+    if feat.GetField('time') != None:
+        gdaltest.post_reason( 'Wrong time field value' )
+        return 'fail'
+
+    wkt = 'POINT (-21.123 -47.231 800)'
+    if ogrtest.check_feature_geometry( feat, wkt):
+        gdaltest.post_reason( 'Unexpected geometry' )
+        return 'fail'
+    feat.Destroy()
+
+    # Test tracks
+    lyr = ds.GetLayerByName('gtm_tracks')
+    if lyr.GetFeatureCount() != 4: # The driver converts multiline
+                                   # into one line for each tuple
+                                   # element
+        gdaltest.post_reason('Bad feature count.')
+        return 'fail'
+
+    # Test 1st track
+    feat = lyr.GetNextFeature()
+
+    if feat.GetField('name') != '1st Street':
+        gdaltest.post_reason( 'Wrong name field value' )
+        return 'fail'
+
+    if feat.GetField('type') != 2:
+        gdaltest.post_reason( 'Wrong type field value' )
+        return 'fail'
+
+    if feat.GetField('color') != 0x0000FF:
+        gdaltest.post_reason( 'Wrong color field value' )
+        return 'fail'
+
+    wkt = 'LINESTRING (-21.1 -47.1, -21.2 -47.2, -21.3 -47.3, -21.4 -47.4)'
+    if ogrtest.check_feature_geometry( feat, wkt):
+        gdaltest.post_reason( 'Unexpected geometry' )
+        return 'fail'
+    feat.Destroy()
+
+    # Test 2nd track
+    feat = lyr.GetNextFeature()
+
+    if feat.GetField('name') != '2nd Street':
+        gdaltest.post_reason( 'Wrong name field value' )
+        return 'fail'
+
+    if feat.GetField('type') != 1:
+        gdaltest.post_reason( 'Wrong type field value' )
+        return 'fail'
+
+    if feat.GetField('color') != 0x000000:
+        gdaltest.post_reason( 'Wrong color field value' )
+        return 'fail'
+
+    wkt = 'LINESTRING (-21.11 -47.1, -21.21 -47.2, -21.31 -47.3, -21.41 -47.4)'
+    if ogrtest.check_feature_geometry( feat, wkt):
+        gdaltest.post_reason( 'Unexpected geometry' )
+        return 'fail'
+    feat.Destroy()
+
+    # Test 3rd track
+    feat = lyr.GetNextFeature()
+
+    if feat.GetField('name') != '3rd Street':
+        gdaltest.post_reason( 'Wrong name field value' )
+        return 'fail'
+
+    if feat.GetField('type') != 2:
+        gdaltest.post_reason( 'Wrong type field value' )
+        return 'fail'
+
+    if feat.GetField('color') != 0x000000:
+        gdaltest.post_reason( 'Wrong color field value' )
+        return 'fail'
+
+    wkt = 'LINESTRING (-21.12 -47.1, -21.22 -47.2, -21.32 -47.3, -21.42 -47.4)'
+    if ogrtest.check_feature_geometry( feat, wkt):
+        gdaltest.post_reason( 'Unexpected geometry' )
+        return 'fail'
+    feat.Destroy()
+
+    # Test 4th track
+    feat = lyr.GetNextFeature()
+
+    if feat.GetField('name') != '3rd Street':
+        gdaltest.post_reason( 'Wrong name field value' )
+        return 'fail'
+
+    if feat.GetField('type') != 2:
+        gdaltest.post_reason( 'Wrong type field value' )
+        return 'fail'
+
+    if feat.GetField('color') != 0x000000:
+        gdaltest.post_reason( 'Wrong color field value' )
+        return 'fail'
+
+    wkt = 'LINESTRING (-21.12 -47.1, -21.02 -47.0, -20.92 -46.9)'
+    if ogrtest.check_feature_geometry( feat, wkt):
+        gdaltest.post_reason( 'Unexpected geometry' )
+        return 'fail'
+    feat.Destroy()
+
+    return 'success'
 
     
 ###############################################################################
@@ -268,12 +488,15 @@ def ogr_gtm_cleanup():
     if gdaltest.gtm_ds is not None:
         gdaltest.gtm_ds.Destroy()
     gdaltest.gtm_ds = None
+    os.remove('tmp/gtm.gtm')
     return 'success'
 
 gdaltest_list = [ 
     ogr_gtm_init,
-    ogr_gtm_1,
-    ogr_gtm_2,
+    ogr_gtm_read_1,
+    ogr_gtm_read_2,
+    ogr_gtm_write_1,
+    ogr_gtm_check_write_1,
     ogr_gtm_cleanup ]
 
 if __name__ == '__main__':
