d4eb7e1a2f5279694af55998685c93072235ff0b
Frank Warmerdam <warmerdam@pobox.com>
2010-01-13 18:05:45 +0000

    minor touch up in copyright message.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@18542 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/dxf/intronurbs.cpp
+++ gdal/ogr/ogrsf_frmts/dxf/intronurbs.cpp
@@ -25,7 +25,7 @@ modification, are permitted provided that the following conditions are met:
     * Redistributions in binary form must reproduce the above copyright notice,
       this list of conditions and the following disclaimer in the documentation
       and/or other materials provided with the distribution.
-    * Neither the name of the David F. Rogers nor the names of its contributors
+    * Neither the name of David F. Rogers nor the names of its contributors
       may be used to endorse or promote products derived from this software 
       without specific prior written permission.
 
@@ -39,7 +39,7 @@ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
 INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
 CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
 ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
-POSSIBILITY OF SUCH DAMAGE. 
+POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include <stdio.h>
