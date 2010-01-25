6dfa50e384f41828da94d9ab19333a78d8c2534f
Even Rouault <even.rouault@mines-paris.org>
2010-01-25 20:28:19 +0000

    Avoid crashing on corrupted .DBF files (#3351, #3093)
    git-svn-id: https://svn.osgeo.org/gdal/branches/1.6@18663 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/shape/dbfopen.c
+++ gdal/ogr/ogrsf_frmts/shape/dbfopen.c
@@ -1,5 +1,5 @@
 /******************************************************************************
- * $Id: dbfopen.c,v 1.83 2008/11/12 14:28:15 fwarmerdam Exp $
+ * $Id: dbfopen.c,v 1.84 2009-10-29 19:59:48 fwarmerdam Exp $
  *
  * Project:  Shapelib
  * Purpose:  Implementation of .dbf access API documented in dbf_api.html.
@@ -34,6 +34,9 @@
  ******************************************************************************
  *
  * $Log: dbfopen.c,v $
+ * Revision 1.84  2009-10-29 19:59:48  fwarmerdam
+ * avoid crash on truncated header (gdal #3093)
+ *
  * Revision 1.83  2008/11/12 14:28:15  fwarmerdam
  * DBFCreateField() now works on files with records
  *
@@ -145,7 +148,7 @@
 #include <ctype.h>
 #include <string.h>
 
-SHP_CVSID("$Id: dbfopen.c,v 1.83 2008/11/12 14:28:15 fwarmerdam Exp $")
+SHP_CVSID("$Id: dbfopen.c,v 1.84 2009-10-29 19:59:48 fwarmerdam Exp $")
 
 #ifndef FALSE
 #  define FALSE		0
@@ -466,6 +469,15 @@ DBFOpenLL( const char * pszFilename, const char * pszAccess, SAHooks *psHooks )
     psDBF->nHeaderLength = nHeadLen = pabyBuf[8] + pabyBuf[9]*256;
     psDBF->nRecordLength = pabyBuf[10] + pabyBuf[11]*256;
     psDBF->iLanguageDriver = pabyBuf[29];
+
+    if (nHeadLen < 32)
+    {
+        psDBF->sHooks.FClose( psDBF->fp );
+        if( pfCPG ) psDBF->sHooks.FClose( pfCPG );
+        free( pabyBuf );
+        free( psDBF );
+        return NULL;
+    }
 
     psDBF->nFields = nFields = (nHeadLen - 32) / 32;
 
