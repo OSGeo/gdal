ec579fe81c9aff7d1fb7ceb8c13553ecc4f08c1d
Even Rouault <even.rouault@mines-paris.org>
2010-02-03 22:03:08 +0000

    Add testing of nearblack utility
    git-svn-id: https://svn.osgeo.org/gdal/trunk@18713 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ autotest/utilities/test_nearblack.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python
+###############################################################################
+# $Id$
+#
+# Project:  GDAL/OGR Test Suite
+# Purpose:  nearblack testing
+# Author:   Even Rouault <even dot rouault @ mines-paris dot org>
+# 
+###############################################################################
+# Copyright (c) 2010, Even Rouault <even dot rouault @ mines-paris dot org>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+###############################################################################
+
+import sys
+import os
+import shutil
+
+sys.path.append( '../pymod' )
+
+import gdal
+import gdaltest
+import test_cli_utilities
+
+###############################################################################
+# Basic test
+
+def test_nearblack_1():
+    if test_cli_utilities.get_nearblack_path() is None:
+        return 'skip'
+
+    gdaltest.runexternal(test_cli_utilities.get_nearblack_path() + ' ../gdrivers/data/rgbsmall.tif -nb 0 -of GTiff -o tmp/nearblack1.tif')
+
+    src_ds = gdal.Open('../gdrivers/data/rgbsmall.tif')
+    ds = gdal.Open('tmp/nearblack1.tif')
+    if ds is None:
+        return 'fail'
+
+    if ds.GetRasterBand(1).Checksum() != 21106:
+        print(ds.GetRasterBand(1).Checksum())
+        gdaltest.post_reason('Bad checksum band 1')
+        return 'fail'
+
+    if ds.GetRasterBand(2).Checksum() != 20736:
+        print(ds.GetRasterBand(2).Checksum())
+        gdaltest.post_reason('Bad checksum band 2')
+        return 'fail'
+
+    if ds.GetRasterBand(3).Checksum() != 21309:
+        print(ds.GetRasterBand(3).Checksum())
+        gdaltest.post_reason('Bad checksum band 3')
+        return 'fail'
+
+    src_gt = src_ds.GetGeoTransform()
+    dst_gt = ds.GetGeoTransform()
+    for i in range(6):
+        if abs(src_gt[i] - dst_gt[i]) > 1e-10:
+            gdaltest.post_reason('Bad geotransform')
+            return 'fail'
+
+    dst_wkt = ds.GetProjectionRef()
+    if dst_wkt.find('AUTHORITY["EPSG","4326"]') == -1:
+        gdaltest.post_reason('Bad projection')
+        return 'fail'
+
+    src_ds = None
+    ds = None
+
+    return 'success'
+
+###############################################################################
+# Add alpha band
+
+def test_nearblack_2():
+    if test_cli_utilities.get_nearblack_path() is None:
+        return 'skip'
+
+    gdaltest.runexternal(test_cli_utilities.get_nearblack_path() + ' ../gdrivers/data/rgbsmall.tif -setalpha -nb 0 -of GTiff -o tmp/nearblack2.tif -co TILED=YES')
+
+    ds = gdal.Open('tmp/nearblack2.tif')
+    if ds is None:
+        return 'fail'
+
+    if ds.GetRasterBand(4).Checksum() != 22002:
+        print(ds.GetRasterBand(4).Checksum())
+        gdaltest.post_reason('Bad checksum band 0')
+        return 'fail'
+
+    ds = None
+
+    return 'success'
+
+###############################################################################
+# Set existing alpha band
+
+def test_nearblack_3():
+    if test_cli_utilities.get_nearblack_path() is None:
+        return 'skip'
+
+    shutil.copy('tmp/nearblack2.tif','tmp/nearblack3.tif')
+    gdaltest.runexternal(test_cli_utilities.get_nearblack_path() + ' -setalpha -nb 0 -of GTiff tmp/nearblack3.tif')
+
+    ds = gdal.Open('tmp/nearblack3.tif')
+    if ds is None:
+        return 'fail'
+
+    if ds.GetRasterBand(4).Checksum() != 22002:
+        print(ds.GetRasterBand(4).Checksum())
+        gdaltest.post_reason('Bad checksum band 0')
+        return 'fail'
+
+    ds = None
+
+    return 'success'
+
+###############################################################################
+# Test -white
+
+def test_nearblack_4():
+    if test_cli_utilities.get_nearblack_path() is None:
+        return 'skip'
+    if test_cli_utilities.get_gdalwarp_path() is None:
+        return 'skip'
+
+    gdaltest.runexternal(test_cli_utilities.get_gdalwarp_path() + ' -wo "INIT_DEST=255" ../gdrivers/data/rgbsmall.tif  tmp/nearblack4_src.tif -srcnodata 0')
+    gdaltest.runexternal(test_cli_utilities.get_nearblack_path() + ' -q -setalpha -white -nb 0 -of GTiff tmp/nearblack4_src.tif -o tmp/nearblack4.tif')
+
+    ds = gdal.Open('tmp/nearblack4.tif')
+    if ds is None:
+        return 'fail'
+
+    if ds.GetRasterBand(4).Checksum() != 24643:
+        print(ds.GetRasterBand(4).Checksum())
+        gdaltest.post_reason('Bad checksum band 0')
+        return 'fail'
+
+    ds = None
+
+    return 'success'
+
+###############################################################################
+# Cleanup
+
+def test_nearblack_cleanup():
+    try:
+        os.remove('tmp/nearblack1.tif')
+    except:
+        pass
+    try:
+        os.remove('tmp/nearblack2.tif')
+    except:
+        pass
+    try:
+        os.remove('tmp/nearblack3.tif')
+    except:
+        pass
+    try:
+        os.remove('tmp/nearblack4_src.tif')
+    except:
+        pass
+    try:
+        os.remove('tmp/nearblack4.tif')
+    except:
+        pass
+    return 'success'
+
+gdaltest_list = [
+    test_nearblack_1,
+    test_nearblack_2,
+    test_nearblack_3,
+    test_nearblack_4,
+    test_nearblack_cleanup
+    ]
+
+
+if __name__ == '__main__':
+
+    gdaltest.setup_run( 'test_nearblack' )
+
+    gdaltest.run_tests( gdaltest_list )
+
+    gdaltest.summarize()
