66fcddb12392bbc3fcbafc19dbdb756043b8374a
Even Rouault <even.rouault@mines-paris.org>
2010-02-13 18:54:22 +0000

    Avoid error on Tamas slavebots
    git-svn-id: https://svn.osgeo.org/gdal/trunk@18828 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/osr/osr_compd.py
+++ autotest/osr/osr_compd.py
@@ -34,7 +34,10 @@ import sys
 sys.path.append( '../pymod' )
 
 import gdaltest
-from osgeo import osr
+try:
+    from osgeo import osr
+except:
+    import osr
 
 example_compd_wkt = 'COMPD_CS["OSGB36 / British National Grid + ODN",PROJCS["OSGB 1936 / British National Grid",GEOGCS["OSGB 1936",DATUM["OSGB_1936",SPHEROID["Airy 1830",6377563.396,299.3249646,AUTHORITY["EPSG",7001]],TOWGS84[375,-111,431,0,0,0,0],AUTHORITY["EPSG",6277]],PRIMEM["Greenwich",0,AUTHORITY["EPSG",8901]],UNIT["DMSH",0.0174532925199433,AUTHORITY["EPSG",9108]],AXIS["Lat",NORTH],AXIS["Long",EAST],AUTHORITY["EPSG",4277]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",49],PARAMETER["central_meridian",-2],PARAMETER["scale_factor",0.999601272],PARAMETER["false_easting",400000],PARAMETER["false_northing",-100000],UNIT["metre_1",1,AUTHORITY["EPSG",9001]],AXIS["E",EAST],AXIS["N",NORTH],AUTHORITY["EPSG",27700]],VERT_CS["Newlyn",VERT_DATUM["Ordnance Datum Newlyn",2005,AUTHORITY["EPSG",5101]],UNIT["metre_2",1,AUTHORITY["EPSG",9001]],AXIS["Up",UP],AUTHORITY["EPSG",5701]],AUTHORITY["EPSG",7405]]'
 
