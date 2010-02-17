76765c52326cab194c43abdf732de8924f524229
Chaitanya Kumar <chaitanya.ch@gmail.com>
2010-02-17 12:29:27 +0000

    Use transactions in CopyLayer for better speed. (https://trac.osgeo.org/gdal/ticket/3335)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@18845 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/ogrsf_frmts/generic/ogrdatasource.cpp
+++ ogr/ogrsf_frmts/generic/ogrdatasource.cpp
@@ -277,14 +277,24 @@ OGRLayer *OGRDataSource::CopyLayer( OGRLayer *poSrcLayer,
         poDstLayer->CreateField( poSrcDefn->GetFieldDefn(iField) );
 
 /* -------------------------------------------------------------------- */
+/*      Check if the destination layer supports transactions and set a  */
+/*      default number of features in a single transaction.             */
+/* -------------------------------------------------------------------- */
+    int nGroupTransactions = 0;
+    if( poDstLayer->TestCapability( OLCTransactions ) )
+        nGroupTransactions = 128;
+
+/* -------------------------------------------------------------------- */
 /*      Transfer features.                                              */
 /* -------------------------------------------------------------------- */
     OGRFeature  *poFeature;
-    
+
     poSrcLayer->ResetReading();
 
-    while( TRUE )
+    if( nGroupTransactions <= 0 )
     {
+      while( TRUE )
+      {
         OGRFeature      *poDstFeature = NULL;
 
         poFeature = poSrcLayer->GetNextFeature();
@@ -316,8 +326,76 @@ OGRLayer *OGRDataSource::CopyLayer( OGRLayer *poSrcLayer,
         }
 
         OGRFeature::DestroyFeature( poDstFeature );
+      }
     }
+    else
+    {
+      int i, bStopTransfer = FALSE, bStopTransaction = FALSE;
+      int nFeatCount = 0; // Number of features in the temporary array
+      int nFeaturesToAdd = 0;
+      while( !bStopTransfer )
+      {
+        OGRFeature **papoDstFeature =
+            (OGRFeature **)CPLCalloc(sizeof(OGRFeature *), nGroupTransactions);
 
+/* -------------------------------------------------------------------- */
+/*      Fill the array with features                                    */
+/* -------------------------------------------------------------------- */
+        for( nFeatCount = 0; nFeatCount < nGroupTransactions; nFeatCount++ )
+        {
+            poFeature = poSrcLayer->GetNextFeature();
+
+            if( poFeature == NULL )
+            {
+                bStopTransfer = 1;
+                break;
+            }
+
+            CPLErrorReset();
+            papoDstFeature[nFeatCount] =
+                        OGRFeature::CreateFeature( poDstLayer->GetLayerDefn() );
+
+            if( papoDstFeature[nFeatCount]->SetFrom( poFeature, TRUE ) != OGRERR_NONE )
+            {
+                OGRFeature::DestroyFeature( poFeature );
+                CPLError( CE_Failure, CPLE_AppDefined,
+                          "Unable to translate feature %ld from layer %s.\n",
+                          poFeature->GetFID(), poSrcDefn->GetName() );
+                bStopTransfer = TRUE;
+                break;
+            }
+
+            papoDstFeature[nFeatCount]->SetFID( poFeature->GetFID() );
+
+            OGRFeature::DestroyFeature( poFeature );
+        }
+        nFeaturesToAdd = nFeatCount;
+
+        CPLErrorReset();
+        bStopTransaction = FALSE;
+        while( !bStopTransaction )
+        {
+            bStopTransaction = TRUE;
+            poDstLayer->StartTransaction();
+            for( i = 0; i < nFeaturesToAdd; i++ )
+            {
+                if( poDstLayer->CreateFeature( papoDstFeature[i] ) != OGRERR_NONE )
+                {
+                    nFeaturesToAdd = i;
+                    bStopTransfer = TRUE;
+                    bStopTransaction = FALSE;
+                }
+            }
+            if( bStopTransaction )
+                poDstLayer->CommitTransaction();
+            else
+                poDstLayer->RollbackTransaction();
+        }
+
+        for( i = 0; i < nFeatCount; i++ )
+            OGRFeature::DestroyFeature( papoDstFeature[i] );
+      }
+    }
     return poDstLayer;
 }
 
