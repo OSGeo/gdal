425de2da9361a3ff4206abb52f84cee41f8dd78d
Chaitanya Kumar <chaitanya.ch@gmail.com>
2010-02-21 09:10:08 +0000

    Burn the attribute value in ALL the bands during rasterization. (#3396)
    git-svn-id: https://svn.osgeo.org/gdal/branches/1.7@18880 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/alg/rasterize.py
+++ autotest/alg/rasterize.py
@@ -229,10 +229,76 @@ def rasterize_3():
     
     return 'success'
 
+###############################################################################
+# Rasterization with ATTRIBUTE.
+
+def rasterize_4():
+
+    try:
+        x = gdal.RasterizeLayer
+        gdaltest.have_ng = 1
+    except:
+        gdaltest.have_ng = 0
+        return 'skip'
+
+    # Setup working spatial reference
+    sr_wkt = 'LOCAL_CS["arbitrary"]'
+    sr = osr.SpatialReference( sr_wkt )
+    
+    # Create a memory raster to rasterize into.
+    target_ds = gdal.GetDriverByName('MEM').Create( '', 100, 100, 3,
+                                                    gdal.GDT_Byte )
+    target_ds.SetGeoTransform( (1000,1,0,1100,0,-1) )
+    target_ds.SetProjection( sr_wkt )
+    
+    # Create a memory layer to rasterize from.
+    rast_ogr_ds = ogr.GetDriverByName('Memory').CreateDataSource( 'wrk' )
+    rast_mem_lyr = rast_ogr_ds.CreateLayer( 'poly', srs=sr )
+    # Setup Schema
+    ogrtest.quick_create_layer_def( rast_mem_lyr,
+                                    [ ('CELSIUS', ogr.OFTReal) ] )
+    
+    # Add polygons and linestrings and a field named CELSIUS.
+    wkt_geom = ['POLYGON((1020 1030 40,1020 1045 30,1050 1045 20,1050 1030 35,1020 1030 40))',
+                'POLYGON((1010 1046 85,1015 1055 35,1055 1060 26,1054 1048 35,1010 1046 85))',
+                'POLYGON((1020 1076 190,1025 1085 35,1065 1090 26,1064 1078 35,1020 1076 190),(1023 1079 5,1061 1081 35,1062 1087 26,1028 1082 35,1023 1079 85))',
+                'LINESTRING(1005 1000 10, 1100 1050 120)',
+                'LINESTRING(1000 1000 150, 1095 1050 -5, 1080 1080 200)']
+    celsius_field_values = [50,255,60,100,180]
+
+    i = 0
+    for g in wkt_geom:
+        feat = ogr.Feature( rast_mem_lyr.GetLayerDefn() )
+        feat.SetGeometryDirectly( ogr.Geometry(wkt = g) )
+        feat.SetField( 'CELSIUS', celsius_field_values[i] )
+        rast_mem_lyr.CreateFeature( feat )
+        i = i + 1
+
+    # Run the algorithm.
+    err = gdal.RasterizeLayer( target_ds, [1,2,3], rast_mem_lyr,
+                               options = ["ATTRIBUTE=CELSIUS"] )
+
+    if err != 0:
+        print(err)
+        gdaltest.post_reason( 'got non-zero result code from RasterizeLayer' )
+        return 'fail'
+
+    # Check results.
+    expected = 16265
+    checksum = target_ds.GetRasterBand(2).Checksum()
+    if checksum != expected:
+        print(checksum)
+        gdaltest.post_reason( 'Did not get expected image checksum' )
+        gdal.GetDriverByName('GTiff').CreateCopy('tmp/rasterize_4.tif',target_ds)
+        return 'fail'
+    
+    return 'success'
+
 gdaltest_list = [
     rasterize_1,
     rasterize_2,
     rasterize_3,
+    rasterize_4
     ]
 
 if __name__ == '__main__':
