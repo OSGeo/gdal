a3b60ce4d7c796ee25664c673366d873b343390d
Chaitanya Kumar <chaitanya.ch@gmail.com>
2010-03-15 13:20:38 +0000

    Test for null geometry from GEOS to avoid crash. (#3467)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@19088 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/ogrpolygon.cpp
+++ ogr/ogrpolygon.cpp
@@ -844,10 +844,14 @@ int OGRPolygon::Centroid( OGRPoint *poPoint ) const
     if( hThisGeosGeom != NULL )
     {
     	hOtherGeosGeom = GEOSGetCentroid( hThisGeosGeom );
+        GEOSGeom_destroy( hThisGeosGeom );
+
+        if( hOtherGeosGeom == NULL )
+            return OGRERR_FAILURE;
+
         OGRPoint *poCentroid = (OGRPoint *) 
             OGRGeometryFactory::createFromGEOS( hOtherGeosGeom );
 
-        GEOSGeom_destroy( hThisGeosGeom );
         GEOSGeom_destroy( hOtherGeosGeom );
 
         if( poPoint == NULL 
@@ -921,10 +925,14 @@ int OGRPolygon::PointOnSurface( OGRPoint *poPoint ) const
     if( hThisGeosGeom != NULL )
     {
      	hOtherGeosGeom = GEOSPointOnSurface( hThisGeosGeom );
+        GEOSGeom_destroy( hThisGeosGeom );
+
+        if( hOtherGeosGeom == NULL )
+            return OGRERR_FAILURE;
+
         OGRPoint *poInsidePoint = (OGRPoint *) 
             OGRGeometryFactory::createFromGEOS( hOtherGeosGeom );
  
-        GEOSGeom_destroy( hThisGeosGeom );
         GEOSGeom_destroy( hOtherGeosGeom );
  
         if( poPoint == NULL 
