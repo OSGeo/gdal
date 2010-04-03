ce2f83d45b056f3987e0802846650fa8abecfc20
Even Rouault <even.rouault@mines-paris.org>
2010-04-03 21:16:32 +0000

    Add missing free in error path
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@19299 f0d54148-0727-0410-94bb-9a71ac55c965

--- apps/gdalinfo.c
+++ apps/gdalinfo.c
@@ -169,6 +169,7 @@ int main( int argc, char ** argv )
                  pszFilename );
 
         CSLDestroy( argv );
+        CSLDestroy( papszExtraMDDomains );
     
         GDALDumpOpenDatasets( stderr );
 
