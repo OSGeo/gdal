efe71f6297e6d86b2fc98945e0fa73eb89b04ad6
Harsh Govind <harsh.govind@spadac.com>
2010-04-07 15:39:28 +0000

    Added new plug-in GDAL Super-Overlay Driver. The driver allows converts raster (like TIF/GeoTIFF, JPEG2000, JPEG, PNG) into a directory structure of small tiles and KML files which can be displayed in Google Earth. By zooming in/out the image in the Google Earth, tiles of appropriate resolution are loaded as portions of the imagery area come into view, with higher resolution tiles loaded as the viewpoint nears.
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@19335 f0d54148-0727-0410-94bb-9a71ac55c965

--- gcore/gdal_frmts.h
+++ gcore/gdal_frmts.h
@@ -147,6 +147,7 @@ void CPL_DLL GDALRegister_WKTRaster(void);
 void CPL_DLL GDALRegister_NWT_GRD(void);
 void CPL_DLL GDALRegister_NWT_GRC(void);
 void CPL_DLL GDALRegister_SAGA(void);
+void CPL_DLL GDALRegister_KMLSUPEROVERLAY(void);
 CPL_C_END
 
 #endif /* ndef GDAL_FRMTS_H_INCLUDED */
