d1e9197b56477359c635f01efe73c01fe5568366
Phil Vachon <philippe@cowpig.ca>
2010-04-22 01:56:18 +0000

    Disable new GDALCopyWords implementation if using C++ Builder, since its handling of templates seems to not work correctly
    git-svn-id: https://svn.osgeo.org/gdal/trunk@19493 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/gcore/rasterio.cpp
+++ gdal/gcore/rasterio.cpp
@@ -31,7 +31,7 @@
 #include "gdal_priv.h"
 
 
-#if !(defined(_MSC_VER) && _MSC_VER <= 1200)
+#if !(defined(_MSC_VER) && _MSC_VER <= 1200) && !(defined(__BORLANDC__))
 #include <stdexcept>
 #include <limits>
 
