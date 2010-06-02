bdd42d49877d4073e04d9b5cb53a89c7150f46cb
Even Rouault <even.rouault@mines-paris.org>
2010-06-02 22:09:59 +0000

    Test GPSBabel driver  write support
    git-svn-id: https://svn.osgeo.org/gdal/trunk@19797 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/ogr/ogr_gpsbabel.py
+++ autotest/ogr/ogr_gpsbabel.py
@@ -47,6 +47,7 @@ def ogr_gpsbabel_init():
 
     # Test if the gpsbabel is accessible
     ogrtest.have_gpsbabel = False
+    ogrtest.have_read_gpsbabel = False
     try:
         ret = gdaltest.runexternal('gpsbabel -V')
     except:
@@ -62,7 +63,8 @@ def ogr_gpsbabel_init():
 
     if ds is None:
         print('GPX driver not configured for read support')
-        return 'skip'
+    else:
+        ogrtest.have_read_gpsbabel = True
 
     ogrtest.have_gpsbabel = True
 
@@ -73,7 +75,7 @@ def ogr_gpsbabel_init():
 
 def ogr_gpsbabel_1():
 
-    if not ogrtest.have_gpsbabel:
+    if not ogrtest.have_read_gpsbabel:
         return 'skip'
 
     ds = ogr.Open('GPSBabel:nmea:data/nmea.txt')
@@ -90,7 +92,7 @@ def ogr_gpsbabel_1():
 
 def ogr_gpsbabel_2():
 
-    if not ogrtest.have_gpsbabel:
+    if not ogrtest.have_read_gpsbabel:
         return 'skip'
 
     ds = ogr.Open('data/nmea.txt')
@@ -102,10 +104,56 @@ def ogr_gpsbabel_2():
 
     return 'success'
 
+###############################################################################
+# Test writing
+
+def ogr_gpsbabel_3():
+
+    if not ogrtest.have_gpsbabel:
+        return 'skip'
+
+    ds = ogr.GetDriverByName('GPSBabel').CreateDataSource('GPSBabel:nmea:tmp/nmea.txt')
+    lyr = ds.CreateLayer('track_points', geom_type = ogr.wkbPoint)
+
+    feat = ogr.Feature(lyr.GetLayerDefn())
+    feat.SetField('track_fid', 0)
+    feat.SetField('track_seg_id', 0)
+    feat.SetField('track_name', 'TRACK_NAME')
+    feat.SetField('name', 'PT_NAME')
+    feat.SetField('hdop', 123)
+    feat.SetField('vdop', 456)
+    feat.SetField('pdop', 789)
+    feat.SetField('sat', 6)
+    feat.SetField('time', '2010/06/03 12:34:56')
+    feat.SetField('fix', '3d')
+    geom = ogr.CreateGeometryFromWkt('POINT(2.50 49.25)')
+    feat.SetGeometry(geom)
+    lyr.CreateFeature(feat)
+
+    feat = None
+    lyr = None
+    ds = None
+
+    f = open('tmp/nmea.txt', 'rt')
+    res = f.read()
+    f.close()
+
+    gdal.Unlink('tmp/nmea.txt')
+
+    if res.find('$GPRMC,123456,A,4915.000,N,00230.000,E,0.00,0.00,030610,,*16') == -1 or \
+       res.find('$GPGGA,123456,4915.000,N,00230.000,E,1,06,123.0,0.000,M,0.0,M,,*7B') == -1 or \
+       res.find('$GPGSA,A,3,,,,,,,,,,,,,789.0,123.0,456.0*33') == -1:
+        gdaltest.post_reason('did not get expected result')
+        print(res)
+        return 'fail'
+
+    return 'success'
+
 gdaltest_list = [ 
     ogr_gpsbabel_init,
     ogr_gpsbabel_1,
-    ogr_gpsbabel_2 ]
+    ogr_gpsbabel_2,
+    ogr_gpsbabel_3 ]
 
 
 if __name__ == '__main__':
