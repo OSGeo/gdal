79aec34acbb6bca903a758bcb665c67808e9ed4f
Frank Warmerdam <warmerdam@pobox.com>
2010-07-08 13:59:22 +0000

    merged NAS driver into trunk
    git-svn-id: https://svn.osgeo.org/gdal/trunk@19991 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ gdal/ogr/ogrsf_frmts/nas/ogrnasdriver.cpp
@@ -0,0 +1,98 @@
+/******************************************************************************
+ * $Id: ogrgmldriver.cpp 10645 2007-01-18 02:22:39Z warmerdam $
+ *
+ * Project:  OGR
+ * Purpose:  OGRNASDriver implementation
+ * Author:   Frank Warmerdam, warmerdam@pobox.com
+ *
+ ******************************************************************************
+ * Copyright (c) 2002, Frank Warmerdam <warmerdam@pobox.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+
+#include "ogr_nas.h"
+#include "cpl_conv.h"
+
+CPL_CVSID("$Id: ogrgmldriver.cpp 10645 2007-01-18 02:22:39Z warmerdam $");
+
+/************************************************************************/
+/*                          ~OGRNASDriver()                           */
+/************************************************************************/
+
+OGRNASDriver::~OGRNASDriver()
+
+{
+}
+
+/************************************************************************/
+/*                              GetName()                               */
+/************************************************************************/
+
+const char *OGRNASDriver::GetName()
+
+{
+    return "NAS";
+}
+
+/************************************************************************/
+/*                                Open()                                */
+/************************************************************************/
+
+OGRDataSource *OGRNASDriver::Open( const char * pszFilename,
+                                   int bUpdate )
+
+{
+    OGRNASDataSource    *poDS;
+
+    if( bUpdate )
+        return NULL;
+
+    poDS = new OGRNASDataSource();
+
+    if( !poDS->Open( pszFilename, TRUE )
+        || poDS->GetLayerCount() == 0 )
+    {
+        delete poDS;
+        return NULL;
+    }
+    else
+        return poDS;
+}
+
+/************************************************************************/
+/*                           TestCapability()                           */
+/************************************************************************/
+
+int OGRNASDriver::TestCapability( const char * pszCap )
+
+{
+    return FALSE;
+}
+
+/************************************************************************/
+/*                           RegisterOGRNAS()                           */
+/************************************************************************/
+
+void RegisterOGRNAS()
+
+{
+    OGRSFDriverRegistrar::GetRegistrar()->RegisterDriver( new OGRNASDriver );
+}
+
