d569c92a8d0436da78d915fa2b46d9943fd3852d
Even Rouault <even.rouault@mines-paris.org>
2010-08-06 18:47:57 +0000

    Add testing of OGR PGeo driver
    git-svn-id: https://svn.osgeo.org/gdal/trunk@20195 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ autotest/ogr/ogr_pgeo.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+###############################################################################
+# $Id
+#
+# Project:  GDAL/OGR Test Suite
+# Purpose:  Test read functionality for OGR PGEO driver.
+# Author:   Even Rouault <even dot rouault at mines dash paris dot org>
+# 
+###############################################################################
+# Copyright (c) 2010, Even Rouault <even dot rouault at mines dash paris dot org>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+###############################################################################
+
+import os
+import sys
+import ogr
+import osr
+
+sys.path.append( '../pymod' )
+
+import gdaltest
+import ogrtest
+
+###############################################################################
+# Basic testing
+
+def ogr_pgeo_1():
+
+    try:
+        drv = ogr.GetDriverByName('PGEO')
+    except:
+        drv = None
+   
+    if drv is None:
+        return 'skip'
+
+    if not gdaltest.download_file('http://download.osgeo.org/gdal/data/pgeo/PGeoTest.zip', 'PGeoTest.zip'):
+        return 'skip'
+
+    try:
+        os.stat('tmp/cache/Autodesk Test.mdb')
+    except:
+        try:
+            gdaltest.unzip( 'tmp/cache', 'tmp/cache/PGeoTest.zip')
+            try:
+                os.stat('tmp/cache/Autodesk Test.mdb')
+            except:
+                return 'skip'
+        except:
+            return 'skip'
+            
+    ogrtest.pgeo_ds = ogr.Open('tmp/cache/Autodesk Test.mdb')
+    if ogrtest.pgeo_ds is None:
+        gdaltest.post_reason('could not open DB. Driver probably misconfigured')
+        return 'skip'
+    
+    if ogrtest.pgeo_ds.GetLayerCount() != 3:
+        gdaltest.post_reason('did not get expected layer count')
+        return 'fail'
+        
+    lyr = ogrtest.pgeo_ds.GetLayer(0)
+    feat = lyr.GetNextFeature()
+    if feat.GetField('OBJECTID') != 1 or \
+       feat.GetField('IDNUM') != 9424 or \
+       feat.GetField('OWNER') != 'City':
+        gdaltest.post_reason('did not get expected attributes')
+        feat.DumpReadable()
+        return 'fail'
+        
+    if ogrtest.check_feature_geometry(feat,'LINESTRING (1910941.703951031 445833.57942859828 0,1910947.927691862 445786.43811868131 0)', max_error = 0.0000001) != 0:
+        gdaltest.post_reason('did not get expected geometry')
+        feat.DumpReadable()
+        return 'fail'
+
+    feat_count = lyr.GetFeatureCount()
+    if feat_count != 9418:
+        gdaltest.post_reason('did not get expected feature count')
+        print(feat_count)
+        return 'fail'
+
+    return 'success'
+
+###############################################################################
+# Test spatial filter
+
+def ogr_pgeo_2():
+    if ogrtest.pgeo_ds is None:
+        return 'skip'
+        
+    lyr = ogrtest.pgeo_ds.GetLayer(0)
+    lyr.ResetReading()
+    feat = lyr.GetNextFeature()
+    geom = feat.GetGeometryRef()
+    bbox = geom.GetEnvelope()
+    
+    lyr.SetSpatialFilterRect(bbox[0], bbox[1], bbox[2], bbox[3])
+
+    feat_count = lyr.GetFeatureCount()
+    if feat_count != 6957:
+        gdaltest.post_reason('did not get expected feature count')
+        print(feat_count)
+        return 'fail'
+
+    feat = lyr.GetNextFeature()
+    if feat.GetField('OBJECTID') != 1 or \
+       feat.GetField('IDNUM') != 9424 or \
+       feat.GetField('OWNER') != 'City':
+        gdaltest.post_reason('did not get expected attributes')
+        feat.DumpReadable()
+        return 'fail'
+
+    return 'success'
+
+###############################################################################
+# Test attribute filter
+
+def ogr_pgeo_3():
+    if ogrtest.pgeo_ds is None:
+        return 'skip'
+        
+    lyr = ogrtest.pgeo_ds.GetLayer(0)
+    lyr.SetAttributeFilter('OBJECTID=1')
+
+    feat_count = lyr.GetFeatureCount()
+    if feat_count != 1:
+        gdaltest.post_reason('did not get expected feature count')
+        print(feat_count)
+        return 'fail'
+
+    feat = lyr.GetNextFeature()
+    if feat.GetField('OBJECTID') != 1 or \
+       feat.GetField('IDNUM') != 9424 or \
+       feat.GetField('OWNER') != 'City':
+        gdaltest.post_reason('did not get expected attributes')
+        feat.DumpReadable()
+        return 'fail'
+
+    return 'success'
+
+###############################################################################
+# Test ExecuteSQL()
+
+def ogr_pgeo_4():
+    if ogrtest.pgeo_ds is None:
+        return 'skip'
+        
+    sql_lyr = ogrtest.pgeo_ds.ExecuteSQL('SELECT * FROM SDPipes WHERE OBJECTID = 1')
+
+    feat_count = sql_lyr.GetFeatureCount()
+    if feat_count != 1:
+        gdaltest.post_reason('did not get expected feature count')
+        print(feat_count)
+        return 'fail'
+
+    feat = sql_lyr.GetNextFeature()
+    if feat.GetField('OBJECTID') != 1 or \
+       feat.GetField('IDNUM') != 9424 or \
+       feat.GetField('OWNER') != 'City':
+        gdaltest.post_reason('did not get expected attributes')
+        feat.DumpReadable()
+        return 'fail'
+        
+    ogrtest.pgeo_ds.ReleaseResultSet(sql_lyr)
+
+    return 'success'
+
+###############################################################################
+# Test GetFeature()
+
+def ogr_pgeo_5():
+    if ogrtest.pgeo_ds is None:
+        return 'skip'
+        
+    lyr = ogrtest.pgeo_ds.GetLayer(0)
+    feat = lyr.GetFeature(9418)
+    if feat.GetField('OBJECTID') != 9418:
+        gdaltest.post_reason('did not get expected attributes')
+        feat.DumpReadable()
+        return 'fail'
+
+    return 'success'
+
+###############################################################################
+
+def ogr_pgeo_cleanup():
+    if ogrtest.pgeo_ds is None:
+        return 'skip'
+
+    ogrtest.pgeo_ds = None
+    return 'success'
+    
+gdaltest_list = [
+    ogr_pgeo_1,
+    ogr_pgeo_2,
+    ogr_pgeo_3,
+    ogr_pgeo_4,
+    ogr_pgeo_5,
+    ogr_pgeo_cleanup    ]
+
+if __name__ == '__main__':
+
+    gdaltest.setup_run( 'ogr_pgeo' )
+
+    gdaltest.run_tests( gdaltest_list )
+
+    gdaltest.summarize()
+
