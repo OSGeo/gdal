ba685a97caedbe7929739f378ce616de4af14de6
Even Rouault <even.rouault@mines-paris.org>
2010-08-07 08:21:46 +0000

    GML: ignore <gml:BoundedBy> inside feature elements, as found in CityGML
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@20217 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/ogrsf_frmts/gml/gmlreaderp.h
+++ ogr/ogrsf_frmts/gml/gmlreaderp.h
@@ -51,6 +51,8 @@ class GMLHandler
     int        m_nDepth;
     int        m_nDepthFeature;
 
+    int        m_bInBoundedBy;
+    int        m_inBoundedByDepth;
 protected:
     GMLReader  *m_poReader;
 
