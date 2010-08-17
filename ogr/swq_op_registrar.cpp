186aa421d868febbf7133edba3d18f12f64359d5
Frank Warmerdam <warmerdam@pobox.com>
2010-08-17 03:24:52 +0000

    implement RFC 28 style OGR SQL Expressions
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@20329 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ ogr/swq_op_registrar.cpp
@@ -0,0 +1,188 @@
+/******************************************************************************
+ *
+ * Component: OGR SQL Engine
+ * Purpose: Implementation of the swq_op_registrar class used to 
+ *          represent operations possible in an SQL expression.
+ * Author: Frank Warmerdam <warmerdam@pobox.com>
+ * 
+ ******************************************************************************
+ * Copyright (C) 2010 Frank Warmerdam <warmerdam@pobox.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+
+#include "cpl_conv.h"
+#include "cpl_multiproc.h"
+#include "swq.h"
+#include <vector>
+
+static void *hOperationsMutex = NULL;
+static int   bOperationsInitialized = FALSE;
+static std::vector<swq_operation*> apoOperations;
+
+/************************************************************************/
+/*                            GetOperator()                             */
+/************************************************************************/
+
+const swq_operation *swq_op_registrar::GetOperator( const char *pszName )
+
+{
+    unsigned int i;
+
+    if( !bOperationsInitialized )
+        Initialize();
+
+    for( i = 0; i < apoOperations.size(); i++ )
+    {
+        if( EQUAL(pszName,apoOperations[i]->osName.c_str()) )
+            return apoOperations[i];
+    }
+
+    return NULL;
+}
+
+/************************************************************************/
+/*                            GetOperator()                             */
+/************************************************************************/
+
+const swq_operation *swq_op_registrar::GetOperator( swq_op eOperator )
+
+{
+    unsigned int i;
+
+    if( !bOperationsInitialized )
+        Initialize();
+
+    for( i = 0; i < apoOperations.size(); i++ )
+    {
+        if( eOperator == apoOperations[i]->eOperation )
+            return apoOperations[i];
+    }
+
+    return NULL;
+}
+
+
+/************************************************************************/
+/*                            AddOperator()                             */
+/************************************************************************/
+
+void swq_op_registrar::AddOperator( const char *pszName, swq_op eOpCode,
+                                    swq_op_evaluator pfnEvaluator,
+                                    swq_op_checker pfnChecker )
+
+{
+    if( GetOperator( pszName ) != NULL )
+        return;
+
+    if( pfnEvaluator == NULL )
+        pfnEvaluator = SWQGeneralEvaluator;
+    if( pfnChecker == NULL )
+        pfnChecker = SWQGeneralChecker;
+
+    swq_operation *poOp = new swq_operation();
+
+    poOp->eOperation = eOpCode;
+    poOp->osName = pszName;
+    poOp->pfnEvaluator = pfnEvaluator;
+    poOp->pfnChecker = pfnChecker;
+
+    apoOperations.push_back( poOp );
+}
+
+
+
+/************************************************************************/
+/*                        SWQColumnFuncChecker()                        */
+/*                                                                      */
+/*      Column summary functions are not legal in any context except    */
+/*      as a root operator on column definitions.  They are removed     */
+/*      from this tree before checking so we just need to issue an      */
+/*      error if they are used in any other context.                    */
+/************************************************************************/
+
+static swq_field_type SWQColumnFuncChecker( swq_expr_node *poNode )
+{
+    CPLError( CE_Failure, CPLE_AppDefined,
+              "Column Summary Function '%s' found in an inappropriate context.",
+              poNode->string_value );
+    return SWQ_ERROR;
+}
+
+
+/************************************************************************/
+/*                             Initialize()                             */
+/************************************************************************/
+
+void swq_op_registrar::Initialize()
+
+{
+    CPLMutexHolderD( &hOperationsMutex );
+
+    if( bOperationsInitialized )
+        return;
+
+    bOperationsInitialized = TRUE;
+
+    AddOperator( "OR", SWQ_OR );
+    AddOperator( "AND", SWQ_AND );
+    AddOperator( "NOT", SWQ_NOT );
+    AddOperator( "=", SWQ_EQ );
+    AddOperator( "<>", SWQ_NE );
+    AddOperator( ">=", SWQ_GE );
+    AddOperator( "<=", SWQ_LE );
+    AddOperator( "<", SWQ_LT );
+    AddOperator( ">", SWQ_GT );
+    AddOperator( "LIKE", SWQ_LIKE );
+    AddOperator( "IS NULL", SWQ_ISNULL );
+    AddOperator( "IN", SWQ_IN );
+    AddOperator( "+", SWQ_ADD );
+    AddOperator( "-", SWQ_SUBTRACT );
+    AddOperator( "*", SWQ_MULTIPLY );
+    AddOperator( "/", SWQ_DIVIDE );
+    AddOperator( "%", SWQ_MODULUS );
+    AddOperator( "CONCAT", SWQ_CONCAT );
+    AddOperator( "SUBSTR", SWQ_SUBSTR );
+
+    AddOperator( "AVG", SWQ_AVG, NULL, SWQColumnFuncChecker );
+    AddOperator( "MIN", SWQ_MIN, NULL, SWQColumnFuncChecker );
+    AddOperator( "MAX", SWQ_MAX, NULL, SWQColumnFuncChecker );
+    AddOperator( "COUNT", SWQ_COUNT, NULL, SWQColumnFuncChecker );
+    AddOperator( "SUM", SWQ_SUM, NULL, SWQColumnFuncChecker );
+
+    AddOperator( "CAST", SWQ_CAST, SWQCastEvaluator, SWQCastChecker );
+}
+
+/************************************************************************/
+/*                            DeInitialize()                            */
+/************************************************************************/
+
+void swq_op_registrar::DeInitialize()
+
+{
+    CPLMutexHolderD( &hOperationsMutex );
+
+    bOperationsInitialized = FALSE;
+
+    for( unsigned int i=0; i < apoOperations.size(); i++ )
+        delete apoOperations[i];
+
+    apoOperations.clear();
+}
+
---
+++ ogr/swq_parser.hpp
@@ -0,0 +1,82 @@
+
+/* A Bison parser, made by GNU Bison 2.4.1.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     SWQT_NUMBER = 258,
+     SWQT_STRING = 259,
+     SWQT_IDENTIFIER = 260,
+     SWQT_IN = 261,
+     SWQT_LIKE = 262,
+     SWQT_NULL = 263,
+     SWQT_IS = 264,
+     SWQT_SELECT = 265,
+     SWQT_LEFT = 266,
+     SWQT_JOIN = 267,
+     SWQT_WHERE = 268,
+     SWQT_ON = 269,
+     SWQT_ORDER = 270,
+     SWQT_BY = 271,
+     SWQT_FROM = 272,
+     SWQT_AS = 273,
+     SWQT_ASC = 274,
+     SWQT_DESC = 275,
+     SWQT_DISTINCT = 276,
+     SWQT_CAST = 277,
+     SWQT_LOGICAL_START = 278,
+     SWQT_VALUE_START = 279,
+     SWQT_SELECT_START = 280,
+     SWQT_NOT = 281,
+     SWQT_AND = 282,
+     SWQT_OR = 283
+   };
+#endif
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+
+
