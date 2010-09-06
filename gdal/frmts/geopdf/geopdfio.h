e77461f06b07d654992739feab6bc24bd01390b3
Even Rouault <even.rouault@mines-paris.org>
2010-09-06 22:02:18 +0000

    GeoPDF: use VSIF*L API
    git-svn-id: https://svn.osgeo.org/gdal/trunk@20533 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ gdal/frmts/geopdf/geopdfio.h
@@ -0,0 +1,93 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  GeoPDF driver
+ * Purpose:  GDALDataset driver for GeoPDF dataset.
+ * Author:   Even Rouault, <even dot rouault at mines dash paris dot org>
+ *
+ ******************************************************************************
+ * Copyright (c) 2010, Even Rouault
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+
+#ifndef GEOPDFIO_H_INCLUDED
+#define GEOPDFIO_H_INCLUDED
+
+#include "cpl_vsi_virtual.h"
+
+
+/* begin of poppler xpdf includes */
+#include <Object.h>
+#include <Stream.h>
+/* end of poppler xpdf includes */
+
+/************************************************************************/
+/*                         VSIPDFFileStream                             */
+/************************************************************************/
+
+#define BUFFER_SIZE 1024
+
+class VSIPDFFileStream: public BaseStream
+{
+    public:
+        VSIPDFFileStream(FILE* f, const char* pszFilename,
+                         Guint startA, GBool limitedA,
+                         Guint lengthA, Object *dictA);
+        VSIPDFFileStream(VSIPDFFileStream* poParent,
+                         Guint startA, GBool limitedA,
+                         Guint lengthA, Object *dictA);
+        virtual ~VSIPDFFileStream();
+
+        virtual Stream *   makeSubStream(Guint startA, GBool limitedA,
+                                         Guint lengthA, Object *dictA);
+        virtual int        getPos();
+        virtual Guint      getStart();
+        virtual StreamKind getKind();
+        virtual GooString *getFileName();
+
+        virtual int        getChar();
+        virtual int        getUnfilteredChar ();
+        virtual int        lookChar();
+
+        virtual void       reset();
+        virtual void       unfilteredReset ();
+        virtual void       close();
+        virtual void       setPos(Guint pos, int dir = 0);
+        virtual void       moveStart(int delta);
+
+    private:
+        VSIPDFFileStream  *poParent;
+        GooString         *poFilename;
+        FILE              *f;
+        int                nStart;
+        int                bLimited;
+        int                nLength;
+
+        int                nCurrentPos;
+        int                nSavedPos;
+
+        GByte              abyBuffer[BUFFER_SIZE];
+        int                nPosInBuffer;
+        int                nBufferLength;
+
+        int                FillBuffer();
+};
+
+#endif // GEOPDFIO_H_INCLUDED
