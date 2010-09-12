9416f2b4255f45f159949cdc75a0022d139d22d3
Tamas Szekeres <szekerest@gmail.com>
2010-09-12 17:11:20 +0000

    Fix header comments
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@20590 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/ogrsf_frmts/mssqlspatial/ogrmssqlgeometryparser.cpp
+++ ogr/ogrsf_frmts/mssqlspatial/ogrmssqlgeometryparser.cpp
@@ -287,7 +287,7 @@ OGRGeometryCollection* OGRMSSQLGeometryParser::ReadGeometryCollection(int iShape
 }
 
 /************************************************************************/
-/*                         CreateFromSqlGeometry()                      */
+/*                         ParseSqlGeometry()                           */
 /************************************************************************/
 
 
