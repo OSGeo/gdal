6a73b9bf61369c6fe5e2662cb0d6478198f380df
Even Rouault <even.rouault@mines-paris.org>
2010-09-14 19:47:40 +0000

    Rename driver to PDF / Geospatial PDF
    git-svn-id: https://svn.osgeo.org/gdal/trunk@20607 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/gcore/gdal_frmts.h
+++ gdal/gcore/gdal_frmts.h
@@ -156,7 +156,7 @@ void CPL_DLL GDALRegister_NTv2(void);
 void CPL_DLL GDALRegister_JP2OpenJPEG(void);
 void CPL_DLL GDALRegister_XYZ(void);
 void CPL_DLL GDALRegister_HF2(void);
-void CPL_DLL GDALRegister_GeoPDF(void);
+void CPL_DLL GDALRegister_PDF(void);
 CPL_C_END
 
 #endif /* ndef GDAL_FRMTS_H_INCLUDED */
