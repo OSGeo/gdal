5e5878fca29fcc29fc1616b7ae9963bdca51105d
Harsh Govind <harsh.govind@spadac.com>
2010-09-23 11:03:24 +0000

    Added copyright notice in kmlsuperoverlaydataset.cpp/.h
    git-svn-id: https://svn.osgeo.org/gdal/trunk@20678 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/kmlsuperoverlay/kmlsuperoverlaydataset.h
+++ gdal/frmts/kmlsuperoverlay/kmlsuperoverlaydataset.h
@@ -1,4 +1,33 @@
-#ifndef KMLSUPEROVERLAYDATASET_H_INCLUDED
+/******************************************************************************
+ * $Id: kmlsuperoverlaydataset.h 
+ *
+ * Project:  KmlSuperOverlay
+ * Purpose:  Implements write support for KML superoverlay - KMZ.
+ * Author:   Harsh Govind, harsh.govind@spadac.com
+ *
+ ******************************************************************************
+ * Copyright (c) 2010, SPADAC Inc. <harsh.govind@spadac.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+ 
+ #ifndef KMLSUPEROVERLAYDATASET_H_INCLUDED
 #define KMLSUPEROVERLAYDATASET_H_INCLUDED
 
 #include "gdal_pam.h"
