0da33becb60334923e73bfc01b4529f1dd1dcb51
Even Rouault <even.rouault@mines-paris.org>
2010-09-29 18:10:19 +0000

    Add read/write JPEGLS driver based on the CharLS library
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@20710 f0d54148-0727-0410-94bb-9a71ac55c965

--- gcore/gdal_frmts.h
+++ gcore/gdal_frmts.h
@@ -157,6 +157,7 @@ void CPL_DLL GDALRegister_JP2OpenJPEG(void);
 void CPL_DLL GDALRegister_XYZ(void);
 void CPL_DLL GDALRegister_HF2(void);
 void CPL_DLL GDALRegister_PDF(void);
+void CPL_DLL GDALRegister_JPEGLS(void);
 CPL_C_END
 
 #endif /* ndef GDAL_FRMTS_H_INCLUDED */
