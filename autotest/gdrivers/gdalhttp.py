5c66326e0912b8c42d0c7e4e2c9e42ebd99bfab7
Even Rouault <even.rouault@mines-paris.org>
2010-10-04 14:10:52 +0000

    Add testing of /vsicurl/
    git-svn-id: https://svn.osgeo.org/gdal/trunk@20743 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/gdrivers/gdalhttp.py
+++ autotest/gdrivers/gdalhttp.py
@@ -51,12 +51,98 @@ def http_1():
     if drv is None:
         return 'skip'
 
+    conn = gdaltest.gdalurlopen('http://home.gdal.org/~warmerda/frank.gif')
+    if conn is None:
+        print('cannot open URL')
+        return 'skip'
+    conn.close()
+
     tst = gdaltest.GDALTest( 'GIF','http://home.gdal.org/~warmerda/frank.gif',
                              1, 35415, filename_absolute = 1 )
     return tst.testOpen()
 
+###############################################################################
+# Verify /vsicurl (subversion file listing)
 
-gdaltest_list = [ http_1 ]
+def http_2():
+
+    try:
+        drv = gdal.GetDriverByName( 'HTTP' )
+    except:
+        drv = None
+
+    if drv is None:
+        return 'skip'
+
+    conn = gdaltest.gdalurlopen('http://svn.osgeo.org/gdal/trunk/autotest/gcore/data/byte.tif')
+    if conn is None:
+        print('cannot open URL')
+        return 'skip'
+    conn.close()
+    
+    tst = gdaltest.GDALTest( 'GTiff','/vsicurl/http://svn.osgeo.org/gdal/trunk/autotest/gcore/data/byte.tif',
+                             1, 4672, filename_absolute = 1 )
+    return tst.testOpen()
+
+###############################################################################
+# Verify /vsicurl (apache file listing)
+
+def http_3():
+
+    try:
+        drv = gdal.GetDriverByName( 'HTTP' )
+    except:
+        drv = None
+
+    if drv is None:
+        return 'skip'
+
+    conn = gdaltest.gdalurlopen('http://download.osgeo.org/gdal/data/ehdr/elggll.bil')
+    if conn is None:
+        print('cannot open URL')
+        return 'skip'
+    conn.close()
+
+    ds = gdal.Open('/vsicurl/http://download.osgeo.org/gdal/data/ehdr/elggll.bil')
+    if ds is None:
+        return 'fail'
+
+    return 'success'
+
+###############################################################################
+# Verify /vsicurl (ftp)
+
+def http_4():
+
+    try:
+        drv = gdal.GetDriverByName( 'HTTP' )
+    except:
+        drv = None
+
+    if drv is None:
+        return 'skip'
+
+    conn = gdaltest.gdalurlopen('http://download.osgeo.org/gdal/data/ehdr/elggll.bil')
+    if conn is None:
+        print('cannot open URL')
+        return 'skip'
+    conn.close()
+
+    ds = gdal.Open('/vsicurl/ftp://ftp.remotesensing.org/gdal/data/ehdr/elggll.bil')
+    if ds is None:
+        return 'fail'
+        
+    filelist = ds.GetFileList()
+    if filelist[1] != '/vsicurl/ftp://ftp.remotesensing.org/gdal/data/ehdr/elggll.hdr':
+        print(filelist)
+        return 'fail'
+
+    return 'success'
+
+gdaltest_list = [ http_1,
+                  http_2,
+                  http_3,
+                  http_4 ]
 
 if __name__ == '__main__':
 
