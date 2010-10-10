30810f2fc1844ad943fc58008e78db42073a97d3
Frank Warmerdam <warmerdam@pobox.com>
2010-10-10 01:49:24 +0000

    added LCC and POLYCONIC.  KNQ also collected now. (#3409)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@20803 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/bsb/bsbdataset.cpp
+++ gdal/frmts/bsb/bsbdataset.cpp
@@ -381,63 +381,113 @@ void BSBDataset::ScanForGCPs( bool isNos, const char *pszFilename )
         GDALHeuristicDatelineWrapGCPs( nGCPCount, pasGCPList );
 
 /* -------------------------------------------------------------------- */
-/*      Can we derive a reasonable coordinate system definition for     */
-/*      this file?  For now we keep it simple, just handling            */
-/*      mercator. In the future we should consider others.              */
+/*      Collect coordinate system related parameters from header.       */
 /* -------------------------------------------------------------------- */
-    CPLString osUnderlyingSRS;
     int i;
+    const char *pszKNP=NULL, *pszKNQ=NULL;
 
     for( i = 0; psInfo->papszHeader[i] != NULL; i++ )
     {
         if( EQUALN(psInfo->papszHeader[i],"KNP/",4) )
         {
-            const char *pszPR = strstr(psInfo->papszHeader[i],"PR=");
-            const char *pszValue, *pszEnd;
-            CPLString osPP;
+            pszKNP = psInfo->papszHeader[i];
+            SetMetadataItem( "BSB_KNP", pszKNP + 4 );
+        }
+        if( EQUALN(psInfo->papszHeader[i],"KNQ/",4) )
+        {
+            pszKNQ = psInfo->papszHeader[i]; 
+            SetMetadataItem( "BSB_KNQ", pszKNP + 4 );
+        }
+    }
 
-            // Capture the PP string.
-            pszValue = strstr(psInfo->papszHeader[i],"PP=");
+    
+/* -------------------------------------------------------------------- */
+/*      Can we derive a reasonable coordinate system definition for     */
+/*      this file?  For now we keep it simple, just handling            */
+/*      mercator. In the future we should consider others.              */
+/* -------------------------------------------------------------------- */
+    CPLString osUnderlyingSRS;
+    if( pszKNP != NULL )
+    {
+        const char *pszPR = strstr(pszKNP,"PR=");
+        const char *pszValue, *pszEnd;
+        CPLString osPP;
+        
+        // Capture the PP string.
+        pszValue = strstr(pszKNP,"PP=");
+        if( pszValue )
+            pszEnd = strstr(pszValue,",");
+        if( pszValue && pszEnd )
+            osPP.assign(pszValue+3,pszEnd-pszValue-3);
+        
+        if( pszPR == NULL )
+        {
+            /* no match */
+        }
+        else if( EQUALN(pszPR,"PR=MERCATOR", 11) )
+        {
+            // We somewhat arbitrarily select our first GCPX as our 
+            // central meridian.  This is mostly helpful to ensure 
+            // that regions crossing the dateline will be contiguous 
+            // in mercator.
+            osUnderlyingSRS.Printf( "PROJCS[\"Global Mercator\",GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563]],PRIMEM[\"Greenwich\",0],UNIT[\"degree\",0.01745329251994328]],PROJECTION[\"Mercator_2SP\"],PARAMETER[\"standard_parallel_1\",0],PARAMETER[\"latitude_of_origin\",0],PARAMETER[\"central_meridian\",%d],PARAMETER[\"false_easting\",0],PARAMETER[\"false_northing\",0],UNIT[\"Meter\",1]]", (int) pasGCPList[0].dfGCPX );
+        }
+
+        else if( EQUALN(pszPR,"PR=TRANSVERSE MERCATOR", 22)
+                 && osPP.size() > 0 )
+        {
+            
+            osUnderlyingSRS.Printf( 
+                "PROJCS[\"unnamed\",GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9108\"]],AUTHORITY[\"EPSG\",\"4326\"]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"latitude_of_origin\",0],PARAMETER[\"central_meridian\",%s],PARAMETER[\"scale_factor\",1],PARAMETER[\"false_easting\",0],PARAMETER[\"false_northing\",0]]", 
+                osPP.c_str() );
+        }
+
+        else if( EQUALN(pszPR,"PR=UNIVERSAL TRANSVERSE MERCATOR", 32)
+                 && osPP.size() > 0 )
+        {
+            // This is not *really* UTM unless the central meridian 
+            // matches a zone which it does not in some (most?) maps. 
+            osUnderlyingSRS.Printf( 
+                "PROJCS[\"unnamed\",GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9108\"]],AUTHORITY[\"EPSG\",\"4326\"]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"latitude_of_origin\",0],PARAMETER[\"central_meridian\",%s],PARAMETER[\"scale_factor\",0.9996],PARAMETER[\"false_easting\",500000],PARAMETER[\"false_northing\",0]]", 
+                osPP.c_str() );
+        }
+
+        else if( EQUALN(pszPR,"PR=POLYCONIC", 12) && osPP.size() > 0 )
+        {
+            osUnderlyingSRS.Printf( 
+                "PROJCS[\"unnamed\",GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9108\"]],AUTHORITY[\"EPSG\",\"4326\"]],PROJECTION[\"Polyconic\"],PARAMETER[\"latitude_of_origin\",0],PARAMETER[\"central_meridian\",%s],PARAMETER[\"false_easting\",0],PARAMETER[\"false_northing\",0]]", 
+                osPP.c_str() );
+        }
+        
+        else if( EQUALN(pszPR,"PR=LAMBERT CONFORMAL CONIC", 26) 
+                 && osPP.size() > 0 && pszKNQ != NULL )
+        {
+            CPLString osP2, osP3;
+        
+            // Capture the KNQ/P2 string.
+            pszValue = strstr(pszKNQ,"P2=");
             if( pszValue )
                 pszEnd = strstr(pszValue,",");
             if( pszValue && pszEnd )
-                osPP.assign(pszValue+3,pszEnd-pszValue-3);
-
-            // Capture whole line as metadata so some apps can do more
-            // specific processing.
-            SetMetadataItem( "BSB_KNP", psInfo->papszHeader[i] + 4 );
-
-            if( pszPR == NULL )
-            {
-                /* no match */
-            }
-            else if( EQUALN(pszPR,"PR=MERCATOR", 11) )
-            {
-                // We somewhat arbitrarily select our first GCPX as our 
-                // central meridian.  This is mostly helpful to ensure 
-                // that regions crossing the dateline will be contiguous 
-                // in mercator.
-                osUnderlyingSRS.Printf( "PROJCS[\"Global Mercator\",GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563]],PRIMEM[\"Greenwich\",0],UNIT[\"degree\",0.01745329251994328]],PROJECTION[\"Mercator_2SP\"],PARAMETER[\"standard_parallel_1\",0],PARAMETER[\"latitude_of_origin\",0],PARAMETER[\"central_meridian\",%d],PARAMETER[\"false_easting\",0],PARAMETER[\"false_northing\",0],UNIT[\"Meter\",1]]", (int) pasGCPList[0].dfGCPX );
-            }
-            else if( EQUALN(pszPR,"PR=TRANSVERSE MERCATOR", 22)
-                     && osPP.size() > 0 )
-            {
-                
-                osUnderlyingSRS.Printf( 
-                    "PROJCS[\"unnamed\",GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9108\"]],AUTHORITY[\"EPSG\",\"4326\"]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"latitude_of_origin\",0],PARAMETER[\"central_meridian\",%s],PARAMETER[\"scale_factor\",1],PARAMETER[\"false_easting\",0],PARAMETER[\"false_northing\",0]]", 
-                    osPP.c_str() );
-            }
-            else if( EQUALN(pszPR,"PR=UNIVERSAL TRANSVERSE MERCATOR", 32)
-                     && osPP.size() > 0 )
-            {
-                // This is not *really* UTM unless the central meridian 
-                // matches a zone which it does not in some (most?) maps. 
-                osUnderlyingSRS.Printf( 
-                    "PROJCS[\"unnamed\",GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9108\"]],AUTHORITY[\"EPSG\",\"4326\"]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"latitude_of_origin\",0],PARAMETER[\"central_meridian\",%s],PARAMETER[\"scale_factor\",0.9996],PARAMETER[\"false_easting\",500000],PARAMETER[\"false_northing\",0]]", 
-                    osPP.c_str() );
-            }
+                osP2.assign(pszValue+3,pszEnd-pszValue-3);
             
-            break;
+            // Capture the KNQ/P3 string.
+            pszValue = strstr(pszKNQ,"P3=");
+            if( pszValue )
+                pszEnd = strstr(pszValue,",");
+            if( pszValue )
+            {
+                if( pszEnd )
+                    osP3.assign(pszValue+3,pszEnd-pszValue-3);
+                else
+                    osP3.assign(pszValue+3);
+            }
+
+            if( osP2.size() > 0 && osP3.size() > 0 )
+                osUnderlyingSRS.Printf( 
+                    "PROJCS[\"unnamed\",GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9108\"]],AUTHORITY[\"EPSG\",\"4326\"]],PROJECTION[\"Lambert_Conformal_Conic_2SP\"],PARAMETER[\"standard_parallel_1\",%s],PARAMETER[\"standard_parallel_2\",%s],PARAMETER[\"latitude_of_origin\",0.0],PARAMETER[\"central_meridian\",%s],PARAMETER[\"false_easting\",0.0],PARAMETER[\"false_northing\",0.0]]",
+                    osP2.c_str(), osP3.c_str(), osPP.c_str() );
+
         }
     }
 
