90ae45eeb7159c10472bea8541ee50c91137fe92
Frank Warmerdam <warmerdam@pobox.com>
2010-10-21 18:23:35 +0000

    update with manifest support for newer visual studio versions
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@20921 f0d54148-0727-0410-94bb-9a71ac55c965

--- swig/python/fallback_build.bat
+++ swig/python/fallback_build.bat
@@ -8,7 +8,7 @@ rem
 set PYTHONHOME=C:\OSGeo4W\apps\Python25
 set OUTDIR=build\lib.win32-2.5\osgeo
 
-set CLARGS=/c /nologo /Ox /MD /GX /DNDEBUG -I../../port -I../../gcore -I../../alg -I../../ogr/ -I%PYTHONHOME%\include -I%PYTHONHOME%\PC -I%PYTHONHOME%\lib\site-packages\numpy\core\include
+set CLARGS=/c /nologo /Ox /MD /EHsc /DNDEBUG -I../../port -I../../gcore -I../../alg -I../../ogr/ -I%PYTHONHOME%\include -I%PYTHONHOME%\PC -I%PYTHONHOME%\lib\site-packages\numpy\core\include
 
 cl.exe %CLARGS% extensions\gdal_wrap.cpp
 cl.exe %CLARGS% extensions\osr_wrap.cpp
@@ -19,9 +19,17 @@ cl.exe %CLARGS% extensions\gdal_array_wrap.cpp
 set LINKOPTS=/DLL /nologo /INCREMENTAL:NO /LIBPATH:../../ /LIBPATH:%PYTHONHOME%\libs /LIBPATH:%PYTHONHOME%\PCBuild gdal_i.lib
 
 link.exe %LINKOPTS% /EXPORT:init_gdal gdal_wrap.obj /OUT:%OUTDIR%\_gdal.pyd 
-link.exe %LINKOPTS% /EXPORT:init_osr  osr_wrap.obj /OUT:%OUTDIR%\_osr.pyd 
-link.exe %LINKOPTS% /EXPORT:init_ogr  ogr_wrap.obj /OUT:%OUTDIR%\_ogr.pyd 
-link.exe %LINKOPTS% /EXPORT:init_gdalconst gdalconst_wrap.obj /OUT:%OUTDIR%\_gdalconst.pyd 
-link.exe %LINKOPTS% /EXPORT:init_gdal_array gdal_array_wrap.obj /OUT:%OUTDIR%\_gdal_array.pyd
+if exist %OUTDIR%\_gdal.pyd.manifest mt -manifest %OUTDIR%\_gdal.pyd.manifest -outputresource:%OUTDIR%\_gdal.pyd;2
 
+link.exe %LINKOPTS% /EXPORT:init_osr  osr_wrap.obj /OUT:%OUTDIR%\_osr.pyd 
+if exist %OUTDIR%\_osr.pyd.manifest mt -manifest %OUTDIR%\_osr.pyd.manifest -outputresource:%OUTDIR%\_osr.pyd;2
+
+link.exe %LINKOPTS% /EXPORT:init_ogr  ogr_wrap.obj /OUT:%OUTDIR%\_ogr.pyd 
+if exist %OUTDIR%\_ogr.pyd.manifest mt -manifest %OUTDIR%\_ogr.pyd.manifest -outputresource:%OUTDIR%\_ogr.pyd;2
+
+link.exe %LINKOPTS% /EXPORT:init_gdalconst gdalconst_wrap.obj /OUT:%OUTDIR%\_gdalconst.pyd 
+if exist %OUTDIR%\_gdalconst.pyd.manifest mt -manifest %OUTDIR%\_gdalconst.pyd.manifest -outputresource:%OUTDIR%\_gdalconst.pyd;2
+
+link.exe %LINKOPTS% /EXPORT:init_gdal_array gdal_array_wrap.obj /OUT:%OUTDIR%\_gdal_array.pyd
+if exist %OUTDIR%\_gdal_array.pyd.manifest mt -manifest %OUTDIR%\_gdal_array.pyd.manifest -outputresource:%OUTDIR%\_gdal_array.pyd;2
 
