3638ff47411e2f2baf1da33006cd7f597875fd30
Frank Warmerdam <warmerdam@pobox.com>
2010-12-08 16:43:11 +0000

    do not let polygon size go over MAX_INT (https://trac.osgeo.org/gdal/ticket/3872)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@21213 f0d54148-0727-0410-94bb-9a71ac55c965

--- alg/gdalsievefilter.cpp
+++ alg/gdalsievefilter.cpp
@@ -34,6 +34,7 @@
 CPL_CVSID("$Id$");
 
 #define GP_NODATA_MARKER -51502112
+#define MY_MAX_INT 2147483647
 
 /*
  * General Plan
@@ -259,7 +260,8 @@ GDALSieveFilter( GDALRasterBandH hSrcBand, GDALRasterBandH hMaskBand,
             iPoly = panThisLineId[iX]; 
 
             CPLAssert( iPoly >= 0 );
-            anPolySizes[iPoly] += 1;
+            if( anPolySizes[iPoly] < MY_MAX_INT )
+                anPolySizes[iPoly] += 1;
         }
 
 /* -------------------------------------------------------------------- */
@@ -300,7 +302,14 @@ GDALSieveFilter( GDALRasterBandH hSrcBand, GDALRasterBandH hMaskBand,
     {
         if( oFirstEnum.panPolyIdMap[iPoly] != iPoly )
         {
-            anPolySizes[oFirstEnum.panPolyIdMap[iPoly]] += anPolySizes[iPoly];
+            GIntBig nSize = anPolySizes[oFirstEnum.panPolyIdMap[iPoly]];
+
+            nSize += anPolySizes[iPoly];
+            
+            if( nSize > MY_MAX_INT )
+                nSize = MY_MAX_INT;
+
+            anPolySizes[oFirstEnum.panPolyIdMap[iPoly]] = (int)nSize;
             anPolySizes[iPoly] = 0;
         }
     }
