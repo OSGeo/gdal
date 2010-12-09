d806425f0fcb65f92785180b0ac296e27e318df4
Mateusz Loskot <mateusz@loskot.net>
2010-12-09 13:55:22 +0000

    Added DllMain callback to set-up and tear-down internal GDAL library resources automatically (Ticket #3824). This patch also includes equivalent functionality for Linux using GCC constructor/destructor attributes, however, temporarily disabled due to unresolved segmentation faults on some occasions - see the ticket comments.
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@21224 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ gcore/gdaldllmain.cpp
@@ -0,0 +1,121 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  GDAL Core
+ * Purpose:  The library set-up/clean-up routines.
+ * Author:   Mateusz Loskot <mateusz@loskot.net>
+ *
+ ******************************************************************************
+ * Copyright (c) 2010, Mateusz Loskot <mateusz@loskot.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+
+#include "gdal.h"
+#include "ogr_api.h"
+#include "cpl_multiproc.h"
+
+
+/* FIXME:
+ * The Linux implementation is temporarily disabled, see Comment 3
+ * http://trac.osgeo.org/gdal/ticket/3824
+ */
+#if 0 
+/************************************************************************/
+/*  The library set-up/clean-up routines implemented with               */
+/*  GNU C/C++ extensions.                                               */
+/*  TODO: Is it Linux-only solution or Unix portable?                   */
+/************************************************************************/
+#ifdef __GNUC__
+
+static void GDALInitialize(void) __attribute__ ((constructor)) ;
+static void GDALDestroy(void)    __attribute__ ((destructor)) ;
+
+/************************************************************************/
+/* Called when GDAL is loaded by loader or by dlopen(),                 */
+/* and before dlopen() returns.                                         */
+/************************************************************************/
+
+static void GDALInitialize(void)
+{
+    // nothing to do
+    //CPLDebug("GDAL", "Library loaded");
+}
+
+/************************************************************************/
+/* Called when GDAL is unloaded by loader or by dlclose(),              */
+/* and before dlclose() returns.                                        */
+/************************************************************************/
+
+static void GDALDestroy(void)
+{
+    // TODO: Confirm if calling CPLCleanupTLS here is safe
+    //CPLCleanupTLS();
+
+    GDALDestroyDriverManager();
+
+#ifdef OGR_ENABLED
+    OGRCleanupAll();
+#endif
+    //CPLDebug("GDAL", "Library unloaded");
+}
+
+#endif // __GNUC__
+
+#endif // #if 0
+
+/************************************************************************/
+/*  The library set-up/clean-up routine implemented as DllMain entry    */
+/*  point specific for Windows.                                         */
+/************************************************************************/
+#ifdef _MSC_VER
+
+#include <windows.h>
+
+extern "C" int WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
+{
+    UNREFERENCED_PARAMETER(hInstance);
+    UNREFERENCED_PARAMETER(lpReserved);
+
+    if (dwReason == DLL_PROCESS_ATTACH)
+    {
+        // nothing to do
+    }
+    else if (dwReason == DLL_THREAD_ATTACH)
+    {
+        // nothing to do
+    }
+    else if (dwReason == DLL_THREAD_DETACH)
+    {
+        ::CPLCleanupTLS();
+    }
+    else if (dwReason == DLL_PROCESS_DETACH)
+    {
+        ::GDALDestroyDriverManager();
+
+#ifdef OGR_ENABLED
+        ::OGRCleanupAll();
+#endif
+    }
+
+	return 1; // ignroed for all reasons but DLL_PROCESS_ATTACH
+}
+
+#endif // _MSC_VER
+
