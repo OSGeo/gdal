e994572b4e503757ae65652a45b2e4e8ee8ae33b
Even Rouault <even.rouault@mines-paris.org>
2010-12-14 23:26:40 +0000

    resync with ogr2ogr.cpp
    git-svn-id: https://svn.osgeo.org/gdal/trunk@21266 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/swig/python/samples/ogr2ogr.py
+++ gdal/swig/python/samples/ogr2ogr.py
@@ -507,8 +507,7 @@ def main(args = None, progress_func = TermProgress, progress_data = None):
                 poODS = ogr.Open( pszDestDataSource, False )
                 if poODS is None:
                     # /* ok the datasource doesn't exist at all */
-                    # /* so let's ignore the -overwrite or -append flag */
-                    bUpdate = bAppend = bOverwrite = False
+                    bUpdate = False
                 else:
                     poODS.delete()
                     poODS = None
