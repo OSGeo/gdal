cfe8f6459b188b0ed78da50687e6a12b235a2e17
Even Rouault <even.rouault@mines-paris.org>
2010-12-22 22:15:07 +0000

    Test that importing the wkb that would be equivalent to MULTIPOINT(POLYGON((0 0)) doesn't work
    git-svn-id: https://svn.osgeo.org/gdal/trunk@21308 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/ogr/ogr_wkbwkt_geom.py
+++ autotest/ogr/ogr_wkbwkt_geom.py
@@ -447,6 +447,25 @@ def ogr_wkbwkt_test_import_wkt_sf12():
     return 'success'
 
 ###############################################################################
+# Test that importing the wkb that would be equivalent to MULTIPOINT(POLYGON((0 0))
+# doesn't work
+
+def ogr_wkbwkt_test_import_bad_multipoint_wkb():
+
+    import struct
+    wkb = struct.pack('B' * 30, 0, 0, 0, 0, 6, 0, 0, 0, 1, 0, 0, 0, 0, 1, 64, 0, 0, 0, 0, 0, 0, 0, 64, 0, 0, 0, 0, 0, 0, 0 )
+    gdal.PushErrorHandler('CPLQuietErrorHandler')
+    old_val = gdal.GetConfigOption('CPL_DEBUG')
+    gdal.SetConfigOption('CPL_DEBUG', 'ON')
+    geom =  ogr.CreateGeometryFromWkb(wkb)
+    gdal.SetConfigOption('CPL_DEBUG', old_val)
+    gdal.PopErrorHandler()
+    if geom is not None:
+        return 'fail'
+
+    return 'success'
+
+###############################################################################
 # When imported build a list of units based on the files available.
 
 #print 'hit enter'
@@ -463,6 +482,7 @@ for filename in files:
 gdaltest_list.append( ogr_wkbwkt_geom_bigexponents )
 gdaltest_list.append( ogr_wkbwkt_test_broken_geom )
 gdaltest_list.append( ogr_wkbwkt_test_import_wkt_sf12 )
+gdaltest_list.append( ogr_wkbwkt_test_import_bad_multipoint_wkb )
 
 if __name__ == '__main__':
 
