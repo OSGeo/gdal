5096b413a52c997662fd00f8a000bb07ec78f430
Even Rouault <even.rouault@mines-paris.org>
2011-01-17 19:30:17 +0000

    png: add compatibility with libpng >= 1.5.0 (https://trac.osgeo.org/gdal/ticket/3914)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@21525 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/png/pngdataset.cpp
+++ gdal/frmts/png/pngdataset.cpp
@@ -1315,7 +1315,7 @@ png_vsi_read_data(png_structp png_ptr, png_bytep data, png_size_t length)
     * instead of an int, which is what fread() actually returns.
     */
    check = (png_size_t)VSIFReadL(data, (png_size_t)1, length,
-                                 (VSILFILE*)png_ptr->io_ptr);
+                                 (VSILFILE*)png_get_io_ptr(png_ptr));
 
    if (check != length)
       png_error(png_ptr, "Read Error");
@@ -1330,7 +1330,7 @@ png_vsi_write_data(png_structp png_ptr, png_bytep data, png_size_t length)
 {
    png_uint_32 check;
 
-   check = VSIFWriteL(data, 1, length, (VSILFILE*)(png_ptr->io_ptr));
+   check = VSIFWriteL(data, 1, length, (VSILFILE*)png_get_io_ptr(png_ptr));
 
    if (check != length)
       png_error(png_ptr, "Write Error");
@@ -1341,7 +1341,7 @@ png_vsi_write_data(png_structp png_ptr, png_bytep data, png_size_t length)
 /************************************************************************/
 static void png_vsi_flush(png_structp png_ptr)
 {
-    VSIFFlushL( (VSILFILE*)(png_ptr->io_ptr) );
+    VSIFFlushL( (VSILFILE*)png_get_io_ptr(png_ptr) );
 }
 
 /************************************************************************/
@@ -1357,7 +1357,7 @@ static void png_gdal_error( png_structp png_ptr, const char *error_message )
     // libpng is generally not built as C++ and so won't honour unwind
     // semantics.  Ugg. 
 
-    jmp_buf* psSetJmpContext = (jmp_buf*) png_ptr->error_ptr;
+    jmp_buf* psSetJmpContext = (jmp_buf*) png_get_error_ptr(png_ptr);
     if (psSetJmpContext)
     {
         longjmp( *psSetJmpContext, 1 );
