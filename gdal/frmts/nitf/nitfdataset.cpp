d3aab999e6747361b347b91682296ddd9c83b5f1
Frank Warmerdam <warmerdam@pobox.com>
2011-01-19 18:15:28 +0000

    support JP2KAK driver for jpeg2000 output, use /vsisubfile/ in all cases
    git-svn-id: https://svn.osgeo.org/gdal/trunk@21534 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/nitf/nitfdataset.cpp
+++ gdal/frmts/nitf/nitfdataset.cpp
@@ -3849,6 +3849,12 @@ NITFDataset::NITFCreateCopy(
             }
             if( poJ2KDriver == NULL )
             {
+                /* Try with JP2KAK as an alternate driver */
+                poJ2KDriver = 
+                    GetGDALDriverManager()->GetDriverByName( "JP2KAK" );
+            }
+            if( poJ2KDriver == NULL )
+            {
                 CPLError( 
                     CE_Failure, CPLE_AppDefined, 
                     "Unable to write JPEG2000 compressed NITF file.\n"
@@ -4213,20 +4219,12 @@ NITFDataset::NITFCreateCopy(
         GUIntBig nImageOffset = psFile->pasSegmentInfo[0].nSegmentStart;
         CPLString osDSName;
 
-        if (EQUAL(poJ2KDriver->GetDescription(), "JP2ECW"))
-        {
-            osDSName.Printf( "J2K_SUBFILE:" CPL_FRMT_GUIB ",%d,%s", nImageOffset, -1,
-                             pszFilename );
-        }
-        else
-        {
-            /* Jasper case */
-            osDSName.Printf( "/vsisubfile/" CPL_FRMT_GUIB "_%d,%s", nImageOffset, -1,
-                             pszFilename );
-        }
-                             
         NITFClose( psFile );
 
+        osDSName.Printf( "/vsisubfile/" CPL_FRMT_GUIB "_%d,%s", 
+                         nImageOffset, -1,
+                         pszFilename );
+                             
         if (EQUAL(poJ2KDriver->GetDescription(), "JP2ECW"))
         {
             char** papszJP2Options = NITFJP2Options(papszOptions);
