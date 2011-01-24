b6f648a6e05f8e3aec57d449f4455b306f59a11f
Ragi Yaser Burhum <unknown@unknown>
2011-01-24 03:38:40 +0000

    Initial FileGDB support. Only works with points (for now). Read-only (for now).
    git-svn-id: https://svn.osgeo.org/gdal/trunk@21571 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ gdal/ogr/ogrsf_frmts/filegdb/FGdbUtils.h
@@ -0,0 +1,64 @@
+/******************************************************************************
+ *
+ * Project:  OpenGIS Simple Features Reference Implementation
+ * Purpose:  Various FileGDB OGR Datasource utility functions
+ * Author:   Ragi Yaser Burhum, ragi@burhum.com
+ *
+ ******************************************************************************
+ * Copyright (c) 2010, Ragi Yaser Burhum
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+
+
+#ifndef _FGDB_UTILS_H_INCLUDED
+#define _FGDB_UTILS_H_INCLUDED
+
+#include "ogr_fgdb.h"
+#include <iostream>
+#include <string>
+#include "cpl_minixml.h"
+
+std::wstring StringToWString(const std::string& s);
+std::string WStringToString(const std::wstring& s);
+
+//
+// GDB API to OGR Geometry Mapping
+//
+bool GDBToOGRGeometry(std::string geoType, bool hasZ, OGRwkbGeometryType* pOut);
+bool GDBToOGRSpatialReference(const std::string & wkt, OGRSpatialReference** ppSR);
+
+bool GDBGeometryToOGRGeometry(bool forceMulti, FileGDBAPI::ShapeBuffer* pGdbGeometry, 
+                              OGRSpatialReference* pOGRSR, OGRGeometry** ppOutGeometry);
+
+//temporary version - until we can parse the full binary format
+bool GhettoGDBGeometryToOGRGeometry(bool forceMulti, FileGDBAPI::ShapeBuffer* pGdbGeometry, 
+                                    OGRSpatialReference* pOGRSR, OGRGeometry** ppOutGeometry);
+//
+// GDB API to OGR Field Mapping
+//
+bool GDBToOGRFieldType(std::string gdbType, OGRFieldType* ogrType);
+
+//
+// GDBAPI error to OGR
+//
+bool GDBErr(long hr, std::string desc);
+
+
+#endif
