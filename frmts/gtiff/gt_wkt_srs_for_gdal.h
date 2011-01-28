eb0122990ea65b0b2f960359b07d821a5185be4f
Frank Warmerdam <warmerdam@pobox.com>
2011-01-28 04:21:40 +0000

    correct author attribution
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@21595 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/gtiff/gt_wkt_srs_for_gdal.h
+++ frmts/gtiff/gt_wkt_srs_for_gdal.h
@@ -3,10 +3,10 @@
  *
  * Project:  GeoTIFF Driver
  * Purpose:  Read/Write in-memory GeoTIFF file
- * Author:   Frank Warmerdam, warmerdam@pobox.com
+ * Author:   Even Rouault
  *
  ******************************************************************************
- * Copyright (c) 1999, Frank Warmerdam
+ * Copyright (c) 2010, Even Rouault
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
