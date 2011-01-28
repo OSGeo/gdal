f2b9362261cc34a1a3ab509e2acc242b90a580dd
Frank Warmerdam <warmerdam@pobox.com>
2011-01-28 18:55:08 +0000

    provide for ABI specific plugin subdirectories on all platforms
    git-svn-id: https://svn.osgeo.org/gdal/trunk@21596 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/generic/ogrsfdriverregistrar.cpp
+++ gdal/ogr/ogrsf_frmts/generic/ogrsfdriverregistrar.cpp
@@ -835,11 +835,26 @@ void OGRSFDriverRegistrar::AutoLoadDrivers()
     }
 
 /* -------------------------------------------------------------------- */
-/*      Scan each directory looking for files starting with gdal_       */
+/*      Format the ABI version specific subdirectory to look in.        */
+/* -------------------------------------------------------------------- */
+    CPLString osABIVersion;
+
+    osABIVersion.Printf( "%d.%d", GDAL_VERSION_MAJOR, GDAL_VERSION_MINOR );
+
+/* -------------------------------------------------------------------- */
+/*      Scan each directory looking for files starting with ogr_        */
 /* -------------------------------------------------------------------- */
     for( int iDir = 0; iDir < CSLCount(papszSearchPath); iDir++ )
     {
-        char  **papszFiles = CPLReadDir( papszSearchPath[iDir] );
+        char **papszFiles = NULL;
+        VSIStatBufL sStatBuf;
+        CPLString osABISpecificDir =
+            CPLFormFilename( papszSearchPath[iDir], osABIVersion, NULL );
+        
+        if( VSIStatL( osABISpecificDir, &sStatBuf ) != 0 )
+            osABISpecificDir = papszSearchPath[iDir];
+
+        papszFiles = CPLReadDir( osABISpecificDir );
 
         for( int iFile = 0; iFile < CSLCount(papszFiles); iFile++ )
         {
@@ -861,7 +876,7 @@ void OGRSFDriverRegistrar::AutoLoadDrivers()
                      CPLGetBasename(papszFiles[iFile]) + 4 );
             
             pszFilename = 
-                CPLFormFilename( papszSearchPath[iDir], 
+                CPLFormFilename( osABISpecificDir,
                                  papszFiles[iFile], NULL );
 
             pRegister = CPLGetSymbol( pszFilename, pszFuncName );
