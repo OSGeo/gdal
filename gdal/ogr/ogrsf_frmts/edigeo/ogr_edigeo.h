18a1603b5f83aaf26699a24457db28a999232b9d
Even Rouault <even.rouault@mines-paris.org>
2011-02-06 13:08:20 +0000

    Add new OGR driver to read files of French EDIGEO exchange format
    git-svn-id: https://svn.osgeo.org/gdal/trunk@21630 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ gdal/ogr/ogrsf_frmts/edigeo/ogr_edigeo.h
@@ -0,0 +1,235 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  EDIGEO Translator
+ * Purpose:  Definition of classes for OGR .edigeo driver.
+ * Author:   Even Rouault, even dot rouault at mines dash paris dot org
+ *
+ ******************************************************************************
+ * Copyright (c) 2011, Even Rouault <even dot rouault at mines dash paris dot org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+
+#ifndef _OGR_EDIGEO_H_INCLUDED
+#define _OGR_EDIGEO_H_INCLUDED
+
+#include "ogrsf_frmts.h"
+#include <vector>
+#include <map>
+
+/************************************************************************/
+/*                           OGREDIGEOLayer                             */
+/************************************************************************/
+
+class OGREDIGEODataSource;
+
+class OGREDIGEOLayer : public OGRLayer
+{
+    OGREDIGEODataSource* poDS;
+
+    OGRFeatureDefn*    poFeatureDefn;
+    OGRSpatialReference *poSRS;
+
+    int                nNextFID;
+
+    OGRFeature *       GetNextRawFeature();
+
+    std::vector<OGRFeature*> aosFeatures;
+
+    /* Map attribute RID ('TEX2_id') to its index in the OGRFeatureDefn */
+    std::map<CPLString, int> mapAttributeToIndex;
+
+  public:
+                        OGREDIGEOLayer(OGREDIGEODataSource* poDS,
+                                       const char* pszName, OGRwkbGeometryType eType,
+                                       OGRSpatialReference* poSRS);
+                        ~OGREDIGEOLayer();
+
+
+    virtual void                ResetReading();
+    virtual OGRFeature *        GetNextFeature();
+    virtual OGRFeature *        GetFeature(long nFID);
+    virtual int                 GetFeatureCount( int bForce );
+
+    virtual OGRFeatureDefn *    GetLayerDefn() { return poFeatureDefn; }
+
+    virtual int                 TestCapability( const char * );
+
+    virtual OGRSpatialReference *GetSpatialRef() { return poSRS; }
+
+    virtual OGRErr              GetExtent(OGREnvelope *psExtent, int bForce);
+
+
+    void                        AddFeature(OGRFeature* poFeature);
+
+    int                         GetAttributeIndex(const CPLString& osRID);
+    void                        AddFieldDefn(const CPLString& osName, OGRFieldType eType,
+                                             const CPLString& osRID);
+};
+
+/************************************************************************/
+/*                         OGREDIGEODataSource                          */
+/************************************************************************/
+
+typedef std::pair<double, double> xyPairType;
+typedef std::vector< xyPairType > xyPairListType;
+typedef std::pair<CPLString, CPLString> strstrType;
+typedef std::vector<CPLString> strListType;
+
+/* From the .DIC file */
+class OGREDIGEOAttributeDef
+{
+    public:
+        OGREDIGEOAttributeDef() {}
+
+        CPLString osLAB; /* e.g. TEX2 */
+        CPLString osTYP; /* e.g. T */
+};
+
+/* From the .SCD file */
+class OGREDIGEOObjectDescriptor
+{
+    public:
+        OGREDIGEOObjectDescriptor() {}
+
+        CPLString osRID;        /* e.g. BATIMENT_id */
+        CPLString osNameRID;    /* e.g. ID_N_OBJ_E_2_1_0 */
+        CPLString osKND;        /* e.g. ARE */
+        strListType aosAttrRID; /* e.g. DUR_id, TEX_id */
+};
+
+/* From the .SCD file */
+class OGREDIGEOAttributeDescriptor
+{
+    public:
+        OGREDIGEOAttributeDescriptor() {}
+
+        CPLString osRID;        /* e.g. TEX2_id */
+        CPLString osNameRID;    /* e.g. ID_N_ATT_TEX2 */
+        int nWidth;             /* e.g. 80 */
+};
+
+/* From the .VEC files */
+class OGREDIGEOFEADesc
+{
+    public:
+        OGREDIGEOFEADesc() {}
+
+        std::vector< strstrType > aosAttIdVal; /* e.g. (TEX2_id,BECHEREL),(IDU_id,022) */
+        CPLString osSCP;                       /* e.g. COMMUNE_id */
+};
+
+class OGREDIGEODataSource : public OGRDataSource
+{
+    friend class OGREDIGEOLayer;
+
+    char*               pszName;
+    VSILFILE*           fpTHF;
+
+    OGRLayer**          papoLayers;
+    int                 nLayers;
+
+    CPLString osLON; /* Nom du lot */
+    CPLString osGNN; /* Nom du sous-ensemble de données générales */
+    CPLString osGON; /* Nom du sous-ensemble de la référence de coordonnées */
+    CPLString osDIN; /* Nom du sous-ensemble de définition de la nomenclature */
+    CPLString osSCN; /* Nom du sous-ensemble de définition du SCD */
+    strListType aosGDN; /* Nom du sous-ensemble de données géographiques */
+    int                 ReadTHF(VSILFILE* fp);
+
+    CPLString           osREL;
+    OGRSpatialReference* poSRS;
+    int                 ReadGEO();
+
+    /* Map from ID_N_OBJ_E_2_1_0 to OBJ_E_2_1_0 */
+    std::map<CPLString,CPLString> mapObjects;
+
+    /* Map from ID_N_ATT_TEX2 to (osLAB=TEX2, osTYP=T) */
+    std::map<CPLString,OGREDIGEOAttributeDef> mapAttributes;
+    int                 ReadDIC();
+
+    std::vector<OGREDIGEOObjectDescriptor> aoObjList;
+    /* Map from TEX2_id to (osNameRID=ID_N_ATT_TEX2, nWidth=80) */
+    std::map<CPLString,OGREDIGEOAttributeDescriptor> mapAttributesSCD;
+    int                 ReadSCD();
+
+    int                 bExtentValid;
+    double              dfMinX;
+    double              dfMinY;
+    double              dfMaxX;
+    double              dfMaxY;
+    int                 ReadGEN();
+
+    std::map<CPLString, OGREDIGEOLayer*> mapLayer;
+
+    int                 CreateLayer(const OGREDIGEOObjectDescriptor& objDesc);
+
+    std::map< CPLString, xyPairType >                 mapPNO; /* Map Noeud_X to (x,y) */
+    std::map< CPLString, xyPairListType >             mapPAR; /* Map Arc_X to ((x1,y1),...(xn,yn)) */
+    std::map< CPLString, OGREDIGEOFEADesc >           mapFEA; /* Map Object_X to FEADesc */
+    std::map< CPLString, strListType >                mapPFE_PAR; /* Map Face_X to (Arc_X1,..Arc_Xn) */
+    std::vector< strstrType >                         listFEA_PFE; /* List of (Object_X,Face_Y) */
+    std::vector< std::pair<CPLString, strListType > > listFEA_PAR; /* List of (Object_X,(Arc_Y1,..Arc_Yn))) */
+    std::vector< strstrType >                         listFEA_PNO; /* List of (Object_X,Noeud_Y) */
+    std::map< CPLString, strstrType >                 mapLNK_FEA_FEA; /* unused for now */
+
+    int                 ReadVEC(const char* pszVECName);
+
+    OGRFeature*         CreateFeature(const CPLString& osFEA);
+    int                 BuildPoints();
+    int                 BuildLineStrings();
+    int                 BuildPolygon(const CPLString& osFEA,
+                                     const CPLString& osPFE);
+    int                 BuildPolygons();
+
+    int                 bHasReadEDIGEO;
+    void                ReadEDIGEO();
+
+  public:
+                        OGREDIGEODataSource();
+                        ~OGREDIGEODataSource();
+
+    int                 Open( const char * pszFilename,
+                              int bUpdate );
+
+    virtual const char*         GetName() { return pszName; }
+
+    virtual int                 GetLayerCount();
+    virtual OGRLayer*           GetLayer( int );
+
+    virtual int                 TestCapability( const char * );
+};
+
+/************************************************************************/
+/*                           OGREDIGEODriver                            */
+/************************************************************************/
+
+class OGREDIGEODriver : public OGRSFDriver
+{
+  public:
+                ~OGREDIGEODriver();
+
+    virtual const char*         GetName();
+    virtual OGRDataSource*      Open( const char *, int );
+    virtual int                 TestCapability( const char * );
+};
+
+
+#endif /* ndef _OGR_EDIGEO_H_INCLUDED */
---
+++ gdal/ogr/ogrsf_frmts/edigeo/ogredigeolayer.cpp
@@ -0,0 +1,218 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  EDIGEO Translator
+ * Purpose:  Implements OGREDIGEOLayer class.
+ * Author:   Even Rouault, <even dot rouault at mines dash paris dot org>
+ *
+ ******************************************************************************
+ * Copyright (c) 2011, Even Rouault <even dot rouault at mines dash paris dot org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+
+#include "ogr_edigeo.h"
+#include "cpl_conv.h"
+#include "cpl_string.h"
+#include "ogr_p.h"
+#include "ogr_srs_api.h"
+
+CPL_CVSID("$Id$");
+
+/************************************************************************/
+/*                          OGREDIGEOLayer()                            */
+/************************************************************************/
+
+OGREDIGEOLayer::OGREDIGEOLayer( OGREDIGEODataSource* poDS,
+                                const char* pszName, OGRwkbGeometryType eType,
+                                OGRSpatialReference* poSRS )
+
+{
+    this->poDS = poDS;
+    nNextFID = 0;
+
+    this->poSRS = poSRS;
+
+    poFeatureDefn = new OGRFeatureDefn( pszName );
+    poFeatureDefn->Reference();
+    poFeatureDefn->SetGeomType( eType );
+}
+
+/************************************************************************/
+/*                          ~OGREDIGEOLayer()                           */
+/************************************************************************/
+
+OGREDIGEOLayer::~OGREDIGEOLayer()
+
+{
+    for(int i=0;i<(int)aosFeatures.size();i++)
+        delete aosFeatures[i];
+    poFeatureDefn->Release();
+}
+
+
+/************************************************************************/
+/*                            ResetReading()                            */
+/************************************************************************/
+
+void OGREDIGEOLayer::ResetReading()
+
+{
+    nNextFID = 0;
+}
+
+
+/************************************************************************/
+/*                           GetNextFeature()                           */
+/************************************************************************/
+
+OGRFeature *OGREDIGEOLayer::GetNextFeature()
+{
+    OGRFeature  *poFeature;
+
+    while(TRUE)
+    {
+        poFeature = GetNextRawFeature();
+        if (poFeature == NULL)
+            return NULL;
+
+        if((m_poFilterGeom == NULL
+            || FilterGeometry( poFeature->GetGeometryRef() ) )
+        && (m_poAttrQuery == NULL
+            || m_poAttrQuery->Evaluate( poFeature )) )
+        {
+            return poFeature;
+        }
+        else
+            delete poFeature;
+    }
+}
+
+/************************************************************************/
+/*                         GetNextRawFeature()                          */
+/************************************************************************/
+
+OGRFeature *OGREDIGEOLayer::GetNextRawFeature()
+{
+    if (nNextFID < (int)aosFeatures.size())
+    {
+        OGRFeature* poFeature = aosFeatures[nNextFID]->Clone();
+        nNextFID++;
+        return poFeature;
+    }
+    else
+        return NULL;
+}
+
+/************************************************************************/
+/*                            GetFeature()                              */
+/************************************************************************/
+
+OGRFeature * OGREDIGEOLayer::GetFeature(long nFID)
+{
+    if (nFID >= 0 && nFID < (int)aosFeatures.size())
+        return aosFeatures[nFID]->Clone();
+    else
+        return NULL;
+}
+
+/************************************************************************/
+/*                           TestCapability()                           */
+/************************************************************************/
+
+int OGREDIGEOLayer::TestCapability( const char * pszCap )
+
+{
+    if (EQUAL(pszCap, OLCFastFeatureCount))
+        return m_poFilterGeom == NULL && m_poAttrQuery == NULL;
+
+    else if (EQUAL(pszCap, OLCRandomRead))
+        return TRUE;
+
+    return FALSE;
+}
+
+/************************************************************************/
+/*                              GetExtent()                             */
+/************************************************************************/
+
+OGRErr OGREDIGEOLayer::GetExtent(OGREnvelope *psExtent, int bForce)
+{
+    /*if (poDS->bExtentValid)
+    {
+        psExtent->MinX = poDS->dfMinX;
+        psExtent->MinY = poDS->dfMinY;
+        psExtent->MaxX = poDS->dfMaxX;
+        psExtent->MaxY = poDS->dfMaxY;
+        return OGRERR_NONE;
+    }*/
+
+    return OGRLayer::GetExtent(psExtent, bForce);
+}
+
+/************************************************************************/
+/*                          GetFeatureCount()                           */
+/************************************************************************/
+
+int OGREDIGEOLayer::GetFeatureCount( int bForce )
+{
+    if (m_poFilterGeom != NULL || m_poAttrQuery != NULL)
+        return OGRLayer::GetFeatureCount(bForce);
+
+    return (int)aosFeatures.size();
+}
+
+/************************************************************************/
+/*                             AddFeature()                             */
+/************************************************************************/
+
+void OGREDIGEOLayer::AddFeature(OGRFeature* poFeature)
+{
+    poFeature->SetFID(aosFeatures.size());
+    aosFeatures.push_back(poFeature);
+}
+
+/************************************************************************/
+/*                         GetAttributeIndex()                          */
+/************************************************************************/
+
+int OGREDIGEOLayer::GetAttributeIndex(const CPLString& osRID)
+{
+    std::map<CPLString,int>::iterator itAttrIndex =
+                                            mapAttributeToIndex.find(osRID);
+    if (itAttrIndex != mapAttributeToIndex.end())
+        return itAttrIndex->second;
+    else
+        return -1;
+}
+
+/************************************************************************/
+/*                           AddFieldDefn()                             */
+/************************************************************************/
+
+void OGREDIGEOLayer::AddFieldDefn(const CPLString& osName,
+                                  OGRFieldType eType,
+                                  const CPLString& osRID)
+{
+    if (osRID.size() != 0)
+        mapAttributeToIndex[osRID] = poFeatureDefn->GetFieldCount();
+
+    OGRFieldDefn oFieldDefn(osName, eType);
+    poFeatureDefn->AddFieldDefn(&oFieldDefn);
+}
