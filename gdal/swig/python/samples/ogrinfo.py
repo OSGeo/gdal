e1f364e89f13edcebe5c7f9155de329436ca5ff7
Even Rouault <even.rouault@mines-paris.org>
2011-02-08 21:33:38 +0000

    RFC30 / SWIG bindings: Correct the signature of Datasource.CreateDataSource() and DeleteDataSource() to accept UTF-8 filenames; Refresh python bindings; Fix in swig/python.samples/ogrinfo.py to work with UTF-8 filenames (https://trac.osgeo.org/gdal/ticket/3766)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@21659 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/swig/python/samples/ogrinfo.py
+++ gdal/swig/python/samples/ogrinfo.py
@@ -193,9 +193,12 @@ def main(argv = None):
         print( "INFO: Open of `%s'\n"
                 "      using driver `%s' successful." % (pszDataSource, poDriver.GetName()) )
 
-    if bVerbose and pszDataSource != poDS.GetName():
+    poDS_Name = poDS.GetName()
+    if str(type(pszDataSource)) == "<type 'unicode'>" and str(type(poDS_Name)) == "<type 'str'>":
+        poDS_Name = unicode(poDS_Name, "utf8")
+    if bVerbose and pszDataSource != poDS_Name:
         print( "INFO: Internal data source name `%s'\n"
-                "      different from user name `%s'." % (poDS.GetName(), pszDataSource ))
+                "      different from user name `%s'." % (poDS_Name, pszDataSource ))
 
 #/* -------------------------------------------------------------------- */
 #/*      Special case for -sql clause.  No source layers required.       */
