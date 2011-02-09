8fbbbb93d59d79942af9db9b86b19726ae0051c4
Even Rouault <even.rouault@mines-paris.org>
2011-02-09 19:38:00 +0000

    adjust results for spheroid renaming logic (#3904)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@21663 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/gdrivers/pdf.py
+++ autotest/gdrivers/pdf.py
@@ -153,7 +153,7 @@ def pdf_1():
             print(expected_gt)
             return 'fail'
 
-    expected_wkt = 'PROJCS["WGS_1984_UTM_Zone_20N",GEOGCS["GCS_WGS_1984",DATUM["WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Transverse_Mercator"],PARAMETER["False_Easting",500000.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",-63.0],PARAMETER["Scale_Factor",0.9996],PARAMETER["Latitude_Of_Origin",0.0],UNIT["Meter",1.0]]'
+    expected_wkt = 'PROJCS["WGS_1984_UTM_Zone_20N",GEOGCS["GCS_WGS_1984",DATUM["WGS_1984",SPHEROID["WGS_84",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Transverse_Mercator"],PARAMETER["False_Easting",500000.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",-63.0],PARAMETER["Scale_Factor",0.9996],PARAMETER["Latitude_Of_Origin",0.0],UNIT["Meter",1.0]]'
     if wkt != expected_wkt:
         gdaltest.post_reason('bad WKT')
         print(wkt)
