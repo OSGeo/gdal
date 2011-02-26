OSGeo/gdal

commit 15447252be35cb30d2d91f6a673a9428c956ad76

TiledWMS: replace unsafe test for empty C++ strings based on testing [0] by proper call to empty() method (https://trac.osgeo.org/gdal/ticket/3493)
git-svn-id: https://svn.osgeo.org/gdal/trunk@21728 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/frmts/wms/minidriver_tiled_wms.cpp
@@ -206,7 +206,7 @@ void FindChangePattern( char *cdata,char **substs, CPLString &ret) {
     }
 
     // if no match is found, return first string
-    if (ret[0]=='\0') ret=papszTokens[0];
+    if (ret.empty()) ret=papszTokens[0];
     CSLDestroy(papszTokens);
 }
 
@@ -265,13 +265,13 @@ CPLErr GDALWMSMiniDriver_TiledWMS::Initialize(CPLXMLNode *config) {
 
         m_end_url = CPLGetXMLValue(config,"AdditionalArgs","");
         m_base_url = CPLGetXMLValue(config, "ServerURL", "");
-        if ('\0' == m_base_url[0]) {
+        if (m_base_url.empty()) {
             CPLError(ret=CE_Failure, CPLE_AppDefined, "GDALWMS, WMS mini-driver: ServerURL missing.");
             break;
         }
 
         m_tiledGroupName = CPLGetXMLValue(config, "TiledGroupName", "");
-        if ('\0' == m_tiledGroupName[0]) {
+        if (m_tiledGroupName.empty()) {
             CPLError(ret=CE_Failure, CPLE_AppDefined, "GDALWMS, Tiled WMS: TiledGroupName missing.");
             break;
         }
@@ -281,7 +281,7 @@ CPLErr GDALWMSMiniDriver_TiledWMS::Initialize(CPLXMLNode *config) {
         TG=CPLSearchXMLNode(config, "Change");
         while(TG!=NULL) {
             CPLString name=CPLGetXMLValue(TG,"key","");
-            if (name[0]!='\0') {
+            if (!name.empty()) {
                 CPLString value=CPLGetXMLValue(TG,"","");
                 substs=CSLSetNameValue(substs,name,value);
             } else {
@@ -314,7 +314,7 @@ CPLErr GDALWMSMiniDriver_TiledWMS::Initialize(CPLXMLNode *config) {
         }
     
         m_base_url=CPLGetXMLValue(tileServiceConfig,"TiledPatterns.OnlineResource.xlink:href","");
-        if (m_base_url[0]=='\0') {
+        if (m_base_url.empty()) {
             CPLError(ret=CE_Failure,CPLE_AppDefined, "GDALWMS, Tiled WMS: Can't locate OnlineResource in the server response.");
             break;
         }
@@ -382,10 +382,10 @@ CPLErr GDALWMSMiniDriver_TiledWMS::Initialize(CPLXMLNode *config) {
 
                 mbsx=atoi(CSLFetchNameValue(papszTokens,"WIDTH"));
                 mbsy=atoi(CSLFetchNameValue(papszTokens,"HEIGHT"));
-                if ('\0'==m_projection_wkt[0]) {
+                if (m_projection_wkt.empty()) {
                     const char* pszSRS = CSLFetchNameValue(papszTokens,"SRS");
                     m_projection_wkt = (pszSRS) ? pszSRS : "";
-                    if ('\0'!=m_projection_wkt[0])
+                    if (m_projection_wkt.empty())
                         m_projection_wkt=ProjToWKT(m_projection_wkt);
                 }
 

commit b0c25f1908df1674353e332a04123781a82f8669

TiledWMS: fix logic error introduced in r21728 (https://trac.osgeo.org/gdal/ticket/3493)
git-svn-id: https://svn.osgeo.org/gdal/trunk@21852 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/frmts/wms/minidriver_tiled_wms.cpp
@@ -383,7 +383,7 @@ CPLErr GDALWMSMiniDriver_TiledWMS::Initialize(CPLXMLNode *config) {
                 if (m_projection_wkt.empty()) {
                     const char* pszSRS = CSLFetchNameValue(papszTokens,"SRS");
                     m_projection_wkt = (pszSRS) ? pszSRS : "";
-                    if (m_projection_wkt.empty())
+                    if (!m_projection_wkt.empty())
                         m_projection_wkt=ProjToWKT(m_projection_wkt);
                 }
 