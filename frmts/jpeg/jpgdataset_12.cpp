8af0e445f52c2794733c913e1e1090ddc82fb051
Even Rouault <even.rouault@mines-paris.org>
2011-03-06 16:57:55 +0000

    Make GIF, JPEG and PNG drivers return a non NULL dataset when outputing to /vsistdout/
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@21907 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/jpeg/jpgdataset_12.cpp
+++ frmts/jpeg/jpgdataset_12.cpp
@@ -3,11 +3,22 @@
 #define JPGDataset        JPGDataset12
 #define JPGRasterBand     JPGRasterBand12
 #define JPGMaskBand       JPGMaskBand12
-#define JPEGCreateCopy    JPEGCreateCopy12
 #include "jpgdataset.cpp"
 
 GDALDataset* JPEGDataset12Open(GDALOpenInfo* poOpenInfo)
 {
     return JPGDataset12::Open(poOpenInfo);
 }
+
+GDALDataset* JPEGDataset12CreateCopy( const char * pszFilename,
+                                    GDALDataset *poSrcDS,
+                                    int bStrict, char ** papszOptions,
+                                    GDALProgressFunc pfnProgress,
+                                    void * pProgressData )
+{
+    return JPGDataset12::CreateCopy(pszFilename, poSrcDS,
+                                    bStrict, papszOptions,
+                                    pfnProgress, pProgressData);
+}
+
 #endif /* defined(JPEG_DUAL_MODE_8_12) */
