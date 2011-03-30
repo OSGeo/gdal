264d63882be54acb4266befc0e4e861188be2899
Frank Warmerdam <warmerdam@pobox.com>
2011-03-30 15:00:46 +0000

    rework attribute section size hack to be safer (https://trac.osgeo.org/gdal/ticket/4020)
    git-svn-id: https://svn.osgeo.org/gdal/branches/1.8@22076 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/nitf/nitfimage.c
+++ gdal/frmts/nitf/nitfimage.c
@@ -2754,8 +2754,6 @@ static void NITFLoadAttributeSection( NITFImage *psImage )
         {
             nASSOffset = psImage->pasLocations[i].nLocOffset;
             nASSSize = psImage->pasLocations[i].nLocSize;
-            if (i + 1 < psImage->nLocCount)
-                nNextOffset = psImage->pasLocations[i + 1].nLocOffset;
         }
     }
 
@@ -2771,6 +2769,9 @@ static void NITFLoadAttributeSection( NITFImage *psImage )
     CPL_MSBPTR16( &nAttrCount );
 
     
+/* -------------------------------------------------------------------- */
+/*      nASSSize Hack                                                   */
+/* -------------------------------------------------------------------- */
     /* OK, now, as often with RPF/CADRG, here is the necessary dirty hack */
     /* -- Begin of lengthy explanation -- */
     /* A lot of CADRG files have a nASSSize value that reports a size */
@@ -2787,11 +2788,24 @@ static void NITFLoadAttributeSection( NITFImage *psImage )
     /* I have observed that nowhere in the NITF driver we make use of the .nLocSize field */
     /* -- End of lengthy explanation -- */
 
+    for( i = 0; i < psImage->nLocCount; i++ )
+    {
+        if( psImage->pasLocations[i].nLocOffset > nASSOffset )
+        {
+            if( nNextOffset == 0 
+                || nNextOffset > psImage->pasLocations[i].nLocOffset )
+                nNextOffset = psImage->pasLocations[i].nLocOffset;
+        }
+    }
+
     if (nNextOffset > 0 && nNextOffset - nASSOffset > nASSSize)
         nASSSize = nNextOffset - nASSOffset;
 
-    /* Be sure that the attribute subsection is large enough to hold the */
-    /* offset table (otherwise NITFFetchAttribute coud read out of the buffer) */
+/* -------------------------------------------------------------------- */
+/*      Be sure that the attribute subsection is large enough to        */
+/*      hold the offset table (otherwise NITFFetchAttribute coud        */
+/*      read out of the buffer)                                         */
+/* -------------------------------------------------------------------- */
     if (nASSSize < 8 * nAttrCount)
     {
         CPLError( CE_Warning, CPLE_AppDefined,
@@ -2811,7 +2825,7 @@ static void NITFLoadAttributeSection( NITFImage *psImage )
                   nASSSize );
         return;
     }
-
+    
     VSIFSeekL( psImage->psFile->fp, nASSOffset, SEEK_SET );
     VSIFReadL( pabyAttributeSubsection, nASSSize, 1, psImage->psFile->fp );
 
