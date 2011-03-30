cc1f25ac8e0951d12910871bfef3f910dd7dbdf1
Frank Warmerdam <warmerdam@pobox.com>
2011-03-30 21:01:13 +0000

    move GDALFindAssociatedFile out of CPL_C_START/CPL_C_END block to avoid warning
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@22078 f0d54148-0727-0410-94bb-9a71ac55c965

--- gcore/gdal_priv.h
+++ gcore/gdal_priv.h
@@ -920,9 +920,9 @@ int GDALReadTabFile2( const char * pszBaseFilename,
                       int *pnGCPCount, GDAL_GCP **ppasGCPs,
                       char** papszSiblingFiles, char** ppszTabFileNameOut );
 
+CPL_C_END
+
 CPLString GDALFindAssociatedFile( const char *pszBasename, const char *pszExt,
                                   char **papszSiblingFiles, int nFlags );
-
-CPL_C_END
 
 #endif /* ndef GDAL_PRIV_H_INCLUDED */
