917b711a30c8793ae2e04f5949f09c6150eaf934
Even Rouault <even.rouault@mines-paris.org>
2011-04-14 20:22:35 +0000

    gdalwarp: take into account memory needed by DstDensity float mask when computing chunk dimensions (https://trac.osgeo.org/gdal/ticket/4042)
    git-svn-id: https://svn.osgeo.org/gdal/branches/1.8@22162 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/alg/gdalwarpoperation.cpp
+++ gdal/alg/gdalwarpoperation.cpp
@@ -990,8 +990,15 @@ CPLErr GDALWarpOperation::CollectChunkList(
     if( psOptions->pfnSrcDensityMaskFunc != NULL )
         nSrcPixelCostInBits += 32; /* ?? float mask */
 
+    GDALRasterBandH hSrcBand = NULL;
+    if( psOptions->nBandCount > 0 )
+        hSrcBand = GDALGetRasterBand(psOptions->hSrcDS,
+                                     psOptions->panSrcBands[0]);
+
     if( psOptions->nSrcAlphaBand > 0 || psOptions->hCutline != NULL )
         nSrcPixelCostInBits += 32; /* UnifiedSrcDensity float mask */
+    else if (hSrcBand != NULL && (GDALGetMaskFlags(hSrcBand) & GMF_PER_DATASET))
+        nSrcPixelCostInBits += 1; /* UnifiedSrcValid bit mask */
 
     if( psOptions->papfnSrcPerBandValidityMaskFunc != NULL 
         || psOptions->padfSrcNoDataReal != NULL )
@@ -1015,6 +1022,9 @@ CPLErr GDALWarpOperation::CollectChunkList(
     if( psOptions->padfDstNoDataReal != NULL
         || psOptions->pfnDstValidityMaskFunc != NULL )
         nDstPixelCostInBits += psOptions->nBandCount;
+
+    if( psOptions->nDstAlphaBand > 0 )
+        nDstPixelCostInBits += 32; /* DstDensity float mask */
 
 /* -------------------------------------------------------------------- */
 /*      Does the cost of the current rectangle exceed our memory        */
