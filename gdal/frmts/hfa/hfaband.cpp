488b59f7ff1c11b15d2ec636930440fe9d31e4ba
Frank Warmerdam <warmerdam@pobox.com>
2011-04-19 19:18:27 +0000

    We want one bit null blocks to default to 1 in ESRI_BUILDs (ESRI-MERGE)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@22213 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/hfa/hfaband.cpp
+++ gdal/frmts/hfa/hfaband.cpp
@@ -974,15 +974,24 @@ static CPLErr UncompressBlock( GByte *pabyCData, int nSrcBytes,
 void HFABand::NullBlock( void *pData )
 
 {
+    int nChunkSize = MAX(1,HFAGetDataTypeBits(nDataType)/8);
+    int nWords = nBlockXSize * nBlockYSize;
+
     if( !bNoDataSet )
-        memset( pData, 0, 
-                HFAGetDataTypeBits(nDataType)*nBlockXSize*nBlockYSize/8 );
+    {
+#ifdef ESRI_BUILD
+        // We want special defaulting for 1 bit data in ArcGIS.
+        if ( nDataType >= EPT_u2 )
+            memset( pData,   0, nChunkSize*nWords );
+        else
+            memset( pData, 255, nChunkSize*nWords );
+#else
+        memset( pData,   0, nChunkSize*nWords );
+#endif
+    }
     else
-            
     {
         double adfND[2];
-        int nChunkSize = MAX(1,HFAGetDataTypeBits(nDataType)/8);
-        int nWords = nBlockXSize * nBlockYSize;
         int i;
 
         switch( nDataType )
