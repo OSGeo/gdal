a74b3c0a56666ee054737e710487028a93a5550d
Frank Warmerdam <warmerdam@pobox.com>
2011-04-30 02:51:52 +0000

    preliminary support for creating NSTRING fields if -lso USE_NSTRING=YES is given (https://trac.osgeo.org/gdal/ticket/4053)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@22263 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/ogrsf_frmts/sde/ogr_sde.h
+++ ogr/ogrsf_frmts/sde/ogr_sde.h
@@ -88,6 +88,8 @@ class OGRSDELayer : public OGRLayer
 
     int                 iShapeColumn;
 
+    int                 bUseNSTRING; 
+
 
     char              **papszAllColumns;
     std::vector<int>    anFieldMap;     // SDE index of OGR field.
@@ -146,6 +148,8 @@ class OGRSDELayer : public OGRLayer
                                 { nFIDColumnType = nType; }
     void                SetPrecisionFlag( int bFlag )
                                 { bPreservePrecision = bFlag; }
+    void                SetUseNSTRING( int bFlag )
+                                { bUseNSTRING = bFlag; }
 };
 
 /************************************************************************/
