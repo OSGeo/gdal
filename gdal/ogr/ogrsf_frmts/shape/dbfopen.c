200d9a4d545c4e0649d3f4d6ce8a9b67b21e4763
Frank Warmerdam <warmerdam@pobox.com>
2011-05-07 22:41:59 +0000

    update from upstream for native add field record flushing fix (#4073)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@22328 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/shape/dbfopen.c
+++ gdal/ogr/ogrsf_frmts/shape/dbfopen.c
@@ -1,5 +1,5 @@
 /******************************************************************************
- * $Id: dbfopen.c,v 1.86 2011-04-17 15:15:29 fwarmerdam Exp $
+ * $Id: dbfopen.c,v 1.87 2011-05-07 22:41:02 fwarmerdam Exp $
  *
  * Project:  Shapelib
  * Purpose:  Implementation of .dbf access API documented in dbf_api.html.
@@ -34,6 +34,9 @@
  ******************************************************************************
  *
  * $Log: dbfopen.c,v $
+ * Revision 1.87  2011-05-07 22:41:02  fwarmerdam
+ * ensure pending record is flushed when adding a native field (GDAL #4073)
+ *
  * Revision 1.86  2011-04-17 15:15:29  fwarmerdam
  * Removed unused variable.
  *
@@ -154,7 +157,7 @@
 #include <ctype.h>
 #include <string.h>
 
-SHP_CVSID("$Id: dbfopen.c,v 1.86 2011-04-17 15:15:29 fwarmerdam Exp $")
+SHP_CVSID("$Id: dbfopen.c,v 1.87 2011-05-07 22:41:02 fwarmerdam Exp $")
 
 #ifndef FALSE
 #  define FALSE		0
@@ -801,6 +804,10 @@ DBFAddNativeFieldType(DBFHandle psDBF, const char * pszFieldName,
     char        *pszRecord;
     char        chFieldFill;
     SAOffset    nRecordOffset;
+
+    /* make sure that everything is written in .dbf */
+    if( !DBFFlushRecord( psDBF ) )
+        return -1;
 
 /* -------------------------------------------------------------------- */
 /*      Do some checking to ensure we can add records to this file.     */
