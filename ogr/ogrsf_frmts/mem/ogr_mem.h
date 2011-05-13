e8e7b161e51460edb2b0dc6eb6c645a8ede955eb
Even Rouault <even.rouault@mines-paris.org>
2011-05-13 18:00:27 +0000

    RFC35 : implementation in Shapefile, Memory and PG drivers (#2671)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@22369 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/ogrsf_frmts/mem/ogr_mem.h
+++ ogr/ogrsf_frmts/mem/ogr_mem.h
@@ -71,6 +71,9 @@ class OGRMemLayer : public OGRLayer
 
     virtual OGRErr      CreateField( OGRFieldDefn *poField,
                                      int bApproxOK = TRUE );
+    virtual OGRErr      DeleteField( int iField );
+    virtual OGRErr      ReorderFields( int* panMap );
+    virtual OGRErr      AlterFieldDefn( int iField, OGRFieldDefn* poNewFieldDefn, int nFlags );
 
     virtual OGRSpatialReference *GetSpatialRef();
     
