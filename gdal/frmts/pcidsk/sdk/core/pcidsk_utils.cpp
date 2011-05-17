a3e9a3a28f140601bdca521bd4a8c2ee6f18641c
Frank Warmerdam <warmerdam@pobox.com>
2011-05-17 20:51:08 +0000

    update
    git-svn-id: https://svn.osgeo.org/gdal/trunk@22393 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/pcidsk/sdk/core/pcidsk_utils.cpp
+++ gdal/frmts/pcidsk/sdk/core/pcidsk_utils.cpp
@@ -39,6 +39,10 @@
 
 using namespace PCIDSK;
 
+#if defined(_MSC_VER) && (_MSC_VER < 1500)
+#  define vsnprintf _vsnprintf
+#endif
+
 /************************************************************************/
 /*                         GetCurrentDateTime()                         */
 /************************************************************************/
