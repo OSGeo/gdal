fac50c4c241777e67a8252952e0910552416407f
Even Rouault <even.rouault@mines-paris.org>
2011-06-03 16:04:53 +0000

    PDF: support linking against podofo library (LGPL) instead of poppler --> however pdftoppm binary from poppler distribution is needed for rasterization
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@22491 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/pdf/pdfio.cpp
+++ frmts/pdf/pdfio.cpp
@@ -27,6 +27,8 @@
  * DEALINGS IN THE SOFTWARE.
  ****************************************************************************/
 
+#ifdef USE_POPPLER
+
 /* hack for PDF driver and poppler >= 0.15.0 that defines incompatible "typedef bool GBool" */
 /* in include/poppler/goo/gtypes.h with the one defined in cpl_port.h */
 #define CPL_GBOOL_DEFINED
@@ -305,3 +307,5 @@ void VSIPDFFileStream::moveStart(int delta)
     VSIFSeekL(f, nCurrentPos = nStart, SEEK_SET);
     nPosInBuffer = nBufferLength = -1;
 }
+
+#endif
