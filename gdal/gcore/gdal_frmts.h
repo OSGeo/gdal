a2dd442b6fdef8c78087e03bbeb5ceecbb4122cd
Even Rouault <even.rouault@mines-paris.org>
2011-07-02 20:20:19 +0000

    Add new GDAL driver for ZMap Plus Grid format (read/creation support)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@22634 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/gcore/gdal_frmts.h
+++ gdal/gcore/gdal_frmts.h
@@ -166,6 +166,7 @@ void CPL_DLL GDALRegister_CTG(void);
 void CPL_DLL GDALRegister_E00GRID(void);
 void CPL_DLL GDALRegister_SNODAS(void);
 void CPL_DLL GDALRegister_WEBP(void);
+void CPL_DLL GDALRegister_ZMap(void);
 CPL_C_END
 
 #endif /* ndef GDAL_FRMTS_H_INCLUDED */
