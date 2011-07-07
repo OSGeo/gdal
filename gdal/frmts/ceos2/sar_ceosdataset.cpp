52df0e4f17aa14080e28b8cf8fe3891e6e41285d
Frank Warmerdam <warmerdam@pobox.com>
2011-07-07 15:07:55 +0000

    add ceos record number sequence checking, backout quietly if the second imagery record is wrong (#4146)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@22662 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/ceos2/sar_ceosdataset.cpp
+++ gdal/frmts/ceos2/sar_ceosdataset.cpp
@@ -1612,13 +1612,11 @@ GDALDataset *SAR_CEOSDataset::Open( GDALOpenInfo * poOpenInfo )
 /* -------------------------------------------------------------------- */
 /*      Try to read the current file as an imagery file.                */
 /* -------------------------------------------------------------------- */
+    
     psVolume->ImagryOptionsFile = TRUE;
-    if( ProcessData( fp, __CEOS_IMAGRY_OPT_FILE, psVolume, 4, -1) )
+    if( ProcessData( fp, __CEOS_IMAGRY_OPT_FILE, psVolume, 4, -1) != CE_None )
     {
         delete poDS;
-        CPLError( CE_Failure, CPLE_OpenFailed, 
-                  "Corrupted or unknown CEOS format:\n%s", 
-                  poOpenInfo->pszFilename );
         return NULL;
     }
 
@@ -1996,6 +1994,7 @@ ProcessData( VSILFILE *fp, int fileid, CeosSARVolume_t *sar, int max_records,
     int                CurrentSequence = 0;
     Link_t             *TheLink;
     CeosRecord_t       *record;
+    int                iThisRecord = 0;
 
     while(max_records != 0 && max_bytes != 0)
     {
@@ -2003,6 +2002,25 @@ ProcessData( VSILFILE *fp, int fileid, CeosSARVolume_t *sar, int max_records,
         VSIFSeekL( fp, start, SEEK_SET );
         VSIFReadL( temp_buffer, 1, __CEOS_HEADER_LENGTH, fp );
         record->Length = DetermineCeosRecordBodyLength( temp_buffer );
+
+        iThisRecord++;
+        CeosToNative( &(record->Sequence), temp_buffer, 4, 4 );
+
+        if( iThisRecord != record->Sequence )
+        {
+            if( fileid == __CEOS_IMAGRY_OPT_FILE && iThisRecord == 2 )
+            {
+                CPLDebug( "SAR_CEOS", "Ignoring CEOS file with wrong second record sequence number - likely it has padded records." );
+                return CE_Warning;
+            }
+            else
+            {
+                CPLError( CE_Failure, CPLE_AppDefined, 
+                          "Corrupt CEOS File - got record seq# %d instead of the expected %d.",
+                          record->Sequence, iThisRecord );
+                return CE_Failure;
+            }
+        }
         
         if( record->Length > CurrentBodyLength )
         {
@@ -2053,7 +2071,7 @@ ProcessData( VSILFILE *fp, int fileid, CeosSARVolume_t *sar, int max_records,
 
     CPLFree(temp_body);
 
-    return 0;
+    return CE_None;
 }
 
 /************************************************************************/
