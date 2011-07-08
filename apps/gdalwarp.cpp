615ac5e73d4489c04e65f2d4002d5b82a84658fd
Even Rouault <even.rouault@mines-paris.org>
2011-07-08 17:15:19 +0000

    GCP refinement: compare directly against tolerance value (gcp_refinement_22669.diff, https://trac.osgeo.org/gdal/ticket/4143)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@22670 f0d54148-0727-0410-94bb-9a71ac55c965

--- apps/gdalwarp.cpp
+++ apps/gdalwarp.cpp
@@ -72,7 +72,7 @@ Usage:
 gdalwarp [--help-general] [--formats]
     [-s_srs srs_def] [-t_srs srs_def] [-to "NAME=VALUE"]
     [-order n] [-tps] [-rpc] [-geoloc] [-et err_threshold]
-    [-refine_gcps tolerance minimum_gcps]
+    [-refine_gcps tolerance [minimum_gcps]]
     [-te xmin ymin xmax ymax] [-tr xres yres] [-tap] [-ts width height]
     [-wo "NAME=VALUE"] [-ot Byte/Int16/...] [-wt Byte/Int16]
     [-srcnodata "value [value...]"] [-dstnodata "value [value...]"] -dstalpha
@@ -232,7 +232,7 @@ static void Usage()
         "Usage: gdalwarp [--help-general] [--formats]\n"
         "    [-s_srs srs_def] [-t_srs srs_def] [-to \"NAME=VALUE\"]\n"
         "    [-order n] [-tps] [-rpc] [-geoloc] [-et err_threshold]\n"
-        "    [-refine_gcps tolerance minimum_gcps]\n"
+        "    [-refine_gcps tolerance [minimum_gcps]]\n"
         "    [-te xmin ymin xmax ymax] [-tr xres yres] [-tap] [-ts width height]\n"
         "    [-wo \"NAME=VALUE\"] [-ot Byte/Int16/...] [-wt Byte/Int16]\n"
         "    [-srcnodata \"value [value...]\"] [-dstnodata \"value [value...]\"] -dstalpha\n" 
