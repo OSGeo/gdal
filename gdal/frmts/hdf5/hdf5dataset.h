8fcfc7a9c69e4d6a8f7c298e06dc9ac9365df3f2
Antonio Valentino <antonio.valentino@tiscali.it>
2011-09-04 10:28:41 +0000

    Convert tabs to spaces
    git-svn-id: https://svn.osgeo.org/gdal/trunk@23035 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/hdf5/hdf5dataset.h
+++ gdal/frmts/hdf5/hdf5dataset.h
@@ -34,14 +34,14 @@
 #include "cpl_list.h"
 
 typedef struct HDF5GroupObjects {
-  char	       *pszName;
+  char         *pszName;
   char         *pszPath;
   char         *pszUnderscorePath;
   char         *pszTemp;
-  int		nType;
-  int		nIndex;
-  hsize_t	nbObjs;
-  int		nbAttrs;
+  int           nType;
+  int           nIndex;
+  hsize_t       nbObjs;
+  int           nbAttrs;
   int           nRank;
   hsize_t       *paDims;
   hid_t          native;
@@ -56,14 +56,14 @@ herr_t HDF5CreateGroupObjs(hid_t, const char *,void *);
 
 /************************************************************************/
 /* ==================================================================== */
-/*				HDF5Dataset				*/
+/*                              HDF5Dataset                             */
 /* ==================================================================== */
 /************************************************************************/
 class HDF5Dataset : public GDALPamDataset
 {
   protected:
 
-  hid_t            hHDF5;     
+  hid_t            hHDF5;
   hid_t            hDatasetID;
   hid_t            hGroupID; /* H handler interface */
   char             **papszSubDatasets;
@@ -82,18 +82,18 @@ class HDF5Dataset : public GDALPamDataset
   HDF5GroupObjects* HDF5FindDatasetObjectsbyPath( HDF5GroupObjects *, const char * );
   char* CreatePath(HDF5GroupObjects *);
   void DestroyH5Objects(HDF5GroupObjects *);
- 
+
   GDALDataType GetDataType(hid_t);
   const char * GetDataTypeName(hid_t);
 
   public:
 
   char	           **papszMetadata;
-  HDF5GroupObjects *poH5CurrentObject; 
+  HDF5GroupObjects *poH5CurrentObject;
 
   HDF5Dataset();
   ~HDF5Dataset();
-    
+
   static GDALDataset *Open(GDALOpenInfo *);
   static int Identify(GDALOpenInfo *);
 };
