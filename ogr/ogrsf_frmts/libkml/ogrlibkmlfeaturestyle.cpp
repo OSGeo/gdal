OSGeo/gdal

commit 0891bc7e6d9da9e8f8b382d9da84a955bad8862b

make libkml driver combine styles from the style table and features styles when LIBKML_RESOLVE_STYLE=YES  #4231
git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@23055 f0d54148-0727-0410-94bb-9a71ac55c965

ogr/ogrsf_frmts/libkml/ogrlibkmlfeaturestyle.cpp
@@ -194,6 +194,7 @@ void featurestyle2kml (
     }
 }
 
+
 /******************************************************************************
  function to read a kml style into ogr's featurestyle
 ******************************************************************************/
@@ -205,18 +206,9 @@ void kml2featurestyle (
     OGRFeature * poOgrFeat )
 {
 
-    /***** does the placemark have a styleselector and a style url? *****/
+    /***** does the placemark have a style url? *****/
 
-    if ( poKmlPlacemark->has_styleselector (  )
-         && poKmlPlacemark->has_styleurl (  ) ) {
-
-        /* todo do the style and styleurl part */
-
-    }
-
-    /***** is the style a style url *****/
-
-    else if ( poKmlPlacemark->has_styleurl (  ) ) {
+    if (    poKmlPlacemark->has_styleselector (  ) ) {
 
         const string poKmlStyleUrl = poKmlPlacemark->get_styleurl (  );
 
@@ -227,16 +219,18 @@ void kml2featurestyle (
         OGRStyleTable *poOgrSTBLLayer;
         const char *pszTest = NULL;
 
-        if ( *pszUrl == '#'
-             && ( poOgrSTBLLayer = poOgrLayer->GetStyleTable (  ) ) ) {
+        /***** is it a layer style ? *****/
 
-            pszTest = poOgrSTBLLayer->Find ( pszUrl + 1 );
+        if ( *pszUrl == '#'
+             && ( poOgrSTBLLayer = poOgrLayer->GetStyleTable (  ) ) )
+        {
+             pszTest = poOgrSTBLLayer->Find ( pszUrl + 1 );
         }
-        
+
         if ( pszTest ) {
 
             /***** should we resolve the style *****/
-            
+
             const char *pszResolve = CPLGetConfigOption ( "LIBKML_RESOLVE_STYLE", "no" );
 
             if (EQUAL(pszResolve, "yes")) {
@@ -254,26 +248,25 @@ void kml2featurestyle (
 
         }
 
-        /***** not a layer style *****/
+        /***** is it a dataset style? *****/
         
-
         else {
-
-            /***** is it a dataset style? *****/
 
             int nPathLen = strlen ( poOgrDS->GetStylePath (  ) );
 
-            if ( nPathLen == 0 || EQUALN ( pszUrl, poOgrDS->GetStylePath (  ), nPathLen )) {
-
+            if (    nPathLen == 0
+                 || EQUALN ( pszUrl, poOgrDS->GetStylePath (  ), nPathLen ))
+            {
 
                 /***** should we resolve the style *****/
-            
+
                 const char *pszResolve = CPLGetConfigOption ( "LIBKML_RESOLVE_STYLE", "no" );
 
-                if ( EQUAL(pszResolve, "yes") &&
-                     ( poOgrSTBLLayer = poOgrDS->GetStyleTable (  ) ) &&
-                     ( pszTest = poOgrSTBLLayer->Find ( pszUrl + nPathLen + 1) )
-                    ) {
+                if (    EQUAL(pszResolve, "yes")
+                     && ( poOgrSTBLLayer = poOgrDS->GetStyleTable (  ) )
+                     && ( pszTest = poOgrSTBLLayer->Find ( pszUrl + nPathLen + 1) )
+                   )
+                {
 
                     poOgrFeat->SetStyleString ( pszTest );
                 }
@@ -283,10 +276,9 @@ void kml2featurestyle (
                     pszUrl[nPathLen] = '@';
                     poOgrFeat->SetStyleString ( pszUrl + nPathLen );
                 }
-
-                
+       
             }
-        
+            
             /**** its someplace else *****/
 
             else {
@@ -300,7 +292,7 @@ void kml2featurestyle (
                     char *pszUrlTmp = CPLStrdup(pszUrl);
                     char *pszPound;
                     if ((pszPound = strchr(pszUrlTmp, '#'))) {
-
+                        
                         *pszPound = '\0';
                     }
 
@@ -308,12 +300,11 @@ void kml2featurestyle (
 
                     VSILFILE *fp = NULL;
 
-                    if ((fp = VSIFOpenL( CPLFormFilename("/vsicurl/",
-                                                        pszUrlTmp,
-                                                        NULL),
-                                        "r" )) ||
-                        (fp = VSIFOpenL( pszUrlTmp,
-                                        "r" )))
+                    if (    (fp = VSIFOpenL( CPLFormFilename( "/vsicurl/",
+                                                              pszUrlTmp,
+                                                              NULL),
+                                             "r" ))
+                        ||  (fp = VSIFOpenL( pszUrlTmp, "r" )))
                     {
 
                         char szbuf[1025];
@@ -322,19 +313,19 @@ void kml2featurestyle (
                         /***** loop, read and copy to a string *****/
 
                         size_t nRead;
-                        
+                    
                         do {
-
+                            
                             nRead = VSIFReadL(szbuf, 1, sizeof(szbuf) - 1, fp);
                             
                             if (nRead == 0)
                                 break;
-                            
+
                             /***** copy buf to the string *****/
-                            
+
                             szbuf[nRead] = '\0';
                             oStyle.append( szbuf );
-    
+
                         } while (!VSIFEofL(fp));
 
                         VSIFCloseL(fp);
@@ -348,14 +339,13 @@ void kml2featurestyle (
                                               poOgrLayer,
                                               poOgrFeat );
                         }
-                        
+
                         else {
 
                             /***** if failed just store the url *****/
-                            
+
                             poOgrFeat->SetStyleString ( pszUrl );
                         }
-
                     }
                     CPLFree(pszUrlTmp);
                 }
@@ -373,7 +363,7 @@ void kml2featurestyle (
 
     /***** does the placemark have a style selector *****/
 
-    else if ( poKmlPlacemark->has_styleselector (  ) ) {
+   if ( poKmlPlacemark->has_styleselector (  ) ) {
 
         StyleSelectorPtr poKmlStyleSelector =
             poKmlPlacemark->get_styleselector (  );
@@ -385,7 +375,19 @@ void kml2featurestyle (
 
             OGRStyleMgr *poOgrSM = new OGRStyleMgr;
 
-            poOgrSM->InitStyleString ( NULL );
+            /***** if were resolveing style the feature  *****/
+            /***** might already have styling to add too *****/
+            
+            const char *pszResolve = CPLGetConfigOption ( "LIBKML_RESOLVE_STYLE", "no" );
+            if (EQUAL(pszResolve, "yes")) {
+                 poOgrSM->InitFromFeature ( poOgrFeat );
+            }
+            else {
+
+                /***** if featyurestyle gets a name tool this needs changed to the above *****/
+                
+                poOgrSM->InitStyleString ( NULL );
+            }
 
             /***** read the style *****/
 

ogr/ogrsf_frmts/libkml/ogrlibkmlstyle.cpp
@@ -353,28 +353,32 @@ void addstylestring2kml (
 ******************************************************************************/
 
 OGRStylePen *kml2pen (
-    LineStylePtr poKmlLineStyle );
+    LineStylePtr poKmlLineStyle,
+    OGRStylePen *poOgrStylePen);
 
 /******************************************************************************
  kml2brush
 ******************************************************************************/
 
 OGRStyleBrush *kml2brush (
-    PolyStylePtr poKmlPolyStyle );
+    PolyStylePtr poKmlPolyStyle,
+    OGRStyleBrush *poOgrStyleBrush);
 
 /******************************************************************************
- kml2brush
+ kml2symbol
 ******************************************************************************/
 
 OGRStyleSymbol *kml2symbol (
-    IconStylePtr poKmlIconStyle );
+    IconStylePtr poKmlIconStyle,
+    OGRStyleSymbol *poOgrStyleSymbol);
 
 /******************************************************************************
  kml2label
 ******************************************************************************/
 
 OGRStyleLabel *kml2label (
-    LabelStylePtr poKmlLabelStyle );
+    LabelStylePtr poKmlLabelStyle,
+    OGRStyleLabel *poOgrStyleLabel);
 
 /******************************************************************************
  kml2stylemgr
@@ -383,54 +387,146 @@ OGRStyleLabel *kml2label (
 void kml2stylestring (
     StylePtr poKmlStyle,
     OGRStyleMgr * poOgrSM )
+
 {
 
+    OGRStyleMgr * poOgrNewSM ;
+    OGRStyleTool *poOgrST = NULL;
+    OGRStyleTool *poOgrTmpST = NULL;
+    int i;
+
+    poOgrNewSM = new OGRStyleMgr( NULL );
+    
     /***** linestyle / pen *****/
 
     if ( poKmlStyle->has_linestyle (  ) ) {
+
+        poOgrNewSM->InitStyleString ( NULL );
+        
         LineStylePtr poKmlLineStyle = poKmlStyle->get_linestyle (  );
 
-        OGRStylePen *poOgrStylePen = kml2pen ( poKmlLineStyle );
+        poOgrTmpST = NULL;
+        for ( i = 0; i < poOgrSM->GetPartCount ( NULL ); i++ ) {
+            poOgrST = poOgrSM->GetPart ( i, NULL );
 
-        poOgrSM->AddPart ( poOgrStylePen );
+            if ( !poOgrST )
+                continue;
+        
+            if ( poOgrST->GetType ( ) == OGRSTCPen ) {
+                poOgrTmpST = poOgrST;
+            }
+            else {
+                poOgrNewSM->AddPart ( poOgrST );
+            }
+        }
+        
+        OGRStylePen *poOgrStylePen = kml2pen ( poKmlLineStyle,
+                                               ( OGRStylePen *) poOgrTmpST);
+        
+        poOgrNewSM->AddPart ( poOgrStylePen );
 
         delete poOgrStylePen;
+        poOgrSM->InitStyleString ( poOgrNewSM->GetStyleString(NULL) );
+        
     }
 
     /***** polystyle / brush *****/
 
     if ( poKmlStyle->has_polystyle (  ) ) {
+
+        poOgrNewSM->InitStyleString ( NULL );
+
         PolyStylePtr poKmlPolyStyle = poKmlStyle->get_polystyle (  );
 
-        OGRStyleBrush *poOgrStyleBrush = kml2brush ( poKmlPolyStyle );
+        poOgrTmpST = NULL;
+        for ( i = 0; i < poOgrSM->GetPartCount ( NULL ); i++ ) {
+            poOgrST = poOgrSM->GetPart ( i, NULL );
 
-        poOgrSM->AddPart ( poOgrStyleBrush );
+            if ( !poOgrST )
+                continue;
+        
+            if ( poOgrST->GetType ( ) == OGRSTCBrush ) {
+                poOgrTmpST = poOgrST;
+            }
+            else {
+                poOgrNewSM->AddPart ( poOgrST );
+            }
+        }
+
+        OGRStyleBrush *poOgrStyleBrush = kml2brush ( poKmlPolyStyle,
+                                                     ( OGRStyleBrush *) poOgrTmpST );
+
+        poOgrNewSM->AddPart ( poOgrStyleBrush );
 
         delete poOgrStyleBrush;
+        poOgrSM->InitStyleString ( poOgrNewSM->GetStyleString(NULL) );
+
     }
 
     /***** iconstyle / symbol *****/
 
     if ( poKmlStyle->has_iconstyle (  ) ) {
+        
+        poOgrNewSM->InitStyleString ( NULL );
+
         IconStylePtr poKmlIconStyle = poKmlStyle->get_iconstyle (  );
 
-        OGRStyleSymbol *poOgrStyleSymbol = kml2symbol ( poKmlIconStyle );
+        poOgrTmpST = NULL;
+        for ( i = 0; i < poOgrSM->GetPartCount ( NULL ); i++ ) {
+            poOgrST = poOgrSM->GetPart ( i, NULL );
 
-        poOgrSM->AddPart ( poOgrStyleSymbol );
+            if ( !poOgrST )
+                continue;
+        
+            if ( poOgrST->GetType ( ) == OGRSTCSymbol ) {
+                poOgrTmpST = poOgrST;
+            }
+            else {
+                poOgrNewSM->AddPart ( poOgrST );
+            }
+        }
+
+        OGRStyleSymbol *poOgrStyleSymbol = kml2symbol ( poKmlIconStyle,
+                                                     ( OGRStyleSymbol *) poOgrTmpST );
+
+        poOgrNewSM->AddPart ( poOgrStyleSymbol );
 
         delete poOgrStyleSymbol;
+        poOgrSM->InitStyleString ( poOgrNewSM->GetStyleString(NULL) );
+
     }
 
     /***** labelstyle / label *****/
 
     if ( poKmlStyle->has_labelstyle (  ) ) {
+        
+        poOgrNewSM->InitStyleString ( NULL );
+
         LabelStylePtr poKmlLabelStyle = poKmlStyle->get_labelstyle (  );
 
-        OGRStyleLabel *poOgrStyleLabel = kml2label ( poKmlLabelStyle );
+        poOgrTmpST = NULL;
+        for ( i = 0; i < poOgrSM->GetPartCount ( NULL ); i++ ) {
+            poOgrST = poOgrSM->GetPart ( i, NULL );
 
-        poOgrSM->AddPart ( poOgrStyleLabel );
+            if ( !poOgrST )
+                continue;
+        
+            if ( poOgrST->GetType ( ) == OGRSTCLabel ) {
+                poOgrTmpST = poOgrST;
+            }
+            else {
+                poOgrNewSM->AddPart ( poOgrST );
+            }
+        }
+
+        OGRStyleLabel *poOgrStyleLabel = kml2label ( poKmlLabelStyle,
+                                                     ( OGRStyleLabel *) poOgrTmpST );
+
+        poOgrNewSM->AddPart ( poOgrStyleLabel );
 
         delete poOgrStyleLabel;
+        poOgrSM->InitStyleString ( poOgrNewSM->GetStyleString(NULL) );
+
     }
 
 }
@@ -442,10 +538,12 @@ void kml2stylestring (
 ******************************************************************************/
 
 OGRStylePen *kml2pen (
-    LineStylePtr poKmlLineStyle )
+    LineStylePtr poKmlLineStyle,
+    OGRStylePen *poOgrStylePen)
 {
 
-    OGRStylePen *poOgrStylePen = new OGRStylePen (  );
+    if (!poOgrStylePen)
+        poOgrStylePen = new OGRStylePen (  );
 
     /***** <LineStyle> should always have a width in pixels *****/
     
@@ -476,10 +574,12 @@ OGRStylePen *kml2pen (
 ******************************************************************************/
 
 OGRStyleBrush *kml2brush (
-    PolyStylePtr poKmlPolyStyle )
+    PolyStylePtr poKmlPolyStyle,
+    OGRStyleBrush *poOgrStyleBrush)
 {
 
-    OGRStyleBrush *poOgrStyleBrush = new OGRStyleBrush (  );
+    if (!poOgrStyleBrush)
+        poOgrStyleBrush = new OGRStyleBrush (  );
 
     /***** color *****/
 
@@ -501,10 +601,12 @@ OGRStyleBrush *kml2brush (
 ******************************************************************************/
 
 OGRStyleSymbol *kml2symbol (
-    IconStylePtr poKmlIconStyle )
+    IconStylePtr poKmlIconStyle,
+    OGRStyleSymbol *poOgrStyleSymbol)
 {
 
-    OGRStyleSymbol *poOgrStyleSymbol = new OGRStyleSymbol (  );
+    if (!poOgrStyleSymbol)
+        poOgrStyleSymbol = new OGRStyleSymbol (  );
 
     /***** id (kml icon) *****/
 
@@ -562,10 +664,12 @@ OGRStyleSymbol *kml2symbol (
 ******************************************************************************/
 
 OGRStyleLabel *kml2label (
-    LabelStylePtr poKmlLabelStyle )
+    LabelStylePtr poKmlLabelStyle,
+    OGRStyleLabel *poOgrStyleLabel)
 {
 
-    OGRStyleLabel *poOgrStyleLabel = new OGRStyleLabel (  );
+    if (!poOgrStyleLabel)
+        poOgrStyleLabel = new OGRStyleLabel (  );
 
     /***** color *****/
 

commit 126df7af133d77bb515d1004034ddda2b62dc8de

LIBKML: fix memory leaks introduced in r23055, and an older one (https://trac.osgeo.org/gdal/ticket/4231)
git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@23061 f0d54148-0727-0410-94bb-9a71ac55c965

ogr/ogrsf_frmts/libkml/ogrlibkmlfeaturestyle.cpp
@@ -356,8 +356,8 @@ void kml2featurestyle (
                 }
             }
 
-            CPLFree ( pszUrl );
         }
+        CPLFree ( pszUrl );
 
     }
 

ogr/ogrsf_frmts/libkml/ogrlibkmlstyle.cpp
@@ -417,6 +417,7 @@ void kml2stylestring (
             }
             else {
                 poOgrNewSM->AddPart ( poOgrST );
+                delete poOgrST;
             }
         }
         
@@ -450,6 +451,7 @@ void kml2stylestring (
             }
             else {
                 poOgrNewSM->AddPart ( poOgrST );
+                delete poOgrST;
             }
         }
 
@@ -483,6 +485,7 @@ void kml2stylestring (
             }
             else {
                 poOgrNewSM->AddPart ( poOgrST );
+                delete poOgrST;
             }
         }
 
@@ -516,6 +519,7 @@ void kml2stylestring (
             }
             else {
                 poOgrNewSM->AddPart ( poOgrST );
+                delete poOgrST;
             }
         }
 
@@ -528,6 +532,8 @@ void kml2stylestring (
         poOgrSM->InitStyleString ( poOgrNewSM->GetStyleString(NULL) );
 
     }
+
+    delete poOgrNewSM;
 
 }
 