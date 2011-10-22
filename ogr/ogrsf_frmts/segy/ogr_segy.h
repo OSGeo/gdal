fc38e5d6c65e553a77ef3a1a5de0638179eb11e0
Even Rouault <even.rouault@mines-paris.org>
2011-10-22 09:50:46 +0000

    Add new OGR driver to read files in SEG-Y format
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@23268 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ ogr/ogrsf_frmts/segy/ogr_segy.h
@@ -0,0 +1,179 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  SEG-Y Translator
+ * Purpose:  Definition of classes for OGR SEG-Y driver.
+ * Author:   Even Rouault, even dot rouault at mines dash paris dot org
+ *
+ ******************************************************************************
+ * Copyright (c) 2011, Even Rouault <even dot rouault at mines dash paris dot org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMSEGYS OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+
+#ifndef _OGR_SEGY_H_INCLUDED
+#define _OGR_SEGY_H_INCLUDED
+
+#include "ogrsf_frmts.h"
+
+GInt16 SEGYReadMSBInt16(const GByte* pabyVal);
+GInt32 SEGYReadMSBInt32(const GByte* pabyVal);
+
+typedef struct
+{
+    int nJobIdNumber;
+    int nLineNumber;
+    int nReelNumber;
+    int nDataTracesPerEnsemble;
+    int nAuxTracesPerEnsemble;
+    int nSampleInterval;
+    int nSampleIntervalOriginal;
+    int nSamplesPerDataTrace;
+    int nSamplesPerDataTraceOriginal;
+    int nDataSampleType;
+    int nEnsembleFold;
+    int nTraceSortingCode;
+    int nVerticalSumCode;
+    int nSweepFrequencyAtStart;
+    int nSweepFrequencyAtEnd;
+    int nSweepLength;
+    int nSweepType;
+    int nTraceNumberOfSweepChannel;
+    int nSweepTraceTaperLengthAtStart;
+    int nSweepTraceTaperLengthAtEnd;
+    int nTaperType;
+    int nCorrelated;
+    int nBinaryGainRecovered;
+    int nAmplitudeRecoveryMethod;
+    int nMeasurementSystem;
+    int nImpulseSignalPolarity;
+    int nVibratoryPolaryCode;
+    int nSEGYRevisionNumber;
+    double dfSEGYRevisionNumber;
+    int nFixedLengthTraceFlag;
+    int nNumberOfExtendedTextualFileHeader;
+} SEGYBinaryFileHeader;
+
+/************************************************************************/
+/*                          OGRSEGYLayer                                */
+/************************************************************************/
+
+class OGRSEGYLayer: public OGRLayer
+{
+    OGRSpatialReference* poSRS;
+    OGRFeatureDefn*    poFeatureDefn;
+    int                bEOF;
+    int                nNextFID;
+    VSILFILE*          fp;
+
+    SEGYBinaryFileHeader sBFH;
+    int                nDataSize;
+
+    OGRFeature *       GetNextRawFeature();
+
+  public:
+                        OGRSEGYLayer(const char* pszFilename,
+                                     VSILFILE* fp,
+                                     SEGYBinaryFileHeader* psBFH);
+                        ~OGRSEGYLayer();
+
+    virtual OGRFeature *        GetNextFeature();
+
+    virtual void                ResetReading();
+
+    virtual OGRSpatialReference *GetSpatialRef() { return poSRS; }
+
+    virtual OGRFeatureDefn *    GetLayerDefn() { return poFeatureDefn; }
+
+    virtual int                 TestCapability( const char * ) { return FALSE; }
+};
+
+/************************************************************************/
+/*                        OGRSEGYHeaderLayer                            */
+/************************************************************************/
+
+class OGRSEGYHeaderLayer: public OGRLayer
+{
+    OGRFeatureDefn*    poFeatureDefn;
+    int                bEOF;
+
+    SEGYBinaryFileHeader sBFH;
+    char*                pszHeaderText;
+
+    OGRFeature *       GetNextRawFeature();
+
+  public:
+                        OGRSEGYHeaderLayer(const char* pszLayerName,
+                                           SEGYBinaryFileHeader* psBFH,
+                                           char* pszHeaderText);
+                        ~OGRSEGYHeaderLayer();
+
+    virtual OGRFeature *        GetNextFeature();
+
+    virtual void                ResetReading();
+
+    virtual OGRSpatialReference *GetSpatialRef() { return NULL; }
+
+    virtual OGRFeatureDefn *    GetLayerDefn() { return poFeatureDefn; }
+
+    virtual int                 TestCapability( const char * ) { return FALSE; }
+};
+
+/************************************************************************/
+/*                          OGRSEGYDataSource                           */
+/************************************************************************/
+
+class OGRSEGYDataSource : public OGRDataSource
+{
+    char*               pszName;
+
+    OGRLayer**          papoLayers;
+    int                 nLayers;
+
+  public:
+                        OGRSEGYDataSource();
+                        ~OGRSEGYDataSource();
+
+    int                 Open( const char * pszFilename,
+                              int bUpdate );
+
+    virtual const char*         GetName() { return pszName; }
+
+    virtual int                 GetLayerCount() { return nLayers; }
+    virtual OGRLayer*           GetLayer( int );
+
+    virtual int                 TestCapability( const char * );
+};
+
+/************************************************************************/
+/*                            OGRSEGYDriver                             */
+/************************************************************************/
+
+class OGRSEGYDriver : public OGRSFDriver
+{
+  public:
+                ~OGRSEGYDriver();
+
+    virtual const char*         GetName();
+    virtual OGRDataSource*      Open( const char *, int );
+    virtual int                 TestCapability( const char * );
+};
+
+
+#endif /* ndef _OGR_SEGY_H_INCLUDED */
---
+++ ogr/ogrsf_frmts/segy/ogrsegydriver.cpp
@@ -0,0 +1,91 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  SEG-Y Translator
+ * Purpose:  Implements OGRSEGYDriver class.
+ * Author:   Even Rouault, even dot rouault at mines dash paris dot org
+ *
+ ******************************************************************************
+ * Copyright (c) 2011, Even Rouault <even dot rouault at mines dash paris dot org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMSEGYS OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+
+#include "ogr_segy.h"
+#include "cpl_conv.h"
+
+CPL_CVSID("$Id$");
+
+/************************************************************************/
+/*                           ~OGRSEGYDriver()                           */
+/************************************************************************/
+
+OGRSEGYDriver::~OGRSEGYDriver()
+
+{
+}
+
+/************************************************************************/
+/*                              GetName()                               */
+/************************************************************************/
+
+const char *OGRSEGYDriver::GetName()
+
+{
+    return "SEGY";
+}
+
+/************************************************************************/
+/*                                Open()                                */
+/************************************************************************/
+
+OGRDataSource *OGRSEGYDriver::Open( const char * pszFilename, int bUpdate )
+
+{
+    OGRSEGYDataSource   *poDS = new OGRSEGYDataSource();
+
+    if( !poDS->Open( pszFilename, bUpdate ) )
+    {
+        delete poDS;
+        poDS = NULL;
+    }
+
+    return poDS;
+}
+
+/************************************************************************/
+/*                           TestCapability()                           */
+/************************************************************************/
+
+int OGRSEGYDriver::TestCapability( const char * pszCap )
+
+{
+    return FALSE;
+}
+
+/************************************************************************/
+/*                           RegisterOGRSEGY()                           */
+/************************************************************************/
+
+void RegisterOGRSEGY()
+
+{
+    OGRSFDriverRegistrar::GetRegistrar()->RegisterDriver( new OGRSEGYDriver );
+}
+
