85585d1757b73aed3bd6e42e41ecef84e1f81e34
Even Rouault <even.rouault@mines-paris.org>
2011-11-13 11:18:25 +0000

    gdalcopyproj.py: make it copy GCPs too
    git-svn-id: https://svn.osgeo.org/gdal/trunk@23368 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/swig/python/samples/gdalcopyproj.py
+++ gdal/swig/python/samples/gdalcopyproj.py
@@ -64,9 +64,15 @@ if dataset2 is None:
     print('Unable to open', output, 'for writing')
     sys.exit(1)
 
-if geotransform is not None:
+if geotransform is not None and geotransform != (0,1,0,0,0,1):
     dataset2.SetGeoTransform( geotransform )
 
-if projection is not None:
+if projection is not None and projection != '':
     dataset2.SetProjection( projection )
 
+gcp_count = dataset.GetGCPs()
+if gcp_count != 0:
+    dataset2.SetGCPs( gcp_count, dataset.GetGCPProjection() )
+
+dataset = None
+dataset2 = None
