b098ed0240a1e66b603eb15ad8fa22bbc4fa1d6b
Even Rouault <even.rouault@mines-paris.org>
2011-11-13 20:16:50 +0000

    Add script to build the python extensions with Python 2.7 and a mingw32 cross-compiler under Linux/Unix
    git-svn-id: https://svn.osgeo.org/gdal/trunk@23376 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ gdal/swig/python/fallback_build_mingw32_under_unix.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# This is a script to build the python extensions with Python 2.7 and
+# a mingw32 cross-compiler under Linux/Unix. It needs wine to run native python.
+
+# You may need to customize the following versions to match your cross-compiler
+# name and your native python installation
+CXX=i586-mingw32msvc-g++
+PYTHONHOME=$HOME/.wine/drive_c/Python27
+OUTDIR=build/lib.win32-2.7/osgeo
+PYTHONLIB=python27
+
+if test -d ${PYTHONHOME}/Lib/site-packages/numpy/core/include; then
+    echo "NumPy found !"
+    HAS_NUMPY=yes
+else
+    HAS_NUMPY=no
+fi
+
+INCFLAGS="-D__MSVCRT_VERSION__=0x0601 -I${PYTHONHOME}/include -I../../port -I../../gcore -I../../alg -I../../ogr/"
+LINKFLAGS="-L../../.libs -lgdal -L${PYTHONHOME}/libs -l${PYTHONLIB}"
+
+# Run native python
+wine ${PYTHONHOME}/python setup.py build
+
+# Build extensions
+${CXX} extensions/gdal_wrap.cpp -shared -o ${OUTDIR}/_gdal.pyd ${INCFLAGS} ${LINKFLAGS}
+${CXX} extensions/ogr_wrap.cpp -shared -o ${OUTDIR}/_ogr.pyd ${INCFLAGS} ${LINKFLAGS}
+${CXX} extensions/osr_wrap.cpp -shared -o ${OUTDIR}/_osr.pyd ${INCFLAGS} ${LINKFLAGS}
+${CXX} extensions/gdalconst_wrap.c -shared -o ${OUTDIR}/_gdalconst.pyd ${INCFLAGS} ${LINKFLAGS}
+
+if test x${HAS_NUMPY} = "xyes"; then
+    ${CXX} extensions/gdal_array_wrap.cpp -shared -o ${OUTDIR}/_gdal_array.pyd ${INCFLAGS} -I${PYTHONHOME}/Lib/site-packages/numpy/core/include ${LINKFLAGS}
+fi
