b4124fdf7a5299c0ff185641d8a75f377b9927ee
Even Rouault <even.rouault@mines-paris.org>
2011-11-17 21:55:45 +0000

    Add new OGR driver for reading MS XLS files and relies on FreeXL library. Unix build support only for now
    git-svn-id: https://svn.osgeo.org/gdal/trunk@23381 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ gdal/ogr/ogrsf_frmts/xls/ogrxlsdatasource.cpp
@@ -0,0 +1,155 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  XLS Translator
+ * Purpose:  Implements OGRXLSDataSource class
+ * Author:   Even Rouault, even dot rouault at mines dash paris dot org
+ *
+ ******************************************************************************
+ * Copyright (c) 2011, Even Rouault <even dot rouault at mines dash paris dot org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+
+#include <freexl.h>
+
+#include "ogr_xls.h"
+#include "cpl_conv.h"
+#include "cpl_string.h"
+
+CPL_CVSID("$Id$");
+
+/************************************************************************/
+/*                          OGRXLSDataSource()                          */
+/************************************************************************/
+
+OGRXLSDataSource::OGRXLSDataSource()
+
+{
+    papoLayers = NULL;
+    nLayers = 0;
+
+    pszName = NULL;
+
+    xlshandle = NULL;
+}
+
+/************************************************************************/
+/*                         ~OGRXLSDataSource()                          */
+/************************************************************************/
+
+OGRXLSDataSource::~OGRXLSDataSource()
+
+{
+    for( int i = 0; i < nLayers; i++ )
+        delete papoLayers[i];
+    CPLFree( papoLayers );
+
+    CPLFree( pszName );
+
+    if (xlshandle)
+        freexl_close(xlshandle);
+}
+
+/************************************************************************/
+/*                           TestCapability()                           */
+/************************************************************************/
+
+int OGRXLSDataSource::TestCapability( const char * pszCap )
+
+{
+    return FALSE;
+}
+
+/************************************************************************/
+/*                              GetLayer()                              */
+/************************************************************************/
+
+OGRLayer *OGRXLSDataSource::GetLayer( int iLayer )
+
+{
+    if( iLayer < 0 || iLayer >= nLayers )
+        return NULL;
+    else
+        return papoLayers[iLayer];
+}
+
+/************************************************************************/
+/*                                Open()                                */
+/************************************************************************/
+
+int OGRXLSDataSource::Open( const char * pszFilename, int bUpdateIn)
+
+{
+    if (bUpdateIn)
+    {
+        return FALSE;
+    }
+
+    pszName = CPLStrdup( pszFilename );
+
+// -------------------------------------------------------------------- 
+//      Does this appear to be a .xls file?
+// --------------------------------------------------------------------
+
+    /* Open only for getting info. To get cell values, we have to use freexl_open */
+    if (freexl_open_info (pszFilename, &xlshandle) != FREEXL_OK)
+        return FALSE;
+
+    unsigned int nSheets = 0;
+    if (freexl_get_info (xlshandle, FREEXL_BIFF_SHEET_COUNT, &nSheets) != FREEXL_OK)
+        return FALSE;
+
+    for(int i=0; i<(int)nSheets; i++)
+    {
+        freexl_select_active_worksheet(xlshandle, i);
+
+        const char* pszSheetname = NULL;
+        if (freexl_get_worksheet_name(xlshandle, i, &pszSheetname) != FREEXL_OK)
+            return FALSE;
+
+        unsigned int nRows = 0;
+        unsigned short nCols = 0;
+        if (freexl_worksheet_dimensions(xlshandle, &nRows, &nCols) != FREEXL_OK)
+            return FALSE;
+
+        papoLayers = (OGRLayer**) CPLRealloc(papoLayers, (nLayers + 1) * sizeof(OGRLayer*));
+        papoLayers[nLayers ++] = new OGRXLSLayer(this, pszSheetname, i, (int)nRows, nCols);
+    }
+
+    freexl_close(xlshandle);
+    xlshandle = NULL;
+
+    return TRUE;
+}
+
+/************************************************************************/
+/*                           GetXLSHandle()                             */
+/************************************************************************/
+
+const void* OGRXLSDataSource::GetXLSHandle()
+{
+    if (xlshandle)
+        return xlshandle;
+
+    if (freexl_open (pszName, &xlshandle) != FREEXL_OK)
+        return NULL;
+
+    return xlshandle;
+}
---
+++ gdal/ogr/ogrsf_frmts/xls/ogrxlsdriver.cpp
@@ -0,0 +1,101 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  XLS Translator
+ * Purpose:  Implements OGRXLSDriver.
+ * Author:   Even Rouault, even dot rouault at mines dash paris dot org
+ *
+ ******************************************************************************
+ * Copyright (c) 2011, Even Rouault <even dot rouault at mines dash paris dot org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+
+#include "ogr_xls.h"
+#include "cpl_conv.h"
+
+CPL_CVSID("$Id$");
+
+/************************************************************************/
+/*                           ~OGRXLSDriver()                            */
+/************************************************************************/
+
+OGRXLSDriver::~OGRXLSDriver()
+
+{
+}
+
+/************************************************************************/
+/*                              GetName()                               */
+/************************************************************************/
+
+const char *OGRXLSDriver::GetName()
+
+{
+    return "XLS";
+}
+
+/************************************************************************/
+/*                                Open()                                */
+/************************************************************************/
+
+OGRDataSource *OGRXLSDriver::Open( const char * pszFilename, int bUpdate )
+
+{
+    if (bUpdate)
+    {
+        return NULL;
+    }
+
+    if (!EQUAL(CPLGetExtension(pszFilename), "XLS"))
+    {
+        return NULL;
+    }
+
+    OGRXLSDataSource   *poDS = new OGRXLSDataSource();
+
+    if( !poDS->Open( pszFilename, bUpdate ) )
+    {
+        delete poDS;
+        poDS = NULL;
+    }
+
+    return poDS;
+}
+
+/************************************************************************/
+/*                           TestCapability()                           */
+/************************************************************************/
+
+int OGRXLSDriver::TestCapability( const char * pszCap )
+
+{
+    return FALSE;
+}
+
+/************************************************************************/
+/*                           RegisterOGRXLS()                           */
+/************************************************************************/
+
+void RegisterOGRXLS()
+
+{
+    OGRSFDriverRegistrar::GetRegistrar()->RegisterDriver( new OGRXLSDriver );
+}
+
