OSGeo/gdal

commit 080ce23b4628e6812467633210cd5404e27d3c80

detect context in a typemap which returns a array, this now returns a list in list context; the change affects at least GetExtent and GetEnvelope methods, which retain backward compatibility though new and/or changed methods: FeatureDefn::Name, FeatureDefn::GeometryIgnored, FeatureDefn::StyleIgnored, Feature::ReferenceGeometry, Feature::SetFrom, FieldDefn::Ignored, Geometry::AsJSON
git-svn-id: https://svn.osgeo.org/gdal/trunk@23038 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/swig/include/perl/ogr_perl.i
@@ -64,6 +64,7 @@ ALTERED_DESTROY(OGRGeometryShadow, OGRc, delete_Geometry)
 
   %rename (_UnsetField) UnsetField;
   %rename (_SetField) SetField;
+  %rename (_SetFrom) SetFrom;
 
 }
 
@@ -455,6 +456,7 @@ ALTERED_DESTROY(OGRGeometryShadow, OGRc, delete_Geometry)
 	sub GetName {
 	  return $Geo::OGR::name_encoding ? decode($Geo::OGR::name_encoding, $_[0]->_GetName) : $_[0]->_GetName;
 	}
+	*Name = *GetName;
 	sub Schema {
 	    my $self = shift;
 	    my %schema;
@@ -490,6 +492,16 @@ ALTERED_DESTROY(OGRGeometryShadow, OGRc, delete_Geometry)
 	    return $Geo::OGR::Geometry::TYPE_INT2STRING{GetGeomType($self)} if defined wantarray;
 	}
 	*GeometryType = *GeomType;
+	sub GeometryIgnored {
+	    my $self = shift;
+	    SetGeometryIgnored($self, $_[0]) if @_;
+	    IsGeometryIgnored($self) if defined wantarray;
+	}
+	sub StyleIgnored {
+	    my $self = shift;
+	    SetStyleIgnored($self, $_[0]) if @_;
+	    IsStyleIgnored($self) if defined wantarray;
+	}
 
 	package Geo::OGR::Feature;
 	use strict;
@@ -711,7 +723,7 @@ ALTERED_DESTROY(OGRGeometryShadow, OGRc, delete_Geometry)
 		_SetField($self, $field, @$list[0..6]);
 	    } 
 	    else {
-		carp "unknown/unsupported field type: $type";
+		carp "unknown or unsupported field type: $type";
 	    }
 	}
 	sub Field {
@@ -735,6 +747,26 @@ ALTERED_DESTROY(OGRGeometryShadow, OGRc, delete_Geometry)
 	    my $geom = GetGeometryRef($self);
 	    $GEOMETRIES{tied(%$geom)} = $self if $geom;
 	    return $geom;
+	}
+	sub ReferenceGeometry {
+	    my $self = shift;
+	    SetGeometryDirectly($self, $_[0]) if @_;
+	    if (defined wantarray) {
+		my $geometry = GetGeometry($self);
+		return $geometry->Clone() if $geometry;
+	    }
+	}
+	sub SetFrom {
+	    my($self, $other) = @_;
+	    _SetFrom($self, $other), return if @_ <= 2;
+	    my $forgiving = $_[2];
+	    _SetFrom($self, $other, $forgiving), return if @_ <= 3;	    
+	    my $map = $_[3];
+	    my @list;
+	    for my $i (1..GetFieldCount($self)) {
+		push @list, ($map->{$i} || -1);
+	    }
+	    SetFromWithMap($self, $other, 1, \@list);
 	}
 
 	package Geo::OGR::FieldDefn;
@@ -822,6 +854,11 @@ ALTERED_DESTROY(OGRGeometryShadow, OGRc, delete_Geometry)
 	    my $self = shift;
 	    SetPrecision($self, $_[0]) if @_;
 	    GetPrecision($self) if defined wantarray;
+	}
+	sub Ignored {
+	    my $self = shift;
+	    SetIgnored($self, $_[0]) if @_;
+	    IsIgnored($self) if defined wantarray;
 	}
 	sub Schema {
 	    my $self = shift;
@@ -1137,6 +1174,7 @@ ALTERED_DESTROY(OGRGeometryShadow, OGRc, delete_Geometry)
 	*AsBinary = *ExportToWkb;
 	*AsGML = *ExportToGML;
 	*AsKML = *ExportToKML;
+	*AsJSON = *ExportToJson;
 	*BuildPolygonFromEdges = *Geo::OGR::BuildPolygonFromEdges;
 	*ForceToPolygon = *Geo::OGR::ForceToPolygon;
 	

gdal/swig/include/perl/typemaps_perl.i
@@ -275,8 +275,19 @@ CreateArrayFromStringArray( char **first ) {
 %typemap(argout,fragment="CreateArrayFromDoubleArray") ( double argout[ANY])
 {
   /* %typemap(argout) (double argout[ANY]) */
-  $result = CreateArrayFromDoubleArray( $1, $dim0 );
-  argvi++;
+  if (GIMME_V == G_ARRAY) {
+    /* return a list */
+    int i;
+    SP -= items;
+    EXTEND(SP, $dim0);
+    for (i = 0; i < $dim0; i++)
+      mPUSHn($1[i]);
+    PUTBACK;
+    return;
+  } else {
+    $result = CreateArrayFromDoubleArray( $1, $dim0 );
+    argvi++;
+  }  
 }
 
 %typemap(in,numinputs=0) ( double *argout[ANY]) (double *argout)

commit 0fcd2bce9e1cf3f327ba237c697123292d8c9cab

Perl bindings: fix a bug in Geometry::Points method caused by r23038
git-svn-id: https://svn.osgeo.org/gdal/trunk@23443 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/swig/include/perl/ogr_perl.i
@@ -1090,16 +1090,16 @@ ALTERED_DESTROY(OGRGeometryShadow, OGRc, delete_Geometry)
 	    } else {
 		$n = $self->GetPointCount;
 		if ($n == 1) {
-		    push @points, $flat ? GetPoint_2D($self) : GetPoint_3D($self);
+		    push @points, $flat ? scalar GetPoint_2D($self) : scalar GetPoint_3D($self);
 		} else {
 		    my $i;
 		    if ($flat) {
 			for my $i (0..$n-1) {
-			    push @points, GetPoint_2D($self, $i);
+			    push @points, scalar GetPoint_2D($self, $i);
 			}
 		    } else {
 			for my $i (0..$n-1) {
-			    push @points, GetPoint_3D($self, $i);
+			    push @points, scalar GetPoint_3D($self, $i);
 			}
 		    }
 		}