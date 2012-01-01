0a8fb97eacd3a5238ba47df5e242b9d75b868fac
Even Rouault <even.rouault@mines-paris.org>
2012-01-01 14:31:47 +0000

    PG driver: compatibility with latest postgis 2.0 SVN that has removed SetSRID(), which breaks spatial filter (https://trac.osgeo.org/gdal/ticket/4417)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@23673 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/pg/ogrpgresultlayer.cpp
+++ gdal/ogr/ogrsf_frmts/pg/ogrpgresultlayer.cpp
@@ -239,8 +239,10 @@ void OGRPGResultLayer::SetSpatialFilter( OGRGeometry * poGeomIn )
                 snprintf(szBox3D_2, sizeof(szBox3D_2), "%.12f %.12f", sEnvelope.MaxX, sEnvelope.MaxY);
                 while((pszComma = strchr(szBox3D_2, ',')) != NULL)
                     *pszComma = '.';
-                osWHERE.Printf("WHERE %s && SetSRID('BOX3D(%s, %s)'::box3d,%d) ",
-                            OGRPGEscapeColumnName(pszGeomColumn).c_str(), szBox3D_1, szBox3D_2, nSRSId );
+                osWHERE.Printf("WHERE %s && %s('BOX3D(%s, %s)'::box3d,%d) ",
+                               OGRPGEscapeColumnName(pszGeomColumn).c_str(),
+                               (poDS->sPostGISVersion.nMajor >= 2) ? "ST_SetSRID" : "SetSRID",
+                               szBox3D_1, szBox3D_2, nSRSId );
             }
             else
             {
