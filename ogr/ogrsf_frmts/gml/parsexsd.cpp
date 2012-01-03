a4a9243881b3808a75f1d24731292049ae7204f0
Even Rouault <even.rouault@mines-paris.org>
2012-01-03 19:50:05 +0000

    GML .xsd parser: recognize type='date' as a string for now
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@23693 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/ogrsf_frmts/gml/parsexsd.cpp
+++ ogr/ogrsf_frmts/gml/parsexsd.cpp
@@ -105,6 +105,12 @@ int GetSimpleTypeProperties(CPLXMLNode *psTypeNode,
         return TRUE;
     }
 
+    /* TODO: Would be nice to have a proper date type */
+    else if( EQUAL(pszBase,"date") )
+    {
+        *pGMLType = GMLPT_String;
+        return TRUE;
+    }
     return FALSE;
 }
 
@@ -262,6 +268,9 @@ GMLFeatureClass* GMLParseFeatureType(CPLXMLNode *psSchemaNode,
             GMLPropertyType gmlType = GMLPT_Untyped;
             if (EQUAL(pszStrippedNSType, "string") ||
                 EQUAL(pszStrippedNSType, "Character"))
+                gmlType = GMLPT_String;
+            /* TODO: Would be nice to have a proper date type */
+            else if (EQUAL(pszStrippedNSType, "date"))
                 gmlType = GMLPT_String;
             else if (EQUAL(pszStrippedNSType, "real") ||
                      EQUAL(pszStrippedNSType, "double") ||
