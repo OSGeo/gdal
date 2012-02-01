af1460ecdce61d9f39902c32a0caeee1cd278499
Even Rouault <even.rouault@mines-paris.org>
2012-02-01 19:20:54 +0000

    gml2ogrgeometry : make sure to use provided control points in linear approximation of gml:Arc (https://trac.osgeo.org/gdal/ticket/4451)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@23863 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/gml2ogrgeometry.cpp
+++ ogr/gml2ogrgeometry.cpp
@@ -885,12 +885,16 @@ OGRGeometry *GML2OGRGeometry_XMLNode( const CPLXMLNode *psNode,
 
         dfStep *= nSign;
 
-        
-        for(alpha = alpha0; (alpha - alpha1) * nSign < 0; alpha += dfStep)
+        poLine->addPoint(x0, y0);
+
+        for(alpha = alpha0 + dfStep; (alpha - alpha1) * nSign < 0; alpha += dfStep)
         {
             poLine->addPoint(cx + R * cos(alpha), cy + R * sin(alpha));
         }
-        for(alpha = alpha1; (alpha - alpha2) * nSign < 0; alpha += dfStep)
+
+        poLine->addPoint(x1, y1);
+
+        for(alpha = alpha1 + dfStep; (alpha - alpha2) * nSign < 0; alpha += dfStep)
         {
             poLine->addPoint(cx + R * cos(alpha), cy + R * sin(alpha));
         }
@@ -901,19 +905,11 @@ OGRGeometry *GML2OGRGeometry_XMLNode( const CPLXMLNode *psNode,
             {
                 poLine->addPoint(cx + R * cos(alpha), cy + R * sin(alpha));
             }
-            poLine->addPoint(cx + R * cos(alpha3), cy + R * sin(alpha3));
+            poLine->addPoint(x0, y0);
         }
         else
         {
-            poLine->addPoint(cx + R * cos(alpha2), cy + R * sin(alpha2));
-
-            // For arcs we reset the first and last point to avoid 
-            // approximations that don't match the source values. 
-            if( poLine->getNumPoints() >= 2 )
-            {
-                poLine->setPoint(0,x0,y0);
-                poLine->setPoint(poLine->getNumPoints()-1,x2,y2);
-            }
+            poLine->addPoint(x2, y2);
         }
 
         return poLine;
