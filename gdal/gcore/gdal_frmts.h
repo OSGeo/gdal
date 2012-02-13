70cc8bcd4f057153c42fe28f43ea6bb934d972aa
Frank Warmerdam <warmerdam@pobox.com>
2012-02-13 08:43:20 +0000

    preliminary CTable2 (datum grid shift format)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@23967 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/gcore/gdal_frmts.h
+++ gdal/gcore/gdal_frmts.h
@@ -157,6 +157,7 @@ void CPL_DLL GDALRegister_GTX(void);
 void CPL_DLL GDALRegister_LOSLAS(void);
 void CPL_DLL GDALRegister_Istar(void);
 void CPL_DLL GDALRegister_NTv2(void);
+void CPL_DLL GDALRegister_CTable2(void);
 void CPL_DLL GDALRegister_JP2OpenJPEG(void);
 void CPL_DLL GDALRegister_XYZ(void);
 void CPL_DLL GDALRegister_HF2(void);
