206cb6ade0046ed71e5c82d41eaf61a91a26f634
Even Rouault <even.rouault@mines-paris.org>
2012-02-21 22:41:52 +0000

    Internal libpng: update to 1.2.47 (security fix)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@24007 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/png/libpng/png.h
+++ frmts/png/libpng/png.h
@@ -1,7 +1,7 @@
 /* png.h - header file for PNG reference library
  *
- * libpng version 1.2.46 - July 9, 2011
- * Copyright (c) 1998-2011 Glenn Randers-Pehrson
+ * libpng version 1.2.47 - February 18, 2012
+ * Copyright (c) 1998-2012 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
  *
@@ -10,7 +10,7 @@
  * Authors and maintainers:
  *  libpng versions 0.71, May 1995, through 0.88, January 1996: Guy Schalnat
  *  libpng versions 0.89c, June 1996, through 0.96, May 1997: Andreas Dilger
- *  libpng versions 0.97, January 1998, through 1.2.46 - July 9, 2011: Glenn
+ *  libpng versions 0.97, January 1998, through 1.2.47 - February 18, 2012: Glenn
  *  See also "Contributing Authors", below.
  *
  * Note about libpng version numbers:
@@ -270,6 +270,11 @@
  *    1.2.46rc01-02           13    10246  12.so.0.46[.0]
  *    1.0.56                  10    10056  10.so.0.56[.0]
  *    1.2.46                  13    10246  12.so.0.46[.0]
+ *    1.2.47beta01            13    10247  12.so.0.47[.0]
+ *    1.2.47rc01              13    10247  12.so.0.47[.0]
+ *    1.0.57rc01              10    10057  10.so.0.57[.0]
+ *    1.2.47                  13    10247  12.so.0.47[.0]
+ *    1.0.57                  10    10057  10.so.0.57[.0]
  *
  *    Henceforth the source version will match the shared-library major
  *    and minor numbers; the shared-library major version number will be
@@ -301,7 +306,7 @@
  *
  * This code is released under the libpng license.
  *
- * libpng versions 1.2.6, August 15, 2004, through 1.2.46, July 9, 2011, are
+ * libpng versions 1.2.6, August 15, 2004, through 1.2.47, February 18, 2012, are
  * Copyright (c) 2004, 2006-2011 Glenn Randers-Pehrson, and are
  * distributed according to the same disclaimer and license as libpng-1.2.5
  * with the following individual added to the list of Contributing Authors:
@@ -413,13 +418,13 @@
  * Y2K compliance in libpng:
  * =========================
  *
- *    July 9, 2011
+ *    February 18, 2012
  *
  *    Since the PNG Development group is an ad-hoc body, we can't make
  *    an official declaration.
  *
  *    This is your unofficial assurance that libpng from version 0.71 and
- *    upward through 1.2.46 are Y2K compliant.  It is my belief that earlier
+ *    upward through 1.2.47 are Y2K compliant.  It is my belief that earlier
  *    versions were also Y2K compliant.
  *
  *    Libpng only has three year fields.  One is a 2-byte unsigned integer
@@ -475,9 +480,9 @@
  */
 
 /* Version information for png.h - this should match the version in png.c */
-#define PNG_LIBPNG_VER_STRING "1.2.46"
+#define PNG_LIBPNG_VER_STRING "1.2.47"
 #define PNG_HEADER_VERSION_STRING \
-   " libpng version 1.2.46 - July 9, 2011\n"
+   " libpng version 1.2.47 - February 18, 2012\n"
 
 #define PNG_LIBPNG_VER_SONUM   0
 #define PNG_LIBPNG_VER_DLLNUM  13
@@ -485,7 +490,7 @@
 /* These should match the first 3 components of PNG_LIBPNG_VER_STRING: */
 #define PNG_LIBPNG_VER_MAJOR   1
 #define PNG_LIBPNG_VER_MINOR   2
-#define PNG_LIBPNG_VER_RELEASE 46
+#define PNG_LIBPNG_VER_RELEASE 47
 /* This should match the numeric part of the final component of
  * PNG_LIBPNG_VER_STRING, omitting any leading zero:
  */
@@ -515,7 +520,7 @@
  * version 1.0.0 was mis-numbered 100 instead of 10000).  From
  * version 1.0.1 it's    xxyyzz, where x=major, y=minor, z=release
  */
-#define PNG_LIBPNG_VER 10246 /* 1.2.46 */
+#define PNG_LIBPNG_VER 10247 /* 1.2.47 */
 
 #ifndef PNG_VERSION_INFO_ONLY
 /* Include the compression library's header */
@@ -1555,7 +1560,7 @@ struct png_struct_def
 /* This triggers a compiler error in png.c, if png.c and png.h
  * do not agree upon the version number.
  */
-typedef png_structp version_1_2_46;
+typedef png_structp version_1_2_47;
 
 typedef png_struct FAR * FAR * png_structpp;
 
