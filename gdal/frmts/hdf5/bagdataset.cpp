3a53f1c0689f8df3e9a62dfcafba16ad52dd14b6
Frank Warmerdam <warmerdam@pobox.com>
2012-03-06 04:38:44 +0000

    fix serious problems with tiled images, particularly when not multiples of tile size (#4548)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@24083 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/hdf5/bagdataset.cpp
+++ gdal/frmts/hdf5/bagdataset.cpp
@@ -277,23 +277,38 @@ CPLErr BAGRasterBand::IReadBlock( int nBlockXOff, int nBlockYOff,
     herr_t      status;
     hsize_t     count[3];
     H5OFFSET_TYPE offset[3];
+    int         nSizeOfData;
     hid_t       memspace;
     hsize_t     col_dims[3];
-    hsize_t     rank = 2;
+    hsize_t     rank;
 
-    offset[0] = nRasterYSize - nBlockYOff*nBlockYSize - 1;
+    rank=2;
+
+    offset[0] = MAX(0,nRasterYSize - (nBlockYOff+1)*nBlockYSize);
     offset[1] = nBlockXOff*nBlockXSize;
     count[0]  = nBlockYSize;
     count[1]  = nBlockXSize;
+
+    nSizeOfData = H5Tget_size( native );
+    memset( pImage,0,nBlockXSize*nBlockYSize*nSizeOfData );
+
+/*  blocksize may not be a multiple of imagesize */
+    count[0]  = MIN( size_t(nBlockYSize), GetYSize() - offset[0]);
+    count[1]  = MIN( size_t(nBlockXSize), GetXSize() - offset[1]);
+
+    if( nRasterYSize - (nBlockYOff+1)*nBlockYSize < 0 )
+    {
+        count[0] += (nRasterYSize - (nBlockYOff+1)*nBlockYSize);
+    }
 
 /* -------------------------------------------------------------------- */
 /*      Select block from file space                                    */
 /* -------------------------------------------------------------------- */
     status =  H5Sselect_hyperslab( dataspace,
-                                   H5S_SELECT_SET, 
-                                   offset, NULL, 
+                                   H5S_SELECT_SET,
+                                   offset, NULL,
                                    count, NULL );
-   
+
 /* -------------------------------------------------------------------- */
 /*      Create memory space to receive the data                         */
 /* -------------------------------------------------------------------- */
@@ -310,10 +325,36 @@ CPLErr BAGRasterBand::IReadBlock( int nBlockXOff, int nBlockYOff,
                        native,
                        memspace,
                        dataspace,
-                       H5P_DEFAULT, 
+                       H5P_DEFAULT,
                        pImage );
-    H5Sclose(memspace);
 
+    H5Sclose( memspace );
+
+/* -------------------------------------------------------------------- */
+/*      Y flip the data.                                                */
+/* -------------------------------------------------------------------- */
+    int nLinesToFlip = count[0];
+    int nLineSize = nSizeOfData * nBlockXSize;
+    GByte *pabyTemp = (GByte *) CPLMalloc(nLineSize);
+
+    for( int iY = 0; iY < nLinesToFlip/2; iY++ )
+    {
+        memcpy( pabyTemp, 
+                ((GByte *)pImage) + iY * nLineSize,
+                nLineSize );
+        memcpy( ((GByte *)pImage) + iY * nLineSize,
+                ((GByte *)pImage) + (nLinesToFlip-iY-1) * nLineSize,
+                nLineSize );
+        memcpy( ((GByte *)pImage) + (nLinesToFlip-iY-1) * nLineSize,
+                pabyTemp,
+                nLineSize );
+    }
+
+    CPLFree( pabyTemp );
+
+/* -------------------------------------------------------------------- */
+/*      Return success or failure.                                      */
+/* -------------------------------------------------------------------- */
     if( status < 0 )
     {
         CPLError( CE_Failure, CPLE_AppDefined,
