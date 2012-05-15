b933de14b23952aea280c23d43f046a41f80391d
Frank Warmerdam <warmerdam@pobox.com>
2012-05-15 14:25:27 +0000

    update for 1.9.1 release
    git-svn-id: https://svn.osgeo.org/gdal/branches/1.9@24419 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/gcore/gdal_version.h
+++ gdal/gcore/gdal_version.h
@@ -6,7 +6,7 @@
 #ifndef GDAL_VERSION_MAJOR
 #  define GDAL_VERSION_MAJOR    1
 #  define GDAL_VERSION_MINOR    9
-#  define GDAL_VERSION_REV      0
+#  define GDAL_VERSION_REV      1
 #  define GDAL_VERSION_BUILD    0
 #endif
 
@@ -15,8 +15,8 @@
 #endif
 
 #ifndef GDAL_RELEASE_DATE
-#  define GDAL_RELEASE_DATE     20111229
+#  define GDAL_RELEASE_DATE     20120515
 #endif
 #ifndef GDAL_RELEASE_NAME
-#  define GDAL_RELEASE_NAME     "1.9.0"
+#  define GDAL_RELEASE_NAME     "1.9.1"
 #endif
