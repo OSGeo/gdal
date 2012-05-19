bef2762ff5f8b369d2e428b324f3bd4dada7f8e4
Even Rouault <even.rouault@mines-paris.org>
2012-05-19 15:23:57 +0000

    Use joinable thread API
    git-svn-id: https://svn.osgeo.org/gdal/trunk@24469 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/cpp/testthreadcond.c
+++ autotest/cpp/testthreadcond.c
@@ -1,9 +1,36 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  GDAL Core
+ * Purpose:  Test thread API
+ * Author:   Even Rouault, <even dot rouault at mines dash paris dot org>
+ *
+ ******************************************************************************
+ * Copyright (c) 2012, Even Rouault
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
 
 #include "cpl_multiproc.h"
 
 void* hCond = NULL;
 void* hCondJobFinished = NULL;
-void* hCondProducerFinished = NULL;
 void* hClientMutex = NULL;
 
 struct _JobItem
@@ -54,7 +81,7 @@ void ProducerThread(void *unused)
     bProducedFinished = 1;
     CPLReleaseMutex(hClientMutex);
 
-    CPLCondSignal(hCondProducerFinished);
+    CPLCondBroadcast(hCond);
 }
 
 void ConsumerThread(void* pIndex)
@@ -64,6 +91,8 @@ void ConsumerThread(void* pIndex)
     JobItem* psNext;
 
     nThreadIndex = *(int*)pIndex;
+    free(pIndex);
+
     printf("Thread %d created\n", nThreadIndex);
 
     nThreadTotal ++;
@@ -71,8 +100,13 @@ void ConsumerThread(void* pIndex)
     while(TRUE)
     {
         CPLAcquireMutex(hClientMutex, 1000.0);
-        while(psJobList == NULL)
+        while(psJobList == NULL && !bProducedFinished)
             CPLCondWait(hCond, hClientMutex);
+        if (bProducedFinished)
+        {
+            CPLReleaseMutex(hClientMutex);
+            break;
+        }
 
         nJobNumber = psJobList->nJobNumber;
         psNext = psJobList->psNext;
@@ -86,17 +120,17 @@ void ConsumerThread(void* pIndex)
         nJobListSize --;
         CPLReleaseMutex(hClientMutex);
 
-        CPLCondBroadcast(hCondJobFinished);
+        CPLCondSignal(hCondJobFinished);
     }
 }
 
 int main(int argc, char* argv[])
 {
     int i;
+    void* apThreads[10];
 
     hCond = CPLCreateCond();
     hCondJobFinished = CPLCreateCond();
-    hCondProducerFinished = CPLCreateCond();
 
     hClientMutex = CPLCreateMutex();
     CPLReleaseMutex(hClientMutex);
@@ -107,19 +141,16 @@ int main(int argc, char* argv[])
     {
         int* pi = (int*)malloc(sizeof(int));
         *pi = i;
-        CPLCreateThread(ConsumerThread, pi);
+        apThreads[i] = CPLCreateJoinableThread(ConsumerThread, pi);
     }
 
-    CPLAcquireMutex(hClientMutex, 1000.0);
-    while (!bProducedFinished)
-        CPLCondWait(hCondProducerFinished, hClientMutex);
-    while (nJobListSize > 0)
-        CPLCondWait(hCondJobFinished, hClientMutex);
-    CPLReleaseMutex(hClientMutex);
+    for(i = 0; i < 10;i++)
+    {
+        CPLJoinThread(apThreads[i]);
+    }
 
     CPLDestroyCond(hCond);
     CPLDestroyCond(hCondJobFinished);
-    CPLDestroyCond(hCondProducerFinished);
     CPLDestroyMutex(hClientMutex);
     return 0;
 }
