75d13d117cd8a024981bd01b4b2c0761f5e25c31
Even Rouault <even.rouault@mines-paris.org>
2012-06-10 20:27:47 +0000

    Warp: add NUM_THREADS warping option to set the number of threads to use to parallelize the computation part of the warping
    git-svn-id: https://svn.osgeo.org/gdal/trunk@24568 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/alg/gdalwarper.cpp
+++ gdal/alg/gdalwarper.cpp
@@ -837,6 +837,10 @@ GDALWarpDstAlphaMasker( void *pMaskFuncArg, int nBandCount, GDALDataType eType,
  * avoids partial writes of compressed blocks and lost space when they are rewritten
  * at the end of the file. However sticking to target block size may cause major
  * processing slowdown for some particular reprojections.
+ *
+ * - NUM_THREADS: (GDAL >= 2.0) Can be set to a numeric value or ALL_CPUS to
+ * set the number of threads to use to parallelize the computation part of the
+ * warping. If not set, computation will be done in a single thread.
  */
 
 /************************************************************************/
