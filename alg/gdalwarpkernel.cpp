8a030b10a8dc57bd7e30995da4eed8ed3539b85d
Even Rouault <even.rouault@mines-paris.org>
2012-10-07 14:13:39 +0000

    OpenCL warper: fix segmentation fault related to source/destination validity masks (#4840)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@25069 f0d54148-0727-0410-94bb-9a71ac55c965

--- alg/gdalwarpkernel.cpp
+++ alg/gdalwarpkernel.cpp
@@ -2819,7 +2819,7 @@ static CPLErr GWKOpenCLCase( GDALWarpKernel *poWK )
                                          poWK->pafUnifiedSrcDensity,
                                          poWK->panUnifiedSrcValid,
                                          poWK->pafDstDensity,
-                                         poWK->panUnifiedSrcValid,
+                                         poWK->panDstValid,
                                          poWK->dfXScale, poWK->dfYScale,
                                          poWK->dfXFilter, poWK->dfYFilter,
                                          poWK->nXRadius, poWK->nYRadius,
