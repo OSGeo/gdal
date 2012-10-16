e61d60d8589a768dc202b0bcf871b6bf9b9714c9
Even Rouault <even.rouault@mines-paris.org>
2012-10-16 18:49:32 +0000

    Remove ugliness so that new contributors don't copy and paste it
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@25143 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/jdem/jdemdataset.cpp
+++ frmts/jdem/jdemdataset.cpp
@@ -111,6 +111,7 @@ class JDEMRasterBand : public GDALPamRasterBand
 
     int          nRecordSize;
     char*        pszRecord;
+    int          bBufferAllocFailed;
     
   public:
 
@@ -139,6 +140,7 @@ JDEMRasterBand::JDEMRasterBand( JDEMDataset *poDS, int nBand )
     /* Cannot overflow as nBlockXSize <= 999 */
     nRecordSize = nBlockXSize*5 + 9 + 2;
     pszRecord = NULL;
+    bBufferAllocFailed = FALSE;
 }
 
 /************************************************************************/
@@ -163,7 +165,7 @@ CPLErr JDEMRasterBand::IReadBlock( int nBlockXOff, int nBlockYOff,
     
     if (pszRecord == NULL)
     {
-        if (nRecordSize < 0)
+        if (bBufferAllocFailed)
             return CE_Failure;
 
         pszRecord = (char *) VSIMalloc(nRecordSize);
@@ -171,7 +173,7 @@ CPLErr JDEMRasterBand::IReadBlock( int nBlockXOff, int nBlockYOff,
         {
             CPLError(CE_Failure, CPLE_OutOfMemory,
                      "Cannot allocate scanline buffer");
-            nRecordSize = -1;
+            bBufferAllocFailed = TRUE;
             return CE_Failure;
         }
     }
