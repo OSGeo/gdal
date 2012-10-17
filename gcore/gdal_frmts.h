5eb13ddf033b7946b04a4f59d4fb515123b1f2ac
Even Rouault <even.rouault@mines-paris.org>
2012-10-17 19:30:15 +0000

    Add new GDAL drivers, IRIS, to read products generated by the IRIS weather radar software (contributed by Roger Veciana, https://trac.osgeo.org/gdal/ticket/4854)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@25150 f0d54148-0727-0410-94bb-9a71ac55c965

--- gcore/gdal_frmts.h
+++ gcore/gdal_frmts.h
@@ -173,6 +173,7 @@ void CPL_DLL GDALRegister_ZMap(void);
 void CPL_DLL GDALRegister_NGSGEOID(void);
 void CPL_DLL GDALRegister_MBTiles(void);
 void CPL_DLL GDALRegister_ARG(void);
+void CPL_DLL GDALRegister_IRIS(void);
 CPL_C_END
 
 #endif /* ndef GDAL_FRMTS_H_INCLUDED */
