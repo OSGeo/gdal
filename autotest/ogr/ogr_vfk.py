daeea103b06df9967c771da686fd9b21329c308e
Martin Landa <landa.martin@gmail.com>
2012-10-18 22:24:10 +0000

    OGR-VFK: fix 5th test
    git-svn-id: https://svn.osgeo.org/gdal/trunk@25158 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/ogr/ogr_vfk.py
+++ autotest/ogr/ogr_vfk.py
@@ -185,7 +185,7 @@ def ogr_vfk_5():
     
     geom_type = gdaltest.vfk_layer_hp.GetGeomType()
 
-    if geom_type != ogr.wkbLineString:
+    if geom_type != ogr.wkbLineString25D:
         gdaltest.post_reason('did not get expected geometry type, got %d' % geom_type)
         return 'fail'
     
