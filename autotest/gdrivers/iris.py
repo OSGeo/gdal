5a76c2714d97e08a72641817ed6e1f61769da416
Even Rouault <even.rouault@mines-paris.org>
2012-10-22 22:03:55 +0000

    IRIS: test a real dataset (https://trac.osgeo.org/gdal/ticket/4854)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@25175 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/gdrivers/iris.py
+++ autotest/gdrivers/iris.py
@@ -31,7 +31,8 @@
 
 import os
 import sys
-import gdal
+from osgeo import gdal
+from osgeo import osr
 
 sys.path.append( '../pymod' )
 
@@ -45,8 +46,83 @@ def iris_1():
     tst = gdaltest.GDALTest( 'IRIS', 'fakeiris.dat', 1, 65532 )
     return tst.testOpen()
 
+###############################################################################
+# Test reading a real world IRIS dataset
+
+def iris_2():
+
+    ds = gdal.Open('data/iristest.dat')
+    if ds.GetRasterBand(1).Checksum() != 52872:
+        gdaltest.post_reason('fail')
+        return 'fail'
+
+    got_wkt = ds.GetProjectionRef()
+    got_wkt = ds.GetProjectionRef()
+    expected_wkt = """PROJCS["unnamed",GEOGCS["unnamed ellipse",DATUM["unknown",SPHEROID["unnamed",6371000.5,0]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433]],PROJECTION["Mercator_1SP"],PARAMETER["latitude_of_origin",41.70833587646484],PARAMETER["central_meridian",1.75000011920929],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],PARAMETER["false_northing",-3816068.273664699]]"""
+    got_srs = osr.SpatialReference(got_wkt)
+    expected_srs = osr.SpatialReference(expected_wkt)
+
+    # There are some differences in the values of the parameters between Linux and Windows
+    # not sure if it is only due to rounding differences, different proj versions, etc...
+    #if got_srs.IsSame(expected_srs) != 1:
+    #    gdaltest.post_reason('fail')
+    #    print('')
+    #    print(expected_wkt)
+    #    print(got_wkt)
+    #    return 'fail'
+
+    got_gt = ds.GetGeoTransform()
+    expected_gt = [ -133000.406250000000000,1023.080017089843750,0,132999.687500000000000,0,-1013.469970703125000 ]
+    for i in range(6):
+        if (expected_gt[i] == 0.0 and got_gt[i] != 0.0) or \
+           (expected_gt[i] != 0.0 and abs(got_gt[i] - expected_gt[i])/abs(expected_gt[i]) > 1e-5) :
+            gdaltest.post_reason('fail')
+            print(got_gt)
+            print(i)
+            return 'fail'
+
+    expected_metadata = [
+  "AZIMUTH_SMOOTHING_FOR_SHEAR=0.0",
+  "CAPPI_HEIGHT=1000.0 m",
+  "COMPOSITED_PRODUCT=YES",
+  "COMPOSITED_PRODUCT_MASK=0x0000080c",
+  "DATA_TYPE=Clutter Corrected H reflectivity (1 byte)",
+  "DATA_TYPE_CODE=dBZ",
+  "DATA_TYPE_INPUT=Clutter Corrected H reflectivity (1 byte)",
+  "DATA_TYPE_INPUT_CODE=dBZ",
+  "DATA_TYPE_UNITS=dBZ",
+  "GROUND_HEIGHT=523 m",
+  "INGEST_HARDWARE_NAME=composada       ",
+  "INGEST_SITE_IRIS_VERSION=8.12",
+  "INGEST_SITE_NAME=composada       ",
+  "MAX_AGE_FOR_SHEAR_VVP_CORRECTION=600 s",
+  "NYQUIST_VELOCITY=6.00 m/s",
+  "PRF=450 Hz",
+  "PRODUCT=CAPPI",
+  "PRODUCT_CONFIGURATION_NAME=CAPPI250CAT ",
+  "PRODUCT_ID=3",
+  "PRODUCT_SITE_IRIS_VERSION=8.12",
+  "PRODUCT_SITE_NAME=SMCXRADSRV01    ",
+  "RADAR_HEIGHT=542 m",
+  "TASK_NAME=PPIVOL_A    ",
+  "TIME_INPUT_INGEST_SWEEP=2012-04-19 14:48:05",
+  "TIME_PRODUCT_GENERATED=2012-04-19 14:48:30",
+  "WAVELENGTH=5.33 cm" ]
+    got_metadata = ds.GetMetadata()
+
+    for md in expected_metadata:
+        key = md[0:md.find('=')]
+        value = md[md.find('=')+1:]
+        if got_metadata[key] != value:
+            gdaltest.post_reason('did not find %s' % key)
+            print(got_metadata)
+            return 'fail'
+
+    return 'success'
+
 gdaltest_list = [
-    iris_1 ]
+    iris_1,
+    iris_2]
 
 if __name__ == '__main__':
 
