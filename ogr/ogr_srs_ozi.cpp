778691deccd7791bb863bfbaa3f00f6c342cf115
Even Rouault <even.rouault@mines-paris.org>
2012-11-26 20:19:03 +0000

    OZI: add support for UTM projection (derived from patch by Jean-Claude Repetto, #4894)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@25256 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/ogr_srs_ozi.cpp
+++ ogr/ogr_srs_ozi.cpp
@@ -54,6 +54,11 @@ OGRErr OSRImportFromOzi( OGRSpatialReferenceH hSRS,
 /************************************************************************/
 
 /**
+ * Note : This method is obsolete, but has been kept to avoid breaking the API.
+ *        It can be removed in GDAL 2.0
+ */
+
+/**
  * Import coordinate system from OziExplorer projection definition.
  *
  * This method will import projection definition in style, used by
@@ -73,6 +78,8 @@ OGRErr OSRImportFromOzi( OGRSpatialReferenceH hSRS,
  * whole in this parameter.
  * 
  * @return OGRERR_NONE on success or an error code in case of failure. 
+ *
+ * @deprecated Use importFromOzi( const char * const* papszLines ) instead
  */
 
 OGRErr OGRSpatialReference::importFromOzi( const char *pszDatum,
@@ -80,7 +87,66 @@ OGRErr OGRSpatialReference::importFromOzi( const char *pszDatum,
                                            const char *pszProjParms )
 
 {
+    const char* papszLines[8];
+
+    // Fake
+    papszLines[0] = "";
+    papszLines[1] = "";
+    papszLines[2] = "";
+    papszLines[3] = "";
+    papszLines[4] = pszDatum; /* Must be in that position */
+    papszLines[5] = pszProj; /* Must be after 5th line */
+    papszLines[6] = pszProjParms; /* Must be after 5th line */
+    papszLines[7] = NULL;
+
+    return importFromOzi(papszLines);
+}
+
+/************************************************************************/
+/*                            importFromOzi()                           */
+/************************************************************************/
+
+/**
+ * Import coordinate system from OziExplorer projection definition.
+ *
+ * This method will import projection definition in style, used by
+ * OziExplorer software.
+ *
+ * @param papszLines Map file lines. This is an array of strings containing
+ * the whole OziExplorer .MAP file. The array is terminated by a NULL pointer.
+ * 
+ * @return OGRERR_NONE on success or an error code in case of failure. 
+ *
+ * @since OGR 1.10
+ */
+
+OGRErr OGRSpatialReference::importFromOzi( const char * const* papszLines )
+{
+    int iLine;
+    const char *pszDatum, *pszProj = NULL, *pszProjParms = NULL;
+
     Clear();
+
+    int nLines = CSLCount((char**)papszLines);
+    if( nLines < 5 )
+        return OGRERR_NOT_ENOUGH_DATA;
+
+    pszDatum = papszLines[4];
+
+    for ( iLine = 5; iLine < nLines; iLine++ )
+    {
+        if ( EQUALN(papszLines[iLine], "Map Projection", 14) )
+        {
+            pszProj = papszLines[iLine];
+        }
+        else if ( EQUALN(papszLines[iLine], "Projection Setup", 16) )
+        {
+            pszProjParms = papszLines[iLine];
+        }
+    }
+
+    if ( ! ( pszDatum && pszProj && pszProjParms ) )
+        return OGRERR_NOT_ENOUGH_DATA;
 
 /* -------------------------------------------------------------------- */
 /*      Operate on the basis of the projection name.                    */
@@ -139,6 +205,197 @@ OGRErr OGRSpatialReference::importFromOzi( const char *pszDatum,
                  CPLAtof(papszProjParms[1]), CPLAtof(papszProjParms[2]),
                  CPLAtof(papszProjParms[4]), CPLAtof(papszProjParms[5]) );
     }
+
+    else if ( EQUALN(papszProj[1], "(UTM) Universal Transverse Mercator", 35) && nLines > 5 )
+    {
+        /* Look for the UTM zone in the calibration point data */
+        for ( iLine = 5; iLine < nLines; iLine++ )
+        {
+            if ( EQUALN(papszLines[iLine], "Point", 5) )
+            {
+                char    **papszTok = NULL;
+                papszTok = CSLTokenizeString2( papszLines[iLine], ",",
+                                               CSLT_ALLOWEMPTYTOKENS
+                                               | CSLT_STRIPLEADSPACES
+                                               | CSLT_STRIPENDSPACES );
+                if ( CSLCount(papszTok) < 17
+                     || EQUAL(papszTok[2], "")
+                     || EQUAL(papszTok[13], "")
+                     || EQUAL(papszTok[14], "")
+                     || EQUAL(papszTok[15], "")
+                     || EQUAL(papszTok[16], "") )
+                {
+                    CSLDestroy(papszTok);
+                    continue;
+                }
+                SetUTM( CPLAtofM(papszTok[13]), EQUAL(papszTok[16], "N") );
+                CSLDestroy(papszTok);
+                break;
+            }
+        }
+        if ( iLine == nLines )    /* Try to guess the UTM zone */
+        {
+            float fMinLongitude = INT_MAX;
+            float fMaxLongitude = INT_MIN;
+            float fMinLatitude = INT_MAX;
+            float fMaxLatitude = INT_MIN;
+            int bFoundMMPLL = FALSE;
+            for ( iLine = 5; iLine < nLines; iLine++ )
+            {
+                if ( EQUALN(papszLines[iLine], "MMPLL", 5) )
+                {
+                    char    **papszTok = NULL;
+                    papszTok = CSLTokenizeString2( papszLines[iLine], ",",
+                                                   CSLT_ALLOWEMPTYTOKENS
+                                                   | CSLT_STRIPLEADSPACES
+                                                   | CSLT_STRIPENDSPACES );
+                    if ( CSLCount(papszTok) < 4 )
+                    {
+                        CSLDestroy(papszTok);
+                        continue;
+                    }
+                    float fLongitude = CPLAtofM(papszTok[2]);
+                    float fLatitude = CPLAtofM(papszTok[3]);
+                    CSLDestroy(papszTok);
+
+                    bFoundMMPLL = TRUE;
+
+                    if ( fMinLongitude > fLongitude )
+                        fMinLongitude = fLongitude;
+                    if ( fMaxLongitude < fLongitude )
+                        fMaxLongitude = fLongitude;
+                    if ( fMinLatitude > fLatitude )
+                        fMinLatitude = fLatitude;
+                    if ( fMaxLatitude < fLatitude )
+                        fMaxLatitude = fLatitude;
+                }
+            }
+            float fMedianLatitude = ( fMinLatitude + fMaxLatitude ) / 2;
+            float fMedianLongitude = ( fMinLongitude + fMaxLongitude ) / 2;
+            if ( bFoundMMPLL && fMaxLatitude <= 90 )
+            {
+                int nUtmZone;
+                if ( fMedianLatitude >= 56 && fMedianLatitude <= 64 && 
+                     fMedianLongitude >= 3 && fMedianLongitude <= 12 )
+                    nUtmZone = 32;                                             /* Norway exception */
+                else if ( fMedianLatitude >= 72 && fMedianLatitude <= 84 && 
+                         fMedianLongitude >= 0 && fMedianLongitude <= 42 )
+                    nUtmZone = (int) ((fMedianLongitude + 3 ) / 12) * 2 + 31;  /* Svalbard exception */
+                else
+                    nUtmZone = (int) ((fMedianLongitude + 180 ) / 6) + 1;
+                SetUTM( nUtmZone, fMedianLatitude >= 0 );
+            }
+            else
+                CPLDebug( "OSR_Ozi", "UTM Zone not found");
+        }
+    }
+
+    else if ( EQUALN(papszProj[1], "(I) France Zone I", 17) )
+    {
+        SetLCC1SP( 49.5, 2.337229167, 0.99987734, 600000, 1200000 );
+    }
+
+    else if ( EQUALN(papszProj[1], "(II) France Zone II", 19) )
+    {
+        SetLCC1SP( 46.8, 2.337229167, 0.99987742, 600000, 2200000 );
+    }
+
+    else if ( EQUALN(papszProj[1], "(III) France Zone III", 21) )
+    {
+        SetLCC1SP( 44.1, 2.337229167, 0.99987750, 600000, 3200000 );
+    }
+
+    else if ( EQUALN(papszProj[1], "(IV) France Zone IV", 19) )
+    {
+        SetLCC1SP( 42.165, 2.337229167, 0.99994471, 234.358, 4185861.369 );
+    }
+
+/*
+ *  Note : The following projections have not been implemented yet
+ *
+ */
+
+/*
+    else if ( EQUALN(papszProj[1], "(BNG) British National Grid", 27) )
+    {
+    }
+
+    else if ( EQUALN(papszProj[1], "(IG) Irish Grid", 15) )
+    {
+    }
+
+    else if ( EQUALN(papszProj[1], "(NZG) New Zealand Grid", 22) )
+    {
+    }
+
+    else if ( EQUALN(papszProj[1], "(NZTM2) New Zealand TM 2000", 27) )
+    {
+    }
+
+    else if ( EQUALN(papszProj[1], "(SG) Swedish Grid", 27) )
+    {
+    }
+
+    else if ( EQUALN(papszProj[1], "(SUI) Swiss Grid", 26) )
+    {
+    }
+
+    else if ( EQUALN(papszProj[1], "(A)Lambert Azimuthual Equal Area", 32) )
+    {
+    }
+
+    else if ( EQUALN(papszProj[1], "(EQC) Equidistant Conic", 23) )
+    {
+    }
+
+    else if ( EQUALN(papszProj[1], "Polyconic (American)", 20) )
+    {
+    }
+
+    else if ( EQUALN(papszProj[1], "Van Der Grinten", 15) )
+    {
+    }
+
+    else if ( EQUALN(papszProj[1], "Vertical Near-Sided Perspective", 31) )
+    {
+    }
+
+    else if ( EQUALN(papszProj[1], "(WIV) Wagner IV", 15) )
+    {
+    }
+
+    else if ( EQUALN(papszProj[1], "Bonne", 5) )
+    {
+    }
+
+    else if ( EQUALN(papszProj[1], "(MT0) Montana State Plane Zone 2500", 35) )
+    {
+    }
+
+    else if ( EQUALN(papszProj[1], "ITA1) Italy Grid Zone 1", 23) )
+    {
+    }
+
+    else if ( EQUALN(papszProj[1], "ITA2) Italy Grid Zone 2", 23) )
+    {
+    }
+
+    else if ( EQUALN(papszProj[1], "(VICMAP-TM) Victoria Aust.(pseudo AMG)", 38) )
+    {
+    }
+
+    else if ( EQUALN(papszProj[1], "VICGRID) Victoria Australia", 27) )
+    {
+    }
+
+    else if ( EQUALN(papszProj[1], "(VG94) VICGRID94 Victoria Australia", 35) )
+    {
+    }
+
+    else if ( EQUALN(papszProj[1], "Gnomonic", 8) )
+    {
+    }
+*/
 
     else
     {
@@ -285,3 +542,4 @@ other_error:
 
     return OGRERR_FAILURE;
 }
+
