694557ac2de19e195e0bf0e10a2045dfd8b09577
Even Rouault <even.rouault@mines-paris.org>
2012-12-31 20:42:56 +0000

    Remove some code duplication
    git-svn-id: https://svn.osgeo.org/gdal/trunk@25416 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/apps/gdalinfo.c
+++ gdal/apps/gdalinfo.c
@@ -90,19 +90,7 @@ int main( int argc, char ** argv )
         exit(1);
     }
 
-
-    /* Must process GDAL_SKIP before GDALAllRegister(), but we can't call */
-    /* GDALGeneralCmdLineProcessor before it needs the drivers to be registered */
-    /* for the --format or --formats options */
-    for( i = 1; i < argc; i++ )
-    {
-        if( EQUAL(argv[i],"--config") && i + 2 < argc && EQUAL(argv[i + 1], "GDAL_SKIP") )
-        {
-            CPLSetConfigOption( argv[i+1], argv[i+2] );
-
-            i += 2;
-        }
-    }
+    EarlySetConfigOptions(argc, argv);
 
     GDALAllRegister();
 
