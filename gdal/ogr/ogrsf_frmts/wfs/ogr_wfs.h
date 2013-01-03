09aef7630738a90bc360ac4162682b2be5aa05cf
Even Rouault <even.rouault@mines-paris.org>
2013-01-03 19:51:48 +0000

    WFS: for WFS paging, change default base start index to 0 (was 1 before), as now clarified by OGR. Make the base start index configurable for servers such as MapServer 6.0 or 6.2 that still use 1 as the first index (#4504)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@25439 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/wfs/ogr_wfs.h
+++ gdal/ogr/ogrsf_frmts/wfs/ogr_wfs.h
@@ -230,6 +230,7 @@ class OGRWFSDataSource : public OGRDataSource
 
     int                 bPagingAllowed;
     int                 nPageSize;
+    int                 nBaseStartIndex;
 
     int                 bIsGEOSERVER;
 
@@ -296,8 +297,9 @@ class OGRWFSDataSource : public OGRDataSource
 
     CPLHTTPResult*              HTTPFetch( const char* pszURL, char** papszOptions );
 
-    int                         IsPagingAllowed() { return bPagingAllowed; }
-    int                         GetPageSize() { return nPageSize; }
+    int                         IsPagingAllowed() const { return bPagingAllowed; }
+    int                         GetPageSize() const { return nPageSize; }
+    int                         GetBaseStartIndex() const { return nBaseStartIndex; }
 
     void                        LoadMultipleLayerDefn(const char* pszLayerName,
                                                       char* pszNS, char* pszNSVal);
