ed7d79938f36f1cc6ad109093e752102ec972e35
Frank Warmerdam <warmerdam@pobox.com>
2013-06-13 01:31:48 +0000

    rename some files, get rid of stdinc.h, other subjective cleanup
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@26079 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/wms/gdalhttp.cpp
+++ frmts/wms/gdalhttp.cpp
@@ -28,7 +28,7 @@
  * DEALINGS IN THE SOFTWARE.
  ****************************************************************************/
 
-#include "stdinc.h"
+#include "wmsdriver.h"
 
 void CPLHTTPSetOptions(CURL *http_handle, char** papszOptions);
 
