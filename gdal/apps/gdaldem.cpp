37c572d228bdf9f4da7e26956d43d8f71c968891
Even Rouault <even.rouault@mines-paris.org>
2013-07-21 16:18:21 +0000

    Merge branch 'typofixes' of https://github.com/jef-n/gdal into trunk
    From: Even Rouault <even.rouault@mines-paris.org>
    
    git-svn-id: https://svn.osgeo.org/gdal/trunk@26191 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/apps/gdaldem.cpp
+++ gdal/apps/gdaldem.cpp
@@ -2430,7 +2430,7 @@ int main( int argc, char ** argv )
         }
         else if( argv[i][0] == '-' )
         {
-            Usage(CPLSPrintf("Unkown option name '%s'", argv[i]));
+            Usage(CPLSPrintf("Unknown option name '%s'", argv[i]));
         }
         else if( pszSrcFilename == NULL )
         {
--- gdal/apps/ogr2ogr.cpp
+++ gdal/apps/ogr2ogr.cpp
@@ -1329,7 +1329,7 @@ int main( int nArgc, char ** papszArgv )
         }
         else if( papszArgv[iArg][0] == '-' )
         {
-            Usage(CPLSPrintf("Unkown option name '%s'", papszArgv[iArg]));
+            Usage(CPLSPrintf("Unknown option name '%s'", papszArgv[iArg]));
         }
         else if( pszDestDataSource == NULL )
             pszDestDataSource = papszArgv[iArg];
