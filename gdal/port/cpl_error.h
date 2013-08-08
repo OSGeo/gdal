6670526c644fa9c901fcc5ec79e873c97677e083
Even Rouault <even.rouault@mines-paris.org>
2013-08-08 21:15:36 +0000

    Fix prototypes of functions taking a void parameter
    git-svn-id: https://svn.osgeo.org/gdal/trunk@26282 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/port/cpl_error.h
+++ gdal/port/cpl_error.h
@@ -62,7 +62,7 @@ int CPL_DLL CPL_STDCALL CPLGetLastErrorNo( void );
 CPLErr CPL_DLL CPL_STDCALL CPLGetLastErrorType( void );
 const char CPL_DLL * CPL_STDCALL CPLGetLastErrorMsg( void );
 void CPL_DLL * CPL_STDCALL CPLGetErrorHandlerUserData(void);
-void CPL_DLL CPLCleanupErrorMutex();
+void CPL_DLL CPLCleanupErrorMutex( void );
 
 typedef void (CPL_STDCALL *CPLErrorHandler)(CPLErr, int, const char*);
 
