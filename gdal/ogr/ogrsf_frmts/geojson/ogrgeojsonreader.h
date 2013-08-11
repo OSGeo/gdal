88824bf5a743eb0fef31ceb45624c7f49109307c
Even Rouault <even.rouault@mines-paris.org>
2013-08-11 09:30:09 +0000

    Rename internal libjson directory from json-c to libjson to avoid include issues when linking against external libjson-c
    git-svn-id: https://svn.osgeo.org/gdal/trunk@26296 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/geojson/ogrgeojsonreader.h
+++ gdal/ogr/ogrsf_frmts/geojson/ogrgeojsonreader.h
@@ -30,7 +30,7 @@
 #define OGR_GEOJSONREADER_H_INCLUDED
 
 #include <ogr_core.h>
-#include <json-c/json.h> // JSON-C
+#include <json.h> // JSON-C
 
 /************************************************************************/
 /*                         FORWARD DECLARATIONS                         */
