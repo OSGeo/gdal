4be8532a03ac9f170819a8f4ab80ba4f34bbc773
Even Rouault <even.rouault@mines-paris.org>
2013-08-24 15:37:04 +0000

    ECW: fix crash in GDALDeregister_ECW() with ECW SDK 5 called from GDALDestroy() (https://trac.osgeo.org/gdal/ticket/5214)
    git-svn-id: https://svn.osgeo.org/gdal/branches/1.10@26364 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/ecw/ecwdataset.cpp
+++ gdal/frmts/ecw/ecwdataset.cpp
@@ -48,6 +48,8 @@ static int    bNCSInitialized = FALSE;
 
 void ECWInitialize( void );
 
+extern "C" int bInGDALGlobalDestructor;
+
 #define BLOCK_SIZE 256
 
 GDALDataset* ECWDatasetOpenJPEG2000(GDALOpenInfo* poOpenInfo);
@@ -988,12 +990,12 @@ ECWDataset::~ECWDataset()
 
     CPLMutexHolder oHolder( &hECWDatasetMutex );
 
-    // GDAL_CLOSE_JP2ECW_RESOURCE is set to NO by gdaldllmain.cpp/GDALDestroy() so as
+    // bInGDALGlobalDestructor is set to TRUE by gdaldllmain.cpp/GDALDestroy() so as
     // to avoid an issue with the ECW SDK 3.3 where the destructor of CNCSJP2File::CNCSJP2FileVector CNCSJP2File::sm_Files;
     // static ressource allocated in NCJP2File.cpp can be called before GDALDestroy(), causing
     // ECW SDK resources ( CNCSJP2File files ) to be closed before we get here.
     if( poFileView != NULL &&
-        (!bIsJPEG2000 || CSLTestBoolean(CPLGetConfigOption("GDAL_CLOSE_JP2ECW_RESOURCE", "YES"))) )
+        (!bIsJPEG2000 || !bInGDALGlobalDestructor) )
     {
         VSIIOStream *poUnderlyingIOStream = (VSIIOStream *)NULL;
 
@@ -3151,7 +3153,10 @@ void GDALDeregister_ECW( GDALDriver * )
     if( bNCSInitialized )
     {
         bNCSInitialized = FALSE;
-        NCSecwShutdown();
+        if( !bInGDALGlobalDestructor )
+        {
+            NCSecwShutdown();
+        }
     }
 #endif
 #endif
