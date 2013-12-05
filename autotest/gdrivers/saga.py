67ddcae87fd1bcb232e4c658cd5c0f0e111558fb
Even Rouault <even.rouault@mines-paris.org>
2013-12-05 19:11:28 +0000

    SAGA: add read/write support for .prj files (patch by Volker Wichmann, #5316)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@26690 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/gdrivers/saga.py
+++ autotest/gdrivers/saga.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 ###############################################################################
 # $Id$
 #
@@ -42,7 +43,19 @@ import gdaltest
 def saga_1():
 
     tst = gdaltest.GDALTest( 'SAGA', '4byteFloat.sdat', 1, 108 )
-    return tst.testOpen()
+    return tst.testOpen(check_prj = """PROJCS["NAD_1927_UTM_Zone_11N",
+    GEOGCS["GCS_North_American_1927",
+        DATUM["North_American_Datum_1927",
+            SPHEROID["Clarke_1866",6378206.4,294.9786982]],
+        PRIMEM["Greenwich",0],
+        UNIT["Degree",0.017453292519943295]],
+    PROJECTION["Transverse_Mercator"],
+    PARAMETER["latitude_of_origin",0],
+    PARAMETER["central_meridian",-117],
+    PARAMETER["scale_factor",0.9996],
+    PARAMETER["false_easting",500000],
+    PARAMETER["false_northing",0],
+    UNIT["Meter",1]]""")
 
 ###############################################################################
 # Test copying a reference sample with CreateCopy()
@@ -50,7 +63,7 @@ def saga_1():
 def saga_2():
 
     tst = gdaltest.GDALTest( 'SAGA', '4byteFloat.sdat', 1, 108 )
-    return tst.testCreateCopy( new_filename = 'tmp/createcopy.sdat' )
+    return tst.testCreateCopy( new_filename = 'tmp/createcopy.sdat', check_srs = True )
 
 ###############################################################################
 # Test copying a reference sample with Create()
