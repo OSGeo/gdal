f156441a4a9ab7c3ad83784abe1b56607f5991b1
Even Rouault <even.rouault@mines-paris.org>
2013-12-13 22:44:05 +0000

    L1B: report correct values for GCP (https://trac.osgeo.org/gdal/ticket/2403); report more GCPS than before; implement geolocation array
    git-svn-id: https://svn.osgeo.org/gdal/trunk@26710 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/gdrivers/l1b.py
+++ autotest/gdrivers/l1b.py
@@ -66,29 +66,68 @@ class TestL1B:
 
         return 'success'
 
+###############################################################################
+# 
+def l1b_geoloc():
+    try:
+        os.stat('tmp/cache/n12gac8bit.l1b')
+    except:
+        return 'skip'
+
+    ds = gdal.Open('tmp/cache/n12gac8bit.l1b')
+    md = ds.GetMetadata('GEOLOCATION')
+    expected_md = {
+      'LINE_OFFSET' : '0',
+      'LINE_STEP' : '1',
+      'PIXEL_OFFSET' : '0',
+      'PIXEL_STEP' : '1',
+      'X_BAND' : '1',
+      'X_DATASET' : 'L1BGCPS_INTERPOL:"tmp/cache/n12gac8bit.l1b"',
+      'Y_BAND' : '2',
+      'Y_DATASET' : 'L1BGCPS_INTERPOL:"tmp/cache/n12gac8bit.l1b"' }
+    for key in expected_md:
+        if md[key] != expected_md[key]:
+            print(md)
+            return 'fail'
+    ds = None
+
+    ds = gdal.Open('L1BGCPS_INTERPOL:"tmp/cache/n12gac8bit.l1b"')
+    cs = ds.GetRasterBand(1).Checksum()
+    if cs != 62397:
+        gdaltest.post_reason('fail')
+        print(cs)
+        return 'fail'
+    cs = ds.GetRasterBand(2).Checksum()
+    if cs != 52616:
+        gdaltest.post_reason('fail')
+        print(cs)
+        return 'fail'
+
+    return 'success'
 
 
 gdaltest_list = []
 
-l1b_list = [ ('http://download.osgeo.org/gdal/data/l1b', 'n12gac8bit.l1b', 51754, -1, 220),
-             ('http://download.osgeo.org/gdal/data/l1b', 'n12gac10bit.l1b', 46039, -1, 220),
-             ('http://download.osgeo.org/gdal/data/l1b', 'n12gac10bit_ebcdic.l1b', 46039, -1, 220), # 2848
-             ('http://download.osgeo.org/gdal/data/l1b', 'n14gac16bit.l1b', 42286, -1, 220),
-             ('http://download.osgeo.org/gdal/data/l1b', 'n15gac8bit.l1b', 55772, -1, 220),
-             ('http://download.osgeo.org/gdal/data/l1b', 'n16gac10bit.l1b', 6749, -1, 220),
-             ('http://download.osgeo.org/gdal/data/l1b', 'n17gac16bit.l1b', 61561, -1, 220),
-             ('http://www2.ncdc.noaa.gov/docs/podug/data/avhrr', 'frang.1b', 33700, 30000, 220),  # 10 bit guess
-             ('http://www2.ncdc.noaa.gov/docs/podug/data/avhrr', 'franh.1b', 56702, 100000, 220), # 10 bit guess
-             ('http://www2.ncdc.noaa.gov/docs/podug/data/avhrr', 'calfirel.1b', 55071, 30000, 220), # 16 bit guess
-             ('http://www2.ncdc.noaa.gov/docs/podug/data/avhrr', 'rapnzg.1b', 58084, 30000, 220), # 16 bit guess
-             ('ftp://ftp.sat.dundee.ac.uk/misc/testdata/new_noaa/new_klm_format', 'noaa18.n1b', 50229, 50000, 220),
-             ('ftp://ftp.sat.dundee.ac.uk/misc/testdata/metop', 'noaa1b', 62411, 150000, 220)
+l1b_list = [ ('http://download.osgeo.org/gdal/data/l1b', 'n12gac8bit.l1b', 51754, -1, 1938),
+             ('http://download.osgeo.org/gdal/data/l1b', 'n12gac10bit.l1b', 46039, -1, 1887),
+             ('http://download.osgeo.org/gdal/data/l1b', 'n12gac10bit_ebcdic.l1b', 46039, -1, 1887), # 2848
+             ('http://download.osgeo.org/gdal/data/l1b', 'n14gac16bit.l1b', 42286, -1, 2142),
+             ('http://download.osgeo.org/gdal/data/l1b', 'n15gac8bit.l1b', 55772, -1, 2091),
+             ('http://download.osgeo.org/gdal/data/l1b', 'n16gac10bit.l1b', 6749, -1, 2142),
+             ('http://download.osgeo.org/gdal/data/l1b', 'n17gac16bit.l1b', 61561, -1, 2040),
+             ('http://www2.ncdc.noaa.gov/docs/podug/data/avhrr', 'frang.1b', 33700, 30000, 357),  # 10 bit guess
+             ('http://www2.ncdc.noaa.gov/docs/podug/data/avhrr', 'franh.1b', 56702, 100000, 255), # 10 bit guess
+             ('http://www2.ncdc.noaa.gov/docs/podug/data/avhrr', 'calfirel.1b', 55071, 30000, 255), # 16 bit guess
+             ('http://www2.ncdc.noaa.gov/docs/podug/data/avhrr', 'rapnzg.1b', 58084, 30000, 612), # 16 bit guess
+             ('ftp://ftp.sat.dundee.ac.uk/misc/testdata/new_noaa/new_klm_format', 'noaa18.n1b', 50229, 50000, 102),
+             ('ftp://ftp.sat.dundee.ac.uk/misc/testdata/metop', 'noaa1b', 62411, 150000, 408)
            ]
 
 for item in l1b_list:
     ut = TestL1B( item[0], item[1], item[2], item[3], item[4] )
     gdaltest_list.append( (ut.test, item[1]) )
 
+gdaltest_list.append(l1b_geoloc)
 
 if __name__ == '__main__':
 
