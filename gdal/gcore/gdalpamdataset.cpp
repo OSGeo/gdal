851fef7977f27853b4033ce8340e9e2655cc8b96
Even Rouault <even.rouault@mines-paris.org>
2013-12-18 18:17:06 +0000

    PAM .aux.xml and VRT: recognize 'GCPZ' attribute of GCP, in addition to 'Z', since 'GCPZ' is what we currently generate. But GDAL 1.11/2.0 will generate 'Z' for consistency (#5326)
    git-svn-id: https://svn.osgeo.org/gdal/branches/1.10@26728 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/gcore/gdalpamdataset.cpp
+++ gdal/gcore/gdalpamdataset.cpp
@@ -480,7 +480,13 @@ CPLErr GDALPamDataset::XMLInit( CPLXMLNode *psTree, const char *pszUnused )
              
             psGCP->dfGCPX = atof(CPLGetXMLValue(psXMLGCP,"X","0.0"));
             psGCP->dfGCPY = atof(CPLGetXMLValue(psXMLGCP,"Y","0.0"));
-            psGCP->dfGCPZ = atof(CPLGetXMLValue(psXMLGCP,"Z","0.0"));
+            const char* pszZ = CPLGetXMLValue(psXMLGCP,"Z",NULL);
+            if( pszZ == NULL )
+            {
+                /* Note: GDAL 1.10.1 and older generated #GCPZ, but could not read it back */
+                pszZ = CPLGetXMLValue(psXMLGCP,"GCPZ","0.0");
+            }
+            psGCP->dfGCPZ = atof(pszZ);
 
             psPam->nGCPCount++;
         }
