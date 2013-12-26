OSGeo/gdal

commit 05d3df6b4c6fdfa9fab83597bad55dbe0235c5b5

TPS and Polynomial GCP transformations: recognize 'GCPZ' attribute of GCP, in addition to 'Z', since 'GCPZ' is what we currently generate. But GDAL 1.11/2.0 will generate 'Z' for consistency (https://trac.osgeo.org/gdal/ticket/5326)
git-svn-id: https://svn.osgeo.org/gdal/branches/1.10@26732 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/alg/gdal_crs.c
@@ -489,7 +489,13 @@ void *GDALDeserializeGCPTransformer( CPLXMLNode *psTree )
              
             psGCP->dfGCPX = atof(CPLGetXMLValue(psXMLGCP,"X","0.0"));
             psGCP->dfGCPY = atof(CPLGetXMLValue(psXMLGCP,"Y","0.0"));
-            psGCP->dfGCPZ = atof(CPLGetXMLValue(psXMLGCP,"Z","0.0"));
+            const char* pszZ = CPLGetXMLValue(psXMLGCP,"Z",NULL);
+            if( pszZ == NULL )
+            {
+                /* Note: GDAL 1.10.1 and older generated #GCPZ, but could not read it back */
+                pszZ = CPLGetXMLValue(psXMLGCP,"GCPZ","0.0");
+            }
+            psGCP->dfGCPZ = atof(pszZ);
             nGCPCount++;
         }
     }

gdal/alg/gdal_tps.cpp
@@ -369,7 +369,13 @@ void *GDALDeserializeTPSTransformer( CPLXMLNode *psTree )
              
             psGCP->dfGCPX = atof(CPLGetXMLValue(psXMLGCP,"X","0.0"));
             psGCP->dfGCPY = atof(CPLGetXMLValue(psXMLGCP,"Y","0.0"));
-            psGCP->dfGCPZ = atof(CPLGetXMLValue(psXMLGCP,"Z","0.0"));
+            const char* pszZ = CPLGetXMLValue(psXMLGCP,"Z",NULL);
+            if( pszZ == NULL )
+            {
+                /* Note: GDAL 1.10.1 and older generated #GCPZ, but could not read it back */
+                pszZ = CPLGetXMLValue(psXMLGCP,"GCPZ","0.0");
+            }
+            psGCP->dfGCPZ = atof(pszZ);
             nGCPCount++;
         }
     }

commit 3517276c23e7dc042fca15d11a3b36305057e7a5

Fix for the build issue introduced in r26732 (#5326)
git-svn-id: https://svn.osgeo.org/gdal/branches/1.10@26747 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/alg/gdal_crs.c
@@ -470,6 +470,7 @@ void *GDALDeserializeGCPTransformer( CPLXMLNode *psTree )
         for( psXMLGCP = psGCPList->psChild; psXMLGCP != NULL; 
              psXMLGCP = psXMLGCP->psNext )
         {
+            const char* pszZ;
             GDAL_GCP *psGCP = pasGCPList + nGCPCount;
 
             if( !EQUAL(psXMLGCP->pszValue,"GCP") || 
@@ -489,7 +490,7 @@ void *GDALDeserializeGCPTransformer( CPLXMLNode *psTree )
              
             psGCP->dfGCPX = atof(CPLGetXMLValue(psXMLGCP,"X","0.0"));
             psGCP->dfGCPY = atof(CPLGetXMLValue(psXMLGCP,"Y","0.0"));
-            const char* pszZ = CPLGetXMLValue(psXMLGCP,"Z",NULL);
+            pszZ = CPLGetXMLValue(psXMLGCP,"Z",NULL);
             if( pszZ == NULL )
             {
                 /* Note: GDAL 1.10.1 and older generated #GCPZ, but could not read it back */