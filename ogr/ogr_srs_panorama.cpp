6283c93233ca45472a5d8015be8bdb6568380560
Dmitry Baryshnikov <polimax at mail.ru>
2014-01-02 13:38:26 +0000

    Add several new projections support to SXF driver (https://trac.osgeo.org/gdal/ticket/4331)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@26771 f0d54148-0727-0410-94bb-9a71ac55c965

--- ogr/ogr_srs_panorama.cpp
+++ ogr/ogr_srs_panorama.cpp
@@ -67,7 +67,7 @@ CPL_CVSID("$Id$");
 #define PAN_PROJ_EQC    27L     // Equirectangular
 #define PAN_PROJ_CEA    28L     // Cylindrical Equal Area (Lambert)
 #define PAN_PROJ_IMWP   29L     // International Map of the World Polyconic
-
+#define PAN_PROJ_MILLER 34L     // Miller
 /************************************************************************/
 /*  "Panorama" datum codes.                                             */
 /************************************************************************/
@@ -126,7 +126,18 @@ static const long aoEllips[] =
     7015,   // Everest, 1830
     7004,   // Bessel, 1841
     7001,   // Airy, 1830
-    7030    // WGS, 1984 (GPS)
+    7030,   // WGS, 1984 (GPS)
+    0,      // FIXME: PZ90.02
+    7019,   // GRS, 1980 (NAD1983)
+    7022,   // International, 1924 (Hayford, 1909) XXX?
+    7036,   // South American, 1969
+    7021,   // Indonesian, 1974
+    7020,   // Helmert 1906
+    0,      // FIXME: Fisher 1960
+    0,      // FIXME: Fisher 1968
+    0,      // FIXME: Haff 1960
+    7042,   // Everest, 1830
+    7003   // Australian National, 1965
 };
 
 #define NUMBER_OF_ELLIPSOIDS    (sizeof(aoEllips)/sizeof(aoEllips[0]))
@@ -390,6 +401,12 @@ OGRErr OGRSpatialReference::importFromPanorama( long iProjSys, long iDatum,
                              TO_DEGREES * padfPrjParams[1],
                              TO_DEGREES * padfPrjParams[3],
                              padfPrjParams[5], padfPrjParams[6] );
+            break;
+
+        case PAN_PROJ_MILLER:
+            SetMC(TO_DEGREES * padfPrjParams[5],
+                TO_DEGREES * padfPrjParams[4],
+                padfPrjParams[6], padfPrjParams[7]);
             break;
 
         default:
