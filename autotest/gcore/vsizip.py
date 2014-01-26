5badbaa581e070c5d8fef797e3c2a58e38c46677
Even Rouault <even.rouault@mines-paris.org>
2014-01-26 13:48:53 +0000

    /vsizip/: use the extented field for UTF-8 filenames to retrieve UTF-8 filenames when the base filename field contains CP437 filename (#5361)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@26882 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/gcore/vsizip.py
+++ autotest/gcore/vsizip.py
@@ -326,6 +326,25 @@ def vsizip_6():
 
     return 'success'
 
+###############################################################################
+# Test that we use the extented field for UTF-8 filenames (#5361)
+
+def vsizip_7():
+
+    content = gdal.ReadDir("/vsizip/data/cp866_plus_utf8.zip")
+    ok = 0
+    try:
+        exec("if content == [u'\u0430\u0431\u0432\u0433\u0434\u0435', u'\u0436\u0437\u0438\u0439\u043a\u043b']: ok = 1")
+    except:
+        if content == ['\u0430\u0431\u0432\u0433\u0434\u0435', '\u0436\u0437\u0438\u0439\u043a\u043b']:
+            ok = 1
+
+    if ok == 0:
+        gdaltest.post_reason('bad content')
+        print(content)
+        return 'fail'
+
+    return 'success'
 
 gdaltest_list = [ vsizip_1,
                   vsizip_2,
@@ -333,6 +352,7 @@ gdaltest_list = [ vsizip_1,
                   vsizip_4,
                   vsizip_5,
                   vsizip_6,
+                  vsizip_7,
                   ]
 
 
