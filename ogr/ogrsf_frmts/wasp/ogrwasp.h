09f52002e96321bc71450f74ca01641f76d40060
Even Rouault <even.rouault@mines-paris.org>
2014-03-13 20:25:06 +0000

    Add OGR WasP driver (by Vincent Mora, https://trac.osgeo.org/gdal/ticket/5416)
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@27030 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ ogr/ogrsf_frmts/wasp/ogrwasp.h
@@ -0,0 +1,204 @@
+/******************************************************************************
+ *
+ * Project:  WAsP Translator
+ * Purpose:  Definition of classes for OGR .map driver.
+ * Author:   Vincent Mora, vincent dot mora at oslandia dot com
+ *
+ ******************************************************************************
+ * Copyright (c) 2014, Oslandia <info at oslandia dot com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+
+#ifndef _OGR_WASP_H_INCLUDED
+#define _OGR_WASP_H_INCLUDED
+
+#include "ogrsf_frmts.h"
+
+#include <memory>
+#include <fstream>
+#include <vector>
+
+/************************************************************************/
+/*                             OGRWAsPLayer                             */
+/************************************************************************/
+
+class OGRWAsPLayer : public OGRLayer
+{
+    /* stuff for polygon processing */
+
+    /* note if shared ptr are available, replace the ptr in the two structs */
+    /* and remove deletion of array elements in ~OGRWAsPLayer() */
+    struct Zone
+    {
+        OGREnvelope       oEnvelope;
+        OGRPolygon *      poPolygon;
+        double            dfZ;
+    };
+
+    struct Boundary
+    {
+        OGRLineString * poLine;
+        double          dfLeft;
+        double          dfRight;
+    };
+
+    const bool              bMerge;
+    std::vector< Zone >     oZones;
+    std::vector< Boundary > oBoundaries;
+
+    static bool isEqual( const double & dfRouhness1, const double & dfRouhness2 ) { return fabs( dfRouhness1 - dfRouhness2 ) < 1e-3; }
+
+    /* end of stuff for polygon processing */
+
+    int iFeatureCount;
+
+    const CPLString       sName;
+    VSILFILE *            hFile;
+
+    /* for roughness zone, two fields for linestrings (left/right), one for polygons */
+    /* for elevation, one fiels (height) */
+    const CPLString       sFirstField;
+    const CPLString       sSecondField;
+    const CPLString       sGeomField;
+    int                   iFirstFieldIdx;
+    int                   iSecondFieldIdx;
+    int                   iGeomFieldIdx;
+
+    OGRFeatureDefn *      poLayerDefn;
+    OGRSpatialReference * poSpatialReference;
+
+    int                   iOffsetFeatureBegin;
+
+    enum OpenMode {READ_ONLY, WRITE_ONLY};
+    OpenMode              eMode;
+
+    std::auto_ptr<double> pdfTolerance;
+
+    OGRErr                WriteRoughness( OGRLineString *,
+                                          const double & dfZleft,
+                                          const double & dfZright );
+    OGRErr                WriteRoughness( OGRPolygon *,
+                                          const double & dfZ );
+    OGRErr                WriteRoughness( OGRGeometry *,
+                                          const double & dfZleft,
+                                          const double & dfZright );
+
+    OGRErr                WriteElevation( OGRLineString *, const double & dfZ );
+    OGRErr                WriteElevation( OGRGeometry *, const double & dfZ );
+
+    static double AvgZ( OGRLineString * poGeom );
+    static double AvgZ( OGRPolygon * poGeom );
+    static double AvgZ( OGRGeometryCollection * poGeom );
+    static double AvgZ( OGRGeometry * poGeom );
+  public:
+                        /* For writing */
+                        /* Takes ownership of poTolerance */
+                        OGRWAsPLayer( const char * pszName, 
+                                      VSILFILE * hFile,
+                                      OGRSpatialReference * poSpatialRef,
+                                      const CPLString & sFirstField,
+                                      const CPLString & sSecondField,
+                                      const CPLString & sGeomField,
+                                      bool bMerge,
+                                      double * poTolerance );
+
+                        /* For reading */
+                        OGRWAsPLayer( const char * pszName, 
+                                      VSILFILE * hFile,
+                                      OGRSpatialReference * poSpatialRef );
+
+                        ~OGRWAsPLayer();
+
+    virtual OGRFeatureDefn *    GetLayerDefn() { return poLayerDefn; }
+
+    virtual void        ResetReading();
+    virtual int         TestCapability( const char * );
+
+    virtual OGRErr      CreateField( OGRFieldDefn *poField,
+                                     int bApproxOK = TRUE );
+    virtual OGRErr      CreateGeomField( OGRGeomFieldDefn *poGeomField,
+                                         int bApproxOK = TRUE );
+
+    virtual OGRErr      CreateFeature( OGRFeature * poFeature );
+
+    virtual OGRFeature *GetNextFeature();
+    OGRFeature *GetNextRawFeature();
+    virtual OGRwkbGeometryType  GetGeomType() { return wkbLineString25D; }
+    virtual OGRSpatialReference *GetSpatialRef() { return poSpatialReference; }
+    virtual const char *GetName() { return sName.c_str(); }
+};
+
+/************************************************************************/
+/*                           OGRWAsPDataSource                          */
+/************************************************************************/
+
+class OGRWAsPDataSource : public OGRDataSource
+{
+    CPLString                     sFilename;
+    VSILFILE *                    hFile;
+    std::auto_ptr<OGRWAsPLayer>   oLayer;
+
+    void               GetOptions(CPLString & sFirstField, 
+                                  CPLString & sSecondField,
+                                  CPLString & sGeomField,
+                                  bool &      bMerge) const;
+  public:
+                        /** @note takes ownership of hFile (i.e. responsibility for closing) */
+                        OGRWAsPDataSource( const char * pszName, 
+                                           VSILFILE * hFile );
+                        ~OGRWAsPDataSource();
+
+    virtual const char *GetName() { return sFilename.c_str(); }
+    virtual int         GetLayerCount() { return oLayer.get() ? 1 : 0; }
+    virtual OGRLayer   *GetLayer( int );
+    virtual OGRLayer   *GetLayerByName( const char * );
+
+    virtual OGRLayer   *CreateLayer( const char *pszName, 
+                                     OGRSpatialReference *poSpatialRef = NULL,
+                                     OGRwkbGeometryType eGType = wkbUnknown,
+                                     char ** papszOptions = NULL );
+    
+    virtual int        TestCapability( const char * );
+    OGRErr             Load( bool bSilent = false );
+};
+
+/************************************************************************/
+/*                             OGRWAsPDriver                            */
+/************************************************************************/
+
+class OGRWAsPDriver : public OGRSFDriver
+{
+
+  public:
+                                ~OGRWAsPDriver() {}
+
+    virtual const char*         GetName() { return "WAsP"; }
+    virtual OGRDataSource*      Open( const char *, int );
+    
+    virtual OGRDataSource       *CreateDataSource( const char *pszName,
+                                                   char ** = NULL );
+
+    virtual OGRErr 	        DeleteDataSource (const char *pszName);
+
+    virtual int                 TestCapability( const char * );
+};
+
+
+#endif /* ndef _OGR_WASP_H_INCLUDED */
---
+++ ogr/ogrsf_frmts/wasp/ogrwaspdatasource.cpp
@@ -0,0 +1,278 @@
+/******************************************************************************
+ * $Id: ogrwaspdatasource.cpp 25307 2012-12-15 09:04:40Z rouault $
+ *
+ * Project:  WAsP Translator
+ * Purpose:  Implements OGRWAsPDataSource class
+ * Author:   Vincent Mora, vincent dot mora at oslandia dot com
+ *
+ ******************************************************************************
+ * Copyright (c) 2014, Oslandia <info at oslandia dot com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+
+#include "ogrwasp.h"
+#include "cpl_conv.h"
+#include "cpl_string.h"
+
+#include <cassert>
+#include <sstream>
+
+/************************************************************************/
+/*                          OGRWAsPDataSource()                          */
+/************************************************************************/
+
+OGRWAsPDataSource::OGRWAsPDataSource( const char * pszName, 
+                                      VSILFILE * hFileHandle )
+    : sFilename( pszName )
+    , hFile( hFileHandle )
+
+{
+}
+
+/************************************************************************/
+/*                         ~OGRWAsPDataSource()                          */
+/************************************************************************/
+
+OGRWAsPDataSource::~OGRWAsPDataSource()
+
+{
+    oLayer.reset(); /* we write to file int layer dtor */
+    VSIFCloseL( hFile ); /* nothing smart can be done here in case of error */
+}
+
+/************************************************************************/
+/*                           TestCapability()                           */
+/************************************************************************/
+
+int OGRWAsPDataSource::TestCapability( const char * pszCap )
+
+{
+    return EQUAL(pszCap,ODsCCreateLayer) ;
+}
+
+/************************************************************************/
+/*                              GetLayerByName()                        */
+/************************************************************************/
+
+OGRLayer *OGRWAsPDataSource::GetLayerByName( const char * pszName )
+
+{
+    return ( oLayer.get() && EQUAL( pszName, oLayer->GetName() ) ) 
+        ? oLayer.get() 
+        : NULL;
+}
+
+/************************************************************************/
+/*                              Load()                                  */
+/************************************************************************/
+
+OGRErr OGRWAsPDataSource::Load(bool bSilent)
+
+{
+    /* if we don't have a layer, we read from file */
+    if ( oLayer.get() )
+    {
+        if (!bSilent) CPLError( CE_Failure, CPLE_NotSupported, "layer already loaded");
+        return OGRERR_FAILURE;
+    }
+    /* Parse the first line of the file in case it'a a spatial ref*/
+    const char * pszLine = CPLReadLineL( hFile );
+    if ( !pszLine )
+    {
+        if (!bSilent) CPLError( CE_Failure, CPLE_FileIO, "empty file");
+        return OGRERR_FAILURE;
+    }
+    CPLString sLine( pszLine );
+    sLine = sLine.substr(0, sLine.find("|"));
+    OGRSpatialReference * poSpatialRef = new OGRSpatialReference;
+    if ( poSpatialRef->importFromProj4( sLine.c_str() ) != OGRERR_NONE )
+    {
+        if (!bSilent) CPLError( CE_Warning, CPLE_FileIO, "Cannot parse spatial reference");
+        delete poSpatialRef;
+        poSpatialRef = NULL;
+    }
+
+    /* TODO Parse those line since they define a coordinate transformation */
+    CPLReadLineL( hFile );
+    CPLReadLineL( hFile );
+    CPLReadLineL( hFile );
+
+    oLayer.reset( new OGRWAsPLayer( sFilename.substr(0,sFilename.length()-4).c_str(), 
+                                    hFile, 
+                                    poSpatialRef ) );
+    if (poSpatialRef) poSpatialRef->Release();
+
+    const int iOffset = VSIFTellL( hFile );
+    pszLine = CPLReadLineL( hFile );
+    if ( !pszLine ) 
+    {
+        if (!bSilent) CPLError( CE_Failure, CPLE_FileIO, "no feature in file");
+        oLayer.reset();
+        return OGRERR_FAILURE;
+    }
+    
+    double dfValues[4];
+    int iNumValues = 0;
+    {
+        std::istringstream iss(pszLine);
+        while ( iNumValues < 4 && (iss >> dfValues[iNumValues] ) ){ ++iNumValues; }
+
+        if ( iNumValues < 2 )
+        {
+            if (!bSilent && iNumValues) 
+                CPLError(CE_Failure, CPLE_FileIO, "no enough values" );
+            else if (!bSilent) 
+                CPLError(CE_Failure, CPLE_FileIO, "no feature in file" );
+
+            oLayer.reset();
+            return OGRERR_FAILURE;
+        }
+    }
+
+    if ( iNumValues == 3 || iNumValues == 4 )
+    {
+        OGRFieldDefn left("z_left", OFTReal);
+        OGRFieldDefn right("z_right", OFTReal);
+        oLayer->CreateField( &left );
+        oLayer->CreateField( &right );
+    }
+    if ( iNumValues == 2 || iNumValues == 4 )
+    {
+        OGRFieldDefn height("elevation", OFTReal);
+        oLayer->CreateField( &height );
+    }
+
+    VSIFSeekL( hFile, iOffset, SEEK_SET );	
+    return OGRERR_NONE;
+}
+
+/************************************************************************/
+/*                              GetLayer()                              */
+/************************************************************************/
+
+
+OGRLayer *OGRWAsPDataSource::GetLayer( int iLayer )
+
+{
+    return ( iLayer == 0 ) ? oLayer.get() : NULL;
+}
+
+
+/************************************************************************/
+/*                              CreateLayer()                           */
+/************************************************************************/
+
+OGRLayer *OGRWAsPDataSource::CreateLayer(const char *pszName, 
+                                     OGRSpatialReference *poSpatialRef,
+                                     OGRwkbGeometryType eGType,
+                                     char ** papszOptions)
+
+{
+
+    if ( eGType != wkbLineString
+      && eGType != wkbLineString25D
+      && eGType != wkbMultiLineString
+      && eGType != wkbMultiLineString25D
+      && eGType != wkbPolygon
+      && eGType != wkbPolygon25D
+      && eGType != wkbMultiPolygon
+      && eGType != wkbMultiPolygon25D )
+    {
+        CPLError( CE_Failure, 
+                CPLE_NotSupported, 
+                "unsupported geometry type %s", OGRGeometryTypeToName( eGType ) );
+        return NULL;
+    }
+
+    if ( oLayer.get() )
+    {
+        CPLError( CE_Failure, 
+                CPLE_NotSupported, 
+                "this data source does not support more than one layer" );
+        return NULL;
+    }
+
+    CPLString sFirstField, sSecondField, sGeomField;
+
+    const char *pszFields = CSLFetchNameValue( papszOptions, "WASP_FIELDS" );
+    const CPLString sFields( pszFields ? pszFields : "" );
+    if ( ! sFields.empty() )
+    {
+        /* parse the coma separated list of fields */
+        const size_t iComa = sFields.find(',');
+        if ( std::string::npos != iComa )
+        {
+            sFirstField = sFields.substr(0, iComa); 
+            sSecondField = sFields.substr( iComa + 1 );
+        }
+        else
+        {
+            sFirstField = sFields;
+        }
+    }
+
+    const char *pszGeomField = CSLFetchNameValue( papszOptions, "WASP_GEOM_FIELD" );
+    sGeomField = CPLString( pszGeomField ? pszGeomField : "" );
+
+    const bool bMerge = CSLTestBoolean(CSLFetchNameValueDef( papszOptions, "WASP_MERGE", "YES" ));
+
+    double * pdfTolerance = NULL;
+    const char *pszToler = CSLFetchNameValue( papszOptions, "WASP_TOLERANCE" );
+    if (pszToler)
+    {
+        pdfTolerance = new double;
+        if (!(std::istringstream( pszToler ) >> *pdfTolerance ))
+        {
+            delete pdfTolerance;
+            CPLError( CE_Failure, 
+                    CPLE_IllegalArg, 
+                    "cannot set tolerance from %s", pszToler );
+            return NULL;
+        }
+    }
+
+    oLayer.reset( new OGRWAsPLayer( pszName, 
+                                    hFile, 
+                                    poSpatialRef,
+                                    sFirstField, 
+                                    sSecondField,
+                                    sGeomField,
+                                    bMerge,
+                                    pdfTolerance ) );
+
+    char * ppszWktSpatialRef = NULL ;
+    if ( poSpatialRef 
+            && poSpatialRef->exportToProj4( &ppszWktSpatialRef ) == OGRERR_NONE )
+    {
+        VSIFPrintfL( hFile, "%s\n", ppszWktSpatialRef );
+        OGRFree( ppszWktSpatialRef );
+    }
+    else
+    {
+        VSIFPrintfL( hFile, "no spatial ref sys\n" );
+    }
+
+    VSIFPrintfL( hFile, "    0.0 0.0 0.0 0.0\n" );
+    VSIFPrintfL( hFile, "    1.0 0.0 1.0 0.0\n" );
+    VSIFPrintfL( hFile, "    1.0 0.0\n" );
+    return oLayer.get();
+}
+
+
