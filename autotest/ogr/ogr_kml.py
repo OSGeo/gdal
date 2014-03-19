421162c7941cbed90e80664cae66ab784f6e8c1b
Even Rouault <even.rouault@mines-paris.org>
2014-03-19 12:27:33 +0000

    KML and LIBKML: assign an id to the root Document to satisfy ATC 21 when there is no layer
    git-svn-id: https://svn.osgeo.org/gdal/trunk@27051 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/ogr/ogr_kml.py
+++ autotest/ogr/ogr_kml.py
@@ -748,7 +748,7 @@ def ogr_kml_write_schema():
 
     expected_content="""<?xml version="1.0" encoding="utf-8" ?>
 <kml xmlns="http://www.opengis.net/kml/2.2">
-<Document>
+<Document id="root_doc">
 <Schema name="lyr" id="lyr">
     <SimpleField name="strfield" type="string"></SimpleField>
     <SimpleField name="intfield" type="int"></SimpleField>
@@ -784,7 +784,7 @@ def ogr_kml_empty_layer():
 
     expected_content="""<?xml version="1.0" encoding="utf-8" ?>
 <kml xmlns="http://www.opengis.net/kml/2.2">
-<Document>
+<Document id="root_doc">
 <Folder><name>empty</name>
 </Folder>
 </Document></kml>"""
@@ -814,7 +814,7 @@ def ogr_kml_two_layers():
     # FIXME: the schema for lyr should be written before the first Folter for XML compliance
     expected_content="""<?xml version="1.0" encoding="utf-8" ?>
 <kml xmlns="http://www.opengis.net/kml/2.2">
-<Document>
+<Document id="root_doc">
 <Folder><name>empty</name>
 </Folder>
 <Folder><name>lyr</name>
