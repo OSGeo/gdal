e3a28420d03a6306862a7168744fd65a86f01620
Even Rouault <even.rouault@mines-paris.org>
2014-04-05 21:51:35 +0000

    Add Vagrantfile
    git-svn-id: https://svn.osgeo.org/gdal/trunk/gdal@27133 f0d54148-0727-0410-94bb-9a71ac55c965

---
+++ scripts/vagrant/openjpeg.sh
@@ -0,0 +1,19 @@
+NUMTHREADS=2
+if [[ -f /sys/devices/system/cpu/online ]]; then
+	# Calculates 1.5 times physical threads
+	NUMTHREADS=$(( ( $(cut -f 2 -d '-' /sys/devices/system/cpu/online) + 1 ) * 15 / 10  ))
+fi
+#NUMTHREADS=1 # disable MP
+export NUMTHREADS
+
+wget http://sourceforge.net/projects/openjpeg.mirror/files/openjpeg-2.0.0.tar.gz/download -O openjpeg-2.0.0.tar.gz
+tar xvzf openjpeg-2.0.0.tar.gz
+cd openjpeg-2.0.0
+mkdir build
+cd build
+cmake ..
+
+make -j $NUMTHREADS
+sudo make install
+
+cd ../..
