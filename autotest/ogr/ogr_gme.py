7f39926b2d8166b994e99ef2aff29e29f6ae9a56
Wolf Bergenheim <wolf+grass@bergenheim.net>
2014-04-12 18:10:20 +0000

    Implemented write capabilities.
    * batchPatch, batchCreate and batchDelete
    * table creation
    
    Known issues:
    * After creating a table it's not possible to read it
    * OGRGMEDataset doesn't know how to create tables (it still uses open)
    
    git-svn-id: https://svn.osgeo.org/gdal/trunk@27172 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/ogr/ogr_gme.py
+++ autotest/ogr/ogr_gme.py
@@ -107,7 +107,8 @@ def ogr_gme_read():
     if feature.YR94_ID is not None:
         gdaltest.post_reason('ignored field is not ignored')
         return 'fail'
- 
+
+    ds = None
     return 'success'
 
 ###############################################################################
@@ -121,60 +122,77 @@ def ogr_gme_write():
         ogrtest.gme_can_write = False
         return 'skip'
 
-    table_id = '08857392491625866347-07947734823975336729'
-    ds = ogr.Open('GME:tables=%s' % table_id)
+    project_id = '09572813676992841461'
+    ds = ogr.Open('GME:project=%s' % project_id, update = 1)
 
     if ds is None:
         ogrtest.gme_can_write = False
         return 'skip'
     ogrtest.gme_can_write = True
 
-#    import random
-#    ogrtest.gme_rand_val = random.randint(0,2147000000)
-#    table_name = "test_%d" % ogrtest.gme_rand_val
+    import random
+    import time
+    ogrtest.gme_rand_val = random.randint(0,2147000000)
+    table_name = "test_%d" % ogrtest.gme_rand_val
 
-#    lyr = ds.CreateLayer(table_name)
-#    lyr.CreateField(ogr.FieldDefn('strcol', ogr.OFTString))
-#    lyr.CreateField(ogr.FieldDefn('numcol', ogr.OFTReal))
-    lyr = ds.GetLayer(0)
-    count = lyr.GetFeatureCount()
+    lyr = ds.CreateLayer(table_name, geom_type=ogr.wkbPolygon)
+#                         options='drafAccessList=19821b415820e9ea:publishedAccessList=e4b26d3d3f4a190d')
+    lyr.CreateField(ogr.FieldDefn('strcol', ogr.OFTString))
+    lyr.CreateField(ogr.FieldDefn('dblcol', ogr.OFTReal))
+    lyr.CreateField(ogr.FieldDefn('intcol', ogr.OFTInteger))
 
-    if lyr is None:
+    feature = ogr.Feature(lyr.GetLayerDefn())
+
+    feature.SetField('strcol', 'foo')
+    feature.SetField('dblcol', 3.45)
+    feature.SetField('intcol', 11)
+    feature.SetField('gx_id', 'GDAL-1')
+    expected_wkt = "POLYGON ((1 1,4 1,4 4,1 4,1 1),(2 2,3 2,3 3,2 3,2 2))"
+    geom = ogr.CreateGeometryFromWkt(expected_wkt)
+    feature.SetGeometry(geom)
+
+    time.sleep(5)
+
+    if lyr.CreateFeature(feature) != 0:
+        gdaltest.post_reason('CreateFeature() failed')
         return 'fail'
 
-    feature = lyr.GetNextFeature()
+    time.sleep(5)
 
-    feature.SetField('YR94_', feature.YR94_ID)
+    feature.SetField('strcol', 'bar')
     if lyr.SetFeature(feature) != 0:
         gdaltest.post_reason('SetFeature() failed')
         return 'fail'
 
-    feature = ogr.Feature(lyr.GetLayerDefn())
-    feature.SetField('AREA', 8.0)
-    feature.SetField('PERIMETER', 9.0)
-    feature.SetField('YR94_', "1102")
-    feature.SetField('YR94_ID', "1103")
-    feature.SetField('ABBREVNAME', "Wolfstein")
-    feature.SetField('FIPS_CODE', "WB")
-    feature.SetField('PR_POP2000', 11.0)
-    feature.SetField('P_0_14_89', 4.0)
-    feature.SetField('P_15_64_89', 11.0)
-    feature.SetField('LIFE_EXP', 236.0)
-    feature.SetField('LIFE_EXP_F', 77.25)
-    expected_wkt = "POLYGON ((1 4,1 1,4 1,4 4,1 4),(2 3,2 2,3 2,3 3,2 3))"
-    geom = ogr.CreateGeometryFromWkt(expected_wkt)
-    feature.SetGeometry(geom)
-    if lyr.CreateFeature(feature) != 0:
-        gdaltest.post_reason('CreateFeature() failed')
-        return 'fail'
-    fid = feature.GetFID()
-    if fid < 1:
-        gdaltest.post_reason('CreateFeature() failed to set feature ID')
+    time.sleep(5)
+
+    lyr.ResetReading()
+    feature = lyr.GetNextFeature()
+    if not feature:
+        gdaltest.post_reason('GetNextFeature() did not return a feature o_O')
         return 'fail'
 
-    if lyr.DeleteFeature(fid) != 0:
-        gdaltest.post_reason('DeleteFeature() failed')
+    if feature.GetFieldAsString('gx_id') != 'GDAL-1':
+        gdaltest.post_reason('GetNextFeature() did not get expected feature')
+        feature.DumpReadable()
         return 'fail'
+
+    got_wkt = feature.GetGeometryRef().ExportToWkt()
+    if got_wkt != expected_wkt:
+        gdaltest.post_reason('did not get expected geometry')
+        print(expected_wkt)
+        print(got_wkt)
+        return 'fail'
+
+#    if lyr.GetFeatureCount() != 1:
+#        gdaltest.post_reason('GetFeatureCount() did not return expected value')
+#        return 'fail'
+
+#    time.sleep(1.5)
+
+#    if lyr.DeleteFeature(feature.GetFID()) != 0:
+#        gdaltest.post_reason('DeleteFeature() failed')
+#        return 'fail'
 
     ds = None
     return 'success'
