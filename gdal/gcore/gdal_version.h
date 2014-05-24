cedfe8f89de2882c953b2b0dc1e9dfcec54ee099
Even Rouault <even.rouault@mines-paris.org>
2014-05-24 12:28:35 +0000

    Update version number to 2.0.0
    From: Even Rouault <even.rouault@mines-paris.org>
    
    git-svn-id: https://svn.osgeo.org/gdal/trunk@27385 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/gcore/gdal_version.h
+++ gdal/gcore/gdal_version.h
@@ -4,8 +4,8 @@
 /* -------------------------------------------------------------------- */
 
 #ifndef GDAL_VERSION_MAJOR
-#  define GDAL_VERSION_MAJOR    1
-#  define GDAL_VERSION_MINOR    12
+#  define GDAL_VERSION_MAJOR    2
+#  define GDAL_VERSION_MINOR    0
 #  define GDAL_VERSION_REV      0
 #  define GDAL_VERSION_BUILD    0
 #endif
@@ -25,5 +25,5 @@
 #  define GDAL_RELEASE_DATE     20140416
 #endif
 #ifndef GDAL_RELEASE_NAME
-#  define GDAL_RELEASE_NAME     "1.12dev"
+#  define GDAL_RELEASE_NAME     "2.0.0dev"
 #endif
