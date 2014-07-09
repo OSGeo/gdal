be647b133b7c9e75d6c7d58d125714b35731dba9
Blake Thompson <flippmoke@gmail.com>
2014-07-09 15:43:41 +0000

    Updated postgisraster driver with proper accessor rather then accessing protected object.

--- gdal/frmts/postgisraster/postgisrasterrasterband.cpp
+++ gdal/frmts/postgisraster/postgisrasterrasterband.cpp
@@ -565,7 +565,7 @@ CPLErr PostGISRasterRasterBand::IRasterIO(GDALRWFlag eRWFlag, int nXOff,
     int bAllBandCaching = FALSE;
     if (nTilesToFetch > 0)
     {
-        GIntBig nCacheMax = (GIntBig) poDS->poRasterBlockManager->GetCacheMax64();
+        GIntBig nCacheMax = (GIntBig) GetRasterBlockManager()->GetCacheMax64();
         if( nMemoryRequiredForTiles > nCacheMax )
         {
             CPLDebug("PostGIS_Raster",
