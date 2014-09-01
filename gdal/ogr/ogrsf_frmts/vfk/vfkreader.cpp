OSGeo/gdal

commit d28f68b3d6432764851df11b25c0178ba224ae55

VFK: recode also header values
git-svn-id: https://svn.osgeo.org/gdal/trunk@27618 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/ogr/ogrsf_frmts/vfk/vfkreader.cpp
@@ -435,7 +435,7 @@ void VFKReader::AddInfo(const char *pszLine)
 {
     int         i, iKeyLength, iValueLength;
     int         nSkip;
-    char       *pszKey, *pszValue;
+    char       *pszKey, *pszValue, *pszValueEnc;
     const char *poChar, *poKey;
     CPLString   key, value;
     
@@ -478,15 +478,21 @@ void VFKReader::AddInfo(const char *pszLine)
 
     pszValue[iValueLength] = '\0';
 
-    poInfo[pszKey] = pszValue;
 
+    /* recode values, assuming Latin2 */
     if (EQUAL(pszKey, "CODEPAGE")) {
         if (!EQUAL(pszValue, "WE8ISO8859P2"))
             m_bLatin2 = FALSE;
     }
 
+    pszValueEnc = CPLRecode(pszValue,
+                            m_bLatin2 ? CPLStrdup("ISO-8859-2") : CPLStrdup("WINDOWS-1250"),
+                            CPL_ENC_UTF8);
+    poInfo[pszKey] = pszValueEnc;
+        
     CPLFree(pszKey);
     CPLFree(pszValue);
+    CPLFree(pszValueEnc);
 }
 
 /*!

commit be12f03b93a380ca0118bcfb16e9739811a134aa

VFK driver: fix memory leak introduced in r27618 (https://trac.osgeo.org/gdal/ticket/5631)
git-svn-id: https://svn.osgeo.org/gdal/trunk@27627 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/ogr/ogrsf_frmts/vfk/vfkreader.cpp
@@ -517,7 +517,7 @@ void VFKReader::AddInfo(const char *pszLine)
     }
 
     pszValueEnc = CPLRecode(pszValue,
-                            m_bLatin2 ? CPLStrdup("ISO-8859-2") : CPLStrdup("WINDOWS-1250"),
+                            m_bLatin2 ? "ISO-8859-2" : "WINDOWS-1250",
                             CPL_ENC_UTF8);
     if (poInfo.find(pszKey) == poInfo.end() ) {
         poInfo[pszKey] = pszValueEnc;