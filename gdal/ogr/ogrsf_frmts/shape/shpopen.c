8bd57a7abc679b1f250fb05624f132ff558525b2
Kurt Schwehr <schwehr@gmail.com>
2014-09-21 15:35:55 +0000

    nPoints and nParts are unsigned.  Comment out tautologies (https://trac.osgeo.org/gdal/ticket/5414)
    git-svn-id: https://svn.osgeo.org/gdal/branches/1.11@27712 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/shape/shpopen.c
+++ gdal/ogr/ogrsf_frmts/shape/shpopen.c
@@ -1829,7 +1829,8 @@ SHPReadObject( SHPHandle psSHP, int hEntity )
         if( bBigEndian ) SwapWord( 4, &nPoints );
         if( bBigEndian ) SwapWord( 4, &nParts );
 
-        if (nPoints < 0 || nParts < 0 ||
+        /* nPoints and nParts are unsigned */
+        if (/* nPoints < 0 || nParts < 0 || */
             nPoints > 50 * 1000 * 1000 || nParts > 10 * 1000 * 1000)
         {
             snprintf(szErrorMsg, sizeof(szErrorMsg),
@@ -2045,7 +2046,8 @@ SHPReadObject( SHPHandle psSHP, int hEntity )
 
         if( bBigEndian ) SwapWord( 4, &nPoints );
 
-        if (nPoints < 0 || nPoints > 50 * 1000 * 1000)
+        /* nPoints is unsigned */
+        if (/* nPoints < 0 || */ nPoints > 50 * 1000 * 1000)
         {
             snprintf(szErrorMsg, sizeof(szErrorMsg),
                      "Corrupted .shp file : shape %d : nPoints = %d",
