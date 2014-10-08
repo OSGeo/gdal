25cc177035ace02f16d8f31c40199270b2cc7de4
Even Rouault <even.rouault@mines-paris.org>
2014-10-08 12:12:38 +0000

    GDALReprojectImage(): takes into account nodata values set on destination dataset
    git-svn-id: https://svn.osgeo.org/gdal/trunk@27822 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/alg/reproject.py
+++ autotest/alg/reproject.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 ###############################################################################
 # $Id$
 #
@@ -108,10 +109,38 @@ def reproject_2():
     else:
         return 'success' 
 
+###############################################################################
+# Test nodata values
+
+def reproject_3():
+
+    data = '\x02\x7f\x7f\x02\x02\x7f\x7f\x02\x02\x7f\x7f\x02'
+    src_ds = gdal.GetDriverByName('MEM').Create('',4,3)
+    src_ds.GetRasterBand(1).WriteRaster(0,0,4,3,data)
+    src_ds.GetRasterBand(1).SetNoDataValue(2)
+    src_ds.SetGeoTransform([10,1,0,10,0,-1])
+
+    dst_ds = gdal.GetDriverByName('MEM').Create('',6,3)
+    dst_ds.GetRasterBand(1).SetNoDataValue(3)
+    dst_ds.GetRasterBand(1).Fill(3)
+    dst_ds.SetGeoTransform([10,2./3.,0,10,0,-1])
+    
+    gdal.ReprojectImage( src_ds, dst_ds, '', '', gdal.GRA_Bilinear)
+    got_data = dst_ds.GetRasterBand(1).ReadRaster(0,0,6,3)
+    expected_data = '\x03\x7f\x7f\x7f\x7f\x03\x03\x7f\x7f\x7f\x7f\x03\x03\x7f\x7f\x7f\x7f\x03'
+    if got_data != expected_data:
+        gdaltest.post_reason('fail')
+        import struct
+        print(struct.unpack('B' * 18, got_data))
+        return 'fail'
+
+    return 'success'
+
 
 gdaltest_list = [
     reproject_1,
-    reproject_2
+    reproject_2,
+    reproject_3,
     ]
 
 if __name__ == '__main__':
