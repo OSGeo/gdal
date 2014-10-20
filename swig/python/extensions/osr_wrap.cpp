56c9b2e03434f1b611d4420e226981b8418ab8ac
Even Rouault <even.rouault@mines-paris.org>
2014-10-20 18:17:05 +0000

    Fix hang of Python in case of repeated call to gdal/ogr.UseExceptions() and CE_Warning emitted (#5704)
    git-svn-id: https://svn.osgeo.org/gdal/branches/1.11/gdal@27891 f0d54148-0727-0410-94bb-9a71ac55c965

--- swig/python/extensions/osr_wrap.cpp
+++ swig/python/extensions/osr_wrap.cpp
@@ -2928,14 +2928,20 @@ int GetUseExceptions() {
 }
 
 void UseExceptions() {
-  bUseExceptions = 1;
-  pfnPreviousHandler = 
-    CPLSetErrorHandler( (CPLErrorHandler) PythonBindingErrorHandler );
+  if( !bUseExceptions )
+  {
+    bUseExceptions = 1;
+    pfnPreviousHandler = 
+        CPLSetErrorHandler( (CPLErrorHandler) PythonBindingErrorHandler );
+  }
 }
 
 void DontUseExceptions() {
-  bUseExceptions = 0;
-  CPLSetErrorHandler( pfnPreviousHandler );
+  if( bUseExceptions )
+  {
+    bUseExceptions = 0;
+    CPLSetErrorHandler( pfnPreviousHandler );
+  }
 }
 
 
