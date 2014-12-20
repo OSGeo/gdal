OSGeo/gdal

commit 9459f21d37f3d882cffa4e0a4c45ac81dcb30dde

gdalwarp: only apply INIT_DEST when processing the first input dataset (#5387)
git-svn-id: https://svn.osgeo.org/gdal/trunk@28183 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/apps/gdalwarp.cpp
@@ -921,8 +921,6 @@ int main( int argc, char ** argv )
 /* -------------------------------------------------------------------- */
 /*      If not, we need to create it.                                   */
 /* -------------------------------------------------------------------- */
-    int   bInitDestSetForFirst = FALSE;
-
     void* hUniqueTransformArg = NULL;
     GDALDatasetH hUniqueSrcDS = NULL;
 
@@ -945,18 +943,18 @@ int main( int argc, char ** argv )
                                        papszOpenOptions);
         bCreateOutput = TRUE;
 
-        if( CSLFetchNameValue( papszWarpOptions, "INIT_DEST" ) == NULL 
-            && pszDstNodata == NULL )
+        if( CSLFetchNameValue( papszWarpOptions, "INIT_DEST" ) == NULL )
         {
-            papszWarpOptions = CSLSetNameValue(papszWarpOptions,
-                                               "INIT_DEST", "0");
-            bInitDestSetForFirst = TRUE;
-        }
-        else if( CSLFetchNameValue( papszWarpOptions, "INIT_DEST" ) == NULL )
-        {
-            papszWarpOptions = CSLSetNameValue(papszWarpOptions,
-                                               "INIT_DEST", "NO_DATA" );
-            bInitDestSetForFirst = TRUE;
+            if ( pszDstNodata == NULL )
+            {
+                papszWarpOptions = CSLSetNameValue(papszWarpOptions,
+                                                "INIT_DEST", "0");
+            }
+            else
+            {
+                papszWarpOptions = CSLSetNameValue(papszWarpOptions,
+                                                "INIT_DEST", "NO_DATA" );
+            }
         }
 
         CSLDestroy( papszCreateOptions );
@@ -1229,7 +1227,7 @@ int main( int argc, char ** argv )
 /* -------------------------------------------------------------------- */
 /*      Clear temporary INIT_DEST settings after the first image.       */
 /* -------------------------------------------------------------------- */
-        if( bInitDestSetForFirst && iSrc == 1 )
+        if( bCreateOutput && iSrc == 1 )
             papszWarpOptions = CSLSetNameValue( papszWarpOptions, 
                                                 "INIT_DEST", NULL );
 
@@ -1526,7 +1524,7 @@ int main( int argc, char ** argv )
                 }
             }
 
-            if( bInitDestSetForFirst && iSrc == 0 )
+            if( bCreateOutput && iSrc == 0 )
             {
                 /* As we didn't know at the beginning if there was source nodata */
                 /* we have initialized INIT_DEST=0. Override this with NO_DATA now */

commit 07261f6e1d497a86e87cc6698f5d301d27829fa4

Fix regression caused by previous commit (#5387)
git-svn-id: https://svn.osgeo.org/gdal/trunk@28185 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/apps/gdalwarp.cpp
@@ -923,6 +923,7 @@ int main( int argc, char ** argv )
 /* -------------------------------------------------------------------- */
     void* hUniqueTransformArg = NULL;
     GDALDatasetH hUniqueSrcDS = NULL;
+    int bInitDestSetByUser = ( CSLFetchNameValue( papszWarpOptions, "INIT_DEST" ) != NULL );
 
     const char* pszWarpThreads = CSLFetchNameValue(papszWarpOptions, "NUM_THREADS");
     if( pszWarpThreads != NULL )
@@ -943,7 +944,7 @@ int main( int argc, char ** argv )
                                        papszOpenOptions);
         bCreateOutput = TRUE;
 
-        if( CSLFetchNameValue( papszWarpOptions, "INIT_DEST" ) == NULL )
+        if( !bInitDestSetByUser )
         {
             if ( pszDstNodata == NULL )
             {
@@ -1524,7 +1525,7 @@ int main( int argc, char ** argv )
                 }
             }
 
-            if( bCreateOutput && iSrc == 0 )
+            if( !bInitDestSetByUser && iSrc == 0 )
             {
                 /* As we didn't know at the beginning if there was source nodata */
                 /* we have initialized INIT_DEST=0. Override this with NO_DATA now */
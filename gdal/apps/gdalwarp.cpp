07261f6e1d497a86e87cc6698f5d301d27829fa4
Even Rouault <even.rouault@mines-paris.org>
2014-12-20 21:14:13 +0000

    Fix regression caused by previous commit (#5387)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@28185 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/apps/gdalwarp.cpp
+++ gdal/apps/gdalwarp.cpp
@@ -923,6 +923,7 @@ int main( int argc, char ** argv )
 /* -------------------------------------------------------------------- */
     void* hUniqueTransformArg = NULL;
     GDALDatasetH hUniqueSrcDS = NULL;
+    int bInitDestSetByUser = ( CSLFetchNameValue( papszWarpOptions, "INIT_DEST" ) != NULL );
 
     const char* pszWarpThreads = CSLFetchNameValue(papszWarpOptions, "NUM_THREADS");
     if( pszWarpThreads != NULL )
@@ -943,7 +944,7 @@ int main( int argc, char ** argv )
                                        papszOpenOptions);
         bCreateOutput = TRUE;
 
-        if( CSLFetchNameValue( papszWarpOptions, "INIT_DEST" ) == NULL )
+        if( !bInitDestSetByUser )
         {
             if ( pszDstNodata == NULL )
             {
@@ -1524,7 +1525,7 @@ int main( int argc, char ** argv )
                 }
             }
 
-            if( bCreateOutput && iSrc == 0 )
+            if( !bInitDestSetByUser && iSrc == 0 )
             {
                 /* As we didn't know at the beginning if there was source nodata */
                 /* we have initialized INIT_DEST=0. Override this with NO_DATA now */
