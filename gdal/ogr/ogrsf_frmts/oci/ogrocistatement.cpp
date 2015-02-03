0596cdcd5a3e2ce8c7d25d3ab56e58408aeaf3aa
Even Rouault <even.rouault@mines-paris.org>
2015-02-03 10:49:44 +0000

    OCI: be able to retrieve the content of the DATA_DEFAULT column of the user_tab_columns system table
    git-svn-id: https://svn.osgeo.org/gdal/trunk@28408 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/oci/ogrocistatement.cpp
+++ gdal/ogr/ogrsf_frmts/oci/ogrocistatement.cpp
@@ -311,8 +311,17 @@ CPLErr OGROCIStatement::Execute( const char *pszSQLStatement,
 
         if( oField.GetType() == OFTBinary )
         {
-            panFieldMap[iParm] = -1;
-            continue;                   
+            /* We could probably generalize that, but at least it works in that */
+            /* use case */
+            if( EQUAL(oField.GetNameRef(), "DATA_DEFAULT") && nOCIType == SQLT_LNG )
+            {
+                oField.SetType(OFTString);
+            }
+            else
+            {
+                panFieldMap[iParm] = -1;
+                continue;
+            }
         }
 
         poDefn->AddFieldDefn( &oField );
