64a0ffaff877b2ab204edc61791dd4dc4ebc24eb
Even Rouault <even.rouault@mines-paris.org>
2015-02-09 17:18:31 +0000

    testblockcache: add a -migrate option
    git-svn-id: https://svn.osgeo.org/gdal/trunk@28443 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/cpp/testblockcache.cpp
+++ autotest/cpp/testblockcache.cpp
@@ -34,10 +34,21 @@
 #include "cpl_multiproc.h"
 #include "gdal_priv.h"
 
+// Not sure if all pthread implementations have spin lock but that's good
+// enough for now
+#ifdef CPL_MULTIPROC_PTHREAD
+#define HAVE_SPINLOCK
+#endif
+
+#ifdef HAVE_SPINLOCK
+#include <pthread.h>
+pthread_spinlock_t* psLock = NULL;
+#endif
+
 void Usage()
 {
     printf("Usage: testblockcache [-threads X] [-loops X] [-strategy random|line|block]\n");
-    printf("                      [[-dataset filename] |\n");
+    printf("                      [-migrate] [ filename |\n");
     printf("                       [[-xsize val] [-ysize val] [-bands val] [-co key=value]*\n");
     printf("                       [[-memdriver] | [-ondisk]] [-check]] ]\n");
     exit(1);
@@ -53,6 +64,35 @@ typedef enum
     STRATEGY_LINE,
     STRATEGY_BLOCK,
 } Strategy;
+
+typedef struct _Request Request;
+struct _Request
+{
+    int nXOff, nYOff, nXWin, nYWin;
+    int nBands;
+    Request* psNext;
+};
+
+typedef struct _Resource Resource;
+struct _Resource
+{
+    GDALDataset* poDS;
+    void* pBuffer;
+    Resource* psNext;
+    Resource* psPrev;
+};
+
+typedef struct
+{
+    GDALDataset* poDS;
+    Request* psRequestList;
+    int nBufferSize;
+} ThreadDescription;
+
+static void* hMutex = NULL;
+static Request* psGlobalRequestList = NULL;
+static Resource* psGlobalResourceList = NULL;
+static Resource* psGlobalResourceLast = NULL;
 
 /* according to rand() man page, POSIX.1-2001 proposes the following implementation */
 /* RAND_MAX assumed to be 32767 */
@@ -94,16 +134,125 @@ static void ReadRaster(GDALDataset* poDS, int nXSize, int nYSize, int nBands,
     }
 }
 
-static void ThreadFuncRandomStrategy(void* _poDS)
+static void AddRequest(Request*& psRequestList, Request*& psRequestLast,
+                       int nXOff, int nYOff, int nXWin, int nYWin, int nBands)
+{
+    Request* psRequest = (Request*)CPLMalloc(sizeof(Request));
+    psRequest->nXOff = nXOff;
+    psRequest->nYOff = nYOff;
+    psRequest->nXWin = nXWin;
+    psRequest->nYWin = nYWin;
+    psRequest->nBands = nBands;
+    if( psRequestLast )
+        psRequestLast->psNext = psRequest;
+    else
+        psRequestList = psRequest;
+    psRequestLast = psRequest;
+    psRequest->psNext = NULL;
+}
+
+static Request* GetNextRequest(Request*& psRequestList)
+{
+    if( hMutex ) CPLAcquireMutex(hMutex, 1000.0);
+#ifdef HAVE_SPINLOCK
+    if( psLock ) pthread_spin_lock(psLock);
+#endif
+    Request* psRet = psRequestList;
+    if( psRequestList )
+    {
+        psRequestList = psRequestList->psNext;
+        psRet->psNext = NULL;
+    }
+    if( hMutex ) CPLReleaseMutex(hMutex);
+#ifdef HAVE_SPINLOCK
+    if( psLock ) pthread_spin_unlock(psLock);
+#endif
+    return psRet;
+}
+
+static Resource* AcquireFirstResource()
+{
+    if( hMutex ) CPLAcquireMutex(hMutex, 1000.0);
+#ifdef HAVE_SPINLOCK
+    if( psLock ) pthread_spin_lock(psLock);
+#endif
+    Resource* psRet = psGlobalResourceList;
+    psGlobalResourceList = psGlobalResourceList->psNext;
+    if( psGlobalResourceList )
+        psGlobalResourceList->psPrev = NULL;
+    else
+        psGlobalResourceLast = NULL;
+    psRet->psNext = NULL;
+    assert(psRet->psPrev == NULL);
+    if( hMutex ) CPLReleaseMutex(hMutex);
+#ifdef HAVE_SPINLOCK
+    if( psLock ) pthread_spin_unlock(psLock);
+#endif
+    return psRet;
+}
+
+static void PutResourceAtEnd(Resource* psResource)
+{
+    if( hMutex ) CPLAcquireMutex(hMutex, 1000.0);
+#ifdef HAVE_SPINLOCK
+    if( psLock ) pthread_spin_lock(psLock);
+#endif
+    psResource->psPrev = psGlobalResourceLast;
+    psResource->psNext = NULL;
+    if( psGlobalResourceList == NULL )
+        psGlobalResourceList = psResource;
+    else
+        psGlobalResourceLast->psNext = psResource;
+    psGlobalResourceLast = psResource;
+    if( hMutex ) CPLReleaseMutex(hMutex);
+#ifdef HAVE_SPINLOCK
+    if( psLock ) pthread_spin_unlock(psLock);
+#endif
+}
+
+static void ThreadFuncDedicatedDataset(void* _psThreadDescription)
+{
+    ThreadDescription* psThreadDescription = (ThreadDescription*)_psThreadDescription;
+    int nXSize = psThreadDescription->poDS->GetRasterXSize();
+    int nYSize = psThreadDescription->poDS->GetRasterYSize();
+    void* pBuffer = CPLMalloc(psThreadDescription->nBufferSize);
+    while( psThreadDescription->psRequestList != NULL )
+    {
+        Request* psRequest = GetNextRequest(psThreadDescription->psRequestList);
+        ReadRaster(psThreadDescription->poDS, nXSize, nYSize, psRequest->nBands,
+                   (GByte*)pBuffer, psRequest->nXOff, psRequest->nYOff, psRequest->nXWin, psRequest->nYWin);
+        CPLFree(psRequest);
+    }
+    CPLFree(pBuffer);
+}
+
+static void ThreadFuncWithMigration(void* _unused)
+{
+    Request* psRequest;
+    while( (psRequest = GetNextRequest(psGlobalRequestList)) != NULL )
+    {
+        Resource* psResource = AcquireFirstResource();
+        assert(psResource);
+        int nXSize = psResource->poDS->GetRasterXSize();
+        int nYSize = psResource->poDS->GetRasterYSize();
+        ReadRaster(psResource->poDS, nXSize, nYSize, psRequest->nBands,
+                   (GByte*)psResource->pBuffer,
+                   psRequest->nXOff, psRequest->nYOff, psRequest->nXWin, psRequest->nYWin);
+        CPLFree(psRequest);
+        PutResourceAtEnd(psResource);
+    }
+}
+
+static int CreateRandomStrategyRequests(GDALDataset* poDS,
+                                        Request*& psRequestList,
+                                        Request*& psRequestLast)
 {
     unsigned long seed = 1;
-    GDALDataset* poDS = (GDALDataset*)_poDS;
     int nXSize = poDS->GetRasterXSize();
     int nYSize = poDS->GetRasterYSize();
     int nMaxXWin = MIN(1000, nXSize/10+1);
     int nMaxYWin = MIN(1000, nYSize/10+1);
     int nQueriedBands = MIN(4, poDS->GetRasterCount());
-    void* pBuffer = malloc(nQueriedBands * nMaxXWin * nMaxYWin);
     int nAverageIterationsToReadWholeFile =
         ((nXSize + nMaxXWin/2-1) / (nMaxXWin/2)) * ((nYSize + nMaxYWin/2-1) / (nMaxYWin/2));
     int nLocalLoops = nLoops * nAverageIterationsToReadWholeFile;
@@ -117,40 +266,37 @@ static void ThreadFuncRandomStrategy(void* _poDS)
             nXWin = nXSize - nXOff;
         if( nYOff + nYWin > nYSize )
             nYWin = nYSize - nYOff;
-
-        ReadRaster(poDS, nXSize, nYSize, nQueriedBands,
-                   (GByte*)pBuffer, nXOff, nYOff, nXWin, nYWin);
+        AddRequest(psRequestList, psRequestLast, nXOff, nYOff, nXWin, nYWin, nQueriedBands);
     }
-    free(pBuffer);
+    return nQueriedBands * nMaxXWin * nMaxYWin;
 }
 
-static void ThreadFuncLineStrategy(void* _poDS)
+static int CreateLineStrategyRequests(GDALDataset* poDS,
+                                      Request*& psRequestList,
+                                      Request*& psRequestLast)
 {
-    GDALDataset* poDS = (GDALDataset*)_poDS;
     int nXSize = poDS->GetRasterXSize();
     int nYSize = poDS->GetRasterYSize();
     int nQueriedBands = MIN(4, poDS->GetRasterCount());
-    void* pBuffer = malloc(nQueriedBands * nXSize);
     for(int iLoop=0;iLoop<nLoops;iLoop++)
     {
         for(int nYOff=0;nYOff<nYSize;nYOff++)
         {
-            ReadRaster(poDS, nXSize, nYSize, nQueriedBands,
-                       (GByte*)pBuffer, 0, nYOff, nXSize, 1);
+            AddRequest(psRequestList, psRequestLast, 0, nYOff, nXSize, 1, nQueriedBands);
         }
     }
-    free(pBuffer);
+    return nQueriedBands * nXSize;
 }
 
-static void ThreadFuncBlockStrategy(void* _poDS)
+static int CreateBlockStrategyRequests(GDALDataset* poDS,
+                                       Request*& psRequestList,
+                                       Request*& psRequestLast)
 {
-    GDALDataset* poDS = (GDALDataset*)_poDS;
     int nXSize = poDS->GetRasterXSize();
     int nYSize = poDS->GetRasterYSize();
     int nMaxXWin = MIN(1000, nXSize/10+1);
     int nMaxYWin = MIN(1000, nYSize/10+1);
     int nQueriedBands = MIN(4, poDS->GetRasterCount());
-    void* pBuffer = malloc(nQueriedBands * nMaxXWin * nMaxYWin);
     for(int iLoop=0;iLoop<nLoops;iLoop++)
     {
         for(int nYOff=0;nYOff<nYSize;nYOff+=nMaxYWin)
@@ -159,14 +305,12 @@ static void ThreadFuncBlockStrategy(void* _poDS)
             for(int nXOff=0;nXOff<nXSize;nXOff+=nMaxXWin)
             {
                 int nReqXSize = (nXOff + nMaxXWin > nXSize) ? nXSize - nXOff : nMaxXWin;
-                ReadRaster(poDS, nXSize, nYSize, nQueriedBands,
-                           (GByte*)pBuffer, nXOff, nYOff, nReqXSize, nReqYSize);
+                AddRequest(psRequestList, psRequestLast, nXOff, nYOff, nReqXSize, nReqYSize, nQueriedBands);
             }
         }
     }
-    free(pBuffer);
+    return nQueriedBands * nMaxXWin * nMaxYWin;
 }
-
 
 int main(int argc, char* argv[])
 {
@@ -174,14 +318,16 @@ int main(int argc, char* argv[])
     int nThreads = CPLGetNumCPUs();
     std::vector<void*> apsThreads;
     Strategy eStrategy = STRATEGY_RANDOM;
+    int bNewDatasetOption = FALSE;
     int nXSize = 5000;
     int nYSize = 5000;
     int nBands = 4;
     char** papszOptions = NULL;
     int bOnDisk = FALSE;
-    std::vector<GDALDataset*> apoDatasets;
+    std::vector<ThreadDescription> asThreadDescription;
     int bMemDriver = FALSE;
     GDALDataset* poMEMDS = NULL;
+    int bMigrate = FALSE;
 
     argc = GDALGeneralCmdLineProcessor( argc, &argv, 0 );
 
@@ -201,11 +347,6 @@ int main(int argc, char* argv[])
             if( nLoops <= 0 )
                 nLoops = INT_MAX;
         }
-        else if( EQUAL(argv[i], "-dataset") && i + 1 < argc)
-        {
-            i ++;
-            pszDataset = argv[i];
-        }
         else if( EQUAL(argv[i], "-strategy") && i + 1 < argc)
         {
             i ++;
@@ -222,33 +363,55 @@ int main(int argc, char* argv[])
         {
             i ++;
             nXSize = atoi(argv[i]);
+            bNewDatasetOption = TRUE;
         }
         else if( EQUAL(argv[i], "-ysize") && i + 1 < argc)
         {
             i ++;
             nYSize = atoi(argv[i]);
+            bNewDatasetOption = TRUE;
         }
         else if( EQUAL(argv[i], "-bands") && i + 1 < argc)
         {
             i ++;
             nBands = atoi(argv[i]);
+            bNewDatasetOption = TRUE;
         }
         else if( EQUAL(argv[i], "-co") && i + 1 < argc)
         {
             i ++;
             papszOptions = CSLAddString(papszOptions, argv[i]);
+            bNewDatasetOption = TRUE;
         }
         else if( EQUAL(argv[i], "-ondisk"))
+        {
             bOnDisk = TRUE;
+            bNewDatasetOption = TRUE;
+        }
         else if( EQUAL(argv[i], "-check"))
+        {
             bCheck = TRUE;
+            bNewDatasetOption = TRUE;
+        }
         else if( EQUAL(argv[i], "-memdriver"))
+        {
             bMemDriver = TRUE;
+            bNewDatasetOption = TRUE;
+        }
+        else if( EQUAL(argv[i], "-migrate"))
+            bMigrate = TRUE;
+        else if( argv[i][0] == '-' )
+            Usage();
+        else if( pszDataset == NULL )
+            pszDataset = argv[i];
         else
         {
             Usage();
         }
     }
+
+    if( pszDataset != NULL && bNewDatasetOption )
+        Usage();
 
     CPLDebug("TEST", "Using %d threads", nThreads);
 
@@ -295,19 +458,55 @@ int main(int argc, char* argv[])
     CSLDestroy(papszOptions);
     papszOptions = NULL;
     
+    Request* psGlobalRequestLast = NULL;
+
     for(i = 0; i < nThreads; i++ )
     {
+        GDALDataset* poDS;
         // Since GDAL 2.0, the MEM driver is thread-safe, i.e. does not use the block
         // cache, but only for operations not involving resampling, which is
         // the case here
         if( poMEMDS ) 
-            apoDatasets.push_back(poMEMDS);
+            poDS = poMEMDS;
         else
         {
-            GDALDataset* poDS = (GDALDataset*)GDALOpen(pszDataset, GA_ReadOnly);
+            poDS = (GDALDataset*)GDALOpen(pszDataset, GA_ReadOnly);
             if( poDS == NULL )
                 exit(1);
-            apoDatasets.push_back(poDS);
+        }
+        if( bMigrate )
+        {
+            Resource* psResource = (Resource*)CPLMalloc(sizeof(Resource));
+            psResource->poDS = poDS;
+            int nBufferSize;
+            if( eStrategy == STRATEGY_RANDOM )
+                nBufferSize = CreateRandomStrategyRequests(
+                        poDS, psGlobalRequestList, psGlobalRequestLast);
+            else if( eStrategy == STRATEGY_LINE )
+                nBufferSize = CreateLineStrategyRequests(
+                        poDS, psGlobalRequestList, psGlobalRequestLast);
+            else
+                nBufferSize = CreateBlockStrategyRequests(
+                        poDS, psGlobalRequestList, psGlobalRequestLast);
+            psResource->pBuffer = CPLMalloc(nBufferSize);
+            PutResourceAtEnd(psResource);
+        }
+        else
+        {
+            ThreadDescription sThreadDescription;
+            sThreadDescription.poDS = poDS;
+            sThreadDescription.psRequestList = NULL;
+            Request* psRequestLast = NULL;
+            if( eStrategy == STRATEGY_RANDOM )
+                sThreadDescription.nBufferSize = CreateRandomStrategyRequests(
+                        poDS, sThreadDescription.psRequestList, psRequestLast);
+            else if( eStrategy == STRATEGY_LINE )
+                sThreadDescription.nBufferSize = CreateLineStrategyRequests(
+                        poDS, sThreadDescription.psRequestList, psRequestLast);
+            else
+                sThreadDescription.nBufferSize = CreateBlockStrategyRequests(
+                        poDS, sThreadDescription.psRequestList, psRequestLast);
+            asThreadDescription.push_back(sThreadDescription);
         }
     }
 
@@ -317,26 +516,50 @@ int main(int argc, char* argv[])
         VSIUnlink(pszDataset);
         CPLPopErrorHandler();
     }
+    
+    if( bMigrate )
+    {
+#ifdef HAVE_SPINLOCK
+        psLock = (pthread_spinlock_t*)CPLMalloc(sizeof(pthread_spinlock_t));
+        pthread_spin_init(psLock, PTHREAD_PROCESS_PRIVATE);
+#else
+        hMutex = CPLCreateMutex();
+        CPLReleaseMutex(hMutex);
+#endif
+    }
 
     for(i = 0; i < nThreads; i++ )
     {
         void* pThread;
-        void (*pfnThreadFunc)(void*) = NULL;
-        if( eStrategy == STRATEGY_RANDOM )
-            pfnThreadFunc = ThreadFuncRandomStrategy;
-        else if( eStrategy == STRATEGY_LINE )
-            pfnThreadFunc = ThreadFuncLineStrategy;
+        if( bMigrate )
+            pThread = CPLCreateJoinableThread(ThreadFuncWithMigration, NULL);
         else
-            pfnThreadFunc = ThreadFuncBlockStrategy;
-        pThread = CPLCreateJoinableThread(pfnThreadFunc, apoDatasets[i]);
+            pThread = CPLCreateJoinableThread(ThreadFuncDedicatedDataset,
+                                              &(asThreadDescription[i]));
         apsThreads.push_back(pThread);
     }
     for(i = 0; i < nThreads; i++ )
     {
         CPLJoinThread(apsThreads[i]);
-        if( poMEMDS == NULL )
-            GDALClose(apoDatasets[i]);
+        if( !bMigrate && poMEMDS == NULL )
+            GDALClose(asThreadDescription[i].poDS);
     }
+    while( psGlobalResourceList != NULL )
+    {
+        CPLFree( psGlobalResourceList->pBuffer);
+        Resource* psNext = psGlobalResourceList->psNext;
+        CPLFree( psGlobalResourceList );
+        psGlobalResourceList = psNext;
+    }
+    if( hMutex )
+        CPLDestroyMutex(hMutex);
+#ifdef HAVE_SPINLOCK
+    if( psLock )
+    {
+        pthread_spin_destroy(psLock);
+        CPLFree((void*)psLock);
+    }
+#endif
 
     if( bCreatedDataset && poMEMDS == NULL  )
     {
