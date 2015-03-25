b1d85ee46df5d227812a7653a75d10e9bae78b28
Even Rouault <even.rouault@mines-paris.org>
2015-03-25 11:03:02 +0000

    dump_jp2.py: add a -dump_crsdictionary option (https://trac.osgeo.org/gdal/ticket/5697)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@28768 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/swig/python/samples/dump_jp2.py
+++ gdal/swig/python/samples/dump_jp2.py
@@ -33,11 +33,13 @@ import sys
 from osgeo import gdal
 
 def Usage():
-    print('Usage:  dump_jp2 [-dump_gmljp2 out.txt|-] test.jp2')
+    print('Usage:  dump_jp2 [-dump_gmljp2 out.txt|-] [-dump_crsdictionary out.txt|-] test.jp2')
     print('')
     print('Options:')
     print('-dump_gmljp2: Writes the content of the GMLJP2 box in the specified')
     print('              file, or on the console if "-" syntax is used.')
+    print('-dump_crsdictionary: Writes the content of the GML CRS dictionary box in the specified')
+    print('                     file, or on the console if "-" syntax is used.')
     return 1
 
 def dump_gmljp2(filename, out_gmljp2):
@@ -58,15 +60,43 @@ def dump_gmljp2(filename, out_gmljp2):
         print('INFO: %s written with content of GMLJP2 box' % filename)
     return 0
 
+def dump_crsdictionary(filename, out_crsdictionary):
+    ds = gdal.Open(filename)
+    if ds is None:
+        print('Cannot open %s' % filename)
+        return 1
+    mdd_list = ds.GetMetadataDomainList()
+    for domain in mdd_list:
+        if domain.startswith('xml:'):
+            mdd_item = ds.GetMetadata(domain)[0]
+            if mdd_item.find('<Dictionary') >= 0 or mdd_item.find('<gml:Dictionary') >= 0:
+                if out_crsdictionary == '-':
+                    print(mdd_item)
+                else:
+                    f = open(out_crsdictionary, 'wt')
+                    f.write(mdd_item)
+                    f.close()
+                    print('INFO: %s written with content of CRS dictionary box (%s)' % (filename, domain[4:]))
+                return 0
+
+    print('No CRS dictionary content found in %s' % filename)
+    return 1
+
 def main():
     i = 1
     out_gmljp2 = None
+    out_crsdictionary = None
     filename = None
     while i < len(sys.argv):
         if sys.argv[i] == "-dump_gmljp2":
             if i >= len(sys.argv) - 1:
                 return Usage()
             out_gmljp2 = sys.argv[i+1]
+            i = i + 1
+        elif sys.argv[i] == "-dump_crsdictionary":
+            if i >= len(sys.argv) - 1:
+                return Usage()
+            out_crsdictionary = sys.argv[i+1]
             i = i + 1
         elif sys.argv[i][0] == '-':
             return Usage()
@@ -80,8 +110,13 @@ def main():
     if filename is None:
         return Usage()
 
-    if out_gmljp2:
-        return(dump_gmljp2(filename, out_gmljp2))
+    if out_gmljp2 or out_crsdictionary:
+        if out_gmljp2:
+            if dump_gmljp2(filename, out_gmljp2) != 0:
+                return 1
+        if out_crsdictionary:
+            if dump_crsdictionary(filename, out_crsdictionary) != 0:
+                return 1
     else:
         s = gdal.GetJPEG2000StructureAsString(filename, ['ALL=YES'])
         print(s)
