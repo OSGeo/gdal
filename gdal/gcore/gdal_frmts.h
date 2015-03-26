6c580269df9704c4c85b7fc756f1a4bbe299f7fb
Even Rouault <even.rouault@mines-paris.org>
2015-03-26 21:13:18 +0000

    Add GDAL VICAR format, patch by Sebastian Walter + a few extra fixes (#5763)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@28786 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/gcore/gdal_frmts.h
+++ gdal/gcore/gdal_frmts.h
@@ -120,6 +120,7 @@ void CPL_DLL GDALRegister_DIPEx(void);
 void CPL_DLL GDALRegister_ISIS3(void);
 void CPL_DLL GDALRegister_ISIS2(void);
 void CPL_DLL GDALRegister_PDS(void);
+void CPL_DLL GDALRegister_VICAR(void);
 void CPL_DLL GDALRegister_IDRISI(void);
 void CPL_DLL GDALRegister_Terragen(void);
 void CPL_DLL GDALRegister_WCS(void);
