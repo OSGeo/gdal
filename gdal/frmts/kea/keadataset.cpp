6fda4dd2b2efe685113c9bc00beb81be16fc7f05
Even Rouault <even.rouault@mines-paris.org>
2015-03-30 14:48:50 +0000

    KEA: fix previous commit that broke kea_4
    git-svn-id: https://svn.osgeo.org/gdal/trunk@28825 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/kea/keadataset.cpp
+++ gdal/frmts/kea/keadataset.cpp
@@ -237,7 +237,7 @@ H5::H5File *KEADataset::CreateLL( const char * pszFilename,
         ndeflate = (unsigned int) atol( pszValue );
 
     kealib::KEADataType keaDataType = GDAL_to_KEA_Type( eType );
-    if( keaDataType == kealib::kea_undefined )
+    if( nBands > 0 && keaDataType == kealib::kea_undefined )
     {
         CPLError( CE_Failure, CPLE_NotSupported,
                   "Data type %s not supported in KEA",
@@ -712,9 +712,18 @@ CPLErr KEADataset::AddBand(GDALDataType eType, char **papszOptions)
             ndeflate = atol(pszValue);
         }
     }
-
+    
+    kealib::KEADataType keaDataType = GDAL_to_KEA_Type( eType );
+    if( keaDataType == kealib::kea_undefined )
+    {
+        CPLError( CE_Failure, CPLE_NotSupported,
+                  "Data type %s not supported in KEA",
+                  GDALGetDataTypeName(eType) );
+        return CE_Failure;
+    }
+    
     try {
-        m_pImageIO->addImageBand(GDAL_to_KEA_Type(eType), "", nimageBlockSize,
+        m_pImageIO->addImageBand(keaDataType, "", nimageBlockSize,
                 nattBlockSize, ndeflate);
     } catch (kealib::KEAIOException &e) {
         CPLError( CE_Failure, CPLE_AppDefined,
