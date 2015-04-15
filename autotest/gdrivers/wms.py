b737b24cc3130b07d5ec853e6e8d5117affbd114
Dmitry Baryshnikov <polimax at mail.ru>
2015-04-15 21:25:16 +0000

    Drop ImageSR tag in favour SRS tag in AGS WMS mini driver Fix WMS documentation
    git-svn-id: https://svn.osgeo.org/gdal/trunk@28913 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/gdrivers/wms.py
+++ autotest/gdrivers/wms.py
@@ -789,7 +789,7 @@ def wms_18():
     # We don't need to check if the remote service is online as we
     # don't need a connection for this test
     
-    fn = '<GDAL_WMS><Service name="AGS"><ServerUrl>http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Specialty/ESRI_StateCityHighway_USA/MapServer</ServerUrl><BBoxOrder>xyXY</BBoxOrder><ImageSR>3857</ImageSR></Service><DataWindow><UpperLeftX>-20037508.34</UpperLeftX><UpperLeftY>20037508.34</UpperLeftY><LowerRightX>20037508.34</LowerRightX><LowerRightY>-20037508.34</LowerRightY><SizeX>512</SizeX><SizeY>512</SizeY></DataWindow></GDAL_WMS>'
+    fn = '<GDAL_WMS><Service name="AGS"><ServerUrl>http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Specialty/ESRI_StateCityHighway_USA/MapServer</ServerUrl><BBoxOrder>xyXY</BBoxOrder><SRS>EPSG:3857</SRS></Service><DataWindow><UpperLeftX>-20037508.34</UpperLeftX><UpperLeftY>20037508.34</UpperLeftY><LowerRightX>20037508.34</LowerRightX><LowerRightY>-20037508.34</LowerRightY><SizeX>512</SizeX><SizeY>512</SizeY></DataWindow></GDAL_WMS>'
 
     ds = gdal.Open( fn )
 
