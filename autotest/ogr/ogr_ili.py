e5c3d1732a03a0a0cb7491f20b096b76af1ed327
Even Rouault <even.rouault@mines-paris.org>
2015-05-01 21:03:29 +0000

    ILI: reset OGR_ARC_STEPSIZE to avoid messing with other tests
    git-svn-id: https://svn.osgeo.org/gdal/trunk@29100 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/ogr/ogr_ili.py
+++ autotest/ogr/ogr_ili.py
@@ -1012,6 +1012,8 @@ def ogr_interlis_arc1():
     #gdal.SetConfigOption('OGR_ARC_STEPSIZE', '0.96')
     ds = ogr.Open('data/ili/Beispiel.itf,data/ili/Beispiel.imd')
 
+    gdal.SetConfigOption('OGR_STROKE_CURVE', None)
+    
     length_0_1_deg = 72.7181992353 # Line length with 0.1 degree segments
 
     #Read Area lines
