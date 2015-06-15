25524f78355a4416e85a612b330952568d9b0046
Even Rouault <even.rouault@mines-paris.org>
2015-06-15 12:13:36 +0000

    Commit file that should have gone with previous commit
    git-svn-id: https://svn.osgeo.org/gdal/trunk@29359 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/swig/python/osgeo/ogr.py
+++ gdal/swig/python/osgeo/ogr.py
@@ -3418,7 +3418,14 @@ class Feature(_object):
             output['id'] = fid
             
         for key in self.keys():
-            output['properties'][key] = self.GetField(key)
+            fld_defn = self.GetFieldDefnRef(self.GetFieldIndex(key))
+            if fld_defn.GetType() == _ogr.OFTInteger and fld_defn.GetSubType() == _ogr.OFSTBoolean:
+                if self.GetField(key):
+                    output['properties'][key] = True
+                else:
+                    output['properties'][key] = False
+            else:
+                output['properties'][key] = self.GetField(key)
         
         if not as_object:
             output = simplejson.dumps(output)
