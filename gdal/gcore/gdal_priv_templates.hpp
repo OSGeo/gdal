212937f80e966ddb8ccb91432230facc523f5d3b
Even Rouault <even.rouault@mines-paris.org>
2015-07-22 08:21:24 +0000

    Improve performance of GDALCopyWords() float->byte/int16/uint16 by using SSE2
    git-svn-id: https://svn.osgeo.org/gdal/trunk@29555 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/gcore/gdal_priv_templates.hpp
+++ gdal/gcore/gdal_priv_templates.hpp
@@ -30,6 +30,8 @@
 #ifndef GDAL_PRIV_TEMPLATES_HPP_INCLUDED
 #define GDAL_PRIV_TEMPLATES_HPP_INCLUDED
 
+#define SSE_USE_SAME_ROUNDING_AS_NON_SSE
+
 #include <limits>
 
 /************************************************************************/
@@ -158,6 +160,62 @@ inline void GDALCopyWord(const float fValueIn, Tout &tValueOut)
         GDALClampValue(fValueIn + 0.5f, fMaxVal, fMinVal));
 }
 
+// Just need SSE, but don't bother being too specific
+#if (defined(__x86_64) || defined(_M_X64)) && defined(this_is_disabled)
+
+#include <xmmintrin.h>
+
+template <class Tout>
+inline void GDALCopyWordSSE(const float fValueIn, Tout &tValueOut)
+{
+    float fMaxVal, fMinVal;
+    GDALGetDataLimits<float, Tout>(fMaxVal, fMinVal);
+    __m128 xmm = _mm_set_ss(fValueIn);
+#ifdef SSE_USE_SAME_ROUNDING_AS_NON_SSE
+    __m128 mask = _mm_cmpge_ss(xmm, _mm_set_ss(0.f));
+    __m128 p0d5 = _mm_set_ss(0.5f);
+    __m128 m0d5 = _mm_set_ss(-0.5f);
+    xmm = _mm_add_ss(xmm, _mm_or_ps(_mm_and_ps(mask, p0d5), _mm_andnot_ps(mask, m0d5)));
+#endif
+    __m128 xmm_min = _mm_set_ss(fMinVal);
+    __m128 xmm_max = _mm_set_ss(fMaxVal);
+    xmm = _mm_min_ss(_mm_max_ss(xmm, xmm_min), xmm_max);
+#ifdef SSE_USE_SAME_ROUNDING_AS_NON_SSE
+    tValueOut = (Tout)_mm_cvttss_si32(xmm);
+#else
+    tValueOut = (Tout)_mm_cvtss_si32(xmm);
+#endif
+}
+
+inline void GDALCopyWord(const float fValueIn, GByte &tValueOut)
+{
+    GDALCopyWordSSE(fValueIn, tValueOut);
+}
+
+inline void GDALCopyWord(const float fValueIn, GInt16 &tValueOut)
+{
+    GDALCopyWordSSE(fValueIn, tValueOut);
+}
+
+inline void GDALCopyWord(const float fValueIn, GUInt16 &tValueOut)
+{
+    GDALCopyWordSSE(fValueIn, tValueOut);
+}
+
+#else
+
+inline void GDALCopyWord(const float fValueIn, short &nValueOut)
+{
+    float fMaxVal, fMinVal;
+    GDALGetDataLimits<float, short>(fMaxVal, fMinVal);
+    float fValue = fValueIn >= 0.0f ? fValueIn + 0.5f :
+        fValueIn - 0.5f;
+    nValueOut = static_cast<short>(
+        GDALClampValue(fValue, fMaxVal, fMinVal));
+}
+
+#endif //  defined(__x86_64) || defined(_M_X64)
+
 template <class Tout>
 inline void GDALCopyWord(const double dfValueIn, Tout &tValueOut)
 {
@@ -175,16 +233,6 @@ inline void GDALCopyWord(const double dfValueIn, int &nValueOut)
         dfValueIn - 0.5;
     nValueOut = static_cast<int>(
         GDALClampValue(dfValue, dfMaxVal, dfMinVal));
-}
-
-inline void GDALCopyWord(const float fValueIn, short &nValueOut)
-{
-    float fMaxVal, fMinVal;
-    GDALGetDataLimits<float, short>(fMaxVal, fMinVal);
-    float fValue = fValueIn >= 0.0f ? fValueIn + 0.5f :
-        fValueIn - 0.5f;
-    nValueOut = static_cast<short>(
-        GDALClampValue(fValue, fMaxVal, fMinVal));
 }
 
 inline void GDALCopyWord(const double dfValueIn, short &nValueOut)
@@ -233,5 +281,137 @@ inline void GDALCopyWord(const float fValueIn, unsigned int &nValueOut)
         nValueOut = static_cast<unsigned int>(fValueIn + 0.5f);
     }
 }
+
+#ifdef notdef
+/************************************************************************/
+/*                         GDALCopy2Words()                             */
+/************************************************************************/
+/**
+ * Copy 2 words, optionally rounding if appropriate (i.e. going
+ * from the float to the integer case).
+ *
+ * @param pValueIn pointer to 2 input values of type Tin.
+ * @param pValueOut pointer to 2 output values of type Tout.
+ */
+
+template <class Tin, class Tout>
+inline void GDALCopy2Words(const Tin* pValueIn, Tout* const &pValueOut)
+{
+    GDALCopyWord(pValueIn[0], pValueOut[0]);
+    GDALCopyWord(pValueIn[1], pValueOut[1]);
+}
+
+// Just need SSE, but don't bother being too specific
+#if defined(__x86_64) || defined(_M_X64)
+
+#include <xmmintrin.h>
+
+template <class Tout>
+inline void GDALCopy2WordsSSE(const float* pValueIn, Tout* const &pValueOut)
+{
+    float fMaxVal, fMinVal;
+    GDALGetDataLimits<float, Tout>(fMaxVal, fMinVal);
+    __m128 xmm = _mm_set_ps(0, 0, pValueIn[1], pValueIn[0]);
+    __m128 xmm_min = _mm_set_ps(0, 0, fMinVal, fMinVal);
+    __m128 xmm_max = _mm_set_ps(0, 0, fMaxVal, fMaxVal);
+    xmm = _mm_min_ps(_mm_max_ps(xmm, xmm_min), xmm_max);
+    pValueOut[0] = _mm_cvtss_si32(xmm);
+    pValueOut[1] = _mm_cvtss_si32(_mm_shuffle_ps(xmm, xmm, _MM_SHUFFLE(0, 0, 0, 1)));
+}
+
+inline void GDALCopy2Words(const float* pValueIn, GByte* const &pValueOut)
+{
+    GDALCopy2WordsSSE(pValueIn, pValueOut);
+}
+
+inline void GDALCopy2Words(const float* pValueIn, GInt16* const &pValueOut)
+{
+    GDALCopy2WordsSSE(pValueIn, pValueOut);
+}
+
+inline void GDALCopy2Words(const float* pValueIn, GUInt16* const &pValueOut)
+{
+    GDALCopy2WordsSSE(pValueIn, pValueOut);
+}
+#endif //  defined(__x86_64) || defined(_M_X64)
+
+#endif
+
+/************************************************************************/
+/*                         GDALCopy4Words()                             */
+/************************************************************************/
+/**
+ * Copy 4 words, optionally rounding if appropriate (i.e. going
+ * from the float to the integer case).
+ *
+ * @param pValueIn pointer to 4 input values of type Tin.
+ * @param pValueOut pointer to 4 output values of type Tout.
+ */
+
+template <class Tin, class Tout>
+inline void GDALCopy4Words(const Tin* pValueIn, Tout* const &pValueOut)
+{
+    GDALCopyWord(pValueIn[0], pValueOut[0]);
+    GDALCopyWord(pValueIn[1], pValueOut[1]);
+    GDALCopyWord(pValueIn[2], pValueOut[2]);
+    GDALCopyWord(pValueIn[3], pValueOut[3]);
+}
+
+// Needs SSE2 for _mm_cvtps_epi32 and store operations
+#if defined(__x86_64) || defined(_M_X64)
+
+#include <emmintrin.h>
+
+template <class Tout>
+inline void GDALCopy4WordsSSE(const float* pValueIn, Tout* const &pValueOut)
+{
+    float fMaxVal, fMinVal;
+    GDALGetDataLimits<float, Tout>(fMaxVal, fMinVal);
+    __m128 xmm = _mm_loadu_ps(pValueIn);
+#ifdef SSE_USE_SAME_ROUNDING_AS_NON_SSE
+    __m128 p0d5 = _mm_set1_ps(0.5f);
+    __m128 m0d5 = _mm_set1_ps(-0.5f);
+    //__m128 mask = _mm_cmpge_ps(xmm, _mm_set1_ps(0.f));
+    __m128 mask = _mm_cmpge_ps(xmm, p0d5);
+    xmm = _mm_add_ps(xmm, _mm_or_ps(_mm_and_ps(mask, p0d5), _mm_andnot_ps(mask, m0d5))); /* f >= 0.5f ? f + 0.5f : f - 0.5f */
+#endif
+    __m128 xmm_min = _mm_set1_ps(fMinVal);
+    __m128 xmm_max = _mm_set1_ps(fMaxVal);
+    xmm = _mm_min_ps(_mm_max_ps(xmm, xmm_min), xmm_max);
+#ifdef SSE_USE_SAME_ROUNDING_AS_NON_SSE
+    __m128i xmm_i = _mm_cvttps_epi32 (xmm);
+#else
+    __m128i xmm_i = _mm_cvtps_epi32(xmm);
+#endif
+#if 0
+    int aTemp[4];
+    _mm_storeu_si128 ( (__m128i *)aTemp, xmm_i);
+    pValueOut[0] = (Tout)aTemp[0];
+    pValueOut[1] = (Tout)aTemp[1];
+    pValueOut[2] = (Tout)aTemp[2];
+    pValueOut[3] = (Tout)aTemp[3];
+#else
+    pValueOut[0] = (Tout)_mm_extract_epi16(xmm_i, 0);
+    pValueOut[1] = (Tout)_mm_extract_epi16(xmm_i, 2);
+    pValueOut[2] = (Tout)_mm_extract_epi16(xmm_i, 4);
+    pValueOut[3] = (Tout)_mm_extract_epi16(xmm_i, 6);
+#endif
+}
+
+inline void GDALCopy4Words(const float* pValueIn, GByte* const &pValueOut)
+{
+    GDALCopy4WordsSSE(pValueIn, pValueOut);
+}
+
+inline void GDALCopy4Words(const float* pValueIn, GInt16* const &pValueOut)
+{
+    GDALCopy4WordsSSE(pValueIn, pValueOut);
+}
+
+inline void GDALCopy4Words(const float* pValueIn, GUInt16* const &pValueOut)
+{
+    GDALCopy4WordsSSE(pValueIn, pValueOut);
+}
+#endif //  defined(__x86_64) || defined(_M_X64)
 
 #endif // GDAL_PRIV_TEMPLATES_HPP_INCLUDED
