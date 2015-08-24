0c46834cdfe2f0273840651a3068c2f1588e74f7
Even Rouault <even.rouault@mines-paris.org>
2015-08-24 12:30:36 +0000

    importFromProj4(): make sure no to include a TOWGS84[0,0,0,0,0,0,0] when the datum is WGS84 (#6080)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@29775 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/utilities/test_gdalsrsinfo.py
+++ autotest/utilities/test_gdalsrsinfo.py
@@ -298,7 +298,7 @@ def test_gdalsrsinfo_13():
     ret = gdaltest.runexternal(test_cli_utilities.get_gdalsrsinfo_path() + \
                                    ' -o wkt "+proj=longlat +datum=WGS84 +no_defs"')
  
-    if ret.strip() != """GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]""":
+    if ret.strip() != """GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]""":
         return 'fail'
 
     return 'success'
