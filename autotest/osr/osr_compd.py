665b5f56cebe20f6e00a232b7ed87429e690d187
Kurt Schwehr <schwehr@gmail.com>
2015-08-28 02:42:04 +0000

    Silence warning for tests osr_comp_3 and osr_comp_4.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@29861 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/osr/osr_compd.py
+++ autotest/osr/osr_compd.py
@@ -149,9 +149,9 @@ def osr_compd_3():
     if gdaltest.equal_srs_from_wkt( exp_wkt, wkt ) == 0:
         gdaltest.post_reason( 'did not get expected compound cs for EPSG:7401')
         return 'fail'
-    elif exp_wkt != wkt:
-        print('warning they are equivalent, but not completely the same')
-        print(wkt)
+    # elif exp_wkt != wkt:
+    #    print('warning they are equivalent, but not completely the same')
+    #    print(wkt)
 
     return 'success'
 
@@ -192,9 +192,9 @@ def osr_compd_4():
     if gdaltest.equal_srs_from_wkt( exp_wkt, wkt ) == 0:
         gdaltest.post_reason( 'did not get expected compound cs for EPSG:7400')
         return 'fail'
-    elif exp_wkt != wkt:
-        print('warning they are equivalent, but not completely the same')
-        print(wkt)
+    # elif exp_wkt != wkt:
+    #    print('warning they are equivalent, but not completely the same')
+    #    print(wkt)
 
     return 'success'
 
@@ -405,4 +405,3 @@ if __name__ == '__main__':
     gdaltest.run_tests( gdaltest_list )
 
     gdaltest.summarize()
-
