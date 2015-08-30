1386d9c93096543c7442734998cb8833846a8c5d
Even Rouault <even.rouault@mines-paris.org>
2015-08-30 12:05:05 +0000

    Fix CPL_LSBINT16PTR() and CPL_LSBINT32PTR() to work with non-byte pointer (https://trac.osgeo.org/gdal/ticket/6090)
    git-svn-id: https://svn.osgeo.org/gdal/branches/2.0@29925 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/port/cpl_port.h
+++ gdal/port/cpl_port.h
@@ -522,11 +522,11 @@ char * strdup (char *instr);
 #endif
 
 /** Return a Int16 from the 2 bytes ordered in LSB order at address x */
-#define CPL_LSBINT16PTR(x)    ((*(GByte*)(x)) | ((*(GByte*)((x)+1)) << 8))
+#define CPL_LSBINT16PTR(x)    ((*(GByte*)(x)) | (*(((GByte*)(x))+1) << 8))
 
 /** Return a Int32 from the 4 bytes ordered in LSB order at address x */
-#define CPL_LSBINT32PTR(x)    ((*(GByte*)(x)) | ((*(GByte*)((x)+1)) << 8) | \
-                              ((*(GByte*)((x)+2)) << 16) | ((*(GByte*)((x)+3)) << 24))
+#define CPL_LSBINT32PTR(x)    ((*(GByte*)(x)) | (*(((GByte*)(x))+1) << 8) | \
+                              (*(((GByte*)(x))+2) << 16) | (*(((GByte*)(x))+3) << 24))
 
 /** Return a signed Int16 from the 2 bytes ordered in LSB order at address x */
 #define CPL_LSBSINT16PTR(x) ((GInt16) CPL_LSBINT16PTR(x))
