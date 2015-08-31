bdf5cbd87f67f549f466ba49bc4cd31c5aca43db
Kurt Schwehr <schwehr@gmail.com>
2015-08-31 23:58:32 +0000

    Comment out status assignments that are ignored.
    CID 1243241
    CID 1243244
    CID 1243251
    CID 1243252
    CID 1243253
    CID 1254424
    
    These will most like turn into unused return value complaints.  Added
    a TODO for actually managing the case where status is not NC_NOERR.
    
    
    git-svn-id: https://svn.osgeo.org/gdal/trunk@29984 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/netcdf/netcdfdataset.cpp
+++ gdal/frmts/netcdf/netcdfdataset.cpp
@@ -975,7 +975,7 @@ CPLErr netCDFRasterBand::CreateBandMetadata( int *paDimIds )
     int      nAtt=0;
 
     netCDFDataset *poDS = (netCDFDataset *) this->poDS;
-  
+
 /* -------------------------------------------------------------------- */
 /*      Compute all dimensions from Band number and save in Metadata    */
 /* -------------------------------------------------------------------- */
@@ -1014,19 +1014,21 @@ CPLErr netCDFRasterBand::CreateBandMetadata( int *paDimIds )
         else {
             result = (int) ( ( nLevel-Taken) % Sum );
         }
-        
-        strcpy(szVarName, 
+
+        strcpy(szVarName,
                poDS->papszDimName[paDimIds[panBandZPos[i]]] );
+
+        // TODO: Make sure all the status checks make sense.
 
         status=nc_inq_varid( cdfid, szVarName, &nVarID );
         if( status != NC_NOERR ) {
             /* Try to uppercase the first letter of the variable */
             /* Note: why is this needed? leaving for safety */
-            szVarName[0]=(char) toupper(szVarName[0]);
-            status=nc_inq_varid( cdfid, szVarName, &nVarID );
+            szVarName[0] = (char) toupper(szVarName[0]);
+            /* status = */nc_inq_varid( cdfid, szVarName, &nVarID );
         }
 
-        status = nc_inq_vartype( cdfid, nVarID, &nVarType );
+        /* status = */ nc_inq_vartype( cdfid, nVarID, &nVarType );
 
         nDims = 0;
         status = nc_inq_varndims( cdfid, nVarID, &nDims );
@@ -1037,41 +1039,41 @@ CPLErr netCDFRasterBand::CreateBandMetadata( int *paDimIds )
             switch( nVarType ) {
                 case NC_SHORT:
                     short sData;
-                    status =  nc_get_vara_short( cdfid, nVarID, 
+                    /* status = */ nc_get_vara_short( cdfid, nVarID,
                                                  start,
                                                  count, &sData );
                     sprintf( szMetaTemp,"%d", sData );
                     break;
                 case NC_INT:
                     int nData;
-                    status =  nc_get_vara_int( cdfid, nVarID, 
+                    /* status = */ nc_get_vara_int( cdfid, nVarID,
                                                start,
                                                count, &nData );
                     sprintf( szMetaTemp,"%d", nData );
                     break;
                 case NC_FLOAT:
                     float fData;
-                    status =  nc_get_vara_float( cdfid, nVarID, 
+                    /* status = */nc_get_vara_float( cdfid, nVarID,
                                                  start,
                                                  count, &fData );
                     CPLsprintf( szMetaTemp,"%.8g", fData );
                     break;
                 case NC_DOUBLE:
                     double dfData;
-                    status =  nc_get_vara_double( cdfid, nVarID, 
+                    /* status = */ nc_get_vara_double( cdfid, nVarID,
                                                   start,
                                                   count, &dfData);
                     CPLsprintf( szMetaTemp,"%.16g", dfData );
                     break;
-                default: 
-                    CPLDebug( "GDAL_netCDF", "invalid dim %s, type=%d", 
+                default:
+                    CPLDebug( "GDAL_netCDF", "invalid dim %s, type=%d",
                               szMetaTemp, nVarType);
                     break;
             }
         }
         else
             sprintf( szMetaTemp,"%d", result+1);
-	
+
 /* -------------------------------------------------------------------- */
 /*      Save dimension value                                            */
 /* -------------------------------------------------------------------- */
