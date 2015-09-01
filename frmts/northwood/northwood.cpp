71ed496b1eefae87ffe3dbc12c497626aa0fa5af
Even Rouault <even.rouault@mines-paris.org>
2015-09-01 08:17:50 +0000

    Northwoord: fix computation of intermediate color table values on non-Intel platforms (patch by gawade, https://trac.osgeo.org/gdal/ticket/6091)
    git-svn-id: https://svn.osgeo.org/gdal/branches/1.11/gdal@30005 f0d54148-0727-0410-94bb-9a71ac55c965

--- frmts/northwood/northwood.cpp
+++ frmts/northwood/northwood.cpp
@@ -387,9 +387,9 @@ void createIP( int index, unsigned char r, unsigned char g, unsigned char b,
     float bslope = (float)(b - map[wm].b) / (float)(index - wm);
     for( i = wm + 1; i < index; i++)
     {
-        map[i].r = map[wm].r + (unsigned char)(((i - wm) * rslope) + 0.5);
-        map[i].g = map[wm].g + (unsigned char)(((i - wm) * gslope) + 0.5);
-        map[i].b = map[wm].b + (unsigned char)(((i - wm) * bslope) + 0.5);
+        map[i].r = (unsigned char)(map[wm].r + ((i - wm) * rslope) + 0.5);
+        map[i].g = (unsigned char)(map[wm].g + ((i - wm) * gslope) + 0.5);
+        map[i].b = (unsigned char)(map[wm].b + ((i - wm) * bslope) + 0.5);
     }
     map[index].r = r;
     map[index].g = g;
