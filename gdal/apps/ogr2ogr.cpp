b7acd9886dae997c7a8c18bf71974edbac9860f7
Kurt Schwehr <schwehr@gmail.com>
2015-09-02 22:32:41 +0000

    Stop use after free of poDupGeometry.
    CID 1270990
    
    SetGeomFieldDirectly takes ownership of poDupGeometry and
    poDupGeometry is an invalid pointer after the call.  poDstGeometry =
    poDupGeometry would be a redundant call even if the pointer were
    valid.
    
    
    git-svn-id: https://svn.osgeo.org/gdal/trunk@30041 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/apps/ogr2ogr.cpp
+++ gdal/apps/ogr2ogr.cpp
@@ -3899,7 +3899,6 @@ int LayerTranslator::Translate( TargetLayerInfo* psInfo,
                     /* This will correct the coordinate dimension to 3 */
                     OGRGeometry* poDupGeometry = poDstGeometry->clone();
                     poDstFeature->SetGeomFieldDirectly(iGeom, poDupGeometry);
-                    poDstGeometry = poDupGeometry;
                 }
 
                 if (nCoordDim == 2 || nCoordDim == 3)
