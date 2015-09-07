1ee8aec393b7af93c249c596e0f2dafd8b805265
Kurt Schwehr <schwehr@gmail.com>
2015-09-07 00:11:29 +0000

    Cleanup CPLListInsert() and CPLListGetLast() a little to help Coverity.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@30165 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/port/cpl_list.cpp
+++ gdal/port/cpl_list.cpp
@@ -55,7 +55,7 @@ CPL_CVSID("$Id$");
 CPLList *CPLListAppend( CPLList *psList, void *pData )
 {
     CPLList *psLast;
-    
+
     /* Allocate room for the new object */
     if ( psList == NULL )
     {
@@ -81,7 +81,7 @@ CPLList *CPLListAppend( CPLList *psList, void *pData )
 /**
  * Insert an object into list at specified position (zero based).
  * If the input list is NULL, then a new list is created.
- * 
+ *
  * @param psList pointer to list head.
  * @param pData pointer to inserted data object. May be NULL.
  * @param nPosition position number to insert an object.
@@ -91,13 +91,8 @@ CPLList *CPLListAppend( CPLList *psList, void *pData )
 
 CPLList *CPLListInsert( CPLList *psList, void *pData, int nPosition )
 {
-    CPLList *psCurrent;
-    int     i, nCount;
-    
     if ( nPosition < 0 )
         return psList;      /* Nothing to do!*/
-
-    nCount = CPLListCount( psList );
 
     if ( nPosition == 0)
     {
@@ -105,12 +100,16 @@ CPLList *CPLListInsert( CPLList *psList, void *pData, int nPosition )
         psNew->pData = pData;
         psNew->psNext = psList;
         psList = psNew;
+        return psList;
     }
-    else if ( nCount < nPosition )
+
+    const int nCount = CPLListCount( psList );
+
+    if ( nCount < nPosition )
     {
         /* Allocate room for the new object */
         CPLList* psLast = CPLListGetLast(psList);
-        for ( i = nCount; i <= nPosition - 1; i++ )
+        for ( int i = nCount; i <= nPosition - 1; i++ )
         {
             psLast = CPLListAppend( psLast, NULL );
             if (psList == NULL)
@@ -121,18 +120,18 @@ CPLList *CPLListInsert( CPLList *psList, void *pData, int nPosition )
         psLast = CPLListAppend( psLast, pData );
         if (psList == NULL)
             psList = psLast;
-    }
-    else
-    {
-        CPLList *psNew = (CPLList *)CPLMalloc( sizeof(CPLList) );
-        psNew->pData = pData;
 
-        psCurrent = psList;
-        for ( i = 0; i < nPosition - 1; i++ )
-            psCurrent = psCurrent->psNext;
-        psNew->psNext = psCurrent->psNext;
-        psCurrent->psNext = psNew;
+        return psList;
     }
+
+    CPLList *psNew = (CPLList *)CPLMalloc( sizeof(CPLList) );
+    psNew->pData = pData;
+
+    CPLList *psCurrent = psList;
+    for ( int i = 0; i < nPosition - 1; i++ )
+        psCurrent = psCurrent->psNext;
+    psNew->psNext = psCurrent->psNext;
+    psCurrent->psNext = psNew;
 
     return psList;
 }
@@ -143,7 +142,7 @@ CPLList *CPLListInsert( CPLList *psList, void *pData, int nPosition )
 
 /**
  * Return the pointer to last element in a list.
- * 
+ *
  * @param psList pointer to list head.
  *
  * @return pointer to last element in a list.
@@ -151,11 +150,10 @@ CPLList *CPLListInsert( CPLList *psList, void *pData, int nPosition )
 
 CPLList *CPLListGetLast( CPLList *psList )
 {
-    CPLList *psCurrent = psList;
-
     if ( psList == NULL )
         return NULL;
 
+    CPLList *psCurrent = psList;
     while ( psCurrent->psNext )
         psCurrent = psCurrent->psNext;
 
@@ -168,7 +166,7 @@ CPLList *CPLListGetLast( CPLList *psList )
 
 /**
  * Return the pointer to the specified element in a list.
- * 
+ *
  * @param psList pointer to list head.
  * @param nPosition the index of the element in the list, 0 being the first element
  *
@@ -336,4 +334,3 @@ void *CPLListGetData( CPLList *psElement )
     else
         return psElement->pData;
 }
-
