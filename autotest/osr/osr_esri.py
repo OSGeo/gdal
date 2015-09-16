19f9df5d88f8af2bb7c71847ce00cdc26e399e16
Even Rouault <even.rouault@mines-paris.org>
2015-09-16 09:41:43 +0000

    morphToESRI(): use Mercator_Auxiliary_Sphere projection for EPSG:3857. morphFromESRI(): map Mercator_Auxiliary_Sphere to EPSG:3857 (https://trac.osgeo.org/gdal/ticket/5924)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@30435 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/osr/osr_esri.py
+++ autotest/osr/osr_esri.py
@@ -1347,6 +1347,63 @@ def osr_esri_28():
     return 'success'
 
 ###############################################################################
+# Test Web Mercator
+
+def osr_esri_29():
+    
+    srs = osr.SpatialReference()
+    srs.ImportFromEPSG(3857)
+
+    srs.MorphToESRI()
+    got_wkt = srs.ExportToPrettyWkt()
+    if got_wkt != """PROJCS["WGS_1984_Web_Mercator_Auxiliary_Sphere",
+    GEOGCS["GCS_WGS_1984",
+        DATUM["D_WGS_1984",
+            SPHEROID["WGS_1984",6378137.0,298.257223563]],
+        PRIMEM["Greenwich",0.0],
+        UNIT["Degree",0.0174532925199433]],
+    PROJECTION["Mercator_Auxiliary_Sphere"],
+    PARAMETER["False_Easting",0.0],
+    PARAMETER["False_Northing",0.0],
+    PARAMETER["Central_Meridian",0.0],
+    PARAMETER["Standard_Parallel_1",0.0],
+    PARAMETER["Auxiliary_Sphere_Type",0.0],
+    UNIT["Meter",1.0]]""":
+        gdaltest.post_reason('fail')
+        print(got_wkt)
+        return 'fail'
+
+    srs.MorphFromESRI()
+    got_wkt = srs.ExportToPrettyWkt()
+    if got_wkt != """PROJCS["WGS 84 / Pseudo-Mercator",
+    GEOGCS["WGS 84",
+        DATUM["WGS_1984",
+            SPHEROID["WGS 84",6378137,298.257223563,
+                AUTHORITY["EPSG","7030"]],
+            AUTHORITY["EPSG","6326"]],
+        PRIMEM["Greenwich",0,
+            AUTHORITY["EPSG","8901"]],
+        UNIT["degree",0.0174532925199433,
+            AUTHORITY["EPSG","9122"]],
+        AUTHORITY["EPSG","4326"]],
+    PROJECTION["Mercator_1SP"],
+    PARAMETER["central_meridian",0],
+    PARAMETER["scale_factor",1],
+    PARAMETER["false_easting",0],
+    PARAMETER["false_northing",0],
+    UNIT["metre",1,
+        AUTHORITY["EPSG","9001"]],
+    AXIS["X",EAST],
+    AXIS["Y",NORTH],
+    EXTENSION["PROJ4","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"],
+    AUTHORITY["EPSG","3857"]]""":
+        gdaltest.post_reason('fail')
+        print(got_wkt)
+        return 'fail'
+
+    return 'success'
+
+###############################################################################
 #
 
 gdaltest_list = [
@@ -1378,7 +1435,10 @@ gdaltest_list = [
     osr_esri_26,
     osr_esri_27,
     osr_esri_28,
+    osr_esri_29,
    None ]
+
+#gdaltest_list = [osr_esri_29]
 
 if __name__ == '__main__':
 
