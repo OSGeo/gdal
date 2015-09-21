3d3e127b19175aedea7569af280493d341773908
Kurt Schwehr <schwehr@gmail.com>
2015-09-21 05:54:36 +0000

    Free pabyOutData if data load via VRTComplexSource::RasterIOInternal fails.
    CID 1258550
    
    
    git-svn-id: https://svn.osgeo.org/gdal/trunk@30574 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/vrt/vrtfilters.cpp
+++ gdal/frmts/vrt/vrtfilters.cpp
@@ -223,7 +223,7 @@ VRTFilteredSource::RasterIO( int nXOff, int nYOff, int nXSize, int nYSize,
 /*      not of the same type as our working format, or if the passed    */
 /*      in buffer has an unusual organization.                          */
 /* -------------------------------------------------------------------- */
-    GByte *pabyOutData;
+    GByte *pabyOutData = NULL;
 
     if( nPixelSpace != nPixelOffset || nLineSpace != nLineOffset
         || eOperDataType != eBufType )
@@ -297,6 +297,9 @@ VRTFilteredSource::RasterIO( int nXOff, int nYOff, int nXSize, int nYSize,
     {
         if( pabyWorkData != pData )
             VSIFree( pabyWorkData );
+
+        if( pabyOutData != pData )
+            VSIFree( pabyOutData );
 
         return eErr;
     }
@@ -645,4 +648,3 @@ VRTSource *VRTParseFilterSources( CPLXMLNode *psChild, const char *pszVRTPath )
 
     return NULL;
 }
-
