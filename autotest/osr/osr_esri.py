ba7f9d5632bd8a27da39dcf5f7763dc25fdd2159
Even Rouault <even.rouault@mines-paris.org>
2015-09-28 19:37:17 +0000

    morphFromESRI(): special case with PROJCS name 'WGS_84_Pseudo_Mercator' (https://trac.osgeo.org/gdal/ticket/6134)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@30732 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/osr/osr_esri.py
+++ autotest/osr/osr_esri.py
@@ -1401,6 +1401,36 @@ def osr_esri_29():
         print(got_wkt)
         return 'fail'
 
+    # 6134
+    srs.SetFromUserInput("""PROJCS["WGS_84_Pseudo_Mercator",GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]],PROJECTION["Mercator"],PARAMETER["central_meridian",0],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["Meter",1],PARAMETER["standard_parallel_1",0.0]]""")
+    srs.MorphFromESRI()
+    got_wkt = srs.ExportToPrettyWkt()
+    if got_wkt != """PROJCS["WGS 84 / Pseudo-Mercator",
+    GEOGCS["WGS 84",
+        DATUM["WGS_1984",
+            SPHEROID["WGS 84",6378137,298.257223563,
+                AUTHORITY["EPSG","7030"]],
+            AUTHORITY["EPSG","6326"]],
+        PRIMEM["Greenwich",0,
+            AUTHORITY["EPSG","8901"]],
+        UNIT["degree",0.0174532925199433,
+            AUTHORITY["EPSG","9122"]],
+        AUTHORITY["EPSG","4326"]],
+    PROJECTION["Mercator_1SP"],
+    PARAMETER["central_meridian",0],
+    PARAMETER["scale_factor",1],
+    PARAMETER["false_easting",0],
+    PARAMETER["false_northing",0],
+    UNIT["metre",1,
+        AUTHORITY["EPSG","9001"]],
+    AXIS["X",EAST],
+    AXIS["Y",NORTH],
+    EXTENSION["PROJ4","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"],
+    AUTHORITY["EPSG","3857"]]""":
+        gdaltest.post_reason('fail')
+        print(got_wkt)
+        return 'fail'
+
     return 'success'
 
 ###############################################################################
