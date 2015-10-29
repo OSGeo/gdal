94370b124eaabe0790efc4dac2429654ce1c9232
Kurt Schwehr <schwehr@gmail.com>
2015-10-29 20:18:17 +0000

    ADM_STD_HEADER_SIZE is not used in fastdataset.cpp
    git-svn-id: https://svn.osgeo.org/gdal/trunk@31269 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/raw/fastdataset.cpp
+++ gdal/frmts/raw/fastdataset.cpp
@@ -1,3 +1,4 @@
+
 /******************************************************************************
  * $Id$
  *
@@ -38,7 +39,7 @@ CPL_C_START
 void	GDALRegister_FAST(void);
 CPL_C_END
 
-static const int ADM_STD_HEADER_SIZE = 4608;  // XXX: Format specification says it
+// static const int ADM_STD_HEADER_SIZE = 4608;  // XXX: Format specification says it
 static const int ADM_HEADER_SIZE = 5000;  // should be 4608, but some vendors
                                           // ship broken large datasets.
 static const int ADM_MIN_HEADER_SIZE = 1536;  // ...and sometimes it can be
