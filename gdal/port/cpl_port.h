7c1908e43b9b78060f1f70093bceffa3c74df471
Even Rouault <even.rouault@mines-paris.org>
2015-11-02 20:18:01 +0000

    GTiff: improve VirtualMemIO() performance in tiled contig read to single band copy
    git-svn-id: https://svn.osgeo.org/gdal/trunk@31325 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/port/cpl_port.h
+++ gdal/port/cpl_port.h
@@ -645,6 +645,13 @@ static char *cvsid_aw() { return( cvsid_aw() ? ((char *) NULL) : cpl_cvsid ); }
 #  define CPL_RETURNS_NONNULL
 #endif
 
+
+#if defined(__GNUC__) && __GNUC__ >= 4 && !defined(DOXYGEN_SKIP)
+#define CPL_RESTRICT __restrict__
+#else
+#define CPL_RESTRICT
+#endif
+
 /* Helper to remove the copy and assignment constructors so that the compiler
    will not generate the default versions.
 
