3ddf92a233a377fc494afe7dd682ab7ab8adacdf
Even Rouault <even.rouault@mines-paris.org>
2015-11-21 13:06:18 +0000

    Disable MSVC warnings for mysql headers
    git-svn-id: https://svn.osgeo.org/gdal/trunk@31677 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/mysql/ogr_mysql.h
+++ gdal/ogr/ogrsf_frmts/mysql/ogr_mysql.h
@@ -32,8 +32,19 @@
 #ifndef _OGR_MYSQL_H_INCLUDED
 #define _OGR_MYSQL_H_INCLUDED
 
+
+#ifdef _MSC_VER
+#pragma warning( push )
+#pragma warning( disable : 4324 ) /* 'my_alignment_imp<0x02>' : structure was padded due to __declspec(align()) */
+#pragma warning( disable : 4201 ) /* nonstandard extension used : nameless struct/union */
+#endif
+
 #include <my_global.h>
 #include <mysql.h>
+
+#ifdef _MSC_VER
+#pragma warning( pop ) 
+#endif
 
 /* my_global.h from mysql 5.1 declares the min and max macros. */
 /* This conflicts with templates in g++-4.3.2 header files. Grrr */
