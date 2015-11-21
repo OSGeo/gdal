0726249d5b8cdd68557ab71c5886012e9b51e691
Kurt Schwehr <schwehr@gmail.com>
2015-11-21 01:00:02 +0000

    Clean cpl_time.cpp and cpl_time.h.
    - Covert #defines to static const int or static functions
    - C casts to C++ casts
    - Collapse definition and initialization
    
    
    
    git-svn-id: https://svn.osgeo.org/gdal/trunk@31668 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/port/cpl_time.cpp
+++ gdal/port/cpl_time.cpp
@@ -20,38 +20,41 @@
 
 #include "cpl_time.h"
 
-#define SECSPERMIN      60L
-#define MINSPERHOUR     60L
-#define HOURSPERDAY     24L
-#define SECSPERHOUR     (SECSPERMIN * MINSPERHOUR)
-#define SECSPERDAY      (SECSPERHOUR * HOURSPERDAY)
-#define DAYSPERWEEK     7
-#define MONSPERYEAR     12
+static const int SECSPERMIN = 60;
+static const int MINSPERHOUR = 60;
+static const int HOURSPERDAY = 24;
+static const int SECSPERHOUR = SECSPERMIN * MINSPERHOUR;
+static const int SECSPERDAY = SECSPERHOUR * HOURSPERDAY;
+static const int DAYSPERWEEK = 7;
+static const int MONSPERYEAR = 12;
 
-#define EPOCH_YEAR      1970
-#define EPOCH_WDAY      4
-#define TM_YEAR_BASE    1900
-#define DAYSPERNYEAR    365
-#define DAYSPERLYEAR    366
+static const int EPOCH_YEAR = 1970;
+static const int EPOCH_WDAY = 4;
+static const int TM_YEAR_BASE = 1900;
+static const int DAYSPERNYEAR = 365;
+static const int DAYSPERLYEAR = 366;
 
-#define isleap(y) ((((y) % 4) == 0 && ((y) % 100) != 0) || ((y) % 400) == 0)
-#define LEAPS_THRU_END_OF(y)	((y) / 4 - (y) / 100 + (y) / 400)
+static bool isleap(int y) {
+  return ((y % 4) == 0 && (y % 100) != 0) || (y % 400) == 0;
+}
+
+static int LEAPS_THRU_END_OF(int y) {
+  return y / 4 - y / 100 + y / 400;
+}
 
 static const int mon_lengths[2][MONSPERYEAR] = {
   {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
   {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 } ;
 
-
-static const int	year_lengths[2] = {
-	DAYSPERNYEAR, DAYSPERLYEAR
-};
+static const int year_lengths[2] = { DAYSPERNYEAR, DAYSPERLYEAR };
 
 /************************************************************************/
 /*                   CPLUnixTimeToYMDHMS()                              */
 /************************************************************************/
 
-/** Converts a time value since the Epoch (aka "unix" time) to a broken-down UTC time.
+/** Converts a time value since the Epoch (aka "unix" time) to a broken-down
+ *  UTC time.
  *
  * This function is similar to gmtime_r().
  * This function will always set tm_isdst to 0.
@@ -66,30 +69,30 @@ struct tm * CPLUnixTimeToYMDHMS(GIntBig unixTime, struct tm* pRet)
 {
     GIntBig days = unixTime / SECSPERDAY;
     GIntBig rem = unixTime % SECSPERDAY;
-    
+
     while (rem < 0) {
         rem += SECSPERDAY;
         --days;
     }
-    
-    pRet->tm_hour = (int) (rem / SECSPERHOUR);
+
+    pRet->tm_hour = static_cast<int>( rem / SECSPERHOUR );
     rem = rem % SECSPERHOUR;
-    pRet->tm_min = (int) (rem / SECSPERMIN);
+    pRet->tm_min = static_cast<int>( rem / SECSPERMIN );
     /*
     ** A positive leap second requires a special
     ** representation.  This uses "... ??:59:60" et seq.
     */
-    pRet->tm_sec = (int) (rem % SECSPERMIN);
-    pRet->tm_wday = (int) ((EPOCH_WDAY + days) % DAYSPERWEEK);
+    pRet->tm_sec = static_cast<int>( rem % SECSPERMIN );
+    pRet->tm_wday = static_cast<int>( (EPOCH_WDAY + days) % DAYSPERWEEK );
     if (pRet->tm_wday < 0)
         pRet->tm_wday += DAYSPERWEEK;
-    GIntBig y = EPOCH_YEAR;
-    int yleap;
-    while (days < 0 || days >= (GIntBig) year_lengths[yleap = isleap(y)])
-    {
-        GIntBig	newy;
 
-        newy = y + days / DAYSPERNYEAR;
+    GIntBig y = EPOCH_YEAR;
+    int yleap = 0;
+    while ( days < 0
+            || days >= static_cast<GIntBig>( year_lengths[yleap = isleap(y)] ) )
+    {
+        GIntBig newy = y + days / DAYSPERNYEAR;
         if (days < 0)
             --newy;
         days -= (newy - y) * DAYSPERNYEAR +
@@ -97,14 +100,19 @@ struct tm * CPLUnixTimeToYMDHMS(GIntBig unixTime, struct tm* pRet)
             LEAPS_THRU_END_OF(y - 1);
         y = newy;
     }
-    pRet->tm_year = (int) (y - TM_YEAR_BASE);
-    pRet->tm_yday = (int) days;
+
+    pRet->tm_year = static_cast<int>( y - TM_YEAR_BASE );
+    pRet->tm_yday = static_cast<int>( days );
     const int* ip = mon_lengths[yleap];
-    for (pRet->tm_mon = 0; days >= (GIntBig) ip[pRet->tm_mon]; ++(pRet->tm_mon))
-        days = days - (GIntBig) ip[pRet->tm_mon];
-    pRet->tm_mday = (int) (days + 1);
+
+    for (pRet->tm_mon = 0;
+         days >= static_cast<GIntBig>( ip[pRet->tm_mon] );
+         ++(pRet->tm_mon) )
+        days = days - static_cast<GIntBig>( ip[pRet->tm_mon] );
+
+    pRet->tm_mday = static_cast<int>( (days + 1) );
     pRet->tm_isdst = 0;
-    
+
     return pRet;
 }
 
@@ -114,36 +122,40 @@ struct tm * CPLUnixTimeToYMDHMS(GIntBig unixTime, struct tm* pRet)
 
 /** Converts a broken-down UTC time into time since the Epoch (aka "unix" time).
  *
- * This function is similar to mktime(), but the passed structure is not modified.
- * This function ignores the tm_wday, tm_yday and tm_isdst fields of the passed value.
- * No timezone shift will be applied. This function returns 0 for the 1/1/1970 00:00:00
+ * This function is similar to mktime(), but the passed structure is not
+ * modified.  This function ignores the tm_wday, tm_yday and tm_isdst fields of
+ * the passed value.  No timezone shift will be applied. This function
+ * returns 0 for the 1/1/1970 00:00:00
  *
  * @param brokendowntime broken-downtime UTC time.
  *
- * @return a number of seconds since the Epoch encoded as a value of type GIntBig,
- *         or -1 if the time cannot be represented.
+ * @return a number of seconds since the Epoch encoded as a value of type
+ *         GIntBig, or -1 if the time cannot be represented.
  */
 
 GIntBig CPLYMDHMSToUnixTime(const struct tm *brokendowntime)
 {
-  GIntBig days;
-  int mon;
-  
+
   if (brokendowntime->tm_mon < 0 || brokendowntime->tm_mon >= 12)
     return -1;
-    
+
   /* Number of days of the current month */
-  days = brokendowntime->tm_mday - 1;
-  
+  GIntBig days = brokendowntime->tm_mday - 1;
+
   /* Add the number of days of the current year */
   const int* ip = mon_lengths[isleap(TM_YEAR_BASE + brokendowntime->tm_year)];
-  for(mon=0;mon<brokendowntime->tm_mon;mon++)
-      days += ip [mon];
+  for( int mon = 0; mon < brokendowntime->tm_mon; mon++ )
+      days += ip[mon];
 
   /* Add the number of days of the other years */
-  days += (TM_YEAR_BASE + (GIntBig)brokendowntime->tm_year - EPOCH_YEAR) * DAYSPERNYEAR +
-          LEAPS_THRU_END_OF(TM_YEAR_BASE + (GIntBig)brokendowntime->tm_year - 1) -
-          LEAPS_THRU_END_OF(EPOCH_YEAR - 1);
+  days +=
+      ( TM_YEAR_BASE
+        + static_cast<GIntBig>( brokendowntime->tm_year )
+        - EPOCH_YEAR ) * DAYSPERNYEAR
+      + LEAPS_THRU_END_OF( TM_YEAR_BASE
+                           + static_cast<GIntBig>( brokendowntime->tm_year )
+                           - 1)
+      - LEAPS_THRU_END_OF( EPOCH_YEAR - 1 );
 
   /* Now add the secondes, minutes and hours to the number of days since EPOCH */
   return brokendowntime->tm_sec +
