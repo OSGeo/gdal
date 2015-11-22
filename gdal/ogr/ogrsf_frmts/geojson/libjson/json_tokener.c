d1f0bb8f3b3edb11f5d9723f631c2d6eb99513cc
Even Rouault <even.rouault@mines-paris.org>
2015-11-22 23:29:11 +0000

    internal libjson: fix out-of-bound read if passed wrong error code (CID 1076806, fixed in upstream libjson)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@31728 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/geojson/libjson/json_tokener.c
+++ gdal/ogr/ogrsf_frmts/geojson/libjson/json_tokener.c
@@ -76,7 +76,7 @@ const char* json_tokener_errors[] = {
 const char *json_tokener_error_desc(enum json_tokener_error jerr)
 {
 	int jerr_int = (int)jerr;
-	if (jerr_int < 0 || jerr_int > (int)sizeof(json_tokener_errors))
+	if (jerr_int < 0 || jerr_int >= (int)(sizeof(json_tokener_errors) / sizeof(json_tokener_errors[0])))
 		return "Unknown error, invalid json_tokener_error value passed to json_tokener_error_desc()";
 	return json_tokener_errors[jerr];
 }
