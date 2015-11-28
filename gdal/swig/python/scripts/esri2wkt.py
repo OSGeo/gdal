49998bacf25a7fb4fd4a68e7d97ef694d36da5f9
Kurt Schwehr <schwehr@gmail.com>
2015-11-28 23:17:53 +0000

    Trivial cleanup just to trigger a Travis-CI build.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@31814 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/swig/python/scripts/esri2wkt.py
+++ gdal/swig/python/scripts/esri2wkt.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
 #******************************************************************************
 #  $Id$
-# 
+#
 #  Project:  GDAL
 #  Purpose:  Simple command line program for translating ESRI .prj files
-#            into WKT. 
+#            into WKT.
 #  Author:   Frank Warmerdam, warmerda@home.com
-# 
+#
 #******************************************************************************
 #  Copyright (c) 2000, Frank Warmerdam
-# 
+#
 #  Permission is hereby granted, free of charge, to any person obtaining a
 #  copy of this software and associated documentation files (the "Software"),
 #  to deal in the Software without restriction, including without limitation
 #  the rights to use, copy, modify, merge, publish, distribute, sublicense,
 #  and/or sell copies of the Software, and to permit persons to whom the
 #  Software is furnished to do so, subject to the following conditions:
-# 
+#
 #  The above copyright notice and this permission notice shall be included
 #  in all copies or substantial portions of the Software.
-# 
+#
 #  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
 #  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 #  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -40,7 +40,7 @@ import string
 if len(sys.argv) < 2:
     print('Usage: esri2wkt.py <esri .prj file>')
     sys.exit(1)
-    
+
 prj_fd = open( sys.argv[1] )
 prj_lines = prj_fd.readlines()
 prj_fd.close()
