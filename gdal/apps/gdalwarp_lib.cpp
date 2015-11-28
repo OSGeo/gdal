ddf006ee73bdefec0c213b4ead45daa4a8daed3c
Even Rouault <even.rouault@mines-paris.org>
2015-11-28 16:44:17 +0000

    GDALWarp(): do not reset error state before flushcache
    git-svn-id: https://svn.osgeo.org/gdal/trunk@31809 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/apps/gdalwarp_lib.cpp
+++ gdal/apps/gdalwarp_lib.cpp
@@ -452,6 +452,7 @@ GDALWarpAppOptions* GDALWarpAppOptionsClone(const GDALWarpAppOptions *psOptionsI
 GDALDatasetH GDALWarp( const char *pszDest, GDALDatasetH hDstDS, int nSrcCount,
                        GDALDatasetH *pahSrcDS, const GDALWarpAppOptions *psOptionsIn, int *pbUsageError )
 {
+    CPLErrorReset();
     if( pszDest == NULL && hDstDS == NULL )
     {
         CPLError( CE_Failure, CPLE_AppDefined, "pszDest == NULL && hDstDS == NULL");
@@ -1398,10 +1399,13 @@ GDALDatasetH GDALWarp( const char *pszDest, GDALDatasetH hDstDS, int nSrcCount,
 /* -------------------------------------------------------------------- */
 /*      Final Cleanup.                                                  */
 /* -------------------------------------------------------------------- */
-    CPLErrorReset();
+    CPLErr eErrBefore = CPLGetLastErrorType();
     GDALFlushCache( hDstDS );
-    if( CPLGetLastErrorType() == CE_Failure )
+    if (eErrBefore == CE_None &&
+        CPLGetLastErrorType() != CE_None)
+    {
         bHasGotErr = TRUE;
+    }
 
 #ifdef OGR_ENABLED
     if( hCutline != NULL )
