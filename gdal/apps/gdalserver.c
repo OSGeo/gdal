326721d33fd321d36d3320527a57b9274c95ff7e
Even Rouault <even.rouault@mines-paris.org>
2015-12-13 00:50:49 +0000

    Compilation fixes for Solaris 11
    git-svn-id: https://svn.osgeo.org/gdal/trunk@32152 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/apps/gdalserver.c
+++ gdal/apps/gdalserver.c
@@ -28,7 +28,9 @@
  ****************************************************************************/
 
 // So that __USE_XOPEN2K is defined to have getaddrinfo
+#ifndef __sun__
 #define _XOPEN_SOURCE 600
+#endif
 
 #include "cpl_port.h"
 
