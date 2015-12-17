986f5275b909bcc2bd4491109fdd9b367b409dbe
Even Rouault <even.rouault@mines-paris.org>
2015-12-17 23:06:20 +0000

    GTiff: SetMetadata() doesn't properly clear existing PAM metadata (complement to https://trac.osgeo.org/gdal/ticket/5807)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@32208 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/gtiff/geotiff.cpp
+++ gdal/frmts/gtiff/geotiff.cpp
@@ -4052,7 +4052,7 @@ CPLErr GTiffRasterBand::SetMetadata( char ** papszMD, const char *pszDomain )
             // Cancel any existing metadata from PAM file
             if( eAccess == GA_Update &&
                 GDALPamRasterBand::GetMetadata(pszDomain) != NULL )
-                GDALPamRasterBand::SetMetadata(papszMD, pszDomain);
+                GDALPamRasterBand::SetMetadata(NULL, pszDomain);
         }
     }
 
@@ -14504,7 +14504,7 @@ CPLErr GTiffDataset::SetMetadata( char ** papszMD, const char *pszDomain )
         // Cancel any existing metadata from PAM file
         if( eAccess == GA_Update &&
             GDALPamDataset::GetMetadata(pszDomain) != NULL )
-            GDALPamDataset::SetMetadata(papszMD, pszDomain);
+            GDALPamDataset::SetMetadata(NULL, pszDomain);
     }
 
     if( (pszDomain == NULL || EQUAL(pszDomain, "")) &&
