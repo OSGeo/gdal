OSGeo/gdal

commit 323604e0519583ec0a5aa302aba82d30f6ee9acb

Remove local C++ declarations for GDALRegister_* and use the gdal_frmts.h include (#6268).
Partially complete - frmts/ git through netcdf.

Mark sentinal2 to leave alone for awhile.


git-svn-id: https://svn.osgeo.org/gdal/trunk@32213 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/frmts/gtiff/geotiff.cpp
@@ -36,14 +36,21 @@
 #define _GNU_SOURCE
 #endif
 
+#include <set>
+
+#include "cpl_string.h"
+#include "cpl_csv.h"
+#include "cpl_minixml.h"
+#include "cpl_multiproc.h"
+#include "cpl_vsi_virtual.h"
+#include "cpl_worker_thread_pool.h"
+#include "gdal_frmts.h"
 #include "gdal_pam.h"
+
 #define CPL_SERV_H_INCLUDED
 
 #include "xtiffio.h"
 #include "geovalues.h"
-#include "cpl_string.h"
-#include "cpl_csv.h"
-#include "cpl_minixml.h"
 #include "gt_overview.h"
 #include "ogr_spatialref.h"
 #include "tif_float.h"
@@ -52,13 +59,9 @@
 #include "gt_wkt_srs.h"
 #include "gt_wkt_srs_priv.h"
 #include "tifvsi.h"
-#include "cpl_multiproc.h"
 #include "cplkeywordparser.h"
 #include "gt_jpeg_copy.h"
-#include "cpl_vsi_virtual.h"
-#include <set>
 #include "gdal_mdreader.h"
-#include "cpl_worker_thread_pool.h"
 
 #ifdef INTERNAL_LIBTIFF
 #include "tiffiop.h"

gdal/frmts/gif/biggifdataset.cpp
@@ -30,15 +30,12 @@
  * DEALINGS IN THE SOFTWARE.
  ****************************************************************************/
 
-#include "gdal_pam.h"
 #include "cpl_string.h"
+#include "gdal_frmts.h"
+#include "gdal_pam.h"
 #include "gifabstractdataset.h"
 
 CPL_CVSID("$Id$");
-
-CPL_C_START
-void GDALRegister_BIGGIF();
-CPL_C_END
 
 /************************************************************************/
 /* ==================================================================== */

gdal/frmts/gif/gifdataset.cpp
@@ -28,15 +28,14 @@
  * DEALINGS IN THE SOFTWARE.
  ****************************************************************************/
 
-#include "gdal_pam.h"
 #include "cpl_string.h"
+#include "gdal_frmts.h"
+#include "gdal_pam.h"
 #include "gifabstractdataset.h"
 
 CPL_CVSID("$Id$");
 
 CPL_C_START
-void GDALRegister_GIF();
-
 #if !(defined(GIFLIB_MAJOR) && GIFLIB_MAJOR >= 5)
 
 // This prototype seems to have been messed up!

gdal/frmts/grass/grass57dataset.cpp
@@ -42,6 +42,11 @@ extern "C" {
 #undef class
 #endif
 
+#include "cpl_string.h"
+#include "gdal_frmts.h"
+#include "gdal_priv.h"
+#include "ogr_spatialref.h"
+
 #include <grass/version.h>
 #include <grass/gprojects.h>
 #include <grass/gis.h>
@@ -57,17 +62,9 @@ char *GPJ_grass_to_wkt(struct Key_Value *,
 #endif
 }
 
-#include "gdal_priv.h"
-#include "cpl_string.h"
-#include "ogr_spatialref.h"
-
 #define GRASS_MAX_COLORS 100000  // what is the right value
 
 CPL_CVSID("$Id$");
-
-CPL_C_START
-void GDALRegister_GRASS();
-CPL_C_END
 
 #if GRASS_VERSION_MAJOR  >= 7
 #define G_get_cellhd             Rast_get_cellhd

gdal/frmts/grass/grassdataset.cpp
@@ -30,15 +30,12 @@
 
 #include <libgrass.h>
 
-#include "gdal_priv.h"
 #include "cpl_string.h"
+#include "gdal_frmts.h"
+#include "gdal_priv.h"
 #include "ogr_spatialref.h"
 
 CPL_CVSID("$Id$");
-
-CPL_C_START
-void GDALRegister_GRASS();
-CPL_C_END
 
 /************************************************************************/
 /*                         Grass2CPLErrorHook()                         */

gdal/frmts/grib/gribdataset.cpp
@@ -30,8 +30,10 @@
  *
  */
 
-#include "gdal_pam.h"
 #include "cpl_multiproc.h"
+#include "gdal_frmts.h"
+#include "gdal_pam.h"
+#include "ogr_spatialref.h"
 
 #include "degrib18/degrib/degrib2.h"
 #include "degrib18/degrib/inventory.h"
@@ -39,13 +41,7 @@
 #include "degrib18/degrib/filedatasource.h"
 #include "degrib18/degrib/memorydatasource.h"
 
-#include "ogr_spatialref.h"
-
 CPL_CVSID("$Id$");
-
-CPL_C_START
-void GDALRegister_GRIB();
-CPL_C_END
 
 static CPLMutex *hGRIBMutex = NULL;
 

gdal/frmts/gsg/gs7bgdataset.cpp
@@ -35,6 +35,7 @@
 #include <climits>
 #include <cmath>
 
+#include "gdal_frmts.h"
 #include "gdal_pam.h"
 
 #ifndef DBL_MAX
@@ -62,10 +63,6 @@
 #endif /* SHRT_MAX */
 
 CPL_CVSID("$Id$");
-
-CPL_C_START
-void GDALRegister_GS7BG();
-CPL_C_END
 
 /************************************************************************/
 /* ==================================================================== */

gdal/frmts/gsg/gsagdataset.cpp
@@ -36,6 +36,7 @@
 #include <limits.h>
 #include <assert.h>
 
+#include "gdal_frmts.h"
 #include "gdal_pam.h"
 
 #ifndef DBL_MAX
@@ -51,10 +52,6 @@
 #endif /* INT_MAX */
 
 CPL_CVSID("$Id$");
-
-CPL_C_START
-void GDALRegister_GSAG();
-CPL_C_END
 
 /************************************************************************/
 /* ==================================================================== */

commit 42dcbf33be67836922a96d54fbdfdd4ea27831f1

Hopefully unbreak mingw caused by r32213 in gtiff.  cpl_port.h being first should fix the lack of VSIStatBufL
git-svn-id: https://svn.osgeo.org/gdal/trunk@32214 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/frmts/gtiff/geotiff.cpp
@@ -36,12 +36,14 @@
 #define _GNU_SOURCE
 #endif
 
+#include "cpl_port.h"  // Must be first.
+
 #include <set>
 
-#include "cpl_string.h"
 #include "cpl_csv.h"
 #include "cpl_minixml.h"
 #include "cpl_multiproc.h"
+#include "cpl_string.h"
 #include "cpl_vsi_virtual.h"
 #include "cpl_worker_thread_pool.h"
 #include "gdal_frmts.h"