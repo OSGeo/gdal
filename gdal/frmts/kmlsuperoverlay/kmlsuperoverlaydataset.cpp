d3b1ac47a83ea498e55bf3a618bc4038ddc1a979
Even Rouault <even.rouault@mines-paris.org>
2016-01-15 16:14:38 +0000

    KMLSuperoverlay: fix 2.0 regression with some RasterIO() requests involving resampling (https://trac.osgeo.org/gdal/ticket/6311)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@33003 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/kmlsuperoverlay/kmlsuperoverlaydataset.cpp
+++ gdal/frmts/kmlsuperoverlay/kmlsuperoverlaydataset.cpp
@@ -1127,15 +1127,27 @@ CPLErr KmlSuperOverlayReadDataset::IRasterIO( GDALRWFlag eRWFlag,
         return CE_Failure;
 
     if( bIsOvr )
+    {
+        GDALRasterIOExtraArg sExtraArgs;
+        GDALCopyRasterIOExtraArg(&sExtraArgs, psExtraArg);
+        const int nOvrFactor = poParent->nFactor / nFactor;
+        if( sExtraArgs.bFloatingPointWindowValidity )
+        {
+            sExtraArgs.dfXOff *= nOvrFactor;
+            sExtraArgs.dfYOff *= nOvrFactor;
+            sExtraArgs.dfXSize *= nOvrFactor;
+            sExtraArgs.dfYSize *= nOvrFactor;
+        }
         return poParent->IRasterIO( eRWFlag,
-                                    nXOff * (poParent->nFactor / nFactor),
-                                    nYOff * (poParent->nFactor / nFactor),
-                                    nXSize * (poParent->nFactor / nFactor),
-                                    nYSize * (poParent->nFactor / nFactor),
+                                    nXOff * nOvrFactor,
+                                    nYOff * nOvrFactor,
+                                    nXSize * nOvrFactor,
+                                    nYSize * nOvrFactor,
                                     pData, nBufXSize, nBufYSize,
                                     eBufType, 
                                     nBandCount, panBandMap,
-                                    nPixelSpace, nLineSpace, nBandSpace, psExtraArg);
+                                    nPixelSpace, nLineSpace, nBandSpace, &sExtraArgs);
+    }
 
     double dfXOff = 1.0 * nXOff / nFactor;
     double dfYOff = 1.0 * nYOff / nFactor;
@@ -1374,6 +1386,9 @@ CPLErr KmlSuperOverlayReadDataset::IRasterIO( GDALRWFlag eRWFlag,
             if( nReqYOff + nReqYSize > poVRTDS->GetRasterYSize() )
                 nReqYSize = poVRTDS->GetRasterYSize() - nReqYOff;
 
+            GDALRasterIOExtraArg sExtraArgs;
+            INIT_RASTERIO_EXTRA_ARG(sExtraArgs);
+            sExtraArgs.eResampleAlg = psExtraArg->eResampleAlg;
             CPLErr eErr = poVRTDS->RasterIO( eRWFlag,
                                              nReqXOff,
                                              nReqYOff,
@@ -1382,7 +1397,7 @@ CPLErr KmlSuperOverlayReadDataset::IRasterIO( GDALRWFlag eRWFlag,
                                              pData, nBufXSize, nBufYSize, eBufType,
                                              nBandCount, panBandMap,
                                              nPixelSpace, nLineSpace, nBandSpace,
-                                             psExtraArg);
+                                             &sExtraArgs);
 
             for(i=0; i < (int)aosImages.size(); i++)
             {
@@ -1427,8 +1442,11 @@ CPLErr KmlSuperOverlayReadDataset::IRasterIO( GDALRWFlag eRWFlag,
         if( nReqYOff + nReqYSize > poDSIcon->GetRasterYSize() )
             nReqYSize = poDSIcon->GetRasterYSize() - nReqYOff;
 
-        psExtraArg->pfnProgress = GDALScaledProgress;
-        psExtraArg->pProgressData = 
+        GDALRasterIOExtraArg sExtraArgs;
+        INIT_RASTERIO_EXTRA_ARG(sExtraArgs);
+        sExtraArgs.eResampleAlg = psExtraArg->eResampleAlg;
+        sExtraArgs.pfnProgress = GDALScaledProgress;
+        sExtraArgs.pProgressData = 
             GDALCreateScaledProgress( 1.0 * iBandIdx / nBandCount,
                                       1.0 * (iBandIdx + 1) / nBandCount,
                                       pfnProgressGlobal,
@@ -1442,9 +1460,9 @@ CPLErr KmlSuperOverlayReadDataset::IRasterIO( GDALRWFlag eRWFlag,
                                                       ((GByte*) pData) + nBandSpace * iBandIdx,
                                                       nBufXSize, nBufYSize, eBufType,
                                                       nPixelSpace, nLineSpace,
-                                                      psExtraArg);
+                                                      &sExtraArgs);
 
-        GDALDestroyScaledProgress( psExtraArg->pProgressData );
+        GDALDestroyScaledProgress( sExtraArgs.pProgressData );
     }
 
     psExtraArg->pfnProgress = pfnProgressGlobal;
