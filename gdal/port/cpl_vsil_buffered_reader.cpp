7e82b71383ac5b6ed3f93310a02b4d400aa3bcfb
Even Rouault <even.rouault@mines-paris.org>
2016-01-24 11:20:40 +0000

    Fix wrong typo fix
    git-svn-id: https://svn.osgeo.org/gdal/trunk@33139 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/port/cpl_vsil_buffered_reader.cpp
+++ gdal/port/cpl_vsil_buffered_reader.cpp
@@ -50,7 +50,7 @@ class VSIBufferedReaderHandle CPL_FINAL : public VSIVirtualHandle
     GUIntBig          nCurOffset;
     int               bNeedBaseHandleSeek;
     int               bEOF;
-    vsi_l_offset      nSheetFileSize;
+    vsi_l_offset      nCheatFileSize;
 
     int               SeekBaseTo(vsi_l_offset nTargetOffset);
 
@@ -59,7 +59,7 @@ class VSIBufferedReaderHandle CPL_FINAL : public VSIVirtualHandle
     VSIBufferedReaderHandle(VSIVirtualHandle* poBaseHandle);
     VSIBufferedReaderHandle(VSIVirtualHandle* poBaseHandle,
                             const GByte* pabyBeginningContent,
-                            vsi_l_offset nSheetFileSizeIn);
+                            vsi_l_offset nCheatFileSizeIn);
     ~VSIBufferedReaderHandle();
 
     virtual int       Seek( vsi_l_offset nOffset, int nWhence );
@@ -82,11 +82,11 @@ VSIVirtualHandle* VSICreateBufferedReaderHandle(VSIVirtualHandle* poBaseHandle)
 
 VSIVirtualHandle* VSICreateBufferedReaderHandle(VSIVirtualHandle* poBaseHandle,
                                                 const GByte* pabyBeginningContent,
-                                                vsi_l_offset nSheetFileSizeIn)
+                                                vsi_l_offset nCheatFileSizeIn)
 {
     return new VSIBufferedReaderHandle(poBaseHandle,
                                        pabyBeginningContent,
-                                       nSheetFileSizeIn);
+                                       nCheatFileSizeIn);
 }
 
 /************************************************************************/
@@ -102,12 +102,12 @@ VSIBufferedReaderHandle::VSIBufferedReaderHandle(VSIVirtualHandle* poBaseHandle)
     nCurOffset = 0;
     bNeedBaseHandleSeek = FALSE;
     bEOF = FALSE;
-    nSheetFileSize = 0;
+    nCheatFileSize = 0;
 }
 
 VSIBufferedReaderHandle::VSIBufferedReaderHandle(VSIVirtualHandle* poBaseHandle,
                                                  const GByte* pabyBeginningContent,
-                                                 vsi_l_offset nSheetFileSizeIn)
+                                                 vsi_l_offset nCheatFileSizeIn)
 {
     m_poBaseHandle = poBaseHandle;
     nBufferOffset = 0;
@@ -117,7 +117,7 @@ VSIBufferedReaderHandle::VSIBufferedReaderHandle(VSIVirtualHandle* poBaseHandle,
     nCurOffset = 0;
     bNeedBaseHandleSeek = TRUE;
     bEOF = FALSE;
-    nSheetFileSize = nSheetFileSizeIn;
+    nCheatFileSize = nCheatFileSizeIn;
 }
 
 /************************************************************************/
@@ -142,8 +142,8 @@ int VSIBufferedReaderHandle::Seek( vsi_l_offset nOffset, int nWhence )
         nCurOffset += nOffset;
     else if (nWhence == SEEK_END)
     {
-        if( nSheetFileSize )
-            nCurOffset = nSheetFileSize;
+        if( nCheatFileSize )
+            nCurOffset = nCheatFileSize;
         else
         {
             m_poBaseHandle->Seek(nOffset, nWhence);
