458111152d4c85146c3e8b2fac51a5bfc5574f0a
Kurt Schwehr <schwehr@gmail.com>
2016-02-29 17:59:45 +0000

    Cleanup ogr_fromepsg.cppp.
    - Remove extra space from Pseudo-Mercator WKT
    - Sort includes
    - Localize variables
    - Combine definition and initialization
    - Intialize more variables
    - Add const to local vars
    - static int foo() -> static bool foo()
    - return( foo ); -> return foo;
    - #define foo 42; -> static const int foo = 42;
    - #define PM_FILENAME CSVFilename() -> const char * pm_filename = CSVFilename
    - C casts -> C++ casts
    
    
    git-svn-id: https://svn.osgeo.org/gdal/trunk@33600 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/osr/osr_epsg.py
+++ autotest/osr/osr_epsg.py
@@ -186,7 +186,7 @@ def osr_epsg_9():
     srs = osr.SpatialReference()
     srs.ImportFromEPSG( 3857 )
 
-    if srs.ExportToWkt() != 'PROJCS["WGS 84 / Pseudo-Mercator",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]],PROJECTION["Mercator_1SP"],PARAMETER["central_meridian",0],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["X",EAST],AXIS["Y",NORTH],EXTENSION["PROJ4","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"],AUTHORITY["EPSG","3857"]]':
+    if srs.ExportToWkt() != 'PROJCS["WGS 84 / Pseudo-Mercator",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]],PROJECTION["Mercator_1SP"],PARAMETER["central_meridian",0],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["X",EAST],AXIS["Y",NORTH],EXTENSION["PROJ4","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs"],AUTHORITY["EPSG","3857"]]':
         gdaltest.post_reason('fail')
         print(srs.ExportToWkt())
         return 'fail'
@@ -199,7 +199,7 @@ def osr_epsg_9():
 
 ###############################################################################
 
-gdaltest_list = [ 
+gdaltest_list = [
     osr_epsg_1,
     osr_epsg_2,
     osr_epsg_3,
@@ -218,4 +218,3 @@ if __name__ == '__main__':
     gdaltest.run_tests( gdaltest_list )
 
     gdaltest.summarize()
-
