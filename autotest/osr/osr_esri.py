8d9cf6685c3ac4ede23de2fc640db83e455f541e
Even Rouault <even.rouault@mines-paris.org>
2016-02-29 20:36:25 +0000

    Adjust expected results to removal of extra space character in r33600
    git-svn-id: https://svn.osgeo.org/gdal/trunk@33603 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/osr/osr_esri.py
+++ autotest/osr/osr_esri.py
@@ -1395,7 +1395,7 @@ def osr_esri_29():
         AUTHORITY["EPSG","9001"]],
     AXIS["X",EAST],
     AXIS["Y",NORTH],
-    EXTENSION["PROJ4","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"],
+    EXTENSION["PROJ4","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs"],
     AUTHORITY["EPSG","3857"]]""":
         gdaltest.post_reason('fail')
         print(got_wkt)
@@ -1425,7 +1425,7 @@ def osr_esri_29():
         AUTHORITY["EPSG","9001"]],
     AXIS["X",EAST],
     AXIS["Y",NORTH],
-    EXTENSION["PROJ4","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"],
+    EXTENSION["PROJ4","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs"],
     AUTHORITY["EPSG","3857"]]""":
         gdaltest.post_reason('fail')
         print(got_wkt)
