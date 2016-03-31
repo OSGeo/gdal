074060a7b6effd05a7204f481e447f131c23a82c
Even Rouault <even.rouault@mines-paris.org>
2016-03-31 16:21:01 +0000

    ILI1: make it obvious that NULL ptr dereferencing cannot happen in the while() cond evaluation (KS CID 159440)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@33829 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/ili/ili1reader.cpp
+++ gdal/ogr/ogrsf_frmts/ili/ili1reader.cpp
@@ -656,6 +656,10 @@ char ** ILI1Reader::ReadParseLine()
       tokens[CSLCount(tokens)-1] = NULL;
 
       pszLine = CPLReadLine( fpItf );
+      if( pszLine == NULL )
+      {
+          break;
+      }
       char **conttok = CSLTokenizeString2( pszLine, " ", CSLT_PRESERVEESCAPES );
       if (!conttok || !EQUAL(conttok[0], "CONT"))
       {
