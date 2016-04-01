7e2b27691190d9d11870ae01498d4663745e7fd0
Even Rouault <even.rouault@mines-paris.org>
2016-04-01 16:47:47 +0000

    Prepare for 2.1.0beta1
    git-svn-id: https://svn.osgeo.org/gdal/trunk@33850 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/gcore/gdal_version.h
+++ gdal/gcore/gdal_version.h
@@ -22,8 +22,8 @@
 #endif
 
 #ifndef GDAL_RELEASE_DATE
-#  define GDAL_RELEASE_DATE     20159999
+#  define GDAL_RELEASE_DATE     20160401
 #endif
 #ifndef GDAL_RELEASE_NAME
-#  define GDAL_RELEASE_NAME     "2.1.0dev"
+#  define GDAL_RELEASE_NAME     "2.1.0beta1"
 #endif
