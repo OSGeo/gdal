OSGeo/gdal

commit 09190ac65227ee4f088c588377d4f862ed110dfa

Clean swq_op_registrar.cpp.
- Fix formatting
- Localize vars
- iFoo++ -> ++iFoo
- Remove extra parens
- Make nullptr comparison explicit

Code review by Pete Klier at Google


git-svn-id: https://svn.osgeo.org/gdal/trunk@34241 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/ogr/swq_op_registrar.cpp
@@ -31,8 +31,8 @@
 #include "cpl_conv.h"
 #include "swq.h"
 
-static swq_field_type SWQColumnFuncChecker( swq_expr_node *poNode,
-                                            int bAllowMismatchTypeOnFieldComparison );
+static swq_field_type SWQColumnFuncChecker(
+    swq_expr_node *poNode, int bAllowMismatchTypeOnFieldComparison );
 
 static const swq_operation swq_apsOperations[] =
 {
@@ -56,7 +56,8 @@ static const swq_operation swq_apsOperations[] =
     { "%", SWQ_MODULUS , SWQGeneralEvaluator, SWQGeneralChecker },
     { "CONCAT", SWQ_CONCAT , SWQGeneralEvaluator, SWQGeneralChecker },
     { "SUBSTR", SWQ_SUBSTR , SWQGeneralEvaluator, SWQGeneralChecker },
-    { "HSTORE_GET_VALUE", SWQ_HSTORE_GET_VALUE , SWQGeneralEvaluator, SWQGeneralChecker },
+    { "HSTORE_GET_VALUE", SWQ_HSTORE_GET_VALUE , SWQGeneralEvaluator,
+        SWQGeneralChecker },
 
     { "AVG", SWQ_AVG, SWQGeneralEvaluator, SWQColumnFuncChecker },
     { "MIN", SWQ_MIN, SWQGeneralEvaluator, SWQColumnFuncChecker },
@@ -76,8 +77,7 @@ static const swq_operation swq_apsOperations[] =
 const swq_operation *swq_op_registrar::GetOperator( const char *pszName )
 
 {
-    unsigned int i;
-    for( i = 0; i < N_OPERATIONS; i++ )
+    for( unsigned int i = 0; i < N_OPERATIONS; ++i )
     {
         if( EQUAL(pszName,swq_apsOperations[i].pszName) )
             return &(swq_apsOperations[i]);
@@ -93,9 +93,7 @@ const swq_operation *swq_op_registrar::GetOperator( const char *pszName )
 const swq_operation *swq_op_registrar::GetOperator( swq_op eOperator )
 
 {
-    unsigned int i;
-
-    for( i = 0; i < N_OPERATIONS; i++ )
+    for( unsigned int i = 0; i < N_OPERATIONS; ++i )
     {
         if( eOperator == swq_apsOperations[i].eOperation )
             return &(swq_apsOperations[i]);
@@ -113,13 +111,13 @@ const swq_operation *swq_op_registrar::GetOperator( swq_op eOperator )
 /*      error if they are used in any other context.                    */
 /************************************************************************/
 
-static swq_field_type SWQColumnFuncChecker( swq_expr_node *poNode,
-                                            CPL_UNUSED int bAllowMismatchTypeOnFieldComparison )
+static swq_field_type SWQColumnFuncChecker(
+    swq_expr_node *poNode, int /* bAllowMismatchTypeOnFieldComparison */ )
 {
     const swq_operation *poOp =
             swq_op_registrar::GetOperator((swq_op)poNode->nOperation);
     CPLError( CE_Failure, CPLE_AppDefined,
               "Column Summary Function '%s' found in an inappropriate context.",
-              (poOp) ? poOp->pszName : "" );
+              poOp == NULL ? poOp->pszName : "" );
     return SWQ_ERROR;
 }

commit 1e911a7465fcd1dde8cf435f2cddfd296e9dd13d

Fix bug introduced by r34241.
I got the wrong sense of the explicit comparison of poOp with nullptr.

Bug identified by rouault


git-svn-id: https://svn.osgeo.org/gdal/trunk@34242 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/ogr/swq_op_registrar.cpp
@@ -118,6 +118,6 @@ static swq_field_type SWQColumnFuncChecker(
             swq_op_registrar::GetOperator((swq_op)poNode->nOperation);
     CPLError( CE_Failure, CPLE_AppDefined,
               "Column Summary Function '%s' found in an inappropriate context.",
-              poOp == NULL ? poOp->pszName : "" );
+              poOp != NULL ? poOp->pszName : "" );
     return SWQ_ERROR;
 }