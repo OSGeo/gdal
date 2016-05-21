38948031f4a61831d779034d7cc775164f57e80f
avyavkumar <avyavkumar@gmail.com>
2016-05-21 23:06:19 +0530

    interfacing methods

---
+++ gdal/ogr/ogr_sfcgal.h
@@ -0,0 +1,15 @@
+#ifdef HAVE_SFCGAL
+
+// API for SFCGAL::TriangulatedSurface
+#include <TriangulatedGeometry.h>
+
+// API for SFCGAL::PolyhedralSurface
+#include <PolyhedralSurface.h>
+
+// API for SFCGAL::Triangle
+#include <Triangle.h>
+
+// API for SFCGAL::io::writeBinaryGeometry(SFCGAL::Geometry)
+#include <SFCGAL/detail/io/Serialization.h>
+
+#endif
