10d8be174a5ffc993ecb9e4f4a0ad3d40d76280f
Tamas Szekeres <szekerest@gmail.com>
2016-05-24 19:43:40 +0000

    Fix MSSQL bulk insert with table names containing spaces (https://trac.osgeo.org/gdal/ticket/6527)
    git-svn-id: https://svn.osgeo.org/gdal/branches/2.1@34289 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/mssqlspatial/ogrmssqlspatialtablelayer.cpp
+++ gdal/ogr/ogrsf_frmts/mssqlspatial/ogrmssqlspatialtablelayer.cpp
@@ -1385,7 +1385,7 @@ OGRErr OGRMSSQLSpatialTableLayer::CreateFeatureBCP( OGRFeature *poFeature )
             return OGRERR_FAILURE;
 
         /* Initialize the bulk copy */
-        if (Failed2( bcp_init(hDBCBCP, pszTableName, NULL, NULL, DB_IN) ))
+        if (Failed2( bcp_init(hDBCBCP, CPLSPrintf("[%s].[%s]", pszSchemaName, pszTableName), NULL, NULL, DB_IN) ))
         {
             CloseBCP();
             return OGRERR_FAILURE;
