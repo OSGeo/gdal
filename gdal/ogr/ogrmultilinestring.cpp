d4ae9ed858da54f4daa5a4328990bfa09b8909e0
Kurt Schwehr <schwehr@gmail.com>
2016-06-05 05:06:32 +0000

    Convert C cast to dynamic_cast to handle down cast in OGRMultiLineString::CastToMultiCurve().
    Be explicit about returning nullptr if the dynamic cast fails to help out static
    analyzers, but if it fails, the program will just terminate.
    
    
    git-svn-id: https://svn.osgeo.org/gdal/trunk@34327 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrmultilinestring.cpp
+++ gdal/ogr/ogrmultilinestring.cpp
@@ -163,7 +163,16 @@ OGRBoolean OGRMultiLineString::hasCurveGeometry(CPL_UNUSED int bLookForNonLinear
  * @return new geometry.
  */
 
-OGRMultiCurve* OGRMultiLineString::CastToMultiCurve(OGRMultiLineString* poMLS)
+OGRMultiCurve* OGRMultiLineString::CastToMultiCurve( OGRMultiLineString* poMLS )
 {
-    return (OGRMultiCurve*) TransferMembersAndDestroy(poMLS, new OGRMultiCurve());
+    OGRMultiCurve *poMultiCurve = dynamic_cast<OGRMultiCurve *>(
+        TransferMembersAndDestroy(poMLS, new OGRMultiCurve()) );
+    if( poMultiCurve == NULL )
+    {
+        CPLError( CE_Fatal, CPLE_AppDefined,
+                  "OGRMultiCurve dynamic_cast failed." );
+        return NULL;
+    }
+
+    return poMultiCurve;
 }
