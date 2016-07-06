5c10e688bb5dea5801b094d9f06391d2d2424301
Kurt Schwehr <schwehr@gmail.com>
2016-07-06 05:49:30 +0000

    Summary: Localize loop counter i in GDALDefaultOverviews::BuildOverviews()
    git-svn-id: https://svn.osgeo.org/gdal/trunk@34559 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/gcore/gdaldefaultoverviews.cpp
+++ gdal/gcore/gdaldefaultoverviews.cpp
@@ -560,7 +560,6 @@ GDALDefaultOverviews::BuildOverviews(
 
 {
     CPLErr       eErr;
-    int          i;
 
     if( pfnProgress == NULL )
         pfnProgress = GDALDummyProgress;
@@ -634,7 +633,7 @@ GDALDefaultOverviews::BuildOverviews(
 
     int nNewOverviews = 0;
     int *panNewOverviewList = (int *) CPLCalloc(sizeof(int),nOverviews);
-    for( i = 0; i < nOverviews && poBand != NULL; i++ )
+    for( int i = 0; i < nOverviews && poBand != NULL; i++ )
     {
         for( int j = 0; j < poBand->GetOverviewCount(); j++ )
         {
@@ -664,7 +663,7 @@ GDALDefaultOverviews::BuildOverviews(
 /* -------------------------------------------------------------------- */
     GDALRasterBand **pahBands
         = (GDALRasterBand **) CPLCalloc(sizeof(GDALRasterBand *),nBands);
-    for( i = 0; i < nBands; i++ )
+    for( int i = 0; i < nBands; i++ )
         pahBands[i] = poDS->GetRasterBand( panBandList[i] );
 
 /* -------------------------------------------------------------------- */
@@ -750,7 +749,7 @@ GDALDefaultOverviews::BuildOverviews(
         poBand = poDS->GetRasterBand( panBandList[iBand] );
 
         nNewOverviews = 0;
-        for( i = 0; i < nOverviews && poBand != NULL; i++ )
+        for( int i = 0; i < nOverviews && poBand != NULL; i++ )
         {
             for( int j = 0; j < poBand->GetOverviewCount(); j++ )
             {
