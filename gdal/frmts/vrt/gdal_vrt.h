2d89a37e2e288afabeaf04987f3b179c5144d42a
Kurt Schwehr <schwehr@gmail.com>
2016-07-07 18:07:57 +0000

    gdal_vrt.h: Sort includes and fix minor formatting issue.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@34592 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/vrt/gdal_vrt.h
+++ gdal/frmts/vrt/gdal_vrt.h
@@ -36,17 +36,17 @@
  * Public (C callable) entry points for virtual GDAL dataset objects.
  */
 
-#include "gdal.h"
 #include "cpl_error.h"
 #include "cpl_minixml.h"
 #include "cpl_port.h"
+#include "gdal.h"
 
 #define VRT_NODATA_UNSET -1234.56
 
 CPL_C_START
 
 void GDALRegister_VRT();
-CPLErr CPL_STDCALL GDALRegisterDefaultPixelFunc(void);
+CPLErr CPL_STDCALL GDALRegisterDefaultPixelFunc( void );
 
 typedef CPLErr
 (*VRTImageReadFunc)( void *hCBData,
