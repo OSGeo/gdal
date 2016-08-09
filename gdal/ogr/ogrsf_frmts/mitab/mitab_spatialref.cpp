bc9d2d40fa41ec105cbe43265c1f93b9b13b51c6
Even Rouault <even.rouault@mines-paris.org>
2016-08-09 21:36:54 +0000

    mitab_spatialref.cpp: make it obvious that a null-ptr assignment to string cannot happen (fix KS CID 164958)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@35029 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/mitab/mitab_spatialref.cpp
+++ gdal/ogr/ogrsf_frmts/mitab/mitab_spatialref.cpp
@@ -2080,12 +2080,15 @@ int TABFile::GetTABProjFromSpatialRef(const OGRSpatialReference* poSpatialRef,
     }
 
     // Google Merc
-    if( (poSpatialRef->GetAuthorityName(NULL) != NULL &&
-        EQUAL(poSpatialRef->GetAuthorityName(NULL), "EPSG") &&
-        poSpatialRef->GetAuthorityCode(NULL) != NULL &&
-        atoi(poSpatialRef->GetAuthorityCode(NULL)) == 3857) ||
-        (poSpatialRef->GetExtension(NULL, "PROJ4") != NULL &&
-         EQUAL(poSpatialRef->GetExtension(NULL, "PROJ4"),
+    const char* pszAuthorityName;
+    const char* pszAuthorityCode;
+    const char* pszExtension;
+    if( ((pszAuthorityName = poSpatialRef->GetAuthorityName(NULL)) != NULL &&
+        EQUAL(pszAuthorityName, "EPSG") &&
+        (pszAuthorityCode = poSpatialRef->GetAuthorityCode(NULL)) != NULL &&
+        atoi(pszAuthorityCode) == 3857) ||
+        ((pszExtension = poSpatialRef->GetExtension(NULL, "PROJ4")) != NULL &&
+         EQUAL(pszExtension,
                "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs")) )
     {
         sTABProj.nDatumId = 157;
