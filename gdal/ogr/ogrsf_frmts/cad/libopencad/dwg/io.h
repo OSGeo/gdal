41c4e6fa78384458400a0c8124c71159e4ba5c30
sandyre <aleksanteri@niuitmo.ru>
2016-08-24 03:43:18 -0700

    Remove git merge-conflict strings

--- gdal/ogr/ogrsf_frmts/cad/libopencad/dwg/io.h
+++ gdal/ogr/ogrsf_frmts/cad/libopencad/dwg/io.h
@@ -62,13 +62,6 @@
 
 static const size_t DWGSentinelLength = 16;
 
-<<<<<<< HEAD
-static const char * DWGHeaderVariablesStart = "\xCF\x7B\x1F\x23\xFD\xDE\x38\xA9\x5F\x7C\x68\xB8\x4E\x6D\x33\x5F";
-static const char * DWGHeaderVariablesEnd   = "\x30\x84\xE0\xDC\x02\x21\xC7\x56\xA0\x83\x97\x47\xB1\x92\xCC\xA0";
-
-static const char * DWGDSPreviewStart = "\x1F\x25\x6D\x07\xD4\x36\x28\x28\x9D\x57\xCA\x3F\x9D\x44\x10\x2B";
-static const char * DWGDSPreviewEnd   = "\xE0\xDA\x92\xF8\x2B\xc9\xD7\xD7\x62\xA8\x35\xC0\x62\xBB\xEF\xD4";
-=======
 static const char * DWGHeaderVariablesStart
             = "\xCF\x7B\x1F\x23\xFD\xDE\x38\xA9\x5F\x7C\x68\xB8\x4E\x6D\x33\x5F";
 static const char * DWGHeaderVariablesEnd
@@ -88,13 +81,6 @@ static const char * DWGSecondFileHeaderStart
             = "\xD4\x7B\x21\xCE\x28\x93\x9F\xBF\x53\x24\x40\x09\x12\x3C\xAA\x01";
 static const char * DWGSecondFileHeaderEnd
             = "\x2B\x84\xDE\x31\xD7\x6C\x60\x40\xAC\xDB\xBF\xF6\xED\xC3\x55\xFE";
->>>>>>> 5832dc642af41e77163752348ff7b4583a677e73
-
-static const char * DWGDSClassesStart = "\x8D\xA1\xC4\xB8\xC4\xA9\xF8\xC5\xC0\xDC\xF4\x5F\xE7\xCF\xB6\x8A";
-static const char * DWGDSClassesEnd   = "\x72\x5E\x3B\x47\x3B\x56\x07\x3A\x3F\x23\x0B\xA0\x18\x30\x49\x75";
-
-static const char * DWGSecondFileHeaderStart = "\xD4\x7B\x21\xCE\x28\x93\x9F\xBF\x53\x24\x40\x09\x12\x3C\xAA\x01";
-static const char * DWGSecondFileHeaderEnd   = "\x2B\x84\xDE\x31\xD7\x6C\x60\x40\xAC\xDB\xBF\xF6\xED\xC3\x55\xFE";
 
 // TODO: probably it would be better to have no dependencies on <algorithm>.
 template<typename T, typename S>
@@ -109,6 +95,7 @@ inline void SwapEndianness( T&& object, S&& size )
  */
 template< unsigned long N >
 struct bin
+{
     enum
     {
         value = ( N % 8 ) + ( bin< N / 8 >::value << 1 )
