9d8636e4baf5dc545e3f5d7fb288f8a96533ace0
Even Rouault <even.rouault@mines-paris.org>
2016-09-03 15:24:05 +0000

    Fix warnings with recent mingw_w64_x86_64 with -Wfloat-conversion
    git-svn-id: https://svn.osgeo.org/gdal/trunk@35307 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/port/cpl_port.h
+++ gdal/port/cpl_port.h
@@ -619,7 +619,7 @@ static inline char* CPL_afl_friendly_strstr(const char* haystack, const char* ne
 #  define CPLIsNan(x) _isnan(x)
 #  define CPLIsInf(x) (!_isnan(x) && !_finite(x))
 #  define CPLIsFinite(x) _finite(x)
-#elif defined(HAVE_CXX11) && HAVE_CXX11 && defined(__GNUC__)
+#elif defined(__GNUC__) && ( __GNUC__ > 4 || ( __GNUC__ == 4 && __GNUC_MINOR__ >= 4 ) )
 /* When including <cmath> in C++11 the isnan() macro is undefined, so that */
 /* std::isnan() can work (#6489). This is a GCC specific workaround for now. */
 #  define CPLIsNan(x)    __builtin_isnan(x)
