6062e50482e0f3dce2b56c6b0511065a7a8e56ad
Kurt Schwehr <schwehr@gmail.com>
2016-09-25 02:45:11 +0000

    int boolean -> bool in frmts/blx
    git-svn-id: https://svn.osgeo.org/gdal/trunk@35511 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/blx/blxdataset.cpp
+++ gdal/frmts/blx/blxdataset.cpp
@@ -49,7 +49,7 @@ class BLXDataset : public GDALPamDataset
     blxcontext_t *blxcontext;
 
     int nOverviewCount;
-    int bIsOverview;
+    bool bIsOverview;
     BLXDataset *papoOverviewDS[BLX_OVERVIEWLEVELS];
 
   public:
@@ -128,7 +128,7 @@ GDALDataset *BLXDataset::Open( GDALOpenInfo * poOpenInfo )
     for(int i=0; i < poDS->nOverviewCount; i++) {
         poDS->papoOverviewDS[i] = new BLXDataset();
         poDS->papoOverviewDS[i]->blxcontext = poDS->blxcontext;
-        poDS->papoOverviewDS[i]->bIsOverview = TRUE;
+        poDS->papoOverviewDS[i]->bIsOverview = true;
         poDS->papoOverviewDS[i]->nRasterXSize = poDS->nRasterXSize >> (i+1);
         poDS->papoOverviewDS[i]->nRasterYSize = poDS->nRasterYSize >> (i+1);
         poDS->nBands = 1;
@@ -159,14 +159,16 @@ GDALDataset *BLXDataset::Open( GDALOpenInfo * poOpenInfo )
 BLXDataset::BLXDataset() :
     blxcontext(NULL),
     nOverviewCount(0),
-    bIsOverview(FALSE)
+    bIsOverview(false)
 {
     for( int i = 0; i < BLX_OVERVIEWLEVELS; i++ )
         papoOverviewDS[i] = NULL;
 }
 
-BLXDataset::~BLXDataset() {
-    if(!bIsOverview) {
+BLXDataset::~BLXDataset()
+{
+    if( !bIsOverview )
+    {
         if(blxcontext) {
             blxclose(blxcontext);
             blx_free_context(blxcontext);
