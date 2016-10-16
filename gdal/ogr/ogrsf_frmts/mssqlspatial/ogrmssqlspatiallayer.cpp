cac71a177719051bdd5dcf8dd2b2a865b81bf32b
Even Rouault <even.rouault@mines-paris.org>
2016-10-16 12:57:32 +0000

    MSSQLSpatial: fix potential null ptr deref (KS CID 164961)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@35757 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/mssqlspatial/ogrmssqlspatiallayer.cpp
+++ gdal/ogr/ogrsf_frmts/mssqlspatial/ogrmssqlspatiallayer.cpp
@@ -376,9 +376,10 @@ OGRFeature *OGRMSSQLSpatialLayer::GetNextRawFeature()
 /* -------------------------------------------------------------------- */
     OGRFeature *poFeature = new OGRFeature( poFeatureDefn );
 
-    if( pszFIDColumn != NULL && poStmt->GetColId(pszFIDColumn) > -1 )
-        poFeature->SetFID(
-            CPLAtoGIntBig(poStmt->GetColData(poStmt->GetColId(pszFIDColumn))) );
+    const char* pszFID;
+    if( pszFIDColumn != NULL && poStmt->GetColId(pszFIDColumn) > -1 &&
+        (pszFID = poStmt->GetColData(poStmt->GetColId(pszFIDColumn))) != NULL )
+        poFeature->SetFID( CPLAtoGIntBig(pszFID) );
     else
         poFeature->SetFID( iNextShapeId );
 
