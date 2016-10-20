OSGeo/gdal

commit 662729c939ccd896f0853d5e888b6fcb17223f74

Initial cleanup of osm.
- int bFoo -> bool bFoo for some
- Use initializer lists for ctors
- Localize variables
- Simplify if (a) return b; else return c;
- Some C casts -> C++ casts
- Define one variable per line
- Add const to local vars when possible
- Switch CPL_UNUSED for unused args to commenting out the arg name

git-svn-id: https://svn.osgeo.org/gdal/trunk@33037 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/ogr/ogrsf_frmts/osm/ogrosmlayer.cpp
@@ -45,47 +45,41 @@ CPL_CVSID("$Id$");
 /************************************************************************/
 
 
-OGROSMLayer::OGROSMLayer(OGROSMDataSource* poDSIn, int nIdxLayerIn, const char* pszName )
+OGROSMLayer::OGROSMLayer( OGROSMDataSource* poDSIn, int nIdxLayerIn,
+                          const char* pszName ) :
+    poDS(poDSIn),
+    nIdxLayer(nIdxLayerIn),
+    poFeatureDefn(new OGRFeatureDefn( pszName )),
+    poSRS(new OGRSpatialReference()),
+    nFeatureCount(0),
+    bResetReadingAllowed(false),
+    nFeatureArraySize(0),
+    nFeatureArrayMaxSize(0),
+    nFeatureArrayIndex(0),
+    papoFeatures(NULL),
+    bHasOSMId(false),
+    nIndexOSMId(-1),
+    nIndexOSMWayId(-1),
+    bHasVersion(false),
+    bHasTimestamp(false),
+    bHasUID(false),
+    bHasUser(false),
+    bHasChangeset(false),
+    bHasOtherTags(true),
+    nIndexOtherTags(-1),
+    bHasAllTags(false),
+    nIndexAllTags(-1),
+    bHasWarnedTooManyFeatures(false),
+    pszAllTags(static_cast<char *>(CPLMalloc(ALLTAGS_LENGTH))),
+    bHasWarnedAllTagsTruncated(false),
+    bUserInterested(true)
 {
-    this->poDS = poDSIn;
-    this->nIdxLayer = nIdxLayerIn;
-
-    poFeatureDefn = new OGRFeatureDefn( pszName );
     SetDescription( poFeatureDefn->GetName() );
     poFeatureDefn->Reference();
 
-    poSRS = new OGRSpatialReference();
     poSRS->SetWellKnownGeogCS("WGS84");
     if( poFeatureDefn->GetGeomFieldCount() != 0 )
         poFeatureDefn->GetGeomFieldDefn(0)->SetSpatialRef(poSRS);
-
-    nFeatureArraySize = 0;
-    nFeatureArrayMaxSize = 0;
-    nFeatureArrayIndex = 0;
-    papoFeatures = NULL;
-
-    nFeatureCount = 0;
-
-    bHasOSMId = FALSE;
-    nIndexOSMId = -1;
-    nIndexOSMWayId = -1;
-    bHasVersion = FALSE;
-    bHasTimestamp = FALSE;
-    bHasUID = FALSE;
-    bHasUser = FALSE;
-    bHasChangeset = FALSE;
-    bHasOtherTags = TRUE;
-    nIndexOtherTags = -1;
-    bHasAllTags = FALSE;
-    nIndexAllTags = -1;
-
-    bResetReadingAllowed = FALSE;
-    bHasWarnedTooManyFeatures = FALSE;
-
-    pszAllTags = (char*)CPLMalloc(ALLTAGS_LENGTH);
-    bHasWarnedAllTagsTruncated = FALSE;
-
-    bUserInterested = TRUE;
 }
 
 /************************************************************************/
@@ -94,29 +88,27 @@ OGROSMLayer::OGROSMLayer(OGROSMDataSource* poDSIn, int nIdxLayerIn, const char* 
 
 OGROSMLayer::~OGROSMLayer()
 {
-    int i;
-
     poFeatureDefn->Release();
 
     if (poSRS)
         poSRS->Release();
 
-    for(i=0;i<nFeatureArraySize;i++)
+    for( int i=0; i<nFeatureArraySize; i++ )
     {
         if (papoFeatures[i])
             delete papoFeatures[i];
     }
 
-    for(i=0;i<(int)apszNames.size();i++)
+    for( int i=0; i<static_cast<int>(apszNames.size()); i++ )
         CPLFree(apszNames[i]);
 
-    for(i=0;i<(int)apszUnsignificantKeys.size();i++)
+    for( int i=0; i<static_cast<int>(apszUnsignificantKeys.size()); i++ )
         CPLFree(apszUnsignificantKeys[i]);
 
-    for(i=0;i<(int)apszIgnoreKeys.size();i++)
+    for( int i=0; i<static_cast<int>(apszIgnoreKeys.size()); i++ )
         CPLFree(apszIgnoreKeys[i]);
 
-    for(i=0; i<(int)oComputedAttributes.size();i++)
+    for( int i=0; i<static_cast<int>(oComputedAttributes.size()); i++ )
     {
         sqlite3_finalize(oComputedAttributes[i].hStmt);
     }
@@ -152,7 +144,7 @@ void OGROSMLayer::ForceResetReading()
     nFeatureArrayIndex = 0;
     nFeatureArraySize = 0;
     nFeatureCount = 0;
-    bResetReadingAllowed = FALSE;
+    bResetReadingAllowed = false;
 }
 
 /************************************************************************/
@@ -180,9 +172,11 @@ OGRErr OGROSMLayer::SetAttributeFilter( const char* pszAttrQuery )
     }
     else
     {
-        CPLError(CE_Warning, CPLE_AppDefined, "The new attribute filter will "
-                 "not be taken into account immediately. It is advised to "
-                 "set attribute filters for all needed layers, before reading *any* layer");
+        CPLError( CE_Warning, CPLE_AppDefined,
+                  "The new attribute filter will "
+                  "not be taken into account immediately. It is advised to "
+                  "set attribute filters for all needed layers, before "
+                  "reading *any* layer" );
     }
 
     return OGRERR_NONE;
@@ -206,7 +200,7 @@ GIntBig OGROSMLayer::GetFeatureCount( int bForce )
 
 OGRFeature *OGROSMLayer::GetNextFeature()
 {
-    bResetReadingAllowed = TRUE;
+    bResetReadingAllowed = true;
 
     if ( nFeatureArraySize == 0)
     {
@@ -323,7 +317,7 @@ int  OGROSMLayer::AddToArray(OGRFeature* poFeature, int bCheckFeatureThreshold)
                     "Use OGR_INTERLEAVED_READING=YES mode",
                     GetName());
         }
-        bHasWarnedTooManyFeatures = TRUE;
+        bHasWarnedTooManyFeatures = true;
         return FALSE;
     }
 
@@ -407,7 +401,7 @@ int  OGROSMLayer::AddFeature(OGRFeature* poFeature,
 /************************************************************************/
 
 OGRErr OGROSMLayer::GetExtent( OGREnvelope *psExtent,
-                               CPL_UNUSED int bForce )
+                               int /* bForce */ )
 {
     if (poDS->GetExtent(psExtent) == OGRERR_NONE)
         return OGRERR_NONE;
@@ -425,20 +419,20 @@ const char* OGROSMLayer::GetLaunderedFieldName(const char* pszName)
     if( poDS->DoesAttributeNameLaundering()  &&
         strchr(pszName, ':') != NULL )
     {
-        size_t i;
-        for( i = 0;
-             pszName[i] != '\0' && i < sizeof(szLaunderedFieldName) - 1; i++ )
+        for( size_t i = 0;
+             i < sizeof(szLaunderedFieldName) - 1 && pszName[i] != '\0';
+             i++ )
         {
             if( pszName[i] == ':' )
                 szLaunderedFieldName[i] = '_';
             else
                 szLaunderedFieldName[i] = pszName[i];
         }
-        szLaunderedFieldName[i] = '\0';
+        szLaunderedFieldName[sizeof(szLaunderedFieldName) - 1] = '\0';
         return szLaunderedFieldName;
     }
-    else
-        return pszName;
+
+    return pszName;
 }
 
 /************************************************************************/
@@ -479,8 +473,8 @@ int OGROSMLayer::GetFieldIndex(const char* pszName)
         oMapFieldNameToIndex.find(pszName);
     if( oIter != oMapFieldNameToIndex.end() )
         return oIter->second;
-    else
-        return -1;
+
+    return -1;
 }
 
 /************************************************************************/
@@ -489,7 +483,7 @@ int OGROSMLayer::GetFieldIndex(const char* pszName)
 
 int OGROSMLayer::AddInOtherOrAllTags(const char* pszK)
 {
-    int bAddToOtherTags = FALSE;
+    bool bAddToOtherTags = false;
 
     if ( aoSetIgnoreKeys.find(pszK) == aoSetIgnoreKeys.end() )
     {
@@ -503,7 +497,7 @@ int OGROSMLayer::AddInOtherOrAllTags(const char* pszK)
             pszColon[1] = chBackup;
         }
         else
-            bAddToOtherTags = TRUE;
+            bAddToOtherTags = true;
     }
 
     return bAddToOtherTags;
@@ -515,13 +509,11 @@ int OGROSMLayer::AddInOtherOrAllTags(const char* pszK)
 
 static int OGROSMFormatForHSTORE(const char* pszV, char* pszAllTags)
 {
-    int k;
-
     int nAllTagsOff = 0;
 
     pszAllTags[nAllTagsOff++] = '"';
 
-    for(k=0;pszV[k] != '\0'; k++)
+    for( int k=0; pszV[k] != '\0'; k++ )
     {
         if( pszV[k] == '"' || pszV[k] == '\\' )
             pszAllTags[nAllTagsOff++] = '\\';
@@ -633,8 +625,10 @@ void OGROSMLayer::SetFieldsFromTags(OGRFeature* poFeature,
                     1 >= ALLTAGS_LENGTH - 1 )
                 {
                     if( !bHasWarnedAllTagsTruncated )
-                        CPLDebug("OSM", "all_tags/other_tags field truncated for feature " CPL_FRMT_GIB, nID);
-                    bHasWarnedAllTagsTruncated = TRUE;
+                        CPLDebug( "OSM",
+                                  "all_tags/other_tags field truncated for "
+                                  "feature " CPL_FRMT_GIB, nID);
+                    bHasWarnedAllTagsTruncated = true;
                     continue;
                 }
 
@@ -684,7 +678,9 @@ void OGROSMLayer::SetFieldsFromTags(OGRFeature* poFeature,
                 }
                 else
                 {
-                    OGRFieldType eType = poFeatureDefn->GetFieldDefn(oAttr.anIndexToBind[j])->GetType();
+                    OGRFieldType eType =
+                        poFeatureDefn->GetFieldDefn(oAttr.anIndexToBind[j])->
+                          GetType();
                     if( eType == OFTInteger )
                         sqlite3_bind_int( oAttr.hStmt, j + 1,
                                           poFeature->GetFieldAsInteger(oAttr.anIndexToBind[j]) );
@@ -702,7 +698,7 @@ void OGROSMLayer::SetFieldsFromTags(OGRFeature* poFeature,
             }
             else
             {
-                int bTagFound = FALSE;
+                bool bTagFound = false;
                 for(unsigned int k = 0; k < nTags; k++)
                 {
                     const char* pszK = pasTags[k].pszK;
@@ -710,7 +706,7 @@ void OGROSMLayer::SetFieldsFromTags(OGRFeature* poFeature,
                     if( strcmp(pszK, oAttr.aosAttrToBind[j]) == 0 )
                     {
                         sqlite3_bind_text( oAttr.hStmt, j + 1, pszV, -1, SQLITE_TRANSIENT);
-                        bTagFound = TRUE;
+                        bTagFound = true;
                         break;
                     }
                 }

gdal/ogr/ogrsf_frmts/osm/ogr_osm.h
@@ -90,32 +90,32 @@ class OGROSMLayer : public OGRLayer
 
     std::vector<OGROSMComputedAttribute> oComputedAttributes;
 
-    int                  bResetReadingAllowed;
+    bool                 bResetReadingAllowed;
 
     int                  nFeatureArraySize;
     int                  nFeatureArrayMaxSize;
     int                  nFeatureArrayIndex;
     OGRFeature**         papoFeatures;
 
-    int                   bHasOSMId;
+    bool                  bHasOSMId;
     int                   nIndexOSMId;
     int                   nIndexOSMWayId;
-    int                   bHasVersion;
-    int                   bHasTimestamp;
-    int                   bHasUID;
-    int                   bHasUser;
-    int                   bHasChangeset;
-    int                   bHasOtherTags;
+    bool                  bHasVersion;
+    bool                  bHasTimestamp;
+    bool                  bHasUID;
+    bool                  bHasUser;
+    bool                  bHasChangeset;
+    bool                  bHasOtherTags;
     int                   nIndexOtherTags;
-    int                   bHasAllTags;
+    bool                  bHasAllTags;
     int                   nIndexAllTags;
 
-    int                   bHasWarnedTooManyFeatures;
+    bool                  bHasWarnedTooManyFeatures;
 
     char                 *pszAllTags;
-    int                   bHasWarnedAllTagsTruncated;
+    bool                  bHasWarnedAllTagsTruncated;
 
-    int                   bUserInterested;
+    bool                  bUserInterested;
 
     int                  AddToArray(OGRFeature* poFeature, int bCheckFeatureThreshold);
 
@@ -163,29 +163,29 @@ class OGROSMLayer : public OGRLayer
     void                AddField(const char* pszName, OGRFieldType eFieldType);
     int                 GetFieldIndex(const char* pszName);
 
-    int                 HasOSMId() const { return bHasOSMId; }
-    void                SetHasOSMId(int bIn) { bHasOSMId = bIn; }
+    bool                HasOSMId() const { return bHasOSMId; }
+    void                SetHasOSMId(bool bIn) { bHasOSMId = bIn; }
 
-    int                 HasVersion() const { return bHasVersion; }
-    void                SetHasVersion(int bIn) { bHasVersion = bIn; }
+    bool                HasVersion() const { return bHasVersion; }
+    void                SetHasVersion(bool bIn) { bHasVersion = bIn; }
 
-    int                 HasTimestamp() const { return bHasTimestamp; }
-    void                SetHasTimestamp(int bIn) { bHasTimestamp = bIn; }
+    bool                HasTimestamp() const { return bHasTimestamp; }
+    void                SetHasTimestamp(bool bIn) { bHasTimestamp = bIn; }
 
-    int                 HasUID() const { return bHasUID; }
-    void                SetHasUID(int bIn) { bHasUID = bIn; }
+    bool                HasUID() const { return bHasUID; }
+    void                SetHasUID(bool bIn) { bHasUID = bIn; }
 
-    int                 HasUser() const { return bHasUser; }
-    void                SetHasUser(int bIn) { bHasUser = bIn; }
+    bool                HasUser() const { return bHasUser; }
+    void                SetHasUser(bool bIn) { bHasUser = bIn; }
 
-    int                 HasChangeset() const { return bHasChangeset; }
-    void                SetHasChangeset(int bIn) { bHasChangeset = bIn; }
+    bool                HasChangeset() const { return bHasChangeset; }
+    void                SetHasChangeset(bool bIn) { bHasChangeset = bIn; }
 
-    void                SetHasOtherTags(int bIn) { bHasOtherTags = bIn; }
-    int                 HasOtherTags() const { return bHasOtherTags; }
+    bool                HasOtherTags() const { return bHasOtherTags; }
+    void                SetHasOtherTags(bool bIn) { bHasOtherTags = bIn; }
 
-    void                SetHasAllTags(int bIn) { bHasAllTags = bIn; }
-    int                 HasAllTags() const { return bHasAllTags; }
+    bool                HasAllTags() const { return bHasAllTags; }
+    void                SetHasAllTags(bool bIn) { bHasAllTags = bIn; }
 
     void                SetFieldsFromTags(OGRFeature* poFeature,
                                           GIntBig nID,
@@ -193,8 +193,8 @@ class OGROSMLayer : public OGRLayer
                                           unsigned int nTags, OSMTag* pasTags,
                                           OSMInfo* psInfo);
 
-    void                SetDeclareInterest(int bIn) { bUserInterested = bIn; }
-    int                 IsUserInterested() const { return bUserInterested; }
+    void                SetDeclareInterest(bool bIn) { bUserInterested = bIn; }
+    bool                IsUserInterested() const { return bUserInterested; }
 
     int                 HasAttributeFilter() const { return m_poAttrQuery != NULL; }
     int                 EvaluateAttributeFilter(OGRFeature* poFeature);
@@ -261,8 +261,8 @@ typedef struct
     IndexedKVP*         pasTags; /*  point to a sub-array of OGROSMDataSource.pasAccumulatedTags */
     OSMInfo             sInfo;
     OGRFeature         *poFeature;
-    int                 bIsArea : 1;
-    int                 bAttrFilterAlreadyEvaluated : 1;
+    bool                bIsArea : 1;
+    bool                bAttrFilterAlreadyEvaluated : 1;
 } WayFeaturePair;
 
 #ifdef ENABLE_NODE_LOOKUP_BY_HASHING
@@ -282,14 +282,15 @@ class OGROSMDataSource : public OGRDataSource
     char*               pszName;
 
     OGREnvelope         sExtent;
-    int                 bExtentValid;
+    bool                bExtentValid;
 
+    // Starts off at -1 to indicate that we do not know.
     int                 bInterleavedReading;
     OGROSMLayer        *poCurrentLayer;
 
     OSMContext         *psParser;
-    int                 bHasParsedFirstChunk;
-    int                 bStopParsing;
+    bool                bHasParsedFirstChunk;
+    bool                bStopParsing;
 
 #ifdef HAVE_SQLITE_VFS
     sqlite3_vfs*        pMyVFS;
@@ -304,13 +305,13 @@ class OGROSMDataSource : public OGRDataSource
     sqlite3_stmt       *hInsertPolygonsStandaloneStmt;
     sqlite3_stmt       *hDeletePolygonsStandaloneStmt;
     sqlite3_stmt       *hSelectPolygonsStandaloneStmt;
-    int                 bHasRowInPolygonsStandalone;
+    bool                bHasRowInPolygonsStandalone;
 
     sqlite3            *hDBForComputedAttributes;
 
     int                 nMaxSizeForInMemoryDBInMB;
-    int                 bInMemoryTmpDB;
-    int                 bMustUnlink;
+    bool                bInMemoryTmpDB;
+    bool                bMustUnlink;
     CPLString           osTmpDBName;
 
     int                 nNodesInTransaction;
@@ -319,36 +320,36 @@ class OGROSMDataSource : public OGRDataSource
 
     LonLat             *pasLonLatCache;
 
-    int                 bReportAllNodes;
-    int                 bReportAllWays;
+    bool                bReportAllNodes;
+    bool                bReportAllWays;
 
-    int                 bFeatureAdded;
+    bool                bFeatureAdded;
 
-    int                 bInTransaction;
+    bool                bInTransaction;
 
-    int                 bIndexPoints;
-    int                 bUsePointsIndex;
-    int                 bIndexWays;
-    int                 bUseWaysIndex;
+    bool                bIndexPoints;
+    bool                bUsePointsIndex;
+    bool                bIndexWays;
+    bool                bUseWaysIndex;
 
-    std::vector<int>      abSavedDeclaredInterest;
-    OGRLayer*             poResultSetLayer;
-    int                   bIndexPointsBackup;
-    int                   bUsePointsIndexBackup;
-    int                   bIndexWaysBackup;
-    int                   bUseWaysIndexBackup;
+    std::vector<int>    abSavedDeclaredInterest;
+    OGRLayer*           poResultSetLayer;
+    bool                bIndexPointsBackup;
+    bool                bUsePointsIndexBackup;
+    bool                bIndexWaysBackup;
+    bool                bUseWaysIndexBackup;
 
-    int                 bIsFeatureCountEnabled;
+    bool                bIsFeatureCountEnabled;
 
-    int                 bAttributeNameLaundering;
+    bool                bAttributeNameLaundering;
 
     GByte              *pabyWayBuffer;
 
     int                 nWaysProcessed;
     int                 nRelationsProcessed;
 
-    int                 bCustomIndexing;
-    int                 bCompressNodes;
+    bool                bCustomIndexing;
+    bool                bCompressNodes;
 
     unsigned int        nUnsortedReqIds;
     GIntBig            *panUnsortedReqIds;
@@ -357,13 +358,13 @@ class OGROSMDataSource : public OGRDataSource
     GIntBig            *panReqIds;
 
 #ifdef ENABLE_NODE_LOOKUP_BY_HASHING
-    int                 bEnableHashedIndex;
+    bool                bEnableHashedIndex;
     /* values >= 0 are indexes of panReqIds. */
     /*        == -1 for unoccupied */
     /*        < -1 are expressed as -nIndexToCollisonBuckets-2 where nIndexToCollisonBuckets point to psCollisionBuckets */
     int                *panHashedIndexes; 
     CollisionBucket    *psCollisionBuckets;
-    int                 bHashedIndexValid;
+    bool                bHashedIndexValid;
 #endif
 
     LonLat             *pasLonLatArray;
@@ -380,8 +381,8 @@ class OGROSMDataSource : public OGRDataSource
     std::map<const char*, KeyDesc*, ConstCharComp> aoMapIndexedKeys; /* map that is the reverse of asKeys */
 
     CPLString           osNodesFilename;
-    int                 bInMemoryNodesFile;
-    int                 bMustUnlinkNodesFile;
+    bool                bInMemoryNodesFile;
+    bool                bMustUnlinkNodesFile;
     GIntBig             nNodesFileSize;
     VSILFILE           *fpNodes;
 
@@ -392,7 +393,7 @@ class OGROSMDataSource : public OGRDataSource
     Bucket             *papsBuckets;
     int                 nBuckets;
 
-    int                 bNeedsToSaveWayInfo;
+    bool                bNeedsToSaveWayInfo;
 
     int                 CompressWay (unsigned int nTags, IndexedKVP* pasTags,
                                      int nPoints, LonLat* pasLonLatPairs,
@@ -484,9 +485,9 @@ class OGROSMDataSource : public OGRDataSource
     OGROSMLayer*        GetCurrentLayer() { return poCurrentLayer; }
     void                SetCurrentLayer(OGROSMLayer* poLyr) { poCurrentLayer = poLyr; }
 
-    int                 IsFeatureCountEnabled() const { return bIsFeatureCountEnabled; }
+    bool                IsFeatureCountEnabled() const { return bIsFeatureCountEnabled; }
 
-    int                 DoesAttributeNameLaundering() const { return bAttributeNameLaundering; }
+    bool                DoesAttributeNameLaundering() const { return bAttributeNameLaundering; }
 };
 
 #endif /* ndef OGR_OSM_H_INCLUDED */

gdal/ogr/ogrsf_frmts/osm/ogrosmdriver.cpp
@@ -44,23 +44,25 @@ static int OGROSMDriverIdentify( GDALOpenInfo* poOpenInfo )
 
 {
     if (poOpenInfo->fpL == NULL || poOpenInfo->nHeaderBytes == 0)
-        return FALSE;
-    
+        return GDAL_IDENTIFY_FALSE;
+
     if( strstr((const char*)poOpenInfo->pabyHeader, "<osm") != NULL )
     {
-        return TRUE;
+        return GDAL_IDENTIFY_TRUE;
     }
-    
-    int nLimitI = poOpenInfo->nHeaderBytes - static_cast<int>(strlen("OSMHeader"));
+
+    const int nLimitI =
+        poOpenInfo->nHeaderBytes - static_cast<int>(strlen("OSMHeader"));
     for(int i = 0; i < nLimitI; i++)
     {
-        if( memcmp(poOpenInfo->pabyHeader + i, "OSMHeader", strlen("OSMHeader") ) == 0 )
+        if( memcmp( poOpenInfo->pabyHeader + i, "OSMHeader",
+                    strlen("OSMHeader") ) == 0 )
         {
-            return TRUE;
+            return GDAL_IDENTIFY_TRUE;
         }
     }
 
-    return FALSE;
+    return GDAL_IDENTIFY_TRUE;
 }
 
 /************************************************************************/
@@ -75,7 +77,7 @@ static GDALDataset *OGROSMDriverOpen( GDALOpenInfo* poOpenInfo )
     if( OGROSMDriverIdentify(poOpenInfo) == FALSE )
         return NULL;
 
-    OGROSMDataSource   *poDS = new OGROSMDataSource();
+    OGROSMDataSource *poDS = new OGROSMDataSource();
 
     if( !poDS->Open( poOpenInfo->pszFilename, poOpenInfo->papszOpenOptions ) )
     {

commit 338eca58e193bed68136b0aef7377eb2ff79c96b

Don't append garbage data to tag names
Bug was introduced in https://github.com/OSGeo/gdal/commit/662729c939ccd896f0853d5e888b6fcb17223f74

The string should be terminated at the
end of the entire shared buffer. Rather,
it should be terminated at the index
after copying stops.

This commit restores the original
behaviour before 662729c939ccd896f0853d5e888b6fcb17223f74

Moving the index variable into the for-loop
scope is a good practice in general, but
not in this particular case

gdal/ogr/ogrsf_frmts/osm/ogrosmlayer.cpp
@@ -426,7 +426,8 @@ const char* OGROSMLayer::GetLaunderedFieldName(const char* pszName)
     if( poDS->DoesAttributeNameLaundering()  &&
         strchr(pszName, ':') != NULL )
     {
-        for( size_t i = 0;
+        size_t i;
+        for( i = 0;
              i < sizeof(szLaunderedFieldName) - 1 && pszName[i] != '\0';
              i++ )
         {
@@ -435,7 +436,7 @@ const char* OGROSMLayer::GetLaunderedFieldName(const char* pszName)
             else
                 szLaunderedFieldName[i] = pszName[i];
         }
-        szLaunderedFieldName[sizeof(szLaunderedFieldName) - 1] = '\0';
+        szLaunderedFieldName[i] = '\0';
         return szLaunderedFieldName;
     }
 