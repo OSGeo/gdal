OSGeo/gdal

commit 2682dfcf209d212fc65d35f202cec0b3e4f89bd7

CutGeometryOnDateLineAndAddToMulti: remove dead code without GEOS support (CID 1215407)
git-svn-id: https://svn.osgeo.org/gdal/trunk@35841 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/ogr/ogrgeometryfactory.cpp
@@ -2499,7 +2499,9 @@ static void CutGeometryOnDateLineAndAddToMulti(OGRGeometryCollection* poMulti,
         case wkbPolygon:
         case wkbLineString:
         {
+#ifdef HAVE_GEOS
             bool bWrapDateline = false;
+#endif
             bool bSplitLineStringAtDateline = false;
             OGREnvelope oEnvelope;
 
@@ -2565,6 +2567,7 @@ static void CutGeometryOnDateLineAndAddToMulti(OGRGeometryCollection* poMulti,
             {
                 SplitLineStringAtDateline(poMulti, (OGRLineString*)poGeom, dfDateLineOffset);
             }
+#ifdef HAVE_GEOS
             else if (bWrapDateline)
             {
                 const OGRGeometry* poWorkGeom = (poDupGeom) ? poDupGeom : poGeom;
@@ -2594,6 +2597,7 @@ static void CutGeometryOnDateLineAndAddToMulti(OGRGeometryCollection* poMulti,
                 delete poGeom2;
                 delete poDupGeom;
             }
+#endif
             else
             {
                 poMulti->addGeometry(poGeom);

commit 1119d751ffd568b5a99543d0c4a68c220dec9abc

Summary: ogrgeometryfactory.cpp: Move definition of poDupGeom inside #ifdef HAVE_GEOS.
Fixes bug introduced in r35841.


git-svn-id: https://svn.osgeo.org/gdal/trunk@35845 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/ogr/ogrgeometryfactory.cpp
@@ -2499,16 +2499,16 @@ static void CutGeometryOnDateLineAndAddToMulti(OGRGeometryCollection* poMulti,
         case wkbPolygon:
         case wkbLineString:
         {
-#ifdef HAVE_GEOS
-            bool bWrapDateline = false;
-#endif
             bool bSplitLineStringAtDateline = false;
             OGREnvelope oEnvelope;
 
             poGeom->getEnvelope(&oEnvelope);
 
             /* Naive heuristics... Place to improvement... */
+#ifdef HAVE_GEOS
             OGRGeometry* poDupGeom = NULL;
+            bool bWrapDateline = false;
+#endif
 
             double dfLeftBorderX = 180 - dfDateLineOffset;
             double dfRightBorderX = -180 + dfDateLineOffset;