8775bfe2d4be07f9d3f17120e4caa167f51e9357
Even Rouault <even.rouault@spatialys.com>
2016-11-01 19:47:10 +0100

    fixes

--- gdal/ci/travis/mingw_w64/before_install.sh
+++ gdal/ci/travis/mingw_w64/before_install.sh
@@ -5,9 +5,4 @@ set -e
 cat /etc/apt/sources.list
 ls -al /etc/apt/sources.list.d
 sudo apt-get update -qq
-sudo apt-get install binutils-mingw-w64-i686
-sudo apt-get install gcc-mingw-w64-i686
-sudo apt-get install g++-mingw-w64-i686
-sudo apt-get install g++-mingw-w64
-sudo apt-get install mingw-w64-tools
-sudo apt-get install -y wine1.4-amd64
+sudo apt-get install -y ccache mingw32 binutils-mingw-w64-i686 gcc-mingw-w64-i686 g++-mingw-w64-i686 g++-mingw-w64 mingw-w64-tools wine1.4-amd64
