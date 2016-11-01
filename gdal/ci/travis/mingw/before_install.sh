6932883dac893281352ad30643f1dd04e5166f13
Even Rouault <even.rouault@spatialys.com>
2016-11-01 21:51:11 +0100

    fixes

--- gdal/ci/travis/mingw/before_install.sh
+++ gdal/ci/travis/mingw/before_install.sh
@@ -3,7 +3,6 @@
 set -e
 
 sudo apt-get update -qq
-sudo apt-get install -qq ccache wine swig curl mingw32
-#sudo apt-get install -y gcc-mingw-w64
-#sudo apt-get install -y g++-mingw-w64
-#sudo apt-get install -y mingw-w64
+sudo apt-get install -qq ccache
+sudo apt-get install -qq wine swig curl
+sudo apt-get install -qq mingw32
