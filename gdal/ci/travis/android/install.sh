88c2affe4b676dab38ccde5a0de8155b71937694
Even Rouault <even.rouault@spatialys.com>
2016-11-01 19:52:02 +0100

    fixes

--- gdal/ci/travis/android/install.sh
+++ gdal/ci/travis/android/install.sh
@@ -2,11 +2,12 @@
 
 set -e
 
-cd gdal
 export CCACHE_CPP2=yes
 
 unset CC
 unset CXX
 export PATH=$HOME/android-8-toolchain/bin:$PATH
+
+cd gdal
 CC="ccache arm-linux-androideabi-gcc" CXX="ccache arm-linux-androideabi-g++" CFLAGS="-mthumb" CXXFLAGS="-mthumb" LIBS="-lsupc++ -lstdc++" ./configure --host=arm-linux-androideabi
 make USER_DEFS="-Wextra -Werror" -j3
--- gdal/ci/travis/big_endian/install.sh
+++ gdal/ci/travis/big_endian/install.sh
@@ -2,13 +2,12 @@
 
 set -e
 
-cd gdal
-export CCACHE_CPP2=yes
-
 export PATH=$PWD/gcc-linaro-5.1-2015.08-x86_64_armeb-linux-gnueabihf/bin:$PATH
 unset CC
 unset CXX
+
 cd gdal
+export CCACHE_CPP2=yes
 CC="ccache armeb-linux-gnueabihf-gcc" CXX="ccache armeb-linux-gnueabihf-g++" ./configure --host=armeb-linux-gnueabihf --without-libtool
 make USER_DEFS="-Werror" -j3
 cd apps
