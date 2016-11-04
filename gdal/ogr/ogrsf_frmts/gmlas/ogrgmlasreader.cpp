3cc29214f89498b0f1ed15f92228baa41145d5c9
Even Rouault <even.rouault@mines-paris.org>
2016-11-04 23:45:24 +0000

    Fix warning with clang and external libjson 0.11
    git-svn-id: https://svn.osgeo.org/gdal/trunk@36110 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/gmlas/ogrgmlasreader.cpp
+++ gdal/ogr/ogrsf_frmts/gmlas/ogrgmlasreader.cpp
@@ -33,7 +33,16 @@
 
 #include "ogr_p.h"
 
-#include <json.h>
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunknown-pragmas"
+#pragma clang diagnostic ignored "-Wdocumentation"
+#endif
+#include "json.h"
+#ifdef __clang
+#pragma clang diagnostic pop
+#endif
+
 
 CPL_CVSID("$Id$");
 
