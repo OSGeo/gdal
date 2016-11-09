26c546d0560e688c3a24cb9a860cda772ddf8fdf
Even Rouault <even.rouault@mines-paris.org>
2016-11-09 10:30:59 +0000

    Android Travis-CI: try with explicit --system=linux-x86_64
    git-svn-id: https://svn.osgeo.org/gdal/trunk@36180 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ci/travis/android/before_install.sh
+++ gdal/ci/travis/android/before_install.sh
@@ -4,4 +4,4 @@ set -e
 
 wget http://dl.google.com/android/ndk/android-ndk-r9d-linux-x86_64.tar.bz2
 tar xjf android-ndk-r9d-linux-x86_64.tar.bz2
-android-ndk-r9d/build/tools/make-standalone-toolchain.sh --platform=android-8 --install-dir=$HOME/android-8-toolchain
+android-ndk-r9d/build/tools/make-standalone-toolchain.sh --system=linux-x86_64 --platform=android-8 --install-dir=$HOME/android-8-toolchain
