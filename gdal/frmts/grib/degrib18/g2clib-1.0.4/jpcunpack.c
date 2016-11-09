d4044769628f6bf59a06414e711ba8603085fb25
Even Rouault <even.rouault@mines-paris.org>
2016-11-09 21:39:10 +0000

    GRIB: fix outdated comments, and add security check in dec_jpeg2000()
    git-svn-id: https://svn.osgeo.org/gdal/trunk@36190 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/grib/degrib18/g2clib-1.0.4/jpcunpack.c
+++ gdal/frmts/grib/degrib18/g2clib-1.0.4/jpcunpack.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include "grib2.h"
 
-   int dec_jpeg2000(char *,g2int ,g2int *);
-
 g2int jpcunpack(unsigned char *cpack,g2int len,g2int *idrstmpl,g2int ndpts,
                 g2float *fld)
 //$$$  SUBPROGRAM DOCUMENTATION BLOCK
@@ -59,7 +57,7 @@ g2int jpcunpack(unsigned char *cpack,g2int len,g2int *idrstmpl,g2int ndpts,
                     "Data field NOT unpacked.\n");
             return(1);
          }
-         /* iret= (g2int) */ dec_jpeg2000((char *) cpack,len,ifld);
+         /* iret= (g2int) */ dec_jpeg2000(cpack,len,ifld,ndpts);
          for (j=0;j<ndpts;j++) {
            fld[j]=(((g2float)ifld[j]*bscale)+ref)*dscale;
          }
