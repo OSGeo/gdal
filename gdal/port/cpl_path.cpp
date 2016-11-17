OSGeo/gdal

commit 13eb65ec03b3796daca16a9300a2d2a753388a9a

Clean cpl_path.cpp.
- Include What You Use (IWYU)
- /* */ -> //
- Fix formatting
- Add const
- Simplify #if setup around CPLGetCurrentDir()
- Fix capitalization and punctuation in comments


git-svn-id: https://svn.osgeo.org/gdal/trunk@36245 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/port/cpl_path.cpp
@@ -27,16 +27,29 @@
  * DEALINGS IN THE SOFTWARE.
  ****************************************************************************/
 
-#include "cpl_atomic_ops.h"
+#include "cpl_port.h"
 #include "cpl_conv.h"
-#include "cpl_string.h"
-#include "cpl_multiproc.h"
+
+#include <ctype.h>
+#include <limits.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <string>
 
 #include <algorithm>
 
+#include "cpl_atomic_ops.h"
+#include "cpl_config.h"
+#include "cpl_error.h"
+#include "cpl_multiproc.h"
+#include "cpl_string.h"
+#include "cpl_vsi.h"
+
 CPL_CVSID("$Id$");
 
-/* should be size of larged possible filename */
+// Should be size of larged possible filename.
 static const int CPL_PATH_BUF_SIZE = 2048;
 static const int CPL_PATH_BUF_COUNT = 10;
 
@@ -46,7 +59,7 @@ static const char SEP_STRING[] = "\\";
 static const char SEP_STRING[] = "/";
 #endif
 
-static const char* CPLStaticBufferTooSmall(char *pszStaticResult)
+static const char* CPLStaticBufferTooSmall( char *pszStaticResult )
 {
     CPLError(CE_Failure, CPLE_AppDefined, "Destination buffer too small");
     if( pszStaticResult == NULL )
@@ -63,8 +76,8 @@ static char *CPLGetStaticResult()
 
 {
     int bMemoryError = FALSE;
-    char *pachBufRingInfo
-        = static_cast<char *>( CPLGetTLSEx( CTLS_PATHBUF, &bMemoryError ) );
+    char *pachBufRingInfo =
+        static_cast<char *>( CPLGetTLSEx( CTLS_PATHBUF, &bMemoryError ) );
     if( bMemoryError )
         return NULL;
     if( pachBufRingInfo == NULL )
@@ -82,8 +95,8 @@ static char *CPLGetStaticResult()
 /*      time.                                                           */
 /* -------------------------------------------------------------------- */
     int *pnBufIndex = reinterpret_cast<int *>( pachBufRingInfo );
-    size_t nOffset
-        = sizeof(int) + static_cast<size_t>( *pnBufIndex * CPL_PATH_BUF_SIZE );
+    const size_t nOffset =
+        sizeof(int) + static_cast<size_t>( *pnBufIndex * CPL_PATH_BUF_SIZE );
     char *pachBuffer = pachBufRingInfo + nOffset;
 
     *pnBufIndex = (*pnBufIndex + 1) % CPL_PATH_BUF_COUNT;
@@ -139,13 +152,13 @@ const char *CPLGetPath( const char *pszFilename )
 
 {
     const int iFileStart = CPLFindFilenameStart(pszFilename);
-    char       *pszStaticResult = CPLGetStaticResult();
+    char *pszStaticResult = CPLGetStaticResult();
 
     if( pszStaticResult == NULL || iFileStart >= CPL_PATH_BUF_SIZE )
         return CPLStaticBufferTooSmall(pszStaticResult);
 
-    CPLAssert( ! (pszFilename >= pszStaticResult
-                  && pszFilename < pszStaticResult + CPL_PATH_BUF_SIZE) );
+    CPLAssert( !(pszFilename >= pszStaticResult
+                 && pszFilename < pszStaticResult + CPL_PATH_BUF_SIZE) );
 
     if( iFileStart == 0 )
     {
@@ -194,13 +207,13 @@ const char *CPLGetDirname( const char *pszFilename )
 
 {
     const int iFileStart = CPLFindFilenameStart(pszFilename);
-    char       *pszStaticResult = CPLGetStaticResult();
+    char *pszStaticResult = CPLGetStaticResult();
 
     if( pszStaticResult == NULL || iFileStart >= CPL_PATH_BUF_SIZE )
         return CPLStaticBufferTooSmall(pszStaticResult);
 
-    CPLAssert( ! (pszFilename >= pszStaticResult
-                  && pszFilename < pszStaticResult + CPL_PATH_BUF_SIZE) );
+    CPLAssert( !(pszFilename >= pszStaticResult
+                 && pszFilename < pszStaticResult + CPL_PATH_BUF_SIZE) );
 
     if( iFileStart == 0 )
     {
@@ -277,14 +290,14 @@ const char *CPLGetFilename( const char *pszFullFilename )
 const char *CPLGetBasename( const char *pszFullFilename )
 
 {
-    const size_t iFileStart
-        = static_cast<size_t>( CPLFindFilenameStart( pszFullFilename ) );
-    char    *pszStaticResult = CPLGetStaticResult();
+    const size_t iFileStart =
+        static_cast<size_t>( CPLFindFilenameStart( pszFullFilename ) );
+    char *pszStaticResult = CPLGetStaticResult();
     if( pszStaticResult == NULL )
         return CPLStaticBufferTooSmall(pszStaticResult);
 
-    CPLAssert( ! ( pszFullFilename >= pszStaticResult
-                   && pszFullFilename < pszStaticResult + CPL_PATH_BUF_SIZE ) );
+    CPLAssert( !( pszFullFilename >= pszStaticResult
+                  && pszFullFilename < pszStaticResult + CPL_PATH_BUF_SIZE ) );
 
     size_t iExtStart = strlen(pszFullFilename);
     for( ;
@@ -330,16 +343,16 @@ const char *CPLGetBasename( const char *pszFullFilename )
 const char *CPLGetExtension( const char *pszFullFilename )
 
 {
-    size_t iFileStart
-        = static_cast<size_t>( CPLFindFilenameStart( pszFullFilename ) );
-    char    *pszStaticResult = CPLGetStaticResult();
+    size_t iFileStart =
+        static_cast<size_t>( CPLFindFilenameStart( pszFullFilename ) );
+    char *pszStaticResult = CPLGetStaticResult();
     if( pszStaticResult == NULL )
         return CPLStaticBufferTooSmall(pszStaticResult);
 
-    CPLAssert( ! ( pszFullFilename >= pszStaticResult
-                   && pszFullFilename < pszStaticResult + CPL_PATH_BUF_SIZE ) );
+    CPLAssert( !( pszFullFilename >= pszStaticResult
+                  && pszFullFilename < pszStaticResult + CPL_PATH_BUF_SIZE ) );
 
-    size_t  iExtStart = strlen(pszFullFilename);
+    size_t iExtStart = strlen(pszFullFilename);
     for( ;
          iExtStart > iFileStart && pszFullFilename[iExtStart] != '.';
          iExtStart-- ) {}
@@ -368,8 +381,8 @@ const char *CPLGetExtension( const char *pszFullFilename )
  * If HAVE_GETCWD macro is not defined, the function returns NULL.
  **/
 
+#ifdef HAVE_GETCWD
 char *CPLGetCurrentDir()
-
 {
 #ifdef _MAX_PATH
     const size_t nPathMax = _MAX_PATH;
@@ -383,12 +396,11 @@ char *CPLGetCurrentDir()
     if ( !pszDirPath )
         return NULL;
 
-#ifdef HAVE_GETCWD
     return getcwd( pszDirPath, nPathMax );
-#else
-    return NULL;
-#endif /* HAVE_GETCWD */
 }
+#else  // !HAVE_GETCWD
+char *CPLGetCurrentDir() { return NULL; }
+#endif // HAVE_GETCWD
 
 /************************************************************************/
 /*                         CPLResetExtension()                          */
@@ -408,7 +420,7 @@ char *CPLGetCurrentDir()
 const char *CPLResetExtension( const char *pszPath, const char *pszExt )
 
 {
-    char    *pszStaticResult = CPLGetStaticResult();
+    char *pszStaticResult = CPLGetStaticResult();
     if( pszStaticResult == NULL )
         return CPLStaticBufferTooSmall(pszStaticResult);
 
@@ -501,8 +513,8 @@ const char *CPLFormFilename( const char * pszPath,
     if( pszBasename[0] == '.' && pszBasename[1] == '/' )
         pszBasename += 2;
 
-    const char  *pszAddedPathSep = "";
-    const char  *pszAddedExtSep = "";
+    const char *pszAddedPathSep = "";
+    const char *pszAddedExtSep = "";
 
     if( pszPath == NULL )
         pszPath = "";
@@ -510,14 +522,14 @@ const char *CPLFormFilename( const char * pszPath,
     if( !CPLIsFilenameRelative(pszPath) &&
         strcmp(pszBasename, "..") == 0 )
     {
-        /* /a/b + .. --> /a */
+        // /a/b + .. --> /a
         if( pszPath[nLenPath-1] == '\\' || pszPath[nLenPath-1] == '/' )
             nLenPath --;
         size_t nLenPathOri = nLenPath;
         while( nLenPath > 0 && pszPath[nLenPath-1] != '\\' &&
                pszPath[nLenPath-1] != '/')
         {
-            nLenPath --;
+            nLenPath--;
         }
         if( nLenPath == 1 && pszPath[0] == '/' )
         {
@@ -527,7 +539,7 @@ const char *CPLFormFilename( const char * pszPath,
                  (nLenPath > 2 && pszPath[1] == ':') ||
                  (nLenPath > 6 && strncmp(pszPath, "\\\\$\\", 4) == 0) )
         {
-            nLenPath --;
+            nLenPath--;
             pszBasename = "";
         }
         else
@@ -540,8 +552,8 @@ const char *CPLFormFilename( const char * pszPath,
              && pszPath[nLenPath-1] != '/'
              && pszPath[nLenPath-1] != '\\' )
     {
-        /* FIXME? would be better to ask the filesystems what they */
-        /* prefer as directory separator */
+        // FIXME? Would be better to ask the filesystems what it
+        // prefers as directory separator?
         if (strcmp(pszPath, "/vsimem") == 0 ||
             STARTS_WITH(pszPath, "/vsimem/") ||
             STARTS_WITH(pszPath, "/vsicurl/") ||
@@ -609,13 +621,12 @@ const char *CPLFormCIFilename( const char * pszPath,
                                const char * pszExtension )
 
 {
-    // On case insensitive filesystems, just default to
-    // CPLFormFilename()
+    // On case insensitive filesystems, just default to CPLFormFilename().
     if( !VSIIsCaseSensitiveFS(pszPath) )
         return CPLFormFilename( pszPath, pszBasename, pszExtension );
 
-    const char  *pszAddedExtSep = "";
-    size_t       nLen = strlen(pszBasename)+2;
+    const char *pszAddedExtSep = "";
+    size_t nLen = strlen(pszBasename) + 2;
 
     if( pszExtension != NULL )
         nLen += strlen(pszExtension);
@@ -706,11 +717,11 @@ const char *CPLProjectRelativeFilename( const char *pszProjectDir,
     if( pszStaticResult == NULL )
         return CPLStaticBufferTooSmall(pszStaticResult);
 
-    CPLAssert( ! ( pszProjectDir >= pszStaticResult
-                   && pszProjectDir < pszStaticResult + CPL_PATH_BUF_SIZE ) );
-    CPLAssert( ! ( pszSecondaryFilename >= pszStaticResult
-                   && pszSecondaryFilename
-                   < pszStaticResult + CPL_PATH_BUF_SIZE) );
+    CPLAssert( !(pszProjectDir >= pszStaticResult
+                 && pszProjectDir < pszStaticResult + CPL_PATH_BUF_SIZE ) );
+    CPLAssert( !(pszSecondaryFilename >= pszStaticResult
+                 && pszSecondaryFilename
+                 < pszStaticResult + CPL_PATH_BUF_SIZE) );
 
     if( !CPLIsFilenameRelative( pszSecondaryFilename ) )
         return pszSecondaryFilename;
@@ -725,8 +736,8 @@ const char *CPLProjectRelativeFilename( const char *pszProjectDir,
     if( pszProjectDir[strlen(pszProjectDir)-1] != '/'
         && pszProjectDir[strlen(pszProjectDir)-1] != '\\' )
     {
-        /* FIXME? would be better to ask the filesystems what they */
-        /* prefer as directory separator */
+        // FIXME: Better to ask the filesystems what it
+        // prefers as directory separator?
         const char* pszAddedPathSep;
         if (strcmp(pszStaticResult, "/vsimem") == 0 ||
             STARTS_WITH(pszStaticResult, "/vsicurl/") ||
@@ -768,7 +779,7 @@ int CPLIsFilenameRelative( const char *pszFilename )
     if( (pszFilename[0] != '\0'
          && (STARTS_WITH(pszFilename+1, ":\\")
              || STARTS_WITH(pszFilename+1, ":/")))
-        || STARTS_WITH(pszFilename, "\\\\?\\") /* Windows extended Length Path */
+        || STARTS_WITH(pszFilename, "\\\\?\\")  // Windows extended Length Path.
         || pszFilename[0] == '\\'
         || pszFilename[0] == '/' )
         return FALSE;
@@ -898,7 +909,7 @@ const char *CPLExtractRelativePath( const char *pszBaseDir,
 const char *CPLCleanTrailingSlash( const char *pszPath )
 
 {
-    char       *pszStaticResult = CPLGetStaticResult();
+    char *pszStaticResult = CPLGetStaticResult();
     if( pszStaticResult == NULL )
         return CPLStaticBufferTooSmall(pszStaticResult);
     CPLAssert( ! ( pszPath >= pszStaticResult
@@ -1021,16 +1032,13 @@ char **CPLCorrespondingPaths( const char *pszOldFilename,
 
     for( int i=0; papszFileList[i] != NULL; i++ )
     {
-        CPLString osNewFilename;
         const CPLString osOldFilename = CPLGetFilename( papszFileList[i] );
 
-        if( osOldBasename == osNewBasename )
-            osNewFilename =
-                CPLFormFilename( osNewPath, osOldFilename, NULL );
-        else
-            osNewFilename =
-                CPLFormFilename( osNewPath, osNewBasename,
-                                 osOldFilename.c_str()+strlen(osOldBasename));
+        const CPLString osNewFilename =
+            osOldBasename == osNewBasename
+            ? CPLFormFilename( osNewPath, osOldFilename, NULL )
+            : CPLFormFilename( osNewPath, osNewBasename,
+                               osOldFilename.c_str() + strlen(osOldBasename));
 
         papszNewList = CSLAddString( papszNewList, osNewFilename );
     }

commit 39f592e698fbe1fc4013126958ef50100bcb7581

Guard include of unistd.h to fix windows builds.
Broken by r36245.

Also switch to using the C++ convention for standard C includes


git-svn-id: https://svn.osgeo.org/gdal/trunk@36261 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/port/cpl_path.cpp
@@ -31,14 +31,16 @@
 #include "cpl_conv.h"
 
 #include <ctype.h>
-#include <limits.h>
-#include <stddef.h>
-#include <stdio.h>
-#include <string.h>
+#include <climits>
+#include <cstddef>
+#include <cstdio>
+#include <cstring>
+#ifndef _WIN32
 #include <unistd.h>
-#include <string>
+#endif
 
 #include <algorithm>
+#include <string>
 
 #include "cpl_atomic_ops.h"
 #include "cpl_config.h"