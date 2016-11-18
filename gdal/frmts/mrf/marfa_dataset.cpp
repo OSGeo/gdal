OSGeo/gdal

commit 613d0faa8f6cd8e077037702166afbf11df8189e

MRF: Add support for spacing (unused space) between tiles. Defaults to none.
git-svn-id: https://svn.osgeo.org/gdal/trunk@36271 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/frmts/mrf/marfa_dataset.cpp
@@ -80,6 +80,7 @@ GDALMRFDataset::GDALMRFDataset() :
     hasVersions(FALSE),
     verCount(0),
     bCrystalized(FALSE), // Assume not in create mode
+    spacing(0),
     poSrcDS(NULL),
     level(-1),
     cds(NULL),
@@ -1092,6 +1093,8 @@ CPLXMLNode * GDALMRFDataset::BuildConfig()
         CPLCreateXMLElementAndValue(raster, "DataFile", full.datfname.c_str());
     if (full.idxfname != getFname(GetFname(), ".idx"))
         CPLCreateXMLElementAndValue(raster, "IndexFile", full.idxfname.c_str());
+    if (spacing != 0)
+        XMLSetAttributeVal(raster, "Spacing", static_cast<double>(spacing), "%.0f");
 
     XMLSetAttributeVal(raster, "Size", full.size, "%.0f");
     XMLSetAttributeVal(raster, "PageSize", full.pagesize, "%.0f");
@@ -1202,6 +1205,7 @@ CPLErr GDALMRFDataset::Initialize(CPLXMLNode *config)
 
     hasVersions = on(CPLGetXMLValue(config, "Raster.versioned", "no"));
     mp_safe = on(CPLGetXMLValue(config, "Raster.mp_safe", "no"));
+    spacing = atoi(CPLGetXMLValue(config, "Raster.Spacing", "0"));
 
     Quality = full.quality;
     if (CE_None != ret)
@@ -1613,6 +1617,9 @@ void GDALMRFDataset::ProcessCreateOptions(char **papszOptions)
     val = opt.FetchNameValue("INDEXNAME");
     if (val) img.idxfname = val;
 
+    val = opt.FetchNameValue("SPACING");
+    if (val) spacing = atoi(val);
+
     optlist.Assign(CSLTokenizeString2(opt.FetchNameValue("OPTIONS"),
         " \t\n\r", CSLT_STRIPLEADSPACES | CSLT_STRIPENDSPACES));
 
@@ -1790,6 +1797,7 @@ CPLErr GDALMRFDataset::WriteTile(void *buff, GUIntBig infooffset, GUIntBig size)
     if (l_ifp == NULL || l_dfp == NULL)
         return CE_Failure;
 
+    // If it has versions, might need to start a new one
     if (hasVersions) {
         int new_version = false; // Assume no need to build new version
         int new_tile = false;
@@ -1798,7 +1806,7 @@ CPLErr GDALMRFDataset::WriteTile(void *buff, GUIntBig infooffset, GUIntBig size)
         VSIFSeekL(l_ifp, infooffset, SEEK_SET);
         VSIFReadL(&tinfo, 1, sizeof(ILIdx), l_ifp);
 
-        if (verCount != 0) { // We have at least two versions before we test buffers
+        if (verCount != 0) { // We need at least two versions before we test buffers
             ILIdx prevtinfo = { 0, 0 };
 
             // Read the previous one
@@ -1848,11 +1856,23 @@ CPLErr GDALMRFDataset::WriteTile(void *buff, GUIntBig infooffset, GUIntBig size)
     tinfo.size = net64(size);
 
     if (size) do {
+
         // These statements are the critical MP section for the data file
         VSIFSeekL(l_dfp, 0, SEEK_END);
         GUIntBig offset = VSIFTellL(l_dfp);
+
+        if (spacing != 0) { // This should not be true in MP safe mode
+            // Use the same buffer, MRF doesn't care about the spacing content
+            int pad = (size >= spacing) ? spacing : size;
+            if (pad != spacing)
+                CPLError(CE_Warning, CPLE_FileIO, "MRF spacing failed, check the output");
+            offset += pad;
+            VSIFWriteL(buff, 1, spacing, l_dfp);
+        }
+
         if (static_cast<size_t>(size) != VSIFWriteL(buff, 1, static_cast<size_t>(size), l_dfp))
             ret = CE_Failure;
+        // End of critical section
 
         tinfo.offset = net64(offset);
         //

gdal/frmts/mrf/marfa.h
@@ -448,6 +448,7 @@ protected:
     int hasVersions;  // Does it support versions
     int verCount;     // The last version
     int bCrystalized; // Unset only during the create process
+    int spacing;      // How many spare bytes before each tile data
 
     // Freeform sticky dataset options, as a list of key-value pairs
     CPLStringList optlist;

gdal/frmts/mrf/mrf_util.cpp
@@ -615,13 +615,18 @@ void GDALRegister_mrf()
         "   <Option name='BLOCKSIZE' type='int' description='Block size, both x and y, default 512'/>\n"
         "   <Option name='BLOCKXSIZE' type='int' description='Block x size, default=512'/>\n"
         "   <Option name='BLOCKYSIZE' type='int' description='Block y size, default=512'/>\n"
-        "   <Option name='NETBYTEORDER' type='boolean' description='Force endian for certain compress options, default is host order'/>\n"
-        "   <Option name='CACHEDSOURCE' type='string' description='The source raster, if this is a cache'/>\n"
+        "   <Option name='NETBYTEORDER' type='boolean' "
+                    "description='Force endian for certain compress options, default is host order'/>\n"
+        "   <Option name='CACHEDSOURCE' type='string' "
+                    "description='The source raster, if this is a cache'/>\n"
         "   <Option name='UNIFORM_SCALE' type='int' description='Scale of overlays in MRF, usually 2'/>\n"
         "   <Option name='NOCOPY' type='boolean' description='Leave created MRF empty, default=no'/>\n"
         "   <Option name='DATANAME' type='string' description='Data file name'/>\n"
         "   <Option name='INDEXNAME' type='string' description='Index file name'/>\n"
-        "   <Option name='PHOTOMETRIC' type='string-select' default='DEFAULT' description='Band interpretation, may affect block encoding'>\n"
+        "   <Option name='SPACING' type='int' "
+                    "description='Leave this many unused bytes before each tile, default=0'/>\n"
+        "   <Option name='PHOTOMETRIC' type='string-select' default='DEFAULT' "
+                    "description='Band interpretation, may affect block encoding'>\n"
         "       <Value>MULTISPECTRAL</Value>"
         "       <Value>RGB</Value>"
         "       <Value>YCC</Value>"

commit 5d0c10ec83a9c17b9fdbd422ea709a84f9962dd5

Force size to int to fix casting issue with size in marfa_dataset.cpp.
Caused by r36271.

marfa_dataset.cpp:1866:29: error: comparison of integers of different signs: 'GUIntBig' (aka 'unsigned long long') and 'int' [-Werror,-Wsign-compare]


git-svn-id: https://svn.osgeo.org/gdal/trunk@36274 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/frmts/mrf/marfa_dataset.cpp
@@ -1861,11 +1861,18 @@ CPLErr GDALMRFDataset::WriteTile(void *buff, GUIntBig infooffset, GUIntBig size)
         VSIFSeekL(l_dfp, 0, SEEK_END);
         GUIntBig offset = VSIFTellL(l_dfp);
 
-        if (spacing != 0) { // This should not be true in MP safe mode
-            // Use the same buffer, MRF doesn't care about the spacing content
-            int pad = (size >= spacing) ? spacing : size;
-            if (pad != spacing)
-                CPLError(CE_Warning, CPLE_FileIO, "MRF spacing failed, check the output");
+        if( spacing != 0 )
+        {
+            // This should not be true in MP safe mode.
+            // Use the same buffer, MRF doesn't care about the spacing content.
+            // TODO(lplesea): Make sure size doesn't overflow.
+            const int pad =
+                static_cast<int>(size) >= spacing
+                ? spacing
+                : static_cast<int>(size);
+            if( pad != spacing )
+                CPLError(CE_Warning, CPLE_FileIO,
+                         "MRF spacing failed, check the output");
             offset += pad;
             VSIFWriteL(buff, 1, spacing, l_dfp);
         }