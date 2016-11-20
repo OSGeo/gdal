a7537212f58dcc27033387cbdd5aa9f7280cdab7
Even Rouault <even.rouault@mines-paris.org>
2016-11-20 16:00:10 +0000

    Really fix scripts/cppcheck.sh
    git-svn-id: https://svn.osgeo.org/gdal/trunk@36336 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/scripts/cppcheck.sh
+++ gdal/scripts/cppcheck.sh
@@ -3,9 +3,25 @@ cppcheck --inline-suppr --template='{file}:{line},{severity},{id},{message}' \
     --enable=all --inconclusive --std=posix -UAFL_FRIENDLY -UANDROID \
     -UCOMPAT_WITH_ICC_CONVERSION_CHECK -DDEBUG -UDEBUG_BOOL \
     alg port gcore ogr frmts gnm -j 8 >${LOG_FILE} 2>&1
-grep "null pointer" ${LOG_FILE} && (echo "Null pointer check failed"; exit 1)
-grep "duplicateBreak" ${LOG_FILE} && (echo "duplicateBreak check failed"; exit 1)
-grep "duplicateBranch" ${LOG_FILE} && (echo "duplicateBranch check failed"; exit 1)
+
+grep "null pointer" ${LOG_FILE}
+if [[ $? -eq 0 ]] ; then
+    echo "Null pointer check failed"
+    exit 1
+fi
+
+grep "duplicateBreak" ${LOG_FILE}
+if [[ $? -eq 0 ]] ; then
+    echo "duplicateBreak check failed"
+    exit 1
+fi
+
+grep "duplicateBranch" ${LOG_FILE}
+if [[ $? -eq 0 ]] ; then
+    echo "duplicateBranch check failed"
+    exit 1
+fi
+
 echo "cppcheck succeeded"
 
 
