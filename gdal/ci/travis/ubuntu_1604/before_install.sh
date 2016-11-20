cdc9653d4a2237117607418a15f2a4cde4556208
Even Rouault <even.rouault@mines-paris.org>
2016-11-20 15:20:13 +0000

    Add a scripts/cppcheck.sh and use it ubuntu_1604 Travis-CI target
    git-svn-id: https://svn.osgeo.org/gdal/trunk@36333 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ci/travis/ubuntu_1604/before_install.sh
+++ gdal/ci/travis/ubuntu_1604/before_install.sh
@@ -32,3 +32,6 @@ sudo chroot "$chroot" apt-get install -y pyflakes3
 sudo chroot "$chroot" sh -c "cd $PWD && pyflakes3 autotest"
 sudo chroot "$chroot" sh -c "cd $PWD && pyflakes3 gdal/swig/python/scripts"
 sudo chroot "$chroot" sh -c "cd $PWD && pyflakes3 gdal/swig/python/samples"
+
+sudo chroot "$chroot" apt-get install -y cppcheck
+sudo chroot "$chroot" sh -c "cd $PWD/gdal && scripts/cppcheck.sh"
