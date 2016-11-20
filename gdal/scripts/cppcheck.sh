6cc3004c6431d908e6d45c5c3b7c5b237ef10e7a
Even Rouault <even.rouault@mines-paris.org>
2016-11-20 15:54:40 +0000

    Fix scripts/cppcheck.sh
    git-svn-id: https://svn.osgeo.org/gdal/trunk@36335 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/scripts/cppcheck.sh
+++ gdal/scripts/cppcheck.sh
@@ -3,7 +3,9 @@ cppcheck --inline-suppr --template='{file}:{line},{severity},{id},{message}' \
     --enable=all --inconclusive --std=posix -UAFL_FRIENDLY -UANDROID \
     -UCOMPAT_WITH_ICC_CONVERSION_CHECK -DDEBUG -UDEBUG_BOOL \
     alg port gcore ogr frmts gnm -j 8 >${LOG_FILE} 2>&1
-grep "null pointer" ${LOG_FILE} && exit 1
-grep "duplicateBreak" ${LOG_FILE} && exit 1
-grep "duplicateBranch" ${LOG_FILE} && exit 1
+grep "null pointer" ${LOG_FILE} && (echo "Null pointer check failed"; exit 1)
+grep "duplicateBreak" ${LOG_FILE} && (echo "duplicateBreak check failed"; exit 1)
+grep "duplicateBranch" ${LOG_FILE} && (echo "duplicateBranch check failed"; exit 1)
+echo "cppcheck succeeded"
+
 
