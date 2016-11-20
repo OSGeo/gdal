19fcff4454a780e70e737bfb7053f77c59af7ac0
Even Rouault <even.rouault@mines-paris.org>
2016-11-20 00:01:59 +0000

    Pyflakes3 fixes
    git-svn-id: https://svn.osgeo.org/gdal/trunk@36313 f0d54148-0727-0410-94bb-9a71ac55c965

--- autotest/gdrivers/netcdf_cfchecks.py
+++ autotest/gdrivers/netcdf_cfchecks.py
@@ -72,6 +72,8 @@ def normalize_whitespace(text):
     "Remove redundant whitespace from a string."
     return ' '.join(text.split())
 
+def my_cmp(a,b):
+    return (a>b)-(a<b)
 
 class CFVersion(object):
     """A CF version number, stored as a tuple, that can be instantiated with
@@ -103,7 +105,7 @@ class CFVersion(object):
             in_o = (pos < len(other.tuple))
             if in_s:
                 if in_o:
-                    c = cmp(self.tuple[pos], other.tuple[pos])
+                    c = my_cmp(self.tuple[pos], other.tuple[pos])
                     if c != 0:
                         return c  # e.g. 1.x <=> 1.y
                 else:  # in_s and not in_o
