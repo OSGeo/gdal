b3075f57862eb298ab9a2e0ed1b39cfb6269b3df
Even Rouault <even.rouault@mines-paris.org>
2016-11-22 22:31:47 +0000

    cppcheck: enable unreachableCode check and fix related warnings
    git-svn-id: https://svn.osgeo.org/gdal/trunk@36451 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/scripts/cppcheck.sh
+++ gdal/scripts/cppcheck.sh
@@ -393,6 +393,12 @@ if [[ $? -eq 0 ]] ; then
     exit 1
 fi
 
+grep "unreachableCode" ${LOG_FILE}
+if [[ $? -eq 0 ]] ; then
+    echo "unreachableCode check failed"
+    exit 1
+fi
+
 # Check any remaining errors
 grep "error," ${LOG_FILE} | grep -v "uninitvar" | \
     grep -v "memleak," | grep -v "memleakOnRealloc" | \
