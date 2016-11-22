fc5c997392c6129a7a21f09c5885ba45a793d9dd
Even Rouault <even.rouault@mines-paris.org>
2016-11-22 22:13:31 +0000

    cppcheck: enable duplicateExpression check and fix related warnings
    git-svn-id: https://svn.osgeo.org/gdal/trunk@36443 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/scripts/cppcheck.sh
+++ gdal/scripts/cppcheck.sh
@@ -345,6 +345,12 @@ if [[ $? -eq 0 ]] ; then
     exit 1
 fi
 
+grep "duplicateExpression" ${LOG_FILE}
+if [[ $? -eq 0 ]] ; then
+    echo "duplicateExpression check failed"
+    exit 1
+fi
+
 # Check any remaining errors
 grep "error," ${LOG_FILE} | grep -v "uninitvar" | \
     grep -v "memleak," | grep -v "memleakOnRealloc" | \
