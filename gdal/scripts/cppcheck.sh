2280f6b0fd1e77588ee2644c1fd5e15901a5e40c
Even Rouault <even.rouault@mines-paris.org>
2016-11-22 22:25:08 +0000

    cppcheck: enable unusedVariable check and fix related warnings
    git-svn-id: https://svn.osgeo.org/gdal/trunk@36447 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/scripts/cppcheck.sh
+++ gdal/scripts/cppcheck.sh
@@ -369,6 +369,12 @@ if [[ $? -eq 0 ]] ; then
     exit 1
 fi
 
+grep "unusedVariable" ${LOG_FILE}
+if [[ $? -eq 0 ]] ; then
+    echo "unusedVariable check failed"
+    exit 1
+fi
+
 # Check any remaining errors
 grep "error," ${LOG_FILE} | grep -v "uninitvar" | \
     grep -v "memleak," | grep -v "memleakOnRealloc" | \
