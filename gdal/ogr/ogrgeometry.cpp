OSGeo/gdal

commit db0e1c92908a120a59c7a346d5a039490f11db49

int bFoo -> bool bFoo.
ogr_fromepsg, ogrcurvecollection, ogrcurvepolygon, ogrfeature, ogrfeaturequery, ogrgeometry, ogrgeometrycollection, ogrgeometryfactory

Also some:
- Add const
- Fix formatting
- Drop else in if(a) return; else
- Initialize local vars
- Localize vars
- Define one var per line
- Fit to 80 cols

git-svn-id: https://svn.osgeo.org/gdal/trunk@35119 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/ogr/ogrgeometry.cpp
@@ -1100,7 +1100,7 @@ void OGR_G_Set3D( OGRGeometryH hGeom, int bIs3D)
  * @since GDAL 2.1
  */
 
-void OGR_G_SetMeasured( OGRGeometryH hGeom, int bIsMeasured)
+void OGR_G_SetMeasured( OGRGeometryH hGeom, int bIsMeasured )
 
 {
     VALIDATE_POINTER0( hGeom, "OGR_G_SetMeasured" );
@@ -1492,7 +1492,7 @@ OGRErr OGRGeometry::importPreambuleFromWkt( char ** ppszInput,
                                             int* pbHasZ, int* pbHasM,
                                             bool* pbIsEmpty )
 {
-    char        szToken[OGR_WKT_TOKEN_MAX];
+    char szToken[OGR_WKT_TOKEN_MAX];
     const char  *pszInput = *ppszInput;
 
 /* -------------------------------------------------------------------- */
@@ -1501,13 +1501,14 @@ OGRErr OGRGeometry::importPreambuleFromWkt( char ** ppszInput,
     empty();
     *pbIsEmpty = false;
 
-    int bHasZ = FALSE, bHasM = FALSE;
-    bool bIsoWKT = true;
-
 /* -------------------------------------------------------------------- */
 /*      Read and verify the type keyword, and ensure it matches the     */
 /*      actual type of this container.                                  */
 /* -------------------------------------------------------------------- */
+    bool bHasM = false;
+    bool bHasZ = false;
+    bool bIsoWKT = true;
+
     pszInput = OGRWktReadToken( pszInput, szToken );
     if( szToken[0] != '\0' )
     {
@@ -1516,7 +1517,7 @@ OGRErr OGRGeometry::importPreambuleFromWkt( char ** ppszInput,
         if( szToken[nTokenLen-1] == 'M' )
         {
             szToken[nTokenLen-1] = '\0';
-            bHasM = TRUE;
+            bHasM = true;
             bIsoWKT = false;
         }
     }
@@ -1527,9 +1528,7 @@ OGRErr OGRGeometry::importPreambuleFromWkt( char ** ppszInput,
 /* -------------------------------------------------------------------- */
 /*      Check for EMPTY ...                                             */
 /* -------------------------------------------------------------------- */
-    const char *pszPreScan;
-
-    pszPreScan = OGRWktReadToken( pszInput, szToken );
+    const char *pszPreScan = OGRWktReadToken( pszInput, szToken );
     if( !bIsoWKT )
     {
         /* go on */
@@ -1548,21 +1547,21 @@ OGRErr OGRGeometry::importPreambuleFromWkt( char ** ppszInput,
 /* -------------------------------------------------------------------- */
     else if( EQUAL(szToken,"Z") )
     {
-        bHasZ = TRUE;
+        bHasZ = true;
     }
     else if( EQUAL(szToken,"M") )
     {
-        bHasM = TRUE;
+        bHasM = true;
     }
     else if( EQUAL(szToken,"ZM") )
     {
-        bHasZ = TRUE;
-        bHasM = TRUE;
+        bHasZ = true;
+        bHasM = true;
     }
     *pbHasZ = bHasZ;
     *pbHasM = bHasM;
 
-    if ( bIsoWKT && (bHasZ || bHasM) )
+    if( bIsoWKT && (bHasZ || bHasM) )
     {
         pszInput = pszPreScan;
         pszPreScan = OGRWktReadToken( pszInput, szToken );
@@ -2817,7 +2816,8 @@ GEOSGeom OGRGeometry::exportToGEOS(UNUSED_IF_NO_GEOS GEOSContextHandle_t hGEOSCt
  * @since GDAL 2.0
  */
 
-OGRBoolean OGRGeometry::hasCurveGeometry(CPL_UNUSED int bLookForNonLinear) const
+OGRBoolean OGRGeometry::hasCurveGeometry(
+    CPL_UNUSED int bLookForNonLinear ) const
 {
     return FALSE;
 }
@@ -2933,12 +2933,13 @@ double OGRGeometry::Distance( const OGRGeometry *poOtherGeom ) const
     hOther = poOtherGeom->exportToGEOS(hGEOSCtxt);
     hThis = exportToGEOS(hGEOSCtxt);
 
-    int bIsErr = 0;
+    bool bIsErr = false;
     double dfDistance = 0.0;
 
     if( hThis != NULL && hOther != NULL )
     {
-        bIsErr = GEOSDistance_r( hGEOSCtxt, hThis, hOther, &dfDistance );
+        bIsErr = CPL_TO_BOOL(
+            GEOSDistance_r( hGEOSCtxt, hThis, hOther, &dfDistance ));
     }
 
     GEOSGeom_destroy_r( hGEOSCtxt, hThis );
@@ -4866,7 +4867,8 @@ OGRGeometry *OGRGeometry::DelaunayTriangulation(double /*dfTolerance*/, int /*bO
     return NULL;
 }
 #else
-OGRGeometry *OGRGeometry::DelaunayTriangulation(double dfTolerance, int bOnlyEdges) const
+OGRGeometry *OGRGeometry::DelaunayTriangulation( double dfTolerance,
+                                                 int bOnlyEdges ) const
 {
     GEOSGeom hThisGeosGeom = NULL;
     GEOSGeom hGeosProduct = NULL;
@@ -4914,7 +4916,8 @@ OGRGeometry *OGRGeometry::DelaunayTriangulation(double dfTolerance, int bOnlyEdg
  * @since OGR 2.1
  */
 
-OGRGeometryH OGR_G_DelaunayTriangulation( OGRGeometryH hThis, double dfTolerance, int bOnlyEdges )
+OGRGeometryH OGR_G_DelaunayTriangulation( OGRGeometryH hThis, double dfTolerance,
+                                          int bOnlyEdges )
 
 {
     VALIDATE_POINTER1( hThis, "OGR_G_DelaunayTriangulation", NULL );
@@ -5184,9 +5187,10 @@ int OGRPreparedGeometryIntersects( UNUSED_IF_NO_GEOS const OGRPreparedGeometry* 
     if( hGEOSOtherGeom == NULL )
         return FALSE;
 
-    int bRet = GEOSPreparedIntersects_r(poPreparedGeom->hGEOSCtxt,
-                                        poPreparedGeom->poPreparedGEOSGeom,
-                                        hGEOSOtherGeom);
+    const bool bRet = CPL_TO_BOOL(
+        GEOSPreparedIntersects_r(poPreparedGeom->hGEOSCtxt,
+                                 poPreparedGeom->poPreparedGEOSGeom,
+                                 hGEOSOtherGeom));
     GEOSGeom_destroy_r( poPreparedGeom->hGEOSCtxt, hGEOSOtherGeom );
 
     return bRet;
@@ -5211,9 +5215,10 @@ int OGRPreparedGeometryContains( UNUSED_IF_NO_GEOS const OGRPreparedGeometry* po
     if( hGEOSOtherGeom == NULL )
         return FALSE;
 
-    int bRet = GEOSPreparedContains_r(poPreparedGeom->hGEOSCtxt,
-                                      poPreparedGeom->poPreparedGEOSGeom,
-                                      hGEOSOtherGeom);
+    const bool bRet = CPL_TO_BOOL(
+        GEOSPreparedContains_r(poPreparedGeom->hGEOSCtxt,
+                               poPreparedGeom->poPreparedGEOSGeom,
+                               hGEOSOtherGeom));
     GEOSGeom_destroy_r( poPreparedGeom->hGEOSCtxt, hGEOSOtherGeom );
 
     return bRet;
@@ -5521,7 +5526,8 @@ OGRErr OGRGeometry::importCurveCollectionFromWkt( char ** ppszInput,
                                                   OGRErr (*pfnAddCurveDirectly)(OGRGeometry* poSelf, OGRCurve* poCurve) )
 
 {
-    int bHasZ = FALSE, bHasM = FALSE;
+    int bHasZ = FALSE;
+    int bHasM = FALSE;
     bool bIsEmpty = false;
     OGRErr      eErr = importPreambuleFromWkt(ppszInput, &bHasZ, &bHasM, &bIsEmpty);
     flags = 0;

gdal/ogr/ogr_fromepsg.cpp
@@ -2468,14 +2468,14 @@ int OGRSpatialReference::GetEPSGGeogCS()
 /* -------------------------------------------------------------------- */
 /*      Is this a "well known" geographic coordinate system?            */
 /* -------------------------------------------------------------------- */
-    const int bWGS = strstr(pszGEOGCS,"WGS") != NULL
+    const bool bWGS = strstr(pszGEOGCS,"WGS") != NULL
         || strstr(pszDatum, "WGS")
         || strstr(pszGEOGCS,"World Geodetic System")
         || strstr(pszGEOGCS,"World_Geodetic_System")
         || strstr(pszDatum, "World Geodetic System")
         || strstr(pszDatum, "World_Geodetic_System");
 
-    const int bNAD = strstr(pszGEOGCS,"NAD") != NULL
+    const bool bNAD = strstr(pszGEOGCS,"NAD") != NULL
         || strstr(pszDatum, "NAD")
         || strstr(pszGEOGCS,"North American")
         || strstr(pszGEOGCS,"North_American")

gdal/ogr/ogrcurvecollection.cpp
@@ -406,7 +406,7 @@ OGRErr OGRCurveCollection::exportToWkb( const OGRGeometry* poGeom,
     GUInt32 nGType = poGeom->getIsoGeometryType();
     if( eWkbVariant == wkbVariantPostGIS1 )
     {
-        int bIs3D = wkbHasZ((OGRwkbGeometryType)nGType);
+        const bool bIs3D = wkbHasZ(static_cast<OGRwkbGeometryType>(nGType));
         nGType = wkbFlatten(nGType);
         if( nGType == wkbCurvePolygon )
             nGType = POSTGIS15_CURVEPOLYGON;

gdal/ogr/ogrcurvepolygon.cpp
@@ -550,14 +550,14 @@ OGRPolygon* OGRCurvePolygon::CurvePolyToPoly(double dfMaxAngleStepSizeDegrees,
 /*                         hasCurveGeometry()                           */
 /************************************************************************/
 
-OGRBoolean OGRCurvePolygon::hasCurveGeometry(int bLookForNonLinear) const
+OGRBoolean OGRCurvePolygon::hasCurveGeometry( int bLookForNonLinear ) const
 {
     if( bLookForNonLinear )
     {
         return oCC.hasCurveGeometry(bLookForNonLinear);
     }
-    else
-        return TRUE;
+
+    return TRUE;
 }
 
 /************************************************************************/

gdal/ogr/ogrfeature.cpp
@@ -2695,10 +2695,11 @@ int OGRFeature::GetFieldAsDateTime( int iField,
                                     int *pnHour, int *pnMinute, int *pnSecond,
                                     int *pnTZFlag )
 {
-    float fSecond;
-    int bRet = GetFieldAsDateTime( iField, pnYear, pnMonth, pnDay, pnHour, pnMinute,
-                                   &fSecond, pnTZFlag);
-    if( bRet && pnSecond ) *pnSecond = (int)fSecond;
+    float fSecond = 0.0f;
+    const bool bRet =
+        GetFieldAsDateTime( iField, pnYear, pnMonth, pnDay, pnHour, pnMinute,
+                            &fSecond, pnTZFlag);
+    if( bRet && pnSecond ) *pnSecond = static_cast<int>(fSecond);
     return bRet;
 }
 
@@ -2737,12 +2738,13 @@ int OGR_F_GetFieldAsDateTime( OGRFeatureH hFeat, int iField,
 {
     VALIDATE_POINTER1( hFeat, "OGR_F_GetFieldAsDateTime", 0 );
 
-    float fSecond;
-    int bRet =((OGRFeature *)hFeat)->GetFieldAsDateTime( iField,
-                                                      pnYear, pnMonth, pnDay,
-                                                      pnHour, pnMinute,&fSecond,
-                                                      pnTZFlag );
-    if( bRet && pnSecond ) *pnSecond = (int)fSecond;
+    float fSecond = 0.0f;
+    const bool bRet =reinterpret_cast<OGRFeature *>(hFeat)->
+        GetFieldAsDateTime( iField,
+                            pnYear, pnMonth, pnDay,
+                            pnHour, pnMinute,&fSecond,
+                            pnTZFlag );
+    if( bRet && pnSecond ) *pnSecond = static_cast<int>(fSecond);
     return bRet;
 }
 
@@ -3252,15 +3254,15 @@ void OGRFeature::SetField( int iField, const char * pszValue )
 
 {
     static int bWarn = -1;
-    OGRFieldDefn *poFDefn = poDefn->GetFieldDefn( iField );
-    char *pszLast = NULL;
-
     if( bWarn < 0 )
         bWarn = CPLTestBool( CPLGetConfigOption( "OGR_SETFIELD_NUMERIC_WARNING",
                                                  "YES" ) );
 
+    OGRFieldDefn *poFDefn = poDefn->GetFieldDefn( iField );
     if( poFDefn == NULL )
         return;
+
+    char *pszLast = NULL;
 
     OGRFieldType eType = poFDefn->GetType();
     if( eType == OFTString )
@@ -5602,8 +5604,8 @@ void OGR_F_SetStyleTable( OGRFeatureH hFeat,
  * @since GDAL 2.0
  */
 
-void OGRFeature::FillUnsetWithDefault(int bNotNullableOnly,
-                                      CPL_UNUSED char** papszOptions)
+void OGRFeature::FillUnsetWithDefault( int bNotNullableOnly,
+                                       CPL_UNUSED char** papszOptions)
 {
     int nFieldCount = poDefn->GetFieldCount();
     for(int i = 0; i < nFieldCount; i ++ )
@@ -5678,7 +5680,7 @@ void OGRFeature::FillUnsetWithDefault(int bNotNullableOnly,
 
 void OGR_F_FillUnsetWithDefault( OGRFeatureH hFeat,
                                  int bNotNullableOnly,
-                                 char** papszOptions)
+                                 char** papszOptions )
 
 {
     VALIDATE_POINTER0( hFeat, "OGR_F_FillUnsetWithDefault" );
@@ -5714,7 +5716,7 @@ void OGR_F_FillUnsetWithDefault( OGRFeatureH hFeat,
 int OGRFeature::Validate( int nValidateFlags, int bEmitError )
 
 {
-    int bRet = TRUE;
+    bool bRet = true;
 
     int nGeomFieldCount = poDefn->GetGeomFieldCount();
     for( int i = 0; i < nGeomFieldCount; i++ )
@@ -5723,7 +5725,7 @@ int OGRFeature::Validate( int nValidateFlags, int bEmitError )
             !poDefn->GetGeomFieldDefn(i)->IsNullable() &&
             GetGeomFieldRef(i) == NULL )
         {
-            bRet = FALSE;
+            bRet = false;
             if( bEmitError )
             {
                 CPLError(CE_Failure, CPLE_AppDefined,
@@ -5748,7 +5750,7 @@ int OGRFeature::Validate( int nValidateFlags, int bEmitError )
                 else if( (eType == wkbSetZ(wkbUnknown) && !wkbHasZ(eFType)) ||
                          (eType != wkbSetZ(wkbUnknown) && eFType != eType) )
                 {
-                    bRet = FALSE;
+                    bRet = false;
                     if( bEmitError )
                     {
                         CPLError(CE_Failure, CPLE_AppDefined,
@@ -5770,7 +5772,7 @@ int OGRFeature::Validate( int nValidateFlags, int bEmitError )
             (!(nValidateFlags & OGR_F_VAL_ALLOW_NULL_WHEN_DEFAULT) ||
                poDefn->GetFieldDefn(i)->GetDefault() == NULL))
         {
-            bRet = FALSE;
+            bRet = false;
             if( bEmitError )
             {
                 CPLError(CE_Failure, CPLE_AppDefined,
@@ -5785,7 +5787,7 @@ int OGRFeature::Validate( int nValidateFlags, int bEmitError )
             CPLIsUTF8(GetFieldAsString(i), -1) &&
             CPLStrlenUTF8(GetFieldAsString(i)) > poDefn->GetFieldDefn(i)->GetWidth())
         {
-            bRet = FALSE;
+            bRet = false;
             if( bEmitError )
             {
                 CPLError(CE_Failure, CPLE_AppDefined,

gdal/ogr/ogrfeaturequery.cpp
@@ -255,11 +255,11 @@ int OGRFeatureQuery::Evaluate( OGRFeature *poFeature )
     if( poResult == NULL )
         return FALSE;
 
-    int bLogicalResult = FALSE;
+    bool bLogicalResult = false;
     if( poResult->field_type == SWQ_INTEGER ||
         poResult->field_type == SWQ_INTEGER64 ||
         poResult->field_type == SWQ_BOOLEAN )
-        bLogicalResult = (int)poResult->int_value;
+        bLogicalResult = CPL_TO_BOOL(poResult->int_value);
 
     delete poResult;
 

gdal/ogr/ogrgeometrycollection.cpp
@@ -617,7 +617,7 @@ OGRErr  OGRGeometryCollection::exportToWkb( OGRwkbByteOrder eByteOrder,
         nGType = getIsoGeometryType();
     else if( eWkbVariant == wkbVariantPostGIS1 )
     {
-        int bIs3D = wkbHasZ((OGRwkbGeometryType)nGType);
+        const bool bIs3D = wkbHasZ(static_cast<OGRwkbGeometryType>(nGType));
         nGType = wkbFlatten(nGType);
         if( nGType == wkbMultiCurve )
             nGType = POSTGIS15_MULTICURVE;
@@ -688,9 +688,10 @@ OGRErr OGRGeometryCollection::importFromWktInternal( char ** ppszInput, int nRec
         return OGRERR_CORRUPT_DATA;
     }
 
-    int bHasZ = FALSE, bHasM = FALSE;
+    int bHasZ = FALSE;
+    int bHasM = FALSE;
     bool bIsEmpty = false;
-    OGRErr      eErr = importPreambuleFromWkt(ppszInput, &bHasZ, &bHasM, &bIsEmpty);
+    OGRErr eErr = importPreambuleFromWkt(ppszInput, &bHasZ, &bHasM, &bIsEmpty);
     if( eErr != OGRERR_NONE )
         return eErr;
     if( bHasZ ) flags |= OGR_G_3D;
@@ -1235,7 +1236,8 @@ OGRBoolean OGRGeometryCollection::isCompatibleSubType(
 /*                         hasCurveGeometry()                           */
 /************************************************************************/
 
-OGRBoolean OGRGeometryCollection::hasCurveGeometry(int bLookForNonLinear) const
+OGRBoolean OGRGeometryCollection::hasCurveGeometry(
+    int bLookForNonLinear ) const
 {
     for( int iGeom = 0; iGeom < nGeomCount; iGeom++ )
     {

gdal/ogr/ogrgeometryfactory.cpp
@@ -1231,7 +1231,6 @@ OGRGeometry* OGRGeometryFactory::organizePolygons( OGRGeometry **papoPolygons,
                                                    int *pbIsValidGeometry,
                                                    const char** papszOptions )
 {
-    int bUseFastVersion;
     int i, j;
     OGRGeometry* geom = NULL;
     OrganizePolygonMethod method = METHOD_NORMAL;
@@ -1251,6 +1250,7 @@ OGRGeometry* OGRGeometryFactory::organizePolygons( OGRGeometry **papoPolygons,
         return geom;
     }
 
+    bool bUseFastVersion = TRUE;
     if( CPLTestBool(CPLGetConfigOption("OGR_DEBUG_ORGANIZE_POLYGONS",
                                        "NO")) )
     {
@@ -1260,16 +1260,15 @@ OGRGeometry* OGRGeometryFactory::organizePolygons( OGRGeometry **papoPolygons,
         static int firstTime = 1;
         if (!haveGEOS() && firstTime)
         {
-            CPLDebug("OGR",
-                    "In OGR_DEBUG_ORGANIZE_POLYGONS mode, GDAL should be built with GEOS support enabled in order "
-                    "OGRGeometryFactory::organizePolygons to provide reliable results on complex polygons.");
+            CPLDebug(
+                "OGR",
+                "In OGR_DEBUG_ORGANIZE_POLYGONS mode, GDAL should be built "
+                "with GEOS support enabled in order "
+                "OGRGeometryFactory::organizePolygons to provide reliable "
+                "results on complex polygons.");
             firstTime = 0;
         }
         bUseFastVersion = !haveGEOS();
-    }
-    else
-    {
-        bUseFastVersion = TRUE;
     }
 
 /* -------------------------------------------------------------------- */
@@ -2267,7 +2266,7 @@ static void SplitLineStringAtDateline(OGRGeometryCollection* poMulti,
     double dfDiffSpace = 360 - dfDateLineOffset;
 
     int i;
-    int bIs3D = poLS->getCoordinateDimension() == 3;
+    const bool bIs3D = poLS->getCoordinateDimension() == 3;
     OGRLineString* poNewLS = new OGRLineString();
     poMulti->addGeometryDirectly(poNewLS);
     for(i=0;i<poLS->getNumPoints();i++)
@@ -2372,7 +2371,7 @@ static void FixPolygonCoordinatesAtDateLine(OGRPolygon* poPoly, double dfDateLin
         OGRLineString* poLS = (iPart == 0) ? poPoly->getExteriorRing() :
                                              poPoly->getInteriorRing(iPart-1);
         bool bGoEast = false;
-        int bIs3D = poLS->getCoordinateDimension() == 3;
+        const bool bIs3D = poLS->getCoordinateDimension() == 3;
         for(i=1;i<poLS->getNumPoints();i++)
         {
             double dfX = poLS->getX(i);
@@ -3103,7 +3102,7 @@ OGRGeometry * OGRGeometryFactory::forceTo( OGRGeometry* poGeom,
         return poRet;
     }
 
-    int bIsCurve = OGR_GT_IsCurve(eType);
+    const bool bIsCurve = CPL_TO_BOOL(OGR_GT_IsCurve(eType));
     if( bIsCurve && eTargetType == wkbCompoundCurve )
     {
         return OGRCurve::CastToCompoundCurve((OGRCurve*)poGeom);
@@ -3598,7 +3597,7 @@ OGRLineString* OGRGeometryFactory::curveToLineString(
         poLine->addPoint(x0, y0);
 
     bool bAddIntermediatePoint = false;
-    int bStealth = TRUE;
+    bool bStealth = true;
     for(const char* const* papszIter = papszOptions; papszIter && *papszIter; papszIter++)
     {
         char* pszKey = NULL;
@@ -3608,12 +3607,12 @@ OGRLineString* OGRGeometryFactory::curveToLineString(
             if( EQUAL(pszValue, "YES") || EQUAL(pszValue, "TRUE") || EQUAL(pszValue, "ON") )
             {
                 bAddIntermediatePoint = true;
-                bStealth = FALSE;
+                bStealth = false;
             }
             else if( EQUAL(pszValue, "NO") || EQUAL(pszValue, "FALSE") || EQUAL(pszValue, "OFF") )
             {
                 bAddIntermediatePoint = false;
-                bStealth = FALSE;
+                bStealth = false;
             }
             else if( EQUAL(pszValue, "STEALTH") )
             {
@@ -3762,7 +3761,7 @@ static int OGRGF_DetectArc(const OGRLineString* poLS, int i,
         (OGRGF_GetHiddenValue(p2.getX(), p2.getY()) << HIDDEN_ALPHA_HALF_WIDTH);
     bool bFoundFFFFFFFFPattern = false;
     bool bFoundReversedAlphaRatioRef = false;
-    int bValidAlphaRatio = (nAlphaRatioRef > 0 && nAlphaRatioRef < 0xFFFFFFFF);
+    bool bValidAlphaRatio = nAlphaRatioRef > 0 && nAlphaRatioRef < 0xFFFFFFFF;
     int nCountValidAlphaRatio = 1;
 
     double dfScale = MAX(1, R_1);
@@ -3943,7 +3942,7 @@ static int OGRGF_DetectArc(const OGRLineString* poLS, int i,
                         "values across arc. Don't use it\n",
                         j, nAlphaRatioReversed);
 #endif
-                bValidAlphaRatio = FALSE;
+                bValidAlphaRatio = false;
             }
         }
 
@@ -3983,11 +3982,13 @@ static int OGRGF_DetectArc(const OGRLineString* poLS, int i,
     if( j < i + 3 )
         return -1;
 
-    bValidAlphaRatio &= (bFoundFFFFFFFFPattern && bFoundReversedAlphaRatioRef );
+    bValidAlphaRatio &= bFoundFFFFFFFFPattern && bFoundReversedAlphaRatioRef;
 
 #ifdef VERBOSE_DEBUG_CURVEFROMLINESTRING
-    printf("bValidAlphaRatio=%d bFoundFFFFFFFFPattern=%d, bFoundReversedAlphaRatioRef=%d\n",
-            bValidAlphaRatio, bFoundFFFFFFFFPattern, bFoundReversedAlphaRatioRef);
+    printf("bValidAlphaRatio=%d bFoundFFFFFFFFPattern=%d, "
+           "bFoundReversedAlphaRatioRef=%d\n",
+           static_cast<int>(bValidAlphaRatio),
+           bFoundFFFFFFFFPattern, bFoundReversedAlphaRatioRef);
     printf("alpha0_1=%f dfLastValidAlpha=%f\n",
             alpha0_1, dfLastValidAlpha);
 #endif
@@ -4079,10 +4080,11 @@ static int OGRGF_DetectArc(const OGRLineString* poLS, int i,
         /* A few rounding strategies in case the mid point was at "exact" coordinates */
         if( R_1 > 1e-5 )
         {
-            int bStartEndInteger = ( IS_ALMOST_INTEGER(p0.getX()) &&
-                                     IS_ALMOST_INTEGER(p0.getY()) &&
-                                     IS_ALMOST_INTEGER(poFinalPoint->getX()) &&
-                                     IS_ALMOST_INTEGER(poFinalPoint->getY()) );
+            const bool bStartEndInteger =
+                IS_ALMOST_INTEGER(p0.getX()) &&
+                IS_ALMOST_INTEGER(p0.getY()) &&
+                IS_ALMOST_INTEGER(poFinalPoint->getX()) &&
+                IS_ALMOST_INTEGER(poFinalPoint->getY());
             if( bStartEndInteger &&
                 fabs(dfXMid - floor(dfXMid+0.5)) / dfScale < 1e-4 &&
                 fabs(dfYMid - floor(dfYMid+0.5)) / dfScale < 1e-4 )

commit 26ba6e080816cb30e015fbc8cb351c91499ada47

Fix OGRGeometry::Distance in the case where hThis or hOther is a nullptr.
I broke this case with r35119.


git-svn-id: https://svn.osgeo.org/gdal/trunk@36548 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/ogr/ogrgeometry.cpp
@@ -3026,15 +3026,12 @@ double OGRGeometry::Distance( const OGRGeometry *poOtherGeom ) const
 
 #else
 
-    // GEOSGeom is a pointer
-    GEOSGeom hThis = NULL;
-    GEOSGeom hOther = NULL;
-
     GEOSContextHandle_t hGEOSCtxt = createGEOSContext();
-    hOther = poOtherGeom->exportToGEOS(hGEOSCtxt);
-    hThis = exportToGEOS(hGEOSCtxt);
+    // GEOSGeom is a pointer.
+    GEOSGeom hOther = poOtherGeom->exportToGEOS(hGEOSCtxt);
+    GEOSGeom hThis = exportToGEOS(hGEOSCtxt);
 
-    bool bIsErr = false;
+    bool bIsErr = true;
     double dfDistance = 0.0;
 
     if( hThis != NULL && hOther != NULL )