a87788514cf9f323d6a894e350bc4ff58edef4cb
Even Rouault <even.rouault@mines-paris.org>
2016-12-01 20:01:05 +0000

    ci/travis/trusty_clang/before_install.sh: remove explicit installation of openjdk-7-jdk, since an update to it in latest ubuntu update repo seems to break with jdk_switcher and Oracle Java 8
    git-svn-id: https://svn.osgeo.org/gdal/trunk@36605 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ci/travis/trusty_clang/before_install.sh
+++ gdal/ci/travis/trusty_clang/before_install.sh
@@ -12,7 +12,7 @@ sudo add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
 sudo add-apt-repository -y ppa:marlam/gta
 sudo apt-get update
 # postgis postgresql-9.5 postgresql-client-9.5 postgresql-9.5-postgis-2.2 libpq-dev
-sudo apt-get install -y ccache python-numpy libpng12-dev libjpeg-dev libgif-dev liblzma-dev libgeos-dev libcurl4-gnutls-dev libproj-dev libxml2-dev libexpat-dev libxerces-c-dev libnetcdf-dev netcdf-bin libpoppler-dev libspatialite-dev gpsbabel swig libhdf4-alt-dev libhdf5-serial-dev libpodofo-dev poppler-utils libfreexl-dev unixodbc-dev libwebp-dev openjdk-7-jdk libepsilon-dev libgta-dev liblcms2-2 libpcre3-dev mercurial cmake libcrypto++-dev
+sudo apt-get install -y ccache python-numpy libpng12-dev libjpeg-dev libgif-dev liblzma-dev libgeos-dev libcurl4-gnutls-dev libproj-dev libxml2-dev libexpat-dev libxerces-c-dev libnetcdf-dev netcdf-bin libpoppler-dev libspatialite-dev gpsbabel swig libhdf4-alt-dev libhdf5-serial-dev libpodofo-dev poppler-utils libfreexl-dev unixodbc-dev libwebp-dev  libepsilon-dev libgta-dev liblcms2-2 libpcre3-dev mercurial cmake libcrypto++-dev
 sudo apt-get install -y python-lxml
 sudo apt-get install -y python-pip
 sudo apt-get install -y libqhull-dev
