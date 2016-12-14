743ffa32faae310715ac9b2c6924e6f70957f331
Kurt Schwehr <schwehr@gmail.com>
2016-12-14 13:51:37 +0000

    Fix formatting and /* */ -> // in cpl_base64.cpp.
    git-svn-id: https://svn.osgeo.org/gdal/trunk@36854 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/port/cpl_base64.cpp
+++ gdal/port/cpl_base64.cpp
@@ -42,31 +42,31 @@
 
 CPL_CVSID("$Id$");
 
-/* Derived from MapServer's mappostgis.c */
+// Derived from MapServer's mappostgis.c.
 
 /*
 ** Decode a base64 character.
 */
 static const unsigned char CPLBase64DecodeChar[256] = {
-    /* not Base64 characters */
+    // Not Base64 characters.
     64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,
     64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,
     64,64,64,64,64,64,64,64,64,64,64,
-    /*  +  */
+    // +
     62,
-    /* not Base64 characters */
+    // Not Base64 characters.
     64,64,64,
-    /*  /  */
+    //  /
     63,
-    /* 0-9 */
+    // 0-9
     52,53,54,55,56,57,58,59,60,61,
-    /* not Base64 characters */
+    // Not Base64 characters.
     64,64,64,64,64,64,64,
-    /* A-Z */
+    // A-Z
     0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,
-    /* not Base64 characters */
+    // Not Base64 characters.
     64,64,64,64,64,64,
-    /* a-z */
+    // a-z
     26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,
     51,
     // Not Base64 characters.
@@ -196,7 +196,8 @@ int CPLBase64DecodeInPlace( GByte* pszBase64 )
 
 /** Base64 encode a buffer. */
 
-char *CPLBase64Encode(int nDataLen, const GByte *pabyBytesToEncode) {
+char *CPLBase64Encode(int nDataLen, const GByte *pabyBytesToEncode)
+{
     static const char base64Chars[] =
         "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
