501283409886027304804613fa5d53de70ec8f36
Even Rouault <even.rouault@mines-paris.org>
2016-12-15 02:32:48 +0000

    FreeBSD 11 compilation fix
    git-svn-id: https://svn.osgeo.org/gdal/trunk@36875 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/cad/libopencad/dwg/r2000.cpp
+++ gdal/ogr/ogrsf_frmts/cad/libopencad/dwg/r2000.cpp
@@ -40,8 +40,8 @@
 #include <memory>
 #include <string>
 
-#if defined(__sun__) && __GNUC__ == 4 && __GNUC_MINOR__ == 8
-// gcc 4.8 on Solaris 11.3 doesn't have std::string
+#if (defined(__sun__) || defined(__FreeBSD__)) && __GNUC__ == 4 && __GNUC_MINOR__ == 8
+// gcc 4.8 on Solaris 11.3 or FreeBSD 11 doesn't have std::string
 #include <sstream>
 template <typename T> std::string to_string(T val)
 {
