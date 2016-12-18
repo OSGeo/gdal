3c7dfd6f433741f2f38d2bd35a52a609365945fe
Even Rouault <even.rouault@mines-paris.org>
2016-12-18 13:31:33 +0000

    Use CPL_SWAP32/64 to implement CPL_SWAP32/64PTR on Intel arch in default non-sanitization mode (fixes https://trac.osgeo.org/gdal/ticket/6746)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@36947 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/port/cpl_port.h
+++ gdal/port/cpl_port.h
@@ -184,7 +184,7 @@
 #    define HAVE_CXX11 1
 #  endif
 /* TODO(schwehr): What are the correct tests for C++ 14 and 17? */
-#endif  /* __cpluscplus */
+#endif  /* __cplusplus */
 
 /*---------------------------------------------------------------------
  *        types for 16 and 32 bits integers, etc...
@@ -793,6 +793,8 @@ template<> struct CPLStaticAssert<true>
     _pabyDataT[1] = byTemp;                                       \
 }
 
+#if defined(MAKE_SANITIZE_HAPPY) || !(defined(__i386__) || defined(__x86_64__) || defined(_M_IX86) || defined(_M_X64))
+
 /** Byte-swap a 32 bit pointer */
 #define CPL_SWAP32PTR(x) \
 {                                                                 \
@@ -826,6 +828,26 @@ template<> struct CPLStaticAssert<true>
     _pabyDataT[3] = _pabyDataT[4];                                \
     _pabyDataT[4] = byTemp;                                       \
 }
+
+#else
+
+/** Byte-swap a 32 bit pointer */
+#define CPL_SWAP32PTR(x) \
+{                                                                           \
+    GUInt32 *_pn32ptr = (GUInt32 *) (x);                                    \
+    CPL_STATIC_ASSERT_IF_AVAILABLE(sizeof(*(x)) == 1 || sizeof(*(x)) == 4); \
+    *_pn32ptr = CPL_SWAP32(*_pn32ptr);                                      \
+}
+
+/** Byte-swap a 64 bit pointer */
+#define CPL_SWAP64PTR(x) \
+{                                                                           \
+    GUInt64 *_pn64ptr = (GUInt64 *) (x);                                    \
+    CPL_STATIC_ASSERT_IF_AVAILABLE(sizeof(*(x)) == 1 || sizeof(*(x)) == 8); \
+    *_pn64ptr = CPL_SWAP64(*_pn64ptr);                                      \
+}
+
+#endif
 
 /** Byte-swap a 64 bit pointer */
 #define CPL_SWAPDOUBLE(p) CPL_SWAP64PTR(p)
