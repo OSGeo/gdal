OSGeo/gdal

commit 8c0633aedc3f8826c173fd57fba3bf587326c78b

Add spy to see which configuration options are get or set (uncomment //#define DEBUG_CONFIG_OPTIONS in file to enable)
git-svn-id: https://svn.osgeo.org/gdal/trunk@22201 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/port/cpl_conv.cpp
@@ -1391,6 +1391,69 @@ void CPLVerifyConfiguration()
                   "CPLVerifyConfiguration(): byte order set wrong.\n" );
 }
 
+/* Uncomment to get list of options that have been fetched and set */
+//#define DEBUG_CONFIG_OPTIONS
+
+#ifdef DEBUG_CONFIG_OPTIONS
+
+#include <set>
+#include "cpl_multiproc.h"
+
+static void* hRegisterConfigurationOptionMutex = 0;
+static std::set<CPLString>* paoGetKeys = NULL;
+static std::set<CPLString>* paoSetKeys = NULL;
+
+/************************************************************************/
+/*                      CPLShowAccessedOptions()                        */
+/************************************************************************/
+
+static void CPLShowAccessedOptions()
+{
+    std::set<CPLString>::iterator aoIter;
+
+    printf("Configuration options accessed in reading : "),
+    aoIter = paoGetKeys->begin();
+    while(aoIter != paoGetKeys->end())
+    {
+        printf("%s, ", (*aoIter).c_str());
+        aoIter ++;
+    }
+    printf("\n");
+
+    printf("Configuration options accessed in writing : "),
+    aoIter = paoSetKeys->begin();
+    while(aoIter != paoSetKeys->end())
+    {
+        printf("%s, ", (*aoIter).c_str());
+        aoIter ++;
+    }
+    printf("\n");
+
+    delete paoGetKeys;
+    delete paoSetKeys;
+    paoGetKeys = paoSetKeys = NULL;
+}
+
+/************************************************************************/
+/*                       CPLAccessConfigOption()                        */
+/************************************************************************/
+
+static void CPLAccessConfigOption(const char* pszKey, int bGet)
+{
+    CPLMutexHolderD(&hRegisterConfigurationOptionMutex);
+    if (paoGetKeys == NULL)
+    {
+        paoGetKeys = new std::set<CPLString>;
+        paoSetKeys = new std::set<CPLString>;
+        atexit(CPLShowAccessedOptions);
+    }
+    if (bGet)
+        paoGetKeys->insert(pszKey);
+    else
+        paoSetKeys->insert(pszKey);
+}
+#endif
+
 /************************************************************************/
 /*                         CPLGetConfigOption()                         */
 /************************************************************************/
@@ -1412,6 +1475,10 @@ const char * CPL_STDCALL
 CPLGetConfigOption( const char *pszKey, const char *pszDefault )
 
 {
+#ifdef DEBUG_CONFIG_OPTIONS
+    CPLAccessConfigOption(pszKey, TRUE);
+#endif
+
     const char *pszResult = NULL;
 
     char **papszTLConfigOptions = (char **) CPLGetTLS( CTLS_CONFIGOPTIONS );
@@ -1466,6 +1533,9 @@ void CPL_STDCALL
 CPLSetConfigOption( const char *pszKey, const char *pszValue )
 
 {
+#ifdef DEBUG_CONFIG_OPTIONS
+    CPLAccessConfigOption(pszKey, FALSE);
+#endif
     CPLMutexHolderD( &hConfigMutex );
 
     papszConfigOptions = (volatile char **) 
@@ -1494,6 +1564,10 @@ void CPL_STDCALL
 CPLSetThreadLocalConfigOption( const char *pszKey, const char *pszValue )
 
 {
+#ifdef DEBUG_CONFIG_OPTIONS
+    CPLAccessConfigOption(pszKey, FALSE);
+#endif
+
     char **papszTLConfigOptions = (char **) CPLGetTLS( CTLS_CONFIGOPTIONS );
 
     papszTLConfigOptions = 

commit 1ab43e0ca8733970f356854b0aa439b65f7c2eca

, -> ; reverting typo in cpl_conv.cpp:CPLShowAccessedOptions() introduced in r22201
git-svn-id: https://svn.osgeo.org/gdal/trunk@37218 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/port/cpl_conv.cpp
@@ -1540,7 +1540,7 @@ static void CPLShowAccessedOptions()
 {
     std::set<CPLString>::iterator aoIter;
 
-    printf("Configuration options accessed in reading : "), /*ok*/
+    printf("Configuration options accessed in reading : "); /*ok*/
     aoIter = paoGetKeys->begin();
     while( aoIter != paoGetKeys->end() )
     {