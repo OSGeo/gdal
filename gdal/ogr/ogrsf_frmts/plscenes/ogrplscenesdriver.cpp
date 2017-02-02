4b11e5bbff502885fa929715cd98479fa1297068
Even Rouault <even.rouault@mines-paris.org>
2017-02-02 13:34:26 +0000

    PLScenes: add support for Data V1 API
    git-svn-id: https://svn.osgeo.org/gdal/trunk@37271 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/plscenes/ogrplscenesdriver.cpp
+++ gdal/ogr/ogrsf_frmts/plscenes/ogrplscenesdriver.cpp
@@ -53,10 +53,12 @@ static GDALDataset* OGRPLScenesOpen(GDALOpenInfo* poOpenInfo)
     CPLString osVersion = CSLFetchNameValueDef(papszOptions, "version",
                 CSLFetchNameValueDef(poOpenInfo->papszOpenOptions, "VERSION", ""));
     CSLDestroy(papszOptions);
-    if( EQUAL(osVersion, "v0") || EQUAL(osVersion, "") )
+    if( EQUAL(osVersion, "v0"))
         return OGRPLScenesDataset::Open(poOpenInfo);
     if( EQUAL(osVersion, "v1") )
         return OGRPLScenesV1Dataset::Open(poOpenInfo);
+    if( EQUAL(osVersion, "data_v1") || EQUAL(osVersion, "")  )
+        return OGRPLScenesDataV1Dataset::Open(poOpenInfo);
     CPLError(CE_Failure, CPLE_AppDefined,
              "Unhandled API version: %s", osVersion.c_str());
     return NULL;
@@ -82,18 +84,19 @@ void RegisterOGRPLSCENES()
     poDriver->SetMetadataItem( GDAL_DMD_CONNECTION_PREFIX, "PLSCENES:" );
     poDriver->SetMetadataItem( GDAL_DMD_OPENOPTIONLIST,
 "<OpenOptionList>"
-"  <Option name='VERSION' type='string-select' description='API version' default='V0'>"
+"  <Option name='VERSION' type='string-select' description='API version' default='DATA_V1'>"
 "    <Value>V0</Value>"
 "    <Value>V1</Value>"
+"    <Value>DATA_V1</Value>"
 "  </Option>"
 "  <Option name='API_KEY' type='string' description='Account API key' required='true'/>"
-"  <Option name='FOLLOW_LINKS' type='boolean' description='Whether assets links should be followed for each scene (API v1 only)' default='NO'/>"
+"  <Option name='FOLLOW_LINKS' type='boolean' description='Whether assets links should be followed for each scene (API v1 / data v1 only)' default='NO'/>"
 "  <Option name='SCENE' type='string' description='Scene id (for raster fetching)'/>"
-"  <Option name='CATALOG' type='string' description='Catalog id (API v1 only, mandatory for raster fetching)'/>"
-"  <Option name='PRODUCT_TYPE' type='string' description='Product type/asset category. For v0 API: visual, analytic or thumb (for raster fetching)' default='visual'/>"
+"  <Option name='ITEMTYPES' alias='CATALOG' type='string' description='Catalog id (API v1 / data v1 only, mandatory for raster fetching)'/>"
+"  <Option name='ASSET' alias='PRODUCT_TYPE' type='string' description='Asset category/Product type. For v0 API: visual, analytic or thumb (for raster fetching)' default='visual'/>"
 "  <Option name='RANDOM_ACCESS' type='boolean' description='Whether raster should be accessed in random access mode (but with potentially not optimal throughput). If no, in-memory ingestion is done' default='YES'/>"
-"  <Option name='ACTIVATION_TIMEOUT' type='int' description='Number of seconds during which to wait for asset activation (API v1 only, raster)' default='3600'/>"
-"  <Option name='FILTER' type='string' description='Custom filter (API v1 only)'/>"
+"  <Option name='ACTIVATION_TIMEOUT' type='int' description='Number of seconds during which to wait for asset activation (API v1 / data v1 only, raster)' default='3600'/>"
+"  <Option name='FILTER' type='string' description='Custom filter (API v1 / data v1 only)'/>"
 "</OpenOptionList>");
 
     poDriver->pfnOpen = OGRPLScenesOpen;
