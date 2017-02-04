cd001d37cc73c893e2828c9fc42d4ac02e8f93ea
Kurt Schwehr <schwehr@gmail.com>
2017-02-04 14:40:57 +0000

    Clean rcreatecopy.cpp.
    - IWYU
    - Partial clang-format
    - /* */ -> //
    - C casts -> C++ casts
    - Add const
    
    
    git-svn-id: https://svn.osgeo.org/gdal/trunk@37294 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/r/rcreatecopy.cpp
+++ gdal/frmts/r/rcreatecopy.cpp
@@ -26,8 +26,19 @@
  * DEALINGS IN THE SOFTWARE.
  ****************************************************************************/
 
+#include <cstdio>
+#include <cstring>
+#include <string>
+
+#include "cpl_conv.h"
+#include "cpl_error.h"
+#include "cpl_port.h"
+#include "cpl_progress.h"
 #include "cpl_string.h"
+#include "cpl_vsi.h"
+#include "gdal.h"
 #include "gdal_pam.h"
+#include "gdal_priv.h"
 
 CPL_CVSID("$Id$");
 
@@ -52,13 +63,13 @@ static void RWriteInteger( VSILFILE *fp, int bASCII, int nValue )
     if( bASCII )
     {
         char szOutput[50] = { '\0' };
-        snprintf( szOutput, sizeof(szOutput), "%d\n", nValue );
-        VSIFWriteL( szOutput, 1, strlen(szOutput), fp );
+        snprintf(szOutput, sizeof(szOutput), "%d\n", nValue);
+        VSIFWriteL(szOutput, 1, strlen(szOutput), fp);
     }
     else
     {
-        CPL_MSBPTR32( &nValue );
-        VSIFWriteL( &nValue, 4, 1, fp );
+        CPL_MSBPTR32(&nValue);
+        VSIFWriteL(&nValue, 4, 1, fp);
     }
 }
 
@@ -69,17 +80,17 @@ static void RWriteInteger( VSILFILE *fp, int bASCII, int nValue )
 static void RWriteString( VSILFILE *fp, int bASCII, const char *pszValue )
 
 {
-    RWriteInteger( fp, bASCII, 4105 );
-    RWriteInteger( fp, bASCII, (int) strlen(pszValue) );
+    RWriteInteger(fp, bASCII, 4105);
+    RWriteInteger(fp, bASCII, static_cast<int>(strlen(pszValue)));
 
     if( bASCII )
     {
-        VSIFWriteL( pszValue, 1, strlen(pszValue), fp );
-        VSIFWriteL( "\n", 1, 1, fp );
+        VSIFWriteL(pszValue, 1, strlen(pszValue), fp);
+        VSIFWriteL("\n", 1, 1, fp);
     }
     else
     {
-        VSIFWriteL( pszValue, 1, (int) strlen(pszValue), fp );
+        VSIFWriteL(pszValue, 1, static_cast<int>(strlen(pszValue)), fp);
     }
 }
 
@@ -98,162 +109,132 @@ RCreateCopy( const char * pszFilename,
     const int nBands = poSrcDS->GetRasterCount();
     const int nXSize = poSrcDS->GetRasterXSize();
     const int nYSize = poSrcDS->GetRasterYSize();
-    const bool bASCII = CPLFetchBool( papszOptions, "ASCII", false );
-    const bool bCompressed = CPLFetchBool( papszOptions, "COMPRESS", !bASCII );
+    const bool bASCII = CPLFetchBool(papszOptions, "ASCII", false);
+    const bool bCompressed = CPLFetchBool(papszOptions, "COMPRESS", !bASCII);
 
-/* -------------------------------------------------------------------- */
-/*      Some some rudimentary checks                                    */
-/* -------------------------------------------------------------------- */
+    // Some some rudimentary checks.
 
-/* -------------------------------------------------------------------- */
-/*      Setup the filename to actually use.  We prefix with             */
-/*      /vsigzip/ if we want compressed output.                         */
-/* -------------------------------------------------------------------- */
-    CPLString osAdjustedFilename;
+    // Setup the filename to actually use.  We prefix with
+    // /vsigzip/ if we want compressed output.
+    const CPLString osAdjustedFilename =
+        std::string(bCompressed ? "/vsigzip/" : "") + pszFilename;
 
-    if( bCompressed )
-        osAdjustedFilename = "/vsigzip/";
-
-    osAdjustedFilename += pszFilename;
-
-/* -------------------------------------------------------------------- */
-/*      Create the file.                                                */
-/* -------------------------------------------------------------------- */
-    VSILFILE *fp = VSIFOpenL( osAdjustedFilename, "wb" );
+    // Create the file.
+    VSILFILE *fp = VSIFOpenL(osAdjustedFilename, "wb");
     if( fp == NULL )
     {
-        CPLError( CE_Failure, CPLE_OpenFailed,
-                  "Unable to create file %s.",
-                  pszFilename );
+        CPLError(CE_Failure, CPLE_OpenFailed,
+                 "Unable to create file %s.",
+                 pszFilename);
         return NULL;
     }
 
-/* -------------------------------------------------------------------- */
-/*      Write header with version, etc.                                 */
-/* -------------------------------------------------------------------- */
+    // Write header with version, etc.
     if( bASCII )
     {
         const char *pszHeader = "RDA2\nA\n";
-        VSIFWriteL( pszHeader, 1, strlen(pszHeader), fp );
+        VSIFWriteL(pszHeader, 1, strlen(pszHeader), fp);
     }
     else
     {
         const char *pszHeader = "RDX2\nX\n";
-        VSIFWriteL( pszHeader, 1, strlen(pszHeader), fp );
+        VSIFWriteL(pszHeader, 1, strlen(pszHeader), fp);
     }
 
-    RWriteInteger( fp, bASCII, 2 );
-    RWriteInteger( fp, bASCII, 133377 );
-    RWriteInteger( fp, bASCII, 131840 );
+    RWriteInteger(fp, bASCII, 2);
+    RWriteInteger(fp, bASCII, 133377);
+    RWriteInteger(fp, bASCII, 131840);
 
-/* -------------------------------------------------------------------- */
-/*      Establish the primary pairlist with one component object.       */
-/* -------------------------------------------------------------------- */
-    RWriteInteger( fp, bASCII, 1026 );
-    RWriteInteger( fp, bASCII, 1 );
+    // Establish the primary pairlist with one component object.
+    RWriteInteger(fp, bASCII, 1026);
+    RWriteInteger(fp, bASCII, 1);
 
-/* -------------------------------------------------------------------- */
-/*      Write the object name.  Eventually we should derive this        */
-/*      from the filename, possible with override by a creation         */
-/*      option.                                                         */
-/* -------------------------------------------------------------------- */
-    RWriteString( fp, bASCII, "gg" );
+    // Write the object name.  Eventually we should derive this
+    // from the filename, possible with override by a creation option.
+    RWriteString(fp, bASCII, "gg");
 
-/* -------------------------------------------------------------------- */
-/*      For now we write the raster as a numeric array with             */
-/*      attributes (526).                                               */
-/* -------------------------------------------------------------------- */
-    RWriteInteger( fp, bASCII, 526 );
-    RWriteInteger( fp, bASCII, nXSize * nYSize * nBands );
+    // For now we write the raster as a numeric array with attributes (526).
+    RWriteInteger(fp, bASCII, 526);
+    RWriteInteger(fp, bASCII, nXSize * nYSize * nBands);
 
-/* -------------------------------------------------------------------- */
-/*      Write the raster data.                                          */
-/* -------------------------------------------------------------------- */
+    // Write the raster data.
     CPLErr eErr = CE_None;
 
     double *padfScanline =
-        static_cast<double *>( CPLMalloc( nXSize * sizeof(double) ) );
+        static_cast<double *>(CPLMalloc(nXSize * sizeof(double)));
 
     for( int iBand = 0; iBand < nBands; iBand++ )
     {
-        GDALRasterBand *poBand = poSrcDS->GetRasterBand( iBand+1 );
+        GDALRasterBand *poBand = poSrcDS->GetRasterBand(iBand + 1);
 
         for( int iLine = 0; iLine < nYSize && eErr == CE_None; iLine++ )
         {
             int iValue;
 
-            eErr = poBand->RasterIO( GF_Read, 0, iLine, nXSize, 1,
-                                     padfScanline, nXSize, 1, GDT_Float64,
-                                     sizeof(double), 0, NULL );
+            eErr = poBand->RasterIO(GF_Read, 0, iLine, nXSize, 1,
+                                    padfScanline, nXSize, 1, GDT_Float64,
+                                    sizeof(double), 0, NULL);
 
             if( bASCII )
             {
                 for( iValue = 0; iValue < nXSize; iValue++ )
                 {
                     char szValue[128] = { '\0' };
-                    CPLsnprintf( szValue, sizeof(szValue), "%.16g\n",
-                                 padfScanline[iValue] );
-                    VSIFWriteL( szValue, 1, strlen(szValue), fp );
+                    CPLsnprintf(szValue, sizeof(szValue), "%.16g\n",
+                                padfScanline[iValue]);
+                    VSIFWriteL(szValue, 1, strlen(szValue), fp);
                 }
             }
             else
             {
                 for( iValue = 0; iValue < nXSize; iValue++ )
-                    CPL_MSBPTR64( padfScanline + iValue );
+                    CPL_MSBPTR64(padfScanline + iValue);
 
-                VSIFWriteL( padfScanline, 8, nXSize, fp );
+                VSIFWriteL(padfScanline, 8, nXSize, fp);
             }
 
-            if( eErr == CE_None
-                && !pfnProgress( (iLine+1) / (double) nYSize,
-                                 NULL, pProgressData ) )
+            if( eErr == CE_None && 
+                !pfnProgress((iLine + 1) / static_cast<double>(nYSize),
+                             NULL, pProgressData) )
             {
                 eErr = CE_Failure;
-                CPLError( CE_Failure, CPLE_UserInterrupt,
-                          "User terminated CreateCopy()" );
+                CPLError(CE_Failure, CPLE_UserInterrupt,
+                         "User terminated CreateCopy()");
             }
         }
     }
 
-    CPLFree( padfScanline );
+    CPLFree(padfScanline);
 
-/* -------------------------------------------------------------------- */
-/*      Write out the dims attribute.                                   */
-/* -------------------------------------------------------------------- */
-    RWriteInteger( fp, bASCII, 1026 );
-    RWriteInteger( fp, bASCII, 1 );
+    // Write out the dims attribute.
+    RWriteInteger(fp, bASCII, 1026);
+    RWriteInteger(fp, bASCII, 1);
 
-    RWriteString( fp, bASCII, "dim" );
+    RWriteString(fp, bASCII, "dim");
 
-    RWriteInteger( fp, bASCII, 13 );
-    RWriteInteger( fp, bASCII, 3 );
-    RWriteInteger( fp, bASCII, nXSize );
-    RWriteInteger( fp, bASCII, nYSize );
-    RWriteInteger( fp, bASCII, nBands );
+    RWriteInteger(fp, bASCII, 13);
+    RWriteInteger(fp, bASCII, 3);
+    RWriteInteger(fp, bASCII, nXSize);
+    RWriteInteger(fp, bASCII, nYSize);
+    RWriteInteger(fp, bASCII, nBands);
 
-    RWriteInteger( fp, bASCII, 254 );
+    RWriteInteger(fp, bASCII, 254);
 
-/* -------------------------------------------------------------------- */
-/*      Terminate overall pairlist.                                     */
-/* -------------------------------------------------------------------- */
-    RWriteInteger( fp, bASCII, 254 );
+    // Terminate overall pairlist.
+    RWriteInteger(fp, bASCII, 254);
 
-/* -------------------------------------------------------------------- */
-/*      Cleanup.                                                        */
-/* -------------------------------------------------------------------- */
-    VSIFCloseL( fp );
+    // Cleanup.
+    VSIFCloseL(fp);
 
     if( eErr != CE_None )
         return NULL;
 
-/* -------------------------------------------------------------------- */
-/*      Re-open dataset, and copy any auxiliary pam information.         */
-/* -------------------------------------------------------------------- */
+    // Re-open dataset, and copy any auxiliary pam information.
     GDALPamDataset *poDS =
-        static_cast<GDALPamDataset *>( GDALOpen( pszFilename, GA_ReadOnly ) );
+        static_cast<GDALPamDataset *>(GDALOpen(pszFilename, GA_ReadOnly));
 
     if( poDS )
-        poDS->CloneInfo( poSrcDS, GCIF_PAM_DEFAULT );
+        poDS->CloneInfo(poSrcDS, GCIF_PAM_DEFAULT);
 
     return poDS;
 }
