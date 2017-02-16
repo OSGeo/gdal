b4858525430b4d925e98557b63ca8f8191725d27
Mateusz Loskot <mateusz@loskot.net>
2017-02-16 18:25:58 +0000

    Rename makegdal_gen.bat to generate_vcxproj.bat
    New name describes purpose of the script better.
    
    git-svn-id: https://svn.osgeo.org/gdal/trunk@37390 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/generate_vcxproj.bat
+++ gdal/generate_vcxproj.bat
@@ -3,7 +3,7 @@
 :: ****************************************************************************
 ::  $Id: $
 :: 
-::  Name:     makegdal_gen.bat
+::  Name:     generate_vcxproj.bat
 ::  Project:  GDAL 
 ::  Purpose:  Generate MS Visual C++ => 10.0 project files    
 ::  Author:   Ivan Lucena, [ivan lucena at outlook dot com]
@@ -97,16 +97,16 @@ goto :continue
 :usage
 ::  *********************
 
-echo Usage: makegdal_gen ^<Visual C++ version^> [32^|64] ^<^(*^) project file name^>
+echo Usage: generate_vcxproj ^<Visual C++ version^> [32^|64] ^<^(*^) project file name^>
 echo Parameters:
 echo    1 : Visual C++ version is not the same as Visual Studio version ^( =^> 10.0 ^)
 echo    2 : Windows platform 32 for Win32 and 64 for Win64
 echo    3 : Base file name, with no path and no extension ^(*^)
 echo Examples:
-echo    makegdal_gen 10.1 32 gdal_vs2010
-echo    makegdal_gen 11.0 64 gdal_vs2012
-echo    makegdal_gen 12.0 64 gdal_vs2013
-echo    makegdal_gen 14.0 64 gdal_vs2015
+echo    generate_vcxproj 10.1 32 gdal_vs2010
+echo    generate_vcxproj 11.0 64 gdal_vs2012
+echo    generate_vcxproj 12.0 64 gdal_vs2013
+echo    generate_vcxproj 14.0 64 gdal_vs2015
 
 goto :end
 
