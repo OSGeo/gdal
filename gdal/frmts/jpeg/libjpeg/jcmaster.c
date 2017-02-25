7b85e791e8fd8695f590d7eea9658aafb068235e
Even Rouault <even.rouault@mines-paris.org>
2017-02-25 18:20:12 +0000

    Internal libjpeg: fix gcc 7 warnings
    git-svn-id: https://svn.osgeo.org/gdal/trunk@37455 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/jpeg/libjpeg/jcmaster.c
+++ gdal/frmts/jpeg/libjpeg/jcmaster.c
@@ -446,6 +446,7 @@ prepare_for_pass (j_compress_ptr cinfo)
     master->pass_number++;
     /*FALLTHROUGH*/
 #endif
+    /*-fallthrough*/
   case output_pass:
     /* Do a data-output pass. */
     /* We need not repeat per-scan setup if prior optimization pass did it. */
