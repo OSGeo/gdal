d43dd2e5cf329671be5fce47203955af052bae71
Even Rouault <even.rouault@mines-paris.org>
2017-03-04 17:27:00 +0000

    cpl_vsil_abstract_archive.cpp: remove useless code
    git-svn-id: https://svn.osgeo.org/gdal/trunk@37597 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/port/cpl_vsil_abstract_archive.cpp
+++ gdal/port/cpl_vsil_abstract_archive.cpp
@@ -618,10 +618,6 @@ VSIArchiveFilesystemHandler::OpenArchiveFile( const char* archiveFilename,
         {
             CPLMutexHolder oHolder( &hMutex );
 
-            VSIStatBufL sStat;
-            if( VSIStatL(archiveFilename, &sStat) != 0 )
-                return NULL;
-
             if( oFileList.find(archiveFilename) == oFileList.end() )
             {
                 if( poReader->GotoFirstFile() == FALSE )
