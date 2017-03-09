5e087ce684ccb2486f5ca99db9362cd494ce5b00
Even Rouault <even.rouault@mines-paris.org>
2017-03-09 07:34:51 +0000

    Attempt at fixing failed ubuntu_1604 ogr_fgdb tests
    git-svn-id: https://svn.osgeo.org/gdal/trunk@37653 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ci/travis/ubuntu_1604/script.sh
+++ gdal/ci/travis/ubuntu_1604/script.sh
@@ -9,4 +9,9 @@ sudo chroot "$chroot" sh -c "cd $PWD/autotest/cpp && make quick_test"
 # Compile and test vsipreload
 sudo chroot "$chroot" sh -c "cd $PWD/autotest/cpp && make vsipreload.so"
 # Run all the Python autotests
+
+# Run ogr_fgdb test in isolation due to likely conflict with libxml2
+sudo chroot "$chroot" sh -c "cd $PWD/autotest/gdrivers && python ogr_fgdb.py && cd ../../.."
+rm autotest/gdrivers/ogr_fgdb.py
+
 sudo chroot "$chroot" sh -c "cd $PWD/autotest && python run_all.py"
