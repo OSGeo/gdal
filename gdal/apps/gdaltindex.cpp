75fedb3c624dc405f10ad1277c260a137e3e7c00
Even Rouault <even.rouault@mines-paris.org>
2017-03-30 11:41:05 +0000

    gdaltindex: emit warning in -src_srs_format WKT when WKT is too large
    git-svn-id: https://svn.osgeo.org/gdal/trunk@37859 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/apps/gdaltindex.cpp
+++ gdal/apps/gdaltindex.cpp
@@ -628,8 +628,16 @@ int main( int argc, char *argv[] )
             {
                 if( nMaxFieldSize == 0 ||
                     strlen(projectionRef) <= nMaxFieldSize )
+                {
                     OGR_F_SetFieldString( hFeature, i_SrcSRSName,
                                           projectionRef );
+                }
+                else
+                {
+                    fprintf(stderr,
+                            "Cannot write WKT for file %s as it is too long!\n",
+                            fileNameToWrite);
+                }
             }
             else if( eSrcSRSFormat == FORMAT_PROJ )
             {
