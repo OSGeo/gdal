2c0ac8c5a31996ed867f43837a6f8034631fa3c5
Kurt Schwehr <schwehr@gmail.com>
2017-03-30 21:31:08 +0000

    Split out the class definitions to hfadataset.h from hfadataset.cpp.
    Also:
    - IWYU
    - Add a const to proNu
    - Disable Identify in Open when FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
    
    git-svn-id: https://svn.osgeo.org/gdal/trunk@37868 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/hfa/hfadataset.cpp
+++ gdal/frmts/hfa/hfadataset.cpp
@@ -28,17 +28,39 @@
  * DEALINGS IN THE SOFTWARE.
  ****************************************************************************/
 
+#include "cpl_port.h"
+#include "hfadataset.h"
 #include "hfa_p.h"
 
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#if HAVE_FCNTL_H
+#  include <fcntl.h>
+#endif
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "cpl_conv.h"
+#include "cpl_error.h"
+#include "cpl_minixml.h"
+#include "cpl_progress.h"
+#include "cpl_string.h"
+#include "cpl_vsi.h"
+#include "gdal.h"
 #include "gdal_frmts.h"
 #include "gdal_pam.h"
+#include "gdal_priv.h"
 #include "gdal_rat.h"
+#include "hfa.h"
+#include "ogr_core.h"
 #include "ogr_spatialref.h"
 #include "ogr_srs_api.h"
-
-#include <cstdlib>
-#include <cmath>
-#include <algorithm>
 
 CPL_CVSID("$Id$");
 
@@ -244,274 +266,6 @@ static const int anUsgsEsriZones[] =
  5202, 6051,
  5300,    0,
  5400,    0
-};
-
-/************************************************************************/
-/* ==================================================================== */
-/*                              HFADataset                              */
-/* ==================================================================== */
-/************************************************************************/
-
-class HFARasterBand;
-
-class HFADataset CPL_FINAL : public GDALPamDataset
-{
-    friend class HFARasterBand;
-
-    HFAHandle   hHFA;
-
-    bool        bMetadataDirty;
-
-    bool        bGeoDirty;
-    double      adfGeoTransform[6];
-    char        *pszProjection;
-
-    bool        bIgnoreUTM;
-
-    CPLErr      ReadProjection();
-    CPLErr      WriteProjection();
-    bool        bForceToPEString;
-
-    int         nGCPCount;
-    GDAL_GCP    asGCPList[36];
-
-    void        UseXFormStack( int nStepCount,
-                               Efga_Polynomial *pasPolyListForward,
-                               Efga_Polynomial *pasPolyListReverse );
-
-  protected:
-    virtual CPLErr IRasterIO( GDALRWFlag, int, int, int, int,
-                              void *, int, int, GDALDataType,
-                              int, int *,
-                              GSpacing nPixelSpace, GSpacing nLineSpace,
-                              GSpacing nBandSpace,
-                              GDALRasterIOExtraArg* psExtraArg ) override;
-
-  public:
-             HFADataset();
-    virtual ~HFADataset();
-
-    static int          Identify( GDALOpenInfo * );
-    static CPLErr       Rename( const char *pszNewName,
-                                const char *pszOldName );
-    static CPLErr       CopyFiles( const char *pszNewName,
-                                   const char *pszOldName );
-    static GDALDataset *Open( GDALOpenInfo * );
-    static GDALDataset *Create( const char * pszFilename,
-                                int nXSize, int nYSize, int nBands,
-                                GDALDataType eType, char ** papszParmList );
-    static GDALDataset *CreateCopy( const char * pszFilename,
-                                    GDALDataset *poSrcDS,
-                                    int bStrict, char ** papszOptions,
-                                    GDALProgressFunc pfnProgress,
-                                    void * pProgressData );
-    static CPLErr       Delete( const char *pszFilename );
-
-    virtual char **GetFileList() override;
-
-    virtual const char *GetProjectionRef() override;
-    virtual CPLErr SetProjection( const char * ) override;
-
-    virtual CPLErr GetGeoTransform( double * ) override;
-    virtual CPLErr SetGeoTransform( double * ) override;
-
-    virtual int    GetGCPCount() override;
-    virtual const char *GetGCPProjection() override;
-    virtual const GDAL_GCP *GetGCPs() override;
-
-    virtual CPLErr SetMetadata( char **, const char * = "" ) override;
-    virtual CPLErr SetMetadataItem( const char *, const char *,
-                                    const char * = "" ) override;
-
-    virtual void   FlushCache() override;
-    virtual CPLErr IBuildOverviews( const char *pszResampling,
-                                    int nOverviews, int *panOverviewList,
-                                    int nListBands, int *panBandList,
-                                    GDALProgressFunc pfnProgress,
-                                    void * pProgressData ) override;
-};
-
-/************************************************************************/
-/* ==================================================================== */
-/*                            HFARasterBand                             */
-/* ==================================================================== */
-/************************************************************************/
-
-class HFARasterBand CPL_FINAL : public GDALPamRasterBand
-{
-    friend class HFADataset;
-    friend class HFARasterAttributeTable;
-
-    GDALColorTable *poCT;
-
-    EPTType     eHFADataType;
-
-    int         nOverviews;
-    int         nThisOverview;
-    HFARasterBand **papoOverviewBands;
-
-    CPLErr      CleanOverviews();
-
-    HFAHandle   hHFA;
-
-    bool        bMetadataDirty;
-
-    GDALRasterAttributeTable *poDefaultRAT;
-
-    void        ReadAuxMetadata();
-    void        ReadHistogramMetadata();
-    void        EstablishOverviews();
-    CPLErr      WriteNamedRAT( const char *pszName,
-                               const GDALRasterAttributeTable *poRAT );
-
-  public:
-                   HFARasterBand( HFADataset *, int, int );
-    virtual        ~HFARasterBand();
-
-    virtual CPLErr IReadBlock( int, int, void * ) override;
-    virtual CPLErr IWriteBlock( int, int, void * ) override;
-
-    virtual const char *GetDescription() const override;
-    virtual void        SetDescription( const char * ) override;
-
-    virtual GDALColorInterp GetColorInterpretation() override;
-    virtual GDALColorTable *GetColorTable() override;
-    virtual CPLErr          SetColorTable( GDALColorTable * ) override;
-    virtual int    GetOverviewCount() override;
-    virtual GDALRasterBand *GetOverview( int ) override;
-
-    virtual double GetMinimum( int *pbSuccess = NULL ) override;
-    virtual double GetMaximum( int *pbSuccess = NULL ) override;
-    virtual double GetNoDataValue( int *pbSuccess = NULL ) override;
-    virtual CPLErr SetNoDataValue( double dfValue ) override;
-
-    virtual CPLErr SetMetadata( char **, const char * = "" ) override;
-    virtual CPLErr SetMetadataItem( const char *, const char *,
-                                    const char * = "" ) override;
-    virtual CPLErr BuildOverviews( const char *, int, int *,
-                                   GDALProgressFunc, void * ) override;
-
-    virtual CPLErr GetDefaultHistogram( double *pdfMin, double *pdfMax,
-                                        int *pnBuckets,
-                                        GUIntBig ** ppanHistogram,
-                                        int bForce,
-                                        GDALProgressFunc, void *pProgressData ) override;
-
-    virtual GDALRasterAttributeTable *GetDefaultRAT() override;
-    virtual CPLErr SetDefaultRAT( const GDALRasterAttributeTable * ) override;
-};
-
-class HFAAttributeField
-{
-  public:
-    CPLString         sName;
-    GDALRATFieldType  eType;
-    GDALRATFieldUsage eUsage;
-    int               nDataOffset;
-    int               nElementSize;
-    HFAEntry         *poColumn;
-    bool              bIsBinValues;  // Handled differently.
-    bool              bConvertColors;  // Map 0-1 floats to 0-255 ints.
-};
-
-class HFARasterAttributeTable CPL_FINAL : public GDALRasterAttributeTable
-{
-  private:
-    HFAHandle   hHFA;
-    HFAEntry   *poDT;
-    CPLString   osName;
-    int         nBand;
-    GDALAccess  eAccess;
-
-    std::vector<HFAAttributeField> aoFields;
-    int         nRows;
-
-    bool bLinearBinning;
-    double dfRow0Min;
-    double dfBinSize;
-
-    CPLString osWorkingResult;
-
-    void AddColumn( const char *pszName, GDALRATFieldType eType,
-                    GDALRATFieldUsage eUsage,
-                    int nDataOffset, int nElementSize, HFAEntry *poColumn,
-                    bool bIsBinValues=false,
-                    bool bConvertColors=false )
-    {
-        HFAAttributeField aField;
-        aField.sName = pszName;
-        aField.eType = eType;
-        aField.eUsage = eUsage;
-        aField.nDataOffset = nDataOffset;
-        aField.nElementSize = nElementSize;
-        aField.poColumn = poColumn;
-        aField.bIsBinValues = bIsBinValues;
-        aField.bConvertColors = bConvertColors;
-
-        aoFields.push_back(aField);
-    }
-
-    void CreateDT()
-    {
-        poDT = HFAEntry::New( hHFA->papoBand[nBand-1]->psInfo,
-                              osName, "Edsc_Table",
-                              hHFA->papoBand[nBand-1]->poNode );
-        poDT->SetIntField( "numrows", nRows );
-    }
-
-  public:
-    HFARasterAttributeTable( HFARasterBand *poBand, const char *pszName );
-    virtual ~HFARasterAttributeTable();
-
-    GDALDefaultRasterAttributeTable *Clone() const override;
-
-    virtual int           GetColumnCount() const override;
-
-    virtual const char   *GetNameOfCol( int ) const override;
-    virtual GDALRATFieldUsage GetUsageOfCol( int ) const override;
-    virtual GDALRATFieldType GetTypeOfCol( int ) const override;
-
-    virtual int           GetColOfUsage( GDALRATFieldUsage ) const override;
-
-    virtual int           GetRowCount() const override;
-
-    virtual const char   *GetValueAsString( int iRow, int iField ) const override;
-    virtual int           GetValueAsInt( int iRow, int iField ) const override;
-    virtual double        GetValueAsDouble( int iRow, int iField ) const override;
-
-    virtual void          SetValue( int iRow, int iField,
-                                    const char *pszValue ) override;
-    virtual void          SetValue( int iRow, int iField, double dfValue ) override;
-    virtual void          SetValue( int iRow, int iField, int nValue ) override;
-
-    virtual CPLErr        ValuesIO( GDALRWFlag eRWFlag, int iField,
-                                    int iStartRow, int iLength,
-                                    double *pdfData ) override;
-    virtual CPLErr        ValuesIO( GDALRWFlag eRWFlag, int iField,
-                                    int iStartRow, int iLength, int *pnData ) override;
-    virtual CPLErr        ValuesIO( GDALRWFlag eRWFlag, int iField,
-                                    int iStartRow, int iLength,
-                                    char **papszStrList ) override;
-
-    virtual int           ChangesAreWrittenToFile() override;
-    virtual void          SetRowCount( int iCount ) override;
-
-    virtual int           GetRowOfValue( double dfValue ) const override;
-    virtual int           GetRowOfValue( int nValue ) const override;
-
-    virtual CPLErr        CreateColumn( const char *pszFieldName,
-                                        GDALRATFieldType eFieldType,
-                                        GDALRATFieldUsage eFieldUsage ) override;
-    virtual CPLErr        SetLinearBinning( double dfRow0Min,
-                                            double dfBinSize ) override;
-    virtual int           GetLinearBinning( double *pdfRow0Min,
-                                            double *pdfBinSize ) const override;
-
-    virtual CPLXMLNode   *Serialize() const override;
-
-protected:
-    CPLErr                ColorsIO( GDALRWFlag eRWFlag, int iField,
-                                    int iStartRow, int iLength, int *pnData );
 };
 
 /************************************************************************/
@@ -2749,6 +2503,7 @@ CPLErr HFARasterBand::IWriteBlock( int nBlockXOff, int nBlockYOff,
 /* -------------------------------------------------------------------- */
 /*      Do we need to pack 1/2/4 bit data?                              */
 /* -------------------------------------------------------------------- */
+    // TODO(schwehr): Make symbolic constants with explanations.
     if( eHFADataType == EPT_u1
         || eHFADataType == EPT_u2
         || eHFADataType == EPT_u4 )
@@ -4710,7 +4465,7 @@ HFAPCSStructToWKT( const Eprj_Datum *psDatum,
               pszUnits = pszUnitsName;
           else
               pszUnits = "meters";
-          int proNu = psPro->proNumber;
+          const int proNu = psPro->proNumber;
           if( oSRS.ImportFromESRIStatePlaneWKT(zoneCode, pszDatum,
                                                pszUnits, proNu) == OGRERR_NONE )
           {
@@ -5427,8 +5182,11 @@ GDALDataset *HFADataset::Open( GDALOpenInfo * poOpenInfo )
 /* -------------------------------------------------------------------- */
 /*      Verify that this is a HFA file.                                 */
 /* -------------------------------------------------------------------- */
+#ifndef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
+    // During fuzzing, do not use Identify to reject crazy content.
     if( !Identify( poOpenInfo ) )
         return NULL;
+#endif
 
 /* -------------------------------------------------------------------- */
 /*      Open the file.                                                  */
