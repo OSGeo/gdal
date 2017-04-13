d3dca09cdd7bf6f4ebcc9f17ae1d5616d0b144d2
Even Rouault <even.rouault@mines-paris.org>
2017-04-13 15:25:27 +0000

    GTX: add a SHIFT_ORIGIN_IN_MINUS_180_PLUS_180 open option
    git-svn-id: https://svn.osgeo.org/gdal/trunk@37977 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/raw/gtxdataset.cpp
+++ gdal/frmts/raw/gtxdataset.cpp
@@ -199,6 +199,15 @@ GDALDataset *GTXDataset::Open( GDALOpenInfo * poOpenInfo )
 
     poDS->adfGeoTransform[5] *= -1;
 
+    if( CPLFetchBool(poOpenInfo->papszOpenOptions,
+                                "SHIFT_ORIGIN_IN_MINUS_180_PLUS_180", false) )
+    {
+        if( poDS->adfGeoTransform[0] < -180.0 )
+            poDS->adfGeoTransform[0] += 360.0;
+        else if( poDS->adfGeoTransform[0] > 180.0 )
+            poDS->adfGeoTransform[0] -= 360.0;
+    }
+
     if (!GDALCheckDatasetDimensions(poDS->nRasterXSize, poDS->nRasterYSize))
     {
         delete poDS;
@@ -404,6 +413,12 @@ void GDALRegister_GTX()
     poDriver->SetMetadataItem( GDAL_DCAP_VIRTUALIO, "YES" );
     // poDriver->SetMetadataItem( GDAL_DMD_HELPTOPIC,
     //                            "frmt_various.html#GTX" );
+    poDriver->SetMetadataItem( GDAL_DMD_OPENOPTIONLIST,
+"<OpenOptionList>"
+"   <Option name='SHIFT_ORIGIN_IN_MINUS_180_PLUS_180' type='boolean' "
+    "description='Whether to apply a +/-360 deg shift to the longitude of "
+    "the top left corner so that it is in the [-180,180] range' default='NO'/>"
+"</OpenOptionList>" );
 
     poDriver->SetMetadataItem( GDAL_DMD_CREATIONDATATYPES,
                                "Float32" );
