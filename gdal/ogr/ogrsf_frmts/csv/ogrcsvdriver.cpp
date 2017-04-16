OSGeo/gdal

commit 4becd89f9cd8640b119594e3b46c61e2fa05f17f

Remove uses of strlen(a_std_string) (fixes #6752)
git-svn-id: https://svn.osgeo.org/gdal/trunk@36979 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/ogr/ogrsf_frmts/csv/ogrcsvdriver.cpp
@@ -69,9 +69,9 @@ static int OGRCSVDriverIdentify( GDALOpenInfo* poOpenInfo )
               STARTS_WITH_CI(osBaseFilename, "NationalFedCodes_") ||
               STARTS_WITH_CI(osBaseFilename, "AllStates_") ||
               STARTS_WITH_CI(osBaseFilename, "AllStatesFedCodes_") ||
-              (strlen(osBaseFilename) > 2
+              (osBaseFilename.size() > 2
                && STARTS_WITH_CI(osBaseFilename+2, "_Features_")) ||
-              (strlen(osBaseFilename) > 2
+              (osBaseFilename.size()
                && STARTS_WITH_CI(osBaseFilename+2, "_FedCodes_"))) &&
              (EQUAL(osExt, "txt") || EQUAL(osExt, "zip")) )
         {

gdal/frmts/adrg/adrgdataset.cpp
@@ -1632,7 +1632,7 @@ GDALDataset *ADRGDataset::Create( const char* pszFilename,
     }
 
     CPLString osBaseFileName(CPLGetBasename(pszFilename));
-    if( strlen(osBaseFileName) != 8 ||
+    if( osBaseFileName.size() != 8 ||
         osBaseFileName[6] != '0' ||
         osBaseFileName[7] != '1' )
     {

gdal/frmts/adrg/srpdataset.cpp
@@ -873,7 +873,7 @@ char **SRPDataset::GetFileList()
 
         papszFileList = CSLAddString(papszFileList, osIMGFileName.c_str());
 
-        if( strlen(osQALFileName) > 0 )
+        if( !osQALFileName.empty() )
             papszFileList = CSLAddString( papszFileList, osQALFileName );
     }
     return papszFileList;

gdal/frmts/aigrid/aigdataset.cpp
@@ -692,7 +692,7 @@ GDALDataset *AIGDataset::Open( GDALOpenInfo * poOpenInfo )
     CSLDestroy( papszFiles );
 
     // Look in parent if we don't find a .clr in the coverage dir.
-    if( strlen(osClrFilename) == 0 )
+    if( osClrFilename.empty() )
     {
         osTestName.Printf( "%s/../%s.clr",
                            psInfo->pszCoverName,
@@ -711,7 +711,7 @@ GDALDataset *AIGDataset::Open( GDALOpenInfo * poOpenInfo )
             osClrFilename = osTestName;
     }
 
-    if( strlen(osClrFilename) > 0 )
+    if( !osClrFilename.empty() )
         poDS->TranslateColorTable( osClrFilename );
 
 /* -------------------------------------------------------------------- */
@@ -927,13 +927,13 @@ static CPLErr AIGRename( const char *pszNewName, const char *pszOldName )
     {
         CPLString osNewFilename;
 
-        if( !EQUALN(papszFileList[i],osOldPath,strlen(osOldPath)) )
+        if( !EQUALN(papszFileList[i],osOldPath,osOldPath.size()) )
         {
             CPLAssert( false );
             return CE_Failure;
         }
 
-        osNewFilename = osNewPath + (papszFileList[i] + strlen(osOldPath));
+        osNewFilename = osNewPath + (papszFileList[i] + osOldPath.size());
 
         papszNewFileList = CSLAddString( papszNewFileList, osNewFilename );
     }

gdal/frmts/dimap/dimapdataset.cpp
@@ -217,7 +217,7 @@ char **DIMAPDataset::GetMetadata( const char *pszDomain )
 const char *DIMAPDataset::GetProjectionRef()
 
 {
-    if( strlen(osProjection) > 0 )
+    if( !osProjection.empty() )
         return osProjection;
 
     return GDALPamDataset::GetProjectionRef();

gdal/frmts/ers/ersdataset.cpp
@@ -375,7 +375,7 @@ CPLErr ERSDataset::SetGCPs( int nGCPCountIn, const GDAL_GCP *pasGCPListIn,
         CPLString osLine;
 
         CPLString osId = pasGCPList[iGCP].pszId;
-        if( strlen(osId) == 0 )
+        if( osId.empty() )
             osId.Printf( "%d", iGCP + 1 );
 
         osLine.Printf( "\t\t\t\t\"%s\"\tYes\tYes\t%.6f\t%.6f\t%.15g\t%.15g\t%.15g\n",
@@ -608,7 +608,7 @@ char **ERSDataset::GetFileList()
     char **papszFileList = GDALPamDataset::GetFileList();
 
     // Add raw data file if we have one.
-    if( strlen(osRawFilename) > 0 )
+    if( !osRawFilename.empty() )
         papszFileList = CSLAddString( papszFileList, osRawFilename );
 
     // If we have a dependent file, merge its list of files in.

gdal/frmts/gtiff/gt_citation.cpp
@@ -425,7 +425,7 @@ void SetGeogCSCitation( GTIF * psGTIF, OGRSpatialReference *poSRS,
         bRewriteGeogCitation = true;
     }
 
-    if( osCitation[strlen(osCitation) - 1] != '|' )
+    if( osCitation.back() != '|' )
         osCitation += "|";
 
     if( bRewriteGeogCitation )

gdal/frmts/ilwis/ilwisdataset.cpp
@@ -239,7 +239,7 @@ void IniFile::Store()
 
         // write the section name
         osLine.Printf( "[%s]\r\n", (*iterSect).first.c_str());
-        VSIFWriteL( osLine.c_str(), 1, strlen(osLine), filIni );
+        VSIFWriteL( osLine.c_str(), 1, osLine.size(), filIni );
         SectionEntries *entries = (*iterSect).second;
         SectionEntries::iterator iterEntry;
         for (iterEntry = (*entries).begin(); iterEntry != (*entries).end(); ++iterEntry)
@@ -247,7 +247,7 @@ void IniFile::Store()
             std::string key = (*iterEntry).first;
             osLine.Printf( "%s=%s\r\n",
                            TrimSpaces(key).c_str(), (*iterEntry).second.c_str());
-            VSIFWriteL( osLine.c_str(), 1, strlen(osLine), filIni );
+            VSIFWriteL( osLine.c_str(), 1, osLine.size(), filIni );
         }
 
         VSIFWriteL( "\r\n", 1, 2, filIni );

commit 60c2c78f6a6d6c77ac798e750483a28cee29e23c

CSV: fix comparison broken in r36979 (fixes https://trac.osgeo.org/gdal/ticket/6862)
git-svn-id: https://svn.osgeo.org/gdal/trunk@38031 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/ogr/ogrsf_frmts/csv/ogrcsvdriver.cpp
@@ -83,7 +83,7 @@ static int OGRCSVDriverIdentify( GDALOpenInfo* poOpenInfo )
               STARTS_WITH_CI(osBaseFilename, "AllStatesFedCodes_") ||
               (osBaseFilename.size() > 2
                && STARTS_WITH_CI(osBaseFilename+2, "_Features_")) ||
-              (osBaseFilename.size()
+              (osBaseFilename.size() > 2
                && STARTS_WITH_CI(osBaseFilename+2, "_FedCodes_"))) &&
              (EQUAL(osExt, "txt") || EQUAL(osExt, "zip")) )
         {