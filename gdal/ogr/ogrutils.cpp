ed469def42f01e0e5292b5daa0b72a68a8b76044
Even Rouault <even.rouault@mines-paris.org>
2017-04-20 19:37:14 +0000

    OGRParse: fix parsing logic to avoid false positive detection of string as datetime (fixes #6867)
    git-svn-id: https://svn.osgeo.org/gdal/branches/2.2@38076 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrutils.cpp
+++ gdal/ogr/ogrutils.cpp
@@ -1050,12 +1050,16 @@ int OGRParseDate( const char *pszInput,
 
         while( *pszInput >= '0' && *pszInput <= '9' )
             ++pszInput;
+        if( *pszInput == '\0' )
+            return TRUE;
 
         bGotSomething = true;
 
         // If ISO 8601 format.
         if( *pszInput == 'T' )
             ++pszInput;
+        else if( *pszInput != ' ' )
+            return FALSE;
     }
 
 /* -------------------------------------------------------------------- */
