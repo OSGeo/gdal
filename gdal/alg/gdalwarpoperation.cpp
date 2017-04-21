61d74a1caaaa348f257bfb9cbd3e7beb2af1de10
Even Rouault <even.rouault@mines-paris.org>
2017-04-21 21:02:44 +0000

    gdalwarp: fix crash when -multi and -to RPC_DEM are used together (fixes #6869)
    git-svn-id: https://svn.osgeo.org/gdal/branches/2.2@38093 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/alg/gdalwarpoperation.cpp
+++ gdal/alg/gdalwarpoperation.cpp
@@ -1690,11 +1690,22 @@ CPLErr GDALWarpOperation::WarpRegionToBuffer(
 /* -------------------------------------------------------------------- */
     if( nSrcXSize == 0 && nSrcYSize == 0 )
     {
+        // TODO: This taking of the warp mutex is suboptimal. We could get rid
+        // of it, but that would require making sure ComputeSourceWindow()
+        // uses a different pTransformerArg than the warp kernel.
+        if( hWarpMutex != NULL && !CPLAcquireMutex( hWarpMutex, 600.0 ) )
+        {
+            CPLError( CE_Failure, CPLE_AppDefined,
+                      "Failed to acquire WarpMutex in WarpRegion()." );
+            return CE_Failure;
+        }
         const CPLErr eErr =
             ComputeSourceWindow( nDstXOff, nDstYOff, nDstXSize, nDstYSize,
                                  &nSrcXOff, &nSrcYOff,
                                  &nSrcXSize, &nSrcYSize,
                                  &nSrcXExtraSize, &nSrcYExtraSize, NULL );
+        if( hWarpMutex != NULL )
+            CPLReleaseMutex( hWarpMutex );
         if( eErr != CE_None )
             return eErr;
     }
@@ -2467,6 +2478,19 @@ CPLErr GDALWarpOperation::ComputeSourceWindow(
     {
         if( !pabSuccess[i] )
         {
+            nFailedCount++;
+            continue;
+        }
+
+        // If this happens this is likely the symptom of a bug somewhere.
+        if( CPLIsNan(padfX[i]) || CPLIsNan(padfY[i]) )
+        {
+            static bool bNanCoordFound = false;
+            if( !bNanCoordFound )
+            {
+                CPLDebug("WARP", "NaN coordinate found.");
+                bNanCoordFound = true;
+            }
             nFailedCount++;
             continue;
         }
