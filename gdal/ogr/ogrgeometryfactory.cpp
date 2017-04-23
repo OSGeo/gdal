604d9efe6296c14386aa910cfa1b3386f2e44d23
Even Rouault <even.rouault@mines-paris.org>
2017-04-23 07:36:29 +0000

    OGRGeometryFactory::organizePolygons(): return POLYGON EMPTY when nPolygonCount == 0
    Found via LLVM ASAN fuzzer.
    
    heap-buffer-overflow crash-a801471c8de24e3cc696ddb0af3cfbeb84a81f86
    
    Port of trunk r38112 (by Kurt Schwehr) and r38116
    
    
    
    git-svn-id: https://svn.osgeo.org/gdal/branches/2.2@38117 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrgeometryfactory.cpp
+++ gdal/ogr/ogrgeometryfactory.cpp
@@ -1337,7 +1337,8 @@ typedef enum
  * @param papszOptions a list of strings for passing options
  *
  * @return a single resulting geometry (either OGRPolygon, OGRCurvePolygon,
- * OGRMultiPolygon, OGRMultiSurface or OGRGeometryCollection).
+ * OGRMultiPolygon, OGRMultiSurface or OGRGeometryCollection). Returns a
+ * POLYGON EMPTY in the case of nPolygonCount being 0.
  */
 
 OGRGeometry* OGRGeometryFactory::organizePolygons( OGRGeometry **papoPolygons,
@@ -1345,6 +1346,14 @@ OGRGeometry* OGRGeometryFactory::organizePolygons( OGRGeometry **papoPolygons,
                                                    int *pbIsValidGeometry,
                                                    const char** papszOptions )
 {
+    if( nPolygonCount == 0 )
+    {
+        if( pbIsValidGeometry )
+            *pbIsValidGeometry = TRUE;
+
+        return new OGRPolygon();
+    }
+
     OGRGeometry* geom = NULL;
     OrganizePolygonMethod method = METHOD_NORMAL;
     bool bHasCurves = false;
