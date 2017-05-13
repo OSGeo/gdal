c1a38146c30123f125953a6885272e61aee6e4d5
Even Rouault <even.rouault@mines-paris.org>
2017-05-13 20:57:45 +0000

    fuzzers: fix path
    git-svn-id: https://svn.osgeo.org/gdal/trunk@38267 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/fuzzers/build_google_oss_fuzzers.sh
+++ gdal/fuzzers/build_google_oss_fuzzers.sh
@@ -22,5 +22,5 @@ for F in $fuzzerFiles; do
     echo "Building fuzzer $fuzzerName"
     $CXX $CXXFLAGS -std=c++11 -I$SRC_DIR/port -I$SRC_DIR/gcore -I$SRC_DIR/alg -I$SRC_DIR/ogr -I$SRC_DIR/ogr/ogrsf_frmts \
         $F -o $OUT/$fuzzerName \
-        -lFuzzingEngine $SRC_DIR/libgdal.a $SRC/install/*.a
+        -lFuzzingEngine $SRC_DIR/libgdal.a $SRC/install/lib/*.a
 done
