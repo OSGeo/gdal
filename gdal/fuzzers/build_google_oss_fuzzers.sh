83cc6e3f4267f8dcec2e9a57a0a96e5898933fd5
Even Rouault <even.rouault@mines-paris.org>
2017-05-13 20:48:35 +0000

    fuzzers: statically link against dependencies
    git-svn-id: https://svn.osgeo.org/gdal/trunk@38266 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/fuzzers/build_google_oss_fuzzers.sh
+++ gdal/fuzzers/build_google_oss_fuzzers.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$SRC" == "" ]; then
+    echo "SRC env var not defined"
+    exit 1
+fi
+
 if [ "$OUT" == "" ]; then
     echo "OUT env var not defined"
     exit 1
@@ -17,5 +22,5 @@ for F in $fuzzerFiles; do
     echo "Building fuzzer $fuzzerName"
     $CXX $CXXFLAGS -std=c++11 -I$SRC_DIR/port -I$SRC_DIR/gcore -I$SRC_DIR/alg -I$SRC_DIR/ogr -I$SRC_DIR/ogr/ogrsf_frmts \
         $F -o $OUT/$fuzzerName \
-        -lFuzzingEngine $SRC_DIR/libgdal.a
+        -lFuzzingEngine $SRC_DIR/libgdal.a $SRC/install/*.a
 done
