211f3a8b6513cee31afaa7bee8bde8a124659fb3
Even Rouault <even.rouault@mines-paris.org>
2017-05-13 21:21:20 +0000

    SEGUKOOA: prevent read beyond end of buffer. Credit to OSS Fuzz (run locally)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@38269 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/segukooa/ogrsegukooalayer.cpp
+++ gdal/ogr/ogrsf_frmts/segukooa/ogrsegukooalayer.cpp
@@ -605,7 +605,7 @@ OGRFeature *OGRSEGP1Layer::GetNextRawFeature()
         }
 
         /* Normal layout -> extract other fields */
-        if (nLatitudeCol == 27)
+        if (nLatitudeCol == 27 && nLineLen >= 26-1+1)
         {
             char szLineName[16 + 1];
             ExtractField(szLineName, pszLine, 2-1, 16);
