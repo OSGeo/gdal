4b21f4a8ded3765721c15a449d43907386c79655
Even Rouault <even.rouault@mines-paris.org>
2017-05-13 23:39:59 +0000

    fuzzers: make build_google_oss_fuzzers.sh robust to presence or not of /install/lib
    git-svn-id: https://svn.osgeo.org/gdal/trunk@38281 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/fuzzers/build_google_oss_fuzzers.sh
+++ gdal/fuzzers/build_google_oss_fuzzers.sh
@@ -17,11 +17,16 @@ fi
 
 SRC_DIR=$(dirname $0)/..
 fuzzerFiles=$(find $(dirname $0) -name "*.cpp")
-mkdir -p $SRC/install/lib
-cp $SRC_DIR/libgdal.a $SRC/install/lib
 for F in $fuzzerFiles; do
     fuzzerName=$(basename $F .cpp)
     echo "Building fuzzer $fuzzerName"
-    $CXX $CXXFLAGS -std=c++11 -I$SRC_DIR/port -I$SRC_DIR/gcore -I$SRC_DIR/alg -I$SRC_DIR/ogr -I$SRC_DIR/ogr/ogrsf_frmts \
-        $F -o $OUT/$fuzzerName $SRC/install/lib/*.a
+    if test -d $SRC/install/lib; then
+        $CXX $CXXFLAGS -std=c++11 -I$SRC_DIR/port -I$SRC_DIR/gcore -I$SRC_DIR/alg -I$SRC_DIR/ogr -I$SRC_DIR/ogr/ogrsf_frmts \
+            $F -o $OUT/$fuzzerName \
+            -lFuzzingEngine $SRC_DIR/libgdal.a $SRC/install/lib/*.a
+    else
+        $CXX $CXXFLAGS -std=c++11 -I$SRC_DIR/port -I$SRC_DIR/gcore -I$SRC_DIR/alg -I$SRC_DIR/ogr -I$SRC_DIR/ogr/ogrsf_frmts \
+            $F -o $OUT/$fuzzerName \
+            -lFuzzingEngine $SRC_DIR/libgdal.a
+    fi
 done
