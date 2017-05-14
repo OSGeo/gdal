c481b5ffad399404c2292aa44151e0a1988e28d3
Even Rouault <even.rouault@mines-paris.org>
2017-05-14 21:22:36 +0000

    KML::unregisterLayerIfMatchingThisNode(): use memmove() instead of memcpy(). Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=1580. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@38352 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/kml/kml.cpp
+++ gdal/ogr/ogrsf_frmts/kml/kml.cpp
@@ -654,7 +654,7 @@ void KML::unregisterLayerIfMatchingThisNode(KMLNode* poNode)
         {
             if( i < nNumLayers_ - 1 )
             {
-                memcpy( papoLayers_ + i, papoLayers_ + i + 1,
+                memmove( papoLayers_ + i, papoLayers_ + i + 1,
                         (nNumLayers_ - 1 - i) * sizeof(KMLNode*) );
             }
             nNumLayers_ --;
