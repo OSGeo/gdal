629db981a755e79615cbe57ea394516dd76282e2
Even Rouault <even.rouault@mines-paris.org>
2017-05-14 02:33:51 +0000

    Add mitab_tab_fuzzer
    git-svn-id: https://svn.osgeo.org/gdal/trunk@38300 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/fuzzers/build_seed_corpus.sh
+++ gdal/fuzzers/build_seed_corpus.sh
@@ -26,3 +26,15 @@ printf "***NEWFILE***:my.prj\n" >> poly_shp.tar
 cat $(dirname $0)/../../autotest/ogr/data/poly.PRJ >> poly_shp.tar
 rm -f $OUT/shape_fuzzer_seed_corpus.zip
 zip -r $OUT/shape_fuzzer_seed_corpus.zip poly_shp.tar >/dev/null
+
+printf "FUZZER_FRIENDLY_ARCHIVE\n" > all_geoms_tab.tar
+printf "***NEWFILE***:my.tab\n" >> all_geoms_tab.tar
+cat $(dirname $0)/../../autotest/ogr/data/all_geoms.tab >> all_geoms_tab.tar
+printf "***NEWFILE***:my.map\n" >> all_geoms_tab.tar
+cat $(dirname $0)/../../autotest/ogr/data/all_geoms.map >> all_geoms_tab.tar
+printf "***NEWFILE***:my.dat\n" >> all_geoms_tab.tar
+cat $(dirname $0)/../../autotest/ogr/data/all_geoms.dat >> all_geoms_tab.tar
+printf "***NEWFILE***:my.id\n" >> all_geoms_tab.tar
+cat $(dirname $0)/../../autotest/ogr/data/all_geoms.id >> all_geoms_tab.tar
+rm -f $OUT/mitab_tab_fuzzer_seed_corpus.zip
+zip -r $OUT/mitab_tab_fuzzer_seed_corpus.zip all_geoms_tab.tar >/dev/null
