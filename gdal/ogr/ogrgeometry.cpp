1b3e5997813b9b932cb8348f42b000071e0ee58f
Even Rouault <even.rouault@mines-paris.org>
2017-05-14 00:40:31 +0000

    Several fixes in importFromWkb() and importFromWkt()
    git-svn-id: https://svn.osgeo.org/gdal/branches/2.2@38291 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrgeometry.cpp
+++ gdal/ogr/ogrgeometry.cpp
@@ -6122,7 +6122,7 @@ OGRErr OGRGeometry::importPreambuleOfCollectionFromWkb( unsigned char * pabyData
     if( OGR_SWAP( eByteOrder ) )
         nGeomCount = CPL_SWAP32(nGeomCount);
 
-    if( nGeomCount < 0 || nGeomCount > INT_MAX / 4 )
+    if( nGeomCount < 0 || nGeomCount > INT_MAX / nMinSubGeomSize )
     {
         nGeomCount = 0;
         return OGRERR_CORRUPT_DATA;
@@ -6232,8 +6232,8 @@ OGRErr OGRGeometry::importCurveCollectionFromWkt(
             poCurve = dynamic_cast<OGRCurve *>(poGeom);
             if( poCurve == NULL )
             {
-                CPLError(CE_Fatal, CPLE_AppDefined,
-                         "dynamic_cast failed.  Expected OGRCurve.");
+                delete poGeom;
+                eErr = OGRERR_CORRUPT_DATA;
             }
         }
         else
