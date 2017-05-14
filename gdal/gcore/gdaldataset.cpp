41d9387ca70a3aa6e62cbe777e40409161b213f1
Even Rouault <even.rouault@mines-paris.org>
2017-05-14 09:16:01 +0000

    GDALDataset and GDALRasterBand::ReportError(): fix crash if dataset name has a % character.Credit to OSS Fuzz (run locally)
    git-svn-id: https://svn.osgeo.org/gdal/branches/2.1@38304 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/gcore/gdaldataset.cpp
+++ gdal/gcore/gdaldataset.cpp
@@ -3320,7 +3320,7 @@ void GDALDataset::ReportError(CPLErr eErrClass, CPLErrorNum err_no, const char *
     const char* pszDSName = GetDescription();
     if (strlen(fmt) + strlen(pszDSName) + 3 >= sizeof(szNewFmt) - 1)
         pszDSName = CPLGetFilename(pszDSName);
-    if (pszDSName[0] != '\0' &&
+    if (pszDSName[0] != '\0' && strchr(pszDSName, '%') == NULL &&
         strlen(fmt) + strlen(pszDSName) + 3 < sizeof(szNewFmt) - 1)
     {
         snprintf(szNewFmt, sizeof(szNewFmt), "%s: %s",
