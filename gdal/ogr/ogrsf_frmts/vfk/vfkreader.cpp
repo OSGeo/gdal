c471fceacdfa89cd6b8809fd31f294032a1dd1f1
Even Rouault <even.rouault@mines-paris.org>
2017-05-15 11:16:26 +0000

    VFK: avoid out-of-bounds read. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=1596. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/branches/2.1@38377 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/vfk/vfkreader.cpp
+++ gdal/ogr/ogrsf_frmts/vfk/vfkreader.cpp
@@ -278,6 +278,7 @@ int VFKReader::ReadDataRecords(IVFKDataBlock *poDataBlock)
                     CPLFree(pszLine);
 
                     while ((pszLine = ReadLine()) != NULL &&
+                           pszLine[0] != '\0' &&
                            pszLine[strlen(pszLine) - 1] == '\244') {
                         /* append line */
                         osMultiLine += pszLine;
