068b2fcbd9a5597b4c51860001956eb37a0ce5ae
Even Rouault <even.rouault@mines-paris.org>
2017-05-16 09:58:46 +0000

    KML: fix null ptr dereference on corrupted file. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=1634. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@38408 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/kml/kml.cpp
+++ gdal/ogr/ogrsf_frmts/kml/kml.cpp
@@ -232,7 +232,8 @@ void XMLCALL KML::startElement( void* pUserData, const char* pszName,
     poKML->nWithoutEventCounter = 0;
 
     if(poKML->poTrunk_ == NULL
-    || (poKML->poCurrent_->getName()).compare("description") != 0)
+    || (poKML->poCurrent_ != NULL &&
+        poKML->poCurrent_->getName().compare("description") != 0))
     {
         if (poKML->nDepth_ == 1024)
         {
@@ -263,7 +264,7 @@ void XMLCALL KML::startElement( void* pUserData, const char* pszName,
 
         poKML->nDepth_++;
     }
-    else
+    else if( poKML->poCurrent_ != NULL )
     {
         std::string sNewContent = "<";
         sNewContent += pszName;
