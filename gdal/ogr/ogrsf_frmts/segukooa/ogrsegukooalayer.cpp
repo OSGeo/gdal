4fdb49ee6d5f42b979e08081161eac5ce8edbd2c
Even Rouault <even.rouault@mines-paris.org>
2017-05-19 18:48:58 +0000

    SEGUKOOA: fix inversion of leap year that caused index-out-of-bound reading on day 366 of leap years (2.2 regression). Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=1698. Credit to OSS-Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@38496 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/segukooa/ogrsegukooalayer.cpp
+++ gdal/ogr/ogrsf_frmts/segukooa/ogrsegukooalayer.cpp
@@ -420,7 +420,7 @@ OGRFeature *OGRUKOOAP190Layer::GetNextRawFeature()
                 if ((bIsLeap && nDayOfYear >= 1 && nDayOfYear <= 366) ||
                     (!bIsLeap && nDayOfYear >= 1 && nDayOfYear <= 365))
                 {
-                    static const int leap_offset = bIsLeap ? 0 : 1;
+                    static const int leap_offset = bIsLeap ? 1 : 0;
                     while( nDayOfYear >
                            nDays +
                            mon_lengths[leap_offset][nMonth] )
