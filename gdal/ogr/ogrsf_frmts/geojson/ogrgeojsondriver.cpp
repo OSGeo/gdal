fd74dc4b5db8d897c634de08c3b9a8a0931ebf1f
Even Rouault <even.rouault@mines-paris.org>
2017-05-20 15:08:39 +0000

    ESRIJson: avoid endless looping on servers that don't support resultOffset (refs https://trac.osgeo.org/gdal/ticket/6895)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@38519 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/geojson/ogrgeojsondriver.cpp
+++ gdal/ogr/ogrsf_frmts/geojson/ogrgeojsondriver.cpp
@@ -58,6 +58,7 @@ class OGRESRIFeatureServiceLayer: public OGRLayer
     OGRESRIFeatureServiceDataset* poDS;
     OGRFeatureDefn* poFeatureDefn;
     GIntBig         nFeaturesRead;
+    GIntBig         nFirstFID;
     GIntBig         nLastFID;
     bool            bOtherPage;
     bool            bUseSequentialFID;
@@ -116,6 +117,7 @@ OGRESRIFeatureServiceLayer::OGRESRIFeatureServiceLayer(
     OGRESRIFeatureServiceDataset* poDSIn) :
     poDS(poDSIn),
     nFeaturesRead(0),
+    nFirstFID(0),
     nLastFID(0),
     bOtherPage(false),
     bUseSequentialFID(false)
@@ -173,12 +175,22 @@ OGRFeature* OGRESRIFeatureServiceLayer::GetNextFeature()
             if( poSrcFeat == NULL )
                 return NULL;
             bOtherPage = true;
+            if( bWasInFirstPage && poSrcFeat->GetFID() != 0 &&
+                poSrcFeat->GetFID() == nFirstFID )
+            {
+                // End-less looping
+                CPLDebug("ESRIJSON", "Scrolling not working. Stopping");
+                delete poSrcFeat;
+                return NULL;
+            }
+            if( bWasInFirstPage && poSrcFeat->GetFID() == 0 &&
+                nLastFID == nFeaturesRead - 1 )
+            {
+                bUseSequentialFID = true;
+            }
         }
-        if( bOtherPage && bWasInFirstPage && poSrcFeat->GetFID() == 0 &&
-            nLastFID == nFeaturesRead - 1 )
-        {
-            bUseSequentialFID = true;
-        }
+        if( nFeaturesRead == 0 )
+            nFirstFID = poSrcFeat->GetFID();
 
         OGRFeature* poFeature = new OGRFeature(poFeatureDefn);
         poFeature->SetFrom(poSrcFeat);
