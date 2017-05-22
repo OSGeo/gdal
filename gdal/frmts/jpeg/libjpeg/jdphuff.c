004b912bd03df482b57479cd9919801bd356241c
Even Rouault <even.rouault@mines-paris.org>
2017-05-22 08:17:00 +0000

    Internal libjpeg. Avoid undefined-shift of negative value. Similarly as https://github.com/libjpeg-turbo/libjpeg-turbo/commit/8e9cef2e6f5156c4b055a04a8f979b7291fc6b7a. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=1768. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@38588 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/jpeg/libjpeg/jdphuff.c
+++ gdal/frmts/jpeg/libjpeg/jdphuff.c
@@ -337,7 +337,7 @@ decode_mcu_DC_first (j_decompress_ptr cinfo, JBLOCKROW *MCU_data)
       s += state.last_dc_val[ci];
       state.last_dc_val[ci] = s;
       /* Scale and output the coefficient (assumes jpeg_natural_order[0]=0) */
-      (*block)[0] = (JCOEF) (s << Al);
+      (*block)[0] = (JCOEF) LEFT_SHIFT(s, Al);
     }
 
     /* Completed MCU, so update state */
@@ -405,7 +405,7 @@ decode_mcu_AC_first (j_decompress_ptr cinfo, JBLOCKROW *MCU_data)
 	  r = GET_BITS(s);
 	  s = HUFF_EXTEND(r, s);
 	  /* Scale and output coefficient in natural (dezigzagged) order */
-	  (*block)[jpeg_natural_order[k]] = (JCOEF) (s << Al);
+	  (*block)[jpeg_natural_order[k]] = (JCOEF) LEFT_SHIFT(s, Al);
 	} else {
 	  if (r == 15) {	/* ZRL */
 	    k += 15;		/* skip 15 zeroes in band */
