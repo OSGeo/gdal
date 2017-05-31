befc391ac9ba64cf5262f0d4e48f84a52dd80c4d
Even Rouault <even.rouault@mines-paris.org>
2017-05-31 11:02:26 +0000

    CEOS: avoid undefined shift behaviour. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=1970. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@38715 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/ceos/ceosopen.c
+++ gdal/frmts/ceos/ceosopen.c
@@ -96,17 +96,17 @@ CEOSRecord * CEOSReadRecord( CEOSImage *psImage )
         CPL_SWAP32PTR( abyHeader + 8 );
     }
 
-    nRecordNumUInt32 = (abyHeader[0] << 24)
+    nRecordNumUInt32 = ((unsigned)abyHeader[0] << 24)
                          + (abyHeader[1] << 16)
                          + (abyHeader[2] << 8)
                          + abyHeader[3];
 
-    psRecord->nRecordType = (abyHeader[4] << 24)
+    psRecord->nRecordType = ((unsigned)abyHeader[4] << 24)
                          + (abyHeader[5] << 16)
                          + (abyHeader[6] << 8)
                          + abyHeader[7];
 
-    nLengthUInt32 = (abyHeader[8] << 24)
+    nLengthUInt32 = ((unsigned)abyHeader[8] << 24)
                          + (abyHeader[9] << 16)
                          + (abyHeader[10] << 8)
                          + abyHeader[11];
