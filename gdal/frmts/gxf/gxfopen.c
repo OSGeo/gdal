8c96a17dbbbd1142ea675d7cd1222bee8ce44809
Even Rouault <even.rouault@mines-paris.org>
2017-05-31 16:38:58 +0000

    GXF: validate nGType to avoid later out-of-bound read in GXFReadRawScanlineFrom(). Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=1975. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@38722 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/gxf/gxfopen.c
+++ gdal/frmts/gxf/gxfopen.c
@@ -333,6 +333,12 @@ GXFHandle GXFOpen( const char * pszFilename )
         else if( STARTS_WITH_CI(szTitle,"#GTYPE") )
         {
             psGXF->nGType = atoi(papszList[0]);
+            if( psGXF->nGType < 0 || psGXF->nGType > 20 )
+            {
+                CSLDestroy( papszList );
+                GXFClose( psGXF );
+                return NULL;
+            }
         }
 
         CSLDestroy( papszList );
