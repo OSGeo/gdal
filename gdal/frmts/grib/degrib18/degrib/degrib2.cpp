6bc92a3a20fd6445a93243343496f5b8969353d2
Even Rouault <even.rouault@mines-paris.org>
2017-05-31 10:59:44 +0000

    GRIB / degrib: pass nd2x3 member so as to check that we don't read out of bounds in iain. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=1969. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@38714 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/grib/degrib18/degrib/degrib2.cpp
+++ gdal/frmts/grib/degrib18/degrib/degrib2.cpp
@@ -1151,14 +1151,14 @@ int ReadGrib2Record (DataSource &fp, sChar f_unit, double **Grib_Data,
 
    if (strcmp (meta->element, "Wx") != 0) {
       ParseGrid (&(meta->gridAttrib), Grib_Data, grib_DataLen, Nx, Ny,
-                 meta->gds.scan, IS->iain, ibitmap, IS->ib, unitM, unitB, 0,
+                 meta->gds.scan, IS->nd2x3, IS->iain, ibitmap, IS->ib, unitM, unitB, 0,
                  NULL, f_subGrid, x1, y1, x2, y2);
    } else {
       /* Handle weather grid.  ParseGrid looks up the values... If they are
        * "<Invalid>" it sets it to missing (or creates one).  If the table
        * entry is used it sets f_valid to 2. */
       ParseGrid (&(meta->gridAttrib), Grib_Data, grib_DataLen, Nx, Ny,
-                 meta->gds.scan, IS->iain, ibitmap, IS->ib, unitM, unitB, 1,
+                 meta->gds.scan, IS->nd2x3, IS->iain, ibitmap, IS->ib, unitM, unitB, 1,
                  (sect2_WxType *) &(meta->pds2.sect2.wx), f_subGrid, x1, y1,
                  x2, y2);
 
--- gdal/frmts/grib/degrib18/degrib/meta.h
+++ gdal/frmts/grib/degrib18/degrib/meta.h
@@ -561,7 +561,7 @@ int MetaParse (grib_MetaData * meta, sInt4 *is0, sInt4 ns0,
 
 void ParseGrid (gridAttribType * attrib, double **Grib_Data,
                 uInt4 *grib_DataLen, uInt4 Nx, uInt4 Ny, int scan,
-                sInt4 *iain, sInt4 ibitmap, sInt4 *ib, double unitM,
+                sInt4 nd2x3, sInt4 *iain, sInt4 ibitmap, sInt4 *ib, double unitM,
                 double unitB, uChar f_wxType, sect2_WxType * WxType,
                 uChar f_subGrid, int startX, int startY, int stopX, int stopY);
 
