f1e96682a06f48b3f0fd8cd83c0932397650ad29
Even Rouault <even.rouault@mines-paris.org>
2017-06-04 09:34:39 +0000

    scripts/detect_printf.sh: add a comment
    git-svn-id: https://svn.osgeo.org/gdal/trunk@38851 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/scripts/detect_printf.sh
+++ gdal/scripts/detect_printf.sh
@@ -3,6 +3,7 @@
 ret_code=0
 
 echo "Checking for printf() statements..."
+# apps is voluntarily missing in the list of directories, due to lots of legitimate uses of such statements in programs
 grep -r --include="*.c*" " printf" alg gnm port ogr gcore frmts | grep -v "/*ok" | grep -v "printf()" | grep -v "printf-like" | grep -v "printf style" | grep -v "with printf"  | grep -v "/\* printf" |  grep -v "//" | grep -v degrib | grep -v aitest | grep -v dted_test | grep -v giflib | grep -v 8211view | grep -v 8211dump | grep -v timetest | grep -v 8211createfromxml | grep -v hfatest | grep -v zlib | grep -v libtiff | grep -v envisat_dump | grep -v dumpgeo | grep -v nitfdump | grep -v ceostest| grep -v libpng | grep -v generate_encoding | grep -v capi_test | grep -v ntfdump | grep -v test_load_virtual_ogr | grep -v ocitest | grep -v fastload | grep -v s57dump | grep -v dgndump | grep -v test_geo_utils | grep -v testparser | grep -v internal_libqhull
 
 if [[ $? -eq 0 ]] ; then
@@ -14,6 +15,7 @@ fi
 
 
 echo "Checking for fprintf(stderr,) statements..."
+# apps is voluntarily missing in the list of directories, due to lots of legitimate uses of such statements in programs
 grep fprintf -r  alg gnm port ogr gcore frmts --include="*.cpp" | grep stderr | grep -v -G "/[/|*][ ]*fprintf" | grep -v "/*ok" | grep -v sdts2shp | grep -v degrib18  | grep -v 8211view | grep -v 8211createfromxml | grep -v 8211dump | grep -v pcidskexception | grep -v vsipreload | grep -v fprintfstderr | grep -v cpl_multiproc | grep -v "truncation occurred" | grep -v xmlreformat | grep -v cpl_error
 
 if [[ $? -eq 0 ]] ; then
