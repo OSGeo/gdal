5f6217122d5af8ead2efcd6988d321d65ee05878
Even Rouault <even.rouault@mines-paris.org>
2017-06-04 09:32:23 +0000

    Fix previous broken script, and add apps to the list
    git-svn-id: https://svn.osgeo.org/gdal/trunk@38850 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/scripts/detect_suspicious_char_digit_zero.sh
+++ gdal/scripts/detect_suspicious_char_digit_zero.sh
@@ -3,11 +3,11 @@
 ret_code=0
 
 echo "Checking for suspicious comparisons to '0'..."
-grep -r --include="*.c*" "== '0'" alg gnm port ogr gcore frmts
+grep -r --include="*.c*" "!= '0'" alg gnm port ogr gcore frmts apps
 if [[ $? -eq 0 ]] ; then
     ret_code=1
 fi
-grep -r --include="*.c*" "!='0'" alg gnm port ogr gcore frmts | grep -v libjson
+grep -r --include="*.c*" "!='0'" alg gnm port ogr gcore frmts apps | grep -v libjson
 if [[ $? -eq 0 ]] ; then
     ret_code=1
 fi
