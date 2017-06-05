d5d80df476151c809538185062b4c507d590e91c
Even Rouault <even.rouault@mines-paris.org>
2017-06-05 08:45:27 +0000

    GTiff: avoid potential stack buffer overflow on corrupted Imagine citation. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=2105. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/branches/2.2@38890 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/gtiff/gt_citation.cpp
+++ gdal/frmts/gtiff/gt_citation.cpp
@@ -108,7 +108,7 @@ char* ImagineCitationTranslation( char* psCitation, geokey_t keyID )
             "NAD = ", "Datum = ", "Ellipsoid = ", "Units = ", NULL };
 
         // This is a handle IMAGING style citation.
-        char name[256] = { '\0' };
+        CPLString osName;
         char* p1 = NULL;
 
         char* p = strchr(psCitation, '$');
@@ -139,21 +139,21 @@ char* ImagineCitationTranslation( char* psCitation, geokey_t keyID )
             {
               case PCSCitationGeoKey:
                 if( strstr(psCitation, "Projection = ") )
-                    strcpy(name, "PRJ Name = ");
+                    osName = "PRJ Name = ";
                 else
-                    strcpy(name, "PCS Name = ");
+                    osName = "PCS Name = ";
                 break;
               case GTCitationGeoKey:
-                strcpy(name, "PCS Name = ");
+                osName = "PCS Name = ";
                 break;
               case GeogCitationGeoKey:
                 if( !strstr(p, "Unable to") )
-                    strcpy(name, "GCS Name = ");
+                    osName = "GCS Name = ";
                 break;
               default:
                 break;
             }
-            if( strlen(name)>0 )
+            if( !osName.empty() )
             {
                 // TODO(schwehr): What exactly is this code trying to do?
                 // Added in r15993 and modified in r21844 by warmerdam.
@@ -176,9 +176,8 @@ char* ImagineCitationTranslation( char* psCitation, geokey_t keyID )
                 }
                 if( p1 >= p )
                 {
-                    strncat(name, p, p1 - p + 1);
-                    strcat(name, "|");
-                    name[strlen(name)] = '\0';
+                    osName.append(p, p1 - p + 1);
+                    osName += '|';
                 }
             }
         }
@@ -207,9 +206,9 @@ char* ImagineCitationTranslation( char* psCitation, geokey_t keyID )
             if( p && p1 && p1>p )
             {
                 if( EQUAL(keyNames[i], "Units = ") )
-                    strcat(name, "LUnits = ");
+                    osName += "LUnits = ";
                 else
-                    strcat(name, keyNames[i]);
+                    osName += keyNames[i];
                 if( p1[0] == '\0' || p1[0] == '\n' || p1[0] == ' ' )
                     p1--;
                 char* p2 = p1 - 1;
@@ -224,14 +223,13 @@ char* ImagineCitationTranslation( char* psCitation, geokey_t keyID )
                 }
                 if( p1 >= p )
                 {
-                    strncat(name, p, p1 - p + 1);
-                    strcat(name, "|");
-                    name[strlen(name)] = '\0';
+                    osName.append(p, p1 - p + 1);
+                    osName += '|';
                 }
             }
         }
-        if( strlen(name) > 0 )
-            ret = CPLStrdup(name);
+        if( !osName.empty() )
+            ret = CPLStrdup(osName);
     }
     return ret;
 }
