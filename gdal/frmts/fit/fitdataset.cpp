fb8053d08b406e8822560bb4c2c5dd56b661d8b6
Even Rouault <even.rouault@mines-paris.org>
2017-06-05 07:52:29 +0000

    FIT: remove useless assertion. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=2102. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@38881 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/fit/fitdataset.cpp
+++ gdal/frmts/fit/fitdataset.cpp
@@ -995,31 +995,6 @@ GDALDataset *FITDataset::Open( GDALOpenInfo * poOpenInfo )
     }
 
 /* -------------------------------------------------------------------- */
-/*      Check if 64 bit seek is needed.                                 */
-/* -------------------------------------------------------------------- */
-    uint64 bytesPerComponent =
-        GDALGetDataTypeSize(fitDataType(poDS->info->dtype)) / 8;
-    uint64 bytesPerPixel = head->cSize * bytesPerComponent;
-    uint64 recordSize = bytesPerPixel * head->xPageSize *
-        head->yPageSize;
-    uint64 numXBlocks =
-        (uint64) ceil((double) head->xSize / head->xPageSize);
-    uint64 numYBlocks =
-        (uint64) ceil((double) head->ySize / head->yPageSize);
-
-    uint64 maxseek = recordSize * numXBlocks * numYBlocks;
-
-//     CPLDebug("FIT", "(sizeof %i) max seek %llx ==> %llx\n", sizeof(uint64),
-//              maxseek, maxseek >> 31);
-    if (maxseek >> 31) // signed long
-#ifdef VSI_LARGE_API_SUPPORTED
-        CPLDebug("FIT", "Using 64 bit version of fseek");
-#else
-        CPLError(CE_Fatal, CPLE_NotSupported,
-                 "FIT - need 64 bit version of fseek");
-#endif
-
-/* -------------------------------------------------------------------- */
 /*      Verify all "unused" header values.                              */
 /* -------------------------------------------------------------------- */
 
