0dd2a90398515ca08516bde45b6579194c3b0fda
Even Rouault <even.rouault@mines-paris.org>
2017-06-07 10:10:07 +0000

    NTF: fix heap buffer overflow. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=2135. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/branches/2.1@38933 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/ntf/ntffilereader.cpp
+++ gdal/ogr/ogrsf_frmts/ntf/ntffilereader.cpp
@@ -931,7 +931,8 @@ int NTFFileReader::ProcessAttRec( NTFRecord * poRecord,
         if( nFWidth == 0 )
         {
             const char * pszData2 = poRecord->GetData();
-
+            if( iOffset + 2 >= poRecord->GetLength() )
+                break;
             for( nEnd = iOffset + 2;
                  pszData2[nEnd] != '\\' && pszData2[nEnd] != '\0';
                  nEnd++ ) {}
@@ -954,6 +955,8 @@ int NTFFileReader::ProcessAttRec( NTFRecord * poRecord,
         if( nFWidth == 0 )
         {
             iOffset = nEnd;
+            if( iOffset >= poRecord->GetLength() )
+                break;
             if( pszData[iOffset] == '\\' )
                 iOffset++;
         }
