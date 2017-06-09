e1c57707c49dd8d2425e1393fc5b4fd6169d0347
Even Rouault <even.rouault@mines-paris.org>
2017-06-09 11:48:41 +0000

    E00GRID: fix heap buffer overflow. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=2182. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@39018 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/e00grid/e00read.c
+++ gdal/frmts/e00grid/e00read.c
@@ -490,7 +490,7 @@ static const char *_UncompressNextLine(E00ReadPtr psInfo)
                  */
                 c = _GetNextSourceChar(psInfo);
                 n = c - ' ';
-                for(i=0; i<n; i++)
+                for(i=0; i<n && iOutBufPtr <= 80; i++)
                     psInfo->szOutBuf[iOutBufPtr++] = ' ';
                 bPreviousCodeWasNumeric = 0;
             }
@@ -564,7 +564,7 @@ static const char *_UncompressNextLine(E00ReadPtr psInfo)
                  */
                 iCurDigit = 0;
                 while((c=_GetNextSourceChar(psInfo)) != '\0' &&
-                      c != ' ' && c != '~')
+                      c != ' ' && c != '~' && iOutBufPtr <= 80 )
                 {
                     n = c - '!';
                     if (n == 92 && (c=_GetNextSourceChar(psInfo)) != '\0')
