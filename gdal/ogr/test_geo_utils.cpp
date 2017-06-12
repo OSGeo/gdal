a85d8b6fb148f0a98a7df698363036a8e95fccb1
Even Rouault <even.rouault@mines-paris.org>
2017-06-12 19:41:20 +0000

    OGR_GreatCircle_ExtendPosition(): avoid division by zero in a few degenerate cases (and fix r39016). Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=2219. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@39084 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/test_geo_utils.cpp
+++ gdal/ogr/test_geo_utils.cpp
@@ -34,5 +34,17 @@ int main( int /* argc */ , char* /* argv */ [])
   OGR_GreatCircle_ExtendPosition(0, 100, 100000, 270, &latC, &lonC);
   printf("lat=%.15f, lon=%.15f\n", latC, lonC);
 
+  OGR_GreatCircle_ExtendPosition(0, 0, 100000, 0, &latC, &lonC);
+  printf("lat=%.15f, lon=%.15f\n", latC, lonC);
+
+  OGR_GreatCircle_ExtendPosition(0, 0, 100000, 90, &latC, &lonC);
+  printf("lat=%.15f, lon=%.15f\n", latC, lonC);
+
+  OGR_GreatCircle_ExtendPosition(0, 0, 100000, 180, &latC, &lonC);
+  printf("lat=%.15f, lon=%.15f\n", latC, lonC);
+
+  OGR_GreatCircle_ExtendPosition(0, 0, 100000, 270, &latC, &lonC);
+  printf("lat=%.15f, lon=%.15f\n", latC, lonC);
+
   return 0;
 }
