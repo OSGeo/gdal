370e556e439fcdf6ecb8fd0d90b75dcdc855281c
Even Rouault <even.rouault@mines-paris.org>
2017-06-14 08:39:39 +0000

    GPKG: fix potential null ptr deref. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=2240. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@39112 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/gpkg/ogrgeopackagedatasource.cpp
+++ gdal/ogr/ogrsf_frmts/gpkg/ogrgeopackagedatasource.cpp
@@ -1616,10 +1616,14 @@ bool GDALGeoPackageDataset::OpenRaster( const char* pszTableName,
     for( int i = 1; i < oResult.nRowCount; i++ )
     {
         GDALGeoPackageDataset* poOvrDS = new GDALGeoPackageDataset();
-        poOvrDS->InitRaster(
+        if( !poOvrDS->InitRaster(
             this, pszTableName, dfMinX, dfMinY, dfMaxX, dfMaxY,
             pszContentsMinX, pszContentsMinY, pszContentsMaxX, pszContentsMaxY,
-            papszOpenOptionsIn, oResult, i);
+            papszOpenOptionsIn, oResult, i) )
+        {
+            delete poOvrDS;
+            break;
+        }
 
         m_papoOverviewDS = (GDALGeoPackageDataset**) CPLRealloc(m_papoOverviewDS,
                         sizeof(GDALGeoPackageDataset*) * (m_nOverviewCount+1));
