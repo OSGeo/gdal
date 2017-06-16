4264d1bfb45bd70d96f2291d8761e6ae82dba7f9
Even Rouault <even.rouault@mines-paris.org>
2017-06-16 15:30:04 +0000

    CEOS: fix memleak in error code path. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=2279. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@39138 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/ceos/ceosopen.c
+++ gdal/frmts/ceos/ceosopen.c
@@ -165,8 +165,11 @@ CEOSRecord * CEOSReadRecord( CEOSImage *psImage )
 void CEOSDestroyRecord( CEOSRecord * psRecord )
 
 {
-    CPLFree( psRecord->pachData );
-    CPLFree( psRecord );
+    if( psRecord )
+    {
+        CPLFree( psRecord->pachData );
+        CPLFree( psRecord );
+    }
 }
 
 /************************************************************************/
@@ -235,6 +238,7 @@ CEOSImage * CEOSOpen( const char * pszFilename, const char * pszAccess )
     psRecord = CEOSReadRecord( psImage );
     if( psRecord == NULL || psRecord->nLength < 288 + 4 )
     {
+        CEOSDestroyRecord( psRecord );
         CEOSClose( psImage );
         return NULL;
     }
