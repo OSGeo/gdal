ec034eff85ab8406a8026576a819a6f9e0f36b31
Even Rouault <even.rouault@mines-paris.org>
2017-06-20 19:09:13 +0000

    PLScenes: remove support for V0 and V1 API. Only Data V1 is kept (fixes #6933)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@39214 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/plscenes/ogrplscenesdriver.cpp
+++ gdal/ogr/ogrsf_frmts/plscenes/ogrplscenesdriver.cpp
@@ -52,16 +52,24 @@ static GDALDataset* OGRPLScenesOpen(GDALOpenInfo* poOpenInfo)
             poOpenInfo->pszFilename+strlen("PLScenes:"), ",", TRUE, FALSE );
     CPLString osVersion = CSLFetchNameValueDef(papszOptions, "version",
                 CSLFetchNameValueDef(poOpenInfo->papszOpenOptions, "VERSION", ""));
-    CSLDestroy(papszOptions);
-    if( EQUAL(osVersion, "v0"))
-        return OGRPLScenesDataset::Open(poOpenInfo);
-    if( EQUAL(osVersion, "v1") )
-        return OGRPLScenesV1Dataset::Open(poOpenInfo);
-    if( EQUAL(osVersion, "data_v1") || EQUAL(osVersion, "")  )
+
+    if( EQUAL(osVersion, "v0") || EQUAL(osVersion, "v1") )
+    {
+        CPLError(CE_Failure, CPLE_AppDefined,
+                 "This API version has been removed or deprecated. "
+                 "Please use DATA_V1 API instead");
+        return NULL;
+    }
+    else if( EQUAL(osVersion, "data_v1") || EQUAL(osVersion, "")  )
+    {
         return OGRPLScenesDataV1Dataset::Open(poOpenInfo);
-    CPLError(CE_Failure, CPLE_AppDefined,
-             "Unhandled API version: %s", osVersion.c_str());
-    return NULL;
+    }
+    else
+    {
+        CPLError(CE_Failure, CPLE_AppDefined,
+                "Unhandled API version: %s", osVersion.c_str());
+        return NULL;
+    }
 }
 
 /************************************************************************/
@@ -85,19 +93,17 @@ void RegisterOGRPLSCENES()
     poDriver->SetMetadataItem( GDAL_DMD_OPENOPTIONLIST,
 "<OpenOptionList>"
 "  <Option name='VERSION' type='string-select' description='API version' default='DATA_V1'>"
-"    <Value>V0</Value>"
-"    <Value>V1</Value>"
 "    <Value>DATA_V1</Value>"
 "  </Option>"
 "  <Option name='API_KEY' type='string' description='Account API key' required='true'/>"
-"  <Option name='FOLLOW_LINKS' type='boolean' description='Whether assets links should be followed for each scene (API v1 / data v1 only)' default='NO'/>"
+"  <Option name='FOLLOW_LINKS' type='boolean' description='Whether assets links should be followed for each scene' default='NO'/>"
 "  <Option name='SCENE' type='string' description='Scene id (for raster fetching)'/>"
-"  <Option name='ITEMTYPES' alias='CATALOG' type='string' description='Catalog id (API v1 / data v1 only, mandatory for raster fetching)'/>"
-"  <Option name='ASSET' alias='PRODUCT_TYPE' type='string' description='Asset category/Product type. For v0 API: visual, analytic or thumb (for raster fetching)' default='visual'/>"
+"  <Option name='ITEMTYPES' alias='CATALOG' type='string' description='Catalog id (mandatory for raster fetching)'/>"
+"  <Option name='ASSET' type='string' description='Asset category' default='visual'/>"
 "  <Option name='RANDOM_ACCESS' type='boolean' description='Whether raster should be accessed in random access mode (but with potentially not optimal throughput). If no, in-memory ingestion is done' default='YES'/>"
-"  <Option name='ACTIVATION_TIMEOUT' type='int' description='Number of seconds during which to wait for asset activation (API v1 / data v1 only, raster)' default='3600'/>"
-"  <Option name='FILTER' type='string' description='Custom filter (API v1 / data v1 only)'/>"
-"  <Option name='METADATA' type='boolean' description='(Raster only, API Data V1) Whether scene metadata should be fetched from the API and attached to the raster dataset' default='YES'/>"
+"  <Option name='ACTIVATION_TIMEOUT' type='int' description='Number of seconds during which to wait for asset activation (raster)' default='3600'/>"
+"  <Option name='FILTER' type='string' description='Custom filter'/>"
+"  <Option name='METADATA' type='boolean' description='(Raster only) Whether scene metadata should be fetched from the API and attached to the raster dataset' default='YES'/>"
 "</OpenOptionList>");
 
     poDriver->pfnOpen = OGRPLScenesOpen;
