376d7409af43a1d461440e6c04b7476b8f737a2b
Even Rouault <even.rouault@mines-paris.org>
2017-06-22 09:00:36 +0000

    GCC 8.0 -Wclass-memaccess fixes
    git-svn-id: https://svn.osgeo.org/gdal/trunk@39232 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrlinestring.cpp
+++ gdal/ogr/ogrlinestring.cpp
@@ -451,8 +451,16 @@ void OGRSimpleCurve::setNumPoints( int nNewPointCount, int bZeroizeNewContent )
         paoPoints = paoNewPoints;
 
         if( bZeroizeNewContent )
-            memset( paoPoints + nPointCount,
-                0, sizeof(OGRRawPoint) * (nNewPointCount - nPointCount) );
+        {
+            // gcc 8.0 (dev) complains about -Wclass-memaccess
+            // since OGRRawPoint() has a constructor. So use a void* pointer.
+            // Doing the memset() here is correct since the constructor sets to 0.
+            // We could instad use a std::fill(), but at every other places
+            // we treat this class as a regular POD (see above use of realloc())
+            void* dest = static_cast<void*>(paoPoints + nPointCount);
+            memset( dest,
+                    0, sizeof(OGRRawPoint) * (nNewPointCount - nPointCount) );
+        }
 
         if( flags & OGR_G_3D )
         {
