OSGeo/gdal

commit 1c764d5ba9e06707bc00c76a654a2c4bff9c84e3

GDALProxyPoolDataset/GDALProxyRasterBand: don't open underlying dataset if not already opened. This is useless and could potentially lead to recursion in some situations (VRT FlushCache implementation to follow) (relates to #6939)
git-svn-id: https://svn.osgeo.org/gdal/trunk@39379 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/gcore/gdalproxypool.cpp
@@ -105,7 +105,8 @@ class GDALDatasetPool
         GDALProxyPoolCacheEntry* _RefDataset(const char* pszFileName,
                                              GDALAccess eAccess,
                                              char** papszOpenOptions,
-                                             int bShared);
+                                             int bShared,
+                                             bool bForceOpen);
         void _CloseDataset(const char* pszFileName, GDALAccess eAccess);
 
 #ifdef DEBUG_PROXY_POOL
@@ -120,7 +121,8 @@ class GDALDatasetPool
         static GDALProxyPoolCacheEntry* RefDataset(const char* pszFileName,
                                                    GDALAccess eAccess,
                                                    char** papszOpenOptions,
-                                                   int bShared);
+                                                   int bShared,
+                                                   bool bForceOpen);
         static void UnrefDataset(GDALProxyPoolCacheEntry* cacheEntry);
         static void CloseDataset(const char* pszFileName, GDALAccess eAccess);
 
@@ -211,7 +213,8 @@ void GDALDatasetPool::CheckLinks()
 GDALProxyPoolCacheEntry* GDALDatasetPool::_RefDataset(const char* pszFileName,
                                                       GDALAccess eAccess,
                                                       char** papszOpenOptions,
-                                                      int bShared)
+                                                      int bShared,
+                                                      bool bForceOpen)
 {
     GDALProxyPoolCacheEntry* cur = firstEntry;
     GIntBig responsiblePID = GDALGetResponsiblePIDForCurrentThread();
@@ -252,6 +255,9 @@ GDALProxyPoolCacheEntry* GDALDatasetPool::_RefDataset(const char* pszFileName,
 
         cur = next;
     }
+
+    if( !bForceOpen )
+        return NULL;
 
     if (currentSize == maxSize)
     {
@@ -457,10 +463,12 @@ void GDALDatasetPoolForceDestroy()
 GDALProxyPoolCacheEntry* GDALDatasetPool::RefDataset(const char* pszFileName,
                                                      GDALAccess eAccess,
                                                      char** papszOpenOptions,
-                                                     int bShared)
+                                                     int bShared,
+                                                     bool bForceOpen)
 {
     CPLMutexHolderD( GDALGetphDLMutex() );
-    return singleton->_RefDataset(pszFileName, eAccess, papszOpenOptions, bShared);
+    return singleton->_RefDataset(pszFileName, eAccess, papszOpenOptions,
+                                  bShared, bForceOpen);
 }
 
 /************************************************************************/
@@ -673,6 +681,11 @@ void GDALProxyPoolDataset::AddSrcBandDescription( GDALDataType eDataType, int nB
 
 GDALDataset* GDALProxyPoolDataset::RefUnderlyingDataset()
 {
+    return RefUnderlyingDataset(true);
+}
+
+GDALDataset* GDALProxyPoolDataset::RefUnderlyingDataset(bool bForceOpen)
+{
     /* We pretend that the current thread is responsiblePID, that is */
     /* to say the thread that created that GDALProxyPoolDataset object. */
     /* This is for the case when a GDALProxyPoolDataset is created by a */
@@ -686,7 +699,7 @@ GDALDataset* GDALProxyPoolDataset::RefUnderlyingDataset()
     GIntBig curResponsiblePID = GDALGetResponsiblePIDForCurrentThread();
     GDALSetResponsiblePIDForCurrentThread(responsiblePID);
     cacheEntry = GDALDatasetPool::RefDataset(GetDescription(), eAccess, papszOpenOptions,
-                                             GetShared());
+                                             GetShared(), bForceOpen);
     GDALSetResponsiblePIDForCurrentThread(curResponsiblePID);
     if (cacheEntry != NULL)
     {
@@ -710,6 +723,20 @@ void GDALProxyPoolDataset::UnrefUnderlyingDataset(
         CPLAssert(cacheEntry->poDS == poUnderlyingDataset);
         if (cacheEntry->poDS != NULL)
             GDALDatasetPool::UnrefDataset(cacheEntry);
+    }
+}
+
+/************************************************************************/
+/*                         FlushCache()                                 */
+/************************************************************************/
+
+void  GDALProxyPoolDataset::FlushCache()
+{
+    GDALDataset* poUnderlyingDataset = RefUnderlyingDataset(false);
+    if (poUnderlyingDataset)
+    {
+        poUnderlyingDataset->FlushCache();
+        UnrefUnderlyingDataset(poUnderlyingDataset);
     }
 }
 
@@ -1013,9 +1040,9 @@ void GDALProxyPoolRasterBand::AddSrcMaskBandDescription( GDALDataType eDataTypeI
 /*                  RefUnderlyingRasterBand()                           */
 /************************************************************************/
 
-GDALRasterBand* GDALProxyPoolRasterBand::RefUnderlyingRasterBand()
+GDALRasterBand* GDALProxyPoolRasterBand::RefUnderlyingRasterBand(bool bForceOpen)
 {
-    GDALDataset* poUnderlyingDataset = ((GDALProxyPoolDataset*)poDS)->RefUnderlyingDataset();
+    GDALDataset* poUnderlyingDataset = ((GDALProxyPoolDataset*)poDS)->RefUnderlyingDataset(bForceOpen);
     if (poUnderlyingDataset == NULL)
         return NULL;
 
@@ -1028,6 +1055,11 @@ GDALRasterBand* GDALProxyPoolRasterBand::RefUnderlyingRasterBand()
     return poBand;
 }
 
+GDALRasterBand* GDALProxyPoolRasterBand::RefUnderlyingRasterBand()
+{
+    return RefUnderlyingRasterBand(true);
+}
+
 /************************************************************************/
 /*                  UnrefUnderlyingRasterBand()                       */
 /************************************************************************/
@@ -1036,6 +1068,22 @@ void GDALProxyPoolRasterBand::UnrefUnderlyingRasterBand(GDALRasterBand* poUnderl
 {
     if (poUnderlyingRasterBand)
         ((GDALProxyPoolDataset*)poDS)->UnrefUnderlyingDataset(poUnderlyingRasterBand->GetDataset());
+}
+
+/************************************************************************/
+/*                             FlushCache()                             */
+/************************************************************************/
+
+CPLErr GDALProxyPoolRasterBand::FlushCache()
+{
+    GDALRasterBand* poUnderlyingRasterBand = RefUnderlyingRasterBand(false);
+    if (poUnderlyingRasterBand)
+    {
+        CPLErr eErr = poUnderlyingRasterBand->FlushCache();
+        UnrefUnderlyingRasterBand(poUnderlyingRasterBand);
+        return eErr;
+    }
+    return CE_None;
 }
 
 /************************************************************************/

gdal/gcore/gdal_proxy.h
@@ -222,6 +222,8 @@ class CPL_DLL GDALProxyPoolDataset : public GDALProxyDataset
 
         GDALProxyPoolCacheEntry* cacheEntry;
 
+        GDALDataset *RefUnderlyingDataset(bool bForceOpen);
+
     protected:
         virtual GDALDataset *RefUnderlyingDataset() CPL_OVERRIDE;
         virtual void UnrefUnderlyingDataset(GDALDataset* poUnderlyingDataset) CPL_OVERRIDE;
@@ -239,6 +241,8 @@ class CPL_DLL GDALProxyPoolDataset : public GDALProxyDataset
 
         void         SetOpenOptions(char** papszOpenOptions);
         void         AddSrcBandDescription( GDALDataType eDataType, int nBlockXSize, int nBlockYSize);
+
+        virtual void FlushCache(void) CPL_OVERRIDE;
 
         virtual const char *GetProjectionRef(void) CPL_OVERRIDE;
         virtual CPLErr SetProjection( const char * ) CPL_OVERRIDE;
@@ -283,6 +287,8 @@ class CPL_DLL GDALProxyPoolRasterBand : public GDALProxyRasterBand
 
         void Init();
 
+        GDALRasterBand* RefUnderlyingRasterBand(bool bForceOpen);
+
     protected:
         virtual GDALRasterBand* RefUnderlyingRasterBand() CPL_OVERRIDE;
         virtual void UnrefUnderlyingRasterBand(GDALRasterBand* poUnderlyingRasterBand) CPL_OVERRIDE;
@@ -312,6 +318,8 @@ class CPL_DLL GDALProxyPoolRasterBand : public GDALProxyRasterBand
         virtual GDALRasterBand *GetOverview(int) CPL_OVERRIDE;
         virtual GDALRasterBand *GetRasterSampleOverview( GUIntBig nDesiredSamples) CPL_OVERRIDE; // TODO
         virtual GDALRasterBand *GetMaskBand() CPL_OVERRIDE;
+
+        virtual CPLErr FlushCache() CPL_OVERRIDE;
   private:
     CPL_DISALLOW_COPY_ASSIGN(GDALProxyPoolRasterBand)
 };

commit 2ee44a0861d7fdc5956c262186339dcdcb4b286d

Proxy pool: fix breakage introduced in r39379 / r39380 that make vrt_read_18 test do use-after-free
git-svn-id: https://svn.osgeo.org/gdal/trunk@39382 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/gcore/gdalproxypool.cpp
@@ -79,6 +79,8 @@ struct _GDALProxyPoolCacheEntry
 class GDALDatasetPool
 {
     private:
+        bool bInDestruction;
+
         /* Ref count of the pool singleton */
         /* Taken by "toplevel" GDALProxyPoolDataset in its constructor and released */
         /* in its destructor. See also refCountOfDisableRefCount for the difference */
@@ -136,6 +138,7 @@ class GDALDatasetPool
 
 GDALDatasetPool::GDALDatasetPool(int maxSizeIn)
 {
+    bInDestruction = false;
     maxSize = maxSizeIn;
     currentSize = 0;
     firstEntry = NULL;
@@ -150,6 +153,7 @@ GDALDatasetPool::GDALDatasetPool(int maxSizeIn)
 
 GDALDatasetPool::~GDALDatasetPool()
 {
+    bInDestruction = true;
     GDALProxyPoolCacheEntry* cur = firstEntry;
     GIntBig responsiblePID = GDALGetResponsiblePIDForCurrentThread();
     while(cur)
@@ -216,6 +220,9 @@ GDALProxyPoolCacheEntry* GDALDatasetPool::_RefDataset(const char* pszFileName,
                                                       int bShared,
                                                       bool bForceOpen)
 {
+    if( bInDestruction )
+        return NULL;
+
     GDALProxyPoolCacheEntry* cur = firstEntry;
     GIntBig responsiblePID = GDALGetResponsiblePIDForCurrentThread();
     GDALProxyPoolCacheEntry* lastEntryWithZeroRefCount = NULL;