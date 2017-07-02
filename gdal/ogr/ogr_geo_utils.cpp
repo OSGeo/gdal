eb328060b7f28fb86d477acb9fc713e532824830
Even Rouault <even.rouault@mines-paris.org>
2017-07-02 11:18:59 +0000

    OGR_GreatCircle_ExtendPosition(): avoid division by zero. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=2453. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@39437 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogr_geo_utils.cpp
+++ gdal/ogr/ogr_geo_utils.cpp
@@ -153,10 +153,10 @@ int OGR_GreatCircle_ExtendPosition(double dfLatA_deg, double dfLonA_deg,
         return 0;
     }
 
-    if( fabs(sin_Heading) < 1e-9 )
+    if( fabs(sin_Heading) < 1e-8 )
     {
         *pdfLonB_deg = dfLonA_deg;
-        if( fabs(fmod(dfHeadingInA+360.0,360.0)) < 1e-9 )
+        if( fabs(fmod(dfHeadingInA+360.0,360.0)) < 1e-8 )
         {
             *pdfLatB_deg = dfLatA_deg + dfDistanceRad * RAD2DEG;
         }
@@ -167,10 +167,10 @@ int OGR_GreatCircle_ExtendPosition(double dfLatA_deg, double dfLonA_deg,
         return 1;
     }
 
-    if( fabs(cos_complement_LatA) < 1e-9 && fabs(cos_Heading) < 1e-9 )
+    if( fabs(cos_complement_LatA) < 1e-8 && fabs(cos_Heading) < 1e-8 )
     {
         *pdfLatB_deg = dfLatA_deg;
-        if( fabs(dfHeadingInA - 90.0) < 1e-9 )
+        if( fabs(dfHeadingInA - 90.0) < 1e-8 )
         {
             *pdfLonB_deg = dfLonA_deg + dfDistanceRad * RAD2DEG;
         }
