0dd6d28f05d51f3f94079b6b691e173939baf598
Even Rouault <even.rouault@mines-paris.org>
2017-07-02 14:14:30 +0000

    Internal libtiff: resync with CVS HEAD. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=2454. Credit to OSS Fuzz. (Bug was likely introduced just a few days ago)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@39438 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/gtiff/libtiff/tif_read.c
+++ gdal/frmts/gtiff/libtiff/tif_read.c
@@ -1,4 +1,4 @@
-/* $Id: tif_read.c,v 1.62 2017-06-30 13:11:18 erouault Exp $ */
+/* $Id: tif_read.c,v 1.63 2017-07-02 14:13:39 erouault Exp $ */
 
 /*
  * Copyright (c) 1988-1997 Sam Leffler
@@ -1421,7 +1421,10 @@ TIFFStartTile(TIFF* tif, uint32 tile)
 	else
 	{
 		tif->tif_rawcp = tif->tif_rawdata;
-		tif->tif_rawcc = (tmsize_t)td->td_stripbytecount[tile];
+		if( tif->tif_rawdataloaded > 0 )
+			tif->tif_rawcc = tif->tif_rawdataloaded;
+		else
+			tif->tif_rawcc = (tmsize_t)td->td_stripbytecount[tile];
 	}
 	return ((*tif->tif_predecode)(tif,
 			(uint16)(tile/td->td_stripsperimage)));
