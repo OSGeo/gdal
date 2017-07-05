ce481b60c41df2bc624012b01661ad6827df5bf8
Even Rouault <even.rouault@mines-paris.org>
2017-07-05 14:10:45 +0000

    cpl_safemaths.hpp: for MSVC 2010 or later, use safeint.h (refs https://trac.osgeo.org/gdal/ticket/6229)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@39490 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/port/cpl_safemaths.hpp
+++ gdal/port/cpl_safemaths.hpp
@@ -40,6 +40,10 @@
 #if __GNUC__ >= 5 || __has_builtin(__builtin_sadd_overflow)
 #  define BUILTIN_OVERFLOW_CHECK_AVAILABLE
 
+#elif defined(_MSC_VER) && _MSC_VER >= 1600
+
+#  include "safeint.h"
+
 #elif defined(GDAL_COMPILATION)
 #  include "cpl_port.h"
 
@@ -107,6 +111,21 @@ inline CPLSafeInt<int> CPLSM(GUInt64 x);
 inline CPLSafeInt<unsigned> CPLSM_TO_UNSIGNED(GUInt64 x);
 #endif
 
+#if defined(_MSC_VER) && _MSC_VER >= 1600
+class CPLMSVCSafeIntException : public msl::utilities::SafeIntException
+{
+public:
+    static void CPLMSVCSafeIntException::SafeIntOnOverflow()
+    {
+        throw CPLSafeIntOverflow();
+    }
+    static void CPLMSVCSafeIntException::SafeIntOnDivZero()
+    {
+        throw CPLSafeIntOverflowDivisionByZero();
+    }
+};
+#endif
+
 inline CPLSafeInt<int> operator+( const CPLSafeInt<int>& A,
                                   const CPLSafeInt<int>& B )
 {
@@ -115,6 +134,10 @@ inline CPLSafeInt<int> operator+( const CPLSafeInt<int>& A,
     if( __builtin_sadd_overflow(A.v(), B.v(), &res) )
         throw CPLSafeIntOverflow();
     return CPLSM(res);
+#elif defined(_MSC_VER) && _MSC_VER >= 1600
+    msl::utilities::SafeInt<int, CPLMSVCSafeIntException> A2(A.v());
+    msl::utilities::SafeInt<int, CPLMSVCSafeIntException> B2(B.v());
+    return CPLSM(static_cast<int>(A2 + B2));
 #elif defined(CPL_HAS_GINT64)
     const int a = A.v();
     const int b = B.v();
@@ -144,6 +167,10 @@ inline CPLSafeInt<unsigned> operator+( const CPLSafeInt<unsigned>& A,
     if( __builtin_uadd_overflow(A.v(), B.v(), &res) )
         throw CPLSafeIntOverflow();
     return CPLSM(res);
+#elif defined(_MSC_VER) && _MSC_VER >= 1600
+    msl::utilities::SafeInt<unsigned, CPLMSVCSafeIntException> A2(A.v());
+    msl::utilities::SafeInt<unsigned, CPLMSVCSafeIntException> B2(B.v());
+    return CPLSM(static_cast<unsigned>(A2 + B2));
 #else
     const unsigned a = A.v();
     const unsigned b = B.v();
@@ -161,6 +188,10 @@ inline CPLSafeInt<int> operator-( const CPLSafeInt<int>& A,
     if( __builtin_ssub_overflow(A.v(), B.v(), &res) )
         throw CPLSafeIntOverflow();
     return CPLSM(res);
+#elif defined(_MSC_VER) && _MSC_VER >= 1600
+    msl::utilities::SafeInt<int, CPLMSVCSafeIntException> A2(A.v());
+    msl::utilities::SafeInt<int, CPLMSVCSafeIntException> B2(B.v());
+    return CPLSM(static_cast<int>(A2 - B2));
 #elif defined(CPL_HAS_GINT64)
     const int a = A.v();
     const int b = B.v();
@@ -191,6 +222,10 @@ inline CPLSafeInt<unsigned> operator-( const CPLSafeInt<unsigned>& A,
     if( __builtin_usub_overflow(A.v(), B.v(), &res) )
         throw CPLSafeIntOverflow();
     return CPLSM(res);
+#elif defined(_MSC_VER) && _MSC_VER >= 1600
+    msl::utilities::SafeInt<unsigned, CPLMSVCSafeIntException> A2(A.v());
+    msl::utilities::SafeInt<unsigned, CPLMSVCSafeIntException> B2(B.v());
+    return CPLSM(static_cast<unsigned>(A2 - B2));
 #else
     const unsigned a = A.v();
     const unsigned b = B.v();
@@ -208,6 +243,10 @@ inline CPLSafeInt<int> operator*( const CPLSafeInt<int>& A,
     if( __builtin_smul_overflow(A.v(), B.v(), &res) )
         throw CPLSafeIntOverflow();
     return CPLSM(res);
+#elif defined(_MSC_VER) && _MSC_VER >= 1600
+    msl::utilities::SafeInt<int, CPLMSVCSafeIntException> A2(A.v());
+    msl::utilities::SafeInt<int, CPLMSVCSafeIntException> B2(B.v());
+    return CPLSM(static_cast<int>(A2 * B2));
 #elif defined(CPL_HAS_GINT64)
     const int a = A.v();
     const int b = B.v();
@@ -252,6 +291,10 @@ inline CPLSafeInt<unsigned> operator*( const CPLSafeInt<unsigned>& A,
     if( __builtin_umul_overflow(A.v(), B.v(), &res) )
         throw CPLSafeIntOverflow();
     return CPLSM(res);
+#elif defined(_MSC_VER) && _MSC_VER >= 1600
+    msl::utilities::SafeInt<unsigned, CPLMSVCSafeIntException> A2(A.v());
+    msl::utilities::SafeInt<unsigned, CPLMSVCSafeIntException> B2(B.v());
+    return CPLSM(static_cast<unsigned>(A2 * B2));
 #elif defined(CPL_HAS_GINT64)
     const unsigned a = A.v();
     const unsigned b = B.v();
