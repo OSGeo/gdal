aff002d1dca165a9c06d0856669fc37079a73b97
Even Rouault <even.rouault@mines-paris.org>
2017-07-24 10:52:34 +0000

    INGR: avoid excessive memory allocation. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=2694. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@39634 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/ingr/IntergraphBand.cpp
+++ gdal/frmts/ingr/IntergraphBand.cpp
@@ -562,6 +562,17 @@ IntergraphRLEBand::IntergraphRLEBand( IntergraphDataset *poDSIn,
         if( eFormat == RunLengthEncodedC || eFormat == RunLengthEncoded )
         {
             nBlockYSize = 1;
+            if( nRasterYSize > 1024 * 1024 )
+            {
+                VSIFSeekL( poDSIn->fp, 0, SEEK_END );
+                // At the very least 2 bytes per row (probably more)
+                if( VSIFTellL( poDSIn->fp ) / 2 <
+                                    static_cast<vsi_l_offset>(nRasterYSize) )
+                {
+                    CPLError(CE_Failure, CPLE_AppDefined, "File too short");
+                    return;
+                }
+            }
             panRLELineOffset = (uint32 *)
                 VSI_CALLOC_VERBOSE(sizeof(uint32),nRasterYSize);
             if( panRLELineOffset == NULL )
