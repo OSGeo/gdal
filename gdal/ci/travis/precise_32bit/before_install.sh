d44d4bbc09a6b523b858335be1d436ceb6a7ecff
Even Rouault <even.rouault@mines-paris.org>
2017-07-29 20:30:59 +0000

    ci: disable gta in trustry_clang and precise_32bit
    git-svn-id: https://svn.osgeo.org/gdal/branches/2.2@39686 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ci/travis/precise_32bit/before_install.sh
+++ gdal/ci/travis/precise_32bit/before_install.sh
@@ -16,11 +16,12 @@ sudo i386 chroot "$chroot" apt-get update
 sudo i386 chroot "$chroot" apt-get install -y clang
 sudo i386 chroot "$chroot" apt-get install -y python-software-properties
 sudo i386 chroot "$chroot" add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
-sudo i386 chroot "$chroot" add-apt-repository -y ppa:marlam/gta
+#sudo i386 chroot "$chroot" add-apt-repository -y ppa:marlam/gta
 sudo i386 chroot "$chroot" apt-get update
 # Disable postgresql since it draws ssl-cert that doesn't install cleanly
 # postgis postgresql-9.1 postgresql-client-9.1 postgresql-9.1-postgis-2.1 postgresql-9.1-postgis-2.1-scripts libpq-dev
-sudo i386 chroot "$chroot" apt-get install -y python-numpy libpng12-dev libjpeg-dev libgif-dev liblzma-dev libgeos-dev libcurl4-gnutls-dev libproj-dev libxml2-dev libexpat-dev libxerces-c-dev libnetcdf-dev netcdf-bin libpoppler-dev libspatialite-dev gpsbabel swig libhdf4-alt-dev libhdf5-serial-dev libpodofo-dev poppler-utils libfreexl-dev unixodbc-dev libwebp-dev libepsilon-dev libgta-dev liblcms2-2 libpcre3-dev libcrypto++-dev
+sudo i386 chroot "$chroot" apt-get install -y python-numpy libpng12-dev libjpeg-dev libgif-dev liblzma-dev libgeos-dev libcurl4-gnutls-dev libproj-dev libxml2-dev libexpat-dev libxerces-c-dev libnetcdf-dev netcdf-bin libpoppler-dev libspatialite-dev gpsbabel swig libhdf4-alt-dev libhdf5-serial-dev libpodofo-dev poppler-utils libfreexl-dev unixodbc-dev libwebp-dev libepsilon-dev  liblcms2-2 libpcre3-dev libcrypto++-dev
+# libgta-dev
 sudo i386 chroot "$chroot" apt-get install -y make
 sudo i386 chroot "$chroot" apt-get install -y python-dev
 sudo i386 chroot "$chroot" apt-get install -y g++
--- gdal/ci/travis/trusty_clang/install.sh
+++ gdal/ci/travis/trusty_clang/install.sh
@@ -5,7 +5,8 @@ set -e
 cd gdal
 # --with-mongocxx=/usr/local
 export CCACHE_CPP2=yes
-CC="ccache clang" CXX="ccache clang" LDFLAGS="-lstdc++" ./configure --prefix=/usr --without-libtool --with-jpeg12 --with-python --with-poppler --with-podofo --with-spatialite --with-mysql --with-liblzma --with-webp --with-java --with-mdb --with-jvm-lib-add-rpath --with-epsilon --with-gta --with-ecw=/usr/local --with-mrsid=/usr/local --with-mrsid-lidar=/usr/local --with-fgdb=/usr/local --with-libkml --with-openjpeg=/usr/local
+CC="ccache clang" CXX="ccache clang" LDFLAGS="-lstdc++" ./configure --prefix=/usr --without-libtool --with-jpeg12 --with-python --with-poppler --with-podofo --with-spatialite --with-mysql --with-liblzma --with-webp --with-java --with-mdb --with-jvm-lib-add-rpath --with-epsilon --with-ecw=/usr/local --with-mrsid=/usr/local --with-mrsid-lidar=/usr/local --with-fgdb=/usr/local --with-libkml --with-openjpeg=/usr/local
+# --with-gta
 # Those ln -s are weird but otherwise Python bindings build fail with clang not being found
 sudo ln -s /usr/local/clang-3.5.0/bin/clang /usr/bin/clang
 sudo ln -s /usr/local/clang-3.5.0/bin/clang++ /usr/bin/clang++
