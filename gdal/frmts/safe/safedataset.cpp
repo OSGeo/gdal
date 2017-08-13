e90daf516d9bf96ae2ef9a73e835d0cd3ec1c4d6
Even Rouault <even.rouault@mines-paris.org>
2017-08-13 12:46:17 +0000

    SAFE: remove GDAL_DMD_SUBDATASETS = 'NO' driver metadata as it unusual to advertize non-existing capabilities
    git-svn-id: https://svn.osgeo.org/gdal/trunk@39798 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/safe/safedataset.cpp
+++ gdal/frmts/safe/safedataset.cpp
@@ -1214,7 +1214,6 @@ void GDALRegister_SAFE()
     poDriver->SetMetadataItem( GDAL_DMD_LONGNAME,
                                "Sentinel-1 SAR SAFE Product" );
     poDriver->SetMetadataItem( GDAL_DMD_HELPTOPIC, "frmt_safe.html" );
-    poDriver->SetMetadataItem( GDAL_DMD_SUBDATASETS, "NO" );
 
     poDriver->pfnOpen = SAFEDataset::Open;
     poDriver->pfnIdentify = SAFEDataset::Identify;
