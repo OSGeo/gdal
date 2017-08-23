e60bbf52e15368a530a16d8c33181ba825662350
Even Rouault <even.rouault@mines-paris.org>
2017-08-23 13:46:51 +0000

    GPKG: remove UNIQUE keyword for PRIMARY KEY of gpkg_metadata (doesn't hurt, but just to align with the examples of DDL in the GeoPackage spec)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@39916 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/gpkg/ogrgeopackagedatasource.cpp
+++ gdal/ogr/ogrsf_frmts/gpkg/ogrgeopackagedatasource.cpp
@@ -2703,7 +2703,7 @@ bool GDALGeoPackageDataset::CreateMetadataTables()
     /* From C.10. gpkg_metadata Table 35. gpkg_metadata Table Definition SQL  */
     CPLString osSQL =
         "CREATE TABLE gpkg_metadata ("
-        "id INTEGER CONSTRAINT m_pk PRIMARY KEY ASC NOT NULL UNIQUE,"
+        "id INTEGER CONSTRAINT m_pk PRIMARY KEY ASC NOT NULL,"
         "md_scope TEXT NOT NULL DEFAULT 'dataset',"
         "md_standard_uri TEXT NOT NULL,"
         "mime_type TEXT NOT NULL DEFAULT 'text/xml',"
