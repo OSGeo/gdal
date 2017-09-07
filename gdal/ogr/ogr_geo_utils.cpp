3b0eb0cd7a2882db750b70256c89690a3a8fb434
Even Rouault <even.rouault@mines-paris.org>
2017-09-07 12:08:12 +0000

    OGR_GreatCircle_InitialHeading(): avoid division by zero. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=3308. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@40023 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogr_geo_utils.cpp
+++ gdal/ogr/ogr_geo_utils.cpp
@@ -81,7 +81,7 @@ double OGR_GreatCircle_InitialHeading( double LatA_deg, double LonA_deg,
     {
         return 0;
     }
-    else if( fabs(LonA_deg - LonB_deg) < 1e-10 &&
+    else if( fabs(fmod(LonA_deg - LonB_deg, 360.0)) < 1e-10 &&
              fabs(LatA_deg - LatB_deg) < 1e-10 )
     {
         return 0; // Arbitrary number
@@ -90,7 +90,7 @@ double OGR_GreatCircle_InitialHeading( double LatA_deg, double LonA_deg,
     {
         return (LonB_deg > LonA_deg) ? 90.0 : 270.0;
     }
-    else if( fabs(LonA_deg - LonB_deg) < 1e-10 )
+    else if( fabs(fmod(LonA_deg - LonB_deg, 360.0)) < 1e-10 )
     {
         return (LatA_deg > LatB_deg) ? 180.0 : 0.0;
     }
