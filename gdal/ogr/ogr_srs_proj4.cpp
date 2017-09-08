c07253b7dd8060d78d0481a841936751569f1936
Kurt Schwehr <schwehr@gmail.com>
2017-09-08 21:19:36 +0000

    Range check utm zone and south in importFromProj4.
    Found by autofuzz
    
    
    git-svn-id: https://svn.osgeo.org/gdal/trunk@40038 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogr_srs_proj4.cpp
+++ gdal/ogr/ogr_srs_proj4.cpp
@@ -681,8 +681,15 @@ OGRErr OGRSpatialReference::importFromProj4( const char * pszProj4 )
     }
     else if( EQUAL(pszProj, "utm") )
     {
-        SetUTM(static_cast<int>(OSR_GDV( papszNV, "zone", 0.0)),
-               static_cast<int>(OSR_GDV( papszNV, "south", 1.0)) );
+        const double dfZone = OSR_GDV(papszNV, "zone", 0.0);
+        const double dfSouth = OSR_GDV(papszNV, "south", 1.0);
+        if( dfZone > 60.0 || dfZone < 0.0 || CPLIsNan(dfZone) ||
+            dfSouth < 0.0 || dfSouth > 1.0 )
+        {
+            CSLDestroy(papszNV);
+            return OGRERR_CORRUPT_DATA;
+        }
+        SetUTM(static_cast<int>(dfZone), static_cast<int>(dfSouth));
     }
     else if( EQUAL(pszProj, "merc")  // 2SP form.
              && OSR_GDV(papszNV, "lat_ts", 1000.0) < 999.0 )
