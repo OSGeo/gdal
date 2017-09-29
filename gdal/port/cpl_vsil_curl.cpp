4a1f65295aca2083d0062b2efa9ab27fa32e4f30
Even Rouault <even.rouault@mines-paris.org>
2017-09-29 14:22:08 +0000

    /vsis3/: fix Seek(Tell(), SEEK_SET) fails if current position is not 0 (fixes #7062)
    git-svn-id: https://svn.osgeo.org/gdal/branches/2.2@40268 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/port/cpl_vsil_curl.cpp
+++ gdal/port/cpl_vsil_curl.cpp
@@ -3676,8 +3676,9 @@ VSIS3WriteHandle::~VSIS3WriteHandle()
 
 int VSIS3WriteHandle::Seek( vsi_l_offset nOffset, int nWhence )
 {
-    if( (nWhence == SEEK_SET && nOffset != m_nCurOffset) ||
-        nOffset != 0 )
+    if( !((nWhence == SEEK_SET && nOffset == m_nCurOffset) ||
+          (nWhence == SEEK_CUR && nOffset == 0) ||
+          (nWhence == SEEK_END && nOffset == 0)) )
     {
         CPLError(CE_Failure, CPLE_NotSupported,
                  "Seek not supported on writable /vsis3 files");
