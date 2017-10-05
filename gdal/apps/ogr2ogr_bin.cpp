f4442503f4401c72dcce2c3ac5a76588968d8f4e
Kurt Schwehr <schwehr@gmail.com>
2017-10-05 12:49:28 +0000

    Cleanup ogr2ogr_bin.cpp
    - Move usage functions so that no prototypes are necessary
    - Partial cleanup of includes
    - int boolean to bool
    - Localize vars
    
    
    git-svn-id: https://svn.osgeo.org/gdal/trunk@40328 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/apps/ogr2ogr_bin.cpp
+++ gdal/apps/ogr2ogr_bin.cpp
@@ -31,13 +31,142 @@
 #include "ogr_p.h"
 #include "gdal_utils_priv.h"
 #include "commonutils.h"
-#include <vector>
+
 #include <algorithm>
+#include <string>
+#include <vector>
 
 CPL_CVSID("$Id$")
 
-static void Usage(int bShort = TRUE);
-static void Usage(const char* pszAdditionalMsg, int bShort = TRUE);
+/************************************************************************/
+/*                               Usage()                                */
+/************************************************************************/
+
+static bool StringCISortFunction(const CPLString& a, const CPLString& b)
+{
+    return STRCASECMP(a.c_str(), b.c_str()) < 0;
+}
+
+static void Usage(const char* pszAdditionalMsg, bool bShort = true)
+{
+    printf( "Usage: ogr2ogr [--help-general] [-skipfailures] [-append] [-update]\n"
+            "               [-select field_list] [-where restricted_where|@filename]\n"
+            "               [-progress] [-sql <sql statement>|@filename] [-dialect dialect]\n"
+            "               [-preserve_fid] [-fid FID] [-limit nb_features]\n"
+            "               [-spat xmin ymin xmax ymax] [-spat_srs srs_def] [-geomfield field]\n"
+            "               [-a_srs srs_def] [-t_srs srs_def] [-s_srs srs_def]\n"
+            "               [-f format_name] [-overwrite] [[-dsco NAME=VALUE] ...]\n"
+            "               dst_datasource_name src_datasource_name\n"
+            "               [-lco NAME=VALUE] [-nln name] \n"
+            "               [-nlt type|PROMOTE_TO_MULTI|CONVERT_TO_LINEAR|CONVERT_TO_CURVE]\n"
+            "               [-dim XY|XYZ|XYM|XYZM|layer_dim] [layer [layer ...]]\n"
+            "\n"
+            "Advanced options :\n"
+            "               [-gt n] [-ds_transaction]\n"
+            "               [[-oo NAME=VALUE] ...] [[-doo NAME=VALUE] ...]\n"
+            "               [-clipsrc [xmin ymin xmax ymax]|WKT|datasource|spat_extent]\n"
+            "               [-clipsrcsql sql_statement] [-clipsrclayer layer]\n"
+            "               [-clipsrcwhere expression]\n"
+            "               [-clipdst [xmin ymin xmax ymax]|WKT|datasource]\n"
+            "               [-clipdstsql sql_statement] [-clipdstlayer layer]\n"
+            "               [-clipdstwhere expression]\n"
+            "               [-wrapdateline][-datelineoffset val]\n"
+            "               [[-simplify tolerance] | [-segmentize max_dist]]\n"
+            "               [-addfields] [-unsetFid]\n"
+            "               [-relaxedFieldNameMatch] [-forceNullable] [-unsetDefault]\n"
+            "               [-fieldTypeToString All|(type1[,type2]*)] [-unsetFieldWidth]\n"
+            "               [-mapFieldType srctype|All=dsttype[,srctype2=dsttype2]*]\n"
+            "               [-fieldmap identity | index1[,index2]*]\n"
+            "               [-splitlistfields] [-maxsubfields val]\n"
+            "               [-explodecollections] [-zfield field_name]\n"
+            "               [-gcp pixel line easting northing [elevation]]* [-order n | -tps]\n"
+            "               [-nomd] [-mo \"META-TAG=VALUE\"]* [-noNativeData]\n");
+
+    if (bShort)
+    {
+        printf( "\nNote: ogr2ogr --long-usage for full help.\n");
+        if( pszAdditionalMsg )
+            fprintf(stderr, "\nFAILURE: %s\n", pszAdditionalMsg);
+        exit( 1 );
+    }
+
+    printf("\n -f format_name: output file format name, possible values are:\n");
+
+    std::vector<CPLString> aoSetDrivers;
+    OGRSFDriverRegistrar *poR = OGRSFDriverRegistrar::GetRegistrar();
+    for( int iDriver = 0; iDriver < poR->GetDriverCount(); iDriver++ )
+    {
+        GDALDriver *poDriver = poR->GetDriver(iDriver);
+
+        if( CPLTestBool( CSLFetchNameValueDef(poDriver->GetMetadata(), GDAL_DCAP_CREATE, "FALSE") ) )
+            aoSetDrivers.push_back( poDriver->GetDescription() );
+    }
+    std::sort (aoSetDrivers.begin(), aoSetDrivers.end(), StringCISortFunction);
+    for( size_t i = 0; i < aoSetDrivers.size(); i++ )
+    {
+        printf( "     -f \"%s\"\n", aoSetDrivers[i].c_str() );
+    }
+
+    printf( " -append: Append to existing layer instead of creating new if it exists\n"
+            " -overwrite: delete the output layer and recreate it empty\n"
+            " -update: Open existing output datasource in update mode\n"
+            " -progress: Display progress on terminal. Only works if input layers have the \n"
+            "                                          \"fast feature count\" capability\n"
+            " -select field_list: Comma-delimited list of fields from input layer to\n"
+            "                     copy to the new layer (defaults to all)\n"
+            " -where restricted_where: Attribute query (like SQL WHERE)\n"
+            " -wrapdateline: split geometries crossing the dateline meridian\n"
+            "                (long. = +/- 180deg)\n"
+            " -datelineoffset: offset from dateline in degrees\n"
+            "                (default long. = +/- 10deg,\n"
+            "                geometries within 170deg to -170deg will be split)\n"
+            " -sql statement: Execute given SQL statement and save result.\n"
+            " -dialect value: select a dialect, usually OGRSQL to avoid native sql.\n"
+            " -skipfailures: skip features or layers that fail to convert\n"
+            " -gt n: group n features per transaction (default 20000). n can be set to unlimited\n"
+            " -spat xmin ymin xmax ymax: spatial query extents\n"
+            " -simplify tolerance: distance tolerance for simplification.\n"
+            " -segmentize max_dist: maximum distance between 2 nodes.\n"
+            "                       Used to create intermediate points\n"
+            " -dsco NAME=VALUE: Dataset creation option (format specific)\n"
+            " -lco  NAME=VALUE: Layer creation option (format specific)\n"
+            " -oo   NAME=VALUE: Input dataset open option (format specific)\n"
+            " -doo  NAME=VALUE: Destination dataset open option (format specific)\n"
+            " -nln name: Assign an alternate name to the new layer\n"
+            " -nlt type: Force a geometry type for new layer.  One of NONE, GEOMETRY,\n"
+            "      POINT, LINESTRING, POLYGON, GEOMETRYCOLLECTION, MULTIPOINT,\n"
+            "      MULTIPOLYGON, or MULTILINESTRING, or PROMOTE_TO_MULTI or CONVERT_TO_LINEAR.  Add \"25D\" for 3D layers.\n"
+            "      Default is type of source layer.\n"
+            " -dim dimension: Force the coordinate dimension to the specified value.\n"
+            " -fieldTypeToString type1,...: Converts fields of specified types to\n"
+            "      fields of type string in the new layer. Valid types are : Integer,\n"
+            "      Integer64, Real, String, Date, Time, DateTime, Binary, IntegerList, Integer64List, RealList,\n"
+            "      StringList. Special value All will convert all fields to strings.\n"
+            " -fieldmap index1,index2,...: Specifies the list of field indexes to be\n"
+            "      copied from the source to the destination. The (n)th value specified\n"
+            "      in the list is the index of the field in the target layer definition\n"
+            "      in which the n(th) field of the source layer must be copied. Index count\n"
+            "      starts at zero. There must be exactly as many values in the list as\n"
+            "      the count of the fields in the source layer. We can use the 'identity'\n"
+            "      setting to specify that the fields should be transferred by using the\n"
+            "      same order. This setting should be used along with the append setting.");
+
+    printf(" -a_srs srs_def: Assign an output SRS\n"
+           " -t_srs srs_def: Reproject/transform to this SRS on output\n"
+           " -s_srs srs_def: Override source SRS\n"
+           "\n"
+           " Srs_def can be a full WKT definition (hard to escape properly),\n"
+           " or a well known definition (i.e. EPSG:4326) or a file with a WKT\n"
+           " definition.\n" );
+
+    if( pszAdditionalMsg )
+        fprintf(stderr, "\nFAILURE: %s\n", pszAdditionalMsg);
+}
+
+static void Usage(bool bShort = TRUE)
+{
+    Usage(NULL, bShort);
+}
 
 /************************************************************************/
 /*                 GDALVectorTranslateOptionsForBinaryNew()             */
@@ -73,7 +202,6 @@ static
 void CheckDestDataSourceNameConsistency(const char* pszDestFilename,
                                         const char* pszDriverName)
 {
-    int i;
     char* pszDestExtension = CPLStrdup(CPLGetExtension(pszDestFilename));
 
     if( EQUAL(pszDriverName, "GMT") )
@@ -92,7 +220,7 @@ void CheckDestDataSourceNameConsistency(const char* pszDestFilename,
                                                { NULL, NULL }
                                              };
 
-    for(i=0; apszBeginName[i][0] != NULL; i++)
+    for(int i=0; apszBeginName[i][0] != NULL; i++)
     {
         if (EQUALN(pszDestFilename, apszBeginName[i][0], strlen(apszBeginName[i][0])) &&
             !EQUAL(pszDriverName, apszBeginName[i][1]))
@@ -163,7 +291,7 @@ int main( int nArgc, char ** papszArgv )
         }
         else if ( EQUAL(papszArgv[iArg], "--long-usage") )
         {
-            Usage(FALSE);
+            Usage(false);
             goto exit;
         }
     }
@@ -313,136 +441,4 @@ exit:
     OGRCleanupAll();
 
     return nRetCode;
-}
-
-/************************************************************************/
-/*                               Usage()                                */
-/************************************************************************/
-
-static void Usage(int bShort)
-{
-    Usage(NULL, bShort);
-}
-
-static bool StringCISortFunction(const CPLString& a, const CPLString& b)
-{
-    return STRCASECMP(a.c_str(), b.c_str()) < 0;
-}
-
-static void Usage(const char* pszAdditionalMsg, int bShort)
-
-{
-    OGRSFDriverRegistrar        *poR = OGRSFDriverRegistrar::GetRegistrar();
-
-    printf( "Usage: ogr2ogr [--help-general] [-skipfailures] [-append] [-update]\n"
-            "               [-select field_list] [-where restricted_where|@filename]\n"
-            "               [-progress] [-sql <sql statement>|@filename] [-dialect dialect]\n"
-            "               [-preserve_fid] [-fid FID] [-limit nb_features]\n"
-            "               [-spat xmin ymin xmax ymax] [-spat_srs srs_def] [-geomfield field]\n"
-            "               [-a_srs srs_def] [-t_srs srs_def] [-s_srs srs_def]\n"
-            "               [-f format_name] [-overwrite] [[-dsco NAME=VALUE] ...]\n"
-            "               dst_datasource_name src_datasource_name\n"
-            "               [-lco NAME=VALUE] [-nln name] \n"
-            "               [-nlt type|PROMOTE_TO_MULTI|CONVERT_TO_LINEAR|CONVERT_TO_CURVE]\n"
-            "               [-dim XY|XYZ|XYM|XYZM|layer_dim] [layer [layer ...]]\n"
-            "\n"
-            "Advanced options :\n"
-            "               [-gt n] [-ds_transaction]\n"
-            "               [[-oo NAME=VALUE] ...] [[-doo NAME=VALUE] ...]\n"
-            "               [-clipsrc [xmin ymin xmax ymax]|WKT|datasource|spat_extent]\n"
-            "               [-clipsrcsql sql_statement] [-clipsrclayer layer]\n"
-            "               [-clipsrcwhere expression]\n"
-            "               [-clipdst [xmin ymin xmax ymax]|WKT|datasource]\n"
-            "               [-clipdstsql sql_statement] [-clipdstlayer layer]\n"
-            "               [-clipdstwhere expression]\n"
-            "               [-wrapdateline][-datelineoffset val]\n"
-            "               [[-simplify tolerance] | [-segmentize max_dist]]\n"
-            "               [-addfields] [-unsetFid]\n"
-            "               [-relaxedFieldNameMatch] [-forceNullable] [-unsetDefault]\n"
-            "               [-fieldTypeToString All|(type1[,type2]*)] [-unsetFieldWidth]\n"
-            "               [-mapFieldType srctype|All=dsttype[,srctype2=dsttype2]*]\n"
-            "               [-fieldmap identity | index1[,index2]*]\n"
-            "               [-splitlistfields] [-maxsubfields val]\n"
-            "               [-explodecollections] [-zfield field_name]\n"
-            "               [-gcp pixel line easting northing [elevation]]* [-order n | -tps]\n"
-            "               [-nomd] [-mo \"META-TAG=VALUE\"]* [-noNativeData]\n");
-
-    if (bShort)
-    {
-        printf( "\nNote: ogr2ogr --long-usage for full help.\n");
-        if( pszAdditionalMsg )
-            fprintf(stderr, "\nFAILURE: %s\n", pszAdditionalMsg);
-        exit( 1 );
-    }
-
-    printf("\n -f format_name: output file format name, possible values are:\n");
-
-    std::vector<CPLString> aoSetDrivers;
-    for( int iDriver = 0; iDriver < poR->GetDriverCount(); iDriver++ )
-    {
-        GDALDriver *poDriver = poR->GetDriver(iDriver);
-
-        if( CPLTestBool( CSLFetchNameValueDef(poDriver->GetMetadata(), GDAL_DCAP_CREATE, "FALSE") ) )
-            aoSetDrivers.push_back( poDriver->GetDescription() );
-    }
-    std::sort (aoSetDrivers.begin(), aoSetDrivers.end(), StringCISortFunction);
-    for( size_t i = 0; i < aoSetDrivers.size(); i++ )
-    {
-        printf( "     -f \"%s\"\n", aoSetDrivers[i].c_str() );
-    }
-
-    printf( " -append: Append to existing layer instead of creating new if it exists\n"
-            " -overwrite: delete the output layer and recreate it empty\n"
-            " -update: Open existing output datasource in update mode\n"
-            " -progress: Display progress on terminal. Only works if input layers have the \n"
-            "                                          \"fast feature count\" capability\n"
-            " -select field_list: Comma-delimited list of fields from input layer to\n"
-            "                     copy to the new layer (defaults to all)\n"
-            " -where restricted_where: Attribute query (like SQL WHERE)\n"
-            " -wrapdateline: split geometries crossing the dateline meridian\n"
-            "                (long. = +/- 180deg)\n"
-            " -datelineoffset: offset from dateline in degrees\n"
-            "                (default long. = +/- 10deg,\n"
-            "                geometries within 170deg to -170deg will be split)\n"
-            " -sql statement: Execute given SQL statement and save result.\n"
-            " -dialect value: select a dialect, usually OGRSQL to avoid native sql.\n"
-            " -skipfailures: skip features or layers that fail to convert\n"
-            " -gt n: group n features per transaction (default 20000). n can be set to unlimited\n"
-            " -spat xmin ymin xmax ymax: spatial query extents\n"
-            " -simplify tolerance: distance tolerance for simplification.\n"
-            " -segmentize max_dist: maximum distance between 2 nodes.\n"
-            "                       Used to create intermediate points\n"
-            " -dsco NAME=VALUE: Dataset creation option (format specific)\n"
-            " -lco  NAME=VALUE: Layer creation option (format specific)\n"
-            " -oo   NAME=VALUE: Input dataset open option (format specific)\n"
-            " -doo  NAME=VALUE: Destination dataset open option (format specific)\n"
-            " -nln name: Assign an alternate name to the new layer\n"
-            " -nlt type: Force a geometry type for new layer.  One of NONE, GEOMETRY,\n"
-            "      POINT, LINESTRING, POLYGON, GEOMETRYCOLLECTION, MULTIPOINT,\n"
-            "      MULTIPOLYGON, or MULTILINESTRING, or PROMOTE_TO_MULTI or CONVERT_TO_LINEAR.  Add \"25D\" for 3D layers.\n"
-            "      Default is type of source layer.\n"
-            " -dim dimension: Force the coordinate dimension to the specified value.\n"
-            " -fieldTypeToString type1,...: Converts fields of specified types to\n"
-            "      fields of type string in the new layer. Valid types are : Integer,\n"
-            "      Integer64, Real, String, Date, Time, DateTime, Binary, IntegerList, Integer64List, RealList,\n"
-            "      StringList. Special value All will convert all fields to strings.\n"
-            " -fieldmap index1,index2,...: Specifies the list of field indexes to be\n"
-            "      copied from the source to the destination. The (n)th value specified\n"
-            "      in the list is the index of the field in the target layer definition\n"
-            "      in which the n(th) field of the source layer must be copied. Index count\n"
-            "      starts at zero. There must be exactly as many values in the list as\n"
-            "      the count of the fields in the source layer. We can use the 'identity'\n"
-            "      setting to specify that the fields should be transferred by using the\n"
-            "      same order. This setting should be used along with the append setting.");
-
-    printf(" -a_srs srs_def: Assign an output SRS\n"
-           " -t_srs srs_def: Reproject/transform to this SRS on output\n"
-           " -s_srs srs_def: Override source SRS\n"
-           "\n"
-           " Srs_def can be a full WKT definition (hard to escape properly),\n"
-           " or a well known definition (i.e. EPSG:4326) or a file with a WKT\n"
-           " definition.\n" );
-
-    if( pszAdditionalMsg )
-        fprintf(stderr, "\nFAILURE: %s\n", pszAdditionalMsg);
 }
