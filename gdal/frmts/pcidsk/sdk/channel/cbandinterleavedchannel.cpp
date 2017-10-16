1dbdb2af978f76aba1d2bdd105dda1fa41a7e473
Even Rouault <even.rouault@mines-paris.org>
2017-10-16 11:20:42 +0000

    PCIDSK SDK: avoid int overflow that can cause later null pointer dereference. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=3647. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@40449 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/pcidsk/sdk/channel/cbandinterleavedchannel.cpp
+++ gdal/frmts/pcidsk/sdk/channel/cbandinterleavedchannel.cpp
@@ -135,7 +135,7 @@ int CBandInterleavedChannel::ReadBlock( int block_index, void *buffer,
         || yoff < 0 || yoff + ysize > GetBlockHeight() )
     {
         return ThrowPCIDSKException( 0,
-            "Invalid window in ReadBloc(): xoff=%d,yoff=%d,xsize=%d,ysize=%d",
+            "Invalid window in ReadBlock(): xoff=%d,yoff=%d,xsize=%d,ysize=%d",
             xoff, yoff, xsize, ysize );
     }
 
@@ -145,6 +145,14 @@ int CBandInterleavedChannel::ReadBlock( int block_index, void *buffer,
     int    pixel_size = DataTypeSize( pixel_type );
     uint64 offset = start_byte + line_offset * block_index
         + pixel_offset * xoff;
+    if( xsize > 1 && pixel_offset > static_cast<uint64>(INT_MAX / (xsize - 1)) )
+    {
+        return ThrowPCIDSKException( 0, "Int overfow in ReadBlock() ");
+    }
+    if( pixel_offset*(xsize-1) > static_cast<uint64>(INT_MAX - pixel_size) )
+    {
+        return ThrowPCIDSKException( 0, "Int overfow in ReadBlock() ");
+    }
     int    window_size = (int) (pixel_offset*(xsize-1) + pixel_size);
 
 /* -------------------------------------------------------------------- */
