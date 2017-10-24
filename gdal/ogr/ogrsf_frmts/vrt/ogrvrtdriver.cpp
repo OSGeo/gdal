OSGeo/gdal

commit 8587274d9c63cd485c7807c77d6825737f72fb86

OGR VRT: Do not check Identify at the start of Open in fuzzing mode
git-svn-id: https://svn.osgeo.org/gdal/trunk@40535 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/ogr/ogrsf_frmts/vrt/ogrvrtdriver.cpp
@@ -78,8 +78,10 @@ static int OGRVRTDriverIdentify( GDALOpenInfo *poOpenInfo )
 static GDALDataset *OGRVRTDriverOpen( GDALOpenInfo *poOpenInfo )
 
 {
+#ifndef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
     if( !OGRVRTDriverIdentify(poOpenInfo) )
         return NULL;
+#endif
 
     // Are we being passed the XML definition directly?
     // Skip any leading spaces/blanks.

commit 42150bca1261d55062e8fe656c035fab7b7770b5

OGR_VRT: in fuzzing mode, avoid null pointer dereference caused by r40535. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=3746
git-svn-id: https://svn.osgeo.org/gdal/trunk@40544 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/ogr/ogrsf_frmts/vrt/ogrvrtdriver.cpp
@@ -99,6 +99,10 @@ static GDALDataset *OGRVRTDriverOpen( GDALOpenInfo *poOpenInfo )
     // Open file and check if it contains appropriate XML.
     else
     {
+#ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
+        if( poOpenInfo->fpL == NULL )
+            return NULL;
+#endif
         VSIStatBufL sStatBuf;
         if( VSIStatL(poOpenInfo->pszFilename, &sStatBuf) != 0 ||
             sStatBuf.st_size > 1024 * 1024 )