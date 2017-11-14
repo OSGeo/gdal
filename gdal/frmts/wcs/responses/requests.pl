8b829d7554984743b4f484d36e0e77b7efbff40c
Ari Jolma <ari.jolma@gmail.com>
2017-11-14 17:58:24 +0200

    Add test code that gets tiles from known servers for manual checking.

---
+++ gdal/frmts/wcs/responses/requests.pl
@@ -0,0 +1,113 @@
+use strict;
+use v5.10;
+
+system "rm -rf /home/ajolma/.gdal/wcs_cache";
+
+my $size = 60;
+
+my $setup = {
+    SimpleGeoServer => {
+        URL => 'https://msp.smartsea.fmi.fi/geoserver/wcs',
+        Options => ["", "-oo OuterExtents=TRUE", "-oo OuterExtents=TRUE", ""],
+        Projwin => "-projwin 145300 6737500 209680 6688700",
+        Outsize => "-outsize $size 0",
+        Coverage => ['smartsea:eusm2016', 'smartsea:eusm2016', 'smartsea:eusm2016', 'smartsea__eusm2016'],
+        Versions => [100, 110, 111, 201]
+    },
+    GeoServer => {
+        URL => 'https://msp.smartsea.fmi.fi/geoserver/wcs',
+        Options => [
+            "", 
+            "-oo OuterExtents=TRUE -oo NoGridSwap=TRUE",
+            "-oo OuterExtents=TRUE -oo NoGridSwap=TRUE",
+            "-oo NoGridSwap=TRUE -oo SubsetAxisSwap=TRUE"
+            ],
+        Projwin => "-projwin 3200000 6670000 3280000 6620000",
+        Outsize => "-outsize $size 0",
+        Coverage => ['smartsea:eusm2016-EPSG2393', 'smartsea:eusm2016-EPSG2393', 'smartsea:eusm2016-EPSG2393', 'smartsea__eusm2016-EPSG2393'],
+        Versions => [100, 110, 111, 201]
+    },
+    MapServer => {
+        URL => 'http://194.66.252.155/cgi-bin/BGS_EMODnet_bathymetry/ows',
+        Options => [
+            "-oo OriginNotCenter100=TRUE",
+            "-oo OffsetsPositive=TRUE -oo Offsets=2 -oo NoGridSwap=TRUE",
+            "-oo OffsetsPositive=TRUE -oo Offsets=2 -oo NoGridSwap=TRUE",
+            "-oo OffsetsPositive=TRUE -oo Offsets=2 -oo NoGridSwap=TRUE",
+            "-oo GridAxisLabelSwap=TRUE",
+            ],
+        Projwin => "-projwin 10 45 15 35",
+        Outsize => "-outsize $size 0",
+        Coverage => 'BGS_EMODNET_CentralMed-MCol',
+        Versions => [100, 110, 111, 112, 201]
+    },
+    Rasdaman => {
+        URL => 'http://ows.rasdaman.org/rasdaman/ows',
+        Options => "",
+        Projwin => "-projwin 10 45 15 35",
+        Outsize => "-outsize $size 0",
+        Coverage => 'BlueMarbleCov',
+        Versions => [201]
+    },
+    ArcGIS => {
+        URL => 'http://paikkatieto.ymparisto.fi/arcgis/services/Testit/Velmu_wcs_testi/MapServer/WCSServer',
+        Options => [
+            "",
+            "-oo Offsets=2",
+            "-oo Offsets=2",
+            "-oo Offsets=2",
+            "-oo UseScaleFactor=TRUE"
+            ],
+        Projwin => "-projwin 181000 7005000 200000 6980000",
+        Outsize => "-outsize $size 0",
+        Coverage => [2, 2, 2, 2, 'Coverage2'],
+        Versions => [100, 110, 111, 112, 201]
+    }
+};
+
+my %do = map {$_ => 1} @ARGV;
+
+for my $server (sort keys %$setup) {
+    next unless $do{$server} || $do{all_servers};
+    say $server;
+    for my $i (0..$#{$setup->{$server}->{Versions}}) {
+        my $url = $setup->{$server}->{URL};
+        my $v = $setup->{$server}{Versions}[$i];
+        my $version = int($v / 100) . '.' . int($v % 100 / 10) . '.' . ($v % 10);
+        next unless $do{$version} || $do{all_versions};
+        say $version;
+        my $coverage = $setup->{$server}{Coverage};
+        $coverage = $coverage->[$i] if ref $coverage;
+        my $options = $setup->{$server}{Options};
+        $options = $options->[$i] if ref $options;
+        my $result = "$server-$version.tiff";
+        my $o = "$options $setup->{$server}->{Projwin} $setup->{$server}->{Outsize} 2>&1";
+        say "gdal_translate $o \"WCS:$url?version=$version&coverage=$coverage\" $result";
+        my $output = qx(gdal_translate $o \"WCS:$url?version=$version&coverage=$coverage\" $result);
+        my @full_output;
+        foreach my $line (split /[\r\n]+/, $output) {
+            #say $line;
+            push @full_output, $line;
+        }
+        $output = qx(gdalinfo $result 2>&1);
+        my $ok = 'not ok';
+        foreach my $line (split /[\r\n]+/, $output) {
+            #say $line;
+            if ($line =~ /Size is (\d+), (\d)/) {
+                if ($1 == $size) {
+                    $ok = "ok";
+                }
+            }
+            push @full_output, $line;
+        }
+        say $ok;
+        if ($ok eq 'not ok' or $do{show}) {
+            say "===";
+            for (@full_output) {
+                say "$_" if $do{all} || /URL/;
+            }
+            say "==="
+        }
+        #exit;
+    }
+}
