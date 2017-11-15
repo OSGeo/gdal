02a078f7341f4d88940d0582c1b73ba56e69b2c2
Even Rouault <even.rouault@mines-paris.org>
2017-11-15 17:18:05 +0000

    Selafin: avoid int overflow. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=4227. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@40723 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/selafin/ogrselafindatasource.cpp
+++ gdal/ogr/ogrsf_frmts/selafin/ogrselafindatasource.cpp
@@ -491,7 +491,9 @@ int OGRSelafinDataSource::OpenTable(const char * pszFilename) {
                     sDate.tm_mday=poHeader->panStartDate[2];
                     sDate.tm_hour=poHeader->panStartDate[3];
                     sDate.tm_min=poHeader->panStartDate[4];
-                    sDate.tm_sec=poHeader->panStartDate[5]+(int)dfTime;
+                    double dfSec=poHeader->panStartDate[5]+dfTime;
+                    if( dfSec >= 0 && dfSec < 60 )
+                        sDate.tm_sec=static_cast<int>(dfSec);
                     mktime(&sDate);
                     strftime(szTemp,29,"%Y_%m_%d_%H_%M_%S",&sDate);
                 }
