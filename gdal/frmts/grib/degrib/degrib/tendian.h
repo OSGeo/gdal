1c54ac7d605a25436f3ba4913b306496559ada34
Even Rouault <even.rouault@mines-paris.org>
2017-11-17 15:13:36 +0000

    degrib: rename tendian.c as .cpp and merge fileendian.cpp/.h into tendian.cpp/.h (no-resync with upstream degrib yet)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@40732 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/grib/degrib/degrib/tendian.h
+++ gdal/frmts/grib/degrib/degrib/tendian.h
@@ -54,8 +54,42 @@ char memBitRead (void *Dst, size_t dstLen, void *Src, size_t numBits,
 char memBitWrite (void *Src, size_t srcLen, void *Dst, size_t numBits,
                   uChar * bufLoc, size_t *numUsed);
 
+
 #ifdef __cplusplus
 }
 #endif  /* __cplusplus */
+
+#ifdef __cplusplus
+
+#include "datasource.h"
+
+/* The following #defines are used to make the code easier to read. */
+#ifdef BIG_ENDIAN
+  #define FREAD_BIG norfread
+  #define FREAD_LIT revfread
+  #define FWRITE_BIG fwrite
+  #define FWRITE_LIT revfwrite
+#else
+  #define FREAD_BIG revfread
+  #define FREAD_LIT norfread
+  #define FWRITE_BIG revfwrite
+  #define FWRITE_LIT fwrite
+#endif
+
+size_t norfread (void *Dst, size_t elem_size, size_t num_elem, DataSource &fp);
+size_t revfread (void *Dst, size_t elem_size, size_t num_elem, DataSource &fp);
+size_t revfwrite (void *Src, size_t elem_size, size_t num_elem, FILE *fp);
+
+size_t FREAD_ODDINT_BIG (sInt4 * dst, uChar len, DataSource &fp);
+size_t FREAD_ODDINT_LIT (sInt4 * dst, uChar len, DataSource &fp);
+size_t FWRITE_ODDINT_BIG (sInt4 * src, uChar len, FILE *fp);
+size_t FWRITE_ODDINT_LIT (sInt4 * src, uChar len, FILE *fp);
+
+int fileBitRead (void *Dst, size_t dstLen, uShort2 num_bits, FILE *fp,
+                 uChar * gbuf, sChar * gbufLoc);
+char fileBitWrite (void *Src, size_t srcLen, uShort2 numBits, FILE *fp,
+                   uChar * pbuf, sChar * pbufLoc);
+
+#endif
 
 #endif /* MEMENDIAN_H */
