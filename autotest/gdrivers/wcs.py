32070e0b783779e117602f3f73df3fc9ad898617
Ari Jolma <ari.jolma@gmail.com>
2017-11-27 16:31:01 +0200

    Is ds NULL?

--- autotest/gdrivers/wcs.py
+++ autotest/gdrivers/wcs.py
@@ -514,6 +514,7 @@ def wcs_6():
             for i, c in enumerate(projwin):
                 projwin[i] = int(c)
             options = [cache]
+            print ds
             ds = gdal.Translate('output.tif', ds, projWin = projwin, width = size, options = options)
     webserver.server_stop(process, port)    
     return 'success'
