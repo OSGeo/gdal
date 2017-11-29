b4f10a11954dc4252dee80c5e34baab19b65998a
Even Rouault <even.rouault@mines-paris.org>
2017-11-29 10:35:31 +0000

    OGRSpatialReference::convertToOtherProjection(): avoid floating point divisions by zero. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=4469. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@40874 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrspatialreference.cpp
+++ gdal/ogr/ogrspatialreference.cpp
@@ -7259,6 +7259,8 @@ OGRSpatialReference* OGRSpatialReference::convertToOtherProjection(
         const double m0 = msfn(phi0, ec);
         const double t0 = tsfn(phi0, ec);
         const double n = sin(phi0);
+        if( n == 0.0 )
+            return NULL;
         OGRSpatialReference* poLCC2SP = new OGRSpatialReference();
         poLCC2SP->CopyGeogCSFrom(this);
         if( fabs(k0 - 1.0) <= 1e-10 )
@@ -7358,6 +7360,8 @@ OGRSpatialReference* OGRSpatialReference::convertToOtherProjection(
         const double t2 = tsfn(phi2, ec);
         const double n = (phi1 == phi2) ? sin(phi1) :
                                 (log(m1) - log(m2)) / (log(t1) - log(t2));
+        if( n == 0.0 )
+            return NULL;
         const double F = m1 / (n * pow(t1, n));
         const double phi0 = asin(n);
         const double m0 = msfn(phi0, ec);
