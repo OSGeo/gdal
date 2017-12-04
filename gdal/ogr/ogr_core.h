13023c67477b035960ef5ecc03d545b2be1c3c8b
Kurt Schwehr <schwehr@gmail.com>
2017-12-04 19:02:16 +0000

    Fix OGR_GET_MS Cast Overflow at OGRFeatureFormatDateTimeBuffer in ogrfeature.cpp:2037 from libkml fuzzer
    - Convert from a macro to an inline function.
    - Range check and clamp before doing any computations
    
    Found with autofuzz
    
    
    git-svn-id: https://svn.osgeo.org/gdal/trunk@40948 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogr_core.h
+++ gdal/ogr/ogr_core.h
@@ -712,8 +712,15 @@ typedef union {
 /*! @endcond */
 } OGRField;
 
+#ifdef __cplusplus
 /** Return the number of milliseconds from a datetime with decimal seconds */
-#define OGR_GET_MS(floatingpoint_sec)   (int)(((floatingpoint_sec) - (int)(floatingpoint_sec)) * 1000 + 0.5)
+inline int OGR_GET_MS(float fSec) {
+  if( fSec >= 999 ) return 999;
+  if( fSec <= 0 ) return 0;
+  const float fValue = (fSec - static_cast<int>(fSec)) * 1000 + 0.5;
+  return static_cast<int>(fValue);
+}
+#endif  // __cplusplus
 
 int CPL_DLL OGRParseDate( const char *pszInput, OGRField *psOutput,
                           int nOptions );
