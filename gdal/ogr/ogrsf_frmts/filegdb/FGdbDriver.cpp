a3ab625c7cd31c6943fca29ed2e981826ee20265
Nyall Dawson <nyall.dawson@gmail.com>
2017-12-12 10:44:12 +1000

    Add DMD_CREATIONFIELDDATASUBTYPES metadata type
    For determining vector field sub-types which can be created
    for a driver

--- gdal/ogr/ogrsf_frmts/filegdb/FGdbDriver.cpp
+++ gdal/ogr/ogrsf_frmts/filegdb/FGdbDriver.cpp
@@ -849,6 +849,7 @@ void RegisterOGRFileGDB()
 
     poDriver->SetMetadataItem( GDAL_DMD_CREATIONFIELDDATATYPES,
                                "Integer Real String Date DateTime Binary" );
+    poDriver->SetMetadataItem( GDAL_DMD_CREATIONFIELDDATASUBTYPES, "Int16 Float32" );
     poDriver->SetMetadataItem( GDAL_DCAP_NOTNULL_FIELDS, "YES" );
     poDriver->SetMetadataItem( GDAL_DCAP_DEFAULT_FIELDS, "YES" );
     poDriver->SetMetadataItem( GDAL_DCAP_NOTNULL_GEOMFIELDS, "YES" );
--- gdal/ogr/ogrsf_frmts/pg/ogrpgdriver.cpp
+++ gdal/ogr/ogrsf_frmts/pg/ogrpgdriver.cpp
@@ -160,6 +160,7 @@ void RegisterOGRPG()
                                "Integer Integer64 Real String Date DateTime "
                                "Time IntegerList Integer64List RealList "
                                "StringList Binary" );
+    poDriver->SetMetadataItem( GDAL_DMD_CREATIONFIELDDATASUBTYPES, "Boolean Int16 Float32" );
     poDriver->SetMetadataItem( GDAL_DCAP_NOTNULL_FIELDS, "YES" );
     poDriver->SetMetadataItem( GDAL_DCAP_DEFAULT_FIELDS, "YES" );
     poDriver->SetMetadataItem( GDAL_DCAP_NOTNULL_GEOMFIELDS, "YES" );
