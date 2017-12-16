dddbbe552820da67b3bc486010a9097c86b17c0a
Even Rouault <even.rouault@mines-paris.org>
2017-12-16 10:41:03 +0000

    MITAB: prevent out-of-bound read. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=4660. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@41050 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/mitab/mitab_indfile.cpp
+++ gdal/ogr/ogrsf_frmts/mitab/mitab_indfile.cpp
@@ -1051,8 +1051,11 @@ int   TABINDNode::IndexKeyCmp(GByte *pKeyValue, int nEntryNo)
     CPLAssert(nEntryNo >= 0 && nEntryNo < m_numEntriesInNode);
 
     m_poDataBlock->GotoByteInBlock(12 + nEntryNo*(m_nKeyLength+4));
-
-    return memcmp(pKeyValue, m_poDataBlock->GetCurDataPtr(), m_nKeyLength);
+    CPLAssert( m_nKeyLength <= 255 );
+    GByte abyKey[255];
+    if( m_poDataBlock->ReadBytes(m_nKeyLength, abyKey) != 0 )
+        return -1;
+    return memcmp(pKeyValue, abyKey, m_nKeyLength);
 }
 
 /**********************************************************************
