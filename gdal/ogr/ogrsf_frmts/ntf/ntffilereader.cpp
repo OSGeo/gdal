05c0c751f0ec9bdbdd60da7692d441a10fa06b92
Even Rouault <even.rouault@mines-paris.org>
2017-12-24 11:59:00 +0000

    NTF: avoid long processing time on corrupted file. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=4762. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@41130 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/ntf/ntffilereader.cpp
+++ gdal/ogr/ogrsf_frmts/ntf/ntffilereader.cpp
@@ -763,20 +763,30 @@ OGRGeometry *NTFFileReader::ProcessGeometry3D( NTFRecord * poRecord,
 
         poGeometry = poLine;
         poLine->setNumPoints( nNumCoord );
+        const GUInt32 nErrorsBefore = CPLGetErrorCounter();
         for( int iCoord = 0; iCoord < nNumCoord; iCoord++ )
         {
             const int iStart = 14 + iCoord * (GetXYLen()*2+nZWidth+2);
 
-            const double dfX = atoi(poRecord->GetField(iStart+0,
-                                          iStart+GetXYLen()-1))
+            const char* pszX = poRecord->GetField(iStart+0,
+                                          iStart+GetXYLen()-1);
+            const double dfX = atoi(pszX)
                 * GetXYMult() + GetXOrigin();
-            const double dfY = atoi(poRecord->GetField(iStart+GetXYLen(),
-                                          iStart+GetXYLen()*2-1))
+            const char* pszY = poRecord->GetField(iStart+GetXYLen(),
+                                          iStart+GetXYLen()*2-1);
+            const double dfY = atoi(pszY)
                 * GetXYMult() + GetYOrigin();
 
-            const double dfZ = atoi(poRecord->GetField(iStart+1+2*GetXYLen(),
-                                          iStart+1+2*GetXYLen()+nZWidth-1))
+            const char* pszZ = poRecord->GetField(iStart+1+2*GetXYLen(),
+                                          iStart+1+2*GetXYLen()+nZWidth-1);
+            const double dfZ = atoi(pszZ)
                 * dfZMult;
+            if( (pszX[0] == ' ' || pszY[0] == ' ' || pszZ[0] == ' ') &&
+                CPLGetErrorCounter() != nErrorsBefore )
+            {
+                delete poGeometry;
+                return nullptr;
+            }
 
             if( iCoord == 0 )
             {
