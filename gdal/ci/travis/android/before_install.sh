3e3871c08fbc554d5fc51d356297bd7951991b71
Kurt Schwehr <schwehr@gmail.com>
2018-01-02 19:02:53 +0000

    Switch from android ndk r16 -> r16b
    A 2nd attempt to fix r41166, which uses std::to_string.
    
    Note that gcc is no longer supported for android builds:
    
    https://developer.android.com/ndk/downloads/revision_hist says
    
    GCC is no longer supported. It will not be removed from the NDK just yet, but is
    no longer receiving backports. It cannot be removed until after libc++ has
    become stable enough to be the default, as some parts of gnustl are still
    incompatible with Clang. It will be removed when the other STLs are removed in
    r18.
    
    
    git-svn-id: https://svn.osgeo.org/gdal/trunk@41169 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ci/travis/android/before_install.sh
+++ gdal/ci/travis/android/before_install.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-wget https://dl.google.com/android/repository/android-ndk-r16-linux-x86_64.zip
-unzip -q android-ndk-r16-linux-x86_64.zip
-android-ndk-r16/build/tools/make-standalone-toolchain.sh --platform=android-24 --install-dir=$HOME/android-toolchain
+wget https://dl.google.com/android/repository/android-ndk-r16b-linux-x86_64.zip
+unzip -q android-ndk-r16b-linux-x86_64.zip
+android-ndk-r16b/build/tools/make-standalone-toolchain.sh --platform=android-24 --install-dir=$HOME/android-toolchain
