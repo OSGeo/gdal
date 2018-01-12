385a3e2e25c7678353a80e58514c6ea5e3d7e166
drons <drons@list.ru>
2018-01-12 23:20:48 +0300

    Add Doxygen documentation for CPLIsPowerOfTwo

--- gdal/port/cpl_conv.h
+++ gdal/port/cpl_conv.h
@@ -281,9 +281,11 @@ char* CPLsetlocale (int category, const char* locale);
 void CPLCleanupSetlocaleMutex(void);
 /*! @endcond */
 
-/* -------------------------------------------------------------------- */
-/*                          CPLIsPowerOfTwo()                           */
-/* -------------------------------------------------------------------- */
+/*!
+    CPLIsPowerOfTwo()
+    @param i - tested number
+    @return TRUE if i is power of two otherwise return FALSE
+*/
 int CPL_DLL CPLIsPowerOfTwo( unsigned int i );
 
 CPL_C_END
