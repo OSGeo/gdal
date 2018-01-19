35bfe5fc6ddded2cd4b7eda9ea522b320e8ded0d
Kieran McCusker <kieran.mccusker@kwest.co>
2018-01-19 16:07:37 +0000

    Fix for merge

--- gdal/ogr/ogrsf_frmts/xlsx/ogrxlsxdatasource.cpp
+++ gdal/ogr/ogrsf_frmts/xlsx/ogrxlsxdatasource.cpp
@@ -966,30 +966,6 @@ void OGRXLSXDataSource::endElementRow(CPL_UNUSED const char *pszNameIn)
             }
 
             /* Add feature for current line */
-            OGRFeature* poFeature = nullptr;
-
-            for( size_t i = 0; i < apoCurLineValues.size(); i++ )
-            {
-                if (!apoCurLineValues[i].empty())
-                {
-                  if (!poFeature) {
-                    poFeature = new OGRFeature(poCurLayer->GetLayerDefn());
-                  }
-
-                  SetField(poFeature, static_cast<int>(i), apoCurLineValues[i].c_str(),
-                          apoCurLineTypes[i].c_str());
-                }
-            }
-            if (poFeature || bAllowEmptyRows)
-            {
-                if (!poFeature) {
-                  poFeature = new OGRFeature(poCurLayer->GetLayerDefn());
-                }
-
-                CPL_IGNORE_RET_VAL(poCurLayer->CreateFeature(poFeature));
-                delete poFeature;
-
-            }
        }
 
         nCurLine++;
