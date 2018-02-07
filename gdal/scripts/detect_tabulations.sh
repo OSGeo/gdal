955b9ddc125f5842fe596512f21a16ca4de99512
Even Rouault <even.rouault@mines-paris.org>
2018-02-07 09:49:49 +0000

    scripts: do not test 0 after running commands. Just use 'if cmd ; then' instead (patch by Ben Elliston, fixes https://github.com/OSGeo/gdal/pull/297)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@41427 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/scripts/detect_tabulations.sh
+++ gdal/scripts/detect_tabulations.sh
@@ -3,9 +3,7 @@
 ret_code=0
 
 echo "Checking for tabulation characters..."
-find alg gnm port ogr gcore frmts apps \( -name "*.cpp" -o -name "*.h" \) -exec sh -c "grep -P '\t' {} >/dev/null && echo {}" \; | grep -v frmts/msg/PublicDecompWT | grep -v frmts/jpeg/libjpeg | grep -v frmts/gtiff/libtiff | grep -v frmts/gtiff/libgeotiff | grep -v frmts/grib/degrib | grep -v ogr/ogrsf_frmts/geojson/libjson | grep -v frmts/hdf4/hdf-eos | grep -v frmts/gif/giflib | grep -v frmts/pcraster/libcsf | grep -v frmts/png/libpng | grep -v cpl_mem_cache
-
-if [[ $? -eq 0 ]] ; then
+if find alg gnm port ogr gcore frmts apps \( -name "*.cpp" -o -name "*.h" \) -exec sh -c "grep -P '\\t' {} >/dev/null && echo {}" \; | grep -v frmts/msg/PublicDecompWT | grep -v frmts/jpeg/libjpeg | grep -v frmts/gtiff/libtiff | grep -v frmts/gtiff/libgeotiff | grep -v frmts/grib/degrib | grep -v ogr/ogrsf_frmts/geojson/libjson | grep -v frmts/hdf4/hdf-eos | grep -v frmts/gif/giflib | grep -v frmts/pcraster/libcsf | grep -v frmts/png/libpng | grep -v cpl_mem_cache ; then
     echo "FAIL: tabulations detected. Please remove them!"
     ret_code=1
 else
