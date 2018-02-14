be43506343d9dae90626ea6e4ee08e92a1a8a6fd
Even Rouault <even.rouault@mines-paris.org>
2018-02-14 16:04:23 +0000

    ci/travis/trusty_clang/install.sh: enable internal libtiff
    git-svn-id: https://svn.osgeo.org/gdal/trunk@41489 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ci/travis/trusty_clang/install.sh
+++ gdal/ci/travis/trusty_clang/install.sh
@@ -17,7 +17,7 @@ else
         cat /proc/cpuinfo  | grep flags | head -n 1
 fi
 
-CFLAGS=$ARCH_FLAGS CXXFLAGS=$ARCH_FLAGS CC="ccache clang" CXX="ccache clang" LDFLAGS="-lstdc++" ./configure --prefix=/usr --without-libtool --with-jpeg12 --with-python --with-poppler --with-podofo --with-spatialite --with-mysql --with-liblzma --with-webp --with-java --with-mdb --with-jvm-lib-add-rpath --with-epsilon --with-ecw=/usr/local --with-mrsid=/usr/local --with-mrsid-lidar=/usr/local --with-fgdb=/usr/local --with-libkml --with-null
+CFLAGS=$ARCH_FLAGS CXXFLAGS=$ARCH_FLAGS CC="ccache clang" CXX="ccache clang" LDFLAGS="-lstdc++" ./configure --prefix=/usr --without-libtool --with-jpeg12 --with-python --with-poppler --with-podofo --with-spatialite --with-mysql --with-liblzma --with-webp --with-java --with-mdb --with-jvm-lib-add-rpath --with-epsilon --with-ecw=/usr/local --with-mrsid=/usr/local --with-mrsid-lidar=/usr/local --with-fgdb=/usr/local --with-libkml --with-null -with-libtiff=internal
 # --with-gta
 # Those ln -s are weird but otherwise Python bindings build fail with clang not being found
 sudo ln -s /usr/local/clang-3.5.0/bin/clang /usr/bin/clang
