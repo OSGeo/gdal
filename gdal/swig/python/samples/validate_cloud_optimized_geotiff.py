99c62df9b58c1783304dffe49d15bf067688e8b8
Kurt Schwehr <schwehr@gmail.com>
2018-02-15 23:00:48 +0000

    Clean validate_cloud_optimized_geotiff.py
    - Fix formatting
    - Use single quote for all strings that are not docstrings
    - Fix docstrings
    - Don't use redundant + for concat of string literals
    - Don't use line continuations
    - if len(errors) != 0 -> if errors:
    - Fix exception from corrupt file
    
    data_offset = int(main_band.GetMetadataItem('BLOCK_OFFSET_0_0', 'TIFF'))
    TypeError: int() argument must be a string or a number, not 'NoneType'
    
    
    git-svn-id: https://svn.osgeo.org/gdal/trunk@41514 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/swig/python/samples/validate_cloud_optimized_geotiff.py
+++ gdal/swig/python/samples/validate_cloud_optimized_geotiff.py
@@ -44,17 +44,25 @@ class ValidateCloudOptimizedGeoTIFFException(Exception):
 
 
 def validate(ds, check_tiled=True):
-    """ Check if the passed file is a (Geo)TIFF file with a cloud
-        optimized compatible structure.
-        Return a tuple, whose first element is an array of error messages
-        (empty if there is no error), and the second tuple, a dictionary
-        with the structure of the GeoTIFF file.
-        Might raise a
-        ValidateCloudOptimizedGeoTIFFException exception. """
+    """Check if a file is a (Geo)TIFF with cloud optimized compatible structure.
+
+    Args:
+      ds: GDAL Dataset for the file to inspect.
+      check_tiled: Set to False to ignore missing tiling.
+
+    Returns:
+      A tuple, whose first element is an array of error messages
+      (empty if there is no error), and the second element, a dictionary
+      with the structure of the GeoTIFF file.
+
+    Raises:
+      ValidateCloudOptimizedGeoTIFFException: Unable to open the file or the
+        file is not a Tiff.
+    """
 
     if int(gdal.VersionInfo('VERSION_NUM')) < 2020000:
         raise ValidateCloudOptimizedGeoTIFFException(
-            "GDAL 2.2 or above required")
+            'GDAL 2.2 or above required')
 
     unicode_type = type(''.encode('utf-8').decode('utf-8'))
     if isinstance(ds, str) or isinstance(ds, unicode_type):
@@ -63,10 +71,10 @@ def validate(ds, check_tiled=True):
         gdal.PopErrorHandler()
         if ds is None:
             raise ValidateCloudOptimizedGeoTIFFException(
-                "Invalid file : %s" % gdal.GetLastErrorMsg())
+                'Invalid file : %s' % gdal.GetLastErrorMsg())
         if ds.GetDriver().ShortName != 'GTiff':
             raise ValidateCloudOptimizedGeoTIFFException(
-                "The file is not a GeoTIFF")
+                'The file is not a GeoTIFF')
 
     details = {}
     errors = []
@@ -75,26 +83,26 @@ def validate(ds, check_tiled=True):
     ovr_count = main_band.GetOverviewCount()
     filelist = ds.GetFileList()
     if filelist is not None and filename + '.ovr' in filelist:
-        errors += ['Overviews found in external .ovr file. ' +
-                   'They should be internal']
+        errors += [
+            'Overviews found in external .ovr file. They should be internal']
 
     if main_band.XSize >= 512 or main_band.YSize >= 512:
         if check_tiled:
             block_size = main_band.GetBlockSize()
             if block_size[0] == main_band.XSize and block_size[0] > 1024:
-                errors += ['The file is greater than 512xH or Wx512, ' +
-                           'but is not tiled']
+                errors += [
+                    'The file is greater than 512xH or Wx512, but is not tiled']
 
         if ovr_count == 0:
-            errors += ["The file is greater than 512xH or Wx512, " +
-                       "but has no overviews"]
+            errors += [
+                'The file is greater than 512xH or Wx512, but has no overviews']
 
     ifd_offset = int(main_band.GetMetadataItem('IFD_OFFSET', 'TIFF'))
     ifd_offsets = [ifd_offset]
     if ifd_offset not in (8, 16):
         errors += [
-            "The offset of the main IFD should be 8 for ClassicTIFF " +
-            "or 16 for BigTIFF. It is %d instead" % ifd_offsets[0]]
+            'The offset of the main IFD should be 8 for ClassicTIFF '
+            'or 16 for BigTIFF. It is %d instead' % ifd_offsets[0]]
     details['ifd_offsets'] = {}
     details['ifd_offsets']['main'] = ifd_offset
 
@@ -102,23 +110,23 @@ def validate(ds, check_tiled=True):
         # Check that overviews are by descending sizes
         ovr_band = ds.GetRasterBand(1).GetOverview(i)
         if i == 0:
-            if ovr_band.XSize > main_band.XSize or \
-               ovr_band.YSize > main_band.YSize:
+            if (ovr_band.XSize > main_band.XSize or
+                ovr_band.YSize > main_band.YSize):
                     errors += [
-                       "First overview has larger dimension than main band"]
+                        'First overview has larger dimension than main band']
         else:
             prev_ovr_band = ds.GetRasterBand(1).GetOverview(i-1)
-            if ovr_band.XSize > prev_ovr_band.XSize or \
-               ovr_band.YSize > prev_ovr_band.YSize:
+            if (ovr_band.XSize > prev_ovr_band.XSize or
+                ovr_band.YSize > prev_ovr_band.YSize):
                     errors += [
-                       "Overview of index %d has larger dimension than "
-                       "overview of index %d" % (i, i-1)]
+                        'Overview of index %d has larger dimension than '
+                        'overview of index %d' % (i, i-1)]
 
         if check_tiled:
             block_size = ovr_band.GetBlockSize()
             if block_size[0] == ovr_band.XSize and block_size[0] > 1024:
                 errors += [
-                    "Overview of index %d is not tiled" % i]
+                    'Overview of index %d is not tiled' % i]
 
         # Check that the IFD of descending overviews are sorted by increasing
         # offsets
@@ -128,20 +136,23 @@ def validate(ds, check_tiled=True):
         if ifd_offsets[-1] < ifd_offsets[-2]:
             if i == 0:
                 errors += [
-                    "The offset of the IFD for overview of index %d is %d, "
-                    "whereas it should be greater than the one of the main "
-                    "image, which is at byte %d" %
+                    'The offset of the IFD for overview of index %d is %d, '
+                    'whereas it should be greater than the one of the main '
+                    'image, which is at byte %d' %
                     (i, ifd_offsets[-1], ifd_offsets[-2])]
             else:
                 errors += [
-                    "The offset of the IFD for overview of index %d is %d, "
-                    "whereas it should be greater than the one of index %d, "
-                    "which is at byte %d" %
+                    'The offset of the IFD for overview of index %d is %d, '
+                    'whereas it should be greater than the one of index %d, '
+                    'which is at byte %d' %
                     (i, ifd_offsets[-1], i-1, ifd_offsets[-2])]
 
     # Check that the imagery starts by the smallest overview and ends with
     # the main resolution dataset
-    data_offset = int(main_band.GetMetadataItem('BLOCK_OFFSET_0_0', 'TIFF'))
+    block_offset = main_band.GetMetadataItem('BLOCK_OFFSET_0_0', 'TIFF')
+    if not block_offset:
+        errors += ['Missing BLOCK_OFFSET_0_0']
+    data_offset = int(block_offset) if block_offset else None
     data_offsets = [data_offset]
     details['data_offsets'] = {}
     details['data_offsets']['main'] = data_offset
@@ -154,29 +165,29 @@ def validate(ds, check_tiled=True):
     if data_offsets[-1] < ifd_offsets[-1]:
         if ovr_count > 0:
             errors += [
-                "The offset of the first block of the smallest overview "
-                "should be after its IFD"]
+                'The offset of the first block of the smallest overview '
+                'should be after its IFD']
         else:
             errors += [
-                "The offset of the first block of the image should "
-                "be after its IFD"]
+                'The offset of the first block of the image should '
+                'be after its IFD']
     for i in range(len(data_offsets)-2, 0, -1):
-        if data_offsets[i] < data_offsets[i+1]:
+        if data_offsets[i] < data_offsets[i + 1]:
             errors += [
-                "The offset of the first block of overview of index %d should "
-                "be after the one of the overview of index %d" %
-                (i-1, i)]
+                'The offset of the first block of overview of index %d should '
+                'be after the one of the overview of index %d' %
+                (i - 1, i)]
     if len(data_offsets) >= 2 and data_offsets[0] < data_offsets[1]:
         errors += [
-            "The offset of the first block of the main resolution image "
-            "should be after the one of the overview of index %d" %
+            'The offset of the first block of the main resolution image'
+            'should be after the one of the overview of index %d' %
             (ovr_count - 1)]
 
     return errors, details
 
 
 def main():
-    """ Returns 0 in case of success """
+    """Return 0 in case of success, 1 for failure."""
 
     i = 1
     filename = None
@@ -191,14 +202,14 @@ def main():
         else:
             return Usage()
 
-        i = i + 1
+        i += 1
 
     if filename is None:
         return Usage()
 
     try:
         errors, _ = validate(filename)
-        if len(errors) != 0:
+        if errors:
             if not quiet:
                 print('%s is NOT a valid cloud optimized GeoTIFF.' % filename)
                 print('The following errors were found:')
