050c58ef8f426880de995aaae4d0144795346dde
Even Rouault <even.rouault@mines-paris.org>
2018-02-16 17:24:52 +0000

    Travis Mac: make it work with bout MacOS 10.11 and 10.12
    git-svn-id: https://svn.osgeo.org/gdal/trunk@41523 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ci/travis/osx/script.sh
+++ gdal/ci/travis/osx/script.sh
@@ -3,7 +3,7 @@
 set -e
 
 cd gdal
-export PYTHONPATH=$PWD/swig/python/build/lib.macosx-10.12-intel-2.7
+export PYTHONPATH=$PWD/swig/python/build/lib.macosx-10.12-intel-2.7:$PWD/swig/python/build/lib.macosx-10.11-x86_64-2.7
 
 # CPP unit tests
 cd ../autotest
