ea22f1dc8973b9f5fdbb7f82e4af481b4edd7ed2
Even Rouault <even.rouault@mines-paris.org>
2018-03-02 12:40:04 +0000

    AVCE00: fix write heap-buffer overflow. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=6663. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@41595 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/avc/avc_e00parse.cpp
+++ gdal/ogr/ogrsf_frmts/avc/avc_e00parse.cpp
@@ -1703,7 +1703,8 @@ AVCTxt   *AVCE00ParseNextTx6Line(AVCE00ParseInfo *psInfo, const char *pszLine)
 
         psInfo->iCurItem++;
     }
-    else if (psInfo->iCurItem < (8 + ABS(psTxt->numVerticesLine) +
+    else if (psInfo->iCurItem >= 8 &&
+             psInfo->iCurItem < (8 + ABS(psTxt->numVerticesLine) +
                                    ABS(psTxt->numVerticesArrow)) && nLen >= 28)
     {
         /*-------------------------------------------------------------
@@ -1718,7 +1719,9 @@ AVCTxt   *AVCE00ParseNextTx6Line(AVCE00ParseInfo *psInfo, const char *pszLine)
 
         psInfo->iCurItem++;
     }
-    else if (psInfo->iCurItem < psInfo->numItems &&
+    else if (psInfo->iCurItem >= (8 + ABS(psTxt->numVerticesLine) +
+                                   ABS(psTxt->numVerticesArrow)) &&
+             psInfo->iCurItem < psInfo->numItems &&
              (psTxt->numChars-1)/80 + 1 - (psInfo->numItems - psInfo->iCurItem) >= 0 )
     {
         /*-------------------------------------------------------------
