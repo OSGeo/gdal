a7f3f3c08a2a90c2cac47e780d44bf100f88c7c6
Even Rouault <even.rouault@mines-paris.org>
2018-03-02 12:24:39 +0000

    AVCE00: fix null pointer dereference. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=6658. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@41593 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/avc/avc_e00parse.cpp
+++ gdal/ogr/ogrsf_frmts/avc/avc_e00parse.cpp
@@ -1889,6 +1889,14 @@ AVCTableDef   *AVCE00ParseNextTableDefLine(AVCE00ParseInfo *psInfo,
             psTableDef->numFields  = (GInt16)AVCE00Str2Int(pszLine+34, 4);
             psTableDef->nRecSize   = (GInt16)AVCE00Str2Int(pszLine+42, 4);
             psTableDef->numRecords = AVCE00Str2Int(pszLine+46, 10);
+            if( psTableDef->numFields < 0 || psTableDef->numFields > 10 * 1024 )
+            {
+                CPLError(CE_Failure, CPLE_AppDefined,
+                        "Error parsing E00 Table Definition line: \"%s\"", pszLine);
+                psInfo->numItems = psInfo->iCurItem = 0;
+                psTableDef->numFields = 0;
+                return nullptr;
+            }
 
             /*---------------------------------------------------------
              * Alloc array of fields defs, will be filled in further calls
