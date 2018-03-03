6af4c1e23f33b09e0321c3432f444afd3a7e8d4e
Even Rouault <even.rouault@mines-paris.org>
2018-03-03 18:13:38 +0000

    AVCE00: fix integer overflow. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=6704. Credit to OSS Fuzz
    git-svn-id: https://svn.osgeo.org/gdal/trunk@41617 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/ogr/ogrsf_frmts/avc/avc_e00parse.cpp
+++ gdal/ogr/ogrsf_frmts/avc/avc_e00parse.cpp
@@ -1401,15 +1401,16 @@ AVCTxt   *AVCE00ParseNextTxtLine(AVCE00ParseInfo *psInfo, const char *pszLine)
             psTxt->pszText = (GByte *)CPLRealloc(psTxt->pszText,
                                                  (psTxt->numChars+1)*
                                                      sizeof(GByte));
-            numVertices = ABS(psTxt->numVerticesLine) +
-                                 ABS(psTxt->numVerticesArrow);
-            if( numVertices > 10*1024*1024 )
+            if( ABS(psTxt->numVerticesLine) > 10*1024*1024 -
+                                                ABS(psTxt->numVerticesArrow) )
             {
                 CPLError(CE_Failure, CPLE_AppDefined,
                         "Error parsing E00 TXT line: \"%s\"", pszLine);
                 psInfo->numItems = psInfo->iCurItem = 0;
                 return nullptr;
             }
+            numVertices = ABS(psTxt->numVerticesLine) +
+                                 ABS(psTxt->numVerticesArrow);
             if (numVertices > 0)
                 psTxt->pasVertices = (AVCVertex*)CPLRealloc(psTxt->pasVertices,
                                               numVertices*sizeof(AVCVertex));
@@ -1627,15 +1628,16 @@ AVCTxt   *AVCE00ParseNextTx6Line(AVCE00ParseInfo *psInfo, const char *pszLine)
                                                  (psTxt->numChars+1)*
                                                  sizeof(GByte));
 
-            numVertices = ABS(psTxt->numVerticesLine) +
-                                 ABS(psTxt->numVerticesArrow);
-            if( numVertices > 10*1024*1024 )
+            if( ABS(psTxt->numVerticesLine) > 10*1024*1024 -
+                                                ABS(psTxt->numVerticesArrow) )
             {
                 CPLError(CE_Failure, CPLE_AppDefined,
                         "Error parsing E00 TX6/TX7 line: \"%s\"", pszLine);
                 psInfo->numItems = psInfo->iCurItem = 0;
                 return nullptr;
             }
+            numVertices = ABS(psTxt->numVerticesLine) +
+                                 ABS(psTxt->numVerticesArrow);
             if (numVertices > 0)
                 psTxt->pasVertices = (AVCVertex*)CPLRealloc(psTxt->pasVertices,
                                               numVertices*sizeof(AVCVertex));
