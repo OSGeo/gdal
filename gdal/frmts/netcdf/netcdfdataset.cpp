OSGeo/gdal

commit 13849173061edc8f7f06afabd3f0a6bda2f6d7b1

netCDF: fix bad interaction of SetNoDataValue() and SetGeoTransform()/SetProjection() for NC4/NC4C modes (fixes #7245)
git-svn-id: https://svn.osgeo.org/gdal/trunk@41577 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/frmts/netcdf/netcdfdataset.h
@@ -794,7 +794,8 @@ class netCDFDataset : public GDALPamDataset
     bool         bDefineMode;
     bool         bSetProjection;
     bool         bSetGeoTransform;
-    bool         bAddedProjectionVars;
+    bool         bAddedProjectionVarsDefs;
+    bool         bAddedProjectionVarsData;
     bool         bAddedGridMappingRef;
 
     /* create vars */
@@ -821,8 +822,9 @@ class netCDFDataset : public GDALPamDataset
 
     void ProcessCreationOptions( );
     int DefVarDeflate( int nVarId, bool bChunkingArg=true );
-    CPLErr AddProjectionVars( GDALProgressFunc pfnProgress=GDALDummyProgress,
-                              void * pProgressData=nullptr );
+    CPLErr AddProjectionVars( bool bDefsOnly,
+                              GDALProgressFunc pfnProgress,
+                              void * pProgressData );
     void AddGridMappingRef();
 
     bool GetDefineMode() { return bDefineMode; }

autotest/utilities/test_gdalwarp_lib.py
@@ -890,7 +890,7 @@ def test_gdalwarp_lib_124():
     out_ds.GetRasterBand(1).Fill(21)
     expected_cs = out_ds.GetRasterBand(1).Checksum()
 
-    gdal.Warp(out_ds, src_ds, format = 'MEM')
+    gdal.Warp(out_ds, src_ds)
 
     cs = out_ds.GetRasterBand(1).Checksum()
     if cs != expected_cs:
@@ -1714,6 +1714,40 @@ def test_gdalwarp_lib_touching_dateline():
     return 'success'
 
 ###############################################################################
+# Test fix for https://trac.osgeo.org/gdal/ticket/7245
+
+def test_gdalwarp_lib_override_default_output_nodata():
+
+    drv = gdal.GetDriverByName('netCDF')
+    if drv is None:
+        return 'skip'
+
+    creationoptionlist = drv.GetMetadataItem('DMD_CREATIONOPTIONLIST')
+    formats = [ 'NC' ]
+    if creationoptionlist.find('<Value>NC2</Value>') >= 0:
+        formats += [ 'NC2' ]
+    if creationoptionlist.find('<Value>NC4</Value>') >= 0:
+        formats += [ 'NC4', 'NC4C' ]
+
+    for format in formats:
+        gdal.Warp('tmp/out.nc', '../gcore/data/byte.tif', srcNodata = 255,
+                format = 'netCDF', creationOptions = ['FORMAT=' + format])
+        ds = gdal.Open('tmp/out.nc')
+        if ds.GetRasterBand(1).GetNoDataValue() != 255:
+            gdaltest.post_reason('fail')
+            print(format)
+            print(ds.GetRasterBand(1).GetNoDataValue())
+            return 'fail'
+        if ds.GetProjection() == '':
+            gdaltest.post_reason('fail')
+            print(format)
+            return 'fail'
+        ds = None
+        os.unlink('tmp/out.nc')
+
+    return 'success'
+
+###############################################################################
 # Cleanup
 
 def test_gdalwarp_lib_cleanup():
@@ -1800,6 +1834,7 @@ gdaltest_list = [
     test_gdalwarp_lib_136,
     test_gdalwarp_lib_several_sources_with_different_srs_no_explicit_target_srs,
     test_gdalwarp_lib_touching_dateline,
+    test_gdalwarp_lib_override_default_output_nodata,
     test_gdalwarp_lib_cleanup,
     ]
 

commit 3fb4a245d6c43796360db4de1e4d4a88011c29ef

netCDF: fix memory leak introduced in r41577 (trunk only, refs https://trac.osgeo.org/gdal/ticket/7245)
git-svn-id: https://svn.osgeo.org/gdal/trunk@41674 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/frmts/netcdf/netcdfdataset.cpp
@@ -1967,7 +1967,6 @@ netCDFDataset::~netCDFDataset()
 
     CPLFree(pszProjection);
     CPLFree(pszCFProjection);
-    CPLFree(pszCFCoordinates);
 
     if( cdfid > 0 )
     {
@@ -4207,7 +4206,7 @@ CPLErr netCDFDataset::AddProjectionVars( bool bDefsOnly,
         // Save value of pszCFCoordinates for later.
         if( bWriteLonLat )
         {
-            pszCFCoordinates = CPLStrdup(NCDF_LONLAT);
+            pszCFCoordinates = NCDF_LONLAT;
         }
     }
     else

gdal/frmts/netcdf/netcdfdataset.h
@@ -778,7 +778,7 @@ class netCDFDataset : public GDALPamDataset
     bool          bIsGdalFile; /* was this file created by GDAL? */
     bool          bIsGdalCfFile; /* was this file created by the (new) CF-compliant driver? */
     char         *pszCFProjection;
-    char         *pszCFCoordinates;
+    const char   *pszCFCoordinates;
     MultipleLayerBehaviour eMultipleLayerBehaviour;
     std::vector<netCDFDataset*> apoVectorDatasets;
 