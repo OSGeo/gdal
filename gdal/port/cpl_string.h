efa3baed8f50e0fcbe6f2d147bd3889bbce34d20
Even Rouault <even.rouault@mines-paris.org>
2018-03-10 18:47:09 +0000

    ENVI: code simplification
    git-svn-id: https://svn.osgeo.org/gdal/trunk@41690 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/port/cpl_string.h
+++ gdal/port/cpl_string.h
@@ -510,6 +510,8 @@ class CPL_DLL CPLStringList
     /** Return string at specified index */
     const char * operator[](size_t i) const {
       return (*this)[static_cast<int>(i)]; }
+    const char * operator[](const char* pszKey) const {
+        return FetchNameValue(pszKey); }
 
     /** Return list. Ownership remains to the object */
     char **List() { return papszList; }
