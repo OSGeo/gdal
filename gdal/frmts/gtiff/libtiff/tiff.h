ee3b75b3dc52d4fab652e1c4d9df29976cb0e730
Even Rouault <even.rouault@mines-paris.org>
2018-03-10 13:08:36 +0000

    Typo fixes
    git-svn-id: https://svn.osgeo.org/gdal/trunk@41686 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/gtiff/libtiff/tiff.h
+++ gdal/frmts/gtiff/libtiff/tiff.h
@@ -188,7 +188,7 @@ typedef enum {
 #define     COMPRESSION_SGILOG24	34677	/* SGI Log 24-bit packed */
 #define     COMPRESSION_JP2000          34712   /* Leadtools JPEG2000 */
 #define	    COMPRESSION_LZMA		34925	/* LZMA2 */
-#define	    COMPRESSION_ZSTD		34926	/* ZSTD: WARNING not registerd in Adobe-maintained registry */
+#define	    COMPRESSION_ZSTD		34926	/* ZSTD: WARNING not registered in Adobe-maintained registry */
 #define	TIFFTAG_PHOTOMETRIC		262	/* photometric interpretation */
 #define	    PHOTOMETRIC_MINISWHITE	0	/* min value is white */
 #define	    PHOTOMETRIC_MINISBLACK	1	/* min value is black */
