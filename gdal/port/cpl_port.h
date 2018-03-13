13ff3fcc8c3d89c8b612a81fb8fcd2cc579564ff
opalsTUW <opals@geo.tuwien.ac.at>
2018-03-13 18:55:01 +0100

    Update cpl_port.h

--- gdal/port/cpl_port.h
+++ gdal/port/cpl_port.h
@@ -338,11 +338,7 @@ typedef unsigned int  GUIntptr_t;
 
 #ifndef CPL_DLL
 #if defined(_MSC_VER) && !defined(CPL_DISABLE_DLL)
-#  ifdef GDAL_EXPORTS
-#    define CPL_DLL     __declspec(dllexport)
-#  else
-#    define CPL_DLL     __declspec(dllimport)
-#  endif
+#  define CPL_DLL     __declspec(dllexport)
 #else
 #  if defined(USE_GCC_VISIBILITY_FLAG)
 #    define CPL_DLL     __attribute__ ((visibility("default")))
