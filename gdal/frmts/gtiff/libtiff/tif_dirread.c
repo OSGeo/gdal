0f9af0ea14ce9df2358c725e5d8e0dd958379b56
Even Rouault <even.rouault@mines-paris.org>
2018-03-13 14:43:49 +0000

    GTiff / internal libtiff: fix CVE-2017-11613 / https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=6832. Credit to OSS Fuzz (independantly discovered by other parties as well)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@41775 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/gtiff/libtiff/tif_dirread.c
+++ gdal/frmts/gtiff/libtiff/tif_dirread.c
@@ -5696,6 +5696,17 @@ ChopUpSingleUncompressedStrip(TIFF* tif)
         if( nstrips == 0 )
             return;
 
+        /* If we are going to allocate a lot of memory, make sure that the */
+        /* file is as big as needed */
+        if( tif->tif_mode == O_RDONLY &&
+            nstrips > 1000000 &&
+            (tif->tif_dir.td_stripoffset[0] >= TIFFGetFileSize(tif) ||
+             tif->tif_dir.td_stripbytecount[0] >
+                    TIFFGetFileSize(tif) - tif->tif_dir.td_stripoffset[0]) )
+        {
+            return;
+        }
+
 	newcounts = (uint64*) _TIFFCheckMalloc(tif, nstrips, sizeof (uint64),
 				"for chopped \"StripByteCounts\" array");
 	newoffsets = (uint64*) _TIFFCheckMalloc(tif, nstrips, sizeof (uint64),
