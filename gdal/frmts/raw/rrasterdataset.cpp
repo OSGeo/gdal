a4d5af47a185489eda9cc419f644d146143fe842
Even Rouault <even.rouault@mines-paris.org>
2018-03-15 18:53:04 +0000

    RRASTER: fix wrong declaration of PIXELTYPE=SIGNEDBYTE for datatype=INT1S
    git-svn-id: https://svn.osgeo.org/gdal/trunk@41813 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/raw/rrasterdataset.cpp
+++ gdal/frmts/raw/rrasterdataset.cpp
@@ -647,7 +647,7 @@ GDALDataset *RRASTERDataset::Open( GDALOpenInfo * poOpenInfo )
         if( EQUAL(osDataType, "INT1S") )
         {
             poDS->GetRasterBand(i)->SetMetadataItem(
-                    "SIGNEDBYTE", "PIXELTYPE", "IMAGE_STRUCTURE" );
+                    "PIXELTYPE", "SIGNEDBYTE", "IMAGE_STRUCTURE" );
         }
         if( !osNoDataValue.empty() && !EQUAL(osNoDataValue, "NA") )
         {
