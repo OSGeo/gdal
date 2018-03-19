6e0c1c4cb883349f17c196ee16a825fad9cd6f7a
Even Rouault <even.rouault@mines-paris.org>
2018-03-19 09:18:43 +0000

    fuzzer: add a -repeat switch
    git-svn-id: https://svn.osgeo.org/gdal/trunk@41864 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/fuzzers/fuzzingengine.cpp
+++ gdal/fuzzers/fuzzingengine.cpp
@@ -28,42 +28,83 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-int LLVMFuzzerTestOneInput(void *buf, size_t len);
+extern "C"
+{
+int LLVMFuzzerTestOneInput(const void *buf, size_t len);
 int LLVMFuzzerInitialize(int* argc, char*** argv);
+}
+
+static void Usage(int, char* argv[])
+{
+    fprintf(stderr, "%s [--help] [-repeat N] filename.\n", argv[0]);
+    exit(1);
+}
 
 int main(int argc, char* argv[])
 {
     LLVMFuzzerInitialize(&argc, &argv);
-    if( argc < 2 )
+
+    int nRet = 0;
+    void* buf = nullptr;
+    int nLen = 0;
+    int nLoops = 1;
+    const char* pszFilename = nullptr;
+    for(int i = 1; i < argc; i++ )
     {
-        return LLVMFuzzerTestOneInput(" ", 1);
+        if( i + 1 < argc && strcmp(argv[i], "-repeat") == 0 )
+        {
+            nLoops = atoi(argv[i+1]);
+            i++;
+        }
+        else if( strcmp(argv[i], "-dummy") == 0 )
+        {
+            return LLVMFuzzerTestOneInput(" ", 1);
+        }
+        else if( strcmp(argv[i], "--help") == 0 )
+        {
+            Usage(argc, argv);
+        }
+        else if( argv[i][0] == '-' )
+        {
+            fprintf(stderr, "Unrecognized option: %s", argv[i]);
+            Usage(argc, argv);
+        }
+        else
+        {
+            pszFilename = argv[i];
+        }
     }
-    else
+    if( pszFilename == nullptr )
     {
-        int nRet = 0;
-        void* buf = NULL;
-        int nLen = 0;
-        FILE* f = fopen(argv[1], "rb");
-        if( !f )
-        {
-            fprintf(stderr, "%s does not exist.\n", argv[1]);
-            exit(1);
-        }
-        fseek(f, 0, SEEK_END);
-        nLen = (int)ftell(f);
-        fseek(f, 0, SEEK_SET);
-        buf = malloc(nLen);
-        if( !buf )
-        {
-            fprintf(stderr, "malloc failed.\n");
-            fclose(f);
-            exit(1);
-        }
-        fread(buf, nLen, 1, f);
+        fprintf(stderr, "No filename specified\n");
+        Usage(argc, argv);
+    }
+    FILE* f = fopen(pszFilename, "rb");
+    if( !f )
+    {
+        fprintf(stderr, "%s does not exist.\n", pszFilename);
+        exit(1);
+    }
+    fseek(f, 0, SEEK_END);
+    nLen = (int)ftell(f);
+    fseek(f, 0, SEEK_SET);
+    buf = malloc(nLen);
+    if( !buf )
+    {
+        fprintf(stderr, "malloc failed.\n");
         fclose(f);
-        nRet = LLVMFuzzerTestOneInput(buf, nLen);
-        free(buf);
-        return nRet;
+        exit(1);
     }
+    fread(buf, nLen, 1, f);
+    fclose(f);
+    for( int i = 0; i < nLoops; i++ )
+    {
+        nRet = LLVMFuzzerTestOneInput(buf, nLen);
+        if( nRet != 0 )
+            break;
+    }
+    free(buf);
+    return nRet;
 }
