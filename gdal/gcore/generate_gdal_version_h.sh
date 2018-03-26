523ce9e722da7c655df054257876325a80cf03a6
Even Rouault <even.rouault@spatialys.com>
2018-03-26 22:58:29 +0200

    generate_gdal_version_h.sh: make it work on non-git extracts, and allow to override with GDAL_SHA1SUM and GDAL_RELEASE_DATE env variables

--- gdal/gcore/generate_gdal_version_h.sh
+++ gdal/gcore/generate_gdal_version_h.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 set -e
-if git --version >/dev/null 2>/dev/null && grep dev gdal_version.h.in >/dev/null; then
+
+if test "x$GDAL_SHA1SUM" != "x" -a "x$GDAL_RELEASE_DATE" != "x"; then
+        if test -f gdal_version.h; then
+                cp gdal_version.h gdal_version.h.bak
+        else
+                touch gdal_version.h.bak
+        fi
+        echo "/* This is a generated file from gdal_version.h.in. DO NOT MODIFY !!!! */" > gdal_version.h.new
+        echo "" >> gdal_version.h.new
+        cat gdal_version.h.in >> gdal_version.h.new
+        sed -i.bak "s/dev/dev\-$GDAL_SHA1SUM/" gdal_version.h.new && rm gdal_version.h.new.bak
+        sed -i.bak "s/define GDAL_RELEASE_DATE.*/define GDAL_RELEASE_DATE     $GDAL_RELEASE_DATE/" gdal_version.h.new && rm gdal_version.h.new.bak
+        diff -u gdal_version.h.new gdal_version.h.bak >/dev/null || \
+            (echo "Update gdal_version.h"; \
+             cp gdal_version.h.new gdal_version.h)
+        rm -f gdal_version.h.bak
+        rm -f gdal_version.h.new
+elif git log -1 >/dev/null 2>/dev/null && grep dev gdal_version.h.in >/dev/null; then
         REV=`git log -1 --format="%h"`
         DATE=`git log -1 --date=format:'%Y%m%d' --format="%ad" 2>/dev/null` || DATE=""
         if git status --porcelain -uno | grep . >/dev/null; then REV="$REV-dirty"; fi
