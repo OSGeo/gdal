3e9d922f6235cbf10dcc765d1326add45431fb8a
Even Rouault <even.rouault@spatialys.com>
2018-04-02 16:53:25 +0200

    class OGRGenSQLResultsLayer: tag it as final, and remove useless CPL_DLL

--- gdal/ogr/ogrsf_frmts/generic/ogr_gensql.h
+++ gdal/ogr/ogrsf_frmts/generic/ogr_gensql.h
@@ -54,7 +54,7 @@
 /*                        OGRGenSQLResultsLayer                         */
 /************************************************************************/
 
-class CPL_DLL OGRGenSQLResultsLayer : public OGRLayer
+class OGRGenSQLResultsLayer final: public OGRLayer
 {
   private:
     GDALDataset *poSrcDS;
