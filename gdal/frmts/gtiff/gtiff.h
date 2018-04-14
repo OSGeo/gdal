7b0895f5efe8a4ef023dc66fffbe8db2b02317c3
Even Rouault <even.rouault@spatialys.com>
2018-04-14 17:22:05 +0200

    External .ovr: make sure that ExtraSamples tag is written

--- gdal/frmts/gtiff/gtiff.h
+++ gdal/frmts/gtiff/gtiff.h
@@ -60,6 +60,11 @@ CPLString GTiffFormatGDALNoDataTagValue( double dfNoData );
 
 const int knGTIFFJpegTablesModeDefault = 1; /* JPEGTABLESMODE_QUANT */
 
+// Note: Was EXTRASAMPLE_ASSOCALPHA in GDAL < 1.10.
+constexpr uint16 DEFAULT_ALPHA_TYPE = EXTRASAMPLE_UNASSALPHA;
+
+uint16 GTiffGetAlphaValue(const char* pszValue, uint16 nDefault);
+
 #define TIFFTAG_GDAL_METADATA  42112
 #define TIFFTAG_GDAL_NODATA    42113
 #define TIFFTAG_RPCCOEFFICIENT 50844
