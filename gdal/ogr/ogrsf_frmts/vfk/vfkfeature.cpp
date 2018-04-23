OSGeo/gdal

commit 66f91646136993b94566d5080b9576482cc5fe0e

New issues with VFK parser logic, see #365

gdal/ogr/ogrsf_frmts/vfk/vfkfeature.cpp
@@ -381,24 +381,33 @@ bool VFKFeature::SetProperties(const char *pszLine)
     if( *poChar == '\0' )
         return false; /* nothing to read */
 
-    poChar++; /* skip ';' after data block name*/
+    poChar++; /* skip ';' after data block name */
 
     /* remove extra quotes (otherwise due to buggy format the parsing is
-     * almost impossible */
+     * almost impossible) */
     CPLString osLine;
-    while ( *poChar != '\0' ) {
-        osLine += *poChar;
+    while( *poChar != '\0' ) {
+        if( *poChar == '"' ) {
+            /* count quotes */
+            int nQuotes = 1;
+            while( *(++poChar) == '"' )
+                nQuotes++;
 
-        if ( *poChar == '"' &&
-             !( *(poChar-1) == ';' && *(poChar+1) == '"' &&     /* skip ;""; */
-                ( *(poChar+2) == ';' || *(poChar+2) == '\0' ) ) ) {
-            while ( *poChar == '"' ) {
-                poChar++;
+            if( nQuotes % 2 != 0 ) {
+                /* even number of quotes -> only last quote used */
+                poChar -= 1;
+            }
+            else {
+                if( ( *poChar == ';' || *poChar == '\0') &&
+                    *(poChar-nQuotes-1) == ';' ) {
+                    /* empty values (;""; / ;"" / ;""""; / ...)
+                       -> only last two quotes used */
+                    poChar -= 2;
+                }
+                /* odd number of quotes -> none of quotes used */
             }
         }
-        else {
-            poChar++;
-        }
+        osLine += *(poChar++);
     }
     poChar = osLine;
 

commit a37c17b713a675cd7e9af32175351b1a8d9557f1

VFK: fix potential heap buffer overflow caused by 66f91646136993b94566d5080b9576482cc5fe0e (refs #365). Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=7953. Credit to OSS Fuzz

gdal/ogr/ogrsf_frmts/vfk/vfkfeature.cpp
@@ -404,6 +404,8 @@ bool VFKFeature::SetProperties(const char *pszLine)
                        -> only last two quotes used */
                     poChar -= 2;
                 }
+                else if( *poChar == '\0' )
+                    break;
                 /* odd number of quotes -> none of quotes used */
             }
         }