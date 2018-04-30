77627c9426f2221d04a4a30721342a58bb01759a
Ben Elliston <ben.elliston@anu.edu.au>
2018-04-30 20:07:13 +1000

    More.

--- gdal/ci/travis/osx/script.sh
+++ gdal/ci/travis/osx/script.sh
@@ -2,14 +2,10 @@
 
 set -e
 
-cd gdal
-export PYTHONPATH=$PWD/swig/python/build/lib.macosx-10.12-intel-2.7:$PWD/swig/python/build/lib.macosx-10.11-x86_64-2.7
+export PYTHONPATH=$PWD/gdal/swig/python/build/lib.macosx-10.12-intel-2.7:$PWD/gdal/swig/python/build/lib.macosx-10.11-x86_64-2.7
 
 # CPP unit tests
-cd ../autotest
-cd cpp
-GDAL_SKIP=JP2ECW make quick_test
-cd ..
+(cd autotest/cpp && GDAL_SKIP=JP2ECW make quick_test)
+
 # Run all the Python autotests
-#make -j test
-python run_all.py
+(cd autotest && python run_all.py)
