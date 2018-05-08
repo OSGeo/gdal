c30a34f419f6f616141669f6c5cd91280322e0a7
Ben Elliston <bje@air.net.au>
2018-05-08 19:27:21 +1000

    Python code: Disable more Pylint warnings (#592)

--- autotest/gcore/rasterio.py
+++ autotest/gcore/rasterio.py
@@ -371,6 +371,7 @@ def rasterio_8_progress_interrupt_callback(pct, message, user_data):
 
 
 def rasterio_8_progress_callback_2(pct, message, user_data):
+    # pylint: disable=unused-argument
     if pct < user_data[0]:
         print('Got %f, last pct was %f' % (pct, user_data[0]))
         return 0
@@ -512,6 +513,7 @@ def rasterio_8():
 
 
 def rasterio_9_progress_callback(pct, message, user_data):
+    # pylint: disable=unused-argument
     if pct < user_data[0]:
         print('Got %f, last pct was %f' % (pct, user_data[0]))
         return 0
