OSGeo/gdal

commit 83d53c738a14b78a2b55ae3367ebb2a5a54e454d

XLSX: avoid stack buffer overflow is creating too many fields. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=8198. Credit to OSS Fuzz

gdal/ogr/ogrsf_frmts/xlsx/ogrxlsxdatasource.cpp
@@ -112,6 +112,20 @@ OGRFeature* OGRXLSXLayer::GetNextFeature()
     return poFeature;
 }
 
+OGRErr OGRXLSXLayer::CreateField( OGRFieldDefn *poField, int bApproxOK )
+{
+    Init();
+    // BuildColString() takes a 4 character string + nul byte
+    if( GetLayerDefn()->GetFieldCount() >= 2000 )
+    {
+        CPLError(CE_Failure, CPLE_AppDefined,
+                 "Maximum number of fields supported is 2000");
+        return OGRERR_FAILURE;
+    }
+    SetUpdated();
+    return OGRMemLayer::CreateField(poField, bApproxOK);
+}
+
 /************************************************************************/
 /*                           GetFeature()                               */
 /************************************************************************/

gdal/ogr/ogrsf_frmts/xlsx/ogr_xlsx.h
@@ -95,8 +95,7 @@ class OGRXLSXLayer final: public OGRMemLayer
     { Init(); return OGRMemLayer::GetFeatureCount(bForce); }
 
     virtual OGRErr      CreateField( OGRFieldDefn *poField,
-                                     int bApproxOK = TRUE ) override
-    { Init(); SetUpdated(); return OGRMemLayer::CreateField(poField, bApproxOK); }
+                                     int bApproxOK = TRUE ) override;
 
     virtual OGRErr      DeleteField( int iField ) override
     { Init(); SetUpdated(); return OGRMemLayer::DeleteField(iField); }

commit 32c46a1a38d45be511d0ce518201503a45d22c72

XLSX: fix null pointer dereference caused by 83d53c738a14b78a2b55ae3367ebb2a5a54e454d. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=8227. Credit to OSS Fuzz

gdal/ogr/ogrsf_frmts/xlsx/ogrxlsxdatasource.cpp
@@ -882,7 +882,10 @@ void OGRXLSXDataSource::endElementRow(CPL_UNUSED const char *pszNameIn)
                     }
                     OGRFieldDefn oFieldDefn(pszFieldName, eType);
                     oFieldDefn.SetSubType(eSubType);
-                    poCurLayer->CreateField(&oFieldDefn);
+                    if( poCurLayer->CreateField(&oFieldDefn) != OGRERR_NONE )
+                    {
+                        return;
+                    }
                 }
             }
             else
@@ -898,7 +901,10 @@ void OGRXLSXDataSource::endElementRow(CPL_UNUSED const char *pszNameIn)
                                             eSubType);
                     OGRFieldDefn oFieldDefn(pszFieldName, eType);
                     oFieldDefn.SetSubType(eSubType);
-                    poCurLayer->CreateField(&oFieldDefn);
+                    if( poCurLayer->CreateField(&oFieldDefn) != OGRERR_NONE )
+                    {
+                        return;
+                    }
                 }
 
                 OGRFeature* poFeature = new OGRFeature(poCurLayer->GetLayerDefn());
@@ -942,7 +948,10 @@ void OGRXLSXDataSource::endElementRow(CPL_UNUSED const char *pszNameIn)
                                                 eSubType);
                     OGRFieldDefn oFieldDefn(pszFieldName, eType);
                     oFieldDefn.SetSubType(eSubType);
-                    poCurLayer->CreateField(&oFieldDefn);
+                    if( poCurLayer->CreateField(&oFieldDefn) != OGRERR_NONE )
+                    {
+                        return;
+                    }
                 }
             }
 