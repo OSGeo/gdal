3962dacf02c7c6f420a9cb13f67185af5ed1ea7e
Even Rouault <even.rouault@spatialys.com>
2018-05-14 11:29:01 +0200

    XLSX: fix null pointer dereference. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=8286. Credit to OSS Fuzz

--- gdal/ogr/ogrsf_frmts/xlsx/ogrxlsxdatasource.cpp
+++ gdal/ogr/ogrsf_frmts/xlsx/ogrxlsxdatasource.cpp
@@ -714,7 +714,10 @@ void OGRXLSXDataSource::endElementTable(CPL_UNUSED const char *pszNameIn)
                                                      eSubType);
                 OGRFieldDefn oFieldDefn(pszFieldName, eType);
                 oFieldDefn.SetSubType(eSubType);
-                poCurLayer->CreateField(&oFieldDefn);
+                if( poCurLayer->CreateField(&oFieldDefn) != OGRERR_NONE )
+                {
+                    return;
+                }
             }
 
             OGRFeature* poFeature = new OGRFeature(poCurLayer->GetLayerDefn());
