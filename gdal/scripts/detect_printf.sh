5e5ca2cf6c7ec8a9d9c3a0c60bf6725f5883d0ef
Even Rouault <even.rouault@spatialys.com>
2018-05-24 16:17:12 +0200

    Fix scripts/detect_printf.sh

--- gdal/scripts/detect_printf.sh
+++ gdal/scripts/detect_printf.sh
@@ -21,7 +21,7 @@ ret_code=0
 
 echo "Checking for printf() statements..."
 # apps is voluntarily missing in the list of directories, due to lots of legitimate uses of such statements in programs
-if grep -r --include="*.c*" " printf" alg gnm port ogr gcore frmts | grep -v -e "/*ok" -e "printf()" -e "printf-like" -e "printf style" -e "with printf"  -e "/\\* printf" -e "//" -e degrib -e aitest -e dted_test -e giflib -e 8211view -e 8211dump -e timetest -e 8211createfromxml -e hfatest -e zlib -e libtiff -e envisat_dump -e dumpgeo -e nitfdump -e ceostest-e libpng -e generate_encoding -e capi_test -e ntfdump -e test_load_virtual_ogr -e ocitest -e fastload -e s57dump -e dgndump -e test_geo_utils -e testparser -e internal_libqhull ; then
+if grep -r --include="*.c*" " printf" alg gnm port ogr gcore frmts | grep -v -e "/*ok" -e "printf()" -e "printf-like" -e "printf style" -e "with printf"  -e "/\\* printf" -e "//" -e degrib -e aitest -e dted_test -e giflib -e 8211view -e 8211dump -e timetest -e 8211createfromxml -e hfatest -e zlib -e libtiff -e envisat_dump -e dumpgeo -e nitfdump -e ceostest -e libpng -e generate_encoding -e capi_test -e ntfdump -e test_load_virtual_ogr -e ocitest -e fastload -e s57dump -e dgndump -e test_geo_utils -e testparser -e internal_libqhull ; then
     echo "FAIL: suspicious printf found. Remove or tag it with /*ok*/"
     ret_code=1
 else
