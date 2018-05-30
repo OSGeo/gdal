1f8d17a1e1c7036365e119400df15f9ba75de6fb
Ben Elliston <ben.elliston@anu.edu.au>
2018-05-30 22:24:13 +1000

    No longer any need to exclude gdal-bash-completion.sh from Shellcheck.

--- gdal/ci/travis/trusty_32bit/script.sh
+++ gdal/ci/travis/trusty_32bit/script.sh
@@ -21,4 +21,4 @@ mv autotest/gcore/vsigs.py autotest/gcore/vsigs.py.disabled
 sudo i386 chroot "$chroot" sh -c "cd $PWD/autotest && python run_all.py"
 
 # Run Shellcheck
-shellcheck -e SC2086,SC2046 $(find $PWD/gdal -name '*.sh' -a -not -name ltmain.sh -a -not -name gdal-bash-completion.sh)
+shellcheck -e SC2086,SC2046 $(find $PWD/gdal -name '*.sh' -a -not -name ltmain.sh)
