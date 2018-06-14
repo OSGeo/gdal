OSGeo/gdal

commit a0845a051c0cd1cc7dfbeebb776dc325af262cac

GPKG/SQLite: make sure to not invalidate POSIX advisory locks
See comment in gcore/gdalopeninfo.cpp preceding the declaration of
GDALOpenInfoDeclareFileNotToOpen() for detailed explanation of this
somewhat messy workaround.

gdal/ogr/ogrsf_frmts/gpkg/ogrgeopackagedatasource.cpp
@@ -40,6 +40,13 @@
 
 CPL_CVSID("$Id$")
 
+// Keep in sync prototype of those 2 functions between gdalopeninfo.cpp,
+// ogrsqlitedatasource.cpp and ogrgeopackagedatasource.cpp
+void GDALOpenInfoDeclareFileNotToOpen(const char* pszFilename,
+                                       const GByte* pabyHeader,
+                                       int nHeaderBytes);
+void GDALOpenInfoUnDeclareFileNotToOpen(const char* pszFilename);
+
 /************************************************************************/
 /*                             Tiling schemes                           */
 /************************************************************************/
@@ -814,6 +821,13 @@ int GDALGeoPackageDataset::Open( GDALOpenInfo* poOpenInfo )
         }
         pabyHeader = abyHeaderLetMeHerePlease;
     }
+    else
+    {
+        m_bCallUndeclareFileNotToOpen = true;
+        GDALOpenInfoDeclareFileNotToOpen(osFilename,
+                                    poOpenInfo->pabyHeader,
+                                    poOpenInfo->nHeaderBytes);
+    }
 
     bUpdate = poOpenInfo->eAccess == GA_Update;
     eAccess = poOpenInfo->eAccess; /* hum annoying duplication */
@@ -913,6 +927,14 @@ int GDALGeoPackageDataset::Open( GDALOpenInfo* poOpenInfo )
     else if( pabyHeader != nullptr )
 #endif
     {
+        if (poOpenInfo->fpL )
+        {
+            // See above comment about -wal locking for the importance of
+            // closing that file, prior to calling sqlite3_open()
+            VSIFCloseL(poOpenInfo->fpL);
+            poOpenInfo->fpL = nullptr;
+        }
+
         /* See if we can open the SQLite database */
         if( !OpenOrCreateDB(bUpdate
                         ? SQLITE_OPEN_READWRITE

gdal/ogr/ogrsf_frmts/sqlite/ogrsqlitedatasource.cpp
@@ -89,6 +89,14 @@
 static int bSpatialiteGlobalLoaded = FALSE;
 #endif
 
+// Keep in sync prototype of those 2 functions between gdalopeninfo.cpp,
+// ogrsqlitedatasource.cpp and ogrgeopackagedatasource.cpp
+void GDALOpenInfoDeclareFileNotToOpen(const char* pszFilename,
+                                       const GByte* pabyHeader,
+                                       int nHeaderBytes);
+void GDALOpenInfoUnDeclareFileNotToOpen(const char* pszFilename);
+
+
 CPL_CVSID("$Id$")
 
 /************************************************************************/
@@ -345,6 +353,12 @@ OGRSQLiteBaseDataSource::~OGRSQLiteBaseDataSource()
     FinishRasterLite2();
 #endif
     CloseDB();
+
+    if( m_bCallUndeclareFileNotToOpen )
+    {
+        GDALOpenInfoUnDeclareFileNotToOpen(m_pszFilename);
+    }
+
     CPLFree(m_pszFilename);
 }
 
@@ -383,6 +397,12 @@ void OGRSQLiteBaseDataSource::CloseDB()
 
                 sqlite3_close( hDB );
                 hDB = nullptr;
+#ifdef DEBUG_VERBOSE
+                if( VSIStatL( CPLSPrintf("%s-wal", m_pszFilename), &sStat) != 0 )
+                {
+                    CPLDebug("SQLite", "%s-wal file has been removed", m_pszFilename);
+                }
+#endif
             }
         }
 
@@ -1323,6 +1343,10 @@ int OGRSQLiteDataSource::Open( GDALOpenInfo* poOpenInfo)
         else
         {
             m_pszFilename = CPLStrdup( pszNewName );
+            m_bCallUndeclareFileNotToOpen = true;
+            GDALOpenInfoDeclareFileNotToOpen(m_pszFilename,
+                                        poOpenInfo->pabyHeader,
+                                        poOpenInfo->nHeaderBytes);
         }
     }
     SetPhysicalFilename(m_pszFilename);
@@ -1364,13 +1388,20 @@ int OGRSQLiteDataSource::Open( GDALOpenInfo* poOpenInfo)
 #endif
 
 #ifdef ENABLE_SQL_SQLITE_FORMAT
-        GDALOpenInfo oOpenInfo(m_pszFilename, GA_ReadOnly);
-        if( oOpenInfo.pabyHeader &&
+        // SQLite -wal locking appears to be extremely fragile. In particular
+        // if we have a file descriptor opened on the file while sqlite3_open
+        // is called, then it will mis-behave (a process opening in update mode
+        // the file and closing it will remove the -wal file !)
+        // So make sure that the GDALOpenInfo object goes out of scope before
+        // going on.
+        {
+          GDALOpenInfo oOpenInfo(m_pszFilename, GA_ReadOnly);
+          if( oOpenInfo.pabyHeader &&
             (STARTS_WITH((const char*)oOpenInfo.pabyHeader, "-- SQL SQLITE") ||
              STARTS_WITH((const char*)oOpenInfo.pabyHeader, "-- SQL RASTERLITE") ||
              STARTS_WITH((const char*)oOpenInfo.pabyHeader, "-- SQL MBTILES")) &&
             oOpenInfo.fpL != nullptr )
-        {
+          {
             if( sqlite3_open_v2( ":memory:", &hDB, SQLITE_OPEN_READWRITE, nullptr )
                     != SQLITE_OK )
             {
@@ -1467,11 +1498,25 @@ int OGRSQLiteDataSource::Open( GDALOpenInfo* poOpenInfo)
                 }
                 sqlite3_free(pszErrMsg);
             }
+          }
         }
-        else
+        if( hDB == nullptr )
 #endif
-        if (!OpenOrCreateDB((bUpdate) ? SQLITE_OPEN_READWRITE : SQLITE_OPEN_READONLY, TRUE) )
-            return FALSE;
+        {
+            if (poOpenInfo->fpL )
+            {
+                // See above comment about -wal locking for the importance of
+                // closing that file, prior to calling sqlite3_open()
+                VSIFCloseL(poOpenInfo->fpL);
+                poOpenInfo->fpL = nullptr;
+            }
+            if (!OpenOrCreateDB((bUpdate) ? SQLITE_OPEN_READWRITE : SQLITE_OPEN_READONLY, TRUE) )
+            {
+                poOpenInfo->fpL = VSIFOpenL(poOpenInfo->pszFilename,
+                            poOpenInfo->eAccess == GA_Update ? "rb+" : "rb");
+                return FALSE;
+            }
+        }
 
 #ifdef SPATIALITE_412_OR_LATER
         InitNewSpatialite();

autotest/ogr/ogr_gpkg.py
@@ -4142,6 +4142,7 @@ def ogr_gpkg_47():
     if gdal.GetLastErrorMsg() == '':
         gdaltest.post_reason('fail')
         return 'fail'
+    ds = None
 
     gdal.SetConfigOption('GPKG_WARN_UNRECOGNIZED_APPLICATION_ID', 'NO')
     ogr.Open('/vsimem/ogr_gpkg_47.gpkg')
@@ -4165,6 +4166,7 @@ def ogr_gpkg_47():
     if gdal.GetLastErrorMsg() != '':
         gdaltest.post_reason('fail')
         return 'fail'
+    ds = None
 
     gdal.SetConfigOption('GPKG_WARN_UNRECOGNIZED_APPLICATION_ID', 'NO')
     ogr.Open('/vsimem/ogr_gpkg_47.gpkg')
@@ -4714,6 +4716,49 @@ def ogr_gpkg_savepoint():
     return 'success'
 
 ###############################################################################
+# Test that we don't open file handles behind the back of sqlite3
+
+
+def ogr_gpkg_wal():
+
+    if gdaltest.gpkg_dr is None:
+        return 'skip'
+
+    import test_cli_utilities
+    if test_cli_utilities.get_ogrinfo_path() is None:
+        return 'skip'
+
+    # needs to be a real file
+    filename = 'tmp/ogr_gpkg_wal.gpkg'
+
+    with gdaltest.config_option('OGR_SQLITE_JOURNAL', 'WAL'):
+        ds = gdaltest.gpkg_dr.CreateDataSource(filename)
+    lyr = ds.CreateLayer('foo')
+    ds = None
+
+    ds = ogr.Open(filename, update=1)
+    os.stat(filename + '-wal')
+
+    # Re-open in read-only mode
+    ds_ro = ogr.Open(filename)
+    os.stat(filename + '-wal')
+
+    # Test external process to read the file
+    gdaltest.runexternal(test_cli_utilities.get_ogrinfo_path() + ' ' + filename)
+
+    # The file must still exist
+    os.stat(filename + '-wal')
+
+    ds = None
+    ds_ro = None
+
+    gdal.Unlink(filename)
+    gdal.Unlink(filename + '-wal')
+    gdal.Unlink(filename + '-shm')
+
+    return 'success'
+
+###############################################################################
 # Remove the test db from the tmp directory
 
 
@@ -4800,6 +4845,7 @@ gdaltest_list = [
     ogr_gpkg_58,
     ogr_gpkg_59,
     ogr_gpkg_savepoint,
+    ogr_gpkg_wal,
     ogr_gpkg_test_ogrsf,
     ogr_gpkg_cleanup,
 ]

gdal/gcore/gdalopeninfo.cpp
@@ -37,6 +37,8 @@
 #endif
 
 #include <algorithm>
+#include <map>
+#include <mutex>
 #include <vector>
 
 #include "cpl_config.h"
@@ -47,6 +49,108 @@
 #include "gdal.h"
 
 CPL_CVSID("$Id$")
+
+// Keep in sync prototype of those 2 functions between gdalopeninfo.cpp,
+// ogrsqlitedatasource.cpp and ogrgeopackagedatasource.cpp
+void GDALOpenInfoDeclareFileNotToOpen(const char* pszFilename,
+                                       const GByte* pabyHeader,
+                                       int nHeaderBytes);
+void GDALOpenInfoUnDeclareFileNotToOpen(const char* pszFilename);
+
+/************************************************************************/
+
+/* This whole section helps for SQLite/GPKG, especially with write-ahead
+ * log enabled. The issue is that sqlite3 relies on POSIX advisory locks to
+ * properly work and decide when to create/delete the wal related files.
+ * One issue with POSIX advisory locks is that if within the same process
+ * you do
+ * f1 = open('somefile')
+ * set locks on f1
+ * f2 = open('somefile')
+ * close(f2)
+ * The close(f2) will cancel the locks set on f1. The work on f1 is done by
+ * libsqlite3 whereas the work on f2 is done by GDALOpenInfo.
+ * So as soon as sqlite3 has opened a file we should make sure not to re-open
+ * it (actually close it) ourselves.
+ */
+
+namespace {
+struct FileNotToOpen
+{
+    CPLString osFilename{};
+    int       nRefCount{};
+    GByte    *pabyHeader{nullptr};
+    int       nHeaderBytes{0};
+};
+}
+
+static std::mutex sFNTOMutex;
+static std::map<CPLString, FileNotToOpen>* pMapFNTO = nullptr;
+
+void GDALOpenInfoDeclareFileNotToOpen(const char* pszFilename,
+                                       const GByte* pabyHeader,
+                                       int nHeaderBytes)
+{
+    std::lock_guard<std::mutex> oLock(sFNTOMutex);
+    if( pMapFNTO == nullptr )
+        pMapFNTO = new std::map<CPLString, FileNotToOpen>();
+    auto oIter = pMapFNTO->find(pszFilename);
+    if( oIter != pMapFNTO->end() )
+    {
+        oIter->second.nRefCount ++;
+    }
+    else
+    {
+        FileNotToOpen fnto;
+        fnto.osFilename = pszFilename;
+        fnto.nRefCount = 1;
+        fnto.pabyHeader = static_cast<GByte*>(CPLMalloc(nHeaderBytes + 1));
+        memcpy(fnto.pabyHeader, pabyHeader, nHeaderBytes);
+        fnto.pabyHeader[nHeaderBytes] = 0;
+        fnto.nHeaderBytes = nHeaderBytes;
+        (*pMapFNTO)[pszFilename] = fnto;
+    }
+}
+
+void GDALOpenInfoUnDeclareFileNotToOpen(const char* pszFilename)
+{
+    std::lock_guard<std::mutex> oLock(sFNTOMutex);
+    CPLAssert(pMapFNTO);
+    auto oIter = pMapFNTO->find(pszFilename);
+    CPLAssert( oIter != pMapFNTO->end() );
+    oIter->second.nRefCount --;
+    if( oIter->second.nRefCount == 0 )
+    {
+        CPLFree(oIter->second.pabyHeader);
+        pMapFNTO->erase(oIter);
+    }
+    if( pMapFNTO->empty() )
+    {
+        delete pMapFNTO;
+        pMapFNTO = nullptr;
+    }
+}
+
+static GByte* GDALOpenInfoGetFileNotToOpen(const char* pszFilename,
+                                           int* pnHeaderBytes)
+{
+    std::lock_guard<std::mutex> oLock(sFNTOMutex);
+    *pnHeaderBytes = 0;
+    if( pMapFNTO == nullptr )
+    {
+        return nullptr;
+    }
+    auto oIter = pMapFNTO->find(pszFilename);
+    if( oIter == pMapFNTO->end() )
+    {
+        return nullptr;
+    }
+    *pnHeaderBytes = oIter->second.nHeaderBytes;
+    GByte* pabyHeader = static_cast<GByte*>(CPLMalloc(*pnHeaderBytes + 1));
+    memcpy(pabyHeader, oIter->second.pabyHeader, *pnHeaderBytes);
+    pabyHeader[*pnHeaderBytes] = 0;
+    return pabyHeader;
+}
 
 /************************************************************************/
 /* ==================================================================== */
@@ -155,10 +259,17 @@ retry:  // TODO(schwehr): Stop using goto.
         }
     }
 
-    if( !bIsDirectory ) {
+    pabyHeader = GDALOpenInfoGetFileNotToOpen(pszFilename, &nHeaderBytes);
+
+    if( !bIsDirectory && pabyHeader == nullptr ) {
         fpL = VSIFOpenExL( pszFilename, (eAccess == GA_Update) ? "r+b" : "rb", (nOpenFlagsIn & GDAL_OF_VERBOSE_ERROR) > 0);
     }
-    if( fpL != nullptr )
+    if( pabyHeader )
+    {
+        bStatOK = TRUE;
+        nHeaderBytesTried = nHeaderBytes;
+    }
+    else if( fpL != nullptr )
     {
         bStatOK = TRUE;
         int nBufSize =

gdal/ogr/ogrsf_frmts/gpkg/ogrgeopackagedriver.cpp
@@ -41,10 +41,6 @@ static int OGRGeoPackageDriverIdentify( GDALOpenInfo* poOpenInfo, bool bEmitWarn
     if( STARTS_WITH_CI(poOpenInfo->pszFilename, "GPKG:") )
         return TRUE;
 
-    /* Check that the filename exists and is a file */
-    if( poOpenInfo->fpL == nullptr)
-        return FALSE;
-
 #ifdef ENABLE_SQL_GPKG_FORMAT
     if( poOpenInfo->pabyHeader &&
         STARTS_WITH((const char*)poOpenInfo->pabyHeader, "-- SQL GPKG") )

gdal/ogr/ogrsf_frmts/ili/ogrili1driver.cpp
@@ -43,7 +43,7 @@ static GDALDataset *OGRILI1DriverOpen( GDALOpenInfo* poOpenInfo )
         (!poOpenInfo->bStatOK && strchr(poOpenInfo->pszFilename, ',') == nullptr) )
         return nullptr;
 
-    if( poOpenInfo->fpL != nullptr )
+    if( poOpenInfo->pabyHeader != nullptr )
     {
         if( strstr((const char*)poOpenInfo->pabyHeader,"SCNT") == nullptr )
         {

gdal/ogr/ogrsf_frmts/ili/ogrili2driver.cpp
@@ -45,7 +45,7 @@ static GDALDataset *OGRILI2DriverOpen( GDALOpenInfo* poOpenInfo )
         (!poOpenInfo->bStatOK && strchr(poOpenInfo->pszFilename, ',') == nullptr) )
         return nullptr;
 
-    if( poOpenInfo->fpL != nullptr )
+    if( poOpenInfo->pabyHeader != nullptr )
     {
         if( poOpenInfo->pabyHeader[0] != '<'
             || strstr((const char*)poOpenInfo->pabyHeader,"interlis.ch/INTERLIS2") == nullptr )

gdal/ogr/ogrsf_frmts/ntf/ogrntfdriver.cpp
@@ -46,7 +46,8 @@ static GDALDataset *OGRNTFDriverOpen( GDALOpenInfo* poOpenInfo )
 {
     if( !poOpenInfo->bStatOK )
         return nullptr;
-    if( poOpenInfo->fpL != nullptr )
+
+    if( poOpenInfo->nHeaderBytes != 0 )
     {
         if( poOpenInfo->nHeaderBytes < 80 )
             return nullptr;

commit d2094fb5fd2b6d9b883e72a22ec4b9b5d6e47ca0

GPKG/SQlite: fix opening SQL dump files (broken by recent a0845a051c0cd1cc7dfbeebb776dc325af262cac)

gdal/ogr/ogrsf_frmts/gpkg/ogrgeopackagedatasource.cpp
@@ -821,7 +821,8 @@ int GDALGeoPackageDataset::Open( GDALOpenInfo* poOpenInfo )
         }
         pabyHeader = abyHeaderLetMeHerePlease;
     }
-    else
+    else if( poOpenInfo->pabyHeader &&
+            STARTS_WITH((const char*)poOpenInfo->pabyHeader, "SQLite format 3") )
     {
         m_bCallUndeclareFileNotToOpen = true;
         GDALOpenInfoDeclareFileNotToOpen(osFilename,

gdal/ogr/ogrsf_frmts/sqlite/ogrsqlitedatasource.cpp
@@ -1343,10 +1343,14 @@ int OGRSQLiteDataSource::Open( GDALOpenInfo* poOpenInfo)
         else
         {
             m_pszFilename = CPLStrdup( pszNewName );
-            m_bCallUndeclareFileNotToOpen = true;
-            GDALOpenInfoDeclareFileNotToOpen(m_pszFilename,
-                                        poOpenInfo->pabyHeader,
-                                        poOpenInfo->nHeaderBytes);
+            if( poOpenInfo->pabyHeader &&
+                STARTS_WITH((const char*)poOpenInfo->pabyHeader, "SQLite format 3") )
+            {
+                m_bCallUndeclareFileNotToOpen = true;
+                GDALOpenInfoDeclareFileNotToOpen(m_pszFilename,
+                                            poOpenInfo->pabyHeader,
+                                            poOpenInfo->nHeaderBytes);
+            }
         }
     }
     SetPhysicalFilename(m_pszFilename);