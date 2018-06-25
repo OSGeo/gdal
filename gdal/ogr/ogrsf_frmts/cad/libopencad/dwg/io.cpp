dff25544667018f6490b55a1dcaf3cff3004d790
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-06-25 16:57:49 +0300

    CAD: Fix read objects map. Upgrade version of libopencad to 0.3.4. (fixes #677)

--- gdal/ogr/ogrsf_frmts/cad/libopencad/dwg/io.cpp
+++ gdal/ogr/ogrsf_frmts/cad/libopencad/dwg/io.cpp
@@ -551,7 +551,6 @@ std::string CADBuffer::ReadTV()
 long CADBuffer::ReadUMCHAR()
 {
     long result = 0;
-    /*bool   negative = false;*/
     size_t nByteOffset = m_nBitOffsetFromStart / 8;
     // TODO: bit offset is calculated, but function has nothing to do with it.
     /*size_t nBitOffsetInByte = m_nBitOffsetFromStart % 8;*/
@@ -565,67 +564,24 @@ long CADBuffer::ReadUMCHAR()
     unsigned char aMCharBytes[8]; // 8 bytes is maximum.
     //memcpy( aMCharBytes, pMCharFirstByte, 8 );
 
-    size_t MCharBytesCount = 0;
-    for( size_t i = 0; i < 8; ++i )
+    unsigned char nMCharBytesCount = 0;
+    for( unsigned char i = 0; i < 8; ++i )
     {
         aMCharBytes[i] = ReadCHAR();
-        ++MCharBytesCount;
+        ++nMCharBytesCount;
         if ( !( aMCharBytes[i] & binary( 10000000 ) ) )
         {
             break;
         }
-    }
-
-    SwapEndianness( aMCharBytes, MCharBytesCount ); // LSB to MSB
-
-    for( size_t i = 0; i < MCharBytesCount; ++i )
-    {
         aMCharBytes[i] &= binary( 01111111 );
     }
 
-    // TODO: this code doesn't cover case when char.bytescount > 3, but its
-    //       possible on large files.
-    // I just can't write an algorithm that does this.
-    switch( MCharBytesCount )
+    int nOffset = 0;
+    for(unsigned char i = 0; i < nMCharBytesCount; ++i)
     {
-        case 1:
-            break;
-        case 2:
-        {
-            char tmp = aMCharBytes[0] & binary( 00000001 );
-            aMCharBytes[0] = aMCharBytes[0] >> 1;
-            aMCharBytes[1] |= ( tmp << 7 );
-            break;
-        }
-        case 3:
-        {
-            unsigned char tmp1 = aMCharBytes[0] & binary( 00000011 );
-            unsigned char tmp2 = aMCharBytes[1] & binary( 00000001 );
-            aMCharBytes[0] = aMCharBytes[0] >> 2;
-            aMCharBytes[1] = aMCharBytes[1] >> 1;
-            aMCharBytes[1] |= ( tmp1 << 6 );
-            aMCharBytes[2] |= ( tmp2 << 7 );
-            break;
-        }
-        case 4:
-        {
-            unsigned char tmp1 = aMCharBytes[0] & binary( 00000111 );
-            unsigned char tmp2 = aMCharBytes[1] & binary( 00000011 );
-            unsigned char tmp3 = aMCharBytes[2] & binary( 00000001 );
-            aMCharBytes[0] = aMCharBytes[0] >> 3;
-            aMCharBytes[1] = aMCharBytes[1] >> 2;
-            aMCharBytes[2] = aMCharBytes[2] >> 1;
-            aMCharBytes[1] |= ( tmp1 << 5 );
-            aMCharBytes[2] |= ( tmp2 << 6 );
-            aMCharBytes[3] |= ( tmp3 << 7 );
-            break;
-        }
+        result += aMCharBytes[i] << nOffset;
+        nOffset += 7;
     }
-
-    SwapEndianness( aMCharBytes, MCharBytesCount ); // MSB to LSB
-
-    memcpy( &result, aMCharBytes, MCharBytesCount );
-
     return result;
 }
 
@@ -645,78 +601,37 @@ long CADBuffer::ReadMCHAR()
         return 0;
     }
     unsigned char aMCharBytes[8]; // 8 bytes is maximum.
-    //memcpy( aMCharBytes, pMCharFirstByte, 8 );
 
-    size_t MCharBytesCount = 0;
-    for( size_t i = 0; i < 8; ++i )
+    unsigned char nMCharBytesCount = 0;
+    for( unsigned char i = 0; i < 8; ++i )
     {
         aMCharBytes[i] = ReadCHAR();
-        ++MCharBytesCount;
+        ++nMCharBytesCount;
         if ( !( aMCharBytes[i] & binary( 10000000 ) ) )
         {
             break;
         }
-    }
-
-    SwapEndianness( aMCharBytes, MCharBytesCount ); // LSB to MSB
-
-    if ( ( aMCharBytes[0] & binary( 01000000 ) ) == binary( 01000000 ) )
-    {
-        aMCharBytes[0] &= binary( 10111111 );
-        negative = true;
-    }
-
-    for( size_t i = 0; i < MCharBytesCount; ++i )
-    {
         aMCharBytes[i] &= binary( 01111111 );
     }
 
-    // TODO: this code doesn't cover case when char.bytescount > 3, but its
-    //       possible on large files.
-    // I just can't write an algorithm that does this.
-    switch( MCharBytesCount )
+    if ( ( aMCharBytes[nMCharBytesCount - 1] & binary( 01000000 ) ) == binary( 01000000 ) )
     {
-        case 1:
-            break;
-        case 2:
-        {
-            char tmp = aMCharBytes[0] & binary( 00000001 );
-            aMCharBytes[0] = aMCharBytes[0] >> 1;
-            aMCharBytes[1] |= ( tmp << 7 );
-            break;
-        }
-        case 3:
-        {
-            unsigned char tmp1 = aMCharBytes[0] & binary( 00000011 );
-            unsigned char tmp2 = aMCharBytes[1] & binary( 00000001 );
-            aMCharBytes[0] = aMCharBytes[0] >> 2;
-            aMCharBytes[1] = aMCharBytes[1] >> 1;
-            aMCharBytes[1] |= ( tmp1 << 6 );
-            aMCharBytes[2] |= ( tmp2 << 7 );
-            break;
-        }
-        case 4:
-        {
-            unsigned char tmp1 = aMCharBytes[0] & binary( 00000111 );
-            unsigned char tmp2 = aMCharBytes[1] & binary( 00000011 );
-            unsigned char tmp3 = aMCharBytes[2] & binary( 00000001 );
-            aMCharBytes[0] = aMCharBytes[0] >> 3;
-            aMCharBytes[1] = aMCharBytes[1] >> 2;
-            aMCharBytes[2] = aMCharBytes[2] >> 1;
-            aMCharBytes[1] |= ( tmp1 << 5 );
-            aMCharBytes[2] |= ( tmp2 << 6 );
-            aMCharBytes[3] |= ( tmp3 << 7 );
-            break;
-        }
+        aMCharBytes[nMCharBytesCount - 1] &= binary( 10111111 );
+        negative = true;
     }
 
-    SwapEndianness( aMCharBytes, MCharBytesCount ); // MSB to LSB
-
-    memcpy( &result, aMCharBytes, MCharBytesCount );
+    int nOffset = 0;
+    for(unsigned char i = 0; i < nMCharBytesCount; ++i)
+    {
+        result += aMCharBytes[i] << nOffset;
+        nOffset += 7;
+    }
 
     if( negative )
+    {
         result *= -1;
-
+    }
+    
     return result;
 }
 
