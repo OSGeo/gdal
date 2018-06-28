6a2c9d41fc75d8bdc401f1147ca79de1f3b5a1ae
Even Rouault <even.rouault@spatialys.com>
2018-06-28 11:04:29 +0200

    Add GDAL EEDAI and OGR EEDA drivers for Google Earth Engine Data API
    New configuration file: data/eedaconf.json

--- gdal/gcore/gdal_frmts.h
+++ gdal/gcore/gdal_frmts.h
@@ -192,6 +192,8 @@ void CPL_DLL GDALRegister_JP2Lura(void);
 void CPL_DLL GDALRegister_PRF(void);
 void CPL_DLL GDALRegister_NULL(void);
 void CPL_DLL GDALRegister_RDA(void);
+void CPL_DLL GDALRegister_EEDAI(void);
+void CPL_DLL GDALRegister_EEDA(void);
 CPL_C_END
 
 #endif /* ndef GDAL_FRMTS_H_INCLUDED */
