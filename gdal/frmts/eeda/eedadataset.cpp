804c6352e843af8581830eed0debce78d5aac99e
Even Rouault <even.rouault@spatialys.com>
2018-07-10 12:31:43 +0200

    Avoid false positive null pointer dereferencing warning. CID 1393942

--- gdal/frmts/eeda/eedadataset.cpp
+++ gdal/frmts/eeda/eedadataset.cpp
@@ -427,15 +427,15 @@ OGRFeature* GDALEEDALayer::GetNextRawFeature()
 
         m_poCurPageAssets = CPL_json_object_object_get(
                                                 m_poCurPageObj, "assets");
-        if( m_poCurPageAssets == nullptr ||
-            json_object_get_type(m_poCurPageAssets) != json_type_array )
-        {
-            json_object_put(m_poCurPageObj);
-            m_poCurPageObj = nullptr;
-            return nullptr;
-        }
     }
 
+    if( m_poCurPageAssets == nullptr ||
+        json_object_get_type(m_poCurPageAssets) != json_type_array )
+    {
+        json_object_put(m_poCurPageObj);
+        m_poCurPageObj = nullptr;
+        return nullptr;
+    }
     json_object* poAsset = json_object_array_get_idx(m_poCurPageAssets,
                                                      m_nIndexInPage);
     if( poAsset == nullptr || json_object_get_type(poAsset) != json_type_object )
