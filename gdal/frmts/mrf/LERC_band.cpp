f847936774b114130d747b8bbdea7ac720dc402a
Even Rouault <even.rouault@spatialys.com>
2018-07-11 19:17:33 +0200

    MRF: avoid int overflow / buffer oveflow on unsupported LERC2 formulations. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=9348. Credit to OSS Fuzz

--- gdal/frmts/mrf/LERC_band.cpp
+++ gdal/frmts/mrf/LERC_band.cpp
@@ -380,6 +380,7 @@ CPLErr LERC_Band::Decompress(buf_mgr &dst, buf_mgr &src)
     if (img.pagesize.x != hdInfo.nCols
         || img.pagesize.y != hdInfo.nRows
         || img.dt != GetL2DataType(hdInfo.dt)
+        || hdInfo.nDim != 1
         || dst.size < static_cast<size_t>(hdInfo.nCols * hdInfo.nRows * GDALGetDataTypeSizeBytes(img.dt))) {
         CPLError(CE_Failure, CPLE_AppDefined, "MRF: Lerc2 format");
         return CE_Failure;
