37843c52332d8a3d5e00e49091ee9dee4e46a461
Hiroshi Miura <miurahr@linux.com>
2018-07-22 10:03:03 +0900

    Vagrant: libkml: drop numthread culculation
    Now Ninja build used. It is not neccesary to calculate
    make -j <num> number because Ninja do it automatically.
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- gdal/scripts/vagrant/libkml.sh
+++ gdal/scripts/vagrant/libkml.sh
@@ -4,14 +4,6 @@
 set -o errexit
 set -o xtrace
 
-NUMTHREADS=2
-if [[ -f /sys/devices/system/cpu/online ]]; then
-	# Calculates 1.5 times physical threads
-	NUMTHREADS=$(( ( $(cut -f 2 -d '-' /sys/devices/system/cpu/online) + 1 ) * 15 / 10  ))
-fi
-#NUMTHREADS=1 # disable MP
-export NUMTHREADS
-
 git clone https://github.com/libkml/libkml.git libkml
 mkdir libkml/build
 cd libkml/build
