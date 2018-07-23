b5a64630b9428156c020b5b43aae276b005e906c
Hiroshi Miura <miurahr@linux.com>
2018-07-23 21:01:36 +0900

    Vagrant: ln libgdal.so.2 to libgdal.so.20
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- gdal/scripts/vagrant/postgis.sh
+++ gdal/scripts/vagrant/postgis.sh
@@ -7,7 +7,7 @@ set -o xtrace
 sudo sed -i  's/md5/trust/' /etc/postgresql/9.3/main/pg_hba.conf
 sudo sed -i  's/peer/trust/' /etc/postgresql/9.3/main/pg_hba.conf
 
-sudo ln -s /usr/lib/libgdal.so.2 /usr/lib/libgdal.so.1
+sudo ln -s /usr/lib/libgdal.so.2 /usr/lib/libgdal.so.20
 sudo service postgresql restart
 psql -c "create database autotest" -U postgres
 psql -c "create extension postgis" -d autotest -U postgres
