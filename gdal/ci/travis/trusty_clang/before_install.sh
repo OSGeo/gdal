9e57df894a97fe6548e7978869a511f2455cb493
Even Rouault <even.rouault@spatialys.com>
2018-08-11 12:49:36 +0200

    trusty_clang: re-enable postgresql

--- gdal/ci/travis/trusty_clang/before_install.sh
+++ gdal/ci/travis/trusty_clang/before_install.sh
@@ -4,15 +4,13 @@ set -e
 
 sudo dpkg -l | grep geos
 sudo apt-get purge -y libgeos* libspatialite*
+sudo apt-get remove libpq* postgresql*
 find  /etc/apt/sources.list.d
 sudo mv /etc/apt/sources.list.d/pgdg* /tmp
-#sudo apt-get remove postgis libpq5 libpq-dev postgresql-9.1-postgis postgresql-9.2-postgis postgresql-9.3-postgis postgresql-9.1 postgresql-9.2 postgresql-9.3 libgdal1
-#sudo apt-get remove -y postgresql-9.1
 sudo add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
 #sudo add-apt-repository -y ppa:marlam/gta
 sudo apt-get update
-# postgis postgresql-9.5 postgresql-client-9.5 postgresql-9.5-postgis-2.2 libpq-dev
-sudo apt-get install -y --allow-unauthenticated ccache python-numpy libpng12-dev libjpeg-dev libgif-dev liblzma-dev libgeos-dev libcurl4-gnutls-dev libproj-dev libxml2-dev libexpat-dev libxerces-c-dev libnetcdf-dev netcdf-bin libpoppler-dev libspatialite-dev gpsbabel swig libhdf4-alt-dev libhdf5-serial-dev libpodofo-dev poppler-utils libfreexl-dev unixodbc-dev libwebp-dev  libepsilon-dev  liblcms2-2 libpcre3-dev mercurial cmake libcrypto++-dev
+sudo apt-get install -y --allow-unauthenticated ccache python-numpy libpng12-dev libjpeg-dev libgif-dev liblzma-dev libgeos-dev libcurl4-gnutls-dev libproj-dev libxml2-dev libexpat-dev libxerces-c-dev libnetcdf-dev netcdf-bin libpoppler-dev libspatialite-dev gpsbabel swig libhdf4-alt-dev libhdf5-serial-dev libpodofo-dev poppler-utils libfreexl-dev unixodbc-dev libwebp-dev  libepsilon-dev  liblcms2-2 libpcre3-dev mercurial cmake libcrypto++-dev postgresql-9.3-postgis-2.2 postgresql-9.3-postgis-scripts libpq-dev
 # libgta-dev
 sudo apt-get install -y python-lxml
 sudo apt-get install -y python-pip
@@ -27,9 +25,9 @@ sudo apt-get install doxygen texlive-latex-base
 pyflakes autotest
 pyflakes gdal/swig/python/scripts
 pyflakes gdal/swig/python/samples
-#psql -c "drop database if exists autotest" -U postgres
-#psql -c "create database autotest" -U postgres
-#psql -c "create extension postgis" -d autotest -U postgres
+psql -c "drop database if exists autotest" -U postgres
+psql -c "create database autotest" -U postgres
+psql -c "create extension postgis" -d autotest -U postgres
 #mysql -e "create database autotest;"
 #mysql -e "GRANT ALL ON autotest.* TO 'root'@'localhost';" -u root
 #mysql -e "GRANT ALL ON autotest.* TO 'travis'@'localhost';" -u root
