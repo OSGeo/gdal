ca96ce2460d05900411c3d00e212d00dae8d62db
Even Rouault <even.rouault@spatialys.com>
2018-09-05 10:11:15 +0200

    OGR_GreatCircle_InitialHeading(): avoid potential division by zero. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=10224. Credit to OSS Fuzz

--- gdal/ogr/ogr_geo_utils.cpp
+++ gdal/ogr/ogr_geo_utils.cpp
@@ -107,6 +107,11 @@ double OGR_GreatCircle_InitialHeading( double LatA_deg, double LonA_deg,
         const double sin_diffG = sin(diffG);
 
         const double denom = sin_LatA * cos_diffG - cos_LatA * tan(LatB_rad);
+        if( denom == 0.0 )
+        {
+            // Can be the the case if Lat_A = -Lat_B and abs(LonA - LonB) = 180
+            return 0.0;
+        }
 
         double track = atan (sin_diffG / denom) * RAD2DEG;
 
