10d2cf3c7944c209da6b9bfa391b0ea0b6222ed8
Even Rouault <even.rouault@spatialys.com>
2018-09-11 08:31:42 +0200

    Utilities: make sure that --config is early evaluated for config options such as CPL_VSIL_CURL_CHUNK_SIZE that are read early

--- gdal/apps/commonutils.cpp
+++ gdal/apps/commonutils.cpp
@@ -164,11 +164,7 @@ void EarlySetConfigOptions( int argc, char ** argv )
     // registered for the --format or --formats options.
     for( int i = 1; i < argc; i++ )
     {
-        if( EQUAL(argv[i],"--config") && i + 2 < argc &&
-            (EQUAL(argv[i + 1], "GDAL_SKIP") ||
-             EQUAL(argv[i + 1], "GDAL_DRIVER_PATH") ||
-             EQUAL(argv[i + 1], "OGR_SKIP") ||
-             EQUAL(argv[i + 1], "OGR_DRIVER_PATH")) )
+        if( EQUAL(argv[i],"--config") && i + 2 < argc )
         {
             CPLSetConfigOption( argv[i+1], argv[i+2] );
 
