f5e74a59663e7c1a15237a742858bf6d79b603d9
liuyimin <solym@sohu.com>
2018-09-28 17:35:10 +0800

    Add template class CPLAutoClose

---
+++ gdal/port/cpl_auto_close.h
@@ -0,0 +1,69 @@
+/**********************************************************************
+ * $Id$
+ *
+ * Name:     cpl_auto_close.h
+ * Project:  CPL - Common Portability Library
+ * Purpose:  CPL Auto Close handling
+ * Author:   liuyimin, ymwh@foxmail.com
+ *
+ **********************************************************************
+ * Copyright (c) 1998, Daniel Morissette
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
+
+#ifndef CPL_AUTO_CLOSE_H_INCLUDED
+#define CPL_AUTO_CLOSE_H_INCLUDED
+
+#if defined(__cplusplus) && __cplusplus >= 201103L
+#include <type_traits>
+
+/************************************************************************/
+/*                           CPLAutoClose                               */
+/************************************************************************/
+
+template<typename _Ty,typename _Dx>
+class CPLAutoClose {
+	static_assert( !std::is_const<_Ty>::value && std::is_pointer<_Ty>::value,
+					"_Ty must is pointer type,_Dx must is function type");
+	private:
+	_Ty& m_ResourcePtr;
+	_Dx  m_CloseFunc;
+	private:
+	CPLAutoClose(const CPLAutoClose&) = delete;
+	void operator=(const CPLAutoClose&) = delete;
+	public:
+		explicit CPLAutoClose(_Ty& ptr,_Dx dt) :
+			m_ResourcePtr(ptr),
+			m_CloseFunc(dt)
+		{}
+		~CPLAutoClose()
+		{
+			if(m_ResourcePtr && m_CloseFunc)
+			  m_CloseFunc(m_ResourcePtr);
+			m_ResourcePtr = nullptr;
+		}
+};
+
+#define CPL_AUTO_CLOSE_WARP(ptr,closefn) \
+	CPLAutoClose<decltype(ptr),decltype(closefn)*> tAutoClose##ptr##closefn(ptr,closefn)
+
+#endif /* __cplusplus */
+
+#endif /* CPL_AUTO_CLOSE_H_INCLUDED */
