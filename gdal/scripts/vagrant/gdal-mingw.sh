a504b33fb4a3408f19f6a8dda02ca26767b4d4a5
Even Rouault <even.rouault@spatialys.com>
2018-10-08 17:59:38 +0200

    shellcheck fixes

--- gdal/scripts/vagrant/gdal-mingw.sh
+++ gdal/scripts/vagrant/gdal-mingw.sh
@@ -20,24 +20,24 @@ chmod +x /home/vagrant/gnumake-build-mingw-w64/resync.sh
 export CCACHE_CPP2=yes
 
 wine64 cmd /c dir
-ln -sf /usr/lib/gcc/x86_64-w64-mingw32/4.8/libstdc++-6.dll  $HOME/.wine/drive_c/windows
-ln -sf /usr/lib/gcc/x86_64-w64-mingw32/4.8/libgcc_s_sjlj-1.dll  $HOME/.wine/drive_c/windows
-ln -sf /usr/x86_64-w64-mingw32/lib/libwinpthread-1.dll  $HOME/.wine/drive_c/windows
-ln -sf /usr/x86_64-w64-mingw32/lib/libproj-9.dll $HOME/.wine/drive_c/windows
-ln -sf /usr/x86_64-w64-mingw32/lib/libgeos_c-1.dll $HOME/.wine/drive_c/windows
-ln -sf /usr/x86_64-w64-mingw32/lib/libgeos-3-5-0.dll $HOME/.wine/drive_c/windows
+ln -sf /usr/lib/gcc/x86_64-w64-mingw32/4.8/libstdc++-6.dll  "$HOME/.wine/drive_c/windows"
+ln -sf /usr/lib/gcc/x86_64-w64-mingw32/4.8/libgcc_s_sjlj-1.dll  "$HOME/.wine/drive_c/windows"
+ln -sf /usr/x86_64-w64-mingw32/lib/libwinpthread-1.dll  "$HOME/.wine/drive_c/windows"
+ln -sf /usr/x86_64-w64-mingw32/lib/libproj-9.dll "$HOME/.wine/drive_c/windows"
+ln -sf /usr/x86_64-w64-mingw32/lib/libgeos_c-1.dll "$HOME/.wine/drive_c/windows"
+ln -sf /usr/x86_64-w64-mingw32/lib/libgeos-3-5-0.dll "$HOME/.wine/drive_c/windows"
 
 cd /home/vagrant/gnumake-build-mingw-w64
 CC="ccache x86_64-w64-mingw32-gcc" CXX="ccache x86_64-w64-mingw32-g++" LD=x86_64-w64-mingw32-ld \
 ./configure --prefix=/usr/x86_64-w64-mingw32  --host=x86_64-w64-mingw32  --with-geos --with-proj-static
-ln -sf $PWD/.libs/libgdal-20.dll $HOME/.wine/drive_c/windows
+ln -sf "$PWD/.libs/libgdal-20.dll" "$HOME/.wine/drive_c/windows"
 
 # Python bindings
 wget -N -nv -P /var/cache/wget/ http://www.python.org/ftp/python/2.7.3/python-2.7.3.amd64.msi
 wine64 msiexec /i /var/cache/wget/python-2.7.3.amd64.msi
 cd swig/python
-gendef $HOME/.wine/drive_c/Python27/python27.dll
-x86_64-w64-mingw32-dlltool --dllname $HOME/.wine/drive_c/Python27/python27.dll --input-def python27.def --output-lib $HOME/.wine/drive_c/Python27/libs/libpython27.a
+gendef "$HOME/.wine/drive_c/Python27/python27.dll"
+x86_64-w64-mingw32-dlltool --dllname "$HOME/.wine/drive_c/Python27/python27.dll" --input-def python27.def --output-lib "$HOME/.wine/drive_c/Python27/libs/libpython27.a"
 cd ../..
 
 #
