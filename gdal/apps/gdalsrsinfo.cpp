c2f67cb9fae85ddf8e6ac40104f61f94a19cc183
Even Rouault <even.rouault@spatialys.com>
2019-02-07 10:13:10 +0100

    gdalsrsinfo: do not silence errors when calling SetFromUserInput()

--- gdal/apps/gdalsrsinfo.cpp
+++ gdal/apps/gdalsrsinfo.cpp
@@ -284,14 +284,13 @@ bool FindSRS( const char *pszInput, OGRSpatialReference &oSRS )
     bool bGotSRS = false;
     GDALDataset *poGDALDS = nullptr;
     OGRLayer      *poLayer = nullptr;
-    CPLErrorHandler oErrorHandler = nullptr;
     bool bIsFile = false;
     OGRErr eErr = OGRERR_NONE;
 
     /* temporarily suppress error messages we may get from xOpen() */
     bool bDebug = CPLTestBool(CPLGetConfigOption("CPL_DEBUG", "OFF"));
     if( !bDebug )
-        oErrorHandler = CPLSetErrorHandler ( CPLQuietErrorHandler );
+        CPLPushErrorHandler ( CPLQuietErrorHandler );
 
     /* Test if argument is a file */
     VSILFILE *fp = VSIFOpenL( pszInput, "r" );
@@ -358,6 +357,10 @@ bool FindSRS( const char *pszInput, OGRSpatialReference &oSRS )
         }
     }
 
+    /* restore error messages */
+    if( !bDebug )
+        CPLPopErrorHandler();
+
     /* Last resort, try OSRSetFromUserInput() */
     if ( ! bGotSRS ) {
         CPLDebug( "gdalsrsinfo",
@@ -376,10 +379,6 @@ bool FindSRS( const char *pszInput, OGRSpatialReference &oSRS )
             bGotSRS = true;
         }
     }
-
-    /* restore error messages */
-    if( !bDebug )
-        CPLSetErrorHandler ( oErrorHandler );
 
     return bGotSRS;
 }
