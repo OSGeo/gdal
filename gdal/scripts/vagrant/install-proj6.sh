e0dd0c5089223afb1d13f9f36730d6efe321b939
Hiroshi Miura <miurahr@linux.com>
2019-02-14 06:21:10 +0900

    Vagrant: PROJ_RENAME_SYMBOLS for proj6
    and fix mingw build.
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- gdal/scripts/vagrant/install-proj6.sh
+++ gdal/scripts/vagrant/install-proj6.sh
@@ -9,4 +9,4 @@ git clone --depth 1 https://github.com/osgeo/proj.4.git proj
 echo "Build and install proj6..."
 mkdir proj/build-x86_64
 (cd proj; ./autogen.sh)
-(cd proj/build-x86_64; ../configure --prefix=/usr/local/ && make -j3 && sudo make install )
+(cd proj/build-x86_64; CFLAGS='-DPROJ_RENAME_SYMBOLS' CXXFLAGS='-DPROJ_RENAME_SYMBOLS' ../configure --prefix=/usr/local/ && make -j3 && sudo make install && sudo mv /usr/local/lib/libproj.so.13.1.1 /usr/local/lib/libinternalproj.so.13.1.1 && sudo rm /usr/local/lib/libproj.so* && sudo rm /usr/local/lib/libproj.a && sudo rm /usr/local/lib/libproj.la && sudo ln -s libinternalproj.so.13.1.1 /usr/local/lib/libinternalproj.so.13 && sudo ln -s libinternalproj.so.13.1.1 /usr/local/lib/libinternalproj.so)
