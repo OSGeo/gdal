7074a84e2e1c43213fed333984a0f3051a0595f8
Even Rouault <even.rouault@spatialys.com>
2019-03-12 17:15:34 +0100

    GPKG: fix DDL of gpkg_spatial_ref_sys when using the CRS WKT extension (refs https://github.com/opengeospatial/ets-gpkg12/issues/86)

--- gdal/ogr/ogrsf_frmts/gpkg/ogrgeopackagedatasource.cpp
+++ gdal/ogr/ogrsf_frmts/gpkg/ogrgeopackagedatasource.cpp
@@ -3579,18 +3579,25 @@ int GDALGeoPackageDataset::Create( const char * pszFilename,
     {
         /* Requirement 10: A GeoPackage SHALL include a gpkg_spatial_ref_sys table */
         /* http://opengis.github.io/geopackage/#spatial_ref_sys */
+        if( CPLTestBool(CPLGetConfigOption("GPKG_ADD_DEFINITION_12_063", "NO")) )
+        {
+            m_bHasDefinition12_063 = true;
+        }
         osSQL =
             "CREATE TABLE gpkg_spatial_ref_sys ("
             "srs_name TEXT NOT NULL,"
             "srs_id INTEGER NOT NULL PRIMARY KEY,"
             "organization TEXT NOT NULL,"
             "organization_coordsys_id INTEGER NOT NULL,"
-            "definition  TEXT NOT NULL,"
-            "description TEXT";
-        if( CPLTestBool(CPLGetConfigOption("GPKG_ADD_DEFINITION_12_063", "NO")) )
+            "definition  TEXT NOT NULL";
+        if( m_bHasDefinition12_063 )
         {
-            m_bHasDefinition12_063 = true;
-            osSQL += ", definition_12_063 TEXT NOT NULL";
+            osSQL += " DEFAULT 'undefined'";
+        }
+        osSQL += ",description TEXT";
+        if( m_bHasDefinition12_063 )
+        {
+            osSQL += ", definition_12_063 TEXT NOT NULL DEFAULT 'undefined'";
         }
         osSQL += 
             ")"
