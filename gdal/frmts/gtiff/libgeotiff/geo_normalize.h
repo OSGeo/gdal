a63e52231cc162328ab48bd8789350fed71a32c6
Bas Couwenberg <sebastic@xs4all.nl>
2019-04-19 19:04:44 +0200

    Fix spelling errors.
     * instanciate -> instantiate

--- gdal/frmts/gtiff/libgeotiff/geo_normalize.h
+++ gdal/frmts/gtiff/libgeotiff/geo_normalize.h
@@ -175,7 +175,7 @@ void GTIF_DLL GTIFFreeMemory( char * );
 
 /* The void* should be a PJ_CONTEXT* */
 void GTIF_DLL GTIFAttachPROJContext( GTIF *psGTIF, void* pjContext );
-void GTIF_DLL *GTIFGetPROJContext( GTIF *psGTIF, int instanciateIfNeeded,
+void GTIF_DLL *GTIFGetPROJContext( GTIF *psGTIF, int instantiateIfNeeded,
                                    int* out_gtif_own_pj_context );
 
 int GTIF_DLL GTIFGetDefn( GTIF *psGTIF, GTIFDefn * psDefn );
