fc4fcaf1f9634bd4cb69ab71100ac23985f13500
Even Rouault <even.rouault@spatialys.com>
2019-04-20 15:27:13 +0200

    Python bindings: rename README.txt as README.rst, fix a link and mark its content type appropriately (fixes #1462)

--- gdal/swig/python/setup.py
+++ gdal/swig/python/setup.py
@@ -364,7 +364,7 @@ if HAVE_NUMPY:
 
 packages = ["osgeo", ]
 
-readme = str(open('README.txt', 'rb').read())
+readme = str(open('README.rst', 'rb').read())
 
 name = 'GDAL'
 version = gdal_version
@@ -408,6 +408,7 @@ setup_kwargs = dict(
     maintainer=maintainer,
     maintainer_email=maintainer_email,
     long_description=readme,
+    long_description_content_type='text/x-rst',
     description=description,
     license=license_type,
     classifiers=classifiers,
