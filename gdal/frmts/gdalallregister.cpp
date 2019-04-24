e058f1116d116ef9c8fe039ab7d47493db2913f9
Even Rouault <even.rouault@spatialys.com>
2019-04-24 23:15:43 +0200

    Fix previous commit (refs #1472)

--- gdal/frmts/gdalallregister.cpp
+++ gdal/frmts/gdalallregister.cpp
@@ -511,6 +511,12 @@ void CPL_STDCALL GDALAllRegister()
     GDALRegister_SAGA();
 #endif
 
+#ifdef FRMT_ignfheightasciigrid
+    // IGNFHeightASCIIGrid must come before XYZ, otherwise XYZ might
+    // try and fail opening such files
+    GDALRegister_IGNFHeightASCIIGrid();
+#endif
+
 #ifdef FRMT_xyz
     GDALRegister_XYZ();
 #endif
@@ -570,10 +576,6 @@ void CPL_STDCALL GDALAllRegister()
 
 #ifdef FRMT_sigdem
     GDALRegister_SIGDEM();
-#endif
-
-#ifdef FRMT_ignfheightasciigrid
-    GDALRegister_IGNFHeightASCIIGrid();
 #endif
 
     // NOTE: you need to generally your own driver before that line.
