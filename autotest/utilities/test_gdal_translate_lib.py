22d34f5877d52966f69e2534efc2754f5e5aff3a
Even Rouault <even.rouault@spatialys.com>
2019-05-24 23:45:07 +0200

    Make 'gdal_translate foo.tif foo.tif.ovr -outsize 50% 50% -of GTiff' work

--- autotest/utilities/test_gdal_translate_lib.py
+++ autotest/utilities/test_gdal_translate_lib.py
@@ -455,7 +455,25 @@ def test_gdal_translate_lib_110():
 
     ds = None
 
-    
+###############################################################################
+# Test gdal_translate foo.tif foo.tif.ovr
+
+
+def test_gdal_translate_lib_generate_ovr():
+
+    gdal.FileFromMemBuffer('/vsimem/foo.tif',
+                           open('../gcore/data/byte.tif', 'rb').read())
+    gdal.GetDriverByName('GTiff').Create('/vsimem/foo.tif.ovr', 10, 10)
+    ds = gdal.Translate('/vsimem/foo.tif.ovr',
+                        '/vsimem/foo.tif',
+                        resampleAlg = gdal.GRA_Average,
+                        format = 'GTiff', width = 10, height = 10)
+    assert ds
+    assert ds.GetRasterBand(1).Checksum() == 1152, 'Bad checksum'
+    ds = None
+
+    gdal.GetDriverByName('GTiff').Delete('/vsimem/foo.tif')
+
 ###############################################################################
 # Cleanup
 
