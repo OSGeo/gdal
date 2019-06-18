32371deeb5cf8dee2e7e4abfb8303299ebf85968
Even Rouault <even.rouault@spatialys.com>
2019-06-18 17:02:14 +0200

    GTiffDataset::PushMetadataToPam(): early exit when PAM is disabled, to avoid error messages

--- gdal/frmts/gtiff/geotiff.cpp
+++ gdal/frmts/gtiff/geotiff.cpp
@@ -11715,6 +11715,9 @@ bool GTiffDataset::WriteMetadata( GDALDataset *poSrcDS, TIFF *l_hTIFF,
 void GTiffDataset::PushMetadataToPam()
 
 {
+    if( GetPamFlags() & GPF_DISABLED )
+        return;
+
     const bool bStardardColorInterp =
         IsStandardColorInterpretation(this, nPhotometric, papszCreationOptions);
 
