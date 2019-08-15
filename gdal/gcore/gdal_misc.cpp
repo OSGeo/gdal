OSGeo/gdal

commit 04892bffec5bd469547d3562715882c080547392

GDALGeneralCmdLineProcessor(): fix double free with --optfile (CID 1404146)

gdal/gcore/gdal_misc.cpp
@@ -3047,11 +3047,10 @@ GDALGeneralCmdLineProcessor( int nArgc, char ***ppapszArgv, int nOptions )
 
             VSIFCloseL( fpOptFile );
 
-            char** papszArgvOptfileMod = papszArgvOptfile;
             if( !bHasOptfile )
             {
                 if( GDALGeneralCmdLineProcessor(CSLCount(papszArgvOptfile),
-                                        &papszArgvOptfileMod, nOptions) < 0 )
+                                        &papszArgvOptfile, nOptions) < 0 )
                 {
                     CSLDestroy( papszReturn );
                     CSLDestroy(papszArgvOptfile);
@@ -3059,14 +3058,13 @@ GDALGeneralCmdLineProcessor( int nArgc, char ***ppapszArgv, int nOptions )
                 }
             }
 
-            char** papszIter = papszArgvOptfileMod + 1;
+            char** papszIter = papszArgvOptfile + 1;
             while( *papszIter )
             {
                 papszReturn = CSLAddString(papszReturn, *papszIter);
                 ++ papszIter;
             }
             CSLDestroy(papszArgvOptfile);
-            CSLDestroy(papszArgvOptfileMod);
 
             iArg += 1;
         }

commit 51d44f0ee9d012b1e58a7111565cf2ecf3bda886

Fix memory leak in GDALGeneralCmdLineProcessor() introduced in 04892bffec5bd469547d3562715882c080547392

gdal/gcore/gdal_misc.cpp
@@ -3049,6 +3049,7 @@ GDALGeneralCmdLineProcessor( int nArgc, char ***ppapszArgv, int nOptions )
 
             if( !bHasOptfile )
             {
+                char** papszArgvOptfileBefore = papszArgvOptfile;
                 if( GDALGeneralCmdLineProcessor(CSLCount(papszArgvOptfile),
                                         &papszArgvOptfile, nOptions) < 0 )
                 {
@@ -3056,6 +3057,7 @@ GDALGeneralCmdLineProcessor( int nArgc, char ***ppapszArgv, int nOptions )
                     CSLDestroy(papszArgvOptfile);
                     return -1;
                 }
+                CSLDestroy(papszArgvOptfileBefore);
             }
 
             char** papszIter = papszArgvOptfile + 1;