30a944fc00482ec3fd8634be8202f7276fe2b493
Even Rouault <even.rouault@spatialys.com>
2019-08-15 21:28:08 +0200

    MITAB: fix potential double-free. CID 1404224

--- gdal/ogr/ogrsf_frmts/mitab/mitab_miffile.cpp
+++ gdal/ogr/ogrsf_frmts/mitab/mitab_miffile.cpp
@@ -1887,6 +1887,7 @@ int MIFFile::SetSpatialRef( OGRSpatialReference * poSpatialRef )
 
 {
     CPLFree( m_pszCoordSys );
+    m_pszCoordSys = nullptr;
 
     char* pszCoordSys = MITABSpatialRef2CoordSys( poSpatialRef );
     if( pszCoordSys )
