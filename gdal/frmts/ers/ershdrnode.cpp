681c3e492f9e8b4d60495c5038b583c8528c7d90
Even Rouault <even.rouault@spatialys.com>
2019-08-23 12:48:40 +0200

    ERS: avoid unsigned integer overflow. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=16640

--- gdal/frmts/ers/ershdrnode.cpp
+++ gdal/frmts/ers/ershdrnode.cpp
@@ -294,8 +294,11 @@ const char *ERSHdrNode::Find( const char *pszPath, const char *pszDefault )
                     {
                         // strip off quotes.
                         osTempReturn = papszItemValue[i];
-                        osTempReturn =
-                            osTempReturn.substr( 1, osTempReturn.length()-2 );
+                        if( osTempReturn.length() < 2 )
+                            osTempReturn.clear();
+                        else
+                            osTempReturn =
+                                osTempReturn.substr( 1, osTempReturn.length()-2 );
                         return osTempReturn;
                     }
                     else
