31ce6310da352977b1c15abeefa7eb64b7f057b3
Martin Franzke <Martin.Franzke@telekom.de>
2019-08-29 17:47:46 +0200

    enhance precision of built in linear system solver

--- autotest/gcore/transformer.py
+++ autotest/gcore/transformer.py
@@ -78,7 +78,6 @@ def test_transformer_2():
 ###############################################################################
 # Test GCP based transformer with thin plate splines.
 
-
 def test_transformer_3():
 
     ds = gdal.Open('data/gcps.vrt')
@@ -736,3 +735,22 @@ def test_transformer_no_reverse_method():
     with gdaltest.error_handler():
         (success, pnt) = tr.TransformPoint(1, 2, 49)
     assert not success
+
+###############################################################################
+# Test precision of GCP based transformer with thin plate splines and lots of GCPs (2115).
+
+def test_transformer_3():
+
+    ds = gdal.Open('data/gcps_2115.vrt')
+    tr = gdal.Transformer(ds, None, ['METHOD=GCP_TPS'])
+    assert tr, 'tps transformation could not be computed'
+    
+    success = True
+    for gcp in ds.GetGCPs():
+        (s, result) = tr.TransformPoint(0, gcp.GCPPixel, gcp.GCPLine)
+        success &= s
+        diffResult = math.sqrt((gcp.GCPX - result[0])**2 + (gcp.GCPY -result[1])**2)
+        maxDiffResult = max(maxDiffResult, diffResult)
+    assert success, 'at least one point could not be transformed'
+    assert maxDiffResult < 1e-3, 'at least one transformation exceeds the error bound'
+
