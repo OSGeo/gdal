7ae16685e0dfae97b89f67385a2c4fbcdd334ac7
Even Rouault <even.rouault@spatialys.com>
2019-10-02 19:04:54 +0200

    GTIFGetOGISDefn: avoid querying UOM length info when it is KvUserDefined to avoid an error to be emitted

--- gdal/frmts/gtiff/gt_wkt_srs.cpp
+++ gdal/frmts/gtiff/gt_wkt_srs.cpp
@@ -384,14 +384,17 @@ char *GTIFGetOGISDefn( GTIF *hGTIF, GTIFDefn * psDefn )
                 oSRS.SetLocalCS( szPCSName );
 
                 // Handle units
+                if( psDefn->UOMLength != KvUserDefined )
+                {
 #if LIBGEOTIFF_VERSION >= 1600 || defined(INTERNAL_LIBGEOTIFF)
-                GTIFGetUOMLengthInfoEx( projContext,
+                    GTIFGetUOMLengthInfoEx( projContext,
 #else
-                GTIFGetUOMLengthInfo(
+                    GTIFGetUOMLengthInfo(
 #endif
-                    psDefn->UOMLength, &pszUnitsName, nullptr );
+                        psDefn->UOMLength, &pszUnitsName, nullptr );
+                }
 
-                if( pszUnitsName != nullptr && psDefn->UOMLength != KvUserDefined )
+                if( pszUnitsName != nullptr )
                 {
                     char szUOMLength[12];
                     snprintf(szUOMLength, sizeof(szUOMLength),
@@ -428,14 +431,17 @@ char *GTIFGetOGISDefn( GTIF *hGTIF, GTIFDefn * psDefn )
 
         char *pszUnitsName = nullptr;
 
+        if( psDefn->UOMLength != KvUserDefined )
+        {
 #if LIBGEOTIFF_VERSION >= 1600 || defined(INTERNAL_LIBGEOTIFF)
-        GTIFGetUOMLengthInfoEx( projContext,
+            GTIFGetUOMLengthInfoEx( projContext,
 #else
-        GTIFGetUOMLengthInfo(
+            GTIFGetUOMLengthInfo(
 #endif
-            psDefn->UOMLength, &pszUnitsName, nullptr );
+                psDefn->UOMLength, &pszUnitsName, nullptr );
+        }
 
-        if( pszUnitsName != nullptr && psDefn->UOMLength != KvUserDefined )
+        if( pszUnitsName != nullptr )
         {
             char szUOMLength[12];
                     snprintf(szUOMLength, sizeof(szUOMLength),
@@ -751,14 +757,17 @@ char *GTIFGetOGISDefn( GTIF *hGTIF, GTIFDefn * psDefn )
     {
         char *pszUnitsName = nullptr;
 
+        if( psDefn->UOMLength != KvUserDefined )
+        {
 #if LIBGEOTIFF_VERSION >= 1600 || defined(INTERNAL_LIBGEOTIFF)
-        GTIFGetUOMLengthInfoEx( projContext,
+            GTIFGetUOMLengthInfoEx( projContext,
 #else
-        GTIFGetUOMLengthInfo(
+            GTIFGetUOMLengthInfo(
 #endif
-            psDefn->UOMLength, &pszUnitsName, nullptr );
+                psDefn->UOMLength, &pszUnitsName, nullptr );
+        }
 
-        if( pszUnitsName != nullptr && psDefn->UOMLength != KvUserDefined )
+        if( pszUnitsName != nullptr )
         {
             char szUOMLength[12];
             snprintf(szUOMLength, sizeof(szUOMLength),
