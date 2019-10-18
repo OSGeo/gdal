52590ad2ca6fddd40fde12654c9f680c2e1e8537
anshulgoelpb27 <48473744+anshulgoelpb27@users.noreply.github.com>
2019-10-18 14:59:45 +0530

    Changing back the header in gpkg file to original header.

--- gdal/ogr/ogrsf_frmts/EFAL/ogrgeopackageutility.h
+++ gdal/ogr/ogrsf_frmts/EFAL/ogrgeopackageutility.h
@@ -1,16 +1,31 @@
-/*****************************************************************************
-* Copyright 2016 Pitney Bowes Inc.
-*
-* Licensed under the MIT License (the “License”); you may not use this file
-* except in the compliance with the License.
-* You may obtain a copy of the License at https://opensource.org/licenses/MIT
-
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an “AS IS” WITHOUT
-* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*****************************************************************************/
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  GeoPackage Translator
+ * Purpose:  Utility header for OGR GeoPackage driver.
+ * Author:   Paul Ramsey, pramsey@boundlessgeo.com
+ *
+ ******************************************************************************
+ * Copyright (c) 2013, Paul Ramsey <pramsey@boundlessgeo.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ****************************************************************************/
 
 #include "ogrsf_frmts.h"
 
