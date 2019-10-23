16868463a1363a63fe89542ea7fa2a254838783f
Even Rouault <even.rouault@spatialys.com>
2019-10-23 15:22:40 +0200

    JPEG: fix further calls to RasterIO after reading full image at full resolution (fixes #1947)

--- gdal/frmts/jpeg/jpgdataset.cpp
+++ gdal/frmts/jpeg/jpgdataset.cpp
@@ -1869,7 +1869,7 @@ CPLErr JPGDatasetCommon::IRasterIO( GDALRWFlag eRWFlag,
                     }
                 }
             }
-            nLoadedScanline = -1;
+            nLoadedScanline = nRasterYSize;
         }
         else
         {
