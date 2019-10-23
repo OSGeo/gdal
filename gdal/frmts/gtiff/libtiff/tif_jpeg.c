fb5911987c8cc133d551f55103896b19b7da5269
Even Rouault <even.rouault@spatialys.com>
2019-10-23 16:53:26 +0200

    Internal libtiff: fix warning caused by previous commit

--- gdal/frmts/gtiff/libtiff/tif_jpeg.c
+++ gdal/frmts/gtiff/libtiff/tif_jpeg.c
@@ -1563,7 +1563,7 @@ JPEGDecodeRaw(TIFF* tif, uint8* buf, tmsize_t cc, uint16 s)
 					JSAMPLE *outptr = (JSAMPLE*)tmpbuf + clumpoffset;
 #else
 					JSAMPLE *outptr = (JSAMPLE*)buf + clumpoffset;
-					if (cc < (clumpoffset + (tmsize_t)samples_per_clump*(clumps_per_line-1) + hsamp)) {
+					if (cc < (tmsize_t)(clumpoffset + (tmsize_t)samples_per_clump*(clumps_per_line-1) + hsamp)) {
 						TIFFErrorExt(tif->tif_clientdata, "JPEGDecodeRaw",
 							     "application buffer not large enough for all data, possible subsampling issue");
 						return 0;
