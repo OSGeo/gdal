e2bd683c24f6f0f95c15665d643105c3fd461efd
Even Rouault <even.rouault@spatialys.com>
2019-11-16 17:52:26 +0100

    Doc: add a decl_configoption role, and a .py script to build an index of such configuration options [skip travis] [skip appveyor]

--- gdal/doc/source/_extensions/configoptions.py
+++ gdal/doc/source/_extensions/configoptions.py
@@ -5,11 +5,20 @@ def setup(app):
     app.add_role('lco', configoption('%s'))
     app.add_role('co', configoption('%s'))
     app.add_role('oo', configoption('%s'))
+    app.add_role('decl_configoption', decl_configoption('%s'))
     return { 'parallel_read_safe': False, 'parallel_write_safe': True }
 
 def configoption(pattern):
     def role(name, rawtext, text, lineno, inliner, options={}, content=[]):
         url = pattern % (text,)
         node = nodes.reference(rawtext, text, refuri=url, **options)
+        return [node], []
+    return role
+
+def decl_configoption(pattern):
+    def role(name, rawtext, text, lineno, inliner, options={}, content=[]):
+        children = [nodes.Text(text, text)]
+        node = nodes.literal(rawtext, '', *children,
+                             role=name.lower(), classes=[name])
         return [node], []
     return role
---
+++ gdal/doc/source/user/configoptions_index.rst
@@ -0,0 +1,9 @@
+.. configoptions_index:
+
+================================================================================
+Index of configuration options
+================================================================================
+
+List of configuration options and where they apply:
+
+.. include:: configoptions_index_generated.rst
