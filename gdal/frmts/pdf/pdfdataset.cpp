c421cccc29aff5ee85624a6441bfdcc1d8e0f685
Even Rouault <even.rouault@spatialys.com>
2020-01-08 14:26:12 +0100

    PDF: add support for poppler 0.85.0dev

--- gdal/frmts/pdf/pdfdataset.cpp
+++ gdal/frmts/pdf/pdfdataset.cpp
@@ -2579,7 +2579,13 @@ static void PDFDatasetErrorFunctionCommon(const CPLString& osError)
     CPLError(CE_Failure, CPLE_AppDefined, "%s", osError.c_str());
 }
 
-static void PDFDatasetErrorFunction(void* /* userData*/,
+static int g_nPopplerErrors = 0;
+constexpr int MAX_POPPLER_ERRORS = 1000;
+
+static void PDFDatasetErrorFunction(
+#if !(POPPLER_MAJOR_VERSION >= 1 || POPPLER_MINOR_VERSION >= 85)
+                                    void* /* userData*/,
+#endif
                                     ErrorCategory /* eErrCategory */,
                                     Goffset nPos,
 #if POPPLER_MAJOR_VERSION >= 1 || POPPLER_MINOR_VERSION >= 71
@@ -2589,6 +2595,10 @@ static void PDFDatasetErrorFunction(void* /* userData*/,
 #endif
                                    )
 {
+    if( g_nPopplerErrors >= MAX_POPPLER_ERRORS )
+        return;
+
+    g_nPopplerErrors ++;
     CPLString osError;
 
     if (nPos >= 0)
@@ -4071,7 +4081,11 @@ GDALDataset *PDFDataset::Open( GDALOpenInfo * poOpenInfo )
     GooString* poUserPwd = nullptr;
 
     /* Set custom error handler for poppler errors */
+#if POPPLER_MAJOR_VERSION >= 1 || POPPLER_MINOR_VERSION >= 85
+    setErrorCallback(PDFDatasetErrorFunction);
+#else
     setErrorCallback(PDFDatasetErrorFunction, nullptr);
+#endif
 
     {
         CPLMutexHolderD(&hGlobalParamsMutex);
