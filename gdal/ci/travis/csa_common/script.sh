1f0d1821abe6ac319ef63786911953c52d98521b
Even Rouault <even.rouault@spatialys.com>
2020-02-04 23:32:52 +0100

    Travis CSA: update to Xenial + CLang 9

--- gdal/ci/travis/csa_common/script.sh
+++ gdal/ci/travis/csa_common/script.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if grep -r "\\.c" gdal/scanbuildoutput | grep "<string>" | grep -v -e "<key>" -e degrib -e libjpeg -e libpng -e EHapi -e GDapi -e SWapi -e osr_cs_wkt_parser -e ods_formula_parser -e swq_parser; then
+if grep -r "\\.c" gdal/scanbuildoutput | grep "<string>" | grep -v -e "clang version" -e "<key>" -e degrib -e libjpeg -e libpng -e EHapi -e GDapi -e SWapi -e osr_cs_wkt_parser -e ods_formula_parser -e swq_parser; then
     echo error && /bin/false
 else
     echo ok
