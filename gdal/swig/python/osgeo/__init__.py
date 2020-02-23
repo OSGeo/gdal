543162a09fc076883a62afd5d589eaa3f9e56337
José de Paula Rodrigues Neto Assis <espinafre@gmail.com>
2020-02-23 15:19:24 -0300

    Python warning, hopefully compatible with 2.7
    This change fixes the Python warning about imp with Python 2.7+. There
    is no warning with Python 3.8+, and it doesn't use incompatible APIs
    from Python 3.4+. This mimicks the behavior of swig-generated source,
    with swig 3.0.2.

--- gdal/swig/python/osgeo/__init__.py
+++ gdal/swig/python/osgeo/__init__.py
@@ -9,19 +9,41 @@ if version_info >= (3, 8, 0) and platform == 'win32':
             if p:
                 os.add_dll_directory(p)
 
-if version_info >= (2, 6, 0):
+if version_info >= (2, 7, 0):
+    def swig_import_helper():
+        import importlib
+        from os.path import dirname, basename
+        mname = basename(dirname(__file__)) + '._gdal'
+        try:
+            return importlib.import_module(mname)
+        except ImportError as e:
+            if version_info >= (3, 8, 0) and platform == 'win32':
+                import os
+                if not 'USE_PATH_FOR_GDAL_PYTHON' in os.environ:
+                    msg = 'On Windows, with Python >= 3.8, DLLs are no longer imported from the PATH.\n'
+                    msg += 'If gdalXXX.dll is in the PATH, then set the USE_PATH_FOR_GDAL_PYTHON=YES environment variable\n'
+                    msg += 'to feed the PATH into os.add_dll_directory().'
+
+                    import sys
+                    import traceback
+                    traceback_string = ''.join(traceback.format_exception(*sys.exc_info()))
+                    raise ImportError(traceback_string + '\n' + msg)
+            return importlib.import_module('_gdal', pkg)
+    _gdal = swig_import_helper()
+    del swig_import_helper
+elif version_info >= (2, 6, 0):
     def swig_import_helper():
         from os.path import dirname
-        import importlib.machinery
-        spec = None
+        import imp
+        fp = None
         try:
-            spec = importlib.machinery.PathFinder().find_spec('_gdal', [dirname(__file__)])
+            fp, pathname, description = imp.find_module('_gdal', [dirname(__file__)])
         except ImportError:
             import _gdal
             return _gdal
-        if spec is not None:
+        if fp is not None:
             try:
-                _mod = spec.loader.load_module()
+                _mod = imp.load_module('_gdal', fp, pathname, description)
             except ImportError as e:
                 if version_info >= (3, 8, 0) and platform == 'win32':
                     import os
@@ -35,6 +57,8 @@ if version_info >= (2, 6, 0):
                         traceback_string = ''.join(traceback.format_exception(*sys.exc_info()))
                         raise ImportError(traceback_string + '\n' + msg)
                 raise
+            finally:
+                fp.close()
             return _mod
     _gdal = swig_import_helper()
     del swig_import_helper
