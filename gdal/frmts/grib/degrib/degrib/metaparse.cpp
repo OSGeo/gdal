7ffc9cc181fb7bcba2ae7946a480fc55259b8bfb
Even Rouault <even.rouault@spatialys.com>
2020-02-27 12:28:20 +0100

    degrib: fix compiler warning introduced by recent commit

--- gdal/frmts/grib/degrib/degrib/metaparse.cpp
+++ gdal/frmts/grib/degrib/degrib/metaparse.cpp
@@ -3012,12 +3012,11 @@ void ParseGrid (VSILFILE *fp, gridAttribType *attrib, double **Grib_Data,
           }
       }
 
-      *grib_DataLen = subNxNy;
       double* newData = nullptr;
-      if( *grib_DataLen <= std::numeric_limits<size_t>::max() / sizeof(double) )
+      const size_t nBufferSize = subNxNy * sizeof (double);
+      if( nBufferSize / sizeof(double) == subNxNy )
       {
-        newData = (double *) realloc ((void *) (*Grib_Data),
-                                       (*grib_DataLen) * sizeof (double));
+        newData = (double *) realloc ((void *) (*Grib_Data), nBufferSize);
       }
       if( newData == nullptr )
       {
@@ -3027,6 +3026,7 @@ void ParseGrid (VSILFILE *fp, gridAttribType *attrib, double **Grib_Data,
           *grib_DataLen = 0;
           return;
       }
+      *grib_DataLen = subNxNy;
       *Grib_Data = newData;
    }
    grib_Data = *Grib_Data;
