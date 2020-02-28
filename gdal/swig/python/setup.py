8ec4e5eb0571a329b18e2e286de8af31cf17735e
Vincent Philippon <sindaewoh@gmail.com>
2020-02-28 16:35:39 -0500

    Python: Add numpy as extras_require dependency (fixes #2158)
    Doing `pip install gdal[numpy]` will install `gdal` with `numpy > 1.0.0`.
    `pip` will ensure that `numpy` is installed first so that it's detected
    and the releveant features/bindings/etc. are compiled and made available
    for users.

--- gdal/swig/python/setup.py
+++ gdal/swig/python/setup.py
@@ -463,7 +463,8 @@ setup_kwargs = dict(
     data_files=data_files,
     ext_modules=ext_modules,
     scripts=glob('scripts/*.py'),
-    cmdclass={'build_ext': gdal_ext}
+    cmdclass={'build_ext': gdal_ext},
+    extras_require={'numpy': ['numpy > 1.0.0']},
 )
 
 # This section can be greatly simplified with python >= 3.5 using **
