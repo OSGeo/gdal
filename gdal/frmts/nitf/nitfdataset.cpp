2c55dafb095a74714264b2c4855b5b110c01bf07
Even Rouault <even.rouault@spatialys.com>
2020-03-03 18:07:43 +0100

    NITF: avoid potential stack read buffer overflow. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=21024

--- gdal/frmts/nitf/nitfdataset.cpp
+++ gdal/frmts/nitf/nitfdataset.cpp
@@ -3435,7 +3435,7 @@ int NITFDataset::ScanJPEGQLevel( GUIntBig *pnDataStart, bool *pbError )
 /* -------------------------------------------------------------------- */
 /*      Do we have an NITF app tag?  If so, pull out the Q level.       */
 /* -------------------------------------------------------------------- */
-    if( !EQUAL((char *)abyHeader+nOffset+6,"NITF") )
+    if( memcmp(abyHeader+nOffset+6,"NITF\0",5) != 0 )
         return 0;
 
     return abyHeader[22+nOffset];
