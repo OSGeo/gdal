198d510a8188eb2e8c6766be5af43f67163e1fff
Even Rouault <even.rouault@spatialys.com>
2020-03-25 13:36:53 +0100

    JP2KAK and JP2OPENJPEG: fix to read images whose origin is not (0,0)

--- gdal/frmts/openjpeg/openjpegdataset.cpp
+++ gdal/frmts/openjpeg/openjpegdataset.cpp
@@ -227,6 +227,8 @@ class JP2OpenJPEGDataset final: public GDALJP2AbstractDataset
     JP2OpenJPEGFile* m_psJP2OpenJPEGFile = nullptr;
     int*             m_pnLastLevel = nullptr;
 #endif
+    int         m_nX0 = 0;
+    int         m_nY0 = 0;
 
     int         nThreads = -1;
     int         m_nBlocksToLoad = 0;
@@ -932,10 +934,10 @@ CPLErr JP2OpenJPEGDataset::ReadBlock( int nBand, VSILFILE* fpIn,
         /* The decode area must be expressed in grid reference, ie at full*/
         /* scale */
         if (!opj_set_decode_area(pCodec,psImage,
-                                 static_cast<int>(static_cast<GIntBig>(nBlockXOff*nBlockXSize) * nParentXSize / nRasterXSize),
-                                 static_cast<int>(static_cast<GIntBig>(nBlockYOff*nBlockYSize) * nParentYSize / nRasterYSize),
-                                 static_cast<int>(static_cast<GIntBig>(nBlockXOff*nBlockXSize+nWidthToRead) * nParentXSize / nRasterXSize),
-                                 static_cast<int>(static_cast<GIntBig>(nBlockYOff*nBlockYSize+nHeightToRead) * nParentYSize / nRasterYSize)))
+                                 m_nX0 + static_cast<int>(static_cast<GIntBig>(nBlockXOff*nBlockXSize) * nParentXSize / nRasterXSize),
+                                 m_nY0 + static_cast<int>(static_cast<GIntBig>(nBlockYOff*nBlockYSize) * nParentYSize / nRasterYSize),
+                                 m_nX0 + static_cast<int>(static_cast<GIntBig>(nBlockXOff*nBlockXSize+nWidthToRead) * nParentXSize / nRasterXSize),
+                                 m_nY0 + static_cast<int>(static_cast<GIntBig>(nBlockYOff*nBlockYSize+nHeightToRead) * nParentYSize / nRasterYSize)))
         {
             CPLError(CE_Failure, CPLE_AppDefined, "opj_set_decode_area() failed");
             eErr = CE_Failure;
@@ -1879,6 +1881,8 @@ GDALDataset *JP2OpenJPEGDataset::Open( GDALOpenInfo * poOpenInfo )
     poDS->bIs420 = bIs420;
     poDS->bSingleTiled = (poDS->nRasterXSize == (int)nTileW &&
                           poDS->nRasterYSize == (int)nTileH);
+    poDS->m_nX0 = psImage->x0;
+    poDS->m_nY0 = psImage->y0;
 
     if( CPLFetchBool(poOpenInfo->papszOpenOptions, "USE_TILE_AS_BLOCK", false) )
     {
@@ -2221,6 +2225,8 @@ GDALDataset *JP2OpenJPEGDataset::Open( GDALOpenInfo * poOpenInfo )
         }
         poODS->m_pnLastLevel = poDS->m_pnLastLevel;
 #endif
+        poODS->m_nX0 = poDS->m_nX0;
+        poODS->m_nY0 = poDS->m_nY0;
 
         for( iBand = 1; iBand <= poDS->nBands; iBand++ )
         {
