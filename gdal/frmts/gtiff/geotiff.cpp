397893fc6ddcac42e2001aa9fb86d4751c31da15
Even Rouault <even.rouault@spatialys.com>
2020-04-19 21:10:17 +0200

    GTiff: avoid crash on single-component file with Whitepoint and PrimaryChromaticities tags

--- gdal/frmts/gtiff/geotiff.cpp
+++ gdal/frmts/gtiff/geotiff.cpp
@@ -13137,8 +13137,11 @@ void GTiffDataset::LoadICCProfile()
         if( TIFFGetField(hTIFF, TIFFTAG_WHITEPOINT, &pWP) )
         {
             if( !TIFFGetFieldDefaulted( hTIFF, TIFFTAG_TRANSFERFUNCTION, &pTFR,
-                                        &pTFG, &pTFB) )
+                                        &pTFG, &pTFB) ||
+                pTFR == nullptr || pTFG == nullptr || pTFB == nullptr )
+            {
                 return;
+            }
 
             const int TIFFTAG_TRANSFERRANGE = 0x0156;
             TIFFGetFieldDefaulted( hTIFF, TIFFTAG_TRANSFERRANGE,
