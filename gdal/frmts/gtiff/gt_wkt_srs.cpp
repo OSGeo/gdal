b8e46ecd5ebd6abddd72537939cf43ed7efe75d4
Even Rouault <even.rouault@spatialys.com>
2020-04-26 22:27:33 +0200

    Typo fixes [ci skip]

--- gdal/frmts/gtiff/gt_wkt_srs.cpp
+++ gdal/frmts/gtiff/gt_wkt_srs.cpp
@@ -909,7 +909,7 @@ OGRSpatialReferenceH GTIFGetOGISDefnAsOSR( GTIF *hGTIF, GTIFDefn * psDefn )
             // into account
             const char* pszUnitsName = nullptr;
             double dfUOMLengthInMeters = oSRS.GetLinearUnits( &pszUnitsName );
-            // Non exact comparision, as there's a slight difference between
+            // Non exact comparison, as there's a slight difference between
             // the evaluation of US Survey foot harcoded in geo_normalize.c to
             // 12.0 / 39.37, and the corresponding value returned by
             // PROJ >= 6.0.0 and <= 7.0.0 for EPSG:9003
--- gdal/port/cpl_vsil_curl_class.h
+++ gdal/port/cpl_vsil_curl_class.h
@@ -594,7 +594,7 @@ struct CurlRequestHelper
     CurlRequestHelper();
     ~CurlRequestHelper();
     long perform(CURL* hCurlHandle,
-                 struct curl_slist* headers, // ownership transfered
+                 struct curl_slist* headers, // ownership transferred
                  VSICurlFilesystemHandler *poFS,
                  IVSIS3LikeHandleHelper *poS3HandleHelper);
 };
