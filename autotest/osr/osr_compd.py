OSGeo/gdal

commit b8da6777a3e5560f6372b2dc3e7b89bf96d64d59

OSR: Make GetAuthorityCode('PROJCS') work on a WKT1 COMPD_CS with a VERT_DATUM type = 2002 (Ellipsoid height)

autotest/osr/osr_compd.py
@@ -327,4 +327,17 @@ def test_osr_compd_8():
     assert wkt.startswith('COMPD_CS'), 'COMPD_CS not recognised as compound.'
 
 
+###############################################################################
+# Test COMPD_CS with a VERT_DATUM type = 2002 (Ellipsoid height)
 
+def test_osr_compd_vert_datum_2002():
+
+    if osr.GetPROJVersionMajor() * 10000 + osr.GetPROJVersionMinor() * 100 < 70100:
+        # Not supported before PROJ 7.1
+        pytest.skip()
+
+    sr = osr.SpatialReference()
+    sr.SetFromUserInput('COMPD_CS["NAD83 / Alabama West + Ellipsoidal height",PROJCS["NAD83 / Alabama West",GEOGCS["NAD83",DATUM["North_American_Datum_1983",SPHEROID["GRS 1980",6378137,298.257222101,AUTHORITY["EPSG","7019"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY["EPSG","6269"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4269"]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",30],PARAMETER["central_meridian",-87.5],PARAMETER["scale_factor",0.999933333],PARAMETER["false_easting",600000],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["X",EAST],AXIS["Y",NORTH],AUTHORITY["EPSG","26930"]],VERT_CS["Ellipsoidal height",VERT_DATUM["Ellipsoid",2002],UNIT["metre",1.0,AUTHORITY["EPSG","9001"]],AXIS["Up",UP]]]')
+    assert sr.IsProjected()
+    assert sr.GetAuthorityCode('PROJCS') == '26930'
+    assert sr.GetAuthorityName('PROJCS') == 'EPSG'

gdal/ogr/ogrspatialreference.cpp
@@ -7654,6 +7654,7 @@ OGRSpatialReference::GetAuthorityCode( const char *pszTargetKey ) const
 
 {
     d->refreshProjObj();
+    const char* pszInputTargetKey = pszTargetKey;
     pszTargetKey = d->nullifyTargetKeyIfPossible(pszTargetKey);
     if( pszTargetKey == nullptr )
     {
@@ -7663,8 +7664,30 @@ OGRSpatialReference::GetAuthorityCode( const char *pszTargetKey ) const
         }
         d->demoteFromBoundCRS();
         auto ret = proj_get_id_code(d->m_pj_crs, 0);
+        bool tryGetAttrNode = false;
+        if( ret == nullptr && d->m_pjType == PJ_TYPE_PROJECTED_CRS )
+        {
+            auto ctxt = d->getPROJContext();
+            auto cs = proj_crs_get_coordinate_system(ctxt, d->m_pj_crs);
+            if( cs )
+            {
+                const int axisCount = proj_cs_get_axis_count(ctxt, cs);
+                proj_destroy(cs);
+                if( axisCount == 3 )
+                {
+                    // This might come from a COMPD_CS with a VERT_DATUM type = 2002
+                    // in which case, using the WKT1 representation will enable
+                    // us to recover the EPSG code.
+                    tryGetAttrNode = true;
+                    pszTargetKey = pszInputTargetKey;
+                }
+            }
+        }
         d->undoDemoteFromBoundCRS();
-        return ret;
+        if( ret != nullptr || !tryGetAttrNode )
+        {
+            return ret;
+        }
     }
 
 /* -------------------------------------------------------------------- */
@@ -7738,6 +7761,7 @@ OGRSpatialReference::GetAuthorityName( const char *pszTargetKey ) const
 
 {
     d->refreshProjObj();
+    const char* pszInputTargetKey = pszTargetKey;
     pszTargetKey = d->nullifyTargetKeyIfPossible(pszTargetKey);
     if( pszTargetKey == nullptr )
     {
@@ -7747,8 +7771,30 @@ OGRSpatialReference::GetAuthorityName( const char *pszTargetKey ) const
         }
         d->demoteFromBoundCRS();
         auto ret = proj_get_id_auth_name(d->m_pj_crs, 0);
+        bool tryGetAttrNode = false;
+        if( ret == nullptr && d->m_pjType == PJ_TYPE_PROJECTED_CRS )
+        {
+            auto ctxt = d->getPROJContext();
+            auto cs = proj_crs_get_coordinate_system(ctxt, d->m_pj_crs);
+            if( cs )
+            {
+                const int axisCount = proj_cs_get_axis_count(ctxt, cs);
+                proj_destroy(cs);
+                if( axisCount == 3 )
+                {
+                    // This might come from a COMPD_CS with a VERT_DATUM type = 2002
+                    // in which case, using the WKT1 representation will enable
+                    // us to recover the EPSG code.
+                    tryGetAttrNode = true;
+                    pszTargetKey = pszInputTargetKey;
+                }
+            }
+        }
         d->undoDemoteFromBoundCRS();
-        return ret;
+        if( ret != nullptr || !tryGetAttrNode )
+        {
+            return ret;
+        }
     }
 
 /* -------------------------------------------------------------------- */

commit 3679d31e4b99754042a31181c15fa89cf12d1371

OSR: fix crashing bug introduced by b8da6777a3e5560f6372b2dc3e7b89bf96d64d59 (master only)

autotest/osr/osr_compd.py
@@ -341,3 +341,5 @@ def test_osr_compd_vert_datum_2002():
     assert sr.IsProjected()
     assert sr.GetAuthorityCode('PROJCS') == '26930'
     assert sr.GetAuthorityName('PROJCS') == 'EPSG'
+    assert sr.GetAuthorityCode(None) is None
+    assert sr.GetAuthorityName(None) is None

gdal/ogr/ogrspatialreference.cpp
@@ -7664,7 +7664,6 @@ OGRSpatialReference::GetAuthorityCode( const char *pszTargetKey ) const
         }
         d->demoteFromBoundCRS();
         auto ret = proj_get_id_code(d->m_pj_crs, 0);
-        bool tryGetAttrNode = false;
         if( ret == nullptr && d->m_pjType == PJ_TYPE_PROJECTED_CRS )
         {
             auto ctxt = d->getPROJContext();
@@ -7678,13 +7677,12 @@ OGRSpatialReference::GetAuthorityCode( const char *pszTargetKey ) const
                     // This might come from a COMPD_CS with a VERT_DATUM type = 2002
                     // in which case, using the WKT1 representation will enable
                     // us to recover the EPSG code.
-                    tryGetAttrNode = true;
                     pszTargetKey = pszInputTargetKey;
                 }
             }
         }
         d->undoDemoteFromBoundCRS();
-        if( ret != nullptr || !tryGetAttrNode )
+        if( ret != nullptr || pszTargetKey == nullptr )
         {
             return ret;
         }
@@ -7771,7 +7769,6 @@ OGRSpatialReference::GetAuthorityName( const char *pszTargetKey ) const
         }
         d->demoteFromBoundCRS();
         auto ret = proj_get_id_auth_name(d->m_pj_crs, 0);
-        bool tryGetAttrNode = false;
         if( ret == nullptr && d->m_pjType == PJ_TYPE_PROJECTED_CRS )
         {
             auto ctxt = d->getPROJContext();
@@ -7785,13 +7782,12 @@ OGRSpatialReference::GetAuthorityName( const char *pszTargetKey ) const
                     // This might come from a COMPD_CS with a VERT_DATUM type = 2002
                     // in which case, using the WKT1 representation will enable
                     // us to recover the EPSG code.
-                    tryGetAttrNode = true;
                     pszTargetKey = pszInputTargetKey;
                 }
             }
         }
         d->undoDemoteFromBoundCRS();
-        if( ret != nullptr || !tryGetAttrNode )
+        if( ret != nullptr || pszTargetKey == nullptr )
         {
             return ret;
         }