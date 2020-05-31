f44b2e40bb33b8df2b23c5430cfff9e4ef28c1a4
Even Rouault <even.rouault@spatialys.com>
2020-05-31 17:58:26 +0200

    detect_suspicious_char_digit_zero.sh: improve to be run from anywhere

--- gdal/scripts/detect_suspicious_char_digit_zero.sh
+++ gdal/scripts/detect_suspicious_char_digit_zero.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+set -eu
+
+SCRIPT_DIR=$(dirname "$0")
+case $SCRIPT_DIR in
+    "/"*)
+        ;;
+    ".")
+        SCRIPT_DIR=$(pwd)
+        ;;
+    *)
+        SCRIPT_DIR=$(pwd)/$(dirname "$0")
+        ;;
+esac
+GDAL_ROOT=$SCRIPT_DIR/..
+cd "$GDAL_ROOT"
+
 ret_code=0
 
 echo "Checking for suspicious comparisons to '0'..."
