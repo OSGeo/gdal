f8e0c61e20afbbf89f5335b08affabf2ec45e5b7
René Buffat <buffat@gmail.com>
2020-06-08 15:03:06 +0200

    test datatypes

--- autotest/gcore/test_driver_metadata.py
+++ autotest/gcore/test_driver_metadata.py
@@ -3,7 +3,7 @@
 # $Id$
 #
 # Project:  GDAL/OGR Test Suite
-# Purpose:  Test driver matadata xml
+# Purpose:  Test driver matadata
 # Author:   Rene Buffat <buffat at gmail dot com>
 #
 ###############################################################################
@@ -491,3 +491,30 @@ def test_metadata_multidim_group_creationoptionslist(driver_name):
         except:
             print(xml)
             raise
+
+
+@pytest.mark.parametrize('driver_name', all_driver_names)
+def test_metadata_creation_field_datatypes(driver_name):
+    """ Test if GDAL_DMD_CREATIONFIELDDATATYPES metadataitem returns valid datatypes """
+
+    valid_datatypes = {'Integer', 'Integer64', 'Real', 'String', 'Date', 'DateTime', 'Time', 'IntegerList',
+                       'Integer64List', 'RealList', 'StringList', 'Binary'}
+
+    driver = gdal.GetDriverByName(driver_name)
+    datatypes_str = driver.GetMetadataItem('GDAL_DMD_CREATIONFIELDDATATYPES')
+    if datatypes_str is not None:
+        for datatype in datatypes_str.split(' '):
+            assert datatype in valid_datatypes
+
+
+@pytest.mark.parametrize('driver_name', all_driver_names)
+def test_metadata_creation_sub_field_datatypes(driver_name):
+    """ Test if GDAL_DMD_CREATIONFIELDDATATYPES metadataitem returns valid datatypes """
+
+    valid_datatypes = {'Boolean', 'Float32', 'Int16'}
+
+    driver = gdal.GetDriverByName(driver_name)
+    datatypes_str = driver.GetMetadataItem('DMD_CREATIONFIELDDATASUBTYPES')
+    if datatypes_str is not None:
+        for datatype in datatypes_str.split(' '):
+            assert datatype in valid_datatypes
