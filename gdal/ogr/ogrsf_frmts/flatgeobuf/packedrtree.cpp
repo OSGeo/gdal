203b1cfa32cd6850c998667a38673655e74cca2d
Björn Harrtell <bjornharrtell@users.noreply.github.com>
2020-07-20 23:04:51 +0200

    FlatGeobuf: fix illegal use of std::vector (fixes #2773)

--- gdal/ogr/ogrsf_frmts/flatgeobuf/packedrtree.cpp
+++ gdal/ogr/ogrsf_frmts/flatgeobuf/packedrtree.cpp
@@ -316,8 +316,7 @@ std::vector<SearchResultItem> PackedRTree::streamSearch(
     auto levelBounds = generateLevelBounds(numItems, nodeSize);
     uint64_t leafNodesOffset = levelBounds.front().first;
     uint64_t numNodes = levelBounds.front().second;
-    std::vector<NodeItem> nodeItems;
-    nodeItems.reserve(nodeSize);
+    auto nodeItems = std::vector<NodeItem>(nodeSize);
     uint8_t *nodesBuf = reinterpret_cast<uint8_t *>(nodeItems.data());
     // use ordered search queue to make index traversal in sequential order
     std::map<uint64_t, uint64_t> queue;
