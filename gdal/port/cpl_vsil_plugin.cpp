ea1621dc0f06cb228a817f8a708a96a0259d4314
Thomas Bonfort <thomas.bonfort@gmail.com>
2020-07-31 18:56:18 +0200

    Update cpl_vsil_plugin.cpp
    fix

--- gdal/port/cpl_vsil_plugin.cpp
+++ gdal/port/cpl_vsil_plugin.cpp
@@ -131,7 +131,7 @@ VSIVirtualHandle* VSIPluginFilesystemHandler::Open( const char *pszFilename,
         }
         return nullptr;
     }
-    if ( m_cb->nBufferSize<=0 ) {
+    if ( m_cb->nBufferSize==0 ) {
         return new VSIPluginHandle(this, cbData);
     } else {
         return VSICreateBufferedReaderHandle(new VSIPluginHandle(this, cbData), m_cb->nBufferSize);
