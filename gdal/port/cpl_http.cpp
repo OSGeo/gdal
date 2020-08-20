d8190ae29cd4d722fa8c06d63aaa762e4c9b0346
Even Rouault <even.rouault@spatialys.com>
2020-08-20 20:46:34 +0200

    CPLHTTPFetch(): reset CURLOPT_HTTPHEADER after request

--- gdal/port/cpl_http.cpp
+++ gdal/port/cpl_http.cpp
@@ -1122,6 +1122,7 @@ CPLHTTPResult *CPLHTTPFetchEx( const char *pszURL, CSLConstList papszOptions,
 
     if( CSLFetchNameValue(papszOptions, "POSTFIELDS") )
         curl_easy_setopt(http_handle, CURLOPT_POST, 0 );
+    curl_easy_setopt(http_handle, CURLOPT_HTTPHEADER, nullptr);
 
     if( !pszPersistent )
         curl_easy_cleanup( http_handle );
