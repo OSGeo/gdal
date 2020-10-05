77c1dae5e40e47e7ea55533f2152dd1856b37ca9
FX Coudert <fxcoudert@gmail.com>
2020-10-05 12:03:31 +0200

    Update jpeg2000_vsil_io.cpp
    Fix https://github.com/OSGeo/gdal/issues/3012

--- gdal/frmts/jpeg2000/jpeg2000_vsil_io.cpp
+++ gdal/frmts/jpeg2000/jpeg2000_vsil_io.cpp
@@ -109,7 +109,7 @@ static int JPEG2000_VSIL_read(jas_stream_obj_t *obj, char *buf, unsigned cnt)
 #ifndef PRIjas_seqent
 static int JPEG2000_VSIL_write(jas_stream_obj_t *obj, char *buf, int cnt)
 #else
-static int JPEG2000_VSIL_write(jas_stream_obj_t *obj, char *buf, unsigned int cnt)
+static int JPEG2000_VSIL_write(jas_stream_obj_t *obj, const char *buf, unsigned int cnt)
 #endif
 {
     jas_stream_VSIFL_t *fileobj = JAS_CAST(jas_stream_VSIFL_t *, obj);
