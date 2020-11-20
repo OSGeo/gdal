1b3582aac263c268752c6f49ac01688151c4948f
Even Rouault <even.rouault@spatialys.com>
2020-11-20 14:09:57 +0100

    tif_lzw.c: fix false positive -Wnull-dereference

--- gdal/frmts/gtiff/libtiff/tif_lzw.c
+++ gdal/frmts/gtiff/libtiff/tif_lzw.c
@@ -214,18 +214,15 @@ LZWSetupDecode(TIFF* tif)
 			return (0);
 		}
 
-		DecoderState(tif)->dec_codetab = NULL;
-		DecoderState(tif)->dec_decode = NULL;
+		sp = DecoderState(tif);
+		sp->dec_codetab = NULL;
+		sp->dec_decode = NULL;
 
 		/*
 		 * Setup predictor setup.
 		 */
 		(void) TIFFPredictorInit(tif);
-
-		sp = DecoderState(tif);
 	}
-
-	assert(sp != NULL);
 
 	if (sp->dec_codetab == NULL) {
 		sp->dec_codetab = (code_t*)_TIFFmalloc(CSIZE*sizeof (code_t));
