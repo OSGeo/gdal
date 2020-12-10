8d85d5b2f6b577729e6723f6e7a6f0343ea88af0
Howard Butler <howard@hobu.co>
2020-12-10 11:41:37 -0600

    test upload of conda package to Anaconda

--- gdal/ci/travis/conda/upload.sh
+++ gdal/ci/travis/conda/upload.sh
@@ -14,5 +14,5 @@ if [ "$PLATFORM" == "macos-latest" ]; then
     CI_PLAT="osx"
 fi
 
-anaconda -t $ANACONDA_TOKEN upload -u gdal-master --force packages/$CI_PLAT-64/gdal*.bz2 packages/$CI_PLAT-64/*gdal*.bz2
+anaconda -t "$ANACONDA_TOKEN" upload -u gdal-master --force packages/$CI_PLAT-64/gdal*.bz2 packages/$CI_PLAT-64/*gdal*.bz2
 
