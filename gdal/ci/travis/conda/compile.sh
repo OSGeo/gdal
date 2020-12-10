22f4b9d4ac7908023125a64a434469ad56f9b4f1
Howard Butler <howard@hobu.co>
2020-12-09 20:09:20 -0600

    bash variable name

--- gdal/ci/travis/conda/compile.sh
+++ gdal/ci/travis/conda/compile.sh
@@ -4,15 +4,15 @@ mkdir -p packages
 
 CI_PLAT=""
 if [ "$PLATFORM" == "windows-latest" ]; then
-    CI_SUPPORT="win"
+    CI_PLAT="win"
 fi
 
 if [ "$PLATFORM" == "ubuntu-latest" ]; then
-    CI_SUPPORT="linux"
+    CI_PLAT="linux"
 fi
 
 if [ "$PLATFORM" == "macos-latest" ]; then
-    CI_SUPPORT="osx"
+    CI_PLAT="osx"
 fi
 
 conda build recipe --clobber-file recipe/recipe_clobber.yaml --output-folder packages -m ".ci_support/${CI_PLAT}_64_.yaml"
--- gdal/ci/travis/conda/upload.sh
+++ gdal/ci/travis/conda/upload.sh
@@ -3,15 +3,15 @@
 
 CI_PLAT=""
 if [ "$PLATFORM" == "windows-latest" ]; then
-    CI_SUPPORT="win"
+    CI_PLAT="win"
 fi
 
 if [ "$PLATFORM" == "ubuntu-latest" ]; then
-    CI_SUPPORT="linux"
+    CI_PLAT="linux"
 fi
 
 if [ "$PLATFORM" == "macos-latest" ]; then
-    CI_SUPPORT="osx"
+    CI_PLAT="osx"
 fi
 
 anaconda -t $ANACONDA_TOKEN upload -u gdal-master --force packages/$CI_PLAT-64/gdal*.bz2 packages/$CI_PLAT-64/*gdal*.bz2
