691ea91ad63db1d5821d5e9c1500b9769d8e6715
Howard Butler <howard@hobu.co>
2020-12-11 15:20:58 -0600

    secrets lint

--- gdal/ci/travis/conda/upload.sh
+++ gdal/ci/travis/conda/upload.sh
@@ -17,5 +17,6 @@ fi
 ls
 pwd
 ls packages
+echo "anaconda upload -t \"$SECRETS_TEST\"  -u gdal-master --force packages/$CI_PLAT-64/gdal*.bz2 packages/$CI_PLAT-64/*gdal*.bz2"
 anaconda upload -t "$ANACONDA_TOKEN"  -u gdal-master --force packages/$CI_PLAT-64/gdal*.bz2 packages/$CI_PLAT-64/*gdal*.bz2
 
