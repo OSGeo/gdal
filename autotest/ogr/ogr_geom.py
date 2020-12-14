a24d36f367d570392a8f5562d7e8b927c8479271
Even Rouault <even.rouault@spatialys.com>
2020-12-15 00:03:49 +0100

    Python bindings: import correct behaviour for copy.copy() and copy.deepcopy() on a Geometry object. Fixes #3278

--- autotest/ogr/ogr_geom.py
+++ autotest/ogr/ogr_geom.py
@@ -27,6 +27,7 @@
 # Boston, MA 02111-1307, USA.
 ###############################################################################
 
+import copy
 import math
 import pickle
 import random
@@ -3413,3 +3414,35 @@ def test_ogr_geom_removeLowerDimensionSubGeoms(input_wkt, expected_wkt):
     g = ogr.CreateGeometryFromWkt(input_wkt)
     g = g.RemoveLowerDimensionSubGeoms()
     assert g.ExportToIsoWkt() == expected_wkt
+
+
+###############################################################################
+
+def test_ogr_geom_copy():
+
+    g = ogr.CreateGeometryFromWkt('POINT (0 1)')
+    sr = osr.SpatialReference()
+    sr.ImportFromEPSG(4326)
+    g.AssignSpatialReference(sr)
+
+    g2 = copy.copy(g)
+    assert g2.ExportToWkt() == g.ExportToWkt()
+    assert g2.GetSpatialReference().IsSame(sr)
+    sr.ImportFromEPSG(32631)
+    assert g2.GetSpatialReference().IsSame(sr)
+
+
+###############################################################################
+
+def test_ogr_geom_deepcopy():
+
+    g = ogr.CreateGeometryFromWkt('POINT (0 1)')
+    sr = osr.SpatialReference()
+    sr.ImportFromEPSG(4326)
+    g.AssignSpatialReference(sr)
+
+    g2 = copy.deepcopy(g)
+    assert g2.ExportToWkt() == g.ExportToWkt()
+    assert g2.GetSpatialReference().IsSame(sr)
+    sr.ImportFromEPSG(32631)
+    assert not g2.GetSpatialReference().IsSame(sr)
