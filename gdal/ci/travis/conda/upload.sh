d7271023eff4b8e2f92c1be89d39f1decf4094ec
Howard Butler <howard@hobu.co>
2020-12-14 07:41:10 -0600

    Fix upload of conda package to Anaconda (#3281)
    Another attempt to fix conda package upload. I'm still not quite sure this will properly upload, but I hope it quits causing failures.

--- gdal/ci/travis/conda/upload.sh
+++ gdal/ci/travis/conda/upload.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 
+if [ -z "${ANACONDA_TOKEN+x}" ]
+then
+    echo "Anaconda token is not set, not uploading"
+    exit 0;
+fi
+
+if [ -z "${ANACONDA_TOKEN}" ]
+then
+    echo "Anaconda token is empty, not uploading"
+    exit 0;
+fi
+
 CI_PLAT=""
 if [ "$PLATFORM" == "windows-latest" ]; then
     CI_PLAT="win"
@@ -14,5 +26,10 @@ if [ "$PLATFORM" == "macos-latest" ]; then
     CI_PLAT="osx"
 fi
 
-anaconda -t $ANACONDA_TOKEN upload -u gdal-master --force packages/$CI_PLAT-64/gdal*.bz2 packages/$CI_PLAT-64/*gdal*.bz2
+ls
+pwd
+ls packages
+
+echo "Anaconda token is available, attempting to upload"
+anaconda upload -t "$ANACONDA_TOKEN"  -u gdal-master --force packages/$CI_PLAT-64/gdal*.bz2 packages/$CI_PLAT-64/*gdal*.bz2
 
