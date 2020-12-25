8a4f8b20d9aa17e6f9e54fb41e3950f88412272b
Howard Butler <howard@hobu.co>
2020-12-25 12:59:51 -0600

    typo

--- gdal/ci/travis/conda/compile.sh
+++ gdal/ci/travis/conda/compile.sh
@@ -18,7 +18,7 @@ fi
 
 
 conda build recipe --clobber-file recipe/recipe_clobber.yaml --output-folder packages -m ".ci_support/${CI_PLAT}_64_.yaml"
-conda create -y -n test - c ./packages python=3.8 libgdal gdal
+conda create -y -n test -c ./packages python=3.8 libgdal gdal
 conda deactivate
 conda activate test
 gdalinfo --version
