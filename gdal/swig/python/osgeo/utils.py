90608af74681f6263bbd4046b330b624018b0bcc
Idan Miara <idan@miara.com>
2021-01-15 16:22:36 +0200

    gdal/swig/python/osgeo/utils.py - show proper ImportError

---
+++ gdal/swig/python/osgeo/utils.py
@@ -0,0 +1,3 @@
+raise ImportError("Please use `import osgeo_utils` (GDAL >= 3.3) "
+                  "instead of `import osgeo.utils` (GDAL == 3.2). "
+                  "For more details see GDAL RFC #78")
