de2e8e517cca6534136c4cf8646a4ef99698d1bb
Victor <victor@amigocloud.com>
2021-02-24 09:53:10 -0500

    AmigoCloud driver: Remove unused declaration.

--- gdal/ogr/ogrsf_frmts/amigocloud/ogr_amigocloud.h
+++ gdal/ogr/ogrsf_frmts/amigocloud/ogr_amigocloud.h
@@ -43,7 +43,6 @@
 
 json_object* OGRAMIGOCLOUDGetSingleRow(json_object* poObj);
 CPLString OGRAMIGOCLOUDEscapeIdentifier(const char* pszStr);
-CPLString OGRAMIGOCLOUDEscapeLiteral(const char* pszStr);
 std::string OGRAMIGOCLOUDJsonEncode(const std::string &value);
 
 /************************************************************************/
