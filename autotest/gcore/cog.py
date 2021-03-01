01336e7b3b315368eb751fa06792f35ad4bbffb2
Blayne Chard <blayne@chard.com>
2021-03-01 15:43:20 +1300

    test: validate that overview quality is being passed through

--- autotest/gcore/cog.py
+++ autotest/gcore/cog.py
@@ -268,16 +268,18 @@ def test_cog_creation_of_overviews_with_compression():
                             options='-of MEM -outsize 2048 300')
 
     ds = gdal.GetDriverByName('COG').CreateCopy(filename, src_ds,
-                                            options = ['COMPRESS=LZW', 'OVERVIEW_COMPRESS=JPEG'])
+                                            options = ['COMPRESS=LZW', 'OVERVIEW_COMPRESS=JPEG', 'OVERVIEW_QUALITY=50'])
 
     assert ds.GetRasterBand(1).GetOverviewCount() == 2
     assert ds.GetMetadata('IMAGE_STRUCTURE')['COMPRESSION'] == 'LZW'
 
     ds_overview_a = gdal.Open('GTIFF_DIR:2:' + filename)
     assert ds_overview_a.GetMetadata('IMAGE_STRUCTURE')['COMPRESSION'] == 'JPEG'
+    assert ds_overview_a.GetMetadata('IMAGE_STRUCTURE')['JPEG_QUALITY'] == '50'
 
     ds_overview_b = gdal.Open('GTIFF_DIR:3:' + filename)
     assert ds_overview_b.GetMetadata('IMAGE_STRUCTURE')['COMPRESSION'] == 'JPEG'
+    assert ds_overview_a.GetMetadata('IMAGE_STRUCTURE')['JPEG_QUALITY'] == '50'
 
     ds_overview_a = None
     ds_overview_b = None
