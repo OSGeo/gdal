4320c64a7648709cc69efbd39af2c63ff89252e5
Even Rouault <even.rouault@spatialys.com>
2021-03-04 17:59:40 +0100

    Fix -Wodr -O2 warnings raised by gcc 10

--- gdal/frmts/tsx/tsxdataset.cpp
+++ gdal/frmts/tsx/tsxdataset.cpp
@@ -38,6 +38,7 @@
 
 CPL_CVSID("$Id$")
 
+namespace {
 enum ePolarization {
     HH=0,
     HV,
@@ -52,6 +53,7 @@ enum eProductType {
     eGEC,
     eUnknown
 };
+} // namespace
 
 /************************************************************************/
 /* Helper Functions                                                     */
