e28a878507c801c29d93c643ab6393acf5042e6e
Even Rouault <even.rouault@spatialys.com>
2021-03-07 19:29:56 +0100

    ci/travis/csa_common/script.sh: make it ready for CSA 10

--- gdal/ci/travis/csa_common/script.sh
+++ gdal/ci/travis/csa_common/script.sh
@@ -7,7 +7,8 @@ sudo apt-get install jq
 rm -f filtered_scanbuild.txt
 files=$(find gdal/scanbuildoutput -name "*.sarif")
 for f in $files; do
-    jq '.runs[].results[] | (if .locations[].physicalLocation.fileLocation.uri | (contains("/usr/include") or contains("degrib") or contains("libpng") or contains("libjpeg") or contains("EHapi") or contains("GDapi") or contains("SWapi") or contains("osr_cs_wkt_parser") or contains("ods_formula_parser") or contains("swq_parser") or contains("json_tokener") ) then empty else { "uri": .locations[].physicalLocation.fileLocation.uri, "msg": .message.text, "location": .codeFlows[-1].threadFlows[-1].locations[-1] } end)' < $f > tmp.txt
+    # CSA 10 uses artifactLocation. Earlier versions used fileLocation
+    (sed 's/fileLocation/artifactLocation/g' < $f) |jq '.runs[].results[] | (if .locations[].physicalLocation.artifactLocation.uri | (contains("/usr/include") or contains("degrib") or contains("libpng") or contains("libjpeg") or contains("EHapi") or contains("GDapi") or contains("SWapi") or contains("osr_cs_wkt_parser") or contains("ods_formula_parser") or contains("swq_parser") or contains("json_tokener") ) then empty else { "uri": .locations[].physicalLocation.artifactLocation.uri, "msg": .message.text, "location": .codeFlows[-1].threadFlows[-1].locations[-1] } end)' > tmp.txt
     if [ -s tmp.txt ]; then
         echo "Errors from $f: "
         cat $f
