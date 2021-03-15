3e991b073f113cc02fcb885ac718d53038085cba
Daniel Baston <dbaston@gmail.com>
2021-03-15 13:01:54 -0400

    OGR: Avoid SWIG generating exception on OGRGeometry::IsValid
    If a geometry has an invalidity that prevents even constructing a GEOS
    geometry to call GEOSisValid(), we should return false instead of
    throwing an exception.
    
    Fixes #3578

--- autotest/ogr/ogr_geos.py
+++ autotest/ogr/ogr_geos.py
@@ -457,6 +457,20 @@ def test_ogr_geos_isvalid_false():
 
     assert isring == 0
 
+
+###############################################################################
+
+
+def test_ogr_geos_isvalid_false_too_few_points():
+    g1 = ogr.CreateGeometryFromWkt('POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0), (2 2, 3 2, 2 2))')
+
+    with ogrtest.enable_exceptions():  # fail test if exception is thrown
+        with gdaltest.error_handler():
+            isvalid = g1.IsValid()
+
+    assert isvalid == 0
+
+
 ###############################################################################
 
 
