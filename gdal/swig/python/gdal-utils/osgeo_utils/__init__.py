d795d5e4294b93c9d4904ae37e3cd0d7166b3f59
Idan Miara <idan@miara.com>
2021-04-29 17:43:52 +0300

    `gdal-utils` version: add `gdal_utils_version` and bump `__version__`; fix typo (#3754)

--- gdal/swig/python/gdal-utils/osgeo_utils/__init__.py
+++ gdal/swig/python/gdal-utils/osgeo_utils/__init__.py
@@ -1,5 +1,6 @@
-__pacakge_name__ = 'gdal-utils'
-__version__ = '3.3.0.6'
+__package_name__ = 'gdal-utils'
+gdal_utils_version = (3, 3, 0, 7)
+__version__ = '.'.join(str(i) for i in gdal_utils_version)
 __author__ = "Frank Warmerdam"
 __author_email__ = "warmerdam@pobox.com"
 __maintainer__ = "Idan Miara"
