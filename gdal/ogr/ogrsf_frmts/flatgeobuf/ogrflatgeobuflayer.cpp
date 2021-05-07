855d0a36b8de2c7038969f8603e842b4ec51ee4e
Even Rouault <even.rouault@spatialys.com>
2021-05-07 19:05:47 +0200

    FlatGeoBuf: add write support for coordinate epoch using COORDINATEMETADATA[] construct

--- gdal/ogr/ogrsf_frmts/flatgeobuf/ogrflatgeobuflayer.cpp
+++ gdal/ogr/ogrsf_frmts/flatgeobuf/ogrflatgeobuflayer.cpp
@@ -346,12 +346,30 @@ void OGRFlatGeobufLayer::writeHeader(VSILFILE *poFp, uint64_t featuresCount, std
 
         // Translate SRS to WKT.
         char *pszWKT = nullptr;
-        const char* const apszOptionsWkt[] = { "FORMAT=WKT2_2018", nullptr };
+        const char* const apszOptionsWkt[] = { "FORMAT=WKT2_2019", nullptr };
         m_poSRS->exportToWkt( &pszWKT, apszOptionsWkt );
         if( pszWKT && pszWKT[0] == '\0' )
         {
             CPLFree(pszWKT);
             pszWKT = nullptr;
+        }
+
+        if( pszWKT && m_poSRS->GetCoordinateEpoch() > 0 )
+        {
+            std::string osCoordinateEpoch = CPLSPrintf("%f", m_poSRS->GetCoordinateEpoch());
+            if( osCoordinateEpoch.find('.') != std::string::npos )
+            {
+                while( osCoordinateEpoch.back() == '0' )
+                    osCoordinateEpoch.resize(osCoordinateEpoch.size()-1);
+            }
+
+            std::string osWKT("COORDINATEMETADATA[");
+            osWKT += pszWKT;
+            osWKT += ",EPOCH[";
+            osWKT += osCoordinateEpoch;
+            osWKT += "]]";
+            CPLFree(pszWKT);
+            pszWKT = CPLStrdup(osWKT.c_str());
         }
 
         crs = CreateCrsDirect(fbb, pszAuthorityName, nAuthorityCode, m_poSRS->GetName(), nullptr, pszWKT);
