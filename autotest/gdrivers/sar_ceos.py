a4ceea82689001ee376aaff1afbf8acdab163395
kdesjard <kdesjard@gmail.com>
2021-06-24 12:43:43 -0400

    update test to use truncated data file

--- autotest/gdrivers/sar_ceos.py
+++ autotest/gdrivers/sar_ceos.py
@@ -51,13 +51,6 @@ def test_sar_ceos_online_1():
     return tst.testOpen()
 
 def test_sar_ceos_online_2():
-    list_files = ['R1_26161_FN1_F164.D',
-                  'R1_26161_FN1_F164.L']
-
-    for filename in list_files:
-        if not gdaltest.download_file('http://download.osgeo.org/gdal/data/ceos/' + filename, filename):
-            pytest.skip()
-
-    tst = gdaltest.GDALTest('SAR_CEOS', 'tmp/cache/R1_26161_FN1_F164.D', 1, 16643, filename_absolute=1)
+    tst = gdaltest.GDALTest('SAR_CEOS', 'data/sar_ceos/R1_26161_FN1_F164.D', 1, 16643, filename_absolute=1)
     return tst.testOpen()
 
