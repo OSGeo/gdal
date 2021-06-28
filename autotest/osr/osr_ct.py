605d05406b08f3e01730ba14d3831c978f67eae8
Even Rouault <even.rouault@spatialys.com>
2021-06-28 17:03:28 +0200

    OGRCoordinateTransformation: fix transformation between CRS that only differ by axis order (related to #4038)

--- autotest/osr/osr_ct.py
+++ autotest/osr/osr_ct.py
@@ -531,3 +531,29 @@ def test_osr_ct_non_specified_time_with_time_dependent_transformation():
     x, y, _ = ct.TransformPoint(50, -40, 0)
     assert x == pytest.approx(50, abs=1e-10)
     assert y == pytest.approx(-40, abs=1e-10)
+
+###############################################################################
+# Test transformation between CRS that only differ by axis order
+
+def test_osr_ct_only_axis_order_different():
+
+    s_wrong_axis_order = osr.SpatialReference()
+    s_wrong_axis_order.SetFromUserInput("""GEOGCS["WGS 84",
+    DATUM["WGS_1984",
+        SPHEROID["WGS 84",6378137,298.257223563,
+            AUTHORITY["EPSG","7030"]],
+        AUTHORITY["EPSG","6326"]],
+    PRIMEM["Greenwich",0,
+        AUTHORITY["EPSG","8901"]],
+    UNIT["degree",0.0174532925199433,
+        AUTHORITY["EPSG","9122"]],
+    AXIS["Longitude",EAST],
+    AXIS["Latitude",NORTH]]""")
+
+    t = osr.SpatialReference()
+    t.ImportFromEPSG(4326)
+
+    ct = osr.CoordinateTransformation(s_wrong_axis_order, t)
+    x, y, _ = ct.TransformPoint(2, 49, 0)
+    assert x == 49
+    assert y == 2
