20cb378a1ac46515d1aad87e93dff401feedffa7
Even Rouault <even.rouault@spatialys.com>
2021-07-06 18:17:27 +0200

    GTiff: set color interpretation on overview bands (fixes #3939)

--- gdal/frmts/gtiff/gt_overview.cpp
+++ gdal/frmts/gtiff/gt_overview.cpp
@@ -837,6 +837,36 @@ GTIFFBuildOverviewsEx( const char * pszFilename,
         GTIFFBuildOverviewMetadata( pszResampling, poBaseDS, osMetadata );
     }
 
+    const bool bStandardColorInterp =
+        poBaseDS != nullptr &&
+        GTIFFIsStandardColorInterpretation(GDALDataset::ToHandle(poBaseDS),
+                                           static_cast<uint16_t>(nPhotometric),
+                                           nullptr);
+    if( poBaseDS != nullptr && !bStandardColorInterp )
+    {
+        if( osMetadata.size() >= strlen("</GDALMetadata>") &&
+            osMetadata.substr(osMetadata.size() - strlen("</GDALMetadata>")) == "</GDALMetadata>" )
+        {
+            osMetadata.resize(osMetadata.size() - strlen("</GDALMetadata>"));
+        }
+        else
+        {
+            CPLAssert(osMetadata.empty());
+            osMetadata = "<GDALMetadata>";
+        }
+        for( int i = 0; i < poBaseDS->GetRasterCount(); ++i )
+        {
+            const GDALColorInterp eInterp =
+                poBaseDS->GetRasterBand(i + 1)->GetColorInterpretation();
+            osMetadata += CPLSPrintf(
+                "<Item sample=\"%d\" name=\"COLORINTERP\" role=\"colorinterp\">",
+                i);
+            osMetadata += GDALGetColorInterpretationName(eInterp);
+            osMetadata += "</Item>";
+        }
+        osMetadata += "</GDALMetadata>";
+    }
+
 /* -------------------------------------------------------------------- */
 /*      Loop, creating overviews.                                       */
 /* -------------------------------------------------------------------- */
