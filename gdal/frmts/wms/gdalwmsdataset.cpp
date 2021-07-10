f532901999d53b57441536dc187bbe276902b811
Even Rouault <even.rouault@spatialys.com>
2021-07-10 13:26:51 +0200

    Update gdal/frmts/wms/gdalwmsdataset.cpp

--- gdal/frmts/wms/gdalwmsdataset.cpp
+++ gdal/frmts/wms/gdalwmsdataset.cpp
@@ -247,7 +247,7 @@ CPLErr GDALWMSDataset::Initialize(CPLXMLNode *config, char **l_papszOpenOptions)
     if (ret == CE_None) {
         const char *pszEnableCache = CPLGetConfigOption("GDAL_ENABLE_WMS_CACHE", "YES");
         CPLXMLNode *cache_node = CPLGetXMLNode(config, "Cache");
-        if (cache_node != nullptr && !EQUAL(pszEnableCache, "NO")) {
+        if (cache_node != nullptr && CPLTestBool(pszEnableCache)) {
             m_cache = new GDALWMSCache();
             if (m_cache->Initialize(CPLGetXMLValue(service_node, "ServerUrl", nullptr),
                                     cache_node) != CE_None) {
