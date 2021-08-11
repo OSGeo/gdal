b54dc2ef8307f546195cc76d8c35276cf6de14a0
Even Rouault <even.rouault@spatialys.com>
2021-08-11 11:43:03 +0200

    autotest/alg/fillnodata.py: test smoothing filter

--- autotest/alg/fillnodata.py
+++ autotest/alg/fillnodata.py
@@ -48,33 +48,88 @@ def test_fillnodata_1x1_no_nodata():
 
 
 fillnodata_tests = {
-    # (width, height, input_ar, maxSearchDist, expected):
-    '1x1_nodata_but_pixel_valid': (1, 1, (1,), 1, (1,)),
-    '1x1_nodata_pixel_invalid': (1, 1, (0,), 1, (0,)),
-    '2x1_valid_invalid': (2, 1, (1, 0), 1, (1, 1)),
-    '2x1_invalid_valid': (2, 1, (0, 1), 1, (1, 1)),
-    '3x1_valid_invalid_valid': (3, 1, (2, 0, 4), 1, (2, 3, 4)),
-    '4x1_valid_invalid_invalid_valid': (4, 1, (2, 0, 0, 4), 1, (2, 2, 4, 4)),
-    '1x2_valid_invalid': (1, 2, (1, 0), 1, (1, 1)),
-    '1x2_invalid_valid': (1, 2, (0, 1), 1, (1, 1)),
-    '1x3_valid_invalid_valid': (1, 3, (2, 0, 4), 1, (2, 3, 4)),
-    '1x4_valid_invalid_invalid_valid': (1, 4, (2, 0, 0, 4), 1, (2, 2, 4, 4)),
+    # (width, height, input_ar, maxSearchDist, expected, smoothingIterations):
+    '1x1_nodata_but_pixel_valid': (1, 1, (1,), 1, (1,), 0),
+    '1x1_nodata_pixel_invalid': (1, 1, (0,), 1, (0,), 0),
+    '2x1_valid_invalid': (2, 1, (1, 0), 1, (1, 1), 0),
+    '2x1_invalid_valid': (2, 1, (0, 1), 1, (1, 1), 0),
+    '3x1_valid_invalid_valid': (3, 1, (2, 0, 4), 1, (2, 3, 4), 0),
+    '4x1_valid_invalid_invalid_valid': (4, 1, (2, 0, 0, 4), 1, (2, 2, 4, 4), 0),
+    '1x2_valid_invalid': (1, 2, (1, 0), 1, (1, 1), 0),
+    '1x2_invalid_valid': (1, 2, (0, 1), 1, (1, 1), 0),
+    '1x3_valid_invalid_valid': (1, 3, (2, 0, 4), 1, (2, 3, 4), 0),
+    '1x4_valid_invalid_invalid_valid': (1, 4, (2, 0, 0, 4), 1, (2, 2, 4, 4), 0),
     '3x3_central_column_invalid': (
-        3, 3, (2, 0, 4, 4, 0, 6, 6, 0, 8), 1, (2, 3, 4, 4, 5, 6, 6, 7, 8)),
+        3, 3,
+        (2, 0, 4,
+         4, 0, 6,
+         6, 0, 8),
+        1,
+        (2, 3, 4,
+         4, 5, 6,
+         6, 7, 8), 0),
     '3x3_central_line_invalid': (
-        3, 3, (2, 3, 4, 0, 0, 0, 6, 7, 8), 1, (2, 3, 4, 4, 5, 6, 6, 7, 8)),
+        3, 3,
+        (2, 3, 4,
+         0, 0, 0,
+         6, 7, 8),
+        1,
+        (2, 3, 4,
+         4, 5, 6,
+         6, 7, 8),
+        0),
     '3x3_central_column_and_line_invalid': (
-        3, 3, (2, 0, 4, 0, 0, 0, 6, 0, 8), 1, (2, 3, 4, 4, 0, 6, 6, 7, 8)),
+        3, 3,
+        (2, 0, 4,
+         0, 0, 0,
+         6, 0, 8),
+        1,
+        (2, 3, 4,
+         4, 0, 6,
+         6, 7, 8),
+        0),
     # 1.5 > sqrt(2)
     '3x3_central_column_and_line_invalid_search_dist_1_5': (
-        3, 3, (2, 0, 4, 0, 0, 0, 6, 0, 8), 1.5, (2, 3, 4, 4, 5, 6, 6, 7, 8)),
+        3, 3,
+        (2, 0, 4,
+         0, 0, 0,
+         6, 0, 8),
+        1.5,
+        (2, 3, 4,
+         4, 5, 6,
+         6, 7, 8),
+        0),
+    '4x4': (
+        4, 4,
+        (20, 30, 40, 50,
+         30, 0,  0,  60,
+         40, 0,  0,  70,
+         50, 60, 70, 80),
+        1,
+        (20, 30, 40, 50,
+         30, 30, 50,  60,
+         40, 50, 70,  70,
+         50, 60, 70, 80),
+        0),
+    '4x4_smooth_1': (
+        4, 4,
+        (20, 30, 40, 50,
+         30, 0,  0,  60,
+         40, 0,  0,  70,
+         50, 60, 70, 80),
+        1,
+        (20, 30, 40, 50,
+         30, 40, 50, 60,
+         40, 50, 60, 70,
+         50, 60, 70, 80),
+        1),
 }
 
 
-@pytest.mark.parametrize('width, height, input_ar, maxSearchDist, expected',
+@pytest.mark.parametrize('width, height, input_ar, maxSearchDist, expected, smoothingIterations',
                          fillnodata_tests.values(),
                          ids=fillnodata_tests.keys())
-def test_fillnodata_nodata(width, height, input_ar, maxSearchDist, expected):
+def test_fillnodata_nodata(width, height, input_ar, maxSearchDist, expected, smoothingIterations):
     ds = gdal.GetDriverByName('MEM').Create('', width, height)
     npixels = ds.RasterXSize * ds.RasterYSize
     ds.GetRasterBand(1).SetNoDataValue(0)
@@ -84,6 +139,7 @@ def test_fillnodata_nodata(width, height, input_ar, maxSearchDist, expected):
     ds.WriteRaster(0, 0, ds.RasterXSize, ds.RasterYSize, ar)
     gdal.FillNodata(targetBand = ds.GetRasterBand(1),
                     maxSearchDist = maxSearchDist,
-                    maskBand = None, smoothingIterations = 0)
+                    maskBand = None,
+                    smoothingIterations = smoothingIterations)
     ar = ds.ReadRaster()
     assert struct.unpack('B' * npixels, ar) == expected
