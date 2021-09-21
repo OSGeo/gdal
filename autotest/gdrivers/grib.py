d2506c66539c02763c6556d18ac656204f88566d
Momtchil Momtchev <momtchil@momtchev.com>
2021-09-21 12:32:49 +0200

    GRIBv2 SplitAndSwap for transparent 0-360 to -180-180 translation
    Refs: For GRIB2, generate an OGRSpatialReference with proj4 +lon_wrap #4524

--- autotest/gdrivers/grib.py
+++ autotest/gdrivers/grib.py
@@ -1420,3 +1420,16 @@ def test_grib_grib2_read_subgrids_reuse_bitmap():
     ds = gdal.Open('data/grib/subgrids_reuse_bitmap.grib2')
     assert ds.GetRasterBand(1).Checksum() == 4672
     assert ds.GetRasterBand(2).Checksum() == 4563
+
+
+###############################################################################
+# Test reading GRIBv2 with 0-360 longitudes
+# Fixes https://github.com/OSGeo/gdal/issues/4524
+
+def test_grib_grib2_split_and_swap():
+
+    ds = gdal.Open('data/grib/gfs.t06z.pgrb2.1p0.grib2')
+    gt = ds.GetGeoTransform()
+    expected_gt = (-185.0, 10.0, 0.0, 90.125, 0.0, -10.0)
+    assert gt == pytest.approx(expected_gt, rel=1e-6)
+    assert ds.GetRasterBand(1).Checksum() == 7514
