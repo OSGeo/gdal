86edd3af84864fd64ed5b8b69d153feb92d3a3a5
Even Rouault <even.rouault@spatialys.com>
2021-10-09 14:30:03 +0200

    /vsitar/: support prefixed filename of ustar .tar archives (fixes #4625)

--- autotest/gcore/vsifile.py
+++ autotest/gcore/vsifile.py
@@ -968,13 +968,21 @@ def test_vsifile_opendir(basepath):
 # Test bugfix for https://github.com/OSGeo/gdal/issues/1559
 
 
-def test_vsitar_verylongfilename():
+def test_vsitar_verylongfilename_posix():
 
     f = gdal.VSIFOpenL('/vsitar/data/verylongfilename.tar/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb/ccccccccccccccccccccccccccccccccccc/ddddddddddddddddddddddddddddddddddddddd/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee/fffffffffffffffffffffffffffffffffffffffffffffff/foo', 'rb')
     assert f
     data = gdal.VSIFReadL(1, 3, f).decode('ascii')
     gdal.VSIFCloseL(f)
     assert data == 'bar'
+
+###############################################################################
+# Test bugfix for https://github.com/OSGeo/gdal/issues/4625
+
+
+def test_vsitar_longfilename_ustar():
+
+    assert gdal.VSIStatL('/vsitar/data/longfilename_ustar.tar/zzzzzzzzzzzzzzzzzzzzzzzzzzzzzz/bbbbbbbbbbbbbbbbbbbbbbb/ccccccccccccccccccccccccc/dddddddddd/e/byte.tif') is not None
 
 
 def test_unlink_batch():
