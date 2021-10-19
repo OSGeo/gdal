OSGeo/gdal

commit 20cb378a1ac46515d1aad87e93dff401feedffa7

GTiff: set color interpretation on overview bands (fixes #3939)

gdal/frmts/gtiff/gt_overview.cpp
@@ -837,6 +837,36 @@ GTIFFBuildOverviewsEx( const char * pszFilename,
         GTIFFBuildOverviewMetadata( pszResampling, poBaseDS, osMetadata );
     }
 
+    const bool bStandardColorInterp =
+        poBaseDS != nullptr &&
+        GTIFFIsStandardColorInterpretation(GDALDataset::ToHandle(poBaseDS),
+                                           static_cast<uint16_t>(nPhotometric),
+                                           nullptr);
+    if( poBaseDS != nullptr && !bStandardColorInterp )
+    {
+        if( osMetadata.size() >= strlen("</GDALMetadata>") &&
+            osMetadata.substr(osMetadata.size() - strlen("</GDALMetadata>")) == "</GDALMetadata>" )
+        {
+            osMetadata.resize(osMetadata.size() - strlen("</GDALMetadata>"));
+        }
+        else
+        {
+            CPLAssert(osMetadata.empty());
+            osMetadata = "<GDALMetadata>";
+        }
+        for( int i = 0; i < poBaseDS->GetRasterCount(); ++i )
+        {
+            const GDALColorInterp eInterp =
+                poBaseDS->GetRasterBand(i + 1)->GetColorInterpretation();
+            osMetadata += CPLSPrintf(
+                "<Item sample=\"%d\" name=\"COLORINTERP\" role=\"colorinterp\">",
+                i);
+            osMetadata += GDALGetColorInterpretationName(eInterp);
+            osMetadata += "</Item>";
+        }
+        osMetadata += "</GDALMetadata>";
+    }
+
 /* -------------------------------------------------------------------- */
 /*      Loop, creating overviews.                                       */
 /* -------------------------------------------------------------------- */

autotest/gcore/tiff_ovr.py
@@ -34,6 +34,7 @@ import os
 import shutil
 import array
 import stat
+import struct
 from osgeo import osr
 from osgeo import gdal
 
@@ -1936,7 +1937,7 @@ def test_tiff_ovr_color_table_bug_3336_bis():
 def test_tiff_ovr_nodata_multiband():
 
     numpy = pytest.importorskip('numpy')
-    
+
     temp_path = '/vsimem/test.tif'
     ds = gdal.GetDriverByName('GTiff').Create(temp_path, 4, 4, 2, gdal.GDT_Float32)
     ds.GetRasterBand(1).SetNoDataValue(-10000)
@@ -1951,14 +1952,64 @@ def test_tiff_ovr_nodata_multiband():
     assert ds.GetRasterBand(1).GetOverviewCount() == 1, \
         'Overview could not be generated'
 
-    pix = ds.GetRasterBand(1).GetOverview(0).ReadAsArray(win_xsize=1, win_ysize=1) 
+    pix = ds.GetRasterBand(1).GetOverview(0).ReadAsArray(win_xsize=1, win_ysize=1)
     assert pix[0,0] == 2.0
 
-    pix = ds.GetRasterBand(2).GetOverview(0).ReadAsArray(win_xsize=1, win_ysize=1) 
+    pix = ds.GetRasterBand(2).GetOverview(0).ReadAsArray(win_xsize=1, win_ysize=1)
     assert pix[0,0] == 3.0
 
     ds = None
 
+###############################################################################
+
+@pytest.mark.parametrize("external_ovr", [False,True])
+def test_tiff_ovr_nodata_multiband_interleave_band_non_default_color_interp(external_ovr):
+
+    nodatavalue = -10000
+    data = struct.pack('f' * 4 * 4,
+        0.5, 0.2, 0.5, 0.2,
+        0.2, nodatavalue, 0.2, nodatavalue,
+        0.5, 0.2, nodatavalue, nodatavalue,
+        0.2, nodatavalue, nodatavalue, nodatavalue)
+    numbands = 5
+
+    temp_path = '/vsimem/test.tif'
+    ds = gdal.GetDriverByName('GTiff').Create(
+        temp_path, 4, 4, numbands, gdal.GDT_Float32,
+        options=['INTERLEAVE=BAND', 'PHOTOMETRIC=MINISBLACK', 'ALPHA=YES'])
+    for i in range(1, numbands):
+        ds.GetRasterBand(i).SetColorInterpretation(gdal.GCI_GreenBand)
+        ds.GetRasterBand(i).SetNoDataValue(nodatavalue)
+        ds.GetRasterBand(i).WriteRaster(0, 0, 4, 4, data)
+
+    ds.GetRasterBand(numbands).SetColorInterpretation(gdal.GCI_AlphaBand)
+    ds.GetRasterBand(numbands).SetNoDataValue(nodatavalue)
+    ds.GetRasterBand(numbands).WriteRaster(0, 0, 4, 4, struct.pack('f' * 4 * 4,
+        255, 255, 255, 255,
+        255, 255, 255, 255,
+        255, 0, 0, 0,
+        0, 0, 0, 0))
+
+    if external_ovr:
+        ds = None
+        ds = gdal.Open(temp_path)
+    ds.BuildOverviews('AVERAGE', overviewlist=[2, 4])
+    assert ds.GetRasterBand(1).GetOverview(0).GetColorInterpretation() == gdal.GCI_GreenBand
+
+    ds = None
+    ds = gdal.Open(temp_path)
+    assert ds.GetRasterBand(1).GetOverviewCount() == 2, 'Overview could not be generated'
+    assert ds.GetRasterBand(1).GetOverview(0).GetColorInterpretation() == gdal.GCI_GreenBand
+
+    for i in range(1, numbands):
+        pix = struct.unpack('f', ds.GetRasterBand(i).GetOverview(1).ReadRaster(0,0,1,1))[0]
+        assert abs(pix - 0.3) < 0.01, 'Error in band ' + str(i)
+
+    pix = struct.unpack('f', ds.GetRasterBand(numbands).GetOverview(1).ReadRaster(0,0,1,1))[0]
+    assert pix == 255, 'Error in alpha band '
+    ds = None
+
+    gdal.GetDriverByName('GTiff').Delete(temp_path)
 
 ###############################################################################
 # Cleanup

gdal/frmts/gtiff/geotiff.cpp
@@ -10024,6 +10024,15 @@ CPLErr GTiffDataset::RegisterNewOverviewDataset(toff_t nOverviewOffset,
         return CE_Failure;
     }
 
+    // Assign color interpretation from main dataset
+    const int l_nBands = GetRasterCount();
+    for(int i = 1; i <= l_nBands; i++ )
+    {
+        auto poBand = dynamic_cast<GTiffRasterBand*>(poODS->GetRasterBand(i));
+        if( poBand )
+            poBand->m_eBandInterp = GetRasterBand(i)->GetColorInterpretation();
+    }
+
     // Do that now that m_nCompression is set
     poODS->RestoreVolatileParameters( poODS->m_hTIFF );
 
@@ -11436,13 +11445,14 @@ void GTiffDataset::WriteRPC( GDALDataset *poSrcDS, TIFF *l_hTIFF,
 }
 
 /************************************************************************/
-/*                  IsStandardColorInterpretation()                     */
+/*                 GTIFFIsStandardColorInterpretation()                 */
 /************************************************************************/
 
-static bool IsStandardColorInterpretation(GDALDataset* poSrcDS,
+bool GTIFFIsStandardColorInterpretation(GDALDatasetH hSrcDS,
                                           uint16_t nPhotometric,
-                                          char** papszCreationOptions)
+                                          CSLConstList papszCreationOptions)
 {
+    GDALDataset* poSrcDS = GDALDataset::FromHandle(hSrcDS);
     bool bStandardColorInterp = true;
     if( nPhotometric == PHOTOMETRIC_MINISBLACK )
     {
@@ -11566,8 +11576,8 @@ bool GTiffDataset::WriteMetadata( GDALDataset *poSrcDS, TIFF *l_hTIFF,
         nPhotometric = PHOTOMETRIC_MINISBLACK;
 
     const bool bStandardColorInterp =
-        IsStandardColorInterpretation(poSrcDS, nPhotometric,
-                                      l_papszCreationOptions);
+        GTIFFIsStandardColorInterpretation(
+            GDALDataset::ToHandle(poSrcDS), nPhotometric, l_papszCreationOptions);
 
 /* -------------------------------------------------------------------- */
 /*      We also need to address band specific metadata, and special     */
@@ -11782,7 +11792,8 @@ void GTiffDataset::PushMetadataToPam()
         return;
 
     const bool bStandardColorInterp =
-        IsStandardColorInterpretation(this, m_nPhotometric, m_papszCreationOptions);
+        GTIFFIsStandardColorInterpretation(GDALDataset::ToHandle(this),
+                                           m_nPhotometric, m_papszCreationOptions);
 
     for( int nBand = 0; nBand <= GetRasterCount(); ++nBand )
     {
@@ -15307,6 +15318,19 @@ void GTiffDataset::ScanDirectories()
                     cpl::down_cast<GTiffDataset*>(GDALDataset::FromHandle(
                         m_papoOverviewDS[i]))->m_poMaskDS;
             }
+        }
+    }
+
+    // Assign color interpretation from main dataset
+    const int l_nBands = GetRasterCount();
+    for( int iOvr = 0; iOvr < m_nOverviewCount; ++iOvr )
+    {
+        for(int i = 1; i <= l_nBands; i++ )
+        {
+            auto poBand = dynamic_cast<GTiffRasterBand*>(
+                                    m_papoOverviewDS[iOvr]->GetRasterBand(i));
+            if( poBand )
+                poBand->m_eBandInterp = GetRasterBand(i)->GetColorInterpretation();
         }
     }
 

gdal/frmts/gtiff/gtiff.h
@@ -68,6 +68,10 @@ constexpr uint16_t DEFAULT_ALPHA_TYPE = EXTRASAMPLE_UNASSALPHA;
 
 uint16_t GTiffGetAlphaValue(const char* pszValue, uint16_t nDefault);
 
+bool GTIFFIsStandardColorInterpretation(GDALDatasetH hSrcDS,
+                                        uint16_t nPhotometric,
+                                        CSLConstList papszCreationOptions);
+
 CPLString CPL_DLL GTiffGetCompressValues(bool& bHasLZW,
                                  bool& bHasDEFLATE,
                                  bool& bHasLZMA,

commit d588fcb910cbd2a5138fc7ba1a79452780133498

COG/GTiff: fix error messages when creating JPEG compressed COGs from RGBA (fixes regression introduced by 20cb378a1ac46515d1aad87e93dff401feedffa7, refs #3939)

autotest/gcore/cog.py
@@ -220,7 +220,8 @@ def test_cog_creation_options():
             assert filesize_lerc_zstd_level_1 > filesize_lerc_zstd
 
     src_ds = None
-    gdal.GetDriverByName('GTiff').Delete(filename)
+    with gdaltest.error_handler():
+        gdal.GetDriverByName('GTiff').Delete(filename)
 
 
 ###############################################################################
@@ -813,6 +814,7 @@ def test_cog_sparse_mask():
         src_ds.GetRasterBand(i+1).WriteRaster(256, 256, 128, 128, '\x00' * 128 * 128)
     src_ds.BuildOverviews('NEAREST', [2])
     gdal.GetDriverByName('COG').CreateCopy(filename, src_ds, options = ['BLOCKSIZE=128', 'SPARSE_OK=YES', 'COMPRESS=JPEG', 'RESAMPLING=NEAREST'])
+    assert gdal.GetLastErrorMsg() == ''
     _check_cog(filename)
     with gdaltest.config_option('GTIFF_HAS_OPTIMIZED_READ_MULTI_RANGE', 'YES'):
         ds = gdal.Open(filename)

gdal/frmts/gtiff/gt_overview.cpp
@@ -837,34 +837,36 @@ GTIFFBuildOverviewsEx( const char * pszFilename,
         GTIFFBuildOverviewMetadata( pszResampling, poBaseDS, osMetadata );
     }
 
-    const bool bStandardColorInterp =
-        poBaseDS != nullptr &&
-        GTIFFIsStandardColorInterpretation(GDALDataset::ToHandle(poBaseDS),
-                                           static_cast<uint16_t>(nPhotometric),
-                                           nullptr);
-    if( poBaseDS != nullptr && !bStandardColorInterp )
+    if( poBaseDS != nullptr && poBaseDS->GetRasterCount() == nBands )
     {
-        if( osMetadata.size() >= strlen("</GDALMetadata>") &&
-            osMetadata.substr(osMetadata.size() - strlen("</GDALMetadata>")) == "</GDALMetadata>" )
+        const bool bStandardColorInterp =
+            GTIFFIsStandardColorInterpretation(GDALDataset::ToHandle(poBaseDS),
+                                               static_cast<uint16_t>(nPhotometric),
+                                               nullptr);
+        if( !bStandardColorInterp )
         {
-            osMetadata.resize(osMetadata.size() - strlen("</GDALMetadata>"));
+            if( osMetadata.size() >= strlen("</GDALMetadata>") &&
+                osMetadata.substr(osMetadata.size() - strlen("</GDALMetadata>")) == "</GDALMetadata>" )
+            {
+                osMetadata.resize(osMetadata.size() - strlen("</GDALMetadata>"));
+            }
+            else
+            {
+                CPLAssert(osMetadata.empty());
+                osMetadata = "<GDALMetadata>";
+            }
+            for( int i = 0; i < poBaseDS->GetRasterCount(); ++i )
+            {
+                const GDALColorInterp eInterp =
+                    poBaseDS->GetRasterBand(i + 1)->GetColorInterpretation();
+                osMetadata += CPLSPrintf(
+                    "<Item sample=\"%d\" name=\"COLORINTERP\" role=\"colorinterp\">",
+                    i);
+                osMetadata += GDALGetColorInterpretationName(eInterp);
+                osMetadata += "</Item>";
+            }
+            osMetadata += "</GDALMetadata>";
         }
-        else
-        {
-            CPLAssert(osMetadata.empty());
-            osMetadata = "<GDALMetadata>";
-        }
-        for( int i = 0; i < poBaseDS->GetRasterCount(); ++i )
-        {
-            const GDALColorInterp eInterp =
-                poBaseDS->GetRasterBand(i + 1)->GetColorInterpretation();
-            osMetadata += CPLSPrintf(
-                "<Item sample=\"%d\" name=\"COLORINTERP\" role=\"colorinterp\">",
-                i);
-            osMetadata += GDALGetColorInterpretationName(eInterp);
-            osMetadata += "</Item>";
-        }
-        osMetadata += "</GDALMetadata>";
     }
 
 /* -------------------------------------------------------------------- */