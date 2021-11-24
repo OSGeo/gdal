eea98ee906563165be648e792285bfde90736e98
Even Rouault <even.rouault@spatialys.com>
2021-11-24 13:47:55 +0100

    CI Conda: adapt to https://github.com/conda-forge/gdal-feedstock/commit/9570a0ee7139e292e621a5c286cc405629c8d8fe

--- ci/travis/conda/compile.sh
+++ ci/travis/conda/compile.sh
@@ -18,7 +18,7 @@ fi
 
 
 export GDAL_ENABLE_DEPRECATED_DRIVER_DODS=YES
-conda build recipe --clobber-file recipe/recipe_clobber.yaml --output-folder packages -m ".ci_support/${CI_PLAT}_64_.yaml"
+conda build recipe --clobber-file recipe/recipe_clobber.yaml --output-folder packages -m ".ci_support/${CI_PLAT}_64_openssl3.yaml"
 conda create -y -n test -c ./packages python=3.8 libgdal gdal
 conda deactivate
 conda activate test
