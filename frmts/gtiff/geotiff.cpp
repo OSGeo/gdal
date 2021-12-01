937f1ef9ff7cd5100b824c6e25f6fba1e09ac692
Even Rouault <even.rouault@spatialys.com>
2021-12-01 14:53:07 +0100

    GTiff: fix performance issue when reading transfer functions (fixes #4923)

--- frmts/gtiff/geotiff.cpp
+++ frmts/gtiff/geotiff.cpp
@@ -13265,12 +13265,9 @@ static CPLString ConvertTransferFunctionToString( const uint16_t *pTable,
 
     for( uint32_t i = 0; i < nTableEntries; ++i )
     {
-        if( i == 0 )
-            sValue = sValue.Printf("%d", static_cast<uint32_t>(pTable[i]));
-        else
-            sValue = sValue.Printf( "%s, %d",
-                                    sValue.c_str(),
-                                    static_cast<uint32_t>(pTable[i]));
+        if( i > 0 )
+            sValue += ", ";
+        sValue += CPLSPrintf("%d", static_cast<uint32_t>(pTable[i]));
     }
 
     return sValue;
