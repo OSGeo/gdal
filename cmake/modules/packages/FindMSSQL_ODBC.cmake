c643aa67c0ed836936add332346598021ae6865c
Even Rouault <even.rouault@spatialys.com>
2021-12-11 11:04:52 +0100

    FindMSSQL_ODBC.cmake: add Linux support

--- cmake/modules/packages/FindMSSQL_ODBC.cmake
+++ cmake/modules/packages/FindMSSQL_ODBC.cmake
@@ -32,38 +32,62 @@ This module defines the following variables:
 
 #]=======================================================================]
 
-if(NOT WIN32)
-    return()
-endif()
+if(WIN32)
+    if(NOT DEFINED MSSQL_ODBC_VERSION)
+        set(MSSQL_ODBC_VERSION_CANDIDATES 17 13)
+        foreach(_vers IN LISTS MSSQL_ODBC_VERSION_CANDIDATES)
+          set(_dir "C:/Program Files/Microsoft SQL Server/Client SDK/ODBC/${_vers}0/SDK")
+          if(EXISTS "${_dir}")
+            set(MSSQL_ODBC_VERSION "${_vers}")
+            break()
+          endif()
+        endforeach()
+    endif()
 
-if(NOT DEFINED MSSQL_ODBC_VERSION)
-    set(MSSQL_ODBC_VERSION_CANDIDATES 17 13)
-    foreach(_vers IN LISTS MSSQL_ODBC_VERSION_CANDIDATES)
-      set(_dir "C:/Program Files/Microsoft SQL Server/Client SDK/ODBC/${_vers}0/SDK")
-      if(EXISTS "${_dir}")
-        set(MSSQL_ODBC_VERSION "${_vers}")
-        break()
-      endif()
-    endforeach()
-endif()
+    if(NOT DEFINED MSSQL_ODBC_ROOT)
+        set(MSSQL_ODBC_ROOT "C:/Program Files/Microsoft SQL Server/Client SDK/ODBC/${MSSQL_ODBC_VERSION}0/SDK")
+    endif()
 
-if(NOT DEFINED MSSQL_ODBC_ROOT)
-    set(MSSQL_ODBC_ROOT "C:/Program Files/Microsoft SQL Server/Client SDK/ODBC/${MSSQL_ODBC_VERSION}0/SDK")
-endif()
+    find_path(MSSQL_ODBC_INCLUDE_DIR NAMES msodbcsql.h
+              PATHS "${MSSQL_ODBC_ROOT}/Include")
+    mark_as_advanced(MSSQL_ODBC_INCLUDE_DIR)
 
-find_path(MSSQL_ODBC_INCLUDE_DIR NAMES msodbcsql.h
-          PATHS "${MSSQL_ODBC_ROOT}/Include")
-mark_as_advanced(MSSQL_ODBC_INCLUDE_DIR)
+    if("${CMAKE_SYSTEM_PROCESSOR}" MATCHES "AMD64")
+        set(MSSQL_ODBC_DIR_ARCH x64)
+    else()
+        set(MSSQL_ODBC_DIR_ARCH x86)
+    endif()
 
-if("${CMAKE_SYSTEM_PROCESSOR}" MATCHES "AMD64")
-    set(MSSQL_ODBC_DIR_ARCH x64)
+    find_library(MSSQL_ODBC_LIBRARY NAMES "msodbcsql${MSSQL_ODBC_VERSION}.lib"
+                 PATHS "${MSSQL_ODBC_ROOT}/Lib/${MSSQL_ODBC_DIR_ARCH}")
+    mark_as_advanced(MSSQL_ODBC_LIBRARY)
 else()
-    set(MSSQL_ODBC_DIR_ARCH x86)
-endif()
+    if(NOT DEFINED MSSQL_ODBC_VERSION)
+        set(MSSQL_ODBC_VERSION_CANDIDATES 17 13)
+        foreach(_vers IN LISTS MSSQL_ODBC_VERSION_CANDIDATES)
+          set(_dir "/opt/microsoft/msodbcsql${_vers}")
+          if(EXISTS "${_dir}")
+            set(MSSQL_ODBC_VERSION "${_vers}")
+            break()
+          endif()
+        endforeach()
+    endif()
 
-find_library(MSSQL_ODBC_LIBRARY NAMES "msodbcsql${MSSQL_ODBC_VERSION}.lib"
-             PATHS "${MSSQL_ODBC_ROOT}/Lib/${MSSQL_ODBC_DIR_ARCH}")
-mark_as_advanced(MSSQL_ODBC_LIBRARY)
+    if(NOT DEFINED MSSQL_ODBC_ROOT)
+        set(MSSQL_ODBC_ROOT "/opt/microsoft/msodbcsql${MSSQL_ODBC_VERSION}")
+    endif()
+
+    find_path(MSSQL_ODBC_INCLUDE_DIR NAMES msodbcsql.h
+              PATHS "${MSSQL_ODBC_ROOT}/include")
+    mark_as_advanced(MSSQL_ODBC_INCLUDE_DIR)
+
+    file(GLOB _libs "${MSSQL_ODBC_ROOT}/lib*/libmsodbc*")
+    list(LENGTH _libs _libs_length)
+    if(_libs_length EQUAL "1")
+        set(MSSQL_ODBC_LIBRARY ${_libs})
+    endif()
+    mark_as_advanced(MSSQL_ODBC_LIBRARY)
+endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MSSQL_ODBC
