fdf2fbcf6ad8e519ab0e88ccaaaf77c34164529f
Even Rouault <even.rouault@spatialys.com>
2021-12-17 23:48:49 +0100

    CMake FindTEIGHA.cmake: update for ODA >= 2021 on Windows

--- cmake/modules/packages/FindTEIGHA.cmake
+++ cmake/modules/packages/FindTEIGHA.cmake
@@ -72,71 +72,124 @@ endif()
 set(TEIGHA_LIBRARIES)
 
 if(EXISTS "${TEIGHA_ROOT}/bin/${TEIGHA_PLATFORM}/TG_Db.tx")
-# Linux shared libs
-list(APPEND TEIGHA_LIBRARIES_NAMES
-    "bin/${TEIGHA_PLATFORM}/TG_Db.tx"
-    "bin/${TEIGHA_PLATFORM}/TD_DbEntities.tx"
-    "lib/${TEIGHA_PLATFORM}/libTD_DrawingsExamplesCommon.a"
-    "lib/${TEIGHA_PLATFORM}/libTG_ExamplesCommon.a"
-    "lib/${TEIGHA_PLATFORM}/libTD_ExamplesCommon.a"
-    "lib/${TEIGHA_PLATFORM}/libTD_Key.a"
-    "bin/${TEIGHA_PLATFORM}/libTD_Db.so"
-    "bin/${TEIGHA_PLATFORM}/libTD_DbCore.so"
-    "bin/${TEIGHA_PLATFORM}/libTD_DbRoot.so"
-    "bin/${TEIGHA_PLATFORM}/libTD_Gi.so"
-    "bin/${TEIGHA_PLATFORM}/libTD_SpatialIndex.so"
-    "bin/${TEIGHA_PLATFORM}/libTD_Root.so"
-    "bin/${TEIGHA_PLATFORM}/libTD_Ge.so"
-    "bin/${TEIGHA_PLATFORM}/libTD_Zlib.so"
-    "bin/${TEIGHA_PLATFORM}/libTD_Alloc.so"
-    "bin/${TEIGHA_PLATFORM}/liboless.so"
-    "bin/${TEIGHA_PLATFORM}/liblibcrypto.so"
-    )
+    # Linux shared libs
+    list(APPEND TEIGHA_LIBRARIES_NAMES
+        "bin/${TEIGHA_PLATFORM}/TG_Db.tx"
+        "bin/${TEIGHA_PLATFORM}/TD_DbEntities.tx"
+        "lib/${TEIGHA_PLATFORM}/libTD_DrawingsExamplesCommon.a"
+        "lib/${TEIGHA_PLATFORM}/libTG_ExamplesCommon.a"
+        "lib/${TEIGHA_PLATFORM}/libTD_ExamplesCommon.a"
+        "lib/${TEIGHA_PLATFORM}/libTD_Key.a"
+        "bin/${TEIGHA_PLATFORM}/libTD_Db.so"
+        "bin/${TEIGHA_PLATFORM}/libTD_DbCore.so"
+        "bin/${TEIGHA_PLATFORM}/libTD_DbRoot.so"
+        "bin/${TEIGHA_PLATFORM}/libTD_Gi.so"
+        "bin/${TEIGHA_PLATFORM}/libTD_SpatialIndex.so"
+        "bin/${TEIGHA_PLATFORM}/libTD_Root.so"
+        "bin/${TEIGHA_PLATFORM}/libTD_Ge.so"
+        "bin/${TEIGHA_PLATFORM}/libTD_Zlib.so"
+        "bin/${TEIGHA_PLATFORM}/libTD_Alloc.so"
+        "bin/${TEIGHA_PLATFORM}/liboless.so"
+        "bin/${TEIGHA_PLATFORM}/liblibcrypto.so"
+        )
 elseif (EXISTS "${TEIGHA_ROOT}/lib/${TEIGHA_PLATFORM}/libTG_Db.a")
-# Linux static libs
-list(APPEND TEIGHA_LIBRARIES_NAMES
-    "lib/${TEIGHA_PLATFORM}/libTD_Db.a"
-    "lib/${TEIGHA_PLATFORM}/libTD_DbCore.a"
-    "lib/${TEIGHA_PLATFORM}/libTD_DbEntities.a"
-    "lib/${TEIGHA_PLATFORM}/libTD_DrawingsExamplesCommon.a"
-    "lib/${TEIGHA_PLATFORM}/libTG_Db.a"
-    "lib/${TEIGHA_PLATFORM}/libTG_ExamplesCommon.a"
-    "lib/${TEIGHA_PLATFORM}/libTD_Alloc.a"
-    "lib/${TEIGHA_PLATFORM}/libTD_DbRoot.a"
-    "lib/${TEIGHA_PLATFORM}/libTD_ExamplesCommon.a"
-    "lib/${TEIGHA_PLATFORM}/libWinBitmap.a"
-    "lib/${TEIGHA_PLATFORM}/libTD_Gi.a"
-    "lib/${TEIGHA_PLATFORM}/libtinyxml.a"
-    "lib/${TEIGHA_PLATFORM}/libTD_Zlib.a"
-    "lib/${TEIGHA_PLATFORM}/libTD_SpatialIndex.a"
-    "lib/${TEIGHA_PLATFORM}/libTD_DgnImport.a"
-    "lib/${TEIGHA_PLATFORM}/libTD_DgnUnderlay.a"
-    "lib/${TEIGHA_PLATFORM}/libModelerGeometry.a"
-    "lib/${TEIGHA_PLATFORM}/libTD_BrepRenderer.a"
-    "lib/${TEIGHA_PLATFORM}/libRecomputeDimBlock.a"
-    "lib/${TEIGHA_PLATFORM}/libTD_AcisBuilder.a"
-    "lib/${TEIGHA_PLATFORM}/libTD_Br.a"
-    "lib/${TEIGHA_PLATFORM}/libTD_Gs.a"
-    "lib/${TEIGHA_PLATFORM}/libTD_Ge.a"
-    "lib/${TEIGHA_PLATFORM}/libTD_Root.a"
-    "lib/${TEIGHA_PLATFORM}/liboless.a"
-    "lib/${TEIGHA_PLATFORM}/liblibcrypto.a"
-    "lib/${TEIGHA_PLATFORM}/libsisl.a"
-    #"lib/${TEIGHA_PLATFORM}/libFreeType.a"  # System one added later
-    #"lib/${TEIGHA_PLATFORM}/libzlib.a"      # System one added later
-    )
+    # Linux static libs
+    list(APPEND TEIGHA_LIBRARIES_NAMES
+        "lib/${TEIGHA_PLATFORM}/libTD_Db.a"
+        "lib/${TEIGHA_PLATFORM}/libTD_DbCore.a"
+        "lib/${TEIGHA_PLATFORM}/libTD_DbEntities.a"
+        "lib/${TEIGHA_PLATFORM}/libTD_DrawingsExamplesCommon.a"
+        "lib/${TEIGHA_PLATFORM}/libTG_Db.a"
+        "lib/${TEIGHA_PLATFORM}/libTG_ExamplesCommon.a"
+        "lib/${TEIGHA_PLATFORM}/libTD_Alloc.a"
+        "lib/${TEIGHA_PLATFORM}/libTD_DbRoot.a"
+        "lib/${TEIGHA_PLATFORM}/libTD_ExamplesCommon.a"
+        "lib/${TEIGHA_PLATFORM}/libWinBitmap.a"
+        "lib/${TEIGHA_PLATFORM}/libTD_Gi.a"
+        "lib/${TEIGHA_PLATFORM}/libtinyxml.a"
+        "lib/${TEIGHA_PLATFORM}/libTD_Zlib.a"
+        "lib/${TEIGHA_PLATFORM}/libTD_SpatialIndex.a"
+        "lib/${TEIGHA_PLATFORM}/libTD_DgnImport.a"
+        "lib/${TEIGHA_PLATFORM}/libTD_DgnUnderlay.a"
+        "lib/${TEIGHA_PLATFORM}/libModelerGeometry.a"
+        "lib/${TEIGHA_PLATFORM}/libTD_BrepRenderer.a"
+        "lib/${TEIGHA_PLATFORM}/libRecomputeDimBlock.a"
+        "lib/${TEIGHA_PLATFORM}/libTD_AcisBuilder.a"
+        "lib/${TEIGHA_PLATFORM}/libTD_Br.a"
+        "lib/${TEIGHA_PLATFORM}/libTD_Gs.a"
+        "lib/${TEIGHA_PLATFORM}/libTD_Ge.a"
+        "lib/${TEIGHA_PLATFORM}/libTD_Root.a"
+        "lib/${TEIGHA_PLATFORM}/liboless.a"
+        "lib/${TEIGHA_PLATFORM}/liblibcrypto.a"
+        "lib/${TEIGHA_PLATFORM}/libsisl.a"
+        #"lib/${TEIGHA_PLATFORM}/libFreeType.a"  # System one added later
+        #"lib/${TEIGHA_PLATFORM}/libzlib.a"      # System one added later
+        )
+elseif (EXISTS "${TEIGHA_ROOT}/lib/${TEIGHA_PLATFORM}/TD_DrawingsExamplesCommon.lib")
+    # Windows ODA 2021
+    set(TD_DRAWINGS_LIBDIR "lib/${TEIGHA_PLATFORM}")
+    set(TD_KERNEL_LIBDIR   "lib/${TEIGHA_PLATFORM}")
+    list(APPEND TEIGHA_LIBRARIES_NAMES
+        "${TD_DRAWINGS_LIBDIR}/TD_Db.lib"
+        "${TD_DRAWINGS_LIBDIR}/TD_DbCore.lib"
+        "${TD_DRAWINGS_LIBDIR}/TD_DbEntities.lib"
+        "${TD_DRAWINGS_LIBDIR}/TD_DrawingsExamplesCommon.lib"
+        "${TD_DRAWINGS_LIBDIR}/TG_Db.lib"
+        "${TD_DRAWINGS_LIBDIR}/TG_ExamplesCommon.lib"
+        "${TD_KERNEL_LIBDIR}/TD_Alloc.lib"
+        "${TD_KERNEL_LIBDIR}/TD_DbRoot.lib"
+        "${TD_KERNEL_LIBDIR}/TD_ExamplesCommon.lib"
+        "${TD_KERNEL_LIBDIR}/TD_Ge.lib"
+        "${TD_KERNEL_LIBDIR}/TD_Root.lib")
+    if (EXISTS "${TEIGHA_ROOT}/exe/${TEIGHA_PLATFORM}/oless.dll")
+    # Dynamic libs
+    list(APPEND TEIGHA_LIBRARIES_NAMES
+        "${TD_DRAWINGS_LIBDIR}/TD_Key.lib")
+    else()
+    # Static libs
+    list(APPEND TEIGHA_LIBRARIES_NAMES
+        "${TD_KERNEL_LIBDIR}/TD_Gi.lib"
+        "${TD_KERNEL_LIBDIR}/tinyxml.lib"
+        "${TD_KERNEL_LIBDIR}/TD_Zlib.lib"
+        "${TD_DRAWINGS_LIBDIR}/TD_DbIO.lib"
+        "${TD_DRAWINGS_LIBDIR}/ISM.lib"
+        "${TD_DRAWINGS_LIBDIR}/WipeOut.lib"
+        "${TD_DRAWINGS_LIBDIR}/RText.lib"
+        "${TD_DRAWINGS_LIBDIR}/ATEXT.lib"
+        "${TD_DRAWINGS_LIBDIR}/SCENEOE.lib"
+        "${TD_DRAWINGS_LIBDIR}/ACCAMERA.lib"
+        "${TD_DRAWINGS_LIBDIR}/AcMPolygonObj15.lib"
+        "${TD_KERNEL_LIBDIR}/TD_Gs.lib"
+        "${TD_KERNEL_LIBDIR}/TD_SpatialIndex.lib"
+        "${TD_KERNEL_LIBDIR}/oless.lib"
+        "${TD_KERNEL_LIBDIR}/UTF.lib"
+        "${TD_KERNEL_LIBDIR}/WinBitmap.lib"
+        "${TD_KERNEL_LIBDIR}/OdBrepModeler.lib"
+        "${TD_KERNEL_LIBDIR}/TD_Br.lib"
+        "${TD_KERNEL_LIBDIR}/TD_AcisBuilder.lib"
+        "${TD_KERNEL_LIBDIR}/TD_BrepBuilder.lib"
+        "${TD_KERNEL_LIBDIR}/TD_BrepBuilderFiller.lib"
+        "${TD_KERNEL_LIBDIR}/TD_BrepRenderer.lib"
+        "${TD_DRAWINGS_LIBDIR}/ModelerGeometry.lib"
+        "${TD_DRAWINGS_LIBDIR}/RecomputeDimBlock.lib")
+    list(APPEND TEIGHA_LIBRARIES crypt32.lib
+                                 secur32.lib
+                                 user32.lib
+                                 gdi32.lib
+                                 ole32.lib)
+    endif()
 elseif (EXISTS "${TEIGHA_ROOT}/lib/${TEIGHA_PLATFORM}/TD_Key.lib")
-# Windows Teigha 4.2.2 shared libs
-list(APPEND TEIGHA_LIBRARIES_NAMES
-     "lib/${TEIGHA_PLATFORM}/TD_Key.lib"
-     "lib/${TEIGHA_PLATFORM}/TD_ExamplesCommon.lib"
-     "lib/${TEIGHA_PLATFORM}/TD_Db.lib"
-     "lib/${TEIGHA_PLATFORM}/TD_DbRoot.lib"
-     "lib/${TEIGHA_PLATFORM}/TD_Root.lib"
-     "lib/${TEIGHA_PLATFORM}/TD_Ge.lib"
-     "lib/${TEIGHA_PLATFORM}/TD_Alloc.lib"
-     "lib/${TEIGHA_PLATFORM}/TG_Db.lib"
-     "lib/${TEIGHA_PLATFORM}/TG_ExamplesCommon.lib")
+    # Windows Teigha 4.2.2 shared libs
+    list(APPEND TEIGHA_LIBRARIES_NAMES
+         "lib/${TEIGHA_PLATFORM}/TD_Key.lib"
+         "lib/${TEIGHA_PLATFORM}/TD_ExamplesCommon.lib"
+         "lib/${TEIGHA_PLATFORM}/TD_Db.lib"
+         "lib/${TEIGHA_PLATFORM}/TD_DbRoot.lib"
+         "lib/${TEIGHA_PLATFORM}/TD_Root.lib"
+         "lib/${TEIGHA_PLATFORM}/TD_Ge.lib"
+         "lib/${TEIGHA_PLATFORM}/TD_Alloc.lib"
+         "lib/${TEIGHA_PLATFORM}/TG_Db.lib"
+         "lib/${TEIGHA_PLATFORM}/TG_ExamplesCommon.lib")
 else()
     # TODO ODA 2021.2 shared and static libs to migrate from nmake.opt
     message(FATAL_ERROR "Teigha/ODA library layout not handled currently")
