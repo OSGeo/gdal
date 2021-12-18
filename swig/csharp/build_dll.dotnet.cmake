546ec4b23cc6e6292785df3197f430b7316c06a7
Paul <runette@gmail.com>
2021-12-18 22:34:46 +0000

    dotnet fixes

--- swig/csharp/build_dll.dotnet.cmake
+++ swig/csharp/build_dll.dotnet.cmake
@@ -2,6 +2,8 @@
 # script to build the C# DLLs using dotnet
 #
 
+find_package(Dotnet)
+
 configure_file("${SOURCE_DIR}/dll_template.csproj" "${BUILD_DIR}/${TARGET_SUBDIR}/${TARGET}.csproj")
 
 ADD_DOTNET("${BUILD_DIR}/${TARGET_SUBDIR}/${TARGET}.csproj")
