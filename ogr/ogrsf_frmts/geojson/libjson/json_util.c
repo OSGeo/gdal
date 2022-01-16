2b0efdf0216abf81c6dd4fef84338de113903d01
Even Rouault <even.rouault@spatialys.com>
2022-01-16 20:47:15 +0100

    Internal libjson: fix printf formatter (CID 1448587)

--- ogr/ogrsf_frmts/geojson/libjson/json_util.c
+++ ogr/ogrsf_frmts/geojson/libjson/json_util.c
@@ -290,7 +290,7 @@ const char *json_type_to_name(enum json_type o_type)
 	int o_type_int = (int)o_type;
 	if (o_type_int < 0 || o_type_int >= (int)NELEM(json_type_name))
 	{
-		_json_c_set_last_err("json_type_to_name: type %d is out of range [0,%d]\n", o_type,
+		_json_c_set_last_err("json_type_to_name: type %d is out of range [0,%u]\n", o_type,
 		                     NELEM(json_type_name));
 		return NULL;
 	}
