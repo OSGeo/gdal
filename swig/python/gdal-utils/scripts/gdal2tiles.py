973d2e3ba72f1455233d62cfb32ccba683750127
matt wilkie <maphew@gmail.com>
2022-02-18 15:40:20 -0700

    undo botched conflict resolution in gdal2tiles.py

--- swig/python/gdal-utils/scripts/gdal2tiles.py
+++ swig/python/gdal-utils/scripts/gdal2tiles.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 
 import sys
-# import osgeo_utils.gdal2tiles as a convenience to use as a script
-from osgeo_utils.gdal2tiles import *  # noqa
-from osgeo_utils.gdal2tiles import main
-from osgeo.gdal import deprecation_warn
 
 # Running main() must be protected that way due to use of multiprocessing on Windows:
 # https://docs.python.org/3/library/multiprocessing.html#the-spawn-and-forkserver-start-methods
