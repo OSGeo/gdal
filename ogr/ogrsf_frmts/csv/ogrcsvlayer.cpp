e0eb91ae1004aae23b7efee6c6f165455e136b14
Even Rouault <even.rouault@spatialys.com>
2022-02-20 22:14:54 +0100

    CSV: avoid coverity scan warning about untrusted value (CID 1475200)

--- ogr/ogrsf_frmts/csv/ogrcsvlayer.cpp
+++ ogr/ogrsf_frmts/csv/ogrcsvlayer.cpp
@@ -287,8 +287,10 @@ void OGRCSVLayer::BuildFeatureDefn( const char *pszNfdcGeomField,
     if( !bNew )
         ResetReading();
 
-    const int nMaxFieldCount = atoi(
+    int nMaxFieldCount = atoi(
         CPLGetConfigOption("OGR_CSV_MAX_FIELD_COUNT", "2000"));
+    if( nMaxFieldCount > 100000 )
+        nMaxFieldCount = 100000; // to please coverity
     if( nFieldCount > nMaxFieldCount )
     {
         CPLError(CE_Warning, CPLE_AppDefined,
