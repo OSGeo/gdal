6b96fc9d48c3070d14faabbfeadf6a00a3e76d08
Momtchil Momtchev <momtchil@momtchev.com>
2022-02-22 11:29:11 +0100

    reformat

--- frmts/vrt/vrtderivedrasterband.cpp
+++ frmts/vrt/vrtderivedrasterband.cpp
@@ -289,7 +289,10 @@ GDALAddDerivedBandPixelFuncWithArgs(const char *pszFuncName,
         return CE_None;
     }
 
-    osMapPixelFunction[pszFuncName] = { pfnNewFunction, pszMetadata != nullptr ? pszMetadata : "" };
+    osMapPixelFunction[pszFuncName] = {
+        pfnNewFunction,
+        pszMetadata != nullptr ? pszMetadata : ""
+    };
 
     return CE_None;
 }
