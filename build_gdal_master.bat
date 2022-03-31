761bf0453fb4faa9a08078dc1dc0bd117ec4a569
MiZhangWhuer <zhangmi1989@163.com>
2022-03-31 16:37:19 +0800

    Update build_gdal_master.bat

--- build_gdal_master.bat
+++ build_gdal_master.bat
@@ -1,3 +1,19 @@
+REM # Copyright 2021, 2022 LuoJiaNET Research and Development Group, Wuhan University
+REM # Modified by Mi Zhang, LuoJiaNET Development and Research Group, Wuhan University, 2022
+REM # Contact: zhangmi1989@163.com
+REM #
+REM # Licensed under the Apache License, Version 2.0 (the "License");
+REM # you may not use this file except in compliance with the License.
+REM # You may obtain a copy of the License at
+REM #
+REM # http://www.apache.org/licenses/LICENSE-2.0
+REM #
+REM # Unless required by applicable law or agreed to in writing, software
+REM # distributed under the License is distributed on an "AS IS" BASIS,
+REM # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM # See the License for the specific language governing permissions and
+REM # limitations under the License.
+REM # ============================================================================
 
 REM build gdal thir_party_libraries from source code
 cd third_party_app
