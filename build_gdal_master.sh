5a13fb2685858f383a840567ef931db0f9d00d7f
MiZhangWhuer <zhangmi1989@163.com>
2022-03-31 16:36:12 +0800

    Update build_gdal_master.sh

--- build_gdal_master.sh
+++ build_gdal_master.sh
@@ -1,3 +1,19 @@
+# Copyright 2021, 2022 LuoJiaNET Research and Development Group, Wuhan University
+# Modified by Mi Zhang, LuoJiaNET Development and Research Group, Wuhan University, 2022
+# Contact: zhangmi1989@163.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
 
 # build gdal thir_party_libraries from source code
 cd third_party_app
