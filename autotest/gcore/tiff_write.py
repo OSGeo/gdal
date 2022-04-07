f5ae81e1b61b43229cdfc92b7260b7fff939bfff
Even Rouault <even.rouault@spatialys.com>
2022-04-07 17:09:53 +0200

    GTiff: fix crash when building overviews and computing approx stats (fixes #5580)

--- autotest/gcore/tiff_write.py
+++ autotest/gcore/tiff_write.py
@@ -8272,5 +8272,22 @@ def test_tiff_write_setcolortable_read_only_overriding_tifftags():
 
     gdal.GetDriverByName('GTiff').Delete(filename)
 
+
+###############################################################################
+# Test scenario of https://github.com/OSGeo/gdal/issues/5580
+
+
+def test_tiff_write_overview_building_and_approx_stats():
+
+    filename = '/vsimem/out.tif'
+    gdal.GetDriverByName('GTiff').Create(filename, 512, 512)
+    ds = gdal.Open(filename)
+    ds.BuildOverviews("NEAREST", [2, 4, 8])
+    ds.GetRasterBand(1).ComputeStatistics(1)
+    ds = None
+
+    gdal.GetDriverByName('GTiff').Delete(filename)
+
+
 def test_tiff_write_cleanup():
     gdaltest.tiff_drv = None
