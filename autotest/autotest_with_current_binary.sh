c4db62f18c821b8c52efbd5beb9c582f7eb0d46f
matt wilkie <maphew@gmail.com>
2022-04-20 11:43:58 -0700

    Create autotest_with_current_binary.sh

---
+++ autotest/autotest_with_current_binary.sh
@@ -0,0 +1,20 @@
+#!/bin/env bash
+printf "Check out source code tree to match the version of installed gdal binaries\
+ and then run all of the autotests...\n"
+
+if ! [ -x "$(command -v git)" ]; then
+  echo 'Error: git is not installed.' >&2
+  exit 1
+elif ! [ -x "$(command -v gdalinfo)" ]; then
+  echo 'Error: gdal is not installed.' >&2
+  exit 1
+fi
+
+git checkout `gdalinfo --version | sed -s "s/GDAL.*-\(.*\), .*/\1/"`
+    # Equiv to:
+    #     $ gdalinfo --version
+    #     GDAL 3.4.0dev-d2f9067ffb15e593e9b826ca939dbd183636c780, released 2021/10/26
+    #     $ git checkout d2f9067ffb15e593e9b826ca939dbd183636c780
+
+pip install -r ./requirements.txt
+pytest
