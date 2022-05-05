c02c85b2597e87e9d4a97812a92701ebe63503ae
Even Rouault <even.rouault@spatialys.com>
2022-05-06 01:53:04 +0200

    CMake: be robust against failure of get_git_head_revision()

--- cmake/helpers/generate_gdal_version_h.cmake
+++ cmake/helpers/generate_gdal_version_h.cmake
@@ -21,32 +21,36 @@ elseif (EXISTS ${SOURCE_DIR}/.git)
     if (GIT_FOUND)
         include(GetGitRevisionDescription)
         get_git_head_revision(GDAL_GIT_REFSPEC GDAL_GIT_HASH ${SOURCE_DIR})
-        include(GetGitHeadDate)
-        get_git_head_date(GDAL_GIT_DATE ${SOURCE_DIR})
-        git_local_changes(GIT_LOCAL_CHG ${SOURCE_DIR})
-        string(SUBSTRING ${GDAL_GIT_HASH} 0 10 REV)
-        set(GDAL_DEV_REVISION "dev-${REV}")
-        if (GIT_LOCAL_CHG STREQUAL "DIRTY")
-          set(GDAL_DEV_REVISION "${GDAL_DEV_REVISION}-dirty")
-        elseif (GIT_LOCAL_CHG STREQUAL "UNKNOWN" AND
-                EXISTS ${BINARY_DIR}/gcore/gdal_version.h AND
-                EXISTS ${BINARY_DIR}/gcore/gdal_version_full/gdal_version.h)
-          # Happens typically when running "sudo make install" after "make" as a regular user
-          file(READ ${BINARY_DIR}/gcore/gdal_version_full/gdal_version.h GDAL_FULL_VERSION_H_CONTENTS)
-          if( "${GDAL_FULL_VERSION_H_CONTENTS}" MATCHES "${REV}" )
-              # Assume the -dirty state has not been modified. This avoids
-              # rebuilding files
-              return()
-          endif()
-          # If we get here, the revision number has changed from the latest build
-          # but we are not able to determine if there are local changes.
-          # Assume none...
-        endif()
+        if ("${GDAL_GIT_HASH}" STREQUAL "")
+            message(WARNING "Cannot determine git hash")
+        else()
+            include(GetGitHeadDate)
+            get_git_head_date(GDAL_GIT_DATE ${SOURCE_DIR})
+            git_local_changes(GIT_LOCAL_CHG ${SOURCE_DIR})
+            string(SUBSTRING ${GDAL_GIT_HASH} 0 10 REV)
+            set(GDAL_DEV_REVISION "dev-${REV}")
+            if (GIT_LOCAL_CHG STREQUAL "DIRTY")
+              set(GDAL_DEV_REVISION "${GDAL_DEV_REVISION}-dirty")
+            elseif (GIT_LOCAL_CHG STREQUAL "UNKNOWN" AND
+                    EXISTS ${BINARY_DIR}/gcore/gdal_version.h AND
+                    EXISTS ${BINARY_DIR}/gcore/gdal_version_full/gdal_version.h)
+              # Happens typically when running "sudo make install" after "make" as a regular user
+              file(READ ${BINARY_DIR}/gcore/gdal_version_full/gdal_version.h GDAL_FULL_VERSION_H_CONTENTS)
+              if( "${GDAL_FULL_VERSION_H_CONTENTS}" MATCHES "${REV}" )
+                  # Assume the -dirty state has not been modified. This avoids
+                  # rebuilding files
+                  return()
+              endif()
+              # If we get here, the revision number has changed from the latest build
+              # but we are not able to determine if there are local changes.
+              # Assume none...
+            endif()
 
-        string(REPLACE "dev" "${GDAL_DEV_REVISION}"
-               GDAL_VERSION_H_CONTENTS "${GDAL_VERSION_H_CONTENTS}")
-        string(REGEX REPLACE "(define GDAL_RELEASE_DATE[ ]+)([0-9]+)(.*)" "\\1${GDAL_GIT_DATE}\\3"
-              GDAL_VERSION_H_CONTENTS "${GDAL_VERSION_H_CONTENTS}")
+            string(REPLACE "dev" "${GDAL_DEV_REVISION}"
+                   GDAL_VERSION_H_CONTENTS "${GDAL_VERSION_H_CONTENTS}")
+            string(REGEX REPLACE "(define GDAL_RELEASE_DATE[ ]+)([0-9]+)(.*)" "\\1${GDAL_GIT_DATE}\\3"
+                  GDAL_VERSION_H_CONTENTS "${GDAL_VERSION_H_CONTENTS}")
+        endif()
     endif ()
 endif ()
 
