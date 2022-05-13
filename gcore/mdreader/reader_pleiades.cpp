e1b8d8d2b7c7ec372885c5afc132e15e43471c26
christopher.elin <christopher.elin@externalgcp.ds.airbus.com>
2022-05-13 16:09:23 +0200

    dimap v3 (PNEO) : the center of upper left pixel is not always 1,1. So now it is dynamically loaded from the DIMAP.

--- gcore/mdreader/reader_pleiades.cpp
+++ gcore/mdreader/reader_pleiades.cpp
@@ -345,6 +345,21 @@ char** GDALMDReaderPleiades::LoadRPCXmlFile()
         return nullptr;
     }
 
+    // search Image to Ground Validity (since DIMAP v3)
+    CPLXMLNode* pValidityNode = CPLSearchXMLNode(pNode, "=ImagetoGround_Validity_Domain");
+
+    double firstCol = 1.0;
+    if( pValidityNode != nullptr )
+    {
+        char** m_papszValidity = nullptr;
+        m_papszValidity = ReadXMLToList(pValidityNode->psChild, m_papszValidity);
+
+        if( m_papszValidity != nullptr )
+        {
+            firstCol = CPLAtofM(CSLFetchNameValue(m_papszValidity, "FIRST_COL"));
+        }
+    }
+
     // If we are not the top-left tile, then we must shift LINE_OFF and SAMP_OFF
     int nLineOffShift = 0;
     int nPixelOffShift = 0;
@@ -386,12 +401,15 @@ char** GDALMDReaderPleiades::LoadRPCXmlFile()
     {
         const char *pszValue = CSLFetchNameValue(papszRawRPCList,
                                                  apszRPBMap[i + 1]);
-        // Pleiades RPCs use "center of upper left pixel is 1,1" convention, convert to
+        // Deprecated : Pleiades RPCs use "center of upper left pixel is 1,1" convention, convert to
         // Digital globe convention of "center of upper left pixel is 0,0".
+
+        // Since DIMAP v3, the center of upper left pixel can be 0, 0. So now it is dynamically loaded
+        // from the DIMAP.
         if ((i == 0 || i == 2) && pszValue)
         {
             CPLString osField;
-            double dfVal = CPLAtofM( pszValue ) -1.0 ;
+            double dfVal = CPLAtofM( pszValue ) - firstCol ;
             if( i == 0 )
                 dfVal += nLineOffShift;
             else
