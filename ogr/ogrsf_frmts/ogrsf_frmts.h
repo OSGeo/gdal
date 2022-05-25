aee0746d1427d4704ebd8a68e49d07e877fb6096
Jake <jake.molnar@safe.com>
2022-05-25 12:39:04 -0700

    OGR: Add Upsert to set of possible operations
    in addition to the exist Create, Set, and Delete
    
    (ie. INSERT, UPDATE, and DELETE)

--- ogr/ogrsf_frmts/ogrsf_frmts.h
+++ ogr/ogrsf_frmts/ogrsf_frmts.h
@@ -111,6 +111,7 @@ class CPL_DLL OGRLayer : public GDALMajorObject
 
     virtual OGRErr      ISetFeature( OGRFeature *poFeature ) CPL_WARN_UNUSED_RESULT;
     virtual OGRErr      ICreateFeature( OGRFeature *poFeature )  CPL_WARN_UNUSED_RESULT;
+    virtual OGRErr      IUpsertFeature( OGRFeature *poFeature )  CPL_WARN_UNUSED_RESULT;
 
   public:
     OGRLayer();
@@ -150,6 +151,7 @@ class CPL_DLL OGRLayer : public GDALMajorObject
 
     OGRErr      SetFeature( OGRFeature *poFeature )  CPL_WARN_UNUSED_RESULT;
     OGRErr      CreateFeature( OGRFeature *poFeature ) CPL_WARN_UNUSED_RESULT;
+    OGRErr      UpsertFeature( OGRFeature *poFeature )  CPL_WARN_UNUSED_RESULT;
 
     virtual OGRErr      DeleteFeature( GIntBig nFID )  CPL_WARN_UNUSED_RESULT;
 
