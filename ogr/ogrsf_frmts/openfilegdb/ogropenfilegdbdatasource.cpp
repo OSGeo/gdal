10adc6bcb448bf698f31f13680b3af93789855f9
Even Rouault <even.rouault@spatialys.com>
2022-06-04 15:03:59 +0200

    OpenFileGDB: avoid crash on 'SELECT ... FROM ... ORDER BY ...' when the index is corrupted

--- ogr/ogrsf_frmts/openfilegdb/ogropenfilegdbdatasource.cpp
+++ ogr/ogrsf_frmts/openfilegdb/ogropenfilegdbdatasource.cpp
@@ -1422,6 +1422,7 @@ OGRLayer* OGROpenFileGDBDataSource::ExecuteSQL( const char *pszSQLCommand,
 
                     /* Check that they are no NULL values */
                     if( oSelect.where_expr == nullptr &&
+                        poIter != nullptr &&
                         poIter->GetRowCount() != poLayer->GetFeatureCount(FALSE) )
                     {
                         delete poIter;
