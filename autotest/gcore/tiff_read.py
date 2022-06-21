49dbeae2c1be0de13be37a458ba12993cc2b0a30
Sam Gillingham <gillingham.sam@gmail.com>
2022-06-21 10:06:57 +1000

    rename gtiff/int64.tif

--- autotest/gcore/tiff_read.py
+++ autotest/gcore/tiff_read.py
@@ -68,8 +68,8 @@ init_list = [
     ('contig_tiled.tif', 2, 15234),
     ('contig_strip.tif', 2, 15234),
     ('empty1bit.tif', 1, 0),
-    ('gtiff/int64.tif', 1, 65535),
-    ('gtiff/uint64.tif', 1, 1),
+    ('gtiff/int64_full_range.tif', 1, 65535),
+    ('gtiff/uint64_full_range.tif', 1, 1),
 ]
 
 
