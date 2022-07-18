74d993423b9815b63cbfa005ffc5f7ee906ac7b8
Even Rouault <even.rouault@spatialys.com>
2022-07-18 16:13:04 +0200

    CMake JP2KAK: add (at least build) support for versions down to 7.3

--- cmake/modules/packages/FindKDU.cmake
+++ cmake/modules/packages/FindKDU.cmake
@@ -19,12 +19,20 @@ if(CMAKE_VERSION VERSION_LESS 3.13)
     set(KDU_ROOT CACHE PATH "KAKADU library base directory")
 endif()
 
+set(KDU_VERSION_VAR "")
 if(KDU_ROOT AND EXISTS "${KDU_ROOT}/coresys")
     if("${CMAKE_SYSTEM_PROCESSOR}" MATCHES "x86_64" AND "${CMAKE_SYSTEM_NAME}" STREQUAL "Linux")
         file(READ "${KDU_ROOT}/coresys/make/Makefile-Linux-x86-64-gcc" CORESYS_MAKEFILE_CONTENTS)
         string(REGEX REPLACE ".*SHARED_LIB_NAME[ \t]*=[ \t]*([^.]*)(\.so).*" "\\1\\2" KDU_SHARED_LIB_NAME ${CORESYS_MAKEFILE_CONTENTS})
         file(READ "${KDU_ROOT}/managed/make/Makefile-Linux-x86-64-gcc" MANAGED_MAKEFILE_CONTENTS)
         string(REGEX REPLACE ".*AUX_SHARED_LIB_NAME[ \t]*=[ \t]*([^.]*)(\.so).*" "\\1\\2" KDU_AUX_SHARED_LIB_NAME ${MANAGED_MAKEFILE_CONTENTS})
+
+        string(REGEX REPLACE "libkdu_v([0-9])([0-9A-F])R\.so" "\\1" KDU_MAJOR_VERSION "${KDU_SHARED_LIB_NAME}")
+        string(REGEX REPLACE "libkdu_v([0-9])([0-9A-F])R\.so" "\\2" KDU_MINOR_VERSION "${KDU_SHARED_LIB_NAME}")
+        if( KDU_MINOR_VERSION STREQUAL "A" )
+            set(KDU_MINOR_VERSION 10)
+        endif()
+        set(KDU_VERSION_VAR "${KDU_MAJOR_VERSION}.${KDU_MINOR_VERSION}")
 
         find_library(KDU_LIBRARY ${KDU_SHARED_LIB_NAME}
                      PATH ${KDU_ROOT}/lib/Linux-x86-64-gcc)
@@ -61,7 +69,8 @@ find_path(KDU_INCLUDE_DIR coresys/common/kdu_elementary.h
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(KDU
-                                  REQUIRED_VARS KDU_INCLUDE_DIR KDU_LIBRARY KDU_AUX_LIBRARY)
+                                  REQUIRED_VARS KDU_INCLUDE_DIR KDU_LIBRARY KDU_AUX_LIBRARY
+                                  VERSION_VAR KDU_VERSION_VAR)
 mark_as_advanced(KDU_INCLUDE_DIR KDU_LIBRARY KDU_AUX_LIBRARY)
 
 if(KDU_FOUND)
@@ -84,5 +93,10 @@ if(KDU_FOUND)
         set_target_properties(KDU::KDU PROPERTIES
                               INTERFACE_INCLUDE_DIRECTORIES "${KDU_INCLUDE_DIR}/coresys/common;${KDU_INCLUDE_DIR}/apps/compressed_io;${KDU_INCLUDE_DIR}/apps/jp2;${KDU_INCLUDE_DIR}/apps/image;${KDU_INCLUDE_DIR}/apps/args;${KDU_INCLUDE_DIR}/apps/support;${KDU_INCLUDE_DIR}/apps/kdu_compress"
                               INTERFACE_LINK_LIBRARIES "KDU::KDU_MAIN;KDU::KDU_AUX")
+        if( DEFINED KDU_MAJOR_VERSION AND ${KDU_MAJOR_VERSION}.${KDU_MINOR_VERSION} VERSION_LESS 7.5 )
+            set_property(TARGET KDU::KDU
+                         APPEND PROPERTY
+                         INTERFACE_COMPILE_DEFINITIONS "KDU_MAJOR_VERSION=${KDU_MAJOR_VERSION};KDU_MINOR_VERSION=${KDU_MINOR_VERSION}")
+        endif()
     endif()
 endif()
