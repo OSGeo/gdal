0feeaf50f27b3d46cd3bb2681b3e19bc52387420
Nyall Dawson <nyall.dawson@gmail.com>
2022-07-20 14:06:22 +1000

    [csv] Advertize delete/alter/reorder field capability

--- ogr/ogrsf_frmts/csv/ogrcsvdriver.cpp
+++ ogr/ogrsf_frmts/csv/ogrcsvdriver.cpp
@@ -301,6 +301,10 @@ void RegisterOGRCSV()
     poDriver->SetMetadataItem(GDAL_DCAP_CREATE_LAYER, "YES");
     poDriver->SetMetadataItem(GDAL_DCAP_DELETE_LAYER, "YES");
     poDriver->SetMetadataItem(GDAL_DCAP_CREATE_FIELD, "YES");
+    poDriver->SetMetadataItem(GDAL_DCAP_DELETE_FIELD, "YES");
+    poDriver->SetMetadataItem(GDAL_DCAP_REORDER_FIELDS, "YES" );
+    poDriver->SetMetadataItem(GDAL_DMD_ALTER_FIELD_DEFN_FLAGS, "Name Type WidthPrecision" );
+
     poDriver->SetMetadataItem(GDAL_DCAP_CURVE_GEOMETRIES, "YES");
     poDriver->SetMetadataItem(GDAL_DCAP_MEASURED_GEOMETRIES, "YES");
 
