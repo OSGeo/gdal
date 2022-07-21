ec3746f4fb5c2645f0d9b5fb3fb172ea396592fc
Even Rouault <even.rouault@spatialys.com>
2022-07-21 13:10:38 +0200

    swig/python/CMakeLists.txt: fix SWIG_REGENERATE_PYTHON mode

--- swig/python/modify_cpp_files.cmake
+++ swig/python/modify_cpp_files.cmake
@@ -1,61 +1,59 @@
 # Sanitize SWIG output for analyzers (cppcheck, coverity, ...)
 
-foreach(_FILE IN ITEMS gdal_wrap.cpp gdalconst_wrap.c ogr_wrap.cpp osr_wrap.cpp gdal_array_wrap.cpp gnm_wrap.cpp)
-    file(READ ${CMAKE_CURRENT_BINARY_DIR}/extensions/${_FILE} _CONTENTS)
+file(READ ${FILE} _CONTENTS)
 
-    # Following is to prevent Coverity Scan to warn about Dereference before null check (REVERSE_INULL)
-    string(REPLACE "if (!PyTuple_Check(args)) SWIG_fail;"
-                   "if (args == NULL || !PyTuple_Check(args)) SWIG_fail;"
+# Following is to prevent Coverity Scan to warn about Dereference before null check (REVERSE_INULL)
+string(REPLACE "if (!PyTuple_Check(args)) SWIG_fail;"
+               "if (args == NULL || !PyTuple_Check(args)) SWIG_fail;"
+       _CONTENTS "${_CONTENTS}")
+
+string(REPLACE "argc = args ? (int)PyObject_Length(args) : 0"
+               "argc = (int)PyObject_Length(args)"
+       _CONTENTS "${_CONTENTS}")
+
+string(REGEX REPLACE "if \\(SWIG_IsTmpObj\\(res([1-9])\\)\\)"
+                     "if \(ReturnSame\(SWIG_IsTmpObj\(res\\1\)\)\)"
+       _CONTENTS "${_CONTENTS}")
+
+string(REGEX REPLACE "if\\( alloc([1-9]) == SWIG_NEWOBJ \\)"
+                     "if \(ReturnSame(alloc\\1) == SWIG_NEWOBJ \)"
+       _CONTENTS "${_CONTENTS}")
+
+string(REPLACE "strncpy(buff, \"swig_ptr: \", 10)"
+               "memcpy(buff, \"swig_ptr: \", 10)"
+       _CONTENTS "${_CONTENTS}")
+
+# Unused assigned variable
+string(REPLACE "module_head = &swig_module;"
+               "/*module_head = &swig_module;*/"
+       _CONTENTS "${_CONTENTS}")
+
+# SWIG defect fixed per https://github.com/swig/swig/commit/b0e29fbdf31bb94b11cb8a7cc830b4a76467afa3#diff-ba23eb3671d250e6a62261f19f653dd2R93
+string(REPLACE "obj = PyUnicode_AsUTF8String(obj);"
+               "obj = PyUnicode_AsUTF8String(obj); if (!obj) return SWIG_TypeError;"
+       _CONTENTS "${_CONTENTS}")
+
+# TODO: To remove once swig/python/GNUmakefile has gone
+string(APPEND _CONTENTS "#define POST_PROCESSING_APPLIED\n")
+
+if("${FILE}" MATCHES "gdal_wrap.cpp")
+    string(REGEX REPLACE "result = \\(CPLErr\\)([^;]+)(\\;)"
+                         [[CPL_IGNORE_RET_VAL(result = (CPLErr)\1)\2]]
+           _CONTENTS "${_CONTENTS}")
+endif()
+
+if(NOT "${FILE}" MATCHES "gdal_array_wrap.cpp")
+    string(REPLACE "PyObject *resultobj = 0;"
+                   "PyObject *resultobj = 0; int bLocalUseExceptionsCode = bUseExceptions;"
            _CONTENTS "${_CONTENTS}")
 
-    string(REPLACE "argc = args ? (int)PyObject_Length(args) : 0"
-                   "argc = (int)PyObject_Length(args)"
+    string(REPLACE "#define SWIGPYTHON"
+                   "#define SWIGPYTHON\n\#define SED_HACKS"
            _CONTENTS "${_CONTENTS}")
 
-    string(REGEX REPLACE "if \\(SWIG_IsTmpObj\\(res([1-9])\\)\\)"
-                         "if \(ReturnSame\(SWIG_IsTmpObj\(res\\1\)\)\)"
+    string(REPLACE "return resultobj;"
+                   "if ( ReturnSame(bLocalUseExceptionsCode) ) { CPLErr eclass = CPLGetLastErrorType(); if ( eclass == CE_Failure || eclass == CE_Fatal ) { Py_XDECREF(resultobj); SWIG_Error( SWIG_RuntimeError, CPLGetLastErrorMsg() ); return NULL; } }\n  return resultobj;"
            _CONTENTS "${_CONTENTS}")
+endif()
 
-    string(REGEX REPLACE "if\\( alloc([1-9]) == SWIG_NEWOBJ \\)"
-                         "if \(ReturnSame(alloc\\1) == SWIG_NEWOBJ \)"
-           _CONTENTS "${_CONTENTS}")
-
-    string(REPLACE "strncpy(buff, \"swig_ptr: \", 10)"
-                   "memcpy(buff, \"swig_ptr: \", 10)"
-           _CONTENTS "${_CONTENTS}")
-
-    # Unused assigned variable
-    string(REPLACE "module_head = &swig_module;"
-                   "/*module_head = &swig_module;*/"
-           _CONTENTS "${_CONTENTS}")
-
-    # SWIG defect fixed per https://github.com/swig/swig/commit/b0e29fbdf31bb94b11cb8a7cc830b4a76467afa3#diff-ba23eb3671d250e6a62261f19f653dd2R93
-    string(REPLACE "obj = PyUnicode_AsUTF8String(obj);"
-                   "obj = PyUnicode_AsUTF8String(obj); if (!obj) return SWIG_TypeError;"
-           _CONTENTS "${_CONTENTS}")
-
-    # TODO: To remove once swig/python/GNUmakefile has gone
-    string(APPEND _CONTENTS "#define POST_PROCESSING_APPLIED\n")
-
-    if("${_FILE}" STREQUAL "gdal_wrap.cpp")
-        string(REGEX REPLACE "result = \\(CPLErr\\)([^;]+)(\\;)"
-                             [[CPL_IGNORE_RET_VAL(result = (CPLErr)\1)\2]]
-               _CONTENTS "${_CONTENTS}")
-    endif()
-
-    if(NOT "${_FILE}" STREQUAL "gdal_array_wrap.cpp")
-        string(REPLACE "PyObject *resultobj = 0;"
-                       "PyObject *resultobj = 0; int bLocalUseExceptionsCode = bUseExceptions;"
-               _CONTENTS "${_CONTENTS}")
-
-        string(REPLACE "#define SWIGPYTHON"
-                       "#define SWIGPYTHON\n\#define SED_HACKS"
-               _CONTENTS "${_CONTENTS}")
-
-        string(REPLACE "return resultobj;"
-                       "if ( ReturnSame(bLocalUseExceptionsCode) ) { CPLErr eclass = CPLGetLastErrorType(); if ( eclass == CE_Failure || eclass == CE_Fatal ) { Py_XDECREF(resultobj); SWIG_Error( SWIG_RuntimeError, CPLGetLastErrorMsg() ); return NULL; } }\n  return resultobj;"
-               _CONTENTS "${_CONTENTS}")
-    endif()
-
-    file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/extensions/${_FILE} "${_CONTENTS}")
-endforeach()
+file(WRITE ${FILE} "${_CONTENTS}")
