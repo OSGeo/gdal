bd883a5e79bd07dec71eda8c3017111136603647
Even Rouault <even.rouault@spatialys.com>
2022-07-21 12:21:47 +0200

    CI: port osx job to cmake

--- ci/travis/osx/script.sh
+++ ci/travis/osx/script.sh
@@ -2,15 +2,10 @@
 
 set -e
 
-PYTHONPATH=$(find $PWD/swig/python/build -name "lib.*")
-export PYTHONPATH
-export PYTEST="python3 -m pytest -vv -p no:sugar --color=no"
-export DYLD_LIBRARY_PATH=$HOME/install-gdal/lib
-export GDAL_DATA=$HOME/install-gdal/share/gdal
 export PROJ_NETWORK=ON
 
 echo 'Running CPP unit tests'
-(cd autotest/cpp && make quick_test)
+(cd build && make quicktest)
 
 echo 'Running Python unit tests'
 # install test dependencies
@@ -22,8 +17,7 @@ mv autotest/utilities/test_ogr2ogr.py autotest/utilities/test_ogr2ogr.py.disable
 mv autotest/pyscripts/test_ogr2ogr_py.py autotest/pyscripts/test_ogr2ogr_py.py.disabled
 
 # Run all the Python autotests
-cd autotest
-$PYTEST
+(cd build && ctest -V -R autotest)
 
 # For some reason, the tests crash at process exit
 # (cd autotest; $PYTEST 2>&1 | tee /tmp/log.txt || /bin/true)
