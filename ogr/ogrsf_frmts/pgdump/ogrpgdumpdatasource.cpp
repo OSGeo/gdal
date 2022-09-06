8e55e53e159e50d2e8a6069a5d9f986cf78fdab7
Even Rouault <even.rouault@spatialys.com>
2022-09-06 16:10:23 +0200

    PGDump: avoid extraneous harmless spaces in CREATE TABLE statements

--- ogr/ogrsf_frmts/pgdump/ogrpgdumpdatasource.cpp
+++ ogr/ogrsf_frmts/pgdump/ogrpgdumpdatasource.cpp
@@ -457,16 +457,16 @@ OGRPGDumpDataSource::ICreateLayer( const char * pszLayerName,
         if (eType == wkbNone)
             osCommand.Printf(
                 "%s ( "
-                "   %s %s, "
-                "   CONSTRAINT \"%s_pk\" PRIMARY KEY (%s) )",
+                "%s %s, "
+                "CONSTRAINT \"%s_pk\" PRIMARY KEY (%s) )",
                 osCreateTable.c_str(), osFIDColumnNameEscaped.c_str(),
                 pszSerialType, pszTableName, osFIDColumnNameEscaped.c_str() );
         else
             osCommand.Printf(
                 "%s ( "
-                "   %s %s, "
-                "   WKB_GEOMETRY %s, "
-                "   CONSTRAINT \"%s_pk\" PRIMARY KEY (%s) )",
+                "%s %s, "
+                "WKB_GEOMETRY %s, "
+                "CONSTRAINT \"%s_pk\" PRIMARY KEY (%s) )",
                 osCreateTable.c_str(), osFIDColumnNameEscaped.c_str(),
                 pszSerialType, pszGeomType, pszTableName,
                 osFIDColumnNameEscaped.c_str() );
