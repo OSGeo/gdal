f89678713d68b5ed27773332dbe13b2047f7b6f8
Even Rouault <even.rouault@spatialys.com>
2022-10-01 00:50:00 +0200

    tiff_read.py: fix issue with build-mac config

--- autotest/gcore/tiff_read.py
+++ autotest/gcore/tiff_read.py
@@ -4824,7 +4824,11 @@ def test_tiff_read_multi_threaded_vsicurl(use_dataset_readraster):
     if not check_libtiff_internal_or_at_least(4, 0, 11):
         pytest.skip()
 
-    if gdaltest.is_travis_branch("macos_build"):
+    if (
+        not use_dataset_readraster
+        and gdaltest.is_travis_branch("macos_build")
+        or gdaltest.is_github_workflow_mac()
+    ):
         pytest.skip("fails randomly")
 
     if gdal.GetDriverByName("HTTP") is None:
