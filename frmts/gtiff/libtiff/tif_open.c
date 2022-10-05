030bf18f61a18122bb9297601c2ccb59dd406c28
Even Rouault <even.rouault@spatialys.com>
2022-10-05 22:45:58 +0200

    Internal libtiff: Improved IFD-Loop Handling
    Includes https://gitlab.com/libtiff/libtiff/-/merge_requests/386

--- frmts/gtiff/libtiff/tif_open.c
+++ frmts/gtiff/libtiff/tif_open.c
@@ -354,7 +354,8 @@ TIFFClientOpen(
 			goto bad;
 		tif->tif_diroff = 0;
 		tif->tif_lastdiroff = 0;
-		tif->tif_dirlist = NULL;
+		tif->tif_dirlistoff = NULL;
+		tif->tif_dirlistdirn = NULL;
 		tif->tif_dirlistsize = 0;
 		tif->tif_dirnumber = 0;
 		return (tif);
