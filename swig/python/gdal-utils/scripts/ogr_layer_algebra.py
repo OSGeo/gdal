6da6052853cf70eec7e4bb703873e12c037f7b07
Radhakrishna <kssvrk@gmail.com>
2022-10-14 07:51:32 +0530

    modified comment for script import description

--- swig/python/gdal-utils/scripts/ogr_layer_algebra.py
+++ swig/python/gdal-utils/scripts/ogr_layer_algebra.py
@@ -4,7 +4,7 @@ import sys
 
 from osgeo.gdal import deprecation_warn
 
-# import osgeo_utils.ogrmerge as a convenience to use as a script
+# import osgeo_utils.ogr_layer_algebra as a convenience to use as a script
 from osgeo_utils.ogr_layer_algebra import *  # noqa
 from osgeo_utils.ogr_layer_algebra import main
 
