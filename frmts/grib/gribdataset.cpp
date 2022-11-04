7274b4830b3204c567c7cd8e989f5f3b02cdec62
Even Rouault <even.rouault@spatialys.com>
2022-11-04 09:56:19 +0100

    GRIB2: fix crash and invalid metadata when processing index .idx file with sub-messages (fixes #6613)

--- frmts/grib/gribdataset.cpp
+++ frmts/grib/gribdataset.cpp
@@ -79,6 +79,8 @@ static CPLMutex *hGRIBMutex = nullptr;
 
 static CPLString ConvertUnitInText( bool bMetricUnits, const char *pszTxt )
 {
+    if( pszTxt == nullptr )
+        return CPLString();
     if( !bMetricUnits )
         return pszTxt;
 
@@ -1193,6 +1195,11 @@ class InventoryWrapperSidecar : public gdal::grib::InventoryWrapper
                 inv_[i].subgNum =
                     static_cast<unsigned short>(strtol(aosNum[1], &endptr, 10));
                 if (*endptr != 0) goto err_sidecar;
+                if( inv_[i].subgNum <= 0 )
+                    goto err_sidecar;
+                // .idx file use a 1-based indexing, whereas DEGRIB uses a
+                // 0-based one
+                -- inv_[i].subgNum;
             }
 
             inv_[i].start = strtoll(aosTokens[1], &endptr, 10);
