c08174c2137f08d5e42689cf824b13cb7a5f3f27
Even Rouault <even.rouault@spatialys.com>
2022-11-05 19:29:04 +0100

    HDF5 multidim: fix crash on 'gdalmdiminfo HDF5:autotest/gdrivers/data/netcdf/alldatatypes.nc'

--- frmts/hdf5/hdf5multidim.cpp
+++ frmts/hdf5/hdf5multidim.cpp
@@ -1490,7 +1490,10 @@ static std::vector<unsigned> CreateMapTargetComponentsToSrc(
     {
         char* pszName = H5Tget_member_name(hSrcDataType, i);
         if( pszName )
+        {
             oMapSrcCompNameToIdx[pszName] = i;
+            H5free_memory(pszName);
+        }
     }
 
     std::vector<unsigned> ret;
@@ -1555,15 +1558,19 @@ static void CopyValue(const GByte* pabySrcBuffer, hid_t hSrcDataType,
         {
             const auto& comps = dstDataType.GetComponents();
             CPLAssert( comps.size() == mapDstCompsToSrcComps.size() );
-            const std::vector<unsigned> empty;
             for( size_t iDst = 0; iDst < comps.size(); ++iDst )
             {
                 const unsigned iSrc = mapDstCompsToSrcComps[iDst];
                 auto hMemberType = H5Tget_member_type(hSrcDataType, iSrc);
+                const auto mapDstSubCompsToSrcSubComps =
+                    (H5Tget_class(hMemberType) == H5T_COMPOUND &&
+                     comps[iDst]->GetType().GetClass() == GEDTC_COMPOUND) ?
+                    CreateMapTargetComponentsToSrc(hMemberType, comps[iDst]->GetType()) :
+                    std::vector<unsigned>();
                 CopyValue( pabySrcBuffer + H5Tget_member_offset(hSrcDataType, iSrc),
                            hMemberType,
                            pabyDstBuffer + comps[iDst]->GetOffset(),
-                           comps[iDst]->GetType(), empty );
+                           comps[iDst]->GetType(), mapDstSubCompsToSrcSubComps );
                 H5Tclose(hMemberType);
             }
         }
