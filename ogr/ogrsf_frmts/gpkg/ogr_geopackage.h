70b9b4c2b4c04a6f23278ec8a7d3a57b601bf7ed
Even Rouault <even.rouault@spatialys.com>
2022-11-09 20:08:48 +0100

    GPKG raster: implement setting the nodata value for Byte dataset (fixes #1569)
    This is implemented as a NODATA_VALUE metadata item in GDAL ``<GDALMultiDomainMetadata>```
    XML element.

--- ogr/ogrsf_frmts/gpkg/ogr_geopackage.h
+++ ogr/ogrsf_frmts/gpkg/ogr_geopackage.h
@@ -148,6 +148,7 @@ class GDALGeoPackageDataset final : public OGRSQLiteBaseDataSource, public GDALG
     int                 m_nBandCountFromMetadata = 0;
     std::unique_ptr<GDALColorTable> m_poCTFromMetadata{};
     std::string         m_osTFFromMetadata{};
+    std::string         m_osNodataValueFromMetadata{};
 
     int                 m_nOverviewCount = 0;
     GDALGeoPackageDataset** m_papoOverviewDS = nullptr;
