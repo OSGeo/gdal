OSGeo/gdal

commit 7cc19d0484a3ddd1d83b6354a466af810b2ee9e7

NITF: add VALIDATE and FAIL_IF_VALIDATION_ERROR open options

frmts/nitf/nitfdataset.cpp
@@ -1738,7 +1738,28 @@ NITFDataset *NITFDataset::OpenInternal( GDALOpenInfo * poOpenInfo,
             ( reinterpret_cast<GDALPamRasterBand *>( poDS->GetRasterBand(1) ) )->
             GDALPamRasterBand::GetOverviewCount() == 0;
 
+    if( CPLFetchBool(poOpenInfo->papszOpenOptions, "VALIDATE", false) )
+    {
+        if( !poDS->Validate() && CPLFetchBool(poOpenInfo->papszOpenOptions, "FAIL_IF_VALIDATION_ERROR", false) )
+        {
+            delete poDS;
+            poDS = nullptr;
+        }
+    }
+
     return poDS;
+}
+
+/************************************************************************/
+/*                             Validate()                               */
+/************************************************************************/
+
+bool NITFDataset::Validate()
+{
+    bool bSuccess = InitializeTREMetadata(true);
+    if( !InitializeNITFDESs(true) )
+        bSuccess = false;
+    return bSuccess;
 }
 
 /************************************************************************/
@@ -2489,15 +2510,16 @@ void NITFDataset::InitializeNITFTREs()
 /*                       InitializeNITFDESs()                           */
 /************************************************************************/
 
-void NITFDataset::InitializeNITFDESs()
+bool NITFDataset::InitializeNITFDESs(bool bValidate)
 {
     char** papszDESsList = oSpecialMD.GetMetadata( "xml:DES" );
 
     if( papszDESsList != nullptr )
     {
-        return;
+        return true;
     }
 
+    bool bSuccess = true;
     CPLXMLNode* psDesListNode = CPLCreateXMLNode(nullptr, CXT_Element, "des_list");
 
     for( int iSegment = 0; iSegment < psFile->nSegmentCount; iSegment++ )
@@ -2506,7 +2528,10 @@ void NITFDataset::InitializeNITFDESs()
 
         if (EQUAL(psSegInfo->szSegmentType, "DE"))
         {
-            CPLXMLNode* psDesNode = NITFDESGetXml(psFile, iSegment);
+            bool bGotError = false;
+            CPLXMLNode* psDesNode = NITFDESGetXml(psFile, iSegment, bValidate, &bGotError);
+            if( bGotError )
+                bSuccess = false;
 
             if (psDesNode != nullptr)
             {
@@ -2523,6 +2548,7 @@ void NITFDataset::InitializeNITFDESs()
         CPLFree(pszXML);
     }
     CPLDestroyXMLNode(psDesListNode);
+    return bSuccess;
 }
 
 /************************************************************************/
@@ -2837,12 +2863,14 @@ void NITFDataset::InitializeTextMetadata()
 /*                       InitializeTREMetadata()                        */
 /************************************************************************/
 
-void NITFDataset::InitializeTREMetadata()
+bool NITFDataset::InitializeTREMetadata(bool bValidate)
 
 {
-    if( oSpecialMD.GetMetadata( "TRE" ) != nullptr )
-        return;
+    if( oSpecialMD.GetMetadata( "TRE" ) != nullptr ||
+        oSpecialMD.GetMetadata( "xml:TRE" ) != nullptr )
+        return true;
 
+    bool bGotError = false;
     CPLXMLNode* psTresNode = CPLCreateXMLNode(nullptr, CXT_Element, "tres");
 
 /* -------------------------------------------------------------------- */
@@ -2888,13 +2916,15 @@ void NITFDataset::InitializeTREMetadata()
                 CPLError(CE_Failure, CPLE_AppDefined, "Invalid size (%d) for TRE %s",
                         nThisTRESize, szTemp);
                 CPLDestroyXMLNode(psTresNode);
-                return;
+                bGotError = true;
+                return bGotError;
             }
             if (nThisTRESize > nTREBytes - 11)
             {
                 CPLError(CE_Failure, CPLE_AppDefined, "Not enough bytes in TRE");
                 CPLDestroyXMLNode(psTresNode);
-                return;
+                bGotError = true;
+                return bGotError;
             }
 
             strncpy( szTag, pszTREData, 6 );
@@ -2904,7 +2934,7 @@ void NITFDataset::InitializeTREMetadata()
             while( strlen(szTag) > 0 && szTag[strlen(szTag)-1] == ' ' )
                 szTag[strlen(szTag)-1] = '\0';
 
-            CPLXMLNode* psTreNode = NITFCreateXMLTre(psFile, szTag, pszTREData + 11,nThisTRESize);
+            CPLXMLNode* psTreNode = NITFCreateXMLTre(psFile, szTag, pszTREData + 11,nThisTRESize, bValidate, &bGotError);
             if (psTreNode)
             {
                 CPLCreateXMLNode(CPLCreateXMLNode(psTreNode, CXT_Attribute, "location"),
@@ -2918,19 +2948,21 @@ void NITFDataset::InitializeTREMetadata()
                                                     CPLES_BackslashQuotable );
             if (pszEscapedData == nullptr)
             {
-                return;
+                bGotError = true;
             }
-
-            char szUniqueTag[32];
-            strcpy(szUniqueTag, szTag);
-            int nCountUnique = 2;
-            while(oSpecialMD.GetMetadataItem( szUniqueTag, "TRE") != nullptr)
+            else
             {
-                snprintf(szUniqueTag, sizeof(szUniqueTag), "%s_%d", szTag, nCountUnique);
-                nCountUnique ++;
+                char szUniqueTag[32];
+                strcpy(szUniqueTag, szTag);
+                int nCountUnique = 2;
+                while(oSpecialMD.GetMetadataItem( szUniqueTag, "TRE") != nullptr)
+                {
+                    snprintf(szUniqueTag, sizeof(szUniqueTag), "%s_%d", szTag, nCountUnique);
+                    nCountUnique ++;
+                }
+                oSpecialMD.SetMetadataItem( szUniqueTag, pszEscapedData, "TRE" );
+                CPLFree( pszEscapedData );
             }
-            oSpecialMD.SetMetadataItem( szUniqueTag, pszEscapedData, "TRE" );
-            CPLFree( pszEscapedData );
 
             nTREBytes -= (nThisTRESize + 11);
             pszTREData += (nThisTRESize + 11);
@@ -2963,14 +2995,15 @@ void NITFDataset::InitializeTREMetadata()
             {
                 NITFDESFreeTREData(pabyTREData);
                 NITFDESDeaccess(psDES);
-                return;
+                bGotError = true;
+                break;
             }
 
             // trim white off tag.
             while( strlen(szTREName) > 0 && szTREName[strlen(szTREName)-1] == ' ' )
                 szTREName[strlen(szTREName)-1] = '\0';
 
-            CPLXMLNode* psTreNode = NITFCreateXMLTre(psFile, szTREName, pabyTREData,nThisTRESize);
+            CPLXMLNode* psTreNode = NITFCreateXMLTre(psFile, szTREName, pabyTREData,nThisTRESize, bValidate, &bGotError);
             if (psTreNode)
             {
                 const char* pszDESID = CSLFetchNameValue(psDES->papszMetadata, "DESID");
@@ -3007,6 +3040,8 @@ void NITFDataset::InitializeTREMetadata()
         CPLFree(pszXML);
     }
     CPLDestroyXMLNode(psTresNode);
+
+    return !bGotError;
 }
 
 /************************************************************************/
@@ -3063,7 +3098,7 @@ char **NITFDataset::GetMetadata( const char * pszDomain )
     {
         // InitializeNITFDESs retrieves all the DES file headers (NOTE: The returned strings are base64-encoded).
 
-        InitializeNITFDESs();
+        InitializeNITFDESs(false);
         return oSpecialMD.GetMetadata( pszDomain );
     }
 
@@ -3072,7 +3107,7 @@ char **NITFDataset::GetMetadata( const char * pszDomain )
     {
         // InitializeNITFDESs retrieves all the DES file headers (NOTE: The returned strings are base64-encoded).
 
-        InitializeNITFDESMetadata();
+        InitializeNITFDESMetadata(false);
         return oSpecialMD.GetMetadata( pszDomain );
     }
 
@@ -3111,13 +3146,13 @@ char **NITFDataset::GetMetadata( const char * pszDomain )
 
     if( pszDomain != nullptr && EQUAL(pszDomain,"TRE") )
     {
-        InitializeTREMetadata();
+        InitializeTREMetadata(false);
         return oSpecialMD.GetMetadata( pszDomain );
     }
 
     if( pszDomain != nullptr && EQUAL(pszDomain,"xml:TRE") )
     {
-        InitializeTREMetadata();
+        InitializeTREMetadata(false);
         return oSpecialMD.GetMetadata( pszDomain );
     }
 
@@ -3153,7 +3188,7 @@ const char *NITFDataset::GetMetadataItem(const char * pszName,
     {
         // InitializeNITFDESs retrieves all the DES file headers (NOTE: The returned strings are base64-encoded).
 
-        InitializeNITFDESMetadata();
+        InitializeNITFDESMetadata(false);
         return oSpecialMD.GetMetadataItem( pszName, pszDomain );
     }
 
@@ -3192,7 +3227,7 @@ const char *NITFDataset::GetMetadataItem(const char * pszName,
 
     if( pszDomain != nullptr && EQUAL(pszDomain,"TRE") )
     {
-        InitializeTREMetadata();
+        InitializeTREMetadata(false);
         return oSpecialMD.GetMetadataItem( pszName, pszDomain );
     }
 
@@ -6911,6 +6946,12 @@ void GDALRegister_NITF()
     poDriver->SetMetadataItem( GDAL_DMD_SUBDATASETS, "YES" );
     poDriver->SetMetadataItem( GDAL_DMD_CREATIONDATATYPES,
                                "Byte UInt16 Int16 UInt32 Int32 Float32" );
+
+    poDriver->SetMetadataItem( GDAL_DMD_OPENOPTIONLIST,
+"<OpenOptionList>"
+"  <Option name='VALIDATE' type='boolean' description='Whether validation of metadata should be done' default='NO' />"
+"  <Option name='FAIL_IF_VALIDATION_ERROR' type='boolean' description='Whether a validation error should cause dataset opening to fail' default='NO' />"
+"</OpenOptionList>" );
 
     poDriver->SetMetadataItem( GDAL_DCAP_VIRTUALIO, "YES" );
 

autotest/gdrivers/nitf.py
@@ -3613,7 +3613,7 @@ def test_nitf_80():
 
 
 def test_nitf_81():
-    tre_data = "TRE=MSTGTA=012340123456789AB0123456789ABCDE0120123456789AB0123456789AB000123401234560123450TGT_LOC=             "
+    tre_data = "TRE=MSTGTA=012340123456789AB0123456789ABCDE0120123456789AB0123456789AB001234501234560123450TGT_LOC=             "
     ds = gdal.GetDriverByName("NITF").Create(
         "/vsimem/nitf_81.ntf", 1, 1, options=[tre_data]
     )
@@ -3635,7 +3635,7 @@ def test_nitf_81():
     <field name="TGT_LTIOV" value="0123456789AB" />
     <field name="TGT_TYPE" value="0" />
     <field name="TGT_COLL" value="0" />
-    <field name="TGT_CAT" value="01234" />
+    <field name="TGT_CAT" value="12345" />
     <field name="TGT_UTC" value="0123456" />
     <field name="TGT_ELEV" value="012345" />
     <field name="TGT_ELEV_UNIT" value="0" />
@@ -4004,7 +4004,7 @@ def test_nitf_87():
 def test_nitf_88():
     tre_data = (
         "TRE=CSWRPB=1F199.9999999900000010000002000000300000040000005000000600000070000008"
-        "1111-9.99999999999999E-99+9.99999999999999E+9900000"
+        "1111-9.99999999999999E-99+0.50000000000000E+0000000"
     )
 
     ds = gdal.GetDriverByName("NITF").Create(
@@ -4051,7 +4051,7 @@ def test_nitf_88():
           <group index="0">
             <repeated number="1">
               <group index="0">
-                <field name="B" value="+9.99999999999999E+99" />
+                <field name="B" value="+0.50000000000000E+00" />
               </group>
             </repeated>
           </group>
@@ -4233,9 +4233,9 @@ def test_nitf_RSMAPB():
 
     tre_data = (
         "TRE=RSMAPB=iid                                                                             "
-        + "edition                                 tid                                     01IG+9.99999999999999E+99"
-        + "+9.99999999999999E+99+9.99999999999999E+99+9.99999999999999E+99+9.99999999999999E+99+9.99999999999999E+99"
-        + "Y01011230001+9.99999999999999E+99+9.99999999999999E+99"
+        + "edition                                 tid                                     01IG+0.50000000000000E+00"
+        + "+0.50000000000000E+00+0.50000000000000E+00+0.50000000000000E+00+0.50000000000000E+00+0.50000000000000E+00"
+        + "Y01011230001+0.50000000000000E+00+0.50000000000000E+00"
     )
 
     ds = gdal.GetDriverByName("NITF").Create(
@@ -4257,12 +4257,12 @@ def test_nitf_RSMAPB():
     <field name="NPAR" value="01" />
     <field name="APTYP" value="I" />
     <field name="LOCTYP" value="G" />
-    <field name="NSFX" value="+9.99999999999999E+99" />
-    <field name="NSFY" value="+9.99999999999999E+99" />
-    <field name="NSFZ" value="+9.99999999999999E+99" />
-    <field name="NOFFX" value="+9.99999999999999E+99" />
-    <field name="NOFFY" value="+9.99999999999999E+99" />
-    <field name="NOFFZ" value="+9.99999999999999E+99" />
+    <field name="NSFX" value="+0.50000000000000E+00" />
+    <field name="NSFY" value="+0.50000000000000E+00" />
+    <field name="NSFZ" value="+0.50000000000000E+00" />
+    <field name="NOFFX" value="+0.50000000000000E+00" />
+    <field name="NOFFY" value="+0.50000000000000E+00" />
+    <field name="NOFFZ" value="+0.50000000000000E+00" />
     <field name="APBASE" value="Y" />
     <field name="NISAP" value="01" />
     <field name="NISAPR" value="01" />
@@ -4279,14 +4279,14 @@ def test_nitf_RSMAPB():
       <group index="0">
         <repeated number="1">
           <group index="0">
-            <field name="AEL" value="+9.99999999999999E+99" />
+            <field name="AEL" value="+0.50000000000000E+00" />
           </group>
         </repeated>
       </group>
     </repeated>
     <repeated number="1">
       <group index="0">
-        <field name="PARVAL" value="+9.99999999999999E+99" />
+        <field name="PARVAL" value="+0.50000000000000E+00" />
       </group>
     </repeated>
   </tre>
@@ -4307,8 +4307,8 @@ def test_nitf_RSMDCB():
         + "edition                                 tid                                     01001iidi"
         + " " * 76
         + "01Y01GN"
-        + "+9.99999999999999E+99" * 6
-        + "N01ABCD+9.99999999999999E+99"
+        + "+0.50000000000000E+00" * 6
+        + "N01ABCD+0.50000000000000E+00"
     )
 
     ds = gdal.GetDriverByName("NITF").Create(
@@ -4339,12 +4339,12 @@ def test_nitf_RSMDCB():
     <field name="NPAR" value="01" />
     <field name="APTYP" value="G" />
     <field name="LOCTYP" value="N" />
-    <field name="NSFX" value="+9.99999999999999E+99" />
-    <field name="NSFY" value="+9.99999999999999E+99" />
-    <field name="NSFZ" value="+9.99999999999999E+99" />
-    <field name="NOFFX" value="+9.99999999999999E+99" />
-    <field name="NOFFY" value="+9.99999999999999E+99" />
-    <field name="NOFFZ" value="+9.99999999999999E+99" />
+    <field name="NSFX" value="+0.50000000000000E+00" />
+    <field name="NSFY" value="+0.50000000000000E+00" />
+    <field name="NSFZ" value="+0.50000000000000E+00" />
+    <field name="NOFFX" value="+0.50000000000000E+00" />
+    <field name="NOFFY" value="+0.50000000000000E+00" />
+    <field name="NOFFZ" value="+0.50000000000000E+00" />
     <field name="APBASE" value="N" />
     <field name="NGSAP" value="01" />
     <repeated number="1">
@@ -4358,7 +4358,7 @@ def test_nitf_RSMDCB():
           <group index="0">
             <repeated number="1">
               <group index="0">
-                <field name="CRSCOV" value="+9.99999999999999E+99" />
+                <field name="CRSCOV" value="+0.50000000000000E+00" />
               </group>
             </repeated>
           </group>
@@ -4382,15 +4382,15 @@ def test_nitf_RSMECB():
         "TRE=RSMECB=iid                                                                             "
         + "edition                                 tid                                     "
         + "YY01012020110201GN"
-        + "+9.99999999999999E+99" * 6
+        + "+0.50000000000000E+00" * 6
         + "N01ABCD02"
-        + "+9.99999999999999E+99" * 3
+        + "+0.50000000000000E+00" * 3
         + "1N2"
-        + "+9.99999999999999E+99" * 8
+        + "+0.50000000000000E+00" * 8
         + "N2"
-        + "+9.99999999999999E+99" * 4
+        + "+0.50000000000000E+00" * 4
         + "2"
-        + "+9.99999999999999E+99" * 4
+        + "+0.50000000000000E+00" * 4
     )
 
     ds = gdal.GetDriverByName("NITF").Create(
@@ -4417,12 +4417,12 @@ def test_nitf_RSMECB():
     <field name="NPAR" value="01" />
     <field name="APTYP" value="G" />
     <field name="LOCTYP" value="N" />
-    <field name="NSFX" value="+9.99999999999999E+99" />
-    <field name="NSFY" value="+9.99999999999999E+99" />
-    <field name="NSFZ" value="+9.99999999999999E+99" />
-    <field name="NOFFX" value="+9.99999999999999E+99" />
-    <field name="NOFFY" value="+9.99999999999999E+99" />
-    <field name="NOFFZ" value="+9.99999999999999E+99" />
+    <field name="NSFX" value="+0.50000000000000E+00" />
+    <field name="NSFY" value="+0.50000000000000E+00" />
+    <field name="NSFZ" value="+0.50000000000000E+00" />
+    <field name="NOFFX" value="+0.50000000000000E+00" />
+    <field name="NOFFY" value="+0.50000000000000E+00" />
+    <field name="NOFFZ" value="+0.50000000000000E+00" />
     <field name="APBASE" value="N" />
     <field name="NGSAP" value="01" />
     <repeated number="1">
@@ -4435,13 +4435,13 @@ def test_nitf_RSMECB():
         <field name="NUMOPG" value="02" />
         <repeated number="3">
           <group index="0">
-            <field name="ERRCVG" value="+9.99999999999999E+99" />
+            <field name="ERRCVG" value="+0.50000000000000E+00" />
           </group>
           <group index="1">
-            <field name="ERRCVG" value="+9.99999999999999E+99" />
+            <field name="ERRCVG" value="+0.50000000000000E+00" />
           </group>
           <group index="2">
-            <field name="ERRCVG" value="+9.99999999999999E+99" />
+            <field name="ERRCVG" value="+0.50000000000000E+00" />
           </group>
         </repeated>
         <field name="TCDF" value="1" />
@@ -4449,12 +4449,12 @@ def test_nitf_RSMECB():
         <field name="NCSEG" value="2" />
         <repeated number="2">
           <group index="0">
-            <field name="CORSEG" value="+9.99999999999999E+99" />
-            <field name="TAUSEG" value="+9.99999999999999E+99" />
+            <field name="CORSEG" value="+0.50000000000000E+00" />
+            <field name="TAUSEG" value="+0.50000000000000E+00" />
           </group>
           <group index="1">
-            <field name="CORSEG" value="+9.99999999999999E+99" />
-            <field name="TAUSEG" value="+9.99999999999999E+99" />
+            <field name="CORSEG" value="+0.50000000000000E+00" />
+            <field name="TAUSEG" value="+0.50000000000000E+00" />
           </group>
         </repeated>
       </group>
@@ -4463,35 +4463,35 @@ def test_nitf_RSMECB():
       <group index="0">
         <repeated number="1">
           <group index="0">
-            <field name="MAP" value="+9.99999999999999E+99" />
+            <field name="MAP" value="+0.50000000000000E+00" />
           </group>
         </repeated>
       </group>
     </repeated>
-    <field name="URR" value="+9.99999999999999E+99" />
-    <field name="URC" value="+9.99999999999999E+99" />
-    <field name="UCC" value="+9.99999999999999E+99" />
+    <field name="URR" value="+0.50000000000000E+00" />
+    <field name="URC" value="+0.50000000000000E+00" />
+    <field name="UCC" value="+0.50000000000000E+00" />
     <field name="UACSMC" value="N" />
     <field name="UNCSR" value="2" />
     <repeated number="2">
       <group index="0">
-        <field name="UCORSR" value="+9.99999999999999E+99" />
-        <field name="UTAUSR" value="+9.99999999999999E+99" />
+        <field name="UCORSR" value="+0.50000000000000E+00" />
+        <field name="UTAUSR" value="+0.50000000000000E+00" />
       </group>
       <group index="1">
-        <field name="UCORSR" value="+9.99999999999999E+99" />
-        <field name="UTAUSR" value="+9.99999999999999E+99" />
+        <field name="UCORSR" value="+0.50000000000000E+00" />
+        <field name="UTAUSR" value="+0.50000000000000E+00" />
       </group>
     </repeated>
     <field name="UNCSC" value="2" />
     <repeated number="2">
       <group index="0">
-        <field name="UCORSC" value="+9.99999999999999E+99" />
-        <field name="UTAUSC" value="+9.99999999999999E+99" />
+        <field name="UCORSC" value="+0.50000000000000E+00" />
+        <field name="UTAUSC" value="+0.50000000000000E+00" />
       </group>
       <group index="1">
-        <field name="UCORSC" value="+9.99999999999999E+99" />
-        <field name="UTAUSC" value="+9.99999999999999E+99" />
+        <field name="UCORSC" value="+0.50000000000000E+00" />
+        <field name="UTAUSC" value="+0.50000000000000E+00" />
       </group>
     </repeated>
   </tre>
@@ -4914,8 +4914,12 @@ def test_nitf_SENSRB():
     <field name="IMAGE_FORMATION_DATA" value="Y" />
     <field name="METHOD" value="" />
     <field name="MODE" value="" />
-    <field name="ROW_COUNT" value="00000000" />
-    <field name="COLUMN_COUNT" value="00000000" />
+    <field name="ROW_COUNT" value="00000000">
+      <warning>Minimum value constraint of 1 not met</warning>
+    </field>
+    <field name="COLUMN_COUNT" value="00000000">
+      <warning>Minimum value constraint of 1 not met</warning>
+    </field>
     <field name="ROW_SET" value="00000000" />
     <field name="COLUMN_SET" value="00000000" />
     <field name="ROW_RATE" value="0000000000" />
@@ -5299,6 +5303,119 @@ def test_nitf_no_image_segment():
 
     with gdaltest.error_handler():
         gdal.GetDriverByName("NITF").Delete(out_filename)
+
+
+###############################################################################
+# Test TRE metadata validation
+
+
+def test_nitf_metadata_validation_tre():
+
+    filename = "/vsimem/test_nitf_metadata_validation_tre.ntf"
+    with gdaltest.error_handler():
+        gdal.GetDriverByName("NITF").Create(
+            filename,
+            1,
+            1,
+            options=[
+                "TRE=BLOCKA=010000001000360-10                +42.319331+020.078400+42.317083+020.126072+42.281634+020.122570+42.283881+020.074924"
+            ],
+        )
+    gdal.ErrorReset()
+    with gdaltest.error_handler():
+        ds = gdal.OpenEx(filename, open_options=["VALIDATE=YES"])
+    assert gdal.GetLastErrorMsg() != ""
+    md = ds.GetMetadata("xml:TRE")[0]
+    assert (
+        md
+        == """<tres>
+  <tre name="BLOCKA" location="image">
+    <error>BLOCKA TRE wrong size (118). Expected 123.</error>
+    <field name="BLOCK_INSTANCE" value="01" />
+    <field name="N_GRAY" value="00000" />
+    <field name="L_LINES" value="01000" />
+    <field name="LAYOVER_ANGLE" value="360">
+      <error>Maximum value constraint of 359 not met</error>
+    </field>
+    <field name="SHADOW_ANGLE" value="-10">
+      <error>Minimum value constraint of 0 not met</error>
+    </field>
+    <field name="FRLC_LOC" value="+42.319331+020.078400" />
+    <field name="LRLC_LOC" value="+42.317083+020.126072" />
+    <field name="LRFC_LOC" value="+42.281634+020.122570" />
+    <field name="FRFC_LOC" value="+42.283881+020.074924" />
+  </tre>
+</tres>
+"""
+    )
+
+    with gdaltest.error_handler():
+        ds = gdal.OpenEx(
+            filename, open_options=["VALIDATE=YES", "FAIL_IF_VALIDATION_ERROR=YES"]
+        )
+    assert ds is None
+
+    gdal.Unlink(filename)
+
+
+###############################################################################
+# Test DES metadata validation
+
+
+def test_nitf_metadata_validation_des():
+
+    filename = "/vsimem/test_nitf_metadata_validation_des.ntf"
+    des_data = b"02U" + b" " * 166 + b"0004ABCD"
+    escaped_data = gdal.EscapeString(des_data, gdal.CPLES_BackslashQuotable)
+    with gdaltest.error_handler():
+        gdal.GetDriverByName("NITF").Create(
+            filename, 1, 1, options=[b"DES=CSATTA DES=" + escaped_data]
+        )
+    gdal.ErrorReset()
+    with gdaltest.error_handler():
+        ds = gdal.OpenEx(filename, open_options=["VALIDATE=YES"])
+    assert gdal.GetLastErrorMsg() != ""
+    md = ds.GetMetadata("xml:DES")[0]
+    assert (
+        md
+        == """<des_list>
+  <des name="CSATTA DES">
+    <field name="DESVER" value="02" />
+    <field name="DECLAS" value="U" />
+    <field name="DESCLSY" value="" />
+    <field name="DESCODE" value="" />
+    <field name="DESCTLH" value="" />
+    <field name="DESREL" value="" />
+    <field name="DESDCTP" value="" />
+    <field name="DESDCDT" value="" />
+    <field name="DESDCXM" value="" />
+    <field name="DESDG" value="" />
+    <field name="DESDGDT" value="" />
+    <field name="DESCLTX" value="" />
+    <field name="DESCATP" value="" />
+    <field name="DESCAUT" value="" />
+    <field name="DESCRSN" value="" />
+    <field name="DESSRDT" value="" />
+    <field name="DESCTLN" value="" />
+    <field name="DESSHL" value="0004" />
+    <field name="DESSHF" value="ABCD">
+      <user_defined_fields>
+        <error>CSATTA DES DES wrong size (4). Expected 52.</error>
+      </user_defined_fields>
+    </field>
+    <field name="DESDATA" value="" />
+  </des>
+</des_list>
+"""
+    )
+
+    with gdaltest.error_handler():
+        ds = gdal.OpenEx(
+            filename, open_options=["VALIDATE=YES", "FAIL_IF_VALIDATION_ERROR=YES"]
+        )
+    assert ds is None
+
+    gdal.Unlink(filename)
 
 
 ###############################################################################

frmts/nitf/nitfdataset.h
@@ -94,11 +94,11 @@ class NITFDataset final: public GDALPamDataset
     void         InitializeNITFDESMetadata();
     void         InitializeNITFTREs();
 #endif
-    void         InitializeNITFDESs();
+    bool         InitializeNITFDESs(bool bValidate);
     void         InitializeNITFMetadata();
     void         InitializeCGMMetadata();
     void         InitializeTextMetadata();
-    void         InitializeTREMetadata();
+    bool         InitializeTREMetadata(bool bValidate);
     void         InitializeImageStructureMetadata();
 
     GIntBig     *panJPEGBlockOffset;
@@ -127,6 +127,8 @@ class NITFDataset final: public GDALPamDataset
 
     int          bExposeUnderlyingJPEGDatasetOverviews;
     int          ExposeUnderlyingJPEGDatasetOverviews() const { return bExposeUnderlyingJPEGDatasetOverviews; }
+
+    bool         Validate();
 
   protected:
     virtual int         CloseDependentDatasets() override;

frmts/nitf/nitfdes.c
@@ -562,7 +562,7 @@ end:
 /*                              NITFDESGetXml()                         */
 /************************************************************************/
 
-CPLXMLNode* NITFDESGetXml(NITFFile* psFile, int iSegment)
+CPLXMLNode* NITFDESGetXml(NITFFile* psFile, int iSegment, bool bValidate, bool* pbGotError)
 {
     CPLXMLNode* psDesNode;
     char** papszTmp;
@@ -618,7 +618,8 @@ CPLXMLNode* NITFDESGetXml(NITFFile* psFile, int iSegment)
             if (strcmp(pszMDname, "DESSHF") == 0)
             {
                 CPLAddXMLAttributeAndValue(psFieldNode, "value", pszMDval);
-                CPLXMLNode* psChild = NITFCreateXMLDesUserDefinedSubHeader(psFile, psDes);
+                CPLXMLNode* psChild = NITFCreateXMLDesUserDefinedSubHeader(
+                    psFile, psDes, bValidate, pbGotError);
                 if( psChild )
                 {
                     CPLAddXMLChild(psFieldNode, psChild);
@@ -642,7 +643,8 @@ CPLXMLNode* NITFDESGetXml(NITFFile* psFile, int iSegment)
                 }
 
                 CPLAddXMLAttributeAndValue(psFieldNode, "value", pszBase64);
-                CPLXMLNode* psChild = NITFCreateXMLDesDataFields(psFile, psDes, (GByte*)pszUnescaped, nLen);
+                CPLXMLNode* psChild = NITFCreateXMLDesDataFields(
+                    psFile, psDes, (GByte*)pszUnescaped, nLen, bValidate, pbGotError);
                 if( psChild )
                 {
                     CPLAddXMLChild(psFieldNode, psChild);

frmts/nitf/nitflib.h
@@ -36,6 +36,8 @@
 #include "cpl_vsi.h"
 #include "cpl_minixml.h"
 
+#include <stdbool.h>
+
 CPL_C_START
 
 /* 1e-12 - 1 */
@@ -282,14 +284,18 @@ void      CPL_DLL  NITFDESFreeTREData( char* pabyTREData );
 int       CPL_DLL  NITFDESExtractShapefile(NITFDES* psDES, const char* pszRadixFileName);
 
 CPLXMLNode* NITFCreateXMLDesUserDefinedSubHeader(NITFFile* psFile,
-                                                 const NITFDES* psDES);
+                                                 const NITFDES* psDES,
+                                                 bool bValidate,
+                                                 bool* pbGotError);
 
 CPLXMLNode* NITFCreateXMLDesDataFields(NITFFile* psFile,
                                        const NITFDES* psDES,
                                        const GByte* pabyData,
-                                       int nDataLen);
+                                       int nDataLen,
+                                       bool bValidate,
+                                       bool* pbGotError);
 
-CPLXMLNode CPL_DLL *NITFDESGetXml(NITFFile*, int iSegment);
+CPLXMLNode CPL_DLL *NITFDESGetXml(NITFFile*, int iSegment, bool bValidate, bool* pbGotError);
 
 /* -------------------------------------------------------------------- */
 /*      These are really intended to be private helper stuff for the    */
@@ -434,7 +440,9 @@ char **NITFGenericMetadataRead(char **papszMD,
 CPLXMLNode* NITFCreateXMLTre(NITFFile* psFile,
                              const char* pszTREName,
                              const char *pachTRE,
-                             int nTRESize);
+                             int nTRESize,
+                             bool bValidate,
+                             bool* pbGotError);
 
 CPL_C_END
 

commit 4a3b93cb3ba3745c934b1c4ff00e4e1960cf9f63

NITFDataset::InitializeTREMetadata(): fix use-after-free in error code path (master only, issue introduced in 7cc19d0484a)

frmts/nitf/nitfdataset.cpp
@@ -2994,7 +2994,6 @@ bool NITFDataset::InitializeTREMetadata(bool bValidate)
             if (pszEscapedData == nullptr)
             {
                 NITFDESFreeTREData(pabyTREData);
-                NITFDESDeaccess(psDES);
                 bGotError = true;
                 break;
             }