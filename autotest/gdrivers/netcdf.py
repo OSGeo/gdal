7774efce2475e5055d957b4b90846a695d583e10
Michael Sumner <mdsumner@gmail.com>
2022-12-16 06:46:47 +1100

    Update autotest/gdrivers/netcdf.py
    Co-authored-by: Even Rouault <even.rouault@spatialys.com>

--- autotest/gdrivers/netcdf.py
+++ autotest/gdrivers/netcdf.py
@@ -1118,10 +1118,7 @@ def test_netcdf_assume_longlat():
         ds = gdal.Open("data/netcdf/trmm-nc2.nc")
         srs = ds.GetSpatialRef()
         assert srs is not None
-        assert (
-            srs.ExportToWkt()
-            == 'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AXIS["Longitude",EAST],AXIS["Latitude",NORTH]]'
-        )
+        assert srs.ExportToWkt().startswith('GEOGCS["WGS 84')
     with gdaltest.config_option("GDAL_NETCDF_ASSUME_LONGLAT", "NO"):
         ds = gdal.Open("data/netcdf/trmm-nc2.nc")
         assert ds.GetSpatialRef() is None
