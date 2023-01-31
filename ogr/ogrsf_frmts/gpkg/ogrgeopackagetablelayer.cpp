OSGeo/gdal

commit 08841c53632c6fbfe13719a6711a3230c854e9c4

GPKG: disable async RTree building when creating several layers

ogr/ogrsf_frmts/gpkg/ogrgeopackagetablelayer.cpp
@@ -2349,7 +2349,8 @@ void OGRGeoPackageTableLayer::SetDeferredSpatialIndexCreation(bool bFlag)
     if (bFlag)
     {
         m_bAllowedRTreeThread =
-            sqlite3_threadsafe() != 0 && CPLGetNumCPUs() >= 2 &&
+            m_poDS->GetLayerCount() == 1 && sqlite3_threadsafe() != 0 &&
+            CPLGetNumCPUs() >= 2 &&
             CPLTestBool(
                 CPLGetConfigOption("OGR_GPKG_ALLOW_THREADED_RTREE", "YES"));
 
@@ -2492,6 +2493,19 @@ void OGRGeoPackageTableLayer::CancelAsyncRTree()
     }
     m_bErrorDuringRTreeThread = true;
     RemoveAsyncRTreeTempDB();
+}
+
+/************************************************************************/
+/*                     FinishOrDisableThreadedRTree()                   */
+/************************************************************************/
+
+void OGRGeoPackageTableLayer::FinishOrDisableThreadedRTree()
+{
+    if (m_bThreadRTreeStarted)
+    {
+        CreateSpatialIndexIfNecessary();
+    }
+    m_bAllowedRTreeThread = false;
 }
 
 /************************************************************************/

ogr/ogrsf_frmts/gpkg/ogr_geopackage.h
@@ -789,6 +789,7 @@ class OGRGeoPackageTableLayer final : public OGRGeoPackageLayer
     }
 
     void CreateSpatialIndexIfNecessary();
+    void FinishOrDisableThreadedRTree();
     bool CreateSpatialIndex(const char *pszTableName = nullptr);
     bool DropSpatialIndex(bool bCalledFromSQLFunction = false);
     CPLString ReturnSQLCreateSpatialIndexTriggers(const char *pszTableName,

ogr/ogrsf_frmts/gpkg/ogrgeopackagedatasource.cpp
@@ -6367,6 +6367,14 @@ OGRLayer *GDALGeoPackageDataset::ICreateLayer(const char *pszLayerName,
         }
     }
 
+    if (m_nLayers == 1)
+    {
+        // Async RTree building doesn't play well with multiple layer:
+        // SQLite3 locks being hold for a long time, random failed commits,
+        // etc.
+        m_papoLayers[0]->FinishOrDisableThreadedRTree();
+    }
+
     /* Create a blank layer. */
     auto poLayer = std::unique_ptr<OGRGeoPackageTableLayer>(
         new OGRGeoPackageTableLayer(this, pszLayerName));

commit 4f94984a0ad0ad4c68969c8d5192847214fb5074

GPKG: fix async RTree building for 1st layer (broken by 08841c53632c6fbfe13719a6711a3230c854e9c4)

ogr/ogrsf_frmts/gpkg/ogrgeopackagetablelayer.cpp
@@ -2348,8 +2348,10 @@ void OGRGeoPackageTableLayer::SetDeferredSpatialIndexCreation(bool bFlag)
     m_bDeferredSpatialIndexCreation = bFlag;
     if (bFlag)
     {
+        // This method is invoked before the layer is added to the dataset,
+        // so GetLayerCount() will return 0 for the first layer added.
         m_bAllowedRTreeThread =
-            m_poDS->GetLayerCount() == 1 && sqlite3_threadsafe() != 0 &&
+            m_poDS->GetLayerCount() == 0 && sqlite3_threadsafe() != 0 &&
             CPLGetNumCPUs() >= 2 &&
             CPLTestBool(
                 CPLGetConfigOption("OGR_GPKG_ALLOW_THREADED_RTREE", "YES"));