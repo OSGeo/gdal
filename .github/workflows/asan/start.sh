OSGeo/gdal

commit ba6c43041f872ac325a79492788d07d7f3e3a09f

autotest: add PROJ grids needed for tests and restrict PROJ search paths to them (fixes #7070)

autotest/CMakeLists.txt
@@ -74,14 +74,14 @@ if (Python_Interpreter_FOUND)
 
   endfunction ()
 
-  # Needed for a autotest/osr test
   if (NOT "${CMAKE_BINARY_DIR}" STREQUAL "${CMAKE_SOURCE_DIR}")
-      symlink_or_copy(${CMAKE_CURRENT_SOURCE_DIR}/cpp/data ${CMAKE_CURRENT_BINARY_DIR}/cpp/data)
-      if (SKIP_COPYING_AUTOTEST_SUBDIRS)
-        message(STATUS "Skipping copying ${CMAKE_CURRENT_SOURCE_DIR}/pymod")
-      else ()
-        symlink_or_copy(${CMAKE_CURRENT_SOURCE_DIR}/pymod ${CMAKE_CURRENT_BINARY_DIR}/pymod)
-      endif ()
+      foreach (subdir IN ITEMS pymod proj_grids cpp/data)
+          if (SKIP_COPYING_AUTOTEST_SUBDIRS)
+            message(STATUS "Skipping copying ${CMAKE_CURRENT_SOURCE_DIR}/${subdir}")
+          else ()
+            symlink_or_copy(${CMAKE_CURRENT_SOURCE_DIR}/${subdir} ${CMAKE_CURRENT_BINARY_DIR}/${subdir})
+          endif ()
+      endforeach ()
   endif()
 
   foreach (

autotest/conftest.py
@@ -34,6 +34,26 @@ if "APPLY_LOCALE" in os.environ:
     locale.setlocale(locale.LC_ALL, "")
 
 
+def setup_proj_search_paths():
+    proj_grids_path = os.path.join(os.path.dirname(__file__), "proj_grids")
+    assert os.path.exists(proj_grids_path)
+    import shutil
+
+    from osgeo import osr
+
+    proj_db_tmpdir = os.path.join(os.path.dirname(__file__), "proj_db_tmpdir")
+    if not os.path.exists(proj_db_tmpdir):
+        os.mkdir(proj_db_tmpdir, 0o755)
+    for path in osr.GetPROJSearchPaths():
+        if os.path.exists(os.path.join(path, "proj.db")):
+            shutil.copy(os.path.join(path, "proj.db"), proj_db_tmpdir)
+            osr.SetPROJSearchPaths([proj_db_tmpdir, proj_grids_path])
+            break
+
+
+setup_proj_search_paths()
+
+
 @pytest.fixture(scope="module", autouse=True)
 def chdir_to_test_file(request):
     """

autotest/cpp/CMakeLists.txt
@@ -120,6 +120,7 @@ endif ()
 if (GDAL_USE_CURL)
   target_compile_definitions(gdal_unit_test PRIVATE -DHAVE_CURL)
 endif ()
+target_compile_definitions(gdal_unit_test PRIVATE "-DPROJ_DB_TMPDIR=\"${CMAKE_CURRENT_BINARY_DIR}/proj_db_tmpdir\"" "-DPROJ_GRIDS_PATH=\"${CMAKE_CURRENT_SOURCE_DIR}/../proj_grids\"")
 
 # gtest with lots of assertion can be very slow to build in optimized mode.
 # As we don't need optimizations for that unit test, override it with -O0 for gcc-style compilers

autotest/cpp/gdal_unit_test.cpp
@@ -164,6 +164,35 @@ CheckEqualGeometries(OGRGeometryH lhs, OGRGeometryH rhs, double tolerance)
 
 int main(int argc, char *argv[])
 {
+#if defined(PROJ_GRIDS_PATH) && defined(PROJ_DB_TMPDIR)
+    // Look for proj.db in PROJ search paths, copy it in PROJ_DB_TMPDIR, and restrict
+    // PROJ search paths to PROJ_DB_TMPDIR and PROJ_GRIDS_PATH
+    VSIMkdir(PROJ_DB_TMPDIR, 0755);
+    static char szProjNetworkOff[] = "PROJ_NETWORK=OFF";
+    putenv(szProjNetworkOff);
+    const CPLStringList aosPathsOri(OSRGetPROJSearchPaths());
+    bool bFoundProjDB = false;
+    for (int i = 0; i < aosPathsOri.size(); ++i)
+    {
+        VSIStatBufL sStat;
+        if (VSIStatL(CPLFormFilename(aosPathsOri[i], "proj.db", nullptr),
+                     &sStat) == 0)
+        {
+            CPLCopyFile(CPLFormFilename(PROJ_DB_TMPDIR, "proj.db", nullptr),
+                        CPLFormFilename(aosPathsOri[i], "proj.db", nullptr));
+            bFoundProjDB = true;
+            break;
+        }
+    }
+    if (bFoundProjDB)
+    {
+        CPLStringList aosPaths;
+        aosPaths.AddString(PROJ_DB_TMPDIR);
+        aosPaths.AddString(PROJ_GRIDS_PATH);
+        OSRSetPROJSearchPaths(aosPaths.List());
+    }
+#endif
+
     // Register GDAL/OGR drivers
     ::GDALAllRegister();
     ::OGRRegisterAll();

autotest/cpp/test_osr_ct.cpp
@@ -183,14 +183,14 @@ TEST_F(test_osr_ct, GetInverse)
     ASSERT_TRUE(poInverse->GetTargetCS() != nullptr);
     ASSERT_TRUE(poInverse->GetTargetCS()->IsSame(&oSRSSource));
 
-    double x = 44;
-    double y = -60;
+    double x = 40;
+    double y = -100;
     ASSERT_TRUE(poCT->Transform(1, &x, &y));
     // Check that the transformed point is different but not too far
-    EXPECT_TRUE(fabs(x - 44) > 1e-10);
-    EXPECT_TRUE(fabs(y - -60) > 1e-10);
-    EXPECT_NEAR(x, 44, 1e-3);
-    EXPECT_NEAR(y, -60, 1e-3);
+    EXPECT_TRUE(fabs(x - 40) > 1e-10);
+    EXPECT_TRUE(fabs(y - -100) > 1e-10);
+    EXPECT_NEAR(x, 40, 1e-3);
+    EXPECT_NEAR(y, -100, 1e-3);
     const double xTransformed = x;
     const double yTransformed = y;
 
@@ -199,8 +199,8 @@ TEST_F(test_osr_ct, GetInverse)
     // Check that the transformed point with the inverse transformation
     // matches the source
     ASSERT_TRUE(poInverse->Transform(1, &x, &y));
-    EXPECT_NEAR(x, 44, 1e-8);
-    EXPECT_NEAR(y, -60, 1e-8);
+    EXPECT_NEAR(x, 40, 1e-8);
+    EXPECT_NEAR(y, -100, 1e-8);
 
     auto poInvOfInv =
         std::unique_ptr<OGRCoordinateTransformation>(poInverse->GetInverse());

autotest/osr/osr_basic.py
@@ -1771,6 +1771,7 @@ def test_SetPROJSearchPath():
         with gdaltest.error_handler():
             arg[0] = sr.ImportFromEPSG(32631)
 
+    backup_search_paths = osr.GetPROJSearchPaths()
     try:
         arg = [-1]
 
@@ -1786,8 +1787,7 @@ def test_SetPROJSearchPath():
         thread.join()
         assert arg[0] > 0
     finally:
-        # Cancel search path (we can't call SetPROJSearchPath(None))
-        osr.SetPROJSearchPaths([])
+        osr.SetPROJSearchPaths(backup_search_paths)
 
     sr = osr.SpatialReference()
     assert sr.ImportFromEPSG(32631) == 0

autotest/osr/osr_ct_proj.py
@@ -160,12 +160,12 @@ transform_list = [
         "EPSG:4979",
         (31.5656461890308, -121.213327277021, 0.0),
         0.1,
-        "+proj=utm +zone=11 +datum=WGS84 +geoidgrids=egm96_15.gtx +units=us-ft",
+        "+proj=utm +zone=11 +datum=WGS84 +geoidgrids=egm96_15_extract.gtx +units=us-ft",
         (328083.333225467, 11482916.6665952, 41.4697855726348),
         0.01,
         "EGM 96 Conversion",
         None,
-        "egm96_15.gtx",
+        "egm96_15_extract.gtx",
         "6.2.1",
     ),
     # Test optimization in case of identical projections (projected)
@@ -283,7 +283,7 @@ transform_list = [
 map_old_grid_name_to_tif_ones = {
     "ntf_r93.gsb": "fr_ign_ntf_r93.tif",
     "BETA2007.gsb": "de_adv_BETA2007.tif",
-    "egm96_15.gtx": "us_nga_egm96_15.tif",
+    "egm96_15_extract.gtx": "egm96_15_extract.gtx",
 }
 
 ###############################################################################

commit 8acd537348817779eb782a81ab6f1f89747d4318

autotest: fix 'random' issue with corrupted proj.db when running parallel ctest
This fixes the issue caused by ba6c43041f872ac325a79492788d07d7f3e3a09f
where concurrent autotest_XXXX targets could compete in copying proj.db
to build_dir/autotest/proj_db_tmpdir, and thus resulting in a process
reading a proj.db file that is being overwritten by another one.

Credits to @dbaston and its #7205 attempt that turned out to be a
reliable reproducer for that issue that was only random in master...

.github/workflows/asan/start.sh
@@ -83,7 +83,7 @@ SANITIZE_FLAGS="-DMAKE_SANITIZE_HAPPY -fsanitize=unsigned-integer-overflow -fno-
 mkdir build
 cd build
 export LDFLAGS="-fsanitize=undefined -fsanitize=address -shared-libasan -lstdc++"
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS="${SANITIZE_FLAGS} -Werror" -DCMAKE_CXX_FLAGS="${SANITIZE_FLAGS} -Werror" -DCMAKE_INSTALL_PREFIX=/usr -DGDAL_USE_GEOTIFF_INTERNAL=ON -DGDAL_USE_TIFF_INTERNAL=ON -DFileGDB_ROOT:PATH=$PWD/../FileGDB_API-64gcc51 -DFileGDB_LIBRARY=/usr/lib/libFileGDBAPI.so
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS="${SANITIZE_FLAGS} -Werror" -DCMAKE_CXX_FLAGS="${SANITIZE_FLAGS} -Werror" -DCMAKE_INSTALL_PREFIX=/usr -DGDAL_USE_GEOTIFF_INTERNAL=ON -DGDAL_USE_TIFF_INTERNAL=ON -DFileGDB_ROOT:PATH=$PWD/../FileGDB_API-64gcc51 -DFileGDB_LIBRARY=/usr/lib/libFileGDBAPI.so -DSKIP_RUN_SETUP_PROJ_DB_TMPDIR=ON
 make -j$NPROC
 
 sudo rm -f /usr/lib/libgdal.so*
@@ -105,11 +105,6 @@ export PRELOAD=$(clang -print-file-name=libclang_rt.asan-x86_64.so)
 sudo python3 -m pip install -U -r autotest/requirements.txt
 sudo python3 -m pip install -U hdbcli
 
-cd build/autotest
-
-# Don't run these
-rm -f ogr/ogr_fgdb.py ogr/ogr_pgeo.py
-
 # Run each module in its own pytest process.
 # This makes sure the output from the address sanitizer is relevant
 # and it doesn't blow out RAM too much.
@@ -124,11 +119,20 @@ rm -f ogr/ogr_fgdb.py ogr/ogr_pgeo.py
 export SKIP_MEM_INTENSIVE_TEST=YES
 export SKIP_VIRTUALMEM=YES
 export LD_PRELOAD=$PRELOAD
-export ASAN_OPTIONS=allocator_may_return_null=1:symbolize=1:suppressions=$PWD/../../autotest/asan_suppressions.txt
-export LSAN_OPTIONS=detect_leaks=1,print_suppressions=0,suppressions=$PWD/../../autotest/lsan_suppressions.txt
+export ASAN_OPTIONS=allocator_may_return_null=1:symbolize=1:suppressions=$PWD/autotest/asan_suppressions.txt
+export LSAN_OPTIONS=detect_leaks=1,print_suppressions=0,suppressions=$PWD/autotest/lsan_suppressions.txt
 
-gdalinfo gcore/data/byte.tif
+gdalinfo autotest/gcore/data/byte.tif
 python3 -c "from osgeo import gdal; print('yes')"
+
+# Normally run at 'make' stage for regular builds, but given this is an ASAN build,
+# python couldn't run without the above LD_PRELOAD. So run it manually
+python3 autotest/setup_proj_db_tmpdir.py build/autotest
+
+cd build/autotest
+
+# Don't run these
+rm -f ogr/ogr_fgdb.py ogr/ogr_pgeo.py
 
 echo "#!/bin/sh" > pytest_wrapper.sh
 echo 'ARGS="$*"' >> pytest_wrapper.sh

.github/workflows/windows_build.yml
@@ -184,7 +184,8 @@ jobs:
             $env:CMAKE_PREFIX_PATH="-DCMAKE_PREFIX_PATH=" + $env:SDK_PREFIX
             $env:MYSQL_LIBRARY="-DMYSQL_LIBRARY=" + $env:SDK_LIB + "\libmysql.lib"
             $env:POPPLER_EXTRA_LIBRARIES="-DPOPPLER_EXTRA_LIBRARIES=" + $env:SDK_LIB + "\freetype.lib;" + $env:SDK_LIB + "\harfbuzz.lib"
-            cmake -G $env:VS_VERSION -A $env:CMAKE_ARCHITECTURE .. $env:CMAKE_INSTALL_PREFIX -DCMAKE_BUILD_TYPE=Release $env:CMAKE_PREFIX_PATH -DCMAKE_C_FLAGS=" /WX $env:ARCH_FLAGS" -DCMAKE_CXX_FLAGS=" /WX $env:ARCH_FLAGS" -DGDAL_USE_DEFLATE=OFF $env:PROJ_ROOT $env:MYSQL_LIBRARY $env:POPPLER_EXTRA_LIBRARIES -DGDAL_USE_ZLIB_INTERNAL=ON -DECW_INTERFACE_COMPILE_DEFINITIONS="_MBCS;_UNICODE;UNICODE;_WINDOWS;LIBECWJ2;WIN32;_WINDLL;NO_X86_MMI" -DBUILD_CSHARP_BINDINGS=OFF -DBUILD_JAVA_BINDINGS=OFF
+            # -DSKIP_RUN_SETUP_PROJ_DB_TMPDIR=ON because we can't actually load gdal.dll (cf last line of script)
+            cmake -G $env:VS_VERSION -A $env:CMAKE_ARCHITECTURE .. $env:CMAKE_INSTALL_PREFIX -DCMAKE_BUILD_TYPE=Release $env:CMAKE_PREFIX_PATH -DCMAKE_C_FLAGS=" /WX $env:ARCH_FLAGS" -DCMAKE_CXX_FLAGS=" /WX $env:ARCH_FLAGS" -DGDAL_USE_DEFLATE=OFF $env:PROJ_ROOT $env:MYSQL_LIBRARY $env:POPPLER_EXTRA_LIBRARIES -DGDAL_USE_ZLIB_INTERNAL=ON -DECW_INTERFACE_COMPILE_DEFINITIONS="_MBCS;_UNICODE;UNICODE;_WINDOWS;LIBECWJ2;WIN32;_WINDLL;NO_X86_MMI" -DBUILD_CSHARP_BINDINGS=OFF -DBUILD_JAVA_BINDINGS=OFF -DSKIP_RUN_SETUP_PROJ_DB_TMPDIR=ON
             exec { cmake --build . --config Release --target install }
 
       - name: Run tests

autotest/CMakeLists.txt
@@ -164,4 +164,13 @@ int main(int argc, char **argv) {
       DEPENDS ${GDAL_LIB_TARGET_NAME} gdalapps python_binding pytest_runner
       WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
   endif ()
+
+  if (BUILD_PYTHON_BINDINGS AND NOT SKIP_RUN_SETUP_PROJ_DB_TMPDIR)
+      add_custom_target(
+          run_setup_proj_db_tmpdir
+          ALL
+          COMMAND ${CMAKE_COMMAND} -E env ${PYTHON_RUN_ENV} ${Python_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/setup_proj_db_tmpdir.py ${CMAKE_CURRENT_BINARY_DIR}
+          DEPENDS ${GDAL_LIB_TARGET_NAME} python_binding)
+  endif()
+
 endif ()

autotest/conftest.py
@@ -37,18 +37,15 @@ if "APPLY_LOCALE" in os.environ:
 def setup_proj_search_paths():
     proj_grids_path = os.path.join(os.path.dirname(__file__), "proj_grids")
     assert os.path.exists(proj_grids_path)
-    import shutil
 
     from osgeo import osr
 
-    proj_db_tmpdir = os.path.join(os.path.dirname(__file__), "proj_db_tmpdir")
-    if not os.path.exists(proj_db_tmpdir):
-        os.mkdir(proj_db_tmpdir, 0o755)
-    for path in osr.GetPROJSearchPaths():
-        if os.path.exists(os.path.join(path, "proj.db")):
-            shutil.copy(os.path.join(path, "proj.db"), proj_db_tmpdir)
-            osr.SetPROJSearchPaths([proj_db_tmpdir, proj_grids_path])
-            break
+    proj_db_tmpdir = os.path.join(
+        os.path.dirname(__file__), "gcore", "tmp", "proj_db_tmpdir"
+    )
+    assert os.path.exists(proj_db_tmpdir)
+    assert os.path.exists(os.path.join(proj_db_tmpdir, "proj.db"))
+    osr.SetPROJSearchPaths([proj_db_tmpdir, proj_grids_path])
 
 
 setup_proj_search_paths()

autotest/setup_proj_db_tmpdir.py
@@ -1,0 +1,24 @@
+# coding: utf-8
+
+import os
+import shutil
+import sys
+
+from osgeo import osr
+
+autotest_dir = sys.argv[1]
+
+proj_db_tmpdir = os.path.join(autotest_dir, "gcore", "tmp", "proj_db_tmpdir")
+# print("Creating " + proj_db_tmpdir)
+if not os.path.exists(proj_db_tmpdir):
+    os.mkdir(proj_db_tmpdir, 0o755)
+src_proj_db_found = False
+for path in osr.GetPROJSearchPaths():
+    if os.path.exists(os.path.join(path, "proj.db")):
+        src_proj_db_found = True
+        shutil.copy(os.path.join(path, "proj.db"), proj_db_tmpdir)
+        break
+
+if not src_proj_db_found:
+    print("Cannot find source proj.db")
+    sys.exit(1)