ef9327047d490730aef570b48840a92ddc67698e
Even Rouault <even.rouault@spatialys.com>
2023-02-21 19:21:40 +0100

    test_py_scripts.py: fix get_py_script() to work without relying on os.getcwd()

--- autotest/pymod/test_py_scripts.py
+++ autotest/pymod/test_py_scripts.py
@@ -52,12 +52,12 @@ def get_data_path(dir):
 
 
 def get_py_script(script_name):
-    # how to get to {root_dir}/gdal from {root_dir}/autotest/X
-    base_gdal_path = os.path.join(os.getcwd(), "..", "..")
+    build_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
     # now we need to look for the script in the utils or samples subdirs...
     for subdir in [scripts_subdir, samples_subdir]:
         try:
-            test_path = os.path.join(base_gdal_path, subdir)
+            test_path = os.path.join(build_dir, subdir)
             test_file_path = os.path.join(test_path, script_name + ".py")
             os.stat(test_file_path)
             return test_path
