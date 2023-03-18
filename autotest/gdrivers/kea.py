674d56cdbf3a5d9b7b3b27e5fc09f00ec62f244f
Even Rouault <even.rouault@spatialys.com>
2023-03-18 19:11:00 +0100

    kea.py: add pytest.mark.require_driver

--- autotest/gdrivers/kea.py
+++ autotest/gdrivers/kea.py
@@ -34,6 +34,8 @@ import pytest
 
 from osgeo import gdal, osr
 
+pytestmark = pytest.mark.require_driver("KEA")
+
 ###############################################################################
 
 
