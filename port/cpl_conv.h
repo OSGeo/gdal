448298f128cbaa8adbd63c749cdf4331713c9f3d
Even Rouault <even.rouault@spatialys.com>
2023-03-30 22:14:54 +0200

    Add CPLSubscribeToSetConfigOption() to subscribe to config option settings

--- port/cpl_conv.h
+++ port/cpl_conv.h
@@ -59,6 +59,16 @@ const char CPL_DLL *CPL_STDCALL CPLGetThreadLocalConfigOption(
 void CPL_DLL CPL_STDCALL CPLSetConfigOption(const char *, const char *);
 void CPL_DLL CPL_STDCALL CPLSetThreadLocalConfigOption(const char *pszKey,
                                                        const char *pszValue);
+
+/** Callback for CPLSubscribeToSetConfigOption() */
+typedef void (*CPLSetConfigOptionSubscriber)(const char *pszKey,
+                                             const char *pszValue,
+                                             bool bThreadLocal,
+                                             void *pUserData);
+int CPL_DLL CPLSubscribeToSetConfigOption(
+    CPLSetConfigOptionSubscriber pfnCallback, void *pUserData);
+void CPL_DLL CPLUnsubscribeToSetConfigOption(int nSubscriberId);
+
 /*! @cond Doxygen_Suppress */
 void CPL_DLL CPL_STDCALL CPLFreeConfig(void);
 /*! @endcond */
