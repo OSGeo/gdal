17f1c50a047895a351070658d0ae069b855b6bf2
Nyall Dawson <nyall.dawson@gmail.com>
2023-04-14 11:54:26 +1000

    Add ALTER_COMMENT_FLAG for altering field comments via OGR_L_AlterFieldDefn

--- ogr/ogr_core.h
+++ ogr/ogr_core.h
@@ -677,13 +677,19 @@ typedef enum
  */
 #define ALTER_ALTERNATIVE_NAME_FLAG 0x80
 
+/** Alter field comment.
+ * Used by OGR_L_AlterFieldDefn().
+ * @since GDAL 3.7
+ */
+#define ALTER_COMMENT_FLAG 0x100
+
 /** Alter all parameters of field definition.
  * Used by OGR_L_AlterFieldDefn().
  */
 #define ALTER_ALL_FLAG                                                         \
     (ALTER_NAME_FLAG | ALTER_TYPE_FLAG | ALTER_WIDTH_PRECISION_FLAG |          \
      ALTER_NULLABLE_FLAG | ALTER_DEFAULT_FLAG | ALTER_UNIQUE_FLAG |            \
-     ALTER_DOMAIN_FLAG | ALTER_ALTERNATIVE_NAME_FLAG)
+     ALTER_DOMAIN_FLAG | ALTER_ALTERNATIVE_NAME_FLAG | ALTER_COMMENT_FLAG)
 
 /** Alter geometry field name.
  * Used by OGR_L_AlterGeomFieldDefn().
