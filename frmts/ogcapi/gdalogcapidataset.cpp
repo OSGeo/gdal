e8286ba07ef001a53e6e51ae902779d97988c42e
doublebyte <doublebyte@hushmail.com>
2023-05-01 16:30:47 +0200

    - Attempts to find an uri of a well-known TMS, to send to the parser. If that fails, instead of throwing an error it does what it was doing before (e.g.: sends an entire document to the parser and lets it find the needed information).

--- frmts/ogcapi/gdalogcapidataset.cpp
+++ frmts/ogcapi/gdalogcapidataset.cpp
@@ -1696,15 +1696,11 @@ bool OGCAPIDataset::InitWithTilesAPI(GDALOpenInfo *poOpenInfo,
                       MEDIA_TYPE_JSON))
         return false;
 
+    // Attempts to find the uri for a well-known TMS; if it does not work, it will send the entire document
     const auto uri = oDoc.GetRoot().GetString("uri");
 
-    if (uri.empty())
-    {
-        CPLError(CE_Failure, CPLE_AppDefined, "Cannot parse TMS uri");
-        return false;
-    }
-
-    auto tms = gdal::TileMatrixSet::parse(uri.c_str());
+    auto tms = gdal::TileMatrixSet::parse(
+        !uri.empty() ? uri.c_str() : oDoc.SaveAsString().c_str());
     if (tms == nullptr)
         return false;
 
