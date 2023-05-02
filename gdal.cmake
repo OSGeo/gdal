397cc22229387d6dcfff84098e644f1b531a7537
Even Rouault <even.rouault@spatialys.com>
2023-05-02 15:03:21 +0200

    Prepare for GDAL 3.7.0

--- gdal.cmake
+++ gdal.cmake
@@ -6,8 +6,8 @@
 # a new member or virtual function in a public C++ class, etc.
 # This will typically happen for each GDAL feature release (change of X or Y in
 # a X.Y.Z numbering scheme), but should not happen for a bugfix release (change of Z)
-# Previous value: 32 for GDAL 3.6
-set(GDAL_SOVERSION 32)
+# Previous value: 33 for GDAL 3.7
+set(GDAL_SOVERSION 33)
 
 # Switches to control build targets(cached)
 option(ENABLE_GNM "Build GNM (Geography Network Model) component" ON)
