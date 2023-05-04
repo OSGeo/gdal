8f60e0eb6660284a6c2a4cd6eb8b8a3bcb7805ca
Alan Thomas <athomas@thinkspatial.com.au>
2023-05-04 23:14:16 +1000

    DXF: Preserve attributes in nested block insertions

--- ogr/ogrsf_frmts/dxf/ogr_dxf.h
+++ ogr/ogrsf_frmts/dxf/ogr_dxf.h
@@ -393,6 +393,11 @@ class OGRDXFFeature final : public OGRFeature
     // Additional data for ATTRIB and ATTDEF entities
     CPLString osAttributeTag;
 
+    // Store ATTRIB entities associated with an INSERT, for use when
+    // DXF_INLINE_BLOCKS is true and a block with attributes is INSERTed
+    // in another block
+    std::vector<std::unique_ptr<OGRDXFFeature>> apoAttribFeatures;
+
   public:
     explicit OGRDXFFeature(OGRFeatureDefn *poFeatureDefn);
 
@@ -425,6 +430,10 @@ class OGRDXFFeature final : public OGRFeature
     CPLString GetAttributeTag() const
     {
         return osAttributeTag;
+    }
+    const std::vector<std::unique_ptr<OGRDXFFeature>> &GetAttribFeatures() const
+    {
+        return apoAttribFeatures;
     }
 
     void SetInsertOCSCoords(const DXFTriple &oTriple)
