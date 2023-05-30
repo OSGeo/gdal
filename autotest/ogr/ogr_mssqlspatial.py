c488cbc9b91c413da3c7c68c44b4b1122ec70d10
Alessandro Pasotti <elpaso@itopen.it>
2023-05-30 16:21:37 +0200

    MSSQL: add test for geometry column identification
    Add test for issue #1990

--- autotest/ogr/ogr_mssqlspatial.py
+++ autotest/ogr/ogr_mssqlspatial.py
@@ -659,20 +659,34 @@ def test_binary_field_bcp():
     src_lyr = ds.CreateLayer("test_binary_field", geom_type=ogr.wkbPoint)
     src_lyr.CreateField(ogr.FieldDefn("binfield", ogr.OFTBinary))
     src_lyr.CreateField(ogr.FieldDefn("strfield", ogr.OFTString))
+    src_lyr.CreateField(ogr.FieldDefn("intfield", ogr.OFTInteger))
+
     f = ogr.Feature(src_lyr.GetLayerDefn())
     f.SetGeometry(ogr.CreateGeometryFromWkt("POINT(1 2)"))
     f.SetFieldBinaryFromHexString("binfield", "007FFF007FFF")
     f["strfield"] = "some text"
+    f["intfield"] = 1
     src_lyr.CreateFeature(f)
+
     f = ogr.Feature(src_lyr.GetLayerDefn())
     f.SetGeometry(ogr.CreateGeometryFromWkt("POINT(2 3)"))
     f.SetFieldBinaryFromHexString("binfield", "")
     f["strfield"] = "some text"
+    # leave int undefined
     src_lyr.CreateFeature(f)
+
     f = ogr.Feature(src_lyr.GetLayerDefn())
     f.SetGeometry(ogr.CreateGeometryFromWkt("POINT(3 4)"))
     f.SetFieldBinaryFromHexString("binfield", "FF007FFF007F")
     f["strfield"] = "some text"
+    f["intfield"] = 3
+    src_lyr.CreateFeature(f)
+
+    f = ogr.Feature(src_lyr.GetLayerDefn())
+    f.SetGeometry(ogr.CreateGeometryFromWkt("POINT(4 5)"))
+    f.SetFieldBinaryFromHexString("binfield", "007FFF007FFF")
+    # leave str undefined
+    f["intfield"] = 4
     src_lyr.CreateFeature(f)
 
     with gdaltest.config_option("MSSQLSPATIAL_USE_BCP", "TRUE"):
@@ -693,18 +707,72 @@ def test_binary_field_bcp():
             f = lyr.GetFeature(1)
             assert f.GetFieldAsBinary("binfield") == b"\x00\x7f\xff\x00\x7f\xff"
             assert f.GetFieldAsString("strfield") == "some text"
+            assert f.GetFieldAsInteger("intfield") == 1
+
             f = lyr.GetFeature(2)
             assert f.GetFieldAsBinary("binfield") == b""
             assert f.GetFieldAsString("strfield") == "some text"
+            # undefined
+            assert f.GetFieldAsInteger("intfield") == 0
+
             f = lyr.GetFeature(3)
             assert f.GetFieldAsBinary("binfield") == b"\xff\x00\x7f\xff\x00\x7f"
             assert f.GetFieldAsString("strfield") == "some text"
+            assert f.GetFieldAsInteger("intfield") == 3
+
+            f = lyr.GetFeature(4)
+            assert f.GetFieldAsBinary("binfield") == b"\x00\x7f\xff\x00\x7f\xff"
+            assert f.GetFieldAsString("strfield") == ""
+            assert f.GetFieldAsInteger("intfield") == 4
 
         finally:
             gdaltest.mssqlspatial_ds.ExecuteSQL("DROP TABLE test_binary_field")
             gdaltest.mssqlspatial_ds.ExecuteSQL(
                 "DELETE from geometry_columns WHERE f_table_name = 'test_binary_field'"
             )
+
+
+###############################################################################
+#
+
+
+def test_geometry_column_identification():
+    """Test for issue GH #1190
+    SQL Server data source: geometry column not identified by ogr2ogr"""
+
+    if gdaltest.mssqlspatial_ds is None:
+        pytest.skip("MSSQLSpatial driver not available")
+
+    # once
+    try:
+        gdaltest.mssqlspatial_ds.ExecuteSQL("ALTER TABLE dbo.tpoly ADD shape GEOMETRY")
+        gdaltest.mssqlspatial_ds.ExecuteSQL(
+            "UPDATE tpoly SET shape = ogr_geometry.STCentroid()"
+        )
+    except Exception:
+        pass
+
+    options = [
+        "-nln",
+        "test_geometry_column_identification",
+        "-f",
+        "ESRI Shapefile",
+        "-sql",
+        "SELECT eas_id, shape FROM tpoly",
+    ]
+
+    ds = gdal.VectorTranslate(
+        "/vsimem/test_geometry_column_identification.shp",
+        gdaltest.mssqlspatial_dsname,
+        options=options,
+    )
+    lyr = ds.GetLayerByName("test_geometry_column_identification")
+    assert lyr
+    f = lyr.GetFeature(1)
+    geom = f.GetGeometryRef()
+    assert geom is not None
+    assert geom.GetGeometryName() == "POINT"
+    assert f.GetFieldCount() == 1
 
 
 ###############################################################################
