f05adea63fc910636fef8cf77555f72316a1ce61
Even Rouault <even.rouault@spatialys.com>
2023-07-06 18:23:49 +0200

    CI: macos: install lxml and jsonschema from conda

--- ci/travis/osx/before_install.sh
+++ ci/travis/osx/before_install.sh
@@ -6,5 +6,5 @@ conda update -n base -c defaults conda
 conda install -y compilers automake pkgconfig cmake
 
 conda config --set channel_priority strict
-conda install --yes --quiet proj python=3.8 swig -y
+conda install --yes --quiet proj python=3.8 swig lxml jsonschema -y
 conda install --yes --quiet libgdal=3.7.0=hc13fe4b_4  --only-deps -y
