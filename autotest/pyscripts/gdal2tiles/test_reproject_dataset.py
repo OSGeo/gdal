2868e30d0d5542beef50b4cc045e500e489f74e8
Even Rouault <even.rouault@spatialys.com>
2023-07-17 16:04:10 +0200

    gdal2tiles: fix exception with dataset in EPSG:4326 with longitudes > 180 in WebMercator profile (fixes #8100)

--- autotest/pyscripts/gdal2tiles/test_reproject_dataset.py
+++ autotest/pyscripts/gdal2tiles/test_reproject_dataset.py
@@ -31,6 +31,8 @@
 
 from unittest import TestCase, mock
 
+import pytest
+
 from osgeo import gdal, osr
 from osgeo_utils import gdal2tiles
 
@@ -53,6 +55,7 @@ class ReprojectDatasetTest(TestCase):
         self.mercator_srs = osr.SpatialReference()
         self.mercator_srs.ImportFromEPSG(3857)
         self.geodetic_srs = osr.SpatialReference()
+        self.geodetic_srs.SetAxisMappingStrategy(osr.OAMS_TRADITIONAL_GIS_ORDER)
         self.geodetic_srs.ImportFromEPSG(4326)
 
     def test_raises_if_no_from_or_to_srs(self):
@@ -83,4 +86,23 @@ class ReprojectDatasetTest(TestCase):
 
         mock_gdal.AutoCreateWarpedVRT.assert_called_with(
             from_ds, self.mercator_srs.ExportToWkt(), self.geodetic_srs.ExportToWkt()
+        )
+
+    def test_4326_to_3857_longitude_beyond_180(self):
+        from_ds = gdal.GetDriverByName("MEM").Create("", 10, 10)
+        from_ds.SetGeoTransform([-180, 36.01, 0, 90, 0, -18])
+        to_ds = gdal2tiles.reproject_dataset(
+            from_ds, self.geodetic_srs, self.mercator_srs
+        )
+        assert to_ds.RasterXSize == 13
+        assert to_ds.RasterYSize == 13
+        assert to_ds.GetGeoTransform() == pytest.approx(
+            (
+                -20037508.342789244,
+                3082693.591198345,
+                0.0,
+                20037508.342789248,
+                0.0,
+                -3082693.5911983456,
+            )
         )
