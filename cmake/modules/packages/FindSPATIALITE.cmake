37391454a76d1d798e5a872dd2e1d8ee45a7a773
Even Rouault <even.rouault@spatialys.com>
2023-08-08 18:36:28 +0200

    FindSPATIALITE.cmake: quote variable

--- cmake/modules/packages/FindSPATIALITE.cmake
+++ cmake/modules/packages/FindSPATIALITE.cmake
@@ -56,7 +56,7 @@ if(SPATIALITE_LIBRARY AND SPATIALITE_INCLUDE_DIR
    AND NOT SPATIALITE_VERSION_STRING)
     file(STRINGS "${SPATIALITE_INCLUDE_DIR}/spatialite.h" _spatialite_h_ver
          REGEX "^[ \t]version[ \t]([0-9]+\\.[0-9]+)[,.].*")
-    string(REGEX REPLACE "[ \t]version[ \t]([0-9]+\\.[0-9]+)[,.].*" "\\1" _spatialite_h_ver ${_spatialite_h_ver})
+    string(REGEX REPLACE "[ \t]version[ \t]([0-9]+\\.[0-9]+)[,.].*" "\\1" _spatialite_h_ver "${_spatialite_h_ver}")
     set(SPATIALITE_VERSION_STRING "${_spatialite_h_ver}")
 endif()
 
