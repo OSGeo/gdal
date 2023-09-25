9c2401d0a50961a9e49cf5be50b959a182993a43
Even Rouault <even.rouault@spatialys.com>
2023-09-25 20:44:13 +0200

    BAG: avoid memleak in error code path of BAGResampledBand::IReadBlock()

--- frmts/hdf5/bagdataset.cpp
+++ frmts/hdf5/bagdataset.cpp
@@ -1257,6 +1257,7 @@ CPLErr BAGResampledBand::IReadBlock(int nBlockXOff, int nBlockYOff,
 
     H5Sclose(memspaceVarresMD);
 
+    CPLErr eErr = CE_None;
     for (int y = nLowResMinIdxY; y <= nLowResMaxIdxY; y++)
     {
         for (int x = nLowResMinIdxX; x <= nLowResMaxIdxX; x++)
@@ -1360,7 +1361,8 @@ CPLErr BAGResampledBand::IReadBlock(int nBlockXOff, int nBlockYOff,
                         nRefinementIndexase + super_x;
                     if (!poGDS->CacheRefinementValues(nRefinementIndex))
                     {
-                        return CE_Failure;
+                        eErr = CE_Failure;
+                        goto end;
                     }
 
                     const unsigned nOffInArray =
@@ -1429,13 +1431,14 @@ CPLErr BAGResampledBand::IReadBlock(int nBlockXOff, int nBlockYOff,
         }
     }
 
+end:
     if (poBlock != nullptr)
     {
         poBlock->DropLock();
         poBlock = nullptr;
     }
 
-    return CE_None;
+    return eErr;
 }
 
 static GDALRasterAttributeTable *
