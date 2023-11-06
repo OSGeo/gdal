b494ada1c6c7366f041950052f2f6b553d357ad1
Even Rouault <even.rouault@spatialys.com>
2023-11-06 22:55:25 +0100

    CI Conda: add patch to enable OGR_REGISTER_DRIVER_ARROW/PARQUET_FOR_LATER_PLUGIN

--- ci/travis/conda/setup.sh
+++ ci/travis/conda/setup.sh
@@ -15,6 +15,12 @@ conda install -c conda-forge conda-build -y
 git clone  https://github.com/conda-forge/gdal-feedstock.git
 
 cd gdal-feedstock
+
+# Set -DOGR_REGISTER_DRIVER_ARROW_FOR_LATER_PLUGIN=ON and -DOGR_REGISTER_DRIVER_PARQUET_FOR_LATER_PLUGIN=ON
+# To be dropped once that's upstreamed in conda-forge/gdal-feedstock.git
+patch -p1 < ../ci/travis/conda/build.sh.patch
+patch -p1 --binary < ../ci/travis/conda/bld.bat.patch
+
 cat > recipe/recipe_clobber.yaml <<EOL
 source:
   path: ../../../gdal
