f96a97e8e989f2cf90fe57c2a30bd17e7927de62
Even Rouault <even.rouault@spatialys.com>
2023-11-23 00:00:03 +0100

    DOQ1/DOQ2: CMAKE_UNITY_BUILD fixes

--- frmts/raw/doq2dataset.cpp
+++ frmts/raw/doq2dataset.cpp
@@ -31,6 +31,9 @@
 #include "gdal_frmts.h"
 #include "rawdataset.h"
 
+#ifndef UTM_FORMAT_defined
+#define UTM_FORMAT_defined
+
 static const char UTM_FORMAT[] =
     "PROJCS[\"%s / UTM zone %dN\",GEOGCS[%s,PRIMEM[\"Greenwich\",0],"
     "UNIT[\"degree\",0.0174532925199433]],PROJECTION[\"Transverse_Mercator\"],"
@@ -51,6 +54,8 @@ static const char NAD27_DATUM[] =
 static const char NAD83_DATUM[] =
     "\"NAD83\",DATUM[\"North_American_Datum_1983\","
     "SPHEROID[\"GRS 1980\",6378137,298.257222101]]";
+
+#endif
 
 /************************************************************************/
 /* ==================================================================== */
