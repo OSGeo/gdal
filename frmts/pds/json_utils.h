5ab106e648bd9d1ff7f25a9da03178353ef3e23a
Even Rouault <even.rouault@spatialys.com>
2023-11-23 00:00:15 +0100

    ENVI/GenBIN: CMAKE_UNITY_BUILD fixes

---
+++ frmts/pds/json_utils.h
@@ -0,0 +1,29 @@
+#ifndef JSON_UTILS_H_INCLUDED
+#define JSON_UTILS_H_INCLUDED
+
+#include "cpl_json.h"
+/**
+ * Get or create CPLJSONObject.
+ * @param  oParent Parent CPLJSONObject.
+ * @param  osKey  Key name.
+ * @return         CPLJSONObject class instance.
+ */
+static CPLJSONObject GetOrCreateJSONObject(CPLJSONObject &oParent,
+                                           const std::string &osKey)
+{
+    CPLJSONObject oChild = oParent[osKey];
+    if (oChild.IsValid() && oChild.GetType() != CPLJSONObject::Type::Object)
+    {
+        oParent.Delete(osKey);
+        oChild.Deinit();
+    }
+
+    if (!oChild.IsValid())
+    {
+        oChild = CPLJSONObject();
+        oParent.Add(osKey, oChild);
+    }
+    return oChild;
+}
+
+#endif /* JSON_UTILS_H_INCLUDED */
