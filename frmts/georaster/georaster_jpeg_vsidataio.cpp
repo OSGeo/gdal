849226ce1a94397dd3520efae8b58e07c5034bad
Even Rouault <even.rouault@spatialys.com>
2023-12-08 11:00:52 +0100

    GeoRaster: fix build on Windows where libjpeg's boolean typedef conflicts with OCI one

---
+++ frmts/georaster/georaster_jpeg_vsidataio.cpp
@@ -0,0 +1,3 @@
+#include "georaster_jpeg_vsidataio.h"
+
+#include "../jpeg/vsidataio.cpp"
---
+++ frmts/georaster/georaster_jpeg_vsidataio.h
@@ -0,0 +1,8 @@
+#ifndef GEORASTER_JPEG_VSIDATAIO_H_INCLUDED
+#define GEORASTER_JPEG_VSIDATAIO_H_INCLUDED
+
+#define jpeg_vsiio_src GEOR_jpeg_vsiio_src
+#define jpeg_vsiio_dest GEOR_jpeg_vsiio_dest
+#include "../jpeg/vsidataio.h"
+
+#endif /* GEORASTER_JPEG_VSIDATAIO_H_INCLUDED */
