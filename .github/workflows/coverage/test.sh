a93adb25dfcf080fe17959a91aed47433151a1ca
Even Rouault <even.rouault@spatialys.com>
2023-12-10 00:18:24 +0100

    CI: coverage: add PostGIS and MySQL

--- .github/workflows/coverage/test.sh
+++ .github/workflows/coverage/test.sh
@@ -6,6 +6,23 @@ export OGR_HANA_CONNECTION_STRING='DRIVER=/usr/sap/hdbclient/libodbcHDB.so;HOST=
 
 ctest -V -j $(nproc)
 
+# Run extra tests depending on services
+export PYTEST="python3 -m pytest -vv -p no:sugar --color=no"
+
+. ../scripts/setdevenv.sh
+
+IP=host.docker.internal
+
+# MySQL 8
+(cd autotest && OGR_MYSQL_CONNECTION_STRING=mysql:test,user=root,password=passwd,port=33060,host=$IP $PYTEST ogr/ogr_mysql.py)
+# MariaDB 10.3.9
+(cd autotest && OGR_MYSQL_CONNECTION_STRING=mysql:test,user=root,password=passwd,port=33061,host=$IP $PYTEST ogr/ogr_mysql.py)
+
+# PostGIS tests
+(cd autotest && OGR_PG_CONNECTION_STRING="host=$IP port=25432 dbname=autotest user=docker password=docker" $PYTEST --capture=no -ra ogr/ogr_pg.py)
+(cd autotest && PGHOST="$IP" PGPORT=25432 PGUSER=docker PGPASSWORD=docker $PYTEST --capture=no -ra gdrivers/postgisraster.py)
+
+# Generate coverage report
 lcov --directory . --capture --output-file gdal.info 2>/dev/null
 cp gdal.info gdal_filtered.info
 lcov --remove gdal_filtered.info '/usr/*' --output-file gdal_filtered.info
