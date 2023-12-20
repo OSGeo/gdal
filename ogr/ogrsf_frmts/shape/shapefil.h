81fef03f3f73a5a8c6d4d9e2b77d52404cf3551f
Even Rouault <even.rouault@spatialys.com>
2023-12-20 21:22:05 +0100

    Shapelib: resync with upstream

--- ogr/ogrsf_frmts/shape/shapefil.h
+++ ogr/ogrsf_frmts/shape/shapefil.h
@@ -27,6 +27,26 @@ extern "C"
 {
 #endif
 
+    /************************************************************************/
+    /*           Version related macros (added in 1.6.0)                    */
+    /************************************************************************/
+
+#define SHAPELIB_VERSION_MAJOR 1
+#define SHAPELIB_VERSION_MINOR 6
+#define SHAPELIB_VERSION_MICRO 0
+
+#define SHAPELIB_MAKE_VERSION_NUMBER(major, minor, micro)                      \
+    ((major)*10000 + (minor)*100 + (micro))
+
+#define SHAPELIB_VERSION_NUMBER                                                \
+    SHAPELIB_MAKE_VERSION_NUMBER(SHAPELIB_VERSION_MAJOR,                       \
+                                 SHAPELIB_VERSION_MINOR,                       \
+                                 SHAPELIB_VERSION_MICRO)
+
+#define SHAPELIB_AT_LEAST(major, minor, micro)                                 \
+    (SHAPELIB_VERSION_NUMBER >=                                                \
+     SHAPELIB_MAKE_VERSION_NUMBER(major, minor, micro))
+
 /************************************************************************/
 /*                        Configuration options.                        */
 /************************************************************************/
