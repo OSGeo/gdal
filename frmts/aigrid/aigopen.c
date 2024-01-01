4a1e6e4a5f90eff9f4932037cdbf455b669bf5a7
Even Rouault <even.rouault@spatialys.com>
2024-01-01 23:59:46 +0100

    AIG: avoid CodeQL cpp/integer-multiplication-cast-to-long

--- frmts/aigrid/aigopen.c
+++ frmts/aigrid/aigopen.c
@@ -169,7 +169,8 @@ AIGInfo_t *AIGOpen(const char *pszInputName, const char *pszAccess)
     /*      Setup tile infos, but defer reading of tile data.               */
     /* -------------------------------------------------------------------- */
     psInfo->pasTileInfo = (AIGTileInfo *)VSI_CALLOC_VERBOSE(
-        sizeof(AIGTileInfo), psInfo->nTilesPerRow * psInfo->nTilesPerColumn);
+        sizeof(AIGTileInfo),
+        (size_t)psInfo->nTilesPerRow * psInfo->nTilesPerColumn);
     if (psInfo->pasTileInfo == NULL)
     {
         AIGClose(psInfo);
