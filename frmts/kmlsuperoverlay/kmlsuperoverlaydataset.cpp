1f229d59106f49bac115171f245e4e74400b2a3f
Even Rouault <even.rouault@spatialys.com>
2024-01-02 01:17:21 +0100

    KMLSuperOverlay: avoid CodeQL cpp/comparison-with-wider-type

--- frmts/kmlsuperoverlay/kmlsuperoverlaydataset.cpp
+++ frmts/kmlsuperoverlay/kmlsuperoverlaydataset.cpp
@@ -2160,7 +2160,7 @@ CPLErr KmlSingleDocRasterRasterBand::IReadBlock(int nBlockXOff, int nBlockYOff,
     GDALDataset *poImageDS = poGDS->poCurTileDS;
     if (poImageDS == nullptr)
     {
-        memset(pImage, 0, nBlockXSize * nBlockYSize);
+        memset(pImage, 0, static_cast<size_t>(nBlockXSize) * nBlockYSize);
         return CE_None;
     }
     int nXSize = poImageDS->GetRasterXSize();
@@ -2188,7 +2188,7 @@ CPLErr KmlSingleDocRasterRasterBand::IReadBlock(int nBlockXOff, int nBlockYOff,
         if (nBand == 4 && poColorTable == nullptr)
         {
             /* Add fake alpha band */
-            memset(pImage, 255, nBlockXSize * nBlockYSize);
+            memset(pImage, 255, static_cast<size_t>(nBlockXSize) * nBlockYSize);
             eErr = CE_None;
         }
         else
@@ -2237,7 +2237,7 @@ CPLErr KmlSingleDocRasterRasterBand::IReadBlock(int nBlockXOff, int nBlockYOff,
     else if (nBand == 4 && poImageDS->GetRasterCount() == 3)
     {
         /* Add fake alpha band */
-        memset(pImage, 255, nBlockXSize * nBlockYSize);
+        memset(pImage, 255, static_cast<size_t>(nBlockXSize) * nBlockYSize);
         eErr = CE_None;
     }
 
