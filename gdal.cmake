e8a39c8e6273f8bfe43dc0732db1c2680fafe6ab
Even Rouault <even.rouault@spatialys.com>
2024-01-16 16:06:58 +0100

    Move data/gdalvrt.xsd to frmts/vrt/data

--- gdal.cmake
+++ gdal.cmake
@@ -660,7 +660,6 @@ set(GDAL_DATA_FILES
     data/epsg.wkt
     data/esri_StatePlane_extra.wkt
     data/gdalicon.png
-    data/gdalvrt.xsd
     data/gt_datum.csv
     data/gt_ellips.csv
     data/ogrvrt.xsd
