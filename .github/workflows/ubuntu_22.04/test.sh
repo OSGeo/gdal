33e24daf3fa529a81061a7cfcb042e9516331cea
Even Rouault <even.rouault@spatialys.com>
2024-02-04 19:02:13 +0100

    CI: add build time testing of OCI driver

--- .github/workflows/ubuntu_22.04/test.sh
+++ .github/workflows/ubuntu_22.04/test.sh
@@ -4,6 +4,9 @@ set -e
 
 . ../scripts/setdevenv.sh
 
+LD_LIBRARY_PATH="/opt/instantclient_19_9:/opt/instantclient_19_9/lib:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH
+
 export PYTEST="python3 -m pytest -vv -p no:sugar --color=no"
 
 # Run C++ tests
