1c4827d80fca1a174b86564f88f23868cac1a606
Even Rouault <even.rouault@spatialys.com>
2024-02-07 18:00:18 +0100

    KML: harmonize on a 8192 byte large parsing buffer on all platforms

--- ogr/ogrsf_frmts/kml/kml.cpp
+++ ogr/ogrsf_frmts/kml/kml.cpp
@@ -41,6 +41,8 @@
 #include "expat.h"
 #endif
 
+constexpr int PARSER_BUF_SIZE = 8192;
+
 KML::KML()
     : poTrunk_(nullptr), nNumLayers_(-1), papoLayers_(nullptr), nDepth_(0),
       validity(KML_VALIDITY_UNKNOWN), pKMLFile_(nullptr), poCurrent_(nullptr),
@@ -95,15 +97,15 @@ bool KML::parse()
 
     int nDone = 0;
     int nLen = 0;
-    char aBuf[BUFSIZ] = {0};
+    std::vector<char> aBuf(PARSER_BUF_SIZE);
     bool bError = false;
 
     do
     {
         nDataHandlerCounter = 0;
-        nLen = (int)VSIFReadL(aBuf, 1, sizeof(aBuf), pKMLFile_);
+        nLen = (int)VSIFReadL(aBuf.data(), 1, aBuf.size(), pKMLFile_);
         nDone = VSIFEofL(pKMLFile_);
-        if (XML_Parse(oParser, aBuf, nLen, nDone) == XML_STATUS_ERROR)
+        if (XML_Parse(oParser, aBuf.data(), nLen, nDone) == XML_STATUS_ERROR)
         {
             CPLError(CE_Failure, CPLE_AppDefined,
                      "XML parsing of KML file failed : %s at line %d, "
@@ -184,23 +186,25 @@ void KML::checkValidity()
 
     int nDone = 0;
     int nLen = 0;
-    char aBuf[BUFSIZ] = {0};
+    std::vector<char> aBuf(PARSER_BUF_SIZE);
 
     // Parses the file until we find the first element.
     do
     {
         nDataHandlerCounter = 0;
-        nLen = static_cast<int>(VSIFReadL(aBuf, 1, sizeof(aBuf), pKMLFile_));
+        nLen =
+            static_cast<int>(VSIFReadL(aBuf.data(), 1, aBuf.size(), pKMLFile_));
         nDone = VSIFEofL(pKMLFile_);
-        if (XML_Parse(oParser, aBuf, nLen, nDone) == XML_STATUS_ERROR)
+        if (XML_Parse(oParser, aBuf.data(), nLen, nDone) == XML_STATUS_ERROR)
         {
-            if (nLen <= BUFSIZ - 1)
+            if (nLen <= PARSER_BUF_SIZE - 1)
                 aBuf[nLen] = 0;
             else
-                aBuf[BUFSIZ - 1] = 0;
-            if (strstr(aBuf, "<?xml") &&
-                (strstr(aBuf, "<kml") ||
-                 (strstr(aBuf, "<Document") && strstr(aBuf, "/kml/2."))))
+                aBuf[PARSER_BUF_SIZE - 1] = 0;
+            if (strstr(aBuf.data(), "<?xml") &&
+                (strstr(aBuf.data(), "<kml") ||
+                 (strstr(aBuf.data(), "<Document") &&
+                  strstr(aBuf.data(), "/kml/2."))))
             {
                 CPLError(
                     CE_Failure, CPLE_AppDefined,
@@ -217,7 +221,7 @@ void KML::checkValidity()
         }
 
         nCount++;
-        /* After reading 50 * BUFSIZE bytes, and not finding whether the file */
+        /* After reading 50 * PARSER_BUF_SIZE bytes, and not finding whether the file */
         /* is KML or not, we give up and fail silently */
     } while (!nDone && nLen > 0 && validity == KML_VALIDITY_UNKNOWN &&
              nCount < 50);
@@ -370,7 +374,7 @@ void XMLCALL KML::dataHandlerValidate(void *pUserData,
     KML *poKML = static_cast<KML *>(pUserData);
 
     poKML->nDataHandlerCounter++;
-    if (poKML->nDataHandlerCounter >= BUFSIZ)
+    if (poKML->nDataHandlerCounter >= PARSER_BUF_SIZE)
     {
         CPLError(CE_Failure, CPLE_AppDefined,
                  "File probably corrupted (million laugh pattern)");
@@ -535,7 +539,7 @@ void XMLCALL KML::dataHandler(void *pUserData, const char *pszData, int nLen)
         return;
 
     poKML->nDataHandlerCounter++;
-    if (poKML->nDataHandlerCounter >= BUFSIZ)
+    if (poKML->nDataHandlerCounter >= PARSER_BUF_SIZE)
     {
         CPLError(CE_Failure, CPLE_AppDefined,
                  "File probably corrupted (million laugh pattern)");
