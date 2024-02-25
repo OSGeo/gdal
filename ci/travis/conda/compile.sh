fe693c6d13ccfd3c33420961891645b274095825
Even Rouault <even.rouault@spatialys.com>
2024-02-25 20:28:12 +0100

    CI: add support for OSX arm64

--- ci/travis/conda/compile.sh
+++ ci/travis/conda/compile.sh
@@ -5,18 +5,24 @@ mkdir -p packages
 CONDA_PLAT=""
 if grep -q "windows" <<< "$GHA_CI_PLATFORM"; then
     CONDA_PLAT="win"
+    ARCH="64"
 fi
 
 if grep -q "ubuntu" <<< "$GHA_CI_PLATFORM"; then
     CONDA_PLAT="linux"
+    ARCH="64"
 fi
 
-if grep -q "macos" <<< "$GHA_CI_PLATFORM"; then
+if grep -q "macos-14" <<< "$GHA_CI_PLATFORM"; then
     CONDA_PLAT="osx"
+    ARCH="arm64"
+elif grep -q "macos" <<< "$GHA_CI_PLATFORM"; then
+    CONDA_PLAT="osx"
+    ARCH="64"
 fi
 
-conda build recipe --clobber-file recipe/recipe_clobber.yaml --output-folder packages -m ".ci_support/${CONDA_PLAT}_64_.yaml"
-conda create -y -n test -c ./packages/${CONDA_PLAT}-64 python libgdal gdal
+conda build recipe --clobber-file recipe/recipe_clobber.yaml --output-folder packages -m ".ci_support/${CONDA_PLAT}_${ARCH}_.yaml"
+conda create -y -n test -c ./packages/${CONDA_PLAT}-${ARCH} python libgdal gdal
 conda deactivate
 
 conda activate test
