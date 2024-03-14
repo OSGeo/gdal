59505b2a0db3436933e2b8e1849512c4e361395b
Even Rouault <even.rouault@spatialys.com>
2024-03-14 13:28:26 +0100

    ZMap: support reading variant of format where there is no newline character at end of column

--- autotest/gdrivers/zmap.py
+++ autotest/gdrivers/zmap.py
@@ -65,3 +65,26 @@ def test_zmap_nodata():
     )
     ds = None
     gdal.GetDriverByName("ZMap").Delete(filename)
+
+
+###############################################################################
+# Test variant of the format where there is no flush at end of column
+
+
+def test_zmap_no_flush_end_of_column(tmp_path):
+
+    src_ds = gdal.GetDriverByName("MEM").Create("", 2, 5)
+    src_ds.WriteRaster(0, 0, 2, 5, b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09")
+    filename = str(tmp_path / "out.zmap")
+    with gdaltest.config_option("ZMAP_EMIT_EOL_AT_END_OF_COLUMN", "NO"):
+        gdal.GetDriverByName("ZMap").CreateCopy(filename, src_ds)
+    f = gdal.VSIFOpenL(filename, "rb")
+    assert f
+    data = gdal.VSIFReadL(1, 1000, f)
+    gdal.VSIFCloseL(f)
+    assert (
+        data
+        == b"!\n! Created by GDAL.\n!\n@GRID FILE, GRID, 4\n        20,               1E+30,          ,         7,         1\n         5,         2,     0.0000000,     2.0000000,    -5.0000000,     0.0000000\n0.0, 0.0, 0.0\n@\n           0.0000000           2.0000000           4.0000000           6.0000000\n           8.0000000           1.0000000           3.0000000           5.0000000\n           7.0000000           9.0000000\n"
+    )
+    ds = gdal.Open(filename)
+    assert ds.ReadRaster(buf_type=gdal.GDT_Byte) == src_ds.ReadRaster()
