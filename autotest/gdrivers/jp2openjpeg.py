a83fd2d04026608cbe7fde0f26b80c42d837b5db
Even Rouault <even.rouault@spatialys.com>
2024-03-17 16:17:28 +0100

    jp2openjpeg.py: add expected checksum for MacOS 14 ARM64

--- autotest/gdrivers/jp2openjpeg.py
+++ autotest/gdrivers/jp2openjpeg.py
@@ -721,7 +721,8 @@ def test_jp2openjpeg_22():
     assert ds.GetRasterBand(2).GetColorInterpretation() == gdal.GCI_GreenBand
     assert ds.GetRasterBand(3).GetColorInterpretation() == gdal.GCI_BlueBand
     assert ds.GetRasterBand(4).GetColorInterpretation() == gdal.GCI_AlphaBand
-    assert ds.GetRasterBand(1).Checksum() in [11457, 11450, 11498, 11502]
+    # 11497 on MacOS 14 ARM64 with CLang 16 in -O2
+    assert ds.GetRasterBand(1).Checksum() in [11457, 11450, 11497, 11498, 11502]
     ds = None
     gdal.Unlink("/vsimem/jp2openjpeg_22.jp2")
 
