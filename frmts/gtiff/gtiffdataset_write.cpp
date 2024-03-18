db4280f4ecdc640d4ca435b6ace42584b4450823
Thomas Bonfort <thomas.bonfort@airbus.com>
2024-03-18 11:37:02 +0100

    GTIFF: fix ambiguous wording for 64b predictor=2

--- frmts/gtiff/gtiffdataset_write.cpp
+++ frmts/gtiff/gtiffdataset_write.cpp
@@ -5132,7 +5132,7 @@ TIFF *GTiffDataset::CreateLL(const char *pszFilename, int nXSize, int nYSize,
                 {
                     ReportError(
                         pszFilename, CE_Failure, CPLE_AppDefined,
-                        "PREDICTOR=2 is only supported with 64 bit samples "
+                        "PREDICTOR=2 is supported on 64 bit samples "
                         "starting with libtiff > 4.3.0.");
                 }
                 else
