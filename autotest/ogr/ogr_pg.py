OSGeo/gdal

commit b00e954fc09c44c390430c28b800bca14cdb0a4c

PG: serialize GDAL multidomain metadata of layer as XML in a ogr_system_tables.metadata table

autotest/ogr/ogr_pg.py
@@ -4905,6 +4905,43 @@ def test_ogr_pg_84(pg_ds):
 
 
 ###############################################################################
+# Test metadata
+
+
+@only_without_postgis
+def test_ogr_pg_metadata(pg_ds):
+
+    pg_ds = reconnect(pg_ds, update=1)
+    lyr = pg_ds.CreateLayer(
+        "test_ogr_pg_metadata", geom_type=ogr.wkbPoint, options=["OVERWRITE=YES"]
+    )
+    lyr.SetMetadata({"foo": "bar"})
+    lyr.SetMetadataItem("bar", "baz")
+    lyr.SetMetadataItem("DESCRIPTION", "my_desc")
+
+    pg_ds = reconnect(pg_ds, update=1)
+    with pg_ds.ExecuteSQL(
+        "SELECT * FROM ogr_system_tables.metadata WHERE table_name = 'test_ogr_pg_metadata'"
+    ) as sql_lyr:
+        assert sql_lyr.GetFeatureCount() == 1
+    lyr = pg_ds.GetLayerByName("test_ogr_pg_metadata")
+    assert lyr.GetMetadata_Dict() == {
+        "DESCRIPTION": "my_desc",
+        "foo": "bar",
+        "bar": "baz",
+    }
+    lyr.SetMetadata(None)
+
+    pg_ds = reconnect(pg_ds, update=1)
+    with pg_ds.ExecuteSQL(
+        "SELECT * FROM ogr_system_tables.metadata WHERE table_name = 'test_ogr_pg_metadata'"
+    ) as sql_lyr:
+        assert sql_lyr.GetFeatureCount() == 0
+    lyr = pg_ds.GetLayerByName("test_ogr_pg_metadata")
+    assert lyr.GetMetadata_Dict() == {}
+
+
+###############################################################################
 # Test append of several layers in PG_USE_COPY mode (#6411)
 
 

ogr/ogrsf_frmts/pg/ogr_pg.h
@@ -300,6 +300,8 @@ class OGRPGTableLayer final : public OGRPGLayer
     char *pszSchemaName = nullptr;
     char *m_pszTableDescription = nullptr;
     CPLString osForcedDescription{};
+    bool m_bMetadataLoaded = false;
+    bool m_bMetadataModified = false;
     char *pszSqlTableName = nullptr;
     int bTableDefinitionValid = -1;
 
@@ -366,6 +368,9 @@ class OGRPGTableLayer final : public OGRPGLayer
                                  int nIdx);
 
     void UpdateSequenceIfNeeded();
+
+    void LoadMetadata();
+    void SerializeMetadata();
 
   public:
     OGRPGTableLayer(OGRPGDataSource *, CPLString &osCurrentSchema,

ogr/ogrsf_frmts/pg/ogrpgtablelayer.cpp
@@ -221,6 +221,7 @@ OGRPGTableLayer::~OGRPGTableLayer()
     if (bCopyActive)
         EndCopy();
     UpdateSequenceIfNeeded();
+    SerializeMetadata();
 
     CPLFree(pszSqlTableName);
     CPLFree(pszTableName);
@@ -232,11 +233,206 @@ OGRPGTableLayer::~OGRPGTableLayer()
 }
 
 /************************************************************************/
+/*                              LoadMetadata()                          */
+/************************************************************************/
+
+void OGRPGTableLayer::LoadMetadata()
+{
+    if (m_bMetadataLoaded)
+        return;
+    m_bMetadataLoaded = true;
+
+    PGconn *hPGConn = poDS->GetPGConn();
+    const std::string osSQL(
+        CPLSPrintf("SELECT metadata FROM ogr_system_tables.metadata WHERE "
+                   "schema_name = %s AND table_name = %s",
+                   OGRPGEscapeString(hPGConn, pszSchemaName).c_str(),
+                   OGRPGEscapeString(hPGConn, pszTableName).c_str()));
+    CPLErrorHandlerPusher oErrorHandler(CPLQuietErrorHandler);
+    CPLErrorStateBackuper oBackuper;
+    auto poSqlLyr = poDS->ExecuteSQL(osSQL.c_str(), nullptr, nullptr);
+    if (poSqlLyr)
+    {
+        auto poFeature =
+            std::unique_ptr<OGRFeature>(poSqlLyr->GetNextFeature());
+        if (poFeature)
+        {
+            if (poFeature->IsFieldSetAndNotNull(0))
+            {
+                const char *pszXML = poFeature->GetFieldAsString(0);
+                if (pszXML)
+                {
+                    auto psRoot = CPLParseXMLString(pszXML);
+                    if (psRoot)
+                    {
+                        oMDMD.XMLInit(psRoot, true);
+                        CPLDestroyXMLNode(psRoot);
+                    }
+                }
+            }
+        }
+        poDS->ReleaseResultSet(poSqlLyr);
+    }
+}
+
+/************************************************************************/
+/*                         SerializeMetadata()                          */
+/************************************************************************/
+
+void OGRPGTableLayer::SerializeMetadata()
+{
+    if (!m_bMetadataModified)
+        return;
+
+    PGconn *hPGConn = poDS->GetPGConn();
+    CPLXMLNode *psMD = oMDMD.Serialize();
+
+    if (psMD)
+    {
+        // Remove DESCRIPTION and OLMD_FID64 items from metadata
+
+        CPLXMLNode *psPrev = nullptr;
+        for (CPLXMLNode *psIter = psMD; psIter;)
+        {
+            CPLXMLNode *psNext = psIter->psNext;
+            if (psIter->eType == CXT_Element &&
+                strcmp(psIter->pszValue, "Metadata") == 0 &&
+                CPLGetXMLNode(psIter, "domain") == nullptr)
+            {
+                bool bFoundInterestingItems = false;
+                for (CPLXMLNode *psIter2 = psIter->psChild; psIter2;)
+                {
+                    CPLXMLNode *psNext2 = psIter2->psNext;
+                    if (psIter2->eType == CXT_Element &&
+                        strcmp(psIter2->pszValue, "MDI") == 0 &&
+                        (EQUAL(CPLGetXMLValue(psIter2, "key", ""),
+                               OLMD_FID64) ||
+                         EQUAL(CPLGetXMLValue(psIter2, "key", ""),
+                               "DESCRIPTION")))
+                    {
+                        CPLRemoveXMLChild(psIter, psIter2);
+                    }
+                    else
+                    {
+                        bFoundInterestingItems = true;
+                    }
+                    psIter2 = psNext2;
+                }
+                if (!bFoundInterestingItems)
+                {
+                    if (psPrev)
+                        psPrev->psNext = psNext;
+                    else
+                        psMD = psNext;
+                    psIter->psNext = nullptr;
+                    CPLDestroyXMLNode(psIter);
+                }
+            }
+            psIter = psNext;
+            psPrev = psIter;
+        }
+    }
+
+    if (psMD)
+    {
+        PGresult *hResult = OGRPG_PQexec(
+            hPGConn, "CREATE SCHEMA IF NOT EXISTS ogr_system_tables");
+        OGRPGClearResult(hResult);
+
+        hResult = OGRPG_PQexec(
+            hPGConn, "CREATE TABLE IF NOT EXISTS ogr_system_tables.metadata("
+                     "id SERIAL, "
+                     "schema_name TEXT NOT NULL, "
+                     "table_name TEXT NOT NULL, "
+                     "metadata TEXT,"
+                     "UNIQUE(schema_name, table_name))");
+        OGRPGClearResult(hResult);
+
+        hResult = OGRPG_PQexec(
+            hPGConn,
+            "DROP FUNCTION IF EXISTS "
+            "ogr_system_tables.event_trigger_function_for_metadata CASCADE");
+        OGRPGClearResult(hResult);
+
+        hResult = OGRPG_PQexec(
+            hPGConn,
+            "CREATE FUNCTION "
+            "ogr_system_tables.event_trigger_function_for_metadata()\n"
+            "RETURNS event_trigger LANGUAGE plpgsql AS $$\n"
+            "DECLARE\n"
+            "    obj record;\n"
+            "BEGIN\n"
+            "    FOR obj IN SELECT * FROM pg_event_trigger_dropped_objects()\n"
+            "    LOOP\n"
+            "        IF obj.object_type = 'table' THEN\n"
+            "            DELETE FROM ogr_system_tables.metadata m WHERE "
+            "m.schema_name = obj.schema_name AND m.table_name = "
+            "obj.object_name;\n"
+            "        END IF;\n"
+            "    END LOOP;\n"
+            "END;\n"
+            "$$;");
+        OGRPGClearResult(hResult);
+
+        hResult = OGRPG_PQexec(hPGConn,
+                               "DROP EVENT TRIGGER IF EXISTS "
+                               "ogr_system_tables_event_trigger_for_metadata");
+        OGRPGClearResult(hResult);
+
+        hResult = OGRPG_PQexec(
+            hPGConn,
+            "CREATE EVENT TRIGGER ogr_system_tables_event_trigger_for_metadata "
+            "ON sql_drop "
+            "EXECUTE FUNCTION "
+            "ogr_system_tables.event_trigger_function_for_metadata()");
+        OGRPGClearResult(hResult);
+
+        CPLString osCommand;
+        osCommand.Printf("DELETE FROM ogr_system_tables.metadata WHERE "
+                         "schema_name = %s AND table_name = %s",
+                         OGRPGEscapeString(hPGConn, pszSchemaName).c_str(),
+                         OGRPGEscapeString(hPGConn, pszTableName).c_str());
+        hResult = OGRPG_PQexec(hPGConn, osCommand.c_str());
+        OGRPGClearResult(hResult);
+
+        CPLXMLNode *psRoot =
+            CPLCreateXMLNode(nullptr, CXT_Element, "GDALMetadata");
+        CPLAddXMLChild(psRoot, psMD);
+        char *pszXML = CPLSerializeXMLTree(psRoot);
+        // CPLDebug("PG", "Serializing %s", pszXML);
+
+        osCommand.Printf("INSERT INTO ogr_system_tables.metadata (schema_name, "
+                         "table_name, metadata) VALUES (%s, %s, %s)",
+                         OGRPGEscapeString(hPGConn, pszSchemaName).c_str(),
+                         OGRPGEscapeString(hPGConn, pszTableName).c_str(),
+                         OGRPGEscapeString(hPGConn, pszXML).c_str());
+        hResult = OGRPG_PQexec(hPGConn, osCommand.c_str());
+        OGRPGClearResult(hResult);
+
+        CPLDestroyXMLNode(psRoot);
+        CPLFree(pszXML);
+    }
+    else
+    {
+        CPLString osCommand;
+        osCommand.Printf("DELETE FROM ogr_system_tables.metadata WHERE "
+                         "schema_name = %s AND table_name = %s",
+                         OGRPGEscapeString(hPGConn, pszSchemaName).c_str(),
+                         OGRPGEscapeString(hPGConn, pszTableName).c_str());
+        PGresult *hResult =
+            OGRPG_PQexec(hPGConn, osCommand.c_str(), false, true);
+        OGRPGClearResult(hResult);
+    }
+}
+
+/************************************************************************/
 /*                          GetMetadataDomainList()                     */
 /************************************************************************/
 
 char **OGRPGTableLayer::GetMetadataDomainList()
 {
+    LoadMetadata();
+
     if (m_pszTableDescription == nullptr)
         GetMetadata();
     if (m_pszTableDescription != nullptr && m_pszTableDescription[0] != '\0')
@@ -250,6 +446,8 @@ char **OGRPGTableLayer::GetMetadataDomainList()
 
 char **OGRPGTableLayer::GetMetadata(const char *pszDomain)
 {
+    LoadMetadata();
+
     if ((pszDomain == nullptr || EQUAL(pszDomain, "")) &&
         m_pszTableDescription == nullptr)
     {
@@ -289,6 +487,8 @@ char **OGRPGTableLayer::GetMetadata(const char *pszDomain)
 const char *OGRPGTableLayer::GetMetadataItem(const char *pszName,
                                              const char *pszDomain)
 {
+    LoadMetadata();
+
     GetMetadata(pszDomain);
     return OGRLayer::GetMetadataItem(pszName, pszDomain);
 }
@@ -299,7 +499,11 @@ const char *OGRPGTableLayer::GetMetadataItem(const char *pszName,
 
 CPLErr OGRPGTableLayer::SetMetadata(char **papszMD, const char *pszDomain)
 {
+    LoadMetadata();
+
     OGRLayer::SetMetadata(papszMD, pszDomain);
+    m_bMetadataModified = true;
+
     if (!osForcedDescription.empty() &&
         (pszDomain == nullptr || EQUAL(pszDomain, "")))
     {
@@ -337,17 +541,23 @@ CPLErr OGRPGTableLayer::SetMetadataItem(const char *pszName,
                                         const char *pszValue,
                                         const char *pszDomain)
 {
+    LoadMetadata();
+
     if ((pszDomain == nullptr || EQUAL(pszDomain, "")) && pszName != nullptr &&
         EQUAL(pszName, "DESCRIPTION") && !osForcedDescription.empty())
     {
         pszValue = osForcedDescription;
     }
+
     OGRLayer::SetMetadataItem(pszName, pszValue, pszDomain);
+    m_bMetadataModified = true;
+
     if (!bDeferredCreation && (pszDomain == nullptr || EQUAL(pszDomain, "")) &&
         pszName != nullptr && EQUAL(pszName, "DESCRIPTION"))
     {
         SetMetadata(GetMetadata());
     }
+
     return CE_None;
 }
 
@@ -506,7 +716,7 @@ int OGRPGTableLayer::ReadTableDefinition()
                 CPLDebug("PG", "Primary key name (FID): %s, type : %s",
                          osPrimaryKey.c_str(), pszFIDType);
                 if (EQUAL(pszFIDType, "int8"))
-                    SetMetadataItem(OLMD_FID64, "YES");
+                    OGRLayer::SetMetadataItem(OLMD_FID64, "YES");
             }
         }
         else if (PQntuples(hResult) > 1)
@@ -1547,7 +1757,7 @@ OGRErr OGRPGTableLayer::ICreateFeature(OGRFeature *poFeature)
 
     /* Auto-promote FID column to 64bit if necessary */
     if (pszFIDColumn != nullptr && !CPL_INT64_FITS_ON_INT32(nFID) &&
-        GetMetadataItem(OLMD_FID64) == nullptr)
+        OGRLayer::GetMetadataItem(OLMD_FID64) == nullptr)
     {
         poDS->EndCopy();
 
@@ -1568,7 +1778,7 @@ OGRErr OGRPGTableLayer::ICreateFeature(OGRFeature *poFeature)
         }
         OGRPGClearResult(hResult);
 
-        SetMetadataItem(OLMD_FID64, "YES");
+        OGRLayer::SetMetadataItem(OLMD_FID64, "YES");
     }
 
     if (bFirstInsertion)

commit accd4b577ada51a288978febb7fbf2e5da90cfe5

PG: avoid aborting transactions when calling LoadMetadata() and ogr_system_tables.metadata doesn't exist
Fixes 3.9.0-dev regression, introduced in b00e954fc09c44c390430c28b800bca14cdb0a4c
This particularly broke ogr2ogr from Shapefile to PostgreSQL.

autotest/ogr/ogr_pg.py
@@ -4859,12 +4859,14 @@ def test_ogr_pg_84(pg_ds):
 def test_ogr_pg_metadata(pg_ds):
 
     pg_ds = reconnect(pg_ds, update=1)
+    pg_ds.StartTransaction()
     lyr = pg_ds.CreateLayer(
         "test_ogr_pg_metadata", geom_type=ogr.wkbPoint, options=["OVERWRITE=YES"]
     )
     lyr.SetMetadata({"foo": "bar"})
     lyr.SetMetadataItem("bar", "baz")
     lyr.SetMetadataItem("DESCRIPTION", "my_desc")
+    pg_ds.CommitTransaction()
 
     pg_ds = reconnect(pg_ds, update=1)
     with pg_ds.ExecuteSQL(

ogr/ogrsf_frmts/pg/ogr_pg.h
@@ -604,7 +604,7 @@ class OGRPGDataSource final : public OGRDataSource
     int bHavePostGIS = false;
     int bHaveGeography = false;
 
-    int bUserTransactionActive = false;
+    bool bUserTransactionActive = false;
     int bSavePointActive = false;
     int nSoftTransactionLevel = 0;
 
@@ -637,6 +637,9 @@ class OGRPGDataSource final : public OGRDataSource
     CPLString osActiveSchema{};
     int bListAllTables = false;
     bool m_bSkipViews = false;
+
+    bool m_bOgrSystemTablesMetadataTableExistenceTested = false;
+    bool m_bOgrSystemTablesMetadataTableFound = false;
 
     void LoadTables();
 
@@ -740,6 +743,14 @@ class OGRPGDataSource final : public OGRDataSource
     int UseCopy();
     void StartCopy(OGRPGTableLayer *poPGLayer);
     OGRErr EndCopy();
+
+    bool IsUserTransactionActive()
+    {
+        return bUserTransactionActive;
+    }
+
+    void CreateOgrSystemTablesMetadataTableIfNeeded();
+    bool HasOgrSystemTablesMetadataTable();
 };
 
 #endif /* ndef OGR_PG_H_INCLUDED */

ogr/ogrsf_frmts/pg/ogrpgdatasource.cpp
@@ -2553,7 +2553,7 @@ OGRErr OGRPGDataSource::StartTransaction(CPL_UNUSED int bForce)
     }
 
     nSoftTransactionLevel++;
-    bUserTransactionActive = TRUE;
+    bUserTransactionActive = true;
 
     /*CPLDebug("PG", "poDS=%p StartTransaction() nSoftTransactionLevel=%d",
              this, nSoftTransactionLevel);*/
@@ -2586,7 +2586,7 @@ OGRErr OGRPGDataSource::CommitTransaction()
     }
 
     nSoftTransactionLevel--;
-    bUserTransactionActive = FALSE;
+    bUserTransactionActive = false;
 
     if (bSavePointActive)
     {
@@ -2631,7 +2631,7 @@ OGRErr OGRPGDataSource::RollbackTransaction()
     FlushCache(false);
 
     nSoftTransactionLevel--;
-    bUserTransactionActive = FALSE;
+    bUserTransactionActive = false;
 
     OGRErr eErr;
     if (bSavePointActive)
@@ -3142,4 +3142,87 @@ OGRErr OGRPGDataSource::EndCopy()
     }
     else
         return OGRERR_NONE;
+}
+
+/************************************************************************/
+/*                CreateOgrSystemTablesMetadataTableIfNeeded()          */
+/************************************************************************/
+
+void OGRPGDataSource::CreateOgrSystemTablesMetadataTableIfNeeded()
+{
+    PGresult *hResult =
+        OGRPG_PQexec(hPGConn, "CREATE SCHEMA IF NOT EXISTS ogr_system_tables");
+    OGRPGClearResult(hResult);
+
+    hResult = OGRPG_PQexec(
+        hPGConn, "CREATE TABLE IF NOT EXISTS ogr_system_tables.metadata("
+                 "id SERIAL, "
+                 "schema_name TEXT NOT NULL, "
+                 "table_name TEXT NOT NULL, "
+                 "metadata TEXT,"
+                 "UNIQUE(schema_name, table_name))");
+    OGRPGClearResult(hResult);
+
+    hResult = OGRPG_PQexec(
+        hPGConn,
+        "DROP FUNCTION IF EXISTS "
+        "ogr_system_tables.event_trigger_function_for_metadata() CASCADE");
+    OGRPGClearResult(hResult);
+
+    hResult = OGRPG_PQexec(
+        hPGConn,
+        "CREATE FUNCTION "
+        "ogr_system_tables.event_trigger_function_for_metadata()\n"
+        "RETURNS event_trigger LANGUAGE plpgsql AS $$\n"
+        "DECLARE\n"
+        "    obj record;\n"
+        "BEGIN\n"
+        "    FOR obj IN SELECT * FROM pg_event_trigger_dropped_objects()\n"
+        "    LOOP\n"
+        "        IF obj.object_type = 'table' THEN\n"
+        "            DELETE FROM ogr_system_tables.metadata m WHERE "
+        "m.schema_name = obj.schema_name AND m.table_name = "
+        "obj.object_name;\n"
+        "        END IF;\n"
+        "    END LOOP;\n"
+        "END;\n"
+        "$$;");
+    OGRPGClearResult(hResult);
+
+    hResult =
+        OGRPG_PQexec(hPGConn, "DROP EVENT TRIGGER IF EXISTS "
+                              "ogr_system_tables_event_trigger_for_metadata");
+    OGRPGClearResult(hResult);
+
+    hResult = OGRPG_PQexec(
+        hPGConn,
+        "CREATE EVENT TRIGGER ogr_system_tables_event_trigger_for_metadata "
+        "ON sql_drop "
+        "EXECUTE FUNCTION "
+        "ogr_system_tables.event_trigger_function_for_metadata()");
+    OGRPGClearResult(hResult);
+}
+
+/************************************************************************/
+/*                    HasOgrSystemTablesMetadataTable()                 */
+/************************************************************************/
+
+bool OGRPGDataSource::HasOgrSystemTablesMetadataTable()
+{
+    if (!m_bOgrSystemTablesMetadataTableExistenceTested &&
+        CPLTestBool(CPLGetConfigOption("OGR_PG_ENABLE_METADATA", "YES")))
+    {
+        m_bOgrSystemTablesMetadataTableExistenceTested = true;
+        // Check that the ogr_system_tables.metadata table exists (without
+        // causing errors that might abort transactions)
+        PGresult *hResult = OGRPG_PQexec(
+            hPGConn,
+            "SELECT c.oid FROM pg_class c "
+            "JOIN pg_namespace n ON c.relnamespace=n.oid "
+            "WHERE c.relname = 'metadata' AND n.nspname = 'ogr_system_tables'");
+        m_bOgrSystemTablesMetadataTableFound =
+            (hResult && PQntuples(hResult) == 1 && !PQgetisnull(hResult, 0, 0));
+        OGRPGClearResult(hResult);
+    }
+    return m_bOgrSystemTablesMetadataTableFound;
 }

ogr/ogrsf_frmts/pg/ogrpgtablelayer.cpp
@@ -248,13 +248,16 @@ void OGRPGTableLayer::LoadMetadata()
         return;
     m_bMetadataLoaded = true;
 
+    if (!poDS->HasOgrSystemTablesMetadataTable())
+        return;
+
     PGconn *hPGConn = poDS->GetPGConn();
+
     const std::string osSQL(
         CPLSPrintf("SELECT metadata FROM ogr_system_tables.metadata WHERE "
                    "schema_name = %s AND table_name = %s",
                    OGRPGEscapeString(hPGConn, pszSchemaName).c_str(),
                    OGRPGEscapeString(hPGConn, pszTableName).c_str()));
-    CPLErrorStateBackuper oBackuper(CPLQuietErrorHandler);
     auto poSqlLyr = poDS->ExecuteSQL(osSQL.c_str(), nullptr, nullptr);
     if (poSqlLyr)
     {
@@ -286,8 +289,11 @@ void OGRPGTableLayer::LoadMetadata()
 
 void OGRPGTableLayer::SerializeMetadata()
 {
-    if (!m_bMetadataModified)
+    if (!m_bMetadataModified &&
+        CPLTestBool(CPLGetConfigOption("OGR_PG_ENABLE_METADATA", "YES")))
+    {
         return;
+    }
 
     PGconn *hPGConn = poDS->GetPGConn();
     CPLXMLNode *psMD = oMDMD.Serialize();
@@ -338,66 +344,25 @@ void OGRPGTableLayer::SerializeMetadata()
         }
     }
 
-    if (psMD)
+    const bool bIsUserTransactionActive = poDS->IsUserTransactionActive();
     {
         PGresult *hResult = OGRPG_PQexec(
-            hPGConn, "CREATE SCHEMA IF NOT EXISTS ogr_system_tables");
+            hPGConn, bIsUserTransactionActive
+                         ? "SAVEPOINT ogr_system_tables_metadata_savepoint"
+                         : "BEGIN");
         OGRPGClearResult(hResult);
+    }
 
-        hResult = OGRPG_PQexec(
-            hPGConn, "CREATE TABLE IF NOT EXISTS ogr_system_tables.metadata("
-                     "id SERIAL, "
-                     "schema_name TEXT NOT NULL, "
-                     "table_name TEXT NOT NULL, "
-                     "metadata TEXT,"
-                     "UNIQUE(schema_name, table_name))");
-        OGRPGClearResult(hResult);
-
-        hResult = OGRPG_PQexec(
-            hPGConn,
-            "DROP FUNCTION IF EXISTS "
-            "ogr_system_tables.event_trigger_function_for_metadata() CASCADE");
-        OGRPGClearResult(hResult);
-
-        hResult = OGRPG_PQexec(
-            hPGConn,
-            "CREATE FUNCTION "
-            "ogr_system_tables.event_trigger_function_for_metadata()\n"
-            "RETURNS event_trigger LANGUAGE plpgsql AS $$\n"
-            "DECLARE\n"
-            "    obj record;\n"
-            "BEGIN\n"
-            "    FOR obj IN SELECT * FROM pg_event_trigger_dropped_objects()\n"
-            "    LOOP\n"
-            "        IF obj.object_type = 'table' THEN\n"
-            "            DELETE FROM ogr_system_tables.metadata m WHERE "
-            "m.schema_name = obj.schema_name AND m.table_name = "
-            "obj.object_name;\n"
-            "        END IF;\n"
-            "    END LOOP;\n"
-            "END;\n"
-            "$$;");
-        OGRPGClearResult(hResult);
-
-        hResult = OGRPG_PQexec(hPGConn,
-                               "DROP EVENT TRIGGER IF EXISTS "
-                               "ogr_system_tables_event_trigger_for_metadata");
-        OGRPGClearResult(hResult);
-
-        hResult = OGRPG_PQexec(
-            hPGConn,
-            "CREATE EVENT TRIGGER ogr_system_tables_event_trigger_for_metadata "
-            "ON sql_drop "
-            "EXECUTE FUNCTION "
-            "ogr_system_tables.event_trigger_function_for_metadata()");
-        OGRPGClearResult(hResult);
+    if (psMD)
+    {
+        poDS->CreateOgrSystemTablesMetadataTableIfNeeded();
 
         CPLString osCommand;
         osCommand.Printf("DELETE FROM ogr_system_tables.metadata WHERE "
                          "schema_name = %s AND table_name = %s",
                          OGRPGEscapeString(hPGConn, pszSchemaName).c_str(),
                          OGRPGEscapeString(hPGConn, pszTableName).c_str());
-        hResult = OGRPG_PQexec(hPGConn, osCommand.c_str());
+        PGresult *hResult = OGRPG_PQexec(hPGConn, osCommand.c_str());
         OGRPGClearResult(hResult);
 
         CPLXMLNode *psRoot =
@@ -417,7 +382,7 @@ void OGRPGTableLayer::SerializeMetadata()
         CPLDestroyXMLNode(psRoot);
         CPLFree(pszXML);
     }
-    else
+    else if (poDS->HasOgrSystemTablesMetadataTable())
     {
         CPLString osCommand;
         osCommand.Printf("DELETE FROM ogr_system_tables.metadata WHERE "
@@ -426,6 +391,15 @@ void OGRPGTableLayer::SerializeMetadata()
                          OGRPGEscapeString(hPGConn, pszTableName).c_str());
         PGresult *hResult =
             OGRPG_PQexec(hPGConn, osCommand.c_str(), false, true);
+        OGRPGClearResult(hResult);
+    }
+
+    {
+        PGresult *hResult = OGRPG_PQexec(
+            hPGConn,
+            bIsUserTransactionActive
+                ? "RELEASE SAVEPOINT ogr_system_tables_metadata_savepoint"
+                : "COMMIT");
         OGRPGClearResult(hResult);
     }
 }