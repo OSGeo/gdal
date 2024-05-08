3d96b1795481ff77817a05a61aae3501fe427ac8
Jesse Anttila <35033029+jesseanttila-cai@users.noreply.github.com>
2024-05-09 01:49:28 +0300

    /vsis3/: include AWS_CONTAINER_CREDENTIALS_TOKEN in container credentials flow (#9877)
    This PR extends the [container credentials](https://docs.aws.amazon.com/sdkref/latest/guide/feature-container-credentials.html) functionality of `vsis3` to cover more cases, particularly [EKS Pod Identities](https://docs.aws.amazon.com/eks/latest/userguide/pod-identities.html).
    
    Two additional environment variables are now considered in the container credentials flow: `AWS_CONTAINER_AUTHORIZATION_TOKEN_FILE` and `AWS_CONTAINER_AUTHORIZATION_TOKEN`, with the former taking precedence over the latter (as described in AWS SDK documentation).
    
    In addition to the included tests, I have tested the `AWS_CONTAINER_AUTHORIZATION_TOKEN_FILE`-based credential flow in practice on EKS.

--- port/cpl_aws.cpp
+++ port/cpl_aws.cpp
@@ -664,24 +664,22 @@ static EC2InstanceCertainty IsMachinePotentiallyEC2Instance()
 }
 
 /************************************************************************/
-/*                   ReadAWSWebIdentityTokenFile()                      */
+/*                   ReadAWSTokenFile()                                 */
 /************************************************************************/
 
-static bool
-ReadAWSWebIdentityTokenFile(const std::string &osWebIdentityTokenFile,
-                            std::string &webIdentityToken)
+static bool ReadAWSTokenFile(const std::string &osAWSTokenFile,
+                             std::string &awsToken)
 {
     GByte *pabyOut = nullptr;
-    if (!VSIIngestFile(nullptr, osWebIdentityTokenFile.c_str(), &pabyOut,
-                       nullptr, -1))
+    if (!VSIIngestFile(nullptr, osAWSTokenFile.c_str(), &pabyOut, nullptr, -1))
         return false;
 
-    webIdentityToken = reinterpret_cast<char *>(pabyOut);
+    awsToken = reinterpret_cast<char *>(pabyOut);
     VSIFree(pabyOut);
     // Remove trailing end-of-line character
-    if (!webIdentityToken.empty() && webIdentityToken.back() == '\n')
-        webIdentityToken.resize(webIdentityToken.size() - 1);
-    return !webIdentityToken.empty();
+    if (!awsToken.empty() && awsToken.back() == '\n')
+        awsToken.resize(awsToken.size() - 1);
+    return !awsToken.empty();
 }
 
 /************************************************************************/
@@ -753,7 +751,7 @@ bool VSIS3HandleHelper::GetConfigurationFromAssumeRoleWithWebIdentity(
 
     // Get token from web identity token file
     std::string webIdentityToken;
-    if (!ReadAWSWebIdentityTokenFile(webIdentityTokenFile, webIdentityToken))
+    if (!ReadAWSTokenFile(webIdentityTokenFile, webIdentityToken))
     {
         CPLDebug("AWS", "%s is empty", webIdentityTokenFile.c_str());
         return false;
@@ -859,6 +857,36 @@ bool VSIS3HandleHelper::GetConfigurationFromEC2(
                                    osPathForOption.c_str(),
                                    "AWS_CONTAINER_CREDENTIALS_RELATIVE_URI", "")
                              : std::string());
+    // coverity[tainted_data]
+    const std::string osECSTokenFile(
+        (osECSFullURI.empty() && osECSRelativeURI.empty())
+            ? std::string()
+            : VSIGetPathSpecificOption(osPathForOption.c_str(),
+                                       "AWS_CONTAINER_AUTHORIZATION_TOKEN_FILE",
+                                       ""));
+
+    // coverity[tainted_data]
+    const std::string osECSTokenValue(
+        (osECSFullURI.empty() && osECSRelativeURI.empty() &&
+         !osECSTokenFile.empty())
+            ? std::string()
+            : VSIGetPathSpecificOption(osPathForOption.c_str(),
+                                       "AWS_CONTAINER_AUTHORIZATION_TOKEN",
+                                       ""));
+
+    std::string osECSToken;
+    if (!osECSTokenFile.empty())
+    {
+        if (!ReadAWSTokenFile(osECSTokenFile, osECSToken))
+        {
+            CPLDebug("AWS", "%s is empty", osECSTokenFile.c_str());
+        }
+    }
+    else if (!osECSTokenValue.empty())
+    {
+        osECSToken = osECSTokenValue;
+    }
+
     std::string osToken;
     if (!osECSFullURI.empty())
     {
@@ -996,6 +1024,11 @@ bool VSIS3HandleHelper::GetConfigurationFromEC2(
     {
         aosOptions.SetNameValue(
             "HEADERS", ("X-aws-ec2-metadata-token: " + osToken).c_str());
+    }
+    else if (!osECSToken.empty())
+    {
+        aosOptions.SetNameValue("HEADERS",
+                                ("Authorization: " + osECSToken).c_str());
     }
     CPLHTTPResult *psResult =
         CPLHTTPFetch(osURLRefreshCredentials.c_str(), aosOptions.List());
