80221898134358b4369bb5746220aca5abbb2d0b
Even Rouault <even.rouault@spatialys.com>
2024-05-12 20:16:39 +0200

    CI Conda: remove no longer needed patches

--- ci/travis/conda/setup.sh
+++ ci/travis/conda/setup.sh
@@ -16,13 +16,6 @@ git clone  https://github.com/conda-forge/gdal-feedstock.git
 
 cd gdal-feedstock
 
-# Set -DOGR_REGISTER_DRIVER_ARROW_FOR_LATER_PLUGIN=ON and -DOGR_REGISTER_DRIVER_PARQUET_FOR_LATER_PLUGIN=ON
-# To be dropped once that's upstreamed in conda-forge/gdal-feedstock.git
-patch -p1 < ../ci/travis/conda/build.sh.patch
-patch -p1 --binary < ../ci/travis/conda/bld.bat.patch
-
-patch -p1 < ../ci/travis/conda/0001-Fix-build-of-Python-bindings-due-to-https-github.com.patch
-
 cat > recipe/recipe_clobber.yaml <<EOL
 source:
   path: ../../../gdal
