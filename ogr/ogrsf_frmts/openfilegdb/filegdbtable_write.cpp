eeb0626cd2764a5292e1867f208387d2cfd874b5
Even Rouault <even.rouault@spatialys.com>
2024-05-25 19:33:19 +0200

    OpenFileGDB writer: .gdbtable header must be rewritten when updating an existing feature at the end of file
    The only consequence of the omission of that fix is that the file size
    in the .gdbtable could be incorrect. A wrong file size in the header has no
    consequence on OpenFileGDB reader/writer. It also doesn't seem to affect
    Esri reader side, but it could *potentially* affect the writing side, if
    that field was trusted to seek to the end of the file.

--- ogr/ogrsf_frmts/openfilegdb/filegdbtable_write.cpp
+++ ogr/ogrsf_frmts/openfilegdb/filegdbtable_write.cpp
@@ -2016,6 +2016,7 @@ bool FileGDBTable::UpdateFeature(int nFID,
         m_nRowBufferMaxSize = std::max(m_nRowBufferMaxSize, m_nRowBlobLength);
         if (nFreeOffset == OFFSET_MINUS_ONE)
         {
+            m_bDirtyHeader = true;
             m_nFileSize += sizeof(uint32_t) + m_nRowBlobLength;
         }
 
