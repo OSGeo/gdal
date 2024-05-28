c954680275f97cabf1e39e717916d594020198b3
Even Rouault <even.rouault@spatialys.com>
2024-05-28 22:12:45 +0200

    CI: skip test_gdal_polygonize_help on sanitize CI

--- autotest/pyscripts/test_gdal_polygonize.py
+++ autotest/pyscripts/test_gdal_polygonize.py
@@ -31,6 +31,7 @@
 
 import os
 
+import gdaltest
 import ogrtest
 import pytest
 import test_py_scripts
@@ -53,6 +54,9 @@ def script_path():
 
 
 def test_gdal_polygonize_help(script_path):
+
+    if gdaltest.is_travis_branch("sanitize"):
+        pytest.skip("fails on sanitize for unknown reason")
 
     assert "ERROR" not in test_py_scripts.run_py_script(
         script_path, "gdal_polygonize", "--help"
