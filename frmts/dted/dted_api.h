204eac9c13e1ef1a1cbd3aedcbd955d896473011
sab56 <sab5601@outlook.com>
2024-06-10 12:09:29 +0100

    Added DTED metadata items Security Control and Security Handling

--- frmts/dted/dted_api.h
+++ frmts/dted/dted_api.h
@@ -194,7 +194,9 @@ typedef enum
     DTEDMD_ORIGINLAT = 21,         /* UHL 13+7 */
     DTEDMD_NIMA_DESIGNATOR = 22,   /* DSI 60 + 5 */
     DTEDMD_PARTIALCELL_DSI = 23,   /* DSI 289 + 2 */
-    DTEDMD_MAX = 23
+    DTEDMD_SECURITYCONTROL = 24,   /* DSI 5 + 3 */
+    DTEDMD_SECURITYHANDLING = 25,  /* DSI 8 + 27 */
+    DTEDMD_MAX = 25
 } DTEDMetaDataCode;
 
 char *DTEDGetMetadata(DTEDInfo *, DTEDMetaDataCode);
