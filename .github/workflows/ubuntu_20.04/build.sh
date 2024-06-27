db2e14d0d97c1b131253ea01971b72475d273a5f
Even Rouault <even.rouault@spatialys.com>
2024-06-27 19:03:44 +0200

    CI: test building MrSID driver in standalone mode

--- .github/workflows/ubuntu_20.04/build.sh
+++ .github/workflows/ubuntu_20.04/build.sh
@@ -5,12 +5,12 @@ set -eu
 export CXXFLAGS="-march=native -O2 -Wodr -flto-odr-type-merging -Werror"
 export CFLAGS="-O2 -march=native -Werror"
 
-cmake ${GDAL_SOURCE_DIR:=..} \
+cmake "${GDAL_SOURCE_DIR:=..}" \
     -DUSE_CCACHE=ON \
     -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
-    -DCMAKE_INSTALL_PREFIX=/usr \
-    -DGDAL_USE_TIFF_INTERNAL=ON \
-    -DGDAL_USE_GEOTIFF_INTERNAL=ON \
+    -DCMAKE_INSTALL_PREFIX=/tmp/install-gdal \
+    -DGDAL_USE_TIFF_INTERNAL=OFF \
+    -DGDAL_USE_GEOTIFF_INTERNAL=OFF \
     -DECW_ROOT=/opt/libecwj2-3.3 \
     -DMRSID_ROOT=/usr/local \
     -DFileGDB_ROOT=/usr/local/FileGDB_API \
@@ -20,5 +20,13 @@ cmake ${GDAL_SOURCE_DIR:=..} \
 unset CXXFLAGS
 unset CFLAGS
 
-make -j$(nproc)
-make -j$(nproc) install DESTDIR=/tmp/install-gdal
+make "-j$(nproc)"
+make "-j$(nproc)" install
+
+# Test building MrSID driver in standalone mode
+mkdir build_mrsid
+cd build_mrsid
+cmake -S ${GDAL_SOURCE_DIR:=..}/frmts/mrsid -DMRSID_ROOT=/usr/local -DCMAKE_PREFIX_PATH=/tmp/install-gdal
+cmake --build . "-j$(nproc)"
+test -f gdal_MrSID.so
+cd ..
