199117a49d48f5f19f232d501b9e23aabc595aa4
Daniel Baston <dbaston@gmail.com>
2024-07-09 10:35:11 -0400

    Doc: Use html_static_path and html_extra_path to handle non-rst HTML content

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -88,6 +88,14 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
 
+html_static_path = [
+    "_static",
+]
+
+html_extra_path = [
+    "../build/html_extra",
+]
+
 # If true, links to the reST sources are added to the pages.
 html_show_sourcelink = False
 
