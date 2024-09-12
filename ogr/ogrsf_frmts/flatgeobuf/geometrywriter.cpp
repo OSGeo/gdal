ff18e0b0d00b873701c33961483d415e39746e56
Björn Harrtell <bjorn@septima.dk>
2024-09-12 20:49:48 +0200

    Revert to changes to writing logic

--- ogr/ogrsf_frmts/flatgeobuf/geometrywriter.cpp
+++ ogr/ogrsf_frmts/flatgeobuf/geometrywriter.cpp
@@ -91,12 +91,6 @@ uint32_t GeometryWriter::writeSimpleCurve(const OGRSimpleCurve *sc)
 
 void GeometryWriter::writeMultiLineString(const OGRMultiLineString *mls)
 {
-    // NOTE: should not write ends if only single part
-    if (mls->getNumGeometries() == 1)
-    {
-        writeSimpleCurve(mls->getGeometryRef(0));
-        return;
-    }
     uint32_t e = 0;
     for (const auto part : *mls)
         if (!part->IsEmpty())
