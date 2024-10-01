8cdf10794cacf9adb571700672c7300d50ed7fee
Even Rouault <even.rouault@spatialys.com>
2024-10-01 17:49:34 +0200

    PDS4: embed pds4_template.xml if EMBED_RESOURCE_FILES

---
+++ frmts/pds/embedded_resources.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+// Copyright 2024, Even Rouault <even.rouault at spatialys.com>
+
+#include "embedded_resources.h"
+
+static const char szPDS4Template[] = {
+#embed "data/pds4_template.xml"
+    , 0};
+
+const char *PDS4GetEmbeddedTemplate()
+{
+    return szPDS4Template;
+}
