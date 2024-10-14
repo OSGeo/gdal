7ef284b339aa382ee6ee90e9f65e36687af34ad9
Even Rouault <even.rouault@spatialys.com>
2024-10-14 13:04:03 +0200

    Add read-only OGR ADBC (Arrow Database Connectivity) driver
    Cf https://arrow.apache.org/adbc/current/index.html for what ADBC is.
    
    Depends on the adbc-driver-manager library.
    
    The driver is read-only, and there is no support for spatial data currently.
    
    Beyond official ADBC drivers (adbc-driver-sqlite,
    adbc-driver-postgresql, adbc-driver-snowflake, adbc-driver-bigquery,
    etc.), it can also be used to read Parquet or DuckDB datasets using libduckdb, if
    libduckdb is installed and can be loaded through dynamic shared library opening.

---
+++ ogr/ogrsf_frmts/adbc/ogradbcdriver.cpp
@@ -0,0 +1,46 @@
+/******************************************************************************
+ *
+ * Project:  GDAL
+ * Purpose:  Arrow Database Connectivity driver
+ * Author:   Even Rouault, <even dot rouault at spatialys.com>
+ *
+ ******************************************************************************
+ * Copyright (c) 2024, Even Rouault <even dot rouault at spatialys.com>
+ *
+ * SPDX-License-Identifier: MIT
+ ****************************************************************************/
+
+#include "ogr_adbc.h"
+#include "ogradbcdrivercore.h"
+
+/************************************************************************/
+/*                         OGRADBCDriverOpen()                          */
+/************************************************************************/
+
+static GDALDataset *OGRADBCDriverOpen(GDALOpenInfo *poOpenInfo)
+{
+    if (!OGRADBCDriverIdentify(poOpenInfo))
+        return nullptr;
+    auto poDS = std::make_unique<OGRADBCDataset>();
+    if (!poDS->Open(poOpenInfo))
+        return nullptr;
+    return poDS.release();
+}
+
+/************************************************************************/
+/*                          RegisterOGRADBC()                           */
+/************************************************************************/
+
+void RegisterOGRADBC()
+{
+    if (!GDAL_CHECK_VERSION(DRIVER_NAME))
+        return;
+
+    if (GDALGetDriverByName(DRIVER_NAME) != nullptr)
+        return;
+
+    GDALDriver *poDriver = new GDALDriver();
+    OGRADBCDriverSetCommonMetadata(poDriver);
+    poDriver->pfnOpen = OGRADBCDriverOpen;
+    GetGDALDriverManager()->RegisterDriver(poDriver);
+}
