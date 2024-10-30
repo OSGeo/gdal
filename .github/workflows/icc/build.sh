38843b6dbf6e3b7fd698b4546c082309161a9353
Erik Schnetter <schnetter@gmail.com>
2024-10-30 11:53:03 -0400

    Use Intel compiler in build_ext

--- .github/workflows/icc/build.sh
+++ .github/workflows/icc/build.sh
@@ -2,6 +2,14 @@
 
 set -eu
 
+# Export the compilers as environment variables so that Python's
+# `build_ext` uses these compilers instead of the system compilers.
+# This is important because they might differ in supporting
+# `_Float16`.
+CC=icx
+CXX=idx
+export CC CXX
+
 cmake ${GDAL_SOURCE_DIR:=..} \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_C_COMPILER=icx \
