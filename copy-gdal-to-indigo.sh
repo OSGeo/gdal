7befa3a4e30a330d765934847b91dc04b33650f6
Erik Schnetter <schnetter@gmail.com>
2024-10-30 11:04:07 -0400

    Remove file

--- copy-gdal-to-indigo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-rsync -Paz --exclude .git --exclude __pycache__ --exclude 'autotest/gdrivers/tmp/cache' --exclude 'autotest/pyscripts/out*' --exclude 'autotest/gcore/tmp' --exclude 'autotest/ogr/tmp' --exclude 'autotest/utilities/tmp' --exclude 'build-*' --exclude '*~' ~/src/gdal indigo:src
