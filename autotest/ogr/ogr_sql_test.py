17bd93433d68009dbb78845b9daff84d45ad5496
Even Rouault <even.rouault@spatialys.com>
2024-11-01 13:37:58 +0100

    OGR SQL: fix wrong handling of NULL expression in AND and NOT (3.10.0 regression)
    Fixes https://github.com/ropensci/osmextract/issues/298 (provided they
    also fix their SQL expressions)

--- autotest/ogr/ogr_sql_test.py
+++ autotest/ogr/ogr_sql_test.py
@@ -1952,6 +1952,13 @@ def get_available_dialects():
         ("(NOT intfield = 0) AND NOT (intfield IS NULL)", 1),
         ("NOT (intfield = 0 OR intfield IS NOT NULL)", 0),
         ("(NOT intfield = 0) AND NOT (intfield IS NOT NULL)", 0),
+        ("intfield <> 0 AND intfield <> 2", 1),
+        ("intfield IS NOT NULL AND intfield NOT IN (2)", 1),
+        ("NOT(intfield NOT IN (1) AND NULL NOT IN (1))", 1),
+        ("NOT(intfield IS NOT NULL AND intfield NOT IN (2))", 1),
+        ("NOT(NOT(intfield IS NOT NULL AND intfield NOT IN (2)))", 1),
+        ("NOT (intfield = 0 AND intfield = 0)", 1),
+        ("(intfield NOT IN (1) AND NULL NOT IN (1)) IS NULL", 1),
         # realfield
         ("1 + realfield >= 0", 1),
         ("realfield = 0", 0),
