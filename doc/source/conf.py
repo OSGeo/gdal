81489921bd5719a329d5420705e9cbef1fa3df3d
Even Rouault <even.rouault@spatialys.com>
2024-11-21 19:35:37 +0100

    Preparatory steps to have https://gdal.org default to a 'stable' branch

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -160,7 +160,9 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_extra_path = ["../build/html_extra"]
+
+# For generated content and robots.txt
+html_extra_path = ["../build/html_extra", "extra_path"]
 
 html_js_files = ["announcement.js"]
 html_css_files = ["announcement.css"]
