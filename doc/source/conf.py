de06f59e106e9891517d063db58ca15d3d35debd
Even Rouault <even.rouault@spatialys.com>
2024-11-22 18:49:32 +0100

    Doc: Revert changes related to user survey
    Fixes #11156
    
    Revert "Doc: user survey announcement: remove end date"
    
    This reverts commit e6ae79a9a1216bc01be6e860c58c5d336d751a19.
    
    Revert "Doc: announcement.js: tweak language"
    
    This reverts commit 051a853584addf8e6ee8fe7a832db154cf1f8f32.
    
    Revert "Doc: add a top-page banner advertizing the survey"
    
    This reverts commit 1e3229f9e3c3489cb2530543dd3490a87630c4fa.
    
    Revert "Doc: main page: link to user survey (to be reverted after Nov 11th)"
    
    This reverts commit 6f3083d6c0b891f99e3d990aa942ee56953df89c.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -165,9 +165,6 @@ html_static_path = ["_static"]
 # For generated content and robots.txt
 html_extra_path = ["../build/html_extra", "extra_path"]
 
-html_js_files = ["announcement.js"]
-html_css_files = ["announcement.css"]
-
 # If true, links to the reST sources are added to the pages.
 html_show_sourcelink = False
 
