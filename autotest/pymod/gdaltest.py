972ee002072b6008b8b2d6059317d9a055c3fd35
Dan Baston <dbaston@gmail.com>
2025-04-05 13:59:17 -0400

    gdaltest.py: Avoid error with pytest < 8.2

--- autotest/pymod/gdaltest.py
+++ autotest/pymod/gdaltest.py
@@ -2147,4 +2147,7 @@ def gdal_has_vrt_expression_dialect(dialect):
 
 
 def importorskip_gdal_array():
-    return pytest.importorskip("osgeo.gdal_array", exc_type=ImportError)
+    pytest_version = [int(x) for x in pytest.__version__.split(".")]
+    if pytest_version >= [8, 2, 0]:
+        return pytest.importorskip("osgeo.gdal_array", exc_type=ImportError)
+    return pytest.importorskip("osgeo.gdal_array")
