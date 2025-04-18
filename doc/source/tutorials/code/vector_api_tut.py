9520ed6a7bc235c13b041ddd69a580f3b01bf429
sethg <sethg@geographika.co.uk>
2025-04-18 09:43:46 +0200

    Move 2 more tutorials to tabs and update Python code

---
+++ doc/source/tutorials/code/vector_api_tut.py
@@ -0,0 +1,42 @@
+import sys
+from osgeo import gdal
+from osgeo import ogr
+
+gdal.UseExceptions()
+
+ds = gdal.OpenEx("point", gdal.OF_VECTOR)
+if ds is None:
+    print("Open failed.\n")
+    sys.exit(1)
+
+lyr = ds.GetLayerByName("point")
+
+lyr.ResetReading()
+
+for feat in lyr:
+
+    feat_defn = lyr.GetLayerDefn()
+    for i in range(feat_defn.GetFieldCount()):
+        field_defn = feat_defn.GetFieldDefn(i)
+
+        # Tests below can be simplified with just :
+        # print feat.GetField(i)
+        if (
+            field_defn.GetType() == ogr.OFTInteger
+            or field_defn.GetType() == ogr.OFTInteger64
+        ):
+            print("%d" % feat.GetFieldAsInteger64(i))
+        elif field_defn.GetType() == ogr.OFTReal:
+            print("%.3f" % feat.GetFieldAsDouble(i))
+        elif field_defn.GetType() == ogr.OFTString:
+            print("%s" % feat.GetFieldAsString(i))
+        else:
+            print("%s" % feat.GetFieldAsString(i))
+
+    geom = feat.GetGeometryRef()
+    if geom is not None and geom.GetGeometryType() == ogr.wkbPoint:
+        print("%.3f, %.3f" % (geom.GetX(), geom.GetY()))
+    else:
+        print("no point geometry\n")
+
+ds = None
---
+++ doc/source/tutorials/code/vector_api_tut2.py
@@ -0,0 +1,55 @@
+import sys
+from osgeo import gdal
+from osgeo import ogr
+
+
+driverName = "ESRI Shapefile"
+drv = gdal.GetDriverByName(driverName)
+if drv is None:
+    print("%s driver not available.\n" % driverName)
+    sys.exit(1)
+
+ds = drv.Create("point_out.shp", 0, 0, 0, gdal.GDT_Unknown)
+if ds is None:
+    print("Creation of output file failed.\n")
+    sys.exit(1)
+
+lyr = ds.CreateLayer("point_out", None, ogr.wkbPoint)
+if lyr is None:
+    print("Layer creation failed.\n")
+    sys.exit(1)
+
+field_defn = ogr.FieldDefn("Name", ogr.OFTString)
+field_defn.SetWidth(32)
+
+if lyr.CreateField(field_defn) != 0:
+    print("Creating Name field failed.\n")
+    sys.exit(1)
+
+# Expected format of user input: x y name
+linestring = input()
+linelist = linestring.split()
+
+while len(linelist) == 3:
+    x = float(linelist[0])
+    y = float(linelist[1])
+    name = linelist[2]
+
+    feat = ogr.Feature(lyr.GetLayerDefn())
+    feat.SetField("Name", name)
+
+    pt = ogr.Geometry(ogr.wkbPoint)
+    pt.SetPoint_2D(0, x, y)
+
+    feat.SetGeometry(pt)
+
+    if lyr.CreateFeature(feat) != 0:
+        print("Failed to create feature in shapefile.\n")
+        sys.exit(1)
+
+    feat.Destroy()
+
+    linestring = input()
+    linelist = linestring.split()
+
+ds = None
