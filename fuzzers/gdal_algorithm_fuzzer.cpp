97e5c9d34f3117a9d77cabec7ab5eb9c8cc5f55c
Even Rouault <even.rouault@spatialys.com>
2025-05-03 01:28:09 +0200

    Add fuzzers/gdal_algorithm_fuzzer.cpp [ci skip]

---
+++ fuzzers/gdal_algorithm_fuzzer.cpp
@@ -0,0 +1,120 @@
+/******************************************************************************
+ *
+ * Project:  GDAL
+ * Purpose:  Fuzzer for GDALAlgorithm
+ * Author:   Even Rouault <even dot rouault at spatialys.com>
+ *
+ ******************************************************************************
+ * Copyright (c) 2025, Even Rouault <even dot rouault at spatialys.com>
+ *
+ * SPDX-License-Identifier: MIT
+ ****************************************************************************/
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "gdal.h"
+#include "gdalalgorithm.h"
+
+#include "cpl_conv.h"
+#include "cpl_vsi.h"
+
+extern "C" int LLVMFuzzerInitialize(int *argc, char ***argv);
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *buf, size_t len);
+
+int LLVMFuzzerInitialize(int * /*argc*/, char ***argv)
+{
+    const char *exe_path = (*argv)[0];
+    if (CPLGetConfigOption("GDAL_DATA", nullptr) == nullptr)
+    {
+        CPLSetConfigOption("GDAL_DATA", CPLGetPathSafe(exe_path).c_str());
+    }
+    CPLSetConfigOption("CPL_TMPDIR", "/tmp");
+    CPLSetConfigOption("DISABLE_OPEN_REAL_NETCDF_FILES", "YES");
+    // Disable PDF text rendering as fontconfig cannot access its config files
+    CPLSetConfigOption("GDAL_PDF_RENDERING_OPTIONS", "RASTER,VECTOR");
+    // to avoid timeout in WMS driver
+    CPLSetConfigOption("GDAL_WMS_ABORT_CURL_REQUEST", "YES");
+    CPLSetConfigOption("GDAL_HTTP_TIMEOUT", "1");
+    CPLSetConfigOption("GDAL_HTTP_CONNECTTIMEOUT", "1");
+    CPLSetConfigOption("GDAL_CACHEMAX", "1000");  // Limit to 1 GB
+
+    GDALAllRegister();
+
+    return 0;
+}
+
+int LLVMFuzzerTestOneInput(const uint8_t *buf, size_t len)
+{
+    VSILFILE *fp = VSIFileFromMemBuffer(
+        "/vsimem/test.tar",
+        reinterpret_cast<GByte *>(const_cast<uint8_t *>(buf)), len, FALSE);
+    VSIFCloseL(fp);
+
+    CPLPushErrorHandler(CPLQuietErrorHandler);
+
+    auto &singleton = GDALGlobalAlgorithmRegistry::GetSingleton();
+    std::unique_ptr<GDALAlgorithm> alg;
+
+    fp = VSIFOpenL("/vsitar//vsimem/test.tar/cmd.txt", "rb");
+    if (fp != nullptr)
+    {
+        const char *pszLine = nullptr;
+        GDALAlgorithmArg *arg = nullptr;
+        while ((pszLine = CPLReadLineL(fp)) != nullptr)
+        {
+            printf("%s\n", pszLine);
+            if (!alg)
+            {
+                alg = singleton.Instantiate(pszLine);
+                if (!alg)
+                    break;
+            }
+            else
+            {
+                auto subalg = alg->InstantiateSubAlgorithm(pszLine);
+                if (subalg)
+                {
+                    alg = std::move(subalg);
+                }
+                else
+                {
+                    if (!arg)
+                    {
+                        arg = alg->GetArg(pszLine);
+                        if (!arg)
+                            break;
+                        if (arg->GetType() == GAAT_BOOLEAN)
+                        {
+                            arg->Set(true);
+                            arg = nullptr;
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            arg->Set(pszLine);
+                        }
+                        catch (const std::exception &)
+                        {
+                        }
+                        arg = nullptr;
+                    }
+                }
+            }
+        }
+        if (alg)
+        {
+            alg->Run();
+            alg->Finalize();
+        }
+        VSIFCloseL(fp);
+    }
+
+    VSIRmdirRecursive("/vsimem/");
+
+    CPLPopErrorHandler();
+
+    return 0;
+}
