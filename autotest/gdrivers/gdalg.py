0381311161428656077894cce3ae6db9c3b95855
Even Rouault <even.rouault@spatialys.com>
2025-05-06 00:52:48 +0200

    gdal raster/vector pipeline to .gdalg.json: add 'gdal_version' member
    as already done for non-pipeline GDALG capable algorithms

--- autotest/gdrivers/gdalg.py
+++ autotest/gdrivers/gdalg.py
@@ -272,7 +272,10 @@ def test_gdalg_generate_from_raster_pipeline(tmp_vsimem):
             "--overwrite",
         ]
     )
-    assert json.loads(gdal.VSIFile(out_filename, "rb").read()) == {
+    j = json.loads(gdal.VSIFile(out_filename, "rb").read())
+    assert "gdal_version" in j
+    del j["gdal_version"]
+    assert j == {
         "command_line": "gdal raster pipeline read --input data/byte.tif ! reproject --dst-crs EPSG:4326",
         "type": "gdal_streamed_alg",
     }
@@ -322,7 +325,10 @@ def test_gdalg_generate_from_vector_pipeline(tmp_vsimem):
             out_filename,
         ]
     )
-    assert json.loads(gdal.VSIFile(out_filename, "rb").read()) == {
+    j = json.loads(gdal.VSIFile(out_filename, "rb").read())
+    assert "gdal_version" in j
+    del j["gdal_version"]
+    assert j == {
         "command_line": "gdal vector pipeline read --input ../ogr/data/poly.shp ! reproject --dst-crs EPSG:4326",
         "type": "gdal_streamed_alg",
     }
@@ -344,7 +350,10 @@ def test_gdalg_generate_from_vector_pipeline_geom(tmp_vsimem):
             out_filename,
         ]
     )
-    assert json.loads(gdal.VSIFile(out_filename, "rb").read()) == {
+    j = json.loads(gdal.VSIFile(out_filename, "rb").read())
+    assert "gdal_version" in j
+    del j["gdal_version"]
+    assert j == {
         "command_line": "gdal vector pipeline read --input ../ogr/data/poly.shp ! geom set-type --geometry-type MULTIPOLYGON",
         "type": "gdal_streamed_alg",
     }
