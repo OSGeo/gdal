8e74964d4913b33e3453009a2aa4b051a9f51746
Even Rouault <even.rouault@spatialys.com>
2025-05-12 22:56:52 +0200

    gdaladdo: fix error message about --partial-refresh-from-source-timestamp only working with VRT (works with GTI too)

--- apps/gdaladdo.cpp
+++ apps/gdaladdo.cpp
@@ -207,15 +207,15 @@ static bool PartialRefreshFromSourceTimestamp(
                        nMinSize, anOvrIndices))
         return false;
 
-    VSIStatBufL sStatVRTOvr;
+    VSIStatBufL sStatOvr;
     std::string osVRTOvr(std::string(poDS->GetDescription()) + ".ovr");
-    if (VSIStatL(osVRTOvr.c_str(), &sStatVRTOvr) != 0)
+    if (VSIStatL(osVRTOvr.c_str(), &sStatOvr) != 0)
     {
         CPLError(CE_Failure, CPLE_AppDefined, "Cannot find %s\n",
                  osVRTOvr.c_str());
         return false;
     }
-    if (sStatVRTOvr.st_mtime == 0)
+    if (sStatOvr.st_mtime == 0)
     {
         CPLError(CE_Failure, CPLE_AppDefined,
                  "Cannot get modification time of %s\n", osVRTOvr.c_str());
@@ -247,7 +247,7 @@ static bool PartialRefreshFromSourceTimestamp(
                 if (VSIStatL(poSource->GetSourceDatasetName().c_str(),
                              &sStatSource) == 0)
                 {
-                    if (sStatSource.st_mtime > sStatVRTOvr.st_mtime)
+                    if (sStatSource.st_mtime > sStatOvr.st_mtime)
                     {
                         double dfXOff, dfYOff, dfXSize, dfYSize;
                         poSource->GetDstWindow(dfXOff, dfYOff, dfXSize,
@@ -308,7 +308,7 @@ static bool PartialRefreshFromSourceTimestamp(
 #else
     else if (auto poGTIDS = GDALDatasetCastToGTIDataset(poDS))
     {
-        regions = GTIGetSourcesMoreRecentThan(poGTIDS, sStatVRTOvr.st_mtime);
+        regions = GTIGetSourcesMoreRecentThan(poGTIDS, sStatOvr.st_mtime);
         for (const auto &region : regions)
         {
             dfTotalPixels +=
@@ -320,7 +320,7 @@ static bool PartialRefreshFromSourceTimestamp(
     {
         CPLError(CE_Failure, CPLE_AppDefined,
                  "--partial-refresh-from-source-timestamp only works on a VRT "
-                 "dataset");
+                 "or GTI dataset");
         return false;
     }
 
