2c04056d4560a78b540a09c2578dd47ce626bb94
Even Rouault <even.rouault@spatialys.com>
2025-05-24 00:44:54 +0200

    MEMDataset::BuildOverviews(): set geotransform and SRS from source dataset

--- frmts/mem/memdataset.cpp
+++ frmts/mem/memdataset.cpp
@@ -888,6 +888,16 @@ CPLErr MEMDataset::IBuildOverviews(const char *pszResampling, int nOverviews,
                 (nRasterXSize + panOverviewList[i] - 1) / panOverviewList[i];
             poOvrDS->nRasterYSize =
                 (nRasterYSize + panOverviewList[i] - 1) / panOverviewList[i];
+            poOvrDS->bGeoTransformSet = bGeoTransformSet;
+            memcpy(poOvrDS->adfGeoTransform, adfGeoTransform,
+                   6 * sizeof(double));
+            const double dfOvrXRatio =
+                static_cast<double>(nRasterXSize) / poOvrDS->nRasterXSize;
+            const double dfOvrYRatio =
+                static_cast<double>(nRasterYSize) / poOvrDS->nRasterYSize;
+            GDALRescaleGeoTransform(poOvrDS->adfGeoTransform, dfOvrXRatio,
+                                    dfOvrYRatio);
+            poOvrDS->m_oSRS = m_oSRS;
             for (int iBand = 0; iBand < nBands; iBand++)
             {
                 const GDALDataType eDT =
