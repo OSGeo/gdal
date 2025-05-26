3296a89be610854dca6ad189cd3373caf182bc70
Even Rouault <even.rouault@spatialys.com>
2025-05-26 16:44:30 +0200

    AirSAR: fix dangerousTypeCast cppcheck warnings

--- frmts/airsar/airsardataset.cpp
+++ frmts/airsar/airsardataset.cpp
@@ -156,11 +156,11 @@ CPLErr AirSARRasterBand::IReadBlock(int /* nBlockXOff */, int nBlockYOff,
     float *pafLine = (float *)pImage;
     const double SQRT_2 = 1.4142135623730951;
 
-    CPLErr eErr = ((AirSARDataset *)poDS)->LoadLine(nBlockYOff);
+    CPLErr eErr = cpl::down_cast<AirSARDataset *>(poDS)->LoadLine(nBlockYOff);
     if (eErr != CE_None)
         return eErr;
 
-    double *padfMatrix = ((AirSARDataset *)poDS)->padfMatrix;
+    double *padfMatrix = cpl::down_cast<AirSARDataset *>(poDS)->padfMatrix;
 
     if (nBand == 1) /* C11 */
     {
