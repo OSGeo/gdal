ff01f8d746ffce3c1aec8b46ccad41a3b7ff1f5e
Even Rouault <even.rouault@spatialys.com>
2025-06-12 15:46:20 +0200

    OGRShapeLayer::GetNextArrowArray(): fix memleak when FID column is not requested

--- ogr/ogrsf_frmts/shape/ogrshapelayer.cpp
+++ ogr/ogrsf_frmts/shape/ogrshapelayer.cpp
@@ -3889,7 +3889,11 @@ int OGRShapeLayer::GetNextArrowArray(struct ArrowArrayStream *stream,
     }
 
     if (!sHelper.m_bIncludeFID)
+    {
+        if (out_array->release)
+            out_array->release(out_array);
         return OGRLayer::GetNextArrowArray(stream, out_array);
+    }
 
     m_bLastGetNextArrowArrayUsedOptimizedCodePath = true;
     int nCount = 0;
