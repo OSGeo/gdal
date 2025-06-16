e9a98381000c9ff9fa89a82d1023e26f2578692b
Yorick de Wid <yorickdewid@users.noreply.github.com>
2025-06-16 13:45:22 +0200

    [Lint] LVBAG: make it compatible with -Weffc++ -Wold-style-cast

--- ogr/ogrsf_frmts/lvbag/ogrlvbaglayer.cpp
+++ ogr/ogrsf_frmts/lvbag/ogrlvbaglayer.cpp
@@ -5,7 +5,7 @@
  * Author:   Laixer B.V., info at laixer dot com
  *
  ******************************************************************************
- * Copyright (c) 2021, Laixer B.V. <info at laixer dot com>
+ * Copyright (c) 2020, Laixer B.V. <info at laixer dot com>
  *
  * SPDX-License-Identifier: MIT
  ****************************************************************************/
@@ -37,7 +37,8 @@ OGRLVBAGLayer::OGRLVBAGLayer(const char *pszFilename, OGRLayerPool *poPoolIn,
       nNextFID{0}, nCurrentDepth{0}, nGeometryElementDepth{0},
       nFeatureCollectionDepth{0}, nFeatureElementDepth{0},
       nAttributeElementDepth{0},
-      eAddressRefState{AddressRefState::ADDRESS_PRIMARY}, bCollectData{false}
+      eAddressRefState{AddressRefState::ADDRESS_PRIMARY}, osElementString{},
+      osAttributeString{}, bCollectData{false}
 {
     SetDescription(CPLGetBasenameSafe(pszFilename).c_str());
 
@@ -62,7 +63,9 @@ OGRLVBAGLayer::~OGRLVBAGLayer()
 void OGRLVBAGLayer::ResetReading()
 {
     if (!TouchLayer())
+    {
         return;
+    }
 
     VSIRewindL(fp);
 
@@ -83,7 +86,9 @@ void OGRLVBAGLayer::ResetReading()
 OGRFeatureDefn *OGRLVBAGLayer::GetLayerDefn()
 {
     if (!TouchLayer())
+    {
         return nullptr;
+    }
 
     if (!bHasReadSchema)
     {
@@ -105,7 +110,9 @@ static inline const char *XMLTagSplit(const char *pszName)
     const char *pszTag = pszName;
     const char *pszSep = strchr(pszTag, ':');
     if (pszSep)
+    {
         pszTag = pszSep + 1;
+    }
 
     return pszTag;
 }
@@ -320,8 +327,10 @@ void OGRLVBAGLayer::CreateFeatureDefn(const char *pszDataset)
         AddSpatialRef(wkbMultiPolygon);
     }
     else
+    {
         CPLError(CE_Failure, CPLE_AppDefined,
                  "Parsing LV BAG extract failed : invalid layer definition");
+    }
 }
 
 /************************************************************************/
@@ -353,7 +362,9 @@ void OGRLVBAGLayer::StopDataCollect()
 void OGRLVBAGLayer::DataHandlerCbk(const char *data, int nLen)
 {
     if (nLen && bCollectData)
+    {
         osElementString.append(data, nLen);
+    }
 }
 
 /************************************************************************/
@@ -395,7 +406,9 @@ bool OGRLVBAGLayer::TouchLayer()
 void OGRLVBAGLayer::CloseUnderlyingLayer()
 {
     if (fp)
+    {
         VSIFCloseL(fp);
+    }
     fp = nullptr;
 
     eFileDescriptorsState = FD_CLOSED;
@@ -755,13 +768,17 @@ void OGRLVBAGLayer::EndElementCbk(const char *pszName)
         }
 
         if (!bHasReadSchema)
+        {
             CreateFeatureDefn(osElementString.c_str());
+        }
         bHasReadSchema = true;
 
         // The parser is suspended but never resumed. Stop
         // without resume indicated an error.
         if (bSchemaOnly)
+        {
             XML_StopParser(oParser.get(), XML_TRUE);
+        }
     }
 }
 
@@ -872,7 +889,9 @@ void OGRLVBAGLayer::ParseDocument()
 OGRFeature *OGRLVBAGLayer::GetNextFeature()
 {
     if (!TouchLayer())
+    {
         return nullptr;
+    }
 
     if (!bHasReadSchema)
     {
@@ -898,7 +917,9 @@ OGRFeature *OGRLVBAGLayer::GetNextRawFeature()
     bSchemaOnly = false;
 
     if (nNextFID == 0)
+    {
         ConfigureParser();
+    }
 
     if (m_poFeature)
     {
@@ -919,10 +940,14 @@ OGRFeature *OGRLVBAGLayer::GetNextRawFeature()
 int OGRLVBAGLayer::TestCapability(const char *pszCap)
 {
     if (!TouchLayer())
+    {
         return FALSE;
+    }
 
     if (EQUAL(pszCap, OLCStringsAsUTF8))
+    {
         return TRUE;
+    }
 
     return FALSE;
 }
