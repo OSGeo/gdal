da97e76b4b77d1298705bf11cf5b11aed9e3afc4
Even Rouault <even.rouault@spatialys.com>
2025-06-30 13:10:24 +0200

    Add GDALGetGDALPath()

---
+++ apps/gdalgetgdalpath.h
@@ -0,0 +1,20 @@
+/******************************************************************************
+ *
+ * Project:  GDAL
+ * Purpose:  Return the path of the "gdal" binary
+ * Author:   Even Rouault <even dot rouault at spatialys.com>
+ *
+ ******************************************************************************
+ * Copyright (c) 2025, Even Rouault <even dot rouault at spatialys.com>
+ *
+ * SPDX-License-Identifier: MIT
+ ****************************************************************************/
+
+#ifndef GDALGETGDALPATH_INCLUDED_H
+#define GDALGETGDALPATH_INCLUDED_H
+
+#include <string>
+
+std::string GDALGetGDALPath();
+
+#endif  // GDALGETGDALPATH_INCLUDED_H
