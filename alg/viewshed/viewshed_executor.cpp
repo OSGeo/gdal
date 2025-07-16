7a7afb328304012706ffcd2d1ddc9a9a534bba93
Andrew Bell <andrew.bell.ia@gmail.com>
2025-07-16 13:56:54 -0400

    Don't allow DEM heights to be below ground level.

--- alg/viewshed/viewshed_executor.cpp
+++ alg/viewshed/viewshed_executor.cpp
@@ -17,6 +17,8 @@
 #include <cmath>
 #include <limits>
 
+#include <iostream>
+
 #include "viewshed_executor.h"
 #include "progress.h"
 #include "util.h"
@@ -181,8 +183,9 @@ void ViewshedExecutor::setOutput(double &dfResult, double &dfCellVal,
 {
     if (oOpts.outputMode != OutputMode::Normal)
     {
-        dfResult += (dfZ - dfCellVal);
-        dfResult = std::max(0.0, dfResult);
+        double adjustment = dfZ - dfCellVal;
+        if (adjustment > 0)
+            dfResult += adjustment;
     }
     else
         dfResult = (dfCellVal + oOpts.targetHeight < dfZ) ? oOpts.invisibleVal
