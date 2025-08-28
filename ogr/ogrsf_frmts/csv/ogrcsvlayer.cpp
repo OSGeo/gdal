5e04940dfaa508b4c24a9b1d7cb06bb13f4885b8
Even Rouault <even.rouault@spatialys.com>
2025-08-29 01:55:58 +0200

    CSV: fix file descriptor leak in one case

--- ogr/ogrsf_frmts/csv/ogrcsvlayer.cpp
+++ ogr/ogrsf_frmts/csv/ogrcsvlayer.cpp
@@ -2081,7 +2081,11 @@ OGRErr OGRCSVLayer::WriteHeader()
                     ? CPLES_CSV_FORCE_QUOTING
                     : CPLES_CSV);
             if (pszEscaped == nullptr)
+            {
+                if (fpCSVT)
+                    VSIFCloseL(fpCSVT);
                 return OGRERR_FAILURE;
+            }
 
             if (fpCSV)
             {
