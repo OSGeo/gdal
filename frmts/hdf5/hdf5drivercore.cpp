afff4b903c2dbe9a45a8660271b103d5126c8a69
Even Rouault <even.rouault@spatialys.com>
2025-11-01 20:42:39 +0100

    S104 and S111: declare GDAL_DMD_SUBDATASETS

--- frmts/hdf5/hdf5drivercore.cpp
+++ frmts/hdf5/hdf5drivercore.cpp
@@ -570,6 +570,7 @@ void S104DriverSetCommonMetadata(GDALDriver *poDriver)
     poDriver->SetMetadataItem(GDAL_DMD_HELPTOPIC, "drivers/raster/s104.html");
     poDriver->SetMetadataItem(GDAL_DCAP_VIRTUALIO, "YES");
     poDriver->SetMetadataItem(GDAL_DMD_EXTENSION, "h5");
+    poDriver->SetMetadataItem(GDAL_DMD_SUBDATASETS, "YES");
 
     poDriver->SetMetadataItem(
         GDAL_DMD_OPENOPTIONLIST,
@@ -595,6 +596,7 @@ void S111DriverSetCommonMetadata(GDALDriver *poDriver)
     poDriver->SetMetadataItem(GDAL_DMD_HELPTOPIC, "drivers/raster/s111.html");
     poDriver->SetMetadataItem(GDAL_DCAP_VIRTUALIO, "YES");
     poDriver->SetMetadataItem(GDAL_DMD_EXTENSION, "h5");
+    poDriver->SetMetadataItem(GDAL_DMD_SUBDATASETS, "YES");
 
     poDriver->SetMetadataItem(
         GDAL_DMD_OPENOPTIONLIST,
