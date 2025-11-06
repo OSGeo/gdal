OSGeo/gdal

commit a31c4d09dbeece58fcb0117f06b42f5d375ac544

Shape: workaround bug in PROJ BoundCRS::identify for CRS based on 'NTF (Paris)'
which is fixe in https://github.com/OSGeo/PROJ/pull/4599

Fixes qgis/qgis#63787

ogr/ogrsf_frmts/shape/ogrshapelayer.cpp
@@ -2501,7 +2501,13 @@ const OGRSpatialReference *OGRShapeGeomFieldDefn::GetSpatialRef() const
 
         if (poSRSNonConst)
         {
-            if (CPLTestBool(CPLGetConfigOption("USE_OSR_FIND_MATCHES", "YES")))
+            double adfTOWGS84[7];
+            if (CPLTestBool(
+                    CPLGetConfigOption("USE_OSR_FIND_MATCHES", "YES")) &&
+                // Below works around bug fixed in PROJ per
+                // https://github.com/OSGeo/PROJ/pull/4599
+                !(strstr(poSRSNonConst->GetName(), "NTF (Paris)") != nullptr &&
+                  poSRSNonConst->GetTOWGS84(adfTOWGS84) == OGRERR_NONE))
             {
                 auto poSRSMatch = poSRSNonConst->FindBestMatch();
                 if (poSRSMatch)

commit ea75dfb98cb6a6fe27d826f840178e8e6b6fe626

Shape: fix potential nullptr deref on corrupted CRS in GetSpatialRef() (fix regression introduced by a31c4d09dbe, https://issues.oss-fuzz.com/issues/458229990)

ogr/ogrsf_frmts/shape/ogrshapelayer.cpp
@@ -2502,11 +2502,12 @@ const OGRSpatialReference *OGRShapeGeomFieldDefn::GetSpatialRef() const
         if (poSRSNonConst)
         {
             double adfTOWGS84[7];
+            const char *pszSRSName = poSRSNonConst->GetName();
             if (CPLTestBool(
                     CPLGetConfigOption("USE_OSR_FIND_MATCHES", "YES")) &&
                 // Below works around bug fixed in PROJ per
                 // https://github.com/OSGeo/PROJ/pull/4599
-                !(strstr(poSRSNonConst->GetName(), "NTF (Paris)") != nullptr &&
+                !(pszSRSName && strstr(pszSRSName, "NTF (Paris)") != nullptr &&
                   poSRSNonConst->GetTOWGS84(adfTOWGS84) == OGRERR_NONE))
             {
                 auto poSRSMatch = poSRSNonConst->FindBestMatch();