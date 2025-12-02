8c935b88c4dc75123fdefdadb0dc44a87acacf1e
Even Rouault <even.rouault@spatialys.com>
2025-12-02 20:14:47 +0100

    gdal dataset identify: add --detailed switch, and ability to output to any GDAL vector format with write capabilities

--- apps/gdalalg_dataset_identify.h
+++ apps/gdalalg_dataset_identify.h
@@ -16,8 +16,11 @@
 #include "gdalalgorithm.h"
 
 #include "cpl_json_streaming_writer.h"
+#include "cpl_vsi_virtual.h"
 
 //! @cond Doxygen_Suppress
+
+class OGRLayer;
 
 /************************************************************************/
 /*                   GDALDatasetIdentifyAlgorithm                       */
@@ -33,22 +36,37 @@ class GDALDatasetIdentifyAlgorithm final : public GDALAlgorithm
         "/programs/gdal_dataset_identify.html";
 
     GDALDatasetIdentifyAlgorithm();
+    ~GDALDatasetIdentifyAlgorithm() override;
 
   private:
     std::vector<std::string> m_filename{};
     std::string m_format{};
-    CPLJSonStreamingWriter m_oWriter;
+    std::vector<std::string> m_creationOptions{};
+    std::vector<std::string> m_layerCreationOptions{};
+    GDALArgDatasetValue m_outputDataset{};
+    std::string m_outputLayerName{};
+    bool m_overwrite = false;
     std::string m_output{};
     bool m_recursive = false;
     bool m_forceRecursive = false;
     bool m_reportFailures = false;
+    bool m_detailed = false;
     bool m_stdout = false;
+
+    CPLJSonStreamingWriter m_oWriter;
+    VSIVirtualHandleUniquePtr m_fpOut{};
+    std::unique_ptr<GDALDataset> m_poOutDS{};
+    OGRLayer *m_poLayer = nullptr;
 
     bool RunImpl(GDALProgressFunc, void *) override;
     void Print(const char *str);
     bool Process(const char *pszTarget, CSLConstList papszSiblingList,
                  GDALProgressFunc pfnProgress, void *pProgressData);
     static void JSONPrint(const char *pszTxt, void *pUserData);
+
+    GDALDatasetIdentifyAlgorithm(const GDALDatasetIdentifyAlgorithm &) = delete;
+    GDALDatasetIdentifyAlgorithm &
+    operator=(const GDALDatasetIdentifyAlgorithm &) = delete;
 };
 
 //! @endcond
