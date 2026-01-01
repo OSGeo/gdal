97da8d4be507130f4b0bfd150f0b67804136e88e
sidhansu10 <sidhanshusingh445@gmail.com>
2026-01-01 18:39:22 +0530

    Python: expose named arguments for GDAL.OpenEx

---
+++ autotest/pyscripts/test_named_args.py
@@ -0,0 +1,14 @@
+import inspect
+from osgeo import gdal
+
+def test_OpenEx_named_arguments():
+    sig = inspect.signature(gdal.OpenEx)
+    params = list(sig.parameters.keys())
+
+    assert params == [
+        "utf8_path",
+        "nOpenFlags",
+        "allowed_drivers",
+        "open_options",
+        "sibling_files",
+    ]
