OSGeo/gdal

commit f60076f73355e892f0434da3e238e3e0d2dddacd

Various drivers: avoid very unlikely/maybe impossible int overflows

frmts/pdf/pdfdataset.cpp
@@ -666,14 +666,12 @@ CPLErr PDFRasterBand::IReadBlockFromTile(int nBlockXOff, int nBlockYOff,
 {
     PDFDataset *poGDS = cpl::down_cast<PDFDataset *>(poDS);
 
-    int nReqXSize = nBlockXSize;
-    int nReqYSize = nBlockYSize;
-    if ((nBlockXOff + 1) * nBlockXSize > nRasterXSize)
-        nReqXSize = nRasterXSize - nBlockXOff * nBlockXSize;
-    if ((nBlockYOff + 1) * nBlockYSize > nRasterYSize)
-        nReqYSize = nRasterYSize - nBlockYOff * nBlockYSize;
+    const int nXOff = nBlockXOff * nBlockXSize;
+    const int nReqXSize = std::min(nBlockXSize, nRasterXSize - nXOff);
+    const int nYOff = nBlockYOff * nBlockYSize;
+    const int nReqYSize = std::min(nBlockYSize, nRasterYSize - nYOff);
 
-    int nXBlocks = DIV_ROUND_UP(nRasterXSize, nBlockXSize);
+    const int nXBlocks = DIV_ROUND_UP(nRasterXSize, nBlockXSize);
     int iTile = poGDS->m_aiTiles[nBlockYOff * nXBlocks + nBlockXOff];
     if (iTile < 0)
     {
@@ -886,14 +884,10 @@ CPLErr PDFRasterBand::IReadBlock(int nBlockXOff, int nBlockYOff, void *pImage)
         }
     }
 
-    int nReqXSize = nBlockXSize;
-    int nReqYSize = nBlockYSize;
-    if ((nBlockXOff + 1) * nBlockXSize > nRasterXSize)
-        nReqXSize = nRasterXSize - nBlockXOff * nBlockXSize;
-    if (nBlockYSize == 1)
-        nReqYSize = nRasterYSize;
-    else if ((nBlockYOff + 1) * nBlockYSize > nRasterYSize)
-        nReqYSize = nRasterYSize - nBlockYOff * nBlockYSize;
+    const int nXOff = nBlockXOff * nBlockXSize;
+    const int nReqXSize = std::min(nBlockXSize, nRasterXSize - nXOff);
+    const int nYOff = nBlockYOff * nBlockYSize;
+    const int nReqYSize = std::min(nBlockYSize, nRasterYSize - nYOff);
 
     if (!poGDS->m_bTried)
     {

frmts/ecw/ecwdataset.cpp
@@ -1057,13 +1057,10 @@ CPLErr ECWRasterBand::IRasterIO(GDALRWFlag eRWFlag, int nXOff, int nYOff,
 CPLErr ECWRasterBand::IReadBlock(int nBlockXOff, int nBlockYOff, void *pImage)
 
 {
-    int nXOff = nBlockXOff * nBlockXSize, nYOff = nBlockYOff * nBlockYSize,
-        nXSize = nBlockXSize, nYSize = nBlockYSize;
-
-    if (nXOff + nXSize > nRasterXSize)
-        nXSize = nRasterXSize - nXOff;
-    if (nYOff + nYSize > nRasterYSize)
-        nYSize = nRasterYSize - nYOff;
+    const int nXOff = nBlockXOff * nBlockXSize;
+    const int nXSize = std::min(nBlockXSize, nRasterXSize - nXOff);
+    const int nYOff = nBlockYOff * nBlockYSize;
+    const int nYSize = std::min(nBlockYSize, nRasterYSize - nYOff);
 
     const GSpacing nPixelSpace = GDALGetDataTypeSizeBytes(eDataType);
     const GSpacing nLineSpace = nPixelSpace * nBlockXSize;

frmts/eeda/eedaidataset.cpp
@@ -309,25 +309,20 @@ bool GDALEEDAIRasterBand::DecodeNPYArray(const GByte *pabyData, int nDataLen,
 
     for (int iYBlock = 0; iYBlock < nYBlocks; iYBlock++)
     {
-        int nBlockActualYSize = nBlockYSize;
-        if ((iYBlock + nBlockYOff + 1) * nBlockYSize > nRasterYSize)
-        {
-            nBlockActualYSize =
-                nRasterYSize - (iYBlock + nBlockYOff) * nBlockYSize;
-        }
+        const int nYOff = (nBlockYOff + iYBlock) * nBlockYSize;
+        const int nBlockActualYSize =
+            std::min(nBlockYSize, nRasterYSize - nYOff);
 
         for (int iXBlock = 0; iXBlock < nXBlocks; iXBlock++)
         {
-            int nBlockActualXSize = nBlockXSize;
-            if ((iXBlock + nBlockXOff + 1) * nBlockXSize > nRasterXSize)
-            {
-                nBlockActualXSize =
-                    nRasterXSize - (iXBlock + nBlockXOff) * nBlockXSize;
-            }
+            const int nXOff = (nBlockXOff + iXBlock) * nBlockXSize;
+            const int nBlockActualXSize =
+                std::min(nBlockXSize, nRasterXSize - nXOff);
 
-            int nOffsetBand =
+            size_t nOffsetBand =
                 10 + nHeaderLen +
-                (iYBlock * nBlockYSize * nReqXSize + iXBlock * nBlockXSize) *
+                (static_cast<size_t>(iYBlock) * nBlockYSize * nReqXSize +
+                 iXBlock * nBlockXSize) *
                     nTotalDataTypeSize;
 
             for (int i = 1; i <= poGDS->GetRasterCount(); i++)
@@ -439,21 +434,15 @@ bool GDALEEDAIRasterBand::DecodeGDALDataset(const GByte *pabyData, int nDataLen,
 
     for (int iYBlock = 0; iYBlock < nYBlocks; iYBlock++)
     {
-        int nBlockActualYSize = nBlockYSize;
-        if ((iYBlock + nBlockYOff + 1) * nBlockYSize > nRasterYSize)
-        {
-            nBlockActualYSize =
-                nRasterYSize - (iYBlock + nBlockYOff) * nBlockYSize;
-        }
+        const int nYOff = (nBlockYOff + iYBlock) * nBlockYSize;
+        const int nBlockActualYSize =
+            std::min(nBlockYSize, nRasterYSize - nYOff);
 
         for (int iXBlock = 0; iXBlock < nXBlocks; iXBlock++)
         {
-            int nBlockActualXSize = nBlockXSize;
-            if ((iXBlock + nBlockXOff + 1) * nBlockXSize > nRasterXSize)
-            {
-                nBlockActualXSize =
-                    nRasterXSize - (iXBlock + nBlockXOff) * nBlockXSize;
-            }
+            const int nXOff = (nBlockXOff + iXBlock) * nBlockXSize;
+            const int nBlockActualXSize =
+                std::min(nBlockXSize, nRasterXSize - nXOff);
 
             for (int i = 1; i <= poGDS->GetRasterCount(); i++)
             {
@@ -536,16 +525,13 @@ CPLErr GDALEEDAIRasterBand::GetBlocks(int nBlockXOff, int nBlockYOff,
     }
     json_object_object_add(poReq, "bandIds", poBands);
 
-    int nReqXSize = nBlockXSize * nXBlocks;
-    if ((nBlockXOff + nXBlocks) * nBlockXSize > nRasterXSize)
-        nReqXSize = nRasterXSize - nBlockXOff * nBlockXSize;
-    int nReqYSize = nBlockYSize * nYBlocks;
-    if ((nBlockYOff + nYBlocks) * nBlockYSize > nRasterYSize)
-        nReqYSize = nRasterYSize - nBlockYOff * nBlockYSize;
-    const double dfX0 =
-        poGDS->m_gt[0] + nBlockXOff * nBlockXSize * poGDS->m_gt[1];
-    const double dfY0 =
-        poGDS->m_gt[3] + nBlockYOff * nBlockYSize * poGDS->m_gt[5];
+    const int nXOff = nBlockXOff * nBlockXSize;
+    const int nReqXSize = std::min(nBlockXSize, nRasterXSize - nXOff);
+    const int nYOff = nBlockYOff * nBlockYSize;
+    const int nReqYSize = std::min(nBlockYSize, nRasterYSize - nYOff);
+
+    const double dfX0 = poGDS->m_gt[0] + nXOff * poGDS->m_gt[1];
+    const double dfY0 = poGDS->m_gt[3] + nYOff * poGDS->m_gt[5];
 #ifdef DEBUG_VERBOSE
     CPLDebug("EEDAI",
              "nBlockYOff=%d nBlockYOff=%d "

frmts/kmlsuperoverlay/kmlsuperoverlaydataset.cpp
@@ -14,10 +14,10 @@
 #include "cpl_port.h"
 #include "kmlsuperoverlaydataset.h"
 
-#include <array>
 #include <cmath>
 #include <cstring>
 #include <algorithm>
+#include <array>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -1103,14 +1103,10 @@ KmlSuperOverlayRasterBand::KmlSuperOverlayRasterBand(
 CPLErr KmlSuperOverlayRasterBand::IReadBlock(int nBlockXOff, int nBlockYOff,
                                              void *pData)
 {
-    int nXOff = nBlockXOff * nBlockXSize;
-    int nYOff = nBlockYOff * nBlockYSize;
-    int nXSize = nBlockXSize;
-    int nYSize = nBlockYSize;
-    if (nXOff + nXSize > nRasterXSize)
-        nXSize = nRasterXSize - nXOff;
-    if (nYOff + nYSize > nRasterYSize)
-        nYSize = nRasterYSize - nYOff;
+    const int nXOff = nBlockXOff * nBlockXSize;
+    const int nXSize = std::min(nBlockXSize, nRasterXSize - nXOff);
+    const int nYOff = nBlockYOff * nBlockYSize;
+    const int nYSize = std::min(nBlockYSize, nRasterYSize - nYOff);
 
     GDALRasterIOExtraArg sExtraArg;
     INIT_RASTERIO_EXTRA_ARG(sExtraArg);
@@ -2163,15 +2159,13 @@ CPLErr KmlSingleDocRasterRasterBand::IReadBlock(int nBlockXOff, int nBlockYOff,
         memset(pImage, 0, static_cast<size_t>(nBlockXSize) * nBlockYSize);
         return CE_None;
     }
-    int nXSize = poImageDS->GetRasterXSize();
-    int nYSize = poImageDS->GetRasterYSize();
+    const int nXSize = poImageDS->GetRasterXSize();
+    const int nYSize = poImageDS->GetRasterYSize();
 
-    int nReqXSize = nBlockXSize;
-    if (nBlockXOff * nBlockXSize + nReqXSize > nRasterXSize)
-        nReqXSize = nRasterXSize - nBlockXOff * nBlockXSize;
-    int nReqYSize = nBlockYSize;
-    if (nBlockYOff * nBlockYSize + nReqYSize > nRasterYSize)
-        nReqYSize = nRasterYSize - nBlockYOff * nBlockYSize;
+    const int nXOff = nBlockXOff * nBlockXSize;
+    const int nReqXSize = std::min(nBlockXSize, nRasterXSize - nXOff);
+    const int nYOff = nBlockYOff * nBlockYSize;
+    const int nReqYSize = std::min(nBlockYSize, nRasterYSize - nYOff);
 
     if (nXSize != nReqXSize || nYSize != nReqYSize)
     {

frmts/nitf/nitfrasterband.cpp
@@ -1166,42 +1166,23 @@ CPLErr NITFComplexRasterBand::IBlockIO(int nBlockXOff, int nBlockYOff,
                                        void *pImage, GDALRWFlag rwFlag)
 
 {
-    int nRequestYSize;
-    int nRequestXSize;
-    bool bMemset = false;
-
     /* -------------------------------------------------------------------- */
     /*      If the last strip is partial, we need to avoid                  */
     /*      over-requesting.  We also need to initialize the extra part     */
     /*      of the block to zero.                                           */
     /* -------------------------------------------------------------------- */
-    if ((nBlockYOff + 1) * nBlockYSize > nRasterYSize)
-    {
-        nRequestYSize = nRasterYSize - nBlockYOff * nBlockYSize;
-        if (rwFlag == GF_Read)
-            bMemset = true;
-    }
-    else
-    {
-        nRequestYSize = nBlockYSize;
-    }
+    const int nYOff = nBlockYOff * nBlockYSize;
+    const int nRequestYSize = std::min(nBlockYSize, nRasterYSize - nYOff);
 
     /*-------------------------------------------------------------------- */
     /*      If the input imagery is tiled, also need to avoid over-        */
     /*      requesting in the X-direction.                                 */
     /* ------------------------------------------------------------------- */
-    if ((nBlockXOff + 1) * nBlockXSize > nRasterXSize)
-    {
-        nRequestXSize = nRasterXSize - nBlockXOff * nBlockXSize;
-        if (rwFlag == GF_Read)
-            bMemset = true;
-    }
-    else
-    {
-        nRequestXSize = nBlockXSize;
-    }
+    const int nXOff = nBlockXOff * nBlockXSize;
+    const int nRequestXSize = std::min(nBlockXSize, nRasterXSize - nXOff);
 
-    if (bMemset)
+    if (rwFlag == GF_Read &&
+        (nRequestXSize < nBlockXSize || nRequestYSize < nBlockYSize))
     {
         memset(pImage, 0,
                static_cast<size_t>(GDALGetDataTypeSizeBytes(eDataType)) *

frmts/pds/isis3dataset.cpp
@@ -1191,7 +1191,7 @@ static void FillMask(void *pvBuffer, GByte *pabyDst, int nReqXSize,
 /*                           IReadBlock()                               */
 /************************************************************************/
 
-CPLErr ISISMaskBand::IReadBlock(int nXBlock, int nYBlock, void *pImage)
+CPLErr ISISMaskBand::IReadBlock(int nBlockXOff, int nBlockYOff, void *pImage)
 
 {
     const GDALDataType eSrcDT = m_poBaseBand->GetRasterDataType();
@@ -1203,14 +1203,10 @@ CPLErr ISISMaskBand::IReadBlock(int nXBlock, int nYBlock, void *pImage)
             return CE_Failure;
     }
 
-    int nXOff = nXBlock * nBlockXSize;
-    int nReqXSize = nBlockXSize;
-    if (nXOff + nReqXSize > nRasterXSize)
-        nReqXSize = nRasterXSize - nXOff;
-    int nYOff = nYBlock * nBlockYSize;
-    int nReqYSize = nBlockYSize;
-    if (nYOff + nReqYSize > nRasterYSize)
-        nReqYSize = nRasterYSize - nYOff;
+    const int nXOff = nBlockXOff * nBlockXSize;
+    const int nReqXSize = std::min(nBlockXSize, nRasterXSize - nXOff);
+    const int nYOff = nBlockYOff * nBlockYSize;
+    const int nReqYSize = std::min(nBlockYSize, nRasterYSize - nYOff);
 
     if (m_poBaseBand->RasterIO(GF_Read, nXOff, nYOff, nReqXSize, nReqYSize,
                                m_pBuffer, nReqXSize, nReqYSize, eSrcDT,

frmts/pds/pds4dataset.cpp
@@ -533,7 +533,7 @@ static void FillMask(void *pvBuffer, GByte *pabyDst, int nReqXSize,
 /*                           IReadBlock()                               */
 /************************************************************************/
 
-CPLErr PDS4MaskBand::IReadBlock(int nXBlock, int nYBlock, void *pImage)
+CPLErr PDS4MaskBand::IReadBlock(int nBlockXOff, int nBlockYOff, void *pImage)
 
 {
     const GDALDataType eSrcDT = m_poBaseBand->GetRasterDataType();
@@ -545,14 +545,10 @@ CPLErr PDS4MaskBand::IReadBlock(int nXBlock, int nYBlock, void *pImage)
             return CE_Failure;
     }
 
-    int nXOff = nXBlock * nBlockXSize;
-    int nReqXSize = nBlockXSize;
-    if (nXOff + nReqXSize > nRasterXSize)
-        nReqXSize = nRasterXSize - nXOff;
-    int nYOff = nYBlock * nBlockYSize;
-    int nReqYSize = nBlockYSize;
-    if (nYOff + nReqYSize > nRasterYSize)
-        nReqYSize = nRasterYSize - nYOff;
+    const int nXOff = nBlockXOff * nBlockXSize;
+    const int nReqXSize = std::min(nBlockXSize, nRasterXSize - nXOff);
+    const int nYOff = nBlockYOff * nBlockYSize;
+    const int nReqYSize = std::min(nBlockYSize, nRasterYSize - nYOff);
 
     if (m_poBaseBand->RasterIO(GF_Read, nXOff, nYOff, nReqXSize, nReqYSize,
                                m_pBuffer, nReqXSize, nReqYSize, eSrcDT,

frmts/rs2/rs2dataset.cpp
@@ -215,40 +215,28 @@ CPLErr RS2RasterBand::IReadBlock(int nBlockXOff, int nBlockYOff, void *pImage)
     /*      over-requesting.  We also need to initialize the extra part     */
     /*      of the block to zero.                                           */
     /* -------------------------------------------------------------------- */
-    int nRequestYSize;
-    if ((nBlockYOff + 1) * nBlockYSize > nRasterYSize)
-    {
-        nRequestYSize = nRasterYSize - nBlockYOff * nBlockYSize;
-        memset(pImage, 0,
-               static_cast<size_t>(GDALGetDataTypeSizeBytes(eDataType)) *
-                   nBlockXSize * nBlockYSize);
-    }
-    else
-    {
-        nRequestYSize = nBlockYSize;
-    }
+    const int nYOff = nBlockYOff * nBlockYSize;
+    const int nRequestYSize = std::min(nBlockYSize, nRasterYSize - nYOff);
 
     /*-------------------------------------------------------------------- */
     /*      If the input imagery is tiled, also need to avoid over-        */
     /*      requesting in the X-direction.                                 */
     /* ------------------------------------------------------------------- */
-    int nRequestXSize;
-    if ((nBlockXOff + 1) * nBlockXSize > nRasterXSize)
+    const int nXOff = nBlockXOff * nBlockXSize;
+    const int nRequestXSize = std::min(nBlockXSize, nRasterXSize - nXOff);
+
+    if (nRequestXSize < nBlockXSize || nRequestYSize < nBlockYSize)
     {
-        nRequestXSize = nRasterXSize - nBlockXOff * nBlockXSize;
         memset(pImage, 0,
                static_cast<size_t>(GDALGetDataTypeSizeBytes(eDataType)) *
                    nBlockXSize * nBlockYSize);
     }
-    else
-    {
-        nRequestXSize = nBlockXSize;
-    }
+
     if (eDataType == GDT_CInt16 && poBandFile->GetRasterCount() == 2)
-        return poBandFile->RasterIO(
-            GF_Read, nBlockXOff * nBlockXSize, nBlockYOff * nBlockYSize,
-            nRequestXSize, nRequestYSize, pImage, nRequestXSize, nRequestYSize,
-            GDT_Int16, 2, nullptr, 4, nBlockXSize * 4, 2, nullptr);
+        return poBandFile->RasterIO(GF_Read, nXOff, nYOff, nRequestXSize,
+                                    nRequestYSize, pImage, nRequestXSize,
+                                    nRequestYSize, GDT_Int16, 2, nullptr, 4,
+                                    nBlockXSize * 4, 2, nullptr);
 
     /* -------------------------------------------------------------------- */
     /*      File has one sample marked as sample format void, a 32bits.     */
@@ -256,9 +244,9 @@ CPLErr RS2RasterBand::IReadBlock(int nBlockXOff, int nBlockYOff, void *pImage)
     else if (eDataType == GDT_CInt16 && poBandFile->GetRasterCount() == 1)
     {
         CPLErr eErr = poBandFile->RasterIO(
-            GF_Read, nBlockXOff * nBlockXSize, nBlockYOff * nBlockYSize,
-            nRequestXSize, nRequestYSize, pImage, nRequestXSize, nRequestYSize,
-            GDT_UInt32, 1, nullptr, 4, nBlockXSize * 4, 0, nullptr);
+            GF_Read, nXOff, nYOff, nRequestXSize, nRequestYSize, pImage,
+            nRequestXSize, nRequestYSize, GDT_UInt32, 1, nullptr, 4,
+            nBlockXSize * 4, 0, nullptr);
 
 #ifdef CPL_LSB
         /* First, undo the 32bit swap. */
@@ -276,16 +264,16 @@ CPLErr RS2RasterBand::IReadBlock(int nBlockXOff, int nBlockYOff, void *pImage)
     /*      looks like a 16bit unsigned data too.                           */
     /* -------------------------------------------------------------------- */
     else if (eDataType == GDT_UInt16)
-        return poBandFile->RasterIO(
-            GF_Read, nBlockXOff * nBlockXSize, nBlockYOff * nBlockYSize,
-            nRequestXSize, nRequestYSize, pImage, nRequestXSize, nRequestYSize,
-            GDT_UInt16, 1, nullptr, 2, nBlockXSize * 2, 0, nullptr);
+        return poBandFile->RasterIO(GF_Read, nXOff, nYOff, nRequestXSize,
+                                    nRequestYSize, pImage, nRequestXSize,
+                                    nRequestYSize, GDT_UInt16, 1, nullptr, 2,
+                                    nBlockXSize * 2, 0, nullptr);
     else if (eDataType == GDT_UInt8)
         /* Ticket #2104: Support for ScanSAR products */
-        return poBandFile->RasterIO(
-            GF_Read, nBlockXOff * nBlockXSize, nBlockYOff * nBlockYSize,
-            nRequestXSize, nRequestYSize, pImage, nRequestXSize, nRequestYSize,
-            GDT_UInt8, 1, nullptr, 1, nBlockXSize, 0, nullptr);
+        return poBandFile->RasterIO(GF_Read, nXOff, nYOff, nRequestXSize,
+                                    nRequestYSize, pImage, nRequestXSize,
+                                    nRequestYSize, GDT_UInt8, 1, nullptr, 1,
+                                    nBlockXSize, 0, nullptr);
 
     CPLAssert(false);
     return CE_Failure;

commit 4026711a1f44660a8fc96b3b043470b5c198f756

PDF: fix error introduced in recent commit f60076f73355e892f0434da3e238e3e0d2dddacd (master only)

frmts/pdf/pdfdataset.cpp
@@ -886,8 +886,10 @@ CPLErr PDFRasterBand::IReadBlock(int nBlockXOff, int nBlockYOff, void *pImage)
 
     const int nXOff = nBlockXOff * nBlockXSize;
     const int nReqXSize = std::min(nBlockXSize, nRasterXSize - nXOff);
-    const int nYOff = nBlockYOff * nBlockYSize;
-    const int nReqYSize = std::min(nBlockYSize, nRasterYSize - nYOff);
+    const int nReqYSize =
+        nBlockYSize == 1
+            ? nRasterYSize
+            : std::min(nBlockYSize, nRasterYSize - nBlockYOff * nBlockYSize);
 
     if (!poGDS->m_bTried)
     {