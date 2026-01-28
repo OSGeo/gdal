7c3edd4875673630fd96a9caf48b601cb33805a9
Dan Baston <dbaston@gmail.com>
2026-01-28 11:37:20 -0500

    VRTDerivedBand: Add 'area' pixel function

--- doc/source/drivers/raster/vrt.rst
+++ doc/source/drivers/raster/vrt.rst
@@ -1213,6 +1213,10 @@ GDAL provides a set of default pixel functions that can be used without writing 
      - Number of input sources
      - PixelFunctionArguments
      - Description
+   * - **area**
+     - 0
+     - 
+     - (GDAL >= 3.13) Returns the area of each pixel in square meters.
    * - **argmax**
      - >= 1
      - ``propagateNoData`` (optional, default=false)
@@ -1583,7 +1587,7 @@ GDAL provides a set of default pixel functions that can be used without writing 
    * - **round**
      - = 1
      - - ``digits`` (optional)
-     - Round the input to the specified number of digits to the right of the decimal point.
+     - (GDAL >= 3.13) Round the input to the specified number of digits to the right of the decimal point.
    * - **scale**
      - = 1
      - -
--- frmts/vrt/pixelfunctions.cpp
+++ frmts/vrt/pixelfunctions.cpp
@@ -19,6 +19,8 @@
 #include "vrtreclassifier.h"
 #include "cpl_float.h"
 
+#include "geodesic.h"  // from PROJ
+
 #if defined(__x86_64) || defined(_M_X64) || defined(USE_NEON_OPTIMIZATIONS)
 #define USE_SSE2
 #include "gdalsse_priv.h"
@@ -3105,6 +3107,174 @@ static CPLErr ExprPixelFunc(void **papoSources, int nSources, void *pData,
     return CE_None;
 }  // ExprPixelFunc
 
+static constexpr char pszAreaPixelFuncMetadata[] =
+    "<PixelFunctionArgumentsList>"
+    "   <Argument type='builtin' value='crs' />"
+    "   <Argument type='builtin' value='xoff' />"
+    "   <Argument type='builtin' value='yoff' />"
+    "   <Argument type='builtin' value='geotransform' />"
+    "</PixelFunctionArgumentsList>";
+
+static CPLErr AreaPixelFunc(void ** /*papoSources*/, int nSources, void *pData,
+                            int nXSize, int nYSize, GDALDataType /* eSrcType */,
+                            GDALDataType eBufType, int nPixelSpace,
+                            int nLineSpace, CSLConstList papszArgs)
+{
+    if (nSources)
+    {
+        CPLError(CE_Failure, CPLE_AppDefined,
+                 "area: unexpected source band(s)");
+        return CE_Failure;
+    }
+
+    const char *pszGT = CSLFetchNameValue(papszArgs, "geotransform");
+    if (pszGT == nullptr)
+    {
+        CPLError(CE_Failure, CPLE_AppDefined,
+                 "area: VRTDataset has no <GeoTransform>");
+        return CE_Failure;
+    }
+
+    GDALGeoTransform gt;
+    {
+        CPLStringList aosGeoTransform(
+            CSLTokenizeString2(pszGT, ",", CSLT_HONOURSTRINGS));
+        if (aosGeoTransform.size() != 6)
+        {
+            CPLError(CE_Failure, CPLE_AppDefined,
+                     "area: invalid GeoTransform argument");
+            return CE_Failure;
+        }
+        for (int i = 0; i < 6; i++)
+        {
+            gt[i] = CPLAtof(aosGeoTransform[i]);
+        }
+    }
+
+    const char *pszXOff = CSLFetchNameValue(papszArgs, "xoff");
+    const int nXOff = std::atoi(pszXOff);
+
+    const char *pszYOff = CSLFetchNameValue(papszArgs, "yoff");
+    const int nYOff = std::atoi(pszYOff);
+
+    const char *pszCrsPtr = CSLFetchNameValue(papszArgs, "crs");
+
+    if (!pszCrsPtr)
+    {
+        CPLError(CE_Failure, CPLE_AppDefined, "area: VRTDataset has no <SRS>");
+        return CE_Failure;
+    }
+
+    size_t nCrsPtr = 0;
+    if (pszCrsPtr)
+    {
+        try
+        {
+            nCrsPtr = std::stoul(pszCrsPtr, nullptr, 10);
+        }
+        catch (std::exception &)
+        {
+            // Since "crs" is populated by GDAL, this should never happen.
+            CPLError(CE_Failure, CPLE_AppDefined, "Failed to read CRS");
+            return CE_Failure;
+        }
+    }
+
+    const OGRSpatialReference *poCRS =
+        reinterpret_cast<const OGRSpatialReference *>(nCrsPtr);
+
+    const OGRSpatialReference *poGeographicCRS = nullptr;
+    std::unique_ptr<OGRSpatialReference> poGeographicCRSHolder;
+    std::unique_ptr<OGRCoordinateTransformation> poTransform;
+
+    if (!poCRS->IsGeographic())
+    {
+        poGeographicCRSHolder = std::make_unique<OGRSpatialReference>();
+        if (poGeographicCRSHolder->CopyGeogCSFrom(poCRS) != OGRERR_NONE)
+        {
+            CPLError(CE_Failure, CPLE_AppDefined,
+                     "Cannot reproject geometry to geographic CRS");
+            return CE_Failure;
+        }
+        poGeographicCRSHolder->SetAxisMappingStrategy(
+            OAMS_TRADITIONAL_GIS_ORDER);
+
+        poTransform.reset(OGRCreateCoordinateTransformation(
+            poCRS, poGeographicCRSHolder.get()));
+
+        if (!poTransform)
+        {
+            CPLError(CE_Failure, CPLE_AppDefined,
+                     "Cannot reproject geometry to geographic CRS");
+            return CE_Failure;
+        }
+
+        poGeographicCRS = poGeographicCRSHolder.get();
+    }
+    else
+    {
+        poGeographicCRS = poCRS;
+    }
+
+    geod_geodesic g{};
+    OGRErr eErr = OGRERR_NONE;
+    double dfSemiMajor = poGeographicCRS->GetSemiMajor(&eErr);
+    if (eErr != OGRERR_NONE)
+        return CE_Failure;
+    const double dfInvFlattening = poGeographicCRS->GetInvFlattening(&eErr);
+    if (eErr != OGRERR_NONE)
+        return CE_Failure;
+    geod_init(&g, dfSemiMajor,
+              dfInvFlattening != 0 ? 1.0 / dfInvFlattening : 0.0);
+
+    std::array<double, 5> adfLon{};
+    std::array<double, 5> adfLat{};
+
+    size_t ii = 0;
+    for (int iLine = 0; iLine < nYSize; ++iLine)
+    {
+        for (int iCol = 0; iCol < nXSize; ++iCol, ++ii)
+        {
+            gt.Apply(static_cast<double>(iCol + nXOff),
+                     static_cast<double>(iLine + nYOff), &adfLon[0],
+                     &adfLat[0]);
+            gt.Apply(static_cast<double>(iCol + nXOff + 1),
+                     static_cast<double>(iLine + nYOff), &adfLon[1],
+                     &adfLat[1]);
+            gt.Apply(static_cast<double>(iCol + nXOff + 1),
+                     static_cast<double>(iLine + nYOff + 1), &adfLon[2],
+                     &adfLat[2]);
+            gt.Apply(static_cast<double>(iCol + nXOff),
+                     static_cast<double>(iLine + nYOff + 1), &adfLon[3],
+                     &adfLat[3]);
+            adfLon[4] = adfLon[0];
+            adfLat[4] = adfLat[0];
+
+            if (poTransform &&
+                !poTransform->Transform(adfLon.size(), adfLon.data(),
+                                        adfLat.data(), nullptr))
+            {
+                CPLError(CE_Failure, CPLE_AppDefined,
+                         "Failed to reproject cell corners to geographic CRS");
+                return CE_Failure;
+            }
+
+            double dfArea = -1.0;
+            geod_polygonarea(&g, adfLat.data(), adfLon.data(),
+                             static_cast<int>(adfLat.size()), &dfArea, nullptr);
+            dfArea = std::fabs(dfArea);
+
+            GDALCopyWords(&dfArea, GDT_Float64, 0,
+                          static_cast<GByte *>(pData) +
+                              static_cast<GSpacing>(nLineSpace) * iLine +
+                              iCol * nPixelSpace,
+                          eBufType, nPixelSpace, 1);
+        }
+    }
+
+    return CE_None;
+}  // AreaPixelFunc
+
 static const char pszReclassifyPixelFuncMetadata[] =
     "<PixelFunctionArgumentsList>"
     "   <Argument name='mapping' "
@@ -4279,5 +4449,7 @@ CPLErr GDALRegisterDefaultPixelFunc()
                                         pszQuantilePixelFuncMetadata);
     GDALAddDerivedBandPixelFuncWithArgs("mode", BasicPixelFunc<ModeKernel>,
                                         pszBasicPixelFuncMetadata);
+    GDALAddDerivedBandPixelFuncWithArgs("area", AreaPixelFunc,
+                                        pszAreaPixelFuncMetadata);
     return CE_None;
 }
