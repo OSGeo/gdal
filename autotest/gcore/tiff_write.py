OSGeo/gdal

commit 7868b1c08f6c500e8b34885cc822452922966df4

GTiff: add support for reading and writing in the GDAL_METADATA TIFF tag

autotest/gcore/tiff_write.py
@@ -12474,3 +12474,63 @@ def test_tiff_write_create_mask_band_and_set_color_interp(tmp_vsimem):
     with gdal.Open(tmp_vsimem / "out.tif") as ds:
         assert ds.GetRasterBand(1).GetMaskBand().GetMetadataItem("foo") == "bar"
         assert ds.GetRasterBand(1).GetMaskBand().GetNoDataValue() == 0
+
+
+###############################################################################
+# Test RAT support
+
+
+@pytest.mark.parametrize("GTIFF_WRITE_RAT_TO_PAM", [None, "YES"])
+def test_tiff_write_rat(tmp_vsimem, GTIFF_WRITE_RAT_TO_PAM):
+
+    filename = tmp_vsimem / "out.tif"
+    filename2 = tmp_vsimem / "out2.tif"
+    with gdal.config_option("GTIFF_WRITE_RAT_TO_PAM", GTIFF_WRITE_RAT_TO_PAM):
+        with gdal.GetDriverByName("GTiff").Create(filename, 1, 1) as ds:
+            rat = gdal.RasterAttributeTable()
+            rat.CreateColumn("VALUE_INT", gdal.GFT_Integer, gdal.GFU_Generic)
+            rat.CreateColumn("VALUE_REAL", gdal.GFT_Real, gdal.GFU_Generic)
+            rat.CreateColumn("VALUE_STRING", gdal.GFT_String, gdal.GFU_Generic)
+            rat.CreateColumn("VALUE_BOOL", gdal.GFT_Boolean, gdal.GFU_Generic)
+            rat.CreateColumn("VALUE_DATETIME", gdal.GFT_DateTime, gdal.GFU_Generic)
+            rat.CreateColumn(
+                "VALUE_WKBGEOMETRY", gdal.GFT_WKBGeometry, gdal.GFU_Generic
+            )
+            rat.SetValueAsInt(0, 0, 123)
+            rat.SetValueAsDouble(0, 1, 45.5)
+            rat.SetValueAsString(0, 2, "str")
+            rat.SetValueAsBoolean(0, 3, True)
+            rat.SetValueAsString(0, 4, "2025-12-31T23:58:59.500+01:15")
+            rat.SetValueAsString(0, 5, "POINT (1.0 2)")
+            ds.GetRasterBand(1).SetDefaultRAT(rat)
+            got_rat = ds.GetRasterBand(1).GetDefaultRAT()
+            assert got_rat.GetColumnCount() == rat.GetColumnCount()
+
+        if GTIFF_WRITE_RAT_TO_PAM:
+            assert gdal.VSIStatL(str(filename) + ".aux.xml") is not None
+        else:
+            assert gdal.VSIStatL(str(filename) + ".aux.xml") is None
+
+        gdal.GetDriverByName("GTiff").CreateCopy(filename2, gdal.Open(filename))
+
+        if GTIFF_WRITE_RAT_TO_PAM:
+            assert gdal.VSIStatL(str(filename2) + ".aux.xml") is not None
+        else:
+            assert gdal.VSIStatL(str(filename2) + ".aux.xml") is None
+
+        with gdal.Open(filename2) as ds:
+            got_rat = ds.GetRasterBand(1).GetDefaultRAT()
+            assert ds.GetRasterBand(1).GetDefaultRAT()  # do it again
+            assert got_rat.GetColumnCount() == rat.GetColumnCount()
+            assert got_rat.GetTypeOfCol(0) == gdal.GFT_Integer
+            assert got_rat.GetTypeOfCol(1) == gdal.GFT_Real
+            assert got_rat.GetTypeOfCol(2) == gdal.GFT_String
+            assert got_rat.GetTypeOfCol(3) == gdal.GFT_Boolean
+            assert got_rat.GetTypeOfCol(4) == gdal.GFT_DateTime
+            assert got_rat.GetTypeOfCol(5) == gdal.GFT_WKBGeometry
+            assert got_rat.GetValueAsInt(0, 0) == 123
+            assert got_rat.GetValueAsDouble(0, 1) == 45.5
+            assert got_rat.GetValueAsString(0, 2) == "str"
+            assert got_rat.GetValueAsBoolean(0, 3)
+            assert got_rat.GetValueAsString(0, 4) == "2025-12-31T23:58:59.500+01:15"
+            assert got_rat.GetValueAsString(0, 5) == "POINT (1 2)"

frmts/gtiff/gtiffdataset_read.cpp
@@ -5444,9 +5444,10 @@ CPLErr GTiffDataset::OpenOffset(TIFF *hTIFFIn, toff_t nDirOffsetIn,
 
     if (TIFFGetField(m_hTIFF, TIFFTAG_GDAL_METADATA, &pszText))
     {
-        CPLXMLNode *psRoot = CPLParseXMLString(pszText);
+        auto psRoot = CPLXMLTreeCloser(CPLParseXMLString(pszText));
         const CPLXMLNode *psItem =
-            psRoot ? CPLGetXMLNode(psRoot, "=GDALMetadata") : nullptr;
+            psRoot.get() ? CPLGetXMLNode(psRoot.get(), "=GDALMetadata")
+                         : nullptr;
         if (psItem)
             psItem = psItem->psChild;
         bool bMaxZErrorFound = false;
@@ -5461,11 +5462,36 @@ CPLErr GTiffDataset::OpenOffset(TIFF *hTIFFIn, toff_t nDirOffsetIn,
             const char *pszKey = CPLGetXMLValue(psItem, "name", nullptr);
             const char *pszValue = CPLGetXMLValue(psItem, nullptr, nullptr);
             int nBand = atoi(CPLGetXMLValue(psItem, "sample", "-1"));
-            if (nBand < -1 || nBand > 65535)
+            if (nBand < -1 || nBand > nBands - 1)
                 continue;
             nBand++;
             const char *pszRole = CPLGetXMLValue(psItem, "role", "");
             const char *pszDomain = CPLGetXMLValue(psItem, "domain", "");
+
+            if (nBand > 0 && pszKey &&
+                strcmp(pszKey, DEFAULT_RASTER_ATTRIBUTE_TABLE) == 0 &&
+                strcmp(pszRole, RAT_ROLE) == 0)
+            {
+                const CPLXMLNode *psValueNode = psItem->psChild;
+                while (psValueNode && psValueNode->eType != CXT_Element)
+                    psValueNode = psValueNode->psNext;
+                if (psValueNode && psValueNode->eType == CXT_Element)
+                {
+                    auto poRAT =
+                        std::make_unique<GDALDefaultRasterAttributeTable>();
+                    if (poRAT->XMLInit(psValueNode, nullptr) == CE_None)
+                    {
+                        GTiffRasterBand *poBand =
+                            cpl::down_cast<GTiffRasterBand *>(
+                                GetRasterBand(nBand));
+                        if (poBand != nullptr)
+                        {
+                            poBand->m_poRAT = std::move(poRAT);
+                            poBand->m_bRATSet = true;
+                        }
+                    }
+                }
+            }
 
             if (pszKey == nullptr || pszValue == nullptr)
                 continue;
@@ -5666,8 +5692,6 @@ CPLErr GTiffDataset::OpenOffset(TIFF *hTIFFIn, toff_t nDirOffsetIn,
         {
             m_dfMaxZErrorOverview = m_dfMaxZError;
         }
-
-        CPLDestroyXMLNode(psRoot);
     }
 
     if (m_bStreamingIn)

frmts/gtiff/gtiffdataset_write.cpp
@@ -4067,39 +4067,45 @@ void GTiffDataset::WriteGeoTIFFInfo()
 
 static void AppendMetadataItem(CPLXMLNode **ppsRoot, CPLXMLNode **ppsTail,
                                const char *pszKey, const char *pszValue,
-                               int nBand, const char *pszRole,
-                               const char *pszDomain)
+                               CPLXMLNode *psValueNode, int nBand,
+                               const char *pszRole, const char *pszDomain)
 
 {
+    CPLAssert(pszValue || psValueNode);
+    CPLAssert(!(pszValue && psValueNode));
+
     /* -------------------------------------------------------------------- */
     /*      Create the Item element, and subcomponents.                     */
     /* -------------------------------------------------------------------- */
     CPLXMLNode *psItem = CPLCreateXMLNode(nullptr, CXT_Element, "Item");
-    CPLCreateXMLNode(CPLCreateXMLNode(psItem, CXT_Attribute, "name"), CXT_Text,
-                     pszKey);
+    CPLAddXMLAttributeAndValue(psItem, "name", pszKey);
 
     if (nBand > 0)
     {
         char szBandId[32] = {};
         snprintf(szBandId, sizeof(szBandId), "%d", nBand - 1);
-        CPLCreateXMLNode(CPLCreateXMLNode(psItem, CXT_Attribute, "sample"),
-                         CXT_Text, szBandId);
+        CPLAddXMLAttributeAndValue(psItem, "sample", szBandId);
     }
 
     if (pszRole != nullptr)
-        CPLCreateXMLNode(CPLCreateXMLNode(psItem, CXT_Attribute, "role"),
-                         CXT_Text, pszRole);
+        CPLAddXMLAttributeAndValue(psItem, "role", pszRole);
 
     if (pszDomain != nullptr && strlen(pszDomain) > 0)
-        CPLCreateXMLNode(CPLCreateXMLNode(psItem, CXT_Attribute, "domain"),
-                         CXT_Text, pszDomain);
+        CPLAddXMLAttributeAndValue(psItem, "domain", pszDomain);
 
-    // Note: this escaping should not normally be done, as the serialization
-    // of the tree to XML also does it, so we end up width double XML escaping,
-    // but keep it for backward compatibility.
-    char *pszEscapedItemValue = CPLEscapeString(pszValue, -1, CPLES_XML);
-    CPLCreateXMLNode(psItem, CXT_Text, pszEscapedItemValue);
-    CPLFree(pszEscapedItemValue);
+    if (pszValue)
+    {
+        // Note: this escaping should not normally be done, as the serialization
+        // of the tree to XML also does it, so we end up width double XML escaping,
+        // but keep it for backward compatibility.
+        char *pszEscapedItemValue = CPLEscapeString(pszValue, -1, CPLES_XML);
+        CPLCreateXMLNode(psItem, CXT_Text, pszEscapedItemValue);
+        CPLFree(pszEscapedItemValue);
+    }
+    else
+    {
+        CPLAddXMLChild(psItem, psValueNode);
+    }
 
     /* -------------------------------------------------------------------- */
     /*      Create root, if missing.                                        */
@@ -4117,6 +4123,20 @@ static void AppendMetadataItem(CPLXMLNode **ppsRoot, CPLXMLNode **ppsTail,
         CPLAddXMLSibling(*ppsTail, psItem);
 
     *ppsTail = psItem;
+}
+
+/************************************************************************/
+/*                         AppendMetadataItem()                         */
+/************************************************************************/
+
+static void AppendMetadataItem(CPLXMLNode **ppsRoot, CPLXMLNode **ppsTail,
+                               const char *pszKey, const char *pszValue,
+                               int nBand, const char *pszRole,
+                               const char *pszDomain)
+
+{
+    AppendMetadataItem(ppsRoot, ppsTail, pszKey, pszValue, nullptr, nBand,
+                       pszRole, pszDomain);
 }
 
 /************************************************************************/
@@ -4714,6 +4734,25 @@ bool GTiffDataset::WriteMetadata(GDALDataset *poSrcDS, TIFF *l_hTIFF,
                 nullptr, "IMAGE_STRUCTURE");
         }
 #endif
+    }
+
+    if (!CPLTestBool(CPLGetConfigOption("GTIFF_WRITE_RAT_TO_PAM", "NO")))
+    {
+        for (int nBand = 1; nBand <= poSrcDS->GetRasterCount(); ++nBand)
+        {
+            GDALRasterBand *poBand = poSrcDS->GetRasterBand(nBand);
+            const auto poRAT = poBand->GetDefaultRAT();
+            if (poRAT)
+            {
+                auto psSerializedRAT = poRAT->Serialize();
+                if (psSerializedRAT)
+                {
+                    AppendMetadataItem(
+                        &psRoot, &psTail, DEFAULT_RASTER_ATTRIBUTE_TABLE,
+                        nullptr, psSerializedRAT, nBand, RAT_ROLE, nullptr);
+                }
+            }
+        }
     }
 
     /* -------------------------------------------------------------------- */

frmts/gtiff/gtiffdataset.h
@@ -448,6 +448,10 @@ class GTiffDataset final : public GDALPamDataset
 
     static GTIF *GTIFNew(TIFF *hTIFF);
 
+    static constexpr const char *DEFAULT_RASTER_ATTRIBUTE_TABLE =
+        "DEFAULT_RASTER_ATTRIBUTE_TABLE";
+    static constexpr const char *RAT_ROLE = "rat";
+
   protected:
     int CloseDependentDatasets() override;
 

frmts/gtiff/gtiffrasterband.h
@@ -42,6 +42,8 @@ class GTiffRasterBand CPL_NON_FINAL : public GDALPamRasterBand
     GDALColorInterp m_eBandInterp = GCI_Undefined;
     std::set<GTiffRasterBand **> m_aSetPSelf{};
     bool m_bHaveOffsetScale = false;
+    bool m_bRATSet = false;
+    bool m_bRATTriedReadingFromPAM = false;
     std::unique_ptr<GDALRasterAttributeTable> m_poRAT{};
 
     int DirectIO(GDALRWFlag eRWFlag, int nXOff, int nYOff, int nXSize,

frmts/gtiff/gtiffrasterband_read.cpp
@@ -35,14 +35,30 @@
 GDALRasterAttributeTable *GTiffRasterBand::GetDefaultRAT()
 
 {
-    if (m_poRAT || m_poGDS->m_poBaseDS != nullptr)
+    if (m_poGDS->m_poBaseDS != nullptr)
         return m_poRAT.get();
 
     m_poGDS->LoadGeoreferencingAndPamIfNeeded();
-    auto poRAT = GDALPamRasterBand::GetDefaultRAT();
-    if (poRAT)
-        return poRAT;
 
+    // RAT from PAM has priority over RAT in GDAL_METADATA TIFF tag
+    if (!m_bRATTriedReadingFromPAM)
+    {
+        m_bRATTriedReadingFromPAM = true;
+        auto poRAT = GDALPamRasterBand::GetDefaultRAT();
+        if (poRAT)
+        {
+            m_bRATSet = true;
+            m_poRAT.reset(poRAT->Clone());
+            return m_poRAT.get();
+        }
+    }
+
+    if (m_bRATSet)
+        return m_poRAT.get();
+
+    m_bRATSet = true;
+
+    // Try reading from a .vat.dbf side car file
     if (!GDALCanFileAcceptSidecarFile(m_poGDS->m_osFilename.c_str()))
         return nullptr;
     const std::string osVATDBF = m_poGDS->m_osFilename + ".vat.dbf";

frmts/gtiff/gtiffrasterband_write.cpp
@@ -31,7 +31,21 @@
 CPLErr GTiffRasterBand::SetDefaultRAT(const GDALRasterAttributeTable *poRAT)
 {
     m_poGDS->LoadGeoreferencingAndPamIfNeeded();
-    return GDALPamRasterBand::SetDefaultRAT(poRAT);
+    m_bRATSet = true;
+    m_bRATTriedReadingFromPAM = true;
+    if (poRAT)
+        m_poRAT.reset(poRAT->Clone());
+    else
+        m_poRAT.reset();
+    const bool bWriteToPAM =
+        CPLTestBool(CPLGetConfigOption("GTIFF_WRITE_RAT_TO_PAM", "NO"));
+    if (!bWriteToPAM)
+        m_poGDS->m_bMetadataChanged = true;
+    if (bWriteToPAM || GDALPamRasterBand::GetDefaultRAT())
+    {
+        return GDALPamRasterBand::SetDefaultRAT(poRAT);
+    }
+    return CE_None;
 }
 
 /************************************************************************/

port/cpl_known_config_options.h
@@ -541,6 +541,7 @@ constexpr static const char* const apszKnownConfigOptions[] =
    "GTIFF_VIRTUAL_MEM_IO", // from gtiffdataset.cpp
    "GTIFF_WRITE_ANGULAR_PARAMS_IN_DEGREE", // from gt_wkt_srs.cpp
    "GTIFF_WRITE_IMAGE_STRUCTURE_METADATA", // from gtiffdataset_write.cpp
+   "GTIFF_WRITE_RAT_TO_PAM", // from gtiffdataset_write.cpp, gtiffrasterband_write.cpp
    "GTIFF_WRITE_TOWGS84", // from gt_wkt_srs.cpp
    "GXF_DATATYPE", // from gxfdataset.cpp
    "HDF4_BLOCK_PIXELS", // from hdf4imagedataset.cpp

commit 09209dd0ca1f7348e6b50807874b3f9c8a5165a0

GTiff: dataset creation: do not trigger output directory listing if GDAL_DISABLE_READDIR_ON_OPEN=TRUE
Fixes a 3.12.0 regression caused by https://github.com/OSGeo/gdal/commit/7868b1c08f6c500e8b34885cc822452922966df4

Fixes #13930

autotest/gcore/tiff_write.py
@@ -12536,6 +12536,11 @@ def test_tiff_write_rat(tmp_vsimem, GTIFF_WRITE_RAT_TO_PAM):
         else:
             assert gdal.VSIStatL(str(filename2) + ".aux.xml") is None
 
+        # Test that RAT gets re-written when modifying another compoonent of
+        # the GDAL_METADATA tag without re-requesting the RAT.
+        with gdal.Open(filename2, gdal.GA_Update) as ds:
+            ds.SetMetadataItem("foo", "bar")
+
         with gdal.Open(filename2) as ds:
             got_rat = ds.GetRasterBand(1).GetDefaultRAT()
             assert ds.GetRasterBand(1).GetDefaultRAT()  # do it again
@@ -12627,3 +12632,47 @@ def test_tiff_cog_layout(tmp_vsimem):
         )
     with gdal.Open(tmp_vsimem / "out.tif") as ds:
         assert ds.GetMetadataItem("LAYOUT", "IMAGE_STRUCTURE") == "COG"
+
+
+###############################################################################
+
+
+def test_tiff_no_get_sibling_files(tmp_vsimem):
+
+    if "DEBUG=YES" not in gdal.VersionInfo("BUILD_INFO"):
+        pytest.skip("Only compatible of a GDAL build with -DDEBUG")
+
+    res = [False]
+
+    def handler(lvl, no, msg):
+        if "file listing" in msg:
+            res[0] = True
+
+    with gdal.config_option("CPL_DEBUG", "GTiff"):
+        with gdaltest.error_handler(handler):
+            gdal.GetDriverByName("GTiff").Create(tmp_vsimem / "foo.tif", 1, 1)
+    assert not res[0]
+
+    with gdal.config_options(
+        {"CPL_DEBUG": "GTiff", "GDAL_DISABLE_READDIR_ON_OPEN": "YES"}
+    ):
+        with gdaltest.error_handler(handler):
+            with gdal.Open(tmp_vsimem / "foo.tif") as ds:
+                ds.GetRasterBand(1).GetDefaultRAT()
+    assert not res[0]
+
+    with gdal.config_options(
+        {"CPL_DEBUG": "GTiff", "GDAL_DISABLE_READDIR_ON_OPEN": "EMPTY_DIR"}
+    ):
+        with gdaltest.error_handler(handler):
+            with gdal.Open(tmp_vsimem / "foo.tif") as ds:
+                ds.GetRasterBand(1).GetDefaultRAT()
+    assert not res[0]
+
+    with gdal.config_options(
+        {"CPL_DEBUG": "GTiff", "GDAL_DISABLE_READDIR_ON_OPEN": "NO"}
+    ):
+        with gdaltest.error_handler(handler):
+            with gdal.Open(tmp_vsimem / "foo.tif") as ds:
+                ds.GetRasterBand(1).GetDefaultRAT()
+    assert res[0]

frmts/gtiff/gtiffdataset_read.cpp
@@ -3942,8 +3942,7 @@ GDALDataset *GTiffDataset::Open(GDALOpenInfo *poOpenInfo)
     }
 
     // In the case of GDAL_DISABLE_READDIR_ON_OPEN = NO / EMPTY_DIR
-    if (poOpenInfo->AreSiblingFilesLoaded() &&
-        CSLCount(poOpenInfo->GetSiblingFiles()) <= 1)
+    if (poOpenInfo->AreSiblingFilesLoaded())
     {
         poDS->oOvManager.TransferSiblingFiles(
             CSLDuplicate(poOpenInfo->GetSiblingFiles()));
@@ -5831,6 +5830,7 @@ CSLConstList GTiffDataset::GetSiblingFiles()
     const int nMaxFiles =
         atoi(CPLGetConfigOption("GDAL_READDIR_LIMIT_ON_OPEN", "1000"));
     const std::string osDirname = CPLGetDirnameSafe(m_osFilename.c_str());
+    CPLDebugOnly("GTiff", "Doing '%s' file listing", osDirname.c_str());
     CPLStringList aosSiblingFiles(VSIReadDirEx(osDirname.c_str(), nMaxFiles));
     if (nMaxFiles > 0 && aosSiblingFiles.size() > nMaxFiles)
     {

frmts/gtiff/gtiffdataset_write.cpp
@@ -4701,8 +4701,21 @@ bool GTiffDataset::WriteMetadata(GDALDataset *poSrcDS, TIFF *l_hTIFF,
     {
         for (int nBand = 1; nBand <= poSrcDS->GetRasterCount(); ++nBand)
         {
-            GDALRasterBand *poBand = poSrcDS->GetRasterBand(nBand);
-            const auto poRAT = poBand->GetDefaultRAT();
+            GDALRasterAttributeTable *poRAT = nullptr;
+            if (poSrcDSGTiff)
+            {
+                auto poBand = cpl::down_cast<GTiffRasterBand *>(
+                    poSrcDSGTiff->GetRasterBand(nBand));
+                // Scenario of https://github.com/OSGeo/gdal/issues/13930
+                // Do not try to fetch the RAT from auxiliary files if creating
+                // a new GeoTIFF file
+                if (poBand->m_bRATSet)
+                    poRAT = poBand->GetDefaultRAT();
+            }
+            else
+            {
+                poRAT = poSrcDS->GetRasterBand(nBand)->GetDefaultRAT();
+            }
             if (poRAT)
             {
                 auto psSerializedRAT = poRAT->Serialize();
@@ -6798,6 +6811,12 @@ GDALDataset *GTiffDataset::Create(const char *pszFilename, int nXSize,
     poDS->nRasterXSize = nXSize;
     poDS->nRasterYSize = nYSize;
     poDS->eAccess = GA_Update;
+
+    // This will avoid GTiffDataset::GetSiblingFiles() to trigger a directory
+    // listing, which is potentially costly and only makes sense when opening
+    // new files, not creating new ones. Helps for scenario like
+    // https://github.com/OSGeo/gdal/issues/13930
+    poDS->m_bHasGotSiblingFiles = true;
 
     poDS->m_nColorTableMultiplier = nColorTableMultiplier;
 

gcore/gdal_misc.cpp
@@ -2911,7 +2911,9 @@ const char *CPL_STDCALL GDALVersionInfo(const char *pszRequest)
 #ifdef USE_ONLY_EMBEDDED_RESOURCE_FILES
         osBuildInfo += "USE_ONLY_EMBEDDED_RESOURCE_FILES=YES\n";
 #endif
-
+#ifdef DEBUG
+        osBuildInfo += "DEBUG=YES\n";
+#endif
 #undef STRINGIFY_HELPER
 #undef STRINGIFY
 