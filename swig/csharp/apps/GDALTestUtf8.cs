c1dca414e61b71f13e319d99ca9d88316aeb83fa
Michael Bucari-Tovo <mbucari1@gmail.com>
2026-03-05 15:56:44 -0700

    Add additional support for UTF-8 strings
    The following Gdal functions now accept UTF-8 strings for their `pszDefault` parameter and will return UTF-8 strings.
    - GetConfigOption
    - GetGlobalConfigOption
    - GetThreadLocalConfigOption
    - GetCredential
    - GetPathSpecificOption
    
    The following Gdal functions now accept UTF-8 strings for their `pszValue` parameter.
    - SetConfigOption
    - SetThreadLocalConfigOption
    - SetCredential
    - SetPathSpecificOption
    
    Changed the C# `utf8_path` typemap to use the built-in UTF-8 encoding marshaller.
    
    Added tests

---
+++ swig/csharp/apps/GDALTestUtf8.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using OSGeo.GDAL;
+
+namespace testapp
+{
+    class GdalTestUtf8
+    {
+        const string ConfigKey = "UNICODE_STRING";
+        const string UnicodeString = "Ĥĕļĺō Ŵŏŕľď";
+		
+        public static void Main()
+        {
+            Gdal.AllRegister();
+
+            Gdal.SetConfigOption(ConfigKey, UnicodeString);
+            var configValue = Gdal.GetConfigOption(ConfigKey, string.Empty);
+            AssertEqual(UnicodeString, configValue, nameof(Gdal.GetConfigOption));
+
+            Gdal.SetThreadLocalConfigOption(ConfigKey, UnicodeString);
+            configValue = Gdal.GetThreadLocalConfigOption(ConfigKey, string.Empty);
+            AssertEqual(UnicodeString, configValue, nameof(Gdal.GetThreadLocalConfigOption));
+
+            Gdal.SetPathSpecificOption("/", ConfigKey, UnicodeString);
+            configValue = Gdal.GetPathSpecificOption("/", ConfigKey, string.Empty);
+            AssertEqual(UnicodeString, configValue, nameof(Gdal.GetPathSpecificOption));
+
+            Gdal.SetCredential("/", ConfigKey, UnicodeString);
+            configValue = Gdal.GetCredential("/", ConfigKey, string.Empty);
+            AssertEqual(UnicodeString, configValue, nameof(Gdal.GetCredential));
+        }
+
+        private static void AssertEqual(string expected, string actual, string funcName)
+        {
+            if (expected == actual)
+            {
+                Console.WriteLine($"{funcName} successfully returned the expected test string.");
+            }
+            else
+            {
+                throw new Exception($"Expected '{expected}' but {funcName} returned '{actual ?? "NULL"}'");
+            }
+        }
+    }
+}
