997b6a37e204861d6ac0fa7d017c14fcb82fe874
Mbucari <37587114+Mbucari@users.noreply.github.com>
2026-04-16 21:44:21 -0700

    Add support for file sizes larger than 2GB
    - Adjust FileFromMemBuffer and VSIF* function definitions to work with buffers and files larger than 2GB.
    - Add FileFromMemBufferNoCopy method to create an in-memory file from a managed buffer. Returns a SafeHandle to the memory file, which is released on disposal.
    - Add helper overloads to VSIF* funcitons
    - Use Int64 for GetCacheMax(), GetCacheUsed(), and SetCacheMax()
    - Add tests

--- swig/csharp/apps/VSIMem.cs
+++ swig/csharp/apps/VSIMem.cs
@@ -51,15 +51,47 @@ class VSIMem
 
         byte[] imageBuffer;
 
-        using (FileStream fs = new FileStream(args[0], FileMode.Open, FileAccess.Read))
+        try
         {
-            using (BinaryReader br = new BinaryReader(fs))
+            IntPtr vfInput = Gdal.VSIFOpenL(args[0], "r");
+            if (vfInput == IntPtr.Zero)
             {
-                long numBytes = new FileInfo(args[0]).Length;
-                imageBuffer = br.ReadBytes((int)numBytes);
-                br.Close();
-                fs.Close();
+                Console.WriteLine($"Failed to {nameof(Gdal.VSIFOpenL)} input file");
+                System.Environment.Exit(-1);
             }
+            if (!Gdal.VSIFSeekL(vfInput, 0, SeekOrigin.End))
+            {
+                Console.WriteLine($"{nameof(Gdal.VSIFSeekL)} failed to seek to end of input file");
+                System.Environment.Exit(-1);
+            }
+            long vfInputLength = Gdal.VSIFTellL(vfInput);
+            if (vfInputLength <= 0)
+            {
+                Console.WriteLine($"{nameof(Gdal.VSIFTellL)} reports incorrect location");
+                System.Environment.Exit(-1);
+            }
+            if (!Gdal.VSIFSeekL(vfInput, 0, SeekOrigin.Begin) || Gdal.VSIFTellL(vfInput) != 0)
+            {
+                Console.WriteLine($"{nameof(Gdal.VSIFSeekL)} failed to seek to beginning of input file");
+                System.Environment.Exit(-1);
+            }
+
+            imageBuffer = new byte[vfInputLength];
+            if (Gdal.VSIFReadL(imageBuffer, 0, imageBuffer.Length, vfInput) != imageBuffer.Length)
+            {
+                Console.WriteLine($"Failed to {nameof(Gdal.VSIFReadL)} from input file");
+                System.Environment.Exit(-1);
+            }
+            if (Gdal.VSIFCloseL(vfInput) != 0)
+            {
+                Console.WriteLine($"Failed to {nameof(Gdal.VSIFCloseL)} input file");
+                System.Environment.Exit(-1);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
         }
 
         Gdal.AllRegister();
@@ -67,36 +99,40 @@ class VSIMem
         string memFilename = "/vsimem/inmemfile";
         try
         {
-            Gdal.FileFromMemBuffer(memFilename, imageBuffer);
-            using (Dataset ds = Gdal.Open(memFilename, Access.GA_ReadOnly))
+            using (var file = Gdal.FileFromMemBufferNoCopy(memFilename, imageBuffer, 0, imageBuffer.Length))
             {
-                if (ds == null)
+                if (file == null)
                 {
-                    Console.WriteLine("Can't open in-memory file.");
+                    Console.WriteLine("Failed to open in-memory file.");
                     System.Environment.Exit(-1);
                 }
-                Console.WriteLine("Raster dataset parameters:");
-                Console.WriteLine("  RasterCount: " + ds.RasterCount);
-                Console.WriteLine("  RasterSize (" + ds.RasterXSize + "," + ds.RasterYSize + ")");
-
-                Driver drv = Gdal.GetDriverByName("GTiff");
-
-                if (drv == null)
+                using (Dataset ds = Gdal.Open(memFilename, Access.GA_ReadOnly))
                 {
-                    Console.WriteLine("Can't get driver.");
-                    System.Environment.Exit(-1);
-                }
+                    if (ds == null)
+                    {
+                        Console.WriteLine("Can't open in-memory file.");
+                        System.Environment.Exit(-1);
+                    }
+                    Console.WriteLine("Raster dataset parameters:");
+                    Console.WriteLine("  RasterCount: " + ds.RasterCount);
+                    Console.WriteLine("  RasterSize (" + ds.RasterXSize + "," + ds.RasterYSize + ")");
 
-                drv.CreateCopy("sample.tif", ds, 0, null, null, null);
+                    Driver drv = Gdal.GetDriverByName("GTiff");
+
+                    if (drv == null)
+                    {
+                        Console.WriteLine("Can't get driver.");
+                        System.Environment.Exit(-1);
+                    }
+
+                    drv.CreateCopy("sample.tif", ds, 0, null, null, null).Dispose();
+                }
             }
         }
         catch (Exception ex)
         {
             Console.WriteLine(ex.Message);
-        }
-        finally
-        {
-            Gdal.Unlink(memFilename);
+            throw;
         }
     }
 }
