470ea42dd556d8f1e7ae1c4aa837419638e5781b
Even Rouault <even.rouault@spatialys.com>
2026-05-14 15:56:33 +0200

    S101: test Date and Time fields

--- autotest/ogr/ogr_s101_informationtype.py
+++ autotest/ogr/ogr_s101_informationtype.py
@@ -582,3 +582,52 @@ def test_ogr_s101_read_information_type_alter_enumerated_value(tmp_path):
         "lightSector.sectorInformation[2].text": "mon texte",
         "lightSector.colour": [1, 987654],
     }
+
+
+###############################################################################
+# Test Date field
+
+
+def test_ogr_s101_read_information_type_date_field():
+
+    with gdaltest.error_raised(gdal.CE_None):
+        ds = ogr.Open("data/s101/information_type_date.000")
+
+    lyr = ds.GetLayerByName("informationType")
+    f = lyr.GetNextFeature()
+    assert f.ExportToJson(as_object=True) == {
+        "type": "Feature",
+        "geometry": None,
+        "properties": {
+            "recordId": 1,
+            "recordVersion": 1,
+            "informationType": "ContactDetails",
+            "fixedDateRange.dateStart": "2026/01/01",
+            "fixedDateRange.dateEnd": "2026/12/31",
+        },
+        "id": 1,
+    }
+
+
+###############################################################################
+# Test Time field
+
+
+def test_ogr_s101_read_information_type_time_field():
+
+    with gdaltest.error_raised(gdal.CE_None):
+        ds = ogr.Open("data/s101/information_type_time.000")
+
+    lyr = ds.GetLayerByName("informationType")
+    f = lyr.GetNextFeature()
+    assert f.ExportToJson(as_object=True) == {
+        "type": "Feature",
+        "geometry": None,
+        "properties": {
+            "recordId": 1,
+            "recordVersion": 1,
+            "informationType": "ServiceHours",
+            "fixedDateRange.timeIntervalsByDayOfWeek.timeOfDayStart": "12:34:56",
+        },
+        "id": 1,
+    }
