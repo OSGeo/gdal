d155daeaa9e01b40f4f3fa3e927d52460cf837ef
Lucian Plesea <LPlesea@esri.com>
2026-05-26 15:31:14 -0700

    Update marfa.h

--- frmts/mrf/marfa.h
+++ frmts/mrf/marfa.h
@@ -594,7 +594,7 @@ class MRFDataset final : public GDALPamDataset
     // A small int actually due to GDAL limitations
     double scale;
 
-    void *pbuffer;    // A place to keep an uncompressed block
+    void *pbuffer;  // A place to keep an uncompressed block
     unsigned int pbsize;
     ILSize tile;      // ID of tile present in buffer
     GUIntBig bdirty;  // used in pixel interleaved (up to 64 bands)
