548f1a86e2c32deaa1d85202e3adacc3488642d9
Even Rouault <even.rouault@spatialys.com>
2026-06-06 19:10:36 +0200

    Azure/ADLS GetOptions() XML: document missing AZURE_STORAGE_ACCESS_TOKEN and AZURE_STORAGE_SAS_TOKEN
    Fixes https://github.com/qgis/QGIS/issues/66274

--- port/cpl_azure.cpp
+++ port/cpl_azure.cpp
@@ -34,6 +34,7 @@
 #include "cpl_http.h"
 #include "cpl_multiproc.h"
 #include "cpl_vsi_virtual.h"
+#include "cpl_vsil_curl_class.h"
 
 #include <mutex>
 
@@ -1311,6 +1312,36 @@ std::string VSIAzureBlobHandleHelper::GetSignedURL(CSLConstList papszOptions)
     if (!osSignedIdentifier.empty())
         AddQueryParameter("si", osSignedIdentifier);
     return m_osURL;
+}
+
+/************************************************************************/
+/*                             GetOptions()                             */
+/************************************************************************/
+
+/* static */
+const char *VSIAzureBlobHandleHelper::GetOptions()
+{
+    static std::string osOptions(
+        std::string("<Options>") +
+        "  <Option name='AZURE_STORAGE_CONNECTION_STRING' type='string' "
+        "description='Connection string that contains account name and "
+        "secret key'/>"
+        "  <Option name='AZURE_STORAGE_ACCOUNT' type='string' "
+        "description='Storage account. To use with AZURE_STORAGE_ACCESS_KEY'/>"
+        "  <Option name='AZURE_STORAGE_ACCESS_KEY' type='string' "
+        "description='Secret key'/>"
+        "  <Option name='AZURE_STORAGE_ACCESS_TOKEN' type='string' "
+        "description='Access token typically obtained using Microsoft "
+        "Authentication Library (MSAL).'/>"
+        "  <Option name='AZURE_STORAGE_SAS_TOKEN' type='string' "
+        "description='Shared Access Signature'/>"
+        "  <Option name='AZURE_NO_SIGN_REQUEST' type='boolean' "
+        "description='Whether to disable signing of requests' default='NO'/>"
+        "  <Option name='VSIAZ_CHUNK_SIZE' type='int' "
+        "description='Size in MB for chunks of files that are uploaded' "
+        "default='4' min='1' max='4'/>" +
+        cpl::VSICurlFilesystemHandlerBase::GetOptionsStatic() + "</Options>");
+    return osOptions.c_str();
 }
 
 #endif  // HAVE_CURL
