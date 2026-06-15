37006b700d1ec84d1875689daa833d8001e8b538
Even Rouault <even.rouault@spatialys.com>
2026-06-15 10:38:08 +0200

    Fix -Werror=null-dereference on build-windows-msys2-mingw with gcc 16.1

--- apps/gdalalg_vector_rename_layer.cpp
+++ apps/gdalalg_vector_rename_layer.cpp
@@ -301,9 +301,8 @@ bool GDALVectorRenameLayerAlgorithm::RunStep(GDALPipelineStepRunContext &)
     std::map<std::string, int> oMapCountNames;
     bool bNonUniqueNames = false;
     const int nLayerCount = poSrcDS->GetLayerCount();
-    for (int i = 0; i < nLayerCount; ++i)
+    for (const OGRLayer *poSrcLayer : poSrcDS->GetLayers())
     {
-        const OGRLayer *poSrcLayer = poSrcDS->GetLayer(i);
         if ((m_inputLayerName == poSrcLayer->GetDescription() ||
              nLayerCount == 1) &&
             !m_outputLayerName.empty())
@@ -409,9 +408,9 @@ bool GDALVectorRenameLayerAlgorithm::RunStep(GDALPipelineStepRunContext &)
         *poSrcDS, aosNames);
 
     // Final pass to create output layers
-    for (int i = 0; i < nLayerCount; ++i)
+    size_t i = 0;
+    for (OGRLayer *poSrcLayer : poSrcDS->GetLayers())
     {
-        OGRLayer *poSrcLayer = poSrcDS->GetLayer(i);
         if (poSrcLayer->GetDescription() != aosNames[i])
         {
             auto poLayer =
@@ -426,6 +425,7 @@ bool GDALVectorRenameLayerAlgorithm::RunStep(GDALPipelineStepRunContext &)
                 std::make_unique<GDALVectorPipelinePassthroughLayer>(
                     *poSrcLayer));
         }
+        ++i;
     }
 
     m_outputDataset.Set(std::move(outDS));
