f8cbd9d82ca02abdd4f6162b023edb4ce06b11f6
tmoore <tmoore@spatial.ca>
2026-08-14 12:20:47 -0400

    Add javadoc strings

--- swig/java/javadoc.java
+++ swig/java/javadoc.java
@@ -10405,6 +10405,33 @@ public class SpatialReference:public double[] GetTOWGS84()
 public class SpatialReference:public int GetTOWGS84(double[] argout)
 
 /**
+ * Try to identify a match between this SRS and a related SRS in a catalog.
+ * <p> 
+ * Matching may be partial, or may fail. Returned entries will be sorted by
+ * decreasing match confidence (first entry has the highest match confidence).
+ * <p>
+ * The exact way matching is done may change in future versions. 
+ * Starting with GDAL 3.0, it relies on PROJ' proj_identify() function.
+ * <p>
+ * The confidence_values array structure will contain output values.  The
+ * outer dimension of the array is the 'box', and the method will allocated
+ * the inner dimension to contain the array of confidence values.
+ * <p>
+ * Typical usage:
+<pre><code>
+    int[][] confidence = new int[1][];
+    SpatialReference[] matches = srs.FindMatches(null, confidence);
+    int[] confidenceValues = confidence[0];
+</code></pre>	
+ * @param options a vector of options or null
+ * @param confidence_values a box to contain an output array, one for each matching SpatialReference,
+ * whose values between 0 and 100 indicate the confidence in the match. 100 is the highest confidence level.
+ *
+ * @return an array, possibly empty, of SpatialReferences that match this reference.
+ */
+public class SpatialReference:public org.gdal.osr.SpatialReference[] FindMatches(java.util.Vector options, int[][] confidence_values)
+
+/**
  * Initialize SRS based on EPSG GCS or PCS code.
  * <p>
  * This method will initialize the spatial reference based on the
