cbc76eb092ee26edfa223763c2c201949537274c
Even Rouault <even.rouault@spatialys.com>
2026-08-27 18:25:32 +0200

    scripts/setdevenv.bat/.ps1: remove USE_PATH_FOR_GDAL_PYTHON=yes
    Refs #15092

--- scripts/setdevenv.ps1
+++ scripts/setdevenv.ps1
@@ -5,4 +5,3 @@ $env:PATH = "$PWD\swig\python\bin;$PWD\apps\Release;$PWD\Release;" + $env:PATH
 $env:GDAL_DATA = "$PWD\data"
 $env:PYTHONPATH = "$PWD\swig\python"
 $env:GDAL_DRIVER_PATH = "$PWD\gdalplugins\Release"
-$env:USE_PATH_FOR_GDAL_PYTHON = "yes"
