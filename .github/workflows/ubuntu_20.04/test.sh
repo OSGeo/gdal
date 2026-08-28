f0b4d0f8b1dad6d9331b79b0613388b3435a0eb2
Even Rouault <even.rouault@spatialys.com>
2026-08-28 18:24:29 +0200

    autotest/conftest.py: check that tests do not generate, modify or delete files in autotest/

--- .github/workflows/ubuntu_20.04/test.sh
+++ .github/workflows/ubuntu_20.04/test.sh
@@ -4,4 +4,7 @@ set -eu
 
 export OGR_HANA_CONNECTION_STRING='DRIVER=/usr/sap/hdbclient/libodbcHDB.so;HOST=917df316-4e01-4a10-be54-eac1b6ab15fb.hana.prod-us10.hanacloud.ondemand.com;PORT=443;USER=GDALCI;PASSWORD=u7t!Ukeugzq7'
 
+# Because of MrSID and ECW that create temporary files in the current directory
+export CHECK_NO_UNINTENDED_FILES=NO
+
 ctest -V -j $(nproc)
