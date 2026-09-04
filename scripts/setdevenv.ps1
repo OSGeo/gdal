b0b8dcc320a42c56f2c414408cf84a2c6e1560b8
sethg <sethg@geographika.co.uk>
2026-09-04 13:37:33 +0200

    Allow for different build configs

--- scripts/setdevenv.ps1
+++ scripts/setdevenv.ps1
@@ -1,7 +1,11 @@
 # This is a very simple script which must be called from a build
-# directory and sets the environment for a Release build
+# directory and by default sets the environment for a Release build.
 
-$env:PATH = "$PWD\swig\python\bin;$PWD\apps\Release;$PWD\Release;" + $env:PATH
+param(
+    [string]$Configuration = "Release"
+)
+
+$env:PATH = "$PWD\swig\python\bin;$PWD\apps\$Configuration;$PWD\$Configuration;$env:PATH"
 $env:GDAL_DATA = "$PWD\data"
 $env:PYTHONPATH = "$PWD\swig\python"
-$env:GDAL_DRIVER_PATH = "$PWD\gdalplugins\Release"
+$env:GDAL_DRIVER_PATH = "$PWD\gdalplugins\$Configuration"
