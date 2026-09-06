OSGeo/gdal

commit 5f4c397c30461e1c536d571ed4133bb40a3d592f

gdal vsi list/copy/move/sync/delete: display object storage errors
Fixes #12467

apps/gdalalg_vsi_copy.cpp
@@ -15,6 +15,7 @@
 #include "cpl_conv.h"
 #include "cpl_string.h"
 #include "cpl_vsi.h"
+#include "cpl_vsi_error.h"
 
 #include <algorithm>
 
@@ -63,6 +64,23 @@ GDALVSICopyAlgorithm::GDALVSICopyAlgorithm()
 bool GDALVSICopyAlgorithm::RunImpl(GDALProgressFunc pfnProgress,
                                    void *pProgressData)
 {
+    const auto ReportSourceNotAccessible =
+        [this](int nOldErrorNum, int nNewErrorNum, const std::string &filename)
+    {
+        if (nOldErrorNum != nNewErrorNum)
+        {
+            ReportError(CE_Failure, CPLE_FileIO,
+                        "'%s' cannot be accessed. %s: %s", filename.c_str(),
+                        VSIErrorNumToString(nNewErrorNum),
+                        VSIGetLastErrorMsg());
+        }
+        else
+        {
+            ReportError(CE_Failure, CPLE_FileIO, "'%s' cannot be accessed.",
+                        filename.c_str());
+        }
+    };
+
     if (m_recursive || cpl::ends_with(m_source, "/*") ||
         cpl::ends_with(m_source, "\\*"))
     {
@@ -73,17 +91,16 @@ bool GDALVSICopyAlgorithm::RunImpl(GDALProgressFunc pfnProgress,
 
         if (!cpl::ends_with(m_source, "/*") && !cpl::ends_with(m_source, "\\*"))
         {
+            VSIErrorReset();
+            const auto nOldErrorNum = VSIGetLastErrorNo();
             VSIStatBufL statBufSrc;
-            bool srcExists =
-                VSIStatExL(m_source.c_str(), &statBufSrc,
-                           VSI_STAT_EXISTS_FLAG | VSI_STAT_NATURE_FLAG) == 0;
+            bool srcExists = VSIStatL(m_source.c_str(), &statBufSrc) == 0;
             if (!srcExists)
             {
-                srcExists =
-                    VSIStatExL(
-                        std::string(m_source).append("/").c_str(), &statBufSrc,
-                        VSI_STAT_EXISTS_FLAG | VSI_STAT_NATURE_FLAG) == 0;
+                srcExists = VSIStatL(std::string(m_source).append("/").c_str(),
+                                     &statBufSrc) == 0;
             }
+            const auto nNewErrorNum = VSIGetLastErrorNo();
             VSIStatBufL statBufDst;
             const bool dstExists =
                 VSIStatExL(m_destination.c_str(), &statBufDst,
@@ -100,10 +117,25 @@ bool GDALVSICopyAlgorithm::RunImpl(GDALProgressFunc pfnProgress,
                     m_destination = CPLFormFilenameSafe(
                         m_destination.c_str(), m_source.c_str(), nullptr);
             }
+            else if (!srcExists)
+            {
+                ReportSourceNotAccessible(nOldErrorNum, nNewErrorNum, m_source);
+                return false;
+            }
         }
         else
         {
             m_source.resize(m_source.size() - 2);
+            VSIErrorReset();
+            const auto nOldErrorNum = VSIGetLastErrorNo();
+            VSIStatBufL statBufSrc;
+            bool srcExists = VSIStatL(m_source.c_str(), &statBufSrc) == 0;
+            if (!srcExists)
+            {
+                const auto nNewErrorNum = VSIGetLastErrorNo();
+                ReportSourceNotAccessible(nOldErrorNum, nNewErrorNum, m_source);
+                return false;
+            }
         }
 
         uint64_t curAmount = 0;
@@ -113,13 +145,13 @@ bool GDALVSICopyAlgorithm::RunImpl(GDALProgressFunc pfnProgress,
     else
     {
         VSIStatBufL statBufSrc;
-        bool srcExists =
-            VSIStatExL(m_source.c_str(), &statBufSrc,
-                       VSI_STAT_EXISTS_FLAG | VSI_STAT_NATURE_FLAG) == 0;
+        VSIErrorReset();
+        const auto nOldErrorNum = VSIGetLastErrorNo();
+        bool srcExists = VSIStatL(m_source.c_str(), &statBufSrc) == 0;
         if (!srcExists)
         {
-            ReportError(CE_Failure, CPLE_FileIO, "%s does not exist",
-                        m_source.c_str());
+            const auto nNewErrorNum = VSIGetLastErrorNo();
+            ReportSourceNotAccessible(nOldErrorNum, nNewErrorNum, m_source);
             return false;
         }
         if (VSI_ISDIR(statBufSrc.st_mode))

apps/gdalalg_vsi_delete.cpp
@@ -15,6 +15,7 @@
 #include "cpl_conv.h"
 #include "cpl_string.h"
 #include "cpl_vsi.h"
+#include "cpl_vsi_error.h"
 
 #include <algorithm>
 
@@ -52,10 +53,23 @@ bool GDALVSIDeleteAlgorithm::RunImpl(GDALProgressFunc, void *)
 {
     bool ret = false;
     VSIStatBufL sStat;
+    VSIErrorReset();
+    const auto nOldErrorNum = VSIGetLastErrorNo();
     if (VSIStatL(m_filename.c_str(), &sStat) != 0)
     {
-        ReportError(CE_Failure, CPLE_FileIO, "%s does not exist",
-                    m_filename.c_str());
+        if (nOldErrorNum != VSIGetLastErrorNo())
+        {
+            ReportError(CE_Failure, CPLE_FileIO,
+                        "'%s' cannot be accessed. %s: %s", m_filename.c_str(),
+                        VSIErrorNumToString(VSIGetLastErrorNo()),
+                        VSIGetLastErrorMsg());
+        }
+        else
+        {
+            ReportError(CE_Failure, CPLE_FileIO,
+                        "'%s' does not exist or cannot be accessed",
+                        m_filename.c_str());
+        }
     }
     else
     {

apps/gdalalg_vsi_list.cpp
@@ -15,6 +15,7 @@
 #include "cpl_string.h"
 #include "cpl_time.h"
 #include "cpl_vsi.h"
+#include "cpl_vsi_error.h"
 
 #include <cinttypes>
 
@@ -249,10 +250,23 @@ void GDALVSIListAlgorithm::PrintEntry(const VSIDIREntry *entry)
 bool GDALVSIListAlgorithm::RunImpl(GDALProgressFunc, void *)
 {
     VSIStatBufL sStat;
+    VSIErrorReset();
+    const auto nOldErrorNum = VSIGetLastErrorNo();
     if (VSIStatL(m_filename.c_str(), &sStat) != 0)
     {
-        ReportError(CE_Failure, CPLE_FileIO, "'%s' does not exist",
-                    m_filename.c_str());
+        if (nOldErrorNum != VSIGetLastErrorNo())
+        {
+            ReportError(CE_Failure, CPLE_FileIO,
+                        "'%s' cannot be accessed. %s: %s", m_filename.c_str(),
+                        VSIErrorNumToString(VSIGetLastErrorNo()),
+                        VSIGetLastErrorMsg());
+        }
+        else
+        {
+            ReportError(CE_Failure, CPLE_FileIO,
+                        "'%s' does not exist or cannot be accessed",
+                        m_filename.c_str());
+        }
         return false;
     }
 

apps/gdalalg_vsi_sync.cpp
@@ -12,8 +12,9 @@
 
 #include "gdalalg_vsi_sync.h"
 
-#include "cpl_vsi.h"
 #include "cpl_string.h"
+#include "cpl_vsi.h"
+#include "cpl_vsi_error.h"
 
 //! @cond Doxygen_Suppress
 
@@ -72,11 +73,23 @@ bool GDALVSISyncAlgorithm::RunImpl(GDALProgressFunc pfnProgress,
                  pfnProgress, pProgressData, nullptr))
     {
         VSIStatBufL sStat;
-        if (VSIStatExL(m_source.c_str(), &sStat,
-                       VSI_STAT_EXISTS_FLAG | VSI_STAT_NATURE_FLAG) != 0)
+        VSIErrorReset();
+        const auto nOldErrorNum = VSIGetLastErrorNo();
+        if (VSIStatL(m_source.c_str(), &sStat) != 0)
         {
-            ReportError(CE_Failure, CPLE_FileIO, "%s does not exist",
-                        m_source.c_str());
+            if (nOldErrorNum != VSIGetLastErrorNo())
+            {
+                ReportError(CE_Failure, CPLE_FileIO,
+                            "'%s' cannot be accessed. %s: %s", m_source.c_str(),
+                            VSIErrorNumToString(VSIGetLastErrorNo()),
+                            VSIGetLastErrorMsg());
+            }
+            else
+            {
+                ReportError(CE_Failure, CPLE_FileIO,
+                            "'%s' does not exist or cannot be accessed",
+                            m_source.c_str());
+            }
         }
         else
         {

apps/gdalalg_vsi_move.cpp
@@ -13,6 +13,7 @@
 #include "gdalalg_vsi_move.h"
 
 #include "cpl_vsi.h"
+#include "cpl_vsi_error.h"
 
 //! @cond Doxygen_Suppress
 
@@ -58,13 +59,24 @@ bool GDALVSIMoveAlgorithm::RunImpl(GDALProgressFunc pfnProgress,
                 pProgressData) != 0)
     {
         VSIStatBufL statBufSrc;
-        const bool srcExists =
-            VSIStatExL(m_source.c_str(), &statBufSrc,
-                       VSI_STAT_EXISTS_FLAG | VSI_STAT_NATURE_FLAG) == 0;
+        VSIErrorReset();
+        const auto nOldErrorNum = VSIGetLastErrorNo();
+        const bool srcExists = VSIStatL(m_source.c_str(), &statBufSrc) == 0;
         if (!srcExists)
         {
-            ReportError(CE_Failure, CPLE_FileIO, "%s does not exist",
-                        m_source.c_str());
+            if (nOldErrorNum != VSIGetLastErrorNo())
+            {
+                ReportError(CE_Failure, CPLE_FileIO,
+                            "'%s' cannot be accessed. %s: %s", m_source.c_str(),
+                            VSIErrorNumToString(VSIGetLastErrorNo()),
+                            VSIGetLastErrorMsg());
+            }
+            else
+            {
+                ReportError(CE_Failure, CPLE_FileIO,
+                            "'%s' does not exist or cannot be accessed",
+                            m_source.c_str());
+            }
         }
         else
         {

autotest/utilities/test_gdalalg_vsi_copy.py
@@ -59,8 +59,43 @@ def test_gdalalg_vsi_copy_single_source_does_not_exist():
     alg = get_alg()
     alg["source"] = "/i_do/not/exist.bin"
     alg["destination"] = "/vsimem/"
-    with pytest.raises(Exception, match="does not exist"):
+    with pytest.raises(Exception, match="cannot be accessed"):
         alg.Run()
+
+
+@pytest.mark.require_curl()
+def test_gdalalg_vsi_copy_single_source_does_not_exist_vsi():
+
+    with gdal.config_option("OSS_SECRET_ACCESS_KEY", ""):
+        alg = get_alg()
+        alg["source"] = "/vsioss/i_do_not/exist.bin"
+        alg["destination"] = "/vsimem/"
+        with pytest.raises(Exception, match="InvalidCredentials"):
+            alg.Run()
+
+
+@pytest.mark.require_curl()
+def test_gdalalg_vsi_copy_recursive_source_does_not_exist_vsi():
+
+    with gdal.config_option("OSS_SECRET_ACCESS_KEY", ""):
+        alg = get_alg()
+        alg["source"] = "/vsioss/i_do_not/exist.bin"
+        alg["destination"] = "/vsimem/"
+        alg["recursive"] = True
+        with pytest.raises(Exception, match="InvalidCredentials"):
+            alg.Run()
+
+
+@pytest.mark.require_curl()
+def test_gdalalg_vsi_copy_recursive_slash_star_source_does_not_exist_vsi():
+
+    with gdal.config_option("OSS_SECRET_ACCESS_KEY", ""):
+        alg = get_alg()
+        alg["source"] = "/vsioss/i_do_not/exist.bin/*"
+        alg["destination"] = "/vsimem/"
+        alg["recursive"] = True
+        with pytest.raises(Exception, match="InvalidCredentials"):
+            alg.Run()
 
 
 def test_gdalalg_vsi_copy_single_source_is_directory():

autotest/utilities/test_gdalalg_vsi_delete.py
@@ -72,3 +72,13 @@ def test_gdalalg_vsi_delete_dir_recursive(tmp_path):
     assert alg.Run()
 
     assert gdal.VSIStatL(tmp_path / "subdir") is None
+
+
+@pytest.mark.require_curl()
+def test_gdalalg_vsi_delete_source_does_not_exist_vsi():
+
+    with gdal.config_option("OSS_SECRET_ACCESS_KEY", ""):
+        alg = get_alg()
+        alg["filename"] = "/vsioss/i_do_not/exist.bin"
+        with pytest.raises(Exception, match="InvalidCredentials"):
+            alg.Run()

autotest/utilities/test_gdalalg_vsi_list.py
@@ -293,3 +293,13 @@ def test_gdalalg_vsi_list(tmp_vsimem):
     assert alg.Run()
     assert "unknown unknown" in alg["output-string"]
     assert "utmsmall.tif" in alg["output-string"]
+
+
+@pytest.mark.require_curl()
+def test_gdalalg_vsi_list_source_does_not_exist_vsi():
+
+    with gdal.config_option("OSS_SECRET_ACCESS_KEY", ""):
+        alg = get_alg()
+        alg["filename"] = "/vsioss/i_do_not/exist.bin"
+        with pytest.raises(Exception, match="InvalidCredentials"):
+            alg.Run()

commit 4a79126e3a2816d8439c4f32978fb0179b93aaa1

fix(apps): correct error capturing order in VSI algorithms
Call `VSIGetLastErrorNo()` before `VSIErrorReset()` to ensure the existing
error state is captured before it is wiped. Previously, resetting first
prevented detecting if subsequent operations introduced new errors.

This fixes the issue introduced in https://github.com/OSGeo/gdal/commit/5f4c397c30461e1c536d571ed4133bb40a3d592f

apps/gdalalg_vsi_copy.cpp
@@ -102,8 +102,9 @@ bool GDALVSICopyAlgorithm::RunImpl(GDALProgressFunc pfnProgress,
 
         if (!cpl::ends_with(m_source, "/*") && !cpl::ends_with(m_source, "\\*"))
         {
-            VSIErrorReset();
             const auto nOldErrorNum = VSIGetLastErrorNo();
+            VSIErrorReset();
+
             VSIStatBufL statBufSrc;
             bool srcExists = VSIStatL(m_source.c_str(), &statBufSrc) == 0;
             if (!srcExists)
@@ -136,9 +137,10 @@ bool GDALVSICopyAlgorithm::RunImpl(GDALProgressFunc pfnProgress,
         }
         else
         {
-            m_source.resize(m_source.size() - 2);
-            VSIErrorReset();
             const auto nOldErrorNum = VSIGetLastErrorNo();
+            VSIErrorReset();
+
+            m_source.resize(m_source.size() - 2);
             VSIStatBufL statBufSrc;
             bool srcExists = VSIStatL(m_source.c_str(), &statBufSrc) == 0;
             if (!srcExists)
@@ -155,9 +157,10 @@ bool GDALVSICopyAlgorithm::RunImpl(GDALProgressFunc pfnProgress,
     }
     else
     {
-        VSIStatBufL statBufSrc;
-        VSIErrorReset();
         const auto nOldErrorNum = VSIGetLastErrorNo();
+        VSIErrorReset();
+
+        VSIStatBufL statBufSrc;
         bool srcExists = VSIStatL(m_source.c_str(), &statBufSrc) == 0;
         if (!srcExists)
         {

apps/gdalalg_vsi_delete.cpp
@@ -53,10 +53,11 @@ GDALVSIDeleteAlgorithm::GDALVSIDeleteAlgorithm()
 
 bool GDALVSIDeleteAlgorithm::RunImpl(GDALProgressFunc, void *)
 {
+    const auto nOldErrorNum = VSIGetLastErrorNo();
+    VSIErrorReset();
+
     bool ret = false;
     VSIStatBufL sStat;
-    VSIErrorReset();
-    const auto nOldErrorNum = VSIGetLastErrorNo();
     if (VSIStatL(m_filename.c_str(), &sStat) != 0)
     {
         if (nOldErrorNum != VSIGetLastErrorNo())

apps/gdalalg_vsi_list.cpp
@@ -250,9 +250,10 @@ bool GDALVSIListAlgorithm::RunImpl(GDALProgressFunc, void *)
     if (m_format.empty())
         m_format = IsCalledFromCommandLine() ? "text" : "json";
 
-    VSIStatBufL sStat;
-    VSIErrorReset();
     const auto nOldErrorNum = VSIGetLastErrorNo();
+    VSIErrorReset();
+
+    VSIStatBufL sStat;
     if (VSIStatL(m_filename.c_str(), &sStat) != 0)
     {
         if (nOldErrorNum != VSIGetLastErrorNo())

apps/gdalalg_vsi_sync.cpp
@@ -72,9 +72,10 @@ bool GDALVSISyncAlgorithm::RunImpl(GDALProgressFunc pfnProgress,
     if (!VSISync(m_source.c_str(), m_destination.c_str(), aosOptions.List(),
                  pfnProgress, pProgressData, nullptr))
     {
-        VSIStatBufL sStat;
-        VSIErrorReset();
         const auto nOldErrorNum = VSIGetLastErrorNo();
+        VSIErrorReset();
+
+        VSIStatBufL sStat;
         if (VSIStatL(m_source.c_str(), &sStat) != 0)
         {
             if (nOldErrorNum != VSIGetLastErrorNo())